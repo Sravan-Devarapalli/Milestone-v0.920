2bde2c24c0002e3501a5c8606bf43f7608830b9e
RaviN <RaviN>
2011-04-25 03:05:00 -0500

    add from branch - Milestone v0.900 none

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Views/v_PersonCalendar.view.sql
@@ -0,0 +1,17 @@
+﻿-- =============================================
+-- Author:		Anatoliy Lokshin
+-- Create date: 6-13-2008
+-- Updated by:	Anatoliy Lokshin
+-- Update date: 8-12-2008
+-- Description:	Determines the calendar for the persons.
+-- =============================================
+CREATE VIEW [dbo].[v_PersonCalendar]
+AS
+	SELECT cal.Date,
+	       p.[PersonId],
+	       ISNULL(pcal.DayOff, cal.DayOff) AS DayOff,
+	       cal.DayOff AS CompanyDayOff
+	  FROM dbo.Calendar AS cal
+	       INNER JOIN dbo.Person AS p ON cal.Date >= p.HireDate AND cal.Date < ISNULL(p.TerminationDate, dbo.GetFutureDate())
+	       LEFT JOIN dbo.PersonCalendar AS pcal ON pcal.Date = cal.Date AND pcal.PersonId = p.PersonId
+
