476ce76c40caf38a879a8dc1f32e5e808511bd21
RaviN <RaviN>
2011-04-25 03:05:00 -0500

    add from branch - Milestone v0.900 none

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/ProjectExpenseUpdate.proc.sql
@@ -0,0 +1,33 @@
+﻿-- =============================================
+-- Author:		Nikita Goncharenko
+-- Create date: 2010-04-20
+-- =============================================
+CREATE PROCEDURE dbo.ProjectExpenseUpdate 
+    @ExpenseId int,
+    @ExpenseName nvarchar(50),
+    @ExpenseAmount decimal(18, 2),
+    @ExpenseReimbursement decimal(18, 2),
+	@MilestoneId int = null 
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	if (@MilestoneId is NULL)
+	begin
+		Update [dbo].[ProjectExpense] Set 
+				[Name] = @ExpenseName
+			   ,[Amount] = @ExpenseAmount
+			   ,[Reimbursement] = @ExpenseReimbursement
+		 Where Id = @ExpenseId
+	end
+	else
+	begin
+		Update [dbo].[ProjectExpense] Set 
+				[Name] = @ExpenseName
+			   ,[Amount] = @ExpenseAmount
+			   ,[Reimbursement] = @ExpenseReimbursement
+			   ,[MilestoneId] = @MilestoneId
+		 Where Id = @ExpenseId
+	end
+END
+
