287e2614d0cbb88fc1e5551c13d7e861f14bd467
RaviN <RaviN>
2011-04-25 03:05:00 -0500

    add from branch - Milestone v0.900 none

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/DataTransferObjects/ContextObjects/ProjectCloningContext.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.Serialization;
+
+namespace DataTransferObjects.ContextObjects
+{
+    /// <summary>
+    /// Pepresents project cloning context
+    /// </summary>
+    [DataContract]
+    [Serializable]
+    [DebuggerDisplay("ProjectCloningContext: ")]
+    public class ProjectCloningContext
+    {
+        /// <summary>
+        /// Project to be cloned
+        /// </summary>
+        [DataMember]
+        public Project Project { get; set; }
+
+        /// <summary>
+        /// Whether to clone milestones too
+        /// </summary>
+        [DataMember]
+        public bool CloneMilestones { get; set; }
+
+        /// <summary>
+        /// Whether to Clone Billing Info too
+        /// </summary>
+        [DataMember]
+        public bool CloneBillingInfo { get; set; }
+
+        /// <summary>
+        /// Whether to clone Billing Notes too
+        /// </summary>
+        [DataMember]
+        public bool CloneNotes { get; set; }
+
+        /// <summary>
+        /// Whether to clone commissions too
+        /// </summary>
+        [DataMember]
+        public bool CloneCommissions { get; set; }
+
+        /// <summary>
+        /// Project Status
+        /// </summary>
+        [DataMember]
+        public ProjectStatus ProjectStatus { get; set; }
+    }
+}
+
