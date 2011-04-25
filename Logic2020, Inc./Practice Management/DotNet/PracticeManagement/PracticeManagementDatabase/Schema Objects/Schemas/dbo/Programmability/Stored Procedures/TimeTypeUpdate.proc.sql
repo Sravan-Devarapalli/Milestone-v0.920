890bfa31349317fb248a6d0b53ba654fd93df618
RaviN <RaviN>
2011-04-25 03:05:00 -0500

    add from branch - Milestone v0.900 none

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/TimeTypeUpdate.proc.sql
@@ -0,0 +1,29 @@
+﻿-- =============================================
+-- Author:		Nikita Goncharenko
+-- Create date: 2009-11-30
+-- Description:	Update existing time type
+-- =============================================
+CREATE PROCEDURE dbo.TimeTypeUpdate
+	@TimeTypeId INT,
+	@Name VARCHAR(50),
+	@IsDefault BIT 
+AS
+BEGIN
+	SET NOCOUNT ON;
+	
+	UPDATE TimeType
+	SET [Name] = @Name
+	WHERE TimeTypeId = @TimeTypeId
+
+	-- If IsDefault value has changed, update table	
+	IF @IsDefault = 1
+	BEGIN
+		UPDATE TimeType
+		SET IsDefault = 0
+		
+		UPDATE TimeType
+		SET IsDefault = 1
+		WHERE TimeTypeId = @TimeTypeId
+	END
+END
+
