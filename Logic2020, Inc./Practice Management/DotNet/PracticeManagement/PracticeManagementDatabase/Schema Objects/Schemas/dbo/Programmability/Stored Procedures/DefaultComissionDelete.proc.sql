6b69b860d9416efdd1e11469a71fea5b7291ea27
RaviN <RaviN>
2011-04-25 03:05:00 -0500

    add from branch - Milestone v0.900 none

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/DefaultComissionDelete.proc.sql
@@ -0,0 +1,37 @@
+﻿--The source is 
+-- =============================================
+-- Author:		Anatoliy Lokshin
+-- Create date: 6-2-2008
+-- Description:	Removes a person's default actual commision
+-- =============================================
+CREATE PROCEDURE [dbo].[DefaultComissionDelete]
+(
+	@PersonId   INT,
+	@Type       INT
+)
+AS
+	SET NOCOUNT ON
+	SET XACT_ABORT ON
+
+	DECLARE @Today DATETIME
+	SET @Today = dbo.[Today]()
+
+	BEGIN TRAN
+
+	-- Delete the commission was set today
+	DELETE
+	  FROM dbo.DefaultCommission
+	 WHERE PersonId = @PersonId
+	   AND [Type] = @Type
+	   AND StartDate = @Today
+
+	-- Expire previously set commission
+	UPDATE dbo.DefaultCommission
+	   SET EndDate = @Today
+	 WHERE PersonId = @PersonId
+	   AND [Type] = @Type
+	   AND @Today >= StartDate
+	   AND @Today < EndDate
+
+	COMMIT TRAN
+
