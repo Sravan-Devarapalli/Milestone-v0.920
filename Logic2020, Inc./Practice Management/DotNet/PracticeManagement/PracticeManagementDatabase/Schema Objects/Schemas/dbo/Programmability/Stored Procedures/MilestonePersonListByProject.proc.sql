903ef136ea247a5380a40419e8ce8132385fd6b1
RaviN <RaviN>
2011-04-25 03:05:00 -0500

    add from branch - Milestone v0.900 none

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/MilestonePersonListByProject.proc.sql
@@ -0,0 +1,43 @@
+﻿
+CREATE PROCEDURE dbo.MilestonePersonListByProject
+(
+	@ProjectId   INT
+)
+AS
+	SET NOCOUNT ON
+	
+	SELECT mp.MilestonePersonId,
+	       mp.MilestoneId,
+	       mp.PersonId,
+	       mp.SeniorityId,
+	       mp.StartDate,
+	       mp.EndDate,
+	       mp.HoursPerDay,
+	       mp.FirstName,
+	       mp.LastName,
+	       mp.ProjectId,
+	       mp.ProjectName,
+	       mp.ProjectStartDate,
+	       mp.ProjectEndDate,
+	       mp.Discount,
+	       mp.ClientId,
+	       mp.ClientName,
+	       mp.MilestoneName,
+	       mp.MilestoneStartDate,
+	       mp.MilestoneProjectedDeliveryDate,
+	       mp.ExpectedHours,
+	       mp.PersonRoleId,
+	       mp.RoleName,
+	       mp.Amount,
+	       mp.IsHourlyAmount,
+	       mp.SalesCommission,
+	       mp.MilestoneExpectedHours,
+	       mp.MilestoneActualDeliveryDate,
+	       mp.MilestoneHourlyRevenue,
+	       mp.ConsultantsCanAdjust,
+	       mp.ClientIsChargeable,
+	       mp.ProjectIsChargeable,
+	       mp.MilestoneIsChargeable
+	  FROM dbo.v_MilestonePerson AS mp
+	 WHERE mp.ProjectId = @ProjectId
+
