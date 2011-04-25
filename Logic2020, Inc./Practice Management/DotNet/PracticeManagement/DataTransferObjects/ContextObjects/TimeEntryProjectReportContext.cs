8f9a6f7e67b9433ff9a9bcd88ab441c2dc41b22b
RaviN <RaviN>
2011-04-25 03:05:00 -0500

    add from branch - Milestone v0.900 none

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/DataTransferObjects/ContextObjects/TimeEntryProjectReportContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Runtime.Serialization;
+using System.Collections.Generic;
+
+namespace DataTransferObjects.ContextObjects
+{
+    /// <summary>
+    /// Represents Time Entry Project Report Context
+    /// </summary>
+    [DataContract]
+    [Serializable]
+    public class TimeEntryProjectReportContext
+    {
+        [DataMember]
+        public int ProjectId { get; set; }
+        [DataMember]
+        public DateTime? StartDate { get; set; }
+        [DataMember]
+        public DateTime? EndDate { get; set; }
+        [DataMember]
+        public IEnumerable<int> PersonIds { get; set; }
+    }
+}
+
