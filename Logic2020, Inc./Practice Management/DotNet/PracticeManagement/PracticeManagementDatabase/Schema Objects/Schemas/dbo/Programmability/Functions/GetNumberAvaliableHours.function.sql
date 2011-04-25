28b03d6573a89170c5787308c4a0bd6d0cef5cd9
RaviN <RaviN>
2011-04-25 03:05:00 -0500

    add from branch - Milestone v0.900 none

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Functions/GetNumberAvaliableHours.function.sql
@@ -0,0 +1,38 @@
+﻿-- =============================================
+-- Author:		Nikita Goncharenko
+-- Create date: 2009-10-29
+-- Description:	Gets number of avaliable hours for the person
+-- =============================================
+CREATE FUNCTION dbo.GetNumberAvaliableHours
+    (
+      @PersonId INT,
+      @startDate DATETIME,
+      @endDate DATETIME,
+   	  @ActiveProjects BIT = 1,
+	  @ProjectedProjects BIT = 1,
+	  @ExperimentalProjects BIT = 1	,
+	  @InternalProjects		BIT = 1
+    )
+RETURNS INT
+AS BEGIN
+    DECLARE @res INT ;
+
+   IF dbo.GetCurrentPayType(@PersonId) = 2 
+   BEGIN	
+		WITH    DefaultHours
+				  AS ( SELECT   Date,
+								dbo.GetDefaultHoursPerDay(PersonId, Date) AS defhrs
+					   FROM     dbo.v_PersonCalendar
+					   WHERE    Date BETWEEN @startDate AND @endDate
+								AND @PersonId = PersonId
+								AND DayOff = 0
+					 )
+			SELECT  @res = SUM(defhrs)
+			FROM    DefaultHours
+	END 
+    ELSE
+		SET @res = ISNULL(dbo.GetNumberProjectedHours(@PersonId, @startDate, @endDate, @ActiveProjects, @ProjectedProjects, @ExperimentalProjects,@InternalProjects), 0)
+    
+    RETURN @res
+   END
+
