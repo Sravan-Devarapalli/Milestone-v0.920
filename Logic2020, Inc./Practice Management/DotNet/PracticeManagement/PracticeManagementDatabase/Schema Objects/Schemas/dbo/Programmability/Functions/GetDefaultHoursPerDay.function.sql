bdba13b3e1e6f7e65cec5fab978647ba51cdd366
RaviN <RaviN>
2011-04-25 03:05:00 -0500

    add from branch - Milestone v0.900 none

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Functions/GetDefaultHoursPerDay.function.sql
@@ -0,0 +1,23 @@
+﻿-- =============================================
+-- Author:		Nikita Goncharenko
+-- Create date: 2009-10-29
+-- Description:	Gets default hours per day for given date
+-- =============================================
+CREATE FUNCTION GetDefaultHoursPerDay
+(
+	@PersonId INT,
+	@Date DATETIME
+)
+RETURNS INT
+AS
+BEGIN
+	DECLARE @res INT
+
+	SELECT @res = SUM(DefaultHoursPerDay) 
+	FROM dbo.v_Pay
+	WHERE @Date BETWEEN StartDate AND ISNULL(EndDate, dbo.GetFutureDate()) AND 
+		  @PersonId = PersonId
+
+	RETURN ISNULL(@res, 8)
+END
+
