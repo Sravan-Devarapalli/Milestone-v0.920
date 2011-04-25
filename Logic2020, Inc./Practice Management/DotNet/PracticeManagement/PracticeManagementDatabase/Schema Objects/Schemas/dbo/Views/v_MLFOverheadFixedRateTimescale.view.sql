4c914daa2703effa0cb69ec838fd21e4b3632db7
RaviN <RaviN>
2011-04-25 03:05:00 -0500

    add from branch - Milestone v0.900 none

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Views/v_MLFOverheadFixedRateTimescale.view.sql
@@ -0,0 +1,21 @@
+﻿CREATE VIEW [dbo].[v_MLFOverheadFixedRateTimescale]
+AS
+	  SELECT o.Description,
+			 ot.Rate,
+			 t.HoursToCollect,
+			 o.StartDate,
+			 o.EndDate,
+			 t.IsPercentage,
+			 t.OverheadRateTypeId,
+			 t.Name OverheadRateTypeName,
+			 CASE t.OverheadRateTypeId
+				WHEN 2 THEN o.Rate
+				ELSE CAST(0 AS DECIMAL) 
+				END AS BillRateMultiplier,
+			ot.TimescaleId
+	  FROM dbo.OverheadFixedRate AS o
+	  JOIN dbo.OverheadRateType AS t ON o.RateType = t.OverheadRateTypeId
+	  JOIN dbo.OverheadFixedRateTimescale AS ot  ON ot.OverheadFixedRateId = o.OverheadFixedRateId
+	  WHERE o.IsMinimumLoadFactor = 1 AND o.Inactive = 0 AND ot.Rate > 0
+
+
