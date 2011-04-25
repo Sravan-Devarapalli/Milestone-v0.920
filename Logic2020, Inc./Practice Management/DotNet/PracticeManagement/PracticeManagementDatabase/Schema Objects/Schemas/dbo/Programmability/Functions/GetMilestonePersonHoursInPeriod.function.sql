083a3380e73d3272d352ff068cd03482d15c4dd1
RaviN <RaviN>
2011-04-25 03:05:00 -0500

    add from branch - Milestone v0.900 none

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Functions/GetMilestonePersonHoursInPeriod.function.sql
@@ -0,0 +1,25 @@
+﻿-- =============================================
+-- Author:		Nikita Goncharenko
+-- Create date: 2010-08-04
+-- Description:	Get number of hours in period for specified milestone person
+-- =============================================
+CREATE FUNCTION GetMilestonePersonHoursInPeriod 
+(
+	-- Add the parameters for the function here
+	@MilestonePersonId int
+)
+RETURNS decimal(18,2)
+AS
+BEGIN
+	-- Declare the return variable here
+	DECLARE @HoursInPeriod decimal(18,2)
+
+	select @HoursInPeriod = SUM(mp.HoursPerDay * mp.VacationDays + mp.ExpectedHours)
+	from v_MilestonePerson mp 
+	where mp.MilestonePersonId=@MilestonePersonId
+	
+	-- Return the result of the function
+	RETURN @HoursInPeriod
+
+END
+
