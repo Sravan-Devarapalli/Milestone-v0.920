b7db079016494a92865447ffd729982dfe240092
RaviN <RaviN>
2011-04-25 03:05:00 -0500

    add from branch - Milestone v0.900 none

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/Utils/ProjectHelper.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using DataTransferObjects;
+
+namespace PraticeManagement.Utils
+{
+    public static class ProjectHelper
+    {
+        private static readonly IDictionary<ProjectStatusType, string> _projectStatuses
+           = new Dictionary<ProjectStatusType, string>
+                 {
+                    {ProjectStatusType.Projected, "ProjectedProject"},
+                    {ProjectStatusType.Experimental, "ExperimentalProject"},
+                    {ProjectStatusType.Completed, "CompletedProject"},
+                    {ProjectStatusType.Active, "pr-indicator"},
+                    {ProjectStatusType.Internal, "pr-internal"},
+                    {ProjectStatusType.Inactive, "ProjectedProject"}
+                };
+
+        public static string GetIndicatorClassByStatusId(int statusId)
+        {
+            if (!_projectStatuses.ContainsKey((ProjectStatusType)statusId)) 
+                return null;
+
+            return _projectStatuses[(ProjectStatusType)statusId];
+        }
+    }
+}
