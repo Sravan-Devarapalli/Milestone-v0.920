69f41c199fe9c7afce7fe1895fe4e1c8fe63ee7a
RaviN <RaviN>
2011-04-25 03:05:00 -0500

    add from branch - Milestone v0.900 none

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/GetPersonListByPersonIds.proc.sql
@@ -0,0 +1,13 @@
+﻿CREATE PROCEDURE GetPersonListByPersonIds
+(
+	@PersonIds NVARCHAR(MAX)
+)
+AS
+BEGIN
+	SELECT  PersonId,
+			FirstName,
+			LastName,
+			IsDefaultManager
+	FROM Person
+	WHERE PersonId in (SELECT ResultId FROM dbo.ConvertStringListIntoTable(@PersonIds))
+END
