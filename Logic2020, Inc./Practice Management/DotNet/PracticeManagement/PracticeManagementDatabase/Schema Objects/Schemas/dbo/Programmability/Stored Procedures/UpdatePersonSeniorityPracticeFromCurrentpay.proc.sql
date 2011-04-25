ee25f269a115ef8d23f3527a9fa3fa745517abe8
RaviN <RaviN>
2011-04-25 03:05:00 -0500

    add from branch - Milestone v0.900 none

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/UpdatePersonSeniorityPracticeFromCurrentpay.proc.sql
@@ -0,0 +1,18 @@
+﻿CREATE PROCEDURE dbo.UpdatePersonSeniorityPracticeFromCurrentPay
+AS
+BEGIN
+	DECLARE @Today DATETIME
+	SELECT @Today = CONVERT(DATETIME,CONVERT(DATE,GETDATE()))
+
+	UPDATE P
+	SET P.SeniorityId = Pa.SeniorityId,
+		P.DefaultPractice = Pa.PracticeId
+	FROM dbo.Person P
+	JOIN dbo.Pay Pa
+	ON P.PersonId = Pa.Person AND 
+		pa.StartDate <= @Today AND ISNULL(EndDate,dbo.GetFutureDate()) > @Today
+
+	UPDATE dbo.Pay
+	SET IsActivePay = CASE WHEN StartDate <= @Today AND  EndDate > @Today
+							   THEN 1 ELSE 0 END
+END
