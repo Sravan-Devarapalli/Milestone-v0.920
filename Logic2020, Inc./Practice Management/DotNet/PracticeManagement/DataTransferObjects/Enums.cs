c2707eac7684cc2f2d665cd8e75a65f3e21ddc55
RaviN <RaviN>
2011-04-25 03:05:00 -0500

    add from branch - Milestone v0.900 none

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/DataTransferObjects/Enums.cs
@@ -0,0 +1,25 @@
+﻿using System.Runtime.Serialization;
+
+namespace DataTransferObjects
+{
+    [DataContract]
+    public enum BudgetCategoryType
+    {
+        [EnumMember]
+        ClientDirector = 1,
+        [EnumMember]
+        PracticeArea = 2,
+        [EnumMember]
+        BusinessDevelopmentManager = 3
+    }
+
+    [DataContract]
+    public enum SettingsType
+    {
+        [EnumMember]
+        Reports = 1,
+        [EnumMember]
+        SMTP = 2
+    }
+}
+
