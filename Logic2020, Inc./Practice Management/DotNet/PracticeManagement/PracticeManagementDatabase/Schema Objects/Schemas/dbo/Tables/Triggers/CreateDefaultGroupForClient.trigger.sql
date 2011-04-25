f0eb14f70cdcb91a3770a49a626a3f64eb3dafe3
RaviN <RaviN>
2011-04-25 03:05:00 -0500

    add from branch - Milestone v0.900 none

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/Triggers/CreateDefaultGroupForClient.trigger.sql
@@ -0,0 +1,22 @@
+﻿
+-- =============================================
+-- Author:		Nikita Goncharenko
+-- Create date: 2009-10-14
+-- Description:	Creates default group on each
+-- =============================================
+CREATE TRIGGER CreateDefaultGroupForClient 
+   ON  dbo.Client 
+   AFTER INSERT
+AS 
+BEGIN
+	SET NOCOUNT ON;
+
+	INSERT INTO dbo.ProjectGroup (
+		ClientId,
+		[Name]
+	)  ( 
+		SELECT i.ClientId, 'Default Group' AS [Name] FROM inserted AS i
+	) 
+
+END
+
