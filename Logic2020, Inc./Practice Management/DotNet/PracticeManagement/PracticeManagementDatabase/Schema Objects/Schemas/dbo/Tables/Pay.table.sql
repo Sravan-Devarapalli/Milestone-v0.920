099b809b9f36bc379f5e9b05615c26eaa246bd53
RaviN <RaviN>
2011-04-25 03:05:00 -0500

    add from branch - Milestone v0.900 none

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/Pay.table.sql
@@ -0,0 +1,18 @@
+﻿CREATE TABLE [dbo].[Pay] (
+    [Person]              INT             NOT NULL,
+    [StartDate]           SMALLDATETIME   NOT NULL,
+    [EndDate]             SMALLDATETIME   NOT NULL,
+    [Amount]              DECIMAL (18, 2) NOT NULL,
+    [Timescale]           INT             NOT NULL,
+    [TimesPaidPerMonth]   INT             NULL,
+    [Terms]               INT             NULL,
+    [VacationDays]        INT             NULL,
+    [BonusAmount]         DECIMAL (18, 2) NOT NULL,
+    [BonusHoursToCollect] INT             NULL,
+    [DefaultHoursPerDay]  DECIMAL (18, 2) NOT NULL,
+	[SeniorityId]		  INT			  NULL,
+	[PracticeId]		  INT			  NULL,
+	[IsActivePay]		  BIT			  NULL
+);
+
+
