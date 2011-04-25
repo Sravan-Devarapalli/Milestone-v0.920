0213330eb155447c15dceb537f8993743d29d8c3
RaviN <RaviN>
2011-04-25 03:05:00 -0500

    add from branch - Milestone v0.900 none

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementService/IPracticeService.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using System.ServiceModel;
+using DataTransferObjects;
+
+namespace PracticeManagementService
+{
+	[ServiceContract]
+	public interface IPracticeService
+	{
+		/// <summary>
+		/// Get all practices
+		/// </summary>
+		/// <returns>A list of <see cref="Practice"/>s in the system</returns>
+		[OperationContract]
+		List<Practice> GetPracticeList();
+
+        /// <summary>
+        /// Get all practices
+        /// </summary>
+        /// <returns>A list of <see cref="Practice"/>s in the system</returns>
+        [OperationContract]
+        List<Practice> PracticeListAll(Person person);
+
+        /// <summary>
+        /// Get practices by id
+        /// </summary>
+        /// <returns>A list of <see cref="Practice"/>s in the system</returns>
+        [OperationContract]
+        List<Practice> PracticeGetById(int? id);
+
+        /// <summary>
+        /// Updates practice
+        /// </summary>
+        /// <returns>A list of <see cref="Practice"/>s in the system</returns>
+        [OperationContract]
+        void UpdatePractice(Practice practice);
+
+        /// <summary>
+        /// Inserts practice
+        /// </summary>
+        /// <returns>A list of <see cref="Practice"/>s in the system</returns>
+        [OperationContract]
+        int InsertPractice(Practice practice);
+
+        /// <summary>
+        /// Removes practice
+        /// </summary>
+        /// <returns>A list of <see cref="Practice"/>s in the system</returns>
+        [OperationContract]
+        void RemovePractice(Practice practice);
+    }
+}
+
