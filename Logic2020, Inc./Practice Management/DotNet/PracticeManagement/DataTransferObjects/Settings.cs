536e9ef071f7803f2c530de7ff7cec8b3f63d24d
RaviN <RaviN>
2011-04-25 03:05:00 -0500

    add from branch - Milestone v0.900 none

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/DataTransferObjects/Settings.cs
@@ -0,0 +1,63 @@
+﻿using DataTransferObjects.Utils;
+using Microsoft.WindowsAzure.ServiceRuntime;
+using System;
+
+namespace DataTransferObjects
+{
+    public class Settings
+    {
+        #region Constants
+
+        private const string SenioritySeparationRangeValue = "senioritySeparationRangeValue";
+
+        #endregion
+
+        #region Properties
+
+        public static int SenioritySeparationRange
+        {
+            get
+            {
+                return GetSenioritySeparationRange(SenioritySeparationRangeValue);
+            }
+        }
+
+        #endregion
+
+        public static int GetSenioritySeparationRange(string key)
+        {
+            if (IsAzureWebRole())
+            {
+                try
+                {
+                    return Convert.ToInt32(RoleEnvironment.GetConfigurationSettingValue(key));
+                }
+                catch
+                {
+                    return Generic.GetIntConfiguration(SenioritySeparationRangeValue); ;
+                }
+            }
+            else
+            {
+                return Generic.GetIntConfiguration(SenioritySeparationRangeValue);;
+            }
+        }
+
+        private static Boolean IsAzureWebRole()
+        {
+            try
+            {
+                if (RoleEnvironment.IsAvailable)
+                {
+                    return true;
+                }
+                return false;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
+
