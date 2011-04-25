7511c70f92e8c5032486ad33712d697677686c0b
RaviN <RaviN>
2011-04-25 03:05:00 -0500

    add from branch - Milestone v0.900 none

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/PersonsGetBySeniorityAndStatus.proc.sql
@@ -0,0 +1,13 @@
+﻿CREATE PROCEDURE [dbo].[PersonsGetBySeniorityAndStatus]
+	@SeniorityId int, 
+	@PersonStatusId    int = NULL
+AS
+	SELECT PersonId,
+		   FirstName,
+		   LastName,
+		   IsDefaultManager,
+		   HireDate
+	FROM dbo.Person
+	WHERE SeniorityId = @SeniorityId
+			AND (PersonStatusId = @PersonStatusId OR @PersonStatusId IS NULL)
+
