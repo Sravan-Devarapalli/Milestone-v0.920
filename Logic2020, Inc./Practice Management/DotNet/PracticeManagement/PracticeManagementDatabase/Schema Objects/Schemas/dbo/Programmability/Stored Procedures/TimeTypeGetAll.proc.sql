0e7f74ba7de2d8fc68f0fca1fd98be736d16646d
RaviN <RaviN>
2011-04-25 03:05:00 -0500

    add from branch - Milestone v0.900 none

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/TimeTypeGetAll.proc.sql
@@ -0,0 +1,31 @@
+﻿-- =============================================
+-- Author:		Nikita Goncharenko
+-- Create date: 2009-11-30
+-- Description:	Gets all avaliable time types
+-- =============================================
+CREATE PROCEDURE dbo.TimeTypeGetAll
+AS
+BEGIN
+
+	WITH UsageInfo AS (
+	SELECT 
+		tt.TimeTypeId, 
+		tt.[Name], 
+		'True' AS InUse,
+		tt.IsDefault
+	FROM TimeType AS tt
+	WHERE tt.TimeTypeId IN (SELECT DISTINCT(TimeTypeId) FROM TimeEntries)
+	UNION 
+	SELECT 
+		tt.TimeTypeId, 
+		tt.[Name], 
+		'False' AS InUse,
+		tt.IsDefault
+	FROM TimeType AS tt
+	WHERE tt.TimeTypeId NOT IN (SELECT DISTINCT(TimeTypeId) FROM TimeEntries)
+	)
+	SELECT * 
+	FROM UsageInfo
+	ORDER BY Name
+END
+
