a8306e10e034a199fe043860a4bf81e3db42c1b1
RaviN <RaviN>
2011-04-25 03:05:00 -0500

    add from branch - Milestone v0.900 none

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementService/ITimescaleService.cs
@@ -0,0 +1,27 @@
+﻿using System.ServiceModel;
+using DataTransferObjects;
+using System.Collections.Generic;
+
+namespace PracticeManagementService
+{
+	[ServiceContract]
+    [ServiceKnownType(typeof(Timescale))]
+    public interface ITimescaleService
+	{
+		/// <summary>
+		/// Retrives a <see cref="Timescale"/> by its ID.
+		/// </summary>
+		/// <param name="timescaleId">An ID of the <see cref="Timescale"/> to be retrieved.</param>
+		/// <returns>A <see cref="Timescale"/> object if found and null otherwise.</returns>
+		[OperationContract]
+		Timescale GetById(TimescaleType timescaleId);
+
+        /// <summary>
+        /// Retrives all the timescale types.
+        /// </summary>
+        /// <returns></returns>
+        [OperationContract]
+        List<Timescale> GetAll();
+	}
+}
+
