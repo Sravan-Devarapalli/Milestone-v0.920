708055c09803bdc7a130235d4fb6d6586621ffac
RaviN <RaviN>
2011-04-25 03:05:00 -0500

    add from branch - Milestone v0.900 none

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/PersonListAllForMilestone.proc.sql
@@ -0,0 +1,32 @@
+﻿-- =============================================
+-- Author:		Anatoliy Lokshin
+-- Create date: 12-04-2008
+-- Updated by:	
+-- Update date: 
+-- Description:	Retrives the filtered list of persons who are not in the Administration practice.
+-- =============================================
+CREATE PROCEDURE dbo.PersonListAllForMilestone
+(
+	@MilestonePersonId   INT,
+	@StartDate           DATETIME,
+    @EndDate             DATETIME
+)
+AS
+	SET NOCOUNT ON
+
+	SELECT p.PersonId,
+	       p.FirstName,
+	       p.LastName,
+		   p.IsDefaultManager,
+		   p.HireDate
+	  FROM dbo.Person AS p
+	 WHERE (   (p.PersonStatusId = 1 OR p.PersonStatusId = 3)
+	        OR EXISTS (SELECT 1
+	                     FROM dbo.MilestonePerson AS mp
+	                    WHERE mp.MilestonePersonId = @MilestonePersonId AND mp.PersonId = p.PersonId))
+	   AND (   @StartDate IS NULL
+	        OR @EndDate IS NULL
+	        OR dbo.GetOverlappingPlacementDays(p.HireDate, ISNULL(p.TerminationDate, dbo.GetFutureDate()), @StartDate, @EndDate) = 1
+	       )	  
+	ORDER BY p.LastName, p.FirstName
+
