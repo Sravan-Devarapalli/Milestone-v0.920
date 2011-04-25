a5fc196d2bc763a9a1de770d776604fe3d73aded
RaviN <RaviN>
2011-04-25 03:05:00 -0500

    add from branch - Milestone v0.900 none

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/DataTransferObjects/Timescale.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace DataTransferObjects
+{
+	/// <summary>
+	/// Represents a timescale entity
+	/// </summary>
+	[DataContract]
+	[Serializable]
+	public class Timescale
+	{
+		/// <summary>
+		/// Gets or sets a <see cref="Timescale"/> Id value.
+		/// </summary>
+		[DataMember]
+		public int Id
+		{
+			get;
+			set;
+		}
+
+		/// <summary>
+		/// Gets or sets a name of the <see cref="Timescale"/>.
+		/// </summary>
+		[DataMember]
+		public string Name
+		{
+			get;
+			set;
+		}
+
+		/// <summary>
+		/// Gets or sets a default Terms for the <see cref="Timescale"/>.
+		/// </summary>
+		[DataMember]
+		public int? DefaultTerms
+		{
+			get;
+			set;
+		}
+	}
+}
+
