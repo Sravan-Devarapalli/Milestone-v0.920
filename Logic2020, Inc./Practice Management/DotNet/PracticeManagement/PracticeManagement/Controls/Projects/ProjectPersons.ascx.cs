ddb9254d1e285569db85ce52eb74897139f728ec
RaviN <RaviN>
2011-04-25 03:05:00 -0500

    add from branch - Milestone v0.900 none

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/Controls/Projects/ProjectPersons.ascx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using PraticeManagement.Utils;
+
+namespace PraticeManagement.Controls.Projects
+{
+    public partial class ProjectPersons : System.Web.UI.UserControl
+    {
+        public const string PERSON_TARGET = "person";
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        #region Redirects
+
+        protected string GetMilestonePersonRedirectUrl(object milestoneId, object milestonePersonId)
+        {
+            return Urls.GerMilestonePersonDetailsUrlWithReturn(milestoneId, milestonePersonId, Request.Url.AbsoluteUri);
+        }
+
+        #endregion
+    }
+}
