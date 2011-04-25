4e847f30991305cc62b3e95f30b657f7642bfade
RaviN <RaviN>
2011-04-25 03:05:00 -0500

    add from branch - Milestone v0.900 none

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/dbo.TimeEntriesRemove.proc.sql
@@ -0,0 +1,12 @@
+﻿CREATE PROCEDURE [dbo].[TimeEntriesRemove]
+	@MilestonePersonId int, 
+	@TimeTypeId int,
+	@StartDate datetime,
+	@EndDate datetime
+AS
+	delete 
+	from TimeEntries
+	where 
+		MilestonePersonId = @MilestonePersonId and 
+		TimeTypeId = @TimeTypeId and 
+		MilestoneDate between @StartDate and @EndDate
