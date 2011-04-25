288bf1ce3e2268c289026cede436110443c3af1c
RaviN <RaviN>
2011-04-25 03:05:00 -0500

    add from branch - Milestone v0.900 none

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementService/ICalendarService.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ServiceModel;
+
+using DataTransferObjects;
+
+namespace PracticeManagementService
+{
+	[ServiceContract]
+	public interface ICalendarService
+	{
+		/// <summary>
+		/// Retrieves a list of the calendar items from the database.
+		/// </summary>
+		/// <param name="startDate">The start of the period.</param>
+		/// <param name="endDate">The end of the period.</param>
+		/// <param name="personId">
+		/// An ID of the person to the cxalendar be retrived for.
+		/// If null the company calendar will be returned.
+		/// </param>
+		/// <param name="practiceManagerId">
+		/// An ID of the practice manager to retrieve the data for his subordinate
+		/// </param>
+		/// <returns>The list of the <see cref="CalendarItem"/> objects.</returns>
+		[OperationContract]
+		List<CalendarItem> GetCalendar(DateTime startDate, DateTime endDate, int? personId, int? practiceManagerId);
+
+		/// <summary>
+		/// Saves a <see cref="CalendarItem"/> object to the database.
+		/// </summary>
+		/// <param name="item">The data to be saved to.</param>
+		[OperationContract]
+		void SaveCalendar(CalendarItem item);
+
+        /// <summary>
+        /// Returns No. of Company holidays in a given year
+        /// </summary>
+        /// <param name="year"></param>
+        /// <returns></returns>
+        [OperationContract]
+        int GetCompanyHolidays(int year);
+	}
+}
+
