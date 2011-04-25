791826e12ed6172724c10d5979bdd6f059f72a14
RaviN <RaviN>
2011-04-25 03:05:00 -0500

    add from branch - Milestone v0.900 none

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/MilestonePersonEntry.table.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[MilestonePersonEntry] (
+    [MilestonePersonId] INT             NOT NULL,
+    [StartDate]         DATETIME        NOT NULL,
+    [EndDate]           DATETIME        NULL,
+    [PersonRoleId]      INT             NULL,
+    [Amount]            DECIMAL (18, 2) NULL,
+	[Location]			NVARCHAR(20)    NULL,
+    [HoursPerDay]       DECIMAL (4, 2)  NOT NULL
+);
+
+
