1358e98cc01afc8a28b0060efb183ad4eb77113a
RaviN <RaviN>
2011-04-25 03:05:00 -0500

    add from branch - Milestone v0.900 none

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Views/v_ProjectExpenses.view.sql
@@ -0,0 +1,14 @@
+﻿CREATE view dbo.v_ProjectExpenses
+as
+	SELECT 
+	   pexp.[Id] as 'ExpenseId'
+      ,pexp.[Name] as 'ExpenseName'
+      ,pexp.[Amount] as 'ExpenseAmount'
+      ,pexp.[Reimbursement] as 'ExpenseReimbursement'
+	  ,pexp.[Reimbursement] * 0.01 * pexp.[Amount] as 'ExpenseReimbursementAmount'
+	  ,m.ProjectId
+      ,m.[MilestoneId] as 'MilestoneId'
+	  ,m.[Description] as 'MilestoneName'
+	from dbo.ProjectExpense as pexp
+	inner join milestone as m on pexp.milestoneId = m.milestoneId
+
