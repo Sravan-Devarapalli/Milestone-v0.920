accf8cfdd72a68c88318e278cd955f9c128d79da
RaviN <RaviN>
2011-04-25 03:05:00 -0500

    add from branch - Milestone v0.900 none

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/GenerateNewProjectNumber.proc.sql
@@ -0,0 +1,24 @@
+﻿CREATE PROCEDURE dbo.GenerateNewProjectNumber (@ProjectNumber AS NVARCHAR (12) OUTPUT) AS
+BEGIN
+DECLARE @StringCounter NVARCHAR(7)
+	DECLARE @Counter INT
+
+	SET @Counter = 0
+
+	WHILE  (1 = 1)
+	BEGIN
+
+		SET @StringCounter = CAST(@Counter AS NVARCHAR(7))
+		IF LEN ( @StringCounter ) = 1
+			SET @StringCounter =  '0' + @StringCounter
+
+		SET @ProjectNumber = dbo.MakeNumberFromDate('P', GETDATE()) + @StringCounter
+	
+		IF NOT EXISTS (SELECT 1 FROM [dbo].[Project] as p WHERE p.[ProjectNumber] = @ProjectNumber)
+			BREAK
+
+		SET @Counter = @Counter + 1
+	END
+END
+
+
