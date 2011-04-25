c62e6fd5a3c1da2f08e38ff9977c38599f2f408b
RaviN <RaviN>
2011-04-25 03:05:00 -0500

    add from branch - Milestone v0.900 none

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/OpportunityPriorities.table.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE dbo.OpportunityPriorities (
+    Id                   INT IDENTITY(1,1) NOT NULL,
+    Priority			 NVARCHAR(1) NOT NULL,
+    Description          NVARCHAR(MAX) NOT NULL
+   CONSTRAINT PK_OpportunityPriorities_Priority PRIMARY KEY CLUSTERED(Priority)
+);
