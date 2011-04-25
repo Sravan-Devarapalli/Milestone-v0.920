4931ba77610a7a3a6ec0dd60641b51b0d5575727
RaviN <RaviN>
2011-04-25 03:05:00 -0500

    add from branch - Milestone v0.900 none

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/ListProjectsByClientShort.proc.sql
@@ -0,0 +1,35 @@
+﻿CREATE PROCEDURE dbo.ListProjectsByClientShort
+(
+	@ClientId INT,
+	@IsOnlyActiveAndProjective BIT
+)
+AS
+BEGIN
+	SET NOCOUNT ON
+	IF (@IsOnlyActiveAndProjective = 1)
+	BEGIN
+		SELECT p.ClientId,
+			   p.ProjectId,
+			   p.Name,
+			   p.ClientName,
+			   p.ProjectNumber,
+			   p.BuyerName
+		FROM dbo.v_Project AS p
+		WHERE p.ClientId = @ClientId 
+			   AND p.ProjectStatusId IN (2,3)
+		ORDER BY p.Name
+	END
+	ELSE
+	BEGIN
+		SELECT p.ClientId,
+			   p.ProjectId,
+			   p.Name,
+			   p.ClientName,
+			   p.ProjectNumber,
+			   p.BuyerName
+		FROM dbo.v_Project AS p
+		WHERE p.ClientId = @ClientId 
+		ORDER BY p.Name
+	END
+END
+	
