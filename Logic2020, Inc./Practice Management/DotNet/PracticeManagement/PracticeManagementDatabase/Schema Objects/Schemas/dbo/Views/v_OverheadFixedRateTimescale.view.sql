76799350f818d6437e10abf3706414d45175841e
RaviN <RaviN>
2011-04-25 03:05:00 -0500

    add from branch - Milestone v0.900 none

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Views/v_OverheadFixedRateTimescale.view.sql
@@ -0,0 +1,24 @@
+﻿-- =============================================
+-- Author:		Anatoliy Lokshin
+-- Create date: 10-16-2008
+-- Updated by:	
+-- Update date:	
+-- Description:	Selects overheads for the timescales.
+-- =============================================
+CREATE VIEW [dbo].[v_OverheadFixedRateTimescale]
+AS
+	SELECT o.OverheadFixedRateId,
+	       o.Description,
+	       o.Rate,
+	       o.StartDate,
+	       o.EndDate,
+	       o.Inactive,
+	       o.OverheadRateTypeId,
+	       o.OverheadRateTypeName,
+	       o.IsPercentage,
+	       o.HoursToCollect,
+	       o.IsCogs,
+	       t.TimescaleId
+	  FROM dbo.v_OverheadFixedRate AS o
+	       INNER JOIN dbo.OverheadFixedRateTimescale AS t ON o.OverheadFixedRateId = t.OverheadFixedRateId
+
