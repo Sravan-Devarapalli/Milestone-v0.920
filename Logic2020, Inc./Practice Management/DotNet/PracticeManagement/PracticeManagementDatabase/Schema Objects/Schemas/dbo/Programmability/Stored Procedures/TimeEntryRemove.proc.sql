7a394cb0738ff718f8a176be5b07ad34bd70a4b4
RaviN <RaviN>
2011-04-25 03:05:00 -0500

    add from branch - Milestone v0.900 none

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/TimeEntryRemove.proc.sql
@@ -0,0 +1,16 @@
+﻿-- =============================================
+-- Author:		Nikita Goncharenko
+-- Create date: 2010-02-02
+-- Description:	Removes TE
+-- =============================================
+CREATE PROCEDURE dbo.TimeEntryRemove
+	@TimeEntryId INT 
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	DELETE 
+	FROM TimeEntries 
+	WHERE TimeEntryId = @TimeEntryId
+END
+
