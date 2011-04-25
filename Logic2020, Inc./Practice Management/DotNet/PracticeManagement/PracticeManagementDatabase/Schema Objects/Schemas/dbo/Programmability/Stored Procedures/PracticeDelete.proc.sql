12949977bce8656fb3310b808687f722361e964a
RaviN <RaviN>
2011-04-25 03:05:00 -0500

    add from branch - Milestone v0.900 none

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/PracticeDelete.proc.sql
@@ -0,0 +1,22 @@
+﻿-- =============================================
+-- Author:		Nikita Goncharenko
+-- Create date: 2010-02-26
+-- Description:	Practices routines
+-- =============================================
+CREATE PROCEDURE dbo.PracticeDelete 
+	@PracticeId INT	
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	DELETE FROM dbo.PracticeStatusHistory
+	WHERE PracticeId = @PracticeId
+
+	DELETE FROM dbo.PracticeManagerHistory
+	WHERE PracticeId = @PracticeId
+
+	DELETE FROM Practice 
+	 WHERE PracticeId = @PracticeId
+ 
+ END
+
