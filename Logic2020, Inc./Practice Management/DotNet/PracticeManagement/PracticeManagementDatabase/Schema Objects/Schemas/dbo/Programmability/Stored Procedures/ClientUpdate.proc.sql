bd471159d6284ef24c3b0c0b52b2ff1e9b78476d
RaviN <RaviN>
2011-04-25 03:05:00 -0500

    add from branch - Milestone v0.900 none

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/ClientUpdate.proc.sql
@@ -0,0 +1,33 @@
+﻿CREATE PROCEDURE dbo.ClientUpdate
+(
+	@ClientId             INT,
+	@Name                 NVARCHAR(50),
+	@DefaultDiscount      DECIMAL(18,2),
+	@DefaultSalespersonId INT,
+	@DefaultDirectorId	  INT,
+	@IsChargeable         BIT,
+	@Inactive             BIT,
+	@DefaultTerms         INT
+)
+AS
+	SET NOCOUNT ON;
+
+	IF EXISTS (SELECT 1 FROM dbo.Client AS c WHERE c.Name = @Name AND c.ClientId <> @ClientId)
+	BEGIN
+		DECLARE @ErrorMessage NVARCHAR(2048)
+		SELECT @ErrorMessage = [dbo].[GetErrorMessage](70004)
+		RAISERROR (@ErrorMessage, 16, 1)
+	END
+	ELSE
+	BEGIN
+		UPDATE dbo.Client
+		   SET DefaultDiscount = @DefaultDiscount,
+			   DefaultTerms = @DefaultTerms,
+			   DefaultSalespersonID = @DefaultSalespersonID,
+			   DefaultDirectorID	= @DefaultDirectorId,
+			   Name = @Name,
+			   Inactive = @Inactive,
+			   IsChargeable = @IsChargeable
+		 WHERE ClientId = @ClientId
+	END
+
