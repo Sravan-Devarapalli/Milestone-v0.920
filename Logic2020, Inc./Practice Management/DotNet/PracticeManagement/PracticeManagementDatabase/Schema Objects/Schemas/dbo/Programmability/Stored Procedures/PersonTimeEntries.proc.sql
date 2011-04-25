4c6de7cc871e98cbbdb8af8249ba3315dc893239
RaviN <RaviN>
2011-04-25 03:05:00 -0500

    add from branch - Milestone v0.900 none

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/PersonTimeEntries.proc.sql
@@ -0,0 +1,23 @@
+﻿-- =============================================
+-- Author:		Nikita Gonhcarenko
+-- Create date: 2009-12-08
+-- Description:	Get time entries for the person
+-- =============================================
+CREATE PROCEDURE [dbo].[PersonTimeEntries] 
+	@PersonId INT,
+	@StartDate DATETIME,
+	@EndDate DATETIME	
+AS
+BEGIN
+	SET NOCOUNT ON;
+	
+SELECT te.*, cal.CompanyDayOff, cal.DayOff, cal.Date, CAST(0 as bit) as 'ReadOnly'
+  FROM v_TimeEntries AS te
+  left join v_PersonCalendar as cal on te.PersonId = cal.PersonId and te.MilestoneDate = cal.Date
+  
+  WHERE 
+	te.MilestoneDate BETWEEN @StartDate AND @EndDate
+	AND te.PersonId = @PersonId  
+END
+
+
