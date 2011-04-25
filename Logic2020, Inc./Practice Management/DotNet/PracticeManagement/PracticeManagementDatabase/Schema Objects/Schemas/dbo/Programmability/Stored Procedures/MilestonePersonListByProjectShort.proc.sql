79448e6c8d6339db868c2bdac43fcc09d160117a
RaviN <RaviN>
2011-04-25 03:05:00 -0500

    add from branch - Milestone v0.900 none

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/MilestonePersonListByProjectShort.proc.sql
@@ -0,0 +1,21 @@
+﻿-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE PROCEDURE dbo.MilestonePersonListByProjectShort 
+	@ProjectId   VARCHAR(1000)
+AS
+BEGIN
+	SET NOCOUNT ON
+	
+	SELECT mp.MilestonePersonId,
+	       mp.PersonId,
+	       mp.FirstName,
+	       mp.LastName,
+	       mp.ProjectId,
+		   mp.SeniorityId
+	  FROM dbo.v_MilestonePerson AS mp
+	 WHERE mp.ProjectId IN (SELECT * FROM dbo.ConvertStringListIntoTable(@ProjectId))
+END
+
