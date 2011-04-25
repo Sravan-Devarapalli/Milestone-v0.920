38c53a143e619c6f520b193f6a6fbede3d60a3bd
RaviN <RaviN>
2011-04-25 03:05:00 -0500

    add from branch - Milestone v0.900 none

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/Client.table.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[Client] (
+    [ClientId]             INT             IDENTITY (1, 1) NOT NULL,
+    [DefaultDiscount]      DECIMAL (18, 2) NOT NULL,
+    [DefaultTerms]         INT             NOT NULL,
+    [DefaultSalespersonID] INT             NOT NULL,
+    [Name]                 NVARCHAR (100)  NOT NULL,
+    [Inactive]             BIT             NOT NULL,
+    [IsChargeable]         BIT             NOT NULL,
+	[DefaultDirectorID]	   INT
+);
+
+
