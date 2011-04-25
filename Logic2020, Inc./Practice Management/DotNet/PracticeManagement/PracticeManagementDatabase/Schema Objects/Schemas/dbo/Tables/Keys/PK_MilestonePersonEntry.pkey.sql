cc9f5fec5d173f5278c53b6e274089bdd46fe2dd
RaviN <RaviN>
2011-04-25 03:05:00 -0500

    add from branch - Milestone v0.900 none

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/Keys/PK_MilestonePersonEntry.pkey.sql
@@ -0,0 +1,4 @@
+﻿ALTER TABLE [dbo].[MilestonePersonEntry]
+    ADD CONSTRAINT [PK_MilestonePersonEntry] PRIMARY KEY CLUSTERED ([MilestonePersonId] ASC, [StartDate] ASC) WITH (IGNORE_DUP_KEY = OFF, STATISTICS_NORECOMPUTE = OFF);
+
+
