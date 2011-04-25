b1f0bb510fdb43f917d51c3be5cc685b3f900d2c
RaviN <RaviN>
2011-04-25 03:05:00 -0500

    add from branch - Milestone v0.900 none

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/DataTransferObjects/CompanyLogo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+
+namespace DataTransferObjects
+{
+   public class CompanyLogo
+    {
+        [DataMember]
+        public string Title { get; set; }
+
+        [DataMember]
+        public string FileName { get; set; }
+
+        [DataMember]
+        public string FilePath { get; set; }
+
+        [DataMember]
+        public Byte[] Data { get; set; }
+
+    }
+}
+
