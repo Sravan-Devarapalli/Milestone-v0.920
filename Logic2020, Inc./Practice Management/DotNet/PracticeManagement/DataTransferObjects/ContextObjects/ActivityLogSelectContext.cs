28e53d9853aed735041e365a3fd891d62d0ed783
RaviN <RaviN>
2011-04-25 03:05:00 -0500

    add from branch - Milestone v0.900 none

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/DataTransferObjects/ContextObjects/ActivityLogSelectContext.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace DataTransferObjects.ContextObjects
+{
+    /// <summary>
+    /// Pepresents activity log select query context
+    /// </summary>
+    [DataContract]
+    [Serializable]
+    public class ActivityLogSelectContext
+    {
+        [DataMember]
+        public ActivityEventSource Source { get; set; }
+
+        [DataMember]
+        public DateTime StartDate { get; set; }
+
+        [DataMember]
+        public DateTime EndDate { get; set; }
+
+        [DataMember]
+        public int? PersonId { get; set; }
+
+        [DataMember]
+        public int? ProjectId { get; set; }
+
+        [DataMember]
+        public int? OpportunityId { get; set; }
+
+        [DataMember]
+        public int? MilestoneId { get; set; }
+    }
+}
