846bbc0c633ff2c5ec9b91e118828537cc191a77
RaviN <RaviN>
2011-04-25 03:05:00 -0500

    add from branch - Milestone v0.900 none

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/UserActivityLogInsert.proc.sql
@@ -0,0 +1,37 @@
+﻿CREATE PROCEDURE UserActivityLogInsert
+	@ActivityTypeID	INT,
+	@LogData		NVARCHAR(MAX)
+AS
+	-- Ensure the temporary table exists
+	EXEC SessionLogPrepare @UserLogin = NULL
+
+	-- Log an activity
+	INSERT INTO dbo.UserActivityLog
+	            (ActivityTypeID,
+	             SessionID,
+	             SystemUser,
+	             Workstation,
+	             ApplicationName,
+	             UserLogin,
+	             PersonID,
+	             LastName,
+	             FirstName,
+	             LogData,
+				 Data)
+	SELECT @ActivityTypeID,
+	       l.SessionID,
+	       l.SystemUser,
+	       l.Workstation,
+	       l.ApplicationName,
+	       l.UserLogin,
+	       l.PersonID,
+	       l.LastName,
+	       l.FirstName,
+		   LogData = (CASE WHEN @ActivityTypeID = 20 THEN '<Error></Error>' ELSE @LogData END), -- if @ActivityTypeID = 20 it is an Error.
+	       Data = @LogData
+	  FROM dbo.SessionLogData AS l
+	  WHERE l.SessionID = @@SPID
+
+	  -- End logging session
+	 EXEC dbo.SessionLogUnprepare
+
