ac7f0b012c0f25d33fc57d4e10a51825b12ce218
RaviN <RaviN>
2011-04-25 03:05:00 -0500

    add from branch - Milestone v0.900 none

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/ExpenseCategoryInsert.proc.sql
@@ -0,0 +1,18 @@
+﻿-- =============================================
+-- Author:		Anatoliy Lokshin
+-- Create date: 7-22-2008
+-- Updated by:	
+-- Update date:	
+-- Description:	Inserts a new Expense Category
+-- =============================================
+CREATE PROCEDURE [dbo].[ExpenseCategoryInsert]
+(
+	@Name   NVARCHAR(25)
+)
+AS
+	SET NOCOUNT ON
+
+	INSERT INTO dbo.ExpenseCategory
+	            (Name)
+	     VALUES (@Name)
+
