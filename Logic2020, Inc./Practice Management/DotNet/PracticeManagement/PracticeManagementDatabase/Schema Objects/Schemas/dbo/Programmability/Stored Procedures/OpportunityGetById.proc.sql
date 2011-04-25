d39806166217b6345579f657bfbfe42c3030f1fc
RaviN <RaviN>
2011-04-25 03:05:00 -0500

    add from branch - Milestone v0.900 none

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/OpportunityGetById.proc.sql
@@ -0,0 +1,47 @@
+﻿CREATE PROCEDURE [dbo].[OpportunityGetById]
+(
+	@OpportunityId   INT
+)
+AS
+	SET NOCOUNT ON
+
+	SELECT o.OpportunityId,
+	       o.Name,
+	       o.ClientId,
+	       o.SalespersonId,
+	       o.OpportunityStatusId,
+	       o.Priority,
+	       o.ProjectedStartDate,
+	       o.ProjectedEndDate,
+	       o.OpportunityNumber,
+	       o.Description,
+	       o.PracticeId,
+	       o.BuyerName,
+	       o.CreateDate,
+	       o.Pipeline,
+	       o.Proposed,
+	       o.SendOut,
+	       o.ClientName,
+	       o.SalespersonFirstName,
+	       o.SalespersonLastName,
+		   ps.Name AS SalespersonStatus,
+	       o.OpportunityStatusName,
+	       o.PracticeName,
+	       o.ProjectId,
+	       o.OpportunityIndex,
+	       o.RevenueType,
+		   o.OwnerId,		   
+		   o.LastUpdate,
+		   o.GroupId,
+		   o.GroupName,
+		   o.PracticeManagerId,
+		   p.LastName as 'OwnerLastName',
+		   p.FirstName as 'OwnerFirstName',
+		   os.Name AS 'OwnerStatus',
+		   o.EstimatedRevenue
+		   
+	 FROM dbo.v_Opportunity AS o
+	 LEFT JOIN dbo.Person p ON o.OwnerId = p.PersonId
+	 LEFT JOIN dbo.PersonStatus ps ON ps.PersonStatusId = o.SalespersonStatusId 	
+	 LEFT JOIN dbo.PersonStatus os ON os.PersonStatusId = o.OwnerStatusId
+	 WHERE o.OpportunityId = @OpportunityId
