419923ebc3be907e32c4792cf03a1fb78c91f368
RaviN <RaviN>
2011-04-25 03:05:00 -0500

    add from branch - Milestone v0.900 none

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/NoteInsert.proc.sql
@@ -0,0 +1,26 @@
+﻿CREATE PROCEDURE [dbo].[NoteInsert]
+(
+	@TargetId		INT,
+	@NoteTargetId	INT,
+	@PersonId		INT,
+	@NoteText		NVARCHAR(MAX),
+	@NoteId         INT OUT 
+)
+AS
+	SET NOCOUNT ON
+
+	INSERT INTO dbo.Note
+	            (
+	              TargetId, 
+	              NoteTargetId, 
+	              PersonId, 
+	              NoteText
+	             )
+	VALUES       (
+				  @TargetId, 
+				  @NoteTargetId, 
+				  @PersonId, 
+				  @NoteText
+				  )
+    SET @NoteId = SCOPE_IDENTITY()
+    SELECT @NoteId
