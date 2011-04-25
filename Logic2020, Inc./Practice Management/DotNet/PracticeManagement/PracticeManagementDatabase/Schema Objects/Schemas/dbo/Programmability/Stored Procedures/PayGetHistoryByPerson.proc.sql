7528f957b9e2850efd58204529e29a0e6557f397
RaviN <RaviN>
2011-04-25 03:05:00 -0500

    add from branch - Milestone v0.900 none

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/PayGetHistoryByPerson.proc.sql
@@ -0,0 +1,39 @@
+﻿--The source is 
+-- =============================================
+-- Author:		Anatoliy Lokshin
+-- Create date: 6-23-2008
+-- Update by:	Anatoliy Lokshin
+-- Update date:	6-24-2008
+-- Description:	Retrives a payment histiry for the specified person.
+-- =============================================
+CREATE PROCEDURE [dbo].[PayGetHistoryByPerson]
+(
+	@PersonId   INT
+)
+AS
+	SET NOCOUNT ON
+
+	SELECT p.PersonId,
+	       p.StartDate,
+	       p.EndDate,
+	       p.Amount,
+	       p.Timescale,
+	       p.AmountHourly,
+	       p.TimesPaidPerMonth,
+	       p.Terms,
+	       p.VacationDays,
+	       p.BonusAmount,
+	       p.BonusHoursToCollect,
+	       p.IsYearBonus,
+	       p.DefaultHoursPerDay,
+	       p.TimescaleName,
+		   p.SeniorityId,
+		   s.Name SeniorityName,
+		   p.PracticeId,
+		   pr.Name PracticeName
+	  FROM dbo.v_Pay AS p
+	  LEFT JOIN dbo.Seniority s ON s.SeniorityId = p.SeniorityId
+	  LEFT JOIN dbo.Practice pr on pr.PracticeId = p.PracticeId
+	 WHERE p.PersonId = @PersonId
+	ORDER BY p.StartDate
+
