c54a55b545e5f04bc2e37017d54fb010ccfd5c2f
RaviN <RaviN>
2011-04-25 03:05:00 -0500

    add from branch - Milestone v0.900 none

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/TimeTypeInsert.proc.sql
@@ -0,0 +1,22 @@
+﻿-- =============================================
+-- Author:		Nikita Goncharenko
+-- Create date: 2009-11-30
+-- Description:	Insert new time type
+-- =============================================
+CREATE PROCEDURE dbo.TimeTypeInsert
+	@Name VARCHAR(50)
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	IF EXISTS(SELECT 1 FROM dbo.TimeType WHERE [Name] = @Name)
+	BEGIN
+		DECLARE @Error NVARCHAR(200)
+		SET @Error = 'This time type already exists. Please add a different time type.'
+		RAISERROR(@Error,16,1)
+		RETURN
+	END
+
+	INSERT INTO dbo.TimeType ([Name]) VALUES (@Name)
+END
+
