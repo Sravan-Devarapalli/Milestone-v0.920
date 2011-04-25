169eb8b7602ddad26c3d52ce4c9fc699405ea31b
RaviN <RaviN>
2011-04-25 03:05:00 -0500

    add from branch - Milestone v0.900 none

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/RecruiterCommissionSave.proc.sql
@@ -0,0 +1,36 @@
+﻿-- =============================================
+-- Author:		Anatoliy Lokshin
+-- Create date: 5-21-2008
+-- Updated by:	Anatoliy Lokshin
+-- Update date:	8-29-2008
+-- Description:	Saves a recruiter commission for the given recruit
+-- =============================================
+CREATE PROCEDURE [dbo].[RecruiterCommissionSave]
+(
+	@RecruiterId          INT,
+	@RecruitId            INT,
+	@HoursToCollect       INT,
+	@Amount               DECIMAL(18,2),
+	@OLD_HoursToCollect   INT
+)
+AS
+	SET NOCOUNT ON
+
+	IF EXISTS (SELECT 1
+	             FROM dbo.RecruiterCommission AS c
+	            WHERE c.RecruitId = @RecruitId
+	              AND c.HoursToCollect = @OLD_HoursToCollect)
+	BEGIN
+		UPDATE dbo.RecruiterCommission
+		   SET Amount = @Amount,
+		       HoursToCollect = @HoursToCollect
+		 WHERE RecruitId = @RecruitId
+		   AND HoursToCollect = @OLD_HoursToCollect
+	END
+	ELSE
+	BEGIN
+		INSERT INTO dbo.RecruiterCommission
+		            (RecruiterId, RecruitId, HoursToCollect, Amount)
+		     VALUES (@RecruiterId, @RecruitId, @HoursToCollect, @Amount)
+	END
+
