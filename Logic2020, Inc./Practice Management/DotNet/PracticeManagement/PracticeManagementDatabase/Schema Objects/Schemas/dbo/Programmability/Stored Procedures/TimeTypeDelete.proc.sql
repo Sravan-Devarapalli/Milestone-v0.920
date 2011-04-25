8cd6d0150ca6c949a983b31e8637d3e6100108eb
RaviN <RaviN>
2011-04-25 03:05:00 -0500

    add from branch - Milestone v0.900 none

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/TimeTypeDelete.proc.sql
@@ -0,0 +1,14 @@
+﻿-- =============================================
+-- Author:		Nikita Goncharenko
+-- Create date: 2009-11-30
+-- Description:	Remove existing time type
+-- =============================================
+CREATE PROCEDURE TimeTypeDelete
+	@TimeTypeId INT
+AS
+BEGIN
+	SET NOCOUNT ON;
+	
+	DELETE FROM TimeType WHERE TimeTypeId = @TimeTypeId
+END
+
