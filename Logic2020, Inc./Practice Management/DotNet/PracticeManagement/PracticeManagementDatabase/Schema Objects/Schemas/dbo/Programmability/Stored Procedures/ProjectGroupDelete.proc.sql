17b4ccf4f043bf9f1dd04f7e76d8cd575be41abe
RaviN <RaviN>
2011-04-25 03:05:00 -0500

    add from branch - Milestone v0.900 none

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/ProjectGroupDelete.proc.sql
@@ -0,0 +1,25 @@
+﻿CREATE PROCEDURE ProjectGroupDelete
+	@GroupId		INT
+AS
+
+	SET NOCOUNT ON;
+	DECLARE @Result INT,
+			@Result_Success INT, @Result_GroupWasNotFound INT, @Result_GroupInUse INT
+	SELECT	@Result_Success = 0,
+			@Result_GroupWasNotFound = 1,
+			@Result_GroupInUse = 2
+		
+	SET @Result = CASE
+		WHEN NOT EXISTS(SELECT 1 FROM ProjectGroup WHERE GroupId = @GroupId)
+			THEN @Result_GroupWasNotFound
+		WHEN EXISTS(SELECT 1 FROM Project WHERE GroupId = @GroupId)
+			THEN @Result_GroupInUse
+		ELSE
+			@Result_Success
+		END
+	
+	IF @Result = @Result_Success
+		DELETE ProjectGroup WHERE GroupId = @GroupId
+
+	SELECT @Result Result
+
