6cd950c61aa908176c4825b91074aafaaf9a20b6
RaviN <RaviN>
2011-04-25 03:05:00 -0500

    add from branch - Milestone v0.900 none

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/OpportunityPersonInsert.proc.sql
@@ -0,0 +1,13 @@
+﻿CREATE PROCEDURE [dbo].[OpportunityPersonInsert]
+(
+	@OpportunityId INT, 
+	@PersonIdList NVARCHAR(MAX)
+)
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	INSERT INTO dbo.OpportunityPersons(OpportunityId, PersonId)
+	SELECT @OpportunityId AS OpportunityId, ResultId 
+	FROM [dbo].[ConvertStringListIntoTable] (@PersonIdList)
+END
