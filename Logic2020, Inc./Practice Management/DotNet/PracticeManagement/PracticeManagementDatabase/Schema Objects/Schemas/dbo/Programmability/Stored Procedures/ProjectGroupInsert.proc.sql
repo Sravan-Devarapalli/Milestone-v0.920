d5402dd34a99aabf1b8b401dccbd1be93f339ceb
RaviN <RaviN>
2011-04-25 03:05:00 -0500

    add from branch - Milestone v0.900 none

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/ProjectGroupInsert.proc.sql
@@ -0,0 +1,17 @@
+﻿CREATE PROCEDURE ProjectGroupInsert
+	@GroupId	INT OUT,
+	@ClientId	INT,
+	@Name		NVARCHAR(100),
+	@IsActive   BIT 
+AS
+	SET NOCOUNT ON
+	IF NOT EXISTS(SELECT 1 FROM Client WHERE ClientId=@ClientId)
+		OR EXISTS(SELECT 1 FROM ProjectGroup WHERE ClientId=@ClientId AND Name=@Name)
+		SET @GroupId = -1
+	ELSE
+	BEGIN
+		INSERT ProjectGroup(ClientId, Name,Active) VALUES (@ClientId, @Name,@IsActive)
+		SET @GroupId = SCOPE_IDENTITY()
+	END
+	 
+
