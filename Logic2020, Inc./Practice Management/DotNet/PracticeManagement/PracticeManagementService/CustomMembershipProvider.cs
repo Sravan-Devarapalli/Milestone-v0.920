255faa04caecdd74a827560e9def445aa15abe72
RaviN <RaviN>
2011-04-25 03:05:00 -0500

    add from branch - Milestone v0.900 none

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementService/CustomMembershipProvider.cs
@@ -0,0 +1,18 @@
+﻿namespace PraticeManagement
+{
+    using System.Web.Security;
+    using PracticeManagementService;
+
+    /// <summary>
+    /// Custom Membership provider to override reset password functionality.
+    /// </summary>
+    public sealed class CustomMembershipProvider : SqlMembershipProvider
+    {
+        public override string GeneratePassword()
+        {
+            string password = base.GeneratePassword();
+            return PasswordGeneratorUtility.GeneratePassword(password);
+        }
+    }
+}
+
