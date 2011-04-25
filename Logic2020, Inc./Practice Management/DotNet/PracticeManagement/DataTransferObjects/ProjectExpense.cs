1154c89fbcc2b8505dbd02306872ebf7ccd54841
RaviN <RaviN>
2011-04-25 03:05:00 -0500

    add from branch - Milestone v0.900 none

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/DataTransferObjects/ProjectExpense.cs
@@ -0,0 +1,50 @@
+﻿using System.Runtime.Serialization;
+
+namespace DataTransferObjects
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class ProjectExpense : IIdNameObject
+    {
+        [DataMember]
+        public int? Id { get; set; }
+
+        /// <summary>
+        /// Expense name
+        /// </summary>
+        [DataMember]
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Expense $
+        /// </summary>
+        [DataMember]
+        public decimal Amount { get; set; }
+
+        /// <summary>
+        /// Reimbursement %
+        /// </summary>
+        [DataMember]
+        public decimal Reimbursement { get; set; }
+
+        /// <summary>
+        /// Milestone
+        /// </summary>
+        [DataMember]
+        public Milestone Milestone { get; set; }
+
+        /// <summary>
+        /// Reimbursed $
+        /// </summary>
+        public decimal ReimbursementAmount
+        {
+            get
+            {
+                return 0.01M*Amount*Reimbursement;
+            }
+        }
+
+    }
+}
+
