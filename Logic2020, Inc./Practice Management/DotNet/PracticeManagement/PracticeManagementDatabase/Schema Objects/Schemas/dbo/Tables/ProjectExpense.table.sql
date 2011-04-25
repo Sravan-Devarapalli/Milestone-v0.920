1c51dcee7e9e5a1a202d7221583a5769f4679aba
RaviN <RaviN>
2011-04-25 03:05:00 -0500

    add from branch - Milestone v0.900 none

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/ProjectExpense.table.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[ProjectExpense] (
+    [Id]            INT             IDENTITY (1, 1) NOT NULL,
+    [Name]          NVARCHAR (50)   NOT NULL,
+    [Amount]        DECIMAL (18, 2) NOT NULL,
+    [Reimbursement] DECIMAL (18, 2) NOT NULL,
+    [MilestoneId]   INT             NOT NULL
+);
+
+
