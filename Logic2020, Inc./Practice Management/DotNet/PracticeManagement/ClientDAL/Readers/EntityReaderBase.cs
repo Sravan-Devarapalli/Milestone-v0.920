98ec880d0bcefed70439f73776088bc3f593d058
RaviN <RaviN>
2011-04-25 03:05:00 -0500

    add from branch - Milestone v0.900 none

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/ClientDAL/Readers/EntityReaderBase.cs
@@ -0,0 +1,30 @@
+﻿using System.Data.Common;
+
+namespace DataAccess.Readers
+{
+    public abstract class EntityReaderBase<T>
+    {
+        private DbDataReader _reader;
+
+        public DbDataReader Reader
+        {
+            get { return _reader; }
+        }
+
+        public virtual void SetReader(DbDataReader value)
+        {
+            _reader = value;
+        }
+
+        protected EntityReaderBase(DbDataReader reader)
+        {
+            _reader = reader;
+        }
+
+        protected EntityReaderBase()
+        {
+        }
+
+        public abstract T ReadEntity();
+    }
+}
