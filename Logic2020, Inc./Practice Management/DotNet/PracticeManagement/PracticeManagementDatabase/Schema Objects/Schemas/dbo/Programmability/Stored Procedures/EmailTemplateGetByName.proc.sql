16e74dcb2241b0b594be46070fa7255e47095acf
RaviN <RaviN>
2011-04-25 03:05:00 -0500

    add from branch - Milestone v0.900 none

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/EmailTemplateGetByName.proc.sql
@@ -0,0 +1,20 @@
+﻿CREATE PROCEDURE [dbo].[EmailTemplateGetByName]
+(
+	@EmailTemplateName	nvarchar(50)
+)
+AS
+BEGIN
+	SET NOCOUNT ON;
+	DECLARE @EmailTemplateNameLocal nvarchar(50)
+	SELECT @EmailTemplateNameLocal = @EmailTemplateName
+	Select 
+			et.EmailTemplateId
+			, et.EmailTemplateName
+			, et.EmailTemplateTo
+			, et.EmailTemplateCc
+			, et.EmailTemplateSubject
+			, et.EmailTemplateBody
+		FROM EmailTemplate AS et
+		WHERE et.EmailTemplateName = @EmailTemplateNameLocal
+END
+
