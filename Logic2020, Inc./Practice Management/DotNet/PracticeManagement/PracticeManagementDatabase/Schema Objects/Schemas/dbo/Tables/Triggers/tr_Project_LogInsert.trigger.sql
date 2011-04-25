bdb037cbc573022757ab115bb353178a01cc7c67
RaviN <RaviN>
2011-04-25 03:05:00 -0500

    add from branch - Milestone v0.900 none

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/Triggers/tr_Project_LogInsert.trigger.sql
@@ -0,0 +1,52 @@
+﻿-- =============================================
+-- Author:		Anatoliy Lokshin
+-- Create date: 11-25-2008
+-- Updated by:	
+-- Update date:	
+-- Description:	Logs the inserting into the dbo.Project table.
+-- =============================================
+CREATE TRIGGER tr_Project_LogInsert
+ON dbo.Project
+AFTER INSERT
+AS
+BEGIN
+	-- Ensure the temporary table exists
+	EXEC SessionLogPrepare @UserLogin = NULL
+
+	-- Log an activity
+	INSERT INTO dbo.UserActivityLog
+	            (ActivityTypeID,
+	             SessionID,
+	             SystemUser,
+	             Workstation,
+	             ApplicationName,
+	             UserLogin,
+	             PersonID,
+	             LastName,
+	             FirstName,
+				 Data,
+	             LogData)
+	SELECT 3 AS ActivityTypeID /* insert only */,
+	       l.SessionID,
+	       l.SystemUser,
+	       l.Workstation,
+	       l.ApplicationName,
+	       l.UserLogin,
+	       l.PersonID,
+	       l.LastName,
+	       l.FirstName,
+	       Data =  CONVERT(NVARCHAR(MAX),(SELECT NEW_VALUES.ProjectId, NEW_VALUES.Name
+					    FROM inserted AS NEW_VALUES
+			           WHERE NEW_VALUES.ProjectId = i.ProjectId
+					  FOR XML AUTO, ROOT('Project'))),
+			LogData = (SELECT NEW_VALUES.ProjectId
+					    FROM inserted AS NEW_VALUES
+			           WHERE NEW_VALUES.ProjectId = i.ProjectId
+					  FOR XML AUTO, ROOT('Project'), TYPE)
+	  FROM inserted AS i
+	       INNER JOIN dbo.SessionLogData AS l ON l.SessionID = @@SPID
+	  
+	  -- End logging session
+	 EXEC dbo.SessionLogUnprepare
+END
+
