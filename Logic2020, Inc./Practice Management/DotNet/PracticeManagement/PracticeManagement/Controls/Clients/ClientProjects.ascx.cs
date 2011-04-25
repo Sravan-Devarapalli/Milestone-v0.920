777c78810894ff17aaef4bdf69d2dff40010d4f6
RaviN <RaviN>
2011-04-25 03:05:00 -0500

    add from branch - Milestone v0.900 none

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/Controls/Clients/ClientProjects.ascx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Web.UI.WebControls;
+
+namespace PraticeManagement.Controls.Clients
+{
+    public partial class ClientProjects : System.Web.UI.UserControl
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnProjectName_Command(object sender, CommandEventArgs e)
+        {
+            ((PracticeManagementPageBase) Page).Redirect(
+                string.Format(Constants.ApplicationPages.DetailRedirectFormat,
+                                   Constants.ApplicationPages.ProjectDetail,
+                                   e.CommandArgument));
+        }
+    }
+}
+
