203d8b0ca43a598aebc3371e2b8c5f1355ded06a
RaviN <RaviN>
2011-04-25 03:05:00 -0500

    add from branch - Milestone v0.900 none

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/Project.table.sql
@@ -0,0 +1,21 @@
+﻿CREATE TABLE [dbo].[Project] (
+    [ClientId]         INT             NOT NULL,
+    [ProjectId]        INT             IDENTITY (1, 1) NOT NULL,
+    [Discount]         DECIMAL (18, 2) NOT NULL,
+    [Terms]            INT             NOT NULL,
+    [Name]             NVARCHAR (100)  NOT NULL,
+    [PracticeId]       INT             NOT NULL,
+    [StartDate]        DATETIME        NULL,
+    [EndDate]          DATETIME        NULL,
+    [ProjectStatusId]  INT             NOT NULL,
+    [ProjectNumber]    NVARCHAR (12)   NOT NULL,
+    [BuyerName]        NVARCHAR (100)  NULL,
+    [OpportunityId]    INT             NULL,
+    [GroupId]          INT             NULL,
+    [IsChargeable]     BIT             NOT NULL,
+    [ProjectManagerId] INT             NOT NULL,
+	[DirectorId]	   INT			   NULL,
+    FOREIGN KEY ([GroupId]) REFERENCES [dbo].[ProjectGroup] ([GroupId]) ON DELETE NO ACTION ON UPDATE NO ACTION
+);
+
+
