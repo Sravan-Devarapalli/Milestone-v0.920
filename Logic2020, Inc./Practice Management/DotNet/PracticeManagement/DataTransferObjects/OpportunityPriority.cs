c477a94167911b2f16633593bf541b93838c658b
RaviN <RaviN>
2011-04-25 03:05:00 -0500

    add from branch - Milestone v0.900 none

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/DataTransferObjects/OpportunityPriority.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+
+namespace DataTransferObjects
+{
+    [Serializable]
+    [DataContract]
+    public class OpportunityPriority
+    {
+        [DataMember]
+        public int Id
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public string Priority
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public string Description
+        {
+            get;
+            set;
+        }
+    }
+}
+
