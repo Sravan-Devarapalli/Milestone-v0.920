4d1eed25a011bbf3984ea20b2f5a308aa37dd6aa
RaviN <RaviN>
2011-04-25 03:05:00 -0500

    add from branch - Milestone v0.900 none

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/DataTransferObjects/ContextObjects/OpportunitySortingContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+
+namespace DataTransferObjects.ContextObjects
+{
+    [DataContract]
+    [Serializable]
+    public class OpportunitySortingContext
+    {
+        [DataMember]
+        public string OrderBy { get; set; }
+
+        [DataMember]
+        public string SortDirection { get; set; }
+    }
+}
+
