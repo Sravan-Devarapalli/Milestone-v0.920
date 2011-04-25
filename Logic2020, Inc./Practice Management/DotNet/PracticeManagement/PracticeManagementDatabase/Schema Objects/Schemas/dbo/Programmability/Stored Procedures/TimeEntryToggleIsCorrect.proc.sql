72d8f5b455a309c6b1e94ece44f3702c926269cb
RaviN <RaviN>
2011-04-25 03:05:00 -0500

    add from branch - Milestone v0.900 none

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/TimeEntryToggleIsCorrect.proc.sql
@@ -0,0 +1,14 @@
+﻿-- =============================================
+-- Author:		Nikita Goncharenko
+-- Create date: 2010-02-19
+-- Description:	Toggles is correct
+-- =============================================
+CREATE PROCEDURE dbo.TimeEntryToggleIsCorrect
+	@TimeEntryId INT 
+AS
+BEGIN
+	UPDATE dbo.TimeEntries
+	SET IsCorrect = 1 - IsCorrect
+	WHERE TimeEntryId = @TimeEntryId
+END
+
