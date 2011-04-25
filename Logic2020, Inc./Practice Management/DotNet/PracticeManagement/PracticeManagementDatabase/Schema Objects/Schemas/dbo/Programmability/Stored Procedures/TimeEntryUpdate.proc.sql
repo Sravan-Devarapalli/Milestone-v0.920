00aae146530a5583706856c115f5920c55f7eb1e
RaviN <RaviN>
2011-04-25 03:05:00 -0500

    add from branch - Milestone v0.900 none

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/TimeEntryUpdate.proc.sql
@@ -0,0 +1,54 @@
+﻿-- =============================================
+-- Author:		Nikita Goncharenko
+-- Create date: 2009-12-08
+-- Description:	Updates time entry record
+-- =============================================
+CREATE PROCEDURE dbo.TimeEntryUpdate
+	@TimeEntryId INT,
+	@EntryDate datetime,
+	@MilestoneDate datetime,
+	@MilestonePersonId int,
+	@ActualHours real,
+	@ForecastedHours real,
+	@TimeTypeId int,
+	@ModifiedBy int,
+	@Note varchar(1000),
+	@IsChargeable BIT,
+	@IsReviewed BIT = NULL,
+	@IsCorrect BIT,
+	@DefaultMilestoneId INT,
+	@PersonId INT   
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	declare @OldIsReviewed bit
+
+	IF @MilestonePersonId = 0 
+            EXECUTE dbo.MilestonePersonEntryCreateProgrammatically @PersonId = @PersonId, --  int
+                @MilestoneId = @DefaultMilestoneId, @MilestoneDate = @PersonId, --  datetime
+                @ActualHours = @ActualHours, --  real
+                @DefaultMpId = @DefaultMilestoneId,
+                @NewMilestonePersonId = @MilestonePersonId OUTPUT
+
+	-- To change TE status from declined according to #2110
+	select @OldIsReviewed = [IsReviewed]
+	from [dbo].[TimeEntries]
+	WHERE TimeEntryId = @TimeEntryId
+	
+	UPDATE [dbo].[TimeEntries]
+	   SET [EntryDate] = @EntryDate
+		  ,[ModifiedDate] = GETDATE()
+		  ,[MilestonePersonId] = @MilestonePersonId
+		  ,[ActualHours] = @ActualHours
+		  ,[ForecastedHours] = @ForecastedHours
+		  ,[TimeTypeId] = @TimeTypeId
+		  ,[ModifiedBy] = @ModifiedBy
+		  ,[Note] = @Note
+		  ,[IsChargeable] = @IsChargeable
+		  ,[MilestoneDate] = @MilestoneDate
+		  ,[IsReviewed] = case when @OldIsReviewed = 0 and @IsReviewed = 0 then NULL else @IsReviewed end
+		  ,[IsCorrect] = @IsCorrect
+	WHERE TimeEntryId = @TimeEntryId
+END
+
