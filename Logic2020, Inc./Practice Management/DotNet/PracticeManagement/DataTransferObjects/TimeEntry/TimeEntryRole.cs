d34d136c32a27f417b48a1251b45a130b1c8e2e1
RaviN <RaviN>
2011-04-25 03:05:00 -0500

    add from branch - Milestone v0.900 none

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/DataTransferObjects/TimeEntry/TimeEntryRole.cs
@@ -0,0 +1,25 @@
+﻿
+namespace DataTransferObjects.TimeEntry
+{
+    /// <summary>
+    /// Time Entry Roles
+    /// </summary>
+    public enum TimeEntryRole
+    {
+        /// <summary>
+        /// Standard User
+        /// </summary>
+        StandardUser,
+
+        /// <summary>
+        /// Management User
+        /// </summary>
+        ManagementUser,
+
+        /// <summary>
+        /// Accounting Administrator
+        /// </summary>
+        AccountingAdministrator
+    }
+}
+
