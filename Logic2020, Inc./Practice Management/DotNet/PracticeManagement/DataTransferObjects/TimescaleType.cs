8e4b20d4ef42b6d4336a3ee18473e5e616c3337d
RaviN <RaviN>
2011-04-25 03:05:00 -0500

    add from branch - Milestone v0.900 none

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/DataTransferObjects/TimescaleType.cs
@@ -0,0 +1,36 @@
+﻿using System.Runtime.Serialization;
+
+namespace DataTransferObjects
+{
+	/// <summary>
+	/// Determines the list of employee payment types.
+	/// </summary>
+	[DataContract]
+	public enum TimescaleType
+	{
+		/// <summary>
+		/// An employee recieve a hourly earnings.
+		/// </summary>
+		[EnumMember]
+		Hourly = 1,
+
+		/// <summary>
+		/// An employee recieve a monthly salary.
+		/// </summary>
+		[EnumMember]
+		Salary = 2,
+
+		/// <summary>
+		/// 1099
+		/// </summary>
+		[EnumMember]
+        _1099Ctc = 3,
+
+        /// <summary>
+        /// % of Revenue
+        /// </summary>
+        [EnumMember]
+        PercRevenue = 4
+	}
+}
+
