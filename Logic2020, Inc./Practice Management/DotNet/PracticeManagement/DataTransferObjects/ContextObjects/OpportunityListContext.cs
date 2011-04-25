048c0b1fe2a25615ace01d26836567e4037cb82f
RaviN <RaviN>
2011-04-25 03:05:00 -0500

    add from branch - Milestone v0.900 none

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/DataTransferObjects/ContextObjects/OpportunityListContext.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+
+namespace DataTransferObjects.ContextObjects
+{
+    [DataContract]
+    [Serializable]
+    public class OpportunityListContext
+    {
+        public OpportunityListContext()
+        {
+            ActiveClientsOnly = true;
+        }
+
+        [DataMember]
+        public bool ActiveClientsOnly { get; set; }
+
+        [DataMember]
+        public string SearchText { get; set; }
+
+        [DataMember]
+        public int? ClientId { get; set; }
+
+        [DataMember]
+        public int? SalespersonId { get; set; }
+
+        [DataMember]
+        public int? TargetPersonId { get; set; }
+
+        [DataMember]
+        public int? CurrentId { get; set; }
+    }
+}
+
