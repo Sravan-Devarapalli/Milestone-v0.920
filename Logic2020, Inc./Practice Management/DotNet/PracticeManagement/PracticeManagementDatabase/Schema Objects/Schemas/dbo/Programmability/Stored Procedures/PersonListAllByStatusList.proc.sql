e8f2d6cb7e7a0ddc7a820d280f0abd4e1078f53d
RaviN <RaviN>
2011-04-25 03:05:00 -0500

    add from branch - Milestone v0.900 none

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/PersonListAllByStatusList.proc.sql
@@ -0,0 +1,43 @@
+﻿CREATE PROCEDURE [dbo].[PersonListAllByStatusList] 
+(
+	@PersonStatusIdsList NVARCHAR(225) = NULL,
+	@PersonId	INT = NULL
+)
+AS
+BEGIN
+	SET NOCOUNT ON
+
+	DECLARE @Query NVARCHAR(4000),
+			@Where NVARCHAR(4000)
+			
+	DECLARE @PersonSeniorityId INT
+		
+	SET @Where = ''
+
+	SET @Query='SELECT PersonId,
+					   FirstName,
+					   LastName,
+					   IsDefaultManager
+				FROM dbo.Person'
+	  
+	  IF(ISNULL(@PersonStatusIdsList,'')<>'')
+	  BEGIN
+		SET @Where = @Where + ' WHERE PersonStatusId IN (' + @PersonStatusIdsList + ')'
+	  END
+	  IF (ISNULL(@PersonId,'') <> '')
+	  BEGIN
+		SELECT @PersonSeniorityId = SeniorityId 
+		FROM Person
+		WHERE PersonId = @PersonId
+				
+		SET @Where = (CASE WHEN @Where='' THEN ' WHERE ' ELSE @Where+ ' AND ' END )
+						+ 'PersonId = ' + CONVERT(NVARCHAR,@PersonId) 
+						+ (CASE WHEN @PersonSeniorityId <= 65 -- According to 2656, Managers and up should be able to see their subordinates, but not equals.
+								THEN ' OR SeniorityId > '+ CONVERT(NVARCHAR,@PersonSeniorityId)
+								ELSE '' END
+							)
+	  END
+	 SET @Query = @Query + @Where
+	 EXEC(@Query)
+
+END
