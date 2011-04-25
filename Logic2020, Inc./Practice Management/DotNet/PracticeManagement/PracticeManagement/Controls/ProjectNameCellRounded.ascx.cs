0622fa12c194d88ec87e44096717de1063e032e7
RaviN <RaviN>
2011-04-25 03:05:00 -0500

    add from branch - Milestone v0.900 none

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/Controls/ProjectNameCellRounded.ascx.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace PraticeManagement.Controls
+{
+    public partial class ProjectNameCellRounded : System.Web.UI.UserControl
+    {
+        #region Property
+
+        /// <summary>
+        /// Project Name Button Id
+        /// </summary>
+        public string ButtonProjectNameId
+        {
+            set
+            {
+                btnProjectName.ID = value;
+            }
+        }
+
+        /// <summary>
+        /// Main Button CssClass Name
+        /// </summary>
+        public string ButtonCssClass
+        {
+            set
+            {
+                btnProjectName.CssClass = value;
+            }
+        }
+
+        /// <summary>
+        /// Project Name Button Text
+        /// </summary>
+        public string ButtonProjectNameText
+        {
+            set
+            {
+                btnProjectName.Text = value;
+            }
+        }
+
+        /// <summary>
+        /// Project Name Button ToolTip content
+        /// </summary>
+        public string ButtonProjectNameToolTip
+        {
+            set
+            {
+                lblTooltipContent.Text = value;
+            }
+        }
+
+        /// <summary>
+        /// Project Name Button ToolTip content
+        /// </summary>
+        public string ButtonProjectNameHref
+        {
+            set
+            {
+                btnProjectName.NavigateUrl = value;
+            }
+        }
+
+
+        public int ToolTipOffsetX
+        {
+            set
+            {
+                hm.OffsetX = value;
+            }
+        }
+
+        public int ToolTipOffsetY
+        {
+            set
+            {
+                hm.OffsetY = value;
+            }
+        }
+
+        public AjaxControlToolkit.HoverMenuPopupPosition ToolTipPopupPosition
+        {
+            set
+            {
+                hm.PopupPosition = value;
+            }
+        }
+        #endregion
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        }
+    }
+}
