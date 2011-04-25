0c4fcfdbcbf0edeb8e24e7bcf523ff01f1831c3c
RaviN <RaviN>
2011-04-25 03:05:00 -0500

    add from branch - Milestone v0.900 none

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/Controls/Generic/LoadingProgress.ascx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace PraticeManagement.Controls.Generic
+{
+    public partial class LoadingProgress : System.Web.UI.UserControl
+    {
+        public string DisplayText { get; set; }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Page.Title == "Practice Management - Opportunity Details")
+            {
+                DisplayText = "Saving ...";
+            }
+            else
+            {
+                DisplayText = "Please Wait..";
+            }            
+        }
+    }
+}
