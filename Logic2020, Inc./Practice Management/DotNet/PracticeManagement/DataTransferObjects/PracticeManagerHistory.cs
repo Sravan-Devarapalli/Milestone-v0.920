ddf604766ec82e8d95d24068905384df8b247991
RaviN <RaviN>
2011-04-25 03:05:00 -0500

    add from branch - Milestone v0.900 none

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/DataTransferObjects/PracticeManagerHistory.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+
+namespace DataTransferObjects
+{
+    [DataContract]
+    [Serializable]
+    public class PracticeManagerHistory
+    {
+
+        [DataMember]
+        public int PracticeId
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public int PracticeManagerId
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public string PracticeManagerLastName
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public string PracticeManagerFirstName
+        {
+            get;
+            set;
+        }
+        /// <summary>
+        /// Date from which the practice manager owned up the practice.
+        /// </summary>
+        [DataMember]
+        public DateTime StartDate
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// End Date, the practice manager owned up the practice.
+        /// </summary>
+        [DataMember]
+        public DateTime EndDate
+        {
+            get;
+            set;
+        }
+    }
+}
+
