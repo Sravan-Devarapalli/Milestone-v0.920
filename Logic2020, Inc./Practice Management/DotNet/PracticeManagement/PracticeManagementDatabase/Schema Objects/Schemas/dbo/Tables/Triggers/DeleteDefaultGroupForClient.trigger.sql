c2c48d3fa3785660e4fa382f2c1bcb361850d506
RaviN <RaviN>
2011-04-25 03:05:00 -0500

    add from branch - Milestone v0.900 none

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/Triggers/DeleteDefaultGroupForClient.trigger.sql
@@ -0,0 +1,20 @@
+﻿
+-- =============================================
+-- Author:		Nikita Goncharenko
+-- Create date: 2009-10-14
+-- Description:	Creates default group on each
+-- =============================================
+CREATE TRIGGER DeleteDefaultGroupForClient 
+   ON  dbo.Client 
+   AFTER DELETE
+AS 
+BEGIN
+	SET NOCOUNT ON;
+
+	DELETE FROM dbo.ProjectGroup 
+	WHERE ClientId IN ( 
+			SELECT d.ClientId FROM deleted AS d
+		)
+
+END
+
