429e03efddacfc94bdd3281862ef974e3a5830ef
RaviN <RaviN>
2011-04-25 03:05:00 -0500

    add from branch - Milestone v0.900 none

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/MilestonePersonDeleteEntries.proc.sql
@@ -0,0 +1,18 @@
+﻿-- =============================================
+-- Author:		Anatoliy Lokshin
+-- Create date: 9-09-2008
+-- Updated by:	
+-- Update date:	
+-- Description:	Removes persons-milestones details for the specified milestone and person.
+-- =============================================
+CREATE PROCEDURE [dbo].[MilestonePersonDeleteEntries]
+(
+	@MilestonePersonId   INT
+)
+AS
+	SET NOCOUNT ON
+	SET NOCOUNT ON
+		DELETE
+		  FROM dbo.MilestonePersonEntry
+		 WHERE MilestonePersonId = @MilestonePersonId
+
