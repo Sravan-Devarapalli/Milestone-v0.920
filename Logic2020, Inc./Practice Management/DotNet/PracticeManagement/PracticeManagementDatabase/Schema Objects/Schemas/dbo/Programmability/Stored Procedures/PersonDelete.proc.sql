07e1b3587f8a0da0a45be2b4d783f0b220418e4d
RaviN <RaviN>
2011-04-25 03:05:00 -0500

    add from branch - Milestone v0.900 none

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/PersonDelete.proc.sql
@@ -0,0 +1,32 @@
+﻿-- =============================================
+-- Author:		Anatoliy Lokshin
+-- Create date: 11-10-2008
+-- Updated by:	
+-- Update date: 
+-- Description:	Deletes a person from the database.
+-- =============================================
+CREATE PROCEDURE [dbo].[PersonDelete]
+(
+	@PersonId   INT
+)
+AS
+	SET NOCOUNT ON
+
+	DELETE dbo.DefaultCommission
+	 WHERE PersonId = @PersonId
+
+	DELETE dbo.Pay
+	 WHERE Person = @PersonId
+
+	DELETE dbo.PersonCalendar
+	 WHERE PersonId = @PersonId
+
+	DELETE dbo.PersonCalendarAuto
+	 WHERE PersonId = @PersonId
+
+	 DELETE dbo.PersonStatusHistory
+	 WHERE PersonId = @PersonId
+
+	DELETE dbo.Person
+	 WHERE PersonId = @PersonId
+
