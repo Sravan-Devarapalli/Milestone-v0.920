3e24e168e86ee221ca42c93bfe37d73b0ad205ab
RaviN <RaviN>
2011-04-25 03:05:00 -0500

    add from branch - Milestone v0.900 none

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/DefaultRecruiterCommissionListByPerson.proc.sql
@@ -0,0 +1,21 @@
+﻿-- =============================================
+-- Author:		Anatoliy Lokshin
+-- Create date: 9-02-2008
+-- Updated by:	
+-- Update date:	
+-- Description:	Selects a list of the default recruiter commissions for the specified person
+-- =============================================
+CREATE PROCEDURE [dbo].[DefaultRecruiterCommissionListByPerson]
+(
+	@PersonId   INT
+)
+AS
+	SET NOCOUNT ON
+
+	SELECT h.DefaultRecruiterCommissionHeaderId, h.PersonId, h.StartDate,
+	       CASE h.EndDate WHEN dbo.GetFutureDate() THEN CAST(NULL AS DATETIME) ELSE h.EndDate END AS EndDate,
+	       dbo.MakeDefaultRecruiterCommissionText(h.defaultrecruitercommissionheaderid) AS TextLine
+	  FROM dbo.DefaultRecruiterCommissionHeader as h
+	 WHERE h.PersonId = @PersonId
+	ORDER BY h.StartDate
+
