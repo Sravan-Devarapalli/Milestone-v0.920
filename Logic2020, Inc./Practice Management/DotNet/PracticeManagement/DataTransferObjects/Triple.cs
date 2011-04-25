933be5bfbccfbd059f5c7bd4070aa97269045389
RaviN <RaviN>
2011-04-25 03:05:00 -0500

    add from branch - Milestone v0.900 none

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/DataTransferObjects/Triple.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace DataTransferObjects
+{
+    /// <summary>
+    /// Provides a basic utility class that is used to store 3 typed objects
+    /// </summary>
+    [DataContract]
+    [Serializable]
+    public class Triple<A, B, C>
+    {
+        #region Properties
+
+	    /// <summary>
+        /// Gets or sets the First object.
+        /// </summary>
+        [DataMember]
+        public A First
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Gets or sets the Second object.
+        /// </summary>
+        [DataMember]
+        public B Second
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Gets or sets the Third object.
+        /// </summary>
+        [DataMember]
+        public C Third
+        {
+            get;
+            set;
+        }
+
+	    #endregion    
+
+        /// <summary>
+        /// Init constructor of Triple.
+        /// </summary>
+            public Triple(A first, B second, C third)
+        {
+            this.First = first;
+            this.Second = second;
+            this.Third = third;
+        }
+    }
+}
+
