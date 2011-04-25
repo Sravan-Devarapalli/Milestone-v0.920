78fd3df474e1d25e8d80af353aff00a953c259d2
RaviN <RaviN>
2011-04-25 03:05:00 -0500

    add from branch - Milestone v0.900 none

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/Reports/UtilizationTimeline.aspx.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace PraticeManagement.Reporting
+{
+    public partial class UtilizationTimeline : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public void RegisterClientScripts()
+        {
+            System.Web.UI.ScriptManager.RegisterClientScriptBlock(this, this.GetType(), "", "EnableDisableRadioButtons();", true);
+        }
+    }
+}
+
