16146dd86bb589a33ae34c7da7be49d3c0fc8c9e
RaviN <RaviN>
2011-04-25 03:05:00 -0500

    add from branch - Milestone v0.900 none

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/Utils/Calendar.cs
@@ -0,0 +1,32 @@
+﻿using DataTransferObjects;
+using PraticeManagement.CalendarService;
+using System;
+
+namespace PraticeManagement.Utils
+{
+    public class Calendar
+    {
+        public const int DefaultHoursPerWeek = 40;
+
+        public static string GetCssClassByCalendarItem(CalendarItem calendarItem)
+        {
+            if (calendarItem == null)
+                return string.Empty;
+
+            return calendarItem.DayOff
+                        ? (calendarItem.CompanyDayOff ? Resources.Controls.CssDayOff : Resources.Controls.CssCompanyDayOn)
+                        : (calendarItem.CompanyDayOff ? Resources.Controls.CssCompanyDayOff : Resources.Controls.CssDayOn);
+        }
+
+        public static decimal GetWorkingHoursInCurrentYear(decimal hoursPerWeek)
+        {
+            //int companyHolidays=0;
+            //using (var service = new CalendarServiceClient())
+            //{
+            //    companyHolidays = service.GetCompanyHolidays(DateTime.Now.Year);
+            //}
+            //return (52 * hoursPerWeek) - (companyHolidays * hoursPerWeek / 5);
+            return (52 * hoursPerWeek);
+        }
+    }
+}
