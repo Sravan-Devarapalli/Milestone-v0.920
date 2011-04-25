3d0a6eea9912110455b9e36e1c2563add95d9e15
RaviN <RaviN>
2011-04-25 03:05:00 -0500

    add from branch - Milestone v0.900 none

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/DefaultComissionSave.proc.sql
@@ -0,0 +1,56 @@
+﻿--The source is 
+-- =============================================
+-- Author:		Anatoliy Lokshin
+-- Create date: 6-2-2008
+-- Description:	Saves a person's default actual commisions
+-- =============================================
+CREATE PROCEDURE [dbo].[DefaultComissionSave]
+(
+	@PersonId           INT,
+	@FractionOfMargin   DECIMAL(18,2),
+	@Type               INT,
+	@MarginTypeId       INT
+)
+AS
+	SET NOCOUNT ON
+	SET XACT_ABORT ON
+
+	DECLARE @Today DATETIME
+	SET @Today = dbo.Today()
+
+	BEGIN TRAN
+
+	IF EXISTS(SELECT 1
+	            FROM dbo.[DefaultCommission] AS c
+	           WHERE c.PersonId = @PersonId
+	             AND c.[Type] = @Type
+	             AND c.StartDate = @Today)
+	BEGIN
+		-- The value was already changed today
+		UPDATE dbo.[DefaultCommission]
+		   SET FractionOfMargin = @FractionOfMargin,
+		       MarginTypeId = @MarginTypeId
+		 WHERE PersonId = @PersonId
+		   AND [Type] = @Type
+		   AND StartDate = @Today
+	END
+	ELSE
+	BEGIN
+		-- There is no value set today
+
+		-- Expire old data
+		UPDATE dbo.[DefaultCommission]
+		   SET EndDate = @Today
+		 WHERE PersonId = @PersonId
+		   AND [Type] = @Type
+		   AND @Today >= StartDate
+		   AND @Today < EndDate
+
+		-- Set new comission
+		INSERT INTO dbo.[DefaultCommission]
+		            (PersonId, StartDate, EndDate, FractionOfMargin, [type], MarginTypeId)
+		     VALUES (@PersonId, @Today, '2029-12-31', @FractionOfMargin, @Type, @MarginTypeId)
+	END
+
+	COMMIT
+
