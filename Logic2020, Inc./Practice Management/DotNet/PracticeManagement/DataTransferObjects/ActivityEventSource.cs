48f01b60ef1044597648d1d706b36085b62d2214
RaviN <RaviN>
2011-04-25 03:05:00 -0500

    add from branch - Milestone v0.900 none

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/DataTransferObjects/ActivityEventSource.cs
@@ -0,0 +1,31 @@
+﻿using System.Runtime.Serialization;
+
+namespace DataTransferObjects
+{
+	/// <summary>
+	/// Determines event sources for the user activity log.
+	/// </summary>
+	[DataContract]
+	public enum ActivityEventSource
+	{
+		[EnumMember]
+		All = 1,
+		[EnumMember]
+		Person = 2,
+		[EnumMember]
+		Project = 3,
+		[EnumMember]
+		Error = 4,
+		[EnumMember]
+		ProjectAndMilestones = 5,
+        [EnumMember]
+        TargetPerson = 6,
+        [EnumMember]
+        TimeEntry = 7,
+        [EnumMember]
+        Opportunity = 8,
+        [EnumMember]
+        Milestone = 9,
+    }
+}
+
