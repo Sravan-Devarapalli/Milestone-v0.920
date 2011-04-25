0f5635462e8336dfb47de93f01c831490baf8ddb
RaviN <RaviN>
2011-04-25 03:05:00 -0500

    add from branch - Milestone v0.900 none

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/GetOpportunityPersons.proc.sql
@@ -0,0 +1,16 @@
+﻿CREATE PROCEDURE [dbo].[GetOpportunityPersons]
+	@OpportunityId int
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	IF EXISTS(SELECT 1 FROM dbo.OpportunityPersons WHERE OpportunityId = @OpportunityId)
+	BEGIN
+		SELECT   op.PersonId
+				,p.FirstName
+				,p.LastName
+		FROM dbo.OpportunityPersons op
+		JOIN dbo.Person p ON p.PersonId = op.PersonId
+		WHERE op.OpportunityId = @OpportunityId
+	END
+END
