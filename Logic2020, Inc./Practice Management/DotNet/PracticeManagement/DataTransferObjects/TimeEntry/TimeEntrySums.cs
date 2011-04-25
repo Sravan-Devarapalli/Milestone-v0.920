6443e445dd66eb40760750f0c6b394302b382811
RaviN <RaviN>
2011-04-25 03:05:00 -0500

    add from branch - Milestone v0.900 none

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/DataTransferObjects/TimeEntry/TimeEntrySums.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+
+namespace DataTransferObjects.TimeEntry
+{
+    [DataContract]
+    [Serializable]
+    public class TimeEntrySums
+    {
+        [DataMember]
+        public double TotalActualHours { get; set; }
+
+        [DataMember]
+        public double TotalForecastedHours { get; set; }
+    }
+}
+
