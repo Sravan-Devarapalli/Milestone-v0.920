4ef361bd1550e204e7dccdbedfa6e19ba156a230
RaviN <RaviN>
2011-04-25 03:05:00 -0500

    add from branch - Milestone v0.900 none

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/PersonListShortByRoleAndStatus.proc.sql
@@ -0,0 +1,20 @@
+﻿CREATE PROCEDURE [dbo].[PersonListShortByRoleAndStatus]
+	@PersonStatusId	INT = NULL,
+	@RoleName		NVARCHAR(256) = NULL
+AS
+BEGIN
+	SELECT DISTINCT p.PersonId,
+					p.FirstName,
+					p.LastName,
+					p.IsDefaultManager
+	FROM dbo.Person p
+	LEFT JOIN dbo.aspnet_Users u
+	ON p.Alias = u.UserName
+	LEFT JOIN dbo.aspnet_UsersInRoles uir
+	ON u.UserId = uir.UserId
+	LEFT JOIN dbo.aspnet_Roles ur
+	ON ur.RoleId = uir.RoleId
+	WHERE (p.PersonStatusId = @PersonStatusId OR @PersonStatusId IS NULL)
+			AND (ur.RoleName = @RoleName OR @RoleName IS NULL)
+	ORDER BY LastName, FirstName
+END
