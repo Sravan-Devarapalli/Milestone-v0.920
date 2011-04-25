fa346dc75f55161fde995f29d12b04cbf5bef407
RaviN <RaviN>
2011-04-25 03:05:00 -0500

    add from branch - Milestone v0.900 none

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/MilestoneGetById.proc.sql
@@ -0,0 +1,39 @@
+﻿-- =============================================
+-- Author:		Anatoliy Lokshin
+-- Create date: 5-22-2008
+-- Updated by:	Anatoliy Lokshin
+-- Update date:	8-26-2008
+-- Description:	Gets a Milestone
+-- =============================================
+CREATE PROCEDURE dbo.MilestoneGetById
+(
+	@MilestoneId   INT
+)
+AS
+	SET NOCOUNT ON
+
+	SELECT m.ClientId,
+	       m.ProjectId,
+	       m.MilestoneId,
+	       m.Description,
+	       m.Amount,
+	       m.StartDate,
+	       m.ProjectedDeliveryDate,
+	       m.ActualDeliveryDate,
+	       m.IsHourlyAmount,
+	       m.ProjectName,
+	       m.ProjectStartDate,
+	       m.ProjectEndDate,
+	       m.Discount,
+	       m.ClientName,
+	       m.ExpectedHours,
+	       m.SalesCommission,
+	       m.PersonCount,
+	       m.ProjectedDuration,
+	       m.ConsultantsCanAdjust,
+	       m.ClientIsChargeable,
+	       m.ProjectIsChargeable,
+	       m.MilestoneIsChargeable
+	  FROM dbo.v_Milestone AS m
+	 WHERE m.MilestoneId = @MilestoneId
+
