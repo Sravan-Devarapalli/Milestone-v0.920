a90cb52bb92e62e6d23d63732d69f5c40b9211aa
RaviN <RaviN>
2011-04-25 03:05:00 -0500

    add from branch - Milestone v0.900 none

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/ClientGetById.proc.sql
@@ -0,0 +1,24 @@
+﻿--The source is 
+-- =============================================
+-- Author:		Anatoliy Lokshin
+-- Create date: 5-21-2008
+-- Description:	Gets a person
+-- =============================================
+CREATE PROCEDURE dbo.ClientGetById
+(
+	@ClientId	INT
+)
+AS
+	SET NOCOUNT ON
+
+	SELECT c.ClientId,
+	       c.DefaultDiscount,
+	       c.DefaultTerms,
+	       c.DefaultSalespersonID,
+		   c.DefaultDirectorID,
+	       c.Name,
+	       c.Inactive,
+	       c.IsChargeable
+	  FROM dbo.Client AS c
+	 WHERE c.ClientId = @ClientId
+
