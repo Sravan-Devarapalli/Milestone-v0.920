a697a4105c36790df744a470dd65d29ebc3caf06
RaviN <RaviN>
2011-04-25 03:05:00 -0500

    add from branch - Milestone v0.900 none

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/DataTransferObjects/SMTPSettings.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+
+namespace DataTransferObjects
+{
+    [DataContract]
+    [Serializable]
+    public class SMTPSettings
+    {
+        [DataMember]
+        public string MailServer { get; set; }
+
+        [DataMember]
+        public int PortNumber { get; set; }
+
+        [DataMember]
+        public bool SSLEnabled { get; set; }
+
+        [DataMember]
+        public string UserName { get; set; }
+
+        [DataMember]
+        public string Password { get; set; }
+
+        [DataMember]
+        public string PMSupportEmail { get; set; }
+    }
+}
+
