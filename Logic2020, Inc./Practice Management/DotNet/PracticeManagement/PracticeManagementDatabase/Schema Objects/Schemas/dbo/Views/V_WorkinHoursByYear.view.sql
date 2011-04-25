2071ab23718b4ad69f2c9c9786fc6cc1eba2828e
RaviN <RaviN>
2011-04-25 03:05:00 -0500

    add from branch - Milestone v0.900 none

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Views/V_WorkinHoursByYear.view.sql
@@ -0,0 +1,9 @@
+﻿CREATE VIEW [dbo].[V_WorkinHoursByYear]
+AS 
+	SELECT YEAR(DATE) [Year] 
+	,CASE WHEN ((YEAR(DATE) % 4) = 0 AND (YEAR(DATE) % 100 ) <> 0) OR (YEAR(DATE) % 400) = 0
+		  THEN 2088 
+		  ELSE 2080 END HoursInYear
+	FROM dbo.Calendar 
+	GROUP BY YEAR(DATE)
+
