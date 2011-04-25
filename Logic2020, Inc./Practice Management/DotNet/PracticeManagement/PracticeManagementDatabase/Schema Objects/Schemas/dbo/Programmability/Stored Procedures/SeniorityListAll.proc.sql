cd5306c2e2a4c78e2e8a21b539772d9d0a699efa
RaviN <RaviN>
2011-04-25 03:05:00 -0500

    add from branch - Milestone v0.900 none

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/SeniorityListAll.proc.sql
@@ -0,0 +1,15 @@
+﻿-- =============================================
+-- Author:		Anatoliy Lokshin
+-- Create date: 9-09-2008
+-- Updated by:	
+-- Update date: 
+-- Description:	Selects a list of the seniorities
+-- =============================================
+CREATE PROCEDURE [dbo].[SeniorityListAll]
+AS
+	SET NOCOUNT ON
+
+	SELECT e.SeniorityId, e.Name
+	  FROM dbo.Seniority AS e
+	ORDER BY e.SeniorityId
+
