184b7940a08efdd4dcc511120681be108fac3ef4
RaviN <RaviN>
2011-04-25 03:05:00 -0500

    add from branch - Milestone v0.900 none

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/DataTransferObjects/UserCredentials.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+
+namespace DataTransferObjects
+{
+    [DataContract]
+    [Serializable]
+    public class UserCredentials
+    {
+        [DataMember]
+        public string UserName
+        {
+            get;
+            set;
+        }
+        [DataMember]
+        public string Password
+        {
+            get;
+            set;
+        }
+        [DataMember]
+        public string PasswordSalt
+        {
+            get;
+            set;
+        }
+    }
+}
+
