7fcf4f3ad28fc63cd4f45975bcab862fdb5ed02c
RaviN <RaviN>
2011-04-25 03:05:00 -0500

    add from branch - Milestone v0.900 none

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/Controls/Generic/Filtering/RealInterval.ascx.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Web.UI.WebControls;
+
+namespace PraticeManagement.Controls.Generic.Filtering
+{
+    public partial class RealInterval : System.Web.UI.UserControl
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public double MinValue
+        {
+            get
+            {
+                return ParseDouble(tbFrom, 0.0);
+            }
+        }
+
+        public double MaxValue
+        {
+            get
+            {
+                return ParseDouble(tbTo, 24.0);
+            }
+        }
+
+        private static double ParseDouble(TextBox sourceTextBox, double defaultValue)
+        {
+            try
+            {
+                return Convert.ToDouble(sourceTextBox.Text);
+            }
+            catch
+            {
+
+                return defaultValue;
+            }
+        }
+
+        public void Reset()
+        {
+            tbTo.Text = "24.0";
+            tbFrom.Text = "0.0";
+        }
+    }
+}
+
