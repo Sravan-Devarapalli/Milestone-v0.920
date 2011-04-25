35c077436c845efa87f60050eb06be6fa3df7f04
RaviN <RaviN>
2011-04-25 03:05:00 -0500

    add from branch - Milestone v0.900 none

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/ProjectGroup.table.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[ProjectGroup] (
+    [GroupId]  INT            IDENTITY (0, 1) NOT NULL,
+    [ClientId] INT            NOT NULL,
+    [Name]     NVARCHAR (100) NOT NULL,
+	Active     BIT            NOT NULL CONSTRAINT DF_ProjectGroup_Active DEFAULT  1,
+    PRIMARY KEY CLUSTERED ([GroupId] ASC) WITH (IGNORE_DUP_KEY = OFF, STATISTICS_NORECOMPUTE = OFF),
+    FOREIGN KEY ([ClientId]) REFERENCES [dbo].[Client] ([ClientId]) ON DELETE NO ACTION ON UPDATE NO ACTION
+);
+
+ 
