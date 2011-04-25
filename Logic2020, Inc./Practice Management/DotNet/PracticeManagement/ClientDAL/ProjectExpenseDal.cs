831d176c726efde7d21df898659bba08e3194c86
RaviN <RaviN>
2011-04-25 03:05:00 -0500

    add from branch - Milestone v0.900 none

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/ClientDAL/ProjectExpenseDal.cs
@@ -0,0 +1,112 @@
+﻿using System.Data;
+using System.Data.Common;
+using System.Data.SqlClient;
+using DataAccess.Generic;
+using DataAccess.Readers;
+using DataTransferObjects;
+
+namespace DataAccess
+{
+    public class ProjectExpenseDal : DalBase<ProjectExpense, ProjectExpenseReader>
+    {
+        #region Overrides of DalBase<ProjectExpense>
+
+        #region Procedure names
+
+        protected override string GetByIdProcedure
+        {
+            get { return Constants.ProcedureNames.ProjectExpenses.GetById; }
+        }
+
+        protected override string AddProcedure
+        {
+            get { return Constants.ProcedureNames.ProjectExpenses.Insert; }
+        }
+
+        protected override string UpdateProcedure
+        {
+            get { return Constants.ProcedureNames.ProjectExpenses.Update; }
+        }
+
+        protected override string RemoveProcedure
+        {
+            get { return Constants.ProcedureNames.ProjectExpenses.Delete; }
+        }
+
+        #endregion
+
+        #region Base initializers
+
+        protected override SqlParameter InitAddCommand(ProjectExpense entity, SqlCommand command)
+        {
+            InitPropertiesNoId(command, entity);
+            command.Parameters.AddWithValue(Constants.ParameterNames.MilestoneId, entity.Milestone.Id.Value);
+
+            var outParam =
+                new SqlParameter(Constants.ParameterNames.ExpenseId, SqlDbType.Int)
+                    {Direction = ParameterDirection.Output};
+
+            command.Parameters.Add(outParam);
+
+            return outParam;
+        }
+
+        protected override void InitRemoveCommand(ProjectExpense entity, SqlCommand command)
+        {
+            InitSingleId(command, entity);
+        }
+
+        protected override void InitUpdateCommand(ProjectExpense entity, SqlCommand command)
+        {
+            InitSingleId(command, entity);
+            InitPropertiesNoId(command, entity);
+        }
+
+        protected override void InitGetById(ProjectExpense entity, SqlCommand command)
+        {
+            InitSingleId(command, entity);
+        }
+
+        protected override EntityReaderBase<ProjectExpense> InitEntityReader(DbDataReader reader)
+        {
+            return new ProjectExpenseReader(reader);
+        }
+
+        #endregion
+
+        #region Derived initializers
+
+        private static void InitPropertiesNoId(SqlCommand command, ProjectExpense entity)
+        {
+            command.Parameters.AddWithValue(Constants.ParameterNames.ExpenseName, entity.Name);
+            command.Parameters.AddWithValue(Constants.ParameterNames.ExpenseAmount, entity.Amount);
+            command.Parameters.AddWithValue(Constants.ParameterNames.ExpenseReimbursement, entity.Reimbursement);
+        }
+
+        private static void InitSingleId(SqlCommand command, IIdNameObject entity)
+        {
+            command.Parameters.AddWithValue(Constants.ParameterNames.ExpenseId, entity.Id.Value);
+        }
+
+        #endregion
+
+        #region Custom data access
+
+        public ProjectExpense[] GetForMilestone(ProjectExpense projectExpense)
+        {
+            return ExecuteReader(
+                        projectExpense, 
+                        Constants.ProcedureNames.ProjectExpenses.GetAllForMilestone,
+                        GetForMilestoneInitializer);
+        }
+
+        private static void GetForMilestoneInitializer(ProjectExpense projectExpense, SqlCommand sqlCommand)
+        {
+            sqlCommand.Parameters.AddWithValue(Constants.ParameterNames.MilestoneId, projectExpense.Milestone.Id);
+        }
+
+        #endregion
+
+        #endregion
+    }
+}
