f0ce5202d6004bde40a0c15fd82bc37e5591bf1c
RaviN <RaviN>
2011-04-25 03:05:00 -0500

    add from branch - Milestone v0.900 none

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/PersonStatusHistoryUpdate.proc.sql
@@ -0,0 +1,67 @@
+﻿CREATE PROCEDURE dbo.PersonStatusHistoryUpdate
+(
+	@PersonId	INT,
+	@PersonStatusId	INT
+)
+AS
+BEGIN
+	 DECLARE @Today DATETIME
+	 SET @Today  = CONVERT(DATETIME,CONVERT(DATE,GETDATE()))
+	 
+	 -- Set the end date of the previous person status record to yester day
+	 IF NOT EXISTS (SELECT 1 FROM dbo.PersonStatusHistory 
+					WHERE EndDate IS NULL 
+						AND  PersonId = @PersonId
+						AND  PersonStatusId = @PersonStatusId)
+	 BEGIN
+		 UPDATE dbo.PersonStatusHistory
+		 SET EndDate = @Today-1
+		 WHERE EndDate IS NULL 
+				AND  PersonId = @PersonId
+				AND StartDate != @Today
+
+		IF EXISTS (SELECT 1 FROM dbo.PersonStatusHistory
+					WHERE EndDate IS NULL 
+								AND PersonId = @PersonId
+								AND StartDate = @Today)
+		BEGIN
+			IF EXISTS (SELECT 1 FROM dbo.PersonStatusHistory
+					   WHERE EndDate = @Today-1
+								AND PersonId = @PersonId
+								AND PersonStatusId = @PersonStatusId)
+			BEGIN
+				UPDATE dbo.PersonStatusHistory
+				SET EndDate = NULL
+				WHERE EndDate = @Today-1
+					  AND PersonId = @PersonId
+					  AND PersonStatusId = @PersonStatusId
+
+				DELETE FROM  dbo.PersonStatusHistory
+				WHERE EndDate IS NULL 
+					AND  PersonId = @PersonId
+					AND  StartDate = @Today
+			END
+			ELSE
+			BEGIN
+				UPDATE dbo.PersonStatusHistory
+				SET PersonStatusId = @PersonStatusId
+				WHERE EndDate IS NULL 
+						AND  PersonId = @PersonId
+						AND StartDate = @Today
+			END
+		END
+		ELSE
+		BEGIN	
+			INSERT INTO [dbo].[PersonStatusHistory]
+			   ([PersonId]
+			   ,[PersonStatusId]
+			   ,[StartDate]
+			   )
+			VALUES
+			   (@PersonId
+			   ,@PersonStatusId
+			   ,@Today
+			   )
+		END
+	END
+END
