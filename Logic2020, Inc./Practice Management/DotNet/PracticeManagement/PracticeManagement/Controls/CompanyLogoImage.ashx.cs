f5c4b4ec7ba3373082d29fd69a2520e5255f63ff
RaviN <RaviN>
2011-04-25 03:05:00 -0500

    add from branch - Milestone v0.900 none

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/Controls/CompanyLogoImage.ashx.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using PraticeManagement.Configuration;
+using System.Reflection;
+using System.IO;
+using System.Drawing;
+
+namespace PraticeManagement.Controls
+{
+    /// <summary>
+    /// Summary description for CompanyLogoImage
+    /// </summary>
+    public class CompanyLogoImage : IHttpHandler
+    {
+
+        public void ProcessRequest(HttpContext context)
+        {
+            byte[] buffer = BrandingConfigurationManager.LogoData.Data;
+
+            context.Response.ContentType = "image/jpeg";
+            context.Response.OutputStream.Write(buffer, 0, buffer.Length);
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
