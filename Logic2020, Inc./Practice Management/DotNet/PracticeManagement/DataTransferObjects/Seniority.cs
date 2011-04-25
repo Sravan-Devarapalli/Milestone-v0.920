09c9473a2abd7c39b19652660bc3c931dd29c60b
RaviN <RaviN>
2011-04-25 03:05:00 -0500

    add from branch - Milestone v0.900 none

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/DataTransferObjects/Seniority.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.Serialization;
+
+namespace DataTransferObjects
+{
+	[Serializable]
+	[DataContract]
+	public class Seniority
+	{
+        #region Constants
+
+	    private static readonly int Separation = Settings.SenioritySeparationRange;
+
+        #endregion
+
+        /// <summary>
+		/// Gets or sets an ID of the seniority
+		/// </summary>
+		[DataMember]
+		public int Id
+		{
+			get;
+			set;
+		}
+
+		/// <summary>
+		/// Gets or sets a Name of the seniority
+		/// </summary>
+		[DataMember]
+		public string Name
+		{
+			get;
+			set;
+		}
+
+        public bool OtherHasGreaterOrEqualSeniority(Seniority other)
+        {
+            //  Currently in our database 
+            //  GREATER number means LOWER seniority
+            //
+            //      Settings.SenioritySeparationRange added according to #1457
+            var greaterOrEqualSeniority = other.Id <= ValueWithSeparation;        
+
+            return greaterOrEqualSeniority;
+        }
+
+	    public int ValueWithSeparation
+	    {
+	        get { return Id + Separation; }
+	    }
+	}
+}
+
