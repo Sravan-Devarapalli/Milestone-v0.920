e5189cb20ec410b69a4ae9961f39457b97c02138
RaviN <RaviN>
2011-04-25 03:05:00 -0500

    add from branch - Milestone v0.900 none

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/Utils/OpportunitiesHelper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PraticeManagement.Utils
+{
+    public static class OpportunitiesHelper
+    {
+        private static readonly IDictionary<string, string> _opportunityStatuses
+           = new Dictionary<string, string>()
+                {
+                    {"Active", "AciveOpportunity"},
+                    {"Won", "WonOpportunity"},
+                    {"Lost", "LostOpportunity"},
+                    {"Experimental", "ExperimentalOpportunity"},
+                    {"Inactive", "InactiveOpportunity"}
+                };
+
+        public static string GetIndicatorClassByStatus(string statusName)
+        {
+            return _opportunityStatuses[statusName];
+        }
+    }
+}
