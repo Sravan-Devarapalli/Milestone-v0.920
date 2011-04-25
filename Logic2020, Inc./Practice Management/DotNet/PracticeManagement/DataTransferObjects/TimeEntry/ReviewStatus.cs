91a48fbd80f385fd88801f27e4b466765043255d
RaviN <RaviN>
2011-04-25 03:05:00 -0500

    add from branch - Milestone v0.900 none

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/DataTransferObjects/TimeEntry/ReviewStatus.cs
@@ -0,0 +1,20 @@
+﻿using System.Runtime.Serialization;
+
+namespace DataTransferObjects.TimeEntry
+{
+    /// <summary>
+    /// Time entry review status
+    /// </summary>
+    [DataContract]
+    public enum ReviewStatus
+    {
+        [EnumMember]
+        Pending,
+
+        [EnumMember]
+        Approved,
+
+        [EnumMember]
+        Declined
+    }
+}
