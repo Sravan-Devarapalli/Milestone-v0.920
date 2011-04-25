1fd238aaf4ea063d0ad1fa55494f02ce29663279
RaviN <RaviN>
2011-04-25 03:05:00 -0500

    add from branch - Milestone v0.900 none

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementService/IConfigurationService.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using System.ServiceModel;
+using DataTransferObjects;
+using DataTransferObjects.ContextObjects;
+using System;
+
+namespace PracticeManagementService
+{
+    [ServiceContract]
+    public interface IConfigurationService
+    {
+        [OperationContract]
+        List<EmailTemplate> GetAllEmailTemplates();
+
+        [OperationContract]
+        EmailTemplate EmailTemplateGetByName(string emailTemplateName);
+
+        [OperationContract]
+        bool UpdateEmailTemplate(EmailTemplate template);
+
+        [OperationContract]
+        bool AddEmailTemplate(EmailTemplate template);
+
+        [OperationContract]
+        bool DeleteEmailTemplate(int templateId);
+
+        [OperationContract]
+        void CheckProjectedProjects(int templateId, string companyName);
+
+        [OperationContract]
+        EmailData GetEmailData(EmailContext emailContext);
+
+        [OperationContract]
+        void CheckProjectedProjectsByHireDate(int templateId, string companyName);
+
+        [OperationContract]
+        CompanyLogo GetCompanyLogoData();
+
+        [OperationContract]
+        void SaveCompanyLogoData(string title, string imagename, string imagePath, Byte[] data);
+
+        [OperationContract]
+        void SaveResourceKeyValuePairs(SettingsType settingType, Dictionary<string, string> dictionary);
+
+
+        [OperationContract]
+        bool SaveResourceKeyValuePairItem(SettingsType settingType, string key, string value);
+
+        [OperationContract]
+        Dictionary<string, string> GetResourceKeyValuePairs(SettingsType settingType);
+
+        [OperationContract]
+        Boolean VerifySMTPSettings(string MailServer, int PortNumber, bool EnableSSl, string UserName, string Password, string PMSupportEmailAddress);
+    }
+}
+
