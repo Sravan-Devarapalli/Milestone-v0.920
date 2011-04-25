99350640143c86eb4bf57c9531d2f52927e143ac
RaviN <RaviN>
2011-04-25 03:05:00 -0500

    add from branch - Milestone v0.900 none

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/PersonListAllSeniorityFilterWithCurrentPay.proc.sql
@@ -0,0 +1,135 @@
+CREATE PROCEDURE [dbo].[PersonListAllSeniorityFilterWithCurrentPay]
+(
+	@PracticeId    INT, 
+	@ShowAll       BIT,
+	@PageSize      INT,
+	@PageNo        INT,
+	@Looked		   NVARCHAR(40),
+    @StartDate     DATETIME,
+    @EndDate       DATETIME,
+	@RecruiterId   INT,
+	@MaxSeniorityLevel	INT,
+	@SortBy			NVARCHAR(225) = NULL
+)
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	DECLARE @FirstRecord INT
+	SET @FirstRecord = @PageSize * @PageNo
+
+	IF @Looked IS NOT NULL
+		SET @Looked = '%' + RTRIM(@Looked) + '%'
+	ELSE
+		SET @Looked = '%'
+
+    DECLARE @flags bit
+    IF (@StartDate IS NULL) OR (@EndDate IS NULL)
+		SET @flags = 1
+	ELSE
+		SET @flags = 0
+
+		-- Listing a specified page
+		DECLARE @LastRecord INT, @Now DATETIME
+		SELECT @LastRecord = @FirstRecord + @PageSize, @Now=GETDATE()
+		
+		
+		DECLARE @SqlQuery NVARCHAR(4000),
+				@OrderBy NVARCHAR(1000)
+		IF ISNULL(@SortBy,'') = ''
+		BEGIN
+		 SET @OrderBy = 'ORDER BY  LastName, FirstName'
+		 END
+		 ELSE
+		  SET @OrderBy = 'ORDER BY ' + @SortBy
+		
+		
+		SELECT @SqlQuery = 'SELECT *
+		  FROM (
+				SELECT TOP (@LastRecord)
+					   p.PersonId,
+					   p.FirstName,
+					   p.LastName,
+					   p.PTODaysPerAnnum,
+					   p.HireDate,
+					   p.TerminationDate,
+					   p.Alias,
+					   M.LastLoginDate,
+					   p.DefaultPractice,
+					   p.PracticeName,
+					   p.PersonStatusId,
+		               p.PersonStatusName,
+					   p.EmployeeNumber,
+					   ROW_NUMBER() OVER(' + @OrderBy + ') - 1 AS rownum,
+		               p.SeniorityId,
+		               p.SeniorityName,
+					   p.ManagerId,
+					   p.ManagerAlias,
+					   p.ManagerFirstName,
+					   p.ManagerLastName,
+					   p.PracticeOwnedId,
+					   p.PracticeOwnedName,
+					   p.TelephoneNumber,
+					   TS.PayPersonId,
+					   TS.StartDate,
+					   TS.EndDate,
+					   TS.Amount,
+					   TS.Timescale,
+					   TS.AmountHourly,
+					   TS.TimesPaidPerMonth,
+					   TS.Terms,
+					   TS.VacationDays,
+					   TS.BonusAmount,
+					   TS.BonusHoursToCollect,
+					   TS.IsYearBonus,
+					   TS.DefaultHoursPerDay,
+					   TS.TimescaleName
+				  FROM dbo.v_Person AS p
+				  OUTER APPLY 
+						(SELECT TOP 1 
+							   pay.PersonId PayPersonId,
+							   pay.StartDate,
+							   pay.EndDate,
+							   pay.Amount,
+							   pay.Timescale,
+							   pay.AmountHourly,
+							   pay.TimesPaidPerMonth,
+							   pay.Terms,
+							   pay.VacationDays,
+							   pay.BonusAmount,
+							   pay.BonusHoursToCollect,
+							   pay.IsYearBonus,
+							   pay.DefaultHoursPerDay,
+							   pay.TimescaleName
+						  FROM dbo.v_Pay AS pay
+						 WHERE p.PersonId = pay.PersonId
+						   AND ((@Now >= pay.StartDate
+						   AND @Now < pay.EndDateOrig) OR @Now < pay.StartDate)
+						   ORDER BY pay.StartDate
+						) TS
+				 LEFT JOIN dbo.aspnet_Users U ON (U.UserName = P.Alias)
+				 LEFT JOIN dbo.aspnet_Membership M ON (U.UserId = M.UserId)
+				 WHERE (   ( p.PersonStatusId = 1 AND @ShowAll = 0 AND @PracticeId IS NULL )
+		                OR ( p.PersonStatusId = 1 AND @ShowAll = 0 AND @PracticeId = p.DefaultPractice)
+		                OR ( @ShowAll = 1 AND @PracticeId IS NULL )
+		                OR ( @ShowAll = 1 AND @PracticeId = p.DefaultPractice ) ) 
+					AND ( p.FirstName LIKE @Looked OR p.LastName LIKE @Looked OR p.EmployeeNumber LIKE @Looked )
+		            AND (   @RecruiterId IS NULL
+		                 OR EXISTS (SELECT 1
+		                              FROM dbo.RecruiterCommission AS c
+		                             WHERE c.RecruitId = p.PersonId AND c.RecruiterId = @RecruiterId))
+					AND ((@MaxSeniorityLevel IS NULL) OR (@MaxSeniorityLevel >= p.SeniorityId))' + @OrderBy + '
+		       ) AS tmp
+		 WHERE tmp.rownum BETWEEN @FirstRecord AND @LastRecord - 1'
+		 
+
+		EXEC sp_executeSql 
+				@SqlQuery,		
+				N'@FirstRecord	INT, @LastRecord	INT, @Looked	NVARCHAR(40), @Now	DATETIME, 
+				@MaxSeniorityLevel	INT, @RecruiterId	INT, @PracticeId	INT, @ShowAll	BIT',				
+				@FirstRecord = @FirstRecord, @LastRecord = @LastRecord,
+				@RecruiterId = @RecruiterId, @PracticeId = @PracticeId, @Looked = @Looked,
+				@MaxSeniorityLevel = @MaxSeniorityLevel, @ShowAll = @ShowAll, @Now = @Now
+	END
+
+
