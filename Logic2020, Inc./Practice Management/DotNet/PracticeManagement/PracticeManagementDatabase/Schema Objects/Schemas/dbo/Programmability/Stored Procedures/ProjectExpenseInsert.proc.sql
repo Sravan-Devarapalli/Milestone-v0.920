0deb0b134dbc245c3289a0ed1d75dbb594e0fb61
RaviN <RaviN>
2011-04-25 03:05:00 -0500

    add from branch - Milestone v0.900 none

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/ProjectExpenseInsert.proc.sql
@@ -0,0 +1,29 @@
+﻿-- =============================================
+-- Author:		Nikita Goncharenko
+-- Create date: 2010-04-20
+-- =============================================
+CREATE PROCEDURE dbo.ProjectExpenseInsert 
+    @ExpenseId int out,
+    @ExpenseName nvarchar(50),
+    @ExpenseAmount decimal(18, 2),
+    @ExpenseReimbursement decimal(18, 2),
+	@MilestoneId int
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	INSERT INTO [dbo].[ProjectExpense]
+           ([Name]
+           ,[Amount]
+           ,[Reimbursement]
+           ,[MilestoneId])
+     VALUES
+           (@ExpenseName 
+           ,@ExpenseAmount
+           ,@ExpenseReimbursement
+           ,@MilestoneId)
+	SELECT @ExpenseId = SCOPE_IDENTITY()
+
+	SELECT @ExpenseId
+END
+
