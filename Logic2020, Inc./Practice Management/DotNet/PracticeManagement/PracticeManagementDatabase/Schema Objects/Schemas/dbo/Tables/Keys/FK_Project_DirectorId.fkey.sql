9913f7da0955c517fb63d0b13920049787615c6b
RaviN <RaviN>
2011-04-25 03:05:00 -0500

    add from branch - Milestone v0.900 none

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/Keys/FK_Project_DirectorId.fkey.sql
@@ -0,0 +1,3 @@
+﻿ALTER TABLE [dbo].[Project]
+    ADD CONSTRAINT [FK_Project_DirectorId] FOREIGN KEY ([DirectorID]) 
+	REFERENCES [dbo].[Person] ([PersonId]) ON DELETE NO ACTION ON UPDATE NO ACTION;
