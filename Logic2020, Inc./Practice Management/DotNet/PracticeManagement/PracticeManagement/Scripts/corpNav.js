b1ef09253d2c55a958689c700d944d7cfba739d1
RaviN <RaviN>
2011-04-27 08:53:00 -0500

    Comments - Defect Number: 2641; Defect RecordID: 2637; Server Address: services.logic2020.com; Server Port: 99; Database Name: Practice Management; Defect Summary: Configuration: UI/Layout (includes changes based on 2640); Defect Description: 1. When the user hovers over each item in the top navigation bar there should be a transparency effect (this is already in place and working). 2. The top navigation bar must retain the white box/black text selection no matter what category (Company Resources, E-Mail, etc) and subcategory (Branding/Logo, Clients, etc) are chosen so that the user knows where they are in PM at all times. 3. When the Configuration tab is chosen, the Company Resources category should be selected by default. 4. When the user hovers over each category (Company Resources, E-Mail, etc) there should be an open box outline (seen below) that indicates the potential choice. 5. When a category is chosen the open box should remain and the text should be bolded (seen below) that does not change no matter what subcategory is chosen so that the user knows where they are in PM at all times. Allow enough space between categories so that the text does not shift left or right when it becomes bolded/chosen. 6. Do not load a subcategory page by default; just display the subcategory links. This should be accomplished without having to reload the entire page. 7. Switching subcategories should be accomplished without having to reload the entire page as well, only the subcategory.

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/Scripts/corpNav.js
@@ -0,0 +1,38 @@
+﻿$(document).ready(function () {
+    var selectDefList;
+    var hoverItem;
+    var menuItem;
+    var timer;
+    var showMenu = function () {
+        $('li#' + menuItem + ' a').addClass('liBG');
+        $('li#' + menuItem).addClass('l1hover');
+        $(selectDefList).slideDown(150);
+    };
+    $('.l1').hover(function () {
+        hoverItem = $(this).attr('id');
+        selectDefList = 'li#' + hoverItem + ' dl';
+        this.children[0].style.color = '#2a4753';
+        menuItem = hoverItem;
+        timer = setTimeout(showMenu, 250);
+    },
+function () {
+    clearTimeout(timer);
+    $('li#' + hoverItem + ' a').removeClass('liBG');
+    $('li#' + menuItem).removeClass('l1hover');
+    $('li#' + hoverItem + ' a')[0].style.color = '';
+    $(selectDefList).slideUp(150);
+    return false;
+});
+    $('dd.l3').hover(function () {
+        var selectL2 = $(this).attr('id');
+        if ($('dd#' + selectL2 + ' .flyout').is(':hidden')) {
+            $('dd#' + selectL2 + ' a').addClass('ddBG');
+            $('dd#' + selectL2 + ' .flyout').show(0);
+        }
+        else {
+            $('dd#' + selectL2 + ' a').removeClass('ddBG');
+            $('dd#' + selectL2 + ' .flyout').hide();
+        }
+    }
+);
+});
