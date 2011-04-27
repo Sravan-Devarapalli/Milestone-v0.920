741697c42001ff83c10acc931ab04594063c4e5a
RaviN <RaviN>
2011-04-27 08:53:00 -0500

    Comments - Defect Number: 2641; Defect RecordID: 2637; Server Address: services.logic2020.com; Server Port: 99; Database Name: Practice Management; Defect Summary: Configuration: UI/Layout (includes changes based on 2640); Defect Description: 1. When the user hovers over each item in the top navigation bar there should be a transparency effect (this is already in place and working). 2. The top navigation bar must retain the white box/black text selection no matter what category (Company Resources, E-Mail, etc) and subcategory (Branding/Logo, Clients, etc) are chosen so that the user knows where they are in PM at all times. 3. When the Configuration tab is chosen, the Company Resources category should be selected by default. 4. When the user hovers over each category (Company Resources, E-Mail, etc) there should be an open box outline (seen below) that indicates the potential choice. 5. When a category is chosen the open box should remain and the text should be bolded (seen below) that does not change no matter what subcategory is chosen so that the user knows where they are in PM at all times. Allow enough space between categories so that the text does not shift left or right when it becomes bolded/chosen. 6. Do not load a subcategory page by default; just display the subcategory links. This should be accomplished without having to reload the entire page. 7. Switching subcategories should be accomplished without having to reload the entire page as well, only the subcategory.

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/Config/Overheads.aspx.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.UI.WebControls;
+using DataTransferObjects;
+using PraticeManagement.Controls;
+using PraticeManagement.OverheadService;
+using System.ServiceModel;
+namespace PraticeManagement.Config
+{
+    public partial class Overheads : PracticeManagementPageBase
+    {
+        private const string MLFOverHeadDesc = "Minimum Load Factor (MLF)";
+
+        protected override void Display()
+        {
+        }
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            DisplayContent();
+            if (!IsPostBack)
+            {
+                var RateMultipliersItems = GetRateMultipliers();
+                ddlW2Hourly.DataSource = RateMultipliersItems;
+                ddlW2Hourly.DataBind();
+                ddlW2Salary.DataSource = RateMultipliersItems;
+                ddlW2Salary.DataBind();
+                ddl1099.DataSource = RateMultipliersItems;
+                ddl1099.DataBind();
+                bool IsMLFInActive = false;
+                Dictionary<int, decimal> MLFRateMultipliers;
+                using (var serviceClient = new OverheadServiceClient())
+                {
+                    MLFRateMultipliers = serviceClient.GetMinimumLoadFactorOverheadMultipliers(MLFOverHeadDesc, ref IsMLFInActive);
+                }
+                if (MLFRateMultipliers != null)
+                {
+                    if (MLFRateMultipliers.Any(item => item.Key == (int)TimescaleType.Hourly))
+                    {
+                        ddlW2Hourly.SelectedValue = ((int)MLFRateMultipliers[(int)TimescaleType.Hourly]).ToString();
+                    }
+                    if (MLFRateMultipliers.Any(item => item.Key == (int)TimescaleType.Salary))
+                    {
+                        ddlW2Salary.SelectedValue = ((int)MLFRateMultipliers[(int)TimescaleType.Salary]).ToString();
+                    }
+                    if (MLFRateMultipliers.Any(item => item.Key == (int)TimescaleType._1099Ctc))
+                    {
+                        ddl1099.SelectedValue = ((int)MLFRateMultipliers[(int)TimescaleType._1099Ctc]).ToString();
+                    }
+                    chbMLFActive.Checked = !IsMLFInActive;
+                }
+            }
+        }
+        private const string EditRecordCommand = "EditRecord";
+
+        protected void DisplayContent()
+        {
+            using (OverheadServiceClient serviceClient = new OverheadServiceClient())
+            {
+                try
+                {
+                    gvOverhead.DataSource = serviceClient.GetOverheadFixedRates(chbActive.Checked);
+                    gvOverhead.DataBind();
+                }
+                catch (FaultException<ExceptionDetail>)
+                {
+                    serviceClient.Abort();
+                    throw;
+                }
+            }
+        }
+
+        protected void btnAddOverhead_Click(object sender, EventArgs e)
+        {
+            Redirect(Constants.ApplicationPages.OverheadDetail);
+        }
+
+        protected void gvOverhead_RowCommand(object sender, GridViewCommandEventArgs e)
+        {
+            if (e.CommandName == EditRecordCommand)
+            {
+                Redirect(string.Format(Constants.ApplicationPages.DetailRedirectFormat,
+                    Constants.ApplicationPages.OverheadDetail,
+                    e.CommandArgument));
+            }
+        }
+
+        protected void chbActive_CheckedChanged(object sender, EventArgs e)
+        {
+            DisplayContent();
+        }
+
+        protected NameValuePair[] GetRateMultipliers()
+        {
+            var nameValuePair = new NameValuePair[51];
+            nameValuePair[0] = new NameValuePair { Id = 0, Name = "N/A" };
+            for (int index = 1; index <= 50; index++)
+            {
+                var nvPair = new NameValuePair();
+                nvPair.Id = (index);
+                nvPair.Name = (index).ToString() + " %";
+                nameValuePair[index] = nvPair;
+            }
+
+            return nameValuePair;
+        }
+
+        protected void MLF_Changed(object sender, EventArgs e)
+        {
+            using (var serviceClient = new OverheadServiceClient())
+            {
+                serviceClient.SaveMinimumLoadFactorOverheadMultipliers(MLFOverHeadDesc,
+                    !chbMLFActive.Checked,
+                    decimal.Parse(ddlW2Hourly.SelectedValue),
+                    decimal.Parse(ddlW2Salary.SelectedValue),
+                    decimal.Parse(ddl1099.SelectedValue));
+            }
+        }
+    }
+
+}
