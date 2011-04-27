c27428a99379c5addc106348ea1336c5972d35e1
RaviN <RaviN>
2011-04-27 08:53:00 -0500

    Comments - Defect Number: 2641; Defect RecordID: 2637; Server Address: services.logic2020.com; Server Port: 99; Database Name: Practice Management; Defect Summary: Configuration: UI/Layout (includes changes based on 2640); Defect Description: 1. When the user hovers over each item in the top navigation bar there should be a transparency effect (this is already in place and working). 2. The top navigation bar must retain the white box/black text selection no matter what category (Company Resources, E-Mail, etc) and subcategory (Branding/Logo, Clients, etc) are chosen so that the user knows where they are in PM at all times. 3. When the Configuration tab is chosen, the Company Resources category should be selected by default. 4. When the user hovers over each category (Company Resources, E-Mail, etc) there should be an open box outline (seen below) that indicates the potential choice. 5. When a category is chosen the open box should remain and the text should be bolded (seen below) that does not change no matter what subcategory is chosen so that the user knows where they are in PM at all times. Allow enough space between categories so that the text does not shift left or right when it becomes bolded/chosen. 6. Do not load a subcategory page by default; just display the subcategory links. This should be accomplished without having to reload the entire page. 7. Switching subcategories should be accomplished without having to reload the entire page as well, only the subcategory.

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/Config/Clients.aspx.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.ServiceModel;
+using System.Web.UI.WebControls;
+using DataTransferObjects;
+using PraticeManagement.ClientService;
+using PraticeManagement.Controls;
+namespace PraticeManagement.Config
+{
+    public partial class Clients : PracticeManagementPageBase
+    {
+
+        protected override void Display()
+        {
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            DisplayContent();
+        }
+
+        protected void DisplayContent()
+        {
+            using (var serviceClient = new ClientServiceClient())
+            {
+                try
+                {
+                    Client[] clients =
+                        chbShowActive.Checked
+                            ? serviceClient.ClientListAll()
+                            : serviceClient.ClientListAllWithInactive();
+                    gvClients.DataSource = clients;
+
+                    if (!IsPostBack && clients.Length > 0)
+                    {
+                        gvClients.SelectedIndex = 0;
+                    }
+
+                    gvClients.DataBind();
+                }
+                catch (FaultException<ExceptionDetail>)
+                {
+                    serviceClient.Abort();
+                    throw;
+                }
+            }
+        }
+
+        protected void chbShowActive_CheckedChanged(object sender, EventArgs e)
+        {
+            DisplayContent();
+        }
+
+
+        protected void btnClientName_Command(object sender, CommandEventArgs e)
+        {
+            object args = e.CommandArgument;
+            Response.Redirect(GetClientDetailsUrl(args));
+        }
+
+        protected string GetClientDetailsUrlWithReturn(object args)
+        {
+            return PraticeManagement.Utils.Generic.GetTargetUrlWithReturn(GetClientDetailsUrl(args), Request.Url.AbsoluteUri);
+        }
+
+        private static string GetClientDetailsUrl(object args)
+        {
+            return string.Format(Constants.ApplicationPages.DetailRedirectFormat,
+                                 Constants.ApplicationPages.ClientDetails,
+                                 args);
+        }
+    }
+}
