8a0c45cc13d21aaa2b66bb01caf107a32f4805c2
RaviN <RaviN>
2011-04-27 08:53:00 -0500

    Comments - Defect Number: 2641; Defect RecordID: 2637; Server Address: services.logic2020.com; Server Port: 99; Database Name: Practice Management; Defect Summary: Configuration: UI/Layout (includes changes based on 2640); Defect Description: 1. When the user hovers over each item in the top navigation bar there should be a transparency effect (this is already in place and working). 2. The top navigation bar must retain the white box/black text selection no matter what category (Company Resources, E-Mail, etc) and subcategory (Branding/Logo, Clients, etc) are chosen so that the user knows where they are in PM at all times. 3. When the Configuration tab is chosen, the Company Resources category should be selected by default. 4. When the user hovers over each category (Company Resources, E-Mail, etc) there should be an open box outline (seen below) that indicates the potential choice. 5. When a category is chosen the open box should remain and the text should be bolded (seen below) that does not change no matter what subcategory is chosen so that the user knows where they are in PM at all times. Allow enough space between categories so that the text does not shift left or right when it becomes bolded/chosen. 6. Do not load a subcategory page by default; just display the subcategory links. This should be accomplished without having to reload the entire page. 7. Switching subcategories should be accomplished without having to reload the entire page as well, only the subcategory.

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/Config/TimeTypes.aspx.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using DataTransferObjects.TimeEntry;
+using PraticeManagement.TimeEntryService;
+using PraticeManagement.Utils;
+using PraticeManagement.Controls;
+
+namespace PraticeManagement.Config
+{
+    public partial class TimeTypes : PracticeManagementPageBase
+    {
+
+        protected override void Display()
+        {
+        }
+
+        #region "Declarations"
+
+        private const string CacheKey = "TimeTypeCachedData";
+        private const string InsertAction = "Inserted";
+        private const string DeleteAction = "Updated";
+        private const string UpdateAction = "Deleted";
+
+        #endregion "Declarations"
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            mlInsertStatus.ClearMessage();
+        }
+
+        protected void btnInsertTimeType_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                TimeEntryHelper.AddTimeType(tbNewTimeType.Text);
+                gvTimeTypes.DataBind();
+
+                HttpContext.Current.Cache[CacheKey] = InsertAction;
+                mlInsertStatus.ShowInfoMessage(Resources.Controls.TimeTypeAddedSuccessfully);
+                tbNewTimeType.Text = "New time type";
+            }
+            catch (Exception ex)
+            {
+                mlInsertStatus.ShowErrorMessage(ex.Message);
+            }
+        }
+
+        protected void odsTimeTypes_Updated(object sender, ObjectDataSourceStatusEventArgs e)
+        {
+            HttpContext.Current.Cache[CacheKey] = UpdateAction;
+        }
+
+        protected void odsTimeTypes_Deleted(object sender, ObjectDataSourceStatusEventArgs e)
+        {
+            HttpContext.Current.Cache[CacheKey] = DeleteAction;
+        }
+
+        protected void gvTimeTypes_RowDataBound(object sender, GridViewRowEventArgs e)
+        {
+            if (e.Row.RowType == DataControlRowType.DataRow)
+            {
+                var tt = e.Row.DataItem as TimeTypeRecord;
+                if (tt != null) e.Row.Cells[gvTimeTypes.Columns.Count - 1].Visible = !tt.InUse;
+            }
+        }
+
+        protected void gvTimeTypes_RowUpdating(object sender, GridViewUpdateEventArgs e)
+        {
+            Page.Validate("UpdateTimeType");
+            if (!Page.IsValid)
+            {
+                e.Cancel = true;
+            }
+            else
+            {
+                string newTimeType = e.NewValues["Name"].ToString().Trim();
+                string oldTimeType = e.OldValues["Name"].ToString().Trim();
+
+                if (newTimeType != oldTimeType)
+                {
+                    if (IsTimeTypeAlreadyExisting(newTimeType))
+                    {
+                        CustomValidator cvUpdatedTimeTypeName = gvTimeTypes.Rows[e.RowIndex].FindControl("cvUpdatedTimeTypeName") as CustomValidator;
+                        cvUpdatedTimeTypeName.IsValid = false;
+                        e.Cancel = true;
+                    }
+                }
+            }
+        }
+
+        private bool IsTimeTypeAlreadyExisting(string newTimeType)
+        {
+            using (TimeEntryServiceClient serviceClient = new TimeEntryServiceClient())
+            {
+                TimeTypeRecord[] timeTypesArray = serviceClient.GetAllTimeTypes();
+
+                foreach (TimeTypeRecord timeType in timeTypesArray)
+                {
+                    if (timeType.Name == newTimeType)
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+    }
+}
