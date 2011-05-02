bdad7cba0ec94a1f35f9b682bbc84f1dfdea543c
RaviN <RaviN>
2011-05-02 09:26:00 -0500

    Comments - Removed SCPH in Margin test and all financils pages( Projects, Milestones,)

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/GetProjectListForGroupingPracticeManagers.proc.sql
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/GetProjectListForGroupingPracticeManagers.proc.sql
@@ -348,7 +348,7 @@ AS
 	       dbo.MakeDate(YEAR(MIN(f.Date)), MONTH(MIN(f.Date)), dbo.GetDaysInMonth(MIN(f.Date))) AS MonthEndDate,
 	       ISNULL(SUM(f.PersonMilestoneDailyAmount),0) AS Revenue,
 	       ISNULL(SUM(f.PersonMilestoneDailyAmount - f.PersonDiscountDailyAmount-
-						(CASE WHEN f.SLHR+f.SCPH >=  f.PayRate +f.MLFOverheadRate 
+						(CASE WHEN f.SLHR  >=  f.PayRate +f.MLFOverheadRate 
 							  THEN f.SLHR ELSE f.PayRate +f.MLFOverheadRate END) 
 					    *ISNULL(f.PersonHoursPerDay, 0)),0) GrossMargin
 	  FROM (
@@ -360,22 +360,6 @@ AS
 				   f.Date, 
 				   f.PersonMilestoneDailyAmount,
 				   f.PersonDiscountDailyAmount,
-				   CASE WHEN ISNULL(f.PersonHoursPerDay,0) = 0 THEN 0
-						ELSE 
-							ISNULL(
-									( ((f.PersonMilestoneDailyAmount-f.PersonDiscountDailyAmount)/f.PersonHoursPerDay)-
-										(ISNULL(f.PayRate, 0) + ISNULL(f.OverheadRate, 0)+f.BonusRate+f.VacationRate + ISNULL(f.RecruitingCommissionRate,0))
-									)* (SELECT SUM(c.FractionOfMargin) 
-									  FROM dbo.Commission AS  c 
-									  WHERE c.ProjectId = f.ProjectId 
-											AND c.CommissionType = 1
-										)/100,0
-									)
-						END SCPH
-					/*
-				   GrossMargin-Semi = Revenue-SCogs
-				   SCPH = GrossMargin-Semi*SC%
-				   */,
 				   (ISNULL(f.PayRate, 0) + ISNULL(f.OverheadRate, 0)+ISNULL(f.BonusRate,0)+ISNULL(f.VacationRate,0)
 					+ISNULL(f.RecruitingCommissionRate,0)) SLHR,
 				   ISNULL(f.PayRate,0) PayRate,
