97cf25ecf09a6ff17896b10437160e0fee793255
RaviN <RaviN>
2011-05-02 09:26:00 -0500

    Comments - Removed SCPH in Margin test and all financils pages( Projects, Milestones,)

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/FinancialsListByProjectPeriod.proc.sql
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/FinancialsListByProjectPeriod.proc.sql
@@ -28,22 +28,6 @@ AS
 		   f.Date, 
 		   f.PersonMilestoneDailyAmount,
 		   f.PersonDiscountDailyAmount,
-		   CASE WHEN ISNULL(f.PersonHoursPerDay,0) = 0 THEN 0
-				ELSE 
-					ISNULL(
-							( ((f.PersonMilestoneDailyAmount-f.PersonDiscountDailyAmount)/f.PersonHoursPerDay)-
-								(ISNULL(f.PayRate, 0) + ISNULL(f.OverheadRate, 0)+f.BonusRate+f.VacationRate + ISNULL(f.RecruitingCommissionRate,0))
-							)* (SELECT SUM(c.FractionOfMargin) 
-							  FROM dbo.Commission AS  c 
-							  WHERE c.ProjectId = f.ProjectId 
-									AND c.CommissionType = 1
-								)/100,0
-							)
-				END SCPH
-			/*
-		   GrossMargin-Semi = Revenue-SCogs
-		   SCPH = GrossMargin-Semi*SC%
-		   */,
 		   ISNULL((SELECT SUM(c.FractionOfMargin) 
 							  FROM dbo.Commission AS  c 
 							  WHERE c.ProjectId = f.ProjectId 
@@ -74,22 +58,21 @@ AS
 	       SUM(f.PersonMilestoneDailyAmount - f.PersonDiscountDailyAmount) AS RevenueNet,
 	       
 		   SUM(f.PersonMilestoneDailyAmount - f.PersonDiscountDailyAmount -
-						(CASE WHEN f.SLHR+f.SCPH >= f.PayRate + f.MLFOverheadRate 
+						(CASE WHEN f.SLHR >= f.PayRate + f.MLFOverheadRate 
 							  THEN f.SLHR ELSE f.PayRate + f.MLFOverheadRate END) 
 					    *ISNULL(f.PersonHoursPerDay, 0)) GrossMargin,
 		   
-		   ISNULL(SUM((CASE WHEN f.SLHR+f.SCPH >=  f.PayRate + f.MLFOverheadRate THEN f.SLHR+f.SCPH ELSE  f.PayRate + f.MLFOverheadRate END)*ISNULL(f.PersonHoursPerDay, 0)),0) Cogs,
+		   ISNULL(SUM((CASE WHEN f.SLHR >=  f.PayRate + f.MLFOverheadRate THEN f.SLHR ELSE  f.PayRate + f.MLFOverheadRate END)*ISNULL(f.PersonHoursPerDay, 0)),0) Cogs,
 	       ISNULL(SUM(f.PersonHoursPerDay), 0) AS Hours,
 	       
-	       (SUM(
-				 CASE WHEN f.SLHR+f.SCPH >=  f.PayRate +f.MLFOverheadRate THEN f.SCPH * ISNULL(f.PersonHoursPerDay, 0)
-				    ELSE (f.PersonMilestoneDailyAmount - f.PersonDiscountDailyAmount-((f.PayRate + f.MLFOverheadRate)*ISNULL(f.PersonHoursPerDay, 0)))
-						* (f.ProjectSalesCommisionFraction/100)
-					END
-				 )) SalesCommission,
+	       (SUM((f.PersonMilestoneDailyAmount - f.PersonDiscountDailyAmount -
+						(CASE WHEN f.SLHR >=  f.PayRate +f.MLFOverheadRate 
+							  THEN f.SLHR ELSE f.PayRate +f.MLFOverheadRate END) 
+					    *ISNULL(f.PersonHoursPerDay, 0))* (f.ProjectSalesCommisionFraction/100))) SalesCommission,
 
 	       SUM((f.PersonMilestoneDailyAmount - f.PersonDiscountDailyAmount -
-	            (f.SLHR) * ISNULL(f.PersonHoursPerDay, 0)) *
+	            (CASE WHEN f.SLHR >=  f.PayRate +f.MLFOverheadRate 
+							  THEN f.SLHR ELSE f.PayRate +f.MLFOverheadRate END) * ISNULL(f.PersonHoursPerDay, 0)) *
 	           (f.PracticeManagementCommissionSub + CASE f.PracticeManagerId WHEN f.PersonId THEN f.PracticeManagementCommissionOwn ELSE 0 END)) / 100 AS PracticeManagementCommission,
 		   min(case when pe.ExpenseSum is null then 0 else pe.ExpenseSum end) as 'Expense',
 		   min(case when pe.ReimbursedExpenseSum is null then 0 else pe.ReimbursedExpenseSum end) as 'ReimbursedExpense',
