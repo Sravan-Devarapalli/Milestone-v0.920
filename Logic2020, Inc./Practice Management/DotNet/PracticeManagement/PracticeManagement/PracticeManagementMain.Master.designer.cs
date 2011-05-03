ca1b26bd0b5a0233dc08e62771c7e4ba02f2abab
SrinivasM <SrinivasM>
2011-05-03 10:44:00 -0500

    Comments - Fixed UI issues.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/PracticeManagementMain.Master.designer.cs
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/PracticeManagementMain.Master.designer.cs
@@ -166,6 +166,15 @@ namespace PraticeManagement {
         protected global::System.Web.UI.WebControls.Literal ltrlMenu;
         
         /// <summary>
+        /// lblCurrentPage control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblCurrentPage;
+        
+        /// <summary>
         /// body control.
         /// </summary>
         /// <remarks>
