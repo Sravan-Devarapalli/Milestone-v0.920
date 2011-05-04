9507776f2b20681fafeefd3cfa687b1ff8ab6f89
RaviN <RaviN>
2011-05-04 06:21:00 -0500

    Comments - Defect Number: 2631; Defect RecordID: 2627; Server Address: services.logic2020.com; Server Port: 99; Database Name: Practice Management; Defect Summary: Reports>Bench Costs: Add filters, change report output/format; Defect Description: Very similar formatting to 2447 (seen below), but with important differences. Also will display calculated costs instead of "Free" output.
    1. Find all persons active and projected by default (unless one is deselected) for all practices by default (unless other selections made via drop-down) in period selected.
    2. Calculate Bench Cost for each person
    2a. Determine Revenue Net of Discount (RNOD)
    2a1. For each billable milestone for each person in period selected, ProjectedTime(hours per day)*HourlyRate
    2a2. For each billable milestone, subtract % discount (if any) attached to each project for each milestone.
    2b. Determine Fully Loaded Hourly Cost (FLHC)
    2b1. Calculate total number of billable days for each person for period selected. Use Start Date if it is later than earliest date in period selected range.
    2b2. Subtract company holidays and any PTO for each person.
    2b3. Mutiply by (HPW / 5)
    2b4. Mutiply x Loaded Hourly Rate (found on Configuration > Persons > [PersonName] > What-If)
    2c. (RNOD)-(FLHC)=BenchCost for each person
    3. Output results for each person into one of two tables (by default) based on practice designation (external vs internal).
    3a. Tables should be separated by a horizontal line break.
    3b. Each table should have the same column headings as the example below.
    3c. Each table should be sortable by the Consultant Name, Practice, and Status columns, default sort is Consultant Name ascending.
    4. Use existing logic to determine what the person running the report can see.
    4a. For the "external" table results, the person running the report should only be able to see costs for subordinates (based on seniority level), not anyone at their same level or higher.
    4b. For the "internal" table results, only persons with Admin or Partner seniority levels should be able to see costs. Everyone else should see "(Hidden)".
    
    UPDATE 3/29/11: Also add filters for Project Type (Active*, Projected*, Completed*, Experimental - check * by default), same location/layout as in Cons Util Timeline. ALSO, update terminology (Practice = Practice Area, etc).

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/Controls/Reports/BenchCosts.ascx.designer.cs
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/Controls/Reports/BenchCosts.ascx.designer.cs
@@ -94,15 +94,6 @@ namespace PraticeManagement.Controls.Reports {
         protected global::System.Web.UI.WebControls.CheckBox chbIncludeOverHeads;
         
         /// <summary>
-        /// lblOverheads control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Label lblOverheads;
-        
-        /// <summary>
         /// custPeriod control.
         /// </summary>
         /// <remarks>
@@ -119,6 +110,15 @@ namespace PraticeManagement.Controls.Reports {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.CustomValidator custPeriodLengthLimit;
+        
+        /// <summary>
+        /// lblOverheads control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblOverheads;
         
         /// <summary>
         /// btnUpdateView control.
