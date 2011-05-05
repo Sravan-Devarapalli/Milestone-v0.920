d61cfdae58169ff65c6141c3f02d1ff5e9de0159
ThulasiramP <ThulasiramP>
2011-05-05 10:42:00 -0500

    Comments - Defect Number: 2667; Defect RecordID: 2663; Server Address: services.logic2020.com; Server Port: 99; Database Name: Practice Management; Defect Summary: Projects>Add Project: Add ability to upload/attach SOW .pdf document to Project; Defect Description: I think it would be very useful to implement an upload/attach feature to the Add Project function so that a SOW .pdf could be linked to the Project. I think it should be restricted to .pdf files only (so that they're read-only) and only allow 1 file to be attached. If the user attempts to upload a 2nd/newer SOW they should be alerted that one already exists and that if they continue that file will be replaced with this one (buttons on pop up message: "Continue" "Cancel").
    We need to consider how this will affect database size in relation to Azure. It should be small files, size-wise, I'd think.
    
    Also, update the color of the icon for each project in this way: If the Project is Active and has a SOW attached, the icon will be fully GREEN (as it is now). If the Project is Active but does not have a SOW attached, the icon will be HALF GREEN, HALF AMBER (split vertically). My plan is to phase out "Projected" as a Project status (projected projects should be considered opportunities is my thinking).

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/Controls/Projects/AttachmentHandler.ashx.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.IO;
+
+namespace PraticeManagement.Controls.Projects
+{
+    /// <summary>
+    /// Summary description for ProjectAttachmentHandler
+    /// </summary>
+    public class ProjectAttachmentHandler : IHttpHandler
+    {
+
+        public void ProcessRequest(HttpContext context)
+        {
+            int projectId = Convert.ToInt32(context.Request.QueryString["ProjectId"]);
+            string FileName = context.Request.QueryString["FileName"];
+            byte[] attachmentData = null;
+
+            AttachmentService.AttachmentService svc = new AttachmentService.AttachmentService();
+            attachmentData = svc.GetProjectAttachmentData(projectId);
+
+            context.Response.ContentType = "Application/pdf";
+            context.Response.AddHeader(
+                "content-disposition", string.Format("attachment; filename={0}", FileName));
+
+            int len = attachmentData.Length;
+            int bytes;
+            byte[] buffer = new byte[1024];
+
+            Stream outStream = context.Response.OutputStream;
+            using (MemoryStream stream = new MemoryStream(attachmentData))
+            {
+                while (len > 0 && (bytes = stream.Read(buffer, 0, buffer.Length)) > 0)
+                {
+                    outStream.Write(buffer, 0, bytes);
+                    context.Response.Flush();
+                    len -= bytes;
+                }
+            }
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
