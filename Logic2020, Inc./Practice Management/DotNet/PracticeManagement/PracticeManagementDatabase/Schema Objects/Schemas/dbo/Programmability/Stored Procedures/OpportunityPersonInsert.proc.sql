6d2a216724a7e783c49d187e155d54ff08ffbc37
RaviN <RaviN>
2011-05-09 10:48:00 -0500

    Comments - Defect Number: 2804; Defect RecordID: 2800; Server Address: services.logic2020.com; Server Port: 99; Database Name: Practice Management; Defect Summary: Opportunities: Create "Discussion Review 2" view; Defect Description: This is an alternative view to the already listed "Discussion Review 1" view. It should be simpler to implement, and it borrows minor functionality from the other view.
    In this view, the Opportunity page will remain mostly the same. Combine the names from Salesperson and Owner fields into a "Sales Team" column where the names are stacked and unlink any names there. Remove the Days Old and Last Change columns. In their place, create a "Note" column at a fixed height. The width can dynamically resize as the page is maximized. In the Note box there should be text in grey italics that reads "To add a note, click here and begin typing. Your note will be saved automatically." Whatever the user enters into that box should be saved in PM as a note, but not until the user either clicks into another note box or as soon as it can be in the background (maybe use an autosave every n # of seconds?) - whatever the save method, the cursor should not disappear or change focus. NOTE: It doesn't appear in the image below, but the Est. Revenue column should remain as well.
    
    Each note that is entered should stay visible on this page, but only during this session. That way the user can edit the note easily.
    
    In a separate frame at the bottom 1/4 of the page (keeping it visible on the page at all times) should be the same Potential and Proposed resources function I've created/proposed in the "Discussion Review 1" issue. It will change/update with each Opportunity line that is clicked/selected. To be clear, this will not require the user clicking the Oppty Name, but rather just clicking any part of the line that isn't linked.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/OpportunityPersonInsert.proc.sql
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/OpportunityPersonInsert.proc.sql
@@ -1,13 +1,40 @@
 ﻿CREATE PROCEDURE [dbo].[OpportunityPersonInsert]
 (
 	@OpportunityId INT, 
-	@PersonIdList NVARCHAR(MAX)
+	@PersonIdList NVARCHAR(MAX),
+	@OutSideResources NVARCHAR(4000)
 )
 AS
 BEGIN
 	SET NOCOUNT ON;
 
-	INSERT INTO dbo.OpportunityPersons(OpportunityId, PersonId)
-	SELECT @OpportunityId AS OpportunityId, ResultId 
-	FROM [dbo].[ConvertStringListIntoTable] (@PersonIdList)
+	DECLARE @OpportunityIdsLocal NVARCHAR(MAX)
+	SELECT @OpportunityIdsLocal = @PersonIdList
+	DECLARE @OpportunityIdTable TABLE
+	(
+		OpportunityId INT
+	)
+	
+	INSERT INTO @OpportunityIdTable
+	SELECT ResultId 
+	FROM [dbo].[ConvertStringListIntoTable] (@OpportunityIdsLocal)
+	
+	IF(@PersonIdList IS NOT NULL)
+	BEGIN
+		DELETE op
+		FROM dbo.OpportunityPersons  op
+		LEFT JOIN @OpportunityIdTable AS p 
+		ON op.OpportunityId = @OpportunityId AND op.PersonId = p.OpportunityId
+		WHERE p.OpportunityId IS NULL and OP.OpportunityId = @OpportunityId
+
+		INSERT INTO OpportunityPersons
+		SELECT @OpportunityId ,P.OpportunityId
+		FROM @OpportunityIdTable AS p 
+		LEFT JOIN dbo.OpportunityPersons op
+		ON p.OpportunityId = op.PersonId AND op.OpportunityId=@OpportunityId
+		WHERE op.PersonId IS NULL 
+	END
+	UPDATE dbo.Opportunity
+	SET OutSideResources = @OutSideResources
+	WHERE OpportunityId = @OpportunityId
 END
