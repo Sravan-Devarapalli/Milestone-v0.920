ceb70d9c4ce7c285a913442d63f02ab24ed021be
ThulasiramP <ThulasiramP>
2011-05-10 09:34:00 -0500

    Comments - Defect Number: 2850; Defect RecordID: 2846; Server Address: services.logic2020.com; Server Port: 99; Database Name: Practice Management; Defect Summary: OpportunityDetail.aspx: Change the OpportunityDetail.aspx page UI to match the Detail pane UI in the Discussion Review 1.; Defect Description: Based on the conversation with Jason on APR-18-2011, UI of OpportunityDetail.aspx page should be changed to match the UI of Detail pane in Discussion review 1 .

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/Controls/Opportunities/ProposedResources.ascx.designer.cs
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/Controls/Opportunities/ProposedResources.ascx.designer.cs
@@ -31,6 +31,15 @@ namespace PraticeManagement.Controls.Opportunities {
         protected global::System.Web.UI.WebControls.HiddenField hdnProposedPersonIdsList;
         
         /// <summary>
+        /// hlnkUtilization control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HyperLink hlnkUtilization;
+        
+        /// <summary>
         /// hintDate control.
         /// </summary>
         /// <remarks>
