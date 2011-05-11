4083026bfc8d70d9f69397dcfe81b4f1c91f8001
ThulasiramP <ThulasiramP>
2011-05-11 08:38:00 -0500

    Comments - Defect Number: 2854; Defect RecordID: 2850; Server Address: services.logic2020.com; Server Port: 99; Database Name: Practice Management; Defect Summary: Projects>Discussion Review 1: Post-production upgrade changes; Defect Description: Opening this as a separate issue so that it's clear these changes/improvements can be made after going to production with the original version.
    1. After making any changes to an Opportunity and saving, the left pane (summary) will re-sort automatically, and the oppty shown in the detail pane may not necessarily be the oppty highlighted in the summary pane. I think it's fine if the summary pane updates to reflect changes made to the oppty, but if it changes order in the list, it should still be highlighted correctly.
    1b. Also - why does simply adding an Est. Revenue change the sort order? I don't think it should.
    
    2. Instead of having "No Notes" when there are no notes entered, can we just have a blank table showing at the same height as if a note actually existed?
    2b. With this blank table in place, there would definitely be a detail pane default/minimum page length. I think that the overall page length should be at least this long so that the pane doesn't independently scroll.
    
    3. On multiple pages, this one included, we have an issue that when there is an error saving the user won't know it because the page focus shifts to the top of the page, but the error message is shown at the bottom of the page. Maybe, if successfully saved, there can be a little overlay in the top-right corner of the detail pane that says "Saved" in green text, then fade away after a few seconds? If it's an error it would say "Error" in red text but not fade away and maybe the fields that are in error or haven't been chosen can be highlighted red as well? (Note: In the mockup image I showed a persistent bar at the bottom of the page and moved the success/error messages to display on this bar, but this solution doesn't solve the issue, so it's probably best ignored.)
    
    4. Let's make the Potential and Proposed Resources boxes less wide so that the checkboxes are closer to the names.
    4b. Change the description just above those resources boxes to: "Select from the list of Potential resources below and click the "Add" button to note them as Proposed for this Opportunity."
    
    5. Generally these changes will hopefully make the detail pane less wide by default so that we can shrink the width of the notes and description, as well as the entry and drop-down boxes at the top of the page. If we can give a little extra width to the left summary pane it will look much better.
    
    6. Regarding the Potential/Proposed Resources boxes, if the logic in determing who is available is what causes the page loading delay, I'd like to see how the page performs with that function removed. I'd still like to be able to select names and add them to the Proposed box, but regardless of what their availability status may be. Also, any names that are added or removed should not be committed immediately, so no need to grey out the names immediately - just wait until the user clicks "Save Changes".

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/OpportunityPriorities.table.sql
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/OpportunityPriorities.table.sql
@@ -1,6 +1,7 @@
 ﻿CREATE TABLE dbo.OpportunityPriorities (
     Id                   INT IDENTITY(1,1) NOT NULL,
-    Priority			 NVARCHAR(1) NOT NULL,
-    Description          NVARCHAR(MAX) NOT NULL
-   CONSTRAINT PK_OpportunityPriorities_Priority PRIMARY KEY CLUSTERED(Priority)
+    Priority			 NVARCHAR(255) NOT NULL,
+    Description          NVARCHAR(MAX) NOT NULL,
+	sortOrder			 INT
+   CONSTRAINT PK_OpportunityPriorities_Id PRIMARY KEY CLUSTERED(Id)
 );
