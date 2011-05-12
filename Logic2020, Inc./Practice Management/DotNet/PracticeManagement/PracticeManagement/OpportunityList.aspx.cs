cff0fb9a4714a1b5a00f0baef2720cd2a6933439
ThulasiramP <ThulasiramP>
2011-05-12 04:28:00 -0500

    Comments - Defect Number: 2850; Defect RecordID: 2846; Server Address: services.logic2020.com; Server Port: 99; Database Name: Practice Management; Defect Summary: OpportunityDetail.aspx: Change the OpportunityDetail.aspx page UI to match the Detail pane UI in the Discussion Review 1.; Defect Description: Based on the conversation with Jason on APR-18-2011, UI of OpportunityDetail.aspx page should be changed to match the UI of Detail pane in Discussion review 1 .

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/OpportunityList.aspx.cs
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/OpportunityList.aspx.cs
@@ -5,6 +5,8 @@ using DataTransferObjects;
 using PraticeManagement.Controls;
 using PraticeManagement.OpportunityService;
 using PraticeManagement.Controls.Generic.Filtering;
+using System.Linq;
+using PraticeManagement.Utils;
 
 namespace PraticeManagement
 {
@@ -19,9 +21,37 @@ namespace PraticeManagement
         {
             DataHelper.InsertExportActivityLogMessage("Opportunity");
 
-            var excelData = DataHelper.GetFilteredOpportunities();
+            var opportunitiesList = DataHelper.GetFilteredOpportunities();
 
-            excelGrid.DataSource = excelData;
+            var opportunitiesData =(from opp in opportunitiesList
+                                    where opp != null
+                                    select new
+                                    {
+                                        Id = opp.Id != null ? opp.Id.ToString() : string.Empty,	
+                                        ProjectId = opp.ProjectId != null ? opp.ProjectId.ToString() : string.Empty,	
+                                        EstimatedRevenue = opp.EstimatedRevenue != null ? opp.EstimatedRevenue.ToString() : string.Empty,		
+                                        OpportunityIndex = opp.OpportunityIndex != null ? opp.OpportunityIndex.ToString() : string.Empty,
+                                        Name = opp.Name != null ? opp.Name.ToString() : string.Empty,
+                                        Priority = opp.Priority != null ? opp.Priority.ToString() : string.Empty,
+                                        ProjectedStartDate = opp.ProjectedStartDate != null ? opp.ProjectedStartDate.ToString() : string.Empty,
+                                        LastUpdate =opp.LastUpdate.ToString(),
+                                        ProjectedEndDate = opp.ProjectedEndDate != null ? opp.ProjectedEndDate.ToString() : string.Empty,
+                                        OpportunityNumber = opp.OpportunityNumber != null ? opp.OpportunityNumber.ToString() : string.Empty,
+                                        Description = opp.Description != null ? opp.Description.ToString() : string.Empty,
+                                        BuyerName = opp.BuyerName != null ? opp.BuyerName.ToString() : string.Empty,
+                                        CreateDate = opp.CreateDate.ToString() ,
+                                        Pipeline = opp.Pipeline != null ? opp.Pipeline.ToString() : string.Empty,
+                                        Proposed = opp.Proposed != null ? opp.Proposed.ToString() : string.Empty,
+                                        SendOut = opp.SendOut != null ? opp.SendOut.ToString() : string.Empty,
+                                        DaysOld =  opp.DaysOld.ToString(),
+                                        LastChange = opp.LastChange.ToString() ,
+                                        ProposedPersonIdList = opp.ProposedPersonIdList != null ? opp.ProposedPersonIdList.ToString() : string.Empty,
+                                        OutSideResources = opp.OutSideResources != null ? opp.OutSideResources.ToString() : string.Empty,
+                                        ClientAndGroup = opp.ClientAndGroup != null ? opp.ClientAndGroup.ToString() : string.Empty,	
+                                    }).ToList();
+
+
+            excelGrid.DataSource = opportunitiesData;
             excelGrid.DataMember = "excelDataTable";
             excelGrid.DataBind();
             excelGrid.Visible = true;
