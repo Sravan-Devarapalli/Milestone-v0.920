367b4e21fd2327f8b3405c53f225efe99ed3d479
SrinivasM <SrinivasM>
2011-05-19 09:02:00 -0500

    Comments - Added alphabet paging, view(25/50/100/All), sorting, filters and UI changes.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/PersonGetCount.proc.sql
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/PersonGetCount.proc.sql
@@ -10,7 +10,12 @@ CREATE PROCEDURE [dbo].[PersonGetCount]
 	@PracticeId    INT = NULL, 
 	@ShowAll       BIT = 0,
 	@Looked		   NVARCHAR(40) = NULL,
-	@RecruiterId   INT
+	@RecruiterId   INT,
+	@TimeScaleId   INT = NULL,
+	@Projected		BIT,
+	@Terminated		BIT,
+	@Inactive		BIT,
+	@Alphabet		NVARCHAR(5)
 )
 AS
 	SET NOCOUNT ON
@@ -19,16 +24,35 @@ AS
 		SET @Looked = '%' + RTRIM(@Looked) + '%'
 	ELSE
 		SET @Looked = '%'
+		
+	IF @Alphabet IS NOT NULL
+		SET @Alphabet = @Alphabet + '%'
+	ELSE
+		SET @Alphabet = '%'
 
 	SELECT COUNT(*) AS num
 	  FROM dbo.v_Person AS p
-	 WHERE (   ( p.PersonStatusId = 1 AND @ShowAll = 0 AND @PracticeId IS NULL )
-	        OR ( p.PersonStatusId = 1 AND @ShowAll = 0 AND @PracticeId = p.DefaultPractice)
-	        OR ( @ShowAll = 1 AND @PracticeId IS NULL )
-	        OR ( @ShowAll = 1 AND @PracticeId = p.DefaultPractice ) )
-	   AND (p.FirstName LIKE @Looked OR p.LastName LIKE @Looked OR p.EmployeeNumber LIKE @Looked )
-	   AND (   @RecruiterId IS NULL
+	 WHERE (   (p.PersonStatusId = 1 AND @ShowAll = 0)  --@ShowAll is reverse of Active
+				OR (p.PersonStatusId = 2 AND @Terminated = 1)
+				OR (p.PersonStatusId = 3 AND @Projected = 1)
+				OR (p.PersonStatusId = 4 AND @Inactive = 1) 
+			) 
+		AND ( p.DefaultPractice = @PracticeId OR @PracticeId IS NULL )
+		AND (p.FirstName LIKE @Looked OR p.LastName LIKE @Looked OR p.EmployeeNumber LIKE @Looked )
+		AND (   @RecruiterId IS NULL
 	        OR EXISTS (SELECT 1
 	                     FROM dbo.RecruiterCommission AS c
 	                    WHERE c.RecruitId = p.PersonId AND c.RecruiterId = @RecruiterId))
+		AND (	@TimeScaleId IS NULL
+			OR EXISTS (SELECT 1
+						FROM dbo.v_Pay AS pay
+						WHERE pay.PersonId = p.PersonId AND pay.Timescale = @TimeScaleId))
+		AND ( p.LastName LIKE @Alphabet )
+
+
+
+GO
+
+
+
 
