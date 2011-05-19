8a1438bf6d95bcbb37ee1e32cf9f31dea0c4b26c
SrinivasM <SrinivasM>
2011-05-19 09:25:00 -0500

    Comments - Defect Number: 2646; Defect RecordID: 2642; Server Address: services.logic2020.com; Server Port: 99; Database Name: Practice Management; Defect Summary: Configuration>Company Resources>Default Line Manager: UI/Layout changes; Defect Description: 1. Shrink drop-down bar and keep background blue color for left side of page per the mockup below. 2. Create "Save Line Manager" save button instead of the save link as it exists currently. 3. On the right-side of the page against a white background add the explanation text as per the mockup below.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/Controls/Configuration/DefaultUser.ascx.designer.cs
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/Controls/Configuration/DefaultUser.ascx.designer.cs
@@ -37,7 +37,7 @@ namespace PraticeManagement.Controls.Configuration {
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.LinkButton btnSetDefault;
+        protected global::System.Web.UI.WebControls.Button btnSetDefault;
         
         /// <summary>
         /// odsPersons control.
