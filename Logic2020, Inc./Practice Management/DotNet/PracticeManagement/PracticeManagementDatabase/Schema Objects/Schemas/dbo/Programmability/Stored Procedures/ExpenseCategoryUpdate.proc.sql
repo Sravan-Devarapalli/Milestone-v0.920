9555f3b3cc0ca18f360f5fe88e0e9fb991f56b6a
SrinivasM <SrinivasM>
2011-05-20 02:31:00 -0500

    Comments - Added tool tips and duplicate name validations.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/ExpenseCategoryUpdate.proc.sql
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/ExpenseCategoryUpdate.proc.sql
@@ -13,19 +13,9 @@ CREATE PROCEDURE [dbo].[ExpenseCategoryUpdate]
 AS
 	SET NOCOUNT ON
 
-	IF EXISTS (SELECT ExpenseCategoryId FROM ExpenseCategory WHERE Name = @Name)
-	BEGIN
-		DECLARE @Error NVARCHAR(200)
-		SET @Error = 'This Expense Category already exists. Please add a different Expense Category.'
-		RAISERROR(@Error,16,1)
-	END
-	ELSE
-	BEGIN	
 	UPDATE dbo.ExpenseCategory
 	   SET Name = @Name
 	 WHERE ExpenseCategoryId = @ExpenseCategoryId
 	 
-	END
-
 
 
