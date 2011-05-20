605b8dad02727436a8d36258eb8ceb6a52c5df2c
ThulasiramP <ThulasiramP>
2011-05-20 07:57:00 -0500

    Comments - Defect Number: 2644; Defect RecordID: 2640; Server Address: services.logic2020.com; Server Port: 99; Database Name: Practice Management; Defect Summary: Configuration>Company Resources>Clients: UI/functionality changes; Defect Description: 1. Make the checkboxes in both the "Active" and "Billable by default" columns functional so that checking/unchecking the boxes is allowed and committed immediately without the page needing to be reloaded/refreshed and/or saved. 2. Make sure that even though the default filter is for "Active" clients by default that unchecking an "Active" checkbox for a client does not result in the table reloading immediately (the client should not disappear, in other words), in case they change their mind, etc. When the user browses away from the page and comes back it should then only show the "Active" clients. 3. Allow more column width for the "Client Name" column. 4. The table contents should change/refresh whenever either the "Show Active Clients Only" is checked/unchecked or whenever a letter (shortcut to clients that begin with the letter chosen) is selected. It should not cause the entire page to be reloaded.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/Config/Clients.aspx.cs
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/Config/Clients.aspx.cs
@@ -4,10 +4,59 @@ using System.Web.UI.WebControls;
 using DataTransferObjects;
 using PraticeManagement.ClientService;
 using PraticeManagement.Controls;
+using System.Linq;
+
 namespace PraticeManagement.Config
 {
     public partial class Clients : PracticeManagementPageBase
     {
+        private string previousAlphabetLnkButtonId
+        {
+            get
+            {
+                string value;
+
+                value = ViewState["PreviousAlphabet"] != null ? (string)ViewState["PreviousAlphabet"] : null;
+
+                return value;
+            }
+            set
+            {
+                ViewState["PreviousAlphabet"] = value;
+            }
+        }
+
+        private const string CLIENTS_LIST_KEY = "ClientsListKey";
+
+        private Client[] ClientsList
+        {
+            get
+            {
+                if (Cache[CLIENTS_LIST_KEY] != null)
+                {
+                    return Cache[CLIENTS_LIST_KEY] as Client[];
+                }
+                else
+                {
+                    using (var serviceClient = new ClientServiceClient())
+                    {
+                        try
+                        {
+                            var result = chbShowActive.Checked
+                                    ? serviceClient.ClientListAll()
+                                    : serviceClient.ClientListAllWithInactive();
+                            Cache[CLIENTS_LIST_KEY] = result;
+                            return result;
+                        }
+                        catch (FaultException<ExceptionDetail>)
+                        {
+                            serviceClient.Abort();
+                            throw;
+                        }
+                    }
+                }
+            }
+        }
 
         protected override void Display()
         {
@@ -15,41 +64,89 @@ namespace PraticeManagement.Config
 
         protected void Page_Load(object sender, EventArgs e)
         {
-            DisplayContent();
+            if (!IsPostBack)
+            {
+                Cache.Remove(CLIENTS_LIST_KEY);
+                DataBindClients(ClientsList);
+                previousAlphabetLnkButtonId = lnkbtnAll.ID;
+            }
+
+            AddAlphabetButtons();
         }
 
-        protected void DisplayContent()
+        protected void DataBindClients(Client[] clientsList)
         {
+            gvClients.DataSource = clientsList;
+
+            if (!IsPostBack && clientsList.Length > 0)
+            {
+                gvClients.SelectedIndex = 0;
+            }
+
+            gvClients.DataBind();
+        }
+
+        protected void chbShowActive_CheckedChanged(object sender, EventArgs e)
+        {
+            Cache.Remove(CLIENTS_LIST_KEY);
+
+            LinkButton previousLinkButton = (LinkButton)tdAlphabeticalPaging.FindControl(previousAlphabetLnkButtonId);
+            Client[] FilteredClientList = previousLinkButton != null && previousLinkButton.Text != "All" ? ClientsList.AsQueryable().Where(c => c.Name.ToUpperInvariant().StartsWith(previousLinkButton.Text.ToUpperInvariant())).ToArray() : ClientsList;
+            DataBindClients(FilteredClientList);
+        }
+
+        protected void chbInactive_CheckedChanged(object sender, EventArgs e)
+        {
+            CheckBox chbInactive = sender as CheckBox;
+            var ClientId = Convert.ToInt32(chbInactive.Attributes["ClientId"]);
+
             using (var serviceClient = new ClientServiceClient())
             {
                 try
                 {
-                    Client[] clients =
-                        chbShowActive.Checked
-                            ? serviceClient.ClientListAll()
-                            : serviceClient.ClientListAllWithInactive();
-                    gvClients.DataSource = clients;
-
-                    if (!IsPostBack && clients.Length > 0)
+                    if (chbInactive.Checked)
                     {
-                        gvClients.SelectedIndex = 0;
+                        serviceClient.ClientReactivate(new Client() { Id = ClientId });
                     }
-
-                    gvClients.DataBind();
+                    else
+                    {
+                        serviceClient.ClientInactivate(new Client() { Id = ClientId });
+                    }
                 }
-                catch (FaultException<ExceptionDetail>)
+                catch (CommunicationException)
                 {
                     serviceClient.Abort();
                     throw;
                 }
             }
+
+            Client client = ClientsList.AsQueryable().Where(c => c.Id == ClientId).ToArray()[0];
+
+            client.Inactive = !client.Inactive;
         }
 
-        protected void chbShowActive_CheckedChanged(object sender, EventArgs e)
+        protected void chbIsChargeable_CheckedChanged(object sender, EventArgs e)
         {
-            DisplayContent();
-        }
+            CheckBox chbIsChargeable = sender as CheckBox;
+            var ClientId = Convert.ToInt32(chbIsChargeable.Attributes["ClientId"]);
 
+            using (var serviceClient = new ClientServiceClient())
+            {
+                try
+                {
+                    serviceClient.UpdateIsChargableForClient(ClientId, chbIsChargeable.Checked);
+                }
+                catch (CommunicationException)
+                {
+                    serviceClient.Abort();
+                    throw;
+                }
+            }
+
+            Client client = ClientsList.AsQueryable().Where(c => c.Id == ClientId).ToArray()[0];
+
+            client.IsChargeable = !client.IsChargeable;
+        }
 
         protected void btnClientName_Command(object sender, CommandEventArgs e)
         {
@@ -67,6 +164,51 @@ namespace PraticeManagement.Config
             return string.Format(Constants.ApplicationPages.DetailRedirectFormat,
                                  Constants.ApplicationPages.ClientDetails,
                                  args);
+        }
+
+        private void AddAlphabetButtons()
+        {
+            if (tdAlphabeticalPaging.HasControls())
+            {
+                for (int index = 65; index <= 65 + 25; index++)
+                {
+                    char alphabet = Convert.ToChar(index);
+                    string alphabetId = "lnkbtn" + alphabet;
+                    LinkButton Alphabet = (LinkButton)tdAlphabeticalPaging.FindControl(alphabetId);
+                    tdAlphabeticalPaging.Controls.Remove(Alphabet);
+                }
+            }
+
+            for (int index = 65; index <= 65 + 25; index++)
+            {
+                char alphabet = Convert.ToChar(index);
+                LinkButton Alphabet = new LinkButton();
+                Alphabet.ID = "lnkbtn" + alphabet;
+                Alphabet.Text = alphabet.ToString();
+                Alphabet.Font.Underline = false;
+                Alphabet.Style.Add("padding-left", "15px");
+                Alphabet.Click += new EventHandler(Alphabet_Clicked);
+
+                tdAlphabeticalPaging.Controls.Add(Alphabet);
+            }
+        }
+
+        protected void Alphabet_Clicked(object sender, EventArgs e)
+        {
+            if (previousAlphabetLnkButtonId != null)
+            {
+                LinkButton previousLinkButton = (LinkButton)tdAlphabeticalPaging.FindControl(previousAlphabetLnkButtonId);
+                previousLinkButton.Font.Bold = false;
+            }
+
+            LinkButton alpha = (LinkButton)sender;
+            alpha.Font.Bold = true;
+            hdnAlphabet.Value = alpha.Text != "All" ? alpha.Text : null;
+            previousAlphabetLnkButtonId = alpha.ID;
+
+            Client[] FilteredClientList = alpha.Text != "All" ? ClientsList.AsQueryable().Where(c => c.Name.ToUpperInvariant().StartsWith(alpha.Text.ToUpperInvariant())).ToArray() : ClientsList;
+
+            DataBindClients(FilteredClientList);
         }
     }
 }
