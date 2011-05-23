a474d3007f5cc63d06804e894a46ea4a2ed21d08
SrinivasM <SrinivasM>
2011-05-23 00:52:00 -0500

    Comments - Added savedirty functionality.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/Config/DefaultLineManager.aspx.cs
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/Config/DefaultLineManager.aspx.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using PraticeManagement.Controls;
+using System.Web.UI;
+
 namespace PraticeManagement.Config
 {
-    public partial class DefaultLineManager : PracticeManagementPageBase
+    public partial class DefaultLineManager : PracticeManagementPageBase, IPostBackEventHandler
     {
         protected void Page_Load(object sender, EventArgs e)
         {
@@ -16,5 +18,17 @@ namespace PraticeManagement.Config
         {
             base.ClearDirty();
         }
+
+        #region RaisePostBackEventHandler
+        
+        public void RaisePostBackEvent(string eventArgument)
+        {
+            if (defaultManager.SavedNewDefaultManager())
+            {
+                Redirect(eventArgument);
+            }
+        }
+        
+        #endregion
     }
 }
