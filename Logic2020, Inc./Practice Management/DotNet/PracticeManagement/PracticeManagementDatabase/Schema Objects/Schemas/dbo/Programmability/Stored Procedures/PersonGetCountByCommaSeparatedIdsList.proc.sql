b55cd017f0fc2fafd886a70d2feac0c765592986
ThulasiramP <ThulasiramP>
2011-05-24 01:12:00 -0500

    Comments - Defect Number: 2647; Defect RecordID: 2643; Server Address: services.logic2020.com; Server Port: 99; Database Name: Practice Management; Defect Summary: Configuration>Company Resources>Persons: UI/Layout changes; Defect Description: (persons) As with the other subcategories, make sure the background color extends to the subcategory as well. 1. Move the search bar/functionality to the basic parameters bar. Include the italicized, slightly grey text that disappears when a user starts typing anything in that box. Expand the paramters bar so that it is two lines (see below). Add the "View 25/50/100/All" dropdown to far right of top parameters bar and drop the "Add Person" button one down. 2. Use similar formatting for the Filters section that we've used with some reports. The defaults should be set to what is shown in the image below. 3. Format the date for the Start and End Date columns as MM/DD/YYYY as it already is displayed in the "Last Login" column. 4. Allow the text in the Practice Area column to wrap (see #7 also). 5. Change the column heading "Manager" to "Line Manager" 6. The column headings do not seem to be centered over the column contents... 7. No matter how the data is sorted (change when column headings are clicked) the column widths should not change. They should retain a minimum width for 4:3 screens. When using a 16:9 view only the Person Name and Practice columns should expand width. The other columns should already be wide enough to accomodate the text. 8. Since the default sort is by Person Name alpha ascending, just add an indicator (not as large as in the image, of course) to the column so that it's easy enough to tell when something is sorted differently. 9. At bottom of view expand that line so that it's two as well, and drop the Export button to the second line. The first/top line will have a "< Previous" link left-aligned and a "Next >" link right-aligned to accommodate cases where fewer than all names are displayed on the page. Center-aligned on the top/first line will be the individual letter links. Center-aligned on the bottom/second line will be a quick summary to let the user know how many records they're viewing of the total that meet their filter criteria.
    UPDATE 4/28 - to the left of the "A B C..." at the bottom, also add "ALL". Whichever is selected (ALL by default) should be bolded.

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/PersonGetCountByCommaSeparatedIdsList.proc.sql
@@ -0,0 +1,69 @@
+﻿CREATE PROCEDURE [dbo].PersonGetCountByCommaSeparatedIdsList
+(
+	@PracticeIdsList     NVARCHAR(MAX), 
+	@ShowAll       BIT = 0,
+	@Looked		   NVARCHAR(40) = NULL,
+	@RecruiterIdsList  NVARCHAR(MAX),
+	@TimescaleIdsList   NVARCHAR(MAX),
+	@Projected		BIT,
+	@Terminated		BIT,
+	@Inactive		BIT,
+	@Alphabet		NVARCHAR(5)
+)
+AS
+	SET NOCOUNT ON
+
+	IF @Looked IS NOT NULL
+		SET @Looked = '%' + RTRIM(@Looked) + '%'
+	ELSE
+		SET @Looked = '%'
+		
+	IF @Alphabet IS NOT NULL
+		SET @Alphabet = @Alphabet + '%'
+	ELSE
+		SET @Alphabet = '%'
+
+	DECLARE @PracticeIdsTable TABLE
+	(
+		PracticeId INT
+	)
+
+	INSERT INTO @PracticeIdsTable
+	SELECT ResultId 
+	FROM [dbo].[ConvertStringListIntoTable] (@PracticeIdsList)
+	
+	DECLARE @TimeScaleIdsTable TABLE
+	(
+		TimeScaleId INT
+	)
+
+	INSERT INTO @TimeScaleIdsTable
+	SELECT ResultId 
+	FROM [dbo].[ConvertStringListIntoTable] (@TimescaleIdsList)
+
+	DECLARE @RecruiterIdsTable TABLE
+	(
+		RecruiterId INT
+	)
+
+	INSERT INTO @RecruiterIdsTable
+	SELECT ResultId 
+	FROM [dbo].[ConvertStringListIntoTable] (@RecruiterIdsList)
+
+	SELECT COUNT(*) AS num
+	  FROM dbo.v_Person AS p
+	 WHERE (   (p.PersonStatusId = 1 AND @ShowAll = 0)  --@ShowAll is reverse of Active
+				OR (p.PersonStatusId = 2 AND @Terminated = 1)
+				OR (p.PersonStatusId = 3 AND @Projected = 1)
+				OR (p.PersonStatusId = 4 AND @Inactive = 1) 
+			) 
+		AND ( p.DefaultPractice  IN (SELECT PracticeId FROM @PracticeIdsTable) )
+		AND (p.FirstName LIKE @Looked OR p.LastName LIKE @Looked OR p.EmployeeNumber LIKE @Looked )
+		AND (   P.PersonId  IN (SELECT RecruiterId FROM @RecruiterIdsTable)
+	        OR EXISTS (SELECT 1
+	                     FROM dbo.RecruiterCommission AS c
+	                    WHERE c.RecruitId = p.PersonId AND c.RecruiterId IN (SELECT RecruiterId FROM @RecruiterIdsTable)))
+		AND (EXISTS (SELECT 1
+						FROM dbo.v_Pay AS pay
+						WHERE pay.PersonId = p.PersonId AND pay.Timescale IN (SELECT TimeScaleId FROM @TimeScaleIdsTable)))
+		AND ( p.LastName LIKE @Alphabet )
