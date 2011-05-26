5ff63461ad0a95f6425bd80e4cce217c7216bd69
SrinivasM <SrinivasM>
2011-05-26 01:26:00 -0500

    Comments - Added Timezones list. Modified triggers, procedures to reflect time in activity log, time entries and in login date.

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/Config/TimeZones.aspx.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using DataTransferObjects;
+using PraticeManagement.Controls;
+
+namespace PraticeManagement.Config
+{
+    public partial class TimeZones : PracticeManagementPageBase, IPostBackEventHandler
+    {
+        private static string _timezone;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            successMessage.Text = string.Empty;
+            errorMessage.Text = string.Empty;
+        }
+
+        protected override void Display()
+        { }
+
+        protected void btnSetTimeZone_Clicked(object sender, EventArgs e)
+        {
+            if (SaveTimeZone())
+            {
+                successMessage.Text = "TimeZone Set successfully.";
+            }
+        }
+
+        private bool SaveTimeZone()
+        {
+            Timezone timezone = new Timezone();
+            timezone.GMT = ddlTimeZones.SelectedValue;
+
+            try
+            {
+                using (var serviceClient = new TimeEntryService.TimeEntryServiceClient())
+                {
+                    serviceClient.SetTimeZone(timezone);
+                }
+
+                ClearDirty();
+                return true;
+            }
+            catch(Exception e)
+            {
+                errorMessage.Text = e.Message;
+                return false;
+            }
+        }
+
+        protected void odsTimeZones_Selected(object sender, ObjectDataSourceStatusEventArgs e)
+        {
+            SelectTimeZone(e.ReturnValue as IEnumerable<Timezone>);
+        }
+
+        private void SelectTimeZone(IEnumerable<Timezone> timezones)
+        {
+            if (timezones != null)
+            {
+                foreach (Timezone timezone in timezones)
+                {
+                    if (timezone.IsActive)
+                    {
+                        _timezone = timezone.GMT;
+                        ddlTimeZones.SelectedValue = timezone.GMT;
+                        break;
+                    }
+                }
+            }
+        }
+
+
+        #region IPostback Event Handler
+
+        public void RaisePostBackEvent(string eventArgument)
+        {
+            if (SaveTimeZone())
+            {
+                Redirect(eventArgument);
+            }
+        }
+
+        #endregion
+    }
+}
