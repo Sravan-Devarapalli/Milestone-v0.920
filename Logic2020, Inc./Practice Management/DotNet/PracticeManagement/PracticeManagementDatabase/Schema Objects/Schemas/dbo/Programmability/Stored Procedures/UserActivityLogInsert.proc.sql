3de02a00a05ab3abfcab9a4c6868e4c3255ce3e9
SrinivasM <SrinivasM>
2011-05-26 01:33:00 -0500

    Comments - Added Timezones list. Modified triggers, procedures to reflect time in activity log, time entries and in login date.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/UserActivityLogInsert.proc.sql
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/UserActivityLogInsert.proc.sql
@@ -1,9 +1,13 @@
-﻿CREATE PROCEDURE UserActivityLogInsert
+﻿CREATE PROCEDURE [dbo].[UserActivityLogInsert]
 	@ActivityTypeID	INT,
 	@LogData		NVARCHAR(MAX)
 AS
 	-- Ensure the temporary table exists
 	EXEC SessionLogPrepare @UserLogin = NULL
+	
+	DECLARE @GMT NVARCHAR(10) = (SELECT Value FROM Settings WHERE SettingsKey = 'TimeZone')
+	DECLARE @CurrentPMTime DATETIME = (CASE WHEN CHARINDEX('-',@GMT) >0 THEN GETUTCDATE() - REPLACE(@GMT,'-','') ELSE 
+											GETUTCDATE() + @GMT END)
 
 	-- Log an activity
 	INSERT INTO dbo.UserActivityLog
@@ -17,7 +21,8 @@ AS
 	             LastName,
 	             FirstName,
 	             LogData,
-				 Data)
+				 Data,
+				 LogDate)
 	SELECT @ActivityTypeID,
 	       l.SessionID,
 	       l.SystemUser,
@@ -28,10 +33,17 @@ AS
 	       l.LastName,
 	       l.FirstName,
 		   LogData = (CASE WHEN @ActivityTypeID = 20 THEN '<Error></Error>' ELSE @LogData END), -- if @ActivityTypeID = 20 it is an Error.
-	       Data = @LogData
+	       Data = @LogData,
+	       LogDate = @CurrentPMTime
 	  FROM dbo.SessionLogData AS l
 	  WHERE l.SessionID = @@SPID
 
 	  -- End logging session
 	 EXEC dbo.SessionLogUnprepare
+
+
+
+GO
+
+
 
