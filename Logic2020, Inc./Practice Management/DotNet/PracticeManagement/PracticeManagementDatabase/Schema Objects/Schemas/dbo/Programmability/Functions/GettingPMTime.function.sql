651f69d60c6af09a5bf3cf5dee3268ae83b57e9e
SrinivasM <SrinivasM>
2011-05-26 12:47:00 -0500

    Comments - Modified insertingtime changes and gettingtime changes.

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Functions/GettingPMTime.function.sql
@@ -0,0 +1,15 @@
+﻿CREATE FUNCTION [dbo].[GettingPMTime]
+(
+	@utcDateTime DateTime 
+)
+RETURNS DATETIME
+AS
+BEGIN
+	DECLARE @currentTimeZone NVARCHAR(10)
+	SET @currentTimeZone = (SELECT Value FROM Settings WHERE SettingsKey = 'TimeZone')
+	
+	DECLARE @resultTime DATETIME
+	SET @resultTime = CONVERT(DATETIME, SWITCHOFFSET(CONVERT(DATETIMEOFFSET, @utcDateTime), @currentTimeZone))
+	
+	RETURN @resultTime
+END
