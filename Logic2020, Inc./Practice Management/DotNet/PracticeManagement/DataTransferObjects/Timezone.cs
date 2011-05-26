4dd47e989f43e9af59974577b0e5e085d08b2bcd
SrinivasM <SrinivasM>
2011-05-26 01:26:00 -0500

    Comments - Added Timezones list. Modified triggers, procedures to reflect time in activity log, time entries and in login date.

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/DataTransferObjects/Timezone.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+
+namespace DataTransferObjects
+{
+    /// <summary>
+    /// Represents a TimeZone entity
+    /// </summary>
+    [DataContract]
+    [Serializable]
+    public class Timezone
+    {
+        /// <summary>
+        /// Gets or sets a <see cref="TimeZone"/> Id value.
+        /// </summary>
+        [DataMember]
+        public int Id
+        {
+            get;
+            set;
+        }
+
+
+        /// <summary>
+        /// Gets or sets a  <see cref="TimeZone"/> GMT value.
+        /// </summary>
+        [DataMember]
+        public string GMT
+        {
+            get;
+            set;
+        }
+
+
+        /// <summary>
+        /// Gets or sets GMTName <see cref="TimeZone"/>.
+        /// </summary>
+        [DataMember]
+        public string GMTName
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Gets or sets a <see cref="TimeZone"/> IsActive value.
+        /// </summary>
+        [DataMember]
+        public bool IsActive
+        {
+            get;
+            set;
+        }
+    }
+}
+
