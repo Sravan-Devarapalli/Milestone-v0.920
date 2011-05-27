0e38dd88734900e07293cbef075ec31c6dd3d564
SrinivasM <SrinivasM>
2011-05-27 11:04:00 -0500

    Comments - Added day light saving time logic.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Functions/GettingPMTime.function.sql
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Functions/GettingPMTime.function.sql
@@ -5,11 +5,20 @@
 RETURNS DATETIME
 AS
 BEGIN
-	DECLARE @currentTimeZone NVARCHAR(10)
-	SET @currentTimeZone = (SELECT Value FROM Settings WHERE SettingsKey = 'TimeZone')
 	
 	DECLARE @resultTime DATETIME
-	SET @resultTime = CONVERT(DATETIME, SWITCHOFFSET(CONVERT(DATETIMEOFFSET, @utcDateTime), @currentTimeZone))
+	
+	DECLARE @currentTimeZone NVARCHAR(10)
+	SET @currentTimeZone = (SELECT Value FROM Settings WHERE SettingsKey = 'TimeZone')
+		
+	IF @currentTimeZone = '-08:00' AND dbo.ISDayLightSavingTime(@utcDateTime) = 1
+	BEGIN
+		SET @resultTime = CONVERT(DATETIME, SWITCHOFFSET(CONVERT(DATETIMEOFFSET, @utcDateTime), '-07:00'))
+	END
+	ELSE
+	BEGIN
+		SET @resultTime = CONVERT(DATETIME, SWITCHOFFSET(CONVERT(DATETIMEOFFSET, @utcDateTime), @currentTimeZone))
+	END
 	
 	RETURN @resultTime
 END
