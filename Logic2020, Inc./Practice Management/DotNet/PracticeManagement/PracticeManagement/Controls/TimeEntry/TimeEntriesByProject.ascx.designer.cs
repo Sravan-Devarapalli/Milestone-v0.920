2907483f5ad709490ecbee325f51bcf500ce772e
SrinivasM <SrinivasM>
2011-05-27 02:07:00 -0500

    Comments - Added Milestone Dropdown.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/Controls/TimeEntry/TimeEntriesByProject.ascx.designer.cs
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/Controls/TimeEntry/TimeEntriesByProject.ascx.designer.cs
@@ -58,6 +58,15 @@ namespace PraticeManagement.Controls.TimeEntry {
         protected global::System.Web.UI.WebControls.DropDownList ddlProjects;
         
         /// <summary>
+        /// ddlMilestones control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList ddlMilestones;
+        
+        /// <summary>
         /// diRange control.
         /// </summary>
         /// <remarks>
