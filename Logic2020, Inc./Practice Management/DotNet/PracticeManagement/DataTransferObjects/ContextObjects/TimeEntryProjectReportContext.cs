0a76b69dd6387ff86e6f44ab6ab1a42bad37bbaf
SrinivasM <SrinivasM>
2011-05-27 02:07:00 -0500

    Comments - Added Milestone Dropdown.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/DataTransferObjects/ContextObjects/TimeEntryProjectReportContext.cs
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/DataTransferObjects/ContextObjects/TimeEntryProjectReportContext.cs
@@ -19,6 +19,8 @@ namespace DataTransferObjects.ContextObjects
         public DateTime? EndDate { get; set; }
         [DataMember]
         public IEnumerable<int> PersonIds { get; set; }
+        [DataMember]
+        public int? MilestoneId { get; set; }
     }
 }
 
