acf1790255f5e118c2092d4b2085861bcd7a9f09
SrinivasM <SrinivasM>
2011-05-27 02:07:00 -0500

    Comments - Added Milestone Dropdown.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/Controls/TimeEntry/TimeEntriesByProject.ascx.cs
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/Controls/TimeEntry/TimeEntriesByProject.ascx.cs
@@ -10,9 +10,13 @@ namespace PraticeManagement.Controls.TimeEntry
 {
     public partial class TimeEntriesByProject : System.Web.UI.UserControl
     {
+        #region constants
+
         private const int HoursCellIndex = 2;
         private double _totalPersonHours;
         private double _grandTotalHours;
+
+        #endregion
 
         protected void Page_Load(object sender, EventArgs e)
         {
@@ -74,10 +78,15 @@ namespace PraticeManagement.Controls.TimeEntry
 
                 lblProjectName.Visible = true;
                 var projectName = ddlProjects.SelectedItem.Text;
-                projectName = ddlClients.SelectedItem.Text + " - " + projectName ;
+                projectName = ddlClients.SelectedItem.Text + " - " + projectName;
                 var startDateMissing = !diRange.FromDate.HasValue;
                 var endDateMissing = !diRange.ToDate.HasValue;
                 var unrestricted = startDateMissing && endDateMissing;
+
+                if (ddlMilestones.SelectedIndex != 0)
+                {
+                    projectName = projectName + " - " + ddlMilestones.SelectedItem.Text;
+                }
 
                 if (unrestricted)
                 {
@@ -124,6 +133,8 @@ namespace PraticeManagement.Controls.TimeEntry
             ddlProjects.Enabled = false;
             dlTimeEntries.Visible = false;
             UpdateGrandTotal();
+            SetFirstItemOfMilestones();
+            ddlMilestones.Enabled = false;//disable Milestone dropdown.
 
             if (ddlClients.SelectedIndex != 0)
             {
@@ -145,27 +156,71 @@ namespace PraticeManagement.Controls.TimeEntry
             {
                 ddlProjects.Items.Add(firstItem);
             }
-            
+
         }
 
         protected void ddlProjects_OnSelectedIndexChanged(object sender, EventArgs e)
         {
+            SetFirstItemOfMilestones();
+
             diRange.FromDate = null;
             diRange.ToDate = null;
             //Clearing Persons Scrolling Dropdown as project is changed and then rebinding TimeEntries list control.
             cblPersons.Items.Clear();
             dlTimeEntries.DataBind();
+
             if (ddlProjects.SelectedValue != string.Empty)
             {
-                dlTimeEntries.Visible = true;
-                using (var projectservice = new ProjectService.ProjectServiceClient())
-                {
-                    var selectedProject = projectservice.ProjectGetById(Convert.ToInt32(ddlProjects.SelectedValue));
-                    diRange.FromDate = selectedProject.StartDate;
-                    diRange.ToDate = selectedProject.EndDate;
-                }
+                ddlMilestones.Enabled = true;
+
+                int projectId = Convert.ToInt32(ddlProjects.SelectedItem.Value);
+
+                var milestones = GetProjectMilestones(projectId);
+
+                ListItem[] items = milestones.Select(
+                                                        milestone => new ListItem(
+                                                                                    milestone.Description,
+                                                                                    milestone.Id.ToString()
+                                                                                )
+                                                    ).ToArray();
+                ddlMilestones.Items.AddRange(items);
+
+                //We need to show Entire Project details by default.
+                ShowEntireProjectDetails();
+            }
+            else
+            {
+                ddlMilestones.Enabled = false;
             }
             UpdateHeaderTitle();
+        }
+
+        protected void ddlMilestones_OnSelectedIndexChanged(object sender, EventArgs e)
+        {
+            diRange.FromDate = null;
+            diRange.ToDate = null;
+            //Clearing Persons Scrolling Dropdown as milestone is changed and then rebinding TimeEntries list control.
+            cblPersons.Items.Clear();
+
+            dlTimeEntries.DataBind();
+            dlTimeEntries.Visible = true;
+
+            if (!string.IsNullOrEmpty(ddlMilestones.SelectedValue))
+            {
+                using (var milestoneService = new MilestoneService.MilestoneServiceClient())
+                {
+                    var selectedMilestone = milestoneService.GetMilestoneDataById(Convert.ToInt32(ddlMilestones.SelectedValue));
+                    diRange.FromDate = selectedMilestone.StartDate;
+                    diRange.ToDate = selectedMilestone.EndDate;
+                }
+            }
+            else
+            {
+                ShowEntireProjectDetails();
+            }
+
+            UpdateHeaderTitle();
+
         }
 
         private void UpdateGrandTotal()
@@ -210,6 +265,11 @@ namespace PraticeManagement.Controls.TimeEntry
 
                 e.InputParameters["projectId"] = ddlProjects.SelectedValue;
 
+                if (!string.IsNullOrEmpty(ddlMilestones.SelectedValue))
+                {
+                    e.InputParameters["milestoneId"] = ddlMilestones.SelectedValue;
+                }
+
                 if (diRange.FromDate.HasValue)
                 {
                     e.InputParameters["startDate"] = diRange.FromDate.Value;
@@ -232,7 +292,7 @@ namespace PraticeManagement.Controls.TimeEntry
                 personList.Add(key);
             }
 
-            //We are not refreshing the Persons List, until and unless the project changes.
+            //We are not refreshing the Persons List, until and unless the project changes OR Milestone changes.
             if (cblPersons.Items.Count == 0)
             {
                 DataHelper.FillTimeEntryPersonList(cblPersons, Resources.Controls.AllPersons, null, personList);
@@ -252,6 +312,33 @@ namespace PraticeManagement.Controls.TimeEntry
             {
                 if (targetItem != null)
                     targetItem.Selected = true;
+            }
+        }
+
+        private void ShowEntireProjectDetails()
+        {
+            dlTimeEntries.Visible = true;
+            using (var projectservice = new ProjectService.ProjectServiceClient())
+            {
+                var selectedProject = projectservice.ProjectGetById(Convert.ToInt32(ddlProjects.SelectedValue));
+                diRange.FromDate = selectedProject.StartDate;
+                diRange.ToDate = selectedProject.EndDate;
+            }
+        }
+
+        private void SetFirstItemOfMilestones()
+        {
+            ListItem firstItem = new ListItem("Entire Project", string.Empty);
+            ddlMilestones.Items.Clear();
+            ddlMilestones.Items.Add(firstItem);
+            ddlMilestones.DataBind();
+        }
+
+        private static Milestone[] GetProjectMilestones(int projectId)
+        {
+            using (var serviceClient = new MilestoneService.MilestoneServiceClient())
+            {
+                return serviceClient.MilestoneListByProject(projectId);
             }
         }
     }
