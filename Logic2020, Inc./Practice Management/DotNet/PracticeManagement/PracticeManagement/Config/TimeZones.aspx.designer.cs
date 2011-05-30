e458b4db7314897268338cec2ec05d4c93fa0be5
SrinivasM <SrinivasM>
2011-05-30 09:50:00 -0500

    Comments - Added logic for check box Day light savings time effect.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/Config/TimeZones.aspx.designer.cs
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/Config/TimeZones.aspx.designer.cs
@@ -31,6 +31,15 @@ namespace PraticeManagement.Config {
         protected global::System.Web.UI.WebControls.Button btnSetTimeZone;
         
         /// <summary>
+        /// cbIsDayLightSavingsTimeEffect control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.CheckBox cbIsDayLightSavingsTimeEffect;
+        
+        /// <summary>
         /// successMessage control.
         /// </summary>
         /// <remarks>
