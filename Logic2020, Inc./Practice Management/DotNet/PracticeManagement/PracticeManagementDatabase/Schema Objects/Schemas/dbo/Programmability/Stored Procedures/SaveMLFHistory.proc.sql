6c014e58fae65ac3912905653df6fb070a955647
ThulasiramP <ThulasiramP>
2011-05-30 11:18:00 -0500

    none

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/SaveMLFHistory.proc.sql
@@ -0,0 +1,39 @@
+﻿CREATE PROCEDURE [dbo].[SaveMLFHistory]
+	@TimescaleId INT, 
+	@Rate DECIMAL,
+	@Today DATETIME
+AS
+BEGIN
+	SET NOCOUNT ON
+	
+	DECLARE @prevStartDate DATETIME, @W2_Hourly DECIMAL, @W2_Salary DECIMAL, @1099 DECIMAL 
+	
+	SELECT @prevStartDate = [StartDate]
+			,@W2_Hourly = [W2-Hourly]
+			, @W2_Salary = [W2-Salary]
+			, @1099 = [1099]
+	FROM [MinimumLoadFactorHistoryForUI]
+	WHERE [EndDate] IS NULL
+	
+	UPDATE [MinimumLoadFactorHistoryForUI]
+		SET [EndDate] = CASE WHEN (@Today  = @prevStartDate)  THEN @Today
+						ELSE @Today - 1 end
+	FROM [MinimumLoadFactorHistoryForUI]
+	WHERE [EndDate] IS NULL
+	
+	IF (@TimescaleId = 1)
+	BEGIN
+		INSERT INTO [MinimumLoadFactorHistoryForUI] ([StartDate], [EndDate], [W2-Hourly], [W2-Salary], [1099])
+		VALUES (@Today, NULL, @Rate, @W2_Salary, @1099)	
+	END
+	ELSE IF (@TimescaleId = 2)
+	BEGIN
+		INSERT INTO [MinimumLoadFactorHistoryForUI] ([StartDate], [EndDate], [W2-Hourly], [W2-Salary], [1099])
+		VALUES (@Today, NULL, @W2_Hourly, @Rate, @1099)	
+	END
+	ELSE IF (@TimescaleId = 3)
+	BEGIN
+		INSERT INTO [MinimumLoadFactorHistoryForUI] ([StartDate], [EndDate], [W2-Hourly], [W2-Salary], [1099])
+		VALUES (@Today, NULL, @W2_Hourly, @W2_Salary, @Rate)	
+	END
+END
