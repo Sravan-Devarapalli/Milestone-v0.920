aa1412c00f91ed4f0a6a5f38fb55e05e757c5bb5
ThulasiramP <ThulasiramP>
2011-05-31 05:42:00 -0500

    Comments - Changes made as per Jason feed back.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/Config/Overheads.aspx.designer.cs
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/Config/Overheads.aspx.designer.cs
@@ -13,6 +13,15 @@ namespace PraticeManagement.Config {
     public partial class Overheads {
         
         /// <summary>
+        /// upnlOverHead control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.UpdatePanel upnlOverHead;
+        
+        /// <summary>
         /// cpe control.
         /// </summary>
         /// <remarks>
