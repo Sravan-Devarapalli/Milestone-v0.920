5e0a2fb3a3aeb5bca47292176241f1184bc8518e
ThulasiramP <ThulasiramP>
2011-05-31 05:43:00 -0500

    Comments - Defect Number: 2644; Defect RecordID: 2640; Server Address: services.logic2020.com; Server Port: 99; Database Name: Practice Management; Defect Summary: Configuration>Company Resources>Clients: UI/functionality changes; Defect Description: 1. Make the checkboxes in both the "Active" and "Billable by default" columns functional so that checking/unchecking the boxes is allowed and committed immediately without the page needing to be reloaded/refreshed and/or saved. 2. Make sure that even though the default filter is for "Active" clients by default that unchecking an "Active" checkbox for a client does not result in the table reloading immediately (the client should not disappear, in other words), in case they change their mind, etc. When the user browses away from the page and comes back it should then only show the "Active" clients. 3. Allow more column width for the "Client Name" column. 4. The table contents should change/refresh whenever either the "Show Active Clients Only" is checked/unchecked or whenever a letter (shortcut to clients that begin with the letter chosen) is selected. It should not cause the entire page to be reloaded.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/Config/Clients.aspx.designer.cs
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/Config/Clients.aspx.designer.cs
@@ -85,6 +85,15 @@ namespace PraticeManagement.Config {
         protected global::System.Web.UI.WebControls.Button btnSearchAll;
         
         /// <summary>
+        /// btnClearResults control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnClearResults;
+        
+        /// <summary>
         /// ddlView control.
         /// </summary>
         /// <remarks>
@@ -146,6 +155,15 @@ namespace PraticeManagement.Config {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.LinkButton lnkbtnNext;
+        
+        /// <summary>
+        /// hdnActive control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HiddenField hdnActive;
         
         /// <summary>
         /// pnlFilters control.
