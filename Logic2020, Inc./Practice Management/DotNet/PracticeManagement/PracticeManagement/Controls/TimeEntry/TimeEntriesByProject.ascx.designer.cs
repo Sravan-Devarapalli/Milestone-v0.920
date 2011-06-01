adfe8249e36367c00be0cd84d8d88ad74cc237a3
SrinivasM <SrinivasM>
2011-06-01 03:51:00 -0500

    Comments - Added all persons in the list regarding either they entered timeentries or not.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/Controls/TimeEntry/TimeEntriesByProject.ascx.designer.cs
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/Controls/TimeEntry/TimeEntriesByProject.ascx.designer.cs
@@ -146,15 +146,6 @@ namespace PraticeManagement.Controls.TimeEntry {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Label lblGrandTotal;
-        
-        /// <summary>
-        /// odsTimeEntries control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.ObjectDataSource odsTimeEntries;
     }
 }
 
