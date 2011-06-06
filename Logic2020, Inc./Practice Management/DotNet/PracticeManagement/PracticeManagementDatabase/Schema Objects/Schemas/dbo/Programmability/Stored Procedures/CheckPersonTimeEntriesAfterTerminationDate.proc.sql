103d4464ca7a3fa358e7e005e09abcb7748596f0
RaviN <RaviN>
2011-06-06 09:29:00 -0500

    Comments - Defect Number: 2867; Defect RecordID: 2863; Server Address: services.logic2020.com; Server Port: 99; Database Name: Practice Management; Defect Summary: Configuration>Company Resources>Persons: Unable to set a future end/termination date; Defect Description: It should be possible to set a future termination date for a person that, upon that day, PM will change the person's status to "Terminated" and add an end date to their compensation record as well.

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/CheckPersonTimeEntriesAfterTerminationDate.proc.sql
@@ -0,0 +1,21 @@
+﻿CREATE PROCEDURE dbo.CheckPersonTimeEntriesAfterTerminationDate
+(
+	@PersonId	INT,
+	@TerminationDate	DATETIME
+)
+AS
+BEGIN
+	DECLARE @TimeEntriesExists BIT
+	IF EXISTS(
+					SELECT 1 FROM dbo.TimeEntries TE
+					JOIN dbo.MilestonePerson MP ON TE.MilestonePersonId = MP.MilestonePersonId
+					WHERE MP.PersonId = @PersonId AND TE.MilestoneDate >= @TerminationDate
+				)
+
+		SET @TimeEntriesExists  = 1
+	ELSE
+		SET @TimeEntriesExists = 0
+
+	SELECT @TimeEntriesExists
+
+END
