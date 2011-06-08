0fb7308a90917c2927069b92eb76083c29e631bb
RaviN <RaviN>
2011-06-08 04:20:00 -0500

    Comments - Changes procedures to get date based on timezone and DayLight saving and added some bjects related to daily auto updation process in Azure.

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/AutoUpdateObjects.proc.sql
@@ -0,0 +1,81 @@
+﻿CREATE PROCEDURE dbo.AutoUpdateObjects
+(
+	@LastRun DATETIME,
+	@NextRun DATETIME
+)
+AS
+BEGIN
+	 
+	DECLARE	 @ERROR_MESSAGE		    nvarchar(2000)
+
+
+	SELECT	 @ERROR_MESSAGE		    = NULL
+	Declare @Error NVARCHAR(2000)
+ 
+
+ --Main code:
+	BEGIN TRY
+
+		 BEGIN TRANSACTION AutoTerminatePersons_Tran
+
+			EXEC dbo.AutoTerminatePersons
+ 
+
+			EXECUTE [dbo].[SaveSchedularLog] 
+			   @LastRun = @LastRun
+			  ,@Status = 'Success'
+			  ,@Comments ='Successfully comepleted running the procedure "dbo.AutoTerminatePersons"'
+			  ,@NextRun = @NextRun
+
+			COMMIT TRANSACTION AutoTerminatePersons_Tran
+
+	END TRY
+	BEGIN CATCH
+
+		SET	 @ERROR_MESSAGE		= ERROR_MESSAGE()
+		ROLLBACK TRANSACTION  AutoTerminatePersons_Tran
+		  SELECT @Error = 'Failed running the procedure "dbo.AutoTerminatePersons". The Error message is :'+@Error
+		  EXECUTE [dbo].[SaveSchedularLog] 
+			   @LastRun = @LastRun
+			  ,@Status = 'Failed'
+			  ,@Comments =@Error
+			  ,@NextRun = @NextRun
+ 
+
+	END CATCH
+
+--Main code:
+	BEGIN TRY
+
+		 BEGIN TRANSACTION SetPersonSeniorityPractice_Tran
+
+			EXEC dbo.UpdatePersonSeniorityPracticeFromCurrentPay
+ 
+
+			EXECUTE dbo.[SaveSchedularLog]
+			   @LastRun = @LastRun
+			  ,@Status = 'Success'
+			  ,@Comments ='Successfully comepleted running the procedure "dbo.UpdatePersonSeniorityPracticeFromCurrentPay"'
+			  ,@NextRun = @NextRun
+
+			COMMIT TRANSACTION SetPersonSeniorityPractice_Tran
+
+	END TRY
+	BEGIN CATCH
+
+		SET	 @ERROR_MESSAGE		= ERROR_MESSAGE()
+		ROLLBACK TRANSACTION  SetPersonSeniorityPractice_Tran
+
+		  SELECT @Error = 'Failed running the procedure "dbo.UpdatePersonSeniorityPracticeFromCurrentPay". The Error message is :'+@Error
+		  EXECUTE [dbo].[SaveSchedularLog] 
+			   @LastRun = @LastRun
+			  ,@Status = 'Failed'
+			  ,@Comments =@Error
+			  ,@NextRun = @NextRun
+ 
+
+	END CATCH
+
+
+END
+ 
