37078411ce4a2b4db94cd1008b824861bf797696
RaviN <RaviN>
2011-06-08 10:50:00 -0500

    Comments - Fixed the issue sent through Email with subject 'Can you check security permissions for "Recruiter" role?'  on 6/2/2011. The fix is made as per the comments sent on 6/4/2011.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/RecruiterCommission.table.sql
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/RecruiterCommission.table.sql
@@ -2,7 +2,7 @@
     [RecruiterId]    INT             NOT NULL,
     [RecruitId]      INT             NOT NULL,
     [HoursToCollect] INT             NOT NULL,
-    [Amount]         DECIMAL (18, 2) NOT NULL
+    [Amount]         DECIMAL (18, 2) NULL
 );
 
 
