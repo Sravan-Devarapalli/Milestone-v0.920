2994088382de05935b9014b577d539f0bdb1943b
ThulasiramP <ThulasiramP>
2011-06-08 09:50:00 -0500

    Comments - Defect Number: 2704; Defect RecordID: 2700; Server Address: services.logic2020.com; Server Port: 99; Database Name: Practice Management; Defect Summary: Configuration>Company Resources: Create "Opportunity Priorities" page; Defect Description: Need to add an "Opportunity Priorities" configuration page. From that page the Admin should be able to define what A B C D are (text) for the company, or which of those letters are used (via a checkbox, all selected by default). Characters should be somewhat limited - 50 max, perhaps.
    On the Opportunity page add a "Legend" bar/drop so that the users can quickly refresh themselves what each priority stands for. This will populate with whatever has been chosen/entered/saved on the config page.
    
    On the Add Opportunity page, when the user uses the drop-down to select the Priority, can it show the definition of each Priority as well, wrapping if necessary (so that we don't have wide drop-down)? EX:
    
    [x]A - Text text text
    [x]B - Text text text
        text text text
    [x]C - Text Text
    [x]D - Text Text
    
    [Save All]

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/GetOpportunityPriorities.proc.sql
@@ -0,0 +1,19 @@
+﻿CREATE PROCEDURE [dbo].[GetOpportunityPriorities]
+@IsInserted BIT
+AS
+BEGIN
+	SET NOCOUNT ON
+	SELECT  	OP.Id,
+			OP.Priority,
+			OP.Description,
+			CASE 
+			WHEN EXISTS(SELECT TOP 1 o.PriorityId FROM  v_Opportunity AS o WHERE o.PriorityId = op.Id)
+				THEN CAST(1 AS BIT)
+			ELSE CAST(0 AS BIT)
+		END AS 'InUse'
+	FROM dbo.OpportunityPriorities AS OP
+	WHERE  op.IsInserted = @IsInserted
+	ORDER BY OP.sortOrder
+END
+
+GO
