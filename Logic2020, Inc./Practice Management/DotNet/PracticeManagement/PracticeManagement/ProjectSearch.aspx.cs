b1ce548c037381c8dd7a8c8f2bd7cd46209da45b
SrinivasM <SrinivasM>
2011-06-10 04:59:00 -0500

    Comments - Modified project summary filter bar, and search results.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/ProjectSearch.aspx.cs
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/ProjectSearch.aspx.cs
@@ -4,131 +4,190 @@ using System.Text.RegularExpressions;
 using System.Web.UI.WebControls;
 using PraticeManagement.Controls;
 using PraticeManagement.ProjectService;
+using DataTransferObjects;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace PraticeManagement
 {
-	public partial class ProjectSearch : PracticeManagementPageBase
-	{
-		private const string RegexSpecialChars = ".[]()\\-*%+|$?^";
+    public partial class ProjectSearch : PracticeManagementPageBase
+    {
+        private const string RegexSpecialChars = ".[]()\\-*%+|$?^";
 
-		private string RegexReplace
-		{
-			get;
-			set;
-		}
+        private string RegexReplace
+        {
+            get;
+            set;
+        }
 
-		protected void Project_Command(object sender, CommandEventArgs e)
-		{
+        protected void Project_Command(object sender, CommandEventArgs e)
+        {
             RedirectWithBack(
                 string.Format(
                 Constants.ApplicationPages.DetailRedirectFormat,
                 Constants.ApplicationPages.ProjectDetail,
                 e.CommandArgument),
                 Constants.ApplicationPages.Projects);
-		}
+        }
 
-		protected void btnClientName_Command(object sender, CommandEventArgs e)
-		{
+        protected void btnClientName_Command(object sender, CommandEventArgs e)
+        {
             RedirectWithBack(
                 string.Format(
                     Constants.ApplicationPages.DetailRedirectFormat,
                     Constants.ApplicationPages.ClientDetails,
                     e.CommandArgument),
                 Constants.ApplicationPages.Projects);
-		}
+        }
 
-		protected void btnMilestoneName_Command(object sender, CommandEventArgs e)
-		{
-			string[] parts = e.CommandArgument.ToString().Split('_');
+        protected void btnMilestoneName_Command(object sender, CommandEventArgs e)
+        {
+            string[] parts = e.CommandArgument.ToString().Split('_');
 
-			if (parts.Length >= 2)
-			{
+            if (parts.Length >= 2)
+            {
                 RedirectWithBack(string.Concat(
-					string.Format(Constants.ApplicationPages.DetailRedirectFormat,
-					    Constants.ApplicationPages.MilestoneDetail,
-					    parts[0]), "&projectId=", parts[1]),
+                    string.Format(Constants.ApplicationPages.DetailRedirectFormat,
+                        Constants.ApplicationPages.MilestoneDetail,
+                        parts[0]), "&projectId=", parts[1]),
                      Constants.ApplicationPages.Projects);
-			}
-		}
+            }
+        }
 
-		protected void btnSearch_Click(object sender, EventArgs e)
-		{
-			DisplaySearch();
-		}
+        protected void btnSearch_Click(object sender, EventArgs e)
+        {
+            DisplaySearch();
+        }
 
-		/// <summary>
-		/// Hightlight the text searched.
-		/// </summary>
-		/// <param name="result">The found value.</param>
-		/// <returns>The text with searched substring highlighted.</returns>
-		protected string HighlightFound(object result)
-		{
-			if (string.IsNullOrEmpty(RegexReplace))
-			{
-				RegexReplace = txtSearchText.Text;
+        /// <summary>
+        /// Hightlight the text searched.
+        /// </summary>
+        /// <param name="result">The found value.</param>
+        /// <returns>The text with searched substring highlighted.</returns>
+        protected string HighlightFound(object result)
+        {
+            if (string.IsNullOrEmpty(RegexReplace))
+            {
+                RegexReplace = txtSearchText.Text;
 
-				foreach (char specialChar in RegexSpecialChars)
-				{
-					RegexReplace =
-						RegexReplace.Replace(specialChar.ToString(), string.Format("\\{0}", specialChar));
-				}
+                foreach (char specialChar in RegexSpecialChars)
+                {
+                    RegexReplace =
+                        RegexReplace.Replace(specialChar.ToString(), string.Format("\\{0}", specialChar));
+                }
 
-				RegexReplace = RegexReplace.Replace(" ", "\\s");
-			}
+                RegexReplace = RegexReplace.Replace(" ", "\\s");
+            }
 
-			string strResult;
+            string strResult;
 
-			if (result != null)
-			{
-				strResult =
-					Regex.Replace(
-					result.ToString(),
-					RegexReplace,
-					Constants.Formatting.SearchResultFormat,
-					RegexOptions.IgnoreCase);
-			}
-			else
-			{
-				strResult = string.Empty;
-			}
+            if (result != null)
+            {
+                strResult =
+                    Regex.Replace(
+                    result.ToString(),
+                    RegexReplace,
+                    Constants.Formatting.SearchResultFormat,
+                    RegexOptions.IgnoreCase);
+            }
+            else
+            {
+                strResult = string.Empty;
+            }
 
-			return strResult;
-		}
+            return strResult;
+        }
 
-		protected override void Display()
-		{
-			if (PreviousPage != null)
-			{
-				txtSearchText.Text = PreviousPage.SearchText;
-				DisplaySearch();
-			}
-		}
+        protected override void Display()
+        {
+            if (PreviousPage != null)
+            {
+                txtSearchText.Text = PreviousPage.SearchText;
+                DisplaySearch();
+            }
+        }
 
-		private void DisplaySearch()
-		{
-			Page.Validate();
-			if (Page.IsValid)
-			{
-				using (var serviceClient = new ProjectServiceClient())
-				{
-					try
-					{
-						var projects =
-							serviceClient.ProjectSearchText(
-                                txtSearchText.Text, 
+        private void DisplaySearch()
+        {
+            Page.Validate();
+            if (Page.IsValid)
+            {
+                using (var serviceClient = new ProjectServiceClient())
+                {
+                    try
+                    {
+                        var projects =
+                            serviceClient.ProjectSearchText(
+                                txtSearchText.Text,
                                 DataHelper.CurrentPerson.Id.Value);
 
-						lvProjects.DataSource = projects;
-						lvProjects.DataBind();
-					}
-					catch (CommunicationException)
-					{
-						serviceClient.Abort();
-						throw;
-					}
-				}
-			}
-		}
-	}
+                        IEnumerable<int> ProjectIds = projects.ToList().FindAll(p => p.Id.HasValue).Select(q => q.Id.Value).Distinct();
+                        var groupedProjects = new List<Project>();
+                        foreach (var ProjectId in ProjectIds)
+                        {
+                            var project = projects.First(p => p.Id.HasValue && p.Id.Value == ProjectId);
+                            var milestones = new List<Milestone>();
+                            foreach (var tempProject in projects.ToList().FindAll(p => p.Id.HasValue && p.Id.Value == ProjectId))
+                            {
+                                if (tempProject.Milestones != null && tempProject.Milestones.Any())
+                                {
+                                    milestones.AddRange(tempProject.Milestones);
+                                }
+                            }
+                            project.Milestones = milestones;
+                            groupedProjects.Add(project);
+                        }
+
+                        lvProjects.DataSource = groupedProjects;
+                        lvProjects.DataBind();
+                    }
+                    catch (CommunicationException)
+                    {
+                        serviceClient.Abort();
+                        throw;
+                    }
+                }
+            }
+        }
+
+        protected void lvProjects_ItemDataBound(object sender, ListViewItemEventArgs e)
+        {
+            if (e.Item.ItemType == ListViewItemType.DataItem)
+            {
+                var panel = e.Item.FindControl("pnlMilestones");
+                var project = (e.Item as ListViewDataItem).DataItem as Project;
+
+                if (project.Milestones != null && project.Milestones.Any())
+                {
+                    if (panel != null)
+                    {
+                        var datalist = panel.FindControl("dtlProposedPersons") as DataList;
+                        if (datalist != null)
+                        {
+                            foreach (var milestone in project.Milestones)
+                            {
+                                milestone.Project = project;
+                            }
+                            datalist.DataSource = project.Milestones;
+                            datalist.DataBind();
+                        }
+                    }
+                }
+                else
+                {
+                    var collapsiblepanelextender = e.Item.FindControl("cpe") as AjaxControlToolkit.CollapsiblePanelExtender;
+                    var btnExpandCollapseMilestones = e.Item.FindControl("btnExpandCollapseMilestones") as Image;
+                    collapsiblepanelextender.Enabled = btnExpandCollapseMilestones.Visible = false;
+                    var btnProjectName = e.Item.FindControl("btnProjectName") as LinkButton;
+                    btnProjectName.Style.Add("padding-left", "15px");
+                }
+
+                //LinkButton milestone = 
+                //ID="btnMilestoneNames" runat="server" Text='<%# HighlightFound(Eval("Milestones[0].Description")) %>'
+                //                CommandArgument='<%# string.Concat(Eval("Milestones[0].Id"), "_", Eval("Id")) %>'
+                //                OnCommand="btnMilestoneName_Command"
+            }
+        }
+    }
 }
 
