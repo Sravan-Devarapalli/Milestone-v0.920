6ca5998601fc419bab749c1ec0c9ba814c3129e3
SrinivasM <SrinivasM>
2011-06-10 04:59:00 -0500

    Comments - Modified project summary filter bar, and search results.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/ProjectSearch.aspx.designer.cs
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/ProjectSearch.aspx.designer.cs
@@ -49,6 +49,15 @@ namespace PraticeManagement {
         protected global::System.Web.UI.WebControls.ValidationSummary vsumProjectSearch;
         
         /// <summary>
+        /// ExpandAll control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button ExpandAll;
+        
+        /// <summary>
         /// lvProjects control.
         /// </summary>
         /// <remarks>
