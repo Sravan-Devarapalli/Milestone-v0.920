1b657466c82b7dd392e0e70f45744ea3c9526f86
RaviN <RaviN>
2011-06-10 15:52:00 -0500

    Comments - Defect Number: 2871; Defect RecordID: 2867; Server Address: services.logic2020.com; Server Port: 99; Database Name: Practice Management; Defect Summary: Projects>Summary: Change parameters bar/function; Defect Description: This issue is reorganizing the parameters bar and filter options for ease of use.
    1. Replace date range function with pre-set ranges that can be selected from drop-down, as in image. "Next 3 months" is default and would really be current + 2 months (as it is now). Other options listed in image. FY selection requires creating Fiscal Year selection functionality under Configuration>Financials>Fiscal Year. I will mock up page in another issue. New selection results in results output auto-reloading (show "Refreshing Data...").
    2. Keep "Add Project" button right justified. Add "View 10/25/50/All" drop-down to left of "Add Project" button. New selection results in output auto-reloading (show "Refreshing Data...") ****Please let me know if this will cause performance issues that can't be overcome****
    3. Expand "Export" functionality. Change "Export" to "Export View" which, when clicked, will produce an export of just the filtered results shown. If the arrow in the button is clicked there will be an option for "Export All" which will produce an export of all projects in PM.
    4. Change hover text for filter drop-down arrow to "Search, Filter and Calculation Options"
    5. Search tab should be first (not accurate in image below). Typing and hitting enter will perform "Search View" and would only return results from filtered results. Clicking "Search All" would return results based on all PM projects. See note in image about results page with regard to how results are displayed.
    6. Change "Advanced filters" to just "Filters", replace existing scroll and checkboxes, with multi-select drop-downs as in image below. Under "Client / Group", groups drop-down would be grey/unselectable until a client or clients are chosen.
    7. Create "Calculations" tab, move "Project Type" selection checkboxes here and rename as "Project Types Included". Layout as in image below (similar to other areas in PM). Add "Calculated Grand Total" section, which will have a single-select drop-down with three options: "Project Value in Range" (default), "Total Project Value", and "Current Fiscal Year". Current Fiscal Year calculation would rely on the range defined in the Fiscal Year page to be created.
    8. Move "Update View" and "Reset Filter" buttons to the Filters and Calculations tabs, rename as "Update" and "Reset". Keep buttons same size as each other, right justify. No selections made in Filters or Calculations would cause results to auto-reload. User must click "Update" button. "Reset" button resets the selections to default for only the tab being viewed, and does not affect the selected date range or amount of results being shown at a time. It also does not auto-reload page.
    9. Just above results output, insert "Viewing n# of nTotal Projects" in bold, slightly larger font size.
    10. ****INQUIRY*** Can you please tell me how the results are sorted? I will consult with others here to determine best sort.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/ProjectSearch.aspx.designer.cs
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/ProjectSearch.aspx.designer.cs
@@ -67,6 +67,15 @@ namespace PraticeManagement {
         protected global::System.Web.UI.WebControls.ListView lvProjects;
         
         /// <summary>
+        /// hdnExpandCollapseExtendersIds control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HiddenField hdnExpandCollapseExtendersIds;
+        
+        /// <summary>
         /// PreviousPage property.
         /// </summary>
         /// <remarks>
