69e415217f6ddcb0aa5f56a212f09236498100b6
RaviN <RaviN>
2011-06-13 11:47:00 -0500

    none

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/PersonMilestoneWithFinancials.proc.sql
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/PersonMilestoneWithFinancials.proc.sql
@@ -16,13 +16,15 @@ SELECT  p.ProjectId,
         mpe.EndDate,
         r.Name AS 'RoleName',
         f.Revenue,
-        f.GrossMargin
+        f.GrossMargin,
+        pa.[FileName]
 FROM    dbo.Project AS p
 		INNER JOIN dbo.ProjectStatus AS s ON p.ProjectStatusId = s.ProjectStatusId
 		INNER JOIN dbo.Milestone AS m ON p.ProjectId = m.ProjectId
         INNER JOIN dbo.MilestonePerson AS mp ON m.MilestoneId = mp.MilestoneId
         INNER JOIN dbo.MilestonePersonEntry AS mpe ON mp.MilestonePersonId = mpe.MilestonePersonId
         LEFT JOIN dbo.PersonRole AS r ON mpe.PersonRoleId = r.PersonRoleId
+		LEFT JOIN dbo.ProjectAttachment AS pa ON p.ProjectId = pa.ProjectId
         INNER JOIN v_FinancialsForMilestones AS f ON m.MilestoneId = f.MilestoneId
                                                      AND mp.PersonId = f.PersonId
 WHERE   f.PersonId = @PersonId
