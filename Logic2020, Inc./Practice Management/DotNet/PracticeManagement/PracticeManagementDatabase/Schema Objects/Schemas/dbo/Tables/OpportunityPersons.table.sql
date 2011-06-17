b49faaca9c899dc4e15bd01770707dc3330085d8
ThulasiramP <ThulasiramP>
2011-06-17 10:57:00 -0500

    Comments - Defect Number: 2877; Defect RecordID: 2873; Server Address: services.logic2020.com; Server Port: 99; Database Name: Practice Management; Defect Summary: Opportunity>Discussion Review 2: Minor expansion to Proposed Resources selection box; Defect Description: I'd like to make the current selection checkbox the "check" column as in the image below. This functions exactly the way it does currently and makes the person name appear in the DR2 page. If this is checked, the X checkbox cannot be checked.
    The X column would make the name appear with a strikethrough on the DR2 page. If this is checked, the check checkbox cannot be checked.
    
    These checkboxes also need to appear in the Add Oppty and Oppty Detail pages. When converting an Oppty to a Project, only the names with the "check" checkbox selected should carry over.
    
    
    Alternatively, is it possible or easier to do this with only a single column of checkboxes, where the first click in the box selects the name, a second click would strikethrough the name, and a third click would clear the name entirely? The progress would look like this:
    
    Name[ ] (default, not selected)
    Name[ / ] (one-click, selected)
    Name[ X ] (two clicks, strikethrough)
    Name[ ] (three clicks, back to default)

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/OpportunityPersons.table.sql
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/OpportunityPersons.table.sql
@@ -3,6 +3,8 @@
 	Id              INT IDENTITY(1,1) NOT NULL,
 	OpportunityId	INT NOT NULL, 
 	PersonId		INT NOT NULL,
+	OpportunityPersonTypeId INT NOT NULL
 	CONSTRAINT PK_OpportunityPersons_Id PRIMARY KEY CLUSTERED(Id),
-    CONSTRAINT FK_OpportunityPersons_OpportunityId FOREIGN KEY(OpportunityId) REFERENCES  dbo.Opportunity(OpportunityId)
-);
+    CONSTRAINT FK_OpportunityPersons_OpportunityId FOREIGN KEY(OpportunityId) REFERENCES  dbo.Opportunity(OpportunityId),
+	CONSTRAINT FK_OpportunityPersons_OpportunityPersonTypeId FOREIGN KEY(OpportunityPersonTypeId) REFERENCES  dbo.OpportunityPersonType([Id])
+); 
