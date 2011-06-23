13f11180ff8a510220dd2d3aabedd4eb4ba002f1
RaviN <RaviN>
2011-06-23 08:41:00 -0500

    Comments - Defect Number: 2884; Defect RecordID: 2880; Server Address: services.logic2020.com; Server Port: 99; Database Name: Practice Management; Defect Summary: Opportunities: Make "Start Date" mandatory, change default sort; Defect Description: Current default sort is:
    Priority > Salesperson > Client > BuyerName
    
    (also raises lower Priority Opptys with same BuyerName so that they can be discussed at same time)
    
    Change to:
    
    Priority > StartDate > Salesperson > Client > BuyerName
    
    (keep raising lower Priority Opptys with same BuyerName)
    
    QUESTION: Though START DATE may be specifically stated as, for example, 6/3/11 or 6/8/11, can they be treated for sorting purposes as just MM/YY? This would be so that opptys for the month can be grouped by Salesperson easier.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/OpportunityDetail.aspx.designer.cs
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/OpportunityDetail.aspx.designer.cs
@@ -76,6 +76,15 @@ namespace PraticeManagement {
         protected global::System.Web.UI.WebControls.RequiredFieldValidator reqStartDate;
         
         /// <summary>
+        /// cmpStartDateDataTypeCheck control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.CompareValidator cmpStartDateDataTypeCheck;
+        
+        /// <summary>
         /// dpEndDate control.
         /// </summary>
         /// <remarks>
@@ -92,6 +101,15 @@ namespace PraticeManagement {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.RequiredFieldValidator reqEndDate;
+        
+        /// <summary>
+        /// cmpEndDateDataTypeCheck control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.CompareValidator cmpEndDateDataTypeCheck;
         
         /// <summary>
         /// compEndDate control.
