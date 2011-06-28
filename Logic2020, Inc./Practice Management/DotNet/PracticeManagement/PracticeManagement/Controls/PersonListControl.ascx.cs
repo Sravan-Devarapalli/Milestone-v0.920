6a3eca78b56c856826d00389056de2c98a312c7c
RaviN <RaviN>
2011-06-28 10:27:00 -0500

    Comments - Defect Number: 2885; Defect RecordID: 2881; Server Address: services.logic2020.com; Server Port: 99; Database Name: Practice Management; Defect Summary: Configuration>Company Resources>Persons>Application Security>Seniority: Add additional seniority roles; Defect Description: We need to add new roles to Seniority drop-down: Architect Lead Developer Senior Developer Developer
    They will be equivalent to existing roles:
    Senior Manager
    Manager
    Senior Consultant
    Consultant

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/Controls/PersonListControl.ascx.cs
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/Controls/PersonListControl.ascx.cs
@@ -270,8 +270,8 @@ namespace PraticeManagement.Controls
 
                 case "Seniority":
                     //  Assign lowest seniority to the ones who have no one
-                    cx = x.Seniority == null ? 105 : x.Seniority.Id;
-                    cy = y.Seniority == null ? 105 : y.Seniority.Id;
+                    cx = x.Seniority == null ? 105 : Seniority.GetSeniorityValueById(x.Seniority.Id);
+                    cy = y.Seniority == null ? 105 : Seniority.GetSeniorityValueById(y.Seniority.Id);
                     break;
 
                 case "Manager":
