97cb1752e6889be7362b23c7ae54fa87f69dd6e9
RaviN <RaviN>
2011-06-28 10:27:00 -0500

    Comments - Defect Number: 2885; Defect RecordID: 2881; Server Address: services.logic2020.com; Server Port: 99; Database Name: Practice Management; Defect Summary: Configuration>Company Resources>Persons>Application Security>Seniority: Add additional seniority roles; Defect Description: We need to add new roles to Seniority drop-down: Architect Lead Developer Senior Developer Developer
    They will be equivalent to existing roles:
    Senior Manager
    Manager
    Senior Consultant
    Consultant

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/PersonListAllByStatusList.proc.sql
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/PersonListAllByStatusList.proc.sql
@@ -10,7 +10,7 @@ BEGIN
 	DECLARE @Query NVARCHAR(4000),
 			@Where NVARCHAR(4000)
 			
-	DECLARE @PersonSeniorityId INT
+	DECLARE @PersonSeniorityValue INT
 		
 	SET @Where = ''
 
@@ -18,7 +18,9 @@ BEGIN
 					   FirstName,
 					   LastName,
 					   IsDefaultManager
-				FROM dbo.Person'
+				FROM dbo.Person P
+				JOIN dbo.Seniority S
+				ON P.SeniorityId = S.SeniorityId'
 	  
 	  IF(ISNULL(@PersonStatusIdsList,'')<>'')
 	  BEGIN
@@ -26,14 +28,15 @@ BEGIN
 	  END
 	  IF (ISNULL(@PersonId,'') <> '')
 	  BEGIN
-		SELECT @PersonSeniorityId = SeniorityId 
-		FROM Person
+		SELECT @PersonSeniorityValue = S.SeniorityValue 
+		FROM dbo.Person P
+		JOIN dbo.Seniority S ON S.SeniorityId = P.SeniorityId
 		WHERE PersonId = @PersonId
 				
 		SET @Where = (CASE WHEN @Where='' THEN ' WHERE ' ELSE @Where+ ' AND ' END )
 						+ 'PersonId = ' + CONVERT(NVARCHAR,@PersonId) 
-						+ (CASE WHEN @PersonSeniorityId <= 65 -- According to 2656, Managers and up should be able to see their subordinates, but not equals.
-								THEN ' OR SeniorityId > '+ CONVERT(NVARCHAR,@PersonSeniorityId)
+						+ (CASE WHEN @PersonSeniorityValue <= 65 -- According to 2656, Managers and up should be able to see their subordinates, but not equals.
+								THEN ' OR S.SeniorityValue > '+ CONVERT(NVARCHAR,@PersonSeniorityValue)
 								ELSE '' END
 							)
 	  END
