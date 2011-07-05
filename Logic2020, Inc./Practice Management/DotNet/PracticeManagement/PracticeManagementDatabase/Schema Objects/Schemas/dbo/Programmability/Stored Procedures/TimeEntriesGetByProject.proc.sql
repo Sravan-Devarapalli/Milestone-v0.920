b7c7d8a7ca267c9df7794706f191fba68dbc8ed4
RaviN <RaviN>
2011-07-05 01:00:00 -0500

    Comments - Defect Number: 2645; Defect RecordID: 2641; Server Address: services.logic2020.com; Server Port: 99; Database Name: Practice Management; Defect Summary: Configuration>Time>Default Project/Milestone: UI/Layout changes; Defect Description: Currently it's required that a user define the upper and lower bounds in order to select a valid project/milestone to set as the default. Also, because all time is going to one milestone we have timeout issues with the site (the data is too large). Let's make this simpler and more efficient, and take into consideration the situation where an admin/company may be setting PM up for the first time.
    1. Rearrange layout of page per mockup below, where all user-configurable options are displayed on the left side of the page. They should have a blue background same as other pages (this same blue background should envelope the "Default Time Entry Project" (previously "Default Milestone") subcategory heading as well so it's clear where the user is.
    2. On the right side of the page, in a white box, please add the explanation text for each function per the mockup below.
    3. I removed the additional drop-down selection of the Milestone because it should not be necessary to manually choose this based on the changes proposed here. Whatever the Project is that gets chosen (or created if using the button below) a milestone should be created automatically for each calendar month going forward from the current date (format [Month Name] [yyyy], so January 2011, Februrary 2011, etc). Some thought needs to be given to this with regard to end users' experience during Time Entry as well. As of now only one row appears by default. It would be smarter to include a row for each monthly Milestone in the cases where a week is split between two months. For example, at the end of the month Jan 31 falls on a Monday, so February begins on Tuesday. As opposed to just having the one row by default, that week should show two rows, the first containing the January 2011 milestone, and the second containing the Februrary 2011 milestone. Like any other milestone in PM, the days that aren't valid for that milestone will be greyed out.
    4. If the admin/user uses the "Create Default Time Entry Project" it should pre-populate as much as possible to eliminate errors/issues. This will probably be a form specific to this purpose and not the existing "Add Project" form. Please think about what fields are needed at a minimum to simplify the process. The Project name should auto-populate as "[Company Name] Internal/PTO/Holiday". Limit the Practice drop-down to only show practices designated as Internal. Once the form is completed, validate before committing. Committing will create the project and return the admin/user to the Default Time Entry Project page and now show the Client and Project drop-downs populated with the newly created Project. It should not be necessary to click the Save Project button at that point, so it should be greyed out unless a change in the drop-down(s) is made.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/TimeEntriesGetByProject.proc.sql
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/TimeEntriesGetByProject.proc.sql
@@ -9,7 +9,8 @@ BEGIN
 	-- SET NOCOUNT ON added to prevent extra result sets from
 	-- interfering with SELECT statements.
 	SET NOCOUNT ON;
-	
+	IF EXISTS (SELECT 1 FROM Milestone WHERE MilestoneId = @MilestoneId) OR @MilestoneID IS NULL
+	BEGIN
 	select te.*
 	from v_TimeEntries as te
 	where te.ProjectId = @ProjectId
@@ -17,5 +18,27 @@ BEGIN
 		AND ((@PersonIds IS NULL) OR (te.PersonId IN (SELECT ResultId FROM dbo.ConvertStringListIntoTable(@PersonIds))))
 		AND (te.MilestoneId = @MilestoneID OR @MilestoneID IS NULL)
 	order by te.PersonId, te.MilestoneDate
+	END
+	ELSE
+	BEGIN
+		DECLARE @MileStoneStartDate DATETIME,
+				@MileStoneEndDate DATETIME,
+				@MilestoneIdLocal INT
+		SELECT @MilestoneIdLocal = MilestoneId
+		FROM dbo.DefaultMilestoneSetting 
+		SELECT @MileStoneStartDate = CONVERT(DATETIME,CONVERT(NVARCHAR,@MilestoneId)) 
+		SELECT @MileStoneEndDate = DATEADD(MM,1,@StartDate)-1
+		
+		select te.*
+		from v_TimeEntries as te
+		where te.ProjectId = @ProjectId
+			AND te.MilestoneDate between ISNULL(@StartDate, te.MilestoneDate) and ISNULL(@EndDate, te.MilestoneDate)
+			AND te.MilestoneDate between @MileStoneStartDate and @MileStoneEndDate
+			AND te.MilestoneId = @MilestoneIdLocal
+			AND ((@PersonIds IS NULL) OR (te.PersonId IN (SELECT ResultId FROM dbo.ConvertStringListIntoTable(@PersonIds))))
+			AND (te.MilestoneId = @MilestoneIdLocal)
+		order by te.PersonId, te.MilestoneDate
+
+	END
 END
 
