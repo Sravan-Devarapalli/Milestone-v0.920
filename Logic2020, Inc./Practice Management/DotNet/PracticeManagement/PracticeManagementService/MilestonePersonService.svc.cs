12464055189b33778d907df924ccd182f3c49f51
RaviN <RaviN>
2011-07-05 01:00:00 -0500

    Comments - Defect Number: 2645; Defect RecordID: 2641; Server Address: services.logic2020.com; Server Port: 99; Database Name: Practice Management; Defect Summary: Configuration>Time>Default Project/Milestone: UI/Layout changes; Defect Description: Currently it's required that a user define the upper and lower bounds in order to select a valid project/milestone to set as the default. Also, because all time is going to one milestone we have timeout issues with the site (the data is too large). Let's make this simpler and more efficient, and take into consideration the situation where an admin/company may be setting PM up for the first time.
    1. Rearrange layout of page per mockup below, where all user-configurable options are displayed on the left side of the page. They should have a blue background same as other pages (this same blue background should envelope the "Default Time Entry Project" (previously "Default Milestone") subcategory heading as well so it's clear where the user is.
    2. On the right side of the page, in a white box, please add the explanation text for each function per the mockup below.
    3. I removed the additional drop-down selection of the Milestone because it should not be necessary to manually choose this based on the changes proposed here. Whatever the Project is that gets chosen (or created if using the button below) a milestone should be created automatically for each calendar month going forward from the current date (format [Month Name] [yyyy], so January 2011, Februrary 2011, etc). Some thought needs to be given to this with regard to end users' experience during Time Entry as well. As of now only one row appears by default. It would be smarter to include a row for each monthly Milestone in the cases where a week is split between two months. For example, at the end of the month Jan 31 falls on a Monday, so February begins on Tuesday. As opposed to just having the one row by default, that week should show two rows, the first containing the January 2011 milestone, and the second containing the Februrary 2011 milestone. Like any other milestone in PM, the days that aren't valid for that milestone will be greyed out.
    4. If the admin/user uses the "Create Default Time Entry Project" it should pre-populate as much as possible to eliminate errors/issues. This will probably be a form specific to this purpose and not the existing "Add Project" form. Please think about what fields are needed at a minimum to simplify the process. The Project name should auto-populate as "[Company Name] Internal/PTO/Holiday". Limit the Practice drop-down to only show practices designated as Internal. Once the form is completed, validate before committing. Committing will create the project and return the admin/user to the Default Time Entry Project page and now show the Client and Project drop-downs populated with the newly created Project. It should not be necessary to click the Save Project button at that point, so it should be greyed out unless a change in the drop-down(s) is made.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementService/MilestonePersonService.svc.cs
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementService/MilestonePersonService.svc.cs
@@ -9,10 +9,10 @@ using DataTransferObjects.Financials;
 
 namespace PracticeManagementService
 {
-	[AspNetCompatibilityRequirements(RequirementsMode = AspNetCompatibilityRequirementsMode.Allowed)]
-	public class MilestonePersonService : IMilestonePersonService
-	{
-		#region IMilestonePersonService Members
+    [AspNetCompatibilityRequirements(RequirementsMode = AspNetCompatibilityRequirementsMode.Allowed)]
+    public class MilestonePersonService : IMilestonePersonService
+    {
+        #region IMilestonePersonService Members
 
         /// <summary>
         /// Gets milestones info for given person
@@ -22,19 +22,19 @@ namespace PracticeManagementService
             return MilestonePersonDAL.GetConsultantMilestones(context);
         }
 
-		/// <summary>
-		/// Retrives the list of the <see cref="Person"/>s for the specified <see cref="Project"/>.
-		/// </summary>
-		/// <param name="projectId">An ID of the project to the data be retrieved for.</param>
-		/// <returns>The list of the <see cref="MilestonePerson"/> objects.</returns>
-		public List<MilestonePerson> GetMilestonePersonListByProject(int projectId)
-		{
+        /// <summary>
+        /// Retrives the list of the <see cref="Person"/>s for the specified <see cref="Project"/>.
+        /// </summary>
+        /// <param name="projectId">An ID of the project to the data be retrieved for.</param>
+        /// <returns>The list of the <see cref="MilestonePerson"/> objects.</returns>
+        public List<MilestonePerson> GetMilestonePersonListByProject(int projectId)
+        {
             List<MilestonePerson> result = GetMilestonePersonListByProjectWithoutPay(projectId);
-		    foreach (MilestonePerson milestonePerson in result)
-		        milestonePerson.Person.CurrentPay = PayDAL.GetCurrentByPerson(milestonePerson.Person.Id.Value);
+            foreach (MilestonePerson milestonePerson in result)
+                milestonePerson.Person.CurrentPay = PayDAL.GetCurrentByPerson(milestonePerson.Person.Id.Value);
 
-		    return result;
-		}
+            return result;
+        }
 
         /// <summary>
         /// Retrives the list of the <see cref="Person"/>s for the specified <see cref="Project"/>.
@@ -46,15 +46,15 @@ namespace PracticeManagementService
             return MilestonePersonDAL.MilestonePersonListByProject(projectId);
         }
 
-		/// <summary>
-		/// Retrives the list of the <see cref="Person"/>s for the specified <see cref="Milestone"/>.
-		/// </summary>
-		/// <param name="milestoneId">An ID of the milestone to the data be retrieved for.</param>
-		/// <returns>The list of the <see cref="MilestonePerson"/> objects.</returns>
-		public List<MilestonePerson> GetMilestonePersonListByMilestone(int milestoneId)
-		{
-		    return PersonRateCalculator.GetMilestonePersonListByMilestone(milestoneId);
-		}
+        /// <summary>
+        /// Retrives the list of the <see cref="Person"/>s for the specified <see cref="Milestone"/>.
+        /// </summary>
+        /// <param name="milestoneId">An ID of the milestone to the data be retrieved for.</param>
+        /// <returns>The list of the <see cref="MilestonePerson"/> objects.</returns>
+        public List<MilestonePerson> GetMilestonePersonListByMilestone(int milestoneId)
+        {
+            return PersonRateCalculator.GetMilestonePersonListByMilestone(milestoneId);
+        }
 
         /// <summary>
         /// Retrives the list of the <see cref="Person"/>s for the specified <see cref="Milestone"/>.
@@ -66,96 +66,101 @@ namespace PracticeManagementService
             return PersonRateCalculator.GetMilestonePersonListByMilestoneNoFinancials(milestoneId);
         }
 
-	    public MilestonePersonComputedFinancials CalculateMilestonePersonFinancials(int milestonePersonId)
-	    {
-	        return ComputedFinancialsDAL.CalculateMilestonePersonFinancials(milestonePersonId);
-	    }
+        public List<MilestonePerson> MilestonePersonsByMilestoneForTEByProject(int milestoneId)
+        {
+            return MilestonePersonDAL.MilestonePersonsByMilestoneForTEByProject(milestoneId);
+        }
 
-	    /// <summary>
-		/// Retrives the list of the <see cref="Milestone"/>s for the specified <see cref="Person"/>.
-		/// </summary>
-		/// <param name="personId">An ID of the person the the data be retrieved for.</param>
-		/// <returns>The list of the <see cref="MilestonePerson"/> objects.</returns>
+        public MilestonePersonComputedFinancials CalculateMilestonePersonFinancials(int milestonePersonId)
+        {
+            return ComputedFinancialsDAL.CalculateMilestonePersonFinancials(milestonePersonId);
+        }
+
+        /// <summary>
+        /// Retrives the list of the <see cref="Milestone"/>s for the specified <see cref="Person"/>.
+        /// </summary>
+        /// <param name="personId">An ID of the person the the data be retrieved for.</param>
+        /// <returns>The list of the <see cref="MilestonePerson"/> objects.</returns>
         public List<MilestonePerson> GetMilestonePersonListByPerson(int personId)
         {
             List<MilestonePerson> mps = MilestonePersonDAL.MilestonePersonListByPerson(personId);
 
-		    foreach (MilestonePerson mp in mps)
-		        mp.ComputedFinancials =
-		            ComputedFinancialsDAL.FinancialsGetByMilestonePerson(
-		                mp.Milestone.Id.Value,
-		                personId);
+            foreach (MilestonePerson mp in mps)
+                mp.ComputedFinancials =
+                    ComputedFinancialsDAL.FinancialsGetByMilestonePerson(
+                        mp.Milestone.Id.Value,
+                        personId);
 
-		    return mps;
+            return mps;
         }
 
-		/// <summary>
-		/// Retrives the milestone-person link details for the specified <see cref="Milestone"/> and
-		/// <see cref="Person"/>
-		/// </summary>
-		/// <param name="milestonePersonId">An ID of the milestone-person association.</param>
-		/// <returns>The <see cref="MilestonePerson"/> object if found and null otherwise.</returns>
-		public MilestonePerson GetMilestonePersonDetail(int milestonePersonId)
-		{
-			MilestonePerson result = MilestonePersonDAL.MilestonePersonGetByMilestonePersonId(milestonePersonId);
-			if (result != null)
-			{
-				result.Entries = MilestonePersonDAL.MilestonePersonEntryListByMilestonePersonId(milestonePersonId);
+        /// <summary>
+        /// Retrives the milestone-person link details for the specified <see cref="Milestone"/> and
+        /// <see cref="Person"/>
+        /// </summary>
+        /// <param name="milestonePersonId">An ID of the milestone-person association.</param>
+        /// <returns>The <see cref="MilestonePerson"/> object if found and null otherwise.</returns>
+        public MilestonePerson GetMilestonePersonDetail(int milestonePersonId)
+        {
+            MilestonePerson result = MilestonePersonDAL.MilestonePersonGetByMilestonePersonId(milestonePersonId);
+            if (result != null)
+            {
+                result.Entries = MilestonePersonDAL.MilestonePersonEntryListByMilestonePersonId(milestonePersonId);
 
-				if (result.Milestone != null && result.Milestone.Id.HasValue &&
-					result.Person != null && result.Person.Id.HasValue)
-				{
-					// Financials for each entry
-					foreach (MilestonePersonEntry entry in result.Entries)
-					{
-						entry.ComputedFinancials =
-							ComputedFinancialsDAL.FinancialsGetByMilestonePersonEntry(
-							result.Milestone.Id.Value,
-							result.Person.Id.Value,
-							entry.StartDate);
-					}
-				}
+                if (result.Milestone != null && result.Milestone.Id.HasValue &&
+                    result.Person != null && result.Person.Id.HasValue)
+                {
+                    // Financials for each entry
+                    foreach (MilestonePersonEntry entry in result.Entries)
+                    {
+                        entry.ComputedFinancials =
+                            ComputedFinancialsDAL.FinancialsGetByMilestonePersonEntry(
+                            result.Milestone.Id.Value,
+                            result.Person.Id.Value,
+                            entry.StartDate);
+                    }
+                }
 
-				// Financials for the milestone person assignment
-				result.ComputedFinancials =
-					ComputedFinancialsDAL.FinancialsGetByMilestonePerson(
-					result.Milestone.Id.Value,
-					result.Person.Id.Value);
-			}
-			return result;
-		}
+                // Financials for the milestone person assignment
+                result.ComputedFinancials =
+                    ComputedFinancialsDAL.FinancialsGetByMilestonePerson(
+                    result.Milestone.Id.Value,
+                    result.Person.Id.Value);
+            }
+            return result;
+        }
 
         /// <summary>
         /// Checks whether there are any time entries for a given MilestonepersonId.
         /// </summary>
         /// <param name="milestonePersonId"></param>
         /// <returns></returns>
-        public bool CheckTimeEntriesForMilestonePerson(int milestonePersonId,  DateTime? startDate, DateTime? endDate,
+        public bool CheckTimeEntriesForMilestonePerson(int milestonePersonId, DateTime? startDate, DateTime? endDate,
                                                         bool checkStartDateEquality, bool checkEndDateEquality)
         {
             return MilestonePersonDAL.CheckTimeEntriesForMilestonePerson(milestonePersonId, startDate, endDate,
                                                                             checkStartDateEquality, checkEndDateEquality);
         }
 
-		/// <summary>
-		/// Saves the specified <see cref="Milestone"/>-<see cref="Person"/> link to the database.
-		/// </summary>
-		/// <param name="milestonePerson">The data to be saved to.</param>
-		public void SaveMilestonePerson(ref MilestonePerson milestonePerson, string userName)
-		{
+        /// <summary>
+        /// Saves the specified <see cref="Milestone"/>-<see cref="Person"/> link to the database.
+        /// </summary>
+        /// <param name="milestonePerson">The data to be saved to.</param>
+        public void SaveMilestonePerson(ref MilestonePerson milestonePerson, string userName)
+        {
             MilestonePersonDAL.SaveMilestonePersonWrapper(milestonePerson, userName);
-		}
+        }
 
-		/// <summary>
-		/// Deletes the specified <see cref="Milestone"/>-<see cref="Person"/> link from the database.
-		/// </summary>
-		/// <param name="milestonePerson">The data to be deleted from.</param>
-		public void DeleteMilestonePerson(MilestonePerson milestonePerson)
-		{
-			MilestonePersonDAL.MilestonePersonDelete(milestonePerson);
-		}
+        /// <summary>
+        /// Deletes the specified <see cref="Milestone"/>-<see cref="Person"/> link from the database.
+        /// </summary>
+        /// <param name="milestonePerson">The data to be deleted from.</param>
+        public void DeleteMilestonePerson(MilestonePerson milestonePerson)
+        {
+            MilestonePersonDAL.MilestonePersonDelete(milestonePerson);
+        }
 
-		#endregion
-	}
+        #endregion
+    }
 }
 
