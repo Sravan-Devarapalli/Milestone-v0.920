60efeafb7b20f041c86ba6f86c78122d516b9159
RaviN <RaviN>
2011-07-05 01:00:00 -0500

    Comments - Defect Number: 2645; Defect RecordID: 2641; Server Address: services.logic2020.com; Server Port: 99; Database Name: Practice Management; Defect Summary: Configuration>Time>Default Project/Milestone: UI/Layout changes; Defect Description: Currently it's required that a user define the upper and lower bounds in order to select a valid project/milestone to set as the default. Also, because all time is going to one milestone we have timeout issues with the site (the data is too large). Let's make this simpler and more efficient, and take into consideration the situation where an admin/company may be setting PM up for the first time.
    1. Rearrange layout of page per mockup below, where all user-configurable options are displayed on the left side of the page. They should have a blue background same as other pages (this same blue background should envelope the "Default Time Entry Project" (previously "Default Milestone") subcategory heading as well so it's clear where the user is.
    2. On the right side of the page, in a white box, please add the explanation text for each function per the mockup below.
    3. I removed the additional drop-down selection of the Milestone because it should not be necessary to manually choose this based on the changes proposed here. Whatever the Project is that gets chosen (or created if using the button below) a milestone should be created automatically for each calendar month going forward from the current date (format [Month Name] [yyyy], so January 2011, Februrary 2011, etc). Some thought needs to be given to this with regard to end users' experience during Time Entry as well. As of now only one row appears by default. It would be smarter to include a row for each monthly Milestone in the cases where a week is split between two months. For example, at the end of the month Jan 31 falls on a Monday, so February begins on Tuesday. As opposed to just having the one row by default, that week should show two rows, the first containing the January 2011 milestone, and the second containing the Februrary 2011 milestone. Like any other milestone in PM, the days that aren't valid for that milestone will be greyed out.
    4. If the admin/user uses the "Create Default Time Entry Project" it should pre-populate as much as possible to eliminate errors/issues. This will probably be a form specific to this purpose and not the existing "Add Project" form. Please think about what fields are needed at a minimum to simplify the process. The Project name should auto-populate as "[Company Name] Internal/PTO/Holiday". Limit the Practice drop-down to only show practices designated as Internal. Once the form is completed, validate before committing. Committing will create the project and return the admin/user to the Default Time Entry Project page and now show the Client and Project drop-downs populated with the newly created Project. It should not be necessary to click the Save Project button at that point, so it should be greyed out unless a change in the drop-down(s) is made.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/PersonMilestoneWithFinancials.proc.sql
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/PersonMilestoneWithFinancials.proc.sql
@@ -4,31 +4,36 @@
 )
 AS
 BEGIN
-SELECT  p.ProjectId,
-		p.Name AS 'ProjectName',
-		p.ProjectStatusId,
-		p.ProjectNumber,
-		s.Name AS 'ProjectStatus',
-		m.MilestoneId,
-        m.Description AS 'MilestoneName',
-        mp.MilestonePersonId,
-        mpe.StartDate,
-        mpe.EndDate,
-        r.Name AS 'RoleName',
-        f.Revenue,
-        f.GrossMargin,
-        pa.[FileName]
-FROM    dbo.Project AS p
-		INNER JOIN dbo.ProjectStatus AS s ON p.ProjectStatusId = s.ProjectStatusId
-		INNER JOIN dbo.Milestone AS m ON p.ProjectId = m.ProjectId
-        INNER JOIN dbo.MilestonePerson AS mp ON m.MilestoneId = mp.MilestoneId
-        INNER JOIN dbo.MilestonePersonEntry AS mpe ON mp.MilestonePersonId = mpe.MilestonePersonId
-        LEFT JOIN dbo.PersonRole AS r ON mpe.PersonRoleId = r.PersonRoleId
-		LEFT JOIN dbo.ProjectAttachment AS pa ON p.ProjectId = pa.ProjectId
-        INNER JOIN v_FinancialsForMilestones AS f ON m.MilestoneId = f.MilestoneId
-                                                     AND mp.PersonId = f.PersonId
-WHERE   f.PersonId = @PersonId
-ORDER BY mpe.StartDate
+
+	DECLARE @DefaultProjectId INT
+	SELECT @DefaultProjectId = ProjectId
+	FROM dbo.DefaultMilestoneSetting
+
+	SELECT  p.ProjectId,
+			p.Name AS 'ProjectName',
+			p.ProjectStatusId,
+			p.ProjectNumber,
+			s.Name AS 'ProjectStatus',
+			m.MilestoneId,
+			m.Description AS 'MilestoneName',
+			mp.MilestonePersonId,
+			mpe.StartDate,
+			mpe.EndDate,
+			r.Name AS 'RoleName',
+			f.Revenue,
+			f.GrossMargin,
+			pa.[FileName]
+	FROM    dbo.Project AS p
+			INNER JOIN dbo.ProjectStatus AS s ON p.ProjectStatusId = s.ProjectStatusId
+			INNER JOIN dbo.Milestone AS m ON p.ProjectId = m.ProjectId
+			INNER JOIN dbo.MilestonePerson AS mp ON m.MilestoneId = mp.MilestoneId
+			INNER JOIN dbo.MilestonePersonEntry AS mpe ON mp.MilestonePersonId = mpe.MilestonePersonId
+			LEFT JOIN dbo.PersonRole AS r ON mpe.PersonRoleId = r.PersonRoleId
+			LEFT JOIN dbo.ProjectAttachment AS pa ON p.ProjectId = pa.ProjectId
+			INNER JOIN v_FinancialsForMilestones AS f ON m.MilestoneId = f.MilestoneId
+														 AND mp.PersonId = f.PersonId
+	WHERE   f.PersonId = @PersonId AND P.ProjectId <> @DefaultProjectId
+	ORDER BY mpe.StartDate
 END
 
 
