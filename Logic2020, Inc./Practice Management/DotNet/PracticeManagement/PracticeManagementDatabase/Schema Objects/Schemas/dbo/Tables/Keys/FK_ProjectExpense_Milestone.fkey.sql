e0f8cf72c101433b40b44dbf1919d027097266f0
RaviN <RaviN>
2011-07-11 02:48:00 -0500

    Comments - Defect Number: 2881; Defect RecordID: 2877; Server Address: services.logic2020.com; Server Port: 99; Database Name: Practice Management; Defect Summary: Projects>Milestone>Expenses: Expenses and Reimbursed expenses not being correctly calculated into Milestone financials, Projec@; Defect Description: Expenses entered into the "Expenses" tab in each Milestone are not being calculated into the Milestone Financials correctly.
    1. Neither the Expense or Reimbursed amount is appearing in the Financials summary as they should, though the Reimbursed amount does incorrectly get added to the "Client Discount" line.
    2. The Client discount percentage is not appearing (ex: 2.0%)
    3. If Reimbused percentage is 105% (as in the example below), the difference between the Expense and the Reimbursed amount should be added to Revenue Net of Discounts. This should also, therefore, increase the Margin appropriately. The reverse is true if the Reimbursed percentage is less than 100%.
    4. These altered Revenue and Margin calculations should also result in changes to the overal Project Revenue and Margin.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/Keys/FK_ProjectExpense_Milestone.fkey.sql
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/Keys/FK_ProjectExpense_Milestone.fkey.sql
@@ -1,4 +1,4 @@
-﻿ALTER TABLE [dbo].[ProjectExpense]
-    ADD CONSTRAINT [FK_ProjectExpense_Milestone] FOREIGN KEY ([MilestoneId]) REFERENCES [dbo].[Milestone] ([MilestoneId]) ON DELETE NO ACTION ON UPDATE NO ACTION;
+﻿--ALTER TABLE [dbo].[ProjectExpense]
+--    ADD CONSTRAINT [FK_ProjectExpense_Milestone] FOREIGN KEY ([MilestoneId]) REFERENCES [dbo].[Milestone] ([MilestoneId]) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 
