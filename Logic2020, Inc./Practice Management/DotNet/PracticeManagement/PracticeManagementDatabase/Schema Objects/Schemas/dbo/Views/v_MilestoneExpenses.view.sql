cf31bfcfb661cabbb458bf5f0019029d4cce1af1
RaviN <RaviN>
2011-07-11 02:48:00 -0500

    Comments - Defect Number: 2881; Defect RecordID: 2877; Server Address: services.logic2020.com; Server Port: 99; Database Name: Practice Management; Defect Summary: Projects>Milestone>Expenses: Expenses and Reimbursed expenses not being correctly calculated into Milestone financials, Projec@; Defect Description: Expenses entered into the "Expenses" tab in each Milestone are not being calculated into the Milestone Financials correctly.
    1. Neither the Expense or Reimbursed amount is appearing in the Financials summary as they should, though the Reimbursed amount does incorrectly get added to the "Client Discount" line.
    2. The Client discount percentage is not appearing (ex: 2.0%)
    3. If Reimbused percentage is 105% (as in the example below), the difference between the Expense and the Reimbursed amount should be added to Revenue Net of Discounts. This should also, therefore, increase the Margin appropriately. The reverse is true if the Reimbursed percentage is less than 100%.
    4. These altered Revenue and Margin calculations should also result in changes to the overal Project Revenue and Margin.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Views/v_MilestoneExpenses.view.sql
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Views/v_MilestoneExpenses.view.sql
@@ -1,11 +1,23 @@
 ﻿CREATE view dbo.v_MilestoneExpenses
 as
-select 
-	m.milestoneId,
-	m.ProjectId,
-	sum(pexp.ExpenseAmount) as 'Expense',
-	sum(pexp.ExpenseReimbursementAmount) as 'ReimbursedExpense'
-from Milestone as m
-inner join v_ProjectExpenses as pexp on pexp.MilestoneId = m.MilestoneId
-group by m.ProjectId, m.milestoneId
-
+ With CTEMileStonesPerDay
+AS
+	(SELECT  
+		pexp.ProjectId,
+	    c.Date,
+	    COUNT(*) MileStonesPerDay 
+	from dbo.ProjectExpense as pexp
+	JOIN dbo.Calendar c ON c.Date BETWEEN pexp.StartDate AND pexp.EndDate
+	JOIN dbo.Milestone M ON M.ProjectId = pexp.ProjectId AND c.Date BETWEEN M.StartDate AND M.ProjectedDeliveryDate
+	GROUP BY c.Date,pexp.ProjectId
+	)
+SELECT M.MilestoneId,
+	  M.ProjectId,
+	  CONVERT(DECIMAL(18,2),SUM(pexp.Amount/ ((DATEDIFF(dd,pexp.StartDate,pexp.EndDate)+1)*MileStonesPerDay))) Expense,
+	  CONVERT(DECIMAL(18,2),SUM(pexp.Reimbursement*0.01*pexp.Amount / ((DATEDIFF(dd,pexp.StartDate,pexp.EndDate)+1)*MileStonesPerDay))) ReimbursedExpense 
+FROM CTEMileStonesPerDay CTE
+JOIN dbo.ProjectExpense as pexp 
+ON CTE.ProjectId = pexp.ProjectId AND CTE.Date BETWEEN pexp.StartDate AND pexp.EndDate
+JOIN dbo.Milestone M ON M.ProjectId = pexp.ProjectId AND CTE.Date BETWEEN M.StartDate AND M.ProjectedDeliveryDate
+GROUP BY M.MilestoneId,M.ProjectId,M.StartDate,M.ProjectedDeliveryDate 
+ 
