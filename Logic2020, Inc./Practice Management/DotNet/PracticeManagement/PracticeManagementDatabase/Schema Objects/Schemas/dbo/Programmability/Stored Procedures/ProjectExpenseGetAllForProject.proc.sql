a5b9f13871bbd916d3be9b8b4445a913379a85f3
RaviN <RaviN>
2011-07-11 02:48:00 -0500

    Comments - Defect Number: 2881; Defect RecordID: 2877; Server Address: services.logic2020.com; Server Port: 99; Database Name: Practice Management; Defect Summary: Projects>Milestone>Expenses: Expenses and Reimbursed expenses not being correctly calculated into Milestone financials, Projec@; Defect Description: Expenses entered into the "Expenses" tab in each Milestone are not being calculated into the Milestone Financials correctly.
    1. Neither the Expense or Reimbursed amount is appearing in the Financials summary as they should, though the Reimbursed amount does incorrectly get added to the "Client Discount" line.
    2. The Client discount percentage is not appearing (ex: 2.0%)
    3. If Reimbused percentage is 105% (as in the example below), the difference between the Expense and the Reimbursed amount should be added to Revenue Net of Discounts. This should also, therefore, increase the Margin appropriately. The reverse is true if the Reimbursed percentage is less than 100%.
    4. These altered Revenue and Margin calculations should also result in changes to the overal Project Revenue and Margin.

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/ProjectExpenseGetAllForProject.proc.sql
@@ -0,0 +1,12 @@
+﻿CREATE PROCEDURE [dbo].[ProjectExpenseGetAllForProject]
+	@ProjectId INT
+AS
+	SELECT Id ExpenseId,
+			Name ExpenseName,
+			ProjectId,
+			StartDate,
+			EndDate,
+			[Amount] ExpenseAmount,
+			[Reimbursement] ExpenseReimbursement
+	FROM dbo.ProjectExpense
+	WHERE ProjectId = @ProjectId
