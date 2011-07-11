b1406809e6d856926a6468225a1b26bc2dd9f712
RaviN <RaviN>
2011-07-11 02:48:00 -0500

    Comments - Defect Number: 2881; Defect RecordID: 2877; Server Address: services.logic2020.com; Server Port: 99; Database Name: Practice Management; Defect Summary: Projects>Milestone>Expenses: Expenses and Reimbursed expenses not being correctly calculated into Milestone financials, Projec@; Defect Description: Expenses entered into the "Expenses" tab in each Milestone are not being calculated into the Milestone Financials correctly.
    1. Neither the Expense or Reimbursed amount is appearing in the Financials summary as they should, though the Reimbursed amount does incorrectly get added to the "Client Discount" line.
    2. The Client discount percentage is not appearing (ex: 2.0%)
    3. If Reimbused percentage is 105% (as in the example below), the difference between the Expense and the Reimbursed amount should be added to Revenue Net of Discounts. This should also, therefore, increase the Margin appropriately. The reverse is true if the Reimbursed percentage is less than 100%.
    4. These altered Revenue and Margin calculations should also result in changes to the overal Project Revenue and Margin.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/Controls/ProjectExpenses/ProjectExpensesControl.ascx.cs
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/Controls/ProjectExpenses/ProjectExpensesControl.ascx.cs
@@ -16,8 +16,15 @@ namespace PraticeManagement.Controls.ProjectExpenses
         private const string LblTotalamount = "lblTotalAmount";
         private const string LblTotalreimbursement = "lblTotalReimbursed";
         private const string LblTotalreimbursementamount = "lblTotalReimbursementAmount";
+        private const string tbStartDate = "txtStartDate";
+        private const string tbEndDate = "txtEndDate";
 
         #endregion
+
+        public PraticeManagement.ProjectDetail HostingPage
+        {
+            get { return ((PraticeManagement.ProjectDetail)Page); }
+        }
 
         #region Fields
 
@@ -39,15 +46,53 @@ namespace PraticeManagement.Controls.ProjectExpenses
             var ctrlName = footerRow.FindControl(Tbeditname) as TextBox;
             var ctrlAmount = footerRow.FindControl(Tbeditamount) as TextBox;
             var ctrlReimb = footerRow.FindControl(Tbeditreimbursement) as TextBox;
+            var txtStartDate = footerRow.FindControl(tbStartDate) as TextBox;
+            var txtEndDate = footerRow.FindControl(tbEndDate) as TextBox;
+            var startDate = DateTime.Parse(txtStartDate.Text);
+            var endDate = DateTime.Parse(txtEndDate.Text);
+            if (startDate > endDate)
+            {
+                CustomValidator cstEndShouldBeGreater = footerRow.FindControl("cstEndShouldBeGreater") as CustomValidator;
+                cstEndShouldBeGreater.IsValid = false;
+                return;
+            }
+            else if (IsPeriodOverlaps(txtStartDate.Text, txtEndDate.Text, null))
+            {
+                CustomValidator cvUpdatedTimeTypeName = footerRow.FindControl("cstPeriodOverlaps") as CustomValidator;
+                cvUpdatedTimeTypeName.IsValid = false;
+                return;
+            }
+            else if (HostingPage.Project.StartDate.HasValue && HostingPage.Project.EndDate.HasValue)
+            {
+                var isValid = true;
+                if (startDate < HostingPage.Project.StartDate.Value.Date ||
+                    startDate > HostingPage.Project.EndDate.Value.Date)
+                {
+                    CustomValidator cstStartDateShouldbewithinProjectPeriod = footerRow.FindControl("cstStartDateShouldbewithinProjectPeriod") as CustomValidator;
+                    cstStartDateShouldbewithinProjectPeriod.IsValid = isValid = false;
+                }
+                if (endDate < HostingPage.Project.StartDate.Value.Date ||
+                    endDate > HostingPage.Project.EndDate.Value.Date)
+                {
+                    CustomValidator cstEndDateShouldbewithinProjectPeriod = footerRow.FindControl("cstEndDateShouldbewithinProjectPeriod") as CustomValidator;
+                    cstEndDateShouldbewithinProjectPeriod.IsValid = isValid = false;
+                }
 
+                if (!isValid)
+                {
+                    return;
+                }
+            }
             if (ctrlName != null && ctrlAmount != null && ctrlReimb != null)
                 ProjectExpenseHelper.AddProjectExpense(
                         ctrlName.Text,
                         ctrlAmount.Text,
                         ctrlReimb.Text,
-                        Request.Params[Constants.QueryStringParameterNames.Id]
-                    );
+                        HostingPage.ProjectId.Value.ToString(),
+                        startDate,
+                        endDate
 
+                    );
             gvProjectExpenses.DataBind();
         }
 
@@ -78,7 +123,7 @@ namespace PraticeManagement.Controls.ProjectExpenses
 
                 case DataControlRowType.Footer:
                     SetRowValue(row, LblTotalamount, _totalAmount);
-                    SetRowValue(row, LblTotalreimbursement, _totalReimbursed/_expensesCount + "%");
+                    SetRowValue(row, LblTotalreimbursement, string.Format("{0:0}%", (_totalReimbursed / _expensesCount)));
                     SetRowValue(row, LblTotalreimbursementamount, _totalReimbursementAmount);
 
                     break;
@@ -94,7 +139,111 @@ namespace PraticeManagement.Controls.ProjectExpenses
 
         private static void SetRowValue(Control row, string ctrlName, decimal number)
         {
-            SetRowValue(row, ctrlName, ((PracticeManagementCurrency) number).ToString());
+            SetRowValue(row, ctrlName, ((PracticeManagementCurrency)number).ToString());
+        }
+
+        protected void gvProjectExpenses_OnRowUpdating(object sender, GridViewUpdateEventArgs e)
+        {
+
+            Page.Validate("ProjectExpensesEdit");
+            if (!Page.IsValid)
+            {
+                e.Cancel = true;
+            }
+            else
+            {
+                string newStartDate = e.NewValues["StartDate"].ToString().Trim();
+                string newEndDate = e.NewValues["EndDate"].ToString().Trim();
+                string oldStartDate = e.OldValues["StartDate"].ToString().Trim();
+                string oldEndDate = e.OldValues["EndDate"].ToString().Trim();
+
+                if (newStartDate != oldStartDate || newEndDate != oldEndDate)
+                {
+
+
+                    var startDate = Convert.ToDateTime(newStartDate);
+                    var endDate = Convert.ToDateTime(newEndDate);
+                    if (startDate > endDate)
+                    {
+                        CustomValidator cstEndShouldBeGreater = gvProjectExpenses.Rows[e.RowIndex].FindControl("cstEndShouldBeGreater") as CustomValidator;
+                        cstEndShouldBeGreater.IsValid = false;
+                        e.Cancel = true;
+                    }
+                    else
+                    {
+                        if (HostingPage.Project.StartDate.HasValue && HostingPage.Project.EndDate.HasValue)
+                        {
+                            var isValid = true;
+
+                            if ((startDate < HostingPage.Project.StartDate.Value.Date ||
+                                startDate > HostingPage.Project.EndDate.Value.Date))
+                            {
+                                CustomValidator cstStartDateShouldbewithinProjectPeriod = gvProjectExpenses.Rows[e.RowIndex].FindControl("cstStartDateShouldbewithinProjectPeriod") as CustomValidator;
+                                cstStartDateShouldbewithinProjectPeriod.IsValid = isValid = false;
+                            }
+                            if ((endDate < HostingPage.Project.StartDate.Value.Date ||
+                               endDate > HostingPage.Project.EndDate.Value.Date))
+                            {
+                                CustomValidator cstEndDateShouldbewithinProjectPeriod = gvProjectExpenses.Rows[e.RowIndex].FindControl("cstEndDateShouldbewithinProjectPeriod") as CustomValidator;
+                                cstEndDateShouldbewithinProjectPeriod.IsValid = isValid = false;
+                            }
+                            if (!isValid)
+                            {
+                                e.Cancel = true;
+                                return;
+                            }
+                        }
+
+                        if (IsPeriodOverlaps(newStartDate, newEndDate, e.Keys[0]))
+                        {
+                            CustomValidator cvUpdatedTimeTypeName = gvProjectExpenses.Rows[e.RowIndex].FindControl("cstPeriodOverlaps") as CustomValidator;
+                            cvUpdatedTimeTypeName.IsValid = false;
+                            e.Cancel = true;
+                        }
+                    }
+                }
+            }
+        }
+
+        private bool IsPeriodOverlaps(string startDateStr, string endDateStr, object expenseIdObj)
+        {
+            var startDate = Convert.ToDateTime(startDateStr);
+            var endDate = Convert.ToDateTime(endDateStr);
+            int? expenseId = null;
+            if (expenseIdObj != null)
+            {
+                expenseId = Convert.ToInt32(expenseIdObj);
+            }
+            var projectId = Convert.ToInt32(Request.Params[Constants.QueryStringParameterNames.Id]);
+            var expenses = ProjectExpenses.ProjectExpenseHelper.ProjectExpensesForProject(projectId);
+            foreach (var expense in expenses)
+            {
+                if (expenseId.HasValue && expense.Id == expenseId.Value)
+                {
+                    continue;
+                }
+                if (startDate >= expense.StartDate && startDate <= expense.EndDate
+                    || endDate >= expense.StartDate && endDate <= expense.EndDate
+                    )
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        protected void odsProjectExpenses_OnSelecting(object sender, ObjectDataSourceSelectingEventArgs e)
+        {
+            if (HostingPage.ProjectId.HasValue)
+            {
+                e.InputParameters["projectId"] = HostingPage.ProjectId.Value;
+            }
+        }
+
+        public void BindExpenses()
+        {
+            gvProjectExpenses.DataBind();
         }
     }
 }
