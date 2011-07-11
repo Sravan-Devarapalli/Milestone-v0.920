9bfde8566e1f81ad4f229b878d2389e5c4ece1e1
RaviN <RaviN>
2011-07-11 02:48:00 -0500

    Comments - Defect Number: 2881; Defect RecordID: 2877; Server Address: services.logic2020.com; Server Port: 99; Database Name: Practice Management; Defect Summary: Projects>Milestone>Expenses: Expenses and Reimbursed expenses not being correctly calculated into Milestone financials, Projec@; Defect Description: Expenses entered into the "Expenses" tab in each Milestone are not being calculated into the Milestone Financials correctly.
    1. Neither the Expense or Reimbursed amount is appearing in the Financials summary as they should, though the Reimbursed amount does incorrectly get added to the "Client Discount" line.
    2. The Client discount percentage is not appearing (ex: 2.0%)
    3. If Reimbused percentage is 105% (as in the example below), the difference between the Expense and the Reimbursed amount should be added to Revenue Net of Discounts. This should also, therefore, increase the Margin appropriately. The reverse is true if the Reimbursed percentage is less than 100%.
    4. These altered Revenue and Margin calculations should also result in changes to the overal Project Revenue and Margin.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Views/v_FinancialsForMilestones.view.sql
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Views/v_FinancialsForMilestones.view.sql
@@ -20,12 +20,13 @@ AS
 		   f.MLFOverheadRate,
 		   f.PersonHoursPerDay,
 		   f.PracticeManagementCommissionSub,
-		   f.PracticeManagementCommissionOwn ,
+		   f.PracticeManagementCommissionOwn,
 		   f.PracticeManagerId,
 		   f.Discount
 	FROM v_FinancialsRetrospective f
-	) 
-	
+	),
+	MilestoneFinancials AS
+	(
 	SELECT f.ProjectId,
 		   f.MilestoneId,
 		   f.PersonId,
@@ -44,19 +45,37 @@ AS
 		   ISNULL(SUM((CASE WHEN f.SLHR >= f.PayRate + f.MLFOverheadRate THEN f.SLHR  ELSE  f.PayRate + f.MLFOverheadRate END)*ISNULL(f.PersonHoursPerDay, 0)),0) Cogs,
 
 	       ISNULL(SUM(f.PersonHoursPerDay), 0) AS Hours,
-	       
-	       (SUM((f.PersonMilestoneDailyAmount - f.PersonDiscountDailyAmount -
-						(CASE WHEN f.SLHR>=  f.PayRate +f.MLFOverheadRate 
-							  THEN f.SLHR  ELSE f.PayRate +f.MLFOverheadRate END) 
-					    *ISNULL(f.PersonHoursPerDay, 0))* (f.ProjectSalesCommisionFraction/100))) SalesCommission,
+	       MAX(f.ProjectSalesCommisionFraction) ProjectSalesCommisionFraction,
 	       SUM((f.PersonMilestoneDailyAmount - f.PersonDiscountDailyAmount -
 	            (CASE WHEN f.SLHR  >=  f.PayRate +f.MLFOverheadRate 
 							  THEN f.SLHR ELSE f.PayRate +f.MLFOverheadRate END)  * ISNULL(f.PersonHoursPerDay, 0)) *
 	           (f.PracticeManagementCommissionSub + CASE f.PracticeManagerId WHEN f.PersonId THEN f.PracticeManagementCommissionOwn ELSE 0 END)) / 100 AS PracticeManagementCommission,
 	           0.0 AS 'actualhours',
 	           0.0 AS 'forecastedhours',
-           ISNULL(SUM(vac.VacationHours), 0) AS 'VacationHours'
-		   
+           ISNULL(SUM(vac.VacationHours), 0) AS 'VacationHours',
+            min(f.Discount) Discount
+		    
 	  FROM FinancialsRetro AS f
 	  LEFT JOIN dbo.v_MilestonePersonVacations AS vac ON f.MilestoneId = vac.MilestoneId AND f.PersonId = vac.PersonId
 	  GROUP BY f.PersonId, f.MilestoneId, f.ProjectId
+	  ) 
+	
+	SELECT  
+			f.ProjectId,
+			f.MilestoneId,
+			PersonId,
+			FinancialDate,
+			MonthEnd,
+			ISNULL(Revenue,0)+ISNULL(ReimbursedExpense,0)-ISNULL(Expense,0) as Revenue, 
+			ISNULL(RevenueNet,0)+((ISNULL(ReimbursedExpense,0)-ISNULL(Expense,0))*(1 - Discount/100)) RevenueNet,
+			ISNULL(GrossMargin + ((ISNULL(ReimbursedExpense,0) - ISNULL(Expense,0))*(1 - Discount/100)),0) as GrossMargin,
+			Cogs, 
+			[Hours],
+			ISNULL(GrossMargin + ((ISNULL(ReimbursedExpense,0) - ISNULL(Expense,0))*(1 - Discount/100)),0) 
+			* ProjectSalesCommisionFraction*0.01 SalesCommission,
+			PracticeManagementCommission,
+			actualhours ,
+			forecastedhours,
+			VacationHours
+	  FROM MilestoneFinancials AS f
+	  LEFT JOIN v_MilestoneExpenses as me on f.ProjectId = me.ProjectId AND f.MilestoneId = me.milestoneId
