ab83222c09f9e9a72f25c2c1b979f319ce7c30de
AshokD <AshokD>
2011-07-13 07:51:00 -0500

    Comments - Defect Number: 2881; Defect RecordID: 2877; Server Address: services.logic2020.com; Server Port: 99; Database Name: Practice Management; Defect Summary: Projects>Milestone>Expenses: Expenses and Reimbursed expenses not being correctly calculated into Milestone financials, Projec@; Defect Description: Expenses entered into the "Expenses" tab in each Milestone are not being calculated into the Milestone Financials correctly.
    1. Neither the Expense or Reimbursed amount is appearing in the Financials summary as they should, though the Reimbursed amount does incorrectly get added to the "Client Discount" line.
    2. The Client discount percentage is not appearing (ex: 2.0%)
    3. If Reimbused percentage is 105% (as in the example below), the difference between the Expense and the Reimbursed amount should be added to Revenue Net of Discounts. This should also, therefore, increase the Margin appropriately. The reverse is true if the Reimbursed percentage is less than 100%.
    4. These altered Revenue and Margin calculations should also result in changes to the overal Project Revenue and Margin.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/ProjectExpenseGetAllForMilestone.proc.sql
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/ProjectExpenseGetAllForMilestone.proc.sql
@@ -8,16 +8,29 @@ AS
 BEGIN
 	SET NOCOUNT ON;
 
-    -- Insert statements for procedure here
-
-	SELECT  0 ExpenseId,
-			'' ExpenseName,
-			ProjectId,
-			CONVERT(DATETIME,'') StartDate,
-			CONVERT(DATETIME,'') EndDate,
-			Expense ExpenseAmount,
-			ReimbursedExpense ExpenseReimbursement
-	from v_MilestoneExpenses 
-	where MilestoneId = @MilestoneId
+	With CTEMileStonesPerDay
+	AS
+	(SELECT  
+		pexp.ProjectId,
+		c.Date,
+		COUNT(*) MileStonesPerDay 
+	from dbo.ProjectExpense as pexp
+	JOIN dbo.Calendar c ON c.Date BETWEEN pexp.StartDate AND pexp.EndDate
+	JOIN dbo.Milestone M ON M.ProjectId = pexp.ProjectId AND c.Date BETWEEN M.StartDate AND M.ProjectedDeliveryDate
+	GROUP BY c.Date,pexp.ProjectId
+	)
+	SELECT  pexp.Id ExpenseId,
+			pexp.Name ExpenseName,
+			M.ProjectId,
+			pexp.StartDate,
+			pexp.EndDate,
+			CONVERT(DECIMAL(18,2),SUM(pexp.Amount/ ((DATEDIFF(dd,pexp.StartDate,pexp.EndDate)+1)*MileStonesPerDay))) ExpenseAmount,
+			MAX(pexp.Reimbursement)  ExpenseReimbursement
+	FROM CTEMileStonesPerDay CTE
+	JOIN dbo.ProjectExpense as pexp 
+	ON CTE.ProjectId = pexp.ProjectId AND CTE.Date BETWEEN pexp.StartDate AND pexp.EndDate
+	JOIN dbo.Milestone M ON M.ProjectId = pexp.ProjectId AND CTE.Date BETWEEN M.StartDate AND M.ProjectedDeliveryDate
+	WHERE M.MilestoneId =  @MilestoneId
+	GROUP BY M.MilestoneId,M.ProjectId,pexp.Id,pexp.StartDate,pexp.EndDate,pexp.Name
 END
 
