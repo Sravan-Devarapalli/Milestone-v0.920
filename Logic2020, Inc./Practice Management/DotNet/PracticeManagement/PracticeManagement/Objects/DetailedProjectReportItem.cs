31ab2f10b744488560d4948c3c81cc02d20e7468
ThulasiramP <ThulasiramP>
2011-07-15 06:03:00 -0500

    Comments - Defect Number: 2829; Defect RecordID: 2825; Server Address: services.logic2020.com; Server Port: 99; Database Name: Practice Management; Defect Summary: Projects: Add Calendar view, allow all roles to view; Defect Description: This will appear very similar to the detail a user can see for each Person on the Consultant Util Timeline report.
    It will just be a simple graph, showing a bar for each active and projected project over a 6 month period. The intention is to just allow everyone to get an idea of what is happening with projects, so all roles should be able to see this. We may expand the functionality later, but for now it's view-only (nothing linked). Only show Client name and Project name on the bar, unless the bar is short (project duration is short) in which case just show the P#. Keep the color coding the same as it is on the Summary page (Green = Active, etc).

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/Objects/DetailedProjectReportItem.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using DataTransferObjects;
+using System.Text;
+
+namespace PraticeManagement.Objects
+{
+    public class DetailedProjectReportItem
+    {
+        #region Constants
+
+        private const string DetailsTooltipFormat = "{0} - {1}";
+        private const string NotEndDate = "No End Date Specified.";
+        private const string DetailsLabelFormat = "{0} - {1}";
+        private const string AppendPersonFormat = "{0}{1} {2}";
+        private const string ToolTipView = "{1}{0}{2}{0}Start: {3:d}{0}End: {4:d}{0}{5}{6}";
+
+        #endregion
+
+        #region ItemType enum
+
+        public enum ItemType
+        {
+            ActiveProject,
+            ProjectedProject
+        }
+
+        #endregion
+
+        #region Constructors
+
+        public DetailedProjectReportItem
+            (DateTime reportStartDate, DateTime reportEndDate, Project project)
+        {
+            Project = project;
+            ReportStartDate = reportStartDate;
+            ReportEndDate = reportEndDate;
+        }
+
+        #endregion
+
+
+
+        #region Properties
+
+        public Project Project { get; set; }
+
+        public DateTime ReportStartDate { get; set; }
+
+        public DateTime ReportEndDate { get; set; }
+
+        public DateTime StartDate
+        {
+            get { return Project.StartDate.Value < ReportStartDate ? ReportStartDate : Project.StartDate.Value; }
+        }
+
+        public DateTime EndDate
+        {
+            get
+            {
+                var endDate = Project.EndDate;
+
+                if (endDate.HasValue)
+                    return endDate.Value > ReportEndDate
+                               ? ReportEndDate : endDate.Value;
+
+                return ReportEndDate;
+            }
+        }
+
+        public ItemType BarType
+        {
+            get
+            {
+                if (Project != null && Project.Status != null)
+                    return Project.Status.StatusType == ProjectStatusType.Projected ? ItemType.ProjectedProject : ItemType.ActiveProject;
+
+                return ItemType.ActiveProject;
+            }
+        }
+
+        public string Label
+        {
+            get
+            {
+                return string.Format(
+                    DetailsLabelFormat,
+                    Project.Client.Name,
+                    Project.Name);
+            }
+        }
+
+        public string smallLabel
+        {
+            get
+            {
+                return string.Format(Project.ProjectNumber);
+            }
+        }
+
+        public string Tooltip
+        {
+            get
+            {
+                return PrepareToolTipView(Project);
+            }
+        }
+
+        public string NavigateUrl
+        {
+            get
+            {
+                return GetRedirectUrl(Project.Id.Value, Constants.ApplicationPages.ProjectDetail); ;
+            }
+        }
+
+
+        private static string GetRedirectUrl(int argProjectId, string targetUrl)
+        {
+            return string.Format(Constants.ApplicationPages.DetailRedirectWithReturnFormat,
+                                 targetUrl,
+                                 argProjectId,
+                                 Constants.ApplicationPages.Projects);
+        }
+
+        private static string PrepareToolTipView(Project project)
+        {
+            var persons = new StringBuilder();
+            var personList = new List<MilestonePerson>();
+            foreach (var projectPerson in project.ProjectPersons)
+            {
+                var personExist = false;
+                if (personList != null)
+                {
+                    foreach (var mp in personList)
+                        if (mp.Person.Id == projectPerson.Person.Id)
+                        {
+                            personExist = true;
+                            break;
+                        }
+                }
+                if (!personExist)
+                {
+                    personList.Add(projectPerson);
+                }
+            }
+            foreach (var t in personList)
+                persons.AppendFormat(AppendPersonFormat,
+                                     Environment.NewLine,
+                                     HttpUtility.HtmlEncode(t.Person.FirstName),
+                                     HttpUtility.HtmlEncode(t.Person.LastName));
+
+            return string.Format(ToolTipView,
+                Environment.NewLine,
+                HttpUtility.HtmlEncode(project.Client.Name),
+                HttpUtility.HtmlEncode(project.Name),
+                project.StartDate,
+                project.EndDate,
+                HttpUtility.HtmlEncode(project.ProjectManager.Name),
+                persons
+                );
+        }
+
+        #endregion
+
+
+    }
+}
