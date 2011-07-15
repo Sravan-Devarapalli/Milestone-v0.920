88fb59a06b1da4e76c8fdaa88851d43c8c2b47ae
ThulasiramP <ThulasiramP>
2011-07-15 06:03:00 -0500

    Comments - Defect Number: 2829; Defect RecordID: 2825; Server Address: services.logic2020.com; Server Port: 99; Database Name: Practice Management; Defect Summary: Projects: Add Calendar view, allow all roles to view; Defect Description: This will appear very similar to the detail a user can see for each Person on the Consultant Util Timeline report.
    It will just be a simple graph, showing a bar for each active and projected project over a 6 month period. The intention is to just allow everyone to get an idea of what is happening with projects, so all roles should be able to see this. We may expand the functionality later, but for now it's view-only (nothing linked). Only show Client name and Project name on the bar, unless the bar is short (project duration is short) in which case just show the P#. Keep the color coding the same as it is on the Summary page (Green = Active, etc).

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/GetProjectListByDateRange.proc.sql
@@ -0,0 +1,43 @@
+﻿CREATE PROCEDURE [dbo].[GetProjectListByDateRange]
+	@ShowProjected		BIT = 0,
+	@ShowCompleted		BIT = 0,
+	@ShowActive			BIT = 0,
+	@showInternal		BIT = 0,
+	@ShowExperimental	BIT = 0,
+	@ShowInactive		BIT = 0,
+	@StartDate			DATETIME,
+	@EndDate			DATETIME
+AS 
+BEGIN
+	SET NOCOUNT ON ;
+
+DECLARE @DefaultProjectId INT
+	SELECT @DefaultProjectId = ProjectId
+	FROM dbo.DefaultMilestoneSetting
+
+	SELECT  p.ClientId,
+			p.ProjectId,
+			p.Name,		
+			p.StartDate,
+			p.EndDate,
+			p.ClientName,
+			p.ProjectStatusId,
+			p.ProjectStatusName,
+			p.ProjectNumber,
+			p.ProjectManagerId,
+		    p.ProjectManagerFirstName,
+		    p.ProjectManagerLastName,
+			pa.[FileName]
+	FROM	dbo.v_Project AS p
+	LEFT JOIN dbo.ProjectAttachment AS pa ON p.ProjectId = pa.ProjectId
+	WHERE	(dbo.IsDateRangeWithingTimeInterval(p.StartDate, p.EndDate, @StartDate, @EndDate) = 1)
+			AND (    ( @ShowProjected = 1 AND p.ProjectStatusId = 2 )
+				  OR ( @ShowActive = 1 AND p.ProjectStatusId = 3 )
+				  OR ( @ShowCompleted = 1 AND p.ProjectStatusId = 4 )
+				  OR ( @showInternal = 1 AND p.ProjectStatusId = 6 ) -- Internal
+				  OR ( @ShowExperimental = 1 AND p.ProjectStatusId = 5 )
+				  OR ( @ShowInactive = 1 AND p.ProjectStatusId = 1 ) -- Inactive
+			)
+			AND P.ProjectId <> @DefaultProjectId
+
+END
