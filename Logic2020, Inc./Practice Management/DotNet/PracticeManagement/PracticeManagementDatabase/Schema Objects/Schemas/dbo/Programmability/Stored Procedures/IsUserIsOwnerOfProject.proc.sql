0e7e37f515409dd8fbbb95695d64a8f8560ae294
ThulasiramP <ThulasiramP>
2011-07-25 11:05:00 -0500

    Comments - Defect Number: 2890; Defect RecordID: 2886; Server Address: services.logic2020.com; Server Port: 99; Database Name: Practice Management; Defect Summary: Projects>Milestones>Persons: Owner need to be able to adjust personhours allocation regardless of seniority; Defect Description: This is similar to issue #2588.
    Anyone defined as the Owner of a Project needs to have the ability to change hour allocation for Resources/Persons assigned to the Milestones, regardless of the seniority difference between the Owner and the Resources/Persons. They don't need to see the Margins still, but they should be able to reallocate time. They should still not be able to completely remove the Resource/Person if the Person has already entered time/notes towards the Milestone.

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/IsUserIsOwnerOfProject.proc.sql
@@ -0,0 +1,38 @@
+﻿CREATE PROCEDURE [dbo].[IsUserIsOwnerOfProject]
+(
+	@UserLogin NVARCHAR(100),
+	@Id INT,
+	@IsProjectId BIT
+)
+AS
+	DECLARE @PersonId INT
+	DECLARE @OwnerId INT
+	
+	SELECT @PersonId = PersonId
+	FROM Person
+	WHERE Alias = @UserLogin
+	
+	IF(@IsProjectId = 1)
+	BEGIN
+	SELECT @OwnerId = proj.ProjectManagerId  
+	FROM v_Project AS proj
+	WHERE proj.ProjectId = @Id
+	END
+	ELSE
+	BEGIN
+		SELECT @OwnerId = milestone.ProjectManagerId 
+		FROM v_Milestone AS milestone
+		WHERE milestone.MilestoneId = @Id
+	END
+
+	IF(@PersonId = @OwnerId)
+	BEGIN
+		SELECT 'True'
+	END
+	ELSE
+	BEGIN
+		SELECT 'False'
+	END
+	 
+	
+GO
