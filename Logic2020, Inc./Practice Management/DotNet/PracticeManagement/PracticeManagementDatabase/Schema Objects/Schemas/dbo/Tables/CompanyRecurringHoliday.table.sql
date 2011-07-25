049a83c8f3611364f0b27d44f18ebc0f4fef0f20
SrinivasM <SrinivasM>
2011-07-25 07:42:00 -0500

    Comments - Added new page for company recurring holidays.

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/CompanyRecurringHoliday.table.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[CompanyRecurringHoliday](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[Description] [nvarchar](255) NOT NULL,
+	[IsSet] [bit] NOT NULL,
+	[Month] [int] NOT NULL,
+	[Day] [int] NULL,
+	[NumberInMonth] [int] NULL,
+	[DayOfTheWeek] [int] NULL,
+	 CONSTRAINT [PK_CompanyRecurringHoliday] PRIMARY KEY CLUSTERED 
+	(
+		[Id] ASC
+	)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
+) ON [PRIMARY]
