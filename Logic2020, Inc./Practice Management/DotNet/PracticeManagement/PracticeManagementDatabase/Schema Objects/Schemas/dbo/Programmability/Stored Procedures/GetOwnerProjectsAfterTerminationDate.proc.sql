121fbba5f843d5998922758a4a51baad4eec7ec2
ThulasiramP <ThulasiramP>
2011-07-26 10:54:00 -0500

    Comments - Defect Number: 2888; Defect RecordID: 2884; Server Address: services.logic2020.com; Server Port: 99; Database Name: Practice Management; Defect Summary: PM General: Resolve issues related to setting future termination date for a person; Defect Description: When you set a future termination date for a person there are a few issues:
    1. It should be possible to enter time through the Termination Date. For example, if the termination date is 7/15/11, that can be considered someone's "last day", so it should be allowed to enter time for 7/15/11 as well. Currently we're having to set termination dates as weekend dates as a workaround.
    2. The notification message that appears if a person's termination date cannot be set needs more detail, and should perhaps display in a printable modal dialog. It should show a report of not only what Projects/Milestone's a person is assigned to (P#, Name), but any current/active ones where they are designated as the Owner.
    3. When someone has a future termination date set, their utilization bar in the Consultant Util Timeline report should be dark grey for the period past their end date. The hover info should show the end date (just like how it appears for persons prior to their start date).

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/GetOwnerProjectsAfterTerminationDate.proc.sql
@@ -0,0 +1,15 @@
+﻿CREATE PROCEDURE [dbo].[GetOwnerProjectsAfterTerminationDate]
+(
+	@PersonId	INT,
+	@TerminationDate	DATETIME 
+	
+)
+AS
+BEGIN 
+
+	SELECT proj.ProjectId,
+		   proj.Name ProjectName
+	FROM dbo.Project AS proj 
+	WHERE proj.ProjectManagerId =@PersonId AND proj.ProjectStatusId =3 
+	
+END
