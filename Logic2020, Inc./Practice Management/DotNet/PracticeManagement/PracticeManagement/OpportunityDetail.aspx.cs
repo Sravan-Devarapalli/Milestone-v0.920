cf15015922db7cf63ae80f529e4654c867a2e08c
ThulasiramP <ThulasiramP>
2011-07-28 08:15:00 -0500

    Comments - Defect Number: 2890; Defect RecordID: 2886; Server Address: services.logic2020.com; Server Port: 99; Database Name: Practice Management; Defect Summary: Projects>Milestones>Persons: Owner need to be able to adjust personhours allocation regardless of seniority; Defect Description: This is similar to issue #2588.
    Anyone defined as the Owner of a Project needs to have the ability to change hour allocation for Resources/Persons assigned to the Milestones, regardless of the seniority difference between the Owner and the Resources/Persons. They don't need to see the Margins still, but they should be able to reallocate time. They should still not be able to completely remove the Resource/Person if the Person has already entered time/notes towards the Milestone.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/OpportunityDetail.aspx.cs
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/OpportunityDetail.aspx.cs
@@ -195,6 +195,7 @@ namespace PraticeManagement
                 DataHelper.FillOpportunityStatusList(ddlStatus, string.Empty);
                 DataHelper.FillPracticeListOnlyActive(ddlPractice, string.Empty);
                 DataHelper.FillOpportunityPrioritiesList(ddlPriority, string.Empty);
+                DataHelper.FillOwnersList(ddlOpportunityOwner, "-- Select Owner --");
 
                 PopulatePriorityHint();
 
@@ -747,7 +748,7 @@ namespace PraticeManagement
                 ddlSalesperson.Enabled =
                 ddlPractice.Enabled =
                 btnSave.Enabled =
-                dfOwner.Enabled =
+                ddlOpportunityOwner.Enabled =
                 ucProposedResources.Enabled = canEdit;
 
             btnConvertToProject.Enabled =
@@ -884,9 +885,22 @@ namespace PraticeManagement
         private void PopulateOwnerDropDown()
         {
             if (Opportunity.Owner != null)
-                dfOwner.SelectedManager = Opportunity.Owner;
+            {
+                var ownerId = Opportunity.Owner.Id.Value.ToString();
+                ListItem selectedOwner = ddlOpportunityOwner.Items.FindByValue(ownerId);
+                if (selectedOwner == null)
+                {
+                    selectedOwner = new ListItem(Opportunity.Owner.PersonLastFirstName, ownerId);
+                    ddlOpportunityOwner.Items.Add(selectedOwner);
+                    ddlOpportunityOwner.SortByText();
+                }
+
+                ddlOpportunityOwner.SelectedValue = selectedOwner.Value;
+            }
             else
-                dfOwner.SetEmptyItem();
+            {
+                ddlOpportunityOwner.SelectedValue = string.Empty;
+            }
         }
 
         private void PopulateClientGroupDropDown()
@@ -952,8 +966,10 @@ namespace PraticeManagement
                 opportunity.ProjectId = int.Parse(ddlProjects.SelectedValue);
             }
 
-            if (dfOwner.SelectedManager != null)
-                opportunity.Owner = dfOwner.SelectedManager;
+            var selectedValue = ddlOpportunityOwner.SelectedValue;
+            opportunity.Owner = string.IsNullOrEmpty(selectedValue) ?
+                null :
+                new Person(Convert.ToInt32(selectedValue));
 
             opportunity.ProposedPersonIdList = ucProposedResources.GetProposedPersonsIdsList();
         }
@@ -972,12 +988,7 @@ namespace PraticeManagement
         }
 
         #region Validations
-
-        protected void cvDfOwnerRequired_ServerValidate(object sender, ServerValidateEventArgs e)
-        {
-            e.IsValid = (dfOwner.SelectedManager != null);
-        }
-
+       
         protected void custTransitionStatus_ServerValidate(object sender, ServerValidateEventArgs e)
         {
             int statusId;
