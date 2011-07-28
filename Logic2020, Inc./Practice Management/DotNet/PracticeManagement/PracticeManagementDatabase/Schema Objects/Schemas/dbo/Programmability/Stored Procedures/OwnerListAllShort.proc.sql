d4d4ada878c1b81ed4fcab9ea935b82f0876b5a8
ThulasiramP <ThulasiramP>
2011-07-28 08:14:00 -0500

    Comments - Defect Number: 2890; Defect RecordID: 2886; Server Address: services.logic2020.com; Server Port: 99; Database Name: Practice Management; Defect Summary: Projects>Milestones>Persons: Owner need to be able to adjust personhours allocation regardless of seniority; Defect Description: This is similar to issue #2588.
    Anyone defined as the Owner of a Project needs to have the ability to change hour allocation for Resources/Persons assigned to the Milestones, regardless of the seniority difference between the Owner and the Resources/Persons. They don't need to see the Margins still, but they should be able to reallocate time. They should still not be able to completely remove the Resource/Person if the Person has already entered time/notes towards the Milestone.

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/OwnerListAllShort.proc.sql
@@ -0,0 +1,17 @@
+﻿CREATE PROCEDURE [dbo].[OwnerListAllShort]
+	@PersonStatusId INT = NULL
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	SELECT DISTINCT p.PersonId,
+	       p.FirstName,
+	       p.LastName
+	FROM dbo.aspnet_Users AS u
+	INNER JOIN dbo.Person AS p ON u.UserName = p.Alias
+	INNER JOIN dbo.aspnet_UsersInRoles AS ur ON u.UserId = ur.UserId
+	INNER JOIN dbo.aspnet_Roles AS r ON ur.RoleId = r.RoleId
+	WHERE (@PersonStatusId IS NULL OR p.PersonStatusId = @PersonStatusId)
+		   AND r.RoleName IN ('Project lead' , 'Practice Area Manager' ,'Client Director')
+	ORDER BY p.LastName, p.FirstName
+END
