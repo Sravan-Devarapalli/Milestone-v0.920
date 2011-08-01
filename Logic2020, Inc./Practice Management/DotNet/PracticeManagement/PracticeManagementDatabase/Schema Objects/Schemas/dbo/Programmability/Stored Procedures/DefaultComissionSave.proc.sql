95e6d8cb6b33f59fe540340fc496c3e3531c923e
AshokD <AshokD>
2011-08-01 10:19:00 -0500

    Comments - Defect Number: 2836; Defect RecordID: 2832; Server Address: services.logic2020.com; Server Port: 99; Database Name: Practice Management; Defect Summary: Configuration>Company Resources>Persons: Move commission percentage (sales or recruiter) to compensation record; Defect Description: We need to move the commission percentage to the compensation record so that if/when it changes for a person it has a start/end date that is updated. If this value changes it should create a new compensation record, similar to when a person's practice is changed. Otherwise when the commission is changed it affects previous projects as well.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/DefaultComissionSave.proc.sql
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/DefaultComissionSave.proc.sql
@@ -15,8 +15,12 @@ AS
 	SET NOCOUNT ON
 	SET XACT_ABORT ON
 
-	DECLARE @Today DATETIME
-	SET @Today = dbo.Today()
+	DECLARE @Today DATETIME,
+			@PayEndDate DATETIME,
+			@PayStartDate DATETIME,
+			@IsActivePay BIT
+
+	SET @Today = CONVERT(DATETIME,CONVERT(DATE,[dbo].[GettingPMTime](GETDATE())))
 
 	BEGIN TRAN
 
@@ -38,18 +42,76 @@ AS
 	BEGIN
 		-- There is no value set today
 
-		-- Expire old data
-		UPDATE dbo.[DefaultCommission]
-		   SET EndDate = @Today
-		 WHERE PersonId = @PersonId
+		DECLARE @OldFractionOfMargin DECIMAL(18,2),
+				@OldMarginTypeId INT
+		
+		SELECT @OldFractionOfMargin = FractionOfMargin,
+			 @OldMarginTypeId = MarginTypeId
+		FROM dbo.[DefaultCommission]
+		WHERE PersonId = @PersonId
 		   AND [Type] = @Type
 		   AND @Today >= StartDate
 		   AND @Today < EndDate
+		IF(@OldFractionOfMargin <> @FractionOfMargin  
+		  OR @OldFractionOfMargin IS NULL AND @FractionOfMargin IS NOT NULL
+		  OR @OldFractionOfMargin IS NOT NULL AND @FractionOfMargin IS NULL
+		  OR @OldMarginTypeId <> @MarginTypeId
+		  OR @OldMarginTypeId IS NULL AND @MarginTypeId IS NOT NULL
+		  OR @OldMarginTypeId IS NOT NULL AND @MarginTypeId IS NULL)
+		BEGIN
+			-- Expire old data
+			UPDATE dbo.[DefaultCommission]
+			   SET EndDate = @Today
+			 WHERE PersonId = @PersonId
+			   AND [Type] = @Type
+			   AND @Today >= StartDate
+			   AND @Today < EndDate
 
-		-- Set new comission
-		INSERT INTO dbo.[DefaultCommission]
-		            (PersonId, StartDate, EndDate, FractionOfMargin, [type], MarginTypeId)
-		     VALUES (@PersonId, @Today, '2029-12-31', @FractionOfMargin, @Type, @MarginTypeId)
+			DECLARE @MaxPayEndDate DATETIME
+			IF(@Type = 1)
+			BEGIN
+				SELECT @MaxPayEndDate = MAX(EndDate)
+				FROM dbo.Pay
+				WHERE Person = @PersonId
+
+				SELECT @PayEndDate = EndDate,
+						@IsActivePay = [IsActivePay]
+				FROM dbo.Pay
+				WHERE Person = @PersonId AND StartDate < @Today AND EndDate > @Today
+			END
+
+			-- Set new comission
+			INSERT INTO dbo.[DefaultCommission]
+						(PersonId, StartDate, EndDate, FractionOfMargin, [type], MarginTypeId)
+			VALUES (@PersonId, @Today, CASE WHEN @MaxPayEndDate>@PayEndDate AND @Type = 1 THEN @PayEndDate ELSE '2029-12-31' END, 
+					@FractionOfMargin, @Type, @MarginTypeId)
+
+			IF(@PayEndDate IS NOT NULL AND @Type = 1)
+			BEGIN
+				UPDATE dbo.Pay
+				SET EndDate = @Today,
+					[IsActivePay] = 0
+				WHERE EndDate = @PayEndDate AND Person = @PersonId
+			
+				INSERT INTO dbo.Pay
+				SELECT [Person]
+					,@Today
+					,@PayEndDate
+					,[Amount]
+					,[Timescale]
+					,[TimesPaidPerMonth]
+					,[Terms]
+					,[VacationDays]
+					,[BonusAmount]
+					,[BonusHoursToCollect]
+					,[DefaultHoursPerDay]
+					,[SeniorityId]
+					,[PracticeId]
+					,@IsActivePay
+				FROM [dbo].[Pay]
+				WHERE Person = @PersonId AND EndDate = @Today			
+			END
+		END
 	END
 
 	COMMIT
