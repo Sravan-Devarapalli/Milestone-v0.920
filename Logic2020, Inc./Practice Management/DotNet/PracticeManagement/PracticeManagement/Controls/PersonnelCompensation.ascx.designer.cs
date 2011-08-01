f03782806a6ff8d40d191050e2c5774fa381dc88
AshokD <AshokD>
2011-08-01 10:19:00 -0500

    Comments - Defect Number: 2836; Defect RecordID: 2832; Server Address: services.logic2020.com; Server Port: 99; Database Name: Practice Management; Defect Summary: Configuration>Company Resources>Persons: Move commission percentage (sales or recruiter) to compensation record; Defect Description: We need to move the commission percentage to the compensation record so that if/when it changes for a person it has a start/end date that is updated. If this value changes it should create a new compensation record, similar to when a person's practice is changed. Otherwise when the commission is changed it affects previous projects as well.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/Controls/PersonnelCompensation.ascx.designer.cs
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/Controls/PersonnelCompensation.ascx.designer.cs
@@ -470,6 +470,33 @@ namespace PraticeManagement.Controls {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.CustomValidator custValPractice;
+        
+        /// <summary>
+        /// trSalesCommisiion control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlTableRow trSalesCommisiion;
+        
+        /// <summary>
+        /// txtSalesCommission control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtSalesCommission;
+        
+        /// <summary>
+        /// custValSalesCommission control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.CustomValidator custValSalesCommission;
     }
 }
 
