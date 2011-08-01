ccebd47ad54d1ea2a32dfd4f5852696888ddc2da
AshokD <AshokD>
2011-08-01 10:19:00 -0500

    Comments - Defect Number: 2836; Defect RecordID: 2832; Server Address: services.logic2020.com; Server Port: 99; Database Name: Practice Management; Defect Summary: Configuration>Company Resources>Persons: Move commission percentage (sales or recruiter) to compensation record; Defect Description: We need to move the commission percentage to the compensation record so that if/when it changes for a person it has a start/end date that is updated. If this value changes it should create a new compensation record, similar to when a person's practice is changed. Otherwise when the commission is changed it affects previous projects as well.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/DefaultComissionDelete.proc.sql
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/DefaultComissionDelete.proc.sql
@@ -13,8 +13,12 @@ AS
 	SET NOCOUNT ON
 	SET XACT_ABORT ON
 
-	DECLARE @Today DATETIME
-	SET @Today = dbo.[Today]()
+	DECLARE @Today DATETIME,
+			@PayEndDate DATETIME,
+			@PayStartDate DATETIME,
+			@IsActivePay BIT
+ 
+	SET @Today = CONVERT(DATETIME,CONVERT(DATE,[dbo].[GettingPMTime](GETDATE())))
 
 	BEGIN TRAN
 
@@ -32,6 +36,37 @@ AS
 	   AND [Type] = @Type
 	   AND @Today >= StartDate
 	   AND @Today < EndDate
-
+	   IF @Type = 1 
+	   BEGIN
+		  SELECT @PayEndDate = EndDate,
+				 @IsActivePay = [IsActivePay]
+		  FROM dbo.Pay
+		  WHERE Person = @PersonId AND StartDate < @Today AND EndDate > @Today
+				IF(@PayEndDate IS NOT NULL)
+				BEGIN
+					UPDATE dbo.Pay
+					SET EndDate = @Today,
+						[IsActivePay] = 0
+					WHERE EndDate = @PayEndDate AND Person = @PersonId
+			
+					INSERT INTO dbo.Pay
+					SELECT [Person]
+						,@Today
+						,@PayEndDate
+						,[Amount]
+						,[Timescale]
+						,[TimesPaidPerMonth]
+						,[Terms]
+						,[VacationDays]
+						,[BonusAmount]
+						,[BonusHoursToCollect]
+						,[DefaultHoursPerDay]
+						,[SeniorityId]
+						,[PracticeId]
+						,@IsActivePay
+					FROM [dbo].[Pay]
+					WHERE Person = @PersonId AND EndDate = @Today	
+				END
+		END
 	COMMIT TRAN
 
