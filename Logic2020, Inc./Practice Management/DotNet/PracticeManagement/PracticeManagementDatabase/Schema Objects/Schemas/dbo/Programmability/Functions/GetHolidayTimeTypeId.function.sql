903d60624f4ad73bac572bcc1652e146c7557ed3
SrinivasM <SrinivasM>
2011-08-02 09:11:00 -0500

    Comments - Added feature for getting seperate row in TimeEntry page for Recurring Holidays.

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Functions/GetHolidayTimeTypeId.function.sql
@@ -0,0 +1,16 @@
+﻿CREATE FUNCTION [dbo].[GetHolidayTimeTypeId] 
+(
+)
+RETURNS INT
+AS
+BEGIN
+
+	DECLARE @Id INT
+
+	SELECT @Id = tt.TimeTypeId 
+	FROM TimeType tt 
+	WHERE tt.Name = 'Holiday' --Here Holiday is the Time Type Name.
+	
+	return @Id
+END
+
