493fad3e5272eab86bad4b7401f5c84e2fb6093b
SrinivasM <SrinivasM>
2011-08-08 10:12:00 -0500

    Comments - Defect Number: 2757; Defect RecordID: 2753; Server Address: services.logic2020.com; Server Port: 99; Database Name: Practice Management; Defect Summary: Configuration>Time>Company Holidays: Create/add functionality; Defect Description: Currently the ability to add company holidays is done by the Admin from the Calendar page. I'd like to move it to it's own section under Configuration>Time Entry>Company Holidays, and change the Calendar page so that it always opens to the logged in user's calendar. Even the Admin should not be able to select "Entire Company" from the drop-down there.
    I'd like to center the year selector for both areas (Calendar and Company Holidays) and make the year a larger font (highlighted below).
    
    On the Company Holidays page there should be descriptive text to the right of the calendar that describes what it's function is (see below).
    
    I'd also like to add a "Recurring Holidays" function (multi-select drop-down) so that an admin/user doesn't have to go back and highlight the same holidays each year. Since PM is localized to the US for now, the drop-down should only include US federal holidays:
    New Year's Day (Jan 1)
    Martin Luther King, Jr. Day (Jan 17)
    Washington's Birthday (Feb 21)
    Memorial Day (last Monday in May)
    Independence Day (July 4)
    Labor Day (first Monday in September)
    Columbus Day (second Monday in October)
    Veterans Day (Nov 11 or nearest weekday)
    Thanksgiving Day (Nov 24)
    Christmas Day (Dec 25)
    
    Typically, if the holiday falls on a Saturday you are given prior Friday off, and if the day falls on a Sunday you are given the following Monday off.
    
    If an admin has selected a recurring holiday for the current year, but then manually changes the date by clicking the day/date in the calendar itself, that change should only affect the current calendar year. Future years would continue to use the logically determined recurring date.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/CalendarUpdate.proc.sql
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/CalendarUpdate.proc.sql
@@ -10,23 +10,103 @@ CREATE PROCEDURE [dbo].[CalendarUpdate]
 	@Date       DATETIME,
 	@DayOff     BIT,
 	@PersonId   INT,
-	@UserLogin	NVARCHAR(255)
+	@UserLogin	NVARCHAR(255),
+	@IsRecurringHoliday BIT,
+	@RecurringHolidayId INT,
+	@HolidayDescription	NVARCHAR(255),
+	@RecurringHolidayDate DATETIME
 )
 AS
 	SET NOCOUNT ON
 	
 	DECLARE @Today DATETIME,
-			@DefaultMilestoneId INT
+			@DefaultMilestoneId INT,
+			@PTOTimeTypeId INT
 
 	SELECT @Today = dbo.GettingPMTime(GETDATE())
 	SELECT @DefaultMilestoneId = MilestoneId FROM DefaultMilestoneSetting
+	SELECT @PTOTimeTypeId = TimeTypeId FROM TimeType WHERE Name = 'PTO'
+
+	DECLARE @Dates TABLE ([date] DATETIME)
+
+	--In case of  special recurring holidays we have choice of removing only.
+	--We need to update companyRecurringHoliday table if @RecurringHolidayId has value.
+	IF @RecurringHolidayId IS NOT NULL AND @DayOff = 0 AND @IsRecurringHoliday = 1 --For special Recurring holidays dates.
+	BEGIN
+		
+		UPDATE CompanyRecurringHoliday
+		SET IsSet = @DayOff
+		WHERE Id = @RecurringHolidayId
+
+		INSERT INTO @Dates([Date])
+		SELECT C1.Date
+		FROM dbo.Calendar AS C1
+		JOIN dbo.CompanyRecurringHoliday crh ON C1.[Date] >= @Today
+				AND 
+				(
+						(crh.[Day] IS NOT NULL --If holiday is on exact Date.
+							AND (	--If Holiday comes in weekends then we need to give prior to weekdays.
+									DAY(C1.[Date]) = crh.[Day] AND MONTH(C1.[Date]) = crh.[Month] AND DATEPART(DW,C1.[Date]) NOT IN(1,7)
+									OR DAY(DATEADD(DD,1,C1.[Date])) = crh.[Day] AND MONTH(DATEADD(DD,1,C1.[Date])) = crh.[Month]  AND DATEPART(DW,C1.[Date]) = 6
+									OR DAY(DATEADD(DD,-1,C1.[Date])) = crh.[Day] AND MONTH(DATEADD(DD,-1,C1.[Date])) = crh.[Month] AND DATEPART(DW,C1.[Date]) = 2
+								)
+							)
+							OR
+							( crh.[Day] IS NULL AND MONTH(C1.[Date]) = crh.[Month]
+							AND (
+									DATEPART(DW,C1.[Date]) = crh.DayOfTheWeek
+									AND (
+											(crh.NumberInMonth IS NOT NULL
+												AND  (C1.[Date] - DAY(C1.[Date])+1) -
+															CASE WHEN (DATEPART(DW,C1.[Date]-DAY(C1.[Date])+1))%7 <= crh.DayOfTheWeek 
+																	THEN (DATEPART(DW,C1.[Date]-DAY(C1.[Date])+1))%7
+																	ELSE (DATEPART(DW,C1.[Date]-DAY(C1.[Date])+1)-7)
+																	END +(7*(crh.NumberInMonth-1))+crh.DayOfTheWeek = C1.[Date]
+												)
+												OR( crh.NumberInMonth IS NULL 
+													AND (DATEADD(MM,1,C1.[Date] - DAY(C1.[Date])+1)- 1) - 
+															(CASE WHEN DATEPART(DW,(DATEADD(MM,1,C1.[Date] - DAY(C1.[Date])+1)- 1)) >= crh.DayOfTheWeek
+																THEN (DATEPART(DW,(DATEADD(MM,1,C1.[Date] - DAY(C1.[Date])+1)- 1)))-7
+																ELSE (DATEPART(DW,(DATEADD(MM,1,C1.[Date] - DAY(C1.[Date])+1)- 1)))
+																END)-(7-crh.DayOfTheWeek)= C1.[Date]
+												)
+											)
+								)
+						
+							)
+				 
+					)	
+		WHERE crh.Id = @RecurringHolidayId
+	END
+	ELSE  -- For Recurring Holidays setting manually.
+	BEGIN
+		INSERT INTO @Dates
+		SELECT [Date]
+		FROM Calendar	 
+		WHERE Date >= @Date 
+		AND ( (@IsRecurringHoliday = 0 AND Date = @Date)
+			OR (@IsRecurringHoliday = 1 AND @RecurringHolidayDate IS NOT NULL  AND RecurringHolidayId IS NULL AND
+					(
+						(DAY([Date]) = DAY(@RecurringHolidayDate) AND MONTH([Date]) = MONTH(@RecurringHolidayDate) AND DATEPART(DW,[Date]) NOT IN(1,7))
+								OR ( DAY(DATEADD(DD,1,[Date])) = DAY(@RecurringHolidayDate) AND MONTH(DATEADD(DD,1,[Date])) = MONTH(@RecurringHolidayDate)  AND DATEPART(DW,[Date]) = 6
+									OR DAY(DATEADD(DD,-1,[Date])) = DAY(@RecurringHolidayDate) AND MONTH(DATEADD(DD,-1,[Date])) = MONTH(@RecurringHolidayDate) AND DATEPART(DW,[Date]) = 2
+								)
+					)
+				)
+			)
+	END
 	
 	IF @PersonId IS NULL
 	BEGIN
 		-- Update the company calendar
-		UPDATE dbo.Calendar
-		   SET DayOff = @DayOff
-		 WHERE Date = @Date		
+		UPDATE C1
+		   SET DayOff = @DayOff,
+				HolidayDescription = CASE WHEN @DayOff = 0 THEN '' ELSE @HolidayDescription END,
+				IsRecurring = CASE WHEN @DayOff = 0 THEN NULL ELSE @IsRecurringHoliday END, --whether it should be applied to future dates or not either dayoff 0 or 1.
+				RecurringHolidayId = CASE WHEN @DayOff = 0 THEN NULL ELSE @RecurringHolidayId END, --we need to set holidayId to null while removing..
+				RecurringHolidayDate = CASE WHEN @DayOff = 0 THEN NULL ELSE @RecurringHolidayDate END-- if date is not recurring OR recurring holiday Id is there then Recurringholidaydate should be null.
+		FROM dbo.Calendar C1
+		JOIN @Dates d ON d.[date] = C1.Date
 	END
 	ELSE IF EXISTS (SELECT 1
 	                  FROM dbo.Calendar AS cal
@@ -58,94 +138,91 @@ AS
 	   SET DayOff = pc.DayOff
 	  FROM dbo.PersonCalendarAuto AS ca
 	       INNER JOIN dbo.v_PersonCalendar AS pc ON ca.date = pc.Date AND ca.PersonId = pc.PersonId
-	 WHERE ca.Date = @Date
-	   AND (@PersonId IS NULL OR ca.PersonId = @PersonId)
+		   JOIN @Dates AS d ON ca.Date = d.[date]
+	 WHERE (@PersonId IS NULL OR ca.PersonId = @PersonId)
+	 
 
-
-	IF @Date >= @Today
+	IF @DayOff = 0
 	BEGIN
-		IF @DayOff = 0
-		BEGIN
-			DELETE TE
-			FROM TimeEntries TE
-			JOIN CompanyRecurringHolidaysByPeriod(@Date,@Date) crh ON crh.Date = TE.MilestoneDate
-			JOIN MilestonePerson MP ON MP.MilestonePersonId = TE.MilestonePersonId AND MP.MilestoneId = @DefaultMilestoneId
-			WHERE crh.IsSet = 1 
-				AND TE.IsAutoGenerated = 1
-				AND (MP.PersonId = @PersonId OR @PersonId IS NULL)
-		END
-		ELSE
-		BEGIN
-			DECLARE @CurrentPMTime DATETIME,
-						@ModifiedBy INT,
-						@HolidayTimeTypeId INT
+		DELETE TE
+		FROM TimeEntries TE
+		JOIN @Dates crh ON crh.[date] = TE.MilestoneDate
+		JOIN MilestonePerson MP ON MP.MilestonePersonId = TE.MilestonePersonId AND MP.MilestoneId = @DefaultMilestoneId
+		WHERE TE.IsAutoGenerated = 1
+			AND (MP.PersonId = @PersonId OR @PersonId IS NULL)
+	END
+	ELSE
+	BEGIN
+		DECLARE @CurrentPMTime DATETIME,
+					@ModifiedBy INT,
+					@HolidayTimeTypeId INT
 				
-			SET @CurrentPMTime = dbo.InsertingTime()
-			SELECT @ModifiedBy = PersonId FROM Person WHERE Alias = @UserLogin
-			SET @HolidayTimeTypeId = dbo.GetHolidayTimeTypeId()
+		SET @CurrentPMTime = dbo.InsertingTime()
+		SELECT @ModifiedBy = PersonId FROM Person WHERE Alias = @UserLogin
+		SET @HolidayTimeTypeId = dbo.GetHolidayTimeTypeId()
 				
-			--If MilestonePersonId is not there for the Person in DefaultMilestone
-			INSERT INTO MilestonePerson(PersonId,MilestoneId)
-			SELECT Distinct P.PersonId, @DefaultMilestoneId AS [MilestoneId]
-			FROM Person P
-			LEFT JOIN MilestonePerson mp ON mp.MilestoneId = @DefaultMilestoneId AND P.PersonId = mp.PersonId
-			JOIN Pay pay ON pay.Person = p.PersonId
-			JOIN CompanyRecurringHolidaysByPeriod(@Date, @Date) as rhd ON rhd.IsSet = 1 AND rhd.Date BETWEEN pay.StartDate AND ISNULL(pay.EndDate, dbo.GetFutureDate())- 1
-			WHERE pay.Timescale = 2 
-					AND ISNULL(P.TerminationDate, dbo.GetFutureDate()) >= @Today 
-					AND  mp.MilestonePersonId IS NULL
-					AND (P.PersonId = @PersonId OR @PersonId IS NULL)	
+		--If MilestonePersonId is not there for the Person in DefaultMilestone
+		INSERT INTO MilestonePerson(PersonId,MilestoneId)
+		SELECT Distinct P.PersonId, @DefaultMilestoneId AS [MilestoneId]
+		FROM Person P
+		LEFT JOIN MilestonePerson mp ON mp.MilestoneId = @DefaultMilestoneId AND P.PersonId = mp.PersonId
+		JOIN Pay pay ON pay.Person = p.PersonId
+		JOIN @Dates as rhd ON rhd.[date] BETWEEN pay.StartDate AND ISNULL(pay.EndDate, dbo.GetFutureDate())- 1
+		WHERE pay.Timescale = 2 
+				AND ISNULL(P.TerminationDate, dbo.GetFutureDate()) >= @Today 
+				AND  mp.MilestonePersonId IS NULL
+				AND (P.PersonId = @PersonId OR @PersonId IS NULL)	
 						
-			-- if any new MilestonePersons inserted then we need to create MilestonePersonEntry for the new milestonePersonIds
-			INSERT INTO MilestonePersonEntry(MilestonePersonId, StartDate, EndDate, Amount, HoursPerDay)
-			SELECT Distinct mp.MilestonePersonId, m.StartDate, m.ProjectedDeliveryDate, 0, 8
-			FROM MilestonePerson mp
-			LEFT JOIN MilestonePersonEntry mpe on mpe.MilestonePersonId = mp.MilestonePersonId
-			JOIN Pay p ON p.Person = mp.PersonId
-			JOIN CompanyRecurringHolidaysByPeriod(@Date, @Date) rhd ON rhd.IsSet = 1 AND rhd.Date BETWEEN p.StartDate AND p.EndDate
-			JOIN Milestone m ON m.MilestoneId = mp.MilestoneId
-			WHERE mp.MilestoneId = @DefaultMilestoneId 
-					AND p.Timescale = 2 
-					AND mpe.MilestonePersonId IS NULL
-					AND (mp.PersonId = @PersonId OR @PersonId IS NULL)
+		-- if any new MilestonePersons inserted then we need to create MilestonePersonEntry for the new milestonePersonIds
+		INSERT INTO MilestonePersonEntry(MilestonePersonId, StartDate, EndDate, Amount, HoursPerDay)
+		SELECT Distinct mp.MilestonePersonId, m.StartDate, m.ProjectedDeliveryDate, 0, 8
+		FROM MilestonePerson mp
+		LEFT JOIN MilestonePersonEntry mpe on mpe.MilestonePersonId = mp.MilestonePersonId
+		JOIN Pay p ON p.Person = mp.PersonId
+		JOIN @Dates rhd ON rhd.[date] BETWEEN p.StartDate AND p.EndDate
+		JOIN Milestone m ON m.MilestoneId = mp.MilestoneId
+		WHERE mp.MilestoneId = @DefaultMilestoneId 
+				AND p.Timescale = 2 
+				AND mpe.MilestonePersonId IS NULL
+				AND (mp.PersonId = @PersonId OR @PersonId IS NULL)
 				
-			INSERT  INTO [dbo].[TimeEntries]
-							( [EntryDate] ,
-								[MilestoneDate] ,
-								[ModifiedDate] ,
-								[MilestonePersonId] ,
-								[ActualHours] ,
-								[ForecastedHours] ,
-								[TimeTypeId] ,
-								[ModifiedBy] ,
-								[Note] ,
-								[IsChargeable] ,
-								[IsCorrect],
-								[IsAutoGenerated]
-							)
-			SELECT @CurrentPMTime
-					,rhd.[Date]
-					,@CurrentPMTime
-					,mp.MilestonePersonId
-					,8
-					,mpe.HoursPerDay
-					,@HolidayTimeTypeId
-					,@ModifiedBy
-					,rhd.[Description]
-					,m.IsChargeable
-					,0
-					,1 --Here it is Auto generated.
-			FROM MilestonePerson mp 
-			JOIN MilestonePersonEntry mpe ON mp.MilestonePersonId = mpe.MilestonePersonId
-			JOIN Milestone m ON m.MilestoneId = mp.MilestoneId
-			JOIN Pay pay ON pay.Person = mp.PersonId  AND pay.Timescale = 2
-			JOIN Person p ON p.PersonId = pay.Person
-			JOIN CompanyRecurringHolidaysByPeriod(@Date, @Date) rhd ON rhd.IsSet = 1 AND rhd.Date BETWEEN pay.StartDate AND (CASE WHEN p.TerminationDate IS NOT NULL AND pay.EndDate - 1 > p.TerminationDate THEN p.TerminationDate
-																																ELSE pay.EndDate - 1
-																																END)
-			LEFT JOIN TimeEntries te ON te.MilestonePersonId = mpe.MilestonePersonId AND te.MilestoneDate = rhd.Date
-			WHERE  mp.MilestoneId = @DefaultMilestoneId 
-					AND te.MilestoneDate IS NULL
-					AND (mp.PersonId = @PersonId OR @PersonId IS NULL)
-		END
+		INSERT  INTO [dbo].[TimeEntries]
+						( [EntryDate] ,
+							[MilestoneDate] ,
+							[ModifiedDate] ,
+							[MilestonePersonId] ,
+							[ActualHours] ,
+							[ForecastedHours] ,
+							[TimeTypeId] ,
+							[ModifiedBy] ,
+							[Note] ,
+							[IsChargeable] ,
+							[IsCorrect],
+							[IsAutoGenerated]
+						)
+		SELECT @CurrentPMTime
+				,rhd.[Date]
+				,@CurrentPMTime
+				,mp.MilestonePersonId
+				,8
+				,mpe.HoursPerDay
+				,CASE WHEN @PersonId IS NOT NULL AND c.DayOff <> @DayOff THEN @PTOTimeTypeId ELSE @HolidayTimeTypeId END 
+				,@ModifiedBy
+				,CASE WHEN @PersonId IS NOT NULL AND c.DayOff <> @DayOff THEN 'PTO' ELSE C.HolidayDescription END
+				,m.IsChargeable
+				,0
+				,1 --Here it is Auto generated.
+		FROM MilestonePerson mp 
+		JOIN MilestonePersonEntry mpe ON mp.MilestonePersonId = mpe.MilestonePersonId
+		JOIN Milestone m ON m.MilestoneId = mp.MilestoneId
+		JOIN Pay pay ON pay.Person = mp.PersonId  AND pay.Timescale = 2
+		JOIN Person p ON p.PersonId = pay.Person
+		JOIN @Dates rhd ON rhd.[date] BETWEEN pay.StartDate AND (CASE WHEN p.TerminationDate IS NOT NULL AND pay.EndDate - 1 > p.TerminationDate THEN p.TerminationDate
+																															ELSE pay.EndDate - 1
+																															END)
+		LEFT JOIN TimeEntries te ON te.MilestonePersonId = mpe.MilestonePersonId AND te.MilestoneDate = rhd.Date
+		JOIN Calendar c ON c.Date = rhd.date AND DATEPART(DW, c.date) NOT IN (1,7)
+		WHERE  mp.MilestoneId = @DefaultMilestoneId 
+				AND te.MilestoneDate IS NULL
+				AND (mp.PersonId = @PersonId OR @PersonId IS NULL)
 	END
