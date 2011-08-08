9494cad5bb9fe3583795a815637eedf04931cda8
SrinivasM <SrinivasM>
2011-08-08 10:12:00 -0500

    Comments - Defect Number: 2757; Defect RecordID: 2753; Server Address: services.logic2020.com; Server Port: 99; Database Name: Practice Management; Defect Summary: Configuration>Time>Company Holidays: Create/add functionality; Defect Description: Currently the ability to add company holidays is done by the Admin from the Calendar page. I'd like to move it to it's own section under Configuration>Time Entry>Company Holidays, and change the Calendar page so that it always opens to the logged in user's calendar. Even the Admin should not be able to select "Entire Company" from the drop-down there.
    I'd like to center the year selector for both areas (Calendar and Company Holidays) and make the year a larger font (highlighted below).
    
    On the Company Holidays page there should be descriptive text to the right of the calendar that describes what it's function is (see below).
    
    I'd also like to add a "Recurring Holidays" function (multi-select drop-down) so that an admin/user doesn't have to go back and highlight the same holidays each year. Since PM is localized to the US for now, the drop-down should only include US federal holidays:
    New Year's Day (Jan 1)
    Martin Luther King, Jr. Day (Jan 17)
    Washington's Birthday (Feb 21)
    Memorial Day (last Monday in May)
    Independence Day (July 4)
    Labor Day (first Monday in September)
    Columbus Day (second Monday in October)
    Veterans Day (Nov 11 or nearest weekday)
    Thanksgiving Day (Nov 24)
    Christmas Day (Dec 25)
    
    Typically, if the holiday falls on a Saturday you are given prior Friday off, and if the day falls on a Sunday you are given the following Monday off.
    
    If an admin has selected a recurring holiday for the current year, but then manually changes the date by clicking the day/date in the calendar itself, that change should only affect the current calendar year. Future years would continue to use the logically determined recurring date.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/Controls/MonthCalendar.ascx.designer.cs
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/Controls/MonthCalendar.ascx.designer.cs
@@ -29,6 +29,132 @@ namespace PraticeManagement.Controls {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.DataList lstCalendar;
+        
+        /// <summary>
+        /// hdnDummyFieldForModalPopup control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HiddenField hdnDummyFieldForModalPopup;
+        
+        /// <summary>
+        /// hndDayOff control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HiddenField hndDayOff;
+        
+        /// <summary>
+        /// hdnDate control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HiddenField hdnDate;
+        
+        /// <summary>
+        /// hdnRecurringHolidayId control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HiddenField hdnRecurringHolidayId;
+        
+        /// <summary>
+        /// hdnRecurringHolidayDate control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HiddenField hdnRecurringHolidayDate;
+        
+        /// <summary>
+        /// btnSaveDay control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnSaveDay;
+        
+        /// <summary>
+        /// mpeHoliday control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::AjaxControlToolkit.ModalPopupExtender mpeHoliday;
+        
+        /// <summary>
+        /// pnlHolidayDetails control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Panel pnlHolidayDetails;
+        
+        /// <summary>
+        /// lblDate control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl lblDate;
+        
+        /// <summary>
+        /// txtHolidayDescription control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtHolidayDescription;
+        
+        /// <summary>
+        /// chkMakeRecurringHoliday control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.CheckBox chkMakeRecurringHoliday;
+        
+        /// <summary>
+        /// btnDayOK control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnDayOK;
+        
+        /// <summary>
+        /// btnDayCancel control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnDayCancel;
+        
+        /// <summary>
+        /// lblValidationMessage control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblValidationMessage;
     }
 }
 
