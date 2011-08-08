87d055aa511205ad483409b69c60be3f31b02dc3
SrinivasM <SrinivasM>
2011-08-08 10:12:00 -0500

    Comments - Defect Number: 2757; Defect RecordID: 2753; Server Address: services.logic2020.com; Server Port: 99; Database Name: Practice Management; Defect Summary: Configuration>Time>Company Holidays: Create/add functionality; Defect Description: Currently the ability to add company holidays is done by the Admin from the Calendar page. I'd like to move it to it's own section under Configuration>Time Entry>Company Holidays, and change the Calendar page so that it always opens to the logged in user's calendar. Even the Admin should not be able to select "Entire Company" from the drop-down there.
    I'd like to center the year selector for both areas (Calendar and Company Holidays) and make the year a larger font (highlighted below).
    
    On the Company Holidays page there should be descriptive text to the right of the calendar that describes what it's function is (see below).
    
    I'd also like to add a "Recurring Holidays" function (multi-select drop-down) so that an admin/user doesn't have to go back and highlight the same holidays each year. Since PM is localized to the US for now, the drop-down should only include US federal holidays:
    New Year's Day (Jan 1)
    Martin Luther King, Jr. Day (Jan 17)
    Washington's Birthday (Feb 21)
    Memorial Day (last Monday in May)
    Independence Day (July 4)
    Labor Day (first Monday in September)
    Columbus Day (second Monday in October)
    Veterans Day (Nov 11 or nearest weekday)
    Thanksgiving Day (Nov 24)
    Christmas Day (Dec 25)
    
    Typically, if the holiday falls on a Saturday you are given prior Friday off, and if the day falls on a Sunday you are given the following Monday off.
    
    If an admin has selected a recurring holiday for the current year, but then manually changes the date by clicking the day/date in the calendar itself, that change should only affect the current calendar year. Future years would continue to use the logically determined recurring date.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/Calendar.table.sql
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/Calendar.table.sql
@@ -1,6 +1,8 @@
 ﻿CREATE TABLE [dbo].[Calendar] (
-    [Date]   DATETIME NOT NULL,
-    [DayOff] BIT      NOT NULL
+    [Date]					DATETIME NOT NULL,
+    [DayOff]				BIT      NOT NULL,
+	[IsRecurring]			BIT NULL,
+	[RecurringHolidayId]	INT NULL,
+	[HolidayDescription]	NVARCHAR(255),
+	[RecurringHolidayDate]  DATETIME NULL
 );
-
-
