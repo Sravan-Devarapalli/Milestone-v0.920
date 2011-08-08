a730aaee08c569fc0d5c0aabf89a5a9dce7384c3
ThulasiramP <ThulasiramP>
2011-08-08 08:01:00 -0500

    Comments - Defect Number: 2820; Defect RecordID: 2816; Server Address: services.logic2020.com; Server Port: 99; Database Name: Practice Management; Defect Summary: Project>Milestone>Add Person: Should not be able to change person from drop-down after first hours/rate row is added; Defect Description: If a person is selected and a row added, you have the option of adding another row, which is normal, but the user can still change the person from the drop-down which leads to unexpected results. This should be changed so that if a row is added for a person, the person drop-down is greyed/inactive. Perhaps add a button for "Add Another Person" so that it's possible for the user to move forward if they need to add a role for a second person to the milestone.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/MilestonePersonList.aspx.designer.cs
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/MilestonePersonList.aspx.designer.cs
@@ -112,6 +112,15 @@ namespace PraticeManagement {
         protected global::System.Web.UI.HtmlControls.HtmlTableRow thInsertMilestonePerson;
         
         /// <summary>
+        /// thHourlyRate control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlTableCell thHourlyRate;
+        
+        /// <summary>
         /// repPerson control.
         /// </summary>
         /// <remarks>
