cdb2b5c7cd7f092e297a5aeb6f09e29d081d7cb3
SrinivasM <SrinivasM>
2011-08-08 10:12:00 -0500

    Comments - Defect Number: 2757; Defect RecordID: 2753; Server Address: services.logic2020.com; Server Port: 99; Database Name: Practice Management; Defect Summary: Configuration>Time>Company Holidays: Create/add functionality; Defect Description: Currently the ability to add company holidays is done by the Admin from the Calendar page. I'd like to move it to it's own section under Configuration>Time Entry>Company Holidays, and change the Calendar page so that it always opens to the logged in user's calendar. Even the Admin should not be able to select "Entire Company" from the drop-down there.
    I'd like to center the year selector for both areas (Calendar and Company Holidays) and make the year a larger font (highlighted below).
    
    On the Company Holidays page there should be descriptive text to the right of the calendar that describes what it's function is (see below).
    
    I'd also like to add a "Recurring Holidays" function (multi-select drop-down) so that an admin/user doesn't have to go back and highlight the same holidays each year. Since PM is localized to the US for now, the drop-down should only include US federal holidays:
    New Year's Day (Jan 1)
    Martin Luther King, Jr. Day (Jan 17)
    Washington's Birthday (Feb 21)
    Memorial Day (last Monday in May)
    Independence Day (July 4)
    Labor Day (first Monday in September)
    Columbus Day (second Monday in October)
    Veterans Day (Nov 11 or nearest weekday)
    Thanksgiving Day (Nov 24)
    Christmas Day (Dec 25)
    
    Typically, if the holiday falls on a Saturday you are given prior Friday off, and if the day falls on a Sunday you are given the following Monday off.
    
    If an admin has selected a recurring holiday for the current year, but then manually changes the date by clicking the day/date in the calendar itself, that change should only affect the current calendar year. Future years would continue to use the logically determined recurring date.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/DataTransferObjects/CalendarItem.cs
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/DataTransferObjects/CalendarItem.cs
@@ -3,94 +3,123 @@ using System.Runtime.Serialization;
 
 namespace DataTransferObjects
 {
-	/// <summary>
-	/// Represents a calendar day
-	/// </summary>
-	[DataContract]
-	[Serializable]
-	public class CalendarItem : IComparable<CalendarItem>, IEquatable<DateTime>, IEquatable<CalendarItem>
-	{
-		#region Properties
+    /// <summary>
+    /// Represents a calendar day
+    /// </summary>
+    [DataContract]
+    [Serializable]
+    public class CalendarItem : IComparable<CalendarItem>, IEquatable<DateTime>, IEquatable<CalendarItem>
+    {
+        #region Properties
 
-		/// <summary>
-		/// Gets or sets an item's date.
-		/// </summary>
-		[DataMember]
-		public DateTime Date
-		{
-			get;
-			set;
-		}
+        /// <summary>
+        /// Gets or sets an item's date.
+        /// </summary>
+        [DataMember]
+        public DateTime Date
+        {
+            get;
+            set;
+        }
 
-		/// <summary>
-		/// Gets or sets is a day is day-off
-		/// </summary>
-		[DataMember]
-		public bool DayOff
-		{
-			get;
-			set;
-		}
+        /// <summary>
+        /// Gets or sets is a day is day-off
+        /// </summary>
+        [DataMember]
+        public bool DayOff
+        {
+            get;
+            set;
+        }
 
-		/// <summary>
-		/// Gets or sets whether the day is the company-wide day off.
-		/// </summary>
-		[DataMember]
-		public bool CompanyDayOff
-		{
-			get;
-			set;
-		}
+        /// <summary>
+        /// Gets or sets whether the day is the company-wide day off.
+        /// </summary>
+        [DataMember]
+        public bool CompanyDayOff
+        {
+            get;
+            set;
+        }
 
-		/// <summary>
-		/// Gets or sets an ID of the person
-		/// </summary>
-		[DataMember]
-		public int? PersonId
-		{
-			get;
-			set;
-		}
+        /// <summary>
+        /// Gets or sets an ID of the person
+        /// </summary>
+        [DataMember]
+        public int? PersonId
+        {
+            get;
+            set;
+        }
 
-		/// <summary>
-		/// Gets or sets if the calendar item is read only for some raeson.
-		/// </summary>
-		/// <remarks>
-		/// Uses to protect data from unapproved editing or when editing is impossible.
-		/// </remarks>
-		[DataMember]
-		public bool ReadOnly
-		{
-			get;
-			set;
-		}
+        /// <summary>
+        /// Gets or sets if the calendar item is read only for some raeson.
+        /// </summary>
+        /// <remarks>
+        /// Uses to protect data from unapproved editing or when editing is impossible.
+        /// </remarks>
+        [DataMember]
+        public bool ReadOnly
+        {
+            get;
+            set;
+        }
 
-		#endregion
+        [DataMember]
+        public bool IsRecurringHoliday
+        {
+            get;
+            set;
+        }
 
-	    public int CompareTo(CalendarItem other)
-	    {
-	        return other == null ? -1 : other.Date.CompareTo(Date);
-	    }
+        [DataMember]
+        public DateTime? RecurringHolidayDate
+        {
+            get;
+            set;
+        }
 
-	    public bool Equals(DateTime other)
-	    {
-	        return other.Equals(Date);
-	    }
+        [DataMember]
+        public string HolidayDescription
+        {
+            get;
+            set;
+        }
 
-	    public bool Equals(CalendarItem other)
-	    {
-	        return other == null ? false : Equals(other.Date);
-	    }
+        [DataMember]
+        public int? RecurringHolidayId
+        {
+            get;
+            set;
+        }
+
+
+
+        #endregion
+
+        public int CompareTo(CalendarItem other)
+        {
+            return other == null ? -1 : other.Date.CompareTo(Date);
+        }
+
+        public bool Equals(DateTime other)
+        {
+            return other.Equals(Date);
+        }
+
+        public bool Equals(CalendarItem other)
+        {
+            return other == null ? false : Equals(other.Date);
+        }
 
         public static implicit operator CalendarItem(DateTime d)
         {
             return ToCalendarItem(d);
         }
 
-	    public static CalendarItem ToCalendarItem(DateTime d)
-	    {
-	        return new CalendarItem {Date = d};
-	    }
-	}
+        public static CalendarItem ToCalendarItem(DateTime d)
+        {
+            return new CalendarItem { Date = d };
+        }
+    }
 }
-
