c45af14de82a78b6fbd1a0fc7c5f863cfc5cb67f
SrinivasM <SrinivasM>
2011-08-09 08:19:00 -0500

    Comments - Added Alert text and removed holiday and PTO time types from the Time type dropdowns other than the holiday and PTO time entered rows.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/PersonTimeEntries.proc.sql
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/PersonTimeEntries.proc.sql
@@ -11,13 +11,12 @@ AS
 BEGIN
 	SET NOCOUNT ON;
 	
-SELECT te.*, cal.CompanyDayOff, cal.DayOff, cal.Date, CAST(0 as bit) as 'ReadOnly'
+SELECT te.*, cal.CompanyDayOff, cal.DayOff, cal.Date, CAST(0 as bit) as 'ReadOnly', tt.IsSystemTimeType
   FROM v_TimeEntries AS te
   left join v_PersonCalendar as cal on te.PersonId = cal.PersonId and te.MilestoneDate = cal.Date
-  
+  LEFT JOIN TimeType tt ON tt.TimeTypeId = te.TimeTypeId
   WHERE 
 	te.MilestoneDate BETWEEN @StartDate AND @EndDate
 	AND te.PersonId = @PersonId  
 END
-
 
