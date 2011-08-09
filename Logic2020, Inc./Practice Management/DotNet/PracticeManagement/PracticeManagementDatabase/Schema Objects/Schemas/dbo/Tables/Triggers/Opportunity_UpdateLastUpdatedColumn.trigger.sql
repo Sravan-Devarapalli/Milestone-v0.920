ab4a589ea3e4c49c98ab484e6a9ef32e29e7a1f6
SrinivasM <SrinivasM>
2011-08-09 02:08:00 -0500

    Comments - Defect Number: 2879; Defect RecordID: 2875; Server Address: services.logic2020.com; Server Port: 99; Database Name: Practice Management; Defect Summary: Configuration>Troubleshooting>Activity Log: Detail output needs improvement; Defect Description: When looking at the Activity Log, there is detail data that provides no real value, or that is incorrect.
    1. The first entry usually notes that the change was made by "pracadmin", the admin for the SQL db, but it would be better to say the real user/person's name that made the change being logged. In other words, the "Modified By" name. This "Modified By Name" entry doesn't need to be in the detail.
    2. We can omit certain items from the detail such as the Id numbers (TimeEntryId, Milestone Person ID, Modified By Id, Person ID, Milestone ID, Project ID, Client Id). We're already seeing the names that relate to these IDs and that will be more helpful for the admin anyhow.
    3. The Entry Date, Modified Date, and Milestone Date should be mm/dd/yyyy. They're not showing the entire date below.
    4. Time Type should show the friendly name, not the ID (ex: 7).
    
    
    Login activities are cumbersome as well:
    
    1. The activity should just say "Login".
    2. The "User Name" should be [First Name] [Last Name]
    3. Why are there multiple entries for "Changed Membership"? What does that even mean? Does it need to be there at all?
    
    
    Now, take this small example of how the log appears when an Oppty changes status:
    
    1. The detail should use the friendly names (ex: Activity = "Changed Opportunity Status Test"; Detail = Opportunity Name: Test; Status = Active => Lost).
    2. We already know the time this happened, so why do we need a line in the log for "LastUpdated"?
    3. Here, again, "by sa" doesn't tell us who made the change, which is critical to the log.
    
    
    There are probably other cases where the log can be improved, but hopefully this is understood without me having to specify each time.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/Triggers/Opportunity_UpdateLastUpdatedColumn.trigger.sql
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/Triggers/Opportunity_UpdateLastUpdatedColumn.trigger.sql
@@ -3,19 +3,19 @@
 -- Create date: 2010-06-23
 -- Description:	Updates LastUpdated column
 -- =============================================
-CREATE TRIGGER Opportunity_UpdateLastUpdatedColumn 
-   ON  dbo.Opportunity 
-   AFTER UPDATE
-AS 
-BEGIN
-	SET NOCOUNT ON;
+--CREATE TRIGGER Opportunity_UpdateLastUpdatedColumn 
+--   ON  dbo.Opportunity 
+--   AFTER UPDATE
+--AS 
+--BEGIN
+--	SET NOCOUNT ON;
 
-	Update Opportunity 
-	set Opportunity.LastUpdated = getdate()
-	From Opportunity inner join inserted on inserted.OpportunityId = Opportunity.OpportunityId
-END
+--	Update Opportunity 
+--	set Opportunity.LastUpdated = getdate()
+--	From Opportunity inner join inserted on inserted.OpportunityId = Opportunity.OpportunityId
+--END
 
 GO
-EXECUTE sp_settriggerorder @triggername = N'[dbo].[Opportunity_UpdateLastUpdatedColumn]', @order = N'first', @stmttype = N'update';
+--EXECUTE sp_settriggerorder @triggername = N'[dbo].[Opportunity_UpdateLastUpdatedColumn]', @order = N'first', @stmttype = N'update';
 
 
