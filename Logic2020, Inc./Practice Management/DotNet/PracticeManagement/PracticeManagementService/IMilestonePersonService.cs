8524037782df0f73d8b5c7fb3306b1cd67f9ce77
ThulasiramP <ThulasiramP>
2011-08-10 06:46:00 -0500

    Comments - Defect Number: 2820; Defect RecordID: 2816; Server Address: services.logic2020.com; Server Port: 99; Database Name: Practice Management; Defect Summary: Project>Milestone>Add Person: Should not be able to change person from drop-down after first hours/rate row is added; Defect Description: If a person is selected and a row added, you have the option of adding another row, which is normal, but the user can still change the person from the drop-down which leads to unexpected results. This should be changed so that if a row is added for a person, the person drop-down is greyed/inactive. Perhaps add a button for "Add Another Person" so that it's possible for the user to move forward if they need to add a role for a second person to the milestone.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementService/IMilestonePersonService.cs
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementService/IMilestonePersonService.cs
@@ -8,16 +8,16 @@ using DataTransferObjects.Financials;
 
 namespace PracticeManagementService
 {
-	[ServiceContract]
-	public interface IMilestonePersonService
-	{
-		/// <summary>
-		/// Retrives the list of the <see cref="Person"/>s for the specified <see cref="Project"/>.
-		/// </summary>
-		/// <param name="projectId">An ID of the project to the data be retrieved for.</param>
-		/// <returns>The list of the <see cref="MilestonePerson"/> objects.</returns>
-		[OperationContract]
-		List<MilestonePerson> GetMilestonePersonListByProject(int projectId);
+    [ServiceContract]
+    public interface IMilestonePersonService
+    {
+        /// <summary>
+        /// Retrives the list of the <see cref="Person"/>s for the specified <see cref="Project"/>.
+        /// </summary>
+        /// <param name="projectId">An ID of the project to the data be retrieved for.</param>
+        /// <returns>The list of the <see cref="MilestonePerson"/> objects.</returns>
+        [OperationContract]
+        List<MilestonePerson> GetMilestonePersonListByProject(int projectId);
 
         /// <summary>
         /// Retrives the list of the <see cref="Person"/>s for the specified <see cref="Project"/>.
@@ -33,29 +33,29 @@ namespace PracticeManagementService
         [OperationContract]
         List<MilestonePersonEntry> GetConsultantMilestones(ConsultantMilestonesContext context);
 
-		/// <summary>
-		/// Retrives the list of the <see cref="Person"/>s for the specified <see cref="Milestone"/>.
-		/// </summary>
-		/// <param name="milestoneId">An ID of the milestone to the data be retrieved for.</param>
-		/// <returns>The list of the <see cref="MilestonePerson"/> objects.</returns>
-		[OperationContract]
-		List<MilestonePerson> GetMilestonePersonListByMilestone(int milestoneId);
+        /// <summary>
+        /// Retrives the list of the <see cref="Person"/>s for the specified <see cref="Milestone"/>.
+        /// </summary>
+        /// <param name="milestoneId">An ID of the milestone to the data be retrieved for.</param>
+        /// <returns>The list of the <see cref="MilestonePerson"/> objects.</returns>
+        [OperationContract]
+        List<MilestonePerson> GetMilestonePersonListByMilestone(int milestoneId);
 
-		/// <summary>
-		/// Retrives the list of the <see cref="Milestone"/>s for the specified <see cref="Person"/>.
-		/// </summary>
-		/// <param name="personId">An ID of the person the the data be retrieved for.</param>
-		/// <returns>The list of the <see cref="MilestonePerson"/> objects.</returns>
-		[OperationContract]
-		List<MilestonePerson> GetMilestonePersonListByPerson(int personId);
+        /// <summary>
+        /// Retrives the list of the <see cref="Milestone"/>s for the specified <see cref="Person"/>.
+        /// </summary>
+        /// <param name="personId">An ID of the person the the data be retrieved for.</param>
+        /// <returns>The list of the <see cref="MilestonePerson"/> objects.</returns>
+        [OperationContract]
+        List<MilestonePerson> GetMilestonePersonListByPerson(int personId);
 
-		/// <summary>
-		/// Retrives the milestone-person link details
-		/// </summary>
-		/// <param name="milestonePersonId">An ID of the milestone-person association.</param>
-		/// <returns>The <see cref="MilestonePerson"/> object if found and null otherwise.</returns>
-		[OperationContract]
-		MilestonePerson GetMilestonePersonDetail(int milestonePersonId);
+        /// <summary>
+        /// Retrives the milestone-person link details
+        /// </summary>
+        /// <param name="milestonePersonId">An ID of the milestone-person association.</param>
+        /// <returns>The <see cref="MilestonePerson"/> object if found and null otherwise.</returns>
+        [OperationContract]
+        MilestonePerson GetMilestonePersonDetail(int milestonePersonId);
 
         /// <summary>
         /// Checks whether there are any time entries for a given MilestonepersonId.
@@ -63,48 +63,51 @@ namespace PracticeManagementService
         /// <param name="milestonePersonId"></param>
         /// <returns></returns>
         [OperationContract]
-        bool CheckTimeEntriesForMilestonePerson(int milestonePersonId,  DateTime? startDate, DateTime? endDate,
+        bool CheckTimeEntriesForMilestonePerson(int milestonePersonId, DateTime? startDate, DateTime? endDate,
                                                 bool checkStartDateEquality, bool checkEndDateEquality);
 
-		/// <summary>
-		/// Saves the specified <see cref="Milestone"/>-<see cref="Person"/> link to the database.
-		/// </summary>
-		/// <param name="milestonePerson">The data to be saved to.</param>
-		/// <param name="userName">A current user.</param>
-		[OperationContract]
-		void SaveMilestonePerson(ref MilestonePerson milestonePerson, string userName);
+        /// <summary>
+        /// Saves the specified <see cref="Milestone"/>-<see cref="Person"/> link to the database.
+        /// </summary>
+        /// <param name="milestonePerson">The data to be saved to.</param>
+        /// <param name="userName">A current user.</param>
+        [OperationContract]
+        void SaveMilestonePerson(ref MilestonePerson milestonePerson, string userName);
 
         [OperationContract]
         void SaveMilestonePersons(List<MilestonePerson> milestonePersons, string userName);
 
-		/// <summary>
-		/// Deletes the specified <see cref="Milestone"/>-<see cref="Person"/> link from the database.
-		/// </summary>
-		/// <param name="milestonePerson">The data to be deleted from.</param>
-		[OperationContract]
-		void DeleteMilestonePerson(MilestonePerson milestonePerson);
+        /// <summary>
+        /// Deletes the specified <see cref="Milestone"/>-<see cref="Person"/> link from the database.
+        /// </summary>
+        /// <param name="milestonePerson">The data to be deleted from.</param>
+        [OperationContract]
+        void DeleteMilestonePerson(MilestonePerson milestonePerson);
 
-	    /// <summary>
-	    /// Retrives the list of the <see cref="Person"/>s for the specified <see cref="Milestone"/>.
-	    /// </summary>
-	    /// <param name="milestoneId">An ID of the milestone to the data be retrieved for.</param>
-	    /// <returns>The list of the <see cref="MilestonePerson"/> objects.</returns>
+        /// <summary>
+        /// Retrives the list of the <see cref="Person"/>s for the specified <see cref="Milestone"/>.
+        /// </summary>
+        /// <param name="milestoneId">An ID of the milestone to the data be retrieved for.</param>
+        /// <returns>The list of the <see cref="MilestonePerson"/> objects.</returns>
         [OperationContract]
         List<MilestonePerson> GetMilestonePersonListByMilestoneNoFinancials(int milestoneId);
 
         [OperationContract]
         List<MilestonePerson> MilestonePersonsByMilestoneForTEByProject(int milestoneId);
 
-	    /// <summary>
-	    /// Computed financials for milestone person
-	    /// </summary>
-	    /// <param name="milestonePersonId">An ID of the milestone-person association</param>
-	    /// <returns>Computed financials</returns>
-	    [OperationContract]
-	    MilestonePersonComputedFinancials CalculateMilestonePersonFinancials(int milestonePersonId);
+        /// <summary>
+        /// Computed financials for milestone person
+        /// </summary>
+        /// <param name="milestonePersonId">An ID of the milestone-person association</param>
+        /// <returns>Computed financials</returns>
+        [OperationContract]
+        MilestonePersonComputedFinancials CalculateMilestonePersonFinancials(int milestonePersonId);
 
         [OperationContract]
         List<MilestonePerson> GetMilestonePersonsDetailsByMileStoneId(int milestoneId);
-	}
+
+        [OperationContract]
+        ComputedFinancials FinancialsGetByMilestonePersonEntry(int milestoneId, int personId, DateTime startDate);
+    }
 }
 
