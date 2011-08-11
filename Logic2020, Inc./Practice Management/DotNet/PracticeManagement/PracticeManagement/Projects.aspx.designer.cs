02a8608c10afc455b632ea01fc293e14513c747e
AshokD <AshokD>
2011-08-11 02:14:00 -0500

    Comments - Defect Number: 2897; Defect RecordID: 2893; Server Address: services.logic2020.com; Server Port: 99; Database Name: Practice Management; Defect Summary: Projects>Summary: Change default search behavior; Defect Description: Currently we have the default search behavior, when used from the chevron drop-down on the Summary page, set to "Search View". We need to remove that option and default to "Search All".

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/Projects.aspx.designer.cs
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/Projects.aspx.designer.cs
@@ -202,15 +202,6 @@ namespace PraticeManagement {
         protected global::System.Web.UI.WebControls.RequiredFieldValidator reqSearchText;
         
         /// <summary>
-        /// btnSearch control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Button btnSearch;
-        
-        /// <summary>
         /// btnSearchAll control.
         /// </summary>
         /// <remarks>
