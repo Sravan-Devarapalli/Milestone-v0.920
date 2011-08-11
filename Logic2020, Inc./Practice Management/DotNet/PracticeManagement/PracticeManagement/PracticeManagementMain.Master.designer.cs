498190883838e0bbe71c9f6bafb9bcd6664399be
AshokD <AshokD>
2011-08-11 02:14:00 -0500

    Comments - Defect Number: 2800; Defect RecordID: 2796; Server Address: services.logic2020.com; Server Port: 99; Database Name: Practice Management; Defect Summary: PM General: Log user off at PM timeout; Defect Description: Right now when PM times out the user is not made aware until they try to click a link or save a change, then they are sent to the login page.
    They should be logged off instead and automatically send to a page with a message stating: "You have been logged off due to inactivity for security reasons. Please click [here] to log back in." [here] should be a link back to the main login page.
    
    If possible, 1 minute before the auto-logoff, can we send a pop-up box (with "Continue" and "Exit" buttons) to the user with a message stating: "Are you still there? If you would like to extend your PM session, please click the "Continue" button below, otherwise you will automatically be logged off in 1 minute. Clicking the "Exit" button will log you off immediately."

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/PracticeManagementMain.Master.designer.cs
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/PracticeManagementMain.Master.designer.cs
@@ -274,6 +274,15 @@ namespace PraticeManagement {
         protected global::System.Web.UI.WebControls.HiddenField hdnLastServerVisit;
         
         /// <summary>
+        /// hdnRedirectToLogedOutPage control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HiddenField hdnRedirectToLogedOutPage;
+        
+        /// <summary>
         /// hdnRunTimeOutPopuUpScript control.
         /// </summary>
         /// <remarks>
