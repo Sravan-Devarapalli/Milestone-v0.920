65f32c495dc9203af66b65ee54aa117574e24d61
SrinivasM <SrinivasM>
2011-08-12 08:37:00 -0500

    Comments - Added feature  for adding hours to PTO Time type time entries in Person calendar page.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/PersonCalendar.table.sql
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/PersonCalendar.table.sql
@@ -1,7 +1,8 @@
 ﻿CREATE TABLE [dbo].[PersonCalendar] (
     [Date]     DATETIME NOT NULL,
     [PersonId] INT      NOT NULL,
-    [DayOff]   BIT      NOT NULL
+    [DayOff]   BIT      NOT NULL,
+	[ActualHours]		REAL NULL
 );
 
 
