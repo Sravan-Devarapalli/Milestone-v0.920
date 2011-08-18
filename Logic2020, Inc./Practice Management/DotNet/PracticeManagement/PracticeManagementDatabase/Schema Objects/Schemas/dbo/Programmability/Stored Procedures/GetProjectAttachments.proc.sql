e01826adc2e55fe710bc6565d24d22ef5c2f3af4
SrinivasM <SrinivasM>
2011-08-18 07:43:00 -0500

    Comments - Allowed to add multiple SOW attachments to the project.

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/GetProjectAttachments.proc.sql
@@ -0,0 +1,13 @@
+﻿CREATE PROCEDURE GetProjectAttachments
+(
+	@ProjectId INT
+)
+AS
+BEGIN
+	SELECT Id,
+			FileName,
+			DATALENGTH(AttachmentData) AS AttachmentSize,
+			UploadedDate
+	FROM ProjectAttachment
+	WHERE ProjectId = @ProjectId
+END
