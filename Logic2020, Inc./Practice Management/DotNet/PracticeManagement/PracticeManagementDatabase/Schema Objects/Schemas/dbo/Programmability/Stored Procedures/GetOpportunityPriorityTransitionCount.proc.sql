7a7814439b765be28c79cd2a580915bce29eca4d
SrinivasM <SrinivasM>
2011-08-22 07:00:00 -0500

    Comments - Defect Number: 2866; Defect RecordID: 2862; Server Address: services.logic2020.com; Server Port: 99; Database Name: Practice Management; Defect Summary: Opporutnities: Changes to Export data; Defect Description: Much like the changes to the Project export, there are many columns in the Oppty export that are not needed or in the wrong order. Plus I'd like to add a few columns into the export. I would also like to add header and summary info if possible. This would ideally end up exportable as a PDF as well.
    Columns for the export should be:
    Oppty NumberPriorityClient - GroupBuyerOpportunity NameEst. RevenueSalespersonOwnerCreated OnLast Updated
    
    Notes on some columns:
    - Est. Revenue should be formatted as a currency to no decimal places
    - Created On could just have date, not time stamp as well
    - Last Updated could just have date, not time stamp as well
    
    See attached for the header/summary layout.
    
    Notes on header/summary info:
    - Total Opportunities means Total Active Opporunities
    - Unique Clients means, of the active opptys, how many unique clients are there total?
    - Priority Trending would look at how many opptys had their priority raised or lowered overall in the last 7 days (presuming this is tracked in the activity log)
    - Opportunity Aging is just reporting how many of the opptys that are noted as Priority A B C or D are between 0-30 days, 31-60 days, and 61-120+ days old
    - Total Estimated Revenue by Practice would just sum the est revenue based on what practice is designated in the oppty
    
    Finally - Would it be possible to also add this summary info into the summary and/or Discussion Review 2 web page, perhaps as a drop-down section?

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/GetOpportunityPriorityTransitionCount.proc.sql
@@ -0,0 +1,59 @@
+﻿CREATE PROCEDURE [dbo].[GetOpportunityPriorityTransitionCount]
+(
+	@DaysPrevious int
+)
+AS
+BEGIN
+	;With OpportunityTransitionPriorityList AS
+	(SELECT [OpportunityTransitionId]
+			  ,[OpportunityId]
+			  ,[OpportunityTransitionStatusId]
+			  ,[TransitionDate]
+			  ,[PersonId]
+			  ,[NoteText]
+			  ,[OpportunityTransitionTypeId]
+			  ,[TargetPersonId]
+			  ,SUBSTRING(REPLACE(REPLACE(REPLACE([NoteText],'Priority changed.  Was: ',''),'now',''),' ',''),0,
+			  CHARINDEX(':',REPLACE(REPLACE(REPLACE([NoteText],'Priority changed.  Was: ',''),'now',''),' ',''))) Previous
+				  ,SUBSTRING( REPLACE(REPLACE(REPLACE([NoteText],'Priority changed.  Was: ',''),'now',''),' ',''),
+								CHARINDEX(':',REPLACE(REPLACE(REPLACE([NoteText],'Priority changed.  Was: ',''),'now',''),' ',''))+1
+								,LEN(REPLACE(REPLACE(REPLACE([NoteText],'Priority changed.  Was: ',''),'now',''),' ',''))-
+								CHARINDEX(':',REPLACE(REPLACE(REPLACE([NoteText],'Priority changed.  Was: ',''),'now',''),' ',''))
+			  ) next
+		  FROM OpportunityTransition
+		  WHERE [OpportunityTransitionStatusId] = 2
+		  AND TransitionDate >= dbo.GettingPMTime(GETUTCDATE()) - @DaysPrevious --Get last @days days record.
+	),
+
+	PriorityListDerived AS
+	(
+	SELECT opl.OpportunityId,
+			opl.TransitionDate,
+			opl.Previous,
+			opl.next,
+			MAX(opl.TransitionDate) OVER(partition by opl.OpportunityId) AS 'MaxDate',
+			MIN(opl.TransitionDate) OVER(partition by opl.OpportunityId) AS 'MinDate',
+			case WHEN opl.TransitionDate = MAX(opl.TransitionDate) OVER(partition by opl.OpportunityId) THEN np.sortOrder ELSE NULL END [NextSortOrder],
+			case WHEN opl.TransitionDate = MIN(opl.TransitionDate) OVER(partition by opl.OpportunityId) THEN pp.sortOrder ELSE NULL END [PreviousSortOrder]
+	FROM OpportunityTransitionPriorityList opl
+	INNER JOIN OpportunityPriorities np On np.Priority = opl.next
+	INNER JOIN OpportunityPriorities pp On pp.Priority = opl.Previous
+	WHERE NoteText LIKE '%Priority changed.%'
+
+	)
+
+	SELECT (case WHEN priorityTrend.Status = 1 THEN 'Up'
+				WHEN priorityTrend.Status = 0 THEN 'Down'
+				ELSE 'Equal' END) [PriorityTrendType], COUNT(priorityTrend.OpportunityId) PriorityTrendCount
+	FROM (
+		SELECT pld.OpportunityId, (case WHEN SUM(pld.NextSortOrder) < SUM(pld.PreviousSortOrder) THEN 1
+													WHEN SUM(pld.NextSortOrder) > SUM(pld.PreviousSortOrder) THEN 0
+													ELSE null END) AS [Status]
+		FROM PriorityListDerived pld
+		GROUP BY pld.OpportunityId
+		) AS priorityTrend
+	WHERE priorityTrend.Status IS NOT NULL --To read only up and down.
+	GROUP BY priorityTrend.Status
+	ORDER BY priorityTrend.Status DESC
+
+END
