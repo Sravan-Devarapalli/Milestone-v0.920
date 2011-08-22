eaa69630cfa502ff9829a0dc3e06e4f9d2526c45
SrinivasM <SrinivasM>
2011-08-22 07:00:00 -0500

    Comments - Defect Number: 2866; Defect RecordID: 2862; Server Address: services.logic2020.com; Server Port: 99; Database Name: Practice Management; Defect Summary: Opporutnities: Changes to Export data; Defect Description: Much like the changes to the Project export, there are many columns in the Oppty export that are not needed or in the wrong order. Plus I'd like to add a few columns into the export. I would also like to add header and summary info if possible. This would ideally end up exportable as a PDF as well.
    Columns for the export should be:
    Oppty NumberPriorityClient - GroupBuyerOpportunity NameEst. RevenueSalespersonOwnerCreated OnLast Updated
    
    Notes on some columns:
    - Est. Revenue should be formatted as a currency to no decimal places
    - Created On could just have date, not time stamp as well
    - Last Updated could just have date, not time stamp as well
    
    See attached for the header/summary layout.
    
    Notes on header/summary info:
    - Total Opportunities means Total Active Opporunities
    - Unique Clients means, of the active opptys, how many unique clients are there total?
    - Priority Trending would look at how many opptys had their priority raised or lowered overall in the last 7 days (presuming this is tracked in the activity log)
    - Opportunity Aging is just reporting how many of the opptys that are noted as Priority A B C or D are between 0-30 days, 31-60 days, and 61-120+ days old
    - Total Estimated Revenue by Practice would just sum the est revenue based on what practice is designated in the oppty
    
    Finally - Would it be possible to also add this summary info into the summary and/or Discussion Review 2 web page, perhaps as a drop-down section?

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/OpportunityList.aspx.cs
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/OpportunityList.aspx.cs
@@ -7,15 +7,88 @@ using PraticeManagement.OpportunityService;
 using PraticeManagement.Controls.Generic.Filtering;
 using System.Linq;
 using PraticeManagement.Utils;
+using System.Web.UI.WebControls;
+using System.Web;
+using System.Web.UI;
+using System.IO;
+using System.Collections.Generic;
 
 namespace PraticeManagement
 {
     public partial class OpportunityList : PracticeManagementPageBase
     {
+        #region Constants
+
+        private const string CurrencyDisplayFormat = "$###,###,###,###,###,##0";
+        private const string ExcelDateFormat = "mso-number-format";
+        private const string ExcelDateFormatStyle = "mm-dd-yyyy";
+
+        #endregion Constants
+
+        #region Properties
+
+        private Opportunity[] OpportunitiesList
+        {
+            get
+            {
+                return DataHelper.GetFilteredOpportunitiesForDiscussionReview2();
+            }
+        }
+
+        private Dictionary<string, int> PriorityTrendList
+        {
+            get
+            {
+                using (var serviceClient = new OpportunityService.OpportunityServiceClient())
+                {
+                    return serviceClient.GetOpportunityPriorityTransitionCount(Constants.Dates.HistoryDays);
+                }
+            }
+        }
+
+        private Dictionary<string, int> StatusChangesList
+        {
+            get
+            {
+                using (var serviceClient = new OpportunityService.OpportunityServiceClient())
+                {
+                    return serviceClient.GetOpportunityStatusChangeCount(Constants.Dates.HistoryDays);
+                }
+            }
+        }
+
+        #endregion
+
+        private void DatabindOpportunities()
+        {
+            opportunities.DatabindOpportunities();
+        }
+
+        protected override void Display()
+        {
+            DatabindOpportunities();
+
+            var summary = GetSummaryDetails();
+            pnlSummary.Controls.Add(summary);
+        }
+
         protected void ofOpportunityList_OnFilterOptionsChanged(object sender, EventArgs e)
         {
             DatabindOpportunities();
+
+            var summary = GetSummaryDetails();
+            pnlSummary.Controls.Add(summary);
         }
+
+        protected void btnResetSort_OnClick(object sender, EventArgs e)
+        {
+            ofOpportunityList.ResetFilter();
+            opportunities.ResetFilter();
+
+            DatabindOpportunities();
+        }
+
+        #region Export
 
         protected void btnExportToExcel_Click(object sender, EventArgs e)
         {
@@ -28,52 +101,88 @@ namespace PraticeManagement
                                      select new
                                      {
                                          Id = opp.Id != null ? opp.Id.ToString() : string.Empty,
-                                         ProjectId = opp.ProjectId != null ? opp.ProjectId.ToString() : string.Empty,
-                                         EstimatedRevenue = opp.EstimatedRevenue != null ? opp.EstimatedRevenue.ToString() : string.Empty,
-                                         OpportunityIndex = opp.OpportunityIndex != null ? opp.OpportunityIndex.ToString() : string.Empty,
-                                         Name = opp.Name != null ? opp.Name.ToString() : string.Empty,
-                                         Priority = opp.Priority != null ? opp.Priority.Priority : string.Empty,
-                                         ProjectedStartDate = opp.ProjectedStartDate != null ? opp.ProjectedStartDate.ToString() : string.Empty,
-                                         LastUpdate = opp.LastUpdate.ToString(),
-                                         ProjectedEndDate = opp.ProjectedEndDate != null ? opp.ProjectedEndDate.ToString() : string.Empty,
                                          OpportunityNumber = opp.OpportunityNumber != null ? opp.OpportunityNumber.ToString() : string.Empty,
+                                         Priority = opp.Priority != null ? opp.Priority.Priority : string.Empty,
+                                         ProjectedStartDate = opp.ProjectedStartDate != null ? opp.ProjectedStartDate.Value.ToShortDateString() : string.Empty,
+                                         ClientAndGroup = opp.ClientAndGroup != null ? opp.ClientAndGroup.ToString() : string.Empty,
+                                         Buyer = opp.BuyerName != null ? opp.BuyerName.ToString() : string.Empty,
+                                         Name = opp.Name != null ? opp.Name.ToString() : string.Empty,
+                                         EstimatedRevenue = opp.EstimatedRevenue != null ? opp.EstimatedRevenue.ToString() : string.Empty,
+                                         SalesTeam = opp.Salesperson != null ? opp.Salesperson.PersonLastFirstName : string.Empty,
+                                         Owner = opp.Owner != null ? opp.Owner.PersonLastFirstName : string.Empty,
+                                         CreateDate = opp.CreateDate.ToShortDateString(),
+                                         LastUpdate = opp.LastUpdate.ToShortDateString(),
+                                         ProjectId = opp.ProjectId != null ? opp.ProjectId.ToString() : string.Empty,
+                                         OpportunityIndex = opp.OpportunityIndex != null ? opp.OpportunityIndex.ToString() : string.Empty,
+                                         ProjectedEndDate = opp.ProjectedEndDate != null ? opp.ProjectedEndDate.ToString() : string.Empty,
                                          Description = opp.Description != null ? opp.Description.ToString() : string.Empty,
-                                         BuyerName = opp.BuyerName != null ? opp.BuyerName.ToString() : string.Empty,
-                                         CreateDate = opp.CreateDate.ToString(),
                                          Pipeline = opp.Pipeline != null ? opp.Pipeline.ToString() : string.Empty,
-                                         Proposed = opp.Proposed != null ? opp.Proposed.ToString() : string.Empty,
                                          SendOut = opp.SendOut != null ? opp.SendOut.ToString() : string.Empty,
                                          DaysOld = opp.DaysOld.ToString(),
                                          LastChange = opp.LastChange.ToString(),
                                          ProposedPersonIdList = opp.ProposedPersonIdList != null ? opp.ProposedPersonIdList.ToString() : string.Empty,
                                          OutSideResources = opp.OutSideResources != null ? opp.OutSideResources.ToString() : string.Empty,
-                                         ClientAndGroup = opp.ClientAndGroup != null ? opp.ClientAndGroup.ToString() : string.Empty,
-                                     }).ToList();
-
+                                     }).ToList(); //Note If you add new property, then change the header in RowDataBound.
 
             excelGrid.DataSource = opportunitiesData;
+            excelGrid.RowDataBound += new GridViewRowEventHandler(excelGrid_RowDataBound);
             excelGrid.DataMember = "excelDataTable";
             excelGrid.DataBind();
             excelGrid.Visible = true;
-            GridViewExportUtil.Export("Opportunities.xls", excelGrid);
+
+            var summaryDetails = GetSummaryDetails();
+
+            GridViewExportUtil.Export("Opportunities.xls", excelGrid, summaryDetails, "Logic20/20 Opportunities");
         }
 
-        private void DatabindOpportunities()
+        private Table GetSummaryDetails()
         {
-            opportunities.DatabindOpportunities();
+            return OpportunitiesHelper.GetFormatedSummaryDetails(OpportunitiesList, PriorityTrendList, StatusChangesList);
         }
 
-        protected void btnResetSort_OnClick(object sender, EventArgs e)
+        public void excelGrid_RowDataBound(object sender, GridViewRowEventArgs e)
         {
-            ofOpportunityList.ResetFilter();
-            opportunities.ResetFilter();
+            int cellsCount = e.Row.Cells.Count;
+            if (e.Row.RowType == DataControlRowType.Header)
+            {
+                e.Row.Cells[1].Text = "Opportunity Number";
+                e.Row.Cells[3].Text = "Start Date";
+                e.Row.Cells[4].Text = "Client - Group";
+                e.Row.Cells[6].Text = "Opportunity Name";
+                e.Row.Cells[7].Text = "Est.Revenue";
+                e.Row.Cells[8].Text = "Sales Team";
+                e.Row.Cells[9].Text = "Owner";
+                e.Row.Cells[10].Text = "Created On";
+                e.Row.Cells[11].Text = "Last Updated";
 
-            DatabindOpportunities();
+                for (int i = 12; i < cellsCount; i++)
+                {
+                    e.Row.Cells.RemoveAt(12);
+                }
+            }
+            else if (e.Row.RowType == DataControlRowType.DataRow)
+            {
+                decimal? estRevenue = null;
+                string revenueText = e.Row.Cells[7].Text;
+                string startdate = e.Row.Cells[3].Text;
+
+                e.Row.Cells[3].Style.Add(ExcelDateFormat, ExcelDateFormatStyle);
+                e.Row.Cells[10].Style.Add(ExcelDateFormat, ExcelDateFormatStyle);
+                e.Row.Cells[11].Style.Add(ExcelDateFormat, ExcelDateFormatStyle);
+                if (!(string.IsNullOrEmpty(revenueText) || revenueText == "&nbsp;"))
+                {
+                    estRevenue = Convert.ToDecimal(revenueText);
+                }
+                e.Row.Cells[7].Text = estRevenue.HasValue ? estRevenue.Value.ToString(CurrencyDisplayFormat) : string.Empty;
+                for (int i = 12; i < cellsCount; i++)
+                {
+                    e.Row.Cells.RemoveAt(12);
+                }
+
+            }
+            e.Row.Cells.RemoveAt(0); //Removing OpportunityId column in the report.
         }
 
-        protected override void Display()
-        {
-            DatabindOpportunities();
-        }
+        #endregion
     }
 }
