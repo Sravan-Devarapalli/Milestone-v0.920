6ff16f5d0cb4e6a81aa19433a080510316f5c061
ThulasiramP <ThulasiramP>
2011-08-22 09:35:00 -0500

    Comments - Defect Number: 2683; Defect RecordID: 2679; Server Address: services.logic2020.com; Server Port: 99; Database Name: Practice Management; Defect Summary: OpportunityDetail.aspx: Can we convert inactive, Lost and Experimental opportunities to projected project.; Defect Description: Steps to Repro:
    1) Open Oppurtunity.aspx page.
    2) Click on inactive or lost or experimental opportunity.
    3) Fill all details and click on Tools tab.
    4) Now click on Won-Convert to Projected project.
    
    Result: Inactive or Lost or experimental Opportunity is converted to Projected project.
    Expected: It doesnt make sense to convert inactive, lost and Experimental opportunities to projected project.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/DiscussionReview1.aspx.designer.cs
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/DiscussionReview1.aspx.designer.cs
@@ -193,6 +193,15 @@ namespace PraticeManagement {
         protected global::System.Web.UI.UpdatePanel upOpportunityDetail;
         
         /// <summary>
+        /// hdnHasProjectIdOrPermission control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HiddenField hdnHasProjectIdOrPermission;
+        
+        /// <summary>
         /// lblReadOnlyWarning control.
         /// </summary>
         /// <remarks>
