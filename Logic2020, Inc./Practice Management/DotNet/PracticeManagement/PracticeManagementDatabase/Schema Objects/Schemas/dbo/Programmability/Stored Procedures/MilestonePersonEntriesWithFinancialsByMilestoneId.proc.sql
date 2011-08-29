5ec5f357c257095b41b4c24114b18602703408e3
ThulasiramP <ThulasiramP>
2011-08-29 09:47:00 -0500

    Comments - Defect Number: 2902; Defect RecordID: 2898; Server Address: services.logic2020.com; Server Port: 99; Database Name: Practice Management; Defect Summary: RE: PM Critical Issue; Defect Description: **For multiple projects, there are issues adding W2-Hourly employees as resources. I have been able to add them to a milestone and save without any apparent issues, but when navigating back to the milestone/project, they changes were not preserved. Also of note, though I am able to save the W2-Hourly employee to the milestone as a resource, their margin is reported as 0%, which is incorrect. See original e-mails below.**
    Jason,
    
    I can confirm this has occurred on another project as well. I went back to check the changes I made to add David Choy to the US Data project. And he had disappeared from the project as well.
    
    Given all these resources are hourly and when you add them their margins are 0% maybe it relates to the person records? (per my other issue I raised this evening).
    
    
    Regards,
    
    Travis
    Travis P. Jones , Director // Logic20/20 // travisj@logic2020.com
    t.206-576-0400//c.360-421-9215// /www.logic2020.com
    
    From: Travis Jones
    Sent: Sunday, August 28, 2011 9:20 PM
    To: Jason Hong-Turney
    Cc: Matt Reilly; Sean R. Cunningham; Stacy Taylor (StacyT@logic2020.com)
    Subject: RE: PM Critical Issue
    
    Jason,
    
    Unfortunately, I need to re-open this issue and raise this to a P! for immediate fix. I went back into PM this evening and the changes didnt persist.
    
    I re-did the changes (on a 1 by 1 basis) and again it appears they didnt persist\save correctly in the resource view. Once you change views and go back the resources have disappeared again. I am trying to add Jodi Sherwell and Kerissa Freeberg to the project.
    
    I am not sure if this is project or system specific at this time.
    
    Let me know if you need any further details.
    
    Regards,
    
    Travis
    Travis P. Jones , Director // Logic20/20 // travisj@logic2020.com
    t.206-576-0400//c.360-421-9215// /www.logic2020.com
    
    From: Matt Reilly
    Sent: Friday, August 26, 2011 4:56 PM
    To: Travis Jones; Jason Hong-Turney
    Subject: RE: PM Critical Issue
    
    Jason,
    
    I spoke with Travis. He has since resolved. It appears PM does not like saving multiple rows at a time. When Travis added and saved one row at a time it seemed to respond properly. Can you add this to our defect list?
    
    Secondarily, per the screenshot below, it seems we have some redundancy in the design  two Add People/Add Resource buttons. Is there a functional difference? If so, it is not readily apparent what that would be. Something else worth correcting.
    
    - Matt
    
    Matthew Reilly, Senior Manager// Logic20/20 // MattR@logic2020.com
    c.425-503-5825// www.logic2020.com
    
    From: Travis Jones
    Sent: Friday, August 26, 2011 3:33 PM
    To: Matt Reilly; Jason Hong-Turney
    Subject: PM Critical Issue
    
    I am trying to add people and I cant get it to save on the project.
    
    I click save all and the green buttons and nothing seems to work. Any ideas?
    
    
    
    Regards,
    
    Travis
    Travis P. Jones , Director // Logic20/20 // travisj@logic2020.com
    t.206-576-0400//c.360-421-9215// /www.logic2020.com

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/MilestonePersonEntriesWithFinancialsByMilestoneId.proc.sql
@@ -0,0 +1,90 @@
+﻿CREATE PROCEDURE [dbo].[MilestonePersonEntriesWithFinancialsByMilestoneId]
+	@MilestoneId      INT
+AS
+BEGIN
+	
+	SELECT mp.MilestonePersonId,
+		   mp.SeniorityId,
+           mp.PersonId,
+	       mp.StartDate,
+	       mp.EndDate,
+	       mp.PersonRoleId,
+	       mp.Amount,
+	       mp.HoursPerDay,
+	       mp.RoleName,
+		   mp.VacationDays,	
+	       mp.ExpectedHours,
+		   mp.Location,
+		   mp.LastName,
+		   mp.FirstName
+	  FROM dbo.v_MilestonePerson AS mp
+	 WHERE mp.MilestoneId = @MilestoneId
+
+
+	 ;WITH FinancialsRetro AS 
+	(
+	SELECT f.ProjectId,
+		   f.PersonId,
+		   f.MilestoneId,
+		   f.EntryStartDate,
+		   f.Date, 
+		   f.PersonMilestoneDailyAmount,
+		   f.PersonDiscountDailyAmount,
+		   ISNULL(c.FractionOfMargin,0) ProjectSalesCommisionFraction,
+		   (ISNULL(f.PayRate, 0) + ISNULL(f.OverheadRate, 0)+ISNULL(f.BonusRate,0)+ISNULL(f.VacationRate,0)
+		   	+ISNULL(f.RecruitingCommissionRate,0)) SLHR,
+		   ISNULL(f.PayRate,0) PayRate,
+		   f.MLFOverheadRate,
+		   f.PersonHoursPerDay,
+		   f.PracticeManagementCommissionSub,
+		   f.PracticeManagementCommissionOwn ,
+		   f.PracticeManagerId,
+		   f.Discount
+	FROM v_FinancialsRetrospective f
+	LEFT JOIN (
+					SELECT ProjectId,SUM(FractionOfMargin) FractionOfMargin
+					FROM dbo.Commission 
+					WHERE CommissionType = 1
+					GROUP BY ProjectId
+		) C ON C.ProjectId = f.ProjectId
+	WHERE f.MilestoneId = @MilestoneId  
+	)
+
+	SELECT f.ProjectId,
+			f.MilestoneId,
+		   f.PersonId,
+		   f.EntryStartDate StartDate,   
+	       dbo.MakeDate(YEAR(MIN(f.Date)), MONTH(MIN(f.Date)), 1) AS FinancialDate,
+	       dbo.MakeDate(YEAR(MIN(f.Date)), MONTH(MIN(f.Date)), dbo.GetDaysInMonth(MIN(f.Date))) AS MonthEnd,
+
+	       ISNULL(SUM(f.PersonMilestoneDailyAmount), 0) AS Revenue,
+
+	       ISNULL(SUM(f.PersonMilestoneDailyAmount - f.PersonDiscountDailyAmount), 0) AS RevenueNet,
+
+		   ISNULL(SUM(f.PersonMilestoneDailyAmount - f.PersonDiscountDailyAmount -
+						(CASE WHEN f.SLHR >= f.PayRate + f.MLFOverheadRate 
+							  THEN f.SLHR ELSE ISNULL(f.PayRate, 0)+f.MLFOverheadRate END) 
+					    *ISNULL(f.PersonHoursPerDay, 0)), 0) GrossMargin,
+		   
+		   ISNULL(SUM((CASE WHEN f.SLHR >=  f.PayRate +f.MLFOverheadRate 
+					 THEN f.SLHR ELSE  f.PayRate +f.MLFOverheadRate END)*ISNULL(f.PersonHoursPerDay, 0)), 0) Cogs,
+
+	       ISNULL(SUM(f.PersonHoursPerDay), 0) AS Hours,
+	       
+	       (SUM((f.PersonMilestoneDailyAmount - f.PersonDiscountDailyAmount -
+						(CASE WHEN f.SLHR >=  f.PayRate +f.MLFOverheadRate 
+							  THEN f.SLHR  ELSE f.PayRate +f.MLFOverheadRate END) 
+					    *ISNULL(f.PersonHoursPerDay, 0))* (f.ProjectSalesCommisionFraction/100))) SalesCommission,
+
+	       SUM((f.PersonMilestoneDailyAmount - f.PersonDiscountDailyAmount -
+	            (CASE WHEN f.SLHR >=  f.PayRate +f.MLFOverheadRate 
+							  THEN f.SLHR ELSE f.PayRate +f.MLFOverheadRate END) * ISNULL(f.PersonHoursPerDay, 0)) *
+	           (f.PracticeManagementCommissionSub + CASE f.PracticeManagerId WHEN f.PersonId THEN f.PracticeManagementCommissionOwn ELSE 0 END)) / 100 AS PracticeManagementCommission,
+	           0.0 AS 'actualhours',
+	           0.0 AS 'forecastedhours',
+           ISNULL(SUM(vac.VacationHours), 0) AS 'VacationHours'
+		   
+	  FROM FinancialsRetro AS f
+	  LEFT JOIN dbo.v_MilestonePersonVacations AS vac ON f.MilestoneId = vac.MilestoneId AND f.PersonId = vac.PersonId
+	  GROUP BY   f.ProjectId, f.MilestoneId, f.PersonId,f.EntryStartDate
+END
