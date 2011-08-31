33b7b5ed2567ac157e70f05ce24b725a04290548
ThulasiramP <ThulasiramP>
2011-08-31 01:33:00 -0500

    Comments - Defect Number: 2773; Defect RecordID: 2769; Server Address: services.logic2020.com; Server Port: 99; Database Name: Practice Management; Defect Summary: Project>Milestone>Detail>Person:  Should not be able to have a person's end date beyond the milestone end date; Defect Description: At present it is able to set a person's end date beyond the milestone end date which is creating problems with revenue reporting. Attempting to do so should result in a message prompt to the user that says "You have tried to enter an end date for a person that is beyond the milestone end date. Please either extend the milestone end date or choose an earlier end date for the person." Can that be a pop up message that just has an OK button (no cancel)? I think these kinds of user prompts/messages can be more effective than an error note that just shows up on the page.

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/HasTimeEntriesForMilestoneBetweenOldAndNewDates.proc.sql
@@ -0,0 +1,48 @@
+﻿CREATE PROCEDURE [dbo].[HasTimeEntriesForMilestoneBetweenOldAndNewDates]
+(
+@MilestoneId	INT,
+@StartDate   DATETIME = NULL,
+@EndDate     DATETIME  = NULL
+)
+AS
+BEGIN
+DECLARE @Result BIT , 
+		@MileStoneStartDate  DATETIME,
+		@MileStoneEndDate  DATETIME,
+		@SwapStartDate  DATETIME,
+		@SwapEndDate  DATETIME
+
+SELECT @MileStoneStartDate = m.StartDate
+	   ,@MileStoneEndDate =m.ProjectedDeliveryDate
+	   FROM Milestone AS m
+	   WHERE m.MilestoneId = @MilestoneId
+
+IF(@StartDate > @MileStoneStartDate)
+BEGIN
+  SET @SwapStartDate = @StartDate
+  SELECT @StartDate = @MileStoneStartDate , @MileStoneStartDate = @SwapStartDate
+END
+
+IF(@EndDate > @MileStoneEndDate)
+BEGIN
+  SET @SwapEndDate = @EndDate
+  SELECT @EndDate = @MileStoneEndDate , @MileStoneEndDate = @SwapEndDate
+END
+
+IF EXISTS(SELECT 1 FROM v_TimeEntries as te	where te.MilestoneId = @MilestoneId AND
+			 MilestoneDate  BETWEEN @StartDate AND @MileStoneStartDate)
+BEGIN 
+	SELECT @Result=1
+END
+ELSE IF EXISTS(SELECT 1 FROM v_TimeEntries as te	where te.MilestoneId = @MilestoneId AND
+			 MilestoneDate  BETWEEN @EndDate AND @MileStoneEndDate)
+BEGIN 
+	SELECT @Result=1
+END
+ELSE
+BEGIN
+	SELECT @Result=0
+END
+	SELECT @Result AS HasTimeEntries
+END
+GO
