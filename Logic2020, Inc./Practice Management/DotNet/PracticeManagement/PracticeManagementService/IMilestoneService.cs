5d789eaf786803741ff3e1fcbc478be0dd237aa7
ThulasiramP <ThulasiramP>
2011-08-31 01:29:00 -0500

    Comments - Defect Number: 2773; Defect RecordID: 2769; Server Address: services.logic2020.com; Server Port: 99; Database Name: Practice Management; Defect Summary: Project>Milestone>Detail>Person:  Should not be able to have a person's end date beyond the milestone end date; Defect Description: At present it is able to set a person's end date beyond the milestone end date which is creating problems with revenue reporting. Attempting to do so should result in a message prompt to the user that says "You have tried to enter an end date for a person that is beyond the milestone end date. Please either extend the milestone end date or choose an earlier end date for the person." Can that be a pop up message that just has an OK button (no cancel)? I think these kinds of user prompts/messages can be more effective than an error note that just shows up on the page.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementService/IMilestoneService.cs
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementService/IMilestoneService.cs
@@ -5,9 +5,9 @@ using DataTransferObjects;
 
 namespace PracticeManagementService
 {
-	[ServiceContract]
-	public interface IMilestoneService
-	{
+    [ServiceContract]
+    public interface IMilestoneService
+    {
 
         /// <summary>
         /// Saves Default Project-milestone details into DB. Persons not assigned to any Project-Milestone 
@@ -26,69 +26,69 @@ namespace PracticeManagementService
         [OperationContract]
         DefaultMilestone GetDefaultMilestone();
 
-		/// <summary>
-		/// Retrieves the list of <see cref="Milestone"/>s for the specified <see cref="Project"/>.
-		/// </summary>
-		/// <param name="projectId">
-		/// An ID of the <see cref="Project"/> to retrieve the <see cref="Milestone"/>s for.
-		/// </param>
-		/// <returns>The list of the <see cref="Project"/>'s <see cref="Milestone"/>s.</returns>
-		[OperationContract]
-		List<Milestone> MilestoneListByProject(int projectId);
+        /// <summary>
+        /// Retrieves the list of <see cref="Milestone"/>s for the specified <see cref="Project"/>.
+        /// </summary>
+        /// <param name="projectId">
+        /// An ID of the <see cref="Project"/> to retrieve the <see cref="Milestone"/>s for.
+        /// </param>
+        /// <returns>The list of the <see cref="Project"/>'s <see cref="Milestone"/>s.</returns>
+        [OperationContract]
+        List<Milestone> MilestoneListByProject(int projectId);
 
         [OperationContract]
         List<Milestone> MilestoneListByProjectForTimeEntryByProjectReport(int projectId);
-		/// <summary>
-		/// Reatrives a <see cref="Milestone"/> by a specified ID.
-		/// </summary>
-		/// <param name="milestoneId">The ID of the <see cref="Milestone"/> to be retrieved.</param>
-		/// <returns>The <see cref="Milestone"/> object if found and null otherwise.</returns>
-		[OperationContract]
-		Milestone GetMilestoneDetail(int milestoneId);
+        /// <summary>
+        /// Reatrives a <see cref="Milestone"/> by a specified ID.
+        /// </summary>
+        /// <param name="milestoneId">The ID of the <see cref="Milestone"/> to be retrieved.</param>
+        /// <returns>The <see cref="Milestone"/> object if found and null otherwise.</returns>
+        [OperationContract]
+        Milestone GetMilestoneDetail(int milestoneId);
 
-		/// <summary>
-		/// Saves a <see cref="Milestone"/> to the database.
-		/// </summary>
-		/// <param name="milestone">The <see cref="Milestone"/> to be saved.</param>
-		/// <param name="userName">A current user.</param>
-		/// <returns>An ID of the saved record.</returns>
-		[OperationContract]
-		int SaveMilestoneDetail(Milestone milestone, string userName);
+        /// <summary>
+        /// Saves a <see cref="Milestone"/> to the database.
+        /// </summary>
+        /// <param name="milestone">The <see cref="Milestone"/> to be saved.</param>
+        /// <param name="userName">A current user.</param>
+        /// <returns>An ID of the saved record.</returns>
+        [OperationContract]
+        int SaveMilestoneDetail(Milestone milestone, MilestoneUpdateObject milestoneUpdateObj, string userName);
 
-		/// <summary>
-		/// Deletes a <see cref="Milestone"/> from the database.
-		/// </summary>
-		/// <param name="milestone">The <see cref="Milestone"/> to be deleted.</param>
-		/// <param name="userName">A current user.</param>
-		[OperationContract]
-		void DeleteMilestone(Milestone milestone, string userName);
+        /// <summary>
+        /// Deletes a <see cref="Milestone"/> from the database.
+        /// </summary>
+        /// <param name="milestone">The <see cref="Milestone"/> to be deleted.</param>
+        /// <param name="userName">A current user.</param>
+        [OperationContract]
+        void DeleteMilestone(Milestone milestone, string userName);
 
-		/// <summary>
-		/// Moves the specified milestone and optionally future milestones forward and backward.
-		/// </summary>
-		/// <param name="milestoneId">An ID of the <see cref="Milestone"/> to be moved.</param>
-		/// <param name="shiftDays">A number of days to move.</param>
-		/// <param name="moveFutureMilestones">Determines whether future milestones must be moved too.</param>
-		[OperationContract]
-		void MilestoneMove(int milestoneId, int shiftDays, bool moveFutureMilestones);
+        /// <summary>
+        /// Moves the specified milestone and optionally future milestones forward and backward.
+        /// </summary>
+        /// <param name="milestoneId">An ID of the <see cref="Milestone"/> to be moved.</param>
+        /// <param name="shiftDays">A number of days to move.</param>
+        /// <param name="moveFutureMilestones">Determines whether future milestones must be moved too.</param>
+        [OperationContract]
+        void MilestoneMove(int milestoneId, int shiftDays, bool moveFutureMilestones);
 
-	    /// <summary>
-	    /// Moves the specified milestone end date forward and backward.
-	    /// </summary>
-	    /// <param name="milestoneId">An ID of the <see cref="Milestone"/> to be moved.</param>
-	    /// <param name="milestonePersonId"></param>
-	    /// <param name="shiftDays">A number of days to move.</param>
-	    [OperationContract]
+        /// <summary>
+        /// Moves the specified milestone end date forward and backward.
+        /// </summary>
+        /// <param name="milestoneId">An ID of the <see cref="Milestone"/> to be moved.</param>
+        /// <param name="milestonePersonId"></param>
+        /// <param name="shiftDays">A number of days to move.</param>
+        [OperationContract]
         void MilestoneMoveEnd(int milestoneId, int milestonePersonId, int shiftDays);
 
-		/// <summary>
-		/// Clones a specified milestones and set a specified duration to a new one.
-		/// </summary>
-		/// <param name="milestoneId">An ID of the milestone to be cloned.</param>
-		/// <param name="cloneDuration">A clone's duration.</param>
-		/// <returns>An ID of a new milestone.</returns>
-		[OperationContract]
-		int MilestoneClone(int milestoneId, int cloneDuration);
+        /// <summary>
+        /// Clones a specified milestones and set a specified duration to a new one.
+        /// </summary>
+        /// <param name="milestoneId">An ID of the milestone to be cloned.</param>
+        /// <param name="cloneDuration">A clone's duration.</param>
+        /// <returns>An ID of a new milestone.</returns>
+        [OperationContract]
+        int MilestoneClone(int milestoneId, int cloneDuration);
 
         /// <summary>
         /// Reatrives a <see cref="Milestone"/> by a specified ID without financial and entries info
@@ -98,11 +98,11 @@ namespace PracticeManagementService
         [OperationContract]
         Milestone GetMilestoneById(int milestoneId);
 
-	    /// <summary>
-	    /// Get's milestone data without person entries
-	    /// </summary>
-	    /// <param name="milestoneId">Milestone Id</param>
-	    /// <returns></returns>
+        /// <summary>
+        /// Get's milestone data without person entries
+        /// </summary>
+        /// <param name="milestoneId">Milestone Id</param>
+        /// <returns></returns>
         [OperationContract]
         Milestone GetMilestoneDataById(int milestoneId);
 
@@ -113,24 +113,24 @@ namespace PracticeManagementService
         [OperationContract]
         bool CanMoveFutureMilestones(int milestoneId, int shiftDays);
 
-	    /// <summary>
-	    /// Insert entity to the database
-	    /// </summary>
-	    /// <param name="entity">Entity instance</param>
-	    /// <returns>Id of the inserted entity</returns>
+        /// <summary>
+        /// Insert entity to the database
+        /// </summary>
+        /// <param name="entity">Entity instance</param>
+        /// <returns>Id of the inserted entity</returns>
         [OperationContract]
         int AddProjectExpense(ProjectExpense entity);
 
-	    /// <summary>
-	    /// Remove entity from the database
-	    /// </summary>
-	    /// <param name="entity">Entity instance</param>
+        /// <summary>
+        /// Remove entity from the database
+        /// </summary>
+        /// <param name="entity">Entity instance</param>
         [OperationContract]
         void RemoveProjectExpense(ProjectExpense entity);
 
-	    /// <summary>
-	    /// Update entity in the database
-	    /// </summary>
+        /// <summary>
+        /// Update entity in the database
+        /// </summary>
         [OperationContract]
         void UpdateProjectExpense(ProjectExpense entity);
 
@@ -158,21 +158,21 @@ namespace PracticeManagementService
         [OperationContract]
         void NoteDelete(int noteId);
 
-	    /// <summary>
-	    /// Get entity by Id
-	    /// </summary>
-	    /// <param name="id">Id of the entity</param>
-	    /// <returns>Entity instance</returns>
+        /// <summary>
+        /// Get entity by Id
+        /// </summary>
+        /// <param name="id">Id of the entity</param>
+        /// <returns>Entity instance</returns>
         [OperationContract]
         ProjectExpense GetProjectExpenseById(int id);
 
-	    /// <summary>
-	    /// Get entity by Id
-	    /// </summary>
-	    /// <param name="entity">Entity instance</param>
-	    /// <returns>Entity instance</returns>
+        /// <summary>
+        /// Get entity by Id
+        /// </summary>
+        /// <param name="entity">Entity instance</param>
+        /// <returns>Entity instance</returns>
         [OperationContract]
         ProjectExpense[] GetProjectExpensesForMilestone(ProjectExpense entity);
-	}
+    }
 }
 
