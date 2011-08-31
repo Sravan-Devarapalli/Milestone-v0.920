6e0938f5d88da543e8e27eb2eaf79ad9154b7064
ThulasiramP <ThulasiramP>
2011-08-31 01:29:00 -0500

    Comments - Defect Number: 2773; Defect RecordID: 2769; Server Address: services.logic2020.com; Server Port: 99; Database Name: Practice Management; Defect Summary: Project>Milestone>Detail>Person:  Should not be able to have a person's end date beyond the milestone end date; Defect Description: At present it is able to set a person's end date beyond the milestone end date which is creating problems with revenue reporting. Attempting to do so should result in a message prompt to the user that says "You have tried to enter an end date for a person that is beyond the milestone end date. Please either extend the milestone end date or choose an earlier end date for the person." Can that be a pop up message that just has an OK button (no cancel)? I think these kinds of user prompts/messages can be more effective than an error note that just shows up on the page.

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/DataTransferObjects/MilestoneUpdateObject.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+
+namespace DataTransferObjects
+{
+    [DataContract]
+    [Serializable]
+    public class MilestoneUpdateObject
+    {
+        [DataMember]
+        public bool? IsStartDateChangeReflectedForMilestoneAndPersons { get; set; }
+
+        [DataMember]
+        public bool? IsEndDateChangeReflectedForMilestoneAndPersons { get; set; }
+    }
+}
+
