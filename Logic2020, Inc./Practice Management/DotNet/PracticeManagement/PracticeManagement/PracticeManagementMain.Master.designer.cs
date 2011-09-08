e2b62e49d8b8c89c4e9bb1063755356ed15eb352
SrinivasM <SrinivasM>
2011-09-08 08:44:00 -0500

    Comments - Added Email Support link at the bottom of every Page.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/PracticeManagementMain.Master.designer.cs
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/PracticeManagementMain.Master.designer.cs
@@ -326,6 +326,15 @@ namespace PraticeManagement {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.ContentPlaceHolder footer;
+        
+        /// <summary>
+        /// emailSupportMailToLink control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlAnchor emailSupportMailToLink;
     }
 }
 
