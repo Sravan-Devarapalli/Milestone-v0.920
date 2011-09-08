dff1124d416579e16e5c96459e4b5db6dbbaa683
SrinivasM <SrinivasM>
2011-09-08 08:44:00 -0500

    Comments - Added Email Support link at the bottom of every Page.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/LoggedOut.aspx.cs
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/LoggedOut.aspx.cs
@@ -11,7 +11,7 @@ namespace PraticeManagement
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            ((PracticeManagementMain)Master)._PageTitle = "Logged Out";
         }
     }
 }
