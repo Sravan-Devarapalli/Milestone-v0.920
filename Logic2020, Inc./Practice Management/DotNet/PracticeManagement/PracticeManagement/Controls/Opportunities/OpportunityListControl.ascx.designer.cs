10b79872d96ce60019e10e8d57b7dfe99a5fd549
ThulasiramP <ThulasiramP>
2011-09-12 11:19:00 -0500

    Comments - Defect Number: 2886; Defect RecordID: 2882; Server Address: services.logic2020.com; Server Port: 99; Database Name: Practice Management; Defect Summary: Opportunities>Discussion Review 2: Add Oppty description as hover for Oppty Name; Defect Description: In the same way hovering over a Project name from the Project Summary page shows some data, is it possible to have a hover/flyout for the Oppty name that shows the Oppty description that has been entered without affecting page performance?
    It could be limited to the first ## of characters from the entered description so that the flyout size can be controlled.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/Controls/Opportunities/OpportunityListControl.ascx.designer.cs
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/Controls/Opportunities/OpportunityListControl.ascx.designer.cs
@@ -22,6 +22,24 @@ namespace PraticeManagement.Controls.Opportunities {
         protected global::System.Web.UI.UpdatePanel UpdatePanel1;
         
         /// <summary>
+        /// oppNameToolTipHolder control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Panel oppNameToolTipHolder;
+        
+        /// <summary>
+        /// lbloppNameTooltipContent control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lbloppNameTooltipContent;
+        
+        /// <summary>
         /// lblOpportunitiesCount control.
         /// </summary>
         /// <remarks>
