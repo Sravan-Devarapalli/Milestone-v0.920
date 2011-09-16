dbe6035bd5edc27cab80e8fc3f1bfd143e55cca4
SrinivasM <SrinivasM>
2011-09-16 07:00:00 -0500

    Comments - Allowed project salesperson same resource editing rights as project owner.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/IsUserIsOwnerOfProject.proc.sql
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/IsUserIsOwnerOfProject.proc.sql
@@ -6,7 +6,8 @@
 )
 AS
 	DECLARE @PersonId INT
-	DECLARE @OwnerId INT
+	DECLARE @OwnerId INT,
+			@IsSalesPerson INT--as per #2914.
 	
 	SELECT @PersonId = PersonId
 	FROM Person
@@ -14,18 +15,30 @@ AS
 	
 	IF(@IsProjectId = 1)
 	BEGIN
-	SELECT @OwnerId = proj.ProjectManagerId  
-	FROM v_Project AS proj
-	WHERE proj.ProjectId = @Id
+		SELECT @OwnerId = proj.ProjectManagerId  
+		FROM v_Project AS proj
+		WHERE proj.ProjectId = @Id
+		IF EXISTS (SELECT 1 FROM dbo.Commission WHERE PersonId = @PersonId AND ProjectId = @Id AND CommissionType = 1)
+			SET @IsSalesPerson = 1
+			ELSE 
+			SET @IsSalesPerson = 0
 	END
 	ELSE
 	BEGIN
 		SELECT @OwnerId = milestone.ProjectManagerId 
 		FROM v_Milestone AS milestone
 		WHERE milestone.MilestoneId = @Id
+
+		IF EXISTS (SELECT 1 FROM Milestone M 
+					JOIN  dbo.Commission C ON  M.ProjectId = C.ProjectId
+					 WHERE C.PersonId = @PersonId AND M.MilestoneId = @Id AND C.CommissionType = 1)
+		SET @IsSalesPerson = 1
+		ELSE 
+		SET @IsSalesPerson = 0
+		
 	END
 
-	IF(@PersonId = @OwnerId)
+	IF(@PersonId = @OwnerId OR @IsSalesPerson =1 )
 	BEGIN
 		SELECT 'True'
 	END
