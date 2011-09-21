f691c305d36a409d914728412bdc8b97a71a3f50
SrinivasM <SrinivasM>
2011-09-21 07:44:00 -0500

    Comments - Added Announcements Feature.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementService/IConfigurationService.cs
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementService/IConfigurationService.cs
@@ -69,6 +69,12 @@ namespace PracticeManagementService
 
         [OperationContract]
         void DeleteQuickLinkById(int id);
+
+        [OperationContract]
+        void SaveAnnouncement(string text, string richText);
+            
+        [OperationContract]
+        string GetLatestAnnouncement();
     }
 }
 
