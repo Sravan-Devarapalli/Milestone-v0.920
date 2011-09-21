a88682a62981112ababdf580adcf7d7c384adba7
ThulasiramP <ThulasiramP>
2011-09-21 01:51:00 -0500

    Comments - Defect Number: 2913; Defect RecordID: 2909; Server Address: services.logic2020.com; Server Port: 99; Database Name: Practice Management; Defect Summary: Create role-based dashboard landing pages; Defect Description: Initially we can identify seven role-based dashboards: 1. Consulant Dashboard (applies to Intern, Analyst, Consultant, Sr. Consultant) 2. Manager Dashboard (applies to Manager, Sr. Manager) 3. Business Development Dashboard (applies to Business Developement Manager) 4. Client Director Dashboard (applies to Client Director) 5. Senior Dashboard (applies to Principal, President) 6. Recruiter Dashboard (applies to Recruiter) 7. Admin Dashboard (applies to Admin)
    For now, the dashboard landing page for each should include the Practice Management Announcements section and a Quick Liinks section. Beneath that will be content that is specific to each role-based dashboard. We are still determining what information is best for each, but the idea will be to keep it somewhat high-level and simplified to keep loading times down.
    
    In the Practice Management Annoucements section, this should be a section that an Admin can edit, but will be shown on all dashboards. Is it possible to have an "Edit" button that applies only to an Admin view, along with a "Save" button (maybe this is done in a modal dialog)? When "Edit" is clicked can we have normal editing controls (underline, bold, italics, hyperlink, font size, etc)?
    
    It would also be good for an Admin user to have a dropdown on the page so that they can switch between dashboard pages in order to edit content specific to each, such as the Quick Links. We would need to be able to edit and add/remove links to this section.
    
    Quick Links for each role are as follows:
    1. Time Entry; Request Time Off (e-mail link to person's Line Manager with subject "Time Off Request")
    2. Add Project; Time Entry by Project; Margin Test
    3. Add Opportunity; Discussion Review; Add Project; Margin Test
    4. Add Opportunity; Discussion Review; Add Project; Group by...; Margin Test
    5. Group by...; Discussion Review; Consultant Util Timeline; Margin Test
    6. Discussion Review; Persons; Margin Test
    7. Persons; Activity Log; Become User
    
    Is it also possible to include differerent search boxes in a section above Quick Links? Or, alternatively, one search box with a drop down next to it where the type of search can be chosen (Project/Opportunity/Person, etc) These would appear on only certain dashboards, and be relevant to the role:
    
    1. No search boxes.
    2. Project search box
    3. Opportunity, Project search box
    4. Opportunity, Project search box
    5. Opportunity, Project search box
    6. Persons search box
    7. Opportunity, Project, Persons search box

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/DashBoard.aspx.cs
@@ -0,0 +1,312 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using PraticeManagement.Controls;
+using System.Web.Security;
+using DataTransferObjects;
+
+
+namespace PraticeManagement
+{
+    public partial class DashBoard : PracticeManagementSearchPageBase
+    {
+
+        #region Fields
+
+        private bool _userIsAdministrator;
+        private bool _userIsClientDirector;
+        private bool _userIsConsultant;
+        private bool _userIsPracticeAreaManger;
+        private bool _userIsProjectLead;
+        private bool _userIsRecruiter;
+        private bool _userIsSalesperson;
+
+
+        #endregion
+
+
+        /// <summary>
+        /// Gets a text to be searched for.
+        /// </summary>
+        public override string SearchText
+        {
+            get
+            {
+                return txtSearchText.Text;
+            }
+        }
+
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            // Security
+            InitSecurity();
+
+            if (!IsPostBack)
+            {
+               
+
+                PopulateDashBoardTypeDropDown();
+
+                //search section
+                PopulateSearchSection();
+
+
+                //All QuickLinks List
+                PopulateAddQuickLinks();
+
+                //Quick links
+                PopulateQuickLinksSection();
+
+
+            }
+        }
+
+        private void PopulateQuickLinksSection()
+        {
+            string dashBoardValue = _userIsAdministrator ? DashBoardType.Admin.ToString() :
+                                    _userIsClientDirector ? DashBoardType.ClientDirector.ToString() :
+                                    (_userIsPracticeAreaManger || _userIsProjectLead) ? DashBoardType.Manager.ToString() :
+                                    _userIsRecruiter ? DashBoardType.Recruiter.ToString() :
+                                    _userIsSalesperson ? DashBoardType.BusinessDevelopment.ToString() :
+                                    _userIsConsultant ? DashBoardType.Consulant.ToString() :
+                                    string.Empty;
+            DashBoardType dashBoardtype = (DashBoardType)Enum.Parse(typeof(DashBoardType), _userIsAdministrator ? ddlDashBoardType.SelectedValue : dashBoardValue);
+            List<QuickLinks> qlinks = DataHelper.GetQuickLinksByDashBoardType(dashBoardtype);
+
+            repQuickLinks.DataSource = qlinks;
+            repQuickLinks.DataBind();
+
+            hdnSelectedQuckLinks.Value = string.Empty;
+
+            var indexes = string.Empty;
+
+            for (int i = 0; i < cblQuickLinks.Items.Count; i++)
+            {
+                cblQuickLinks.Items[i].Selected = false;
+                if (qlinks.Any(v => cblQuickLinks.Items[i].Value == v.VirtualPath))
+                {
+                    cblQuickLinks.Items[i].Selected = true;
+                    indexes += i.ToString() + ",";
+                }
+            }
+
+            hdnSelectedQuckLinks.Value = indexes;
+            txtSearchBox.Text = string.Empty;
+
+        }
+
+        private void PopulateDashBoardTypeDropDown()
+        {
+
+            if (!_userIsAdministrator)
+            {
+                ddlDashBoardType.Visible = false;
+                pnlDashBoard.Visible = false;
+            }
+            else
+            {
+                ddlDashBoardType.Items.Clear();
+                ddlDashBoardType.DataSource = Enum.GetNames(typeof(DashBoardType));
+                ddlDashBoardType.DataBind();
+
+                ddlDashBoardType.SelectedValue = DashBoardType.Admin.ToString();
+            }
+        }
+
+        private void PopulateAddQuickLinks()
+        {
+            btnAddQuicklink.Visible = _userIsAdministrator;
+            if (_userIsAdministrator)
+            {
+                SiteMapDataSource smdsMain = new SiteMapDataSource();
+
+                Dictionary<string, string> dicQuickLinks = new Dictionary<string, string>();
+
+                foreach (SiteMapNode childNode in ((System.Web.SiteMapNodeCollection)(smdsMain.Provider.RootNode.ChildNodes)))
+                {
+                    if (childNode.HasChildNodes)
+                    {
+                        AddQuckLinks(childNode.ChildNodes, dicQuickLinks);
+                    }
+                    else
+                    {
+                        if (childNode["VirtualPath"] != null && childNode["QuickLinkTitle"] != null)
+                        {
+                            if (!dicQuickLinks.Any(k => k.Key == childNode["QuickLinkTitle"]))
+                                dicQuickLinks.Add(childNode["QuickLinkTitle"], childNode["VirtualPath"]);
+                        }
+
+                    }
+                }
+
+                cblQuickLinks.DataSource = dicQuickLinks;
+                cblQuickLinks.DataBind();
+            }
+        }
+
+        private void AddQuckLinks(SiteMapNodeCollection smncollection, Dictionary<string, string> dicQuickLinks)
+        {
+            foreach (SiteMapNode childNode in smncollection)
+            {
+                if (childNode.HasChildNodes)
+                {
+                    AddQuckLinks(childNode.ChildNodes, dicQuickLinks);
+                }
+                else
+                {
+                    if (childNode["VirtualPath"] != null && childNode["QuickLinkTitle"] != null)
+                    {
+                        if (!dicQuickLinks.Any(k => k.Key == childNode["QuickLinkTitle"]))
+                            dicQuickLinks.Add(childNode["QuickLinkTitle"], childNode["VirtualPath"]);
+                    }
+                }
+            }
+        }
+
+        private void PopulateSearchSection()
+        {
+            Dictionary<string, string> listOfItems = new Dictionary<string, string>();
+
+            if (_userIsAdministrator)
+            {
+                listOfItems.Add("Project", "Project");
+                listOfItems.Add("Opportunity", "Opportunity");
+                listOfItems.Add("Person", "Person");
+            }
+
+            if (_userIsClientDirector || _userIsSalesperson)//new role
+            {
+                if (!listOfItems.Any(k => k.Key == "Project"))
+                {
+                    listOfItems.Add("Project", "Project");
+                }
+
+                if (!listOfItems.Any(k => k.Key == "Opportunity"))
+                {
+                    listOfItems.Add("Opportunity", "Opportunity");
+                }
+            }
+
+            if (_userIsPracticeAreaManger || _userIsProjectLead)
+            {
+                if (!listOfItems.Any(k => k.Key == "Project"))
+                {
+                    listOfItems.Add("Project", "Project");
+                }
+            }
+
+            if (_userIsRecruiter)
+            {
+                if (!listOfItems.Any(k => k.Key == "Person"))
+                {
+                    listOfItems.Add("Person", "Person");
+                }
+            }
+
+            ddlSearchType.Items.Clear();
+
+            foreach (var item in listOfItems)
+            {
+                ddlSearchType.Items.Add(new ListItem(item.Key, item.Value));
+            }
+            ddlSearchType.SelectedValue = "Project";
+
+            pnlSearchSection.Visible = IsShowSearchSection();
+        }
+
+        private void InitSecurity()
+        {
+            var roles = new List<string>(Roles.GetRolesForUser());
+
+            _userIsAdministrator =
+                roles.Contains(DataTransferObjects.Constants.RoleNames.AdministratorRoleName);
+            _userIsClientDirector =
+            roles.Contains(DataTransferObjects.Constants.RoleNames.DirectorRoleName);
+            _userIsProjectLead =
+                 roles.Contains(DataTransferObjects.Constants.RoleNames.ProjectLead);
+            _userIsSalesperson =
+                roles.Contains(DataTransferObjects.Constants.RoleNames.SalespersonRoleName);
+            _userIsRecruiter =
+                roles.Contains(DataTransferObjects.Constants.RoleNames.RecruiterRoleName);
+            _userIsPracticeAreaManger =
+                roles.Contains(DataTransferObjects.Constants.RoleNames.PracticeManagerRoleName);
+            _userIsConsultant =
+                roles.Contains(DataTransferObjects.Constants.RoleNames.ConsultantRoleName);
+        }
+
+        protected void imgEditAnnouncement_OnClick(object sender, EventArgs e)
+        {
+            imgEditAnnouncement.Visible = pnlHtmlAnnounceMent.Visible = false;
+            pnlEditAnnounceMent.Visible = true;
+
+        }
+
+        protected void ddlDashBoardType_OnSelectedIndexChanged(object sender, EventArgs e)
+        {
+            PopulateQuickLinksSection();
+        }
+
+        protected void imgDeleteQuickLink_OnClick(object sender, EventArgs e)
+        {
+            var imgDelete = sender as ImageButton;
+
+            var quickLinkId = imgDelete.Attributes["QuickLinkId"];
+
+            if (!string.IsNullOrEmpty(quickLinkId))
+            {
+                int id = Convert.ToInt32(quickLinkId);
+                DataHelper.DeleteQuickLinkById(id);
+
+                PopulateQuickLinksSection();
+            }
+
+        }
+
+        protected void btnSaveAnnouncement_OnClick(object sender, EventArgs e)
+        {
+            imgEditAnnouncement.Visible = pnlHtmlAnnounceMent.Visible = true;
+            pnlEditAnnounceMent.Visible = false;
+        }
+
+        protected void btnSaveQuickLinks_OnClick(object sender, EventArgs e)
+        {
+            var names = "";
+            var virtualPaths = "";
+
+            foreach (ListItem listItem in cblQuickLinks.Items)
+            {
+                if (listItem.Selected)
+                {
+                    names += listItem.Text + ",";
+                    virtualPaths += listItem.Value + ",";
+                }
+            }
+
+            DataHelper.SaveQuickLinksForDashBoard(names, virtualPaths, (DashBoardType)Enum.Parse(typeof(DashBoardType), ddlDashBoardType.SelectedValue));
+            PopulateQuickLinksSection();
+
+        }
+
+        protected void repQuickLinks_OnItemDataBound(object sender, RepeaterItemEventArgs e)
+        {
+            if (!_userIsAdministrator)
+            {
+                var imgDelete = e.Item.FindControl("imgDeleteQuickLink") as ImageButton;
+                imgDelete.Visible = false;
+            }
+        }
+
+        protected bool IsShowSearchSection()
+        {
+            var result = _userIsAdministrator || _userIsClientDirector || _userIsPracticeAreaManger || _userIsProjectLead || _userIsRecruiter || _userIsSalesperson;
+
+            return result;
+        }
+
+    }
+}
