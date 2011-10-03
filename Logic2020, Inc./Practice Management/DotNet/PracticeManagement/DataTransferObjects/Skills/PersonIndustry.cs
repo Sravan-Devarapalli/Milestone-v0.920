fc9253a856255191042930ae0e6470e6706e00a4
AshokD <AshokD>
2011-10-03 04:19:00 -0500

    none

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/DataTransferObjects/Skills/PersonIndustry.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+
+namespace DataTransferObjects.Skills
+{
+    [DataContract]
+    [Serializable]
+    public class PersonIndustry
+    {
+        [DataMember]
+        public int? ClientId
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public Person Person { get; set; }
+
+        [DataMember]
+        public LookupBase Industry { get; set; }
+
+        [DataMember]
+        public int YearsExperience { get; set; }
+
+        [DataMember]
+        public int DisplayOrder { get; set; }
+
+        [DataMember]
+        public DateTime? ModificationDate { get; set; }
+
+
+    }
+}
+
