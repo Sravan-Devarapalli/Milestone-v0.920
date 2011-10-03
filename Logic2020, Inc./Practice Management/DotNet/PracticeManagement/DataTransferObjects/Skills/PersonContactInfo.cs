f31077dbc82ba86ad27b36244d96bdfa8ed439ae
AshokD <AshokD>
2011-10-03 04:19:00 -0500

    none

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/DataTransferObjects/Skills/PersonContactInfo.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+
+namespace DataTransferObjects.Skills
+{
+    [DataContract]
+    [Serializable]
+    public class PersonContactInfo
+    {
+        [DataMember]
+        public Person Person
+        {
+            set;
+            get;
+        }
+
+        [DataMember]
+        public string Info
+        {
+            set;
+            get;
+        }
+
+        [DataMember]
+        public int? DisplayOrder
+        {
+            set;
+            get;
+        }
+
+        [DataMember]
+        public DateTime ModificationDate
+        {
+            set;
+            get;
+        }
+
+        [DataMember]
+        public ContactInfoType ContactInfoType
+        {
+            set;
+            get;
+        }
+
+        [DataMember]
+        public int? ClientId
+        {
+            get;
+            set;
+        }
+    }
+}
+
