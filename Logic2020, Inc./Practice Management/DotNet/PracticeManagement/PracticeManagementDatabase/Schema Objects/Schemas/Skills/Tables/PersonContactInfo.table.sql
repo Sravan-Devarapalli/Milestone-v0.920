e62edbe975d5d66b3ebaba228f8b9c6043764ae2
AshokD <AshokD>
2011-10-03 02:41:00 -0500

    none

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/Skills/Tables/PersonContactInfo.table.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [Skills].[PersonContactInfo](
+	[ContactInfoTypeId] [int] NOT NULL,
+	[PersonId] [bigint] NOT NULL,
+	[Info] [nvarchar](max) NULL,
+	[ModificationDate] [datetime] NOT NULL,
+	[TenantId]		[int] NOT NULL,
+ CONSTRAINT [PK_PersonContactInfo] PRIMARY KEY CLUSTERED 
+(
+	[TenantId] ASC,
+	[ContactInfoTypeId] ASC,
+	[PersonId] ASC
+)
+)
