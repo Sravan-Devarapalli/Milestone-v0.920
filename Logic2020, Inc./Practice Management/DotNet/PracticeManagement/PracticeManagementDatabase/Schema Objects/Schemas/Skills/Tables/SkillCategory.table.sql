dac6f357172e41e16681268a3031644e900940c9
AshokD <AshokD>
2011-10-03 02:38:00 -0500

    Comments - Moved all Skills related objects to Practice Management Database project instead of maintaining it as a separate Database file.

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/Skills/Tables/SkillCategory.table.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [Skills].[SkillCategory](
+	[SkillCategoryId] [INT] NOT NULL IDENTITY(1,1),
+	[SkillTypeId] [int] NOT NULL,
+	[Description] [nvarchar](max) NULL,
+	[DisplayOrder] [tinyint] NULL,
+	[TenantId]		[int] NOT NULL,
+ CONSTRAINT [PK_SkillCategory] PRIMARY KEY CLUSTERED 
+(
+	[TenantId] ASC,
+	[SkillCategoryId] ASC
+)
+)
+ 
