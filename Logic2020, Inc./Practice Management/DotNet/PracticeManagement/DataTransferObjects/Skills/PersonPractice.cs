c392d4a54cb2e5636d1bd1bdd650e9459bc15284
AshokD <AshokD>
2011-10-03 04:19:00 -0500

    none

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/DataTransferObjects/Skills/PersonPractice.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+
+namespace DataTransferObjects.Skills
+{
+    [DataContract]
+    [Serializable]
+    public class PersonPractice
+    {
+        [DataMember]
+        public int? ClientId
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public Person Person
+        {
+            set;
+            get;
+        }
+
+        [DataMember]
+        public Practice Pactice
+        {
+            set;
+            get;
+        }
+    }
+}
+
