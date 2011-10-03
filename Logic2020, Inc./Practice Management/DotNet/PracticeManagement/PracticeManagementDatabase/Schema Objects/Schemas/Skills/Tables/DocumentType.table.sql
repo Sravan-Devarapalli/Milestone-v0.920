32cdf5f848895caa1223fada40c56447efd4424e
AshokD <AshokD>
2011-10-03 02:41:00 -0500

    none

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/Skills/Tables/DocumentType.table.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [Skills].[DocumentType](
+	[DocumentTypeId] [int] NOT NULL IDENTITY(1,1),
+	[Description] [nvarchar](max) NULL,
+	[DisplayOrder] [tinyint] NULL,
+	[TenantId]		[int] NOT NULL,
+ CONSTRAINT [PK_DocumentType] PRIMARY KEY CLUSTERED 
+(
+	[TenantId] ASC,
+	[DocumentTypeId] ASC
+)
+)
