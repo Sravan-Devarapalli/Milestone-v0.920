dd7ffad16d437718cdb473ec908d7e2064ca1801
AshokD <AshokD>
2011-10-03 02:41:00 -0500

    none

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/Skills/Tables/Keys/FK_PersonContact_InfoPersonId.fkey.sql
@@ -0,0 +1,5 @@
+﻿ALTER TABLE [Skills].[PersonContactInfo]  WITH CHECK ADD  CONSTRAINT [FK_PersonContact_InfoPersonId] FOREIGN KEY([TenantId],[PersonId])
+REFERENCES [Skills].[Person] ([TenantId],[PersonId])
+GO
+ALTER TABLE [Skills].[PersonContactInfo] CHECK CONSTRAINT [FK_PersonContact_InfoPersonId]
+GO
