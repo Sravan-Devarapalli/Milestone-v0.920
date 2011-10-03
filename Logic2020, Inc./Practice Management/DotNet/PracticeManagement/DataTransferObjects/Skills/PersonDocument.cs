ac3e9224e39cf706679ee4632484d9dd34a63f34
AshokD <AshokD>
2011-10-03 04:19:00 -0500

    none

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/DataTransferObjects/Skills/PersonDocument.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+
+namespace DataTransferObjects.Skills
+{
+    [DataContract]
+    [Serializable]
+    public class PersonDocument
+    {
+        [DataMember]
+        public int? ClientId
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public DocumentType Type
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public Person Person
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public string Url
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public DateTime? ModificationDate
+        {
+            get;
+            set;
+        }
+    }
+}
+
