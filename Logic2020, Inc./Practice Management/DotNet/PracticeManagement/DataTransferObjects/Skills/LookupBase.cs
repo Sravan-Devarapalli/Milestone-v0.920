5f694f20b7d4b2ba2aeeb5723a791c62a255ff9b
AshokD <AshokD>
2011-10-03 04:19:00 -0500

    none

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/DataTransferObjects/Skills/LookupBase.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+namespace DataTransferObjects.Skills
+{
+    [DataContract]
+    [Serializable]
+    public class LookupBase
+    {
+        [DataMember]
+        public int Id { get; set; }
+
+        [DataMember]
+        public string Description { get; set; }
+
+        [DataMember]
+        public int? DisplayOrder { get; set; }
+
+        [DataMember]
+        public int? ClientId { get; set; }
+    }
+}
+
