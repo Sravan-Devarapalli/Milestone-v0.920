27fa7fd2b575be9ad56ca44d73838ee51a8bb923
AshokD <AshokD>
2011-10-03 02:41:00 -0500

    none

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/Skills/Tables/PersonQualification.table.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [Skills].[PersonQualification](
+	[QualificationTypeId] int NOT NULL ,
+	[PersonId] [bigint] NOT NULL,
+	[Info] [nvarchar](max) NULL,
+	[DisplayOrder] [tinyint] NULL,
+	[ModificationDate] [datetime] NOT NULL,
+	[TenantId]		[int] NOT NULL,
+ CONSTRAINT [PK_PersonQualification] PRIMARY KEY CLUSTERED 
+(
+	[TenantId] ASC,
+	[QualificationTypeId] ASC,
+	[PersonId] ASC
+)
+)
