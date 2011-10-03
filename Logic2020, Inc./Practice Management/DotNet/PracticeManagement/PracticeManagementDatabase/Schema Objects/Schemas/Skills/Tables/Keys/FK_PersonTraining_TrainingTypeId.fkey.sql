3ac4b3fa5820f2829d268d3820fd5f38c09b4b5d
AshokD <AshokD>
2011-10-03 02:41:00 -0500

    none

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/Skills/Tables/Keys/FK_PersonTraining_TrainingTypeId.fkey.sql
@@ -0,0 +1,5 @@
+﻿ALTER TABLE [Skills].[PersonTraining]  WITH CHECK ADD  CONSTRAINT [FK_PersonTraining_TrainingTypeId] FOREIGN KEY([TenantId],[TrainingTypeId])
+REFERENCES [Skills].[TrainingType] ([TenantId],[TrainingTypeId])
+ON UPDATE CASCADE
+GO
+ALTER TABLE [Skills].[PersonTraining] CHECK CONSTRAINT [FK_PersonTraining_TrainingTypeId]
