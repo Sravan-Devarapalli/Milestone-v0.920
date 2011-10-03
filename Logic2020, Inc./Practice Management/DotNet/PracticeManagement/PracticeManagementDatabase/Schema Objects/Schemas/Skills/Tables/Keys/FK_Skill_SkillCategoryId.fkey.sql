d08dea39a484bbe3444f0463740adbdec957ba9e
AshokD <AshokD>
2011-10-03 02:41:00 -0500

    none

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/Skills/Tables/Keys/FK_Skill_SkillCategoryId.fkey.sql
@@ -0,0 +1,5 @@
+﻿ALTER TABLE [Skills].[Skill]  WITH CHECK ADD  CONSTRAINT [FK_Skill_SkillCategoryId] FOREIGN KEY([TenantId],[SkillCategoryId])
+REFERENCES [Skills].[SkillCategory] ([TenantId],[SkillCategoryId])
+ON UPDATE CASCADE
+GO
+ALTER TABLE [Skills].[Skill] CHECK CONSTRAINT [FK_Skill_SkillCategoryId]
