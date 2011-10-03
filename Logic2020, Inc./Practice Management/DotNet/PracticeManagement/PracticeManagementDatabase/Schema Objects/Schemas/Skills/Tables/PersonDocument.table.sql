7f6163eb7402a115c24faee4deba2973647e0055
AshokD <AshokD>
2011-10-03 02:41:00 -0500

    none

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/Skills/Tables/PersonDocument.table.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [Skills].[PersonDocument](
+	[DocumentTypeId] [int] NOT NULL,
+	[PersonId] [bigint] NOT NULL,
+	[Url] [nvarchar](max) NOT NULL,
+	[ModificationDate] [datetime] NOT NULL,
+	[TenantId]		[int] NOT NULL,
+ CONSTRAINT [PK_PersonDocument] PRIMARY KEY CLUSTERED 
+(
+	[TenantId] ASC,
+	[DocumentTypeId] ASC,
+	[PersonId] ASC
+)
+)
