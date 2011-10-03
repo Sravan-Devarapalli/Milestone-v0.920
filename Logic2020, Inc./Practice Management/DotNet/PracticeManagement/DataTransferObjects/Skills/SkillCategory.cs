50e2b4d30e7f68d46b7cda8927bd5039a8e34f52
AshokD <AshokD>
2011-10-03 04:19:00 -0500

    none

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/DataTransferObjects/Skills/SkillCategory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+
+namespace DataTransferObjects.Skills
+{
+    [DataContract]
+    [Serializable]
+    public class SkillCategory : LookupBase
+    {
+        [DataMember]
+        public LookupBase SkillType { get; set; }
+    }
+}
+
