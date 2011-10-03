e9c0d17e765f03d746e531a34e74f18317fd38d2
AshokD <AshokD>
2011-10-03 04:19:00 -0500

    none

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/DataTransferObjects/Skills/PersonSkill.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+
+namespace DataTransferObjects.Skills
+{
+    [DataContract]
+    [Serializable]
+    public class PersonSkill
+    {
+        [DataMember]
+        public int? ClientId
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public Person Person
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public Skill Skill
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public SkillLevel SkillLevel
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public int? YearsExperience
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public DateTime LastUsed
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public int? DisplayOrder
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public DateTime ModificationDate
+        {
+            get;
+            set;
+        }
+    }
+}
+
