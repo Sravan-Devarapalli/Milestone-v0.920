4743fd746255c31229df90d9c3ff5f505b421be3
SrinivasM <SrinivasM>
2011-10-05 09:17:00 -0500

    Comments - Defect Number: 2930; Defect RecordID: 2926; Server Address: services.logic2020.com; Server Port: 99; Database Name: Practice Management; Defect Summary: Reports>Time>Time Entry by Project: Change to filter logic; Defect Description: FIRST PART: Currently several areas in PM use filter logic that depends on what "Project/Opportunity Permissions" are selected per person in Config Resources>Resources>Person. This is flawed because it is not efficient and does not allow us to give persons access to specific, relevant projects without giving them access to other projects under the same grouping.
    We can begin with Time Entry by Project to change this. For persons with Admin or Senior Leadership roles the page should continue to show all data it currently does.
    
    For all other roles, the page should determine what clients and projects are relevant based on the relationship of the person that is logged in to projects, based on a few key fields in the project.
    
    The key fields are Client Director, Salesperson, and Project Manager(s) (previously called "Owner"). If the logged in user is assigned to any of those roles on a project, then the client dropdown in the Time Entry by Project page should have the Client for that Project available.
    
    The report output should appear as it does now, and allow the user to see all time and notes entered by the project/milestone resources.
    
    SECOND PART:
    Add a checkbox beneath the Project dropdown selector for "Active and Internal Projects only" that is checked by default. When unchecked this would allow the user to see Completed projects as well. Here's a reference:

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/ClientListAll.proc.sql
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/ClientListAll.proc.sql
@@ -5,43 +5,8 @@ AS
 BEGIN
 	SET NOCOUNT ON;
 	
-	-- Table variable to store list of Clients that owner is allowed to see	
-	DECLARE @OwnerProjectClientList TABLE(
-		ClientId INT NULL -- As per #2890
-	)
-	
-	DECLARE @ClientPermissions TABLE(
-		ClientId INT NULL
-	)
-	-- Populate is with the data from the permissions table
-	--		TargetType = 1 means that we are looking for the clients in the permissions table
-	INSERT INTO @ClientPermissions (
-		ClientId
-	) SELECT prm.TargetId FROM dbo.Permission AS prm WHERE prm.PersonId = @PersonId AND prm.TargetType = 1
-	
-	-- If there are nulls in permission table, it means that eveything is allowed to be seen,
-	--		so set @PersonId to NULL which will extract all records from the table
-	DECLARE @NullsNumber INT
-	SELECT @NullsNumber = COUNT(*) FROM @ClientPermissions AS sp WHERE sp.ClientId IS NULL 	
-	IF @NullsNumber > 0 
-	BEGIN
-		SET @PersonId = NULL
-	END
-	ELSE
-	BEGIN
-		-- Populate is with the data from the v_Project 
-		INSERT INTO @OwnerProjectClientList (ClientId) 
-		SELECT proj.ClientId 
-		FROM dbo.v_Project AS proj
-		LEFT JOIN dbo.Commission C ON C.ProjectId = proj.ProjectId AND C.CommissionType = 1
-		WHERE proj.ProjectManagerId = @PersonId OR C.PersonId = @PersonId -- Adding Salesperson - Project clients into the list.
-
-	END
-
-	IF @ShowAll = 0
-	BEGIN
-		SELECT 
-				  c.ClientId
+	--As per #2930, Not using Person Detail page Permissions.
+	SELECT distinct C.ClientId
 				, c.DefaultDiscount
 				, c.DefaultTerms
 				, c.DefaultSalespersonId
@@ -49,33 +14,18 @@ BEGIN
 				, c.[Name]
 				, c.Inactive
 				, c.IsChargeable
-			FROM Client AS c
-			WHERE Inactive = 0
-				AND (
-					@PersonId IS NULL 
-					OR ClientId IN (SELECT cp.ClientId FROM @ClientPermissions AS cp)
-					OR ClientId IN (SELECT opc.ClientId FROM @OwnerProjectClientList AS opc)
-				)
-			ORDER BY c.[Name]
-	END
-	ELSE
-	BEGIN
-		SELECT 
-				  ClientId
-				, DefaultDiscount
-				, DefaultTerms
-				, DefaultSalespersonId
-				, DefaultDirectorID
-				, [Name]
-				, Inactive
-				, IsChargeable
-			FROM Client
-			WHERE (
-				@PersonId IS NULL 
-				OR ClientId IN (SELECT cp.ClientId FROM @ClientPermissions AS cp)
-				OR ClientId IN (SELECT opc.ClientId FROM @OwnerProjectClientList AS opc)
-			)
-			ORDER BY [Name]
-	END
+	FROM Client C
+	JOIN Project P ON P.ClientId = C.ClientId
+	LEFT JOIN Person CD ON CD.PersonId = p.DirectorId
+	LEFT JOIN Commission Cm ON Cm.ProjectId = p.ProjectId AND Cm.CommissionType = 1
+	LEFT JOIN Person SP ON SP.PersonId = Cm.PersonId
+	LEFT JOIN Person PM ON PM.PersonId = p.ProjectManagerId
+	WHERE ((@ShowAll = 0 AND C.Inactive = 0) OR @ShowAll <> 0)
+	AND	(@PersonId IS null
+		OR CD.PersonId = @PersonId 
+		OR SP.PersonId = @PersonId
+		OR PM.PersonId = @PersonId
+		)
+	ORDER BY C.Name
 END
 
