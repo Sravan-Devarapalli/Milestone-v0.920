bd9e6b59b5d61ad9f74e594ff5abd0773f15dec6
AshokD <AshokD>
2011-10-10 05:09:00 -0500

    Comments - 1.Removed TabnantId from all the tables. 2.Added columns IsActive and IsDeleted to all the master tables. 3. Removed Title, Practice tables from Skills Schema as the PM tables Seniority and Practice  respectively can be used alternatively.

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/Skills/Programmability/Stored Procedures/GetSkillLevelsAll.proc.sql
@@ -0,0 +1,9 @@
+﻿CREATE PROCEDURE [dbo].[GetSkillLevelsAll]
+	 
+AS
+	SELECT S.[Description] SkillLevelName,
+		   S.[SkillLevelId],
+		   S.DisplayOrder
+	FROM Skills.SkillLevel S
+	WHERE IsActive = 1
+	 
