2243b54d381f19fe56586790f1185cbce0f75f3a
AshokD <AshokD>
2011-10-10 05:51:00 -0500

    Comments - 1.As the person table in skills is replaced with Person Table in PM, the DTO in skills also no longer be used and modified Person DTO in PM to support Skills features. 2. Also the DTOs Practice and Titile are removed from Skills.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/DataTransferObjects/Skills/PersonIndustry.cs
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/DataTransferObjects/Skills/PersonIndustry.cs
@@ -10,12 +10,7 @@ namespace DataTransferObjects.Skills
     [Serializable]
     public class PersonIndustry
     {
-        [DataMember]
-        public int? ClientId
-        {
-            get;
-            set;
-        }
+       
 
         [DataMember]
         public Person Person { get; set; }
