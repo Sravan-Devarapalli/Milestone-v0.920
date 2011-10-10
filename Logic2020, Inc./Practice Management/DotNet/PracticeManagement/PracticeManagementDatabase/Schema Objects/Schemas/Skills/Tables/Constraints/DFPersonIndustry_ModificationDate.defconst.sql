fa6d444db283d6eca0d5e6036185e48a5a6af2a5
AshokD <AshokD>
2011-10-10 05:09:00 -0500

    Comments - 1.Removed TabnantId from all the tables. 2.Added columns IsActive and IsDeleted to all the master tables. 3. Removed Title, Practice tables from Skills Schema as the PM tables Seniority and Practice  respectively can be used alternatively.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/Skills/Tables/Constraints/DFPersonIndustry_ModificationDate.defconst.sql
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/Skills/Tables/Constraints/DFPersonIndustry_ModificationDate.defconst.sql
@@ -1,2 +1,2 @@
 ﻿ALTER TABLE [Skills].[PersonIndustry]
-ADD  CONSTRAINT [DFPersonIndustry_ModificationDate]  DEFAULT (getdate()) FOR [ModificationDate]
+ADD  CONSTRAINT [DFPersonIndustry_ModificationDate]  DEFAULT (getdate()) FOR [ModifiedDate]
