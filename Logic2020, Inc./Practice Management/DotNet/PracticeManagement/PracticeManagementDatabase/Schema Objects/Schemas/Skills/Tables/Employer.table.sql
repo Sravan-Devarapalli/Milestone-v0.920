ac88e405fc230508e1193aee66bbdd64f84b3998
AshokD <AshokD>
2011-10-10 05:09:00 -0500

    Comments - 1.Removed TabnantId from all the tables. 2.Added columns IsActive and IsDeleted to all the master tables. 3. Removed Title, Practice tables from Skills Schema as the PM tables Seniority and Practice  respectively can be used alternatively.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/Skills/Tables/Employer.table.sql
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/Skills/Tables/Employer.table.sql
@@ -2,10 +2,10 @@
 	[EmployerId] [int] NOT NULL IDENTITY(1,1),
 	[Description] [nvarchar](max) NULL,
 	[DisplayOrder] [tinyint] NULL,
-	[TenantId]		[int] NOT NULL,
+	[IsActive]		[bit] NOT NULL CONSTRAINT DF_Employer_IsActive DEFAULT(1),
+	[IsDeleted]		[bit] NOT NULL CONSTRAINT DF_Employer_IsDeleted DEFAULT(0),
  CONSTRAINT [PK_Employer] PRIMARY KEY CLUSTERED 
 (
-	[TenantId] ASC,
 	[EmployerId] ASC
 )
 )
