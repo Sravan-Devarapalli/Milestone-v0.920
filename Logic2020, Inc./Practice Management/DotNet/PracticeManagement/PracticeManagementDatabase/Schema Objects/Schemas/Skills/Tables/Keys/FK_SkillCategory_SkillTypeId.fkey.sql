6aa06b9cb06ca2939e0768fe4684317a704ac1b1
AshokD <AshokD>
2011-10-10 05:09:00 -0500

    Comments - 1.Removed TabnantId from all the tables. 2.Added columns IsActive and IsDeleted to all the master tables. 3. Removed Title, Practice tables from Skills Schema as the PM tables Seniority and Practice  respectively can be used alternatively.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/Skills/Tables/Keys/FK_SkillCategory_SkillTypeId.fkey.sql
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/Skills/Tables/Keys/FK_SkillCategory_SkillTypeId.fkey.sql
@@ -1,5 +1,5 @@
-﻿ALTER TABLE [Skills].[SkillCategory]  WITH CHECK ADD  CONSTRAINT [FK_SkillCategory_SkillTypeId] FOREIGN KEY([TenantId],[SkillTypeId])
-REFERENCES [Skills].[SkillType] ([TenantId],[SkillTypeId])
+﻿ALTER TABLE [Skills].[SkillCategory]  WITH CHECK ADD  CONSTRAINT [FK_SkillCategory_SkillTypeId] FOREIGN KEY([SkillTypeId])
+REFERENCES [Skills].[SkillType] ([SkillTypeId])
 ON UPDATE CASCADE
 GO
 ALTER TABLE [Skills].[SkillCategory] CHECK CONSTRAINT [FK_SkillCategory_SkillTypeId]
