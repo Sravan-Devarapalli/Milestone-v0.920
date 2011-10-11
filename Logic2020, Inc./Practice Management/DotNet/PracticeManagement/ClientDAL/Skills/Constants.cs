8786d6632edc131d7e3f0e71073872a2e8ee51f6
AshokD <AshokD>
2011-10-11 07:43:00 -0500

    none

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/ClientDAL/Skills/Constants.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace DataAccess.Skills
+{
+    public class Constants
+    {
+        #region "Column Names"
+        public class ColumnNames
+        {
+            public const string DisplayOrder = "DisplayOrder";
+            public const string SkillCategoryId = "SkillCategoryId";
+            public const string SkillTypeId = "SkillTypeId";
+            public const string SkillLevelId = "SkillLevelId";
+            public const string SkillId = "SkillId";
+            public const string SkillCategoryName = "SkillCategoryName";
+            public const string SkillLevelName = "SkillLevelName";            
+            public const string SkillName = "SkillName";
+        }
+
+        public class FunctionNames
+        {
+        }
+
+        public class ParameterNames
+        {
+
+            public const string TenantId = "@TenantId";
+
+        }
+
+        public class ProcedureNames
+        {
+            public const string GetSkillCategoriesAll = "Skills.GetSkillCategoriesAll";
+            public const string GetSkillLevelsAll = "Skills.GetSkillLevelsAll";
+            public const string GetSkillsAll = "Skills.GetSkillsAll";
+        }
+
+        #endregion
+    }
+}
+
