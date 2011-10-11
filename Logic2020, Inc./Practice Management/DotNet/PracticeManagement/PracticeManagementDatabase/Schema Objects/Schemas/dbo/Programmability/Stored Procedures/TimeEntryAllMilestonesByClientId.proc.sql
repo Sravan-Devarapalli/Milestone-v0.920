93bb23578091bcda33916ef282fa5967a4a97bfb
SrinivasM <SrinivasM>
2011-10-11 08:01:00 -0500

    Comments - Defect Number: 2930; Defect RecordID: 2926; Server Address: services.logic2020.com; Server Port: 99; Database Name: Practice Management; Defect Summary: Reports>Time>Time Entry by Project: Change to filter logic; Defect Description: FIRST PART: Currently several areas in PM use filter logic that depends on what "Project/Opportunity Permissions" are selected per person in Config Resources>Resources>Person. This is flawed because it is not efficient and does not allow us to give persons access to specific, relevant projects without giving them access to other projects under the same grouping.
    We can begin with Time Entry by Project to change this. For persons with Admin or Senior Leadership roles the page should continue to show all data it currently does.
    
    For all other roles, the page should determine what clients and projects are relevant based on the relationship of the person that is logged in to projects, based on a few key fields in the project.
    
    The key fields are Client Director, Salesperson, and Project Manager(s) (previously called "Owner"). If the logged in user is assigned to any of those roles on a project, then the client dropdown in the Time Entry by Project page should have the Client for that Project available.
    
    The report output should appear as it does now, and allow the user to see all time and notes entered by the project/milestone resources.
    
    SECOND PART:
    Add a checkbox beneath the Project dropdown selector for "Active and Internal Projects only" that is checked by default. When unchecked this would allow the user to see Completed projects as well. Here's a reference:

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/TimeEntryAllMilestonesByClientId.proc.sql
@@ -0,0 +1,48 @@
+﻿CREATE  PROCEDURE [dbo].[TimeEntryAllMilestonesByClientId] 
+@ClientId INT = NULL,
+@PersonId INT = NULL,
+@ShowAll BIT = 1
+AS
+BEGIN
+	SET NOCOUNT ON;
+	
+        SELECT  DISTINCT m.MilestoneId,
+                m.Description AS 'MilestoneName',
+                proj.ProjectId,
+                proj.[Name] AS 'ProjectName',
+				proj.ProjectNumber,
+				proj.ClientId
+        FROM    TimeEntries AS te
+                INNER JOIN dbo.MilestonePerson AS mp ON mp.MilestonePersonId = te.MilestonePersonId
+                INNER JOIN dbo.MilestonePersonEntry AS mpe ON mp.MilestonePersonId = mpe.MilestonePersonId
+                INNER JOIN dbo.Milestone AS m ON m.MilestoneId = mp.MilestoneId
+                INNER JOIN dbo.Project AS proj ON proj.ProjectId = m.ProjectId
+				LEFT JOIN dbo.Commission AS C ON C.ProjectId = proj.ProjectId AND C.CommissionType = 1 --1 is SalesCommission
+        WHERE   (proj.ClientId =  @ClientId  OR @ClientId IS NULL)
+				AND (
+						@PersonId IS NULL
+						OR proj.ProjectManagerId = @PersonId
+						OR proj.DirectorId = @PersonId
+						OR C.PersonId = @PersonId
+					)
+				AND (@ShowAll = 1 OR (@ShowAll = 0 AND (proj.ProjectStatusId = 3 or proj.ProjectStatusId = 6) ) )
+		UNION
+        SELECT M.MilestoneId,
+				M.Description AS 'MilestoneName',
+				P.ProjectId,
+				P.Name AS 'ProjectName',
+				P.ProjectNumber,
+				P.ClientId
+        FROM dbo.Project P
+        JOIN Milestone M ON M.ProjectId = P.ProjectId
+		LEFT JOIN dbo.Commission AS C ON C.ProjectId = P.ProjectId AND C.CommissionType = 1 --1 is SalesCommission
+        WHERE (@ClientId IS NULL OR P.ClientId = @ClientId)
+				AND (
+						@PersonId IS NULL
+						OR P.ProjectManagerId = @PersonId
+						OR P.DirectorId = @PersonId
+						OR C.PersonId = @PersonId
+					)
+              AND (@ShowAll = 1 OR (@ShowAll = 0 AND (P.ProjectStatusId = 3 or P.ProjectStatusId = 6) ) )
+        
+END
