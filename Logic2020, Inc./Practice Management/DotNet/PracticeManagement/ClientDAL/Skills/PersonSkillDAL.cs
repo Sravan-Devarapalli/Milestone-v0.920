adfd96820dd274816481667c1d17783437750b1f
AshokD <AshokD>
2011-10-11 07:43:00 -0500

    none

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/ClientDAL/Skills/PersonSkillDAL.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.SqlClient;
+using System.Data;
+using DataTransferObjects.Skills;
+using DataAccess.Other;
+
+namespace DataAccess.Skills
+{
+    public static class PersonSkillDAL
+    {
+        public static List<Skill> GetSkillsAll()
+        {
+            var skills = new List<Skill>();
+            using (var connection = new SqlConnection(DataSourceHelper.DataConnection))
+            {
+                using (var command = new SqlCommand(Constants.ProcedureNames.GetSkillsAll, connection))
+                {
+                    command.CommandType = CommandType.StoredProcedure;
+                    command.CommandTimeout = connection.ConnectionTimeout;
+                    connection.Open();
+                    var reader = command.ExecuteReader();
+                    ReadSkills(reader, skills);
+                }
+            }
+            return skills;
+        }
+
+        public static List<SkillCategory> GetSkillCategoriesAll()
+        {
+            var skillCategories = new List<SkillCategory>();
+            using (var connection = new SqlConnection(DataSourceHelper.DataConnection))
+            {
+                using (var command = new SqlCommand(Constants.ProcedureNames.GetSkillCategoriesAll, connection))
+                {
+                    command.CommandType = CommandType.StoredProcedure;
+                    command.CommandTimeout = connection.ConnectionTimeout;
+                    connection.Open();
+                    var reader = command.ExecuteReader();
+                    ReadSkillCategories(reader, skillCategories);
+                }
+            }
+            return skillCategories;
+        }
+
+        public static List<SkillLevel> GetSkillLevelsAll()
+        {
+            var skillLevels = new List<SkillLevel>();
+            using (var connection = new SqlConnection(DataSourceHelper.DataConnection))
+            {
+                using (var command = new SqlCommand(Constants.ProcedureNames.GetSkillLevelsAll, connection))
+                {
+                    command.CommandType = CommandType.StoredProcedure;
+                    command.CommandTimeout = connection.ConnectionTimeout;
+                    connection.Open();
+                    var reader = command.ExecuteReader();
+                    ReadSkillLevels(reader, skillLevels);
+                }
+            }
+            return skillLevels;
+        }
+
+        private static void ReadSkillLevels(SqlDataReader reader, List<SkillLevel> skillLevels)
+        {
+            var skillLevelIdColumn = reader.GetOrdinal(Constants.ColumnNames.SkillLevelId);
+            var skillLevelNameColumn = reader.GetOrdinal(Constants.ColumnNames.SkillLevelName);
+            var displayOrderColumn = reader.GetOrdinal(Constants.ColumnNames.DisplayOrder);
+            if (reader.HasRows)
+            {
+                while (reader.Read())
+                {
+                    var skillLevel = new SkillLevel
+                    {
+                        Id = reader.GetInt32(skillLevelIdColumn),
+                        Description = reader.GetString(skillLevelNameColumn),
+                        DisplayOrder = reader.IsDBNull(displayOrderColumn) ? null : (int?)reader.GetInt32(displayOrderColumn)
+                    };
+                    skillLevels.Add(skillLevel);
+                }
+            }
+        }
+
+        private static void ReadSkillCategories(SqlDataReader reader, List<SkillCategory> skillCategories)
+        {
+            var skillCategoryIdColumn = reader.GetOrdinal(Constants.ColumnNames.SkillCategoryId);
+            var SkillTypeId = reader.GetOrdinal(Constants.ColumnNames.SkillTypeId);
+            var skillCategoryNameColumn = reader.GetOrdinal(Constants.ColumnNames.SkillCategoryName);
+            var displayOrderColumn = reader.GetOrdinal(Constants.ColumnNames.DisplayOrder);
+            if (reader.HasRows)
+            {
+                while (reader.Read())
+                {
+                    var skillCat = new SkillCategory
+                    {
+                        Id = reader.GetInt32(skillCategoryIdColumn),
+                        Description = reader.GetString(skillCategoryNameColumn),
+                        DisplayOrder = reader.IsDBNull(displayOrderColumn) ? null : (int?) reader.GetInt32(displayOrderColumn),
+                        SkillType = new SkillType
+                        {
+                            Id = reader.GetInt32(SkillTypeId)
+                        }
+                    };
+                    skillCategories.Add(skillCat);
+                }
+            }
+        }
+
+        private static void ReadSkills(SqlDataReader reader, List<Skill> skills)
+        {
+            var skillCategoryIdColumn = reader.GetOrdinal(Constants.ColumnNames.SkillCategoryId);
+            var skillIdColumn = reader.GetOrdinal(Constants.ColumnNames.SkillId);
+            var skillNameColumn = reader.GetOrdinal(Constants.ColumnNames.SkillName);
+            var displayOrderColumn = reader.GetOrdinal(Constants.ColumnNames.DisplayOrder);
+            if (reader.HasRows)
+            {
+                while (reader.Read())
+                {
+                    var skill = new Skill
+                        {
+                            Id = reader.GetInt32(skillIdColumn),
+                            Description = reader.GetString(skillNameColumn),
+                            DisplayOrder = reader.GetInt32(displayOrderColumn),
+                            Category = new SkillCategory
+                            {
+                                Id = reader.GetInt32(skillCategoryIdColumn)
+                            }
+                        };
+                    skills.Add(skill);
+                }
+            }
+        }
+
+
+    }
+}
+
