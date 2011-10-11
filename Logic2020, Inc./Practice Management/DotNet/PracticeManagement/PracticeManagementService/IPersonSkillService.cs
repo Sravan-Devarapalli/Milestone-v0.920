5fa4ac4e211e262316dfde617396fc6d0b043316
AshokD <AshokD>
2011-10-11 06:49:00 -0500

    Comments - Added  new page SkillsEntry and it's dependents

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementService/IPersonSkillService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using DataTransferObjects.Skills;
+
+namespace PracticeManagementService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IPersonSkillService" in both code and config file together.
+    [ServiceContract]
+    public interface IPersonSkillService
+    {
+
+        //[OperationContract]
+        //List<SkillType> GetSkillStatuses();
+
+        [OperationContract]
+        List<SkillCategory> SkillCategoriesAll();
+
+        [OperationContract]
+        List<Skill> SkillsAll();
+
+        [OperationContract]
+        List<SkillLevel> SkillLevelsAll();
+
+        //[OperationContract]
+        //List<PersonSkill> PersonSkillsGetBySkillCategory(int SkillCategoryId);
+
+    }
+}
+
