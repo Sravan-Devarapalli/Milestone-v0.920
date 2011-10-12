58545f3444931b5f5cdfa32ca7c9b7cfc949af20
ThulasiramP <ThulasiramP>
2011-10-12 09:36:00 -0500

    Comments - Defect Number: 2929; Defect RecordID: 2925; Server Address: services.logic2020.com; Server Port: 99; Database Name: Practice Management; Defect Summary: Projects: Change "Owner" to "Project Manager(s)"; change dropdown to multi-select; Defect Description: Change "Owner" to "Project Manager(s)" and make the single-select dropdown a multi-select. This should enable multiple persons to have the ability to edit resources on a project if they are selected.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/Controls/Clients/ClientProjects.ascx.cs
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/Controls/Clients/ClientProjects.ascx.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Web.UI.WebControls;
 using PraticeManagement.Configuration;
+using DataTransferObjects;
+using AjaxControlToolkit;
 
 namespace PraticeManagement.Controls.Clients
 {
@@ -13,7 +15,7 @@ namespace PraticeManagement.Controls.Clients
 
         protected void btnProjectName_Command(object sender, CommandEventArgs e)
         {
-            ((PracticeManagementPageBase) Page).Redirect(
+            ((PracticeManagementPageBase)Page).Redirect(
                 string.Format(Constants.ApplicationPages.DetailRedirectFormat,
                                    Constants.ApplicationPages.ProjectDetail,
                                    e.CommandArgument));
@@ -24,6 +26,23 @@ namespace PraticeManagement.Controls.Clients
             var defaultProjectId = MileStoneConfigurationManager.GetProjectId();
             var projectId = Int32.Parse(projectIdObj.ToString());
             return defaultProjectId.HasValue && defaultProjectId.Value == projectId;
+        }
+
+        protected void gvProjects_OnRowDataBound(object sender, GridViewRowEventArgs e)
+        {
+            if (e.Row.RowType == DataControlRowType.DataRow)
+            {
+                var proj = e.Row.DataItem as Project;
+
+                if (proj.ProjectManagers.Count <= 1)
+                {
+                    var control = e.Row.FindControl("cpe") as CollapsiblePanelExtender;
+                    var control2 = e.Row.FindControl("btnExpandCollapseFilter") as Image;
+                    control.Collapsed = false;
+                    control2.Style["display"] = "none";
+                }
+            }
+
         }
     }
 }
