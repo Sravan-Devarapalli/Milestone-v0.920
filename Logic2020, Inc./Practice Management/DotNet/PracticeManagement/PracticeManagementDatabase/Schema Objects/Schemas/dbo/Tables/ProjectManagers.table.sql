d92818434455c789da0866dfba1915b0e72fedae
ThulasiramP <ThulasiramP>
2011-10-12 09:35:00 -0500

    Comments - Defect Number: 2929; Defect RecordID: 2925; Server Address: services.logic2020.com; Server Port: 99; Database Name: Practice Management; Defect Summary: Projects: Change "Owner" to "Project Manager(s)"; change dropdown to multi-select; Defect Description: Change "Owner" to "Project Manager(s)" and make the single-select dropdown a multi-select. This should enable multiple persons to have the ability to edit resources on a project if they are selected.

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/ProjectManagers.table.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[ProjectManagers]
+(
+	Id                      INT IDENTITY(1,1) NOT NULL,
+	ProjectId	            INT NOT NULL, 
+	ProjectManagerId		INT NOT NULL
+	CONSTRAINT PK_ProjectManagers_Id PRIMARY KEY CLUSTERED(Id),
+    CONSTRAINT FK_ProjectManagers_ProjectId FOREIGN KEY(ProjectId) REFERENCES  dbo.Project(ProjectId),
+	CONSTRAINT FK_ProjectManagers_ProjectManagerId FOREIGN KEY(ProjectManagerId) REFERENCES  dbo.Person(PersonId)
+);
