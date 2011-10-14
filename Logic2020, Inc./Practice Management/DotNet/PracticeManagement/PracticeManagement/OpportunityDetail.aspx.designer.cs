37ef14960dad44429914a41cbd70cdd3a875a861
AshokD <AshokD>
2011-10-14 07:39:00 -0500

    Comments - Defect Number: 2928; Defect RecordID: 2924; Server Address: services.logic2020.com; Server Port: 99; Database Name: Practice Management; Defect Summary: Opportunities>Summary:  Repurpose Status light to reflect whether Opportunity is linked to Project instead; Defect Description: The status light that reflects whether an Opportunity is Active, Projected, etc needs to be changed so that it instead reflects whether an Opportunity is linked to a Project. If it is linked, light is green; if it is not linked, the light should just be dark.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/OpportunityDetail.aspx.designer.cs
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/OpportunityDetail.aspx.designer.cs
@@ -67,6 +67,15 @@ namespace PraticeManagement {
         protected global::System.Web.UI.WebControls.Label lblLastUpdate;
         
         /// <summary>
+        /// hpProject control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HyperLink hpProject;
+        
+        /// <summary>
         /// dpStartDate control.
         /// </summary>
         /// <remarks>
