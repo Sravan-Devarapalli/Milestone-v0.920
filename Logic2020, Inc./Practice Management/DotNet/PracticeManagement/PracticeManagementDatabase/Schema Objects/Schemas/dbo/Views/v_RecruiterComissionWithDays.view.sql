846ec5dbd0fd9118b4b2324b6bc29ce13b469585
AshokD <AshokD>
2011-10-14 08:24:00 -0500

    Comments - Defect Number: 2938; Defect RecordID: 2934; Server Address: services.logic2020.com; Server Port: 99; Database Name: Practice Management; Defect Summary: Person>Recruiter:  Change to functionality; Defect Description: 1. Recruiter field should default to "--Select a Person--" unelss logged in user has Recruiter role assigned, then it should use their name and link this to the person record. 2. Can we remove the Recruiter commission line? We're not using it, and until it is better implemented (our mistake), it seems to just be getting in the way.
    The objective here is to have the Recruiter field populated when an export of the Person list is done.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Views/v_RecruiterComissionWithDays.view.sql
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Views/v_RecruiterComissionWithDays.view.sql
@@ -2,7 +2,7 @@
 AS
 WITH Ordered AS (
 	SELECT
-			ROW_NUMBER() OVER (ORDER BY p.PersonId ASC) AS rownum, 
+			ROW_NUMBER() OVER (PARTITION BY p.PersonId ORDER BY p.PersonId ASC) AS rownum, 
 			rec.PersonId AS RecruiterId, rec.FirstName + ' ' + rec.LastName AS RecruiterName, rcomm1.RecruitId,
 			rcomm1.Amount AS cc1, rcomm1.HoursToCollect / 24 AS cd1, 
 			rcomm2.Amount AS cc2, rcomm2.HoursToCollect / 24 AS cd2
@@ -11,6 +11,7 @@ WITH Ordered AS (
 			INNER JOIN dbo.RecruiterCommission rcomm2 ON p.PersonId = rcomm2.RecruitId
 			INNER JOIN dbo.Person AS rec ON rcomm2.RecruiterId = rec.PersonId
 			WHERE (rcomm1.HoursToCollect != rcomm2.HoursToCollect OR 
-					rcomm1.Amount != rcomm2.Amount)
+					rcomm1.Amount != rcomm2.Amount
+					OR rcomm1.Amount IS NULL OR rcomm2.Amount IS NULL)
 )
 SELECT * FROM Ordered WHERE rownum % 2 = 1
