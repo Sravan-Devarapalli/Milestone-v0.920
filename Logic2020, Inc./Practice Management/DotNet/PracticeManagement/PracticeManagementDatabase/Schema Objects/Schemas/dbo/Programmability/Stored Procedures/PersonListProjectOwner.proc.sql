e6e18c12585dbb25f86f693f3e43f6be70aa14df
SrinivasM <SrinivasM>
2011-10-20 07:12:00 -0500

    Comments - Defect Number: 2941; Defect RecordID: 2937; Server Address: services.logic2020.com; Server Port: 99; Database Name: Practice Management; Defect Summary: PM Config: Project Lead role should have access to Project Summary page; should see Projects they are Owner of; Defect Description: We have the ability to specify now that a person with the Project Lead role is a "Project Manager" for a project, but because that role does not have access to the Project Summary page, they are unable to access the Project.
    We need to give persons with the Project Lead role access to the Project Summary page (as well as Project Search), but limit their view to be only projects that are relevant to their person, ie ones they are assigned as Project Manager.
    
    They should not have the ability to Add Project, and do not need to see Opportunities. They should only see reports for Time Entry by Project and Time Entry by Person.
    
    We may need to add an additional dashboard view as well, so that we can remove the "Add Project" link from their dashboard.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/PersonListProjectOwner.proc.sql
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/PersonListProjectOwner.proc.sql
@@ -5,58 +5,117 @@
 )
 AS
 	SET NOCOUNT ON
-
-	-- Table variable to store list of ProjectManagers user is allowed to see	
-    DECLARE @ProjectManagersPermissions TABLE ( PersonId INT NULL )
-	-- Populate is with the data from the permissions table
-	--		TargetType = 4 means that we are looking for the ProjectManagers in the permissions table
-    INSERT  INTO @ProjectManagersPermissions
-            (PersonId)
-            SELECT  prm.TargetId
-            FROM    v_permissions AS prm
-            WHERE   prm.PersonId = @PersonId
-                    AND prm.PermissionTypeId = 4
 	
-	-- If there are nulls in permission table, it means that eveything is allowed to be seen,
-	--		so set @PersonId to NULL which will extract all records from the table
-    DECLARE @NullsNumber INT
-    SELECT  @NullsNumber = COUNT(*)
-    FROM    @ProjectManagersPermissions AS pmp
-    WHERE   pmp.PersonId IS NULL 
+	/*
+		Showing Project Owner of the Projects, Where the Logged in User is Project Manager OR Sales Person of 
+		the Project(applicable only for Project Lead role Person) as part of #2941.
+	*/
 
-    IF @NullsNumber > 0 
-        SET @PersonId = NULL
+	DECLARE @UserHasHighRoleThanProjectLead INT = NULL
+	--Adding Project Lead as per #2941.
+	IF @PersonId IS NOT NULL AND EXISTS ( SELECT 1
+				FROM aspnet_Users U
+				JOIN aspnet_UsersInRoles UR ON UR.UserId = U.UserId
+				JOIN aspnet_Roles R ON R.RoleId = UR.RoleId
+				JOIN Person P ON P.Alias = U.UserName
+				WHERE P.PersonId = @PersonId AND R.LoweredRoleName = 'project lead')
+	BEGIN
+		SET @UserHasHighRoleThanProjectLead = 0
+		
+		SELECT @UserHasHighRoleThanProjectLead = COUNT(*)
+		FROM aspnet_Users U
+		JOIN aspnet_UsersInRoles UR ON UR.UserId = U.UserId
+		JOIN aspnet_Roles R ON R.RoleId = UR.RoleId
+		JOIN Person P ON P.Alias = U.UserName
+		WHERE P.PersonId = @PersonId
+			AND R.LoweredRoleName IN ('administrator','client director','practice area manager','senior leadership')		
+	END
 
-	SELECT  DISTINCT 
-			pers.PersonId ,
-			pers.PTODaysPerAnnum ,
-	        pers.HireDate ,
-	        pers.TerminationDate,
-	        pers.TelephoneNumber,
-	        pers.DefaultPractice,
-	        'Unknown' AS 'PracticeName',
-	        pers.Alias ,
-	        pers.FirstName ,
-	        pers.LastName ,
-	        pers.PersonStatusId ,
-	        'Unknown' AS 'PersonStatusName',
-	        pers.EmployeeNumber ,
-	        pers.SeniorityId,
-	        'Unknown' AS 'SeniorityName',
-	        pers.ManagerId,
-	        'Unknown' AS 'ManagerFirstName',	-- just stubs 
-	        'Unknown' AS 'ManagerLastName'	-- just stubs
-	FROM dbo.Project AS proj
-	INNER JOIN dbo.ProjectManagers AS projManagers ON projManagers.ProjectId = proj.ProjectId
-	INNER JOIN dbo.Person AS pers ON projManagers.ProjectManagerId = pers.PersonId
-	LEFT JOIN dbo.Commission AS C ON C.ProjectId = proj.ProjectId AND C.CommissionType = 1
-	WHERE (@IncludeInactive = 1 OR pers.PersonStatusId != 4)
-            AND ( @PersonId IS NULL
-                  OR pers.PersonId IN (
-                  SELECT    *
-                  FROM      @ProjectManagersPermissions )
-				   OR projManagers.ProjectManagerId  = @PersonId
-				   OR C.PersonId = @PersonId
-                )
-	order by pers.lastname, pers.firstname
+	IF @UserHasHighRoleThanProjectLead = 0--Adding Project Lead as per #2941.
+	BEGIN
+		
+		SELECT  DISTINCT 
+				pers.PersonId ,
+				pers.PTODaysPerAnnum ,
+				pers.HireDate ,
+				pers.TerminationDate,
+				pers.TelephoneNumber,
+				pers.DefaultPractice,
+				'Unknown' AS 'PracticeName',
+				pers.Alias ,
+				pers.FirstName ,
+				pers.LastName ,
+				pers.PersonStatusId ,
+				'Unknown' AS 'PersonStatusName',
+				pers.EmployeeNumber ,
+				pers.SeniorityId,
+				'Unknown' AS 'SeniorityName',
+				pers.ManagerId,
+				'Unknown' AS 'ManagerFirstName',	-- just stubs 
+				'Unknown' AS 'ManagerLastName'	-- just stubs
+		FROM Project Proj
+		LEFT JOIN ProjectManagers PM ON PM.ProjectId = Proj.ProjectId
+		LEFT JOIN Commission C ON C.ProjectId = Proj.ProjectId AND C.CommissionType = 1
+		JOIN Person Pers ON PM.ProjectManagerId = Pers.PersonId
+		WHERE (PM.ProjectManagerId = @PersonId OR C.PersonId = @PersonId)
+			AND (@IncludeInactive = 1 OR pers.PersonStatusId != 4)
+
+	END
+	ELSE
+	BEGIN
+
+		-- Table variable to store list of ProjectManagers user is allowed to see	
+		DECLARE @ProjectManagersPermissions TABLE ( PersonId INT NULL )
+		-- Populate is with the data from the permissions table
+		--		TargetType = 4 means that we are looking for the ProjectManagers in the permissions table
+		INSERT  INTO @ProjectManagersPermissions
+				(PersonId)
+				SELECT  prm.TargetId
+				FROM    v_permissions AS prm
+				WHERE   prm.PersonId = @PersonId
+						AND prm.PermissionTypeId = 4
+	
+		-- If there are nulls in permission table, it means that eveything is allowed to be seen,
+		--		so set @PersonId to NULL which will extract all records from the table
+		DECLARE @NullsNumber INT
+		SELECT  @NullsNumber = COUNT(*)
+		FROM    @ProjectManagersPermissions AS pmp
+		WHERE   pmp.PersonId IS NULL 
+
+		IF @NullsNumber > 0 
+			SET @PersonId = NULL
+
+		SELECT  DISTINCT 
+				pers.PersonId ,
+				pers.PTODaysPerAnnum ,
+				pers.HireDate ,
+				pers.TerminationDate,
+				pers.TelephoneNumber,
+				pers.DefaultPractice,
+				'Unknown' AS 'PracticeName',
+				pers.Alias ,
+				pers.FirstName ,
+				pers.LastName ,
+				pers.PersonStatusId ,
+				'Unknown' AS 'PersonStatusName',
+				pers.EmployeeNumber ,
+				pers.SeniorityId,
+				'Unknown' AS 'SeniorityName',
+				pers.ManagerId,
+				'Unknown' AS 'ManagerFirstName',	-- just stubs 
+				'Unknown' AS 'ManagerLastName'	-- just stubs
+		FROM dbo.Project AS proj
+		INNER JOIN dbo.ProjectManagers AS projManagers ON projManagers.ProjectId = proj.ProjectId
+		INNER JOIN dbo.Person AS pers ON projManagers.ProjectManagerId = pers.PersonId
+		LEFT JOIN dbo.Commission AS C ON C.ProjectId = proj.ProjectId AND C.CommissionType = 1
+		WHERE (@IncludeInactive = 1 OR pers.PersonStatusId != 4)
+				AND ( @PersonId IS NULL
+					  OR pers.PersonId IN (
+					  SELECT    *
+					  FROM      @ProjectManagersPermissions )
+					   OR projManagers.ProjectManagerId  = @PersonId
+					   OR C.PersonId = @PersonId
+					)
+		order by pers.lastname, pers.firstname
+	END
 
