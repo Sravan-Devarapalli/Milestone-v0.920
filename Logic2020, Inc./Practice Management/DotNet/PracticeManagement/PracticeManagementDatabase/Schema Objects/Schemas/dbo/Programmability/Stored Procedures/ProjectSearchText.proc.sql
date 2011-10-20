1a9105e7d09310626dbd235f7a2ed48be977e7e5
SrinivasM <SrinivasM>
2011-10-20 07:12:00 -0500

    Comments - Defect Number: 2941; Defect RecordID: 2937; Server Address: services.logic2020.com; Server Port: 99; Database Name: Practice Management; Defect Summary: PM Config: Project Lead role should have access to Project Summary page; should see Projects they are Owner of; Defect Description: We have the ability to specify now that a person with the Project Lead role is a "Project Manager" for a project, but because that role does not have access to the Project Summary page, they are unable to access the Project.
    We need to give persons with the Project Lead role access to the Project Summary page (as well as Project Search), but limit their view to be only projects that are relevant to their person, ie ones they are assigned as Project Manager.
    
    They should not have the ability to Add Project, and do not need to see Opportunities. They should only see reports for Time Entry by Project and Time Entry by Person.
    
    We may need to add an additional dashboard view as well, so that we can remove the "Add Project" link from their dashboard.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/ProjectSearchText.proc.sql
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/ProjectSearchText.proc.sql
@@ -23,120 +23,155 @@ AS
 	IF @PersonRole = 'Administrator' 
 		SET @PersonId = null
 
-	DECLARE @SearchText NVARCHAR(257)
-	SET @SearchText = '%' + ISNULL(@Looked, '') + '%'
+		
+
+		DECLARE @SearchText NVARCHAR(257)
+		SET @SearchText = '%' + ISNULL(@Looked, '') + '%'
+
+	
+	/*
+		Showing client of the Projects, Where the Logged in User is Project Manager OR Sales Person of 
+		the Project(applicable only for Project Lead role Person) as part of #2941.
+	*/
+
+	DECLARE @UserHasHighRoleThanProjectLead INT = NULL
+	--Adding Project Lead as per #2941.
+	IF @PersonId IS NOT NULL AND EXISTS ( SELECT 1
+				FROM aspnet_Users U
+				JOIN aspnet_UsersInRoles UR ON UR.UserId = U.UserId
+				JOIN aspnet_Roles R ON R.RoleId = UR.RoleId
+				JOIN Person P ON P.Alias = U.UserName
+				WHERE P.PersonId = @PersonId AND R.LoweredRoleName = 'project lead')
+	BEGIN
+		SET @UserHasHighRoleThanProjectLead = 0
+		
+		SELECT @UserHasHighRoleThanProjectLead = COUNT(*)
+		FROM aspnet_Users U
+		JOIN aspnet_UsersInRoles UR ON UR.UserId = U.UserId
+		JOIN aspnet_Roles R ON R.RoleId = UR.RoleId
+		JOIN Person P ON P.Alias = U.UserName
+		WHERE P.PersonId = @PersonId
+			AND R.LoweredRoleName IN ('administrator','client director','practice area manager','senior leadership')		
+	END
 
 
-	-- Convert client ids from string to table
-	DECLARE @ClientsList TABLE (Id INT)
-	INSERT INTO @ClientsList
-	SELECT TargetId FROM dbo.PersonClientPermissions(@PersonId)
+		-- Convert client ids from string to table
+		DECLARE @ClientsList TABLE (Id INT)
+		INSERT INTO @ClientsList
+		SELECT TargetId FROM dbo.PersonClientPermissions(@PersonId)
 
-	-- Convert practice ids from string to table
-	DECLARE @PracticesList TABLE (Id INT)
-	INSERT INTO @PracticesList
-	SELECT TargetId FROM dbo.PersonPracticePermissions(@PersonId)
+		-- Convert practice ids from string to table
+		DECLARE @PracticesList TABLE (Id INT)
+		INSERT INTO @PracticesList
+		SELECT TargetId FROM dbo.PersonPracticePermissions(@PersonId)
 
-	-- Convert project group ids from string to table
-	DECLARE @ProjectGroupsList TABLE (Id INT)
-	INSERT INTO @ProjectGroupsList
-	SELECT TargetId FROM dbo.PersonProjectGroupPermissions(@PersonId)
+		-- Convert project group ids from string to table
+		DECLARE @ProjectGroupsList TABLE (Id INT)
+		INSERT INTO @ProjectGroupsList
+		SELECT TargetId FROM dbo.PersonProjectGroupPermissions(@PersonId)
 
-	-- Convert project group ids from string to table
-	DECLARE @ProjectOwnerList TABLE (Id INT)
-	INSERT INTO @ProjectOwnerList
-	SELECT TargetId FROM dbo.PersonProjectOwnerPermissions(@PersonId)
+		-- Convert project group ids from string to table
+		DECLARE @ProjectOwnerList TABLE (Id INT)
+		INSERT INTO @ProjectOwnerList
+		SELECT TargetId FROM dbo.PersonProjectOwnerPermissions(@PersonId)
 
-	DECLARE @DefaultProjectId INT
-	SELECT @DefaultProjectId = ProjectId
-	FROM dbo.DefaultMilestoneSetting
+		DECLARE @DefaultProjectId INT
+		SELECT @DefaultProjectId = ProjectId
+		FROM dbo.DefaultMilestoneSetting
 
-		-- Table variable to store list of Clients that owner and salesPerson is allowed to see	
-	DECLARE @OwnerProjectClientList TABLE(
-		ClientId INT NULL -- As per #2890
-	)
+			-- Table variable to store list of Clients that owner and salesPerson is allowed to see	
+		DECLARE @OwnerProjectClientList TABLE(
+			ClientId INT NULL -- As per #2890
+		)
 
-   -- Table variable to store list of groups that owner and salesPerson is allowed to see	
-	DECLARE @OwnerProjectGroupList TABLE(
-		GroupId INT NULL -- As per #2890
-	)
+	   -- Table variable to store list of groups that owner and salesPerson is allowed to see	
+		DECLARE @OwnerProjectGroupList TABLE(
+			GroupId INT NULL -- As per #2890
+		)
 
-	-- Populate is with the data from the Project
-	INSERT INTO @OwnerProjectClientList (ClientId) 
-	SELECT proj.ClientId 
-	FROM dbo.Project AS proj
-	INNER JOIN ProjectManagers AS projManagers ON projManagers.ProjectId = proj.ProjectId
-	LEFT JOIN dbo.Commission C ON C.ProjectId = proj.ProjectId AND C.CommissionType = 1
-	WHERE projManagers.ProjectManagerId = @PersonId OR C.PersonId = @PersonId -- Adding Salesperson - Project clients into the list.
+		-- Populate is with the data from the Project
+		INSERT INTO @OwnerProjectClientList (ClientId) 
+		SELECT proj.ClientId 
+		FROM dbo.Project AS proj
+		INNER JOIN ProjectManagers AS projManagers ON projManagers.ProjectId = proj.ProjectId
+		LEFT JOIN dbo.Commission C ON C.ProjectId = proj.ProjectId AND C.CommissionType = 1
+		WHERE projManagers.ProjectManagerId = @PersonId OR C.PersonId = @PersonId -- Adding Salesperson - Project clients into the list.
 
-	INSERT INTO @OwnerProjectGroupList (GroupId) 
-	SELECT GroupId FROM  dbo.ProjectGroup
-	WHERE ClientId IN (SELECT opc.ClientId FROM @OwnerProjectClientList AS opc)
+		INSERT INTO @OwnerProjectGroupList (GroupId) 
+		SELECT GroupId FROM  dbo.ProjectGroup
+		WHERE ClientId IN (SELECT opc.ClientId FROM @OwnerProjectClientList AS opc)
 
 
-	-- Search for a project with milestone(s)
-	;WITH FoundProjects AS (
-	SELECT m.ClientId,
-	       m.ProjectId,
-	       m.MilestoneId,
-	       m.ClientName,
-	       m.ProjectName,
-	       m.Description,
-	       m.ProjectStartDate,
-	       m.ProjectEndDate,
-	       m.ProjectNumber,
-	       m.ProjectStatusId,
-	       s.Name AS ProjectStatusName,
-           m.GroupId,
-		   CASE WHEN A.ProjectId IS NOT NULL THEN 1 
-					ELSE 0 END AS HasAttachments
-	  FROM dbo.v_Milestone AS m
-	       INNER JOIN ProjectManagers AS projManagers ON projManagers.ProjectId = m.ProjectId
-	       INNER JOIN dbo.Commission AS c ON m.ProjectId = c.ProjectId 
-	       INNER JOIN dbo.ProjectStatus AS s ON m.ProjectStatusId = s.ProjectStatusId
-	  OUTER APPLY (SELECT TOP 1 ProjectId FROM ProjectAttachment as pa WHERE pa.ProjectId = m.ProjectId) A
-	 WHERE (
-			m.ProjectName LIKE @SearchText COLLATE SQL_Latin1_General_CP1_CI_AS
-	        OR m.ProjectNumber LIKE @SearchText COLLATE SQL_Latin1_General_CP1_CI_AS
-	        OR m.ClientName LIKE @SearchText COLLATE SQL_Latin1_General_CP1_CI_AS
-	        OR m.Description LIKE @SearchText COLLATE SQL_Latin1_General_CP1_CI_AS
-	        OR m.BuyerName LIKE @SearchText COLLATE SQL_Latin1_General_CP1_CI_AS
-	       )
-		   AND (@PersonId is NULL OR m.ClientId IN (select * from @ClientsList) OR  m.ClientId IN (SELECT opc.ClientId FROM @OwnerProjectClientList AS opc))
-		   AND (@PersonId is NULL OR m.GroupId IN (select * from @ProjectGroupsList) OR  m.GroupId IN (SELECT opG.GroupId FROM @OwnerProjectGroupList AS opG))
-		   AND (@PersonId is NULL OR projManagers.ProjectManagerId = @PersonId OR c.PersonId = @PersonId OR projManagers.ProjectManagerId in (select * from @ProjectOwnerList))   
-	UNION ALL
-	SELECT p.ClientId,
-	       p.ProjectId,
-	       NULL AS MilestoneId,
-	       p.ClientName,
-	       p.Name AS ProjectName,
-	       NULL AS Description,
-	       NULL AS ProjectStartDate,
-	       NULL AS ProjectEndDate,
-	       p.ProjectNumber,
-	       p.ProjectStatusId,
-	       p.ProjectStatusName,
-           p.GroupId,
-		   CASE WHEN A.ProjectId IS NOT NULL THEN 1 
-					ELSE 0 END AS HasAttachments
-	  FROM dbo.v_Project AS p
-	  INNER JOIN ProjectManagers AS projManagers ON projManagers.ProjectId = p.ProjectId
-	    INNER JOIN dbo.Commission AS c ON  p.ProjectId = c.ProjectId 
-	  OUTER APPLY (SELECT TOP 1 ProjectId FROM ProjectAttachment as pa WHERE pa.ProjectId = p.ProjectId) A
-	 WHERE NOT EXISTS (SELECT 1 FROM dbo.Milestone AS m WHERE m.ProjectId = p.ProjectId)
-	   AND (   
-			p.Name LIKE @SearchText COLLATE SQL_Latin1_General_CP1_CI_AS
-	        OR p.ProjectNumber LIKE @SearchText COLLATE SQL_Latin1_General_CP1_CI_AS
-	        OR p.ClientName LIKE @SearchText COLLATE SQL_Latin1_General_CP1_CI_AS
-	        OR p.BuyerName LIKE @SearchText COLLATE SQL_Latin1_General_CP1_CI_AS
-	       )
-	   AND (@PersonId is NULL OR p.ClientId in (SELECT * FROM @ClientsList) OR  p.ClientId IN (SELECT opc.ClientId FROM @OwnerProjectClientList AS opc))
-	   AND (@PersonId is NULL OR p.GroupId in (SELECT * FROM @ProjectGroupsList) OR  P.GroupId IN (SELECT opG.GroupId FROM @OwnerProjectGroupList AS opG))
-	   AND (@PersonId is NULL OR p.PracticeId in (SELECT * FROM @PracticesList))	
-	   AND (@PersonId is NULL OR projManagers.ProjectManagerId = @PersonId  OR c.PersonId = @PersonId OR projManagers.ProjectManagerId in (SELECT * FROM @ProjectOwnerList))   
-	)
-	SELECT DISTINCT *
-	FROM FoundProjects
-	WHERE ProjectId <> @DefaultProjectId
-	ORDER BY ProjectName, Description
+		-- Search for a project with milestone(s)
+		;WITH FoundProjects AS (
+		SELECT m.ClientId,
+			   m.ProjectId,
+			   m.MilestoneId,
+			   m.ClientName,
+			   m.ProjectName,
+			   m.Description,
+			   m.ProjectStartDate,
+			   m.ProjectEndDate,
+			   m.ProjectNumber,
+			   m.ProjectStatusId,
+			   s.Name AS ProjectStatusName,
+			   m.GroupId,
+			   CASE WHEN A.ProjectId IS NOT NULL THEN 1 
+						ELSE 0 END AS HasAttachments
+		  FROM dbo.v_Milestone AS m
+			   INNER JOIN ProjectManagers AS projManagers ON projManagers.ProjectId = m.ProjectId
+			   INNER JOIN dbo.Commission AS c ON m.ProjectId = c.ProjectId 
+			   INNER JOIN dbo.ProjectStatus AS s ON m.ProjectStatusId = s.ProjectStatusId
+		  OUTER APPLY (SELECT TOP 1 ProjectId FROM ProjectAttachment as pa WHERE pa.ProjectId = m.ProjectId) A
+		 WHERE (
+				m.ProjectName LIKE @SearchText COLLATE SQL_Latin1_General_CP1_CI_AS
+				OR m.ProjectNumber LIKE @SearchText COLLATE SQL_Latin1_General_CP1_CI_AS
+				OR m.ClientName LIKE @SearchText COLLATE SQL_Latin1_General_CP1_CI_AS
+				OR m.Description LIKE @SearchText COLLATE SQL_Latin1_General_CP1_CI_AS
+				OR m.BuyerName LIKE @SearchText COLLATE SQL_Latin1_General_CP1_CI_AS
+			   )
+			   AND (@PersonId is NULL OR m.ClientId IN (select * from @ClientsList) OR  m.ClientId IN (SELECT opc.ClientId FROM @OwnerProjectClientList AS opc))
+			   AND (@PersonId is NULL OR m.GroupId IN (select * from @ProjectGroupsList) OR  m.GroupId IN (SELECT opG.GroupId FROM @OwnerProjectGroupList AS opG))
+			   AND (@PersonId is NULL OR projManagers.ProjectManagerId = @PersonId OR c.PersonId = @PersonId OR projManagers.ProjectManagerId in (select * from @ProjectOwnerList))   
+		UNION ALL
+		SELECT p.ClientId,
+			   p.ProjectId,
+			   NULL AS MilestoneId,
+			   p.ClientName,
+			   p.Name AS ProjectName,
+			   NULL AS Description,
+			   NULL AS ProjectStartDate,
+			   NULL AS ProjectEndDate,
+			   p.ProjectNumber,
+			   p.ProjectStatusId,
+			   p.ProjectStatusName,
+			   p.GroupId,
+			   CASE WHEN A.ProjectId IS NOT NULL THEN 1 
+						ELSE 0 END AS HasAttachments
+		  FROM dbo.v_Project AS p
+		  INNER JOIN ProjectManagers AS projManagers ON projManagers.ProjectId = p.ProjectId
+			INNER JOIN dbo.Commission AS c ON  p.ProjectId = c.ProjectId 
+		  OUTER APPLY (SELECT TOP 1 ProjectId FROM ProjectAttachment as pa WHERE pa.ProjectId = p.ProjectId) A
+		 WHERE NOT EXISTS (SELECT 1 FROM dbo.Milestone AS m WHERE m.ProjectId = p.ProjectId)
+		   AND (   
+				p.Name LIKE @SearchText COLLATE SQL_Latin1_General_CP1_CI_AS
+				OR p.ProjectNumber LIKE @SearchText COLLATE SQL_Latin1_General_CP1_CI_AS
+				OR p.ClientName LIKE @SearchText COLLATE SQL_Latin1_General_CP1_CI_AS
+				OR p.BuyerName LIKE @SearchText COLLATE SQL_Latin1_General_CP1_CI_AS
+			   )
+		   AND (@PersonId is NULL OR p.ClientId in (SELECT * FROM @ClientsList) OR  p.ClientId IN (SELECT opc.ClientId FROM @OwnerProjectClientList AS opc))
+		   AND (@PersonId is NULL OR p.GroupId in (SELECT * FROM @ProjectGroupsList) OR  P.GroupId IN (SELECT opG.GroupId FROM @OwnerProjectGroupList AS opG))
+		   AND (@PersonId is NULL OR p.PracticeId in (SELECT * FROM @PracticesList))	
+		   AND (@PersonId is NULL OR projManagers.ProjectManagerId = @PersonId  OR c.PersonId = @PersonId OR projManagers.ProjectManagerId in (SELECT * FROM @ProjectOwnerList))   
+		)
+		SELECT DISTINCT *
+		FROM FoundProjects FP
+		LEFT JOIN ProjectManagers PM ON PM.ProjectId = FP.ProjectId
+		LEFT JOIN Commission C ON C.ProjectId = FP.ProjectId
+		WHERE FP.ProjectId <> @DefaultProjectId
+			AND ( @UserHasHighRoleThanProjectLead IS NULL 
+					OR @UserHasHighRoleThanProjectLead <> 0
+					OR (@UserHasHighRoleThanProjectLead = 0 AND (PM.ProjectManagerId = @PersonId OR C.PersonId = @PersonId ))
+				)
+		ORDER BY FP.ProjectName, Description
+
