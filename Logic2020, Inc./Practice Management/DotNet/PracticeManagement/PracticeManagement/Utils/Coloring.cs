413c63540769216d444dc9a80890c8b7868836c8
SrinivasM <SrinivasM>
2011-10-25 08:52:00 -0500

    Comments - Added New Page "Consulting Capacity". and given permissions similar to "Consulting Utilization" page.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/Utils/Coloring.cs
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/Utils/Coloring.cs
@@ -48,6 +48,64 @@ namespace PraticeManagement.Utils
         }
 
         /// <summary>
+        /// Returns color based on capacity value
+        /// </summary>
+        /// <param name="utilization">Capacity value in percents</param>
+        /// <param name="isVac">Is that vacation period</param>
+        /// <returns>Color based on config settings</returns>
+        public static Color GetColorByCapacity(int capacity, bool isVac, bool isHired = true, bool isTerminated = false)
+        {
+            //  Get settings from web.config
+            ConsReportColoringElementSection coloring =
+                ConsReportColoringElementSection.ColorSettings;
+
+            if (!isHired || isTerminated)
+                return coloring.HiredColor;
+
+            //  If that's vacation, return vacation color
+            if (isVac)
+                return coloring.VacationColor;
+
+            if (capacity >= 50 && capacity <= 100)
+            {
+                return Color.FromArgb(255, 0, 0);//Red color.
+            }
+            else if (capacity >= 10 && capacity <= 49)
+            {
+                return Color.FromArgb(255, 255, 0);//Yellow Color.
+            }
+            else if (capacity >= -5 && capacity <= 9)
+            {
+                return Color.FromArgb(82, 178, 0);//Green Color.
+            }
+            else if (capacity <= -6)
+            {
+                return Color.FromArgb(51, 204, 255);//sky blue Color.
+            }
+
+            //  Return default color if nothing was foung in config
+            return DEFAULT_COLOR;
+        }
+
+        public static void CapacityColorLegends(Legend legend)
+        {
+            //  Clear legend items first
+            LegendItemsCollection legendItems = legend.CustomItems;
+            legendItems.Clear();
+
+            //  Iterate through all colors and put them on legend
+            legendItems.Add(Color.FromArgb(255, 0, 0), "Capacity = 100 - 50%");//Red
+            legendItems.Add(Color.FromArgb(255, 255, 0), "Capacity = 49 - 10%");//Yellow
+            legendItems.Add(Color.FromArgb(82, 178, 0), "Capacity = 9 - (-5)%");//Green
+            legendItems.Add(Color.FromArgb(51, 204, 255), "Capacity = (-6)+%");//Sky Blue.
+
+            ConsReportColoringElementSection coloring =
+                ConsReportColoringElementSection.ColorSettings;
+            //  Add vacation item
+            legendItems.Add(coloring.VacationColor, coloring.VacationTitle); 
+        }
+
+        /// <summary>
         /// Adds color coding to legend
         /// </summary>
         /// <param name="legend">Legend to put colors to</param>
