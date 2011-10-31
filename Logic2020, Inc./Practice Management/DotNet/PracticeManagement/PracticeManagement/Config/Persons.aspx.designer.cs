61894d3a8e70d7efebf9fd680d5d760a11f292dd
SrinivasM <SrinivasM>
2011-10-31 09:42:00 -0500

    none

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/Config/Persons.aspx.designer.cs
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/Config/Persons.aspx.designer.cs
@@ -103,6 +103,15 @@ namespace PraticeManagement.Config {
         protected global::System.Web.UI.WebControls.DropDownList ddlView;
         
         /// <summary>
+        /// lnkAddStrawman control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::PraticeManagement.Controls.Generic.Buttons.ShadowedHyperlink lnkAddStrawman;
+        
+        /// <summary>
         /// lnkAddPerson control.
         /// </summary>
         /// <remarks>
