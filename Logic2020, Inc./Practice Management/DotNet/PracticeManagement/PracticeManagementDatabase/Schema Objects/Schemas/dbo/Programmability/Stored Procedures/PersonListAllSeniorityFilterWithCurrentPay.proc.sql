9b6b90f7564fcc858fe6ec23740183ca9c55bdf0
RaviN <RaviN>
2011-11-01 10:34:00 -0500

    Comments - Defect Number: 2945; Defect RecordID: 2941; Server Address: services.logic2020.com; Server Port: 99; Database Name: Practice Management; Defect Summary: Configuration>Company Resources>Persons: Add "Is Strawman" checkbox to person record to flag record; Defect Description: Also, please give consideration to ramifications of this change.
    What fields in the Person record can we grey out and ignore? (Status, Line Manager, Default Practice, Start/Termination Date, Recruiter, what else?)
    
    We will still require a salary for this record so that COGS/Margin can be calculated when the Strawman is used in a Project.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/PersonListAllSeniorityFilterWithCurrentPay.proc.sql
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/PersonListAllSeniorityFilterWithCurrentPay.proc.sql
@@ -120,7 +120,8 @@ BEGIN
 						) TS
 				 LEFT JOIN dbo.aspnet_Users U ON (U.UserName = P.Alias)
 				 LEFT JOIN dbo.aspnet_Membership M ON (U.UserId = M.UserId)
-				 WHERE  (   (p.PersonStatusId = 1 AND @ShowAll = 0) 
+				 WHERE   ISNULL(p.IsStrawman,0) = 0
+				 (   (p.PersonStatusId = 1 AND @ShowAll = 0) 
 							OR (p.PersonStatusId = 2 AND @Terminated = 1)
 							OR (p.PersonStatusId = 3 AND @Projected = 1)
 							OR (p.PersonStatusId = 4 AND @Inactive = 1) 
