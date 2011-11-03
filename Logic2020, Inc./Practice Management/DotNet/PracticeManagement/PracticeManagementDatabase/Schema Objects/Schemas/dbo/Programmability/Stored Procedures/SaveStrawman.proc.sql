9c62e6e4ed8200d3cd53fd44eeeaaccae74a6204
RaviN <RaviN>
2011-11-03 07:33:00 -0500

    Comments - Defect Number: 2945; Defect RecordID: 2941; Server Address: services.logic2020.com; Server Port: 99; Database Name: Practice Management; Defect Summary: Configuration>Company Resources>Persons: Add "Is Strawman" checkbox to person record to flag record; Defect Description: Also, please give consideration to ramifications of this change.
    What fields in the Person record can we grey out and ignore? (Status, Line Manager, Default Practice, Start/Termination Date, Recruiter, what else?)
    
    We will still require a salary for this record so that COGS/Margin can be calculated when the Strawman is used in a Project.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/SaveStrawman.proc.sql
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/SaveStrawman.proc.sql
@@ -16,60 +16,69 @@ AS
 BEGIN
 	BEGIN TRY
 		DECLARE @Today DATETIME
+		DECLARE @ErrorMessage NVARCHAR(500)
 
 		SELECT @Today = dbo.GettingPMTime(GETDATE())
 
 		SELECT @Today = CONVERT(TIME,@Today)
 
 		SET @StartDate = ISNULL(CONVERT(DATE, @StartDate), '1900-01-01')
-		IF EXISTS (SELECT 1 FROM Person WHERE FirstName = @FirstName AND LastName = @LastName)
+		
+			
+		IF @PersonId IS NULL
+		BEGIN
+			IF EXISTS (SELECT 1 FROM Person WHERE FirstName = @FirstName AND LastName = @LastName)
 			BEGIN
 			
-				DECLARE @ErrorMessage NVARCHAR(500)
 				-- Person First and Last Name uniqueness violation
 				SELECT @ErrorMessage = [dbo].[GetErrorMessage](70001)
 				RAISERROR (@ErrorMessage, 16, 1)
 			END
-			
-		IF @PersonId IS NULL
-		BEGIN
+			EXEC dbo.SessionLogPrepare @userLogin = @UserLogin
 
-				EXEC dbo.SessionLogPrepare @userLogin = @UserLogin
+			DECLARE @Counter INT,
+				@StringCounter NVARCHAR(7),
+				@EmployeeNumber NVARCHAR(12),
+				@HireDate DATETIME
 
-				DECLARE @Counter INT,
-					@StringCounter NVARCHAR(7),
-					@EmployeeNumber NVARCHAR(12),
-					@HireDate DATETIME
+			SELECT @Counter = 0, @HireDate = dbo.GettingPMTime(GETUTCDATE())
 
-				SELECT @Counter = 0, @HireDate = dbo.GettingPMTime(GETUTCDATE())
+			WHILE  (1 = 1)
+			BEGIN
 
-				WHILE  (1 = 1)
-				BEGIN
+				SET @StringCounter = CONVERT(NVARCHAR(7), @Counter )
+				IF LEN ( @StringCounter ) = 1
+					SET @StringCounter =  '0' + @StringCounter
 
-					SET @StringCounter = CONVERT(NVARCHAR(7), @Counter )
-					IF LEN ( @StringCounter ) = 1
-						SET @StringCounter =  '0' + @StringCounter
+				SET @EmployeeNumber = 'C'+ SUBSTRING ( CONVERT(NVARCHAR(255), @HireDate, 10) ,0 , 3 ) +
+					SUBSTRING ( CONVERT(NVARCHAR(255), @HireDate, 10) ,7 , 3 ) + @StringCounter
+		
+				IF (NOT EXISTS (SELECT 1 FROM [dbo].[Person] as p WHERE p.[EmployeeNumber] = @EmployeeNumber) )
+					BREAK
 
-					SET @EmployeeNumber = 'C'+ SUBSTRING ( CONVERT(NVARCHAR(255), @HireDate, 10) ,0 , 3 ) +
-						SUBSTRING ( CONVERT(NVARCHAR(255), @HireDate, 10) ,7 , 3 ) + @StringCounter
-			
-					IF (NOT EXISTS (SELECT 1 FROM [dbo].[Person] as p WHERE p.[EmployeeNumber] = @EmployeeNumber) )
-						BREAK
-
-					SET @Counter = @Counter + 1
-				END
-			
-				INSERT INTO Person(FirstName, LastName, EmployeeNumber, PTODaysPerAnnum, IsStrawman,HireDate)
-				VALUES (@FirstName, @LastName, @EmployeeNumber, 0, 1, @Today) --For strawmem we will use HireDate field as created date field
-			
-				SET @PersonId = SCOPE_IDENTITY()
-
-				EXEC dbo.SessionLogUnprepare
+				SET @Counter = @Counter + 1
 			END
+		
+			INSERT INTO Person(FirstName, LastName, EmployeeNumber, PTODaysPerAnnum, IsStrawman,HireDate)
+			VALUES (@FirstName, @LastName, @EmployeeNumber, 0, 1, @Today) --For strawmem we will use HireDate field as created date field
+		
+			SET @PersonId = SCOPE_IDENTITY()
+
+			EXEC dbo.SessionLogUnprepare
+		END
 
 
 		IF @PersonId IS NOT NULL
 		BEGIN
+			IF EXISTS (SELECT 1 
+					   FROM Person AS P 
+					   WHERE P.PersonId != @PersonId AND FirstName = @FirstName AND LastName = @LastName)
+			BEGIN
+			
+				-- Person First and Last Name uniqueness violation
+				SELECT @ErrorMessage = [dbo].[GetErrorMessage](70001)
+				RAISERROR (@ErrorMessage, 16, 1)
+			END
 			IF EXISTS (SELECT 1 FROM Person WHERE PersonId = @PersonId AND ( FirstName <> @FirstName OR LastName <> @LastName ) )
 			BEGIN
 				UPDATE Person
