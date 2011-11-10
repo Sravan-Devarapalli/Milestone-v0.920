5f516eddfb0e93f94d36680d00b2be69ab107572
ThulasiramP <ThulasiramP>
2011-11-10 02:40:00 -0500

    Comments - Defect Number: 2925; Defect RecordID: 2921; Server Address: services.logic2020.com; Server Port: 99; Database Name: Practice Management; Defect Summary: Time Entry: Duplication of hours due to resources issue on project; Defect Description: Original e-mail is below. I believe this issue is due to an issue in the Resources tab. PM will not allow a resource to be removed from a milestone if the person has already entered time and notes towards the milestone, which has always been the case, but the issue occurs when a person is added to a milestone more than once for overlapping periods. It is not possible to remove the person from a milestone, even when it is a redundant entry.
    For example:
    The current date is 9/27
    MilestoneA = 9/1-9/30
    PersonA is added as a resource to MilestoneA for 9/1-9/30
    PersonA is also added as a resource to MilestoneA for 9/20-9/30
    PersonA has entered time and notes for MilestoneA from 9/1-9/21
    It is now not possilble to remove the second entry for PersonA from MilestoneA because PM sees time and notes entered through 9/21 and does not allow the removal.
    
    I checked the time report (copied below) which says Kerissa has 121hrs booked but every entry is repeated, so her hours are in fact 60.5 which matches my expectations and what I believe we should bill. The report you (Ivana) have got, gives over 70hrs and contains duplicate entries for 19-21st (totaling 14hrs).

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/PersonMilestoneWithFinancials.proc.sql
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/PersonMilestoneWithFinancials.proc.sql
@@ -7,9 +7,39 @@ BEGIN
 
 	DECLARE @DefaultProjectId INT,
 			@PersonIdLocal INT
+
 	SELECT @DefaultProjectId = ProjectId
 	FROM dbo.DefaultMilestoneSetting
+
 	SELECT @PersonIdLocal = @PersonId
+
+	;WITH FinancialsRetro AS 
+	(
+	SELECT f.ProjectId,
+		   f.PersonId,
+		   f.MilestoneId,
+		   f.Date, 
+		   f.PersonMilestoneDailyAmount,
+		   f.PersonDiscountDailyAmount,
+		   ISNULL((SELECT SUM(c.FractionOfMargin) 
+							  FROM dbo.Commission AS  c 
+							  WHERE c.ProjectId = f.ProjectId 
+									AND c.CommissionType = 1
+								),0) ProjectSalesCommisionFraction,
+		   (ISNULL(f.PayRate, 0) + ISNULL(f.OverheadRate, 0)+ISNULL(f.BonusRate,0)+ISNULL(f.VacationRate,0)
+		   	+ISNULL(f.RecruitingCommissionRate,0)) SLHR,
+		   ISNULL(f.PayRate,0) PayRate,
+		   f.MLFOverheadRate,
+		   f.PersonHoursPerDay,
+		   f.PracticeManagementCommissionSub,
+		   f.PracticeManagementCommissionOwn ,
+		   f.PracticeManagerId,
+		   f.Discount,
+		   f.EntryId
+	FROM v_FinancialsRetrospective f
+	WHERE f.PersonId = @PersonIdLocal  AND f.ProjectId != @DefaultProjectId
+	)
+
 	SELECT  p.ProjectId,
 			p.Name AS 'ProjectName',
 			p.ProjectStatusId,
@@ -17,24 +47,29 @@ BEGIN
 			s.Name AS 'ProjectStatus',
 			m.MilestoneId,
 			m.Description AS 'MilestoneName',
+			f.EntryId,
 			mp.MilestonePersonId,
 			mpe.StartDate,
 			mpe.EndDate,
 			r.Name AS 'RoleName',
-			f.Revenue,
-			f.GrossMargin,
 			CASE WHEN A.ProjectId IS NOT NULL THEN 1 
-					ELSE 0 END AS HasAttachments
-	FROM    dbo.Project AS p
-			INNER JOIN dbo.ProjectStatus AS s ON p.ProjectStatusId = s.ProjectStatusId
-			INNER JOIN dbo.Milestone AS m ON p.ProjectId = m.ProjectId
-			INNER JOIN dbo.MilestonePerson AS mp ON m.MilestoneId = mp.MilestoneId
-			INNER JOIN dbo.MilestonePersonEntry AS mpe ON mp.MilestonePersonId = mpe.MilestonePersonId
-			LEFT JOIN dbo.PersonRole AS r ON mpe.PersonRoleId = r.PersonRoleId
-			OUTER APPLY (SELECT TOP 1 ProjectId FROM ProjectAttachment as pa WHERE pa.ProjectId = p.ProjectId) A
-			INNER JOIN v_FinancialsForMilestones AS f ON m.MilestoneId = f.MilestoneId
-														 AND mp.PersonId = f.PersonId
-	WHERE   f.PersonId = @PersonIdLocal AND P.ProjectId <> @DefaultProjectId
-	ORDER BY mpe.StartDate
+					ELSE 0 END AS HasAttachments,
+	       ISNULL(SUM(f.PersonMilestoneDailyAmount), 0) AS Revenue,
+		   ISNULL(SUM(f.PersonMilestoneDailyAmount - f.PersonDiscountDailyAmount -
+						(CASE WHEN f.SLHR >= f.PayRate + f.MLFOverheadRate 
+							  THEN f.SLHR ELSE ISNULL(f.PayRate, 0)+f.MLFOverheadRate END) 
+					    *ISNULL(f.PersonHoursPerDay, 0)), 0) GrossMargin
+	  FROM FinancialsRetro AS f
+	  INNER JOIN dbo.Project AS p ON p.ProjectId = F.ProjectId
+	  INNER JOIN dbo.ProjectStatus AS s ON p.ProjectStatusId = s.ProjectStatusId
+	  INNER JOIN dbo.Milestone AS m ON p.ProjectId = m.ProjectId
+	  INNER JOIN dbo.MilestonePerson AS mp ON m.MilestoneId = mp.MilestoneId
+	  INNER JOIN dbo.MilestonePersonEntry AS mpe ON mp.MilestonePersonId = mpe.MilestonePersonId AND mpe.Id = f.EntryId
+	  LEFT JOIN dbo.PersonRole AS r ON mpe.PersonRoleId = r.PersonRoleId
+	  OUTER APPLY (SELECT TOP 1 ProjectId FROM ProjectAttachment as pa WHERE pa.ProjectId = p.ProjectId) A
+	  WHERE f.PersonId = @PersonIdLocal AND P.ProjectId <> @DefaultProjectId
+	  GROUP BY f.EntryId,p.ProjectId,p.Name,p.ProjectStatusId,p.ProjectNumber,m.MilestoneId,m.Description,mpe.StartDate,
+			mpe.EndDate,r.Name,A.ProjectId,s.Name,mp.MilestonePersonId
+	  ORDER BY mpe.StartDate
 END
 
