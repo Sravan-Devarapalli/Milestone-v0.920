6052142570dbf663a17d3a694cb70940e3eabd7f
ThulasiramP <ThulasiramP>
2011-11-10 02:39:00 -0500

    Comments - Defect Number: 2925; Defect RecordID: 2921; Server Address: services.logic2020.com; Server Port: 99; Database Name: Practice Management; Defect Summary: Time Entry: Duplication of hours due to resources issue on project; Defect Description: Original e-mail is below. I believe this issue is due to an issue in the Resources tab. PM will not allow a resource to be removed from a milestone if the person has already entered time and notes towards the milestone, which has always been the case, but the issue occurs when a person is added to a milestone more than once for overlapping periods. It is not possible to remove the person from a milestone, even when it is a redundant entry.
    For example:
    The current date is 9/27
    MilestoneA = 9/1-9/30
    PersonA is added as a resource to MilestoneA for 9/1-9/30
    PersonA is also added as a resource to MilestoneA for 9/20-9/30
    PersonA has entered time and notes for MilestoneA from 9/1-9/21
    It is now not possilble to remove the second entry for PersonA from MilestoneA because PM sees time and notes entered through 9/21 and does not allow the removal.
    
    I checked the time report (copied below) which says Kerissa has 121hrs booked but every entry is repeated, so her hours are in fact 60.5 which matches my expectations and what I believe we should bill. The report you (Ivana) have got, gives over 70hrs and contains duplicate entries for 19-21st (totaling 14hrs).

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/UpdateMilestonePersonEntry.proc.sql
@@ -0,0 +1,67 @@
+﻿CREATE PROCEDURE [dbo].[UpdateMilestonePersonEntry]
+(
+	@Id   INT,
+	@UserLogin NVARCHAR(255),
+	@PersonId            INT = NULL,
+	@MilestonePersonId   INT OUT,
+	@StartDate     DATETIME,
+	@EndDate       DATETIME,
+	@HoursPerDay   DECIMAL(4,2),
+	@PersonRoleId  INT,
+	@Amount        DECIMAL(18,2)
+)
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	-- Start logging session
+	EXEC dbo.SessionLogPrepare @UserLogin = @UserLogin
+
+	DECLARE @MilestoneId INT , @OldPersonId INT
+
+	SELECT @MilestoneId = mp.MilestoneId,@OldPersonId = mp.PersonId  FROM dbo.MilestonePerson AS mp
+	INNER JOIN dbo.MilestonePersonEntry AS mpe ON mpe.MilestonePersonId = mp.MilestonePersonId
+	WHERE mp.MilestonePersonId = @MilestonePersonId AND mpe.Id = @Id
+
+
+	UPDATE dbo.MilestonePersonEntry
+	SET  StartDate = @StartDate, EndDate = @EndDate, PersonRoleId=@PersonRoleId, Amount=@Amount, HoursPerDay=@HoursPerDay
+	WHERE Id = @Id          
+	     
+	     
+	IF (@PersonId IS NOT NULL AND @OldPersonId != @PersonId)
+	BEGIN 
+
+		IF NOT EXISTS (SELECT 1 FROM dbo.MilestonePerson WHERE MilestoneId = @MilestoneId AND  PersonId = @PersonId )
+		BEGIN
+
+	
+		    INSERT INTO [dbo].[MilestonePerson]([MilestoneId],[PersonId])
+			VALUES(@MilestoneId,@PersonId)
+
+			SET @MilestonePersonId = SCOPE_IDENTITY()
+
+			UPDATE dbo.MilestonePersonEntry
+			SET MilestonePersonId = @MilestonePersonId
+			WHERE Id=@Id
+
+		END
+		ELSE
+		BEGIN
+
+			SELECT @MilestonePersonId = MilestonePersonId 
+			FROM dbo.MilestonePerson WHERE MilestoneId = @MilestoneId AND  PersonId = @PersonId 
+
+
+			UPDATE dbo.MilestonePersonEntry
+			SET MilestonePersonId = @MilestonePersonId
+			WHERE Id=@Id
+
+		END
+
+	END 
+
+	-- End logging session
+	EXEC dbo.SessionLogUnprepare
+
+END
