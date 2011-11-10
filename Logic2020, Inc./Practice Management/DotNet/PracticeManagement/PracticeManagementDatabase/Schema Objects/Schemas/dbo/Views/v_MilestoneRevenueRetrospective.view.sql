6c23878e0f494967cda3af32acca11bc471b8254
ThulasiramP <ThulasiramP>
2011-11-10 02:40:00 -0500

    Comments - Defect Number: 2925; Defect RecordID: 2921; Server Address: services.logic2020.com; Server Port: 99; Database Name: Practice Management; Defect Summary: Time Entry: Duplication of hours due to resources issue on project; Defect Description: Original e-mail is below. I believe this issue is due to an issue in the Resources tab. PM will not allow a resource to be removed from a milestone if the person has already entered time and notes towards the milestone, which has always been the case, but the issue occurs when a person is added to a milestone more than once for overlapping periods. It is not possible to remove the person from a milestone, even when it is a redundant entry.
    For example:
    The current date is 9/27
    MilestoneA = 9/1-9/30
    PersonA is added as a resource to MilestoneA for 9/1-9/30
    PersonA is also added as a resource to MilestoneA for 9/20-9/30
    PersonA has entered time and notes for MilestoneA from 9/1-9/21
    It is now not possilble to remove the second entry for PersonA from MilestoneA because PM sees time and notes entered through 9/21 and does not allow the removal.
    
    I checked the time report (copied below) which says Kerissa has 121hrs booked but every entry is repeated, so her hours are in fact 60.5 which matches my expectations and what I believe we should bill. The report you (Ivana) have got, gives over 70hrs and contains duplicate entries for 19-21st (totaling 14hrs).

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Views/v_MilestoneRevenueRetrospective.view.sql
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Views/v_MilestoneRevenueRetrospective.view.sql
@@ -5,34 +5,35 @@
 -- Update date:	10-13-2008
 -- Description:	Selects a revenue day-by-day
 -- =============================================
-CREATE VIEW dbo.v_MilestoneRevenueRetrospective
+CREATE VIEW dbo.v_MilestoneRevenueRetrospective--Day level Milestone Amount & hours
 AS
 	SELECT -- Milestones with a fixed amount
 		   m.MilestoneId,
 		   m.ProjectId,
 		   cal.Date,
 		   m.IsHourlyAmount,
-	       ISNULL((m.Amount / (
-							SELECT SUM(HoursPerDay*DayCount)
-							FROM(
-								SELECT MP.MilestoneId,MPE.HoursPerDay,MP.MilestonePersonId,MPE.StartDate,COUNT(*) DayCount
+	       ISNULL((m.Amount/* Milestone fixed amount */ 
+						   / 
+							(   SELECT SUM(HoursPerDay)
 								FROM   MilestonePerson MP  
 								JOIN MilestonePersonEntry  MPE ON MP.MileStonePersonId = MPE.MileStonePersonId
 								JOIN PersonCalendarAuto cal ON cal.Date BETWEEN mpe.Startdate AND ISNULL(mpe.EndDate, m.ProjectedDeliveryDate) AND cal.DayOff=0
-								AND cal.PersonId = mp.PersonId
-								GROUP BY MP.MilestoneId,MPE.HoursPerDay,MP.MilestonePersonId,MPE.StartDate
-								) Temp
-							WHERE  Temp.MileStoneId = m.MilestoneId
-                            ))* ISNULL(d.HoursPerDay, 0),
+								AND cal.PersonId = mp.PersonId 
+								WHERE  mp.MileStoneId = m.MilestoneId
+								GROUP BY MP.MilestoneId
+							
+                            ) /* Milestone Total  Hours */
+							
+							)* ISNULL(d.HoursPerDay, 0)/* Milestone Total  Hours per day*/,
 	              (CASE (DATEDIFF(dd, m.StartDate, m.ProjectedDeliveryDate) + 1)
 	                   WHEN 0 THEN 0
 	                   ELSE m.Amount / (DATEDIFF(dd, m.StartDate, m.ProjectedDeliveryDate) + 1)
-	               END)) AS MilestoneDailyAmount,
+	               END)) AS MilestoneDailyAmount /* ((Milestone fixed amount/Milestone Total  Hours)* Milestone Total  Hours per day)  */,
 		   m.StartDate AS MilestoneStartDate,
 		   m.ProjectedDeliveryDate,
            p.Discount,
 	       prac.PracticeManagerId,
-	       d.HoursPerDay
+	       d.HoursPerDay/* Milestone Total  Hours per day*/
 	  FROM dbo.Milestone AS m
 		   INNER JOIN dbo.Calendar AS cal ON cal.Date BETWEEN m.StartDate AND m.ProjectedDeliveryDate
 	       INNER JOIN dbo.Project AS p ON m.ProjectId = p.ProjectId
@@ -52,7 +53,7 @@ AS
 		   MAX(mp.ProjectedDeliveryDate) AS ProjectedDeliveryDate,
            MAX(p.Discount) AS Discount,
 	       prac.PracticeManagerId,
-	       SUM(mp.HoursPerDay) AS HoursPerDay
+	       SUM(mp.HoursPerDay) AS HoursPerDay/* Milestone Total  Hours per day*/
 	  FROM dbo.v_MilestonePersonSchedule mp
 	       INNER JOIN dbo.Project AS p ON mp.ProjectId = p.ProjectId
 	       INNER JOIN dbo.Practice AS prac ON p.PracticeId = prac.PracticeId
