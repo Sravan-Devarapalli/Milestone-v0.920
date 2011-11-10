4121d2a8c502f947b419ca3a1d89d22fa993ab61
ThulasiramP <ThulasiramP>
2011-11-10 02:39:00 -0500

    Comments - Defect Number: 2925; Defect RecordID: 2921; Server Address: services.logic2020.com; Server Port: 99; Database Name: Practice Management; Defect Summary: Time Entry: Duplication of hours due to resources issue on project; Defect Description: Original e-mail is below. I believe this issue is due to an issue in the Resources tab. PM will not allow a resource to be removed from a milestone if the person has already entered time and notes towards the milestone, which has always been the case, but the issue occurs when a person is added to a milestone more than once for overlapping periods. It is not possible to remove the person from a milestone, even when it is a redundant entry.
    For example:
    The current date is 9/27
    MilestoneA = 9/1-9/30
    PersonA is added as a resource to MilestoneA for 9/1-9/30
    PersonA is also added as a resource to MilestoneA for 9/20-9/30
    PersonA has entered time and notes for MilestoneA from 9/1-9/21
    It is now not possilble to remove the second entry for PersonA from MilestoneA because PM sees time and notes entered through 9/21 and does not allow the removal.
    
    I checked the time report (copied below) which says Kerissa has 121hrs booked but every entry is repeated, so her hours are in fact 60.5 which matches my expectations and what I believe we should bill. The report you (Ivana) have got, gives over 70hrs and contains duplicate entries for 19-21st (totaling 14hrs).

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/MilestoneResourceUpdate.proc.sql
@@ -0,0 +1,119 @@
+﻿CREATE PROCEDURE dbo.MilestoneResourceUpdate
+(
+	@MilestoneId              INT,
+	@StartDate                DATETIME,
+	@ProjectedDeliveryDate    DATETIME,
+	@IsStartDateChangeReflectedForMilestoneAndPersons BIT,
+	@IsEndDateChangeReflectedForMilestoneAndPersons   BIT
+)
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+		DECLARE @TempTable TABLE(MilestonePersonId INT)
+		
+		INSERT INTO @TempTable
+		SELECT   mp.MilestonePersonId
+		FROM [dbo].[MilestonePerson] AS mp
+		INNER JOIN dbo.Person AS p ON mp.PersonId = p.PersonId
+		WHERE  (mp.MilestoneId = @MilestoneId) 
+		  AND  (
+			    (p.TerminationDate < @StartDate) 
+			    OR (p.HireDate > @ProjectedDeliveryDate)
+			   )
+	
+		 
+		DELETE MPE FROM dbo.MilestonePersonEntry MPE
+		JOIN @TempTable Temp ON Temp.MilestonePersonId = MPE.MilestonePersonId
+
+		 
+		DELETE MP FROM dbo.MilestonePerson MP
+		JOIN @TempTable Temp ON Temp.MilestonePersonId = MP.MilestonePersonId
+
+		DECLARE @TempMPE TABLE
+		(MilestonePersonId INT,--StartDate DATETIME,EndDate DATETIME,
+		PersonRoleId INT,Amount DECIMAL(18,2),HoursPerDay DECIMAL(4,2))
+		
+		INSERT INTO @TempMPE(MilestonePersonId,PersonRoleId ,Amount ,HoursPerDay )
+		SELECT MPE.MilestonePersonId,MPE.PersonRoleId,MAX(MPE.Amount),MAX(MPE.HoursPerDay)
+		FROM dbo.MilestonePersonEntry MPE
+		INNER JOIN dbo.MilestonePerson MP ON MP.MilestonePersonId = MPE.MilestonePersonId 
+		WHERE MP.MilestoneId = @MilestoneId 
+			AND ( MPE.StartDate > @ProjectedDeliveryDate OR MPE.EndDate < @StartDate )
+		GROUP BY MPE.MilestonePersonId,MPE.PersonRoleId
+		
+		
+		DELETE  MPE FROM dbo.MilestonePersonEntry MPE
+		JOIN dbo.MilestonePerson MP ON MP.MilestonePersonId = MPE.MilestonePersonId 
+		WHERE MP.MilestoneId = @MilestoneId 
+			AND ( MPE.StartDate > @ProjectedDeliveryDate OR MPE.EndDate < @StartDate )
+			
+		INSERT INTO dbo.MilestonePersonEntry(MilestonePersonId,StartDate,EndDate,PersonRoleId,Amount,Location,HoursPerDay )
+		SELECT TEMPmpe.MilestonePersonId,@StartDate,@StartDate,TEMPmpe.PersonRoleId,TEMPmpe.Amount, NULL, TEMPmpe.HoursPerDay  
+		FROM @TempMPE AS TEMPmpe
+		WHERE NOT EXISTS (SELECT 1 FROM dbo.MilestonePersonEntry MPE
+						 WHERE  TEMPmpe.MilestonePersonId = MPE.MilestonePersonId 
+								AND TEMPmpe.PersonRoleId = MPE.PersonRoleId)
+		
+		DELETE MP FROM dbo.MilestonePerson MP
+		LEFT JOIN dbo.MilestonePersonEntry MPE ON MPE.MilestonePersonId = MP.MilestonePersonId
+		WHERE MPE.MilestonePersonId IS NULL AND MP.MilestoneId = @MilestoneId
+
+
+	    UPDATE mpentry
+			   SET StartDate = CASE
+									 WHEN ( P.HireDate > @StartDate) THEN  P.HireDate
+									 ELSE @StartDate
+								   END
+								
+			  FROM MilestonePersonEntry as mpentry
+			  INNER JOIN 
+						(
+						 SELECT RANK() OVER(PARTITION BY  mpe.MilestonePersonId,ISNULL(mpe.PersonRoleId,0) ORDER BY StartDate) AS RANKnO,
+								mpe.Id,
+								mp.MilestoneId,
+								mp.PersonId
+						 FROM dbo.MilestonePersonEntry AS mpe
+						 INNER JOIN dbo.MilestonePerson AS mp ON mp.MilestonePersonId = mpe.MilestonePersonId
+						 ) AS q ON mpentry.Id = q.Id
+			  INNER JOIN dbo.Person AS p ON p.PersonId = q.PersonId
+
+			  WHERE 1 = q.RANKnO AND q.MilestoneId = @MilestoneId  AND @IsStartDateChangeReflectedForMilestoneAndPersons = 1 
+
+		UPDATE mpentry
+				SET EndDate =  CASE
+									WHEN ( @ProjectedDeliveryDate > p.TerminationDate ) THEN  p.TerminationDate
+									ELSE (@ProjectedDeliveryDate)
+								  END
+			    FROM MilestonePersonEntry as mpentry
+				INNER JOIN 
+					(
+					 SELECT RANK() OVER(PARTITION BY  mpe.MilestonePersonId,ISNULL(mpe.PersonRoleId,0) ORDER BY EndDate DESC) AS RANKnO,
+							mpe.Id,
+							mp.MilestoneId,
+							mp.PersonId
+					 FROM dbo.MilestonePersonEntry AS mpe
+					 INNER JOIN dbo.MilestonePerson AS mp ON mp.MilestonePersonId = mpe.MilestonePersonId
+					 ) AS q ON mpentry.Id = q.Id
+				INNER JOIN dbo.Person AS p ON p.PersonId = q.PersonId
+				WHERE  1 = q.RANKnO AND q.MilestoneId = @MilestoneId AND  @IsEndDateChangeReflectedForMilestoneAndPersons = 1 
+
+	    UPDATE mpe
+			   SET EndDate =  CASE WHEN @IsStartDateChangeReflectedForMilestoneAndPersons =1 THEN 
+								( CASE
+									 WHEN ( mpe.StartDate > mpe.EndDate) THEN  mpe.StartDate
+									 ELSE mpe.EndDate
+								   END
+								) ELSE mpe.EndDate END,
+				  StartDate = CASE WHEN @IsEndDateChangeReflectedForMilestoneAndPersons =1 THEN 
+								( CASE
+									 WHEN ( mpe.StartDate > mpe.EndDate) THEN  mpe.EndDate
+									 ELSE mpe.StartDate
+								   END
+								) ELSE mpe.StartDate END
+			  FROM dbo.MilestonePersonEntry AS mpe
+				   INNER JOIN dbo.MilestonePerson AS mp ON mp.MilestonePersonId = mpe.MilestonePersonId
+				   INNER JOIN dbo.Person AS p ON p.PersonId = mp.PersonId
+			 WHERE mp.MilestoneId = @MilestoneId AND (@IsStartDateChangeReflectedForMilestoneAndPersons = 1 OR @IsEndDateChangeReflectedForMilestoneAndPersons = 1 )
+
+ END
