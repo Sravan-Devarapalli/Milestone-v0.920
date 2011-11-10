fd02854fe544d6a13f63f0b16c3347e586870340
ThulasiramP <ThulasiramP>
2011-11-10 02:40:00 -0500

    Comments - Defect Number: 2925; Defect RecordID: 2921; Server Address: services.logic2020.com; Server Port: 99; Database Name: Practice Management; Defect Summary: Time Entry: Duplication of hours due to resources issue on project; Defect Description: Original e-mail is below. I believe this issue is due to an issue in the Resources tab. PM will not allow a resource to be removed from a milestone if the person has already entered time and notes towards the milestone, which has always been the case, but the issue occurs when a person is added to a milestone more than once for overlapping periods. It is not possible to remove the person from a milestone, even when it is a redundant entry.
    For example:
    The current date is 9/27
    MilestoneA = 9/1-9/30
    PersonA is added as a resource to MilestoneA for 9/1-9/30
    PersonA is also added as a resource to MilestoneA for 9/20-9/30
    PersonA has entered time and notes for MilestoneA from 9/1-9/21
    It is now not possilble to remove the second entry for PersonA from MilestoneA because PM sees time and notes entered through 9/21 and does not allow the removal.
    
    I checked the time report (copied below) which says Kerissa has 121hrs booked but every entry is repeated, so her hours are in fact 60.5 which matches my expectations and what I believe we should bill. The report you (Ivana) have got, gives over 70hrs and contains duplicate entries for 19-21st (totaling 14hrs).

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/MilestonePersonEntriesWithFinancialsByMilestoneId.proc.sql
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/MilestonePersonEntriesWithFinancialsByMilestoneId.proc.sql
@@ -29,9 +29,9 @@ BEGIN
 	                WHERE cal.Date BETWEEN mpe.StartDate AND ISNULL(mpe.EndDate, m.[ProjectedDeliveryDate])
 	                  AND cal.PersonId = mp.PersonId
 	                  AND cal.DayOff = 0), 0) AS ExpectedHours,
-		   mpe.Location,
 		   p.LastName,
-		   p.FirstName
+		   p.FirstName,
+		   mpe.Id
 		  
 	  FROM dbo.MilestonePerson AS mp
 	       INNER JOIN dbo.MilestonePersonEntry AS mpe ON mp.MilestonePersonId = mpe.MilestonePersonId
@@ -44,74 +44,33 @@ BEGIN
 	  GROUP BY mp.MilestonePersonId, p.SeniorityId,mp.PersonId,mpe.StartDate,mpe.EndDate,mpe.PersonRoleId,mpe.Amount,
 	       mpe.HoursPerDay,r.Name,mpe.Location,
 		   p.LastName,
-		   p.FirstName,m.ProjectedDeliveryDate
+		   p.FirstName,m.ProjectedDeliveryDate,mpe.Id
 	 
-
-
 	 ;WITH FinancialsRetro AS 
 	(
-	SELECT f.ProjectId,
+	SELECT f.EntryId,
 		   f.PersonId,
-		   f.MilestoneId,
-		   f.EntryStartDate,
-		   f.Date, 
 		   f.PersonMilestoneDailyAmount,
 		   f.PersonDiscountDailyAmount,
-		   ISNULL(c.FractionOfMargin,0) ProjectSalesCommisionFraction,
 		   (ISNULL(f.PayRate, 0) + ISNULL(f.OverheadRate, 0)+ISNULL(f.BonusRate,0)+ISNULL(f.VacationRate,0)
 		   	+ISNULL(f.RecruitingCommissionRate,0)) SLHR,
 		   ISNULL(f.PayRate,0) PayRate,
 		   f.MLFOverheadRate,
-		   f.PersonHoursPerDay,
-		   f.PracticeManagementCommissionSub,
-		   f.PracticeManagementCommissionOwn ,
-		   f.PracticeManagerId,
-		   f.Discount
+		   f.PersonHoursPerDay
 	FROM v_FinancialsRetrospective f
-	LEFT JOIN (
-					SELECT ProjectId,SUM(FractionOfMargin) FractionOfMargin
-					FROM dbo.Commission 
-					WHERE CommissionType = 1
-					GROUP BY ProjectId
-		) C ON C.ProjectId = f.ProjectId
 	WHERE f.MilestoneId = @MilestoneId  
 	)
 
-	SELECT f.ProjectId,
-			f.MilestoneId,
-		   f.PersonId,
-		   f.EntryStartDate StartDate,   
-	       dbo.MakeDate(YEAR(MIN(f.Date)), MONTH(MIN(f.Date)), 1) AS FinancialDate,
-	       dbo.MakeDate(YEAR(MIN(f.Date)), MONTH(MIN(f.Date)), dbo.GetDaysInMonth(MIN(f.Date))) AS MonthEnd,
-
-	       ISNULL(SUM(f.PersonMilestoneDailyAmount), 0) AS Revenue,
-
-	       ISNULL(SUM(f.PersonMilestoneDailyAmount - f.PersonDiscountDailyAmount), 0) AS RevenueNet,
-
-		   ISNULL(SUM(f.PersonMilestoneDailyAmount - f.PersonDiscountDailyAmount -
+	SELECT  f.EntryId,
+			f.PersonId,
+			ISNULL(SUM(f.PersonMilestoneDailyAmount), 0) AS Revenue,
+			ISNULL(SUM(f.PersonMilestoneDailyAmount - f.PersonDiscountDailyAmount -
 						(CASE WHEN f.SLHR >= f.PayRate + f.MLFOverheadRate 
-							  THEN f.SLHR ELSE ISNULL(f.PayRate, 0)+f.MLFOverheadRate END) 
-					    *ISNULL(f.PersonHoursPerDay, 0)), 0) GrossMargin,
-		   
-		   ISNULL(SUM((CASE WHEN f.SLHR >=  f.PayRate +f.MLFOverheadRate 
-					 THEN f.SLHR ELSE  f.PayRate +f.MLFOverheadRate END)*ISNULL(f.PersonHoursPerDay, 0)), 0) Cogs,
+							  THEN f.SLHR 
+							  ELSE ISNULL(f.PayRate, 0)+f.MLFOverheadRate END) 
+					    *ISNULL(f.PersonHoursPerDay, 0)), 0) GrossMargin
 
-	       ISNULL(SUM(f.PersonHoursPerDay), 0) AS Hours,
-	       
-	       (SUM((f.PersonMilestoneDailyAmount - f.PersonDiscountDailyAmount -
-						(CASE WHEN f.SLHR >=  f.PayRate +f.MLFOverheadRate 
-							  THEN f.SLHR  ELSE f.PayRate +f.MLFOverheadRate END) 
-					    *ISNULL(f.PersonHoursPerDay, 0))* (f.ProjectSalesCommisionFraction/100))) SalesCommission,
-
-	       SUM((f.PersonMilestoneDailyAmount - f.PersonDiscountDailyAmount -
-	            (CASE WHEN f.SLHR >=  f.PayRate +f.MLFOverheadRate 
-							  THEN f.SLHR ELSE f.PayRate +f.MLFOverheadRate END) * ISNULL(f.PersonHoursPerDay, 0)) *
-	           (f.PracticeManagementCommissionSub + CASE f.PracticeManagerId WHEN f.PersonId THEN f.PracticeManagementCommissionOwn ELSE 0 END)) / 100 AS PracticeManagementCommission,
-	           0.0 AS 'actualhours',
-	           0.0 AS 'forecastedhours',
-           ISNULL(SUM(vac.VacationHours), 0) AS 'VacationHours'
-		   
 	  FROM FinancialsRetro AS f
-	  LEFT JOIN dbo.v_MilestonePersonVacations AS vac ON f.MilestoneId = vac.MilestoneId AND f.PersonId = vac.PersonId
-	  GROUP BY   f.ProjectId, f.MilestoneId, f.PersonId,f.EntryStartDate
+	  GROUP BY   f.EntryId,f.PersonId
 END
+
