ef9adfdb0bb7bf908ef9e3332b3a8c1efbcec56c
ThulasiramP <ThulasiramP>
2011-11-10 02:40:00 -0500

    Comments - Defect Number: 2925; Defect RecordID: 2921; Server Address: services.logic2020.com; Server Port: 99; Database Name: Practice Management; Defect Summary: Time Entry: Duplication of hours due to resources issue on project; Defect Description: Original e-mail is below. I believe this issue is due to an issue in the Resources tab. PM will not allow a resource to be removed from a milestone if the person has already entered time and notes towards the milestone, which has always been the case, but the issue occurs when a person is added to a milestone more than once for overlapping periods. It is not possible to remove the person from a milestone, even when it is a redundant entry.
    For example:
    The current date is 9/27
    MilestoneA = 9/1-9/30
    PersonA is added as a resource to MilestoneA for 9/1-9/30
    PersonA is also added as a resource to MilestoneA for 9/20-9/30
    PersonA has entered time and notes for MilestoneA from 9/1-9/21
    It is now not possilble to remove the second entry for PersonA from MilestoneA because PM sees time and notes entered through 9/21 and does not allow the removal.
    
    I checked the time report (copied below) which says Kerissa has 121hrs booked but every entry is repeated, so her hours are in fact 60.5 which matches my expectations and what I believe we should bill. The report you (Ivana) have got, gives over 70hrs and contains duplicate entries for 19-21st (totaling 14hrs).

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/MilestoneUpdate.proc.sql
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/MilestoneUpdate.proc.sql
@@ -1,11 +1,4 @@
-﻿-- =============================================
--- Author:		Anatoliy Lokshin
--- Create date: 5-22-2008
--- Updated by:	Anatoliy Lokshin
--- Update date:	11-27-2008
--- Description:	Updates a Milestone
--- =============================================
-CREATE PROCEDURE dbo.MilestoneUpdate
+﻿CREATE PROCEDURE dbo.MilestoneUpdate
 (
 	@MilestoneId              INT,
 	@ProjectId                INT,
@@ -17,20 +10,11 @@ CREATE PROCEDURE dbo.MilestoneUpdate
 	@IsHourlyAmount           BIT,
 	@UserLogin                NVARCHAR(255),
 	@ConsultantsCanAdjust	  BIT,
-	@IsChargeable			  BIT,
-	@IsStartDateChangeReflectedForMilestoneAndPersons BIT,
-	@IsEndDateChangeReflectedForMilestoneAndPersons   BIT
+	@IsChargeable			  BIT
 )
 AS
 BEGIN
 	SET NOCOUNT ON;
-
-	DECLARE @IsStartOrEndDateChanged BIT  = 0
-
-	IF NOT EXISTS(SELECT 1 FROM dbo.Milestone as m WHERE m.MilestoneId = @MilestoneId AND m.StartDate = @StartDate AND m.ProjectedDeliveryDate = @ProjectedDeliveryDate)
-	BEGIN
-	SET @IsStartOrEndDateChanged = 1
-	END
 
 	-- Start logging session
 	EXEC dbo.SessionLogPrepare @UserLogin = @UserLogin
@@ -47,71 +31,6 @@ BEGIN
 	       IsChargeable = @IsChargeable,
 	       ConsultantsCanAdjust = @ConsultantsCanAdjust
 	 WHERE MilestoneId = @MilestoneId
-
-
-	 IF(@IsStartOrEndDateChanged = 1)
-	 BEGIN
-	 
-		DECLARE @TempTable TABLE(MilestonePersonId INT)
-		
-		INSERT INTO @TempTable
-		SELECT   mp.MilestonePersonId
-		FROM [dbo].[MilestonePerson] AS mp
-		INNER JOIN dbo.Person AS p ON mp.PersonId = p.PersonId
-		WHERE  (mp.MilestoneId = @MilestoneId) 
-		  AND  (
-			    (p.TerminationDate < @StartDate) 
-			    OR (p.HireDate > @ProjectedDeliveryDate)
-			   )
-	
-		 
-		DELETE MPE FROM dbo.MilestonePersonEntry MPE
-		JOIN @TempTable Temp ON Temp.MilestonePersonId = MPE.MilestonePersonId
-
-		 
-		DELETE MP FROM dbo.MilestonePerson MP
-		JOIN @TempTable Temp ON Temp.MilestonePersonId = MP.MilestonePersonId
-
-
-	    UPDATE mpe
-			   SET StartDate =  CASE WHEN @IsStartDateChangeReflectedForMilestoneAndPersons =1 THEN 
-								( CASE
-									 WHEN ( P.HireDate > @StartDate) THEN  P.HireDate
-									 ELSE @StartDate
-								   END
-								) ELSE StartDate END,
-				  EndDate = CASE WHEN  @IsEndDateChangeReflectedForMilestoneAndPersons = 1 THEN
-								( CASE
-									WHEN ( @ProjectedDeliveryDate > p.TerminationDate ) THEN  p.TerminationDate
-									ELSE (@ProjectedDeliveryDate)
-								  END
-								) 
-								ELSE EndDate END
-			  FROM dbo.MilestonePersonEntry AS mpe
-				   INNER JOIN dbo.MilestonePerson AS mp ON mp.MilestonePersonId = mpe.MilestonePersonId
-				   INNER JOIN dbo.Person AS p ON p.PersonId = mp.PersonId
-			 WHERE mp.MilestoneId = @MilestoneId AND (@IsStartDateChangeReflectedForMilestoneAndPersons = 1 OR @IsEndDateChangeReflectedForMilestoneAndPersons = 1 )
-
-	    UPDATE mpe
-			   SET EndDate =  CASE WHEN @IsStartDateChangeReflectedForMilestoneAndPersons =1 THEN 
-								( CASE
-									 WHEN ( mpe.StartDate > mpe.EndDate) THEN  mpe.StartDate
-									 ELSE mpe.EndDate
-								   END
-								) ELSE mpe.EndDate END,
-				  StartDate = CASE WHEN @IsEndDateChangeReflectedForMilestoneAndPersons =1 THEN 
-								( CASE
-									 WHEN ( mpe.StartDate > mpe.EndDate) THEN  mpe.EndDate
-									 ELSE mpe.StartDate
-								   END
-								) ELSE mpe.StartDate END
-			  FROM dbo.MilestonePersonEntry AS mpe
-				   INNER JOIN dbo.MilestonePerson AS mp ON mp.MilestonePersonId = mpe.MilestonePersonId
-				   INNER JOIN dbo.Person AS p ON p.PersonId = mp.PersonId
-			 WHERE mp.MilestoneId = @MilestoneId AND (@IsStartDateChangeReflectedForMilestoneAndPersons = 1 OR @IsEndDateChangeReflectedForMilestoneAndPersons = 1 )
-
-	  END
-
 
 	-- End logging session
 	EXEC dbo.SessionLogUnprepare
