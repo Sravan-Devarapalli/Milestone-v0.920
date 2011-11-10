abbd7fc297fba390242d321bf06e585e92ab8f5b
ThulasiramP <ThulasiramP>
2011-11-10 02:39:00 -0500

    Comments - Defect Number: 2925; Defect RecordID: 2921; Server Address: services.logic2020.com; Server Port: 99; Database Name: Practice Management; Defect Summary: Time Entry: Duplication of hours due to resources issue on project; Defect Description: Original e-mail is below. I believe this issue is due to an issue in the Resources tab. PM will not allow a resource to be removed from a milestone if the person has already entered time and notes towards the milestone, which has always been the case, but the issue occurs when a person is added to a milestone more than once for overlapping periods. It is not possible to remove the person from a milestone, even when it is a redundant entry.
    For example:
    The current date is 9/27
    MilestoneA = 9/1-9/30
    PersonA is added as a resource to MilestoneA for 9/1-9/30
    PersonA is also added as a resource to MilestoneA for 9/20-9/30
    PersonA has entered time and notes for MilestoneA from 9/1-9/21
    It is now not possilble to remove the second entry for PersonA from MilestoneA because PM sees time and notes entered through 9/21 and does not allow the removal.
    
    I checked the time report (copied below) which says Kerissa has 121hrs booked but every entry is repeated, so her hours are in fact 60.5 which matches my expectations and what I believe we should bill. The report you (Ivana) have got, gives over 70hrs and contains duplicate entries for 19-21st (totaling 14hrs).

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/MilestonePersonEntryWithFinancials.proc.sql
@@ -0,0 +1,78 @@
+﻿CREATE PROCEDURE [dbo].[MilestonePersonEntryWithFinancials]
+(
+	@Id   INT
+)
+AS
+BEGIN
+
+SELECT 
+	CASE COUNT(te.TimeEntryId)
+	           WHEN 0
+	           THEN  CONVERT(BIT,0)
+	           ELSE
+	               CONVERT(BIT,1)
+	       END AS HasTimeEntries,
+	       mp.MilestonePersonId,
+		   p.SeniorityId,
+           mp.PersonId,
+	       mpe.StartDate,
+	       mpe.EndDate,
+	       mpe.PersonRoleId,
+	       mpe.Amount,
+	       mpe.HoursPerDay,
+	       r.Name AS RoleName,
+		   ISNULL((SELECT COUNT(*)
+				FROM dbo.v_PersonCalendar AS pcal
+				WHERE pcal.DayOff = 1 AND pcal.CompanyDayOff = 0 
+					AND pcal.Date BETWEEN mpe.StartDate AND ISNULL(mpe.EndDate, m.[ProjectedDeliveryDate])
+					AND pcal.PersonId = mp.PersonId ),0) as VacationDays,	
+	       ISNULL((SELECT COUNT(*) * mpe.HoursPerDay
+	                 FROM dbo.PersonCalendarAuto AS cal
+	                WHERE cal.Date BETWEEN mpe.StartDate AND ISNULL(mpe.EndDate, m.[ProjectedDeliveryDate])
+	                  AND cal.PersonId = mp.PersonId
+	                  AND cal.DayOff = 0), 0) AS ExpectedHours,
+		   p.LastName,
+		   p.FirstName,
+		   mpe.Id
+		  
+	  FROM dbo.MilestonePerson AS mp
+	       INNER JOIN dbo.MilestonePersonEntry AS mpe ON mp.MilestonePersonId = mpe.MilestonePersonId
+	       INNER JOIN dbo.Milestone AS m ON mp.MilestoneId = m.MilestoneId
+	       INNER JOIN dbo.Person AS p ON mp.PersonId = p.PersonId
+	       LEFT JOIN dbo.PersonRole AS r ON mpe.PersonRoleId = r.PersonRoleId
+	       LEFT JOIN dbo.TimeEntries as te on te.MilestonePersonId = mp.MilestonePersonId
+		  AND (te.MilestoneDate BETWEEN mpe.StartDate AND  mpe.EndDate)
+	  WHERE mpe.Id = @Id
+	  GROUP BY mp.MilestonePersonId, p.SeniorityId,mp.PersonId,mpe.StartDate,mpe.EndDate,mpe.PersonRoleId,mpe.Amount,
+	       mpe.HoursPerDay,r.Name,mpe.Location,
+		   p.LastName,
+		   p.FirstName,m.ProjectedDeliveryDate,mpe.Id
+	 
+
+
+	 ;WITH FinancialsRetro AS 
+	(
+	SELECT f.EntryId,
+		   f.PersonMilestoneDailyAmount,
+		   f.PersonDiscountDailyAmount,
+		   (ISNULL(f.PayRate, 0) + ISNULL(f.OverheadRate, 0)+ISNULL(f.BonusRate,0)+ISNULL(f.VacationRate,0)
+		   	+ISNULL(f.RecruitingCommissionRate,0)) SLHR,
+		   ISNULL(f.PayRate,0) PayRate,
+		   f.MLFOverheadRate,
+		   f.PersonHoursPerDay
+	FROM v_FinancialsRetrospective f
+	WHERE f.EntryId = @Id  
+	)
+
+	SELECT  f.EntryId,
+			ISNULL(SUM(f.PersonMilestoneDailyAmount), 0) AS Revenue,
+			ISNULL(SUM(f.PersonMilestoneDailyAmount - f.PersonDiscountDailyAmount -
+						(CASE WHEN f.SLHR >= f.PayRate + f.MLFOverheadRate 
+							  THEN f.SLHR 
+							  ELSE ISNULL(f.PayRate, 0)+f.MLFOverheadRate END) 
+					    *ISNULL(f.PersonHoursPerDay, 0)), 0) GrossMargin
+
+	  FROM FinancialsRetro AS f
+	  GROUP BY   f.EntryId
+
+END
