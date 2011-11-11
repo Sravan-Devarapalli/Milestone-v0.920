bb18669cf29e5365805b503afd5da72e64cbf38c
SainathC <SainathC>
2011-11-11 04:41:00 -0500

    Comments - Made Changes according to the mail sent by Jason on Date:Fri, Nov 11, 2011 at 6:16 AM PST.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/DashBoard.aspx.designer.cs
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/DashBoard.aspx.designer.cs
@@ -254,6 +254,15 @@ namespace PraticeManagement {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::PraticeManagement.Controls.Generic.LoadingProgress lpDashBoard;
+        
+        /// <summary>
+        /// lblCurrentVersion control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblCurrentVersion;
     }
 }
 
