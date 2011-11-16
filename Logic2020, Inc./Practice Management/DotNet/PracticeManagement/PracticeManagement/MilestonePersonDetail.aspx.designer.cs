e546b75d036842f1046fe1ac364cd7290ba5d72c
ThulasiramP <ThulasiramP>
2011-11-16 07:10:00 -0500

    Comments - Defect Number: 2954; Defect RecordID: 2950; Server Address: services.logic2020.com; Server Port: 99; Database Name: Practice Management; Defect Summary: Project>Milestone: Allow "Strawman" resources to be listed multiple times with overlapping start/end dates; Defect Description: Allow a project to contain multiple "Strawman" resources of the same name/type to be added to a project even if the "Strawman" resource has the same Start Date/End Date timeframe.
    The idea here is that "Strawman" resources are placeholder resources for the project team. Therefore, I may need to display that I need 3 Data Analyst Strawman resources for overlapping time periods within a Milestone. Unlike normal "Person" records, we need the business logic relaxed for those where isStrawman = yes that typically restricts the same "Person" from being added to a Project Milestone with overlapping data/time ranges.
    
    
    E.g.
    
    Within a Project and within a Milestone, I should be able to see the following
    
    Person NameStart DateEnd Date
    Jr. Data Analyst Strawman11/1/201112/1/2011
    Jr. Data Analyst Strawman11/1/201112/1/2011
    Jr. Data Analyst Strawman11/15/201111/22/2011
    Matt Reilly11/1/201112/1/2011

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/MilestonePersonDetail.aspx.designer.cs
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/MilestonePersonDetail.aspx.designer.cs
@@ -148,13 +148,22 @@ namespace PraticeManagement {
         protected global::System.Web.UI.WebControls.HiddenField hdnPersonId;
         
         /// <summary>
+        /// hdnPersonIsStrawMan control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HiddenField hdnPersonIsStrawMan;
+        
+        /// <summary>
         /// ddlPersonName control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.DropDownList ddlPersonName;
+        protected global::PraticeManagement.Controls.CustomDropDown ddlPersonName;
         
         /// <summary>
         /// cvMaxRows control.
