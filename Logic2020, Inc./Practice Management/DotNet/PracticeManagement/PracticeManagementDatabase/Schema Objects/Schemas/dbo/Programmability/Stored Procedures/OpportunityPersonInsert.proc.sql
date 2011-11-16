9ef8ed266c105e428e9576bf357a7221f7526f12
AshokD <AshokD>
2011-11-16 08:56:00 -0500

    Comments - Defect Number: 2944; Defect RecordID: 2940; Server Address: services.logic2020.com; Server Port: 99; Database Name: Practice Management; Defect Summary: Opportunity Summary: Change "Proposed Resources" to "Team Structure"; adjust functionality; Defect Description: 1. Change the "Proposed Resources" column on the Opportunity Summary page to "Team Structure" 2. Using a very similar interface to what we already have, create a second selection panel called "Team Structure" where persons marked as "Strawman" (via the checkbox added to the person record) are listed alphabetically. Each row should have a QTY (quantity) selector dropdown box (1-10 for now). This selection will be displayed on the Summary page with named resources (left-hand selection panel) above the Strawman selections, example:
    LastnameA, FirstnameA
    LasntameB, FirstnameB
    Analyst (QTY)
    Consultant (QTY)
    
    
    3.) Need to provide a way to remove a single row in case the Team Structure changes (e.g. If the original Team Structure was saved with 1 Analyst and 1 Consultant, we need a way to show that the Team Structure has changed to only require 1 Consultant). I envision the "X" button or something akin to what we use in the Proposed Resources window when we want to remove a resource.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/OpportunityPersonInsert.proc.sql
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/OpportunityPersonInsert.proc.sql
@@ -16,22 +16,31 @@ BEGIN
 	(
 	PersonId INT,
 	PersonType INT,
-	Quantity INT
+	Quantity INT,
+	NeedBy  DATETIME
 	)
 
 	IF(@RelationTypeId = 1) --Proposed Resources
 	BEGIN
-		IF(ISNULL(@PersonIdList,'')<>'')
+		IF(@PersonIdList IS NOT NULL)
 		BEGIN
 			INSERT INTO @OpportunityPersonIdsWithTypeTable(PersonId,PersonType)
 			SELECT ResultId ,ResultType
 			FROM [dbo].[ConvertStringListIntoTableWithTwoColoumns] (@PersonIdListLocal)
+
+			DELETE op
+			FROM dbo.OpportunityPersons  op
+			LEFT JOIN @OpportunityPersonIdsWithTypeTable AS p 
+			ON op.OpportunityId = @OpportunityId AND op.PersonId = p.PersonId AND op.OpportunityPersonTypeId=p.PersonType
+			WHERE p.PersonId IS NULL and OP.OpportunityId = @OpportunityId AND op.RelationTypeId = @RelationTypeId
+
+			INSERT INTO OpportunityPersons(OpportunityId,PersonId,OpportunityPersonTypeId)
+			SELECT @OpportunityId ,p.PersonId,p.PersonType
+			FROM @OpportunityPersonIdsWithTypeTable AS p 
+			LEFT JOIN dbo.OpportunityPersons op
+			ON p.PersonId = op.PersonId AND op.OpportunityId=@OpportunityId AND op.OpportunityPersonTypeId=p.PersonType
+			WHERE op.PersonId IS NULL
 		END
-	
-		--UPDATE dbo.Opportunity
-		--SET OutSideResources = @OutSideResources,
-		--	LastUpdated = GETDATE()
-		--WHERE OpportunityId = @OpportunityId
 	END
 	ELSE
 	BEGIN
@@ -40,40 +49,32 @@ BEGIN
 			DECLARE @PersonIdListLocalXML XML
 			IF(SUBSTRING(@PersonIdListLocal,LEN(@PersonIdListLocal),1)=',')
 			SET @PersonIdListLocal = SUBSTRING(@PersonIdListLocal,1,LEN(@PersonIdListLocal)-1)
-			SET @PersonIdListLocal = '<root><item><personid>'+@PersonIdListLocal+'</qty></item></root>'
+			SET @PersonIdListLocal = '<root><item><personid>'+@PersonIdListLocal+'</needby></item></root>'
 
 			SET @PersonIdListLocal = REPLACE(@PersonIdListLocal,':','</personid><persontypeid>')
 			SET @PersonIdListLocal = REPLACE(@PersonIdListLocal,'|','</persontypeid><qty>')
-			SET @PersonIdListLocal = REPLACE(@PersonIdListLocal,',','</qty></item><item><personid>')
+			SET @PersonIdListLocal = REPLACE(@PersonIdListLocal,'?','</qty><needby>')
+			SET @PersonIdListLocal = REPLACE(@PersonIdListLocal,',','</needby></item><item><personid>')
 			
-			SELECT @PersonIdListLocal
 			SELECT @PersonIdListLocalXML  = CONVERT(XML,@PersonIdListLocal)
 
 			INSERT INTO @OpportunityPersonIdsWithTypeTable
 			(PersonId ,
 			PersonType ,
-			Quantity )
+			Quantity,
+			NeedBy)
 			SELECT C.value('personid[1]','int') personid,
 					C.value('persontypeid[1]','int') persontypeid,
-					C.value('qty[1]','int') qty
+					C.value('qty[1]','int') qty,
+					C.value('needby[1]','DATETIME') needby
 			FROM @PersonIdListLocalXML.nodes('/root/item') as T(C)
 		END
 
-	END
-	IF @PersonIdList IS NOT NULL
-	BEGIN
-			DELETE op
-			FROM dbo.OpportunityPersons  op
-			LEFT JOIN @OpportunityPersonIdsWithTypeTable AS p 
-			ON op.OpportunityId = @OpportunityId AND op.PersonId = p.PersonId AND op.OpportunityPersonTypeId=p.PersonType 
-			WHERE p.PersonId IS NULL and OP.OpportunityId = @OpportunityId
-			AND op.RelationTypeId = @RelationTypeId
-
-			INSERT INTO OpportunityPersons(OpportunityId,PersonId,OpportunityPersonTypeId,RelationTypeId,Quantity)
-			SELECT @OpportunityId ,p.PersonId,p.PersonType,@RelationTypeId,p.Quantity
-			FROM @OpportunityPersonIdsWithTypeTable AS p 
-			LEFT JOIN dbo.OpportunityPersons op
-			ON p.PersonId = op.PersonId AND op.OpportunityId=@OpportunityId AND op.OpportunityPersonTypeId=p.PersonType
-			WHERE op.PersonId IS NULL 
+		DELETE FROM dbo.OpportunityPersons
+		WHERE opportunityId = @OpportunityId AND RelationTypeId = @RelationTypeId
+			
+		INSERT INTO OpportunityPersons(OpportunityId,PersonId,OpportunityPersonTypeId,RelationTypeId,Quantity,NeedBy)
+		SELECT @OpportunityId ,p.PersonId,p.PersonType,@RelationTypeId,p.Quantity,p.NeedBy
+		FROM @OpportunityPersonIdsWithTypeTable AS p 
    END
 END
