9aa1bd29b2b1d5bc415b2134b4df22226e033b90
SrinivasM <SrinivasM>
2011-11-18 07:12:00 -0500

    Comments - Defect Number: 2944; Defect RecordID: 2940; Server Address: services.logic2020.com; Server Port: 99; Database Name: Practice Management; Defect Summary: Opportunity Summary: Change "Proposed Resources" to "Team Structure"; adjust functionality; Defect Description: 1. Change the "Proposed Resources" column on the Opportunity Summary page to "Team Structure" 2. Using a very similar interface to what we already have, create a second selection panel called "Team Structure" where persons marked as "Strawman" (via the checkbox added to the person record) are listed alphabetically. Each row should have a QTY (quantity) selector dropdown box (1-10 for now). This selection will be displayed on the Summary page with named resources (left-hand selection panel) above the Strawman selections, example:
    LastnameA, FirstnameA
    LasntameB, FirstnameB
    Analyst (QTY)
    Consultant (QTY)
    
    
    3.) Need to provide a way to remove a single row in case the Team Structure changes (e.g. If the original Team Structure was saved with 1 Analyst and 1 Consultant, we need a way to show that the Team Structure has changed to only require 1 Consultant). I envision the "X" button or something akin to what we use in the Proposed Resources window when we want to remove a resource.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/OpportunityDetail.aspx.designer.cs
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/OpportunityDetail.aspx.designer.cs
@@ -301,6 +301,15 @@ namespace PraticeManagement {
         protected global::System.Web.UI.WebControls.CustomValidator cvPriority;
         
         /// <summary>
+        /// cvLinkedToProject control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.CustomValidator cvLinkedToProject;
+        
+        /// <summary>
         /// ddlClientGroup control.
         /// </summary>
         /// <remarks>
