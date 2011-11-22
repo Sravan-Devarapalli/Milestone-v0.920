eaab67290e15aa54c3d5e5d759d07224ba8f2781
SainathC <SainathC>
2011-11-22 08:56:00 -0500

    Comments - Made changes for better UI in the SkillsSearch Page.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/SkillsSearch.aspx.designer.cs
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/SkillsSearch.aspx.designer.cs
@@ -13,15 +13,6 @@ namespace PraticeManagement {
     public partial class SkillsSearch {
         
         /// <summary>
-        /// pnlSearch control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Panel pnlSearch;
-        
-        /// <summary>
         /// lblSearchTitle control.
         /// </summary>
         /// <remarks>
@@ -29,6 +20,15 @@ namespace PraticeManagement {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Label lblSearchTitle;
+        
+        /// <summary>
+        /// pnlSearch control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Panel pnlSearch;
         
         /// <summary>
         /// txtSearch control.
@@ -110,6 +110,15 @@ namespace PraticeManagement {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Label lblSearchcriteria;
+        
+        /// <summary>
+        /// dlPersonDiv control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl dlPersonDiv;
         
         /// <summary>
         /// dlPerson control.
