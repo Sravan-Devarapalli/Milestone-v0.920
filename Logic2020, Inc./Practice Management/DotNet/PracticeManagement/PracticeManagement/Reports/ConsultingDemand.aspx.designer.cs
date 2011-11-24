8a768ac6d596ff576820ab1fab43d2fba185a3e0
SrinivasM <SrinivasM>
2011-11-24 01:34:00 -0500

    Comments - Defect Number: 2956; Defect RecordID: 2952; Server Address: services.logic2020.com; Server Port: 99; Database Name: Practice Management; Defect Summary: New Report: Consulting Demand Report ; Defect Description: We would like to create a new Report called, "Consulting Demand". See the attached documentation and mockup for a description of the requirements of this report.
    Please review and let's discuss if you have any questions, comments, concerns, etc.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/Reports/ConsultingDemand.aspx.designer.cs
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/Reports/ConsultingDemand.aspx.designer.cs
@@ -229,13 +229,13 @@ namespace PraticeManagement.Reporting {
         protected global::System.Web.UI.WebControls.ValidationSummary valSum;
         
         /// <summary>
-        /// lblPeriodRange control.
+        /// updReport control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Label lblPeriodRange;
+        protected global::System.Web.UI.UpdatePanel updReport;
         
         /// <summary>
         /// chartDiv control.
