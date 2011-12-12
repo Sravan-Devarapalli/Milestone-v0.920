61f0f422d9f8741e2d9a75c87b5c30482ef99fae
ThulasiramP <ThulasiramP>
2011-12-12 09:56:00 -0500

    Comments - Defect Number: 2969; Defect RecordID: 2965; Server Address: services.logic2020.com; Server Port: 99; Database Name: Practice Management; Defect Summary: Reports->Time->Time Entry by person: Exported Excel format needs to be formatted. See Attached excel sheet for the new format.; Defect Description: The format of the Exported Excel report should be formatted same as the format in the attached Excel sheet.
    Email sent by Jason for this issue:
    
    From: Jason Hong-Turney [mailto:jasonht@logic2020.com]
    Sent: Tuesday, December 06, 2011 6:08 AM
    To: MadhanA (madhana@ggktech.com)
    Subject: Timeout issues in TE by Person report
    
    Hi Madhan,
    
    Im trying to run a report for all W2-Salary employees, not in the Admin practice, from 1/1/11 to 11/30/11, but the report times out (also, unselecting filters results in undesired names still appearing both in the person dropdown list as well as the report output). I did manage to get the report to run once, but was completely unable to export the results to Excel.
    
    Also, it would be ideal for the Excel output to be not formatted like a report (leave that to PDF), but instead formatted like a true table.
    
    As soon as we can have this working, please update PM.
    
    Jason Hong-Turney // Logic20/20 // jasonht@logic2020.com
    t.206-576-0472 // c. 425-390-4664

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/TimeEntriesGetByPersonsForExcel.proc.sql
@@ -0,0 +1,52 @@
+﻿CREATE PROCEDURE [dbo].[TimeEntriesGetByPersonsForExcel]
+	@PersonIds	NVARCHAR(MAX),
+	@StartDate	DATETIME = NULL,
+	@EndDate	DATETIME = NULL,
+	@TimescaleIds NVARCHAR(4000),
+	@PracticeIds  NVARCHAR(MAX)
+	
+AS
+BEGIN
+    
+	DECLARE @PersonList TABLE (Id INT)
+	INSERT INTO @PersonList
+	SELECT * FROM dbo.ConvertStringListIntoTable(@PersonIds)
+	
+	DECLARE @TimescaleIdList TABLE (Id INT)
+	INSERT INTO @TimescaleIdList
+	SELECT * FROM dbo.ConvertStringListIntoTable(@TimescaleIds)
+	
+	DECLARE @PracticeIdsList TABLE (Id INT)
+	INSERT INTO @PracticeIdsList
+	SELECT * FROM dbo.ConvertStringListIntoTable(@PracticeIds)
+
+	SELECT DISTINCT 
+		   p.LastName +', ' +p.FirstName Name,
+		   proj.ProjectNumber AS 'P#',
+		   c.Name Client,
+		   proj.Name AS 'Project Name',
+		   m.Description AS Milestone,
+		   tt.Name TimeType,
+		   te.Note Note,
+		   CONVERT(VARCHAR(10), te.MilestoneDate, 1)  AS 'Date',
+		   te.ActualHours AS 'Hours'
+	FROM dbo.Person p
+	JOIN dbo.MilestonePerson mp on mp.PersonId = p.PersonId
+	JOIN dbo.MilestonePersonEntry mpe on mpe.MilestonePersonId = mp.MilestonePersonId
+	JOIN dbo.Milestone m on m.MilestoneId = mp.MilestoneId
+	JOIN dbo.Project proj on proj.ProjectId = m.ProjectId
+	JOIN dbo.Client c on proj.ClientId = c.ClientId
+	JOIN dbo.TimeEntries te on te.MilestonePersonId = mp.MilestonePersonId
+									AND te.MilestoneDate BETWEEN ISNULL(@StartDate, te.MilestoneDate) AND ISNULL(@EndDate, te.MilestoneDate)
+	LEFT JOIN dbo.TimeType tt on tt.TimeTypeId = te.TimeTypeId
+	WHERE p.PersonId IN (select Id from @PersonList)
+		AND	(ISNULL(@StartDate,Mpe.StartDate) >= mpe.StartDate AND ISNULL(@EndDate,mpe.EndDate) <= mpe.EndDate
+			OR ISNULL(@StartDate,Mpe.StartDate - 1) < mpe.StartDate AND ISNULL(@EndDate,mpe.StartDate) >= mpe.StartDate
+			OR ISNULL(@StartDate,Mpe.EndDate) <= mpe.EndDate AND ISNULL(@EndDate,mpe.EndDate + 1) >= mpe.EndDate
+			)
+		AND (dbo.GetCurrentPayType(p.PersonId) IN (select Id from @TimescaleIdList)  OR dbo.GetCurrentPayType(p.PersonId) IS NULL)
+		AND p.DefaultPractice IN (SELECT id FROM @PracticeIdsList)
+		AND (te.TimeEntryId IS NOT NULL OR proj.ProjectStatusId = 3) --For getting only active projects or there are timeentries for projects.
+	ORDER BY Name ,[Date], proj.ProjectNumber 
+
+END
