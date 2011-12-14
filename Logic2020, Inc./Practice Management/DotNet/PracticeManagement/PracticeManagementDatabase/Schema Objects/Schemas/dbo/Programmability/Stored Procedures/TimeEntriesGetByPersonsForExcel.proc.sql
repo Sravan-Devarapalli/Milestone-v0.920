90869c92b1fa572aa85e46454b37ba27d5b27065
ThulasiramP <ThulasiramP>
2011-12-14 08:42:00 -0500

    Comments - Defect Number: 2962; Defect RecordID: 2958; Server Address: services.logic2020.com; Server Port: 99; Database Name: Practice Management; Defect Summary: Reports > Time > Time Entry by Person : Filters not working; Defect Description: The Pay Type filter is definitely not working on this page. I have not fully tested the other filters.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/TimeEntriesGetByPersonsForExcel.proc.sql
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/TimeEntriesGetByPersonsForExcel.proc.sql
@@ -2,8 +2,8 @@
 	@PersonIds	NVARCHAR(MAX),
 	@StartDate	DATETIME = NULL,
 	@EndDate	DATETIME = NULL,
-	@TimescaleIds NVARCHAR(4000),
-	@PracticeIds  NVARCHAR(MAX)
+	@TimescaleIds NVARCHAR(4000)= NULL,
+	@PracticeIds  NVARCHAR(MAX) = NULL
 	
 AS
 BEGIN
@@ -12,13 +12,28 @@ BEGIN
 	INSERT INTO @PersonList
 	SELECT * FROM dbo.ConvertStringListIntoTable(@PersonIds)
 	
-	DECLARE @TimescaleIdList TABLE (Id INT)
-	INSERT INTO @TimescaleIdList
-	SELECT * FROM dbo.ConvertStringListIntoTable(@TimescaleIds)
-	
+	--@TimescaleIds is null means all timescales.
+	IF @TimescaleIds IS NOT NULL
+	BEGIN
+		DECLARE @TimescaleIdList table (Id int)
+		INSERT INTO @TimescaleIdList
+		SELECT * FROM dbo.ConvertStringListIntoTable(@TimescaleIds)
+	END	
 	DECLARE @PracticeIdsList TABLE (Id INT)
 	INSERT INTO @PracticeIdsList
 	SELECT * FROM dbo.ConvertStringListIntoTable(@PracticeIds)
+
+    ;WITH PersonsFilteredByPersonIdsAndPayIds AS
+	(
+		SELECT Distinct P.PersonId
+						, P.FirstName
+						, P.LastName
+		FROM Person P
+		JOIN @PersonList PL ON PL.Id = P.PersonId
+		LEFT JOIN Pay pa ON pa.Person = P.PersonId  AND pa.StartDate <= @EndDate AND (ISNULL(pa.EndDate, dbo.GetFutureDate()) -1) >= @StartDate
+		WHERE (@TimescaleIds IS NULL OR pa.Timescale IN (SELECT Id FROM @TimescaleIdList))
+		      AND (@PracticeIds IS NULL) OR ISNULL(pa.PracticeId,P.DefaultPractice) IN (SELECT Id FROM @PracticeIdsList)
+	)
 
 	SELECT DISTINCT 
 		   p.LastName +', ' +p.FirstName Name,
@@ -30,7 +45,7 @@ BEGIN
 		   te.Note Note,
 		   CONVERT(VARCHAR(10), te.MilestoneDate, 1)  AS 'Date',
 		   te.ActualHours AS 'Hours'
-	FROM dbo.Person p
+	FROM PersonsFilteredByPersonIdsAndPayIds p
 	JOIN dbo.MilestonePerson mp on mp.PersonId = p.PersonId
 	JOIN dbo.MilestonePersonEntry mpe on mpe.MilestonePersonId = mp.MilestonePersonId
 	JOIN dbo.Milestone m on m.MilestoneId = mp.MilestoneId
@@ -39,14 +54,13 @@ BEGIN
 	JOIN dbo.TimeEntries te on te.MilestonePersonId = mp.MilestonePersonId
 									AND te.MilestoneDate BETWEEN ISNULL(@StartDate, te.MilestoneDate) AND ISNULL(@EndDate, te.MilestoneDate)
 	LEFT JOIN dbo.TimeType tt on tt.TimeTypeId = te.TimeTypeId
-	WHERE p.PersonId IN (select Id from @PersonList)
-		AND	(ISNULL(@StartDate,Mpe.StartDate) >= mpe.StartDate AND ISNULL(@EndDate,mpe.EndDate) <= mpe.EndDate
+	WHERE 	(ISNULL(@StartDate,Mpe.StartDate) >= mpe.StartDate AND ISNULL(@EndDate,mpe.EndDate) <= mpe.EndDate
 			OR ISNULL(@StartDate,Mpe.StartDate - 1) < mpe.StartDate AND ISNULL(@EndDate,mpe.StartDate) >= mpe.StartDate
 			OR ISNULL(@StartDate,Mpe.EndDate) <= mpe.EndDate AND ISNULL(@EndDate,mpe.EndDate + 1) >= mpe.EndDate
 			)
-		AND (dbo.GetCurrentPayType(p.PersonId) IN (select Id from @TimescaleIdList)  OR dbo.GetCurrentPayType(p.PersonId) IS NULL)
-		AND p.DefaultPractice IN (SELECT id FROM @PracticeIdsList)
+		
 		AND (te.TimeEntryId IS NOT NULL OR proj.ProjectStatusId = 3) --For getting only active projects or there are timeentries for projects.
 	ORDER BY Name ,[Date], proj.ProjectNumber 
 
 END
+
