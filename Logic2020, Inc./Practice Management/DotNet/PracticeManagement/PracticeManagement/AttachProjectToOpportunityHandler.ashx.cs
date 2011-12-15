b3ecee801945f6335c0b1fecedb3f4cd3000d8b9
SainathC <SainathC>
2011-12-15 10:32:00 -0500

    Comments - Defect Number: 2966; Defect RecordID: 2962; Server Address: services.logic2020.com; Server Port: 99; Database Name: Practice Management; Defect Summary: Oppty Detail: Changing status to PO should prompt to link a Project; Defect Description: If the user changes an Oppty to PO and tries to save they will get an error that the Oppty isn't linked to a Project. Instead of this, selecting the status of PO should prompt the user to select a Project to link to if one has not already been linked.

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/AttachProjectToOpportunityHandler.ashx.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using DataTransferObjects;
+using System.Web.Script.Serialization;
+
+namespace PraticeManagement
+{
+    /// <summary>
+    /// Summary description for AttachProjectToOpportunityHandler
+    /// </summary>
+    public class AttachProjectToOpportunityHandler : IHttpHandler
+    {
+        //private class clientProject
+        //{ 
+        //    string 
+        //}
+        public void ProcessRequest(HttpContext context)
+        {
+            if (!String.IsNullOrEmpty(context.Request.QueryString["getClientProjects"]))
+            {
+                var getClientProjects = Convert.ToBoolean(context.Request.QueryString["getClientProjects"]);
+                if (getClientProjects)
+                {
+                    var clientId = Convert.ToInt32(context.Request.QueryString["clientId"]);
+                    var projects = ServiceCallers.Custom.Project(client => client.ListProjectsByClientShort(clientId, true));
+
+                    Dictionary<string, string> values = new Dictionary<string, string>();
+                    values.Add("-1", "Select Project ...");
+                    foreach (Project project in projects)
+                    {
+                        values.Add(project.Id.ToString(), project.DetailedProjectTitle);
+                    }
+                    string ClientProjects =AttachProjectToOpportunityHandler.ToJSON(values);
+                    context.Response.Write(ClientProjects);
+                }
+            }
+            else
+            {
+                if (!(String.IsNullOrEmpty(context.Request.QueryString["opportunityID"]) && String.IsNullOrEmpty(context.Request.QueryString["projectId"])))
+                {
+                    var opportunityId = Convert.ToInt32(context.Request.QueryString["opportunityID"]);
+                    var projectId = Convert.ToInt32(context.Request.QueryString["projectId"]);
+                    var priorityId = Convert.ToInt32(context.Request.QueryString["priorityId"]);
+                    ServiceCallers.Custom.Opportunity(os => os.AttachProjectToOpportunity(opportunityId, projectId, priorityId, context.User.Identity.Name));
+                    context.Response.Write("Save Completed.");
+                }
+
+            }
+
+         
+        }
+        private static string ToJSON(Object obj) 
+        { 
+            JavaScriptSerializer serializer = new JavaScriptSerializer(); 
+            return serializer.Serialize(obj); 
+        } 
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
