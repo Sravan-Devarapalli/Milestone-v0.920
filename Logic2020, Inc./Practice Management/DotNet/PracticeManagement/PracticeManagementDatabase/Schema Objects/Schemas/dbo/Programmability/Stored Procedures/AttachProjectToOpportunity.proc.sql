05d87822327ed1d982a174e4672e5d009d03dc84
SainathC <SainathC>
2011-12-15 10:32:00 -0500

    Comments - Defect Number: 2966; Defect RecordID: 2962; Server Address: services.logic2020.com; Server Port: 99; Database Name: Practice Management; Defect Summary: Oppty Detail: Changing status to PO should prompt to link a Project; Defect Description: If the user changes an Oppty to PO and tries to save they will get an error that the Oppty isn't linked to a Project. Instead of this, selecting the status of PO should prompt the user to select a Project to link to if one has not already been linked.

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/AttachProjectToOpportunity.proc.sql
@@ -0,0 +1,31 @@
+﻿CREATE PROCEDURE [dbo].[AttachProjectToOpportunity]
+(
+	@ProjectId			INT,
+	@PriorityId			INT,
+	@OpportunityId      INT,
+	@UserLogin          NVARCHAR(255)
+)
+AS
+BEGIN
+	SET NOCOUNT ON;
+	-- Start logging session
+		EXEC dbo.SessionLogPrepare @UserLogin = @UserLogin
+		DECLARE @ErrorMessage NVARCHAR(MAX)
+	
+		BEGIN TRY
+			UPDATE dbo.Opportunity
+			SET   ProjectId = @ProjectId,
+				  PriorityId = @PriorityId,
+				  LastUpdated = GETUTCDATE()
+			WHERE OpportunityId = @OpportunityId
+		END TRY
+		BEGIN CATCH
+			
+			SELECT @ErrorMessage = ERROR_MESSAGE()
+			RAISERROR(@ErrorMessage, 16, 1) 
+
+		END CATCH
+
+		-- End logging session
+		EXEC dbo.SessionLogUnprepare
+END
