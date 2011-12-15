94eef18b259a8e6eaaf3698e73c1757c4d4a9cf2
SainathC <SainathC>
2011-12-15 10:33:00 -0500

    Comments - Defect Number: 2966; Defect RecordID: 2962; Server Address: services.logic2020.com; Server Port: 99; Database Name: Practice Management; Defect Summary: Oppty Detail: Changing status to PO should prompt to link a Project; Defect Description: If the user changes an Oppty to PO and tries to save they will get an error that the Oppty isn't linked to a Project. Instead of this, selecting the status of PO should prompt the user to select a Project to link to if one has not already been linked.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementService/IOpportunityService.cs
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementService/IOpportunityService.cs
@@ -6,18 +6,21 @@ using DataTransferObjects.ContextObjects;
 
 namespace PracticeManagementService
 {
-	[ServiceContract]
-	public interface IOpportunityService
+    [ServiceContract]
+    public interface IOpportunityService
     {
         [OperationContract]
         void UpdatePriorityIdForOpportunity(int opportunityId, int priorityId, string userName);
 
-		/// <summary>
-		/// Retrives a list of the opportunities for excel export.
-		/// </summary>
-		/// <returns>A list of the <see cref="Opportunity"/> objects.</returns>
-		[OperationContract]
-		System.Data.DataSet OpportunityGetExcelSet();
+        [OperationContract]
+        void AttachProjectToOpportunity(int opportunityId, int projectId, int priorityId, string userName);
+
+        /// <summary>
+        /// Retrives a list of the opportunities for excel export.
+        /// </summary>
+        /// <returns>A list of the <see cref="Opportunity"/> objects.</returns>
+        [OperationContract]
+        System.Data.DataSet OpportunityGetExcelSet();
 
         [OperationContract]
         bool IsOpportunityPriorityInUse(int priorityId);
@@ -25,13 +28,13 @@ namespace PracticeManagementService
         [OperationContract]
         bool IsOpportunityHaveTeamStructure(int opportunityId);
 
-	    /// <summary>
-	    /// List Opportunity Transitions
-	    /// </summary>
-	    /// <param name="opportunityId"></param>
-	    /// <param name="statusType"></param>
-	    /// <returns></returns>
-	    [OperationContract]
+        /// <summary>
+        /// List Opportunity Transitions
+        /// </summary>
+        /// <param name="opportunityId"></param>
+        /// <param name="statusType"></param>
+        /// <returns></returns>
+        [OperationContract]
         List<OpportunityTransition> GetOpportunityTransitions(int opportunityId, OpportunityTransitionStatusType statusType);
 
         /// <summary>
@@ -41,12 +44,12 @@ namespace PracticeManagementService
         [OperationContract]
         List<OpportunityTransition> GetOpportunityTransitionsByPerson(int personId);
 
-	    /// <summary>
-	    /// Inserts Opportunity Transition
-	    /// </summary>
-	    /// <param name="transition">Opportunity Transition</param>
-	    [OperationContract]
-	    int OpportunityTransitionInsert(OpportunityTransition transition);
+        /// <summary>
+        /// Inserts Opportunity Transition
+        /// </summary>
+        /// <param name="transition">Opportunity Transition</param>
+        [OperationContract]
+        int OpportunityTransitionInsert(OpportunityTransition transition);
 
         /// <summary>
         /// Inserts Opportunity Transition
@@ -67,7 +70,7 @@ namespace PracticeManagementService
 
         [OperationContract]
         List<Opportunity> OpportunityListAllShort(OpportunityListContext context);
-        
+
         [OperationContract]
         List<OpportunityPriority> GetOpportunityPrioritiesListAll();
 
@@ -83,43 +86,43 @@ namespace PracticeManagementService
         [OperationContract]
         void DeleteOpportunityPriority(int? updatedPriorityId, int deletedPriorityId, string userName);
 
-		/// <summary>
-		/// Retrives an <see cref="Opportunity"/> be a specified ID.
-		/// </summary>
-		/// <param name="opportunityId">An ID of the record to be retrieved.</param>
-		/// <returns>An <see cref="Opportunity"/> object if found and null otherwise.</returns>
-		[OperationContract]
-		Opportunity GetById(int opportunityId);
+        /// <summary>
+        /// Retrives an <see cref="Opportunity"/> be a specified ID.
+        /// </summary>
+        /// <param name="opportunityId">An ID of the record to be retrieved.</param>
+        /// <returns>An <see cref="Opportunity"/> object if found and null otherwise.</returns>
+        [OperationContract]
+        Opportunity GetById(int opportunityId);
 
-		/// <summary>
-		/// Saves a new <see cref="Opportunity"/> into the database.
-		/// </summary>
-		/// <param name="userName">The name of the current user.</param>
-		/// <param name="opportunity">The data to be saved.</param>
-		[OperationContract]
-		int? OpportunitySave(Opportunity opportunity, string userName);
+        /// <summary>
+        /// Saves a new <see cref="Opportunity"/> into the database.
+        /// </summary>
+        /// <param name="userName">The name of the current user.</param>
+        /// <param name="opportunity">The data to be saved.</param>
+        [OperationContract]
+        int? OpportunitySave(Opportunity opportunity, string userName);
 
-		/// <summary>
-		/// Retrieves a list of the Opportunity Statuses.
-		/// </summary>
-		/// <returns>A list of the <see cref="OpportunityStatus"/> objects.</returns>
-		[OperationContract]
-		List<OpportunityStatus> OpportunityStatusListAll();
+        /// <summary>
+        /// Retrieves a list of the Opportunity Statuses.
+        /// </summary>
+        /// <returns>A list of the <see cref="OpportunityStatus"/> objects.</returns>
+        [OperationContract]
+        List<OpportunityStatus> OpportunityStatusListAll();
 
-		/// <summary>
-		/// Retrieves a list of the Opportunity Transition Statuses objects.
-		/// </summary>
-		/// <returns>A list of the <see cref="OpportunityTransitionStatus"/> objects.</returns>
-		[OperationContract]
-		List<OpportunityTransitionStatus> OpportunityTransitionStatusListAll();
+        /// <summary>
+        /// Retrieves a list of the Opportunity Transition Statuses objects.
+        /// </summary>
+        /// <returns>A list of the <see cref="OpportunityTransitionStatus"/> objects.</returns>
+        [OperationContract]
+        List<OpportunityTransitionStatus> OpportunityTransitionStatusListAll();
 
-	    /// <summary>
-	    /// Creates a project from an opportunity.
-	    /// </summary>
-	    /// <param name="opportunityId">An ID of the opportunity to be converted.</param>
-	    /// <param name="userName">A current user.</param>
-	    [OperationContract]
-		int OpportunityConvertToProject(int opportunityId, string userName);
+        /// <summary>
+        /// Creates a project from an opportunity.
+        /// </summary>
+        /// <param name="opportunityId">An ID of the opportunity to be converted.</param>
+        /// <param name="userName">A current user.</param>
+        [OperationContract]
+        int OpportunityConvertToProject(int opportunityId, string userName);
 
         /// <summary>
         /// Gets a opportunity id by the oportunity number
@@ -152,7 +155,7 @@ namespace PracticeManagementService
         ///<param name="opportunityId">An ID of the opportunity</param>
         ///<param name="personId">An Id of the person</param>
         [OperationContract]
-        void OpportunityPersonInsert(int opportunityId, string personIdList,int relationTypeId,string outSideResources);
+        void OpportunityPersonInsert(int opportunityId, string personIdList, int relationTypeId, string outSideResources);
 
         ///<summary>
         /// Deletes proposed person from the Opportunity
@@ -170,7 +173,7 @@ namespace PracticeManagementService
         [OperationContract]
         void OpportunityDelete(int opportunityId, string userName);
 
-        
+
         ///<summary>
         /// Reads an Opportunity Priority Changes count
         ///</summary>
