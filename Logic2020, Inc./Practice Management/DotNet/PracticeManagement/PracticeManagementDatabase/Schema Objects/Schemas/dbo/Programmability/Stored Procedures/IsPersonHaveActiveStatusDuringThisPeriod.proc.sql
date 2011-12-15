27b15268557954fe52e8f9124a07e9cbed1667d6
SainathC <SainathC>
2011-12-15 10:45:00 -0500

    Comments - Defect Number: 2936; Defect RecordID: 2932; Server Address: services.logic2020.com; Server Port: 99; Database Name: Practice Management; Defect Summary: Person>Compensation: Need to be able to delete compensation record as Admin; Defect Description: User Igor Kashinskiy is appearing twice in the Margin Test dropdown for unknown reason. I suspect it has to do with his compensation record that was changed today. While investigating I found that I am unable to delete a compensation record. It would be ideal if this tab functioned like the Resources tab in a project, allowing me to edit some properties in-line, and to copy/delete as well.

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/IsPersonHaveActiveStatusDuringThisPeriod.proc.sql
@@ -0,0 +1,53 @@
+﻿CREATE PROCEDURE [dbo].[IsPersonHaveActiveStatusDuringThisPeriod]
+(
+	@PersonId           INT,
+	@StartDate          DATETIME,
+	@EndDate            DATETIME = NULL
+)	
+AS
+BEGIN
+
+	DECLARE @IsPersonHasActiveStatus BIT
+	DECLARE @FutureDate DATETIME , @MinStartDate DATETIME ,@NOW DATETIME 
+	
+	
+	SELECT @MinStartDate = MIN(PSH.StartDate) FROM dbo.PersonStatusHistory PSH
+					   WHERE PSH.PersonId = @PersonId
+
+	SET  @FutureDate = dbo.GetFutureDate()
+	SET  @NOW = dbo.GettingPMTime(GETUTCDATE())
+	
+	
+	
+	IF (@NOW < @StartDate)
+	BEGIN
+			SET @IsPersonHasActiveStatus = 0
+	END
+	ELSE
+		BEGIN
+			IF EXISTS(SELECT 1 	FROM dbo.PersonStatusHistory PSH
+						WHERE PSH.PersonId = @PersonId AND PSH.PersonStatusId = 1 
+								AND ( PSH.StartDate <=  ISNULL(@EndDate,@FutureDate) 
+								AND ISNULL(PSH.EndDate,@FutureDate) >= @StartDate))
+			BEGIN
+				SET @IsPersonHasActiveStatus  = 1
+			END
+			ELSE 
+				BEGIN
+	   
+					IF ((@StartDate < @MinStartDate) AND 
+					EXISTS(SELECT 1 FROM dbo.PersonStatusHistory PSH WHERE PSH.PersonId = @PersonId AND PSH.PersonStatusId = 1 AND PSH.StartDate = @MinStartDate))
+						BEGIN
+							SET @IsPersonHasActiveStatus  = 1
+						END
+					ELSE
+						BEGIN
+							SET @IsPersonHasActiveStatus = 0
+						END
+				END
+		END
+	
+	
+	SELECT @IsPersonHasActiveStatus
+
+END
