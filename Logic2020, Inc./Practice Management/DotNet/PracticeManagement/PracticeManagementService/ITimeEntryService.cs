efe4e988602a5e3b2dba1d16ff5eb54035717fdd
ThulasiramP <ThulasiramP>
2012-01-20 05:39:00 -0500

    Comments - Defect Number: 2980; Defect RecordID: 2976; Server Address: services.logic2020.com; Server Port: 99; Database Name: Practice Management; Defect Summary: New Enhancement - Improved Time Tracking ; Defect Description: Madhan,
    Attached is the draft specification for some significant changes to the way Logic20/20 wants to track time in Practice Management. Please review the Word document and reference the attached Excel workbook to provide additional detail. Ill step through each with you and your team tomorrow morning (Pacific time).
    
    I imagine some impacts on our existing data model/schema. I will need some expert guidance from your team on the impacts of this specification and how best to architect the data model to provide the operational reporting flexibility we will need as a result of the system changes. We will want to be able to have a far more interactive reporting experience than what is presently afforded in PM to enable roll-up and drill-down capability leveraging this new time tracking design.
    
    Additionally, I am attaching some sample UI changes we would like to discuss as these represent conceptual thoughts on the direction wed like to move for our Time Entry interface. These are not to be viewed as the only approach but should be seen as a sample for discussion. I actually think that combining elements of both is the right solution but lets discuss based on your reading of the specifications.
    
    Our hope is that GGK will be able to run with this specification while Jason and I are on holiday from 12/22  1/2/2012 and return with some sample prototypes if not working code in a test environment by that first week of January. I can make myself available for a call during my holiday if the team gets stuck or has urgent questions that must be answered to avoid getting stuck.
    
    Best,
    Matt
    
    Matthew Reilly, Senior Manager  Consulting Operations// Logic20/20 // MattR@logic2020.com
    t.206-576-0491//c.425-503-5825// www.logic2020.com

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementService/ITimeEntryService.cs
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementService/ITimeEntryService.cs
@@ -8,42 +8,42 @@ using DataTransferObjects.TimeEntry;
 
 namespace PracticeManagementService
 {
-	[ServiceContract]
-	[ServiceKnownType(typeof(TimeTypeRecord))]
-	public interface ITimeEntryService
-	{
-		#region Time types
+    [ServiceContract]
+    [ServiceKnownType(typeof(TimeTypeRecord))]
+    public interface ITimeEntryService
+    {
+        #region Time types
 
-		/// <summary>
-		/// Retrieves all existing time types
-		/// </summary>
-		/// <returns>Collection of new time types</returns>
-		[OperationContract]
-		IEnumerable<TimeTypeRecord> GetAllTimeTypes();
+        /// <summary>
+        /// Retrieves all existing time types
+        /// </summary>
+        /// <returns>Collection of new time types</returns>
+        [OperationContract]
+        IEnumerable<TimeTypeRecord> GetAllTimeTypes();
 
-		/// <summary>
-		/// Removes given time type
-		/// </summary>
-		/// <param name="timeType">Time type to remove</param>
-		[OperationContract]
-		void RemoveTimeType(TimeTypeRecord timeType);
+        /// <summary>
+        /// Removes given time type
+        /// </summary>
+        /// <param name="timeType">Time type to remove</param>
+        [OperationContract]
+        void RemoveTimeType(TimeTypeRecord timeType);
 
-		/// <summary>
-		/// Updates given time type
-		/// </summary>
-		/// <param name="timeType">Time type to update</param>
-		[OperationContract]
-		void UpdateTimeType(TimeTypeRecord timeType);
+        /// <summary>
+        /// Updates given time type
+        /// </summary>
+        /// <param name="timeType">Time type to update</param>
+        [OperationContract]
+        void UpdateTimeType(TimeTypeRecord timeType);
 
-		/// <summary>
-		/// Adds new time type
-		/// </summary>
-		/// <param name="timeType">Time type to add</param>
-		/// <returns>Id of added time type</returns>
-		[OperationContract]
-		int AddTimeType(TimeTypeRecord timeType);
+        /// <summary>
+        /// Adds new time type
+        /// </summary>
+        /// <param name="timeType">Time type to add</param>
+        /// <returns>Id of added time type</returns>
+        [OperationContract]
+        int AddTimeType(TimeTypeRecord timeType);
 
-		#endregion    
+        #endregion
 
         #region Time Zones
 
@@ -68,26 +68,26 @@ namespace PracticeManagementService
         [OperationContract]
         void RemoveTimeEntries(int milestonePersonId, int timeTypeId, DateTime startDate, DateTime endDate);
 
-		/// <summary>
-		/// Removes entry
-		/// </summary>
-		/// <param name="timeEntry">Time entry to remove</param>
-		[OperationContract]
-		void RemoveTimeEntryById(int id);
+        /// <summary>
+        /// Removes entry
+        /// </summary>
+        /// <param name="timeEntry">Time entry to remove</param>
+        [OperationContract]
+        void RemoveTimeEntryById(int id);
 
-		/// <summary>
-		/// Removes entry
-		/// </summary>
-		/// <param name="timeEntry">Time entry to remove</param>
-		[OperationContract]
-		void RemoveTimeEntry(TimeEntryRecord timeEntry);
+        /// <summary>
+        /// Removes entry
+        /// </summary>
+        /// <param name="timeEntry">Time entry to remove</param>
+        [OperationContract]
+        void RemoveTimeEntry(TimeEntryRecord timeEntry);
 
-		/// <summary>
-		/// Adds new time entry
-		/// </summary>
-		/// <param name="timeEntry">Time entry to add</param>
-		[OperationContract]
-		int AddTimeEntry(TimeEntryRecord timeEntry, int defaultMpId);
+        /// <summary>
+        /// Adds new time entry
+        /// </summary>
+        /// <param name="timeEntry">Time entry to add</param>
+        [OperationContract]
+        int AddTimeEntry(TimeEntryRecord timeEntry, int defaultMpId);
 
         /// <summary>
         /// Has time entries
@@ -96,62 +96,62 @@ namespace PracticeManagementService
         [OperationContract]
         bool HasTimeEntriesForMilestone(int milestoneId, DateTime startDate, DateTime endDate);
 
-		/// <summary>
-		/// Updates time entry
-		/// </summary>
-		/// <param name="timeEntry">Time entry to add</param>
-		[OperationContract]
-		void UpdateTimeEntry(TimeEntryRecord timeEntry, int defaultMilestoneId);
+        /// <summary>
+        /// Updates time entry
+        /// </summary>
+        /// <param name="timeEntry">Time entry to add</param>
+        [OperationContract]
+        void UpdateTimeEntry(TimeEntryRecord timeEntry, int defaultMilestoneId);
 
-		/// <summary>
-		/// Constructs the objects and passes it to update the time entry
-		/// </summary>
-		[OperationContract]
-		void ConstructAndUpdateTimeEntry(
-			int id,
-			string milestoneDate,
-			string entryDate,
-			int milestonePersonId,
-			double actualHours,
-			double forecastedHours,
-			int timeTypeId,
-			string note,
-			string isReviewed,
-			bool isChargeable,
-			bool isCorrect,
-			int modifiedById
-			);
+        /// <summary>
+        /// Constructs the objects and passes it to update the time entry
+        /// </summary>
+        [OperationContract]
+        void ConstructAndUpdateTimeEntry(
+            int id,
+            string milestoneDate,
+            string entryDate,
+            int milestonePersonId,
+            double actualHours,
+            double forecastedHours,
+            int timeTypeId,
+            string note,
+            string isReviewed,
+            bool isChargeable,
+            bool isCorrect,
+            int modifiedById
+            );
 
-		/// <summary>
-		/// Get time entries by person
-		/// </summary>
-		[OperationContract]
-		TimeEntryRecord[] GetTimeEntriesForPerson(Person person, DateTime startDate, DateTime endDate);
+        /// <summary>
+        /// Get time entries by person
+        /// </summary>
+        [OperationContract]
+        TimeEntryRecord[] GetTimeEntriesForPerson(Person person, DateTime startDate, DateTime endDate);
 
-		/// <summary>
-		/// Get time entries by project grouped by person
-		/// </summary>
-		[OperationContract]
-		GroupedTimeEntries<Person> GetTimeEntriesProject(TimeEntryProjectReportContext reportContext);
+        /// <summary>
+        /// Get time entries by project grouped by person
+        /// </summary>
+        [OperationContract]
+        GroupedTimeEntries<Person> GetTimeEntriesProject(TimeEntryProjectReportContext reportContext);
 
-		/// <summary>
-		/// Get time entries by project grouped by person
-		/// </summary>
-		[OperationContract]
-		GroupedTimeEntries<TimeEntryHours> GetTimeEntriesProjectCumulative(TimeEntryPersonReportContext reportContext);
+        /// <summary>
+        /// Get time entries by project grouped by person
+        /// </summary>
+        [OperationContract]
+        GroupedTimeEntries<TimeEntryHours> GetTimeEntriesProjectCumulative(TimeEntryPersonReportContext reportContext);
 
-		/// <summary>
-		/// Get time entries by project grouped by person
-		/// </summary>
-		[OperationContract]
-		GroupedTimeEntries<Project> GetTimeEntriesPerson(TimeEntryPersonReportContext reportContext);
+        /// <summary>
+        /// Get time entries by project grouped by person
+        /// </summary>
+        [OperationContract]
+        GroupedTimeEntries<Project> GetTimeEntriesPerson(TimeEntryPersonReportContext reportContext);
 
-		/// <summary>
-		/// Get milestones by person for given time period
-		/// </summary>
-		[OperationContract]
-		MilestonePersonEntry[] GetCurrentMilestones(
-			Person person, DateTime startDate, DateTime endDate, int defaultMilestoneId);
+        /// <summary>
+        /// Get milestones by person for given time period
+        /// </summary>
+        [OperationContract]
+        MilestonePersonEntry[] GetCurrentMilestones(
+            Person person, DateTime startDate, DateTime endDate, int defaultMilestoneId);
 
         /// <summary>
         /// Get milestones by person for given time period exclusively for Time Entry page.
@@ -159,11 +159,11 @@ namespace PracticeManagementService
         [OperationContract]
         MilestonePersonEntry[] GetTimeEntryMilestones(Person person, DateTime startDate, DateTime endDate);
 
-		/// <summary>
+        /// <summary>
         /// Returns all time entries for given Context/Filter conditions
-		/// </summary>
-		[OperationContract]
-		TimeEntryRecord[] GetAllTimeEntries(TimeEntrySelectContext selectContext, int startRow, int maxRows);
+        /// </summary>
+        [OperationContract]
+        TimeEntryRecord[] GetAllTimeEntries(TimeEntrySelectContext selectContext, int startRow, int maxRows);
 
         /// <summary>
         /// Gets TimeEntries count for given Context/Filter conditions
@@ -184,41 +184,54 @@ namespace PracticeManagementService
         [OperationContract]
         System.Data.DataSet TimeEntriesByPersonGetExcelSet(TimeEntryPersonReportContext reportContext);
 
+        //new timetrack methods
+        [OperationContract]
+        void DeleteTimeTrack(int clientId, int projectId, int personId, int timetypeId, DateTime startDate, DateTime endDate);
+
+        [OperationContract]
+        void SaveTimeTrack(string timeEntriesXml, int personId, DateTime startDate, DateTime endDate, string userLogin);
+
+        [OperationContract]
+        void SetPersonTimeEntryRecursiveSelection(int personId, int clientId, int projectGroupId, int projectId, int timeEntrySectionId, bool isRecursive, DateTime startDate);
+
+        [OperationContract]
+        void SetPersonTimeEntrySelection(int personId, int clientId, int projectGroupId, int projectId, int timeEntrySectionId, bool isDelete, DateTime startDate, DateTime endDate);
+
         #endregion
 
-		#region Toggling
+        #region Toggling
 
-		/// <summary>
-		/// Toggle IsCorrect property
-		/// </summary>
-		/// <param name="timeEntry">Time entry to add</param>
-		[OperationContract]
-		void ToggleIsCorrect(TimeEntryRecord timeEntry);
+        /// <summary>
+        /// Toggle IsCorrect property
+        /// </summary>
+        /// <param name="timeEntry">Time entry to add</param>
+        [OperationContract]
+        void ToggleIsCorrect(TimeEntryRecord timeEntry);
 
-		/// <summary>
-		/// Toggle IsReviewed property
-		/// </summary>
-		/// <param name="timeEntry">Time entry to add</param>
-		[OperationContract]
-		void ToggleIsReviewed(TimeEntryRecord timeEntry);
+        /// <summary>
+        /// Toggle IsReviewed property
+        /// </summary>
+        /// <param name="timeEntry">Time entry to add</param>
+        [OperationContract]
+        void ToggleIsReviewed(TimeEntryRecord timeEntry);
 
-		/// <summary>
-		/// Toggle IsChargeable property
-		/// </summary>
-		/// <param name="timeEntry">Time entry to add</param>
-		[OperationContract]
-		void ToggleIsChargeable(TimeEntryRecord timeEntry);
+        /// <summary>
+        /// Toggle IsChargeable property
+        /// </summary>
+        /// <param name="timeEntry">Time entry to add</param>
+        [OperationContract]
+        void ToggleIsChargeable(TimeEntryRecord timeEntry);
 
-		#endregion
+        #endregion
 
-		#region Time entry filters
+        #region Time entry filters
 
-		/// <summary>
-		/// Gets all projects that have TE records assigned to them
-		/// </summary>
-		/// <returns>Projects list</returns>
-		[OperationContract]
-		Project[] GetAllTimeEntryProjects();
+        /// <summary>
+        /// Gets all projects that have TE records assigned to them
+        /// </summary>
+        /// <returns>Projects list</returns>
+        [OperationContract]
+        Project[] GetAllTimeEntryProjects();
 
         /// <summary>
         /// Gets all projects that have TE records assigned to particular clientId
@@ -227,21 +240,30 @@ namespace PracticeManagementService
         [OperationContract]
         Project[] GetTimeEntryProjectsByClientId(int? clientId, int? personId, bool showActiveAndInternalProjectsOnly);
 
-		/// <summary>
-		/// Gets all milestones that have TE records assigned to them
-		/// </summary>
-		/// <returns>Milestones list</returns>
-		[OperationContract]
-		Milestone[] GetAllTimeEntryMilestones();
+        /// <summary>
+        /// Gets all milestones that have TE records assigned to them
+        /// </summary>
+        /// <returns>Milestones list</returns>
+        [OperationContract]
+        Milestone[] GetAllTimeEntryMilestones();
 
-		/// <summary>
-		/// Gets all persons that have entered at least one TE
-		/// </summary>
-		/// <returns>List of persons</returns>
-		[OperationContract]
-		Person[] GetAllTimeEntryPersons(DateTime entryDateFrom, DateTime entryDateTo);
+        /// <summary>
+        /// Gets all persons that have entered at least one TE
+        /// </summary>
+        /// <returns>List of persons</returns>
+        [OperationContract]
+        Person[] GetAllTimeEntryPersons(DateTime entryDateFrom, DateTime entryDateTo);
 
-		#endregion
-	}
+
+        [OperationContract]
+        List<TimeEntrySection> PersonTimeEntriesByPeriod(int personId, DateTime startDate, DateTime endDate);
+
+        [OperationContract]
+        double? GetPersonTimeEnteredHoursByDay(int personId, DateTime date, bool includePTOAndHoliday);
+
+        #endregion
+
+
+    }
 }
 
