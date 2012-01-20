25c1903eae5bc5cee71056c71a15bdfe70aae0af
SrinivasM <SrinivasM>
2012-01-20 08:44:00 -0500

    Comments - DBChanges regarding new TimeEntry page.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/ProjectGroup.table.sql
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/ProjectGroup.table.sql
@@ -1,10 +1,9 @@
 ﻿CREATE TABLE [dbo].[ProjectGroup] (
-    [GroupId]  INT            IDENTITY (0, 1) NOT NULL,
-    [ClientId] INT            NOT NULL,
-    [Name]     NVARCHAR (100) NOT NULL,
-	Active     BIT            NOT NULL CONSTRAINT DF_ProjectGroup_Active DEFAULT  1,
-    PRIMARY KEY CLUSTERED ([GroupId] ASC) WITH (IGNORE_DUP_KEY = OFF, STATISTICS_NORECOMPUTE = OFF),
-    FOREIGN KEY ([ClientId]) REFERENCES [dbo].[Client] ([ClientId]) ON DELETE NO ACTION ON UPDATE NO ACTION
+    [GroupId]			INT				IDENTITY (0, 1) NOT NULL,
+    [ClientId]			INT				NOT NULL,
+	[Code]				NVARCHAR(5)		NOT NULL,
+    [Name]				NVARCHAR (100)	NOT NULL,
+	Active				BIT				NOT NULL CONSTRAINT DF_ProjectGroup_Active DEFAULT  1,
+    PRIMARY KEY CLUSTERED ([GroupId] ASC) WITH (IGNORE_DUP_KEY = OFF, STATISTICS_NORECOMPUTE = OFF)
 );
 
- 
