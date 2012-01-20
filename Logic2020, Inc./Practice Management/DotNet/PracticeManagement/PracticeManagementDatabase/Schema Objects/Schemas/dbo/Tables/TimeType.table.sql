cc504ff236e8102b6e3032429fdc5c9f18a16006
SrinivasM <SrinivasM>
2012-01-20 08:44:00 -0500

    Comments - DBChanges regarding new TimeEntry page.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/TimeType.table.sql
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/TimeType.table.sql
@@ -1,8 +1,10 @@
 ﻿CREATE TABLE [dbo].[TimeType] (
-    [TimeTypeId] INT          IDENTITY (1, 1) NOT NULL,
-    [Name]       VARCHAR (50) NOT NULL,
-    [IsDefault]  BIT          NOT NULL,
-	[IsSystemTimeType] BIT	NULL
+    [TimeTypeId]		INT	IDENTITY (1, 1) NOT NULL,
+    [Code]				NVARCHAR(5)			NOT NULL,
+    [Name]				VARCHAR (50)		NOT NULL,
+    [IsDefault]			BIT					NOT NULL,
+    [IsAllowedToEdit]	BIT					NULL,
+    [IsInternal]		BIT					NOT NULL,
+	[IsActive]			BIT					NOT NULL
 );
-
 
