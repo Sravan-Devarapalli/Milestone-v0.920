c092cc82e016e58b72bc0980a7c9809ce4475dd3
ThulasiramP <ThulasiramP>
2012-01-20 05:38:00 -0500

    Comments - Defect Number: 2980; Defect RecordID: 2976; Server Address: services.logic2020.com; Server Port: 99; Database Name: Practice Management; Defect Summary: New Enhancement - Improved Time Tracking ; Defect Description: Madhan,
    Attached is the draft specification for some significant changes to the way Logic20/20 wants to track time in Practice Management. Please review the Word document and reference the attached Excel workbook to provide additional detail. Ill step through each with you and your team tomorrow morning (Pacific time).
    
    I imagine some impacts on our existing data model/schema. I will need some expert guidance from your team on the impacts of this specification and how best to architect the data model to provide the operational reporting flexibility we will need as a result of the system changes. We will want to be able to have a far more interactive reporting experience than what is presently afforded in PM to enable roll-up and drill-down capability leveraging this new time tracking design.
    
    Additionally, I am attaching some sample UI changes we would like to discuss as these represent conceptual thoughts on the direction wed like to move for our Time Entry interface. These are not to be viewed as the only approach but should be seen as a sample for discussion. I actually think that combining elements of both is the right solution but lets discuss based on your reading of the specifications.
    
    Our hope is that GGK will be able to run with this specification while Jason and I are on holiday from 12/22  1/2/2012 and return with some sample prototypes if not working code in a test environment by that first week of January. I can make myself available for a call during my holiday if the team gets stuck or has urgent questions that must be answered to avoid getting stuck.
    
    Best,
    Matt
    
    Matthew Reilly, Senior Manager  Consulting Operations// Logic20/20 // MattR@logic2020.com
    t.206-576-0491//c.425-503-5825// www.logic2020.com

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/Controls/Projects/ProjectTimeTypes.ascx.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using PraticeManagement.TimeEntryService;
+using PraticeManagement.Config;
+using DataTransferObjects.TimeEntry;
+using PraticeManagement.ProjectService;
+using DataTransferObjects.Utils;
+using System.Web.Security;
+using DataTransferObjects;
+
+namespace PraticeManagement.Controls.Projects
+{
+    public partial class ProjectTimeTypes : System.Web.UI.UserControl
+    {
+
+        public TimeTypeRecord[] AllTimeTypes
+        {
+            get
+            {
+                return (TimeTypeRecord[])ViewState[ProjectDetail.AllTimeTypesKey];
+            }
+            set
+            {
+                ViewState[ProjectDetail.AllTimeTypesKey] = value;
+            }
+        }
+
+        public TimeTypeRecord[] ProjectTimetypes
+        {
+            get
+            {
+                return (TimeTypeRecord[])ViewState[ProjectDetail.ProjectTimeTypesKey];
+            }
+            set
+            {
+                ViewState[ProjectDetail.ProjectTimeTypesKey] = value;
+            }
+
+        }
+
+        public void PopulateControls()
+        {
+            if (AllTimeTypes == null && ProjectTimetypes == null)
+            {
+                AllTimeTypes = ServiceCallers.Invoke<TimeEntryServiceClient, TimeTypeRecord[]>(TimeType => TimeType.GetAllTimeTypes());
+                TimeTypeRecord[] NotIncludedTimeTypes = AllTimeTypes.AsQueryable().Where(T => T.Name == "PTO" || T.Name == "Holiday").ToArray();
+                AllTimeTypes = AllTimeTypes.AsQueryable().Except(NotIncludedTimeTypes).ToArray();
+
+                if (Page is ProjectDetail && ((ProjectDetail)Page).ProjectId != null)
+                {
+                    Project project = ((ProjectDetail)Page).Project;
+                    if (project.Client.IsInternal) //defalult and internal timetypes for all internal projects
+                    {
+                        AllTimeTypes = AllTimeTypes.AsQueryable().Where(T => T.IsDefault || T.IsInternal).ToArray();
+                    }
+                    else //default and external timetypes for all external projects
+                    {
+                        AllTimeTypes = AllTimeTypes.AsQueryable().Where(T => T.IsDefault || !T.IsInternal).ToArray();
+                    }
+                    int projectId = ((ProjectDetail)Page).ProjectId.Value;
+                    ProjectTimetypes = ServiceCallers.Invoke<ProjectServiceClient, TimeTypeRecord[]>(Project => Project.GetTimeTypesByProjectId(projectId));
+                    string projectTimeTypesInUseIds = "";
+                    string ProjectTimetypesIds = "";
+                    foreach (TimeTypeRecord ptt in ProjectTimetypes)
+                    {
+                        ProjectTimetypesIds = ProjectTimetypesIds + ptt.Id + ',';
+                        if (ptt.InUse)
+                        {
+                            projectTimeTypesInUseIds = projectTimeTypesInUseIds + ptt.Id + ',';
+                        }
+                    }
+                    ProjectTimetypes = GetSelectedList(AllTimeTypes.ToList(), ProjectTimetypesIds).ToArray();
+                    hdnProjectTimeTypesInUse.Value = projectTimeTypesInUseIds;
+                    if (project.CanCreateCustomWorkTypes)
+                    {
+                        imgAddNewTimeType.Style["display"] = "none";
+                    }
+                }
+                else
+                {
+                    ProjectTimetypes = AllTimeTypes.Where(t => t.IsDefault).ToArray();
+                    AllTimeTypes = AllTimeTypes.AsQueryable().Where(T => 1 == 2).ToArray();//remove all
+                }
+                AllTimeTypes = AllTimeTypes.AsQueryable().Except(ProjectTimetypes).ToArray();
+            }
+
+            if (!String.IsNullOrEmpty(hdnTimeTypesAssignedToProject.Value))
+            {
+                string TimeTypesAssignedToProject = hdnTimeTypesAssignedToProject.Value;
+                List<TimeTypeRecord> CombinedList = new List<TimeTypeRecord>();
+                CombinedList.AddRange(ProjectTimetypes);
+                CombinedList.AddRange(AllTimeTypes);
+                CombinedList = CombinedList.Distinct().ToList();
+                ProjectTimetypes = GetSelectedList(CombinedList, TimeTypesAssignedToProject).ToArray();
+                AllTimeTypes = CombinedList.Except(ProjectTimetypes).ToArray();
+            }
+            cblTimeTypesAssignedToProject.DataSource = ProjectTimetypes.OrderBy(t => t.Name);
+            cblTimeTypesAssignedToProject.DataBind();
+            cblTimeTypesNotAssignedToProject.DataSource = AllTimeTypes.OrderBy(t => t.Name);
+            cblTimeTypesNotAssignedToProject.DataBind();
+            AddAttributescblTimeTypesNotAssignedToProject();
+            AddAttributesTocblTimeTypesAssignedToProject();
+            ScriptManager.RegisterStartupScript(this, this.GetType(), "SetTimeTypesAssignedToProject", "SetTimeTypesAssignedToProject();changeAlternateitemscolrsForCBL();chbCanCreateCustomWorkTypes_Change();", true);
+        }
+
+        private List<TimeTypeRecord> GetSelectedList(List<TimeTypeRecord> CombinedList, string Ids)
+        {
+            List<string> IdsList = Ids.Split(',').ToList();
+            List<TimeTypeRecord> SelectedList = new List<TimeTypeRecord>();
+            foreach (string Id in IdsList)
+            {
+                int id = 0;
+                int.TryParse(Id, out id);
+                if (CombinedList.AsQueryable().Any(t => t.Id == id))
+                {
+                    TimeTypeRecord tt = CombinedList.AsQueryable().First(t => t.Id == id);
+                    SelectedList.Add(tt);
+                }
+            }
+            return SelectedList;
+        }
+
+        private void AddAttributescblTimeTypesNotAssignedToProject()
+        {
+            foreach (ListItem item in cblTimeTypesNotAssignedToProject.Items)
+            {
+                item.Attributes["timeTypeid"] = item.Value;
+                item.Attributes["timeTypename"] = item.Text;
+            }
+        }
+
+        private void AddAttributesTocblTimeTypesAssignedToProject()
+        {
+            foreach (ListItem item in cblTimeTypesAssignedToProject.Items)
+            {
+                item.Attributes["timeTypeid"] = item.Value;
+                item.Attributes["timeTypename"] = item.Text;
+            }
+        }
+
+        protected void imgUpdateTimeType_OnClick(object sender, EventArgs e)
+        {
+            if (IsTimeTypeAlreadyExisting(txtNewTimeType.Text))
+            {
+                cvUpdatedTimeTypeName.IsValid = false;
+            }
+            else
+            {
+                try
+                {
+                    TimeTypeRecord customTimeType = new TimeTypeRecord();
+                    customTimeType.IsDefault = false;
+                    customTimeType.IsInternal = false;
+                    customTimeType.Name = txtNewTimeType.Text;
+                    customTimeType.IsInternal = false;
+                    int customTimeTypeId = ServiceCallers.Invoke<TimeEntryServiceClient, int>(TimeType => TimeType.AddTimeType(customTimeType));
+                    customTimeType.Id = customTimeTypeId;
+                    var ptt = ProjectTimetypes.ToList();
+                    ptt.Add(customTimeType);
+                    ProjectTimetypes = ptt.ToArray();
+                    cblTimeTypesAssignedToProject.DataSource = ProjectTimetypes.OrderBy(t => t.Name);
+                    cblTimeTypesAssignedToProject.DataBind();
+                    AddAttributesTocblTimeTypesAssignedToProject();
+                }
+                catch (Exception e1)
+                {
+
+                }
+            }
+        }
+        private bool IsTimeTypeAlreadyExisting(string newTimeType)
+        {
+            using (TimeEntryServiceClient serviceClient = new TimeEntryServiceClient())
+            {
+                TimeTypeRecord[] timeTypesArray = serviceClient.GetAllTimeTypes();
+
+                foreach (TimeTypeRecord timeType in timeTypesArray)
+                {
+                    if (timeType.Name.ToLower() == newTimeType.ToLower())
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+
+
+    }
+}
