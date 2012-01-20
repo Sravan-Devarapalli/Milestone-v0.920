76075c9a710b5cef12e5044413a0a6359fc2fa75
ThulasiramP <ThulasiramP>
2012-01-20 05:38:00 -0500

    Comments - Defect Number: 2980; Defect RecordID: 2976; Server Address: services.logic2020.com; Server Port: 99; Database Name: Practice Management; Defect Summary: New Enhancement - Improved Time Tracking ; Defect Description: Madhan,
    Attached is the draft specification for some significant changes to the way Logic20/20 wants to track time in Practice Management. Please review the Word document and reference the attached Excel workbook to provide additional detail. Ill step through each with you and your team tomorrow morning (Pacific time).
    
    I imagine some impacts on our existing data model/schema. I will need some expert guidance from your team on the impacts of this specification and how best to architect the data model to provide the operational reporting flexibility we will need as a result of the system changes. We will want to be able to have a far more interactive reporting experience than what is presently afforded in PM to enable roll-up and drill-down capability leveraging this new time tracking design.
    
    Additionally, I am attaching some sample UI changes we would like to discuss as these represent conceptual thoughts on the direction wed like to move for our Time Entry interface. These are not to be viewed as the only approach but should be seen as a sample for discussion. I actually think that combining elements of both is the right solution but lets discuss based on your reading of the specifications.
    
    Our hope is that GGK will be able to run with this specification while Jason and I are on holiday from 12/22  1/2/2012 and return with some sample prototypes if not working code in a test environment by that first week of January. I can make myself available for a call during my holiday if the team gets stuck or has urgent questions that must be answered to avoid getting stuck.
    
    Best,
    Matt
    
    Matthew Reilly, Senior Manager  Consulting Operations// Logic20/20 // MattR@logic2020.com
    t.206-576-0491//c.425-503-5825// www.logic2020.com

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/Controls/TimeEntry/SingleTimeEntry_New.ascx.cs
@@ -0,0 +1,334 @@
+﻿using System;
+using System.Drawing;
+using System.Web.Security;
+using System.Web.UI.WebControls;
+using DataTransferObjects.TimeEntry;
+using PraticeManagement.Utils;
+using System.Collections.Generic;
+using System.Xml.Linq;
+
+namespace PraticeManagement.Controls.TimeEntry
+{
+    public partial class SingleTimeEntry_New : System.Web.UI.UserControl
+    {
+        #region Constants
+
+        private const string DateBehindViewstate = "7555B3A7-8713-490F-8D5B-368A02E6A205";
+
+        #endregion
+
+        #region Properties
+
+        public bool IsThereAtleastOneTimeEntryrecord
+        {
+            get
+            {
+                return ((!string.IsNullOrEmpty(tbNotes.Text)) || (!string.IsNullOrEmpty(tbActualHours.Text)));
+            }
+        }
+
+        public bool IsPTO { get; set; }
+
+        public XElement TimeEntryRecordElement
+        {
+            get;
+            set;
+        }
+
+        public DateTime DateBehind
+        {
+            get
+            {
+                return (DateTime)ViewState[DateBehindViewstate];
+            }
+            set
+            {
+                ViewState[DateBehindViewstate] = value;
+            }
+        }
+
+        public string VerticalTotalCalculatorExtenderId
+        {
+            get
+            {
+                return hfVerticalTotalCalculatorExtender.Value;
+            }
+            set
+            {
+                hfVerticalTotalCalculatorExtender.Value = value;
+            }
+        }
+
+        public TimeEntry_New HostingPage
+        {
+            get
+            {
+                return ((TimeEntry_New)Page);
+            }
+        }
+
+        public string ProjectMilestoneDropdownClientId
+        {
+            set
+            {
+                this.deActualHours.ProjectMilestoneDropdownIdValue = value;
+            }
+        }
+
+        public string IsNoteRequired
+        {
+            set
+            {
+                hdnIsNoteRequired.Value = value;
+            }
+            get
+            {
+                return hdnIsNoteRequired.Value;
+            }
+        }
+
+        public string HorizontalTotalCalculatorExtenderId
+        {
+            get
+            {
+                return hfHorizontalTotalCalculatorExtender.Value;
+            }
+            set
+            {
+                hfHorizontalTotalCalculatorExtender.Value = value;
+            }
+        }
+
+        public string SpreadSheetTotalCalculatorExtenderId
+        {
+            get
+            {
+                return hfSpreadSheetTotalCalculatorExtender.Value;
+            }
+            set
+            {
+                hfSpreadSheetTotalCalculatorExtender.Value = value;
+            }
+        }
+
+        #endregion
+
+        #region Control events
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            tbNotes.Attributes["imgNoteClientId"] = imgNote.ClientID;
+        }
+
+        protected void Page_PreRender(object sender, EventArgs e)
+        {
+            SpreadSheetTotalCalculatorExtenderId = HostingPage.SpreadSheetTotalCalculatorExtenderId;
+
+
+        }
+
+        public void CanelControlStyle()
+        {
+            tbActualHours.BackColor = Color.White;
+        }
+
+
+        protected override void OnPreRender(EventArgs e)
+        {
+            base.OnPreRender(e);
+
+            if (TimeEntryRecordElement != null)
+            {
+                FillControls();
+            }
+
+            CanelControlStyle();
+            ApplyControlStyle();
+
+            tbActualHours.Attributes["IsPTO"] = IsPTO.ToString();
+            tbActualHours.Attributes["txtboxNoteClienId"] = tbNotes.ClientID;
+
+            MaintainEditedtbActualHoursStyle();
+        }
+
+        private void MaintainEditedtbActualHoursStyle()
+        {
+            if (tbActualHours.Style["background-color"] != "red")
+            {
+                tbActualHours.Style["background-color"] = (hfDirtyHours.Value == "dirty") ? "gold" : "none";
+            }
+        }
+
+        protected void cvNotes_OnServerValidate(object source, ServerValidateEventArgs args)
+        {
+            var note = args.Value;
+            if (note.Length < 3 || note.Length > 1000)
+                args.IsValid = false;
+        }
+
+        #endregion
+
+        #region Methods
+
+        private void FillControls()
+        {
+            EnsureChildControls();
+            tbNotes.Text = TimeEntryRecordElement.Attribute(XName.Get("Note")).Value;
+            hdnNotes.Value = TimeEntryRecordElement.Attribute(XName.Get("Note")).Value;
+            imgNote.ImageUrl
+                = string.IsNullOrEmpty(tbNotes.Text)
+                      ? Constants.ApplicationResources.AddCommentIcon
+                      : Constants.ApplicationResources.RecentCommentIcon;
+
+
+            tbActualHours.Text = TimeEntryRecordElement.Attribute(XName.Get("ActualHours")).Value;
+            hdnActualHours.Value = tbActualHours.Text;
+            chbIsChargeable.Checked = Convert.ToBoolean(TimeEntryRecordElement.Attribute(XName.Get("IsChargeable")).Value);
+            hdnIsChargeable.Value = chbIsChargeable.Checked.ToString().ToLower();
+            chbForDiffProject.Checked = !Convert.ToBoolean(TimeEntryRecordElement.Attribute(XName.Get("IsCorrect")).Value); ;
+            hdnForDiffProject.Value = (!(chbForDiffProject.Checked)).ToString().ToLower();
+
+            var isReviewd = TimeEntryRecordElement.Attribute(XName.Get("IsReviewed")).Value;
+            lblReview.Text = isReviewd;
+            if (isReviewd == ReviewStatus.Approved.ToString())
+                Disabled = true;
+
+            hfDirtyHours.Value = TimeEntryRecordElement.Attribute(XName.Get("IsDirty")).Value;
+
+            imgNote.ToolTip = tbNotes.Text;
+        }
+
+        protected string GetNowDate()
+        {
+            return DateTime.Now.ToString(Constants.Formatting.EntryDateFormat);
+        }
+
+        private void ApplyControlStyle()
+        {
+            if (!tbActualHours.Enabled && string.IsNullOrEmpty(tbActualHours.Text))
+                tbActualHours.BackColor = Color.Gray;
+        }
+
+        public bool Disabled
+        {
+            set
+            {
+                var enabled = !value;
+
+                btnSaveNotes.Enabled =
+                tbNotes.Enabled =
+                chbForDiffProject.Enabled =
+                tbActualHours.Enabled = enabled;
+
+
+            }
+        }
+
+        public bool InActiveNotes
+        {
+            set
+            {
+                imgNote.Enabled = !value;
+            }
+        }
+
+        internal void UpdateEditedValues(XElement element)
+        {
+            if (element.HasAttributes && element.Attribute(XName.Get("ActualHours")) != null)
+            {
+                element.Attribute(XName.Get("ActualHours")).Value = tbActualHours.Text;
+                element.Attribute(XName.Get("Note")).Value = tbNotes.Text;
+                element.Attribute(XName.Get("IsCorrect")).Value = (!(chbForDiffProject.Checked)).ToString();
+                element.Attribute(XName.Get("IsDirty")).Value = hfDirtyHours.Value;
+            }
+            else
+            {
+                var time = SettingsHelper.GetCurrentPMTime();
+                element.SetAttributeValue(XName.Get("ActualHours"), tbActualHours.Text);
+                element.SetAttributeValue(XName.Get("Note"), tbNotes.Text);
+                element.SetAttributeValue(XName.Get("EntryDate"), time.ToString(Constants.Formatting.EntryDateFormat));
+                element.SetAttributeValue(XName.Get("IsCorrect"), (!(chbForDiffProject.Checked)).ToString());
+                element.SetAttributeValue(XName.Get("IsDirty"), hfDirtyHours.Value );
+            }
+        }
+
+        internal void UpdateVerticalTotalCalculatorExtenderId(string clientId)
+        {
+            VerticalTotalCalculatorExtenderId = clientId;
+        }
+
+        internal void ValidateNoteAndHours()
+        {
+            var isValidNote = IsValidNote();
+            var isValidHours = IsValidHours();
+
+            if (!isValidNote)
+                HostingPage.IsValidNote = isValidNote;
+
+            if (!isValidHours)
+                HostingPage.IsValidHours = isValidHours;
+
+            if (isValidNote && isValidHours)
+            {
+                tbActualHours.Style["background-color"] = "none";
+            }
+            else
+            {
+                tbActualHours.Style["background-color"] = "red";
+            }
+        }
+
+        private bool IsValidNote()
+        {
+            imgNote.ImageUrl = string.IsNullOrEmpty(tbNotes.Text) ?
+                        PraticeManagement.Constants.ApplicationResources.AddCommentIcon :
+                        PraticeManagement.Constants.ApplicationResources.RecentCommentIcon;
+
+            var note = tbNotes.Text;
+
+            if (hdnIsNoteRequired.Value == "true")
+            {
+                if (string.IsNullOrEmpty(note) || note.Length < 3 || note.Length > 1000)
+                {
+                    return false;
+                }
+            }
+            else if (!string.IsNullOrEmpty(note))
+            {
+                if (note.Length < 3 || note.Length > 1000)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        private bool IsValidHours()
+        {
+            double hours;
+            if (string.IsNullOrEmpty(tbActualHours.Text))
+            {
+                if (string.IsNullOrEmpty(tbNotes.Text))
+                {
+                    return true;
+                }
+
+                return false;
+            }
+            //  Check that hours is double between 0.0 and 24.0
+            if (double.TryParse(tbActualHours.Text, out hours))
+            {
+                if (hours > 0.0 && hours <= 24.0)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        #endregion
+    }
+}
+
