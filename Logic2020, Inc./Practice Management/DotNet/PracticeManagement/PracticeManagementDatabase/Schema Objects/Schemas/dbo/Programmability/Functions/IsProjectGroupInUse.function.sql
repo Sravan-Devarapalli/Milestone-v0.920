9b9b6b9318e85be2d9b3da2b33bb914029ecd3d2
SrinivasM <SrinivasM>
2012-01-20 08:44:00 -0500

    Comments - DBChanges regarding new TimeEntry page.

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Functions/IsProjectGroupInUse.function.sql
@@ -0,0 +1,23 @@
+﻿CREATE FUNCTION [dbo].[IsProjectGroupInUse]
+(
+	@ProjectGroupId int
+)
+RETURNS INT
+AS
+BEGIN
+
+	IF EXISTS(SELECT TOP 1 1 FROM Project WHERE GroupId = @ProjectGroupId)
+	BEGIN
+		RETURN 1
+	END
+	ELSE IF EXISTS(SELECT TOP 1 1 FROM Opportunity WHERE GroupId = @ProjectGroupId)
+	BEGIN
+		RETURN 1
+	END
+	ELSE IF EXISTS(SELECT TOP 1 1 FROM dbo.ChargeCode cc INNER JOIN dbo.TimeTrack tt on tt.ChargeCodeId = cc.Id AND cc.ProjectGroupId = @ProjectGroupId)
+	BEGIN
+		RETURN 1
+	END
+	RETURN 0
+	
+END
