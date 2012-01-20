c1592dea5443927e228c38ae5be5ec2dec6321c0
SrinivasM <SrinivasM>
2012-01-20 08:44:00 -0500

    Comments - DBChanges regarding new TimeEntry page.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/ClientInsert.proc.sql
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/ClientInsert.proc.sql
@@ -15,26 +15,78 @@ CREATE PROCEDURE dbo.ClientInsert
 	@IsChargeable         BIT,
 	@DefaultTerms         INT,
 	@ClientId             INT OUTPUT,
-	@IsMarginColorInfoEnabled BIT = NULL
+	@IsMarginColorInfoEnabled BIT = NULL,
+	@IsInternal BIT
 )
 AS
 	SET NOCOUNT ON
-
+	
+	DECLARE @ErrorMessage NVARCHAR(2048)
 	IF EXISTS (SELECT 1 FROM dbo.Client AS c WHERE c.Name = @Name)
 	BEGIN
-		DECLARE @ErrorMessage NVARCHAR(2048)
 		SELECT @ErrorMessage = [dbo].[GetErrorMessage](70004)
 		RAISERROR (@ErrorMessage, 16, 1)
 	END
 	ELSE
 	BEGIN
+		BEGIN TRY
+		
+		/*
+		NOTE:At present there is no seprate range specified for internal or external clients and For Logic2020 code: C2020.
+		RANGE : C0000 - C9999
+		*/
+
+		DECLARE  @clientCode			 NVARCHAR(10),
+				 @LowerLimitRange		 INT ,
+				 @HigherLimitRange		 INT ,
+				 @NextClientNumber		 INT,
+				 @Error					 NVARCHAR(MAX)
+		SET @LowerLimitRange = 0
+		SET @HigherLimitRange = 9999
+		SET @Error = 'Account code not avaliable'
+
+		DECLARE @ClientRanksList TABLE (clientNumber INT,clientNumberRank INT)
+		INSERT INTO @ClientRanksList 
+		SELECT Convert(INT,SUBSTRING(Code,2,5)) as clientNumber ,
+				RANK() OVER (ORDER BY Convert(INT,SUBSTRING(Code,2,5)))+@LowerLimitRange-1 AS  clientNumberRank
+		FROM dbo.Client  
+		WHERE ISNUMERIC( SUBSTRING(Code,2,5)) = 1
+
+
+		INSERT INTO @ClientRanksList 
+		SELECT -1,MAX(clientNumberRank)+1 FROM @ClientRanksList
+
+		SELECT TOP 1 @NextClientNumber = clientNumberRank 
+			FROM @ClientRanksList  
+			WHERE clientNumber != clientNumberRank 
+			ORDER BY clientNumberRank
+
+				
+		IF (@NextClientNumber IS NULL AND NOT EXISTS(SELECT 1 FROM @ClientRanksList WHERE clientNumber != -1))
+		BEGIN 
+			SET  @NextClientNumber = @LowerLimitRange
+		END 
+		ELSE IF (@NextClientNumber > @HigherLimitRange )
+		BEGIN
+			RAISERROR (@Error, 16, 1)
+		END
+
+		SET @clientCode = 'C'+ REPLICATE('0',4-LEN(@NextClientNumber)) +CONVERT(NVARCHAR,@NextClientNumber)
+
 		INSERT INTO Client
-					(DefaultDiscount, DefaultTerms, DefaultSalespersonId, DefaultDirectorID, Name, Inactive, IsChargeable,IsMarginColorInfoEnabled)
-			 VALUES (@DefaultDiscount, @DefaultTerms, @DefaultSalespersonId, @DefaultDirectorId, @Name, @Inactive, @IsChargeable,@IsMarginColorInfoEnabled)
+					(DefaultDiscount, DefaultTerms, DefaultSalespersonId, DefaultDirectorID, Name, Inactive, IsChargeable,IsMarginColorInfoEnabled,IsInternal,Code)
+			 VALUES (@DefaultDiscount, @DefaultTerms, @DefaultSalespersonId, @DefaultDirectorId, @Name, @Inactive, @IsChargeable,@IsMarginColorInfoEnabled,@IsInternal,@clientCode)
 
 		SELECT @clientId = SCOPE_IDENTITY()
 
 		SELECT @clientId
+
+		END TRY
+		BEGIN CATCH
+			SELECT @ErrorMessage = ERROR_MESSAGE()
+			
+			RAISERROR (@ErrorMessage, 16, 1)
+		END CATCH
 	END
 GO
 
