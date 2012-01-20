334d6c56544b20471ab6c3baabde98b532f94aa4
SrinivasM <SrinivasM>
2012-01-20 08:44:00 -0500

    Comments - DBChanges regarding new TimeEntry page.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/ProjectGroupInsert.proc.sql
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/ProjectGroupInsert.proc.sql
@@ -7,11 +7,32 @@ AS
 	SET NOCOUNT ON
 	IF NOT EXISTS(SELECT 1 FROM Client WHERE ClientId=@ClientId)
 		OR EXISTS(SELECT 1 FROM ProjectGroup WHERE ClientId=@ClientId AND Name=@Name)
+	BEGIN
 		SET @GroupId = -1
+	END
 	ELSE
 	BEGIN
-		INSERT ProjectGroup(ClientId, Name,Active) VALUES (@ClientId, @Name,@IsActive)
+		
+		BEGIN TRY
+		DECLARE @IsInternal BIT,
+				@ProjectGroupCode NVARCHAR (5)
+				
+		SELECT @IsInternal = c.IsInternal FROM dbo.Client c  WHERE c.ClientId = @ClientId
+
+		EXEC [GenerateNewProjectGroupCode] @IsInternal, @ProjectGroupCode OUTPUT
+
+		INSERT ProjectGroup(ClientId, Name,Active,Code) 
+		VALUES (@ClientId, @Name,@IsActive,@ProjectGroupCode)
+
 		SET @GroupId = SCOPE_IDENTITY()
+
+		END TRY
+		BEGIN CATCH
+			DECLARE @ErrorMessage NVARCHAR(2048)
+			SELECT @ErrorMessage = ERROR_MESSAGE()
+			
+			RAISERROR (@ErrorMessage, 16, 1)
+		END CATCH
 	END
 	 
 
