274c63149fa27f1bd69ac86e9fb3b2557f8c8f4e
ThulasiramP <ThulasiramP>
2012-01-20 05:38:00 -0500

    Comments - Defect Number: 2980; Defect RecordID: 2976; Server Address: services.logic2020.com; Server Port: 99; Database Name: Practice Management; Defect Summary: New Enhancement - Improved Time Tracking ; Defect Description: Madhan,
    Attached is the draft specification for some significant changes to the way Logic20/20 wants to track time in Practice Management. Please review the Word document and reference the attached Excel workbook to provide additional detail. Ill step through each with you and your team tomorrow morning (Pacific time).
    
    I imagine some impacts on our existing data model/schema. I will need some expert guidance from your team on the impacts of this specification and how best to architect the data model to provide the operational reporting flexibility we will need as a result of the system changes. We will want to be able to have a far more interactive reporting experience than what is presently afforded in PM to enable roll-up and drill-down capability leveraging this new time tracking design.
    
    Additionally, I am attaching some sample UI changes we would like to discuss as these represent conceptual thoughts on the direction wed like to move for our Time Entry interface. These are not to be viewed as the only approach but should be seen as a sample for discussion. I actually think that combining elements of both is the right solution but lets discuss based on your reading of the specifications.
    
    Our hope is that GGK will be able to run with this specification while Jason and I are on holiday from 12/22  1/2/2012 and return with some sample prototypes if not working code in a test environment by that first week of January. I can make myself available for a call during my holiday if the team gets stuck or has urgent questions that must be answered to avoid getting stuck.
    
    Best,
    Matt
    
    Matthew Reilly, Senior Manager  Consulting Operations// Logic20/20 // MattR@logic2020.com
    t.206-576-0491//c.425-503-5825// www.logic2020.com

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/Controls/TimeEntry/AdministrativeTimeEntryBar.ascx.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using DataTransferObjects;
+using DataTransferObjects.TimeEntry;
+using System.Xml.Linq;
+
+namespace PraticeManagement.Controls.TimeEntry
+{
+    public partial class AdministrativeTimeEntryBar : System.Web.UI.UserControl
+    {
+        public List<XElement> TeBarDataSource
+        {
+            get;
+            set;
+        }
+
+        public bool Disabled { get; set; }
+
+
+        public bool IsPTO { get; set; }
+
+        public TimeTypeRecord WorkType { get; set; }
+
+        public Dictionary<int, string> TbAcutualHoursClientIds
+        {
+            get
+            {
+                return ViewState["AdministrativeTimeEntryBar_TbAcutualHoursClientIds"] as Dictionary<int, string>;
+            }
+            set
+            {
+                ViewState["AdministrativeTimeEntryBar_TbAcutualHoursClientIds"] = value;
+            }
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Page_PreRender(object sender, EventArgs e)
+        {
+            
+        }
+
+
+        public TimeEntry_New HostingPage
+        {
+            get
+            {
+                return ((TimeEntry_New)Page);
+            }
+        }
+
+
+        protected string GetDayOffCssCalss(XElement calendarItem)
+        {
+            return calendarItem.Attribute(XName.Get("CssClass")).Value;
+        }
+
+
+        private void InitTimeEntryControl(SingleTimeEntry_New ste, DateTime date, XElement terXlement)
+        {
+            ste.DateBehind = date;
+            ste.TimeEntryRecordElement = terXlement;
+        }
+
+        protected void repEntries_ItemDataBound(object sender, RepeaterItemEventArgs e)
+        {
+            if (e.Item.ItemType == ListItemType.Item || e.Item.ItemType == ListItemType.AlternatingItem)
+            {
+                var ste = e.Item.FindControl("ste") as SingleTimeEntry_New;
+
+                var calendarItem = (XElement)e.Item.DataItem;
+
+                var textBoxId = ste.Controls[1].ClientID;
+
+                TbAcutualHoursClientIds = TbAcutualHoursClientIds ?? new Dictionary<int, string>();
+
+                TbAcutualHoursClientIds.Add(e.Item.ItemIndex, textBoxId);
+                TbAcutualHoursClientIds = TbAcutualHoursClientIds;
+                extTotalHours.ControlsToCheck += textBoxId + ";";
+
+                ste.HorizontalTotalCalculatorExtenderId = extTotalHours.ClientID;
+                ste.IsNoteRequired = calendarItem.Attribute(XName.Get("IsNoteRequired")).Value;
+                ste.Disabled = Disabled;
+                ste.IsPTO = IsPTO;
+
+                var nbterecord = (calendarItem.HasElements && calendarItem.Descendants(XName.Get("TimeEntryRecord")).Where(ter => ter.Attribute(XName.Get("IsChargeable")).Value.ToLowerInvariant() == "false").ToList().Count > 0) ? calendarItem.Descendants(XName.Get("TimeEntryRecord")).Where(ter => ter.Attribute(XName.Get("IsChargeable")).Value.ToLowerInvariant() == "false").First() : null;
+                var date = Convert.ToDateTime(calendarItem.Attribute(XName.Get("Date")).Value);
+                InitTimeEntryControl(ste, date, nbterecord);
+
+
+            }
+        }
+
+        protected void imgDropTes_Click(object sender, ImageClickEventArgs e)
+        {
+            //var daysNumber = HostingPage.SelectedDates.Length;
+            //var startDate = HostingPage.SelectedDates[0];
+            //var endDate = HostingPage.SelectedDates[daysNumber - 1];
+
+            //TimeEntryHelper.RemoveTimeEntries(ddlProjectMilestone.SelectedValue, ddlTimeTypes.SelectedValue, startDate, endDate);
+
+            //HostingPage.TimeEntryControl.UpdateTimeEntries();
+            //HostingPage.ClearDirtyState();
+            //HostingPage.SetAttributesToDropDowns();
+        }
+
+        public void UpdateTimeEntries()
+        {
+            hdnworkTypeId.Value = WorkType.Id.ToString();
+            lblTimeType.Text = lblTimeType.ToolTip = WorkType.Name;
+            tes.DataSource = TeBarDataSource;
+            tes.DataBind();
+        }
+
+        internal void UpdateNoteAndActualHours(List<XElement> calendarItemElements)
+        {
+            for (int k = 0; k < calendarItemElements.Count; k++)
+            {
+
+                var nonbillableSte = tes.Items[k].FindControl("ste") as SingleTimeEntry_New;
+
+                var calendarItemElement = calendarItemElements[k];
+                if (calendarItemElement.HasElements)
+                {
+                    var nonBillableElements = calendarItemElement.Descendants(XName.Get("TimeEntryRecord")).ToList().Count > 0 ? calendarItemElement.Descendants(XName.Get("TimeEntryRecord")).Where(ter => ter.Attribute(XName.Get("IsChargeable")).Value.ToLowerInvariant() == "false").ToList() : null;
+                    if (nonBillableElements != null && nonBillableElements.Count > 0)
+                    {
+                        var nonbillableElement = nonBillableElements != null && nonBillableElements.Count > 0 ? nonBillableElements.First() : null;
+                        nonbillableSte.UpdateEditedValues(nonbillableElement);
+                    }
+                }
+                else
+                {
+                    //Add Element
+                    var nonBillableElement = new XElement("TimeEntryRecord");
+                    nonBillableElement.SetAttributeValue(XName.Get("IsChargeable"), "false");
+                    nonbillableSte.UpdateEditedValues(nonBillableElement);
+
+                    if (nonBillableElement.Attribute(XName.Get("ActualHours")).Value != "" || nonBillableElement.Attribute(XName.Get("Note")).Value != "")
+                    {
+                        calendarItemElement.Add(nonBillableElement);
+                    }
+
+                }
+            }
+
+
+        }
+
+
+        internal void UpdateVerticalTotalCalculatorExtenderId(int index, string clientId)
+        {
+            var nonbillableSte = tes.Items[index].FindControl("ste") as SingleTimeEntry_New;
+            nonbillableSte.UpdateVerticalTotalCalculatorExtenderId(clientId);
+
+        }
+
+        internal void ValidateAll()
+        {
+            foreach (RepeaterItem tesItem in tes.Items)
+            {
+                var nonbillableSte = tesItem.FindControl("ste") as SingleTimeEntry_New;
+                nonbillableSte.ValidateNoteAndHours();
+            }
+        }
+
+        internal void UpdateWorkType(XElement workTypeElement)
+        {
+            workTypeElement.Attribute(XName.Get("Id")).Value = hdnworkTypeId.Value;
+        }
+
+    }
+}
