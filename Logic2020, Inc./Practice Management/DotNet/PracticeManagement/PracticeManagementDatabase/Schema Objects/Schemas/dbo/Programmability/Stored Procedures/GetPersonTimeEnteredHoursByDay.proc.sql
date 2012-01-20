eb1647563d7b51b92f468bf5868692e0595ef7a4
SrinivasM <SrinivasM>
2012-01-20 08:44:00 -0500

    Comments - DBChanges regarding new TimeEntry page.

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/GetPersonTimeEnteredHoursByDay.proc.sql
@@ -0,0 +1,27 @@
+﻿CREATE PROCEDURE [dbo].[GetPersonTimeEnteredHoursByDay]
+	@PersonId INT, 
+	@Date		DATETIME,
+	@IncludePTOAndHoliday	BIT
+AS
+BEGIN
+	DECLARE @Hours REAL,
+		@PTOChargeCodeId INT,
+		@HolidayChargeCodeId INT
+
+	SELECT @PTOChargeCodeId = Id
+	FROM ChargeCode
+	WHERE TimeTypeId = dbo.GetPTOTimeTypeId()
+
+	SELECT @HolidayChargeCodeId = Id
+	FROM ChargeCode
+	WHERE TimeTypeId = dbo.GetHolidayTimeTypeId()
+	
+	SELECT @Hours = SUM(ActualHours)
+	FROM TimeTrack
+	WHERE PersonId = @PersonId
+	AND ChargeCodeDate = @Date
+	AND (@IncludePTOAndHoliday = 1 OR (@IncludePTOAndHoliday = 0 AND ChargeCodeId NOT IN (@PTOChargeCodeId, @HolidayChargeCodeId)))
+	GROUP BY PersonId, ChargeCodeDate
+
+	SELECT @Hours
+END
