0b4ad8dd818c72ec40b6815ea5212ced3067210c
SrinivasM <SrinivasM>
2012-01-20 08:44:00 -0500

    Comments - DBChanges regarding new TimeEntry page.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Functions/GetHolidayTimeTypeId.function.sql
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Functions/GetHolidayTimeTypeId.function.sql
@@ -9,7 +9,7 @@ BEGIN
 
 	SELECT @Id = tt.TimeTypeId 
 	FROM TimeType tt 
-	WHERE tt.Name = 'Holiday' --Here Holiday is the Time Type Name.
+	WHERE tt.Code = 'W9320' --Here 'W9320' is code of Holiday Work Type.
 	
 	return @Id
 END
