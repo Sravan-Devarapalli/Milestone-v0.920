b054e6b32cd7d32a6420c4bf0a3477cc41712172
SrinivasM <SrinivasM>
2012-01-20 08:44:00 -0500

    Comments - DBChanges regarding new TimeEntry page.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/TimeTypeDelete.proc.sql
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/TimeTypeDelete.proc.sql
@@ -9,6 +9,33 @@ AS
 BEGIN
 	SET NOCOUNT ON;
 	
-	DELETE FROM TimeType WHERE TimeTypeId = @TimeTypeId
+	BEGIN TRY
+	BEGIN TRAN timeTypeDelete
+		--Delete chargeCode records related to the @TimeTypeId
+		IF EXISTS (SELECT TOP 1 1 FROM dbo.ChargeCode cc WHERE cc.TimeTypeId = @TimeTypeId)
+		BEGIN
+
+			DELETE cch
+			FROM dbo.ChargeCodeTurnOffHistory cch INNER JOIN dbo.ChargeCode cc ON cch.ChargeCodeId = cc.Id WHERE cc.TimeTypeId = @TimeTypeId
+
+			DELETE cc 
+			FROM dbo.ChargeCode cc WHERE cc.TimeTypeId = @TimeTypeId
+
+		END
+			
+		DELETE FROM ProjectTimeType WHERE TimeTypeId = @TimeTypeId
+
+		DELETE FROM TimeType WHERE TimeTypeId = @TimeTypeId
+
+	COMMIT TRAN timeTypeDelete
+	END TRY
+	BEGIN CATCH
+	ROLLBACK TRAN timeTypeDelete
+	
+		DECLARE @ErrorMessage NVARCHAR(MAX)
+		SELECT @ErrorMessage = ERROR_MESSAGE()
+			
+		RAISERROR(@ErrorMessage, 16, 1)
+	END CATCH
 END
 
