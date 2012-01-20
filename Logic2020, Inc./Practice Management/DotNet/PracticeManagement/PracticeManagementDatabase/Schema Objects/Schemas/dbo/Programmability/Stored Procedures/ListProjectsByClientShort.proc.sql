e3abee7dc295afafd1505b1e8a2ac49d17ca92dd
SrinivasM <SrinivasM>
2012-01-20 08:44:00 -0500

    Comments - DBChanges regarding new TimeEntry page.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/ListProjectsByClientShort.proc.sql
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/ListProjectsByClientShort.proc.sql
@@ -1,37 +1,36 @@
 ﻿CREATE PROCEDURE dbo.ListProjectsByClientShort
 (
 	@ClientId INT,
-	@IsOnlyActiveAndProjective BIT
+	@IsOnlyActiveAndProjective BIT,
+	@IsOnlyActiveAndInternal	BIT = 0
 )
 AS
 BEGIN
 	SET NOCOUNT ON
-	IF (@IsOnlyActiveAndProjective = 1)
-	BEGIN
-		SELECT p.ClientId,
-			   p.ProjectId,
-			   p.Name,
-			   p.ClientName,
-			   p.ProjectNumber,
-			   p.BuyerName,
-			   p.Description
-		FROM dbo.v_Project AS p
-		WHERE p.ClientId = @ClientId 
-			   AND p.ProjectStatusId IN (2,3) 
-		ORDER BY p.Name
-	END
-	ELSE
-	BEGIN
-		SELECT p.ClientId,
-			   p.ProjectId,
-			   p.Name,
-			   p.ClientName,
-			   p.ProjectNumber,
-			   p.BuyerName,
-			   p.Description    
-		FROM dbo.v_Project AS p
-		WHERE p.ClientId = @ClientId 
-		ORDER BY p.Name
-	END
+	/*
+		1	Inactive
+		2	Projected
+		3	Active
+		4	Completed
+		5	Experimental
+		6	Internal
+	*/
+
+	SELECT p.ClientId,
+			p.ProjectId,
+			p.Name,
+			p.ClientName,
+			p.ProjectNumber,
+			p.BuyerName,
+			p.Description
+	FROM dbo.v_Project AS p
+	WHERE p.ClientId = @ClientId AND p.IsAllowedToShow = 1
+		AND ( (@IsOnlyActiveAndProjective = 1 AND @IsOnlyActiveAndInternal = 0 AND p.ProjectStatusId IN (2,3))
+			OR (@IsOnlyActiveAndProjective = 0 AND @IsOnlyActiveAndInternal = 1 AND p.ProjectStatusId IN (3,6))
+			OR (@IsOnlyActiveAndProjective = 1 AND @IsOnlyActiveAndInternal = 1 AND p.ProjectStatusId IN (2,3,6))
+			OR (@IsOnlyActiveAndInternal = 0 AND @IsOnlyActiveAndProjective = 0)
+			)
+	ORDER BY p.Name
 END
 	
+
