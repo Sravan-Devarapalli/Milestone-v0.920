22a59db579e2518fe5a53ef26ebc6a851fe20ab8
SrinivasM <SrinivasM>
2012-01-20 08:44:00 -0500

    Comments - DBChanges regarding new TimeEntry page.

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/PersonTimeEntryRecursiveSelection.table.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[PersonTimeEntryRecursiveSelection]
+(
+	Id					INT IDENTITY (1, 1) NOT NULL,
+	PersonId			INT NOT NULL,
+	ClientId			INT NOT NULL,
+	ProjectId			INT NOT NULL,
+	ProjectGroupId		INT NULL,
+	TimeEntrySectionId	INT NOT NULL,
+	StartDate			DATETIME NOT NULL,
+	EndDate				DATETIME NULL
+	CONSTRAINT PK_PersonTimeEntryRecursiveSelection_Id PRIMARY KEY CLUSTERED (Id ASC)
+);
+
