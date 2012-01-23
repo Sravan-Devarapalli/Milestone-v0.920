591a626c5308b119bbf379b3a87902ed1216794d
SainathC <SainathC>
2012-01-23 03:59:00 -0500

    none

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/Controls/Generic/EnableDisableExtender/EnableDisableExtender.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.UI;
+using AjaxControlToolkit;
+
+[assembly: WebResource("PraticeManagement.Controls.Generic.EnableDisableExtender.EnableDisableExtender.js", "text/javascript")]
+
+namespace PraticeManagement.Controls.Generic.EnableDisableExtender
+{
+    using System.ComponentModel;
+
+    [ClientScriptResource("PraticeManagement.Controls.Generic.EnableDisableExtender.EnableDisableExtenderBehavior", "PraticeManagement.Controls.Generic.EnableDisableExtender.EnableDisableExtender.js")]
+    [TargetControlType(typeof(Control))]
+    public class EnableDisableExtender : ExtenderControlBase
+    {
+        [ExtenderControlProperty]
+        [DefaultValue("")]
+        [ClientPropertyName("controlsToCheck")]
+        public string ControlsToCheck
+        {
+            get { return GetPropertyValue("controlsToCheck", string.Empty); }
+            set { SetPropertyValue("controlsToCheck", value); }
+        }
+    }
+}
+
