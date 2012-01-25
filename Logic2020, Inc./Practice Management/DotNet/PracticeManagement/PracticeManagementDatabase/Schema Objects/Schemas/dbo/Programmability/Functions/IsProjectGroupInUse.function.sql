09c844f1f3973882a3e354fdd5f8e8b8943ef6e8
SrinivasM <SrinivasM>
2012-01-25 01:56:00 -0500

    Comments - Defect Number: 2980; Defect RecordID: 2976; Server Address: services.logic2020.com; Server Port: 99; Database Name: Practice Management; Defect Summary: New Enhancement - Improved Time Tracking ; Defect Description: Madhan,
    Attached is the draft specification for some significant changes to the way Logic20/20 wants to track time in Practice Management. Please review the Word document and reference the attached Excel workbook to provide additional detail. Ill step through each with you and your team tomorrow morning (Pacific time).
    
    I imagine some impacts on our existing data model/schema. I will need some expert guidance from your team on the impacts of this specification and how best to architect the data model to provide the operational reporting flexibility we will need as a result of the system changes. We will want to be able to have a far more interactive reporting experience than what is presently afforded in PM to enable roll-up and drill-down capability leveraging this new time tracking design.
    
    Additionally, I am attaching some sample UI changes we would like to discuss as these represent conceptual thoughts on the direction wed like to move for our Time Entry interface. These are not to be viewed as the only approach but should be seen as a sample for discussion. I actually think that combining elements of both is the right solution but lets discuss based on your reading of the specifications.
    
    Our hope is that GGK will be able to run with this specification while Jason and I are on holiday from 12/22  1/2/2012 and return with some sample prototypes if not working code in a test environment by that first week of January. I can make myself available for a call during my holiday if the team gets stuck or has urgent questions that must be answered to avoid getting stuck.
    
    Best,
    Matt
    
    Matthew Reilly, Senior Manager  Consulting Operations// Logic20/20 // MattR@logic2020.com
    t.206-576-0491//c.425-503-5825// www.logic2020.com

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Functions/IsProjectGroupInUse.function.sql
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Functions/IsProjectGroupInUse.function.sql
@@ -6,15 +6,15 @@ RETURNS INT
 AS
 BEGIN
 
-	IF EXISTS(SELECT TOP 1 1 FROM Project WHERE GroupId = @ProjectGroupId)
+	IF EXISTS(SELECT TOP 1 1 FROM dbo.Project WHERE GroupId = @ProjectGroupId)
 	BEGIN
 		RETURN 1
 	END
-	ELSE IF EXISTS(SELECT TOP 1 1 FROM Opportunity WHERE GroupId = @ProjectGroupId)
+	ELSE IF EXISTS(SELECT TOP 1 1 FROM dbo.Opportunity WHERE GroupId = @ProjectGroupId)
 	BEGIN
 		RETURN 1
 	END
-	ELSE IF EXISTS(SELECT TOP 1 1 FROM dbo.ChargeCode cc INNER JOIN dbo.TimeTrack tt on tt.ChargeCodeId = cc.Id AND cc.ProjectGroupId = @ProjectGroupId)
+	ELSE IF EXISTS(SELECT TOP 1 1 FROM dbo.ChargeCode CC INNER JOIN dbo.TimeEntry TE on TE.ChargeCodeId = CC.Id AND cc.ProjectGroupId = @ProjectGroupId)
 	BEGIN
 		RETURN 1
 	END
