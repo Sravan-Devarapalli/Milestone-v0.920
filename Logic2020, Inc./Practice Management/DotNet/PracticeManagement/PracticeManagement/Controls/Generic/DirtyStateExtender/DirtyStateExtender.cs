af01ec29e6d699b3ede623c14757f435bdb346a5
SrinivasM <SrinivasM>
2012-01-27 11:02:00 -0500

    Comments - Defect Number: 2980; Defect RecordID: 2976; Server Address: services.logic2020.com; Server Port: 99; Database Name: Practice Management; Defect Summary: New Enhancement - Improved Time Tracking ; Defect Description: Madhan,
    Attached is the draft specification for some significant changes to the way Logic20/20 wants to track time in Practice Management. Please review the Word document and reference the attached Excel workbook to provide additional detail. Ill step through each with you and your team tomorrow morning (Pacific time).
    
    I imagine some impacts on our existing data model/schema. I will need some expert guidance from your team on the impacts of this specification and how best to architect the data model to provide the operational reporting flexibility we will need as a result of the system changes. We will want to be able to have a far more interactive reporting experience than what is presently afforded in PM to enable roll-up and drill-down capability leveraging this new time tracking design.
    
    Additionally, I am attaching some sample UI changes we would like to discuss as these represent conceptual thoughts on the direction wed like to move for our Time Entry interface. These are not to be viewed as the only approach but should be seen as a sample for discussion. I actually think that combining elements of both is the right solution but lets discuss based on your reading of the specifications.
    
    Our hope is that GGK will be able to run with this specification while Jason and I are on holiday from 12/22  1/2/2012 and return with some sample prototypes if not working code in a test environment by that first week of January. I can make myself available for a call during my holiday if the team gets stuck or has urgent questions that must be answered to avoid getting stuck.
    
    Best,
    Matt
    
    Matthew Reilly, Senior Manager  Consulting Operations// Logic20/20 // MattR@logic2020.com
    t.206-576-0491//c.425-503-5825// www.logic2020.com

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/Controls/Generic/DirtyStateExtender/DirtyStateExtender.cs
@@ -0,0 +1,209 @@
+﻿using System.ComponentModel;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using AjaxControlToolkit;
+
+
+[assembly: WebResource("PraticeManagement.Controls.Generic.DirtyStateExtender.DirtyStateExtender.js", "text/javascript")]
+
+namespace PraticeManagement.Controls.Generic.DirtyStateExtender
+{
+    [ClientScriptResource(
+        "PraticeManagement.Controls.Generic.DirtyStateExtender.DirtyStateBehavior",
+        "PraticeManagement.Controls.Generic.DirtyStateExtender.DirtyStateExtender.js")]
+    [TargetControlType(typeof(HiddenField))]
+    public class DirtyStateExtender : ExtenderControlBase
+    {
+        #region Constants
+
+        private const string NOTE_ID = "NoteIdValue";
+        private const string HIDDEN_NOTE_ID = "HiddenNoteIdValue";
+        private const string ACTUAL_HOURS_ID = "ActualHoursIdValue";
+        private const string HIDDEN_ACTUAL_HOURS_ID = "HiddenActualHoursIdValue";
+        private const string HORIZONTAL_TCEXTENDER_ID = "HorizontalTotalCalculatorExtenderIdValue";
+        private const string VERTICAL_TCEXTENDER_ID = "VerticalTotalCalculatorExtenderIdValue";
+        private const string SPREADSHEET_TCEXTENDER_ID = "SpreadSheetExtenderIdValue";
+        private const string TIMETYPE_DROPDOWN_ID = "TimeTypeDropdownIdValue";
+        private const string PROJECT_MILESTONE_DROPDOWN_ID = "ProjectMilestoneDropdownIdValue";
+
+        #endregion
+
+        #region Properties
+
+        [ExtenderControlProperty]
+        [DefaultValue("")]
+        [IDReferenceProperty(typeof(TextBox))]
+        [ClientPropertyName("NoteIdValue")]
+        public string NoteId
+        {
+            get
+            {
+                return GetPropertyValue(NOTE_ID, string.Empty);
+            }
+            set
+            {
+                SetPropertyValue(NOTE_ID, value);
+            }
+        }
+
+        [ExtenderControlProperty]
+        [DefaultValue("")]
+        [IDReferenceProperty(typeof(HiddenField))]
+        [ClientPropertyName("HiddenNoteIdValue")]
+        public string HiddenNoteId
+        {
+            get
+            {
+                return GetPropertyValue(HIDDEN_NOTE_ID, string.Empty);
+            }
+            set
+            {
+                SetPropertyValue(HIDDEN_NOTE_ID, value);
+            }
+        }
+
+        [ExtenderControlProperty]
+        [DefaultValue("")]
+        [IDReferenceProperty(typeof(TextBox))]
+        [ClientPropertyName("ActualHoursIdValue")]
+        public string ActualHoursId
+        {
+            get
+            {
+                return GetPropertyValue(ACTUAL_HOURS_ID, string.Empty);
+            }
+            set
+            {
+                SetPropertyValue(ACTUAL_HOURS_ID, value);
+            }
+        }
+
+        [ExtenderControlProperty]
+        [DefaultValue("")]
+        [IDReferenceProperty(typeof(HiddenField))]
+        [ClientPropertyName("HiddenActualHoursIdValue")]
+        public string HiddenActualHoursId
+        {
+            get
+            {
+                return GetPropertyValue(HIDDEN_ACTUAL_HOURS_ID, string.Empty);
+            }
+            set
+            {
+                SetPropertyValue(HIDDEN_ACTUAL_HOURS_ID, value);
+            }
+        }
+
+        [ExtenderControlProperty]
+        [DefaultValue("")]
+        [IDReferenceProperty(typeof(HiddenField))]
+        [ClientPropertyName("HorizontalTotalCalculatorExtenderIdValue")]
+        public string HorizontalTotalCalculatorExtenderId
+        {
+            get
+            {
+                return GetPropertyValue(HORIZONTAL_TCEXTENDER_ID, string.Empty);
+            }
+            set
+            {
+                SetPropertyValue(HORIZONTAL_TCEXTENDER_ID, value);
+            }
+        }
+
+        [ExtenderControlProperty]
+        [DefaultValue("")]
+        [IDReferenceProperty(typeof(HiddenField))]
+        [ClientPropertyName("VerticalTotalCalculatorExtenderIdValue")]
+        public string VerticalTotalCalculatorExtenderId
+        {
+            get
+            {
+                return GetPropertyValue(VERTICAL_TCEXTENDER_ID, string.Empty);
+            }
+            set
+            {
+                SetPropertyValue(VERTICAL_TCEXTENDER_ID, value);
+            }
+        }
+
+        [ExtenderControlProperty]
+        [DefaultValue("")]
+        [IDReferenceProperty(typeof(HiddenField))]
+        [ClientPropertyName("SpreadSheetExtenderIdValue")]
+        public string SpreadSheetExtenderId
+        {
+            get
+            {
+                return GetPropertyValue(SPREADSHEET_TCEXTENDER_ID, string.Empty);
+            }
+            set
+            {
+                SetPropertyValue(SPREADSHEET_TCEXTENDER_ID, value);
+            }
+        }
+
+        [ExtenderControlProperty]
+        [DefaultValue("")]
+        [ClientPropertyName("TimeTypeDropdownIdValue")]
+        public string TimeTypeDropdownId
+        {
+            get
+            {
+                return GetPropertyValue(TIMETYPE_DROPDOWN_ID, string.Empty);
+            }
+            set
+            {
+                SetPropertyValue(TIMETYPE_DROPDOWN_ID, value);
+            }
+        }
+
+        [ExtenderControlProperty]
+        [DefaultValue("")]
+        [ClientPropertyName("ProjectMilestoneDropdownIdValue")]
+        public string ProjectMilestoneDropdownIdValue
+        {
+            get
+            {
+                return GetPropertyValue(PROJECT_MILESTONE_DROPDOWN_ID, string.Empty);
+            }
+            set
+            {
+                SetPropertyValue(PROJECT_MILESTONE_DROPDOWN_ID, value);
+            }
+        }
+
+        [ExtenderControlProperty]
+        [DefaultValue("true")]
+        [IDReferenceProperty(typeof(HiddenField))]
+        [ClientPropertyName("IsNoteRequired")]
+        public string IsNoteRequired
+        {
+            get
+            {
+                return GetPropertyValue("IsNoteRequired", string.Empty);
+            }
+            set
+            {
+                SetPropertyValue("IsNoteRequired", value);
+            }
+        }
+
+        [ExtenderControlProperty]
+        [DefaultValue("false")]
+        [IDReferenceProperty(typeof(HiddenField))]
+        [ClientPropertyName("IsPTOTimeType")]
+        public string IsPTOTimeType
+        {
+            get
+            {
+                return GetPropertyValue("IsPTOTimeType", string.Empty);
+            }
+            set
+            {
+                SetPropertyValue("IsPTOTimeType", value);
+            }
+        }
+
+        #endregion
+    }
+}
