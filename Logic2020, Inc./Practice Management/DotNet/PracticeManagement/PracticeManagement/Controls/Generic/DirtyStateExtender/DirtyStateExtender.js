79b23a3ef4674e73f01dfcdebfb297e79e628a9d
SrinivasM <SrinivasM>
2012-01-27 11:02:00 -0500

    Comments - Defect Number: 2980; Defect RecordID: 2976; Server Address: services.logic2020.com; Server Port: 99; Database Name: Practice Management; Defect Summary: New Enhancement - Improved Time Tracking ; Defect Description: Madhan,
    Attached is the draft specification for some significant changes to the way Logic20/20 wants to track time in Practice Management. Please review the Word document and reference the attached Excel workbook to provide additional detail. Ill step through each with you and your team tomorrow morning (Pacific time).
    
    I imagine some impacts on our existing data model/schema. I will need some expert guidance from your team on the impacts of this specification and how best to architect the data model to provide the operational reporting flexibility we will need as a result of the system changes. We will want to be able to have a far more interactive reporting experience than what is presently afforded in PM to enable roll-up and drill-down capability leveraging this new time tracking design.
    
    Additionally, I am attaching some sample UI changes we would like to discuss as these represent conceptual thoughts on the direction wed like to move for our Time Entry interface. These are not to be viewed as the only approach but should be seen as a sample for discussion. I actually think that combining elements of both is the right solution but lets discuss based on your reading of the specifications.
    
    Our hope is that GGK will be able to run with this specification while Jason and I are on holiday from 12/22  1/2/2012 and return with some sample prototypes if not working code in a test environment by that first week of January. I can make myself available for a call during my holiday if the team gets stuck or has urgent questions that must be answered to avoid getting stuck.
    
    Best,
    Matt
    
    Matthew Reilly, Senior Manager  Consulting Operations// Logic20/20 // MattR@logic2020.com
    t.206-576-0491//c.425-503-5825// www.logic2020.com

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/Controls/Generic/DirtyStateExtender/DirtyStateExtender.js
@@ -0,0 +1,318 @@
+﻿Type.registerNamespace("PraticeManagement.Controls.Generic.DirtyStateExtender");
+
+// Array to store all single time entries on the page
+var allSingleTEs = new Array();
+
+PraticeManagement.Controls.Generic.DirtyStateExtender.DirtyStateBehavior = function (element) {
+    PraticeManagement.Controls.Generic.DirtyStateExtender.DirtyStateBehavior.initializeBase(this, [element]);
+
+    this._NoteIdValue = null;
+    this._HiddenNoteIdValue = null;
+    this._ActualHoursIdValue = null;
+    this._HiddenActualHoursIdValue = null;
+    this._HorizontalTotalCalculatorExtenderIdValue = null;
+    this._VerticalTotalCalculatorExtenderIdValue = null;
+    this._SpreadSheetExtenderIdValue = null;
+    this._TimeTypeDropdownIdValue = null;
+    this._ProjectMilestoneDropdownIdValue = null;
+    this._IsNoteRequired = null;
+    this._IsPTOTimeType = null;
+
+    // Add particular STE to the array if it's not there yet
+    if (!Array.contains(allSingleTEs, this))
+        Array.add(allSingleTEs, this);
+}
+
+PraticeManagement.Controls.Generic.DirtyStateExtender.DirtyStateBehavior.prototype = {
+    _initBase: function () {
+        PraticeManagement.Controls.Generic.DirtyStateExtender.DirtyStateBehavior.callBaseMethod(this, 'initialize');
+    },
+    initialize: function () {
+        this._initBase();
+
+        //  On clicking the label, call the corresponding function
+        $addHandler($get(this.get_ActualHoursIdValue()), 'change', Function.createDelegate(this, this._setDirty));
+    },
+    dispose: function () {
+        //Add custom dispose actions here
+        PraticeManagement.Controls.Generic.DirtyStateExtender.DirtyStateBehavior.callBaseMethod(this, 'dispose');
+    },
+    _note: function () {
+        return new String($get(this.get_NoteIdValue()).value);
+    },
+    _hours: function () {
+        return new String($get(this.get_ActualHoursIdValue()).value);
+    },
+    _isDirty: function () {
+        if (this.get_element())
+            return this.get_element().value != "";
+
+        return false;
+    },
+    _setDirty: function () {
+        this._upDateTotals();
+
+        this._setBackground('gold');
+
+        var note = new String($get(this.get_NoteIdValue()).value);
+        var hours = new String($get(this.get_ActualHoursIdValue()).value);
+
+        var stuff = new String(note + hours);
+
+        if (stuff.trim().length == 0)
+            this.get_element().value = "";
+        else
+            this.get_element().value = "dirty";
+    },
+    _upDateTotals: function () {
+
+        var spreadSheetExtenderId = $get(this.get_SpreadSheetExtenderIdValue()).value;
+        if (spreadSheetExtenderId != '') {
+
+            var spreadSheetExtenderIdArray = spreadSheetExtenderId.split(';');
+            for (var i = 0; i < spreadSheetExtenderIdArray.length; i++) {
+                var clientId = spreadSheetExtenderIdArray[i];
+                if (clientId != '')
+                    $find(clientId).update();
+            }
+
+        }
+
+    },
+    _checkDirty: function () {
+        var note = new String($get(this.get_NoteIdValue()).value);
+        var hours = new String($get(this.get_ActualHoursIdValue()).value);
+
+        var stuff = new String(note + hours);
+
+        if (stuff.trim().length == 0)
+            this.get_element().value = "";
+        else if (note == $get(this.get_HiddenNoteIdValue()).value) {
+
+            this.get_element().value = "";
+        }
+        else {
+            this._setBackground('gold');
+            this.get_element().value = "dirty";
+        }
+    },
+    checkDirty: function () {
+        this._checkDirty();
+    },
+
+    get_NoteIdValue: function () {
+        return this._NoteIdValue;
+    },
+
+    set_NoteIdValue: function (value) {
+        this._NoteIdValue = value;
+    },
+
+    get_HiddenNoteIdValue: function () {
+        return this._HiddenNoteIdValue;
+    },
+
+    set_HiddenNoteIdValue: function (value) {
+        this._HiddenNoteIdValue = value;
+    },
+
+    get_ActualHoursIdValue: function () {
+        return this._ActualHoursIdValue;
+    },
+
+    set_ActualHoursIdValue: function (value) {
+        this._ActualHoursIdValue = value;
+    },
+
+    get_HiddenActualHoursIdValue: function () {
+        return this._HiddenActualHoursIdValue;
+    },
+
+    set_HiddenActualHoursIdValue: function (value) {
+        this._HiddenActualHoursIdValue = value;
+    },
+
+    get_HorizontalTotalCalculatorExtenderIdValue: function () {
+        return this._HorizontalTotalCalculatorExtenderIdValue;
+    },
+
+    set_HorizontalTotalCalculatorExtenderIdValue: function (value) {
+        this._HorizontalTotalCalculatorExtenderIdValue = value;
+    },
+
+    get_VerticalTotalCalculatorExtenderIdValue: function () {
+        return this._VerticalTotalCalculatorExtenderIdValue;
+    },
+
+    set_VerticalTotalCalculatorExtenderIdValue: function (value) {
+        this._VerticalTotalCalculatorExtenderIdValue = value;
+    },
+
+    get_SpreadSheetExtenderIdValue: function () {
+        return this._SpreadSheetExtenderIdValue;
+    },
+
+    set_SpreadSheetExtenderIdValue: function (value) {
+        this._SpreadSheetExtenderIdValue = value;
+    },
+
+    get_NoteIdValue: function () {
+        return this._NoteIdValue;
+    },
+
+    set_NoteIdValue: function (value) {
+        this._NoteIdValue = value;
+    },
+
+    get_TimeTypeDropdownIdValue: function () {
+        return this._TimeTypeDropdownIdValue;
+    },
+    set_TimeTypeDropdownIdValue: function (value) {
+        this._TimeTypeDropdownIdValue = value;
+    },
+    get_TimeTypeDropdown: function () {
+        return $get(this._TimeTypeDropdownIdValue);
+    },
+    get_ProjectMilestoneDropdownIdValue: function () {
+        return this._ProjectMilestoneDropdownIdValue;
+    },
+
+    set_ProjectMilestoneDropdownIdValue: function (value) {
+        this._ProjectMilestoneDropdownIdValue = value;
+    },
+    get_ProjectMilestoneDropdown: function () {
+        return $get(this._ProjectMilestoneDropdownIdValue);
+    },
+    set_IsNoteRequired: function (value) {
+        this._IsNoteRequired = value;
+    },
+    get_IsNoteRequired: function () {
+        return this._IsNoteRequired;
+    },
+    set_IsPTOTimeType: function (value) {
+        this._IsPTOTimeType = value;
+    },
+    get_IsPTOTimeType: function () {
+        return this._IsPTOTimeType;
+    },
+
+
+    isValidNote: function () {
+        if (!this._isDirty())
+            return true;
+        var note = this._note();
+        if (note.trim().length == 0) {
+            if (this._hours().trim().length == 0) {
+                return true;
+            }
+            else {
+                if ($get(this._IsPTOTimeType).value == "true") {
+                    return true;
+                }
+                else if ($get(this._IsNoteRequired).value == "true") {
+                    return false;
+                }
+                else {
+                    return true;
+                }
+            }
+        }
+
+        return note.length >= 3 && note.length <= 1000;
+    },
+
+    isValidHours: function () {
+        if (!this._isDirty())
+            return true;
+        var hours = this._hours();
+
+        if (hours.trim().length == 0) {
+            if (this._note().trim().length == 0)
+                return true;
+            else
+                return false;
+        }
+
+        var parsedHrs = Number.parseInvariant(hours);
+        return parsedHrs >= 0.0 && parsedHrs <= 24.00;
+    },
+
+    showErrorMessageColor: function () {
+        this._setBackground('red');
+    },
+
+    clearData: function () {
+        this._clearData();
+    },
+
+    clearNotes: function () {
+        this._clearNotes();
+    },
+    makeDirty: function () {
+        this._makeDirty();
+    },
+    cancelBackground: function () {
+        this._setBackground('none');
+    },
+
+    _setBackground: function (bgcolor) {
+        var actualHours = $get(this.get_ActualHoursIdValue());
+        if (!actualHours.disabled)
+            actualHours.style.background = bgcolor;
+    },
+
+    _clearData: function () {
+        var ActualHours = $get(this.get_ActualHoursIdValue());
+        if (!$get(this.get_NoteIdValue()).disabled && !ActualHours.disabled) {
+
+            ActualHours.value = '';
+            $get(this.get_NoteIdValue()).value = '';
+
+            var horizontalExtenderId = $get(this.get_HorizontalTotalCalculatorExtenderIdValue()).value;
+            var verticalExtenderId = $get(this.get_VerticalTotalCalculatorExtenderIdValue()).value;
+
+
+            if (horizontalExtenderId != '') {
+                $find(horizontalExtenderId).update();
+            }
+
+            if (verticalExtenderId != '') {
+                $find(verticalExtenderId).update();
+            }
+
+            this._upDateTotals();
+
+            if ($get(this.get_HiddenActualHoursIdValue()).value != '') {
+                this.get_element().value = "dirty";
+                setDirty();
+                EnableSaveButton(true);
+            }
+            else {
+                this.get_element().value = '';
+            }
+            this.cancelBackground();
+            this._setBackground('white');
+        }
+    },
+
+    _makeDirty: function () {
+        if ($get(this.get_HiddenActualHoursIdValue()).value == ''
+            && $get(this.get_ActualHoursIdValue()).value == ''
+            && $get(this.get_NoteIdValue()).value == '') {
+            this.get_element().value = '';
+        }
+        else {
+            this.get_element().value = "dirty";
+            setDirty();
+            EnableSaveButton(true);
+        }
+    },
+    _clearNotes: function () {
+        if (!$get(this.get_NoteIdValue()).disabled) {
+            $get(this.get_NoteIdValue()).value = $get(this.get_HiddenNoteIdValue()).value;
+            if ($get(this.get_ActualHoursIdValue()).value == '' && $get(this.get_HiddenNoteIdValue()).value == '') this.cancelBackground();
+        }
+    }
+}
+PraticeManagement.Controls.Generic.DirtyStateExtender.DirtyStateBehavior.registerClass('PraticeManagement.Controls.Generic.DirtyStateExtender.DirtyStateBehaviour', AjaxControlToolkit.BehaviorBase);
+
