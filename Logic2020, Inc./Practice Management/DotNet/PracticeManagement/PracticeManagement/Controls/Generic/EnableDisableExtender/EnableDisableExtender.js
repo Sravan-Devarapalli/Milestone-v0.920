ec199aca89afb7f0750f049c704f2aa7c3bad137
SainathC <SainathC>
2012-02-02 07:55:00 -0500

    Comments - Defect Number: 2980; Defect RecordID: 2976; Server Address: services.logic2020.com; Server Port: 99; Database Name: Practice Management; Defect Summary: New Enhancement - Improved Time Tracking ; Defect Description: Madhan,
    Attached is the draft specification for some significant changes to the way Logic20/20 wants to track time in Practice Management. Please review the Word document and reference the attached Excel workbook to provide additional detail. Ill step through each with you and your team tomorrow morning (Pacific time).
    
    I imagine some impacts on our existing data model/schema. I will need some expert guidance from your team on the impacts of this specification and how best to architect the data model to provide the operational reporting flexibility we will need as a result of the system changes. We will want to be able to have a far more interactive reporting experience than what is presently afforded in PM to enable roll-up and drill-down capability leveraging this new time tracking design.
    
    Additionally, I am attaching some sample UI changes we would like to discuss as these represent conceptual thoughts on the direction wed like to move for our Time Entry interface. These are not to be viewed as the only approach but should be seen as a sample for discussion. I actually think that combining elements of both is the right solution but lets discuss based on your reading of the specifications.
    
    Our hope is that GGK will be able to run with this specification while Jason and I are on holiday from 12/22  1/2/2012 and return with some sample prototypes if not working code in a test environment by that first week of January. I can make myself available for a call during my holiday if the team gets stuck or has urgent questions that must be answered to avoid getting stuck.
    
    Best,
    Matt
    
    Matthew Reilly, Senior Manager  Consulting Operations// Logic20/20 // MattR@logic2020.com
    t.206-576-0491//c.425-503-5825// www.logic2020.com

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/Controls/Generic/EnableDisableExtender/EnableDisableExtender.js
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/Controls/Generic/EnableDisableExtender/EnableDisableExtender.js
@@ -1,5 +1,5 @@
 ﻿Type.registerNamespace("PraticeManagement.Controls.Generic.EnableDisableExtender");
-
+var obj = null;
 PraticeManagement.Controls.Generic.EnableDisableExtender.EnableDisableExtenderBehavior = function (element) {
     PraticeManagement.Controls.Generic.EnableDisableExtender.EnableDisableExtenderBehavior.initializeBase(this, [element]);
 }
@@ -13,60 +13,226 @@ PraticeManagement.Controls.Generic.EnableDisableExtender.EnableDisableExtenderBe
                          'change': this._onChange
                      },
                      this);
-        this._onChange();
 
+        this._initializeEnableDisableTextBoxes();
     },
 
     dispose: function () {
         // Cleanup code 
         PraticeManagement.Controls.Generic.EnableDisableExtender.EnableDisableExtenderBehavior.callBaseMethod(this, 'dispose');
     },
-     getControlIdList: function () {
+    getControlIdList: function () {
         var contolsToCheckList = this.controlsToCheck;
         if (contolsToCheckList)
             return contolsToCheckList.split(';');
         else
             return [];
     },
+    showPopUp: function () {
+        $find(this.popUpBehaviourId).show();
+    },
 
-    setEnableDisableTextBox: function (disable) {
-        var controlIds = this.getControlIdList();
-        for (i = 0; i < controlIds.length; i++) {
-            var control = document.getElementById(controlIds[i]);
-            var Isdisable = 0;
-            if (control != null && control.getAttribute('disable') != undefined && control.getAttribute('disable') != null) {
-                Isdisable = control.getAttribute('disable');
+    _initializeEnableDisableTextBoxes: function () {
+        var target = this.get_element();
+        if (target != null) {
+            if (target.value == -1) {
+                this._disableTextBoxes();
             }
-            if (control != null) {
-                if (disable == 1) {
-                    control.setAttribute('disabled', 'disabled');
-                    if (control.value == '')
-                        control.style.backgroundColor = 'gray';
-                }
-                else {
-                    if (Isdisable == 0) {
-                        control.removeAttribute('disabled');
-                        control.style.backgroundColor = 'white';
-                    } else {
-                        control.setAttribute('disabled', 'disabled');
-                        if (control.value == '')
-                            control.style.backgroundColor = 'gray';
+            else {
+                var controlIds = this.getControlIdList();
+                for (var i = 0; i < controlIds.length; i++) {
+                    var control = document.getElementById(controlIds[i]);
+                    if (control != null) {
+                        var isHourlyRevenueDisable = 0;
+                        var isChargeCodeTurnOffDisable = 0;
+                        var isTerminationDateDisable = 0;
+                        var isHireDateDisable = 0;
+                        if (control != null) {
+                            if (control.getAttribute('isHourlyRevenueDisable') != undefined && control.getAttribute('isHourlyRevenueDisable') != null) {
+                                isHourlyRevenueDisable = control.getAttribute('isHourlyRevenueDisable');
+                            }
+                            if (control.getAttribute('isChargeCodeTurnOffDisable') != undefined && control.getAttribute('isChargeCodeTurnOffDisable') != null) {
+                                isChargeCodeTurnOffDisable = control.getAttribute('isChargeCodeTurnOffDisable');
+                            }
+                            if (control.getAttribute('IsHireDateDisable') != undefined && control.getAttribute('IsHireDateDisable') != null) {
+                                isHireDateDisable = control.getAttribute('IsHireDateDisable');
+                            }
+                            if (control.getAttribute('IsTerminationDateDisable') != undefined && control.getAttribute('IsTerminationDateDisable') != null) {
+                                isTerminationDateDisable = control.getAttribute('IsTerminationDateDisable');
+                            }
+                        }
+                        if (isHourlyRevenueDisable == 0 && isChargeCodeTurnOffDisable == 0 && isHireDateDisable == 0 && isTerminationDateDisable == 0) {
+                            if (control.getAttribute('disabled') == 'disabled') {
+                                control.removeAttribute('disabled');
+                                control.style.backgroundColor = 'white';
+                            }
+                        } else {
+                            control.setAttribute('disabled', 'disabled');
+                            if (control.value == '')
+                                control.style.backgroundColor = 'gray';
+                        }
                     }
-
                 }
             }
         }
     },
+    _checkEnableDisableTextBoxes: function (isChargeCodeTurnOffJsonString) {
+        var isChargeCodeTurnOffJson = jQuery.parseJSON(isChargeCodeTurnOffJsonString);
+        var showPopup = false;
+        var controlIds = obj.getControlIdList();
+        var isNonBillable = true;
+        if (controlIds.length >= 14) {
+            isNonBillable = false;
+        }
+        var j = 0;
+        for (var i = 0; i < 7; i++) {
+            var isChargeCodeTurnOffJsonObject = isChargeCodeTurnOffJson[i];
+            var isNewchargeCodeTurnOff = isChargeCodeTurnOffJsonObject.IschargeCodeTurnOff == "true";
+            if (isNonBillable) {
+                var nonBillableControl = document.getElementById(controlIds[i]);
+                var nonBillableControlHasValue = nonBillableControl.value != '';
+                var nonBillableControlIsDirty = nonBillableControl.style.backgroundColor != 'white' && nonBillableControl.style.backgroundColor != 'gray';
+                if (isNewchargeCodeTurnOff && (nonBillableControlIsDirty || nonBillableControlHasValue)) {
+                    showPopup = true;
+                }
+            }
+            else {
+                var billableControl = document.getElementById(controlIds[j]);
+                var nonBillableControl = document.getElementById(controlIds[j + 1]);
+                var billableControlHasValue = billableControl.value != '';
+                var billableControlIsDirty = billableControl.style.backgroundColor != 'white' && billableControl.style.backgroundColor != 'gray';
+                var nonBillableControlHasValue = nonBillableControl.value != '';
+                var nonBillableControlIsDirty = nonBillableControl.style.backgroundColor != 'white' && nonBillableControl.style.backgroundColor != 'gray';
+                if (isNewchargeCodeTurnOff && (billableControlHasValue || billableControlHasValue || nonBillableControlIsDirty || nonBillableControlHasValue)) {
+                    showPopup = true;
+                }
+                j = j + 2;
+            }
+        }
+        if (!showPopup) {
+            obj._enableTextBoxes(isChargeCodeTurnOffJson);
+        } else {
+            var target = obj.get_element();
+            var previousId = target.getAttribute('previousId');
+            target.value = previousId;
+            obj.showPopUp();
+        }
+    },
+    _enableTextBoxes: function (isChargeCodeTurnOffJson) {
+        var controlIds = this.getControlIdList();
+        var isNonBillable = true;
+        if (controlIds.length >= 14) {
+            isNonBillable = false;
+        }
+        var j = 0;
+        for (var i = 0; i < 7; i++) {
+            var isChargeCodeTurnOffJsonObject = isChargeCodeTurnOffJson[i];
+            var ischargeCodeTurnOff = isChargeCodeTurnOffJsonObject.IschargeCodeTurnOff == "true";
+            if (isNonBillable) {
+                var nonBillableControl = document.getElementById(controlIds[i]);
+                if (ischargeCodeTurnOff) {
+                    nonBillableControl.setAttribute('isChargeCodeTurnOffDisable', 1);
+                }
+                else {
+                    nonBillableControl.setAttribute('isChargeCodeTurnOffDisable', 0);
+                }
+            }
+            else {
+                var billableControl = document.getElementById(controlIds[j]);
+                var nonBillableControl = document.getElementById(controlIds[j + 1]);
+                j = j + 2;
+                if (ischargeCodeTurnOff) {
+                    billableControl.setAttribute('isChargeCodeTurnOffDisable', 1);
+                    nonBillableControl.setAttribute('isChargeCodeTurnOffDisable', 1);
+                }
+                else {
+                    billableControl.setAttribute('isChargeCodeTurnOffDisable', 0);
+                    nonBillableControl.setAttribute('isChargeCodeTurnOffDisable', 0);
+                }
+
+            }
+        }
+
+        for (var i = 0; i < controlIds.length; i++) {
+            var control = document.getElementById(controlIds[i]);
+
+            if (control != null) {
+                var isHourlyRevenueDisable = 0;
+                var isChargeCodeTurnOffDisable = 0;
+                var isTerminationDateDisable = 0;
+                var isHireDateDisable = 0;
+                if (control != null) {
+                    if (control.getAttribute('isHourlyRevenueDisable') != undefined && control.getAttribute('isHourlyRevenueDisable') != null) {
+                        isHourlyRevenueDisable = control.getAttribute('isHourlyRevenueDisable');
+                    }
+                    if (control.getAttribute('isChargeCodeTurnOffDisable') != undefined && control.getAttribute('isChargeCodeTurnOffDisable') != null) {
+                        isChargeCodeTurnOffDisable = control.getAttribute('isChargeCodeTurnOffDisable');
+                    }
+                    if (control.getAttribute('IsHireDateDisable') != undefined && control.getAttribute('IsHireDateDisable') != null) {
+                        isHireDateDisable = control.getAttribute('IsHireDateDisable');
+                    }
+                    if (control.getAttribute('IsTerminationDateDisable') != undefined && control.getAttribute('IsTerminationDateDisable') != null) {
+                        isTerminationDateDisable = control.getAttribute('IsTerminationDateDisable');
+                    }
+                }
+                if (isHourlyRevenueDisable == 0 && isChargeCodeTurnOffDisable == 0 && isHireDateDisable == 0 && isTerminationDateDisable == 0) {
+                    control.removeAttribute('disabled');
+                    var controlIsDirty = control.style.backgroundColor != 'white' && control.style.backgroundColor != 'gray';
+                    if (!controlIsDirty) {
+                        control.style.backgroundColor = 'white';
+                    }
+                } else {
+                    control.setAttribute('disabled', 'disabled');
+                    if (control.value == '')
+                        control.style.backgroundColor = 'gray';
+                }
+            }
+        }
+        var target = obj.get_element();
+        target.setAttribute('previousId', target.value);
+    },
+    _disableTextBoxes: function () {
+        var controlIds = this.getControlIdList();
+        for (i = 0; i < controlIds.length; i++) {
+            var control = document.getElementById(controlIds[i]);
+
+            if (control) {
+                control.setAttribute('disabled', 'disabled');
+                var controlIsDirty = control.style.backgroundColor != 'white' && control.style.backgroundColor != 'gray';
+                if (!controlIsDirty) {
+                    control.style.backgroundColor = 'gray';
+                }
+            }
+        }
+        var target = this.get_element();
+        target.setAttribute('previousId', target.value);
+    },
+    _getChargeCodeTurnOffDates: function (timeEntryId) {
+
+        var urlVal = "TimeEntryHandler.ashx?accountId=" + this.accountId
+                                            + "&projectId=" + this.projectId
+                                            + "&businessUnitId=" + this.businessUnitId
+                                            + "&timeEntryId=" + timeEntryId
+                                            + "&weekStartDate=" + this.weekStartDate
+                                            + "&personId=" + this.personId;
+        obj = this;
+
+        $.post(urlVal, this._checkEnableDisableTextBoxes);
+    },
     _onChange: function () {
+
         var target = this.get_element();
         if (target != null) {
             if (target.value == -1) {
-                this.setEnableDisableTextBox(1);
+                this._disableTextBoxes();
             } else {
-                this.setEnableDisableTextBox(0);
+                this._getChargeCodeTurnOffDates(target.value);
             }
+
         }
+
     }
 }
 
 PraticeManagement.Controls.Generic.EnableDisableExtender.EnableDisableExtenderBehavior.registerClass('PraticeManagement.Controls.Generic.EnableDisableExtender.EnableDisableExtenderBehavior', AjaxControlToolkit.BehaviorBase);
+
