2ba967251b7283da66acc59b5243e759201b17a6
SainathC <SainathC>
2012-02-02 07:55:00 -0500

    Comments - Defect Number: 2980; Defect RecordID: 2976; Server Address: services.logic2020.com; Server Port: 99; Database Name: Practice Management; Defect Summary: New Enhancement - Improved Time Tracking ; Defect Description: Madhan,
    Attached is the draft specification for some significant changes to the way Logic20/20 wants to track time in Practice Management. Please review the Word document and reference the attached Excel workbook to provide additional detail. Ill step through each with you and your team tomorrow morning (Pacific time).
    
    I imagine some impacts on our existing data model/schema. I will need some expert guidance from your team on the impacts of this specification and how best to architect the data model to provide the operational reporting flexibility we will need as a result of the system changes. We will want to be able to have a far more interactive reporting experience than what is presently afforded in PM to enable roll-up and drill-down capability leveraging this new time tracking design.
    
    Additionally, I am attaching some sample UI changes we would like to discuss as these represent conceptual thoughts on the direction wed like to move for our Time Entry interface. These are not to be viewed as the only approach but should be seen as a sample for discussion. I actually think that combining elements of both is the right solution but lets discuss based on your reading of the specifications.
    
    Our hope is that GGK will be able to run with this specification while Jason and I are on holiday from 12/22  1/2/2012 and return with some sample prototypes if not working code in a test environment by that first week of January. I can make myself available for a call during my holiday if the team gets stuck or has urgent questions that must be answered to avoid getting stuck.
    
    Best,
    Matt
    
    Matthew Reilly, Senior Manager  Consulting Operations// Logic20/20 // MattR@logic2020.com
    t.206-576-0491//c.425-503-5825// www.logic2020.com

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/TimeTypeInsert.proc.sql
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/TimeTypeInsert.proc.sql
@@ -12,71 +12,97 @@ CREATE PROCEDURE [dbo].[TimeTypeInsert]
 AS
 BEGIN
 	SET NOCOUNT ON;
+	BEGIN TRY
+			BEGIN TRANSACTION TimeTypeInsert
+			
+			DECLARE @Error NVARCHAR(200)
 
-	IF EXISTS(SELECT 1 FROM dbo.TimeType WHERE [Name] = @Name)
-	BEGIN
-		DECLARE @Error NVARCHAR(200)
-		SET @Error = 'This work type already exists. Please add a different work type.'
-		RAISERROR(@Error,16,1)
-		RETURN
-	END
-	/*
-	@IsDefault = 0 for Custom WorkType  W0000 - W5999.
-				= 1 for Default WorkType. W6000 - W6999.
-	 */
-	 DECLARE	 @TimeTypeCode			 NVARCHAR(10),
-				 @LowerLimitRange		 INT ,
-				 @HigherLimitRange		 INT ,
-				 @NextTimeTypeNumber	 INT
+			IF EXISTS(SELECT 1 FROM dbo.TimeType WHERE [Name] = @Name)
+			BEGIN
+				
+				SET @Error = 'This work type already exists. Please add a different work type.'
+				RAISERROR(@Error,16,1)
+				RETURN
+			END
+			/*
+			@IsDefault = 0 for Custom WorkType  W0000 - W5999.
+						= 1 for Default WorkType. W6000 - W6999.
+			 */
+			 DECLARE	 @TimeTypeCode			 NVARCHAR(10),
+						 @LowerLimitRange		 INT ,
+						 @HigherLimitRange		 INT ,
+						 @NextTimeTypeNumber	 INT
 		
-	IF (@IsDefault = 1)
-	BEGIN
-		SET @LowerLimitRange = 6000
-		SET @HigherLimitRange = 6999
-		SET @Error = 'Default worktype code not avaliable'
-	END
-	ELSE
-	BEGIN
-		SET @LowerLimitRange = 0
-		SET @HigherLimitRange = 5999
-		SET @Error = 'Custom worktype code not avaliable'
-	END
+			IF (@IsDefault = 1)
+			BEGIN
+				SET @LowerLimitRange = 6000
+				SET @HigherLimitRange = 6999
+				SET @Error = 'Default worktype code not avaliable'
+			END
+			ELSE
+			BEGIN
+				SET @LowerLimitRange = 0
+				SET @HigherLimitRange = 5999
+				SET @Error = 'Custom worktype code not avaliable'
+			END
 
-	DECLARE @TimeTypeRanksList TABLE (TimeTypeNumber INT,TimeTypeNumberRank INT)
-	INSERT INTO @TimeTypeRanksList 
-	SELECT Convert(INT,SUBSTRING(Code,2,5)) as TimeTypeNumber ,
-			RANK() OVER (ORDER BY Convert(INT,SUBSTRING(Code,2,5)))+@LowerLimitRange-1 AS  TimeTypeNumberRank
-	FROM dbo.TimeType 
-	WHERE IsDefault = @IsDefault AND ISNUMERIC( SUBSTRING(Code,2,5)) = 1
+			DECLARE @TimeTypeRanksList TABLE (TimeTypeNumber INT,TimeTypeNumberRank INT)
+			INSERT INTO @TimeTypeRanksList 
+			SELECT Convert(INT,SUBSTRING(Code,2,5)) as TimeTypeNumber ,
+					RANK() OVER (ORDER BY Convert(INT,SUBSTRING(Code,2,5)))+@LowerLimitRange-1 AS  TimeTypeNumberRank
+			FROM dbo.TimeType 
+			WHERE IsDefault = @IsDefault AND ISNUMERIC( SUBSTRING(Code,2,5)) = 1
 
 
-	INSERT INTO @TimeTypeRanksList 
-	SELECT -1,MAX(TimeTypeNumberRank)+1 FROM @TimeTypeRanksList
+			INSERT INTO @TimeTypeRanksList 
+			SELECT -1,MAX(TimeTypeNumberRank)+1 FROM @TimeTypeRanksList
 
-	SELECT TOP 1 @NextTimeTypeNumber = TimeTypeNumberRank 
-		FROM @TimeTypeRanksList  
-		WHERE TimeTypeNumber != TimeTypeNumberRank 
-		ORDER BY TimeTypeNumberRank
+			SELECT TOP 1 @NextTimeTypeNumber = TimeTypeNumberRank 
+				FROM @TimeTypeRanksList  
+				WHERE TimeTypeNumber != TimeTypeNumberRank 
+				ORDER BY TimeTypeNumberRank
 
 				
-	IF (@NextTimeTypeNumber IS NULL AND NOT EXISTS(SELECT 1 FROM @TimeTypeRanksList WHERE TimeTypeNumber != -1))
-	BEGIN 
-		SET  @NextTimeTypeNumber = @LowerLimitRange
-	END 
-	ELSE IF (@NextTimeTypeNumber > @HigherLimitRange )
-	BEGIN
-		RAISERROR (@Error, 16, 1)
-		RETURN
-	END
+			IF (@NextTimeTypeNumber IS NULL AND NOT EXISTS(SELECT 1 FROM @TimeTypeRanksList WHERE TimeTypeNumber != -1))
+			BEGIN 
+				SET  @NextTimeTypeNumber = @LowerLimitRange
+			END 
+			ELSE IF (@NextTimeTypeNumber > @HigherLimitRange )
+			BEGIN
+				RAISERROR (@Error, 16, 1)
+				RETURN
+			END
 
-	SET @TimeTypeCode = 'W'+ REPLICATE('0',4-LEN(@NextTimeTypeNumber)) + CONVERT(NVARCHAR,@NextTimeTypeNumber)
+			SET @TimeTypeCode = 'W'+ REPLICATE('0',4-LEN(@NextTimeTypeNumber)) + CONVERT(NVARCHAR,@NextTimeTypeNumber)
 
-	INSERT INTO dbo.TimeType ([Name], [IsDefault], [IsInternal], [IsAllowedToEdit],Code,[IsActive]) VALUES (@Name, @IsDefault, @IsInternal, 1,@TimeTypeCode,@IsActive)
+			INSERT INTO dbo.TimeType ([Name], [IsDefault], [IsInternal], [IsAllowedToEdit],Code,[IsActive]) VALUES (@Name, @IsDefault, @IsInternal, 1,@TimeTypeCode,@IsActive)
 
-	SET @TimeTypeId = SCOPE_IDENTITY()
+			SET @TimeTypeId = SCOPE_IDENTITY()
+
+			--if default timetype is inserted need to unassign it for the pto project and holiday project
+			IF (@IsDefault = 1)
+			BEGIN
+				DECLARE @PtoProjectId INT 
+				SELECT @PtoProjectId = [ProjectId] FROM [dbo].[Project] where ProjectNumber = 'P999905'
+				DECLARE @HolidayProjectId INT 
+				SELECT @HolidayProjectId = [ProjectId] FROM [dbo].[Project] where ProjectNumber = 'P999906'
+
+
+				INSERT INTO dbo.ProjectTimeType(ProjectId,TimeTypeId,IsAllowedToShow)
+				VALUES(@PtoProjectId,@TimeTypeId,0),
+					  (@HolidayProjectId,@TimeTypeId,0)
+		
+			END
+			COMMIT TRANSACTION TimeTypeInsert
+	END TRY
+	BEGIN CATCH
+		ROLLBACK TRANSACTION TimeTypeInsert
+
+		SET @Error = ERROR_MESSAGE()
+
+		RAISERROR(@Error, 16, 1)
+
+	END CATCH
+
 END
-
-GO
-
-
 
