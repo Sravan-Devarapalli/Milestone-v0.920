e9a62dfa2936fc722134cbe2c2e8d12a971ad1dc
SrinivasM <SrinivasM>
2012-02-07 08:55:00 -0500

    Comments - Added IsInternal column in ProjectGroup table.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/ProjectGroup.table.sql
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/ProjectGroup.table.sql
@@ -3,7 +3,7 @@
     [ClientId]			INT				NOT NULL,
 	[Code]				NVARCHAR(5)		NOT NULL,
     [Name]				NVARCHAR (100)	NOT NULL,
-	Active				BIT				NOT NULL CONSTRAINT DF_ProjectGroup_Active DEFAULT  1,
+	[Active]			BIT				NOT NULL CONSTRAINT DF_ProjectGroup_Active DEFAULT  1,
+	[IsInternal]		BIT				NOT NULL CONSTRAINT DF_ProjectGroup_IsInternal DEFAULT 0,
     PRIMARY KEY CLUSTERED ([GroupId] ASC) WITH (IGNORE_DUP_KEY = OFF, STATISTICS_NORECOMPUTE = OFF)
 );
-
