a85984e5fced6a1d1ea9b5d77c25e8abf1c0bf7c
ThulasiramP <ThulasiramP>
2012-02-14 01:58:00 -0500

    Comments - Defect Number: 2980; Defect RecordID: 2976; Server Address: services.logic2020.com; Server Port: 99; Database Name: Practice Management; Defect Summary: New Enhancement - Improved Time Tracking ; Defect Description: Madhan,
    Attached is the draft specification for some significant changes to the way Logic20/20 wants to track time in Practice Management. Please review the Word document and reference the attached Excel workbook to provide additional detail. Ill step through each with you and your team tomorrow morning (Pacific time).
    
    I imagine some impacts on our existing data model/schema. I will need some expert guidance from your team on the impacts of this specification and how best to architect the data model to provide the operational reporting flexibility we will need as a result of the system changes. We will want to be able to have a far more interactive reporting experience than what is presently afforded in PM to enable roll-up and drill-down capability leveraging this new time tracking design.
    
    Additionally, I am attaching some sample UI changes we would like to discuss as these represent conceptual thoughts on the direction wed like to move for our Time Entry interface. These are not to be viewed as the only approach but should be seen as a sample for discussion. I actually think that combining elements of both is the right solution but lets discuss based on your reading of the specifications.
    
    Our hope is that GGK will be able to run with this specification while Jason and I are on holiday from 12/22  1/2/2012 and return with some sample prototypes if not working code in a test environment by that first week of January. I can make myself available for a call during my holiday if the team gets stuck or has urgent questions that must be answered to avoid getting stuck.
    
    Best,
    Matt
    
    Matthew Reilly, Senior Manager  Consulting Operations// Logic20/20 // MattR@logic2020.com
    t.206-576-0491//c.425-503-5825// www.logic2020.com

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/ClientDAL/TimeTypeDAL.cs
@@ -0,0 +1,243 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Linq;
+using DataAccess.Other;
+using DataTransferObjects;
+using DataTransferObjects.CompositeObjects;
+using DataTransferObjects.ContextObjects;
+using DataTransferObjects.TimeEntry;
+
+namespace DataAccess
+{
+    public static class TimeTypeDAL
+    {
+
+        /// <summary>
+        /// Retrieves all existing time types
+        /// </summary>
+        /// <returns>Collection of new time types</returns>
+        public static IEnumerable<TimeTypeRecord> GetAllTimeTypes()
+        {
+            using (var connection = new SqlConnection(DataSourceHelper.DataConnection))
+            using (var command = new SqlCommand(Constants.ProcedureNames.TimeType.GetAll, connection))
+            {
+                command.CommandType = CommandType.StoredProcedure;
+
+                connection.Open();
+
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    return new List<TimeTypeRecord>(ReadTimeTypes(reader));
+                }
+            }
+        }
+
+        public static List<TimeTypeRecord> GetAllAdministrativeTimeTypes()
+        {
+            using (var connection = new SqlConnection(DataSourceHelper.DataConnection))
+            using (var command = new SqlCommand(Constants.ProcedureNames.TimeType.GetAllAdministrativeTimeTypes, connection))
+            {
+                command.CommandType = CommandType.StoredProcedure;
+
+                connection.Open();
+
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    var result = new List<TimeTypeRecord>();
+                    ReadTimeTypesShort(reader, result);
+                    return result;
+                }
+            }
+        }
+
+
+        public static Triple<int, int, int> GetAdministrativeChargeCodeValues(int timeTypeId)
+        {
+            using (var connection = new SqlConnection(DataSourceHelper.DataConnection))
+            using (var command = new SqlCommand(Constants.ProcedureNames.TimeType.GetAdministrativeChargeCodeValues, connection))
+            {
+                command.CommandType = CommandType.StoredProcedure;
+                command.Parameters.AddWithValue(Constants.ParameterNames.TimeTypeId, timeTypeId);
+
+                connection.Open();
+
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    int clientId = -1, projectId = -1, businessUnitId = -1;
+                   
+                    if (reader.HasRows)
+                    {
+                        int clientIdIndex = reader.GetOrdinal(Constants.ColumnNames.ClientIdColumn);
+                        int projectIdIndex = reader.GetOrdinal(Constants.ColumnNames.ProjectIdColumn);
+                        int businessUnitIdIndex = reader.GetOrdinal(Constants.ColumnNames.ProjectGroupIdColumn);
+
+                        while (reader.Read())
+                        {
+                            clientId = reader.GetInt32(clientIdIndex);
+                            projectId = reader.GetInt32(projectIdIndex);
+                            businessUnitId = reader.GetInt32(businessUnitIdIndex);
+                        }
+
+                    }
+
+                    return new Triple<int, int, int>(clientId, projectId, businessUnitId);
+                }
+            }
+        }
+
+
+        /// <summary>
+        /// Updates given time type
+        /// </summary>
+        /// <param name="timeType">Time type to update</param>
+        public static void UpdateTimeType(TimeTypeRecord timeType)
+        {
+            using (var connection = new SqlConnection(DataSourceHelper.DataConnection))
+            using (var command = new SqlCommand(Constants.ProcedureNames.TimeType.Update, connection))
+            {
+                command.CommandType = CommandType.StoredProcedure;
+                command.Parameters.AddWithValue(Constants.ParameterNames.TimeTypeId, timeType.Id);
+                command.Parameters.AddWithValue(Constants.ParameterNames.Name, timeType.Name);
+                command.Parameters.AddWithValue(Constants.ParameterNames.IsDefault, timeType.IsDefault);
+                command.Parameters.AddWithValue(Constants.ParameterNames.IsInternalParam, timeType.IsInternal);
+                command.Parameters.AddWithValue(Constants.ParameterNames.IsActive, timeType.IsActive);
+                connection.Open();
+
+                command.ExecuteNonQuery();
+            }
+        }
+
+        /// <summary>
+        /// Adds new time type
+        /// </summary>
+        /// <param name="timeType">Time type to add</param>
+        /// <returns>Id of added time type</returns>
+        public static int AddTimeType(TimeTypeRecord timeType)
+        {
+            using (var connection = new SqlConnection(DataSourceHelper.DataConnection))
+            using (var command = new SqlCommand(Constants.ProcedureNames.TimeType.Insert, connection))
+            {
+                command.CommandType = CommandType.StoredProcedure;
+                command.Parameters.AddWithValue(Constants.ParameterNames.Name, timeType.Name);
+                command.Parameters.AddWithValue(Constants.ParameterNames.IsDefault, timeType.IsDefault);
+                command.Parameters.AddWithValue(Constants.ParameterNames.IsInternalParam, timeType.IsInternal);
+                command.Parameters.AddWithValue(Constants.ParameterNames.IsActive, timeType.IsActive);
+
+                SqlParameter timeTypeIdParam = new SqlParameter(Constants.ParameterNames.TimeTypeId, SqlDbType.Int) { Direction = ParameterDirection.Output };
+                command.Parameters.Add(timeTypeIdParam);
+
+                connection.Open();
+
+                command.ExecuteNonQuery();
+                timeType.Id = (int)timeTypeIdParam.Value;
+                return timeType.Id;
+            }
+        }
+
+        /// <summary>
+        /// Removes given time type
+        /// </summary>
+        /// <param name="timeType">Time type to remove</param>
+        public static void RemoveTimeType(TimeTypeRecord timeType)
+        {
+            using (var connection = new SqlConnection(DataSourceHelper.DataConnection))
+            using (var command = new SqlCommand(Constants.ProcedureNames.TimeType.Delete, connection))
+            {
+                command.CommandType = CommandType.StoredProcedure;
+                command.Parameters.AddWithValue(Constants.ParameterNames.TimeTypeId, timeType.Id);
+
+                connection.Open();
+
+                command.ExecuteNonQuery();
+            }
+        }
+
+        private static IEnumerable<TimeTypeRecord> ReadTimeTypes(DbDataReader reader)
+        {
+            if (reader.HasRows)
+            {
+                int timeTypeIdIndex = reader.GetOrdinal(Constants.ColumnNames.TimeTypeId);
+                int nameIndex = reader.GetOrdinal(Constants.ColumnNames.Name);
+                int inUseIndex = reader.GetOrdinal(Constants.ColumnNames.InUse);
+                int inFutureUseIndex = reader.GetOrdinal(Constants.ColumnNames.InFutureUse);
+                int isDefaultIndex = reader.GetOrdinal(Constants.ColumnNames.IsDefault);
+                int isAllowedToEditColumnIndex = reader.GetOrdinal(Constants.ColumnNames.IsAllowedToEditColumn);
+                int isActiveColumnIndex = reader.GetOrdinal(Constants.ColumnNames.IsActive);
+                int isInternalColumnIndex = reader.GetOrdinal(Constants.ColumnNames.IsInternalColumn);
+
+                while (reader.Read())
+                {
+                    var tt = new TimeTypeRecord
+                    {
+                        Id = reader.GetInt32(timeTypeIdIndex),
+                        Name = reader.GetString(nameIndex),
+                        IsDefault = reader.GetBoolean(isDefaultIndex),
+                        IsAllowedToEdit = reader.GetBoolean(isAllowedToEditColumnIndex),
+                        IsActive = reader.GetBoolean(isActiveColumnIndex),
+                        IsInternal = reader.GetBoolean(isInternalColumnIndex),
+                        InFutureUse = Convert.ToBoolean(reader.GetInt32(inFutureUseIndex)),
+                        InUse = bool.Parse(reader.GetString(inUseIndex))
+                    };
+                    yield return tt;
+
+                }
+            }
+        }
+
+
+        private static void ReadTimeTypesShort(DbDataReader reader, List<TimeTypeRecord> result)
+        {
+            if (reader.HasRows)
+            {
+                int timeTypeIdIndex = reader.GetOrdinal(Constants.ColumnNames.TimeTypeId);
+                int nameIndex = reader.GetOrdinal(Constants.ColumnNames.Name);
+
+                while (reader.Read())
+                {
+                    var tt = new TimeTypeRecord
+                    {
+                        Id = reader.GetInt32(timeTypeIdIndex),
+                        Name = reader.GetString(nameIndex),
+
+                    };
+                    result.Add(tt);
+
+                }
+            }
+
+        }
+
+        public static string GetWorkTypeNameById(int worktypeId)
+        {
+            string name = "";
+            using (var connection = new SqlConnection(DataSourceHelper.DataConnection))
+            using (var command = new SqlCommand(Constants.ProcedureNames.TimeEntry.GetWorkTypeNameByIdProcedure, connection))
+            {
+                command.CommandType = CommandType.StoredProcedure;
+                command.Parameters.AddWithValue(Constants.ParameterNames.TimeTypeId, worktypeId);
+
+                connection.Open();
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    if (reader.HasRows)
+                    {
+                        while (reader.Read())
+                        {
+                            name = reader.GetString(reader.GetOrdinal(Constants.ParameterNames.Name));
+                        }
+                    }
+
+                }
+            }
+
+            return name;
+
+        }
+
+
+    }
+}
+
