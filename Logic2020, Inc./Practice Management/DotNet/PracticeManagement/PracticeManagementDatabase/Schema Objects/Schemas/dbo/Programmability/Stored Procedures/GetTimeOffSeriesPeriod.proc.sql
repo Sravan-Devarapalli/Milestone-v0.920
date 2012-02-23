5f1e4f9c3aeaa0644a7e5789a246bb018b1103f5
SrinivasM <SrinivasM>
2012-02-23 10:07:00 -0500

    Comments - Defect Number: 2983; Defect RecordID: 2979; Server Address: services.logic2020.com; Server Port: 99; Database Name: Practice Management; Defect Summary: Time->Calendar:New Time off/ PTO/Calendar functionality; Defect Description: From: Jason Hong-Turney [mailto:jasonht@logic2020.com] Sent: Friday, February 10, 2012 12:13 PM To: MadhanA; Matt Reilly Cc: ''Logic 20/20 Core Team'' Subject: RE: Time tracking Feature Status update.
    Hi Madhan 
    
    Finally, unrelated to the Time Entry page, but related to PTO/Calendar functionality.
     See attached mockup for changes to time off entry that allow for entering and editing a range of time, as well as a change to holiday substitution (replaces Floating Holiday).
    
    Thank you!
    
    Jason Hong-Turney // Logic20/20 // jasonht@logic2020.com
    t.206-576-0472 // c. 425-390-4664

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/GetTimeOffSeriesPeriod.proc.sql
@@ -0,0 +1,41 @@
+﻿CREATE PROCEDURE [dbo].[GetTimeOffSeriesPeriod]
+(
+	@PersonId INT, 
+	@Date	DATETIME
+)
+AS
+BEGIN
+	
+	IF 0 = (SELECT PC.IsSeries
+			FROM dbo.PersonCalendar PC
+			WHERE PC.PersonId = @PersonId AND PC.Date = @Date)
+	BEGIN
+		SELECT @Date 'StartDate', @Date 'EndDate'
+	END
+	ELSE
+	BEGIN
+		
+		;WITH ConsecutiveDates AS
+		(
+			SELECT PCC.*
+			FROM PersonCalendar PCC
+			WHERE PCC.Date = @Date AND PCC.PersonId = @PersonId AND PCC.IsSeries = 1
+			UNION ALL
+			SELECT PC.*
+			FROM ConsecutiveDates CD
+			JOIN PersonCalendar PC ON ((DATEPART(DW, CD.date) = 6 AND PC.date = DATEADD(DD,3, CD.date) )
+											OR (DATEPART(DW, CD.date) = 2 AND PC.date = DATEADD(DD, -3, CD.date))
+											OR  PC.date = DATEADD(DD,1, CD.date)
+											OR  PC.date = DATEADD(DD, -1, CD.date)
+										)
+									AND PC.PersonId = CD.PersonId AND PC.IsSeries = 1 AND PC.TimeTypeId = CD.TimeTypeId
+									AND PC.ActualHours = CD.ActualHours
+		)
+
+		SELECT MIN(CD.Date) 'StartDate', MAX(CD.Date) 'EndDate'
+		FROM ConsecutiveDates CD
+		GROUP BY CD.PersonId
+
+	END
+
+END
