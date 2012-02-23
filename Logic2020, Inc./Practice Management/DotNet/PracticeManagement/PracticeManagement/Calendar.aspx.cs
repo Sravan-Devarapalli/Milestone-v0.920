83c3b579c4f222cd520808f49f4d1e6ba6246226
ThulasiramP <ThulasiramP>
2012-02-23 09:40:00 -0500

    Comments - Defect Number: 2983; Defect RecordID: 2979; Server Address: services.logic2020.com; Server Port: 99; Database Name: Practice Management; Defect Summary: Time->Calendar:New Time off/ PTO/Calendar functionality; Defect Description: From: Jason Hong-Turney [mailto:jasonht@logic2020.com] Sent: Friday, February 10, 2012 12:13 PM To: MadhanA; Matt Reilly Cc: ''Logic 20/20 Core Team'' Subject: RE: Time tracking Feature Status update.
    Hi Madhan 
    
    Finally, unrelated to the Time Entry page, but related to PTO/Calendar functionality.
     See attached mockup for changes to time off entry that allow for entering and editing a range of time, as well as a change to holiday substitution (replaces Floating Holiday).
    
    Thank you!
    
    Jason Hong-Turney // Logic20/20 // jasonht@logic2020.com
    t.206-576-0472 // c. 425-390-4664

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/Calendar.aspx.cs
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/Calendar.aspx.cs
@@ -11,28 +11,13 @@ namespace PraticeManagement
 {
     public partial class Calendar : System.Web.UI.Page
     {
-        protected void Page_Init(object sender, EventArgs e)
-        {
-            if (!IsPostBack)
-            {
-                var btnRetrieveCalendar = (Button)calendar.FindControl("btnRetrieveCalendar");
-                AsyncPostBackTrigger tr =
-                    new AsyncPostBackTrigger() { ControlID = btnRetrieveCalendar.UniqueID, EventName = "Click" };
-                pnlHeader.Triggers.Add(tr);
-            }
-        }
+       
 
         protected void Page_Load(object sender, EventArgs e)
-        { }
-
-        protected void Page_PreRender(object sender, EventArgs e)
-        {
-            var ddlPerson = (DropDownList)calendar.FindControl("ddlPerson");
-            if (ddlPerson != null)
-            {
-                lblCalendarOwnerName.Text = ddlPerson.SelectedItem.Text;
-            }
+        { 
         }
+
+        
 
     }
 }
