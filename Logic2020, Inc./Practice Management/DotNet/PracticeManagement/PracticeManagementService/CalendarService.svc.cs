da8968f5796739dff78e27625a215f303ce8c155
ThulasiramP <ThulasiramP>
2012-02-23 06:35:00 -0500

    Comments - Defect Number: 2983; Defect RecordID: 2979; Server Address: services.logic2020.com; Server Port: 99; Database Name: Practice Management; Defect Summary: Time->Calendar:New Time off/ PTO/Calendar functionality; Defect Description: From: Jason Hong-Turney [mailto:jasonht@logic2020.com] Sent: Friday, February 10, 2012 12:13 PM To: MadhanA; Matt Reilly Cc: ''Logic 20/20 Core Team'' Subject: RE: Time tracking Feature Status update.
    Hi Madhan 
    
    Finally, unrelated to the Time Entry page, but related to PTO/Calendar functionality.
     See attached mockup for changes to time off entry that allow for entering and editing a range of time, as well as a change to holiday substitution (replaces Floating Holiday).
    
    Thank you!
    
    Jason Hong-Turney // Logic20/20 // jasonht@logic2020.com
    t.206-576-0472 // c. 425-390-4664

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementService/CalendarService.svc.cs
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementService/CalendarService.svc.cs
@@ -6,37 +6,37 @@ using DataTransferObjects;
 
 namespace PracticeManagementService
 {
-	[AspNetCompatibilityRequirements(RequirementsMode = AspNetCompatibilityRequirementsMode.Allowed)]
-	public class CalendarService : ICalendarService
-	{
-		#region ICalendarService Members
+    [AspNetCompatibilityRequirements(RequirementsMode = AspNetCompatibilityRequirementsMode.Allowed)]
+    public class CalendarService : ICalendarService
+    {
+        #region ICalendarService Members
 
-		/// <summary>
-		/// Retrieves a list of the calendar items from the database.
-		/// </summary>
-		/// <param name="startDate">The start of the period.</param>
-		/// <param name="endDate">The end of the period.</param>
-		/// <param name="personId">
-		/// An ID of the person to the cxalendar be retrived for.
-		/// If null the company calendar will be returned.
-		/// </param>
-		/// <param name="practiceManagerId">
-		/// An ID of the practice manager to retrieve the data for his subordinate
-		/// </param>
-		/// <returns>The list of the <see cref="CalendarItem"/> objects.</returns>
-		public List<CalendarItem> GetCalendar(DateTime startDate, DateTime endDate, int? personId, int? practiceManagerId)
-		{
-			return CalendarDAL.CalendarList(startDate, endDate, personId, practiceManagerId);
-		}
+        /// <summary>
+        /// Retrieves a list of the calendar items from the database.
+        /// </summary>
+        /// <param name="startDate">The start of the period.</param>
+        /// <param name="endDate">The end of the period.</param>
+        /// <param name="personId">
+        /// An ID of the person to the calendar be retrieved for.
+        /// If null the company calendar will be returned.
+        /// </param>
+        /// <param name="practiceManagerId">
+        /// An ID of the practice manager to retrieve the data for his subordinate
+        /// </param>
+        /// <returns>The list of the <see cref="CalendarItem"/> objects.</returns>
+        public List<CalendarItem> GetCalendar(DateTime startDate, DateTime endDate, int? personId, int? practiceManagerId)
+        {
+            return CalendarDAL.CalendarList(startDate, endDate, personId, practiceManagerId);
+        }
 
-		/// <summary>
-		/// Saves a <see cref="CalendarItem"/> object to the database.
-		/// </summary>
-		/// <param name="item">The data to be saved to.</param>
-		public void SaveCalendar(CalendarItem item, string userLogin)
-		{
+        /// <summary>
+        /// Saves a <see cref="CalendarItem"/> object to the database.
+        /// </summary>
+        /// <param name="item">The data to be saved to.</param>
+        public void SaveCalendar(CalendarItem item, string userLogin)
+        {
             CalendarDAL.CalendarUpdate(item, userLogin);
-		}
+        }
 
 
         public void SaveSubstituteDay(CalendarItem item, string userLogin)
@@ -59,7 +59,7 @@ namespace PracticeManagementService
             return CalendarDAL.GetCompanyHolidays(year);
         }
 
-        public List<Triple<int,string,bool>> GetRecurringHolidaysList()
+        public List<Triple<int, string, bool>> GetRecurringHolidaysList()
         {
             return CalendarDAL.GetRecurringHolidaysList();
         }
@@ -74,8 +74,13 @@ namespace PracticeManagementService
             return CalendarDAL.GetRecurringHolidaysInWeek(date, personId);
         }
 
+        public void SaveTimeOff(DateTime startDate, DateTime endDate, bool dayOff, int personId, double? actualHours, int timeTypeId, string userLogin)
+        {
+            CalendarDAL.SaveTimeOff(startDate, endDate, dayOff, personId, actualHours, timeTypeId, userLogin);
+        }
 
-		#endregion
-	}
+
+        #endregion
+    }
 }
 
