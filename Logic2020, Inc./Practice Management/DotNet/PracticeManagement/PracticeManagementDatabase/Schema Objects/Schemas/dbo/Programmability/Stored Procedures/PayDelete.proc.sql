30384e698c75abb39e6ff48ba272da52e19dd3e3
SrinivasM <SrinivasM>
2012-02-23 10:08:00 -0500

    Comments - Defect Number: 2983; Defect RecordID: 2979; Server Address: services.logic2020.com; Server Port: 99; Database Name: Practice Management; Defect Summary: Time->Calendar:New Time off/ PTO/Calendar functionality; Defect Description: From: Jason Hong-Turney [mailto:jasonht@logic2020.com] Sent: Friday, February 10, 2012 12:13 PM To: MadhanA; Matt Reilly Cc: ''Logic 20/20 Core Team'' Subject: RE: Time tracking Feature Status update.
    Hi Madhan 
    
    Finally, unrelated to the Time Entry page, but related to PTO/Calendar functionality.
     See attached mockup for changes to time off entry that allow for entering and editing a range of time, as well as a change to holiday substitution (replaces Floating Holiday).
    
    Thank you!
    
    Jason Hong-Turney // Logic20/20 // jasonht@logic2020.com
    t.206-576-0472 // c. 425-390-4664

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/PayDelete.proc.sql
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/PayDelete.proc.sql
@@ -3,42 +3,87 @@
 	@StartDate DATETIME
 AS
 BEGIN
-	DECLARE @TempStartDate DATETIME,
-			@TempEndDate DATETIME
-	IF EXISTS(SELECT 1 FROM dbo.Person WHERE PersonId = @PersonId AND IsStrawman =1 )
-	BEGIN
-		IF NOT EXISTS (SELECT 1 FROM dbo.Pay WHERE Person = @PersonId AND StartDate < @StartDate)
-		BEGIN
-			SELECT @TempEndDate = EndDate
-			FROM  dbo.Pay
-			WHERE Person = @PersonId AND StartDate = @StartDate
+	BEGIN TRY
+		BEGIN TRAN tran_PayDelete
 
+		DECLARE @TempStartDate DATETIME,
+				@TempEndDate DATETIME
+		IF EXISTS(SELECT 1 FROM dbo.Person WHERE PersonId = @PersonId AND IsStrawman =1 )
+		BEGIN
+			IF NOT EXISTS (SELECT 1 FROM dbo.Pay WHERE Person = @PersonId AND StartDate < @StartDate)
+			BEGIN
+				SELECT @TempEndDate = EndDate
+				FROM  dbo.Pay
+				WHERE Person = @PersonId AND StartDate = @StartDate
+
+				DELETE FROM dbo.Pay
+				WHERE Person = @PersonId AND StartDate = @StartDate
+
+				UPDATE  dbo.Pay
+				SET StartDate = @StartDate 
+				WHERE StartDate = @TempEndDate AND Person = @PersonId
+			END
+			ELSE 
+			BEGIN
+
+				SELECT @TempEndDate = EndDate
+				FROM  dbo.Pay
+				WHERE Person = @PersonId AND StartDate = @StartDate
+
+				DELETE FROM dbo.Pay
+				WHERE Person = @PersonId AND StartDate = @StartDate
+
+				UPDATE  dbo.Pay
+				SET EndDate = @TempEndDate 
+				WHERE EndDate = @StartDate AND Person = @PersonId
+			END
+		END
+		ELSE IF EXISTS(SELECT 1 FROM dbo.Person WHERE PersonId = @PersonId AND IsStrawman =0 )
+		BEGIN
+			DECLARE @EndDate DATETIME , @Timescale INT ,@W2SalaryId INT
+			SELECT @EndDate = enddate,@Timescale = Timescale FROM dbo.Pay WHERE Person = @PersonId AND StartDate = @StartDate
+			SELECT @W2SalaryId = TimescaleId FROM Timescale WHERE Name = 'W2-Salary'
+
+			--Delete Holiday timeEntries if person is not w2salaried.
+			IF (@Timescale = @W2SalaryId)
+			BEGIN
+				DECLARE @HolidayChargeCodeId INT
+				SELECT @HolidayChargeCodeId = Id FROM ChargeCode WHERE TimeTypeId = dbo.GetHolidayTimeTypeId()
+
+				DELETE TEH
+				FROM dbo.TimeEntryHours TEH
+				JOIN dbo.TimeEntry TE ON TE.TimeEntryId = TEH.TimeEntryId 
+				WHERE TE.ChargeCodeId  IN (@HolidayChargeCodeId ) 
+						AND TE.PersonId = @PersonId 
+						AND TE.ChargeCodeDate BETWEEN @StartDate AND @EndDate
+
+				DELETE TE
+				FROM dbo.TimeEntry TE 
+				WHERE TE.ChargeCodeId  IN (@HolidayChargeCodeId ) 
+						AND TE.PersonId = @PersonId 
+						AND TE.ChargeCodeDate BETWEEN @StartDate AND @EndDate
+
+			END
+			
 			DELETE FROM dbo.Pay
 			WHERE Person = @PersonId AND StartDate = @StartDate
 
-			UPDATE  dbo.Pay
-			SET StartDate = @StartDate 
-			WHERE StartDate = @TempEndDate AND Person = @PersonId
+			COMMIT TRAN tran_PayDelete
 		END
-		ELSE 
-		BEGIN
+	END TRY
+	BEGIN CATCH
+		ROLLBACK TRAN tran_PayDelete
+		
+		DECLARE	 @ERROR_STATE	tinyint
+		,@ERROR_SEVERITY		tinyint
+		,@ERROR_MESSAGE		    nvarchar(2000)
+		,@InitialTranCount		tinyint
 
-			SELECT @TempEndDate = EndDate
-			FROM  dbo.Pay
-			WHERE Person = @PersonId AND StartDate = @StartDate
+		SET	 @ERROR_MESSAGE		= ERROR_MESSAGE()
+		SET  @ERROR_SEVERITY	= ERROR_SEVERITY()
+		SET  @ERROR_STATE		= ERROR_STATE()
+		RAISERROR ('%s', @ERROR_SEVERITY, @ERROR_STATE, @ERROR_MESSAGE)
 
-			DELETE FROM dbo.Pay
-			WHERE Person = @PersonId AND StartDate = @StartDate
-
-			UPDATE  dbo.Pay
-			SET EndDate = @TempEndDate 
-			WHERE EndDate = @StartDate AND Person = @PersonId
-		END
-	END
-	ELSE IF EXISTS(SELECT 1 FROM dbo.Person WHERE PersonId = @PersonId AND IsStrawman =0 )
-	BEGIN
-	        DELETE FROM dbo.Pay
-			WHERE Person = @PersonId AND StartDate = @StartDate
-	END
+	END CATCH
 END
 
