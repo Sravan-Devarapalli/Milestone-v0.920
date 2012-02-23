c1891626b9b1b7edf5a6e4ade88a72974a42ffbe
SainathC <SainathC>
2012-02-23 02:16:00 -0500

    Comments - Defect Number: 2983; Defect RecordID: 2979; Server Address: services.logic2020.com; Server Port: 99; Database Name: Practice Management; Defect Summary: Time->Calendar:New Time off/ PTO/Calendar functionality; Defect Description: From: Jason Hong-Turney [mailto:jasonht@logic2020.com] Sent: Friday, February 10, 2012 12:13 PM To: MadhanA; Matt Reilly Cc: ''Logic 20/20 Core Team'' Subject: RE: Time tracking Feature Status update.
    Hi Madhan 
    
    Finally, unrelated to the Time Entry page, but related to PTO/Calendar functionality.
     See attached mockup for changes to time off entry that allow for entering and editing a range of time, as well as a change to holiday substitution (replaces Floating Holiday).
    
    Thank you!
    
    Jason Hong-Turney // Logic20/20 // jasonht@logic2020.com
    t.206-576-0472 // c. 425-390-4664

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/PersonsListHavingActiveStatusDuringThisPeriod.proc.sql
@@ -0,0 +1,23 @@
+﻿CREATE PROCEDURE [dbo].[PersonsListHavingActiveStatusDuringThisPeriod]
+(
+	@StartDate     DATETIME ,
+	@EndDate       DATETIME 
+)
+AS
+BEGIN
+		SELECT p.PersonId,
+		p.FirstName,
+		p.LastName,
+		p.IsDefaultManager
+		FROM dbo.Person AS p
+		INNER JOIN dbo.PersonStatusHistory PSH ON PSH.PersonId = p.PersonId 
+		WHERE 
+		p.IsStrawman = 0
+		AND PSH.PersonStatusId = 1 -- ACTIVE Status
+		AND ( 
+			 @StartDate BETWEEN PSH.StartDate  AND ISNULL(PSH.EndDate,dbo.GetFutureDate()) OR
+			 @EndDate BETWEEN  PSH.StartDate  AND ISNULL(PSH.EndDate,dbo.GetFutureDate())
+			)
+		ORDER BY p.LastName, p.FirstName
+
+END
