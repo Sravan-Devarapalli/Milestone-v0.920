9787c97cdefc4e38e329a306961452460273b453
SrinivasM <SrinivasM>
2012-02-23 10:08:00 -0500

    Comments - Defect Number: 2983; Defect RecordID: 2979; Server Address: services.logic2020.com; Server Port: 99; Database Name: Practice Management; Defect Summary: Time->Calendar:New Time off/ PTO/Calendar functionality; Defect Description: From: Jason Hong-Turney [mailto:jasonht@logic2020.com] Sent: Friday, February 10, 2012 12:13 PM To: MadhanA; Matt Reilly Cc: ''Logic 20/20 Core Team'' Subject: RE: Time tracking Feature Status update.
    Hi Madhan 
    
    Finally, unrelated to the Time Entry page, but related to PTO/Calendar functionality.
     See attached mockup for changes to time off entry that allow for entering and editing a range of time, as well as a change to holiday substitution (replaces Floating Holiday).
    
    Thank you!
    
    Jason Hong-Turney // Logic20/20 // jasonht@logic2020.com
    t.206-576-0472 // c. 425-390-4664

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/PersonCalendar.table.sql
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/PersonCalendar.table.sql
@@ -3,11 +3,11 @@
     [PersonId]			INT			NOT NULL,
     [DayOff]			BIT			NOT NULL,
 	[ActualHours]		REAL		NULL,
-	[TimeTypeId]		INT			NULL,
-	[IsFromTimeEntry]	BIT			NULL,
-	[SubstituteDate]	DATETIME	NULL,
-	[IsFloatingHoliday] BIT NULL,
-	[Description]		NVARCHAR(500) NULL
+	[IsSeries]			BIT NOT NULL CONSTRAINT DF_PersonCalendar_IsSeries DEFAULT 0,
+	[TimeTypeId]		INT NULL,
+	[SubstituteDate]	DATETIME NULL,
+	[Description]		NVARCHAR(500) NULL,
+	[IsFromTimeEntry]	BIT NULL
 );
 
 
