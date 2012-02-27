64078ac8a190120544f06af159d01999c50d4cf4
ThulasiramP <ThulasiramP>
2012-02-27 01:32:00 -0500

    Comments - Defect Number: 2983; Defect RecordID: 2979; Server Address: services.logic2020.com; Server Port: 99; Database Name: Practice Management; Defect Summary: Time->Calendar:New Time off/ PTO/Calendar functionality; Defect Description: From: Jason Hong-Turney [mailto:jasonht@logic2020.com] Sent: Friday, February 10, 2012 12:13 PM To: MadhanA; Matt Reilly Cc: ''Logic 20/20 Core Team'' Subject: RE: Time tracking Feature Status update.
    Hi Madhan 
    
    Finally, unrelated to the Time Entry page, but related to PTO/Calendar functionality.
     See attached mockup for changes to time off entry that allow for entering and editing a range of time, as well as a change to holiday substitution (replaces Floating Holiday).
    
    Thank you!
    
    Jason Hong-Turney // Logic20/20 // jasonht@logic2020.com
    t.206-576-0472 // c. 425-390-4664

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Functions/GetProjectManagerNames.function.sql
@@ -0,0 +1,27 @@
+﻿-- =============================================
+-- Author:		ThulasiRam.P
+-- Create date: 02-27-2012
+-- Description:	Gets Project Manager Names By ; separated for a single project
+-- =============================================
+
+CREATE FUNCTION [dbo].[GetProjectManagerNames]
+(
+  @ProjectId INT
+)
+RETURNS NVARCHAR(MAX)
+AS
+BEGIN
+  
+  DECLARE @Temp NVARCHAR(MAX) = ''
+  
+SELECT @Temp = @Temp + Pers.LastName + ', ' +Pers.FirstName +'; '
+FROM Project P
+JOIN ProjectManagers PM ON PM.ProjectId = P.ProjectId
+JOIN Person Pers ON Pers.Personid = PM.ProjectManagerId
+WHERE P.ProjectId = @ProjectId
+RETURN @Temp
+END
+
+
+GO
+
