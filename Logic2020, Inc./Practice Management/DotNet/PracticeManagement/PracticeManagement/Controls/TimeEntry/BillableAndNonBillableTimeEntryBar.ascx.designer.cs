14c84348e5d1f95ee07f88a636cd8c343446dd32
ThulasiramP <ThulasiramP>
2012-02-28 07:23:00 -0500

    Comments - Defect Number: 2983; Defect RecordID: 2979; Server Address: services.logic2020.com; Server Port: 99; Database Name: Practice Management; Defect Summary: Time->Calendar:New Time off/ PTO/Calendar functionality; Defect Description: From: Jason Hong-Turney [mailto:jasonht@logic2020.com] Sent: Friday, February 10, 2012 12:13 PM To: MadhanA; Matt Reilly Cc: ''Logic 20/20 Core Team'' Subject: RE: Time tracking Feature Status update.
    Hi Madhan 
    
    Finally, unrelated to the Time Entry page, but related to PTO/Calendar functionality.
     See attached mockup for changes to time off entry that allow for entering and editing a range of time, as well as a change to holiday substitution (replaces Floating Holiday).
    
    Thank you!
    
    Jason Hong-Turney // Logic20/20 // jasonht@logic2020.com
    t.206-576-0472 // c. 425-390-4664

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/Controls/TimeEntry/BillableAndNonBillableTimeEntryBar.ascx.designer.cs
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/Controls/TimeEntry/BillableAndNonBillableTimeEntryBar.ascx.designer.cs
@@ -13,6 +13,15 @@ namespace PraticeManagement.Controls.TimeEntry {
     public partial class BillableAndNonBillableTimeEntryBar {
         
         /// <summary>
+        /// tdplusProjectSection control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlTableCell tdplusProjectSection;
+        
+        /// <summary>
         /// ddlTimeTypes control.
         /// </summary>
         /// <remarks>
