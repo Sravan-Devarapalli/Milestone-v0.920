6ba3ac68d7ec95b1eed48b1eb2062931331f365c
SainathC <SainathC>
2012-02-29 08:37:00 -0500

    Comments - Defect Number: 2983; Defect RecordID: 2979; Server Address: services.logic2020.com; Server Port: 99; Database Name: Practice Management; Defect Summary: Time->Calendar:New Time off/ PTO/Calendar functionality; Defect Description: From: Jason Hong-Turney [mailto:jasonht@logic2020.com] Sent: Friday, February 10, 2012 12:13 PM To: MadhanA; Matt Reilly Cc: ''Logic 20/20 Core Team'' Subject: RE: Time tracking Feature Status update.
    Hi Madhan 
    
    Finally, unrelated to the Time Entry page, but related to PTO/Calendar functionality.
     See attached mockup for changes to time off entry that allow for entering and editing a range of time, as well as a change to holiday substitution (replaces Floating Holiday).
    
    Thank you!
    
    Jason Hong-Turney // Logic20/20 // jasonht@logic2020.com
    t.206-576-0472 // c. 425-390-4664

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/IsPersonHaveActiveStatusDuringThisPeriod.proc.sql
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/IsPersonHaveActiveStatusDuringThisPeriod.proc.sql
@@ -8,17 +8,21 @@ AS
 BEGIN
 
 	DECLARE @IsPersonHasActiveStatus BIT
-	DECLARE @FutureDate DATETIME , @MinStartDate DATETIME ,@NOW DATETIME 
+	DECLARE @FutureDate DATETIME , @MinStartDate DATETIME ,@NOW DATETIME,@MinStartDateStatusId INT,@PersonHireDate DATETIME
 	
 	
 	SELECT @MinStartDate = MIN(PSH.StartDate) FROM dbo.PersonStatusHistory PSH
 					   WHERE PSH.PersonId = @PersonId
 
+	SELECT @MinStartDateStatusId = PSH.PersonStatusId FROM dbo.PersonStatusHistory PSH
+					   WHERE PSH.PersonId = @PersonId AND PSH.StartDate = @MinStartDate
+
+	SELECT @PersonHireDate = P.HireDate FROM dbo.Person P WHERE P.PersonId = @PersonId
+
 	SET  @FutureDate = dbo.GetFutureDate()
 	SET  @NOW = dbo.GettingPMTime(GETUTCDATE())
 	
 	SET @IsPersonHasActiveStatus = 0
-	
 	
 	IF EXISTS(SELECT 1 	FROM dbo.PersonStatusHistory PSH
 				WHERE PSH.PersonId = @PersonId AND PSH.PersonStatusId = 1 
@@ -29,10 +33,12 @@ BEGIN
 	END
 	ELSE 
 		BEGIN
-	   
-			IF ((@StartDate < @MinStartDate) AND 
-			EXISTS(SELECT 1 FROM dbo.PersonStatusHistory PSH WHERE PSH.PersonId = @PersonId AND PSH.PersonStatusId = 1 AND PSH.StartDate = @MinStartDate))
-				BEGIN
+			IF (@MinStartDateStatusId = 1 
+				AND @PersonHireDate < @StartDate  
+				AND @PersonHireDate  < @MinStartDate 
+				AND @StartDate < @MinStartDate 
+				AND @PersonHireDate < ISNULL(@EndDate,@FutureDate))
+			BEGIN
 					SET @IsPersonHasActiveStatus  = 1
 				END
 			ELSE
@@ -40,6 +46,7 @@ BEGIN
 					SET @IsPersonHasActiveStatus = 0
 				END
 		END
+
 		
 	SELECT @IsPersonHasActiveStatus
 
