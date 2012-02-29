4aa8c5f360c5b37268a9d6258b484eeef51aa143
SainathC <SainathC>
2012-02-29 08:37:00 -0500

    Comments - Defect Number: 2983; Defect RecordID: 2979; Server Address: services.logic2020.com; Server Port: 99; Database Name: Practice Management; Defect Summary: Time->Calendar:New Time off/ PTO/Calendar functionality; Defect Description: From: Jason Hong-Turney [mailto:jasonht@logic2020.com] Sent: Friday, February 10, 2012 12:13 PM To: MadhanA; Matt Reilly Cc: ''Logic 20/20 Core Team'' Subject: RE: Time tracking Feature Status update.
    Hi Madhan 
    
    Finally, unrelated to the Time Entry page, but related to PTO/Calendar functionality.
     See attached mockup for changes to time off entry that allow for entering and editing a range of time, as well as a change to holiday substitution (replaces Floating Holiday).
    
    Thank you!
    
    Jason Hong-Turney // Logic20/20 // jasonht@logic2020.com
    t.206-576-0472 // c. 425-390-4664

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/PersonsListHavingActiveStatusDuringThisPeriod.proc.sql
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/PersonsListHavingActiveStatusDuringThisPeriod.proc.sql
@@ -5,19 +5,33 @@
 )
 AS
 BEGIN
+		
+		DECLARE @MinStartdates TABLE (personId INT ,minStartDate DATETIME,statusId INT)
+		
+		INSERT INTO @MinStartdates (personId ,minStartDate)
+		SELECT PSH.PersonId,MIN(PSH.StartDate) 
+		FROM dbo.PersonStatusHistory PSH
+		GROUP BY PSH.PersonId 
+		
+		UPDATE minSD
+		SET statusId = PSH.PersonStatusId
+		FROM @MinStartdates minSD
+		INNER JOIN dbo.PersonStatusHistory PSH ON PSH.PersonId = minSD.personId AND minSD.minStartDate = PSH.StartDate
+
+
 		SELECT p.PersonId,
 		p.FirstName,
 		p.LastName,
 		p.IsDefaultManager
 		FROM dbo.Person AS p
 		INNER JOIN dbo.PersonStatusHistory PSH ON PSH.PersonId = p.PersonId 
+		INNER JOIN @MinStartdates minSD ON minSD.personId = p.PersonId
 		WHERE 
 		p.IsStrawman = 0
 		AND PSH.PersonStatusId = 1 -- ACTIVE Status
-		AND ( 
-			 @StartDate BETWEEN PSH.StartDate  AND ISNULL(PSH.EndDate,dbo.GetFutureDate()) OR
-			 @EndDate BETWEEN  PSH.StartDate  AND ISNULL(PSH.EndDate,dbo.GetFutureDate())
-			)
+		AND P.HireDate < @StartDate 
+		AND  ( @StartDate < ISNULL(PSH.EndDate,dbo.GetFutureDate()) AND @EndDate > PSH.StartDate ) OR
+		     ( minSD.statusId = 1 AND  P.HireDate < minSD.minStartDate AND @StartDate < minSD.minStartDate AND @EndDate > P.HireDate )
 		ORDER BY p.LastName, p.FirstName
 
 END
