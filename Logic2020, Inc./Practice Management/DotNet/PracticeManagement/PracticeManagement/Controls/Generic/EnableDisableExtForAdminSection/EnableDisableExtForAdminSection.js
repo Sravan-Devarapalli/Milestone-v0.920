e455309871a951c2c01ad4c3a04e3ec7ce614cae
ThulasiramP <ThulasiramP>
2012-03-05 10:08:00 -0500

    Comments - Defect Number: 2983; Defect RecordID: 2979; Server Address: services.logic2020.com; Server Port: 99; Database Name: Practice Management; Defect Summary: Time->Calendar:New Time off/ PTO/Calendar functionality; Defect Description: From: Jason Hong-Turney [mailto:jasonht@logic2020.com] Sent: Friday, February 10, 2012 12:13 PM To: MadhanA; Matt Reilly Cc: ''Logic 20/20 Core Team'' Subject: RE: Time tracking Feature Status update.
    Hi Madhan 
    
    Finally, unrelated to the Time Entry page, but related to PTO/Calendar functionality.
     See attached mockup for changes to time off entry that allow for entering and editing a range of time, as well as a change to holiday substitution (replaces Floating Holiday).
    
    Thank you!
    
    Jason Hong-Turney // Logic20/20 // jasonht@logic2020.com
    t.206-576-0472 // c. 425-390-4664

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/Controls/Generic/EnableDisableExtForAdminSection/EnableDisableExtForAdminSection.js
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/Controls/Generic/EnableDisableExtForAdminSection/EnableDisableExtForAdminSection.js
@@ -1,63 +1,63 @@
-﻿Type.registerNamespace( "PraticeManagement.Controls.Generic.EnableDisableExtForAdminSection" );
+﻿Type.registerNamespace("PraticeManagement.Controls.Generic.EnableDisableExtForAdminSection");
 
-PraticeManagement.Controls.Generic.EnableDisableExtForAdminSection.EnableDisableExtForAdminSectionBehavior = function ( element ) {
-    PraticeManagement.Controls.Generic.EnableDisableExtForAdminSection.EnableDisableExtForAdminSectionBehavior.initializeBase( this, [element] );
+PraticeManagement.Controls.Generic.EnableDisableExtForAdminSection.EnableDisableExtForAdminSectionBehavior = function (element) {
+    PraticeManagement.Controls.Generic.EnableDisableExtForAdminSection.EnableDisableExtForAdminSectionBehavior.initializeBase(this, [element]);
     this.controlToDisableId = null;
     this._isControlWasDisabled = false;
 }
 
 PraticeManagement.Controls.Generic.EnableDisableExtForAdminSection.EnableDisableExtForAdminSectionBehavior.prototype = {
     initialize: function () {
-        PraticeManagement.Controls.Generic.EnableDisableExtForAdminSection.EnableDisableExtForAdminSectionBehavior.callBaseMethod( this, 'initialize' );
+        PraticeManagement.Controls.Generic.EnableDisableExtForAdminSection.EnableDisableExtForAdminSectionBehavior.callBaseMethod(this, 'initialize');
 
-        var hoursControlIds = this.getControlIdList( this.hoursControlsToCheck );
-        for ( var i = 0; i < hoursControlIds.length; i++ ) {
-            var control = document.getElementById( hoursControlIds[i] );
-            if ( control ) {
-                $addHandlers( 
+        var hoursControlIds = this.getControlIdList(this.hoursControlsToCheck);
+        for (var i = 0; i < hoursControlIds.length; i++) {
+            var control = document.getElementById(hoursControlIds[i]);
+            if (control) {
+                $addHandlers(
                      control,
                      {
                          'change': this._onChange
                      },
-                     this );
+                     this);
             }
         }
 
-        var notesControlIds = this.getControlIdList( this.notesControlsToCheck );
-        for ( var i = 0; i < notesControlIds.length; i++ ) {
-            var control = document.getElementById( notesControlIds[i] );
-            if ( control ) {
-                $addHandlers( 
+        var notesControlIds = this.getControlIdList(this.notesControlsToCheck);
+        for (var i = 0; i < notesControlIds.length; i++) {
+            var control = document.getElementById(notesControlIds[i]);
+            if (control) {
+                $addHandlers(
                      control,
                      {
                          'change': this._onChange
                      },
-                     this );
+                     this);
             }
         }
-        var deleteControlIds = this.getControlIdList( this.deleteControlsToCheck );
-        for ( var i = 0; i < deleteControlIds.length; i++ ) {
-            var control = document.getElementById( deleteControlIds[i] );
-            if ( control ) {
-                $addHandlers( 
+        var deleteControlIds = this.getControlIdList(this.deleteControlsToCheck);
+        for (var i = 0; i < deleteControlIds.length; i++) {
+            var control = document.getElementById(deleteControlIds[i]);
+            if (control) {
+                $addHandlers(
                      control,
                      {
                          'mousedown': this._onMousedown
                      },
-                     this );
+                     this);
             }
         }
 
-        var closeControlIds = this.getControlIdList( this.closeControlsToCheck );
-        for ( var i = 0; i < closeControlIds.length; i++ ) {
-            var control = document.getElementById( closeControlIds[i] );
-            if ( control ) {
-                $addHandlers( 
+        var closeControlIds = this.getControlIdList(this.closeControlsToCheck);
+        for (var i = 0; i < closeControlIds.length; i++) {
+            var control = document.getElementById(closeControlIds[i]);
+            if (control) {
+                $addHandlers(
                      control,
                      {
                          'click': this._onCloseClick
                      },
-                     this );
+                     this);
             }
         }
 
@@ -67,53 +67,53 @@ PraticeManagement.Controls.Generic.EnableDisableExtForAdminSection.EnableDisable
 
     dispose: function () {
         // Cleanup code 
-        PraticeManagement.Controls.Generic.EnableDisableExtForAdminSection.EnableDisableExtForAdminSectionBehavior.callBaseMethod( this, 'dispose' );
+        PraticeManagement.Controls.Generic.EnableDisableExtForAdminSection.EnableDisableExtForAdminSectionBehavior.callBaseMethod(this, 'dispose');
     },
 
-    getControlIdList: function ( controlsToCheck ) {
+    getControlIdList: function (controlsToCheck) {
         var contolsToCheckList = controlsToCheck;
-        if ( contolsToCheckList )
-            return contolsToCheckList.split( ';' );
+        if (contolsToCheckList)
+            return contolsToCheckList.split(';');
         else
             return [];
     },
-    isHoursControlId: function ( targetId ) {
-        var hoursControlIds = this.getControlIdList( this.hoursControlsToCheck );
-        for ( var i = 0; i < hoursControlIds.length; i++ ) {
-            if ( hoursControlIds[i] == targetId ) {
+    isHoursControlId: function (targetId) {
+        var hoursControlIds = this.getControlIdList(this.hoursControlsToCheck);
+        for (var i = 0; i < hoursControlIds.length; i++) {
+            if (hoursControlIds[i] == targetId) {
                 return true;
             }
         }
         return false;
     },
     getTargetControl: function () {
-        var hoursControlIds = this.getControlIdList( this.hoursControlsToCheck );
-        for ( var i = 0; i < hoursControlIds.length; i++ ) {
-            var targetAcutalHours = document.getElementById( hoursControlIds[i] );
-            if ( targetAcutalHours != null && targetAcutalHours.value != '' ) {
+        var hoursControlIds = this.getControlIdList(this.hoursControlsToCheck);
+        for (var i = 0; i < hoursControlIds.length; i++) {
+            var targetAcutalHours = document.getElementById(hoursControlIds[i]);
+            if (targetAcutalHours != null && targetAcutalHours.value != '') {
                 return targetAcutalHours;
             }
         }
-        var notesControlIds = this.getControlIdList( this.notesControlsToCheck );
-        for ( var i = 0; i < notesControlIds.length; i++ ) {
-            var targetNotes = document.getElementById( notesControlIds[i] );
-            if ( targetNotes != null && targetNotes.value != '' ) {
+        var notesControlIds = this.getControlIdList(this.notesControlsToCheck);
+        for (var i = 0; i < notesControlIds.length; i++) {
+            var targetNotes = document.getElementById(notesControlIds[i]);
+            if (targetNotes != null && targetNotes.value != '') {
                 return targetNotes;
             }
         }
         return null;
     },
-    getTargetControlIndex: function ( targetId ) {
-        var hoursControlIds = this.getControlIdList( this.hoursControlsToCheck );
-        for ( var i = 0; i < hoursControlIds.length; i++ ) {
-            if ( hoursControlIds[i] == targetId ) {
+    getTargetControlIndex: function (targetId) {
+        var hoursControlIds = this.getControlIdList(this.hoursControlsToCheck);
+        for (var i = 0; i < hoursControlIds.length; i++) {
+            if (hoursControlIds[i] == targetId) {
                 return i;
             }
         }
-        var notesControlIds = this.getControlIdList( this.notesControlsToCheck );
-        for ( var i = 0; i < notesControlIds.length; i++ ) {
-            var targetNotes = document.getElementById( notesControlIds[i] );
-            if ( notesControlIds[i] == targetId ) {
+        var notesControlIds = this.getControlIdList(this.notesControlsToCheck);
+        for (var i = 0; i < notesControlIds.length; i++) {
+            var targetNotes = document.getElementById(notesControlIds[i]);
+            if (notesControlIds[i] == targetId) {
                 return i;
             }
         }
@@ -121,95 +121,81 @@ PraticeManagement.Controls.Generic.EnableDisableExtForAdminSection.EnableDisable
     },
     _onMousedown: function () {
         var target = this.getTargetControl();
-        if ( target != null ) {
+        if (target != null) {
             this.enableAllColtrols();
         }
     },
-    disableOtherColtrols: function ( index ) {
-        var hoursControlIds = this.getControlIdList( this.hoursControlsToCheck );
-        for ( var i = 0; i < hoursControlIds.length; i++ ) {
-            var isReviewed = false;
-            var targetAcutalHours = document.getElementById( hoursControlIds[i] );
-            if ( targetAcutalHours != null ) {
-                if ( targetAcutalHours.getAttribute( 'IsReviewed' ) != undefined && targetAcutalHours.getAttribute( 'IsReviewed' ) != null ) {
-                    isReviewed = targetAcutalHours.getAttribute( 'IsReviewed' ).toLowerCase() == 'approved';
+    disableOtherColtrols: function (index) {
+        var hoursControlIds = this.getControlIdList(this.hoursControlsToCheck);
+        for (var i = 0; i < hoursControlIds.length; i++) {
+
+            var targetAcutalHours = document.getElementById(hoursControlIds[i]);
+            if (targetAcutalHours != null) {
+                if (i != index) {
+                    targetAcutalHours.setAttribute('readonly', 'readonly');
                 }
-                if ( !isReviewed ) {
-                    if ( i != index )
-                        targetAcutalHours.setAttribute( 'readonly', 'readonly' );
-                    else
-                        targetAcutalHours.removeAttribute( 'readonly' );
-                } else {
-                    targetAcutalHours.setAttribute( 'disabled', 'disabled' );
+                else {
+                    targetAcutalHours.removeAttribute('readonly');
                 }
             }
         }
-        var notesControlIds = this.getControlIdList( this.notesControlsToCheck );
-        for ( var i = 0; i < notesControlIds.length; i++ ) {
-            var targetNotes = document.getElementById( notesControlIds[i] );
-            if ( targetNotes != null ) {
-                if ( i != index )
-                    targetNotes.setAttribute( 'readonly', 'readonly' );
+        var notesControlIds = this.getControlIdList(this.notesControlsToCheck);
+        for (var i = 0; i < notesControlIds.length; i++) {
+            var targetNotes = document.getElementById(notesControlIds[i]);
+            if (targetNotes != null) {
+                if (i != index)
+                    targetNotes.setAttribute('readonly', 'readonly');
                 else
-                    targetNotes.removeAttribute( 'readonly' );
+                    targetNotes.removeAttribute('readonly');
             }
         }
-        var deleteControlIds = this.getControlIdList( this.deleteControlsToCheck );
-        for ( var i = 0; i < deleteControlIds.length; i++ ) {
-            var targetDelete = document.getElementById( deleteControlIds[i] );
-            if ( targetDelete != null ) {
-                if ( i != index )
-                    targetDelete.setAttribute( 'disabled', 'disabled' );
+        var deleteControlIds = this.getControlIdList(this.deleteControlsToCheck);
+        for (var i = 0; i < deleteControlIds.length; i++) {
+            var targetDelete = document.getElementById(deleteControlIds[i]);
+            if (targetDelete != null) {
+                if (i != index)
+                    targetDelete.setAttribute('disabled', 'disabled');
                 else
-                    targetDelete.removeAttribute( 'disabled' );
+                    targetDelete.removeAttribute('disabled');
             }
         }
 
 
     },
     enableAllColtrols: function () {
-        var hoursControlIds = this.getControlIdList( this.hoursControlsToCheck );
-        for ( var i = 0; i < hoursControlIds.length; i++ ) {
-            var isReviewed = false;
-            var targetAcutalHours = document.getElementById( hoursControlIds[i] );
-            if ( targetAcutalHours != null ) {
-                if ( targetAcutalHours.getAttribute( 'IsReviewed' ) != undefined && targetAcutalHours.getAttribute( 'IsReviewed' ) != null ) {
-                    isReviewed = targetAcutalHours.getAttribute( 'IsReviewed' ).toLowerCase() == 'approved';
-                }
-                if ( !isReviewed ) {
-                    targetAcutalHours.removeAttribute( 'readonly' );
-                }
-                else {
-                    targetAcutalHours.setAttribute( 'disabled', 'disabled' );
-                }
+        var hoursControlIds = this.getControlIdList(this.hoursControlsToCheck);
+        for (var i = 0; i < hoursControlIds.length; i++) {
+            var targetAcutalHours = document.getElementById(hoursControlIds[i]);
+            if (targetAcutalHours != null) {
+                targetAcutalHours.removeAttribute('readonly');
             }
         }
-        var notesControlIds = this.getControlIdList( this.notesControlsToCheck );
-        for ( var i = 0; i < notesControlIds.length; i++ ) {
-            var targetNotes = document.getElementById( notesControlIds[i] );
-            if ( targetNotes != null ) {
-                targetNotes.removeAttribute( 'readonly' );
+        var notesControlIds = this.getControlIdList(this.notesControlsToCheck);
+        for (var i = 0; i < notesControlIds.length; i++) {
+            var targetNotes = document.getElementById(notesControlIds[i]);
+            if (targetNotes != null) {
+                targetNotes.removeAttribute('readonly');
             }
         }
-        var deleteControlIds = this.getControlIdList( this.deleteControlsToCheck );
-        for ( var i = 0; i < deleteControlIds.length; i++ ) {
-            var targetDelete = document.getElementById( deleteControlIds[i] );
-            if ( targetDelete != null ) {
-                targetDelete.removeAttribute( 'disabled' );
+        var deleteControlIds = this.getControlIdList(this.deleteControlsToCheck);
+        for (var i = 0; i < deleteControlIds.length; i++) {
+            var targetDelete = document.getElementById(deleteControlIds[i]);
+            if (targetDelete != null) {
+                targetDelete.removeAttribute('disabled');
             }
         }
     },
     _onCloseClick: function () {
-        
+
         var target = this.getTargetControl();
-        if ( target != null ) {
-            var index = this.getTargetControlIndex( target.id );
-            var hiddenNotesControlIds = this.getControlIdList( this.hiddenNotesControlsToCheck );
-            var hoursControlIds = this.getControlIdList( this.hoursControlsToCheck );
-            var hiddenNotesControl = document.getElementById( hiddenNotesControlIds[index] );
-            var hoursControl = document.getElementById( hoursControlIds[index] );
-            if ( hiddenNotesControl != null && hoursControl != null ) {
-                if ( hiddenNotesControl.value == '' && hoursControl.value == '' ) {
+        if (target != null) {
+            var index = this.getTargetControlIndex(target.id);
+            var hiddenNotesControlIds = this.getControlIdList(this.hiddenNotesControlsToCheck);
+            var hoursControlIds = this.getControlIdList(this.hoursControlsToCheck);
+            var hiddenNotesControl = document.getElementById(hiddenNotesControlIds[index]);
+            var hoursControl = document.getElementById(hoursControlIds[index]);
+            if (hiddenNotesControl != null && hoursControl != null) {
+                if (hiddenNotesControl.value == '' && hoursControl.value == '') {
                     //notes not save roll back the onchange event and enable all  
                     this.enableAllColtrols();
                 }
@@ -220,38 +206,38 @@ PraticeManagement.Controls.Generic.EnableDisableExtForAdminSection.EnableDisable
     _onChange: function () {
 
         var target = this.getTargetControl();
-        if ( target != null ) {
+        if (target != null) {
 
-            var isHourTextBox = this.isHoursControlId( target.id );
-            var targetAcutalHoursHiddenField = document.getElementById( this.targetAcutalHoursHiddenFieldId );
-            var targetNotesHiddenField = document.getElementById( this.targetNotesHiddenFieldId );
+            var isHourTextBox = this.isHoursControlId(target.id);
+            var targetAcutalHoursHiddenField = document.getElementById(this.targetAcutalHoursHiddenFieldId);
+            var targetNotesHiddenField = document.getElementById(this.targetNotesHiddenFieldId);
 
             var targetAcutalHours = null;
             var targetNotes = null;
 
-            if ( targetAcutalHoursHiddenField.value != '' )
-                var targetAcutalHours = document.getElementById( targetAcutalHoursHiddenField.value );
+            if (targetAcutalHoursHiddenField.value != '')
+                var targetAcutalHours = document.getElementById(targetAcutalHoursHiddenField.value);
 
-            if ( targetNotesHiddenField.value != '' )
-                var targetNotes = document.getElementById( targetNotesHiddenField.value );
+            if (targetNotesHiddenField.value != '')
+                var targetNotes = document.getElementById(targetNotesHiddenField.value);
 
-            if ( ( targetAcutalHours == null || targetAcutalHours == undefined ) && isHourTextBox ) {
+            if ((targetAcutalHours == null || targetAcutalHours == undefined) && isHourTextBox) {
                 targetAcutalHoursHiddenField.value = target.id;
-                targetAcutalHours = document.getElementById( target.id );
+                targetAcutalHours = document.getElementById(target.id);
             }
-            if ( ( targetNotes == null || targetNotes == undefined ) && !isHourTextBox ) {
+            if ((targetNotes == null || targetNotes == undefined) && !isHourTextBox) {
                 targetNotesHiddenField.value = target.id;
-                targetNotes = document.getElementById( target.id );
+                targetNotes = document.getElementById(target.id);
             }
 
-            if ( targetAcutalHours != null || targetNotes != null ) {
-                var index = this.getTargetControlIndex( target.id );
-                this.disableOtherColtrols( index );
+            if (targetAcutalHours != null || targetNotes != null) {
+                var index = this.getTargetControlIndex(target.id);
+                this.disableOtherColtrols(index);
             }
         }
     }
 
 }
 
-PraticeManagement.Controls.Generic.EnableDisableExtForAdminSection.EnableDisableExtForAdminSectionBehavior.registerClass( 'PraticeManagement.Controls.Generic.EnableDisableExtForAdminSection.EnableDisableExtForAdminSectionBehavior', AjaxControlToolkit.BehaviorBase );
+PraticeManagement.Controls.Generic.EnableDisableExtForAdminSection.EnableDisableExtForAdminSectionBehavior.registerClass('PraticeManagement.Controls.Generic.EnableDisableExtForAdminSection.EnableDisableExtForAdminSectionBehavior', AjaxControlToolkit.BehaviorBase);
 
