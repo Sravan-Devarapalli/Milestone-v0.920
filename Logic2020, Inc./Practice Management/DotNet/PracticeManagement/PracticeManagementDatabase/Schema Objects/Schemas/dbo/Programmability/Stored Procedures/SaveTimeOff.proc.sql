fdbff378fb224742441db98a96471eee9a2ddebb
SrinivasM <SrinivasM>
2012-03-06 03:42:00 -0500

    Comments - Added Approved By in New TimeEntry page.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/SaveTimeOff.proc.sql
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/SaveTimeOff.proc.sql
@@ -47,16 +47,18 @@ BEGIN
 			@ModifiedBy INT,
 			@HolidayTimeTypeId INT,
 			@Description NVARCHAR(500),
-			@IsSeries BIT
+			@IsSeries BIT,
+			@ORTTimeTypeId INT
 			
 	SELECT @Today = dbo.GettingPMTime(GETUTCDATE()),
 			@CurrentPMTime = dbo.InsertingTime(),
-			@HolidayTimeTypeId = dbo.GetHolidayTimeTypeId()
+			@HolidayTimeTypeId = dbo.GetHolidayTimeTypeId(),
+			@ORTTimeTypeId = dbo.GetORTTimeTypeId()
 
 	SELECT @ModifiedBy = PersonId FROM Person WHERE Alias = @UserLogin
-	SELECT @Description = Name FROM TimeType WHERE TimeTypeId = @TimeTypeId
+	SELECT @Description = Name + '.' FROM TimeType WHERE TimeTypeId = @TimeTypeId
 	SELECT @IsSeries = CASE WHEN DATEDIFF(DD, @StartDate, @EndDate) = 0 THEN 0 ELSE 1 END
-	SELECT @ApprovedBy = CASE WHEN @ApprovedBy IS NOT NULL THEN @ApprovedBy ELSE @ModifiedBy END
+	--SELECT @ApprovedBy = CASE WHEN @ApprovedBy IS NOT NULL THEN @ApprovedBy ELSE @ModifiedBy END
 
 	BEGIN TRY
 	BEGIN TRANSACTION tran_SaveTimeOff
@@ -92,13 +94,13 @@ BEGIN
 			SELECT PC.PersonId, AD.Date, CONVERT(BIT, 0) 'IsSeries'
 			FROM Calendar C
 			JOIN PersonCalendar PC ON PC.Date IN (@StartDate, @EndDate) AND PC.PersonId = @PersonId AND PC.DayOff = 1 AND PC.IsSeries = 1 AND C.Date = PC.Date
-			LEFT JOIN PersonCalendar AD ON PC.PersonId = AD.PersonId AND AD.DayOff = 1 AND AD.IsSeries = 1 AND PC.TimeTypeId = AD.TimeTypeId AND PC.ActualHours = AD.ActualHours AND ISNULL(PC.ApprovedBy, 0) = AD.ApprovedBy
+			LEFT JOIN PersonCalendar AD ON PC.PersonId = AD.PersonId AND AD.DayOff = 1 AND AD.IsSeries = 1 AND PC.TimeTypeId = AD.TimeTypeId AND PC.ActualHours = AD.ActualHours AND ISNULL(PC.ApprovedBy, 0) = ISNULL(AD.ApprovedBy, 0)
 										AND ( (DATEPART(DW, @EndDate) = 6 AND AD.date = DATEADD(DD,3, @EndDate) )
 												OR (DATEPART(DW, @StartDate) = 2 AND AD.date = DATEADD(DD, -3, @StartDate))
 												OR AD.date = DATEADD(DD,1, @EndDate)
 												OR AD.date = DATEADD(DD, -1, @StartDate)
 											)
-			LEFT JOIN PersonCalendar EDFS ON AD.PersonId = EDFS.PersonId AND EDFS.DayOff = 1 AND EDFS.IsSeries = 1 AND EDFS.TimeTypeId = AD.TimeTypeId AND EDFS.ActualHours = AD.ActualHours AND ISNULL(EDFS.ApprovedBy, 0) = AD.ApprovedBy
+			LEFT JOIN PersonCalendar EDFS ON AD.PersonId = EDFS.PersonId AND EDFS.DayOff = 1 AND EDFS.IsSeries = 1 AND EDFS.TimeTypeId = AD.TimeTypeId AND EDFS.ActualHours = AD.ActualHours AND ISNULL(EDFS.ApprovedBy, 0) = ISNULL(AD.ApprovedBy, 0)
 										AND ( (DATEPART(DW, AD.date) = 6 AND EDFS.date = DATEADD(DD,3, AD.date) )
 												OR (DATEPART(DW, AD.date) = 2 AND EDFS.date = DATEADD(DD, -3, AD.date))
 												OR EDFS.date = DATEADD(DD,1, AD.date)
@@ -127,7 +129,7 @@ BEGIN
 
 		--Insert new Offs.
 		INSERT INTO PersonCalendar(PersonId, Date, DayOff, TimeTypeId, ActualHours, Description, IsSeries, IsFromTimeEntry, ApprovedBy)
-		SELECT @PersonId, DEH.Date, @DayOff, @TimeTypeId, @ActualHours, @Description, @IsSeries, 0, @ApprovedBy
+		SELECT @PersonId, DEH.Date, @DayOff, @TimeTypeId, @ActualHours, @Description, @IsSeries, 0, CASE WHEN @TimeTypeId = @ORTTimeTypeId THEN @ApprovedBy ELSE NULL END
 		FROM @DaysExceptHolidays DEH
 		LEFT JOIN PersonCalendar PC ON PC.Date = DEH.Date AND PC.PersonId = @PersonId
 		WHERE PC.Date IS NULL
@@ -140,14 +142,14 @@ BEGIN
 				Description = @Description,
 				ApprovedBy = @ApprovedBy
 		FROM PersonCalendar PC
-		JOIN @DaysExceptHolidays DEH ON PC.PersonId = @PersonId AND PC.Date = DEH.Date AND (PC.TimeTypeId <> @TimeTypeId OR PC.ActualHours <> @ActualHours OR ISNULL(PC.ApprovedBy, 0) <> @ApprovedBy)
+		JOIN @DaysExceptHolidays DEH ON PC.PersonId = @PersonId AND PC.Date = DEH.Date AND (PC.TimeTypeId <> @TimeTypeId OR PC.ActualHours <> @ActualHours OR ISNULL(PC.ApprovedBy, 0) <> ISNULL(@ApprovedBy, 0))
 
 		;WITH ExcludeDates AS
 		(
 			SELECT PC.PersonId, PC.Date, CONVERT(BIT, 0) 'IsSeries'
 			FROM @DaysExceptHolidays DEH
 			JOIN PersonCalendar PC ON PC.Date = DEH.Date AND PC.PersonId = @PersonId
-			LEFT JOIN PersonCalendar CD ON CD.PersonId = PC.PersonId AND CD.IsSeries = 1 AND PC.TimeTypeId = CD.TimeTypeId AND PC.ActualHours = CD.ActualHours AND ISNULL(PC.ApprovedBy, 0) = CD.ApprovedBy
+			LEFT JOIN PersonCalendar CD ON CD.PersonId = PC.PersonId AND CD.IsSeries = 1 AND PC.TimeTypeId = CD.TimeTypeId AND PC.ActualHours = CD.ActualHours AND ISNULL(PC.ApprovedBy, 0) = ISNULL(CD.ApprovedBy, 0)
 											AND ( (DATEPART(DW, PC.date) = 6 AND CD.date = DATEADD(DD,3, PC.date) )
 													OR (DATEPART(DW, PC.date) = 2 AND CD.date = DATEADD(DD, -3, PC.date))
 													OR CD.date = DATEADD(DD,1, PC.date)
@@ -175,13 +177,13 @@ BEGIN
 			SELECT  PC.PersonId 'PersonId', APC.Date 'Date', CONVERT(BIT, 0) 'IsSeries'
 			FROM Calendar C
 			JOIN PersonCalendar PC ON C.Date IN (@StartDate, @EndDate) AND PersonId = @PersonId AND C.Date = PC.Date AND PC.DayOff = 1 AND PC.IsSeries = 1
-			LEFT JOIN PersonCalendar APC ON PC.PersonId = APC.PersonId AND PC.DayOff = 1 AND APC.IsSeries = 1 AND APC.TimeTypeId = PC.TimeTypeId AND APC.ActualHours = PC.ActualHours
+			LEFT JOIN PersonCalendar APC ON PC.PersonId = APC.PersonId AND PC.DayOff = 1 AND APC.IsSeries = 1 AND APC.TimeTypeId = PC.TimeTypeId AND APC.ActualHours = PC.ActualHours AND ISNULL(APC.ApprovedBy, 0) = ISNULL(PC.ApprovedBy, 0)
 											AND ((DATEPART(DW, C.date) = 6 AND APC.date = DATEADD(DD,3, C.date) )
 													OR (DATEPART(DW, C.date) = 2 AND APC.date = DATEADD(DD, -3, C.date))
 													OR  APC.date = DATEADD(DD,1, C.date)
 													OR  APC.date = DATEADD(DD, -1, C.date)
 												)
-			LEFT JOIN PersonCalendar AFAPC ON APC.PersonId = AFAPC.PersonId AND AFAPC.DayOff = 1 AND AFAPC.IsSeries = 1 AND AFAPC.TimeTypeId =APC.TimeTypeId AND AFAPC.ActualHours = APC.ActualHours
+			LEFT JOIN PersonCalendar AFAPC ON APC.PersonId = AFAPC.PersonId AND AFAPC.DayOff = 1 AND AFAPC.IsSeries = 1 AND AFAPC.TimeTypeId =APC.TimeTypeId AND AFAPC.ActualHours = APC.ActualHours AND ISNULL(APC.ApprovedBy, 0) = ISNULL(AFAPC.ApprovedBy, 0)
 											AND ((DATEPART(DW, APC.date) = 6 AND AFAPC.date = DATEADD(DD,3, APC.date) )
 													OR (DATEPART(DW, APC.date) = 2 AND AFAPC.date = DATEADD(DD, -3, APC.date))
 													OR AFAPC.date = DATEADD(DD,1, APC.date)
