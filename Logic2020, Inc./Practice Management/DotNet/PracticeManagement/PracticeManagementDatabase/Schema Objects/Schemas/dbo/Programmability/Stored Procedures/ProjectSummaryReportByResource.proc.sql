9a0ae69cebaf89a07645ccea982d1d2d58f77f51
SainathC <SainathC>
2012-03-07 07:15:00 -0500

    Comments - Defect Number: 2984; Defect RecordID: 2980; Server Address: services.logic2020.com; Server Port: 99; Database Name: Practice Management; Defect Summary: New Time Entry Reporting.; Defect Description: From: Jason Hong-Turney [mailto:jasonht@logic2020.com] Sent: Tuesday, February 28, 2012 12:22 PM To: MadhanA (madhana@ggktech.com) Cc: logic2020@ggktech.com; Matt Reilly Subject: Time Entry reporting changes
    Hi Madhan 
    
    Ive redesigned the Time Entry reporting to reflect the changes were making in Time Entry. My goal is to make the report output simpler, especially for Excel exports. I also tried to add some information and graphs that will make it the time entries were capturing more relevant to our business, and that will be easier to get summary/high-level data from. Ive included placeholders for PDF exports, but these can be worked on later. The Excel reports will be more important initially.
    
    Begin by looking at the Time Period Summary image (it uses the correct top selection bar text, for example) before looking at the other two images. The Time Period Summary and Project Summary are somewhat similar in design, while the Person Detail reporting is more specific to the information it contains.
    
    Please look at these and let me know what questions you have and/or what I need to clarify. I dont expect the team to work on these changes until the Time Entry changes I sent earlier and the Time Off functionality changes are completed. In the meantime, it should give you and I a chance to get this as clear as we can.
    
    Jason Hong-Turney // Logic20/20 // jasonht@logic2020.com
    t.206-576-0472 // c. 425-390-4664
    
    From: MadhanA [mailto:madhana@ggktech.com]
    Sent: Thursday, March 01, 2012 9:33 AM
    To: Jason Hong-Turney
    Cc: logic2020@ggktech.com; Matt Reilly
    Subject: RE: Time Entry reporting changes
    
    Hi Jason,
    
    Can you please provide your answers to the below questions.
    
    Time Period Summary Report-> B vs NB Graph %:
    
    1. we assumed that the Exclude Admin group means the time entries entered in Administrative section should be excluded. Please let us know if we are wrong.
    [Jason]  What I intended was to exclude the time entries from people in the Administrative practice (the internal employees here).
    
    Project Summary Report:
    
    1. As per existing functionality, person may be assigned to a milestone/Project with multiple roles. In that scenario, How do you want us to handle(Like showing all roles in project Role column as comma separated list or Showing only the highest role in the ladder?
    [Jason] Show only the highest role for now. I plan to change this in the future, to better align the Team Structure options with the role options.
    2. There is no date range selector in this Report, so we assumed that the date range should be the project start date and end date. Please let us know if we are wrong.
    [Jason] Theres no date range because the report will just look at the Project overall, so I dont want any date filtering options at this point. Were going to be moving away from the concept of milestones and people being linked at that level. Resources will simply be linked to the Project. Were still working on these parts and thinking through how calculations, etc, are affected.
    --
    Thanks & Regards,
    Madhan Annabemoju / GGK Technologies
    
    From: Jason Hong-Turney [mailto:jasonht@logic2020.com]
    Sent: Friday, March 02, 2012 6:53 AM
    To: MadhanA
    Cc: logic2020@ggktech.com
    Subject: RE: Time Entry reporting changes
    
    Hi Madhan  see answers below!
    
    Jason Hong-Turney // Logic20/20 // jasonht@logic2020.com
    t.206-576-0472 // c. 425-390-4664
    
    Hi Jason,
    
    Can you please provide your answers to the below questions.
    
    Questions:
    Project Summary Report:
    
    1) As per the new time entry functionality user is able to enter time for a project outside its start date and end date. In this case do we need to show those time entries?
    [Jason] Yes, any time that is applied towards a Project should be shown.
    2) In the same way the person who

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/ProjectSummaryReportByResource.proc.sql
@@ -0,0 +1,80 @@
+﻿CREATE PROCEDURE [dbo].[ProjectSummaryReportByResource]
+(
+	@ProjectId INT,
+	@PersonRoleIds NVARCHAR(MAX) = NULL,
+	@OrderByCerteria NVARCHAR(20) = 'resource'-- resource,personrole,total
+)
+AS
+BEGIN
+
+	--Presently @PersonRoleIds is not used as personrole is based on milestone. After the personrole is linked to project we need to include  @PersonRoleIds.
+	DECLARE @MinimumTimeEntryDate DATETIME,@MaximumTimeEntryDate DATETIME,@GroupByCerteria NVARCHAR(20),@DaysDiff INT 
+
+	SELECT @MinimumTimeEntryDate = MIN(TE.ChargeCodeDate), 
+		   @MaximumTimeEntryDate = MAX(TE.ChargeCodeDate) 
+	FROM dbo.TimeEntry TE
+	INNER JOIN dbo.ChargeCode CC ON CC.Id = TE.ChargeCodeId 
+	WHERE ProjectId = @ProjectId
+
+	SET @DaysDiff = DATEDIFF(dd,@MinimumTimeEntryDate,@MaximumTimeEntryDate)
+	IF(@DaysDiff <= 7 )
+	BEGIN
+		SET @GroupByCerteria = 'day'
+	END
+	ELSE IF(@DaysDiff > 7 AND @DaysDiff <= 31 )
+	BEGIN
+		SET @GroupByCerteria = 'week'
+	END
+	ELSE IF(@DaysDiff > 31 AND @DaysDiff <= 365 )
+	BEGIN
+		SET @GroupByCerteria = 'month'
+	END
+	ELSE 
+	BEGIN
+		SET @GroupByCerteria = 'year'
+	END
+	
+	DECLARE @PersonRoleIdsTable TABLE(ID INT)
+	INSERT INTO @PersonRoleIdsTable
+	SELECT ResultId 
+	FROM [dbo].[ConvertStringListIntoTable] (@PersonRoleIds)
+
+	SELECT P.PersonId,
+	P.LastName + ', ' + P.FirstName AS PersonName,
+	Data.IsChargeable,
+	Data.TotalHours,
+	Data.StartDate,
+	GroupByCerteria
+
+	FROM 
+	(
+	SELECT TE.PersonId,
+	CC.ProjectId,
+	TEH.IsChargeable,
+	Round(SUM(TEH.ActualHours),2) AS TotalHours,
+	CASE WHEN @GroupByCerteria = 'day' THEN TE.ChargeCodeDate -- date
+			WHEN @GroupByCerteria = 'week' THEN (TE.ChargeCodeDate - (DATEPART(dw,TE.ChargeCodeDate) -1 ))  --week startdate 
+			WHEN @GroupByCerteria = 'month' THEN TE.ChargeCodeDate - (DAY(TE.ChargeCodeDate)-1)  -- month StartDate
+			WHEN @GroupByCerteria = 'year' THEN TE.ChargeCodeDate - (DATEPART(DAYOFYEAR,TE.ChargeCodeDate)-1)-- year StartDate
+	END as StartDate,
+	@GroupByCerteria AS GroupByCerteria
+	FROM dbo.TimeEntry TE
+	INNER JOIN dbo.TimeEntryHours TEH ON TEH.TimeEntryId = TE.TimeEntryId 
+	INNER JOIN dbo.ChargeCode CC ON CC.Id = TE.ChargeCodeId AND CC.ProjectId = @ProjectId
+	GROUP BY TE.PersonId,
+			 CC.ProjectId,
+			TEH.IsChargeable,
+			CASE WHEN @GroupByCerteria = 'day' THEN TE.ChargeCodeDate -- date
+				WHEN @GroupByCerteria = 'week' THEN (TE.ChargeCodeDate - (DATEPART(dw,TE.ChargeCodeDate) -1 ))  --week startdate 
+				WHEN @GroupByCerteria = 'month' THEN TE.ChargeCodeDate - (DAY(TE.ChargeCodeDate)-1)  -- month StartDate
+				WHEN @GroupByCerteria = 'year' THEN TE.ChargeCodeDate - (DATEPART(DAYOFYEAR,TE.ChargeCodeDate)-1)-- year StartDate
+			END
+			)Data
+	INNER JOIN dbo.Person P ON P.PersonId = Data.PersonId 
+	INNER JOIN dbo.Project Pro ON Pro.ProjectId = Data.ProjectId 
+
+	ORDER BY CASE WHEN @OrderByCerteria = 'resource' THEN 2
+					WHEN @OrderByCerteria = 'total' THEN 4
+			END
+
+END
