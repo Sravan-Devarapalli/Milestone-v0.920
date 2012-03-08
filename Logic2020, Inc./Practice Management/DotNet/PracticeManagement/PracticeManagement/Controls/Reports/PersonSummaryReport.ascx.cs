14499549b55827f1078b10ae18991b6a20b29831
ThulasiramP <ThulasiramP>
2012-03-08 06:24:00 -0500

    Comments - Defect Number: 2983; Defect RecordID: 2979; Server Address: services.logic2020.com; Server Port: 99; Database Name: Practice Management; Defect Summary: Time->Calendar:New Time off/ PTO/Calendar functionality; Defect Description: From: Jason Hong-Turney [mailto:jasonht@logic2020.com] Sent: Friday, February 10, 2012 12:13 PM To: MadhanA; Matt Reilly Cc: ''Logic 20/20 Core Team'' Subject: RE: Time tracking Feature Status update.
    Hi Madhan 
    
    Finally, unrelated to the Time Entry page, but related to PTO/Calendar functionality.
     See attached mockup for changes to time off entry that allow for entering and editing a range of time, as well as a change to holiday substitution (replaces Floating Holiday).
    
    Thank you!
    
    Jason Hong-Turney // Logic20/20 // jasonht@logic2020.com
    t.206-576-0472 // c. 425-390-4664

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/Controls/Reports/PersonSummaryReport.ascx.cs
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/Controls/Reports/PersonSummaryReport.ascx.cs
@@ -4,14 +4,22 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using DataTransferObjects.Reports;
 
 namespace PraticeManagement.Controls.Reports
 {
     public partial class PersonSummaryReport : System.Web.UI.UserControl
     {
+       
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+
+        public void DatabindRepepeaterSummary(List<TimeEntriesGroupByClientAndProject> timeEntriesGroupByClientAndProjectList)
+        {
+            repSummary.DataSource = timeEntriesGroupByClientAndProjectList;
+            repSummary.DataBind();
         }
     }
 }
