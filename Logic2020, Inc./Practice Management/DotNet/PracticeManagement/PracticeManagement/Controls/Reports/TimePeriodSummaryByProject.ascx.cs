c7e2302078ee6e5b24bdf4a43b877871aae85aa5
SainathC <SainathC>
2012-03-27 10:29:00 -0500

    Comments - Defect Number: 2984; Defect RecordID: 2980; Server Address: services.logic2020.com; Server Port: 99; Database Name: Practice Management; Defect Summary: New Time Entry Reporting.; Defect Description: From: Jason Hong-Turney [mailto:jasonht@logic2020.com] Sent: Tuesday, February 28, 2012 12:22 PM To: MadhanA (madhana@ggktech.com) Cc: logic2020@ggktech.com; Matt Reilly Subject: Time Entry reporting changes
    Hi Madhan 
    
    Ive redesigned the Time Entry reporting to reflect the changes were making in Time Entry. My goal is to make the report output simpler, especially for Excel exports. I also tried to add some information and graphs that will make it the time entries were capturing more relevant to our business, and that will be easier to get summary/high-level data from. Ive included placeholders for PDF exports, but these can be worked on later. The Excel reports will be more important initially.
    
    Begin by looking at the Time Period Summary image (it uses the correct top selection bar text, for example) before looking at the other two images. The Time Period Summary and Project Summary are somewhat similar in design, while the Person Detail reporting is more specific to the information it contains.
    
    Please look at these and let me know what questions you have and/or what I need to clarify. I dont expect the team to work on these changes until the Time Entry changes I sent earlier and the Time Off functionality changes are completed. In the meantime, it should give you and I a chance to get this as clear as we can.
    
    Jason Hong-Turney // Logic20/20 // jasonht@logic2020.com
    t.206-576-0472 // c. 425-390-4664
    
    From: MadhanA [mailto:madhana@ggktech.com]
    Sent: Thursday, March 01, 2012 9:33 AM
    To: Jason Hong-Turney
    Cc: logic2020@ggktech.com; Matt Reilly
    Subject: RE: Time Entry reporting changes
    
    Hi Jason,
    
    Can you please provide your answers to the below questions.
    
    Time Period Summary Report-> B vs NB Graph %:
    
    1. we assumed that the Exclude Admin group means the time entries entered in Administrative section should be excluded. Please let us know if we are wrong.
    [Jason]  What I intended was to exclude the time entries from people in the Administrative practice (the internal employees here).
    
    Project Summary Report:
    
    1. As per existing functionality, person may be assigned to a milestone/Project with multiple roles. In that scenario, How do you want us to handle(Like showing all roles in project Role column as comma separated list or Showing only the highest role in the ladder?
    [Jason] Show only the highest role for now. I plan to change this in the future, to better align the Team Structure options with the role options.
    2. There is no date range selector in this Report, so we assumed that the date range should be the project start date and end date. Please let us know if we are wrong.
    [Jason] Theres no date range because the report will just look at the Project overall, so I dont want any date filtering options at this point. Were going to be moving away from the concept of milestones and people being linked at that level. Resources will simply be linked to the Project. Were still working on these parts and thinking through how calculations, etc, are affected.
    --
    Thanks & Regards,
    Madhan Annabemoju / GGK Technologies
    
    From: Jason Hong-Turney [mailto:jasonht@logic2020.com]
    Sent: Friday, March 02, 2012 6:53 AM
    To: MadhanA
    Cc: logic2020@ggktech.com
    Subject: RE: Time Entry reporting changes
    
    Hi Madhan  see answers below!
    
    Jason Hong-Turney // Logic20/20 // jasonht@logic2020.com
    t.206-576-0472 // c. 425-390-4664
    
    Hi Jason,
    
    Can you please provide your answers to the below questions.
    
    Questions:
    Project Summary Report:
    
    1) As per the new time entry functionality user is able to enter time for a project outside its start date and end date. In this case do we need to show those time entries?
    [Jason] Yes, any time that is applied towards a Project should be shown.
    2) In the same way the person who

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/Controls/Reports/TimePeriodSummaryByProject.ascx.cs
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/Controls/Reports/TimePeriodSummaryByProject.ascx.cs
@@ -6,79 +6,185 @@ using System.Web.UI;
 using System.Web.UI.WebControls;
 using DataTransferObjects.Reports;
 using System.Web.UI.HtmlControls;
+using System.Text;
 
 namespace PraticeManagement.Controls.Reports
 {
     public partial class TimePeriodSummaryByProject : System.Web.UI.UserControl
     {
-         #region Constants
+        public List<ProjectLevelGroupedHours> TimeEntriesGroupByProject
+        {
+            get
+            {
+                return ViewState["TimeEntriesGroupByProject"] as List<ProjectLevelGroupedHours>;
+            }
+            set
+            {
+                ViewState["TimeEntriesGroupByProject"] = value;
+            }
+        }
 
-        private const string Repeater_ProjectHeaders = "repProjectHeaders";
-        private const string Repeater_ProjectHoursPerDay = "reProjectHoursPerDay";
-
-        #endregion
-
-        public Dictionary<DateTime, String> Dates { get; set; }
+        private PraticeManagement.Reporting.TimePeriodSummaryReport HostingPage
+        {
+            get { return ((PraticeManagement.Reporting.TimePeriodSummaryReport)Page); }
+        }
 
         protected void Page_Load(object sender, EventArgs e)
         {
-            extBillableNonBillableAndTotalExtender.ControlsToCheck = rbBillable.ClientID + ";" + rbCombined.ClientID + ";" + rbNonBillable.ClientID;
+
         }
 
-        protected void repProject_ItemDataBound(object sender, RepeaterItemEventArgs e)
+        protected string GetDoubleFormat(double value)
         {
-            //if (e.Item.ItemType == ListItemType.Header)
-            //{
-            //    var repProjectHeaders = e.Item.FindControl(Repeater_ProjectHeaders) as Repeater;
-            //    repProjectHeaders.DataSource = Dates;
-            //    repProjectHeaders.DataBind();
-            //    extBillableNonBillableAndTotalExtender.TargetControlsToCheck = string.Empty;
-            //}
-            //else if (e.Item.ItemType == ListItemType.Item || e.Item.ItemType == ListItemType.AlternatingItem)
-            //{
-            //    var repProjectHoursPerDay = e.Item.FindControl(Repeater_ProjectHoursPerDay) as Repeater;
-            //    var tdProjectTotalHours = e.Item.FindControl("tdProjectTotalHours") as HtmlTableCell;
-            //    extBillableNonBillableAndTotalExtender.TargetControlsToCheck += tdProjectTotalHours.ClientID + ";";
-
-            //    var projectHoursPerDay = new Dictionary<DateTime, GroupedHours>();
-            //    var groupedHours = ((ProjectLevelGroupedHours)e.Item.DataItem).GroupedHoursList;
-
-            //    foreach (var day in Dates)
-            //    {
-            //        projectHoursPerDay.Add(day.Key, GetHours(groupedHours, day));
-            //    }
-
-            //    repProjectHoursPerDay.DataSource = projectHoursPerDay;
-            //    repProjectHoursPerDay.DataBind();
-            //}
+            return value.ToString(Constants.Formatting.DoubleValue);
         }
 
-        protected void reProjectHoursPerDay_OnItemDataBound(object sender, RepeaterItemEventArgs e)
+        protected string GetCurrencyFormat(double value)
         {
+            return value > 0 ? value.ToString(Constants.Formatting.CurrencyFormat) : "$0";
+        }
 
-            if (e.Item.ItemType == ListItemType.Item || e.Item.ItemType == ListItemType.AlternatingItem)
+        protected string GetBillableValue(double billableValue, bool isFixedProject)
+        {
+            if (isFixedProject)
             {
-                var tdDayTotalHours = e.Item.FindControl("tdDayTotalHours") as HtmlTableCell;
-                extBillableNonBillableAndTotalExtender.TargetControlsToCheck += tdDayTotalHours.ClientID + ";";
+                return "Fixed";
+            }
+            else
+            {
+                return billableValue > 0 ? billableValue.ToString(Constants.Formatting.CurrencyFormat) : "$0";
             }
         }
 
-
-        public void DataBindProject(ProjectLevelGroupedHours[] reportData, Dictionary<DateTime, String> datesList)
+        protected string GetBillableSortValue(double billableValue, bool isFixedProject)
         {
-            Dates = datesList;
-            repProject.DataSource = reportData;
-            repProject.DataBind();
-        }
-
-        private GroupedHours GetHours(List<GroupedHours> groupedHours, KeyValuePair<DateTime, string> day)
-        {
-            if (groupedHours.Any(d => d.StartDate.Date == day.Key.Date))
+            if (isFixedProject)
             {
-                return groupedHours.First(gh => gh.StartDate.Date == day.Key.Date);
+                return "-1";
             }
-
-            return new GroupedHours();
+            else
+            {
+                return billableValue > 0 ? billableValue.ToString() : "0";
+            }
         }
+
+        protected string GetVarianceSortValue(string variance)
+        {
+            if (variance.Equals("N/A"))
+            {
+                return int.MinValue.ToString();
+            }
+            return variance;
+        }
+
+        protected void btnExportToExcel_OnClick(object sender, EventArgs e)
+        {
+
+            if (HostingPage.StartDate.HasValue && HostingPage.EndDate.HasValue)
+            {
+
+                StringBuilder sb = new StringBuilder();
+                sb.Append("TimePeriod_ByProject Report");
+                sb.Append("\t");
+                sb.AppendLine();
+                sb.Append(HostingPage.StartDate.Value.ToString("MM/dd/yyyy") + " - " + HostingPage.EndDate.Value.ToString("MM/dd/yyyy"));
+                sb.Append("\t");
+                sb.AppendLine();
+                sb.AppendLine();
+
+                if (TimeEntriesGroupByProject.Count > 0)
+                {
+                    //Header
+                    sb.Append("Account");
+                    sb.Append("\t");
+                    sb.Append("Account Name");
+                    sb.Append("\t");
+                    sb.Append("Business Unit");
+                    sb.Append("\t");
+                    sb.Append("Business Unit Name");
+                    sb.Append("\t");
+                    sb.Append("Project");
+                    sb.Append("\t");
+                    sb.Append("Project Name");
+                    sb.Append("\t");
+                    sb.Append("Status");
+                    sb.Append("\t");
+                    sb.Append("Billable");
+                    sb.Append("\t");
+                    sb.Append("Non-Billable");
+                    sb.Append("\t");
+                    sb.Append("Total");
+                    sb.Append("\t");
+                    sb.Append("Value");
+                    sb.Append("\t");
+                    sb.Append("Project Variance (in Hours)");
+                    sb.Append("\t");
+                    sb.AppendLine();
+
+                    //Data
+                    foreach (var projectLevelGroupedHours in TimeEntriesGroupByProject)
+                    {
+                        sb.Append(projectLevelGroupedHours.Project.Client.Code);
+                        sb.Append("\t");
+                        sb.Append(projectLevelGroupedHours.Project.Client.Name);
+                        sb.Append("\t");
+                        sb.Append(projectLevelGroupedHours.Project.Group.Code);
+                        sb.Append("\t");
+                        sb.Append(projectLevelGroupedHours.Project.Group.Name);
+                        sb.Append("\t");
+                        sb.Append(projectLevelGroupedHours.Project.ProjectNumber);
+                        sb.Append("\t");
+                        sb.Append(projectLevelGroupedHours.Project.Name);
+                        sb.Append("\t");
+                        sb.Append(projectLevelGroupedHours.Project.Status.Name);
+                        sb.Append("\t");
+                        sb.Append(GetDoubleFormat(projectLevelGroupedHours.BillableHours));
+                        sb.Append("\t");
+                        sb.Append(GetDoubleFormat(projectLevelGroupedHours.NonBillableHours));
+                        sb.Append("\t");
+                        sb.Append(GetDoubleFormat(projectLevelGroupedHours.TotalHours));
+                        sb.Append("\t");
+                        sb.Append(GetBillableValue(projectLevelGroupedHours.BillableValue, projectLevelGroupedHours.IsFixedProject));
+                        sb.Append("\t");
+                        sb.Append(projectLevelGroupedHours.Variance);
+                        sb.Append("\t");
+                        sb.AppendLine();
+                    }
+
+                }
+                else
+                {
+                    sb.Append("There are no Time Entries towards this range selected.");
+                }
+                //“TimePeriod_ByProject_Excel.xls”.  
+                var filename = "TimePeriod_ByProject_Excel.xls";
+                GridViewExportUtil.Export(filename, sb);
+            }
+        }
+
+        protected void btnExportToPDF_OnClick(object sender, EventArgs e)
+        {
+
+        }
+
+        public void DataBindProject(ProjectLevelGroupedHours[] reportData)
+        {
+            TimeEntriesGroupByProject = reportData.ToList();
+            if (TimeEntriesGroupByProject.Count > 0)
+            {
+                HostingPage.SetGraphVisibility(true);
+                divEmptyMessage.Style["display"] = "none";
+                repProject.Visible = true;
+                repProject.DataSource = reportData;
+                repProject.DataBind();
+            }
+            else
+            {
+                divEmptyMessage.Style["display"] = "";
+                repProject.Visible = false;
+                HostingPage.SetGraphVisibility(false);
+            }
+        }
+
     }
 }
