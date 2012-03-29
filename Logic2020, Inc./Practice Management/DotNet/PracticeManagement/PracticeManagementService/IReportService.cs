a53c28e558699a147861d6e0f6e34e73a13dbe3e
ThulasiramP <ThulasiramP>
2012-03-29 10:12:00 -0500

    none

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementService/IReportService.cs
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementService/IReportService.cs
@@ -32,7 +32,7 @@ namespace PracticeManagementService
         List<WorkTypeLevelGroupedHours> TimePeriodSummaryReportByWorkType(DateTime startDate, DateTime endDate, string timeTypeCategoryIds, string orderByCerteria);
 
         [OperationContract]
-        List<PersonLevelGroupedHours> ProjectSummaryReportByResource(string projectNumber);
+        List<PersonLevelGroupedHours> ProjectSummaryReportByResource(string projectNumber, int? mileStoneId);
 
         [OperationContract]
         List<WorkTypeLevelGroupedHours> ProjectSummaryReportByWorkType(string projectNumber, string timeTypeCategoryIds, string orderByCerteria);
@@ -42,6 +42,9 @@ namespace PracticeManagementService
 
         [OperationContract]
         List<Project> ProjectSearchByName(string name);
+
+        [OperationContract]
+        List<Milestone> GetMilestonesForProject(string projectNumber);
 
     }
 }
