49edcd959e683011ae3d5b57a6079c149dd554a5
SainathC <SainathC>
2012-03-29 01:57:00 -0500

    Comments - Defect Number: 2984; Defect RecordID: 2980; Server Address: services.logic2020.com; Server Port: 99; Database Name: Practice Management; Defect Summary: New Time Entry Reporting.; Defect Description: From: Jason Hong-Turney [mailto:jasonht@logic2020.com] Sent: Tuesday, February 28, 2012 12:22 PM To: MadhanA (madhana@ggktech.com) Cc: logic2020@ggktech.com; Matt Reilly Subject: Time Entry reporting changes
    Hi Madhan 
    
    Ive redesigned the Time Entry reporting to reflect the changes were making in Time Entry. My goal is to make the report output simpler, especially for Excel exports. I also tried to add some information and graphs that will make it the time entries were capturing more relevant to our business, and that will be easier to get summary/high-level data from. Ive included placeholders for PDF exports, but these can be worked on later. The Excel reports will be more important initially.
    
    Begin by looking at the Time Period Summary image (it uses the correct top selection bar text, for example) before looking at the other two images. The Time Period Summary and Project Summary are somewhat similar in design, while the Person Detail reporting is more specific to the information it contains.
    
    Please look at these and let me know what questions you have and/or what I need to clarify. I dont expect the team to work on these changes until the Time Entry changes I sent earlier and the Time Off functionality changes are completed. In the meantime, it should give you and I a chance to get this as clear as we can.
    
    Jason Hong-Turney // Logic20/20 // jasonht@logic2020.com
    t.206-576-0472 // c. 425-390-4664
    
    From: MadhanA [mailto:madhana@ggktech.com]
    Sent: Thursday, March 01, 2012 9:33 AM
    To: Jason Hong-Turney
    Cc: logic2020@ggktech.com; Matt Reilly
    Subject: RE: Time Entry reporting changes
    
    Hi Jason,
    
    Can you please provide your answers to the below questions.
    
    Time Period Summary Report-> B vs NB Graph %:
    
    1. we assumed that the Exclude Admin group means the time entries entered in Administrative section should be excluded. Please let us know if we are wrong.
    [Jason]  What I intended was to exclude the time entries from people in the Administrative practice (the internal employees here).
    
    Project Summary Report:
    
    1. As per existing functionality, person may be assigned to a milestone/Project with multiple roles. In that scenario, How do you want us to handle(Like showing all roles in project Role column as comma separated list or Showing only the highest role in the ladder?
    [Jason] Show only the highest role for now. I plan to change this in the future, to better align the Team Structure options with the role options.
    2. There is no date range selector in this Report, so we assumed that the date range should be the project start date and end date. Please let us know if we are wrong.
    [Jason] Theres no date range because the report will just look at the Project overall, so I dont want any date filtering options at this point. Were going to be moving away from the concept of milestones and people being linked at that level. Resources will simply be linked to the Project. Were still working on these parts and thinking through how calculations, etc, are affected.
    --
    Thanks & Regards,
    Madhan Annabemoju / GGK Technologies
    
    From: Jason Hong-Turney [mailto:jasonht@logic2020.com]
    Sent: Friday, March 02, 2012 6:53 AM
    To: MadhanA
    Cc: logic2020@ggktech.com
    Subject: RE: Time Entry reporting changes
    
    Hi Madhan  see answers below!
    
    Jason Hong-Turney // Logic20/20 // jasonht@logic2020.com
    t.206-576-0472 // c. 425-390-4664
    
    Hi Jason,
    
    Can you please provide your answers to the below questions.
    
    Questions:
    Project Summary Report:
    
    1) As per the new time entry functionality user is able to enter time for a project outside its start date and end date. In this case do we need to show those time entries?
    [Jason] Yes, any time that is applied towards a Project should be shown.
    2) In the same way the person who

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/TimePeriodSummaryReportByProject.proc.sql
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/TimePeriodSummaryReportByProject.proc.sql
@@ -1,8 +1,8 @@
 ﻿-- =========================================================================
 -- Author:		Sainath.CH
 -- Create date: 03-05-2012
--- Updated by : Thulasiram.P
--- Update Date: 03-15-2012
+-- Updated by : Sainath.CH
+-- Update Date: 03-29-2012
 -- Description:  Time Entries grouped by Project for a particular period.
 -- =========================================================================
 CREATE PROCEDURE [dbo].[TimePeriodSummaryReportByProject]
@@ -10,33 +10,15 @@ CREATE PROCEDURE [dbo].[TimePeriodSummaryReportByProject]
 	@StartDate DATETIME,
 	@EndDate   DATETIME,
 	@ClientIds NVARCHAR(MAX) = NULL,
-	@PersonStatusIds NVARCHAR(MAX) = NULL,
-	@OrderByCerteria NVARCHAR(20) = 'client' -- client,projectStatus,total
+	@PersonStatusIds NVARCHAR(MAX) = NULL
 )
 AS
 BEGIN
 
+	DECLARE @Today DATETIME
 	SET @StartDate = CONVERT(DATE,@StartDate)
 	SET @EndDate = CONVERT(DATE,@EndDate)
-
-	DECLARE @GroupByCerteria NVARCHAR(20),@DaysDiff INT 
-	SET @DaysDiff = DATEDIFF(dd,@StartDate,@EndDate)
-	IF(@DaysDiff <= 7 )
-	BEGIN
-		SET @GroupByCerteria = 'day'
-	END
-	ELSE IF(@DaysDiff > 7 AND @DaysDiff <= 31  )
-	BEGIN
-		SET @GroupByCerteria = 'week'
-	END
-	ELSE IF(@DaysDiff > 31 AND @DaysDiff <= 365  )
-	BEGIN
-		SET @GroupByCerteria = 'month'
-	END
-	ELSE 
-	BEGIN
-		SET @GroupByCerteria = 'year'
-	END
+	SET @Today = dbo.GettingPMTime(GETUTCDATE())
 
 	DECLARE @ClientIdsTable TABLE(ID INT)
 	INSERT INTO @ClientIdsTable
@@ -48,51 +30,84 @@ BEGIN
 	SELECT ResultId 
 	FROM [dbo].[ConvertStringListIntoTable] (@PersonStatusIds)
 
+	;WITH PersonProjectBillrates AS
+	(
+	  SELECT MP.PersonId,
+			 M.ProjectId,
+			 AVG(ISNULL(MPE.Amount,0)) AS AvgBillRate,
+			 C.Date
+	  FROM  dbo.MilestonePersonEntry AS MPE 
+	  INNER JOIN dbo.MilestonePerson AS MP ON MP.MilestonePersonId = MPE.MilestonePersonId
+	  INNER JOIN dbo.Milestone AS M ON M.MilestoneId = MP.MilestoneId
+	  INNER JOIN dbo.Calendar AS C ON C.Date BETWEEN MPE.StartDate AND MPE.EndDate
+	  INNER JOIN dbo.Person AS P ON MP.PersonId = P.PersonId 
+	  GROUP BY MP.PersonId,M.ProjectId,C.Date
+	)
+	,ProjectForeCastedHoursUntilToday AS
+	(
+	   SELECT M.ProjectId,
+			 SUM(MPE.HoursPerDay) AS ForecastedHoursUntilToday,
+			 MIN(CAST(M.IsHourlyAmount AS INT)) AS IsFixedProject --if return 0 then fixed project  else if return 1 Hourly project
+	  FROM  dbo.MilestonePersonEntry AS MPE 
+	  INNER JOIN dbo.MilestonePerson AS MP ON MP.MilestonePersonId = MPE.MilestonePersonId
+	  INNER JOIN dbo.Milestone AS M ON M.MilestoneId = MP.MilestoneId
+	  INNER JOIN dbo.v_PersonCalendar PC ON PC.PersonId = MP.PersonId 
+											AND PC.Date BETWEEN MPE.StartDate AND MPE.EndDate
+											AND PC.Date BETWEEN @StartDate AND CASE WHEN @EndDate > @Today THEN @Today ELSE  @EndDate END
+											AND ( 
+													(PC.CompanyDayOff = 0 AND ISNULL(PC.TimeTypeId, 0) != dbo.GetHolidayTimeTypeId()) 
+													OR ( PC.CompanyDayOff =1 AND PC.SubstituteDate IS NOT NULL)
+												)
+	  GROUP BY M.ProjectId 
+	)
 
-SELECT		Data.ClientId,
+
+	SELECT  C.ClientId,
 			C.Name AS ClientName,
-			Data.ProjectId,
+			C.Code AS ClientCode,
+			PG.Name AS GroupName,
+			PG.Code AS GroupCode,
+			P.ProjectId,
 			P.Name AS ProjectName,
 			P.ProjectNumber,
-			Data.IsChargeable,
+			PS.ProjectStatusId,
 			PS.Name AS ProjectStatusName,
 			BillableHours,
 			NonBillableHours,
-			Data.StartDate,
-			GroupByCerteria,
-			P.ProjectNumber,
-			PS.ProjectStatusId
-FROM(
-	SELECT  CC.ClientId,
-			CC.ProjectId,
-			TEH.IsChargeable,
-			Round(SUM(CASE WHEN TEH.IsChargeable = 1 THEN TEH.ActualHours ELSE 0 END),2) AS BillableHours,
-			Round(SUM(CASE WHEN TEH.IsChargeable = 0 THEN TEH.ActualHours ELSE 0 END),2) AS NonBillableHours,
-			CASE WHEN @GroupByCerteria = 'day' THEN TE.ChargeCodeDate -- date
-				WHEN @GroupByCerteria = 'week' THEN (TE.ChargeCodeDate - (DATEPART(dw,TE.ChargeCodeDate) -1 ))  --week startdate 
-				WHEN @GroupByCerteria = 'month' THEN TE.ChargeCodeDate - (DAY(TE.ChargeCodeDate)-1)  -- month StartDate
-				WHEN @GroupByCerteria = 'year' THEN TE.ChargeCodeDate - (DATEPART(DAYOFYEAR,TE.ChargeCodeDate)-1)-- year StartDate
-			END as StartDate,
-			@GroupByCerteria 'GroupByCerteria'
-	FROM dbo.TimeEntry TE
-		INNER JOIN dbo.TimeEntryHours TEH ON TEH.TimeEntryId = te.TimeEntryId AND TE.ChargeCodeDate BETWEEN @StartDate AND @EndDate 
-		INNER JOIN dbo.ChargeCode CC ON CC.Id = TE.ChargeCodeId AND (CC.ClientId IN (SELECT * FROM @ClientIdsTable) OR @ClientIds IS NULL)
-	GROUP BY CC.ClientId,
-		 	 CC.ProjectId,
-			 TEH.IsChargeable,
-			CASE WHEN @GroupByCerteria = 'day' THEN TE.ChargeCodeDate -- date
-				 WHEN @GroupByCerteria = 'week' THEN (TE.ChargeCodeDate - (DATEPART(dw,TE.ChargeCodeDate) -1 ))  --week startdate 
-				 WHEN @GroupByCerteria = 'month' THEN TE.ChargeCodeDate - (DAY(TE.ChargeCodeDate)-1)  -- month StartDate
-				 WHEN @GroupByCerteria = 'year' THEN TE.ChargeCodeDate - (DATEPART(DAYOFYEAR,TE.ChargeCodeDate)-1)-- year StartDate
-			END 
-	) Data
-	INNER JOIN dbo.Project P ON P.ProjectId = Data.ProjectId
-	INNER JOIN dbo.Client C ON C.ClientId = Data.ClientId 
-	INNER JOIN dbo.ProjectStatus PS ON PS.ProjectStatusId = P.ProjectStatusId AND (PS.ProjectStatusId IN (SELECT * FROM @PersonStatusIdsTable) OR @PersonStatusIds IS NULL)
-
-	ORDER BY CASE WHEN @OrderByCerteria = 'client' THEN 2
-				  WHEN @OrderByCerteria = 'projectStatus' THEN 7
-				  WHEN @OrderByCerteria = 'total' THEN 8
-		 	 END
+			BillableValue,
+			ISNULL(pfh.IsFixedProject,2) AS IsFixedProject, --if return 0 then fixed project else if return 1 Hourly project else if return 2 person not assigned to project.
+			ISNULL(pfh.ForecastedHoursUntilToday,0) AS ForecastedHoursUntilToday,
+			BillableHoursUntilToday,
+			TimeEntrySectionId
+	FROM(
+			SELECT  CC.ClientId,
+					CC.ProjectId,
+					CC.ProjectGroupId,
+					CC.TimeEntrySectionId,
+					Round(SUM(CASE WHEN TEH.IsChargeable = 1 THEN TEH.ActualHours ELSE 0 END),2) AS BillableHours,
+					Round(SUM(CASE WHEN TEH.IsChargeable = 0 THEN TEH.ActualHours ELSE 0 END),2) AS NonBillableHours,
+					ROUND(SUM(CASE WHEN (TEH.IsChargeable = 1 AND TE.ChargeCodeDate <= @Today) THEN TEH.ActualHours ELSE 0 END),2) AS BillableHoursUntilToday,
+					ROUND(SUM(ISNULL(ppbr.AvgBillRate,0) * ( CASE WHEN TEH.IsChargeable = 1 THEN TEH.ActualHours 
+																			ELSE 0	
+																			END
+																	)
+						),2) AS BillableValue
+			FROM dbo.TimeEntry TE
+				INNER JOIN dbo.TimeEntryHours TEH ON TEH.TimeEntryId = te.TimeEntryId AND TE.ChargeCodeDate BETWEEN @StartDate AND @EndDate 
+				INNER JOIN dbo.ChargeCode CC ON CC.Id = TE.ChargeCodeId AND (CC.ClientId IN (SELECT * FROM @ClientIdsTable) OR @ClientIds IS NULL)
+				INNER JOIN dbo.Person AS P ON P.PersonId = TE.PersonId 
+				LEFT JOIN PersonProjectBillrates ppbr ON ppbr.PersonId = P.PersonId AND TE.PersonId = ppbr.PersonId AND ppbr.ProjectId = CC.ProjectId AND ppbr.Date = TE.ChargeCodeDate
+			GROUP BY CC.TimeEntrySectionId,
+					 CC.ClientId,
+					 CC.ProjectGroupId,
+		 			 CC.ProjectId
+		) Data
+		INNER JOIN dbo.Project P ON P.ProjectId = Data.ProjectId
+		INNER JOIN dbo.Client C ON C.ClientId = Data.ClientId 
+		INNER JOIN dbo.ProjectStatus PS ON PS.ProjectStatusId = P.ProjectStatusId AND (PS.ProjectStatusId IN (SELECT * FROM @PersonStatusIdsTable) OR @PersonStatusIds IS NULL)
+		INNER JOIN dbo.ProjectGroup PG ON PG.ClientId = C.ClientId AND PG.GroupId = Data.ProjectGroupId
+		LEFT JOIN  ProjectForeCastedHoursUntilToday pfh ON pfh.ProjectId = P.ProjectId
+		ORDER BY TimeEntrySectionId,P.ProjectNumber
+	
 END
 	
