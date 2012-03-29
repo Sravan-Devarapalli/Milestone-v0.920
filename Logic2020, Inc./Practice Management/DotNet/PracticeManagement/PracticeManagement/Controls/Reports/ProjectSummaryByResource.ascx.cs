3c1e6451559c1a4ac7c7258d0ae0d7a6bd0d9304
ThulasiramP <ThulasiramP>
2012-03-29 10:12:00 -0500

    none

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/Controls/Reports/ProjectSummaryByResource.ascx.cs
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/Controls/Reports/ProjectSummaryByResource.ascx.cs
@@ -32,7 +32,7 @@ namespace PraticeManagement.Controls.Reports
 
         protected void Page_Load(object sender, EventArgs e)
         {
-            
+
         }
 
         protected void repResource_ItemDataBound(object sender, RepeaterItemEventArgs e)
@@ -50,15 +50,18 @@ namespace PraticeManagement.Controls.Reports
             if (reportData.Count() > 0)
             {
                 divEmptyMessage.Style["display"] = "none";
-                repResource.Visible =  true;
+                repResource.Visible = true;
                 repResource.DataSource = reportData;
                 repResource.DataBind();
+                PopulateHeaderSection(reportData.ToList());
             }
             else
             {
                 divEmptyMessage.Style["display"] = "";
                 repResource.Visible = false;
             }
+
+
         }
 
 
@@ -94,7 +97,7 @@ namespace PraticeManagement.Controls.Reports
             sb.Append("\t");
             sb.AppendLine();
 
-            var list = TimeEntriesGroupByPerson.OrderBy(p=>p.Person.PersonLastFirstName);
+            var list = TimeEntriesGroupByPerson.OrderBy(p => p.Person.PersonLastFirstName);
 
             //Data
             foreach (var byPerson in list)
@@ -108,8 +111,6 @@ namespace PraticeManagement.Controls.Reports
                 sb.Append(GetDoubleFormat(byPerson.NonBillableHours));
                 sb.Append("\t");
                 sb.Append(GetDoubleFormat(byPerson.TotalHours));
-                sb.Append("\t");
-                sb.Append(GetCurrencyFormat(byPerson.BillableValue, byPerson.IsPersonNotAssignedToFixedProject));
                 sb.Append("\t");
                 sb.Append(byPerson.Variance);
                 sb.Append("\t");
@@ -128,36 +129,64 @@ namespace PraticeManagement.Controls.Reports
             return value.ToString(Constants.Formatting.DoubleValue);
         }
 
-        protected string GetCurrencyFormat(double value, bool isNotFixed)
-        {
-            if (!isNotFixed)
-            {
-                return "Fixed";
-            }
-
-            return value > 0 ? value.ToString(Constants.Formatting.CurrencyFormat) : "$0";
-        }
-
-
-        protected string GetBillableSortValue(double billableValue, bool isPersonNotAssignedToFixedProject)
-        {
-            if (!isPersonNotAssignedToFixedProject)
-            {
-                return "-1";
-            }
-            else
-            {
-                return billableValue > 0 ? billableValue.ToString() : "0";
-            }
-        }
-
 
         protected void btnExportToPDF_OnClick(object sender, EventArgs e)
         {
 
         }
 
+        private void PopulateHeaderSection(List<PersonLevelGroupedHours> personLevelGroupedHoursList)
+        {
 
+            var project = ServiceCallers.Custom.Project(p => p.GetProjectShortByProjectNumber(HostingPage.ProjectNumber)); 
+
+            double billableHours = personLevelGroupedHoursList.Sum(p => p.BillableHours);
+            double nonBillableHours = personLevelGroupedHoursList.Sum(p => p.NonBillableHours);
+            double projectedHours = personLevelGroupedHoursList.Sum(p => p.ForecastedHours);
+
+            var billablePercent = 0;
+            var nonBillablePercent = 0;
+            if (billableHours != 0 || nonBillableHours != 0)
+            {
+                billablePercent = DataTransferObjects.Utils.Generic.GetBillablePercentage(billableHours, nonBillableHours);
+                nonBillablePercent = (100 - billablePercent);
+            }
+
+            ltrlAccount.Text = project.Client.Name;
+            ltrlBusinessUnit.Text = project.Group.Name;
+            ltrlProjectedHours.Text = projectedHours.ToString(Constants.Formatting.DoubleValue);
+            ltrlProjectName.Text = project.Name;
+            ltrlProjectNumber.Text = project.ProjectNumber;
+            ltrlProjectStatus.Text = project.Status.Name;
+            ltrlTotalHours.Text = (billableHours + nonBillableHours).ToString(Constants.Formatting.DoubleValueWithZeroPadding);
+            ltrlBillableHours.Text = billableHours.ToString(Constants.Formatting.DoubleValue);
+            ltrlNonBillableHours.Text = nonBillableHours.ToString(Constants.Formatting.DoubleValue);
+            ltrlBillablePercent.Text = billablePercent.ToString();
+            ltrlNonBillablePercent.Text = nonBillablePercent.ToString();
+
+            if (billablePercent == 0 && nonBillablePercent == 0)
+            {
+                trBillable.Height = "1px";
+                trNonBillable.Height = "1px";
+            }
+            else if (billablePercent == 100)
+            {
+                trBillable.Height = "80px";
+                trNonBillable.Height = "1px";
+            }
+            else if (billablePercent == 0 && nonBillablePercent == 100)
+            {
+                trBillable.Height = "1px";
+                trNonBillable.Height = "80px";
+            }
+            else
+            {
+                int billablebarHeight = (int)(((float)80 / (float)100) * billablePercent);
+                trBillable.Height = billablebarHeight.ToString() + "px";
+                trNonBillable.Height = (80 - billablebarHeight).ToString() + "px";
+            }
+
+        }
 
     }
 }
