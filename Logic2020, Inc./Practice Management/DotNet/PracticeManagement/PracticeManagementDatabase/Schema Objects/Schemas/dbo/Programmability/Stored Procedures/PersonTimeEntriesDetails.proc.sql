1ddffa3a4af2cf030601968bcb7be5b28d55eb54
SainathC <SainathC>
2012-03-30 03:10:00 -0500

    Comments - Defect Number: 2984; Defect RecordID: 2980; Server Address: services.logic2020.com; Server Port: 99; Database Name: Practice Management; Defect Summary: New Time Entry Reporting.; Defect Description: From: Jason Hong-Turney [mailto:jasonht@logic2020.com] Sent: Tuesday, February 28, 2012 12:22 PM To: MadhanA (madhana@ggktech.com) Cc: logic2020@ggktech.com; Matt Reilly Subject: Time Entry reporting changes
    Hi Madhan 
    
    Ive redesigned the Time Entry reporting to reflect the changes were making in Time Entry. My goal is to make the report output simpler, especially for Excel exports. I also tried to add some information and graphs that will make it the time entries were capturing more relevant to our business, and that will be easier to get summary/high-level data from. Ive included placeholders for PDF exports, but these can be worked on later. The Excel reports will be more important initially.
    
    Begin by looking at the Time Period Summary image (it uses the correct top selection bar text, for example) before looking at the other two images. The Time Period Summary and Project Summary are somewhat similar in design, while the Person Detail reporting is more specific to the information it contains.
    
    Please look at these and let me know what questions you have and/or what I need to clarify. I dont expect the team to work on these changes until the Time Entry changes I sent earlier and the Time Off functionality changes are completed. In the meantime, it should give you and I a chance to get this as clear as we can.
    
    Jason Hong-Turney // Logic20/20 // jasonht@logic2020.com
    t.206-576-0472 // c. 425-390-4664
    
    From: MadhanA [mailto:madhana@ggktech.com]
    Sent: Thursday, March 01, 2012 9:33 AM
    To: Jason Hong-Turney
    Cc: logic2020@ggktech.com; Matt Reilly
    Subject: RE: Time Entry reporting changes
    
    Hi Jason,
    
    Can you please provide your answers to the below questions.
    
    Time Period Summary Report-> B vs NB Graph %:
    
    1. we assumed that the Exclude Admin group means the time entries entered in Administrative section should be excluded. Please let us know if we are wrong.
    [Jason]  What I intended was to exclude the time entries from people in the Administrative practice (the internal employees here).
    
    Project Summary Report:
    
    1. As per existing functionality, person may be assigned to a milestone/Project with multiple roles. In that scenario, How do you want us to handle(Like showing all roles in project Role column as comma separated list or Showing only the highest role in the ladder?
    [Jason] Show only the highest role for now. I plan to change this in the future, to better align the Team Structure options with the role options.
    2. There is no date range selector in this Report, so we assumed that the date range should be the project start date and end date. Please let us know if we are wrong.
    [Jason] Theres no date range because the report will just look at the Project overall, so I dont want any date filtering options at this point. Were going to be moving away from the concept of milestones and people being linked at that level. Resources will simply be linked to the Project. Were still working on these parts and thinking through how calculations, etc, are affected.
    --
    Thanks & Regards,
    Madhan Annabemoju / GGK Technologies
    
    From: Jason Hong-Turney [mailto:jasonht@logic2020.com]
    Sent: Friday, March 02, 2012 6:53 AM
    To: MadhanA
    Cc: logic2020@ggktech.com
    Subject: RE: Time Entry reporting changes
    
    Hi Madhan  see answers below!
    
    Jason Hong-Turney // Logic20/20 // jasonht@logic2020.com
    t.206-576-0472 // c. 425-390-4664
    
    Hi Jason,
    
    Can you please provide your answers to the below questions.
    
    Questions:
    Project Summary Report:
    
    1) As per the new time entry functionality user is able to enter time for a project outside its start date and end date. In this case do we need to show those time entries?
    [Jason] Yes, any time that is applied towards a Project should be shown.
    2) In the same way the person who

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/PersonTimeEntriesDetails.proc.sql
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/PersonTimeEntriesDetails.proc.sql
@@ -3,7 +3,7 @@
 -- Create date: 03-05-2012
 -- Description: Person TimeEntries Details By Period.
 -- Updated by : Sainath.CH
--- Update Date: 03-29-2012
+-- Update Date: 03-30-2012
 -- =============================================
 CREATE PROCEDURE [dbo].[PersonTimeEntriesDetails]
 (
@@ -23,51 +23,78 @@ BEGIN
 
 	SET @ORTTimeTypeId = dbo.GetORTTimeTypeId()
 	
+	;WITH PersonDayWiseByProjectsBillableTypes AS
+	(
+	  SELECT M.ProjectId,C.Date,
+			MIN(CAST(M.IsHourlyAmount AS INT)) MinimumValue,
+			MAX(CAST(M.IsHourlyAmount AS INT)) MaximumValue
+	  FROM  dbo.MilestonePersonEntry AS MPE 
+	  INNER JOIN dbo.MilestonePerson AS MP ON MP.MilestonePersonId = MPE.MilestonePersonId
+	  INNER JOIN dbo.Milestone AS M ON M.MilestoneId = MP.MilestoneId
+	  INNER JOIN dbo.Calendar AS C ON C.Date BETWEEN MPE.StartDate AND MPE.EndDate 
+									AND C.Date BETWEEN @StartDate AND @EndDate 
+	  WHERE MP.PersonId = @PersonId 
+			AND M.StartDate BETWEEN @StartDate AND @EndDate 
+			AND M.ProjectedDeliveryDate  BETWEEN @StartDate AND @EndDate 
+	  GROUP BY M.ProjectId,C.Date
+	)
 
-	  SELECT C.Code AS ClientCode,
-			 C.ClientId,
-			 PRO.ProjectId,
-			 PRO.Name AS ProjectName,
-			 PRO.ProjectNumber,
-			 C.Name AS  ClientName,
-			 TE.ChargeCodeDate,
-			 TT.Name AS TimeTypeName,
-			 TT.TimeTypeId,
-			 (CASE  WHEN TT.TimeTypeId = @ORTTimeTypeId THEN TE.Note + dbo.GetApprovedByName(TE.ChargeCodeDate,@ORTTimeTypeId,@PersonId)
+	  SELECT    C.ClientId,
+				C.Name AS  ClientName,
+				C.Code AS ClientCode,
+				BU.Name AS GroupName,
+				BU.Code AS GroupCode,
+				PRO.ProjectId,
+				PRO.Name AS ProjectName,
+				PRO.ProjectNumber,
+				(CASE WHEN (CC.TimeEntrySectionId <> 1 ) THEN '' ELSE PS.Name  END ) AS ProjectStatusName,
+				TT.TimeTypeId,
+				TT.Name  AS TimeTypeName,
+				TT.Code AS TimeTypeCode,
+				TE.ChargeCodeDate,
+				TE.Note,
+			(CASE WHEN (PDBR.MinimumValue IS NULL OR CC.TimeEntrySectionId <> 1 ) THEN '' 
+			WHEN (PDBR.MinimumValue = PDBR.MaximumValue AND PDBR.MinimumValue = 0) THEN 'Fixed'
+			WHEN (PDBR.MinimumValue = PDBR.MaximumValue AND PDBR.MinimumValue = 1) THEN 'Hourly'
+			ELSE 'Both' END) AS BillingType,
+  		   (CASE  WHEN TT.TimeTypeId = @ORTTimeTypeId THEN TE.Note + dbo.GetApprovedByName(TE.ChargeCodeDate,@ORTTimeTypeId,@PersonId)
 				   ELSE TE.Note
-				   END) AS Note
-			 ,
+				   END) AS Note,
 			 ROUND(SUM(CASE WHEN TEH.IsChargeable = 1 THEN TEH.ActualHours 
 					  ELSE 0 
 				  END),2) AS BillableHours,
 			 ROUND(SUM(CASE WHEN TEH.IsChargeable = 0 THEN TEH.ActualHours 
 				      ELSE 0 
-				 END),2) AS NonBillableHours,
-			BU.Name AS GroupName,
-			BU.Code AS GroupCode,
-			TT.Code AS TimeTypeCode
+				 END),2) AS NonBillableHours
 	  FROM dbo.TimeEntry AS TE 
-	  JOIN dbo.TimeEntryHours AS TEH  ON TEH.TimeEntryId = TE.TimeEntryId 
-	  JOIN dbo.ChargeCode CC ON CC.Id = TE.ChargeCodeId 
+	  INNER JOIN dbo.TimeEntryHours AS TEH  ON TEH.TimeEntryId = TE.TimeEntryId 
+	  INNER JOIN dbo.ChargeCode CC ON CC.Id = TE.ChargeCodeId 
 	  INNER JOIN dbo.ProjectGroup BU ON BU.GroupId = CC.ProjectGroupId
-	  JOIN dbo.Client C ON CC.ClientId = C.ClientId
-	  JOIN dbo.Project PRO ON PRO.ProjectId = CC.ProjectId
-	  JOIN dbo.TimeType TT ON TT.TimeTypeId = CC.TimeTypeId
+	  INNER JOIN dbo.Client C ON CC.ClientId = C.ClientId
+	  INNER JOIN dbo.Project PRO ON PRO.ProjectId = CC.ProjectId
+	  INNER JOIN dbo.ProjectStatus PS ON PRO.ProjectStatusId = PS.ProjectStatusId
+	  INNER JOIN dbo.TimeType TT ON TT.TimeTypeId = CC.TimeTypeId
+	  LEFT JOIN PersonDayWiseByProjectsBillableTypes PDBR ON PDBR.ProjectId = CC.ProjectId  AND PDBR.Date = TE.ChargeCodeDate
 	  WHERE TE.PersonId = @PersonId AND TE.ChargeCodeDate BETWEEN @StartDate AND @EndDate
-	  GROUP BY	C.ClientId,
+	  GROUP BY	CC.TimeEntrySectionId,
+				C.ClientId,
+				C.Name,
+				C.Code,
+				BU.Name,
+				BU.Code,
 				PRO.ProjectId,
 				PRO.Name,
 				PRO.ProjectNumber,
-				C.Name,
-				TE.ChargeCodeDate,
+				PS.Name,
 				TT.TimeTypeId,
 				TT.Name,
-				TE.Note,
-				BU.Name,
-				BU.Code,
-				C.Code,
 				TT.Code,
-				CC.TimeEntrySectionId
+				TE.ChargeCodeDate,
+				TE.Note,
+			(CASE WHEN (PDBR.MinimumValue IS NULL OR CC.TimeEntrySectionId <> 1 ) THEN '' 
+			WHEN (PDBR.MinimumValue = PDBR.MaximumValue AND PDBR.MinimumValue = 0) THEN 'Fixed'
+			WHEN (PDBR.MinimumValue = PDBR.MaximumValue AND PDBR.MinimumValue = 1) THEN 'Hourly'
+			ELSE 'Both' END)
 	  ORDER BY  CC.TimeEntrySectionId,PRO.ProjectNumber,TE.ChargeCodeDate,TT.Name
 END	
 	
