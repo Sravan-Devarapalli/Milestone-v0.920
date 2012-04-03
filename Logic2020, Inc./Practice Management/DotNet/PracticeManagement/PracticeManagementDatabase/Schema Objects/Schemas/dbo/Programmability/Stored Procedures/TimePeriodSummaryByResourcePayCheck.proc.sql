312a7132642eb51006fd0236c9f4dfecf0c8a51e
SainathC <SainathC>
2012-04-03 09:20:00 -0500

    Comments - Defect Number: 2984; Defect RecordID: 2980; Server Address: services.logic2020.com; Server Port: 99; Database Name: Practice Management; Defect Summary: New Time Entry Reporting.; Defect Description: From: Jason Hong-Turney [mailto:jasonht@logic2020.com] Sent: Tuesday, February 28, 2012 12:22 PM To: MadhanA (madhana@ggktech.com) Cc: logic2020@ggktech.com; Matt Reilly Subject: Time Entry reporting changes
    Hi Madhan 
    
    Ive redesigned the Time Entry reporting to reflect the changes were making in Time Entry. My goal is to make the report output simpler, especially for Excel exports. I also tried to add some information and graphs that will make it the time entries were capturing more relevant to our business, and that will be easier to get summary/high-level data from. Ive included placeholders for PDF exports, but these can be worked on later. The Excel reports will be more important initially.
    
    Begin by looking at the Time Period Summary image (it uses the correct top selection bar text, for example) before looking at the other two images. The Time Period Summary and Project Summary are somewhat similar in design, while the Person Detail reporting is more specific to the information it contains.
    
    Please look at these and let me know what questions you have and/or what I need to clarify. I dont expect the team to work on these changes until the Time Entry changes I sent earlier and the Time Off functionality changes are completed. In the meantime, it should give you and I a chance to get this as clear as we can.
    
    Jason Hong-Turney // Logic20/20 // jasonht@logic2020.com
    t.206-576-0472 // c. 425-390-4664
    
    From: MadhanA [mailto:madhana@ggktech.com]
    Sent: Thursday, March 01, 2012 9:33 AM
    To: Jason Hong-Turney
    Cc: logic2020@ggktech.com; Matt Reilly
    Subject: RE: Time Entry reporting changes
    
    Hi Jason,
    
    Can you please provide your answers to the below questions.
    
    Time Period Summary Report-> B vs NB Graph %:
    
    1. we assumed that the Exclude Admin group means the time entries entered in Administrative section should be excluded. Please let us know if we are wrong.
    [Jason]  What I intended was to exclude the time entries from people in the Administrative practice (the internal employees here).
    
    Project Summary Report:
    
    1. As per existing functionality, person may be assigned to a milestone/Project with multiple roles. In that scenario, How do you want us to handle(Like showing all roles in project Role column as comma separated list or Showing only the highest role in the ladder?
    [Jason] Show only the highest role for now. I plan to change this in the future, to better align the Team Structure options with the role options.
    2. There is no date range selector in this Report, so we assumed that the date range should be the project start date and end date. Please let us know if we are wrong.
    [Jason] Theres no date range because the report will just look at the Project overall, so I dont want any date filtering options at this point. Were going to be moving away from the concept of milestones and people being linked at that level. Resources will simply be linked to the Project. Were still working on these parts and thinking through how calculations, etc, are affected.
    --
    Thanks & Regards,
    Madhan Annabemoju / GGK Technologies
    
    From: Jason Hong-Turney [mailto:jasonht@logic2020.com]
    Sent: Friday, March 02, 2012 6:53 AM
    To: MadhanA
    Cc: logic2020@ggktech.com
    Subject: RE: Time Entry reporting changes
    
    Hi Madhan  see answers below!
    
    Jason Hong-Turney // Logic20/20 // jasonht@logic2020.com
    t.206-576-0472 // c. 425-390-4664
    
    Hi Jason,
    
    Can you please provide your answers to the below questions.
    
    Questions:
    Project Summary Report:
    
    1) As per the new time entry functionality user is able to enter time for a project outside its start date and end date. In this case do we need to show those time entries?
    [Jason] Yes, any time that is applied towards a Project should be shown.
    2) In the same way the person who

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/TimePeriodSummaryByResourcePayCheck.proc.sql
@@ -0,0 +1,92 @@
+﻿-- =========================================================================
+-- Author:		Sainath.CH
+-- Create date: 04-02-2012
+-- =========================================================================
+CREATE PROCEDURE [dbo].[TimePeriodSummaryByResourcePayCheck]
+(
+	@StartDate DATETIME,
+	@EndDate   DATETIME
+)
+AS
+BEGIN
+
+	SET @StartDate = CONVERT(DATE,@StartDate)
+	SET @EndDate = CONVERT(DATE,@EndDate)
+
+	DECLARE @NOW DATE
+	SET @NOW = dbo.GettingPMTime(GETUTCDATE())
+
+	;WITH AssignedPersons AS
+	(
+	  SELECT MP.PersonId
+	  FROM  dbo.MilestonePersonEntry AS MPE 
+	  INNER JOIN dbo.MilestonePerson AS MP ON MP.MilestonePersonId = MPE.MilestonePersonId 
+											AND ( MPE.StartDate BETWEEN @StartDate AND @EndDate
+													OR MPE.EndDate BETWEEN @StartDate AND @EndDate )
+	  INNER JOIN dbo.Milestone AS M ON M.MilestoneId = MP.MilestoneId
+	  INNER JOIN dbo.Person AS P ON MP.PersonId = P.PersonId AND p.IsStrawman = 0
+	  GROUP BY MP.PersonId
+	),
+	PersonWithLatestPay AS
+	(
+		SELECT P.PersonId,
+			MAX(PA.StartDate) AS RecentStartDate
+		FROM  dbo.Person P
+			LEFT JOIN dbo.Pay PA ON PA.Person = P.PersonId 
+		WHERE P.IsStrawman = 0
+		GROUP BY P.PersonId
+	),
+	PersonWithCurrentPay AS
+	(
+		SELECT P.PersonId
+				,TS.Name AS Timescale
+		FROM  dbo.Person P
+			LEFT JOIN PersonWithLatestPay PLP ON P.PersonId = PLP.PersonId
+			LEFT JOIN dbo.Pay PA ON PA.Person = P.PersonId 
+								AND ( 
+										(@NOW BETWEEN PA.StartDate  AND ISNULL(PA.EndDate-1,dbo.GetFutureDate()))
+										 OR (@NOW NOT BETWEEN PA.StartDate  AND ISNULL(PA.EndDate-1,dbo.GetFutureDate()) AND  PLP.RecentStartDate = PA.StartDate)
+									)
+			LEFT JOIN dbo.Timescale TS ON PA.Timescale = TS.TimescaleId
+		WHERE P.IsStrawman = 0
+	)
+
+	SELECT	1 AS BranchID,
+			CASE WHEN P.DefaultPractice = 4 THEN 100 ELSE 200 END AS DeptID,
+			P.PersonId,
+			P.LastName,
+			P.FirstName,
+			P.EmployeeNumber,
+			P.PaychexID,
+			ISNULL(Data.TotalHours,0) AS TotalHours,
+			ISNULL(Data.PTOHours,0) AS PTOHours,
+			ISNULL(Data.HolidayHours,0) AS HolidayHours,
+			ISNULL(Data.JuryDutyHours,0) AS JuryDutyHours,
+			ISNULL(Data.BereavementHours,0) AS BereavementHours,
+			ISNULL(Data.ORTHours,0) AS ORTHours,
+			PCP.Timescale
+	FROM  
+		(
+			SELECT  TE.PersonId,
+					ROUND(SUM(CASE WHEN CC.TimeEntrySectionId <> 4 THEN TEH.ActualHours ELSE 0 END),2) AS TotalHours,
+					ROUND(SUM(CASE WHEN TT.Code = 'W9310' THEN TEH.ActualHours ELSE 0 END),2) AS PTOHours,
+					ROUND(SUM(CASE WHEN TT.Code = 'W9320' THEN TEH.ActualHours ELSE 0 END),2) AS HolidayHours,
+					ROUND(SUM(CASE WHEN TT.Code = 'W9330' THEN TEH.ActualHours ELSE 0 END),2) AS JuryDutyHours,
+					ROUND(SUM(CASE WHEN TT.Code = 'W9340' THEN TEH.ActualHours ELSE 0 END),2) AS BereavementHours,
+					ROUND(SUM(CASE WHEN TT.Code = 'W9300' THEN TEH.ActualHours ELSE 0 END),2) AS ORTHours
+					FROM dbo.TimeEntry TE
+						INNER JOIN dbo.TimeEntryHours TEH ON TEH.TimeEntryId = te.TimeEntryId 
+															AND TE.ChargeCodeDate BETWEEN @StartDate AND @EndDate 
+						INNER JOIN dbo.ChargeCode CC ON CC.Id = TE.ChargeCodeId 
+						INNER JOIN dbo.Project PRO ON PRO.ProjectId = CC.ProjectId
+						INNER JOIN dbo.TimeType TT ON CC.TimeTypeId = TT.TimeTypeId
+						INNER JOIN dbo.Person P ON P.PersonId = TE.PersonId
+					WHERE Pro.ProjectNumber != 'P031000' AND TE.ChargeCodeDate < ISNULL(P.TerminationDate,dbo.GetFutureDate())
+					GROUP BY TE.PersonId
+		) Data
+		FULL JOIN AssignedPersons AP ON AP.PersonId = Data.PersonId 
+		INNER JOIN dbo.Person P ON (P.PersonId = Data.PersonId OR AP.PersonId = P.PersonId) AND P.IsStrawman = 0
+		INNER JOIN PersonWithCurrentPay PCP ON P.PersonId = PCP.PersonId
+		ORDER BY P.LastName,P.FirstName
+END
+
