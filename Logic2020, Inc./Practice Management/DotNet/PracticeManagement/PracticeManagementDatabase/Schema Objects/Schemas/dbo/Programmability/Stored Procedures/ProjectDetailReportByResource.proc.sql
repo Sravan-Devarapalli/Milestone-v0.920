e5cac7a4f92b8e1cc329520b1f61ba4f5016813f
SainathC <SainathC>
2012-04-05 04:24:00 -0500

    Comments - Defect Number: 2984; Defect RecordID: 2980; Server Address: services.logic2020.com; Server Port: 99; Database Name: Practice Management; Defect Summary: New Time Entry Reporting.; Defect Description: From: Jason Hong-Turney [mailto:jasonht@logic2020.com] Sent: Tuesday, February 28, 2012 12:22 PM To: MadhanA (madhana@ggktech.com) Cc: logic2020@ggktech.com; Matt Reilly Subject: Time Entry reporting changes
    Hi Madhan 
    
    Ive redesigned the Time Entry reporting to reflect the changes were making in Time Entry. My goal is to make the report output simpler, especially for Excel exports. I also tried to add some information and graphs that will make it the time entries were capturing more relevant to our business, and that will be easier to get summary/high-level data from. Ive included placeholders for PDF exports, but these can be worked on later. The Excel reports will be more important initially.
    
    Begin by looking at the Time Period Summary image (it uses the correct top selection bar text, for example) before looking at the other two images. The Time Period Summary and Project Summary are somewhat similar in design, while the Person Detail reporting is more specific to the information it contains.
    
    Please look at these and let me know what questions you have and/or what I need to clarify. I dont expect the team to work on these changes until the Time Entry changes I sent earlier and the Time Off functionality changes are completed. In the meantime, it should give you and I a chance to get this as clear as we can.
    
    Jason Hong-Turney // Logic20/20 // jasonht@logic2020.com
    t.206-576-0472 // c. 425-390-4664
    
    From: MadhanA [mailto:madhana@ggktech.com]
    Sent: Thursday, March 01, 2012 9:33 AM
    To: Jason Hong-Turney
    Cc: logic2020@ggktech.com; Matt Reilly
    Subject: RE: Time Entry reporting changes
    
    Hi Jason,
    
    Can you please provide your answers to the below questions.
    
    Time Period Summary Report-> B vs NB Graph %:
    
    1. we assumed that the Exclude Admin group means the time entries entered in Administrative section should be excluded. Please let us know if we are wrong.
    [Jason]  What I intended was to exclude the time entries from people in the Administrative practice (the internal employees here).
    
    Project Summary Report:
    
    1. As per existing functionality, person may be assigned to a milestone/Project with multiple roles. In that scenario, How do you want us to handle(Like showing all roles in project Role column as comma separated list or Showing only the highest role in the ladder?
    [Jason] Show only the highest role for now. I plan to change this in the future, to better align the Team Structure options with the role options.
    2. There is no date range selector in this Report, so we assumed that the date range should be the project start date and end date. Please let us know if we are wrong.
    [Jason] Theres no date range because the report will just look at the Project overall, so I dont want any date filtering options at this point. Were going to be moving away from the concept of milestones and people being linked at that level. Resources will simply be linked to the Project. Were still working on these parts and thinking through how calculations, etc, are affected.
    --
    Thanks & Regards,
    Madhan Annabemoju / GGK Technologies
    
    From: Jason Hong-Turney [mailto:jasonht@logic2020.com]
    Sent: Friday, March 02, 2012 6:53 AM
    To: MadhanA
    Cc: logic2020@ggktech.com
    Subject: RE: Time Entry reporting changes
    
    Hi Madhan  see answers below!
    
    Jason Hong-Turney // Logic20/20 // jasonht@logic2020.com
    t.206-576-0472 // c. 425-390-4664
    
    Hi Jason,
    
    Can you please provide your answers to the below questions.
    
    Questions:
    Project Summary Report:
    
    1) As per the new time entry functionality user is able to enter time for a project outside its start date and end date. In this case do we need to show those time entries?
    [Jason] Yes, any time that is applied towards a Project should be shown.
    2) In the same way the person who

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/ProjectDetailReportByResource.proc.sql
@@ -0,0 +1,111 @@
+﻿-- =========================================================================
+-- Author:		SainathC
+-- Create date: 04-05-2012
+-- =========================================================================
+CREATE PROCEDURE [dbo].[ProjectDetailReportByResource]
+(
+	@ProjectNumber NVARCHAR(12),
+	@MilestoneId   INT = NULL,
+	@StartDate DATETIME = NULL,
+	@EndDate   DATETIME = NULL
+)
+AS
+BEGIN
+	DECLARE @ProjectId INT = NULL,
+			@Today DATETIME,
+			@MilestoneStartDate DATETIME = NULL,
+			@MilestoneEndDate DATETIME = NULL,
+			@ORTTimeTypeId INT
+
+	SELECT @ProjectId = P.ProjectId
+	FROM dbo.Project AS P
+	WHERE P.ProjectNumber = @ProjectNumber 
+
+	IF(@ProjectId IS NOT NULL)
+	BEGIN
+		SELECT @Today = dbo.GettingPMTime(GETUTCDATE())
+
+		SET @ORTTimeTypeId = dbo.GetORTTimeTypeId()
+
+		IF(@StartDate IS NOT NULL AND @EndDate IS NOT NULL)
+		BEGIN
+			SET @StartDate = CONVERT(DATE,@StartDate)
+			SET @EndDate = CONVERT(DATE,@EndDate)
+		END
+	
+		IF(@MilestoneId IS NOT NULL)
+		BEGIN
+			SELECT @MilestoneStartDate = M.StartDate,
+				   @MilestoneEndDate = M.ProjectedDeliveryDate
+			FROM dbo.Milestone AS M
+			WHERE M.MilestoneId = @MilestoneId 
+		END
+
+	;WITH PersonMaxRoleValues AS
+	(
+	  SELECT MP.PersonId,
+			 MAX(ISNULL(PR.RoleValue,0)) AS MaxRoleValue
+	  FROM  dbo.MilestonePersonEntry AS MPE 
+	  INNER JOIN dbo.MilestonePerson AS MP ON MP.MilestonePersonId = MPE.MilestonePersonId
+	  INNER JOIN dbo.Milestone AS M ON M.MilestoneId = MP.MilestoneId
+	  INNER JOIN dbo.Calendar AS C ON C.Date BETWEEN MPE.StartDate AND MPE.EndDate AND C.DayOff = 0  
+	  LEFT  JOIN dbo.PersonRole AS PR ON PR.PersonRoleId = MPE.PersonRoleId
+	  WHERE  M.ProjectId = @ProjectId AND (@MilestoneId IS NULL OR M.MilestoneId = @MilestoneId)
+	   AND ((@StartDate IS NULL AND @EndDate IS NULL) OR (C.Date BETWEEN  @StartDate AND @EndDate))
+	  GROUP BY MP.PersonId
+	)
+	,PersonForeCastedHours AS
+	(
+	  SELECT MP.PersonId,
+			 SUM(CASE WHEN c.Date <= @Today THEN  MPE.HoursPerDay ELSE 0 END) AS ForecastedHoursUntilToday,
+			 SUM(MPE.HoursPerDay) AS ForecastedHours
+	  FROM  dbo.MilestonePersonEntry AS MPE 
+	  INNER JOIN dbo.MilestonePerson AS MP ON MP.MilestonePersonId = MPE.MilestonePersonId
+	  INNER JOIN dbo.Milestone AS M ON M.MilestoneId = MP.MilestoneId
+	  INNER JOIN dbo.Calendar AS C ON C.Date BETWEEN MPE.StartDate AND MPE.EndDate AND C.DayOff = 0  
+	  WHERE  M.ProjectId = @ProjectId  AND (@MilestoneId IS NULL OR M.MilestoneId = @MilestoneId)
+	  AND ((@StartDate IS NULL AND @EndDate IS NULL) OR (C.Date BETWEEN  @StartDate AND @EndDate))
+	  GROUP BY MP.PersonId
+	)
+
+	SELECT P.PersonId,
+		   P.LastName,
+		    P.FirstName,
+			TT.Name AS TimeTypeName,
+			TT.Code AS TimeTypeCode,
+			TE.ChargeCodeDate,
+			(CASE  WHEN TT.TimeTypeId = @ORTTimeTypeId THEN TE.Note + dbo.GetApprovedByName(TE.ChargeCodeDate,@ORTTimeTypeId,P.PersonId)
+				ELSE TE.Note
+			END) AS Note,
+		   ROUND(SUM(CASE WHEN TEH.IsChargeable = 1  THEN TEH.ActualHours ELSE 0 END),2) AS BillableHours,
+	       ROUND(SUM(CASE WHEN TEH.IsChargeable = 0 THEN TEH.ActualHours ELSE 0 END),2) AS NonBillableHours,
+		   ISNULL(PR.Name,'') AS ProjectRoleName,
+		   CC.TimeEntrySectionId,
+		   ROUND(MAX(ISNULL(PFH.ForecastedHours,0)),2) AS ForecastedHours
+	FROM dbo.TimeEntry AS TE
+	INNER JOIN dbo.TimeEntryHours AS TEH ON TEH.TimeEntryId = TE.TimeEntryId 
+										AND ((@MilestoneId IS NULL) OR (TE.ChargeCodeDate BETWEEN @MilestoneStartDate AND @MilestoneEndDate))
+										AND ((@StartDate IS NULL AND @EndDate IS NULL) OR (TE.ChargeCodeDate BETWEEN @StartDate AND @EndDate))
+	INNER JOIN dbo.ChargeCode AS CC ON CC.Id = TE.ChargeCodeId AND CC.ProjectId = @ProjectId
+	INNER JOIN dbo.TimeType TT ON TT.TimeTypeId = CC.TimeTypeId
+	FULL  JOIN PersonMaxRoleValues AS PMRV ON PMRV.PersonId = TE.PersonId 
+	INNER JOIN dbo.Person AS P ON (P.PersonId = TE.PersonId OR  PMRV.PersonId = P.PersonId) AND p.IsStrawman = 0
+	LEFT  JOIN PersonForeCastedHours AS PFH ON PFH.PersonId = P.PersonId  AND PFH.PersonId= PMRV.PersonId 
+	LEFT  JOIN dbo.PersonRole AS PR ON PR.RoleValue = PMRV.MaxRoleValue
+	GROUP BY P.PersonId,
+			P.LastName,
+		    P.FirstName,
+			TT.Name,
+			TT.Code,
+			TE.ChargeCodeDate,
+			TT.TimeTypeId,
+ 			PR.Name,
+			TE.Note,
+			CC.TimeEntrySectionId
+	END
+	ELSE
+	BEGIN
+		RAISERROR('There is no Project with this Project Number.', 16, 1)
+	END
+END
+
