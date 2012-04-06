a09bbfcedfbe2927f5f51d8658bd5c36c2cab932
SainathC <SainathC>
2012-04-06 02:39:00 -0500

    Comments - Defect Number: 2984; Defect RecordID: 2980; Server Address: services.logic2020.com; Server Port: 99; Database Name: Practice Management; Defect Summary: New Time Entry Reporting.; Defect Description: From: Jason Hong-Turney [mailto:jasonht@logic2020.com] Sent: Tuesday, February 28, 2012 12:22 PM To: MadhanA (madhana@ggktech.com) Cc: logic2020@ggktech.com; Matt Reilly Subject: Time Entry reporting changes
    Hi Madhan 
    
    Ive redesigned the Time Entry reporting to reflect the changes were making in Time Entry. My goal is to make the report output simpler, especially for Excel exports. I also tried to add some information and graphs that will make it the time entries were capturing more relevant to our business, and that will be easier to get summary/high-level data from. Ive included placeholders for PDF exports, but these can be worked on later. The Excel reports will be more important initially.
    
    Begin by looking at the Time Period Summary image (it uses the correct top selection bar text, for example) before looking at the other two images. The Time Period Summary and Project Summary are somewhat similar in design, while the Person Detail reporting is more specific to the information it contains.
    
    Please look at these and let me know what questions you have and/or what I need to clarify. I dont expect the team to work on these changes until the Time Entry changes I sent earlier and the Time Off functionality changes are completed. In the meantime, it should give you and I a chance to get this as clear as we can.
    
    Jason Hong-Turney // Logic20/20 // jasonht@logic2020.com
    t.206-576-0472 // c. 425-390-4664
    
    From: MadhanA [mailto:madhana@ggktech.com]
    Sent: Thursday, March 01, 2012 9:33 AM
    To: Jason Hong-Turney
    Cc: logic2020@ggktech.com; Matt Reilly
    Subject: RE: Time Entry reporting changes
    
    Hi Jason,
    
    Can you please provide your answers to the below questions.
    
    Time Period Summary Report-> B vs NB Graph %:
    
    1. we assumed that the Exclude Admin group means the time entries entered in Administrative section should be excluded. Please let us know if we are wrong.
    [Jason]  What I intended was to exclude the time entries from people in the Administrative practice (the internal employees here).
    
    Project Summary Report:
    
    1. As per existing functionality, person may be assigned to a milestone/Project with multiple roles. In that scenario, How do you want us to handle(Like showing all roles in project Role column as comma separated list or Showing only the highest role in the ladder?
    [Jason] Show only the highest role for now. I plan to change this in the future, to better align the Team Structure options with the role options.
    2. There is no date range selector in this Report, so we assumed that the date range should be the project start date and end date. Please let us know if we are wrong.
    [Jason] Theres no date range because the report will just look at the Project overall, so I dont want any date filtering options at this point. Were going to be moving away from the concept of milestones and people being linked at that level. Resources will simply be linked to the Project. Were still working on these parts and thinking through how calculations, etc, are affected.
    --
    Thanks & Regards,
    Madhan Annabemoju / GGK Technologies
    
    From: Jason Hong-Turney [mailto:jasonht@logic2020.com]
    Sent: Friday, March 02, 2012 6:53 AM
    To: MadhanA
    Cc: logic2020@ggktech.com
    Subject: RE: Time Entry reporting changes
    
    Hi Madhan  see answers below!
    
    Jason Hong-Turney // Logic20/20 // jasonht@logic2020.com
    t.206-576-0472 // c. 425-390-4664
    
    Hi Jason,
    
    Can you please provide your answers to the below questions.
    
    Questions:
    Project Summary Report:
    
    1) As per the new time entry functionality user is able to enter time for a project outside its start date and end date. In this case do we need to show those time entries?
    [Jason] Yes, any time that is applied towards a Project should be shown.
    2) In the same way the person who

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/TimePeriodSummaryReportByWorkType.proc.sql
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/TimePeriodSummaryReportByWorkType.proc.sql
@@ -1,16 +1,14 @@
 ﻿-- =========================================================================
 -- Author:		Sainath.CH
 -- Create date: 03-05-2012
--- Updated by : Thulasiram.P
--- Update Date: 03-15-2012
+-- Updated by : Sainath.CH
+-- Update Date: 04-06-2012
 -- Description:  Time Entries grouped by workType for a particular period.
 -- =========================================================================
 CREATE PROCEDURE [dbo].[TimePeriodSummaryReportByWorkType]
 (
 	@StartDate DATETIME,
-	@EndDate   DATETIME,
-	@TimeTypeCategoryIds NVARCHAR(MAX) = NULL,--work types: (Administrative = 1,Default  = 2,Internal = 3,Project =4)
-	@OrderByCerteria NVARCHAR(20) = 'workType'-- workType,category,total
+	@EndDate   DATETIME
 )
 AS
 BEGIN
@@ -18,78 +16,35 @@ BEGIN
     SET @StartDate = CONVERT(DATE,@StartDate)
 	SET @EndDate = CONVERT(DATE,@EndDate)
 
-	DECLARE @GroupByCerteria NVARCHAR(20),@DaysDiff INT 
-	SET @DaysDiff = DATEDIFF(dd,@StartDate,@EndDate)
-	IF(@DaysDiff <= 7 )
-	BEGIN
-		SET @GroupByCerteria = 'day'
-	END
-	ELSE IF(@DaysDiff > 7 AND @DaysDiff <= 31  )
-	BEGIN
-		SET @GroupByCerteria = 'week'
-	END
-	ELSE IF(@DaysDiff > 31 AND @DaysDiff <= 365  )
-	BEGIN
-		SET @GroupByCerteria = 'month'
-	END
-	ELSE 
-	BEGIN
-		SET @GroupByCerteria = 'year'
-	END
-
-	DECLARE @TimeTypeCategoryIdsTable TABLE(WorkTypeName NVARCHAR(30))
-	INSERT INTO @TimeTypeCategoryIdsTable
-	SELECT ResultId 
-	FROM [dbo].[ConvertStringListIntoTable] (@TimeTypeCategoryIds)
-
-	SELECT   TT.TimeTypeId,
+	SELECT  TT.TimeTypeId,
 			TT.Name AS TimeTypeName,
 			TT.IsDefault,
 			TT.IsInternal,
 			TT.IsAdministrative,
-			ROUND(SUM(CASE WHEN TEH.IsChargeable = 1 THEN TEH.ActualHours ELSE 0 END),2) AS BillableHours,
-			ROUND(SUM(CASE WHEN TEH.IsChargeable = 0 THEN TEH.ActualHours ELSE 0 END),2) AS NonBillableHours,
+			ROUND(SUM(CASE WHEN TEH.IsChargeable = 1 AND Pro.ProjectNumber != 'P031000' THEN TEH.ActualHours ELSE 0 END),2) AS BillableHours,
+			ROUND(SUM(CASE WHEN TEH.IsChargeable = 0 OR Pro.ProjectNumber = 'P031000' THEN TEH.ActualHours ELSE 0 END),2) AS NonBillableHours,
 			CASE WHEN TT.IsAdministrative = 1 THEN 'Administrative'
 				 WHEN TT.IsDefault = 1 THEN 'Default'
 				 WHEN TT.IsInternal = 1 THEN 'Internal'
 				 ELSE 'Project'
-			END AS 'Category',
-			CASE WHEN @GroupByCerteria = 'day' THEN TE.ChargeCodeDate -- date
-				 WHEN @GroupByCerteria = 'week' THEN (TE.ChargeCodeDate - (DATEPART(dw,TE.ChargeCodeDate) -1 ))  --week start date 
-				 WHEN @GroupByCerteria = 'month' THEN TE.ChargeCodeDate - (DAY(TE.ChargeCodeDate)-1)  -- month StartDate
-				 WHEN @GroupByCerteria = 'year' THEN TE.ChargeCodeDate - (DATEPART(DAYOFYEAR,TE.ChargeCodeDate)-1)-- year StartDate
-			END as StartDate,
-			@GroupByCerteria AS GroupByCerteria
-			FROM dbo.TimeEntry TE
-				INNER JOIN dbo.TimeEntryHours TEH ON TEH.TimeEntryId = te.TimeEntryId 
-				INNER JOIN dbo.ChargeCode CC ON CC.Id = TE.ChargeCodeId 
-				INNER JOIN dbo.TimeType TT ON CC.TimeTypeId = TT.TimeTypeId 
-			WHERE TE.ChargeCodeDate BETWEEN @StartDate AND @EndDate 
-					AND ( @TimeTypeCategoryIds IS NULL OR
-					((SELECT COUNT(WorkTypeName) FROM @TimeTypeCategoryIdsTable WHERE WorkTypeName = 1) = 1 AND TT.IsAdministrative = 1) OR 
-					((SELECT COUNT(WorkTypeName) FROM @TimeTypeCategoryIdsTable WHERE WorkTypeName = 2) = 1 AND TT.IsDefault = 1) OR 
-					((SELECT COUNT(WorkTypeName) FROM @TimeTypeCategoryIdsTable WHERE WorkTypeName = 3) = 1 AND TT.IsInternal= 1 AND TT.IsAdministrative = 0) OR
-					((SELECT COUNT(WorkTypeName) FROM @TimeTypeCategoryIdsTable WHERE WorkTypeName = 4) = 1 AND TT.IsAdministrative = 0 AND TT.IsDefault = 0 AND TT.IsInternal = 0)
-					)
-			GROUP BY TT.Name,
-					 TT.IsDefault,
-					 TT.IsInternal,
-					 TT.IsAdministrative,
-					 TEH.IsChargeable,
-					 TT.TimeTypeId,
-					 CASE WHEN TT.IsAdministrative = 1 THEN 'Administrative'
-						  WHEN TT.IsDefault = 1 THEN 'Default'
-						  WHEN TT.IsInternal = 1 THEN 'Internal'
-	  					  ELSE 'Project'
-					  END,
-					CASE WHEN @GroupByCerteria = 'day' THEN TE.ChargeCodeDate -- date
-						 WHEN @GroupByCerteria = 'week' THEN (TE.ChargeCodeDate - (DATEPART(dw,TE.ChargeCodeDate) -1 ))  --week start date 
-						 WHEN @GroupByCerteria = 'month' THEN TE.ChargeCodeDate - (DAY(TE.ChargeCodeDate)-1)  -- month StartDate
-						 WHEN @GroupByCerteria = 'year' THEN TE.ChargeCodeDate - (DATEPART(DAYOFYEAR,TE.ChargeCodeDate)-1)-- year StartDate
-					END 
-			ORDER BY CASE WHEN @OrderByCerteria = 'workType' THEN 1
-						  WHEN @OrderByCerteria = 'category' THEN 7
-						  WHEN @OrderByCerteria = 'total' THEN 6
-		  			 END
+			END AS 'Category'
+	FROM dbo.TimeEntry TE
+		INNER JOIN dbo.TimeEntryHours TEH ON TEH.TimeEntryId = te.TimeEntryId 
+		INNER JOIN dbo.ChargeCode CC ON CC.Id = TE.ChargeCodeId 
+		INNER JOIN dbo.Project Pro ON CC.ProjectId = Pro.ProjectId
+		INNER JOIN dbo.TimeType TT ON CC.TimeTypeId = TT.TimeTypeId 
+	WHERE TE.ChargeCodeDate BETWEEN @StartDate AND @EndDate 
+	GROUP BY TT.TimeTypeId,
+		 	TT.Name,
+			TT.IsDefault,
+			TT.IsInternal,
+			TT.IsAdministrative,
+			CASE WHEN TT.IsAdministrative = 1 THEN 'Administrative'
+				WHEN TT.IsDefault = 1 THEN 'Default'
+				WHEN TT.IsInternal = 1 THEN 'Internal'
+	  			ELSE 'Project'
+			END
+	ORDER BY TT.Name
+	
 END
 
