ee8259a1930b2f2557a4e171ff75c96ba91176b9
thulasiramp <thulasiramp>
2012-04-12 04:52:00 -0500

    Comments - Defect Number: 3009; Defect RecordID: 3005; Server Address: services.logic2020.com; Server Port: 99; Database Name: Practice Management; Defect Summary: Time entry: Notes are required or not is not dependent on the Practice a person is assigned to (as it is now), but instead tog@; Defect Description: Time Entry - Notes functionality: Notes are required or not is not dependent on the Practice a person is assigned to (as it is now), but instead toggled on/off (required/optional) per Project. Default to required. Notes always required for hours entered in Non-Billable for Projects, even if notes are set to optional in project. Will also add icon to Time Entry page to indicate which projects require notes and which are optional (design to come).

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/GetBusinessDevelopmentProject.proc.sql
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/GetBusinessDevelopmentProject.proc.sql
@@ -1,7 +1,15 @@
-﻿CREATE PROCEDURE [dbo].[GetBusinessDevelopmentProject]
+﻿-- =============================================
+-- Description:	Get BusinessDevelopmentProject.
+-- Updated by:	ThulasiRam.P
+-- Update date:	04-12-2012
+-- =============================================
+CREATE PROCEDURE [dbo].[GetBusinessDevelopmentProject]
 AS
 BEGIN
-	SELECT  ProjectId,Name,ProjectNumber
-	FROM [dbo].[Project]
+	SELECT  P.ProjectId,
+			P.Name,
+			P.ProjectNumber,
+			P.IsNoteRequired
+	FROM [dbo].[Project] AS P
 	WHERE ProjectNumber = 'P999918'
 END
