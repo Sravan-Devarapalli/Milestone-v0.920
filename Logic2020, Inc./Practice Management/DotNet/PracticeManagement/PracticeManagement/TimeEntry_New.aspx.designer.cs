99393539996ae3268707504b6bb9f47cd4a75fcb
thulasiramp <thulasiramp>
2012-04-12 04:33:00 -0500

    Comments - Defect Number: 3009; Defect RecordID: 3005; Server Address: services.logic2020.com; Server Port: 99; Database Name: Practice Management; Defect Summary: Time entry: Notes are required or not is not dependent on the Practice a person is assigned to (as it is now), but instead tog@; Defect Description: Time Entry - Notes functionality: Notes are required or not is not dependent on the Practice a person is assigned to (as it is now), but instead toggled on/off (required/optional) per Project. Default to required. Notes always required for hours entered in Non-Billable for Projects, even if notes are set to optional in project. Will also add icon to Time Entry page to indicate which projects require notes and which are optional (design to come).

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/TimeEntry_New.aspx.designer.cs
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/TimeEntry_New.aspx.designer.cs
@@ -40,15 +40,6 @@ namespace PraticeManagement {
         protected global::PraticeManagement.Controls.TimeEntry.WeekSelector_New wsChoose;
         
         /// <summary>
-        /// lblAlertNote control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Label lblAlertNote;
-        
-        /// <summary>
         /// LoadingProgress1 control.
         /// </summary>
         /// <remarks>
