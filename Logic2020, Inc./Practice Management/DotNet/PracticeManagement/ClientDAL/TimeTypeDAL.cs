b5c0feba6aaeec2f7d4007f56868c2eb4b30bc3e
SainathC <SainathC>
2012-05-02 07:17:00 -0500

    Comments - Defect Number: 3004; Defect RecordID: 3000; Server Address: localhost; Server Port: 99; Database Name: Practice Management; Defect Summary: ProjectDetails > project work types tab : Add delete option to Custom Work Type.; Defect Description: ProjectDetails > project work types tab : Add delete option to Custom Work Type.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/ClientDAL/TimeTypeDAL.cs
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/ClientDAL/TimeTypeDAL.cs
@@ -257,6 +257,41 @@ namespace DataAccess
 
         }
 
+        public static List<TimeTypeRecord> GetWorkTypesInUseDetails(string timeTypeIds)
+        {
+            using (SqlConnection connection = new SqlConnection(DataSourceHelper.DataConnection))
+            using (SqlCommand command = new SqlCommand(Constants.ProcedureNames.TimeEntry.GetWorkTypesInUseDetailsProcedure, connection))
+            {
+                command.CommandType = CommandType.StoredProcedure;
+                command.CommandTimeout = connection.ConnectionTimeout;
+                command.Parameters.AddWithValue(Constants.ParameterNames.TimeTypeIdsParam, timeTypeIds);
+                connection.Open();
+                using (var reader = command.ExecuteReader())
+                {
+                    List<TimeTypeRecord> result = new List<TimeTypeRecord>();
+                    ReadTimeTypes(reader, result);
+                    return result;
+                }
+            }
+        }
+
+        private static void ReadTimeTypes(SqlDataReader reader, List<TimeTypeRecord> result)
+        {
+            if (reader.HasRows)
+            {
+                int timeTypeIdIndex = reader.GetOrdinal(Constants.ColumnNames.TimeTypeId);
+                int inUseIndex = reader.GetOrdinal(Constants.ColumnNames.InUse);
+                while (reader.Read())
+                {
+                    var tt = new TimeTypeRecord
+                    {
+                        Id = reader.GetInt32(timeTypeIdIndex),
+                        InUse = Convert.ToBoolean(reader.GetInt32(inUseIndex))
+                    };
+                    result.Add(tt);
+                }
+            }
+        }
 
     }
 }
