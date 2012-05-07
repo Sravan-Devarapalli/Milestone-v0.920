063b724d6c0fb833d10786da5c30c137a76c12fd
SainathC <SainathC>
2012-05-07 09:12:00 -0500

    Comments - Defect Number: 3033; Defect RecordID: 3029; Server Address: localhost; Server Port: 99; Database Name: Practice Management; Defect Summary: Create Time Entry Audit page; Defect Description: See attached mockup and Excel example for new Time Entry Audit page, to be created under Reports > Time > Audit and accessible to Admin only.

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/DataTransferObjects/Reports/ProjectLevelTimeEntriesHistory.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+using DataTransferObjects.TimeEntry;
+
+namespace DataTransferObjects.Reports
+{
+    [DataContract]
+    [Serializable]
+    public class ProjectLevelTimeEntriesHistory
+    {
+        [DataMember]
+        public Project Project
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public List<TimeEntryRecord> TimeEntryRecords
+        {
+            get;
+            set;
+        }
+
+        public double NetChange
+        {
+            get
+            {
+                return TimeEntryRecords.Sum(t => t.NetChange);
+            }
+        }
+    }
+}
+
