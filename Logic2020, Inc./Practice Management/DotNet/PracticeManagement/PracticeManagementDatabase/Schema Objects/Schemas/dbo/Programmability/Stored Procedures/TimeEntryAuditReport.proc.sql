33d7ea5aa3df9b47b08e8a056c42cb649979bb08
thulasiramp <thulasiramp>
2012-05-07 06:02:00 -0500

    Comments - Defect Number: 3033; Defect RecordID: 3029; Server Address: localhost; Server Port: 99; Database Name: Practice Management; Defect Summary: Create Time Entry Audit page; Defect Description: See attached mockup and Excel example for new Time Entry Audit page, to be created under Reports > Time > Audit and accessible to Admin only.

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/TimeEntryAuditReport.proc.sql
@@ -0,0 +1,110 @@
+﻿-- =========================================================================
+-- Author:      ThulasiRam.P
+-- Create date: 05-07-2012
+-- Description: Time Entries for a particular period.
+-- =========================================================================
+CREATE PROCEDURE [dbo].[TimeEntryAuditReport]
+    (
+      @StartDate DATETIME = NULL ,
+      @EndDate DATETIME = NULL
+    )
+AS 
+    BEGIN
+
+        SET NOCOUNT ON;
+
+				/*
+				Person Name	Status	Pay Type	Affected Date	Modified Date	Account Name	Business Unit	Project	Project Name
+				Phase	Work Type	B/NB	Original	New	Net Change	Note
+				*/
+
+        DECLARE @StartDateLocal DATETIME ,
+            @EndDateLocal DATETIME 
+
+        SET @StartDateLocal = CONVERT(DATE, @StartDate)
+        SET @EndDateLocal = CONVERT(DATE, @EndDate);
+
+
+        WITH    TimeEntriesHistory
+                  AS ( SELECT   TE.PersonId ,
+                                TE.ChargeCodeDate ,
+                                TEH.ModifiedDate ,
+                                TE.ChargeCodeId ,
+                                TEH.IsChargeable ,
+                                0 AS OriginalHours ,
+                                TEH.ActualHours ,
+                                TE.Note
+                       FROM     dbo.TimeEntry AS TE
+                                INNER JOIN dbo.TimeEntryHours AS TEH ON TE.TimeEntryId = TEH.TimeEntryId
+                       WHERE    TEH.ModifiedDate > @EndDateLocal
+                                AND TE.ChargeCodeDate BETWEEN @StartDateLocal
+                                                      AND     @EndDateLocal
+                       UNION
+                       SELECT   TH.PersonId ,
+                                TH.ChargeCodeDate ,
+                                TH.ModifiedDate ,
+                                TH.ChargeCodeId ,
+                                TH.IsChargeable ,
+                                TH.OldHours AS OriginalHours ,
+                                TH.ActualHours ,
+                                TH.Note
+                       FROM     dbo.TimeEntryHistory AS TH
+                       WHERE    TH.ModifiedDate > @EndDateLocal
+                                AND TH.OldHours <> TH.ActualHours
+                                AND TH.ChargeCodeDate BETWEEN @StartDateLocal
+                                                      AND     @EndDateLocal
+                     )
+            SELECT  TH.PersonId ,
+                    PROJ.ProjectId ,
+                    PROJ.Name AS ProjectName ,
+                    PROJ.ProjectNumber ,
+                    CLNT.ClientId ,
+                    CLNT.Name AS ClientName ,
+                    Pg.GroupId ,
+                    Pg.Name AS ProjectGroupName ,
+                    TT.TimeTypeId ,
+                    TT.Name AS TimeTypeName ,
+                    TH.ChargeCodeDate ,
+                    TH.ModifiedDate ,
+                    TH.ChargeCodeId ,
+                    TH.IsChargeable ,
+                    TH.OriginalHours ,
+                    TH.ActualHours ,
+                    TH.Note ,
+                    '1' AS Phase
+            FROM    TimeEntriesHistory AS TH
+                    INNER JOIN dbo.ChargeCode AS CC ON CC.Id = TH.ChargeCodeId
+                    INNER JOIN dbo.Project AS PROJ ON PROJ.ProjectId = CC.ProjectId
+                    INNER JOIN dbo.Client AS CLNT ON CC.ClientId = CLNT.ClientId
+                    INNER JOIN dbo.ProjectGroup AS PG ON PG.GroupId = CC.ProjectGroupId
+                    INNER JOIN dbo.TimeType AS TT ON TT.TimeTypeId = CC.TimeTypeId;
+
+        WITH    EffectedPersons
+                  AS ( SELECT   TE.PersonId
+                       FROM     dbo.TimeEntry AS TE
+                                INNER JOIN dbo.TimeEntryHours AS TEH ON TE.TimeEntryId = TEH.TimeEntryId
+                       WHERE    TEH.ModifiedDate > @EndDateLocal
+                                AND TE.ChargeCodeDate BETWEEN @StartDateLocal
+                                                      AND     @EndDateLocal
+                       UNION
+                       SELECT   TH.PersonId
+                       FROM     dbo.TimeEntryHistory AS TH
+                       WHERE    TH.ModifiedDate > @EndDateLocal
+                                AND TH.OldHours <> TH.ActualHours
+                                AND TH.ChargeCodeDate BETWEEN @StartDateLocal
+                                                      AND     @EndDateLocal
+                     )
+            SELECT  P.PersonId ,
+                    P.LastName ,
+                    P.FirstName ,
+                    P.PersonStatusId ,
+                    PS.Name ,
+                    TSC.TimescaleId ,
+                    TSC.Name AS TimescaleName
+            FROM    EffectedPersons AS EP
+                    INNER JOIN dbo.Person AS P ON EP.PersonId = P.PersonId
+                    INNER JOIN dbo.PersonStatus AS PS ON P.PersonStatusId = PS.PersonStatusId
+                    INNER JOIN dbo.Timescale AS TSC ON TSC.TimescaleId = dbo.GetCurrentPayType(P.PersonId)
+     
+    END
+
