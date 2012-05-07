9875ed8a13e8dcd6e1dc965f4593b8c08901665e
thulasiramp <thulasiramp>
2012-05-07 06:02:00 -0500

    Comments - Defect Number: 3033; Defect RecordID: 3029; Server Address: localhost; Server Port: 99; Database Name: Practice Management; Defect Summary: Create Time Entry Audit page; Defect Description: See attached mockup and Excel example for new Time Entry Audit page, to be created under Reports > Time > Audit and accessible to Admin only.

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/DataTransferObjects/TimeEntry/ChargeCode.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+
+namespace DataTransferObjects.TimeEntry
+{
+    [DataContract]
+    [Serializable]
+    public class ChargeCode
+    {
+        private const string seperator = " - ";
+
+
+        public string ChargeCodeName
+        {
+            get
+            {
+                StringBuilder sb = new StringBuilder();
+                sb.Append(Client.Name);
+                sb.Append(seperator);
+                sb.Append(ProjectGroup.Name);
+                sb.Append(seperator);
+                sb.Append(Project.ProjectNumber);
+                sb.Append(seperator);
+                sb.Append(Project.Name);
+                sb.Append(seperator);
+                sb.Append(Phase);
+                sb.Append(seperator);
+                sb.Append(TimeType.Name);
+
+                return sb.ToString();
+
+            }
+
+        }
+
+        [DataMember]
+        public int ChargeCodeId
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public Project Project
+        {
+            get;
+            set;
+
+        }
+
+        [DataMember]
+        public Client Client
+        {
+            get;
+            set;
+
+        }
+
+
+        [DataMember]
+        public ProjectGroup ProjectGroup
+        {
+            get;
+            set;
+
+        }
+
+        [DataMember]
+        public int Phase
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public TimeTypeRecord TimeType
+        {
+            get;
+            set;
+
+        }
+    }
+}
+
