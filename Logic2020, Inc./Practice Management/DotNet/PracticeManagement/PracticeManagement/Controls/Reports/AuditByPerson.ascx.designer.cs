8ee555d3503f553aadd3371e61b4f091f9cc7241
SainathC <SainathC>
2012-05-08 04:15:00 -0500

    Comments - Defect Number: 3033; Defect RecordID: 3029; Server Address: localhost; Server Port: 99; Database Name: Practice Management; Defect Summary: Create Time Entry Audit page; Defect Description: See attached mockup and Excel example for new Time Entry Audit page, to be created under Reports > Time > Audit and accessible to Admin only.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/Controls/Reports/AuditByPerson.ascx.designer.cs
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/Controls/Reports/AuditByPerson.ascx.designer.cs
@@ -20,6 +20,15 @@ namespace PraticeManagement.Controls.Reports {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Repeater repPersons;
+        
+        /// <summary>
+        /// divEmptyMessage control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl divEmptyMessage;
     }
 }
 
