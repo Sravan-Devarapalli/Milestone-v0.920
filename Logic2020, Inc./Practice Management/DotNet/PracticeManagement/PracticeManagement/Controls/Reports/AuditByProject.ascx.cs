58baad211ecf63be2f4f7d63fa8637e2b697623d
SainathC <SainathC>
2012-05-08 09:12:00 -0500

    Comments - Defect Number: 3033; Defect RecordID: 3029; Server Address: localhost; Server Port: 99; Database Name: Practice Management; Defect Summary: Create Time Entry Audit page; Defect Description: See attached mockup and Excel example for new Time Entry Audit page, to be created under Reports > Time > Audit and accessible to Admin only.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/Controls/Reports/AuditByProject.ascx.cs
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/Controls/Reports/AuditByProject.ascx.cs
@@ -4,14 +4,185 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using DataTransferObjects.Reports;
+using System.Text;
+using DataTransferObjects.TimeEntry;
+using DataTransferObjects;
 
 namespace PraticeManagement.Controls.Reports
 {
     public partial class AuditByProject : System.Web.UI.UserControl
     {
+        private PraticeManagement.Reporting.Audit HostingPage
+        {
+            get { return ((PraticeManagement.Reporting.Audit)Page); }
+        }
+
         protected void Page_Load(object sender, EventArgs e)
         {
 
         }
+
+        protected string GetDoubleFormat(double value)
+        {
+            return value.ToString(Constants.Formatting.DoubleValue);
+        }
+
+        protected string GetDateFormat(DateTime date)
+        {
+            return date.ToString(Constants.Formatting.ReportDateFormat);
+        }
+
+        public void PopulateByProjectData(ProjectLevelTimeEntriesHistory[] reportDataByPerson)
+        {
+            var reportDataList = reportDataByPerson.ToList();
+
+            if (reportDataList.Count > 0)
+            {
+                divEmptyMessage.Style["display"] = "none";
+                repProject.Visible = true;
+                repProject.DataSource = reportDataList;
+                repProject.DataBind();
+            }
+            else
+            {
+                divEmptyMessage.Style["display"] = "";
+                repProject.Visible = false;
+            }
+        }
+
+        protected void btnExportToExcel_OnClick(object sender, EventArgs e)
+        {
+            if (HostingPage.StartDate.HasValue && HostingPage.EndDate.HasValue)
+            {
+                List<ProjectLevelTimeEntriesHistory> data = ServiceCallers.Custom.Report(r => r.TimeEntryAuditReportByProject(HostingPage.StartDate.Value, HostingPage.EndDate.Value)).ToList();
+                data = data.OrderBy(p => p.Project.ProjectNumber).ToList();
+                StringBuilder sb = new StringBuilder();
+                sb.Append("Time Entry Audit");
+                sb.Append("\t");
+                sb.AppendLine();
+                sb.Append(data.Count + " Person(s) Affected");
+                sb.Append("\t");
+                sb.AppendLine();
+                sb.Append(HostingPage.Range);
+                sb.Append("\t");
+                sb.AppendLine();
+                sb.AppendLine();
+                if (data.Count > 0)
+                {
+                    //Header
+                    sb.Append("Person Name");
+                    sb.Append("\t");
+                    sb.Append("Status");
+                    sb.Append("\t");
+                    sb.Append("Pay Types");
+                    sb.Append("\t");
+                    sb.Append("Affected Date");
+                    sb.Append("\t");
+                    sb.Append("Modified Date");
+                    sb.Append("\t");
+                    sb.Append("Account Name");
+                    sb.Append("\t");
+                    sb.Append("Business Unit");
+                    sb.Append("\t");
+                    sb.Append("Project");
+                    sb.Append("\t");
+                    sb.Append("Project Name");
+                    sb.Append("\t");
+                    sb.Append("Phase");
+                    sb.Append("\t");
+                    sb.Append("Work Type");
+                    sb.Append("\t");
+                    sb.Append("B/NB");
+                    sb.Append("\t");
+                    sb.Append("Original");
+                    sb.Append("\t");
+                    sb.Append("New");
+                    sb.Append("\t");
+                    sb.Append("Net Change");
+                    sb.Append("\t");
+                    sb.Append("Note");
+                    sb.Append("\t");
+                    sb.AppendLine();
+
+                    //Data
+                    foreach (ProjectLevelTimeEntriesHistory projectLevelTimeEntriesHistory in data)
+                    {
+                        foreach (PersonLevelTimeEntriesHistory personLevelTimeEntrie in projectLevelTimeEntriesHistory.PersonLevelTimeEntries)
+                        {
+                            foreach (TimeEntryRecord timeEntryRecord in personLevelTimeEntrie.TimeEntryRecords)
+                            {
+                                sb.Append(personLevelTimeEntrie.Person.PersonLastFirstName);
+                                sb.Append("\t");
+                                sb.Append(personLevelTimeEntrie.Person.Status.Name);
+                                sb.Append("\t");
+                                sb.Append(personLevelTimeEntrie.Person.CurrentPay.TimescaleName);
+                                sb.Append("\t");
+                                sb.Append(timeEntryRecord.MilestoneDate.ToString("MM/dd/yyyy"));
+                                sb.Append("\t");
+                                sb.Append(timeEntryRecord.ModifiedDate.ToString("MM/dd/yyyy"));
+                                sb.Append("\t");
+                                sb.Append(timeEntryRecord.ChargeCode.Client.Name);
+                                sb.Append("\t");
+                                sb.Append(timeEntryRecord.ChargeCode.ProjectGroup.Name);
+                                sb.Append("\t");
+                                sb.Append(timeEntryRecord.ChargeCode.Project.ProjectNumber);
+                                sb.Append("\t");
+                                sb.Append(timeEntryRecord.ChargeCode.Project.Name);
+                                sb.Append("\t");
+                                sb.Append(timeEntryRecord.ChargeCode.Phase);
+                                sb.Append("\t");
+                                sb.Append(timeEntryRecord.ChargeCode.TimeType.Name);
+                                sb.Append("\t");
+                                sb.Append(timeEntryRecord.IsChargeable ? "B" : "NB");
+                                sb.Append("\t");
+                                sb.Append(timeEntryRecord.OldHours);
+                                sb.Append("\t");
+                                sb.Append(timeEntryRecord.ActualHours);
+                                sb.Append("\t");
+                                sb.Append(timeEntryRecord.NetChange);
+                                sb.Append("\t");
+                                sb.Append(timeEntryRecord.NoteForExport);
+                                sb.Append("\t");
+                                sb.AppendLine();
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    sb.Append("There are no Time Entries that were changed afterwards by any Employee for the selected range.");
+                }
+                //“Time_Entry_Audit_[StartOfRange]_[EndOfRange].xls”.  
+                var filename = string.Format("{0}_{1}-{2}.xls", "Time_Entry_Audit", HostingPage.StartDate.Value.ToString("MM.dd.yyyy"), HostingPage.EndDate.Value.ToString("MM.dd.yyyy"));
+                GridViewExportUtil.Export(filename, sb);
+            }
+        }
+
+        protected void btnExportToPDF_OnClick(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnGroupBy_OnClick(object sender, EventArgs e)
+        {
+            HostingPage.SelectView(0);
+        }
+
+        protected List<KeyValuePair<Person, TimeEntryRecord>> GetModifiedDatasource(Object personLevelTimeEntries)
+        {
+            List<PersonLevelTimeEntriesHistory> _personLevelTimeEntries = (List<PersonLevelTimeEntriesHistory>)personLevelTimeEntries;
+            List<KeyValuePair<Person, TimeEntryRecord>> modifiedPersonLevelTimeEntries = new List<KeyValuePair<Person, TimeEntryRecord>>();
+
+            foreach (PersonLevelTimeEntriesHistory plteh in _personLevelTimeEntries)
+            {
+                foreach (TimeEntryRecord ter in plteh.TimeEntryRecords)
+                {
+                    modifiedPersonLevelTimeEntries.Add(new KeyValuePair<Person, TimeEntryRecord>(plteh.Person, ter));
+                }
+            }
+            return modifiedPersonLevelTimeEntries;
+        }
+
     }
 }
