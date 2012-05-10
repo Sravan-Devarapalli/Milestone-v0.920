462ffb42790d63e30053ab895f09d285057806fd
thulasiramp <thulasiramp>
2012-05-10 04:54:00 -0500

    Comments - Defect Number: 3044; Defect RecordID: 3040; Server Address: localhost; Server Port: 99; Database Name: Practice Management; Defect Summary: Prohibit 1099 and W2-Hourly from entering PTO; Defect Description: Can you please provide answers for below questions?
    1)Do you want us to Prohibit 1099 and W2-Hourly from entering only PTO or other Time-Offs (Jury Duty, Bereavement and ORT) as well?
    [Jason] We can prevent them from entering time towards any Time Off work types. The section should appear still on their Time Entry page, because it will be useful to note when holidays appear still (though they will have no time towards that day, either).
    
    2)There are some existing PTO entries for 1099 and W2-Hourly persons in production Database. Please let us know what we need to do with this data.
    Please see attached spread sheet for existing PTO entries.
    [Jason] Can we just clear these entries? We will not need them.

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/GetPayHistoryShortByPerson.proc.sql
@@ -0,0 +1,22 @@
+﻿-- =============================================
+-- Author:		ThulasiRam.P
+-- Create date: 05-10-2012
+-- Description:	Retrieves a payment history for the specified person.
+-- =============================================
+CREATE PROCEDURE [dbo].[GetPayHistoryShortByPerson]
+(
+	@PersonId   INT
+)
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	SELECT P.StartDate,
+	       P.EndDate,
+	       P.Timescale
+	  FROM dbo.Pay AS P
+	  WHERE p.PersonId = @PersonId
+	  ORDER BY p.StartDate
+
+END
+
