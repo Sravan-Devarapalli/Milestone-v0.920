5122d4d8adf2658419375aea08dd5258fd6598ce
SainathC <SainathC>
2012-05-14 03:18:00 -0500

    Comments - Defect Number: 3044; Defect RecordID: 3040; Server Address: localhost; Server Port: 99; Database Name: Practice Management; Defect Summary: Prohibit 1099 and W2-Hourly from entering PTO; Defect Description: Can you please provide answers for below questions?
    1)Do you want us to Prohibit 1099 and W2-Hourly from entering only PTO or other Time-Offs (Jury Duty, Bereavement and ORT) as well?
    [Jason] We can prevent them from entering time towards any Time Off work types. The section should appear still on their Time Entry page, because it will be useful to note when holidays appear still (though they will have no time towards that day, either).
    
    2)There are some existing PTO entries for 1099 and W2-Hourly persons in production Database. Please let us know what we need to do with this data.
    Please see attached spread sheet for existing PTO entries.
    [Jason] Can we just clear these entries? We will not need them.

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/IsPersonSalaryTypeListByPeriod.proc.sql
@@ -0,0 +1,28 @@
+﻿-- ====================================================================================================
+-- Author:		Sainathc
+-- Create date: 05-10-2012
+-- Description : Returns IsPerson SalaryType or not for the given period of time.
+-- ====================================================================================================
+CREATE PROCEDURE [dbo].[IsPersonSalaryTypeListByPeriod]
+(
+	@PersonId   INT,
+	@StartDate DATETIME ,
+    @EndDate DATETIME
+)
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	SELECT C.Date,
+		   CASE WHEN ISNULL(P.Timescale,0) = 2 --2:W2-salary
+					THEN CONVERT(BIT,1) 
+				ELSE CONVERT(BIT,0) 
+			END  AS IsSalaryType--if there is no pay for any date consider as not salary Type
+	FROM dbo.Calendar C 
+	LEFT JOIN dbo.Pay AS P ON C.date BETWEEN P.StartDate AND P.EndDate-1
+		  					AND P.Person = @PersonId 
+	WHERE C.date BETWEEN @StartDate AND @EndDate 
+	ORDER BY C.Date 
+
+END
+
