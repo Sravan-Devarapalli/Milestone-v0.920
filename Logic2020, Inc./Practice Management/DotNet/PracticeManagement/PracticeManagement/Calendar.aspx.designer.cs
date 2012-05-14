0fe75aa0959e89be273433cbeb9f23ba8fa599a2
thulasiramp <thulasiramp>
2012-05-14 02:17:00 -0500

    Comments - Defect Number: 3040; Defect RecordID: 3036; Server Address: localhost; Server Port: 99; Database Name: Practice Management; Defect Summary: Prohibit 1099 and W2-Hourly from entering PTO; Defect Description: Can you please provide answers for below questions?
    1)Do you want us to Prohibit 1099 and W2-Hourly from entering only PTO or other Time-Offs (Jury Duty, Bereavement and ORT) as well?
    [Jason] We can prevent them from entering time towards any Time Off work types. The section should appear still on their Time Entry page, because it will be useful to note when holidays appear still (though they will have no time towards that day, either).
    
    2)There are some existing PTO entries for 1099 and W2-Hourly persons in production Database. Please let us know what we need to do with this data.
    Please see attached spread sheet for existing PTO entries.
    [Jason] Can we just clear these entries? We will not need them.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/Calendar.aspx.designer.cs
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/Calendar.aspx.designer.cs
@@ -19,7 +19,7 @@ namespace PraticeManagement {
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::PraticeManagement.Controls.Calendar calendar;
+        protected global::PraticeManagement.Controls.PersonCalendar calendar;
         
         /// <summary>
         /// CalendarLegend control.
