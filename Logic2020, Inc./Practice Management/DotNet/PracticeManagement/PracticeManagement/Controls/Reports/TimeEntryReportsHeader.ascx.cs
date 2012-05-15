139b3ac9edc7e4a63f985b5453d62f0575d7e108
thulasiramp <thulasiramp>
2012-05-15 07:16:00 -0500

    Comments - Defect Number: 3037; Defect RecordID: 3033; Server Address: localhost; Server Port: 99; Database Name: Practice Management; Defect Summary: Implement role-based access permissions to Time Reports; Defect Description: a. By Time Period  Administrator, Client Director b. By Account  Administrator, Client Director, Business Unit Manager c. By Project  Administrator, Client Director, BU Manager, Manager, Project Lead d. By Person  ALL (see only self); Administrator, Client Director (see all persons)
    5/1 - We're still polishing the By Account report page design.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/Controls/Reports/TimeEntryReportsHeader.ascx.cs
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/Controls/Reports/TimeEntryReportsHeader.ascx.cs
@@ -4,34 +4,38 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Web.Security;
 
 namespace PraticeManagement.Controls.Reports
 {
 
     public partial class TimeEntryReportsHeader : System.Web.UI.UserControl
     {
+
+        public int Count { get; set; }
+
         private void ApplyCssHeader()
         {
             if (Page is PraticeManagement.Reporting.TimePeriodSummaryReport)
             {
                 thTimePeriod.Style["background-color"] = "#e2ebff";
-                thProject.Style["background-color"]  = "White";
-                thPerson.Style["background-color"]  = "White";
+                thProject.Style["background-color"] = "White";
+                thPerson.Style["background-color"] = "White";
 
             }
             else if (Page is PraticeManagement.Reporting.ProjectSummaryReport)
             {
-                thTimePeriod.Style["background-color"]  = "White";
+                thTimePeriod.Style["background-color"] = "White";
                 thProject.Style["background-color"] = "#e2ebff";
-                thPerson.Style["background-color"]  = "White";
+                thPerson.Style["background-color"] = "White";
             }
             else if (Page is PraticeManagement.Reporting.PersonDetailTimeReport)
             {
-                thTimePeriod.Style["background-color"]  = "White";
-                thProject.Style["background-color"]  = "White";
+                thTimePeriod.Style["background-color"] = "White";
+                thProject.Style["background-color"] = "White";
                 thPerson.Style["background-color"] = "#e2ebff";
             }
-         }
+        }
 
         protected void Page_Load(object sender, EventArgs e)
         {
@@ -39,6 +43,30 @@ namespace PraticeManagement.Controls.Reports
             {
                 ApplyCssHeader();
             }
+            var count = 0;
+
+            thTimePeriod.Visible = UrlAuthorizationModule.CheckUrlAccessForPrincipal(Constants.ApplicationPages.TimePeriodSummaryReport, new RolePrincipal(HttpContext.Current.User.Identity), "GET");
+            count = thTimePeriod.Visible ? count + 1 : count;
+            thProject.Visible = UrlAuthorizationModule.CheckUrlAccessForPrincipal(Constants.ApplicationPages.ProjectSummaryReport, new RolePrincipal(HttpContext.Current.User.Identity), "GET");
+            count = thProject.Visible ? count + 1 : count;
+            thPerson.Visible = UrlAuthorizationModule.CheckUrlAccessForPrincipal(Constants.ApplicationPages.PersonDetailReport, new RolePrincipal(HttpContext.Current.User.Identity), "GET");
+            count = thPerson.Visible ? count + 1 : count;
+
+            if (count == 1)
+            {
+                tdFirst.Style["width"] = "35%";
+                tdSecond.Style["width"] = "30%";
+                tdThird.Style["width"] = "35%";
+            }
+            else if(count == 2)
+            {
+                tdFirst.Style["width"] = "20%";
+                tdSecond.Style["width"] = "60%";
+                tdThird.Style["width"] = "20%";
+            }
+
+            Count = count;
+
         }
     }
 }
