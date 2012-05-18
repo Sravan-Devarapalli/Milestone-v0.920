52318d41ab39ab3aa9c17976f75dcb2870ba2ada
SainathC <SainathC>
2012-05-18 07:22:00 -0500

    Comments - Defect Number: 3030; Defect RecordID: 3026; Server Address: localhost; Server Port: 99; Database Name: Practice Management; Defect Summary: Possible to apply multiple filters to single column/category in By Time Period (and other) reports?; Defect Description: We're already using a filter in the Resources column to select Domestic or Offshore. Can we possible apply multiple filters to the Resources column as in the image below? In the example below, I've changed the funnel icon in the Resources column heading to the globe icon, but it would function the same way. I've also added another icon in the column heading that, if possible, would be used to filter by person status (Active/Terminated). If possible, we would no longer report "Terminated" in the "Pay Type" column, but would just report the person's last applicable pay type. The new icon is attached as well.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementService/IReportService.cs
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementService/IReportService.cs
@@ -50,7 +50,7 @@ namespace PracticeManagementService
         List<Milestone> GetMilestonesForProject(string projectNumber);
 
         [OperationContract]
-        List<PersonLevelPayCheck> TimePeriodSummaryByResourcePayCheck(DateTime startDate, DateTime endDate, bool includePersonsWithNoTimeEntries, string personIds, string seniorityIds, string timescaleNames);
+        List<PersonLevelPayCheck> TimePeriodSummaryByResourcePayCheck(DateTime startDate, DateTime endDate, bool includePersonsWithNoTimeEntries, string personIds, string seniorityIds, string timescaleNames, string personStatusIds);
 
         [OperationContract]
         List<PersonLevelTimeEntriesHistory> TimeEntryAuditReportByPerson(DateTime startDate, DateTime endDate);
