1ebcfc9142de2350df445885aa220a4b93e96a99
thulasiramp <thulasiramp>
2012-05-21 05:58:00 -0500

    Comments - Defect Number: 3034; Defect RecordID: 3030; Server Address: localhost; Server Port: 99; Database Name: Practice Management; Defect Summary: Refresh Project Detail page layout; Defect Description: We need to add a few items to the Project Details page, so it makes most sense to update the layout some so that the page makes more sense and is easier to use:
    1. Project name will appear larger at top of page, with pencil icon to edit title in modal dialog. The date range will appear after the project name, but only after milestones are added (no need to show "Undefined - Undefined").
    2. Project status is more prominent, upper left, and will show color icon indicator.
    3. Rearranged/spaced dropdowns, grouped. Dropdowns or text entry boxes are same size.
    4. Drop text for Practice Area Owner from Practice Area dropdown.
    5. Add "Project Owner" single-select dropdown. Display this value instead of listing the Project Manager(s) in the Project Summary hover information. Populate dropdown with same names as Project Manager(s) dropdown. Owner will have same rights to project, and/or more.
    6. Moved Description and text entry box (ignore pencil/edit icon).
    7. Create headings for Description, Opportunity Linking, and Time Entry Notes.
    8. Opportunity Linking will have link icon in order to prompt selection of Opportunity. Arrow icon will open Oppty in new page. X icon will unlink Oppty from Project (modal to confirm).
    9. Time Entry Notes will have single select dropdown ("Required" or "Optional", default to "Required"). Should only be possible for Admin, Client Director, and Project Owner to change this selection.
    10. Rename "Financials" tab to "Financial Summary" and move between "Attachments" tab (new) and "Commissions" tab.
    11. I've added an edit icon to the Milestone entries that would just allow in-line editing of the Milestone Name (for now), but could be used for more later. Move "Add Milestone" button into the milestone tab.
    12. Get rid of "Allow consultants to change" column in Milestone table. That does not need to be an option anymore.
    13. "Attachments" tab will be a table where the Attach SOW functionality is moved. If possible, can we allow a category for the attachments? Then people could upload "SOW" or "Support Document". The table would list each file name, category, file size, date uploaded, and uploader.
    14. Remove "Billing Info" tab (it's not used, to my knowledge).
    15. Remove "Note" from History tab.
    16. Separate "Project Work Types" from the other tabs and present it on the main page below so that it is always seen.
    
    
    Also, we need to update the Summary page hover info:

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/ProjectDetail.aspx.designer.cs
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/ProjectDetail.aspx.designer.cs
@@ -247,6 +247,15 @@ namespace PraticeManagement {
         protected global::System.Web.UI.WebControls.HiddenField hidPracticeManagementCommissionId;
         
         /// <summary>
+        /// ddlProjectOwner control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList ddlProjectOwner;
+        
+        /// <summary>
         /// chbIsChargeable control.
         /// </summary>
         /// <remarks>
