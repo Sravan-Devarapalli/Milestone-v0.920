f352305fb5ccfc107c8a7af76dd63995e9204f0e
SrinivasM <SrinivasM>
2012-05-25 02:40:00 -0500

    Comments - Defect Number: 3049; Defect RecordID: 3045; Server Address: localhost; Server Port: 99; Database Name: Practice Management; Defect Summary: Create "By Account" report for Time Entry reporting; Defect Description: See below. This report will be accessible by persons with the Business Unit Manager, Client Director, Senior Leadership, and Administrator roles.

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/AccountSummaryByBusinessUnit.proc.sql
@@ -0,0 +1,93 @@
+﻿CREATE PROCEDURE [dbo].[AccountSummaryByBusinessUnit]
+(
+	@AccountId	INT,
+	@BusinessUnitIds	NVARCHAR(MAX) = NULL,
+	@StartDate	DATETIME,
+	@EndDate	DATETIME
+)
+AS
+BEGIN
+	
+	
+	DECLARE @StartDateLocal DATETIME ,
+		@EndDateLocal DATETIME ,
+		@NOW DATE ,
+		@HolidayTimeType INT
+
+	SET @StartDateLocal = CONVERT(DATE, @StartDate)
+	SET @EndDateLocal = CONVERT(DATE, @EndDate)
+	SET @NOW = dbo.GettingPMTime(GETUTCDATE())
+	SET @HolidayTimeType = dbo.GetHolidayTimeTypeId()
+	
+	;WITH PersonTotalStatusHistory
+    AS ( SELECT   PSH.PersonId ,
+                CASE WHEN PSH.StartDate = MinPayStartDate
+                        THEN PS.HireDate
+                        ELSE PSH.StartDate
+                END AS StartDate ,
+                ISNULL(PSH.EndDate, dbo.GetFutureDate()) AS EndDate ,
+                PSH.PersonStatusId
+        FROM     dbo.PersonStatusHistory PSH
+                LEFT JOIN ( SELECT  PSH.PersonId ,
+                                    P.HireDate ,
+                                    MIN(PSH.StartDate) AS MinPayStartDate
+                            FROM    dbo.PersonStatusHistory PSH
+                                    INNER JOIN dbo.Person P ON PSH.PersonId = P.PersonId
+                            WHERE   P.IsStrawman = 0
+                            GROUP BY PSH.PersonId ,
+                                    P.HireDate
+                            HAVING  P.HireDate < MIN(PSH.StartDate)
+                            ) AS PS ON PS.PersonId = PSH.PersonId
+        WHERE    CASE WHEN PSH.StartDate = MinPayStartDate
+                        THEN PS.HireDate
+                        ELSE PSH.StartDate
+                END < @EndDateLocal
+                AND @StartDateLocal < ISNULL(PSH.EndDate,
+                                                dbo.GetFutureDate())
+        )
+
+		SELECT    PG.GroupId, PG.Name, PG.Active,
+                    ROUND(SUM(CASE WHEN TEH.IsChargeable = 1
+                                        AND Pro.ProjectNumber != 'P031000'
+                                    THEN TEH.ActualHours
+                                    ELSE 0
+                                END), 2) AS BillableHours ,
+                    ROUND(SUM(CASE WHEN TEH.IsChargeable = 0
+                                        AND CC.TimeEntrySectionId = 1
+                                        AND Pro.ProjectNumber != 'P031000'
+                                    THEN TEH.ActualHours
+                                    ELSE 0
+                                END), 2) AS NonBillableHours ,
+                    ROUND(SUM(CASE WHEN CC.TimeEntrySectionId = 2
+                                    THEN TEH.ActualHours
+                                    ELSE 0
+                                END), 2) AS BusinessDevelopmentHours,
+					COUNT(DISTINCT CC.ProjectId) AS ProjectsCount
+        FROM      dbo.TimeEntry TE
+                INNER JOIN dbo.TimeEntryHours TEH ON TEH.TimeEntryId = te.TimeEntryId
+                                                AND TE.ChargeCodeDate BETWEEN @StartDateLocal
+                                                AND
+                                                @EndDateLocal
+                INNER JOIN dbo.ChargeCode CC ON CC.Id = TE.ChargeCodeId
+                INNER JOIN dbo.Person P ON P.PersonId = TE.PersonId
+                INNER JOIN PersonTotalStatusHistory PTSH ON PTSH.PersonId = P.PersonId
+                                                AND TE.ChargeCodeDate BETWEEN PTSH.StartDate
+																			AND PTSH.EndDate
+				INNER JOIN dbo.ProjectGroup PG ON PG.GroupId = CC.ProjectGroupId
+				LEFT JOIN dbo.Project Pro ON Pro.ProjectId = CC.ProjectId
+        WHERE CC.ClientId = @AccountId
+				AND	TE.ChargeCodeDate <= ISNULL(P.TerminationDate,
+                                            dbo.GetFutureDate())
+                AND ( CC.timeTypeId != @HolidayTimeType
+                        OR ( CC.timeTypeId = @HolidayTimeType
+                            AND PTSH.PersonStatusId = 1
+                            )
+                    )
+				AND (@BusinessUnitIds IS NULL
+					OR CC.ProjectGroupId IN (SELECT *
+											FROM dbo.ConvertStringListIntoTable(@BusinessUnitIds)
+											)
+					)
+        GROUP BY  PG.GroupId, PG.Name, PG.Active
+
+END
