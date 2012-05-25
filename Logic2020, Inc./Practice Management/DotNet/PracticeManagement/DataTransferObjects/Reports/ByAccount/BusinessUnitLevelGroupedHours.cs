346de82400ae7f4f3e39e45192116564025ecae5
thulasiramp <thulasiramp>
2012-05-25 05:40:00 -0500

    Comments - Defect Number: 3049; Defect RecordID: 3045; Server Address: localhost; Server Port: 99; Database Name: Practice Management; Defect Summary: Create "By Account" report for Time Entry reporting; Defect Description: See below. This report will be accessible by persons with the Business Unit Manager, Client Director, Senior Leadership, and Administrator roles.

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/DataTransferObjects/Reports/ByAccount/BusinessUnitLevelGroupedHours.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+
+namespace DataTransferObjects.Reports.ByAccount
+{
+
+    [DataContract]
+    [Serializable]
+    public class BusinessUnitLevelGroupedHours
+    {
+        [DataMember]
+        public ProjectGroup BusinessUnit
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public double BillableHours
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public double NonBillableHours
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public double BusinessDevelopmentHours
+        {
+            get;
+            set;
+        }
+
+
+
+        [DataMember]
+        public double ForecastedHours
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public int BusinessUnitTotalHoursPercent
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public double ProjectsCount { get; set; }
+
+        public double TotalHours
+        {
+            get
+            {
+                return BillableHours + NonBillableHours + BusinessDevelopmentHours;
+            }
+        }
+
+
+    }
+}
+
