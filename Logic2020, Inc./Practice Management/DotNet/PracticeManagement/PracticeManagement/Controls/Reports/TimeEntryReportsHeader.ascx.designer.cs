0dd3f2bea366ba1ed4ced31741d592f49fd37f1b
thulasiramp <thulasiramp>
2012-05-28 08:27:00 -0500

    Comments - Defect Number: 3049; Defect RecordID: 3045; Server Address: localhost; Server Port: 99; Database Name: Practice Management; Defect Summary: Create "By Account" report for Time Entry reporting; Defect Description: See below. This report will be accessible by persons with the Business Unit Manager, Client Director, Senior Leadership, and Administrator roles.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/Controls/Reports/TimeEntryReportsHeader.ascx.designer.cs
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/Controls/Reports/TimeEntryReportsHeader.ascx.designer.cs
@@ -49,6 +49,24 @@ namespace PraticeManagement.Controls.Reports {
         protected global::System.Web.UI.WebControls.HyperLink hlByTimePeriod;
         
         /// <summary>
+        /// thByAccount control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlTableCell thByAccount;
+        
+        /// <summary>
+        /// hlnkByAccount control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HyperLink hlnkByAccount;
+        
+        /// <summary>
         /// thProject control.
         /// </summary>
         /// <remarks>
