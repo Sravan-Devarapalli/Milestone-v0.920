1ac00fad1d0ed7432ebd05e1167cf16ad3afce12
SrinivasM <SrinivasM>
2012-05-28 09:22:00 -0500

    Comments - Defect Number: 3049; Defect RecordID: 3045; Server Address: localhost; Server Port: 99; Database Name: Practice Management; Defect Summary: Create "By Account" report for Time Entry reporting; Defect Description: See below. This report will be accessible by persons with the Business Unit Manager, Client Director, Senior Leadership, and Administrator roles.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/Controls/Reports/ByAccount/ByBusinessDevelopment.ascx.cs
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/Controls/Reports/ByAccount/ByBusinessDevelopment.ascx.cs
@@ -9,9 +9,53 @@ namespace PraticeManagement.Controls.Reports.ByAccount
 {
     public partial class ByBusinessDevelopment : System.Web.UI.UserControl
     {
+        #region Properties
+
+        private const string Text_GroupByBusinessUnit = "Group By Business Unit";
+        private const string Text_GroupByPerson = "Group By Person";
+        
+        private PraticeManagement.Reporting.AccountSummaryReport HostingPage
+        {
+            get { return ((PraticeManagement.Reporting.AccountSummaryReport)Page); }
+        }
+
+        #endregion
+
         protected void Page_Load(object sender, EventArgs e)
         {
+        }
 
+        protected void btnGroupBy_Click(object sender, EventArgs e)
+        {
+            PopulateByBusinessDevelopment();
+        }
+
+        private void PopulateGroupByBusinessUnit()
+        {
+            tpByBusinessUnit.PopulateData(HostingPage.AccountId, HostingPage.BusinessUnitIds, HostingPage.StartDate.Value, HostingPage.EndDate.Value);
+        }
+
+        private void PopulateGroupByPerson()
+        {
+            tpByPerson.PopulateData(HostingPage.AccountId, HostingPage.BusinessUnitIds, HostingPage.StartDate.Value, HostingPage.EndDate.Value);
+        }
+
+        public void PopulateByBusinessDevelopment()
+        {
+            if (btnGroupBy.Text == Text_GroupByPerson)
+            {
+                btnGroupBy.Text = Text_GroupByBusinessUnit;
+                btnGroupBy.ToolTip = Text_GroupByBusinessUnit;
+                mvBusinessDevelopmentReport.ActiveViewIndex = 1;
+                PopulateGroupByBusinessUnit();
+            }
+            else
+            {
+                mvBusinessDevelopmentReport.ActiveViewIndex = 0;
+                btnGroupBy.Text = Text_GroupByPerson;
+                btnGroupBy.ToolTip = Text_GroupByPerson;
+                PopulateGroupByPerson();
+            }
         }
     }
 }
