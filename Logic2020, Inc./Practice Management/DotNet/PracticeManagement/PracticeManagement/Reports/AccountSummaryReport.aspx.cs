bf77be89aa46d3b65b24b6de4a1183031a425fb9
SrinivasM <SrinivasM>
2012-05-28 05:41:00 -0500

    Comments - Defect Number: 3049; Defect RecordID: 3045; Server Address: localhost; Server Port: 99; Database Name: Practice Management; Defect Summary: Create "By Account" report for Time Entry reporting; Defect Description: See below. This report will be accessible by persons with the Business Unit Manager, Client Director, Senior Leadership, and Administrator roles.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/Reports/AccountSummaryReport.aspx.cs
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/Reports/AccountSummaryReport.aspx.cs
@@ -4,29 +4,368 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using PraticeManagement.Controls;
 
-namespace PraticeManagement.Reports
+namespace PraticeManagement.Reporting
 {
-    public partial class AccountSummaryReport : System.Web.UI.Page
+    public partial class AccountSummaryReport : PracticeManagementPageBase
     {
+        #region Properties
+
+        public int AccountId
+        {
+            get
+            {
+                return Convert.ToInt32(ddlAccount.SelectedValue);
+            }
+        }
+
+        public String BusinessUnitIds
+        {
+            get
+            {
+                return cblProjectGroup.SelectedItems;
+            }
+        }
+
+        public DateTime? StartDate
+        {
+            get
+            {
+                int selectedVal = 0;
+                if (int.TryParse(ddlPeriod.SelectedValue, out selectedVal))
+                {
+                    if (selectedVal == 0)
+                    {
+                        return diRange.FromDate.Value;
+                    }
+                    else
+                    {
+                        var now = Utils.Generic.GetNowWithTimeZone();
+                        if (selectedVal > 0)
+                        {
+                            if (selectedVal == 7)
+                            {
+                                return Utils.Calendar.WeekStartDate(now);
+                            }
+                            else if (selectedVal == 30)
+                            {
+                                return Utils.Calendar.MonthStartDate(now);
+                            }
+                            else if (selectedVal == 15)
+                            {
+                                return Utils.Calendar.PayrollCurrentStartDate(now);
+                            }
+                            else
+                            {
+                                return Utils.Calendar.YearStartDate(now);
+                            }
+
+                        }
+                        else if (selectedVal < 0)
+                        {
+                            if (selectedVal == -7)
+                            {
+                                return Utils.Calendar.LastWeekStartDate(now);
+                            }
+                            else if (selectedVal == -15)
+                            {
+                                return Utils.Calendar.PayrollPerviousStartDate(now);
+                            }
+                            else if (selectedVal == -30)
+                            {
+                                return Utils.Calendar.LastMonthStartDate(now);
+                            }
+                            else
+                            {
+                                return Utils.Calendar.LastYearStartDate(now);
+                            }
+                        }
+                        else
+                        {
+                            return diRange.FromDate.Value;
+                        }
+                    }
+                }
+                return null;
+            }
+        }
+
+        public DateTime? EndDate
+        {
+            get
+            {
+                int selectedVal = 0;
+                if (int.TryParse(ddlPeriod.SelectedValue, out selectedVal))
+                {
+                    if (selectedVal == 0)
+                    {
+                        return diRange.ToDate.Value;
+                    }
+                    else
+                    {
+                        var now = Utils.Generic.GetNowWithTimeZone();
+                        DateTime firstDay = new DateTime(now.Year, now.Month, 1);
+                        if (selectedVal > 0)
+                        {
+                            //7
+                            //15
+                            //30
+                            //365
+                            if (selectedVal == 7)
+                            {
+                                return Utils.Calendar.WeekEndDate(now);
+                            }
+                            else if (selectedVal == 15)
+                            {
+                                return Utils.Calendar.PayrollCurrentEndDate(now);
+                            }
+                            else if (selectedVal == 30)
+                            {
+                                return Utils.Calendar.MonthEndDate(now);
+                            }
+                            else
+                            {
+                                return Utils.Calendar.YearEndDate(now);
+                            }
+                        }
+                        else if (selectedVal < 0)
+                        {
+                            if (selectedVal == -7)
+                            {
+                                return Utils.Calendar.LastWeekEndDate(now);
+                            }
+                            else if (selectedVal == -15)
+                            {
+                                return Utils.Calendar.PayrollPerviousEndDate(now);
+                            }
+                            else if (selectedVal == -30)
+                            {
+                                return Utils.Calendar.LastMonthEndDate(now);
+                            }
+                            else
+                            {
+                                return Utils.Calendar.LastYearEndDate(now);
+                            }
+                        }
+                        else
+                        {
+                            return diRange.ToDate.Value;
+                        }
+                    }
+                }
+                return null;
+            }
+        }
+
+        public String RangeSelected
+        {
+            get
+            {
+                return ddlPeriod.SelectedValue;
+            }
+        }
+
+        public String Range
+        {
+            get
+            {
+                string range = string.Empty;
+                if (StartDate.HasValue && EndDate.HasValue)
+                {
+                    if (StartDate.Value == Utils.Calendar.MonthStartDate(StartDate.Value) && EndDate.Value == Utils.Calendar.MonthEndDate(StartDate.Value))
+                    {
+                        range = StartDate.Value.ToString("MMMM yyyy");
+                    }
+                    else if (StartDate.Value == Utils.Calendar.YearStartDate(StartDate.Value) && EndDate.Value == Utils.Calendar.YearEndDate(StartDate.Value))
+                    {
+                        range = StartDate.Value.ToString("yyyy");
+                    }
+                    else
+                    {
+                        range = StartDate.Value.ToString(Constants.Formatting.EntryDateFormat) + " - " + EndDate.Value.ToString(Constants.Formatting.EntryDateFormat);
+                    }
+                }
+                return range;
+            }
+        }
+
+        #endregion
+
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!IsPostBack)
+            {
+                DataHelper.FillClientList(ddlAccount, "- - Select Account - -");
+            }
+        }
+
+        protected void ddlAccount_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //Fill BusinessUnits.
+            if (ddlAccount.SelectedIndex != 0)
+            {
+                DataHelper.FillProjectGroupList(cblProjectGroup, Convert.ToInt32(ddlAccount.SelectedValue), null);
+            }
+        }
+
+        protected override void Display()
+        {
+            string rangeSelected = Request.QueryString[Constants.QueryStringParameterNames.RangeArgument];
+            if (!string.IsNullOrEmpty(rangeSelected))
+            {
+                if (ddlPeriod.Items.FindByValue(rangeSelected) != null)
+                {
+                    ddlPeriod.SelectedValue = rangeSelected;
+                }
+                if (ddlPeriod.SelectedValue == "0")
+                {
+                    DateTime? startDate = GetArgumentDateTime(Constants.QueryStringParameterNames.StartDateArgument);
+                    DateTime? endDate = GetArgumentDateTime(Constants.QueryStringParameterNames.EndDateArgument);
+                    var now = Utils.Generic.GetNowWithTimeZone();
+                    diRange.FromDate = startDate.HasValue ? startDate : Utils.Calendar.WeekStartDate(now);
+                    diRange.ToDate = endDate.HasValue ? endDate : Utils.Calendar.WeekEndDate(now);
+                }
+            }
+        }
+
+        protected void Page_Prerender(object sender, EventArgs e)
+        {
+            var now = Utils.Generic.GetNowWithTimeZone();
+            diRange.FromDate = StartDate.HasValue ? StartDate : Utils.Calendar.WeekStartDate(now);
+            diRange.ToDate = EndDate.HasValue ? EndDate : Utils.Calendar.WeekEndDate(now);
+            lblCustomDateRange.Text = string.Format("({0}&nbsp;-&nbsp;{1})",
+                    diRange.FromDate.Value.ToString(Constants.Formatting.EntryDateFormat),
+                    diRange.ToDate.Value.ToString(Constants.Formatting.EntryDateFormat)
+                    );
+
+            if (ddlPeriod.SelectedValue == "0")
+            {
+                lblCustomDateRange.Attributes.Add("class", "");
+                imgCalender.Attributes.Add("class", "");
+            }
+            else
+            {
+                lblCustomDateRange.Attributes.Add("class", "displayNone");
+                imgCalender.Attributes.Add("class", "displayNone");
+            }
+
+            hdnStartDate.Value = diRange.FromDate.Value.ToString(Constants.Formatting.EntryDateFormat);
+            hdnEndDate.Value = diRange.ToDate.Value.ToString(Constants.Formatting.EntryDateFormat);
+
+            if (!IsPostBack)
+            {
+                SelectView();
+            }
+        }
+
+        protected void btnView_Command(object sender, CommandEventArgs e)
+        {
+            int viewIndex = int.Parse((string)e.CommandArgument);
+            SwitchView((Control)sender, viewIndex);
+        }
+
+        private void SelectView()
+        {
+            if (ddlPeriod.SelectedValue != "Please Select")
+            {
+                divWholePage.Style.Remove("display");
+                LoadActiveView();
+            }
+            else
+            {
+                divWholePage.Style.Add("display", "none");
+            }
 
         }
 
-        protected void btnCustDatesCancel_OnClick(object sender, EventArgs e)
+        private void SwitchView(Control control, int viewIndex)
         {
+            SelectView(control, viewIndex);
+            LoadActiveView();
+        }
 
+        private void SetCssClassEmpty()
+        {
+            foreach (TableCell cell in tblProjectViewSwitch.Rows[0].Cells)
+            {
+                cell.CssClass = string.Empty;
+            }
+        }
+
+        public void SelectView(Control sender, int viewIndex)
+        {
+            mvAccountReport.ActiveViewIndex = viewIndex;
+
+            SetCssClassEmpty();
+
+            ((WebControl)sender.Parent).CssClass = "SelectedSwitch";
+        }        
+
+        private void LoadActiveView()
+        {
+            if (StartDate.HasValue && EndDate.HasValue)
+            {
+                int activeView = mvAccountReport.ActiveViewIndex;
+                switch (activeView)
+                {
+                    case 0:
+                        PopulateByBusinessUnitReport();
+                        break;
+                    case 1:
+                        PopulateByProjectReport();
+                        break;
+                    case 2:
+                        PopulateByBusinessDevelopmentReport();
+                        break;
+                }
+            }
         }
 
         protected void btnCustDatesOK_Click(object sender, EventArgs e)
         {
-
+            Page.Validate(valSumDateRange.ValidationGroup);
+            if (Page.IsValid)
+            {
+                hdnStartDate.Value = StartDate.Value.Date.ToShortDateString();
+                hdnEndDate.Value = EndDate.Value.Date.ToShortDateString();
+                SelectView();
+            }
+            else
+            {
+                mpeCustomDates.Show();
+            }
         }
 
         protected void ddlPeriod_SelectedIndexChanged(object sender, EventArgs e)
         {
+            if (ddlPeriod.SelectedValue != "0")
+            {
+                SelectView();
+            }
+            else
+            {
+                mpeCustomDates.Show();
+            }
+        }
 
+        protected void btnCustDatesCancel_OnClick(object sender, EventArgs e)
+        {
+            diRange.FromDate = Convert.ToDateTime(hdnStartDate.Value);
+            diRange.ToDate = Convert.ToDateTime(hdnEndDate.Value);
+        }
+
+        private void PopulateByBusinessUnitReport()
+        {
+            tpByBusinessUnit.PopulateByBusinessUnitReport();
+        }
+
+        private void PopulateByProjectReport()
+        { 
+        }
+
+        private void PopulateByBusinessDevelopmentReport()
+        { 
         }
     }
 }
