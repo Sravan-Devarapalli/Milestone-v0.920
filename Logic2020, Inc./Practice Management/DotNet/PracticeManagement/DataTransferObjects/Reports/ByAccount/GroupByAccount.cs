d3e34849336084f86a71132a3790004441bbcd95
SrinivasM <SrinivasM>
2012-05-29 07:19:00 -0500

    Comments - Defect Number: 3049; Defect RecordID: 3045; Server Address: localhost; Server Port: 99; Database Name: Practice Management; Defect Summary: Create "By Account" report for Time Entry reporting; Defect Description: See below. This report will be accessible by persons with the Business Unit Manager, Client Director, Senior Leadership, and Administrator roles.

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/DataTransferObjects/Reports/ByAccount/GroupByAccount.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+
+namespace DataTransferObjects.Reports.ByAccount
+{
+    [DataContract]
+    [Serializable]
+    public class GroupByAccount
+    {
+        [DataMember]
+        public List<BusinessUnitLevelGroupedHours> GroupedBusinessUnits { get; set; }
+
+        [DataMember]
+        public List<ProjectLevelGroupedHours> GroupedProjects { get; set; }
+
+        [DataMember]
+        public Client Account { get; set; }
+
+        public int BusinessUnitsCount 
+        {
+            get
+            {
+                if (GroupedBusinessUnits != null)
+                {
+                    return GroupedBusinessUnits.Count;
+                }
+
+                return GroupedProjects.Select(p => p.Project.Group.Id).Distinct().Count();
+            }
+        }
+
+        public int ProjectsCount 
+        {
+            get
+            {
+                if (GroupedBusinessUnits != null)
+                {
+                    return GroupedBusinessUnits.Sum(g => g.ProjectsCount);
+                }
+                return GroupedProjects.Count;
+            }
+        }
+
+        [DataMember]
+        public int PersonsCount { get; set; }
+
+        public Double TotalProjectHours 
+        {
+            get
+            {
+                if (GroupedBusinessUnits != null)
+                {
+                    return GroupedBusinessUnits.Sum(g => g.TotalHours);
+                }
+                return GroupedProjects.Sum(p => p.TotalHours);
+            }
+        }
+
+        public Double BillableHours
+        {
+            get
+            {
+                if (GroupedBusinessUnits != null)
+                {
+                    return GroupedBusinessUnits.Sum(g => g.BillableHours);
+                }
+                return GroupedProjects.Sum(p => p.BillableHours);
+            }
+        }
+
+        public Double NonBillableHours
+        {
+            get
+            {
+                if (GroupedBusinessUnits != null)
+                {
+                    return GroupedBusinessUnits.Sum(g => g.NonBillableHours);
+                }
+                return GroupedProjects.Sum(p => p.NonBillableHours);
+            }
+        }
+
+        public Double BusinessDevelopmentHours
+        {
+            get
+            {
+                if (GroupedBusinessUnits != null)
+                {
+                    return GroupedBusinessUnits.Sum(g => g.BusinessDevelopmentHours);
+                }
+                return GroupedProjects.Where(p => p.Project.TimeEntrySectionId == (int)TimeEntrySectionType.BusinessDevelopment).Sum(p => p.NonBillableHours);
+            }
+        }
+    }
+}
+
