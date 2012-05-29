28f46a8c43b208be4fecc5542cd2174ff1474f8b
SainathC <SainathC>
2012-05-29 06:05:00 -0500

    Comments - Defect Number: 3049; Defect RecordID: 3045; Server Address: localhost; Server Port: 99; Database Name: Practice Management; Defect Summary: Create "By Account" report for Time Entry reporting; Defect Description: See below. This report will be accessible by persons with the Business Unit Manager, Client Director, Senior Leadership, and Administrator roles.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/Reports/PersonDetailTimeReport.aspx.designer.cs
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/Reports/PersonDetailTimeReport.aspx.designer.cs
@@ -49,6 +49,15 @@ namespace PraticeManagement.Reporting {
         protected global::System.Web.UI.HtmlControls.HtmlTableCell tdFirst;
         
         /// <summary>
+        /// tdSecond control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlTableCell tdSecond;
+        
+        /// <summary>
         /// ddlPerson control.
         /// </summary>
         /// <remarks>
