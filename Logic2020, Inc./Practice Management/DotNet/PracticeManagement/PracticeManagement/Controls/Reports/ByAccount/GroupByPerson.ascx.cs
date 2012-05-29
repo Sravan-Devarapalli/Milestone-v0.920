b611a0c418371ade84f3d50766d0bb94c9ff0b73
thulasiramp <thulasiramp>
2012-05-29 08:30:00 -0500

    Comments - Defect Number: 3049; Defect RecordID: 3045; Server Address: localhost; Server Port: 99; Database Name: Practice Management; Defect Summary: Create "By Account" report for Time Entry reporting; Defect Description: See below. This report will be accessible by persons with the Business Unit Manager, Client Director, Senior Leadership, and Administrator roles.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/Controls/Reports/ByAccount/GroupByPerson.ascx.cs
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/Controls/Reports/ByAccount/GroupByPerson.ascx.cs
@@ -4,19 +4,128 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using DataTransferObjects.Reports.ByAccount;
+using DataTransferObjects.Reports;
+using AjaxControlToolkit;
+using System.Web.Script.Serialization;
+
 
 namespace PraticeManagement.Controls.Reports.ByAccount
 {
     public partial class GroupByPerson : System.Web.UI.UserControl
     {
+
+        private PraticeManagement.Reporting.AccountSummaryReport HostingPage
+        {
+            get { return ((PraticeManagement.Reporting.AccountSummaryReport)Page); }
+        }
+
+
+        private List<string> CollapsiblePanelExtenderClientIds
+        {
+            get;
+            set;
+        }
+
+
         protected void Page_Load(object sender, EventArgs e)
         {
+        }
 
+        private void DatabindbyBusinessUnitDetails(List<DataTransferObjects.Reports.ByAccount.GroupByPerson> reportdata)
+        {
+            if (reportdata.Count > 0)
+            {
+                reportdata = reportdata.OrderBy(bu => bu.Person.Name).ToList();
+                divEmptyMessage.Style["display"] = "none";
+                repPersonsList.Visible = true;
+                repPersonsList.DataSource = reportdata;
+                repPersonsList.DataBind();
+            }
+            else
+            {
+                divEmptyMessage.Style["display"] = "";
+                repPersonsList.Visible = false;
+            }
+
+            HostingPage.ByBusinessDevelopmentControl.ApplyAttributes(reportdata.Count);
+
+        }
+
+        protected void repPersonsList_ItemDataBound(object sender, RepeaterItemEventArgs e)
+        {
+            if (e.Item.ItemType == ListItemType.Header)
+            {
+                CollapsiblePanelExtenderClientIds = new List<string>();
+
+            }
+            else if (e.Item.ItemType == ListItemType.Item || e.Item.ItemType == ListItemType.AlternatingItem)
+            {
+                var cpePerson = e.Item.FindControl("cpePerson") as CollapsiblePanelExtender;
+                CollapsiblePanelExtenderClientIds.Add(cpePerson.BehaviorID);
+
+            }
+            else if (e.Item.ItemType == ListItemType.Footer)
+            {
+                JavaScriptSerializer jss = new JavaScriptSerializer();
+                var output = jss.Serialize(CollapsiblePanelExtenderClientIds);
+                HostingPage.ByBusinessDevelopmentControl.SetExpandCollapseIdsTohiddenField(output);
+            }
+        }
+
+        protected void repBusinessUnit_ItemDataBound(object sender, RepeaterItemEventArgs e)
+        {
+            if (e.Item.ItemType == ListItemType.Header)
+            {
+            }
+            else if (e.Item.ItemType == ListItemType.Item || e.Item.ItemType == ListItemType.AlternatingItem)
+            {
+            }
+            else if (e.Item.ItemType == ListItemType.Footer)
+            {
+            }
+        }
+
+        protected void repDate_ItemDataBound(object sender, RepeaterItemEventArgs e)
+        {
+            if (e.Item.ItemType == ListItemType.Header)
+            {
+
+            }
+            else if (e.Item.ItemType == ListItemType.Item || e.Item.ItemType == ListItemType.AlternatingItem)
+            {
+            }
+        }
+
+        protected string GetDoubleFormat(double value)
+        {
+            return value.ToString(Constants.Formatting.DoubleValue);
+        }
+
+        protected string GetBusinessUnitStatus(bool isActive)
+        {
+            return isActive ? "( Active )" : "( InActive )";
+        }
+
+        protected bool GetNoteVisibility(String note)
+        {
+            if (!String.IsNullOrEmpty(note))
+            {
+                return true;
+            }
+            return false;
+
+        }
+
+        protected string GetDateFormat(DateTime date)
+        {
+            return date.ToString(Constants.Formatting.ReportDateFormat);
         }
 
         public void PopulateData(int accountId, string businessUnitIds, DateTime startDate, DateTime endDate)
         {
-            var data = ServiceCallers.Custom.Report(r => r.AccountReportGroupByPerson(accountId, businessUnitIds, startDate, endDate));
+            var data = ServiceCallers.Custom.Report(r => r.AccountReportGroupByPerson(accountId, businessUnitIds, startDate, endDate)).ToList();
+            DatabindbyBusinessUnitDetails(data);
         }
 
     }
