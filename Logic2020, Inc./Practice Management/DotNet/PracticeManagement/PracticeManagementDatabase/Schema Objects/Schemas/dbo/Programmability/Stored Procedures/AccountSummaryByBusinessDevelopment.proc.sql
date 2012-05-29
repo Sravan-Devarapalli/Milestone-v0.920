9b54507c549157014e3a0b68b20e8911857223ec
SrinivasM <SrinivasM>
2012-05-29 07:19:00 -0500

    Comments - Defect Number: 3049; Defect RecordID: 3045; Server Address: localhost; Server Port: 99; Database Name: Practice Management; Defect Summary: Create "By Account" report for Time Entry reporting; Defect Description: See below. This report will be accessible by persons with the Business Unit Manager, Client Director, Senior Leadership, and Administrator roles.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/AccountSummaryByBusinessDevelopment.proc.sql
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/AccountSummaryByBusinessDevelopment.proc.sql
@@ -13,44 +13,46 @@ BEGIN
 		@AccountIdLocal INT,
 		@BusinessUnitIdsLocal	NVARCHAR(MAX)
 
-SELECT @StartDateLocal = CONVERT(DATE, @StartDate)
-	 , @EndDateLocal = CONVERT(DATE, @EndDate)
-	 , @AccountIdLocal = @AccountId
-	 , @BusinessUnitIdsLocal = @BusinessUnitIds
+	SELECT @StartDateLocal = CONVERT(DATE, @StartDate)
+		 , @EndDateLocal = CONVERT(DATE, @EndDate)
+		 , @AccountIdLocal = @AccountId
+		 , @BusinessUnitIdsLocal = @BusinessUnitIds
 
-SELECT PG.GroupId AS [BusinessUnitId]
-	 , PG.Name AS [BusinessUnitName]
-	 , PG.Active
-	 , TT.Name AS [TimeTypeName]
-	 , P.PersonId
-	 , P.FirstName
-	 , P.LastName
-	 , TE.ChargeCodeDate
-	 , TEH.ActualHours AS NonBillableHours
-	 , TE.Note
-FROM
-	dbo.TimeEntry TE
-	INNER JOIN dbo.TimeEntryHours TEH
-		ON TEH.TimeEntryId = TE.TimeEntryId AND TE.ChargeCodeDate BETWEEN @StartDateLocal AND @EndDateLocal
-	INNER JOIN dbo.ChargeCode CC
-		ON CC.Id = TE.ChargeCodeId AND CC.TimeEntrySectionId = 2 --Here 2 is Business Development section.
-	INNER JOIN dbo.ProjectGroup PG
-		ON PG.GroupId = CC.ProjectGroupId
-	INNER JOIN dbo.TimeType AS TT
-		ON TT.TimeTypeId = CC.TimeTypeId
-	INNER JOIN dbo.Person P
-		ON P.PersonId = TE.PersonId AND TE.ChargeCodeDate <= ISNULL(P.TerminationDate, dbo.GetFutureDate())
+	SELECT PG.GroupId AS [BusinessUnitId]
+		 , PG.Name AS [BusinessUnitName]
+		 , PG.Code AS [GroupCode]
+		 , PG.Active
+		 , TT.Name AS [TimeTypeName]
+		 , TT.Code AS [TimeTypeCode]
+		 , P.PersonId
+		 , P.FirstName
+		 , P.LastName
+		 , TE.ChargeCodeDate
+		 , TEH.ActualHours AS NonBillableHours
+		 , TE.Note
+	FROM
+		dbo.TimeEntry TE
+		INNER JOIN dbo.TimeEntryHours TEH
+			ON TEH.TimeEntryId = TE.TimeEntryId AND TE.ChargeCodeDate BETWEEN @StartDateLocal AND @EndDateLocal
+		INNER JOIN dbo.ChargeCode CC
+			ON CC.Id = TE.ChargeCodeId AND CC.TimeEntrySectionId = 2 --Here 2 is Business Development section.
+		INNER JOIN dbo.ProjectGroup PG
+			ON PG.GroupId = CC.ProjectGroupId
+		INNER JOIN dbo.TimeType AS TT
+			ON TT.TimeTypeId = CC.TimeTypeId
+		INNER JOIN dbo.Person P
+			ON P.PersonId = TE.PersonId AND TE.ChargeCodeDate <= ISNULL(P.TerminationDate, dbo.GetFutureDate())
 
-WHERE
-	CC.ClientId = @AccountIdLocal
-	AND (@BusinessUnitIdsLocal IS NULL
-	OR PG.GroupId IN (SELECT ResultId
-					  FROM
-						  dbo.ConvertStringListIntoTable(@BusinessUnitIdsLocal)))
-ORDER BY
-	P.LastName
-  , P.FirstName
-  , TE.ChargeCodeDate
-  , TT.Name
+	WHERE
+		CC.ClientId = @AccountIdLocal
+		AND (@BusinessUnitIdsLocal IS NULL
+		OR PG.GroupId IN (SELECT ResultId
+						  FROM
+							  dbo.ConvertStringListIntoTable(@BusinessUnitIdsLocal)))
+	ORDER BY
+		P.LastName
+	  , P.FirstName
+	  , TE.ChargeCodeDate
+	  , TT.Name
 
  END
