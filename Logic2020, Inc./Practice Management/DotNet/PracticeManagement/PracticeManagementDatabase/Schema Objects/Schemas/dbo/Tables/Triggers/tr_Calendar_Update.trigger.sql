e8f91f12e8385857b4cd3349f9e7cd332e39a74a
thulasiramp <thulasiramp>
2012-05-31 01:59:00 -0500

    Comments - Defect Number: 3052; Defect RecordID: 3048; Server Address: localhost; Server Port: 99; Database Name: Practice Management; Defect Summary: Milestone hours not being calculated correctly for one resource, only affecting a single week.; Defect Description:
    Another example:

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/Triggers/tr_Calendar_Update.trigger.sql
@@ -0,0 +1,23 @@
+﻿-- =============================================
+-- Author:		ThulasiRam.P
+-- Create date: 30-05-2012
+-- Updated by:	ThulasiRam.P
+-- Update date: 30-05-2012
+-- Description:	Updates a PersonCalendarAuto table according to the data in the Calendar and PersonCalendar table
+-- =============================================
+CREATE TRIGGER [dbo].[tr_Calendar_Update]
+ON [dbo].[Calendar]
+AFTER  UPDATE
+AS
+BEGIN
+SET NOCOUNT ON;
+
+	UPDATE PCA
+	SET PCA.DayOff = ISNULL(pcal.DayOff, i.DayOff)
+	FROM dbo.PersonCalendarAuto AS PCA
+	INNER JOIN INSERTED AS i ON i.Date = PCA.Date
+	INNER JOIN dbo.Person AS p ON  p.PersonId = PCA.PersonId
+	LEFT JOIN dbo.PersonCalendar AS pcal ON pcal.Date = i.Date AND pcal.PersonId = p.PersonId AND PCA.PersonId = pcal.PersonId
+	WHERE PCA.DayOff <> ISNULL(pcal.DayOff, i.DayOff) 
+
+END
