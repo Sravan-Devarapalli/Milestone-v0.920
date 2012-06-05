8922d546f0f207fc135a1a205b020847e576212a
thulasiramp <thulasiramp>
2012-06-05 09:29:00 -0500

    Comments - Defect Number: 3017; Defect RecordID: 3013; Server Address: localhost; Server Port: 99; Database Name: Practice Management; Defect Summary: By Time Period/By Resource Report: Add Drilldown functionality, on clicking a row in a report to pass parameters to a more d@; Defect Description: Drilldown functionality, which would allow clicking a row in a report to pass parameters (person/project, date range) to a more detailed report, with UI to allow user to nav back to original report easily. Must be quick/cached. Example: Clicking a single person row in By Time Period/By Resource report would pass name and date range parameters to By Person report so that user could view Person detail. Example 2: Clicking a single project row in By Time Period/By Project report would pass project number (imply Entire Project, By Resource) to By Project report so that user could view Project detail.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/Controls/Reports/ProjectDetailTabByResource.ascx.designer.cs
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/Controls/Reports/ProjectDetailTabByResource.ascx.designer.cs
@@ -31,6 +31,15 @@ namespace PraticeManagement.Controls.Reports {
         protected global::System.Web.UI.WebControls.HiddenField hdnCollapsed;
         
         /// <summary>
+        /// tblExportSection control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlTable tblExportSection;
+        
+        /// <summary>
         /// btnExpandOrCollapseAll control.
         /// </summary>
         /// <remarks>
