c252c4bb4edef25b4053f328c535b24549ae23fd
SrinivasM <SrinivasM>
2012-06-11 03:46:00 -0500

    Comments - Defect Number: 3009; Defect RecordID: 3005; Server Address: localhost; Server Port: 99; Database Name: Practice Management; Defect Summary: Time entry: Notes are required or not is not dependent on the Practice a person is assigned to (as it is now), but instead tog@; Defect Description: Time Entry - Notes functionality: Notes are required or not is not dependent on the Practice a person is assigned to (as it is now), but instead toggled on/off (required/optional) per Project. Default to required. Notes always required for hours entered in Non-Billable for Projects, even if notes are set to optional in project. Will also add icon to Time Entry page to indicate which projects require notes and which are optional (design to come).

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/ClientDetails.aspx.designer.cs
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/ClientDetails.aspx.designer.cs
@@ -67,6 +67,15 @@ namespace PraticeManagement {
         protected global::System.Web.UI.WebControls.CheckBox chbIsInternal;
         
         /// <summary>
+        /// chbIsNoteRequired control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.CheckBox chbIsNoteRequired;
+        
+        /// <summary>
         /// txtClientName control.
         /// </summary>
         /// <remarks>
