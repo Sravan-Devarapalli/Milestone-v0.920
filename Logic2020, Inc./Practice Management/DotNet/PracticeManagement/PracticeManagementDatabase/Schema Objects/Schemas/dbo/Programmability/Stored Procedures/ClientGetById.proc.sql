bab44359969acc7bd098af657ab535cd0522a755
thulasiramp <thulasiramp>
2012-06-11 02:56:00 -0500

    Comments - Defect Number: 3009; Defect RecordID: 3005; Server Address: localhost; Server Port: 99; Database Name: Practice Management; Defect Summary: Time entry: Notes are required or not is not dependent on the Practice a person is assigned to (as it is now), but instead tog@; Defect Description: Time Entry - Notes functionality: Notes are required or not is not dependent on the Practice a person is assigned to (as it is now), but instead toggled on/off (required/optional) per Project. Default to required. Notes always required for hours entered in Non-Billable for Projects, even if notes are set to optional in project. Will also add icon to Time Entry page to indicate which projects require notes and which are optional (design to come).

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/ClientGetById.proc.sql
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/ClientGetById.proc.sql
@@ -1,15 +1,16 @@
-﻿--The source is 
--- =============================================
+﻿-- =============================================
 -- Author:		Anatoliy Lokshin
 -- Create date: 5-21-2008
--- Description:	Gets a person
+-- Updated by:	ThulasiRam.P
+-- Update date: 6-08-2012
+-- Description:	Gets a client
 -- =============================================
 CREATE PROCEDURE dbo.ClientGetById
 (
 	@ClientId	INT
 )
 AS
-	SET NOCOUNT ON
+	SET NOCOUNT ON;
 
 	SELECT c.ClientId,
 	       c.DefaultDiscount,
@@ -21,7 +22,8 @@ AS
 	       c.IsChargeable,
 		   c.IsMarginColorInfoEnabled,
 		   c.IsInternal,
-		   c.Code AS ClientCode
+		   c.Code AS ClientCode,
+		   c.IsNoteRequired
 	  FROM dbo.Client AS c
 	 WHERE c.ClientId = @ClientId
 
