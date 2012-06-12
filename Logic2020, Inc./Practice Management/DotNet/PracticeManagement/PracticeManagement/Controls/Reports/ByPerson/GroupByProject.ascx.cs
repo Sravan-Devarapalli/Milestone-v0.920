40dcff84f123c27cc4882157a6c2411ed15d1e30
SrinivasM <SrinivasM>
2012-06-12 01:51:00 -0500

    Comments - Defect Number: 3017; Defect RecordID: 3013; Server Address: localhost; Server Port: 99; Database Name: Practice Management; Defect Summary: By Time Period/By Resource Report: Add Drilldown functionality, on clicking a row in a report to pass parameters to a more d@; Defect Description: Drilldown functionality, which would allow clicking a row in a report to pass parameters (person/project, date range) to a more detailed report, with UI to allow user to nav back to original report easily. Must be quick/cached. Example: Clicking a single person row in By Time Period/By Resource report would pass name and date range parameters to By Person report so that user could view Person detail. Example 2: Clicking a single project row in By Time Period/By Project report would pass project number (imply Entire Project, By Resource) to By Project report so that user could view Project detail.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/Controls/Reports/ByPerson/GroupByProject.ascx.cs
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/Controls/Reports/ByPerson/GroupByProject.ascx.cs
@@ -11,87 +11,17 @@ namespace PraticeManagement.Controls.Reports.ByPerson
 {
     public partial class GroupByProject : System.Web.UI.UserControl
     {
-        private int SectionId
-        {
-            get;
-            set;
-        }
-
         public void DatabindRepepeaterPersonDetails(List<TimeEntriesGroupByClientAndProject> timeEntriesGroupByClientAndProjectList, string name)
         {
             lblPerson.Text = name;
             lblTotalHours.Text = GetDoubleFormat(timeEntriesGroupByClientAndProjectList.Sum(p => p.TotalHours));
-            repProjects.Visible = true;
-            repProjects.DataSource = timeEntriesGroupByClientAndProjectList;
-            repProjects.DataBind();
-        }
-
-        protected string GetDateFormat(DateTime date)
-        {
-            return date.ToString(Constants.Formatting.ReportDateFormat);
-        }
-
-        protected bool GetNonBillableImageVisibility(int sectionId, double nonBillableHours)
-        {
-            return sectionId == -1 ? SectionId == 1 && nonBillableHours > 0 : sectionId == 1 && nonBillableHours > 0;
-        }
-
-        protected string GetProjectStatus(string status)
-        {
-            return string.IsNullOrEmpty(status) ? "" : "(" + status + ")";
+            ucPersonDetailReport.DatabindRepepeaterPersonDetails(timeEntriesGroupByClientAndProjectList);
         }
 
         protected string GetDoubleFormat(double value)
         {
             return value.ToString(Constants.Formatting.DoubleValue);
         }
-
-        protected bool GetNoteVisibility(String note)
-        {
-            if (String.IsNullOrEmpty(note))
-            {
-                return false;
-            }
-            return true;
-
-        }
-
-        protected void repProjects_ItemDataBound(object sender, RepeaterItemEventArgs e)
-        {
-            if (e.Item.ItemType == ListItemType.Header)
-            {
-
-
-            }
-            else if (e.Item.ItemType == ListItemType.Item || e.Item.ItemType == ListItemType.AlternatingItem)
-            {
-                var repDate = e.Item.FindControl("repDate") as Repeater;
-                TimeEntriesGroupByClientAndProject dataitem = (TimeEntriesGroupByClientAndProject)e.Item.DataItem;
-                SectionId = dataitem.Project.TimeEntrySectionId;
-
-                repDate.DataSource = dataitem.DayTotalHours;
-                repDate.DataBind();
-            }
-            else if (e.Item.ItemType == ListItemType.Footer)
-            {
-            }
-        }
-
-        protected void repDate_ItemDataBound(object sender, RepeaterItemEventArgs e)
-        {
-            if (e.Item.ItemType == ListItemType.Header)
-            {
-
-            }
-            else if (e.Item.ItemType == ListItemType.Item || e.Item.ItemType == ListItemType.AlternatingItem)
-            {
-                var repWorktype = e.Item.FindControl("repWorktype") as Repeater;
-                TimeEntriesGroupByDate dataitem = (TimeEntriesGroupByDate)e.Item.DataItem;
-                repWorktype.DataSource = dataitem.DayTotalHoursList;
-                var rep = sender as Repeater;
-                repWorktype.DataBind();
-            }
-        }
-
     }
 }
+
