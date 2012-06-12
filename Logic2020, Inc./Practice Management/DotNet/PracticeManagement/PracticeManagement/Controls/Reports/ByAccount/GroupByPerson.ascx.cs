3d60ee99055505a6ca4ad67d29676bdfcf03897b
SainathC <SainathC>
2012-06-12 07:30:00 -0500

    Comments - Defect Number: 3017; Defect RecordID: 3013; Server Address: localhost; Server Port: 99; Database Name: Practice Management; Defect Summary: By Time Period/By Resource Report: Add Drilldown functionality, on clicking a row in a report to pass parameters to a more d@; Defect Description: Drilldown functionality, which would allow clicking a row in a report to pass parameters (person/project, date range) to a more detailed report, with UI to allow user to nav back to original report easily. Must be quick/cached. Example: Clicking a single person row in By Time Period/By Resource report would pass name and date range parameters to By Person report so that user could view Person detail. Example 2: Clicking a single project row in By Time Period/By Project report would pass project number (imply Entire Project, By Resource) to By Project report so that user could view Project detail.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/Controls/Reports/ByAccount/GroupByPerson.ascx.cs
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/Controls/Reports/ByAccount/GroupByPerson.ascx.cs
@@ -8,25 +8,18 @@ using DataTransferObjects.Reports.ByAccount;
 using DataTransferObjects.Reports;
 using AjaxControlToolkit;
 using System.Web.Script.Serialization;
+using PraticeManagement.Reporting;
 
 
 namespace PraticeManagement.Controls.Reports.ByAccount
 {
     public partial class GroupByPerson : System.Web.UI.UserControl
     {
-
-        private PraticeManagement.Reporting.AccountSummaryReport HostingPage
-        {
-            get { return ((PraticeManagement.Reporting.AccountSummaryReport)Page); }
-        }
-
-
         private List<string> CollapsiblePanelExtenderClientIds
         {
             get;
             set;
         }
-
 
         protected void Page_Load(object sender, EventArgs e)
         {
@@ -48,8 +41,11 @@ namespace PraticeManagement.Controls.Reports.ByAccount
                 repPersonsList.Visible = false;
             }
 
-            HostingPage.ByBusinessDevelopmentControl.ApplyAttributes(reportdata.Count);
-
+            if (Page is AccountSummaryReport)
+            {
+                var hostingPage = (AccountSummaryReport)Page;
+                hostingPage.ByBusinessDevelopmentControl.ApplyAttributes(reportdata.Count);
+            }
         }
 
         protected void repPersonsList_ItemDataBound(object sender, RepeaterItemEventArgs e)
@@ -69,7 +65,11 @@ namespace PraticeManagement.Controls.Reports.ByAccount
             {
                 JavaScriptSerializer jss = new JavaScriptSerializer();
                 var output = jss.Serialize(CollapsiblePanelExtenderClientIds);
-                HostingPage.ByBusinessDevelopmentControl.SetExpandCollapseIdsTohiddenField(output);
+                if (Page is AccountSummaryReport)
+                {
+                    var hostingPage = (AccountSummaryReport)Page;
+                    hostingPage.ByBusinessDevelopmentControl.SetExpandCollapseIdsTohiddenField(output);
+                }
             }
         }
 
@@ -122,26 +122,32 @@ namespace PraticeManagement.Controls.Reports.ByAccount
             return date.ToString(Constants.Formatting.ReportDateFormat);
         }
 
-        public void PopulateData(int accountId, string businessUnitIds, DateTime startDate, DateTime endDate)
+        public double PopulateData(int accountId, string businessUnitIds, DateTime startDate, DateTime endDate)
         {
             var data = ServiceCallers.Custom.Report(r => r.AccountReportGroupByPerson(accountId, businessUnitIds, startDate, endDate)).ToList();
             DatabindbyBusinessUnitDetails(data);
 
             SetHeaderSectionValues(data);
+
+            return data.Sum(d => d.TotalHours);
         }
 
         private void SetHeaderSectionValues(List<DataTransferObjects.Reports.ByAccount.GroupByPerson> reportData)
         {
-            HostingPage.UpdateHeaderSection = true;
+            if (Page is AccountSummaryReport)
+            {
+                var hostingPage = (AccountSummaryReport)Page;
+                hostingPage.UpdateHeaderSection = true;
 
-            HostingPage.BusinessUnitsCount = reportData.SelectMany(p => p.BusinessUnitLevelGroupedHoursList.Select(g => g.BusinessUnit.Id.Value )).Distinct().Count();
-            HostingPage.ProjectsCount = reportData.Count > 0 ? 1 : 0;
-            HostingPage.PersonsCount = reportData.Select(p => p.Person.Id.Value).Distinct().Count();
+                hostingPage.BusinessUnitsCount = reportData.SelectMany(p => p.BusinessUnitLevelGroupedHoursList.Select(g => g.BusinessUnit.Id.Value)).Distinct().Count();
+                hostingPage.ProjectsCount = reportData.Count > 0 ? 1 : 0;
+                hostingPage.PersonsCount = reportData.Select(p => p.Person.Id.Value).Distinct().Count();
 
-            HostingPage.TotalProjectHours = reportData.Sum(p => p.TotalHours);
-            HostingPage.BDHours = reportData.Sum(p => p.TotalHours);
-            HostingPage.BillableHours = 0d;
-            HostingPage.NonBillableHours = reportData.Sum(g => g.TotalHours);
+                hostingPage.TotalProjectHours = reportData.Sum(p => p.TotalHours);
+                hostingPage.BDHours = reportData.Sum(p => p.TotalHours);
+                hostingPage.BillableHours = 0d;
+                hostingPage.NonBillableHours = reportData.Sum(g => g.TotalHours);
+            }
         }
 
     }
