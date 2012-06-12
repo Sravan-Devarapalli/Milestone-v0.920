4ffeee7036150f211cd30faaceeba30fdf50ff7e
SainathC <SainathC>
2012-06-12 05:11:00 -0500

    Comments - Defect Number: 3017; Defect RecordID: 3013; Server Address: localhost; Server Port: 99; Database Name: Practice Management; Defect Summary: By Time Period/By Resource Report: Add Drilldown functionality, on clicking a row in a report to pass parameters to a more d@; Defect Description: Drilldown functionality, which would allow clicking a row in a report to pass parameters (person/project, date range) to a more detailed report, with UI to allow user to nav back to original report easily. Must be quick/cached. Example: Clicking a single person row in By Time Period/By Resource report would pass name and date range parameters to By Person report so that user could view Person detail. Example 2: Clicking a single project row in By Time Period/By Project report would pass project number (imply Entire Project, By Resource) to By Project report so that user could view Project detail.

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/DataTransferObjects/Reports/ByPerson/GroupByPersonByWorktype.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataTransferObjects.Reports
+{
+    public class GroupByPersonByWorktype
+    {
+        public Person Person
+        {
+            get;
+            set;
+        }
+
+        public double BillableHours
+        {
+            get
+            {
+                if (ProjectTotalHoursList != null)
+                {
+                    return ProjectTotalHoursList.Sum(d => d.BillableHours);
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+        }
+
+        public double NonBillableHours
+        {
+            get
+            {
+                if (ProjectTotalHoursList != null)
+                {
+                    return ProjectTotalHoursList.Sum(d => d.NonBillableHours);
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+        }
+
+        public double TotalHours
+        {
+            get
+            {
+                return ProjectTotalHoursList != null ? ProjectTotalHoursList.Sum(p => p.TotalHours) : 0;
+            }
+        }
+
+        public List<TimeEntryByWorkType> ProjectTotalHoursList
+        {
+            get;
+            set;
+        }
+    }
+}
+
