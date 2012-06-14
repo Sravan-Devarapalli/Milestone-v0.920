182813e2055a9b675244f375f322d9538d062cfd
thulasiramp <thulasiramp>
2012-06-14 08:45:00 -0500

    Comments - Defect Number: 3055; Defect RecordID: 3051; Server Address: localhost; Server Port: 99; Database Name: Practice Management; Defect Summary: Update legacy Time Report by Person to point to newer time tables; Defect Description: Can we update the legacy Time Report by Person page to point to the new time tables? We only need to update the cell containing the reported hours to distinguish between B and NB time (show both in same cell). The Account-Project-WorkType column can be updated to show Account - P# - Project Name - Work Type, and we can allocate more width to this column since the table already spills off the page. We do not need to change permissions for this page as only one person will be using it.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/TimeEntriesGetByPersonsForExcel.proc.sql
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/TimeEntriesGetByPersonsForExcel.proc.sql
@@ -1,12 +1,14 @@
 ﻿CREATE PROCEDURE [dbo].[TimeEntriesGetByPersonsForExcel]
-	@PersonIds	NVARCHAR(MAX),
+(   @PersonIds	NVARCHAR(MAX),
 	@StartDate	DATETIME = NULL,
 	@EndDate	DATETIME = NULL,
 	@TimescaleIds NVARCHAR(4000)= NULL,
 	@PracticeIds  NVARCHAR(MAX) = NULL
-	
+)
 AS
 BEGIN
+  
+  SET NOCOUNT ON;
     
 	DECLARE @PersonList TABLE (Id INT)
 	INSERT INTO @PersonList
@@ -25,7 +27,7 @@ BEGIN
 
     ;WITH PersonsFilteredByPersonIdsAndPayIds AS
 	(
-		SELECT Distinct P.PersonId
+		SELECT DISTINCT P.PersonId
 						, P.FirstName
 						, P.LastName
 		FROM Person P
@@ -34,33 +36,49 @@ BEGIN
 		WHERE (@TimescaleIds IS NULL OR pa.Timescale IN (SELECT Id FROM @TimescaleIdList))
 		      AND (@PracticeIds IS NULL) OR ISNULL(pa.PracticeId,P.DefaultPractice) IN (SELECT Id FROM @PracticeIdsList)
 	)
-
-	SELECT DISTINCT 
-		   p.LastName +', ' +p.FirstName Name,
-		   proj.ProjectNumber AS 'P#',
-		   c.Name 'Account',
-		   proj.Name AS 'Project Name',
-		   m.Description AS Milestone,
-		   tt.Name TimeType,
-		   te.Note Note,
-		   CONVERT(VARCHAR(10), te.MilestoneDate, 1)  AS 'Date',
-		   te.ActualHours AS 'Hours'
-	FROM PersonsFilteredByPersonIdsAndPayIds p
-	JOIN dbo.MilestonePerson mp on mp.PersonId = p.PersonId
-	JOIN dbo.MilestonePersonEntry mpe on mpe.MilestonePersonId = mp.MilestonePersonId
-	JOIN dbo.Milestone m on m.MilestoneId = mp.MilestoneId
-	JOIN dbo.Project proj on proj.ProjectId = m.ProjectId
-	JOIN dbo.Client c on proj.ClientId = c.ClientId
-	JOIN dbo.TimeEntries te on te.MilestonePersonId = mp.MilestonePersonId
-									AND te.MilestoneDate BETWEEN ISNULL(@StartDate, te.MilestoneDate) AND ISNULL(@EndDate, te.MilestoneDate)
-	LEFT JOIN dbo.TimeType tt on tt.TimeTypeId = te.TimeTypeId
-	WHERE 	(ISNULL(@StartDate,Mpe.StartDate) >= mpe.StartDate AND ISNULL(@EndDate,mpe.EndDate) <= mpe.EndDate
-			OR ISNULL(@StartDate,Mpe.StartDate - 1) < mpe.StartDate AND ISNULL(@EndDate,mpe.StartDate) >= mpe.StartDate
-			OR ISNULL(@StartDate,Mpe.EndDate) <= mpe.EndDate AND ISNULL(@EndDate,mpe.EndDate + 1) >= mpe.EndDate
-			)
-		
-		AND (te.TimeEntryId IS NOT NULL OR proj.ProjectStatusId = 3) --For getting only active projects or there are timeentries for projects.
-	ORDER BY Name ,[Date], proj.ProjectNumber 
+	SELECT 
+		   p.LastName +', ' +p.FirstName AS Name,
+		   C.Name AS Account,
+		   PG.Name AS [Business Unit],
+		   PROJ.ProjectNumber AS [P#],
+		   PROJ.Name AS [Project Name],
+		   TT.Name AS [Work Type],
+		   TE.ChargeCodeDate AS [Date],
+		   TE.Note ,
+		 ROUND(SUM(CASE
+					WHEN TEH.IsChargeable = 1 AND PROJ.ProjectNumber != 'P031000' THEN
+						TEH.ActualHours
+					ELSE
+						0
+				END), 2) AS [BillableHours],
+		 ROUND(SUM(CASE
+					WHEN TEH.IsChargeable = 0 OR PROJ.ProjectNumber = 'P031000' THEN
+						TEH.ActualHours
+					ELSE
+						0
+				END), 2) AS [NonBillableHours] ,
+          ROUND(SUM(TEH.ActualHours), 2) AS [Total Hours]
+	FROM PersonsFilteredByPersonIdsAndPayIds P
+	INNER JOIN dbo.TimeEntry TE ON TE.PersonId = P.PersonId
+									AND TE.ChargeCodeDate BETWEEN ISNULL(@StartDate, te.ChargeCodeDate) and ISNULL(@EndDate, te.ChargeCodeDate)
+    INNER JOIN dbo.TimeEntryHours AS TEH ON TE.TimeEntryId = TEH.TimeEntryId
+	INNER JOIN dbo.ChargeCode AS CC ON CC.Id = TE.ChargeCodeId				
+	INNER JOIN dbo.Project AS PROJ ON PROJ.ProjectId = CC.ProjectId
+	INNER JOIN dbo.Client AS C ON C.ClientId = CC.ClientId
+	INNER JOIN dbo.TimeType AS TT ON TT.TimeTypeId = CC.TimeTypeId
+	INNER JOIN dbo.ProjectGroup AS PG ON PG.GroupId = CC.ProjectGroupId
+	GROUP BY p.PersonId,
+			 TE.ChargeCodeDate,
+			 TE.ChargeCodeId,
+		     PROJ.ProjectNumber,
+		     PROJ.Name,
+		     C.Name,
+		     TT.Name,
+		     TE.Note,
+			 PG.Name,
+			 p.LastName,
+			 p.FirstName
+	ORDER BY  p.LastName,p.FirstName,C.Name,PG.Name,PROJ.ProjectNumber,TT.Name,TE.ChargeCodeDate
 
 END
 
