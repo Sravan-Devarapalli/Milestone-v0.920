ca4222efa06aaa2326ec0c337d55dfe9493359db
SainathC <SainathC>
2012-06-19 02:11:00 -0500

    Comments - Defect Number: 3049; Defect RecordID: 3045; Server Address: localhost; Server Port: 99; Database Name: Practice Management; Defect Summary: Create "By Account" report for Time Entry reporting; Defect Description: See below. This report will be accessible by persons with the Business Unit Manager, Client Director, Senior Leadership, and Administrator roles.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/Reports/PersonDetailTimeReport.aspx.cs
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/Reports/PersonDetailTimeReport.aspx.cs
@@ -255,23 +255,15 @@ namespace PraticeManagement.Reporting
         protected void Page_PreRender(object sender, EventArgs e)
         {
 
-            if (timeEntryReportHeader.Count == 1)
-            {
-                tdFirst.Style["width"] = timeEntryReportHeader.TdFirstWidth;
-                tdSecond.Style["width"] = timeEntryReportHeader.TdSecondWidth;
-                tdThird.Style["width"] = timeEntryReportHeader.TdThirdWidth;
-            }
-            else if (timeEntryReportHeader.Count == 2)
+            if (timeEntryReportHeader.Count == 2)
             {
                 tdFirst.Style["width"] = "50%";
-                tdSecond.Style["width"] = "25%";
-                tdThird.Style["width"] = timeEntryReportHeader.TdThirdWidth;
+                tdThird.Style["width"] = "20%";
             }
-            else if (timeEntryReportHeader.Count == 3)
+            else if (timeEntryReportHeader.Count == 1)
             {
-                tdFirst.Style["width"] = "63%";
-                tdSecond.Style["width"] = "25%";
-                tdThird.Style["width"] = timeEntryReportHeader.TdThirdWidth;
+                tdFirst.Style["width"] = "35%";
+                tdThird.Style["width"] = "35%";
             }
 
             int personId = int.Parse(ddlPerson.SelectedItem.Value);
