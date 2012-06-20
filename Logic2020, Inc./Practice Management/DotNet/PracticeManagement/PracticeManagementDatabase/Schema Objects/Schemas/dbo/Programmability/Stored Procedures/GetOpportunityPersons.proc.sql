b7cc32bdbb6b79d443b0b1a11cef3d0820c6c9ec
SainathC <SainathC>
2012-06-20 12:55:00 -0500

    Comments - Defect Number: 2978; Defect RecordID: 2974; Server Address: localhost; Server Port: 99; Database Name: Practice Management; Defect Summary: Practice Management password reset issue; Defect Description: Please verify issue as stated below:
    If you request a password reset, then try to type in the temp password (at the URL below) it will fail to login.
    
    https://practice.logic2020.com/Login.aspx
    The URL must be:
    https://practice.logic2020.com/ChangePassword.aspx
    
    for the password reset to work.
    Michael Ashby// Logic20/20 // michaela@logic2020.com
    c.206-372-7926// www.logic2020.com
    
    From: Jason Hong-Turney
    Sent: Wednesday, December 21, 2011 4:51 PM
    To: Michael Ashby
    Subject: RE: Practice Management password reset requested
    
    Sure, of course he does.
    
    BTW, what issue? That you cant click the link, or that you didnt know to?
    
    Jason Hong-Turney // Logic20/20 // jasonht@logic2020.com
    t.206-576-0472 // c. 425-390-4664
    
    From: Michael Ashby
    Sent: Wednesday, December 21, 2011 4:51 PM
    To: Jason Hong-Turney
    Subject: RE: Practice Management password reset requested
    
    J Nikitin has the same issue.
    Michael Ashby// Logic20/20 // michaela@logic2020.com
    c.206-372-7926// www.logic2020.com
    
    From: Jason Hong-Turney
    Sent: Wednesday, December 21, 2011 4:50 PM
    To: Michael Ashby
    Subject: RE: Practice Management password reset requested
    
    Im not sure what you mean can be fixed the link is indeed by design, but you can also just type that password in as your password in PM and it should prompt you to change it. This should say, if anything, New Temporary Password. I can never seem to repro problems other people have with this
    
    Jason Hong-Turney // Logic20/20 // jasonht@logic2020.com
    t.206-576-0472 // c. 425-390-4664
    
    From: Michael Ashby
    Sent: Wednesday, December 21, 2011 4:49 PM
    To: Jason Hong-Turney
    Subject: FW: Practice Management password reset requested
    
    Jason, this is something I hope can be fixed. The user needs to click on the link below to reset password.
    
    Michael Ashby// Logic20/20 // michaela@logic2020.com
    c.206-372-7926// www.logic2020.com
    
    From: Logic20/20 - IT
    Sent: Wednesday, December 21, 2011 12:51 PM
    To: Michael Ashby
    Subject: Practice Management password reset requested
    
    You have or someone on your behalf has requested that your password for Practice Management be reset.
    If you did not make or authorize this request simply ignore this e-mail and continue to use your existing password. If this is a valid request, please click the link below which will open PM in a new browser window where you will be prompted to change your password. This new temporary password will automatically expire if not used within 24 hours.
    New Password: 3#$wXP%
    If you have any problems or questions, please reply to this e-mail.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/GetOpportunityPersons.proc.sql
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/GetOpportunityPersons.proc.sql
@@ -4,19 +4,20 @@ AS
 BEGIN
 	SET NOCOUNT ON;
 
-	IF EXISTS(SELECT 1 FROM dbo.OpportunityPersons WHERE OpportunityId = @OpportunityId)
-	BEGIN
-		SELECT   op.PersonId
-				,p.FirstName
-				,p.LastName,
-				op.OpportunityPersonTypeId,
-				op.RelationTypeId,
-				op.Quantity,
-				op.NeedBy
-		FROM dbo.OpportunityPersons op
-		JOIN dbo.Person p ON p.PersonId = op.PersonId
-		WHERE op.OpportunityId = @OpportunityId
-				AND p.PersonStatusId IN(1,3)
-				
-	END
+	SELECT   op.PersonId
+			,p.FirstName
+			,p.LastName,
+			op.OpportunityPersonTypeId,
+			op.RelationTypeId,
+			op.Quantity,
+			op.NeedBy,
+			p.PersonStatusId 
+	FROM dbo.OpportunityPersons op
+	INNER JOIN dbo.Person p ON p.PersonId = op.PersonId
+	WHERE op.OpportunityId = @OpportunityId
+			AND (
+					p.PersonStatusId IN(1,3)
+					OR
+					p.IsStrawman = 1
+				)
 END
