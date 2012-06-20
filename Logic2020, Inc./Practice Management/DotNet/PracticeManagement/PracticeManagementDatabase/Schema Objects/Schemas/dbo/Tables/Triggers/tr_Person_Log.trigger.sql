9373ee5a8ea1f1a93cf586bbda1d20726fac6b45
SainathC <SainathC>
2012-06-20 12:55:00 -0500

    Comments - Defect Number: 2978; Defect RecordID: 2974; Server Address: localhost; Server Port: 99; Database Name: Practice Management; Defect Summary: Practice Management password reset issue; Defect Description: Please verify issue as stated below:
    If you request a password reset, then try to type in the temp password (at the URL below) it will fail to login.
    
    https://practice.logic2020.com/Login.aspx
    The URL must be:
    https://practice.logic2020.com/ChangePassword.aspx
    
    for the password reset to work.
    Michael Ashby// Logic20/20 // michaela@logic2020.com
    c.206-372-7926// www.logic2020.com
    
    From: Jason Hong-Turney
    Sent: Wednesday, December 21, 2011 4:51 PM
    To: Michael Ashby
    Subject: RE: Practice Management password reset requested
    
    Sure, of course he does.
    
    BTW, what issue? That you cant click the link, or that you didnt know to?
    
    Jason Hong-Turney // Logic20/20 // jasonht@logic2020.com
    t.206-576-0472 // c. 425-390-4664
    
    From: Michael Ashby
    Sent: Wednesday, December 21, 2011 4:51 PM
    To: Jason Hong-Turney
    Subject: RE: Practice Management password reset requested
    
    J Nikitin has the same issue.
    Michael Ashby// Logic20/20 // michaela@logic2020.com
    c.206-372-7926// www.logic2020.com
    
    From: Jason Hong-Turney
    Sent: Wednesday, December 21, 2011 4:50 PM
    To: Michael Ashby
    Subject: RE: Practice Management password reset requested
    
    Im not sure what you mean can be fixed the link is indeed by design, but you can also just type that password in as your password in PM and it should prompt you to change it. This should say, if anything, New Temporary Password. I can never seem to repro problems other people have with this
    
    Jason Hong-Turney // Logic20/20 // jasonht@logic2020.com
    t.206-576-0472 // c. 425-390-4664
    
    From: Michael Ashby
    Sent: Wednesday, December 21, 2011 4:49 PM
    To: Jason Hong-Turney
    Subject: FW: Practice Management password reset requested
    
    Jason, this is something I hope can be fixed. The user needs to click on the link below to reset password.
    
    Michael Ashby// Logic20/20 // michaela@logic2020.com
    c.206-372-7926// www.logic2020.com
    
    From: Logic20/20 - IT
    Sent: Wednesday, December 21, 2011 12:51 PM
    To: Michael Ashby
    Subject: Practice Management password reset requested
    
    You have or someone on your behalf has requested that your password for Practice Management be reset.
    If you did not make or authorize this request simply ignore this e-mail and continue to use your existing password. If this is a valid request, please click the link below which will open PM in a new browser window where you will be prompted to change your password. This new temporary password will automatically expire if not used within 24 hours.
    New Password: 3#$wXP%
    If you have any problems or questions, please reply to this e-mail.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/Triggers/tr_Person_Log.trigger.sql
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/Triggers/tr_Person_Log.trigger.sql
@@ -15,120 +15,244 @@ BEGIN
 	
 	DECLARE @CurrentPMTime DATETIME 
 	SET @CurrentPMTime = dbo.InsertingTime()
+	
+	IF (SELECT COUNT(ISNULL(i.PersonID,d.PersonID)) 
+		FROM inserted AS i	
+		FULL JOIN deleted AS d ON i.PersonID = d.PersonID 
+		WHERE ISNULL(i.IsStrawman, d.IsStrawman) = 0
+		) > 0
+	BEGIN 
 
-	;WITH NEW_VALUES AS
-	(
-		SELECT i.PersonId,
-		       i.PTODaysPerAnnum,
-		       CONVERT(NVARCHAR(10), i.HireDate, 101) AS HireDate,
-		       CONVERT(NVARCHAR(10), i.TerminationDate, 101) AS TerminationDate,
-		       i.Alias,
-		       p.Name AS DefaultPractice,
-		       i.FirstName,
-		       i.LastName,
-		       s.Name AS PersonStatusName,
-		       i.EmployeeNumber,
-		       r.Name AS Seniority,
-		       i.IsDefaultManager,
-		       mngr.LastName + ', ' + mngr.FirstName as 'ManagerName',
-			   i.TelephoneNumber,
-			   i.DivisionId,
-			   PD.DivisionName
-		  FROM inserted AS i
-		       LEFT JOIN dbo.Practice AS p ON i.DefaultPractice = p.PracticeId
-		       INNER JOIN dbo.PersonStatus AS s ON i.PersonStatusId = s.PersonStatusId
-		       LEFT JOIN dbo.Seniority AS r ON i.SeniorityId = r.SeniorityId
-		       INNER JOIN dbo.Person as mngr ON mngr.PersonId = i.ManagerId
-			   LEFT JOIN dbo.PersonDivision PD ON PD.DivisionId = i.DivisionId
-	),
+		;WITH NEW_VALUES AS
+		(
+			SELECT i.PersonId,
+					CONVERT(NVARCHAR(10), i.HireDate, 101) AS HireDate,
+					CONVERT(NVARCHAR(10), i.TerminationDate, 101) AS TerminationDate,
+					i.Alias ,
+					p.Name AS DefaultPractice,
+					i.FirstName,
+					i.LastName,
+					s.Name AS PersonStatusName,
+					i.EmployeeNumber,
+					i.SeniorityId,
+					r.Name AS Seniority,
+					CASE WHEN i.IsDefaultManager = 1 THEN 'Yes' ELSE 'NO' END AS [IsDefaultManager],
+					i.ManagerId,
+					mngr.LastName + ', ' + mngr.FirstName as [ManagerName],
+					i.TelephoneNumber,
+					i.DivisionId,
+					PD.DivisionName,
+					i.PayChexID,
+					CASE WHEN i.IsOffshore = 1 THEN 'YES' ELSE 'NO' END AS [IsOffshore]
+			FROM inserted AS i
+					LEFT JOIN dbo.Practice AS p ON i.DefaultPractice = p.PracticeId
+					INNER JOIN dbo.PersonStatus AS s ON i.PersonStatusId = s.PersonStatusId
+					LEFT JOIN dbo.Seniority AS r ON i.SeniorityId = r.SeniorityId
+					LEFT JOIN dbo.Person as mngr ON mngr.PersonId = i.ManagerId
+					LEFT JOIN dbo.PersonDivision PD ON PD.DivisionId = i.DivisionId
+			WHERE i.IsStrawman = 0 
+		),
 
-	OLD_VALUES AS
-	(
-		SELECT d.PersonId,
-		       d.PTODaysPerAnnum,
-		       CONVERT(NVARCHAR(10), d.HireDate, 101) AS HireDate,
-		       CONVERT(NVARCHAR(10), d.TerminationDate, 101) AS TerminationDate,
-		       d.Alias,
-		       p.Name AS DefaultPractice,
-		       d.FirstName,
-		       d.LastName,
-		       s.Name AS PersonStatusName,
-		       d.EmployeeNumber,
-		       r.Name AS Seniority,
-		       d.IsDefaultManager,
-		       mngr.LastName + ', ' + mngr.FirstName as 'ManagerName',
-			   d.TelephoneNumber,
-			   d.DivisionId,
-			   PD.DivisionName
-		  FROM deleted AS d
-		       LEFT JOIN dbo.Practice AS p ON d.DefaultPractice = p.PracticeId
-		       INNER JOIN dbo.PersonStatus AS s ON d.PersonStatusId = s.PersonStatusId
-		       LEFT JOIN dbo.Seniority AS r ON d.SeniorityId = r.SeniorityId
-		       INNER JOIN dbo.Person as mngr ON mngr.PersonId = d.ManagerId
-			   LEFT JOIN dbo.PersonDivision PD ON PD.DivisionId = d.DivisionId
-	)
+		OLD_VALUES AS
+		(
+			SELECT d.PersonId,
+					CONVERT(NVARCHAR(10), d.HireDate, 101) AS HireDate,
+					CONVERT(NVARCHAR(10), d.TerminationDate, 101) AS TerminationDate,
+					d.Alias ,
+					p.Name AS DefaultPractice,
+					d.FirstName,
+					d.LastName,
+					s.Name AS PersonStatusName,
+					d.EmployeeNumber,
+					d.SeniorityId,
+					r.Name AS Seniority,
+					CASE WHEN d.IsDefaultManager = 1 THEN 'Yes' ELSE 'NO' END AS [IsDefaultManager],
+					d.ManagerId,
+					mngr.LastName + ', ' + mngr.FirstName as [ManagerName],
+					d.TelephoneNumber,
+					d.DivisionId,
+					PD.DivisionName,
+					d.PayChexID,
+					CASE WHEN d.IsOffshore = 1 THEN 'YES' ELSE 'NO' END AS [IsOffshore]
+			FROM deleted AS d
+					LEFT JOIN dbo.Practice AS p ON d.DefaultPractice = p.PracticeId
+					INNER JOIN dbo.PersonStatus AS s ON d.PersonStatusId = s.PersonStatusId
+					LEFT JOIN dbo.Seniority AS r ON d.SeniorityId = r.SeniorityId
+					LEFT JOIN dbo.Person as mngr ON mngr.PersonId = d.ManagerId
+					LEFT JOIN dbo.PersonDivision PD ON PD.DivisionId = d.DivisionId
+			WHERE d.IsStrawman = 0
+		)
 
-	-- Log an activity
-	INSERT INTO dbo.UserActivityLog
-	            (ActivityTypeID,
-	             SessionID,
-	             SystemUser,
-	             Workstation,
-	             ApplicationName,
-	             UserLogin,
-	             PersonID,
-	             LastName,
-	             FirstName,
-				 Data,
-	             LogData,
-	             LogDate)
-	SELECT CASE
-	           WHEN d.PersonID IS NULL THEN 3
-	           WHEN i.PersonID IS NULL THEN 5
-	           ELSE 4
-	       END AS ActivityTypeID,
-	       l.SessionID,
-	       l.SystemUser,
-	       l.Workstation,
-	       l.ApplicationName,
-	       l.UserLogin,
-	       l.PersonID,
-	       l.LastName,
-	       l.FirstName,
-	       Data =  CONVERT(NVARCHAR(MAX),(SELECT *
-					    FROM NEW_VALUES
-					         FULL JOIN OLD_VALUES ON NEW_VALUES.PersonID = OLD_VALUES.PersonID
-			           WHERE NEW_VALUES.PersonID = ISNULL(i.PersonId, d.PersonID) OR OLD_VALUES.PersonID = ISNULL(i.PersonId, d.PersonID)
-					  FOR XML AUTO, ROOT('Person'))),
-			LogData = (SELECT NEW_VALUES.PersonId,
-							  NEW_VALUES.PTODaysPerAnnum,
-							  NEW_VALUES.HireDate,
-							  NEW_VALUES.TerminationDate,
-							  NEW_VALUES.EmployeeNumber,
-							  NEW_VALUES.IsDefaultManager,
-							  NEW_VALUES.TelephoneNumber,
-							  OLD_VALUES.PersonId,
-							  OLD_VALUES.PTODaysPerAnnum,
-							  OLD_VALUES.HireDate,
-							  OLD_VALUES.TerminationDate,
-							  OLD_VALUES.EmployeeNumber,
-							  OLD_VALUES.IsDefaultManager,
-							  OLD_VALUES.TelephoneNumber
-					    FROM NEW_VALUES
-					         FULL JOIN OLD_VALUES ON NEW_VALUES.PersonID = OLD_VALUES.PersonID
-			           WHERE NEW_VALUES.PersonID = ISNULL(i.PersonId, d.PersonID) OR OLD_VALUES.PersonID = ISNULL(i.PersonId, d.PersonID)
-					  FOR XML AUTO, ROOT('Person'), TYPE),
-			@CurrentPMTime
-	  FROM inserted AS i
-	       FULL JOIN deleted AS d ON i.PersonID = d.PersonID
-	       INNER JOIN dbo.SessionLogData AS l ON l.SessionID = @@SPID
+		-- Log an activity
+		INSERT INTO dbo.UserActivityLog
+					(ActivityTypeID,
+						SessionID,
+						SystemUser,
+						Workstation,
+						ApplicationName,
+						UserLogin,
+						PersonID,
+						LastName,
+						FirstName,
+						Data,
+						LogData,
+						LogDate)
+		SELECT CASE
+					WHEN d.PersonID IS NULL THEN 3
+					WHEN i.PersonID IS NULL THEN 5
+					ELSE 4
+				END AS ActivityTypeID,
+				l.SessionID,
+				l.SystemUser,
+				l.Workstation,
+				l.ApplicationName,
+				l.UserLogin,
+				l.PersonID,
+				l.LastName,
+				l.FirstName,
+				Data =  CONVERT(NVARCHAR(MAX),(SELECT *
+							FROM NEW_VALUES
+									FULL JOIN OLD_VALUES ON NEW_VALUES.PersonID = OLD_VALUES.PersonID
+							WHERE NEW_VALUES.PersonID = ISNULL(i.PersonId, d.PersonID) OR OLD_VALUES.PersonID = ISNULL(i.PersonId, d.PersonID)
+							FOR XML AUTO, ROOT('Person'))),
+				LogData = (SELECT NEW_VALUES.PersonId,
+									NEW_VALUES.HireDate,
+									NEW_VALUES.TerminationDate,
+									NEW_VALUES.EmployeeNumber,
+									NEW_VALUES.IsDefaultManager,
+									NEW_VALUES.TelephoneNumber,
+									OLD_VALUES.PersonId,
+									OLD_VALUES.HireDate,
+									OLD_VALUES.TerminationDate,
+									OLD_VALUES.EmployeeNumber,
+									OLD_VALUES.IsDefaultManager,
+									OLD_VALUES.TelephoneNumber
+							FROM NEW_VALUES
+									FULL JOIN OLD_VALUES ON NEW_VALUES.PersonID = OLD_VALUES.PersonID
+							WHERE NEW_VALUES.PersonID = ISNULL(i.PersonId, d.PersonID) OR OLD_VALUES.PersonID = ISNULL(i.PersonId, d.PersonID)
+							FOR XML AUTO, ROOT('Person'), TYPE),
+				@CurrentPMTime
+		FROM inserted AS i
+				FULL JOIN deleted AS d ON i.PersonID = d.PersonID 
+				INNER JOIN dbo.SessionLogData AS l ON l.SessionID = @@SPID
+		WHERE ISNULL(i.IsStrawman, d.IsStrawman) = 0 
+			AND (
+					ISNULL(i.HireDate,'') <> ISNULL(d.HireDate,'')
+					OR ISNULL(i.TerminationDate,'') <> ISNULL(d.TerminationDate,'')
+					OR ISNULL(i.Alias,'') <> ISNULL(d.Alias,'')
+					OR ISNULL(i.DefaultPractice,'') <> ISNULL(d.DefaultPractice,'')
+					OR ISNULL(i.FirstName,'') <> ISNULL(d.FirstName,'')
+					OR ISNULL(i.LastName,'') <> ISNULL(d.LastName,'')
+					OR ISNULL(i.PersonStatusId,'') <> ISNULL(d.PersonStatusId,'')
+					OR ISNULL(i.EmployeeNumber,'') <> ISNULL(d.EmployeeNumber,'')
+					OR ISNULL(i.SeniorityId,0) <> ISNULL(d.SeniorityId,0)
+					OR ISNULL(i.IsDefaultManager,'') <> ISNULL(d.IsDefaultManager,'')
+					OR ISNULL(i.ManagerId,0) <> ISNULL(d.ManagerId,0)
+					OR ISNULL(i.TelephoneNumber,'') <> ISNULL(d.TelephoneNumber,'')
+					OR ISNULL(i.DivisionId,0) <> ISNULL(d.DivisionId,0)
+					OR ISNULL(i.PayChexID,'') <> ISNULL(d.PayChexID,'')
+					OR ISNULL(i.IsOffshore,'') <> ISNULL(d.IsOffshore,'')
+				)
 
-	  -- End logging session
+	END
+
+	IF (SELECT COUNT(ISNULL(i.PersonID,d.PersonID)) 
+		FROM inserted AS i	
+		FULL JOIN deleted AS d ON i.PersonID = d.PersonID 
+		WHERE ISNULL(i.IsStrawman, d.IsStrawman) = 1
+		) > 0
+	BEGIN 
+
+		;WITH NEW_VALUES AS
+		(
+			SELECT i.PersonId,
+					CONVERT(NVARCHAR(10), i.HireDate, 101) AS [HireDate],
+					CONVERT(NVARCHAR(10), i.TerminationDate, 101) AS [TerminationDate],
+					i.FirstName AS [Skill],
+					i.LastName AS [Role],
+					s.Name AS PersonStatusName,
+					i.EmployeeNumber
+			FROM inserted AS i
+					INNER JOIN dbo.PersonStatus AS s ON i.PersonStatusId = s.PersonStatusId
+			WHERE i.IsStrawman = 1
+		),
+		OLD_VALUES AS
+		(
+			SELECT d.PersonId,
+					CONVERT(NVARCHAR(10), d.HireDate, 101) AS [HireDate],
+					CONVERT(NVARCHAR(10), d.TerminationDate, 101) AS [TerminationDate],
+					d.FirstName AS [Skill],
+					d.LastName AS [Role],
+					s.Name AS PersonStatusName,
+					d.EmployeeNumber
+			FROM deleted AS d
+					INNER JOIN dbo.PersonStatus AS s ON d.PersonStatusId = s.PersonStatusId
+			WHERE d.IsStrawman = 1
+		)
+
+		-- Log an activity
+		INSERT INTO dbo.UserActivityLog
+					(ActivityTypeID,
+						SessionID,
+						SystemUser,
+						Workstation,
+						ApplicationName,
+						UserLogin,
+						PersonID,
+						LastName,
+						FirstName,
+						Data,
+						LogData,
+						LogDate)
+		SELECT CASE
+					WHEN d.PersonID IS NULL THEN 3
+					WHEN i.PersonID IS NULL THEN 5
+					ELSE 4
+				END AS ActivityTypeID,
+				l.SessionID,
+				l.SystemUser,
+				l.Workstation,
+				l.ApplicationName,
+				l.UserLogin,
+				l.PersonID,
+				l.LastName,
+				l.FirstName,
+				Data =  CONVERT(NVARCHAR(MAX),(SELECT *
+							FROM NEW_VALUES
+									FULL JOIN OLD_VALUES ON NEW_VALUES.PersonID = OLD_VALUES.PersonID
+							WHERE NEW_VALUES.PersonID = ISNULL(i.PersonId, d.PersonID) OR OLD_VALUES.PersonID = ISNULL(i.PersonId, d.PersonID)
+							FOR XML AUTO, ROOT('Strawman'))),
+				LogData = (SELECT NEW_VALUES.PersonId,
+									NEW_VALUES.HireDate,
+									NEW_VALUES.TerminationDate,
+									NEW_VALUES.EmployeeNumber,
+									OLD_VALUES.PersonId,
+									OLD_VALUES.HireDate,
+									OLD_VALUES.TerminationDate,
+									OLD_VALUES.EmployeeNumber
+							FROM NEW_VALUES
+									FULL JOIN OLD_VALUES ON NEW_VALUES.PersonID = OLD_VALUES.PersonID
+							WHERE NEW_VALUES.PersonID = ISNULL(i.PersonId, d.PersonID) OR OLD_VALUES.PersonID = ISNULL(i.PersonId, d.PersonID)
+							FOR XML AUTO, ROOT('Strawman'), TYPE),
+				@CurrentPMTime
+ 		FROM inserted AS i
+				FULL JOIN deleted AS d ON i.PersonID = d.PersonID 
+				INNER JOIN dbo.SessionLogData AS l ON l.SessionID = @@SPID
+		WHERE ISNULL(i.IsStrawman, d.IsStrawman) = 1 
+		AND (
+				ISNULL(i.HireDate,'') <> ISNULL(d.HireDate,'')
+				OR ISNULL(i.TerminationDate,'') <> ISNULL(d.TerminationDate,'')
+				OR ISNULL(i.FirstName,'') <> ISNULL(d.FirstName,'')
+				OR ISNULL(i.LastName,'') <> ISNULL(d.LastName,'')
+				OR ISNULL(i.PersonStatusId,'') <> ISNULL(d.PersonStatusId,'')
+				OR ISNULL(i.EmployeeNumber,'') <> ISNULL(d.EmployeeNumber,'')
+			)
+			
+	END
+
+		-- End logging session
 	EXEC dbo.SessionLogUnprepare
 END
-
-
-GO
 
 
 
