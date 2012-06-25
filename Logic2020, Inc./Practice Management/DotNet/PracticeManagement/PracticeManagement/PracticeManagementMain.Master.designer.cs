89bced0718eb005ed09ebcf989a942b953f828f9
thulasiramp <thulasiramp>
2012-06-25 08:20:00 -0500

    Comments - Defect Number: 3058; Defect RecordID: 3054; Server Address: localhost; Server Port: 99; Database Name: Practice Management; Defect Summary: Site optimization request; Defect Description: I've been looking at pages on the site with the Firebug add-in for Firefox to see how well it is performing. Page loading always seems slow to me, and Firebug notes multiple issues that are affecting the site's efficiency. When there isn't a more pressing feature request, please begin using Firebug (or another tool) to analyze the site and make optimizations to reduce loading time, improve caching, etc.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/PracticeManagementMain.Master.designer.cs
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/PracticeManagementMain.Master.designer.cs
@@ -85,15 +85,6 @@ namespace PraticeManagement {
         protected global::System.Web.UI.WebControls.Literal ltrScript;
         
         /// <summary>
-        /// TabsScriptBlock control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Literal TabsScriptBlock;
-        
-        /// <summary>
         /// hlHome control.
         /// </summary>
         /// <remarks>
