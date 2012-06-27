e3ec7301ca87ba797d451247444ae57accbfdacc
SrinivasM <SrinivasM>
2012-06-27 07:11:00 -0500

    Comments - Defect Number: 3061; Defect RecordID: 3057; Server Address: localhost; Server Port: 99; Database Name: Practice Management; Defect Summary: Performance: Projects>Summary / Projects>Group BY- Improve performance by removing unnecessary DB calls.; Defect Description: Note: While navigating to Projects>Summary / Projects>Group BY page (with multiple accounts being selected in filters), DB calls are made for each account.

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Functions/GetHolidayTimeTypeIdTable.function.sql
@@ -0,0 +1,17 @@
+﻿CREATE FUNCTION [dbo].[GetHolidayTimeTypeIdTable]
+(
+)
+RETURNS @Result TABLE 
+(
+	HolidayTimeTypeId INT
+)
+AS
+BEGIN
+	
+	INSERT INTO @Result
+	SELECT tt.TimeTypeId 
+	FROM TimeType tt 
+	WHERE tt.Code = 'W9320' --Here 'W9320' is code of Holiday Work Type.
+
+	RETURN
+END
