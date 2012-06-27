72820be32afd36a8dfe96cef2171204c65a77e66
thulasiramp <thulasiramp>
2012-06-27 05:50:00 -0500

    Comments - Defect Number: 3062; Defect RecordID: 3058; Server Address: localhost; Server Port: 99; Database Name: Practice Management; Defect Summary: Time Report by Person: Create a view to combine the rows to summarize at the Project level versus the breakout at the Work Typ@; Defect Description: Madhan, Is it possible to combine the rows to summarize at the Project level versus the breakout at the Work Type level? Would it be possible to have a toggle to enable flipping between the two views  one with the Work Type broken out and one with the aggregation at the Project level? The default would be the Project level view.
    - Matt
    
    Matthew Reilly, Director of Consulting// Logic20/20 // MattR@logic2020.com
    t.206-576-0491//c.425-503-5825// www.logic2020.com
    
    From: Ivana Blank
    Sent: Monday, June 18, 2012 12:13 PM
    To: Matt Reilly; Vanessa Schy-Berg; Lisa Johnson Neel
    Cc: Jason Hong-Turney
    Subject: RE: Time Report by Person in UAT
    
    It looks great. The only thing that would be nice would be if the same project numbers could be combined instead of separated by work type. See below with Jaysons timesheet with 3 line items for Apptio. 1 line item for the hours would be better if possible.
    Thanks so much for setting up this report! I really appreciate it.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/Controls/Reports/TimeEntriesByPerson.ascx.designer.cs
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/Controls/Reports/TimeEntriesByPerson.ascx.designer.cs
@@ -13,13 +13,85 @@ namespace PraticeManagement.Controls.Reports {
     public partial class TimeEntriesByPerson {
         
         /// <summary>
-        /// repPersons control.
+        /// divPersonListSummary control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Repeater repPersons;
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl divPersonListSummary;
+        
+        /// <summary>
+        /// divPersonName control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl divPersonName;
+        
+        /// <summary>
+        /// divTeTable control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl divTeTable;
+        
+        /// <summary>
+        /// repTeTable control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Repeater repTeTable;
+        
+        /// <summary>
+        /// divProjects control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl divProjects;
+        
+        /// <summary>
+        /// dlProjects control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Repeater dlProjects;
+        
+        /// <summary>
+        /// divPersonNotEntered control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl divPersonNotEntered;
+        
+        /// <summary>
+        /// lblnoDataMesssage control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Literal lblnoDataMesssage;
+        
+        /// <summary>
+        /// divhr control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl divhr;
     }
 }
 
