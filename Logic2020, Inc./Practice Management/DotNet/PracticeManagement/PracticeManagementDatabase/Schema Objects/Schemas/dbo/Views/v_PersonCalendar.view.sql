97e776ef6522fecc8564df8f399af36d6c6eb9e6
SrinivasM <SrinivasM>
2012-07-04 05:57:00 -0500

    Comments - Defect Number: 3061; Defect RecordID: 3057; Server Address: localhost; Server Port: 99; Database Name: Practice Management; Defect Summary: Performance: Projects>Summary / Projects>Group BY- Improve performance by removing unnecessary DB calls.; Defect Description: Note: While navigating to Projects>Summary / Projects>Group BY page (with multiple accounts being selected in filters), DB calls are made for each account.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Views/v_PersonCalendar.view.sql
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Views/v_PersonCalendar.view.sql
@@ -11,13 +11,15 @@ AS
 	       p.[PersonId],
 	       ISNULL(pcal.DayOff, cal.DayOff) AS DayOff,
 	       cal.DayOff AS CompanyDayOff,
-		   (CASE WHEN ISNULL(pcal.TimeTypeId, 0) = dbo.GetHolidayTimeTypeId()  THEN 1 ELSE 0 END)   AS 'IsFloatingHoliday',
+		   (CASE WHEN ISNULL(pcal.TimeTypeId, 0) = HTT.HolidayTimeTypeId  THEN 1 ELSE 0 END)   AS 'IsFloatingHoliday',
 		   pcal.ActualHours,
 		   pcal.TimeTypeId,
 		   pcal.Description,
 		   pcal.ApprovedBy,
 		   pcal.SubstituteDate
 	  FROM dbo.Calendar AS cal
-	       INNER JOIN dbo.Person AS p ON cal.Date >= p.HireDate AND cal.Date < ISNULL(p.TerminationDate, dbo.GetFutureDate())
+		   INNER JOIN dbo.GetFutureDateTable() FD ON 1 = 1
+		   INNER JOIN dbo.GetHolidayTimeTypeIdTable() HTT ON 1 = 1
+	       INNER JOIN dbo.Person AS p ON cal.Date >= p.HireDate AND cal.Date < ISNULL(p.TerminationDate, FD.FutureDate)
 	       LEFT JOIN dbo.PersonCalendar AS pcal ON pcal.Date = cal.Date AND pcal.PersonId = p.PersonId
 
