7f95ccc8fc005e7493aa8974c63521f026e98f9e
SrinivasM <SrinivasM>
2012-07-04 05:57:00 -0500

    Comments - Defect Number: 3061; Defect RecordID: 3057; Server Address: localhost; Server Port: 99; Database Name: Practice Management; Defect Summary: Performance: Projects>Summary / Projects>Group BY- Improve performance by removing unnecessary DB calls.; Defect Description: Note: While navigating to Projects>Summary / Projects>Group BY page (with multiple accounts being selected in filters), DB calls are made for each account.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/PersonListByCategoryTypeAndPeriod.proc.sql
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/PersonListByCategoryTypeAndPeriod.proc.sql
@@ -8,34 +8,68 @@ AS
 BEGIN
 	DECLARE  @CategoryTypeIdLocal		INT,
 			 @StartDateLocal			DATETIME,
-			 @EndDateLocal				DATETIME
+			 @EndDateLocal				DATETIME,
+			 @FutureDateLocal			DATETIME
 
 
 	SELECT 
 	 @CategoryTypeIdLocal			=	@CategoryTypeId,
 	 @StartDateLocal				=	@StartDate,
-	 @EndDateLocal					=	@EndDate
+	 @EndDateLocal					=	@EndDate,
+	 @FutureDateLocal				=   dbo.GetFutureDate()
 	
 	IF(@CategoryTypeId = 3) --Business Development managers
 	BEGIN
-	 SELECT DISTINCT 
-				P.PersonId,
+	 
+	 ;WITH PersonCTE AS
+	 (
+	     SELECT P.PersonId,
+				P.LastName,
+				P.FirstName,
+				P.IsDefaultManager	
+		 FROM	dbo.aspnet_Users U 
+				INNER JOIN dbo.Person P ON P.Alias = U.UserName AND
+										   P.HireDate <=  @EndDateLocal AND 
+											ISNULL(P.TerminationDate,@FutureDateLocal) >= @StartDateLocal
+				INNER JOIN dbo.aspnet_UsersInRoles UR ON UR.UserId = u.UserId 
+				INNER JOIN dbo.aspnet_UsersRolesHistory URH ON U.UserId = URH.UserId  AND 
+															   URH.StartDate <=  @EndDateLocal AND 
+															   ISNULL(URH.EndDate,@FutureDateLocal) >= @StartDateLocal
+				INNER JOIN dbo.aspnet_Roles R ON URH.RoleId = R.RoleId AND R.RoleName = 'Salesperson'
+				INNER JOIN dbo.PersonStatusHistory PSH ON PSH.PersonId = P.PersonId AND 
+														 PSH.PersonStatusId =1  AND 
+														 PSH.StartDate <=  @EndDateLocal AND 
+														 ISNULL(PSH.EndDate,@FutureDateLocal) >= @StartDateLocal
+
+	 ),
+	 ProjectSalesPerson 
+	 AS
+	 (
+	     SELECT P.PersonId,
+				P.LastName,
+				P.FirstName,
+				P.IsDefaultManager
+		 FROM dbo.Person AS P 
+		 INNER JOIN dbo.Commission Com ON Com.PersonId = P.PersonId AND Com.CommissionType = 1
+		 INNER JOIN dbo.Project Proj ON Proj.ProjectId = Com.ProjectId AND 
+										Proj.StartDate <=  @EndDateLocal AND 
+										ISNULL(Proj.EndDate,@FutureDateLocal) >= @StartDateLocal
+
+	 )
+
+
+
+	     SELECT P.PersonId,
 				P.LastName,
 				P.FirstName,
 				P.IsDefaultManager				
-			FROM dbo.Person P
-			JOIN dbo.Calendar C ON C.Date BETWEEN P.HireDate AND ISNULL(P.TerminationDate,dbo.GetFutureDate()) 
-									AND C.Date  BETWEEN @StartDateLocal AND	 @EndDateLocal
-			LEFT JOIN dbo.PersonStatusHistory PSH 
-				ON PSH.PersonId = P.PersonId AND PSH.PersonStatusId =1  AND C.Date >= PSH.StartDate AND (C.Date <= PSH.EndDate OR PSH.EndDate IS NULL)
-			LEFT JOIN dbo.aspnet_Users U ON P.Alias = U.UserName
-			LEFT JOIN dbo.aspnet_UsersRolesHistory  UIR
-			ON UIR.UserId = U.UserId  AND C.Date >= UIR.StartDate AND (C.Date <= UIR.EndDate OR UIR.EndDate IS NULL)
-			LEFT JOIN dbo.aspnet_Roles UR ON UIR.RoleId = UR.RoleId AND UR.RoleName='Salesperson'
-			LEFT JOIN dbo.Commission Com ON Com.PersonId = P.PersonId AND Com.CommissionType = 1
-			LEFT JOIN dbo.Project Proj ON Proj.ProjectId = Com.ProjectId AND C.Date BETWEEN Proj.StartDate  AND ISNULL(Proj.EndDate,dbo.GetFutureDate())
-			WHERE (UR.RoleId IS NOT NULL OR Com.PersonId IS NOT NULL AND Proj.ProjectId IS NOT NULL)
-				  AND (PSH.PersonStatusId IS NOT NULL OR(Com.PersonId IS NOT NULL AND Proj.ProjectId IS NOT NULL))
+		 FROM PersonCTE P
+		 UNION 
+		  SELECT P.PersonId,
+				P.LastName,
+				P.FirstName,
+				P.IsDefaultManager				
+		 FROM ProjectSalesPerson P	
 	END
 
 END
