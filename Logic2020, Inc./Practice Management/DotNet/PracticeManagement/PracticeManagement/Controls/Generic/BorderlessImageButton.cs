2a3f33e607d47efbf6474eae1a8bd7cea4ca3a5c
SainathC <SainathC>
2012-07-04 04:26:00 -0500

    Comments - Defect Number: 3058; Defect RecordID: 3054; Server Address: localhost; Server Port: 99; Database Name: Practice Management; Defect Summary: Site optimization request; Defect Description: I've been looking at pages on the site with the Firebug add-in for Firefox to see how well it is performing. Page loading always seems slow to me, and Firebug notes multiple issues that are affecting the site's efficiency. When there isn't a more pressing feature request, please begin using Firebug (or another tool) to analyze the site and make optimizations to reduce loading time, improve caching, etc.

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/Controls/Generic/BorderlessImageButton.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PraticeManagement.Controls.Generic
+{
+    public class BorderlessImageButton : System.Web.UI.WebControls.ImageButton
+    {
+        public override System.Web.UI.WebControls.Unit BorderWidth
+        {
+            get
+            {
+                if (base.BorderWidth.IsEmpty)
+                {
+                    return System.Web.UI.WebControls.Unit.Pixel(0);
+                }
+                else
+                {
+                    return base.BorderWidth;
+                }
+            }
+            set
+            {
+                base.BorderWidth = value;
+            }
+        }
+
+    }
+}
+
+
