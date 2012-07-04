cf5257414176d49b056c086debe461968e385806
SainathC <SainathC>
2012-07-04 04:27:00 -0500

    Comments - Defect Number: 3058; Defect RecordID: 3054; Server Address: localhost; Server Port: 99; Database Name: Practice Management; Defect Summary: Site optimization request; Defect Description: I've been looking at pages on the site with the Firebug add-in for Firefox to see how well it is performing. Page loading always seems slow to me, and Firebug notes multiple issues that are affecting the site's efficiency. When there isn't a more pressing feature request, please begin using Firebug (or another tool) to analyze the site and make optimizations to reduce loading time, improve caching, etc.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/Controls/DatePicker.ascx.designer.cs
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/Controls/DatePicker.ascx.designer.cs
@@ -22,6 +22,15 @@ namespace PraticeManagement.Controls {
         protected global::System.Web.UI.WebControls.TextBox txtDate;
         
         /// <summary>
+        /// fte control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::AjaxControlToolkit.FilteredTextBoxExtender fte;
+        
+        /// <summary>
         /// lnkCalendar control.
         /// </summary>
         /// <remarks>
