68934565ab8730b08eb2f0d6daec177ed97f79c2
thulasiramp <thulasiramp>
2012-07-09 04:01:00 -0500

    Comments - Defect Number: 3063; Defect RecordID: 3059; Server Address: localhost; Server Port: 99; Database Name: Practice Management; Defect Summary: Configuration: Person - Termination should lockout user on Termination Date + 1 day; Defect Description: Currently, if a user terminates a person in the system, the user enters the Termination Date. Clicking Save will lock the person being terminated out of PM on the termination date itself. This is problematic as the person's employment technically ends at the end of that day, not the beginning. The person still needs access to the system to enable them to enter their time, finalize any last minute project/opportunity changes, etc.
    Therefore, we'd like the system to lock the person out of the system on the next calendar day instead. (i.e. Termination Date + 1)
    
    For example, if I terminate Employee Jane Doe with an Effective Date on July 2, 2012. Jane Doe will not be able to access PM on July 2nd. With this change, Jane Doe would retain access to PM through 7/2 and would not be able to access PM at 00:00:00 on July 3rd.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/PersonUpdate.proc.sql
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/PersonUpdate.proc.sql
@@ -1,8 +1,8 @@
 ﻿	-- =============================================
 	-- Author:		Anatoliy Lokshin
 	-- Create date: 8-04-2008
-	-- Updated by:	sainathc
-	-- Update date: 05-31-2012
+	-- Last Updated by:	ThulasiRam.P
+	-- Last Update date: 07-06-2012
 	-- Description:	Updates the Person.
 	-- =============================================
 	CREATE PROCEDURE [dbo].[PersonUpdate] 
@@ -36,10 +36,13 @@
 				@W2SalaryId INT,
 				@IsHireDateChanged BIT
 
-		SELECT @Today = CONVERT(DATETIME,CONVERT(DATE,[dbo].[GettingPMTime](GETDATE())))
+		SELECT @Today = CONVERT(DATETIME,CONVERT(DATE,[dbo].[GettingPMTime](GETUTCDATE())))
 		SELECT @W2SalaryId = TimescaleId FROM Timescale WHERE Name = 'W2-Salary'
 
-		SELECT @PersonStatusId = CASE WHEN @TerminationDate<= @Today THEN 2 ELSE @PersonStatusId END
+
+		SELECT @PersonStatusId = CASE WHEN @TerminationDate < @Today THEN 2 
+								      ELSE @PersonStatusId 
+							     END
 
 		IF EXISTS(SELECT 1
 					FROM dbo.[Person] AS p
@@ -70,9 +73,6 @@
 			-- Start logging session
 			EXEC dbo.SessionLogPrepare @UserLogin = @UserLogin
 		
-			-- Try to get default manager id
-			--SELECT @ManagerId = p.PersonId FROM person AS p WHERE p.IsDefaultManager = 1
-
 			IF @PersonStatusId = 2
 			BEGIN
 
@@ -83,31 +83,31 @@
 			IF @PersonStatusId <> 1
 			BEGIN
 				-- SET new manager for subordinates
-				DECLARE @currentPractice INT
-				DECLARE @newManager INT
+				DECLARE @CurrentPractice INT
+				DECLARE @NewManager INT
 
-				SELECT @currentPractice = DefaultPractice
-				FROM Person 
-				WHERE PersonId = @PersonId
+				SELECT @CurrentPractice = p.DefaultPractice
+				FROM [dbo].Person AS P
+				WHERE P.PersonId = @PersonId
 
-				SELECT @newManager = PersonId
-				FROM Person P
-				JOIN dbo.Seniority S ON S.SeniorityId = P.SeniorityId
-				where DefaultPractice = @currentPractice
-				AND S.SeniorityValue <= 65 AND PersonId != @PersonId AND PersonStatusId = 1 -- ActivePerson
+				SELECT @NewManager = P.PersonId
+				FROM [dbo].Person AS P
+				INNER JOIN dbo.Seniority S ON S.SeniorityId = P.SeniorityId
+				WHERE P.DefaultPractice = @CurrentPractice
+				AND S.SeniorityValue <= 65 AND P.PersonId != @PersonId AND P.PersonStatusId = 1 -- ActivePerson
 
-				IF (@newManager IS NULL)
-					SELECT @newManager = PersonId
-					FROM Person 
-					WHERE IsDefaultManager = 1 AND PersonId != @PersonId AND PersonStatusId = 1 -- ActivePerson
+				IF (@NewManager IS NULL)
+					SELECT @NewManager = P.PersonId
+					FROM dbo.Person AS P
+					WHERE P.IsDefaultManager = 1 AND P.PersonId != @PersonId AND P.PersonStatusId = 1 -- ActivePerson
 
-				IF (@newManager IS NULL)
-					SELECT @newManager = PracticeManagerId
-					FROM Practice 
-					WHERE PracticeId = @currentPractice
+				IF (@NewManager IS NULL)
+					SELECT @NewManager = pr.PracticeManagerId
+					FROM Practice AS pr
+					WHERE pr.PracticeId = @currentPractice
 
 				UPDATE dbo.Person
-				SET ManagerId = @newManager
+				SET ManagerId = @NewManager
 				WHERE ManagerId = @PersonId
 			END
 
@@ -119,8 +119,9 @@
 			SELECT @ExistingSeniorityId = SeniorityId,
 					@ExistingPracticeId = DefaultPractice,
 					@PreviousTerminationDate = TerminationDate
-			FROM dbo.Person P
-			WHERE PersonId = @PersonId
+			FROM dbo.Person AS P
+			WHERE P.PersonId = @PersonId
+
 			IF(@ExistingSeniorityId <> @SeniorityId 
 						OR (@ExistingSeniorityId IS NULL AND  @SeniorityId IS NOT NULL)
 						OR (@ExistingSeniorityId IS NOT NULL AND @SeniorityId IS NULL)
@@ -131,16 +132,16 @@
 			BEGIN
 
 
-				SELECT @CurrentPayEndDate = EndDate
-				FROM dbo.Pay P
-				WHERE Person = @PersonId  
-						AND @Today BETWEEN StartDate AND EndDate-1
+				SELECT @CurrentPayEndDate = pay.EndDate
+				FROM dbo.Pay AS pay
+				WHERE pay.Person = @PersonId  
+						AND @Today BETWEEN pay.StartDate AND pay.EndDate-1
 			
-				SELECT @CurrentSalesCommEndDate = EndDate
+				SELECT @CurrentSalesCommEndDate = P.EndDate
 				FROM dbo.[DefaultCommission] P
-				WHERE PersonId = @PersonId  
-					 AND [Type] = 1 -- Sales Commission
-					 AND @Today BETWEEN StartDate AND EndDate-1
+				WHERE P.PersonId = @PersonId  
+					 AND P.[type] = 1 -- Sales Commission
+					 AND @Today BETWEEN P.StartDate AND P.EndDate-1
 
 				BEGIN TRY
 					
@@ -304,13 +305,13 @@
 				--Delete all PTO/Holiday timeEntries greater than @TerminationDate.
 				DELETE TEH
 				FROM TimeEntry TE
-				JOIN ChargeCode CC ON TE.PersonId = @PersonId  AND CC.Id = TE.ChargeCodeId AND TE.ChargeCodeDate >= @TerminationDate
+				JOIN ChargeCode CC ON TE.PersonId = @PersonId  AND CC.Id = TE.ChargeCodeId AND TE.ChargeCodeDate > @TerminationDate
 				JOIN TimeType TT ON TT.TimeTypeId = CC.TimeTypeId AND TT.IsAdministrative = 1
 				JOIN TimeEntryHours TEH ON TEH.TimeEntryId = TE.TimeEntryId
 			
 				DELETE TE
 				FROM TimeEntry TE
-				JOIN ChargeCode CC ON TE.PersonId = @PersonId  AND CC.Id = TE.ChargeCodeId AND TE.ChargeCodeDate >= @TerminationDate
+				JOIN ChargeCode CC ON TE.PersonId = @PersonId  AND CC.Id = TE.ChargeCodeId AND TE.ChargeCodeDate > @TerminationDate
 				JOIN TimeType TT ON TT.TimeTypeId = CC.TimeTypeId AND TT.IsAdministrative = 1
 
 			END
@@ -342,7 +343,7 @@
 						1,
 						1
 				FROM dbo.Calendar C
-				JOIN dbo.Pay p ON C.Date BETWEEN p.StartDate and ( CASE WHEN @TerminationDate IS NOT NULL AND @TerminationDate < p.EndDate - 1 THEN @TerminationDate
+				JOIN dbo.Pay p ON C.Date BETWEEN p.StartDate AND ( CASE WHEN @TerminationDate IS NOT NULL AND @TerminationDate < p.EndDate - 1 THEN @TerminationDate
 																	ELSE p.EndDate- 1 END) AND p.Person = @PersonId AND p.Timescale = @W2SalaryId
 				LEFT JOIN dbo.PersonCalendar PC ON PC.Date = C.Date AND PC.PersonId = P.Person AND PC.DayOff = 1
 				INNER JOIN dbo.TimeType TT ON ( (C.DayOff = 1  AND DATEPART(DW,C.DATE) NOT IN (1,7) AND TT.TimeTypeId = @HolidayTimeTypeId) 
@@ -371,7 +372,7 @@
 						CASE WHEN C.DayOff = 1 OR (PC.PersonId IS NOT NULL AND PC.DayOff = 1 AND PC.TimeTypeId <> @HolidayTimeTypeId AND PC.IsFromTimeEntry <> 1) THEN 2 
 							ELSE 1 END
 				FROM dbo.Calendar C
-				JOIN dbo.Pay p ON C.Date BETWEEN p.StartDate and ( CASE WHEN @TerminationDate IS NOT NULL AND @TerminationDate < p.EndDate - 1 THEN @TerminationDate
+				JOIN dbo.Pay p ON C.Date BETWEEN p.StartDate AND ( CASE WHEN @TerminationDate IS NOT NULL AND @TerminationDate < p.EndDate - 1 THEN @TerminationDate
 																	ELSE p.EndDate- 1 END) AND p.Person = @PersonId AND p.Timescale = @W2SalaryId
 				LEFT JOIN dbo.PersonCalendar PC ON PC.Date = C.Date AND PC.PersonId = P.Person AND PC.DayOff = 1
 				INNER JOIN dbo.TimeType TT ON ( (C.DayOff = 1  AND DATEPART(DW,C.DATE) NOT IN (1,7) AND TT.TimeTypeId = @HolidayTimeTypeId) 
