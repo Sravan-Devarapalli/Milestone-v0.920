9c1b1c38ebd9cde7b7b110bae68023d6477db051
SrinivasM <SrinivasM>
2012-07-09 02:19:00 -0500

    Comments - Defect Number: 3064; Defect RecordID: 3060; Server Address: localhost; Server Port: 99; Database Name: Practice Management; Defect Summary: Configuration: Persons - We need a Termination Reason whenever we terminate an employee; Defect Description: We need to begin capturing a Termination Reason whenever we terminate an employee. Currently we simply just add a Termination Date, click Save and the system changes the status to "Terminated". With this enhancement we'd like to also capture why a person is being Terminated.
    I am proposing the following changes to support this enhancement.
    
    1.) Add a "Termination Reason" field --> locate to the right of the Termination Date field. Disabled unless status == Terminated. Be sure the field length is set so that the box displayed is longer than the longest reason available. Users should be able to read the full text of the reason selected.
    
    2.) Disable the "Termination Date" field unless status == Terminated
    
    3.) Create a "Terminate Employee" button. --> to be located next to the Termination Reason Field field which should be disabled until the "Terminate Employee" process completes. If status == Terminated, button is disabled, else enabled.
    
    4.) Upon clicking the "Terminate Employee" button:
    
    - Open a modal dialog box with two fields: Termination Reason and Termination Date.
    - Both fields are required to save. Display descriptive error message if user clicks OK without completing required fields. (or simply don't enable "OK" unless required fields are completed).
    
    - Modal dialog box should have "OK" and "Cancel" buttons.
    
    - OK button saves data, populates the Termination Date field and the Termination Reason fields on the Persons page. Status changes to Terminated and Terminate Employee button disables.
    
    - Cancel button closes the modal dialog box and does not save the information entered.
    
    - Termination Reason options include (please alphabetize):
    "Involuntary - Job Abandonment"
    "Involuntary - Conduct/Performance"
    "Involuntary - No Work"
    "Involuntary - Reduction in Force"
    "Voluntary - No Reason Provided"
    "Voluntary - New Employment"
    "Voluntary - No Work (Hourly only)"
    
    Note: Additional reasons may be added in the future.
    
    Please be sure the modal dialog box is sized broad enough to enable the full display of the Termination Reason.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/PersonsListHavingActiveStatusDuringThisPeriod.proc.sql
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/PersonsListHavingActiveStatusDuringThisPeriod.proc.sql
@@ -5,48 +5,20 @@
     )
 AS 
     BEGIN
-		
-        DECLARE @MinStartdates TABLE
-            (
-              personId INT ,
-              minStartDate DATETIME ,
-              statusId INT
-            )
-		
-        INSERT  INTO @MinStartdates
-                ( personId ,
-                  minStartDate
-                )
-                SELECT  PSH.PersonId ,
-                        MIN(PSH.StartDate)
-                FROM    dbo.PersonStatusHistory PSH
-                GROUP BY PSH.PersonId 
-		
-        UPDATE  minSD
-        SET     statusId = PSH.PersonStatusId
-        FROM    @MinStartdates minSD
-                INNER JOIN dbo.PersonStatusHistory PSH ON PSH.PersonId = minSD.personId
-                                                          AND minSD.minStartDate = PSH.StartDate
+		DECLARE @FutureDate DATETIME
+		SELECT @FutureDate = dbo.GetFutureDate()
 
-
-        SELECT  p.PersonId ,
+        SELECT DISTINCT p.PersonId ,
                 p.FirstName ,
                 p.LastName ,
                 p.IsDefaultManager
         FROM    dbo.Person AS p
                 INNER JOIN dbo.PersonStatusHistory PSH ON PSH.PersonId = p.PersonId
-                INNER JOIN @MinStartdates minSD ON minSD.personId = p.PersonId
         WHERE   p.IsStrawman = 0
                 AND PSH.PersonStatusId = 1 -- ACTIVE Status
                 AND ( P.HireDate - ( DATEPART(dw, P.HireDate) - 1 ) ) <= @StartDate
-                AND ( ( @StartDate < ISNULL(PSH.EndDate, dbo.GetFutureDate())
+                AND ( @StartDate < ISNULL(PSH.EndDate, @FutureDate)
                         AND @EndDate > PSH.StartDate
-                      )
-                      OR ( minSD.statusId = 1
-                           AND P.HireDate < minSD.minStartDate
-                           AND @StartDate < minSD.minStartDate
-                           AND @EndDate > P.HireDate
-                         )
                     )
         ORDER BY p.LastName ,
                 p.FirstName
