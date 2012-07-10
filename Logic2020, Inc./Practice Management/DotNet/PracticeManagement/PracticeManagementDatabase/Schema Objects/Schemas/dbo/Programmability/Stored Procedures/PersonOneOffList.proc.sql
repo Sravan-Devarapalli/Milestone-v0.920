32fa65c53ece4546137819a48b285cb92ad39054
thulasiramp <thulasiramp>
2012-07-10 02:20:00 -0500

    Comments - Defect Number: 3063; Defect RecordID: 3059; Server Address: localhost; Server Port: 99; Database Name: Practice Management; Defect Summary: Configuration: Person - Termination should lockout user on Termination Date + 1 day; Defect Description: Currently, if a user terminates a person in the system, the user enters the Termination Date. Clicking Save will lock the person being terminated out of PM on the termination date itself. This is problematic as the person's employment technically ends at the end of that day, not the beginning. The person still needs access to the system to enable them to enter their time, finalize any last minute project/opportunity changes, etc.
    Therefore, we'd like the system to lock the person out of the system on the next calendar day instead. (i.e. Termination Date + 1)
    
    For example, if I terminate Employee Jane Doe with an Effective Date on July 2, 2012. Jane Doe will not be able to access PM on July 2nd. With this change, Jane Doe would retain access to PM through 7/2 and would not be able to access PM at 00:00:00 on July 3rd.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/PersonOneOffList.proc.sql
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/PersonOneOffList.proc.sql
@@ -1,42 +1,36 @@
-﻿CREATE PROCEDURE dbo.PersonOneOffList
+﻿-- =============================================
+-- Author:		
+-- Create date:
+-- Last Updated by:	Shwetha
+-- Last Update date: 10-07-2012
+-- Read All persons firstname and last name  except having inactive status and must have compensation for today or in future.
+-- =============================================
+CREATE PROCEDURE dbo.PersonOneOffList
 (
-	@DateToday   DATETIME,
-	@MaxSeniorityLevel	INT
+	@DateToday   DATETIME
 )
 AS
-	SET NOCOUNT ON
+BEGIN
+
+	SET NOCOUNT ON;
+
+	 DECLARE @FutureDate DATETIME    
+    SET @FutureDate = dbo.GetFutureDate()
 
 	SELECT p.PersonId,
 	       p.FirstName,
-	       p.LastName,
-	       p.PTODaysPerAnnum,
-	       p.HireDate,
-	       p.TerminationDate,
-	       p.Alias,
-	       p.DefaultPractice,
-	       p.PracticeName,
-	       p.PersonStatusId,
-	       p.PersonStatusName,
-		   p.EmployeeNumber,
-		   p.SeniorityId,
-		   p.SeniorityName,
-	       p.ManagerId,
-	       p.ManagerAlias,
-	       p.ManagerFirstName,
-	       p.ManagerLastName,
-	       p.PracticeOwnedId,
-	       p.PracticeOwnedName, 
-	       p.TelephoneNumber
-	  FROM dbo.v_Person AS p
-      LEFT JOIN dbo.Practice AS pr ON p.DefaultPractice = pr.PracticeId
-	 WHERE p.PersonStatusId in (1,2,3) AND ISNULL(pr.IsCompanyInternal, 0) = 0
-           AND ((@MaxSeniorityLevel IS NULL) OR (@MaxSeniorityLevel < p.SeniorityValue))
-		   AND EXISTS (SELECT 1 FROM dbo.v_Pay y
-						WHERE p.PersonId = y.PersonId 
+	       p.LastName	       
+	  FROM dbo.Person AS p
+      LEFT  JOIN dbo.Practice AS pr ON p.DefaultPractice = pr.PracticeId
+	  WHERE p.PersonStatusId in (1,2,3) AND ISNULL(pr.IsCompanyInternal, 0) = 0
+		   AND EXISTS (SELECT 1 FROM dbo.Pay y
+						WHERE p.PersonId = y.Person 
 						AND  (@DateToday <= y.StartDate
-						 OR @DateToday BETWEEN y.StartDate AND (ISNULL(y.EndDate, dbo.GetFutureDate()) - 1)
+						 OR @DateToday BETWEEN y.StartDate AND (ISNULL(y.EndDate, @FutureDate) - 1)
 						 )
 					  )
 		  AND p.IsStrawman = 0
      ORDER BY p.LastName
+
+END
 
