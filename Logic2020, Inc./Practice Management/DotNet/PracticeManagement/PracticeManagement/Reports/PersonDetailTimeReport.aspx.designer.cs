e22f804936a8e06b0513af69a16831a877add260
SainathC <SainathC>
2012-07-12 03:59:00 -0500

    Comments - Defect Number: 3058; Defect RecordID: 3054; Server Address: localhost; Server Port: 99; Database Name: Practice Management; Defect Summary: Site optimization request; Defect Description: I've been looking at pages on the site with the Firebug add-in for Firefox to see how well it is performing. Page loading always seems slow to me, and Firebug notes multiple issues that are affecting the site's efficiency. When there isn't a more pressing feature request, please begin using Firebug (or another tool) to analyze the site and make optimizations to reduce loading time, improve caching, etc.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/Reports/PersonDetailTimeReport.aspx.designer.cs
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/Reports/PersonDetailTimeReport.aspx.designer.cs
@@ -409,15 +409,6 @@ namespace PraticeManagement.Reporting {
         protected global::PraticeManagement.Controls.Reports.PersonDetailReport ucpersonDetailReport;
         
         /// <summary>
-        /// btnCancelAndReturn control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::PraticeManagement.Controls.Generic.Buttons.CancelAndReturnButton btnCancelAndReturn;
-        
-        /// <summary>
         /// mpePersonSearch control.
         /// </summary>
         /// <remarks>
