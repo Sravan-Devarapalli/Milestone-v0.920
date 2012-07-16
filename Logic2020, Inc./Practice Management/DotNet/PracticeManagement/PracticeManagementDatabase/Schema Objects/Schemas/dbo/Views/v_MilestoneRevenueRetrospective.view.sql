f6ce1b509c63bc1c30972df3ae1bdf1e90104cb8
SrinivasM <SrinivasM>
2012-07-16 01:24:00 -0500

    Comments - Defect Number: 3061; Defect RecordID: 3057; Server Address: localhost; Server Port: 99; Database Name: Practice Management; Defect Summary: Performance: Projects>Summary / Projects>Group BY- Improve performance by removing unnecessary DB calls.; Defect Description: Note: While navigating to Projects>Summary / Projects>Group BY page (with multiple accounts being selected in filters), DB calls are made for each account.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Views/v_MilestoneRevenueRetrospective.view.sql
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Views/v_MilestoneRevenueRetrospective.view.sql
@@ -8,40 +8,34 @@
 CREATE VIEW dbo.v_MilestoneRevenueRetrospective--Day level Milestone Amount & hours
 AS
 	SELECT -- Milestones with a fixed amount
-		   m.MilestoneId,
-		   m.ProjectId,
-		   cal.Date,
-		   m.IsHourlyAmount,
-	       ISNULL((m.Amount/* Milestone fixed amount */ 
-						   / 
-							(   SELECT SUM(HoursPerDay)
-								FROM   MilestonePerson MP  
-								JOIN MilestonePersonEntry  MPE ON MP.MileStonePersonId = MPE.MileStonePersonId
-								JOIN PersonCalendarAuto cal ON cal.Date BETWEEN mpe.Startdate AND ISNULL(mpe.EndDate, m.ProjectedDeliveryDate) AND cal.DayOff=0
-								AND cal.PersonId = mp.PersonId 
-								WHERE  mp.MileStoneId = m.MilestoneId
-								GROUP BY MP.MilestoneId
+		m.MilestoneId,
+		m.ProjectId,
+		cal.Date,
+		m.IsHourlyAmount,
+		ISNULL((m.Amount/* Milestone fixed amount */ 
+						/ 
+						MTHours.TotalHours
+							/* Milestone Total  Hours */
 							
-                            ) /* Milestone Total  Hours */
-							
-							)* ISNULL(d.HoursPerDay, 0)/* Milestone Total  Hours per day*/,
-	              (CASE (DATEDIFF(dd, m.StartDate, m.ProjectedDeliveryDate) + 1)
-	                   WHEN 0 THEN 0
-	                   ELSE m.Amount / (DATEDIFF(dd, m.StartDate, m.ProjectedDeliveryDate) + 1)
-	               END)) AS MilestoneDailyAmount /* ((Milestone fixed amount/Milestone Total  Hours)* Milestone Total  Hours per day)  */,
-		   m.StartDate AS MilestoneStartDate,
-		   m.ProjectedDeliveryDate,
-           p.Discount,
-	       prac.PracticeManagerId,
-	       d.HoursPerDay/* Milestone Total  Hours per day*/
-	  FROM dbo.Milestone AS m
-		   INNER JOIN dbo.Calendar AS cal ON cal.Date BETWEEN m.StartDate AND m.ProjectedDeliveryDate
-	       INNER JOIN dbo.Project AS p ON m.ProjectId = p.ProjectId
-	       INNER JOIN dbo.Practice AS prac ON p.PracticeId = prac.PracticeId
-           LEFT JOIN (SELECT s.Date, s.MilestoneId, SUM(HoursPerDay) AS HoursPerDay
-	                    FROM dbo.v_MilestonePersonSchedule AS s
-	                  GROUP BY s.Date, s.MilestoneId) d ON d.date = cal.Date and m.MilestoneId = d.MileStoneId
-	 WHERE m.IsHourlyAmount = 0
+						)* ISNULL(d.HoursPerDay, 0)/* Milestone Total  Hours per day*/,
+				(CASE (DATEDIFF(dd, m.StartDate, m.ProjectedDeliveryDate) + 1)
+					WHEN 0 THEN 0
+					ELSE m.Amount / (DATEDIFF(dd, m.StartDate, m.ProjectedDeliveryDate) + 1)
+				END)) AS MilestoneDailyAmount /* ((Milestone fixed amount/Milestone Total  Hours)* Milestone Total  Hours per day)  */,
+		m.StartDate AS MilestoneStartDate,
+		m.ProjectedDeliveryDate,
+		p.Discount,
+		prac.PracticeManagerId,
+		d.HoursPerDay/* Milestone Total  Hours per day*/
+	FROM dbo.Milestone AS m
+		OUTER APPLY dbo.GetMilestoneNetAssignedHours(m.MilestoneId) AS MTHours 
+		INNER JOIN dbo.Calendar AS cal ON cal.Date BETWEEN m.StartDate AND m.ProjectedDeliveryDate
+		INNER JOIN dbo.Project AS p ON m.ProjectId = p.ProjectId
+		INNER JOIN dbo.Practice AS prac ON p.PracticeId = prac.PracticeId
+		LEFT JOIN (SELECT s.Date, s.MilestoneId, SUM(HoursPerDay) AS HoursPerDay
+					FROM dbo.v_MilestonePersonSchedule AS s
+					GROUP BY s.Date, s.MilestoneId) d ON d.date = cal.Date and m.MilestoneId = d.MileStoneId
+	WHERE m.IsHourlyAmount = 0
 	UNION ALL
 	SELECT -- Milestones with a hourly amount
 		   mp.MilestoneId,
