c6894b49c671e4d62239de402f3f1b80c50ffa35
SainathC <SainathC>
2012-07-16 10:15:00 -0500

    Comments - Defect Number: 3061; Defect RecordID: 3057; Server Address: localhost; Server Port: 99; Database Name: Practice Management; Defect Summary: Performance: Projects>Summary / Projects>Group BY- Improve performance by removing unnecessary DB calls.; Defect Description: Note: While navigating to Projects>Summary / Projects>Group BY page (with multiple accounts being selected in filters), DB calls are made for each account.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/OpportunityListAllShort.proc.sql
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/OpportunityListAllShort.proc.sql
@@ -5,14 +5,16 @@
 AS
 BEGIN
 	SET NOCOUNT ON
+	DECLARE @FutureDate DATETIME 
+	SET @FutureDate = dbo.GetFutureDate()
 
 	;WITH CTE
 	AS
 	(
 	SELECT ROW_NUMBER() OVER(PARTITION BY O.ClientName + isnull(O.BuyerName, '') 
 							ORDER BY CASE OP.sortOrder WHEN 0 THEN 1000 ELSE OP.sortOrder END,
-							YEAR(ISNULL(O.ProjectedStartDate,dbo.GetFutureDate())),
-							MONTH(ISNULL(O.ProjectedStartDate,dbo.GetFutureDate())),
+							YEAR(ISNULL(O.ProjectedStartDate,@FutureDate)),
+							MONTH(ISNULL(O.ProjectedStartDate,@FutureDate)),
 							O.SalespersonLastName) RowNumber,
 		   o.OpportunityId,
 		   o.Name,
@@ -27,12 +29,12 @@ BEGIN
 		   o.SalespersonFirstName,
 		   o.SalespersonLastName,
 		   o.OwnerId,
-		   p.LastName as 'OwnerLastName',
-		   p.FirstName as 'OwnerFirstName',
+		   p.LastName as [OwnerLastName],
+		   p.FirstName as [OwnerFirstName],
 		   o.EstimatedRevenue,
 		   o.BuyerName,
 		   O.ProjectedStartDate
-	FROM v_Opportunity AS o
+	FROM dbo.v_Opportunity AS o
 	LEFT JOIN dbo.Person p ON o.OwnerId = p.PersonId
 	INNER JOIN dbo.OpportunityPriorities AS op ON op.Id = o.PriorityId
 	WHERE o.OpportunityStatusId = 1 OR @ActiveOnly = 0
@@ -41,14 +43,16 @@ BEGIN
 			B.*
 		FROM CTE A
 		JOIN CTE B
-		ON (A.ClientName =B.ClientName AND isnull(A.BuyerName, '')  = isnull(B.BuyerName, '') 
+		ON (A.ClientName =B.ClientName AND ISNULL(A.BuyerName, '')  = ISNULL(B.BuyerName, '') 
 			AND A.RowNumber=1 AND A.PrioritySortOrder!=0 AND B.PrioritySortOrder != 0 ) 
 			OR (A.OpportunityId = B.OpportunityId AND A.PrioritySortOrder=0)
 		ORDER BY A.PrioritySortOrder,
-		YEAR(ISNULL(A.ProjectedStartDate,dbo.GetFutureDate())),
-		MONTH(ISNULL(A.ProjectedStartDate,dbo.GetFutureDate())),
-		A.SalespersonLastName,
-		B.ClientName,
-		isnull(B.BuyerName, ''),B.PrioritySortOrder,B.SalespersonLastName
+				YEAR(ISNULL(A.ProjectedStartDate,@FutureDate)),
+				MONTH(ISNULL(A.ProjectedStartDate,@FutureDate)),
+				A.SalespersonLastName,
+				B.ClientName,
+				ISNULL(B.BuyerName, ''),
+				B.PrioritySortOrder,
+				B.SalespersonLastName
 
 END
