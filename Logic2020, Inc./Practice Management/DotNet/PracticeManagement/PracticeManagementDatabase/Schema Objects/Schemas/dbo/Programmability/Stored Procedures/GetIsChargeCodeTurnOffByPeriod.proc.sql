1e590ccf495dd2d28d17633d59e6bac5c7528eb4
SainathC <SainathC>
2012-07-17 09:24:00 -0500

    Comments - Defect Number: 3061; Defect RecordID: 3057; Server Address: localhost; Server Port: 99; Database Name: Practice Management; Defect Summary: Performance: Projects>Summary / Projects>Group BY- Improve performance by removing unnecessary DB calls.; Defect Description: Note: While navigating to Projects>Summary / Projects>Group BY page (with multiple accounts being selected in filters), DB calls are made for each account.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/GetIsChargeCodeTurnOffByPeriod.proc.sql
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/GetIsChargeCodeTurnOffByPeriod.proc.sql
@@ -10,12 +10,14 @@
 )
 AS
 BEGIN 
+	DECLARE @False BIT,@True BIT,@FutureDate DATETIME
+	SELECT @False = CONVERT(BIT,0) ,@True = CONVERT(BIT,1),@FutureDate = dbo.GetFutureDate()
 
 	SELECT D.ChargeCodeDate
-		,CASE WHEN CCH.ChargeCodeId IS NULL THEN CONVERT(BIT,0) ELSE CONVERT(BIT,1) END AS 'IsChargeCodeOff'
-	FROM (select date AS ChargeCodeDate from dbo.Calendar where DATE BETWEEN @StartDate and @EndDate) D 
+		,CASE WHEN CCH.ChargeCodeId IS NULL THEN @False ELSE @True END AS [IsChargeCodeOff]
+	FROM (SELECT DATE AS ChargeCodeDate FROM dbo.Calendar WHERE DATE BETWEEN @StartDate and @EndDate) D 
 	LEFT JOIN dbo.ChargeCode CC ON CC.ClientId = @ClientId AND 	CC.ProjectGroupId = @ProjectGroupId AND CC.ProjectId = @ProjectId AND CC.TimeTypeId = @TimeTypeId
-	LEFT JOIN dbo.ChargeCodeTurnOffHistory CCH ON CC.Id = CCH.ChargeCodeId AND D.ChargeCodeDate BETWEEN CCH.StartDate AND ISNULL(CCH.EndDate,dbo.GetFutureDate())
+	LEFT JOIN dbo.ChargeCodeTurnOffHistory CCH ON CC.Id = CCH.ChargeCodeId AND D.ChargeCodeDate BETWEEN CCH.StartDate AND ISNULL(CCH.EndDate,@FutureDate)
 
 END
 
