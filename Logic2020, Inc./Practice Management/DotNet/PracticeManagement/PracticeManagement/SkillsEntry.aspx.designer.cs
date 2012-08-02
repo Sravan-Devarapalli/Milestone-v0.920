8070846bbd7f84f3e92a039f01a51995804d807b
thulasiramp <thulasiramp>
2012-08-02 08:18:00 -0500

    Comments - Defect Number: 3058; Defect RecordID: 3054; Server Address: localhost; Server Port: 99; Database Name: Practice Management; Defect Summary: Site optimization request; Defect Description: I've been looking at pages on the site with the Firebug add-in for Firefox to see how well it is performing. Page loading always seems slow to me, and Firebug notes multiple issues that are affecting the site's efficiency. When there isn't a more pressing feature request, please begin using Firebug (or another tool) to analyze the site and make optimizations to reduce loading time, improve caching, etc.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/SkillsEntry.aspx.designer.cs
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/SkillsEntry.aspx.designer.cs
@@ -229,15 +229,6 @@ namespace PraticeManagement {
         protected global::System.Web.UI.WebControls.Button btnOk;
         
         /// <summary>
-        /// btnCancelValidations control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Button btnCancelValidations;
-        
-        /// <summary>
         /// mpeValidations control.
         /// </summary>
         /// <remarks>
