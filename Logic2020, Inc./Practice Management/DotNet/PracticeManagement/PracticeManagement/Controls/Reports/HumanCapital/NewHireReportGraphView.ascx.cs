4efab5d581b5f696a9e4aaed9d74448f09e430dc
SainathC <SainathC>
2012-08-13 11:35:00 -0500

    Comments - Defect Number: 3072; Defect RecordID: 3068; Server Address: localhost; Server Port: 99; Database Name: Practice Management; Defect Summary: Reports: Human Capital --> New Hire Report; Defect Description: We would like to add a few new reports. This ticket is one of those new reports. It is for a New Hire report. It should be created under a new sub-menu called, "Human Capital" under the primary menu "Reports". It is to be accessible only by users with the following roles: Senior Leadership Client Director HR Recruiter Administrator
    Upon selecting "Human Capital" from Reports-->Human Capital the page will display allowing a user to select the New Hire report -- reference the Time reports for similar behaviors.
    
    Refer to the attached specification for further details on the New Hire report functionality.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/Controls/Reports/HumanCapital/NewHireReportGraphView.ascx.cs
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/Controls/Reports/HumanCapital/NewHireReportGraphView.ascx.cs
@@ -4,14 +4,37 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using DataTransferObjects;
 
 namespace PraticeManagement.Controls.Reports.HumanCapital
 {
     public partial class NewHireGraphView : System.Web.UI.UserControl
     {
+        private PraticeManagement.Reporting.NewHireReport HostingPage
+        {
+            get { return ((PraticeManagement.Reporting.NewHireReport)Page); }
+        }
+
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+
+        public void PopulateGraph()
+        {
+            List<Person> data = ServiceCallers.Custom.Report(r => r.NewHireReport(HostingPage.StartDate.Value, HostingPage.EndDate.Value, HostingPage.PersonStatus, HostingPage.PersonStatus, HostingPage.Practices, HostingPage.ExcludeInternalProjects, null, null, null, null)).ToList();
+                
+            
+        }
+
+        protected void btnExportToExcel_OnClick(object sender, EventArgs e)
+        {
+            if (HostingPage.StartDate.HasValue && HostingPage.EndDate.HasValue)
+            {
+                List<Person> data = ServiceCallers.Custom.Report(r => r.NewHireReport(HostingPage.StartDate.Value, HostingPage.EndDate.Value, HostingPage.PersonStatus, HostingPage.PersonStatus, HostingPage.Practices, HostingPage.ExcludeInternalProjects, null, null, null, null)).ToList();
+                List<string> filteredColoums = new List<string>();
+                HostingPage.ExportToExcel(data, filteredColoums);
+            }
         }
     }
 }
