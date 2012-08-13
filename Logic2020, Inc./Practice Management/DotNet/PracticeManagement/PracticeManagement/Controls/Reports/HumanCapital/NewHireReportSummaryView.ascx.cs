f62f1b2d4ba98f18ea9d4078ecb688bf9bf0d5d7
SainathC <SainathC>
2012-08-13 11:35:00 -0500

    Comments - Defect Number: 3072; Defect RecordID: 3068; Server Address: localhost; Server Port: 99; Database Name: Practice Management; Defect Summary: Reports: Human Capital --> New Hire Report; Defect Description: We would like to add a few new reports. This ticket is one of those new reports. It is for a New Hire report. It should be created under a new sub-menu called, "Human Capital" under the primary menu "Reports". It is to be accessible only by users with the following roles: Senior Leadership Client Director HR Recruiter Administrator
    Upon selecting "Human Capital" from Reports-->Human Capital the page will display allowing a user to select the New Hire report -- reference the Time reports for similar behaviors.
    
    Refer to the attached specification for further details on the New Hire report functionality.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/Controls/Reports/HumanCapital/NewHireReportSummaryView.ascx.cs
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/Controls/Reports/HumanCapital/NewHireReportSummaryView.ascx.cs
@@ -4,14 +4,228 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Web.UI.HtmlControls;
+using DataTransferObjects;
+using System.Text;
 
 namespace PraticeManagement.Controls.Reports.HumanCapital
 {
-    public partial class HumanCapitalSummaryReport : System.Web.UI.UserControl
+    public partial class NewHireReportSummaryView : System.Web.UI.UserControl
     {
+       
+        #region Properties
+
+        private HtmlImage ImgSeniorityFilter { get; set; }
+
+        private HtmlImage ImgPayTypeFilter { get; set; }
+
+        private HtmlImage ImgHiredateFilter { get; set; }
+
+        private HtmlImage ImgPersonStatusTypeFilter { get; set; }
+
+        private HtmlImage ImgDivisionFilter { get; set; }
+
+        private HtmlImage ImgRecruiterFilter { get; set; }
+
+        private PraticeManagement.Reporting.NewHireReport HostingPage
+        {
+            get { return ((PraticeManagement.Reporting.NewHireReport)Page); }
+        }
+
+        #endregion
+
+        #region PageEvents
+
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            cblRecruiter.OKButtonId = cblHireDate.OKButtonId = cblSeniorities.OKButtonId = cblPayTypes.OKButtonId = cblPersonStatusType.OKButtonId = cblDivision.OKButtonId = btnFilterOK.ClientID;
         }
+
+        #endregion
+
+        #region ControlEvents
+
+        protected void btnExportToExcel_OnClick(object sender, EventArgs e)
+        {
+            
+            if (HostingPage.StartDate.HasValue && HostingPage.EndDate.HasValue)
+            {
+
+                var data = ServiceCallers.Custom.Report(r => r.NewHireReport(HostingPage.StartDate.Value, HostingPage.EndDate.Value, cblPersonStatusType.SelectedItemsXmlFormat, cblPayTypes.SelectedItemsXmlFormat, HostingPage.Practices, HostingPage.ExcludeInternalProjects, cblDivision.SelectedItemsXmlFormat, cblSeniorities.SelectedItemsXmlFormat, cblHireDate.SelectedItemsXmlFormat, cblRecruiter.SelectedItemsXmlFormat)).ToList();
+
+                
+                List<string> filteredColoums = new List<string>();
+                if (!cblHireDate.AllItemsSelected)
+                {
+                    filteredColoums.Add("Hire Date");
+                }
+                if (!cblSeniorities.AllItemsSelected)
+                {
+                    filteredColoums.Add("Seniority");
+                }
+                if (!cblPayTypes.AllItemsSelected)
+                {
+                    filteredColoums.Add("Pay Type");
+                }
+                if (!cblDivision.AllItemsSelected)
+                {
+                    filteredColoums.Add("Resource");
+                }
+                if (!cblPersonStatusType.AllItemsSelected)
+                {
+                    filteredColoums.Add("Status");
+                }
+                if (!cblRecruiter.AllItemsSelected)
+                {
+                    filteredColoums.Add("Recruiter");
+                }
+                HostingPage.ExportToExcel(data, filteredColoums);
+            }
+        }
+
+        protected void repResource_ItemDataBound(object sender, RepeaterItemEventArgs e)
+        {
+            if (e.Item.ItemType == ListItemType.Header)
+            {
+                ImgDivisionFilter = e.Item.FindControl("imgDivisionFilter") as HtmlImage;
+                ImgSeniorityFilter = e.Item.FindControl("imgSeniorityFilter") as HtmlImage;
+                ImgPayTypeFilter = e.Item.FindControl("imgPayTypeFilter") as HtmlImage;
+                ImgHiredateFilter = e.Item.FindControl("imgHiredateFilter") as HtmlImage;
+                ImgPersonStatusTypeFilter = e.Item.FindControl("imgPersonStatusTypeFilter") as HtmlImage;
+                ImgRecruiterFilter = e.Item.FindControl("imgRecruiterFilter") as HtmlImage;
+            }
+        }
+
+        protected void btnFilterOK_OnClick(object sender, EventArgs e)
+        {
+            PopulateData();
+        }
+
+        #endregion
+
+        #region Methods
+
+        protected string GetDateFormat(DateTime date)
+        {
+            return date.ToString(Constants.Formatting.EntryDateFormat);
+        }
+
+        public void PopulateData()
+        {
+            List<Person> data;
+            if (HostingPage.SetSelectedFilters)
+            {
+                data = ServiceCallers.Custom.Report(r => r.NewHireReport(HostingPage.StartDate.Value, HostingPage.EndDate.Value, HostingPage.PersonStatus, HostingPage.PersonStatus, HostingPage.Practices, HostingPage.ExcludeInternalProjects, null, null, null, null)).ToList();
+                PopulateFilterPanels(data);
+            }
+            else
+            {
+                data = ServiceCallers.Custom.Report(r => r.NewHireReport(HostingPage.StartDate.Value, HostingPage.EndDate.Value, cblPersonStatusType.SelectedItemsXmlFormat, cblPayTypes.SelectedItemsXmlFormat, HostingPage.Practices, HostingPage.ExcludeInternalProjects, cblDivision.SelectedItemsXmlFormat, cblSeniorities.SelectedItemsXmlFormat, cblHireDate.SelectedItemsXmlFormat, cblRecruiter.SelectedItemsXmlFormat)).ToList();
+            }
+            DataBindResource(data);
+        }
+
+        public void DataBindResource(List<Person> reportData)
+        {
+            var reportDataList = reportData.ToList();
+            if (reportDataList.Count > 0 || cblSeniorities.Items.Count > 1 || cblPayTypes.Items.Count > 1 || cblHireDate.Items.Count > 1 || cblDivision.Items.Count > 1 || cblPersonStatusType.Items.Count > 1 || cblRecruiter.Items.Count > 1)
+            {
+                divEmptyMessage.Style["display"] = "none";
+                repResource.Visible = true;
+                repResource.DataSource = reportDataList;
+                repResource.DataBind();
+                SetAttribitesForFiltersImages();
+            }
+            else
+            {
+                divEmptyMessage.Style["display"] = "";
+                repResource.Visible = false;
+            }
+            HostingPage.PopulateHeaderSection(reportDataList);
+        }
+
+        private void SetAttribitesForFiltersImages()
+        {
+            cblSeniorities.SaveSelectedIndexesInViewState();
+            cblPayTypes.SaveSelectedIndexesInViewState();
+            cblRecruiter.SaveSelectedIndexesInViewState();
+            cblDivision.SaveSelectedIndexesInViewState();
+            cblHireDate.SaveSelectedIndexesInViewState();
+            cblPersonStatusType.SaveSelectedIndexesInViewState();
+
+            ImgSeniorityFilter.Attributes["onclick"] = string.Format("Filter_Click(\'{0}\',\'{1}\',\'{2}\',\'{3}\');", cblSeniorities.FilterPopupClientID,
+              cblSeniorities.SelectedIndexes, cblSeniorities.CheckBoxListObject.ClientID, cblSeniorities.WaterMarkTextBoxBehaviorID);
+
+            ImgPayTypeFilter.Attributes["onclick"] = string.Format("Filter_Click(\'{0}\',\'{1}\',\'{2}\',\'{3}\');", cblPayTypes.FilterPopupClientID,
+               cblPayTypes.SelectedIndexes, cblPayTypes.CheckBoxListObject.ClientID, cblPayTypes.WaterMarkTextBoxBehaviorID);
+
+            ImgHiredateFilter.Attributes["onclick"] = string.Format("Filter_Click(\'{0}\',\'{1}\',\'{2}\',\'{3}\');", cblHireDate.FilterPopupClientID,
+               cblHireDate.SelectedIndexes, cblHireDate.CheckBoxListObject.ClientID, cblHireDate.WaterMarkTextBoxBehaviorID);
+
+            ImgDivisionFilter.Attributes["onclick"] = string.Format("Filter_Click(\'{0}\',\'{1}\',\'{2}\',\'{3}\');", cblDivision.FilterPopupClientID,
+               cblDivision.SelectedIndexes, cblDivision.CheckBoxListObject.ClientID, cblDivision.WaterMarkTextBoxBehaviorID);
+
+            ImgPersonStatusTypeFilter.Attributes["onclick"] = string.Format("Filter_Click(\'{0}\',\'{1}\',\'{2}\',\'{3}\');", cblPersonStatusType.FilterPopupClientID,
+               cblPersonStatusType.SelectedIndexes, cblPersonStatusType.CheckBoxListObject.ClientID, cblPersonStatusType.WaterMarkTextBoxBehaviorID);
+
+            ImgRecruiterFilter.Attributes["onclick"] = string.Format("Filter_Click(\'{0}\',\'{1}\',\'{2}\',\'{3}\');", cblRecruiter.FilterPopupClientID,
+              cblRecruiter.SelectedIndexes, cblRecruiter.CheckBoxListObject.ClientID, cblRecruiter.WaterMarkTextBoxBehaviorID);
+        }
+
+        private void PopulateFilterPanels(List<Person> reportData)
+        {
+            PopulateDivisionFilter(reportData);
+            PopulateSeniorityFilter(reportData);
+            PopulateHireDateFilter(reportData);
+            PopulatePayTypeFilter(reportData);
+            PopulatPersonStatusTypeFilter(reportData);
+            PopulateRecruiterFilter(reportData);
+        }
+
+        private void PopulateDivisionFilter(List<Person> reportData)
+        {
+            var divisionList = reportData.Select(r => new { Value = (int)r.DivisionType == 0 ? string.Empty : ((int)r.DivisionType).ToString(), Name = (int)r.DivisionType == 0 ? "Unassigned" : r.DivisionType.ToString() }).Distinct().ToList().OrderBy(d => d.Value).ToArray();
+            DataHelper.FillListDefault(cblDivision.CheckBoxListObject, "All Division Types", divisionList, false, "Value", "Name");
+            cblDivision.SelectAllItems(true);
+        }
+
+        private void PopulateHireDateFilter(List<Person> reportData)
+        {
+            var hireDateList = reportData.Select(r => new { Name = r.HireDate.ToString("yyyy MMM") }).ToList().OrderBy(s => s.Name);
+            DataHelper.FillListDefault(cblHireDate.CheckBoxListObject, "All Months ", hireDateList.ToArray(), false, "Name", "Name");
+            cblHireDate.SelectAllItems(true);
+        }
+
+        private void PopulatPersonStatusTypeFilter(List<Person> reportData)
+        {
+            var personStatusTypeList = reportData.Select(r => new { Name = r.Status.Name, Id = r.Status.Id }).Distinct().ToList().OrderBy(s => s.Name);
+            DataHelper.FillListDefault(cblPersonStatusType.CheckBoxListObject, "All Person Status Types", personStatusTypeList.ToArray(), false, "Id", "Name");
+            cblPersonStatusType.SelectAllItems(true);
+        }
+
+        private void PopulateSeniorityFilter(List<Person> reportData)
+        {
+            var seniorities = reportData.Select(r => new { Id = r.Seniority.Id, Name = r.Seniority.Name }).Distinct().ToList().OrderBy(s => s.Name);
+            DataHelper.FillListDefault(cblSeniorities.CheckBoxListObject, "All Seniorities", seniorities.ToArray(), false, "Id", "Name");
+            cblSeniorities.SelectAllItems(true);
+        }
+
+        private void PopulatePayTypeFilter(List<Person> reportData)
+        {
+            var payTypes = reportData.Select(r => new { Text = string.IsNullOrEmpty(r.CurrentPay.TimescaleName) ? "Unassigned" : r.CurrentPay.TimescaleName, Value = r.CurrentPay.TimescaleName }).Distinct().ToList().OrderBy(t => t.Value);
+            DataHelper.FillListDefault(cblPayTypes.CheckBoxListObject, "All Pay Types", payTypes.ToArray(), false, "Value", "Text");
+            cblPayTypes.SelectAllItems(true);
+        }
+
+        private void PopulateRecruiterFilter(List<Person> reportData)
+        {
+            var payTypes = reportData.Select(r => new { Text = string.IsNullOrEmpty(r.CurrentPay.TimescaleName) ? "Unassigned" : r.CurrentPay.TimescaleName, Value = r.CurrentPay.TimescaleName }).Distinct().ToList().OrderBy(t => t.Value);
+            DataHelper.FillListDefault(cblRecruiter.CheckBoxListObject, "All Pay Types", payTypes.ToArray(), false, "Value", "Text");
+            cblRecruiter.SelectAllItems(true);
+        }
+
+        #endregion
+
     }
 }
+
