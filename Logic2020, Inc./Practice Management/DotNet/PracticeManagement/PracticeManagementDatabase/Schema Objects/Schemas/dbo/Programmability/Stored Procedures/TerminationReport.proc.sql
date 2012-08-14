07aadcebcdf3c5c9a1d32db37ef4910d62ee1acc
SainathC <SainathC>
2012-08-14 02:34:00 -0500

    Comments - Defect Number: 3072; Defect RecordID: 3068; Server Address: localhost; Server Port: 99; Database Name: Practice Management; Defect Summary: Reports: Human Capital --> New Hire Report; Defect Description: We would like to add a few new reports. This ticket is one of those new reports. It is for a New Hire report. It should be created under a new sub-menu called, "Human Capital" under the primary menu "Reports". It is to be accessible only by users with the following roles: Senior Leadership Client Director HR Recruiter Administrator
    Upon selecting "Human Capital" from Reports-->Human Capital the page will display allowing a user to select the New Hire report -- reference the Time reports for similar behaviors.
    
    Refer to the attached specification for further details on the New Hire report functionality.

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/TerminationReport.proc.sql
@@ -0,0 +1,44 @@
+﻿CREATE PROCEDURE [dbo].[TerminationReport]
+(
+	@StartDate	DATETIME,
+	@EndDate	DATETIME,
+	@TimeScaleIds	XML = null,
+	@PersonStatusIds	XML = null,
+	@SeniorityIds	XML = null,
+	@TerminationReasonIds XML = NULL,
+	@PracticeIds	XML = null,
+	@ExcludeInternalPractices	BIT,
+	@PersonDivisionIds	XML = null,
+	@RecruiterIds	XML = null,
+	@HireDates	XML = null,
+	@TerminationDates	XML = null
+)
+AS
+BEGIN
+		SELECT P.PersonId,
+			P.FirstName,
+			P.LastName,
+			Ps.PersonStatusId,
+			PS.Name AS PersonStatusName,
+			TS.TimescaleId,
+			TS.Name AS TimescaleName,
+			RC.RecruiterId,
+			RCP.FirstName AS RecruiterFirstName ,
+			RCP.LastName RecruiterLastName,
+			S.SeniorityId,
+			S.Name AS SeniorityName,
+			P.DivisionId,
+			P.HireDate,
+			P.TerminationDate,
+			P.TerminationReasonId,
+			TR.TerminationReason
+	FROM dbo.Person P
+	INNER JOIN dbo.Pay Pay ON P.PersonId = Pay.Person
+	INNER JOIN dbo.Timescale TS ON TS.TimescaleId = Pay.Timescale
+	INNER JOIN dbo.RecruiterCommission RC ON RC.RecruitId = P.PersonId
+	INNER JOIN dbo.Person RCP ON RC.RecruiterId = RCP.PersonId
+	INNEr JOIN dbo.PersonStatus PS ON PS.PersonStatusId = P.PersonStatusId
+	INNER JOIN dbo.Seniority S ON S.[SeniorityId] = Pay.[SeniorityId]
+	INNER JOIN dbo.TerminationReasons TR ON TR.TerminationReasonId = P.TerminationReasonId
+
+END
