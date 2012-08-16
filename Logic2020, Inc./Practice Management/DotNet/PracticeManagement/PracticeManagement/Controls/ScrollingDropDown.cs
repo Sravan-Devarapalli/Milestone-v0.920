ddd401ab9d903f392938a1659ad78fff6e9357d8
SainathC <SainathC>
2012-08-16 07:06:00 -0500

    Comments - Defect Number: 3072; Defect RecordID: 3068; Server Address: localhost; Server Port: 99; Database Name: Practice Management; Defect Summary: Reports: Human Capital --> New Hire Report; Defect Description: We would like to add a few new reports. This ticket is one of those new reports. It is for a New Hire report. It should be created under a new sub-menu called, "Human Capital" under the primary menu "Reports". It is to be accessible only by users with the following roles: Senior Leadership Client Director HR Recruiter Administrator
    Upon selecting "Human Capital" from Reports-->Human Capital the page will display allowing a user to select the New Hire report -- reference the Time reports for similar behaviors.
    
    Refer to the attached specification for further details on the New Hire report functionality.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/Controls/ScrollingDropDown.cs
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/Controls/ScrollingDropDown.cs
@@ -533,6 +533,7 @@ namespace PraticeManagement.Controls
             string writeText = string.Empty;
             writeText = string.IsNullOrEmpty(CssClass) ?  string.Format("<div id='{0}' class='scroll_{0}' />" , ID) : string.Format("<div id='{0}' class='{1}' />", ID, CssClass);
             writer.WriteLine(writeText);
+            this.Width = new Unit(100d, UnitType.Percentage);
             base.RenderControl(writer);
             writer.WriteLine("</div>");
         }
