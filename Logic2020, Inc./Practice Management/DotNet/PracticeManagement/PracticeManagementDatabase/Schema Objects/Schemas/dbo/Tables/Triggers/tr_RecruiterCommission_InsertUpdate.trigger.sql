ae45da323c1cfa508acfbcd26ea2b65dedcfd0ef
SrinivasM <SrinivasM>
2012-08-17 01:52:00 -0500

    Comments - Defect Number: 3064; Defect RecordID: 3060; Server Address: localhost; Server Port: 99; Database Name: Practice Management; Defect Summary: Configuration: Persons - We need a Change Employee Status capability; Defect Description: We need to begin capturing a Termination Reason whenever we terminate an employee. Currently we simply just add a Termination Date, click Save and the system changes the status to "Terminated". With this enhancement we'd like to also capture why a person is being Terminated.
    I am proposing the following changes to support this enhancement.
    
    1.) Add a "Termination Reason" field --> locate to the right of the Termination Date field. Disabled unless status == Terminated. Be sure the field length is set so that the box displayed is longer than the longest reason available. Users should be able to read the full text of the reason selected.
    
    2.) Disable the "Termination Date" field unless status == Terminated
    
    3.) Create a "Terminate Employee" button. --> to be located next to the Termination Reason Field field which should be disabled until the "Terminate Employee" process completes. If status == Terminated, button is disabled, else enabled.
    
    4.) Upon clicking the "Terminate Employee" button:
    
    - Open a modal dialog box with two fields: Termination Reason and Termination Date.
    - Both fields are required to save. Display descriptive error message if user clicks OK without completing required fields. (or simply don't enable "OK" unless required fields are completed).
    
    - Modal dialog box should have "OK" and "Cancel" buttons.
    
    - OK button saves data, populates the Termination Date field and the Termination Reason fields on the Persons page. Status changes to Terminated and Terminate Employee button disables.
    
    - Cancel button closes the modal dialog box and does not save the information entered.
    
    - Termination Reason options include (please alphabetize):
    "Involuntary - Job Abandonment"
    "Involuntary - Conduct/Performance"
    "Involuntary - No Work"
    "Involuntary - Reduction in Force"
    "Voluntary - No Reason Provided"
    "Voluntary - New Employment"
    "Voluntary - No Work (Hourly only)"
    
    Note: Additional reasons may be added in the future.
    
    Please be sure the modal dialog box is sized broad enough to enable the full display of the Termination Reason.

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/Triggers/tr_RecruiterCommission_InsertUpdate.trigger.sql
@@ -0,0 +1,57 @@
+﻿CREATE TRIGGER [tr_RecruiterCommission_InsertUpdate]
+    ON [dbo].[RecruiterCommission]
+    FOR INSERT, UPDATE 
+    AS 
+    BEGIN
+    	SET NOCOUNT ON;
+
+		--IF EXISTS (SELECT *
+		--			FROM RecruiterCommissionHistory RCH
+		--			)
+		--INSERT INTO RecruiterCommissionHistory(    [RecruitId],
+		--											[RecruiterId],
+		--											[HoursToCollect],
+		--											[Amount],
+		--											[StartDate],
+		--											[EndDate]
+		--										)
+		--VALUES
+
+		DECLARE @Today DATE,
+				@InsertTime	DATETIME
+
+		SELECT @InsertTime = dbo.InsertingTime()
+		SELECT @Today = dbo.GettingPMTime(@InsertTime)
+
+		UPDATE RCH
+			SET Amount = I.Amount,
+				HoursToCollect = I.HoursToCollect
+		FROM inserted I
+		INNER JOIN RecruiterCommissionHistory RCH ON RCH.RecruitId = I.RecruitId AND RCH.RecruiterId = I.RecruiterId AND RCH.EndDate IS NULL
+		WHERE RCH.Amount <> I.Amount AND RCH.HoursToCollect <> I.HoursToCollect
+		
+		--If exists with other recruiter then insert close the existing one and insert the new one.
+		Update RCH
+		SET EndDate = @Today - 1
+		FROM RecruiterCommissionHistory RCH
+		INNER JOIN inserted I ON I.RecruitId = RCH.RecruitId AND I.RecruiterId <> RCH.RecruiterId AND RCH.EndDate IS NULL
+
+		--If not exists then insert with start date as hiredate/today and enddate as NULL.
+		INSERT INTO RecruiterCommissionHistory(    [RecruitId],
+													[RecruiterId],
+													[HoursToCollect],
+													[Amount],
+													[StartDate]
+												)
+		SELECT I.RecruitId,
+				I.RecruiterId,
+				I.HoursToCollect,
+				I.Amount,
+				CASE WHEN PH.StartDate < @InsertTime THEN PH.StartDate
+					ELSE @InsertTime END
+		FROM inserted I
+		OUTER APPLY (SELECT top 1 * FROM PersonStatusHistory PSH WHERE PSH.PersonId = I.RecruitId) PH
+		LEFT JOIN RecruiterCommissionHistory RCH ON RCH.RecruitId = I.RecruitId AND I.RecruiterId = RCH.RecruiterId AND RCH.EndDate IS NULL
+		WHERE I.RecruitId IS NULL
+
+    END
