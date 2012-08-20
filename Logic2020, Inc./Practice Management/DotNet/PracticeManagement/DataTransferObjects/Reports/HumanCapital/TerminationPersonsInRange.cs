5db6fb6ed657c32cee41cdb306cd9465526ac370
SainathC <SainathC>
2012-08-20 09:48:00 -0500

    Comments - Defect Number: 3072; Defect RecordID: 3068; Server Address: localhost; Server Port: 99; Database Name: Practice Management; Defect Summary: Reports: Human Capital --> New Hire Report; Defect Description: We would like to add a few new reports. This ticket is one of those new reports. It is for a New Hire report. It should be created under a new sub-menu called, "Human Capital" under the primary menu "Reports". It is to be accessible only by users with the following roles: Senior Leadership Client Director HR Recruiter Administrator
    Upon selecting "Human Capital" from Reports-->Human Capital the page will display allowing a user to select the New Hire report -- reference the Time reports for similar behaviors.
    
    Refer to the attached specification for further details on the New Hire report functionality.

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/DataTransferObjects/Reports/HumanCapital/TerminationPersonsInRange.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+
+namespace DataTransferObjects.Reports.HumanCapital
+{
+    [DataContract]
+    [Serializable]
+    public class TerminationPersonsInRange
+    {
+        [DataMember]
+        public DateTime StartDate { get; set; }
+
+        [DataMember]
+        public DateTime EndDate { get; set; }
+
+        [DataMember]
+        public List<Person> PersonList { get; set; }
+
+        [DataMember]
+        public int ActivePersonsCountAtTheBeginning { get; set; }
+
+        [DataMember]
+        public int NewHiresCountInTheRange { get; set; }
+
+        [DataMember]
+        public int TerminationsCountInTheRange { get; set; }
+
+        public int Attrition
+        {
+            get
+            {
+                if ((ActivePersonsCountAtTheBeginning + NewHiresCountInTheRange - TerminationsCountInTheRange) != 0)
+                {
+                    return TerminationsCountInTheRange / (ActivePersonsCountAtTheBeginning + NewHiresCountInTheRange - TerminationsCountInTheRange);
+                }
+                return 0;
+            }
+        }
+    }
+}
+
