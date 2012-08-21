a42f9af40215803dd5efc9700ccbf5ce9a89e3a5
SainathC <SainathC>
2012-08-21 04:45:00 -0500

    Comments - Defect Number: 3073; Defect RecordID: 3069; Server Address: localhost; Server Port: 99; Database Name: Practice Management; Defect Summary: Reports: Human Capital --> Terminations Report; Defect Description: We would like to add a few new reports. This ticket is one of those new reports. It is for a Terminations report. It should be created under a new sub-menu called, "Human Capital" under the primary menu "Reports". It is to be accessible only by users with the following roles: Senior Leadership Client Director HR Recruiter Administrator
    Upon selecting "Human Capital" from Reports-->Human Capital the page will display allowing a user to select the Terminations report -- reference the Time reports for similar behaviors.
    
    Refer to the attached specification for further details on the Terminations report functionality.

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/TerminationReportGraph.proc.sql
@@ -0,0 +1,18 @@
+﻿CREATE PROCEDURE [dbo].[TerminationReportGraph]
+(
+	@StartDate	DATETIME,
+	@EndDate	DATETIME,
+	@TimeScaleIds	XML = null,
+	@SeniorityIds	XML = null,
+	@TerminationReasonIds XML = NULL,
+	@PracticeIds	XML = null,
+	@ExcludeInternalPractices	BIT
+)
+AS
+BEGIN
+			SELECT	0 AS [ActivePersonsAtTheBeginning],
+					0 AS [NewHiredInTheRange] ,
+					0 AS [TerminationsInTheRange],
+					@StartDate AS StartDate,
+					@EndDate AS EndDate
+END
