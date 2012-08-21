a98ded47a6569b53aee8dfa9082b348e71d1849e
thulasiramp <thulasiramp>
2012-08-21 09:08:00 -0500

    Comments - Defect Number: 3073; Defect RecordID: 3069; Server Address: localhost; Server Port: 99; Database Name: Practice Management; Defect Summary: Reports: Human Capital --> Terminations Report; Defect Description: We would like to add a few new reports. This ticket is one of those new reports. It is for a Terminations report. It should be created under a new sub-menu called, "Human Capital" under the primary menu "Reports". It is to be accessible only by users with the following roles: Senior Leadership Client Director HR Recruiter Administrator
    Upon selecting "Human Capital" from Reports-->Human Capital the page will display allowing a user to select the Terminations report -- reference the Time reports for similar behaviors.
    
    Refer to the attached specification for further details on the Terminations report functionality.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/Controls/Reports/HumanCapital/TerminationReportGraphView.ascx.cs
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/Controls/Reports/HumanCapital/TerminationReportGraphView.ascx.cs
@@ -4,14 +4,138 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using DataTransferObjects;
+using System.Web.UI.DataVisualization.Charting;
+using iTextSharp.text;
+using System.Drawing;
+using DataTransferObjects.Reports.HumanCapital;
 
 namespace PraticeManagement.Controls.Reports.HumanCapital
 {
     public partial class TerminationReportGraphView : System.Web.UI.UserControl
     {
+
+        #region constant
+
+        private const string MAIN_CHART_AREA_NAME = "MainArea";
+        private const string SeeYearToDate = "See Year To Date";
+        private const string SeeLast12Months = "See Last 12 Months";
+
+        #endregion
+
+        #region properties
+
+        private PraticeManagement.Reporting.TerminationReport HostingPage
+        {
+            get { return ((PraticeManagement.Reporting.TerminationReport)Page); }
+        }
+
+        #endregion
+
         protected void Page_Load(object sender, EventArgs e)
         {
 
         }
+
+        #region Methods
+
+
+        protected void hlnkGraph_Click(object sender, EventArgs e)
+        {
+            if (hlnkGraph.Text == SeeYearToDate)
+            {
+                hlnkGraph.Text = SeeLast12Months;
+            }
+            else
+            {
+                hlnkGraph.Text = SeeYearToDate;
+            }
+            PopulateGraph();
+        }
+
+        protected void chrtTerminationAndAttritionLast12Months_Click(object sender, ImageMapEventArgs e)
+        {
+            string[] postBackDetails = e.PostBackValue.Split(',');
+
+            lbTotalTerminations.Text = postBackDetails[1];
+            var startDate = Utils.Calendar.MonthStartDate(Convert.ToDateTime(postBackDetails[0]));
+            var endDate = Utils.Calendar.MonthEndDate(Convert.ToDateTime(postBackDetails[0]));
+            List<Person> data = ServiceCallers.Custom.Report(r => r.TerminationReport(startDate, endDate, HostingPage.PayTypes, null, HostingPage.Seniorities, HostingPage.TerminationReasons, HostingPage.Practices, HostingPage.ExcludeInternalProjects, null, null, null, null)).PersonList;
+            lbName.Text = "Month : " + postBackDetails[0];
+            mpeDetailView.Show();
+        }
+
+        public void PopulateGraph()
+        {
+            var now = Utils.Generic.GetNowWithTimeZone();
+            DateTime startDate;
+            DateTime endDate;
+            List<TerminationPersonsInRange> terminationPersonInRange;
+            if (hlnkGraph.Text == SeeYearToDate)
+            {
+                startDate = Utils.Calendar.Last12MonthStartDate(now);
+            }
+            else
+            {
+                startDate = Utils.Calendar.YearStartDate(now);
+            }
+            endDate = Utils.Calendar.LastMonthEndDate(now);
+            terminationPersonInRange = ServiceCallers.Custom.Report(r => r.TerminationReportGraph(startDate, endDate, HostingPage.PayTypes, HostingPage.Seniorities, HostingPage.TerminationReasons, HostingPage.Practices, HostingPage.ExcludeInternalProjects)).ToList();
+            //HostingPage.PopulateHeaderSection(terminationPersonInRange);
+
+            LoadChartData(terminationPersonInRange);
+        }
+        private void LoadChartData(List<TerminationPersonsInRange> data)
+        {
+            InitChart(data.Count);
+            chrtTerminationAndAttritionLast12Months.DataSource = data;
+            chrtTerminationAndAttritionLast12Months.DataBind();
+        }
+
+        private void InitChart(int count)
+        {
+            chrtTerminationAndAttritionLast12Months.Width = ((count < 4) ? 4 : count) * 80;
+            chrtTerminationAndAttritionLast12Months.Height = 500;
+            InitAxis(chrtTerminationAndAttritionLast12Months.ChartAreas[MAIN_CHART_AREA_NAME].AxisX, "Month", false);
+            InitAxis(chrtTerminationAndAttritionLast12Months.ChartAreas[MAIN_CHART_AREA_NAME].AxisY, "Number of Terminations", true);
+            InitAxis(chrtTerminationAndAttritionLast12Months.ChartAreas[MAIN_CHART_AREA_NAME].AxisY2, "Attrition Percentage", true);
+            chrtTerminationAndAttritionLast12Months.ChartAreas[0].AxisY2.LabelStyle.Format = "{#}%";
+            chrtTerminationAndAttritionLast12Months.ChartAreas[0].AxisY2.TextOrientation = TextOrientation.Rotated90;
+            UpdateChartTitle();
+        }
+
+        private void UpdateChartTitle()
+        {
+            chrtTerminationAndAttritionLast12Months.Titles.Clear();
+            if (hlnkGraph.Text == SeeYearToDate)
+            {
+                chrtTerminationAndAttritionLast12Months.Titles.Add("Termination and Attrition");
+                chrtTerminationAndAttritionLast12Months.Titles.Add("Last 12 Months");
+            }
+            else
+            {
+                chrtTerminationAndAttritionLast12Months.Titles.Add("Termination and Attrition");
+                chrtTerminationAndAttritionLast12Months.Titles.Add("Year To Date");
+            }
+
+            chrtTerminationAndAttritionLast12Months.Titles[0].Font = new System.Drawing.Font("Arial", 18, FontStyle.Bold);
+            chrtTerminationAndAttritionLast12Months.Titles[1].Font = new System.Drawing.Font("Arial", 18, FontStyle.Bold);
+        }
+
+        private void InitAxis(Axis horizAxis, string title, bool isVertical)
+        {
+            horizAxis.IsStartedFromZero = true;
+            if (!isVertical)
+                horizAxis.Interval = 1;
+            horizAxis.TextOrientation = isVertical ? TextOrientation.Rotated270 : TextOrientation.Horizontal;
+            horizAxis.LabelStyle.Angle = 0;
+            horizAxis.TitleFont = new System.Drawing.Font("Arial", 15, FontStyle.Bold);
+            horizAxis.ArrowStyle = AxisArrowStyle.None;
+            horizAxis.MajorGrid.Enabled = false;
+            horizAxis.ToolTip = horizAxis.Title = title;
+
+        }
+
+        #endregion
     }
 }
