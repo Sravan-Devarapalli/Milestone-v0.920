5dfdb0975bf8faf515b98aab09ae4823e97df244
SainathC <SainathC>
2012-08-23 10:02:00 -0500

    Comments - Defect Number: 3073; Defect RecordID: 3069; Server Address: localhost; Server Port: 99; Database Name: Practice Management; Defect Summary: Reports: Human Capital --> Terminations Report; Defect Description: We would like to add a few new reports. This ticket is one of those new reports. It is for a Terminations report. It should be created under a new sub-menu called, "Human Capital" under the primary menu "Reports". It is to be accessible only by users with the following roles: Senior Leadership Client Director HR Recruiter Administrator
    Upon selecting "Human Capital" from Reports-->Human Capital the page will display allowing a user to select the Terminations report -- reference the Time reports for similar behaviors.
    
    Refer to the attached specification for further details on the Terminations report functionality.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/Reports/NewHireReport.aspx.cs
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/Reports/NewHireReport.aspx.cs
@@ -452,7 +452,7 @@ namespace PraticeManagement.Reporting
                     sb.Append("\t");
                     sb.Append(person.Status.Name);
                     sb.Append("\t");
-                    sb.Append(person.RecruiterCommission.Count > 0 ? person.RecruiterCommission.First().Recruiter.PersonFirstLastName : string.Empty);
+                    sb.Append(person.RecruiterCommission.Count > 0 ? person.RecruiterCommission.First().Recruiter.PersonLastFirstName : string.Empty);
                     sb.Append("\t");
                     sb.AppendLine();
                 }
