74d5cd8de3fe9ab95ba2a240917cb95fbd122cd4
SrinivasM <SrinivasM>
2012-08-24 04:44:00 -0500

    Comments - Defect Number: 3064; Defect RecordID: 3060; Server Address: localhost; Server Port: 99; Database Name: Practice Management; Defect Summary: Configuration: Persons - We need a Change Employee Status capability; Defect Description: We need to begin capturing a Termination Reason whenever we terminate an employee. Currently we simply just add a Termination Date, click Save and the system changes the status to "Terminated". With this enhancement we'd like to also capture why a person is being Terminated.
    I am proposing the following changes to support this enhancement.
    
    1.) Add a "Termination Reason" field --> locate to the right of the Termination Date field. Disabled unless status == Terminated. Be sure the field length is set so that the box displayed is longer than the longest reason available. Users should be able to read the full text of the reason selected.
    
    2.) Disable the "Termination Date" field unless status == Terminated
    
    3.) Create a "Terminate Employee" button. --> to be located next to the Termination Reason Field field which should be disabled until the "Terminate Employee" process completes. If status == Terminated, button is disabled, else enabled.
    
    4.) Upon clicking the "Terminate Employee" button:
    
    - Open a modal dialog box with two fields: Termination Reason and Termination Date.
    - Both fields are required to save. Display descriptive error message if user clicks OK without completing required fields. (or simply don't enable "OK" unless required fields are completed).
    
    - Modal dialog box should have "OK" and "Cancel" buttons.
    
    - OK button saves data, populates the Termination Date field and the Termination Reason fields on the Persons page. Status changes to Terminated and Terminate Employee button disables.
    
    - Cancel button closes the modal dialog box and does not save the information entered.
    
    - Termination Reason options include (please alphabetize):
    "Involuntary - Job Abandonment"
    "Involuntary - Conduct/Performance"
    "Involuntary - No Work"
    "Involuntary - Reduction in Force"
    "Voluntary - No Reason Provided"
    "Voluntary - New Employment"
    "Voluntary - No Work (Hourly only)"
    
    Note: Additional reasons may be added in the future.
    
    Please be sure the modal dialog box is sized broad enough to enable the full display of the Termination Reason.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/TerminationReasons.table.sql
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/TerminationReasons.table.sql
@@ -1,6 +1,10 @@
 ﻿CREATE TABLE [dbo].[TerminationReasons]
 (
 	TerminationReasonId	INT IDENTITY(1,1),
-	TerminationReason	NVARCHAR(2000)
+	TerminationReason	NVARCHAR(2000),
+	IsW2SalaryRule		BIT CONSTRAINT DF_TerminationReasons_IsW2SalaryRule DEFAULT(0),
+	IsW2HourlyRule		BIT CONSTRAINT DF_TerminationReasons_IsW2HourlyRule DEFAULT(0),
+	Is1099Rule			BIT CONSTRAINT DF_TerminationReasons_Is1099Rule DEFAULT(0),
+	IsContingentRule	BIT CONSTRAINT DF_TerminationReasons_IsContingentRule DEFAULT(0),
 	CONSTRAINT PK_TerminationReasons_TerminationReasonId PRIMARY KEY (TerminationReasonId)
 )
