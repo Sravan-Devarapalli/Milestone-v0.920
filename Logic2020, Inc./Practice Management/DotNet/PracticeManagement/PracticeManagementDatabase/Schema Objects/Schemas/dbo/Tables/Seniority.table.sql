58ccada045b9774b1bf96005c666e5487a6cddca
SainathC <SainathC>
2012-08-27 11:06:00 -0500

    Comments - Defect Number: 3072; Defect RecordID: 3068; Server Address: localhost; Server Port: 99; Database Name: Practice Management; Defect Summary: Reports: Human Capital --> New Hire Report; Defect Description: We would like to add a few new reports. This ticket is one of those new reports. It is for a New Hire report. It should be created under a new sub-menu called, "Human Capital" under the primary menu "Reports". It is to be accessible only by users with the following roles: Senior Leadership Client Director HR Recruiter Administrator
    Upon selecting "Human Capital" from Reports-->Human Capital the page will display allowing a user to select the New Hire report -- reference the Time reports for similar behaviors.
    
    Refer to the attached specification for further details on the New Hire report functionality.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/Seniority.table.sql
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/Seniority.table.sql
@@ -1,7 +1,8 @@
 ﻿CREATE TABLE [dbo].[Seniority] (
     [SeniorityId]	INT           NOT NULL,
     [Name]			NVARCHAR (30) NOT NULL,
-	[SeniorityValue] INT NOT NULL
+	[SeniorityValue] INT NOT NULL,
+	[SeniorityCategoryId] INT NOT NULL
 );
 
 
