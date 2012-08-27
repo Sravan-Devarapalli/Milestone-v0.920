75c1f855620355236d8eebc1a478265074ebe24d
SainathC <SainathC>
2012-08-27 11:06:00 -0500

    Comments - Defect Number: 3072; Defect RecordID: 3068; Server Address: localhost; Server Port: 99; Database Name: Practice Management; Defect Summary: Reports: Human Capital --> New Hire Report; Defect Description: We would like to add a few new reports. This ticket is one of those new reports. It is for a New Hire report. It should be created under a new sub-menu called, "Human Capital" under the primary menu "Reports". It is to be accessible only by users with the following roles: Senior Leadership Client Director HR Recruiter Administrator
    Upon selecting "Human Capital" from Reports-->Human Capital the page will display allowing a user to select the New Hire report -- reference the Time reports for similar behaviors.
    
    Refer to the attached specification for further details on the New Hire report functionality.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/TerminationReportGraph.proc.sql
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/TerminationReportGraph.proc.sql
@@ -26,9 +26,10 @@ BEGIN
 		(
 			SELECT TT.StartDate,COUNT(DISTINCT PSH.PersonId) AS ActivePersonsAtTheBeginning
 			FROM dbo.PersonStatusHistory PSH 
-			INNER JOIN dbo.Person P ON PSH.PersonId = P.PersonId AND P.IsStrawman = 0 AND PSH.personstatusId = 1
-			INNER JOIN RangeValue TT ON TT.StartDate BETWEEN PSH.StartDate AND ISNULL(PSH.EndDate,@FutureDate)
-			INNER JOIN dbo.Pay pa ON pa.Person = PSH.PersonId AND TT.StartDate  BETWEEN pa.StartDate  AND ISNULL(pa.EndDate,@FutureDate) AND pa .Timescale IN (@W2SalaryId,@W2HourlyId) 
+			INNER JOIN dbo.Person P ON PSH.PersonId = P.PersonId AND P.IsStrawman = 0 AND PSH.personstatusId = 1 
+			INNER JOIN RangeValue TT ON TT.StartDate BETWEEN PSH.StartDate AND ISNULL(PSH.EndDate,@FutureDate) 
+			INNER JOIN dbo.Pay pa ON pa.Person = PSH.PersonId AND TT.StartDate  BETWEEN pa.StartDate  AND ISNULL(pa.EndDate,@FutureDate) AND pa .Timescale IN (@W2SalaryId,@W2HourlyId)
+			INNER JOIN dbo.v_PersonHistory PH ON PH.PersonId = PSH.PersonId AND TT.StartDate BETWEEN PH.HireDate AND ISNULL(PH.TerminationDate,@FutureDate) -- if status start date is less then person hire date we need to consider only hire date
 			GROUP BY TT.StartDate 
 		),
 		FilteredPersonTerminationHistory
@@ -37,7 +38,7 @@ BEGIN
 			SELECT FPH.*,Pay.Timescale
 			FROM v_PersonHistory FPH 
 			OUTER APPLY (SELECT TOP 1 pa.* FROM dbo.Pay pa WHERE pa.Person = FPH.PersonId AND ISNULL(pa.EndDate,@FutureDate)-1 >= FPH.HireDate AND pa.StartDate <= FPH.TerminationDate ORDER BY pa.StartDate DESC ) pay
-			WHERE FPH.TerminationDate BETWEEN @StartDate AND @EndDate AND pay.Timescale IN (@W2SalaryId,@W2HourlyId) 
+			WHERE FPH.TerminationDate BETWEEN @StartDate AND @EndDate --AND pay.Timescale IN (@W2SalaryId,@W2HourlyId) 
 		),
 		PersonTerminationInRange
 		AS 
@@ -45,7 +46,8 @@ BEGIN
 			SELECT TT.StartDate,
 					SUM(CASE WHEN FPT.Timescale = @W2SalaryId THEN 1 ELSE 0 END) AS TerminationsW2SalaryCountInTheRange,
 					SUM(CASE WHEN FPT.Timescale = @W2HourlyId THEN 1 ELSE 0 END) AS TerminationsW2HourlyCountInTheRange,
-					SUM(CASE WHEN FPT.Timescale IN (@1099HourlyId,@1099PROId) THEN 1 ELSE 0 END) AS TerminationsContractorsCountInTheRange
+					SUM(CASE WHEN FPT.Timescale IN (@1099HourlyId,@1099PROId) THEN 1 ELSE 0 END) AS TerminationsContractorsCountInTheRange,
+					COUNT(FPT.PersonId) AS TerminationsCountInTheRange
 			FROM RangeValue TT
 			LEFT JOIN  FilteredPersonTerminationHistory FPT  ON FPT.TerminationDate BETWEEN TT.StartDate AND TT.EndDate
 			GROUP BY TT.StartDate
@@ -85,6 +87,7 @@ BEGIN
 				FPTH.TerminationsW2SalaryCountInTheRange,
 				FPTH.TerminationsW2HourlyCountInTheRange,
 				FPTH.TerminationsContractorsCountInTheRange,
+				FPTH.TerminationsCountInTheRange,
 				AP.ActivePersonsAtTheBeginning
 		FROM RangeValue TT
 		INNER JOIN PersonTerminationInRange FPTH ON FPTH.StartDate = TT.StartDate
