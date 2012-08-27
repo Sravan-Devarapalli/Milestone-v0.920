6ccfbcf2a095fffd4db713f1ac88b159a792f610
SainathC <SainathC>
2012-08-27 11:05:00 -0500

    Comments - Defect Number: 3072; Defect RecordID: 3068; Server Address: localhost; Server Port: 99; Database Name: Practice Management; Defect Summary: Reports: Human Capital --> New Hire Report; Defect Description: We would like to add a few new reports. This ticket is one of those new reports. It is for a New Hire report. It should be created under a new sub-menu called, "Human Capital" under the primary menu "Reports". It is to be accessible only by users with the following roles: Senior Leadership Client Director HR Recruiter Administrator
    Upon selecting "Human Capital" from Reports-->Human Capital the page will display allowing a user to select the New Hire report -- reference the Time reports for similar behaviors.
    
    Refer to the attached specification for further details on the New Hire report functionality.

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/GetPersonListWithRole.proc.sql
@@ -0,0 +1,17 @@
+﻿CREATE PROCEDURE [dbo].[GetPersonListWithRole]
+(
+	@RoleName NVARCHAR(100)
+)
+AS
+BEGIN
+
+	 SELECT p.PersonId ,
+            p.FirstName ,
+            p.LastName ,
+            p.IsDefaultManager
+    FROM    dbo.Person AS p
+			INNER JOIN v_UsersInRoles AS ur ON ur.UserName = p.Alias AND LOWER(ur.RoleName) = LOWER(@RoleName)
+    WHERE   p.PersonStatusId = 1 -- Active person only
+    ORDER BY p.LastName ,p.FirstName
+END
+
