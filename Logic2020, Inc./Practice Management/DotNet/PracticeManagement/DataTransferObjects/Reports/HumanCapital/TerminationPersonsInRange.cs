8abdb4ccffb086cf25f51ac94c58a9ebd9fb47b3
SainathC <SainathC>
2012-08-29 09:44:00 -0500

    Comments - Defect Number: 3072; Defect RecordID: 3068; Server Address: localhost; Server Port: 99; Database Name: Practice Management; Defect Summary: Reports: Human Capital --> New Hire Report; Defect Description: We would like to add a few new reports. This ticket is one of those new reports. It is for a New Hire report. It should be created under a new sub-menu called, "Human Capital" under the primary menu "Reports". It is to be accessible only by users with the following roles: Senior Leadership Client Director HR Recruiter Administrator
    Upon selecting "Human Capital" from Reports-->Human Capital the page will display allowing a user to select the New Hire report -- reference the Time reports for similar behaviors.
    
    Refer to the attached specification for further details on the New Hire report functionality.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/DataTransferObjects/Reports/HumanCapital/TerminationPersonsInRange.cs
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/DataTransferObjects/Reports/HumanCapital/TerminationPersonsInRange.cs
@@ -19,20 +19,35 @@ namespace DataTransferObjects.Reports.HumanCapital
         [DataMember]
         public List<Person> PersonList { get; set; }
 
+        public List<int> PayTypesList { get; set; }
+
         [DataMember]
         public int ActivePersonsCountAtTheBeginning { get; set; }
 
         [DataMember]
         public int NewHiresCountInTheRange { get; set; }
 
-        [DataMember]
-        public int TerminationsContractorsCountInTheRange { get; set; }
+
+        public int TerminationsContractorsCountInTheRange
+        {
+            get
+            {
+
+                return Terminations1099HourlyCountInTheRange + Terminations1099PORCountInTheRange;
+            }
+        }
 
         [DataMember]
         public int TerminationsW2SalaryCountInTheRange { get; set; }
 
         [DataMember]
         public int TerminationsW2HourlyCountInTheRange { get; set; }
+
+        [DataMember]
+        public int Terminations1099HourlyCountInTheRange { get; set; }
+
+        [DataMember]
+        public int Terminations1099PORCountInTheRange { get; set; }
 
         [DataMember]
         public int TerminationsCountInTheRange { get; set; }
@@ -63,6 +78,40 @@ namespace DataTransferObjects.Reports.HumanCapital
             }
             return 0d;
 
+        }
+
+        public int TerminationsCountForSelectedPaytypes
+        {
+            get
+            {
+                int count = 0;
+                if (PayTypesList == null)
+                {
+                    count = TerminationsCountInTheRange;
+                }
+                else
+                {
+                    foreach (int i in PayTypesList)
+                    {
+                        switch (i)
+                        {
+                            case (int)TimescaleType.Hourly:
+                                count += TerminationsW2HourlyCountInTheRange;
+                                break;
+                            case (int)TimescaleType.Salary:
+                                count += TerminationsW2SalaryCountInTheRange;
+                                break;
+                            case (int)TimescaleType._1099Ctc:
+                                count += Terminations1099HourlyCountInTheRange;
+                                break;
+                            case (int)TimescaleType.PercRevenue:
+                                count += Terminations1099PORCountInTheRange;
+                                break;
+                        }
+                    }
+                }
+                return count;
+            }
         }
 
         public double AttritionPercentage
