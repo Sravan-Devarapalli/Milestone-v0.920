ad0cb7d1ea2493b8457acb74adaa57042c2cdf94
SainathC <SainathC>
2012-08-29 09:49:00 -0500

    Comments - Defect Number: 3072; Defect RecordID: 3068; Server Address: localhost; Server Port: 99; Database Name: Practice Management; Defect Summary: Reports: Human Capital --> New Hire Report; Defect Description: We would like to add a few new reports. This ticket is one of those new reports. It is for a New Hire report. It should be created under a new sub-menu called, "Human Capital" under the primary menu "Reports". It is to be accessible only by users with the following roles: Senior Leadership Client Director HR Recruiter Administrator
    Upon selecting "Human Capital" from Reports-->Human Capital the page will display allowing a user to select the New Hire report -- reference the Time reports for similar behaviors.
    
    Refer to the attached specification for further details on the New Hire report functionality.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/SeniorityListAll.proc.sql
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/SeniorityListAll.proc.sql
@@ -12,7 +12,8 @@ AS
 	SELECT S.SeniorityId,
 			S.Name AS Seniority,
 			S.SeniorityCategoryId,
-			SC.Name AS SeniorityCategory
+			SC.Name AS SeniorityCategory,
+			S.SeniorityValue
 	FROM dbo.Seniority AS S
 	INNER JOIN dbo.SeniorityCategory SC ON S.SeniorityCategoryId = SC.SeniorityCategoryId
 	ORDER BY S.SeniorityValue,S.Name
