16645c6677104b5ca0f1fb4bd3419a9824c72131
SainathC <SainathC>
2012-09-11 08:06:00 -0500

    Comments - Defect Number: 3064; Defect RecordID: 3060; Server Address: localhost; Server Port: 99; Database Name: Practice Management; Defect Summary: Configuration: Persons - We need a Change Employee Status capability; Defect Description: We need to begin capturing a Termination Reason whenever we terminate an employee. Currently we simply just add a Termination Date, click Save and the system changes the status to "Terminated". With this enhancement we'd like to also capture why a person is being Terminated.
    I am proposing the following changes to support this enhancement.
    
    1.) Add a "Termination Reason" field --> locate to the right of the Termination Date field. Disabled unless status == Terminated. Be sure the field length is set so that the box displayed is longer than the longest reason available. Users should be able to read the full text of the reason selected.
    
    2.) Disable the "Termination Date" field unless status == Terminated
    
    3.) Create a "Terminate Employee" button. --> to be located next to the Termination Reason Field field which should be disabled until the "Terminate Employee" process completes. If status == Terminated, button is disabled, else enabled.
    
    4.) Upon clicking the "Terminate Employee" button:
    
    - Open a modal dialog box with two fields: Termination Reason and Termination Date.
    - Both fields are required to save. Display descriptive error message if user clicks OK without completing required fields. (or simply don't enable "OK" unless required fields are completed).
    
    - Modal dialog box should have "OK" and "Cancel" buttons.
    
    - OK button saves data, populates the Termination Date field and the Termination Reason fields on the Persons page. Status changes to Terminated and Terminate Employee button disables.
    
    - Cancel button closes the modal dialog box and does not save the information entered.
    
    - Termination Reason options include (please alphabetize):
    "Involuntary - Job Abandonment"
    "Involuntary - Conduct/Performance"
    "Involuntary - No Work"
    "Involuntary - Reduction in Force"
    "Voluntary - No Reason Provided"
    "Voluntary - New Employment"
    "Voluntary - No Work (Hourly only)"
    
    Note: Additional reasons may be added in the future.
    
    Please be sure the modal dialog box is sized broad enough to enable the full display of the Termination Reason.

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/OnPersonHireDateChange.proc.sql
@@ -0,0 +1,70 @@
+﻿CREATE PROCEDURE [dbo].[OnPersonHireDateChange]
+(
+	@PersonId        INT,
+	@NewHireDate	 DATETIME
+)
+AS
+/*
+A.Update The person Compensation 
+1.Get the previous termination date of the person
+2.Delete all the compensation records that are before the new hire date and greater than the previous termination date
+3.Get the latest start date of the compensation record after the previous termination date
+4.Update the latest compensation start date with the hire date.
+
+B.Need to adjust  person status history record if the person is hired for the first time
+
+*/
+BEGIN
+--a.Update The person Compensation 
+	DECLARE @PreviousTerminationDate DATETIME ,@LatestCompensationStartDate DATETIME
+
+	--1.Get the previous termination date of the person
+	SELECT Top 1 @PreviousTerminationDate = VP.TerminationDate 
+	FROM dbo.v_PersonHistory VP
+	WHERE VP.TerminationDate < @NewHireDate
+			AND VP.PersonId = @PersonId 
+	ORDER BY VP.HireDate DESC
+
+	--2.Delete all the compensation records that are before the new hire date and greater than the previous termination date
+	DELETE pay
+	FROM dbo.pay pay 
+	WHERE Pay.Person = @PersonId 
+			AND ( @PreviousTerminationDate  IS NULL OR pay.StartDate > @PreviousTerminationDate) -- Compensation After the previous termination date
+			AND Pay.EndDate-1 < @NewHireDate -- Compensation before new hire date
+
+
+	--3.Get the latest start date of the compensation record after the previous termination date
+	SELECT TOP 1 @LatestCompensationStartDate = pay.StartDate
+	FROM dbo.pay 
+	WHERE Pay.Person = @PersonId 
+				--AND @NewHireDate < pay.StartDate 
+				AND (@PreviousTerminationDate  IS NULL OR pay.StartDate > @PreviousTerminationDate) -- Compensation After the previous termination date
+				--AND	@NewHireDate BETWEEN pay.StartDate AND pay.EndDate-1
+	ORDER BY pay.StartDate
+
+	--4.Update the latest compensation start date with the hire date.
+	UPDATE pay
+	SET Pay.StartDate = @NewHireDate
+	FROM dbo.pay pay 
+	WHERE Pay.Person = @PersonId 
+			AND Pay.StartDate = @LatestCompensationStartDate
+
+	--B.Need to adjust  person status history record if the person is hired for the first time
+
+	;WITH FirstPSHRow  AS 
+	(
+		SELECT MIN(StartDate) AS StartDate,
+			@PersonId AS PersonId				 
+		FROM dbo.PersonStatusHistory AS PSH 
+		WHERE  PSH.PersonId = @PersonId
+	)
+
+	UPDATE PSH 
+	SET PSH.startdate = CONVERT(DATE,P.HireDate)
+	FROM dbo.Person AS P 
+	INNER JOIN FirstPSHRow AS FPSHR ON P.PersonId = FPSHR.PersonId
+	INNER JOIN dbo.PersonStatusHistory AS PSH ON P.PersonId = FPSHR.PersonId   AND  FPSHR.startdate = PSH.startdate    
+	WHERE  P.HireDate < PSH.startdate  
+
+
+END
