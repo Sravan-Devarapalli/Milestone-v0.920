e8b74e4a7841bea17f1ef3fd00e33e4a45e11c58
SainathC <SainathC>
2012-09-12 05:57:00 -0500

    Comments - Defect Number: 3064; Defect RecordID: 3060; Server Address: localhost; Server Port: 99; Database Name: Practice Management; Defect Summary: Configuration: Persons - We need a Change Employee Status capability; Defect Description: We need to begin capturing a Termination Reason whenever we terminate an employee. Currently we simply just add a Termination Date, click Save and the system changes the status to "Terminated". With this enhancement we'd like to also capture why a person is being Terminated.
    I am proposing the following changes to support this enhancement.
    
    1.) Add a "Termination Reason" field --> locate to the right of the Termination Date field. Disabled unless status == Terminated. Be sure the field length is set so that the box displayed is longer than the longest reason available. Users should be able to read the full text of the reason selected.
    
    2.) Disable the "Termination Date" field unless status == Terminated
    
    3.) Create a "Terminate Employee" button. --> to be located next to the Termination Reason Field field which should be disabled until the "Terminate Employee" process completes. If status == Terminated, button is disabled, else enabled.
    
    4.) Upon clicking the "Terminate Employee" button:
    
    - Open a modal dialog box with two fields: Termination Reason and Termination Date.
    - Both fields are required to save. Display descriptive error message if user clicks OK without completing required fields. (or simply don't enable "OK" unless required fields are completed).
    
    - Modal dialog box should have "OK" and "Cancel" buttons.
    
    - OK button saves data, populates the Termination Date field and the Termination Reason fields on the Persons page. Status changes to Terminated and Terminate Employee button disables.
    
    - Cancel button closes the modal dialog box and does not save the information entered.
    
    - Termination Reason options include (please alphabetize):
    "Involuntary - Job Abandonment"
    "Involuntary - Conduct/Performance"
    "Involuntary - No Work"
    "Involuntary - Reduction in Force"
    "Voluntary - No Reason Provided"
    "Voluntary - New Employment"
    "Voluntary - No Work (Hourly only)"
    
    Note: Additional reasons may be added in the future.
    
    Please be sure the modal dialog box is sized broad enough to enable the full display of the Termination Reason.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/AutoTerminatePersons.proc.sql
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/AutoTerminatePersons.proc.sql
@@ -3,10 +3,12 @@ AS
 BEGIN
 
 	DECLARE @Today DATETIME,
-			@FutureDate DATETIME
+			@FutureDate DATETIME,
+			@TerminationReasonId INT
 
 	SELECT @Today = CONVERT(DATETIME,CONVERT(DATE,[dbo].[GettingPMTime](GETUTCDATE()))),
 		   @FutureDate = dbo.GetFutureDate()
+	SELECT @TerminationReasonId = TR.TerminationReasonId FROM dbo.TerminationReasons TR WHERE TR.TerminationReason = 'Voluntary - 1099 Contract Ended'
 	-- Close a current compensation for the terminated persons
 	
 	DECLARE @TerminatedPersons TABLE
@@ -14,28 +16,49 @@ BEGIN
 	PersonID INT,
 	TerminationDate DATETIME,
 	Alias NVARCHAR(100),
-	DefaultPractice INT
+	DefaultPractice INT,
+	IsTerminatedDueToPay BIT,
+	ReHiredate DATETIME
 	)
 	
 	INSERT INTO @TerminatedPersons
 	SELECT	PersonId,
 			TerminationDate,
 			Alias,
-			DefaultPractice
+			DefaultPractice,
+			0 AS IsTerminatedDueToPay,
+			NULL AS ReHiredate 
 	FROM dbo.Person AS P
 	WHERE   P.PersonStatusId <> 2
 			AND P.TerminationDate = (@Today-1)
 
+
+	INSERT INTO @TerminatedPersons
+	SELECT	p.PersonId,
+			@Today,
+			P.Alias,
+			P.DefaultPractice,
+			1 AS IsTerminatedDueToPay,
+			MIN(Apay.StartDate) AS ReHiredate 
+	FROM dbo.Person P  
+	INNER JOIN dbo.Pay Bpay  ON Bpay.Person = P.PersonId AND (P.TerminationDate IS NULL OR P.TerminationDate > (@Today-1))  AND Bpay.EndDate = @Today
+	INNER JOIN dbo.Timescale BT ON BT.TimescaleId = Bpay.Timescale AND BT.IsContractType = 1 
+	INNER JOIN dbo.Pay Apay  ON Apay.Person = P.PersonId AND Apay.StartDate >= @Today 
+	INNER JOIN dbo.Timescale AT ON AT.TimescaleId = Apay.Timescale AND AT.IsContractType = 0
+	GROUP BY p.PersonId,
+			P.Alias,
+			P.DefaultPractice
+
 	UPDATE pay
 	   SET pay.EndDate = P.TerminationDate + 1
 	FROM dbo.Pay AS pay
-	INNER JOIN @TerminatedPersons P ON pay.Person = P.PersonID
+	INNER JOIN @TerminatedPersons P ON pay.Person = P.PersonID AND P.IsTerminatedDueToPay = 0
 	WHERE pay.EndDate > P.TerminationDate + 1
 			AND pay.StartDate < P.TerminationDate + 1
 
 	DELETE pay
 	FROM dbo.Pay AS pay
-	INNER JOIN @TerminatedPersons P ON pay.Person = P.PersonId
+	INNER JOIN @TerminatedPersons P ON pay.Person = P.PersonId AND P.IsTerminatedDueToPay = 0
 	WHERE  pay.StartDate >= P.TerminationDate + 1
 	 
 	-- SET new manager for subordinates
@@ -43,7 +66,7 @@ BEGIN
 	UPDATE P
 	SET P.ManagerId = COALESCE(SamePracticedManagerOrUp.PersonId,DefalutManager.PersonId,pr.PracticeManagerId)
 	FROM dbo.Person P
-	INNER JOIN @TerminatedPersons AS Manager ON P.ManagerId = Manager.PersonId
+	INNER JOIN @TerminatedPersons AS Manager ON P.ManagerId = Manager.PersonId AND (Manager.IsTerminatedDueToPay = 0 OR ReHiredate > @Today)
 	LEFT  JOIN Practice pr ON P.DefaultPractice = pr.PracticeId
 	OUTER APPLY(
 				SELECT TOP 1 PersonId
@@ -76,11 +99,14 @@ BEGIN
 	INNER JOIN dbo.aspnet_Applications AS a ON u.ApplicationId = a.ApplicationId
 	INNER JOIN  dbo.aspnet_Membership AS m ON u.UserId = m.UserId
 	INNER JOIN @TerminatedPersons as P ON u.LoweredUserName = LOWER(P.Alias) 
-	WHERE a.LoweredApplicationName =LOWER('PracticeManagement')
+	WHERE a.LoweredApplicationName =LOWER('PracticeManagement') AND P.IsTerminatedDueToPay = 0
+
 
 	-- set Terminated status to persons
 	UPDATE P
-	SET P.PersonStatusId = 2 --Terminated status
+	SET P.PersonStatusId = 2, --Terminated status
+		P.TerminationDate = (@Today-1),
+		P.TerminationReasonId = CASE WHEN TP.IsTerminatedDueToPay = 0 THEN P.TerminationReasonId ELSE @TerminationReasonId END
 	FROM dbo.Person AS P 
 	INNER JOIN @TerminatedPersons TP ON P.PersonId = TP.PersonID
 
@@ -100,6 +126,23 @@ BEGIN
 	SELECT PersonID,
 		   2, --Terminated
 		   @Today
-	FROM @TerminatedPersons
+	FROM @TerminatedPersons TP
+
+	--Rehire Person
+	UPDATE P
+	SET P.PersonStatusId = 1, --ACTIVE status
+		P.HireDate = TP.ReHiredate,
+		P.TerminationDate = NULL,
+		P.TerminationReasonId = NULL
+	FROM dbo.Person AS P 
+	INNER JOIN @TerminatedPersons TP ON P.PersonId = TP.PersonID AND TP.IsTerminatedDueToPay = 1
+
+	--Update Person Status History
+	UPDATE PH
+	 SET PersonStatusId = 1
+	 FROM dbo.PersonStatusHistory PH
+	 INNER JOIN @TerminatedPersons P ON P.PersonId = PH.PersonID AND P.IsTerminatedDueToPay = 1
+	 WHERE EndDate IS NULL AND StartDate = @Today
+	 
 END
 
