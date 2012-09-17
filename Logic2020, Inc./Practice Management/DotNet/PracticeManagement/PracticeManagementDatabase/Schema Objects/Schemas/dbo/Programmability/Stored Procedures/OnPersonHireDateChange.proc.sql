05a582c39be240160cbb79a6258ad0b4317cf19d
SrinivasM <SrinivasM>
2012-09-17 04:34:00 -0500

    Comments - Defect Number: 3064; Defect RecordID: 3060; Server Address: localhost; Server Port: 99; Database Name: Practice Management; Defect Summary: Configuration: Persons - We need a Change Employee Status capability; Defect Description: We need to begin capturing a Termination Reason whenever we terminate an employee. Currently we simply just add a Termination Date, click Save and the system changes the status to "Terminated". With this enhancement we'd like to also capture why a person is being Terminated.
    I am proposing the following changes to support this enhancement.
    
    1.) Add a "Termination Reason" field --> locate to the right of the Termination Date field. Disabled unless status == Terminated. Be sure the field length is set so that the box displayed is longer than the longest reason available. Users should be able to read the full text of the reason selected.
    
    2.) Disable the "Termination Date" field unless status == Terminated
    
    3.) Create a "Terminate Employee" button. --> to be located next to the Termination Reason Field field which should be disabled until the "Terminate Employee" process completes. If status == Terminated, button is disabled, else enabled.
    
    4.) Upon clicking the "Terminate Employee" button:
    
    - Open a modal dialog box with two fields: Termination Reason and Termination Date.
    - Both fields are required to save. Display descriptive error message if user clicks OK without completing required fields. (or simply don't enable "OK" unless required fields are completed).
    
    - Modal dialog box should have "OK" and "Cancel" buttons.
    
    - OK button saves data, populates the Termination Date field and the Termination Reason fields on the Persons page. Status changes to Terminated and Terminate Employee button disables.
    
    - Cancel button closes the modal dialog box and does not save the information entered.
    
    - Termination Reason options include (please alphabetize):
    "Involuntary - Job Abandonment"
    "Involuntary - Conduct/Performance"
    "Involuntary - No Work"
    "Involuntary - Reduction in Force"
    "Voluntary - No Reason Provided"
    "Voluntary - New Employment"
    "Voluntary - No Work (Hourly only)"
    
    Note: Additional reasons may be added in the future.
    
    Please be sure the modal dialog box is sized broad enough to enable the full display of the Termination Reason.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/OnPersonHireDateChange.proc.sql
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/OnPersonHireDateChange.proc.sql
@@ -45,26 +45,35 @@ BEGIN
 	--4.Update the latest compensation start date with the hire date.
 	UPDATE pay
 	SET Pay.StartDate = @NewHireDate
-	FROM dbo.pay pay 
+	FROM dbo.pay pay
 	WHERE Pay.Person = @PersonId 
 			AND Pay.StartDate = @LatestCompensationStartDate
 
 	--B.Need to adjust  person status history record if the person is hired for the first time
 
-	;WITH FirstPSHRow  AS 
+	;WITH IsFirstHire AS
+	(
+		SELECT PH.PersonId, MIN(PH.HireDate) 'HireDate', MIN(PH.CreatedDate) AS 'CreatedDate', MIN(PH.TerminationDate) AS 'TerminationDate'
+		FROM dbo.v_PersonHistory PH
+		WHERE PH.PersonId = @PersonId
+		GROUP BY PH.PersonId
+		HAVING COUNT(*) = 1 AND MIN(PH.TerminationDate) IS NULL
+	)
+	,FirstPSHRow  AS
 	(
 		SELECT MIN(StartDate) AS StartDate,
-			@PersonId AS PersonId				 
-		FROM dbo.PersonStatusHistory AS PSH 
-		WHERE  PSH.PersonId = @PersonId
+			@PersonId AS PersonId,
+			CONVERT(DATE, MIN(FH.CreatedDate)) AS CreatedDate,
+			CONVERT(DATE, MIN(FH.HireDate)) AS HireDate
+		FROM dbo.PersonStatusHistory AS PSH
+		INNER JOIN IsFirstHire FH ON FH.PersonId = PSH.PersonId
 	)
 
-	UPDATE PSH 
-	SET PSH.startdate = CONVERT(DATE,P.HireDate)
-	FROM dbo.Person AS P 
-	INNER JOIN FirstPSHRow AS FPSHR ON P.PersonId = FPSHR.PersonId
-	INNER JOIN dbo.PersonStatusHistory AS PSH ON P.PersonId = FPSHR.PersonId   AND  FPSHR.startdate = PSH.startdate    
-	WHERE  P.HireDate < PSH.startdate  
-
+	UPDATE PSH
+	SET PSH.startdate = CASE WHEN FPSHR.HireDate < FPSHR.CreatedDate THEN FPSHR.HireDate
+										ELSE FPSHR.CreatedDate END
+	FROM FirstPSHRow AS FPSHR
+	INNER JOIN dbo.PersonStatusHistory AS PSH ON PSH.PersonId = FPSHR.PersonId AND  FPSHR.startdate = PSH.startdate
+	WHERE  (FPSHR.HireDate < FPSHR.CreatedDate AND FPSHR.HireDate <> PSH.StartDate) OR (FPSHR.HireDate > FPSHR.CreatedDate AND FPSHR.CreatedDate <> PSH.StartDate)
 
 END
