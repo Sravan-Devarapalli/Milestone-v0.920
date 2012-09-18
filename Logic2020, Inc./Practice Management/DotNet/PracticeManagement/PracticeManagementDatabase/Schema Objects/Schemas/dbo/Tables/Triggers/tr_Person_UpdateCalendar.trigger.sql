34237fc90694d1358b405d7e84369bc34aac1fc6
SrinivasM <SrinivasM>
2012-09-18 02:40:00 -0500

    Comments - Defect Number: 3064; Defect RecordID: 3060; Server Address: localhost; Server Port: 99; Database Name: Practice Management; Defect Summary: Configuration: Persons - We need a Change Employee Status capability; Defect Description: We need to begin capturing a Termination Reason whenever we terminate an employee. Currently we simply just add a Termination Date, click Save and the system changes the status to "Terminated". With this enhancement we'd like to also capture why a person is being Terminated.
    I am proposing the following changes to support this enhancement.
    
    1.) Add a "Termination Reason" field --> locate to the right of the Termination Date field. Disabled unless status == Terminated. Be sure the field length is set so that the box displayed is longer than the longest reason available. Users should be able to read the full text of the reason selected.
    
    2.) Disable the "Termination Date" field unless status == Terminated
    
    3.) Create a "Terminate Employee" button. --> to be located next to the Termination Reason Field field which should be disabled until the "Terminate Employee" process completes. If status == Terminated, button is disabled, else enabled.
    
    4.) Upon clicking the "Terminate Employee" button:
    
    - Open a modal dialog box with two fields: Termination Reason and Termination Date.
    - Both fields are required to save. Display descriptive error message if user clicks OK without completing required fields. (or simply don't enable "OK" unless required fields are completed).
    
    - Modal dialog box should have "OK" and "Cancel" buttons.
    
    - OK button saves data, populates the Termination Date field and the Termination Reason fields on the Persons page. Status changes to Terminated and Terminate Employee button disables.
    
    - Cancel button closes the modal dialog box and does not save the information entered.
    
    - Termination Reason options include (please alphabetize):
    "Involuntary - Job Abandonment"
    "Involuntary - Conduct/Performance"
    "Involuntary - No Work"
    "Involuntary - Reduction in Force"
    "Voluntary - No Reason Provided"
    "Voluntary - New Employment"
    "Voluntary - No Work (Hourly only)"
    
    Note: Additional reasons may be added in the future.
    
    Please be sure the modal dialog box is sized broad enough to enable the full display of the Termination Reason.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/Triggers/tr_Person_UpdateCalendar.trigger.sql
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/Triggers/tr_Person_UpdateCalendar.trigger.sql
@@ -13,21 +13,29 @@ AS
 BEGIN
 	SET NOCOUNT ON
 
-	-- Deleting redundand records
-	DELETE PCA
-	FROM deleted AS d
-	INNER JOIN  dbo.PersonCalendarAuto AS PCA ON d.PersonId = PCA.PersonId
-	LEFT JOIN dbo.v_PersonCalendar AS C ON C.PersonId = PCA.PersonId AND C.Date = PCA.Date
-	WHERE C.Date IS NULL AND C.PersonId IS NULL
+	IF EXISTS (SELECT 1
+				FROM inserted i
+				LEFT JOIN deleted d ON d.PersonId = i.PersonId
+				WHERE i.HireDate <> d.HireDate OR ISNULL(i.TerminationDate, '') <> ISNULL(d.TerminationDate, ''))
+	BEGIN
 
-	-- Inserting new records
-	INSERT INTO dbo.PersonCalendarAuto
-	            (Date, PersonId, DayOff)
-	SELECT c.Date, c.PersonId, c.DayOff
-	FROM inserted i
-	INNER JOIN  dbo.v_PersonCalendar AS C ON i.PersonId = c.PersonId
-	LEFT JOIN dbo.PersonCalendarAuto AS PCA ON C.PersonId = PCA.PersonId AND C.Date = PCA.Date
-	WHERE PCA.Date IS NULL AND PCA.PersonId IS NULL
+		-- Deleting redundand records
+		DELETE PCA
+		FROM deleted AS d
+		INNER JOIN  dbo.PersonCalendarAuto AS PCA ON d.PersonId = PCA.PersonId
+		LEFT JOIN dbo.v_PersonCalendar AS C ON C.PersonId = PCA.PersonId AND C.Date = PCA.Date
+		WHERE C.Date IS NULL AND C.PersonId IS NULL
+
+		-- Inserting new records
+		INSERT INTO dbo.PersonCalendarAuto
+					(Date, PersonId, DayOff)
+		SELECT c.Date, c.PersonId, c.DayOff
+		FROM inserted i
+		INNER JOIN  dbo.v_PersonCalendar AS C ON i.PersonId = c.PersonId
+		LEFT JOIN dbo.PersonCalendarAuto AS PCA ON C.PersonId = PCA.PersonId AND C.Date = PCA.Date
+		WHERE PCA.Date IS NULL AND PCA.PersonId IS NULL
+
+	END
 
 END
 
