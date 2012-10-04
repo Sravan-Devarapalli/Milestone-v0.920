a63d74212a65114a5b30ee99ca1caa0913bbe045
SainathC <SainathC>
2012-10-04 10:27:00 -0500

    Comments - Defect Number: 3070; Defect RecordID: 3066; Server Address: localhost; Server Port: 99; Database Name: Practice Management; Defect Summary: Skills: New enhancements to the Skills Functionality; Defect Description: Please see the attachment for the desired Skills enhancements. In section 1.4 of the document there are new skills to be added to the db. These can be added independently from the functional enhancements described in the rest of the document.
    As before, contact Mike Paulsmeyer (cc: me) for specific functional questions as he created the specification. (MikeP@logic2020.com)
    
    - Matt

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/Skills/Programmability/Stored Procedures/SavePersonPictureUrl.proc.sql
@@ -0,0 +1,21 @@
+﻿CREATE PROCEDURE [Skills].[SavePersonPictureUrl]
+(
+	@PersonId         INT,		
+	@PictureUrl		  NVARCHAR (MAX) = NULL,
+	@UserLogin		  NVARCHAR (100)      
+)
+AS
+BEGIN
+	-- Start logging session
+	EXEC dbo.SessionLogPrepare @UserLogin = @UserLogin
+	DECLARE @Now DATETIME
+	SET @Now = dbo.InsertingTime()
+
+	UPDATE dbo.Person
+	SET PictureUrl = @PictureUrl,
+		PictureModifiedDate = @Now
+	WHERE PersonId = @PersonId
+
+	EXEC dbo.SessionLogUnprepare
+END
+
