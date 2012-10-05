89a86f11c83111b61431dbedd13b146fca09f691
SainathC <SainathC>
2012-10-05 08:35:00 -0500

    Comments - Defect Number: 3070; Defect RecordID: 3066; Server Address: localhost; Server Port: 99; Database Name: Practice Management; Defect Summary: Skills: New enhancements to the Skills Functionality; Defect Description: Please see the attachment for the desired Skills enhancements. In section 1.4 of the document there are new skills to be added to the db. These can be added independently from the functional enhancements described in the rest of the document.
    As before, contact Mike Paulsmeyer (cc: me) for specific functional questions as he created the specification. (MikeP@logic2020.com)
    
    - Matt

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/Triggers/tr_Person_Log.trigger.sql
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/Triggers/tr_Person_Log.trigger.sql
@@ -23,89 +23,28 @@ BEGIN
 		) > 0
 	BEGIN 
 
-		;WITH NEW_VALUES AS
-		(
-			SELECT i.PersonId,
-					CONVERT(NVARCHAR(10), i.HireDate, 101) AS HireDate,
-					CONVERT(NVARCHAR(10), i.TerminationDate, 101) AS TerminationDate,
-					i.Alias ,
-					p.Name AS DefaultPractice,
-					i.FirstName,
-					i.LastName,
-					s.Name AS PersonStatusName,
-					i.EmployeeNumber,
-					i.SeniorityId,
-					r.Name AS Seniority,
-					CASE WHEN i.IsDefaultManager = 1 THEN 'Yes' ELSE 'NO' END AS [IsDefaultManager],
-					i.ManagerId,
-					mngr.LastName + ', ' + mngr.FirstName as [ManagerName],
-					i.TelephoneNumber,
-					i.DivisionId,
-					PD.DivisionName,
-					i.PayChexID,
-					CASE WHEN i.IsOffshore = 1 THEN 'YES' ELSE 'NO' END AS [IsOffshore],
-					TR.TerminationReasonId,
-					TR.TerminationReason
-			FROM inserted AS i
-					LEFT JOIN dbo.Practice AS p ON i.DefaultPractice = p.PracticeId
-					INNER JOIN dbo.PersonStatus AS s ON i.PersonStatusId = s.PersonStatusId
-					LEFT JOIN dbo.Seniority AS r ON i.SeniorityId = r.SeniorityId
-					LEFT JOIN dbo.Person as mngr ON mngr.PersonId = i.ManagerId
-					LEFT JOIN dbo.PersonDivision PD ON PD.DivisionId = i.DivisionId
-					LEFT JOIN dbo.TerminationReasons TR ON TR.TerminationReasonId = i.TerminationReasonId
-			WHERE i.IsStrawman = 0 
-		),
-
-		OLD_VALUES AS
-		(
-			SELECT d.PersonId,
-					CONVERT(NVARCHAR(10), d.HireDate, 101) AS HireDate,
-					CONVERT(NVARCHAR(10), d.TerminationDate, 101) AS TerminationDate,
-					d.Alias ,
-					p.Name AS DefaultPractice,
-					d.FirstName,
-					d.LastName,
-					s.Name AS PersonStatusName,
-					d.EmployeeNumber,
-					d.SeniorityId,
-					r.Name AS Seniority,
-					CASE WHEN d.IsDefaultManager = 1 THEN 'Yes' ELSE 'NO' END AS [IsDefaultManager],
-					d.ManagerId,
-					mngr.LastName + ', ' + mngr.FirstName as [ManagerName],
-					d.TelephoneNumber,
-					d.DivisionId,
-					PD.DivisionName,
-					d.PayChexID,
-					CASE WHEN d.IsOffshore = 1 THEN 'YES' ELSE 'NO' END AS [IsOffshore],
-					TR.TerminationReasonId,
-					TR.TerminationReason
-			FROM deleted AS d
-					LEFT JOIN dbo.Practice AS p ON d.DefaultPractice = p.PracticeId
-					INNER JOIN dbo.PersonStatus AS s ON d.PersonStatusId = s.PersonStatusId
-					LEFT JOIN dbo.Seniority AS r ON d.SeniorityId = r.SeniorityId
-					LEFT JOIN dbo.Person as mngr ON mngr.PersonId = d.ManagerId
-					LEFT JOIN dbo.PersonDivision PD ON PD.DivisionId = d.DivisionId
-					LEFT JOIN dbo.TerminationReasons TR ON TR.TerminationReasonId = d.TerminationReasonId
-			WHERE d.IsStrawman = 0
-		)
-
-		-- Log an activity
-		INSERT INTO dbo.UserActivityLog
-					(ActivityTypeID,
-						SessionID,
-						SystemUser,
-						Workstation,
-						ApplicationName,
-						UserLogin,
-						PersonID,
-						LastName,
-						FirstName,
-						Data,
-						LogData,
-						LogDate)
-		SELECT CASE
-					WHEN d.PersonID IS NULL THEN 3
-					WHEN i.PersonID IS NULL THEN 5
+		IF EXISTS (SELECT 1 FROM inserted AS i	
+					INNER JOIN deleted AS d ON i.PersonID = d.PersonID 
+					WHERE ISNULL(i.PictureUrl,'') <> ISNULL(d.PictureUrl,'')
+				) 
+		BEGIN 
+			-- Log an activity for person picture change
+				INSERT INTO dbo.UserActivityLog
+				(ActivityTypeID,
+					SessionID,
+					SystemUser,
+					Workstation,
+					ApplicationName,
+					UserLogin,
+					PersonID,
+					LastName,
+					FirstName,
+					Data,
+					LogData,
+					LogDate)
+				SELECT CASE
+					WHEN d.PictureUrl IS NULL THEN 3
+					WHEN i.PictureUrl IS NULL THEN 5
 					ELSE 4
 				END AS ActivityTypeID,
 				l.SessionID,
@@ -116,51 +55,172 @@ BEGIN
 				l.PersonID,
 				l.LastName,
 				l.FirstName,
-				Data =  CONVERT(NVARCHAR(MAX),(SELECT *
-							FROM NEW_VALUES
-									FULL JOIN OLD_VALUES ON NEW_VALUES.PersonID = OLD_VALUES.PersonID
-							WHERE NEW_VALUES.PersonID = ISNULL(i.PersonId, d.PersonID) OR OLD_VALUES.PersonID = ISNULL(i.PersonId, d.PersonID)
-							FOR XML AUTO, ROOT('Person'))),
-				LogData = (SELECT NEW_VALUES.PersonId,
-									NEW_VALUES.HireDate,
-									NEW_VALUES.TerminationDate,
-									NEW_VALUES.EmployeeNumber,
-									NEW_VALUES.IsDefaultManager,
-									NEW_VALUES.TelephoneNumber,
+				Data =  CONVERT(NVARCHAR(MAX),(SELECT NEW_VALUES.PersonId,
+									NEW_VALUES.PictureUrl ,
+									NEW_VALUES.LastName + ','+ NEW_VALUES.FirstName AS Person,
 									OLD_VALUES.PersonId,
-									OLD_VALUES.HireDate,
-									OLD_VALUES.TerminationDate,
-									OLD_VALUES.EmployeeNumber,
-									OLD_VALUES.IsDefaultManager,
-									OLD_VALUES.TelephoneNumber
-							FROM NEW_VALUES
-									FULL JOIN OLD_VALUES ON NEW_VALUES.PersonID = OLD_VALUES.PersonID
-							WHERE NEW_VALUES.PersonID = ISNULL(i.PersonId, d.PersonID) OR OLD_VALUES.PersonID = ISNULL(i.PersonId, d.PersonID)
-							FOR XML AUTO, ROOT('Person'), TYPE),
+									OLD_VALUES.PictureUrl,
+									OLD_VALUES.LastName + ','+ OLD_VALUES.FirstName AS Person
+							FROM inserted AS NEW_VALUES
+									INNER JOIN deleted AS OLD_VALUES ON NEW_VALUES.PersonID = OLD_VALUES.PersonID 
+							FOR XML AUTO, ROOT('PersonSkill'))),
+				LogData = (SELECT NEW_VALUES.PersonId,
+									NEW_VALUES.PictureUrl,
+									NEW_VALUES.LastName + ','+ NEW_VALUES.FirstName AS Person,
+									OLD_VALUES.PersonId,
+									OLD_VALUES.PictureUrl,
+									OLD_VALUES.LastName + ','+ OLD_VALUES.FirstName AS Person
+							FROM inserted AS NEW_VALUES
+									INNER JOIN deleted AS OLD_VALUES ON NEW_VALUES.PersonID = OLD_VALUES.PersonID 
+							FOR XML AUTO, ROOT('PersonSkill'), TYPE),
 				@CurrentPMTime
-		FROM inserted AS i
-				FULL JOIN deleted AS d ON i.PersonID = d.PersonID 
+				FROM inserted AS i
+				INNER JOIN deleted AS d ON i.PersonID = d.PersonID 
 				INNER JOIN dbo.SessionLogData AS l ON l.SessionID = @@SPID
-		WHERE ISNULL(i.IsStrawman, d.IsStrawman) = 0 
-			AND (
-					ISNULL(i.HireDate,'') <> ISNULL(d.HireDate,'')
-					OR ISNULL(i.TerminationDate,'') <> ISNULL(d.TerminationDate,'')
-					OR ISNULL(i.Alias,'') <> ISNULL(d.Alias,'')
-					OR ISNULL(i.DefaultPractice,'') <> ISNULL(d.DefaultPractice,'')
-					OR ISNULL(i.FirstName,'') <> ISNULL(d.FirstName,'')
-					OR ISNULL(i.LastName,'') <> ISNULL(d.LastName,'')
-					OR ISNULL(i.PersonStatusId,'') <> ISNULL(d.PersonStatusId,'')
-					OR ISNULL(i.EmployeeNumber,'') <> ISNULL(d.EmployeeNumber,'')
-					OR ISNULL(i.SeniorityId,0) <> ISNULL(d.SeniorityId,0)
-					OR ISNULL(i.IsDefaultManager,'') <> ISNULL(d.IsDefaultManager,'')
-					OR ISNULL(i.ManagerId,0) <> ISNULL(d.ManagerId,0)
-					OR ISNULL(i.TelephoneNumber,'') <> ISNULL(d.TelephoneNumber,'')
-					OR ISNULL(i.DivisionId,0) <> ISNULL(d.DivisionId,0)
-					OR ISNULL(i.PayChexID,'') <> ISNULL(d.PayChexID,'')
-					OR ISNULL(i.IsOffshore,'') <> ISNULL(d.IsOffshore,'')
-					OR ISNULL(i.TerminationReasonId, -1) <> ISNULL(d.TerminationReasonId, -1)
-				)
+				WHERE ISNULL(i.IsStrawman, d.IsStrawman) = 0 
+				AND ISNULL(i.PictureUrl,'') <> ISNULL(d.PictureUrl,'')
 
+		END
+		ELSE
+		BEGIN	
+			;WITH NEW_VALUES AS
+			(
+			SELECT i.PersonId,
+				CONVERT(NVARCHAR(10), i.HireDate, 101) AS HireDate,
+				CONVERT(NVARCHAR(10), i.TerminationDate, 101) AS TerminationDate,
+				i.Alias ,
+				p.Name AS DefaultPractice,
+				i.FirstName,
+				i.LastName,
+				s.Name AS PersonStatusName,
+				i.EmployeeNumber,
+				i.SeniorityId,
+				r.Name AS Seniority,
+				CASE WHEN i.IsDefaultManager = 1 THEN 'Yes' ELSE 'NO' END AS [IsDefaultManager],
+				i.ManagerId,
+				mngr.LastName + ', ' + mngr.FirstName as [ManagerName],
+				i.TelephoneNumber,
+				i.DivisionId,
+				PD.DivisionName,
+				i.PayChexID,
+				CASE WHEN i.IsOffshore = 1 THEN 'YES' ELSE 'NO' END AS [IsOffshore],
+				TR.TerminationReasonId,
+				TR.TerminationReason
+			FROM inserted AS i
+				LEFT JOIN dbo.Practice AS p ON i.DefaultPractice = p.PracticeId
+				INNER JOIN dbo.PersonStatus AS s ON i.PersonStatusId = s.PersonStatusId
+				LEFT JOIN dbo.Seniority AS r ON i.SeniorityId = r.SeniorityId
+				LEFT JOIN dbo.Person as mngr ON mngr.PersonId = i.ManagerId
+				LEFT JOIN dbo.PersonDivision PD ON PD.DivisionId = i.DivisionId
+				LEFT JOIN dbo.TerminationReasons TR ON TR.TerminationReasonId = i.TerminationReasonId
+			WHERE i.IsStrawman = 0 
+			),
+
+			OLD_VALUES AS
+			(
+			SELECT d.PersonId,
+				CONVERT(NVARCHAR(10), d.HireDate, 101) AS HireDate,
+				CONVERT(NVARCHAR(10), d.TerminationDate, 101) AS TerminationDate,
+				d.Alias ,
+				p.Name AS DefaultPractice,
+				d.FirstName,
+				d.LastName,
+				s.Name AS PersonStatusName,
+				d.EmployeeNumber,
+				d.SeniorityId,
+				r.Name AS Seniority,
+				CASE WHEN d.IsDefaultManager = 1 THEN 'Yes' ELSE 'NO' END AS [IsDefaultManager],
+				d.ManagerId,
+				mngr.LastName + ', ' + mngr.FirstName as [ManagerName],
+				d.TelephoneNumber,
+				d.DivisionId,
+				PD.DivisionName,
+				d.PayChexID,
+				CASE WHEN d.IsOffshore = 1 THEN 'YES' ELSE 'NO' END AS [IsOffshore],
+				TR.TerminationReasonId,
+				TR.TerminationReason
+			FROM deleted AS d
+				LEFT JOIN dbo.Practice AS p ON d.DefaultPractice = p.PracticeId
+				INNER JOIN dbo.PersonStatus AS s ON d.PersonStatusId = s.PersonStatusId
+				LEFT JOIN dbo.Seniority AS r ON d.SeniorityId = r.SeniorityId
+				LEFT JOIN dbo.Person as mngr ON mngr.PersonId = d.ManagerId
+				LEFT JOIN dbo.PersonDivision PD ON PD.DivisionId = d.DivisionId
+				LEFT JOIN dbo.TerminationReasons TR ON TR.TerminationReasonId = d.TerminationReasonId
+			WHERE d.IsStrawman = 0
+			)
+
+			-- Log an activity
+			INSERT INTO dbo.UserActivityLog
+				(ActivityTypeID,
+					SessionID,
+					SystemUser,
+					Workstation,
+					ApplicationName,
+					UserLogin,
+					PersonID,
+					LastName,
+					FirstName,
+					Data,
+					LogData,
+					LogDate)
+			SELECT CASE
+				WHEN d.PersonID IS NULL THEN 3
+				WHEN i.PersonID IS NULL THEN 5
+				ELSE 4
+			END AS ActivityTypeID,
+			l.SessionID,
+			l.SystemUser,
+			l.Workstation,
+			l.ApplicationName,
+			l.UserLogin,
+			l.PersonID,
+			l.LastName,
+			l.FirstName,
+			Data =  CONVERT(NVARCHAR(MAX),(SELECT *
+						FROM NEW_VALUES
+								FULL JOIN OLD_VALUES ON NEW_VALUES.PersonID = OLD_VALUES.PersonID
+						WHERE NEW_VALUES.PersonID = ISNULL(i.PersonId, d.PersonID) OR OLD_VALUES.PersonID = ISNULL(i.PersonId, d.PersonID)
+						FOR XML AUTO, ROOT('Person'))),
+			LogData = (SELECT NEW_VALUES.PersonId,
+								NEW_VALUES.HireDate,
+								NEW_VALUES.TerminationDate,
+								NEW_VALUES.EmployeeNumber,
+								NEW_VALUES.IsDefaultManager,
+								NEW_VALUES.TelephoneNumber,
+								OLD_VALUES.PersonId,
+								OLD_VALUES.HireDate,
+								OLD_VALUES.TerminationDate,
+								OLD_VALUES.EmployeeNumber,
+								OLD_VALUES.IsDefaultManager,
+								OLD_VALUES.TelephoneNumber
+						FROM NEW_VALUES
+								FULL JOIN OLD_VALUES ON NEW_VALUES.PersonID = OLD_VALUES.PersonID
+						WHERE NEW_VALUES.PersonID = ISNULL(i.PersonId, d.PersonID) OR OLD_VALUES.PersonID = ISNULL(i.PersonId, d.PersonID)
+						FOR XML AUTO, ROOT('Person'), TYPE),
+			@CurrentPMTime
+			FROM inserted AS i
+			FULL JOIN deleted AS d ON i.PersonID = d.PersonID 
+			INNER JOIN dbo.SessionLogData AS l ON l.SessionID = @@SPID
+			WHERE ISNULL(i.IsStrawman, d.IsStrawman) = 0 
+			AND (
+				ISNULL(i.HireDate,'') <> ISNULL(d.HireDate,'')
+				OR ISNULL(i.TerminationDate,'') <> ISNULL(d.TerminationDate,'')
+				OR ISNULL(i.Alias,'') <> ISNULL(d.Alias,'')
+				OR ISNULL(i.DefaultPractice,'') <> ISNULL(d.DefaultPractice,'')
+				OR ISNULL(i.FirstName,'') <> ISNULL(d.FirstName,'')
+				OR ISNULL(i.LastName,'') <> ISNULL(d.LastName,'')
+				OR ISNULL(i.PersonStatusId,'') <> ISNULL(d.PersonStatusId,'')
+				OR ISNULL(i.EmployeeNumber,'') <> ISNULL(d.EmployeeNumber,'')
+				OR ISNULL(i.SeniorityId,0) <> ISNULL(d.SeniorityId,0)
+				OR ISNULL(i.IsDefaultManager,'') <> ISNULL(d.IsDefaultManager,'')
+				OR ISNULL(i.ManagerId,0) <> ISNULL(d.ManagerId,0)
+				OR ISNULL(i.TelephoneNumber,'') <> ISNULL(d.TelephoneNumber,'')
+				OR ISNULL(i.DivisionId,0) <> ISNULL(d.DivisionId,0)
+				OR ISNULL(i.PayChexID,'') <> ISNULL(d.PayChexID,'')
+				OR ISNULL(i.IsOffshore,'') <> ISNULL(d.IsOffshore,'')
+				OR ISNULL(i.TerminationReasonId, -1) <> ISNULL(d.TerminationReasonId, -1)
+			)
+		END
 	END
 
 	IF (SELECT COUNT(ISNULL(i.PersonID,d.PersonID)) 
