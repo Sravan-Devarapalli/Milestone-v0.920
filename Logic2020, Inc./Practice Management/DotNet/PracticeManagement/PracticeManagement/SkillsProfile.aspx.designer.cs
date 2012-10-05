98bdb91e2af4e31bb8556d7939d6c262dc731a48
SainathC <SainathC>
2012-10-05 06:51:00 -0500

    Comments - Defect Number: 3070; Defect RecordID: 3066; Server Address: localhost; Server Port: 99; Database Name: Practice Management; Defect Summary: Skills: New enhancements to the Skills Functionality; Defect Description: Please see the attachment for the desired Skills enhancements. In section 1.4 of the document there are new skills to be added to the db. These can be added independently from the functional enhancements described in the rest of the document.
    As before, contact Mike Paulsmeyer (cc: me) for specific functional questions as he created the specification. (MikeP@logic2020.com)
    
    - Matt

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/SkillsProfile.aspx.designer.cs
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/SkillsProfile.aspx.designer.cs
@@ -40,6 +40,24 @@ namespace PraticeManagement {
         protected global::System.Web.UI.WebControls.Repeater repTypes;
         
         /// <summary>
+        /// repProfiles control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Repeater repProfiles;
+        
+        /// <summary>
+        /// imgPersonPicture control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Image imgPersonPicture;
+        
+        /// <summary>
         /// btnUpdate control.
         /// </summary>
         /// <remarks>
