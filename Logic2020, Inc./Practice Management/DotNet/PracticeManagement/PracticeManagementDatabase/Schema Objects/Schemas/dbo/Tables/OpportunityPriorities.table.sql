a3bc80208e0ca7082caf7a600b9dd9fef88b723e
thulasiramp <thulasiramp>
2012-10-16 10:10:00 -0500

    Comments - Defect Number: 3092; Defect RecordID: 3088; Server Address: localhost; Server Port: 99; Database Name: Practice Management; Defect Summary: Opportunity Summary: Need an Excel export of the Opportunity Summary page; Defect Description: Similar to an export of the Project Summary data we are requesting an export into Excel of the Opportunity Summary data. See the attachment for the fields we'd like to capture in the export as it includes field values from the Opportunity that are not visible on the Opportunity Summary page itself.
    Note: This export enhancement is dependent on the changes requested in tickets 3091 and 3092.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/OpportunityPriorities.table.sql
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/OpportunityPriorities.table.sql
@@ -3,6 +3,7 @@
     Priority			 NVARCHAR(255) NOT NULL,
     Description          NVARCHAR(MAX) NULL,
 	sortOrder			 INT	,
-	[IsInserted]        bit NOT NULL
+	[IsInserted]        bit NOT NULL,
+	DisplayName			NVARCHAR(15) NULL
    CONSTRAINT PK_OpportunityPriorities_Id PRIMARY KEY CLUSTERED(Id)
 );
