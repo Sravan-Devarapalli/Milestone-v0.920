5a46743fd6bb979fa454a3d993d010046e798b2a
thulasiramp <thulasiramp>
2012-10-16 10:10:00 -0500

    Comments - Defect Number: 3092; Defect RecordID: 3088; Server Address: localhost; Server Port: 99; Database Name: Practice Management; Defect Summary: Opportunity Summary: Need an Excel export of the Opportunity Summary page; Defect Description: Similar to an export of the Project Summary data we are requesting an export into Excel of the Opportunity Summary data. See the attachment for the fields we'd like to capture in the export as it includes field values from the Opportunity that are not visible on the Opportunity Summary page itself.
    Note: This export enhancement is dependent on the changes requested in tickets 3091 and 3092.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/OpportunityExcelSet.proc.sql
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/OpportunityExcelSet.proc.sql
@@ -1,23 +1,35 @@
 ﻿CREATE PROCEDURE [dbo].[OpportunityExcelSet]
 AS 
-    BEGIN
+BEGIN
+SELECT O.OpportunityNumber AS 'Opportunity'
+	 , O.ClientName AS 'Account'
+	 , O.GroupName AS 'Business Unit'
+	 , O.BuyerName AS 'Buyer'
+	 , O.Name AS 'OpportunityName'
+	 , O.DisplayName AS 'Sales Stage'
+	 , O.OpportunityStatusName AS 'Status'
+	 , CONVERT(NVARCHAR(10),CONVERT(DATE,O.CloseDate)) AS 'Close Date'
+	 , CONVERT(NVARCHAR(10),CONVERT(DATE,O.ProjectedStartDate)) AS 'Project Start Date'
+	 , CONVERT(NVARCHAR(10),CONVERT(DATE,O.ProjectedEndDate)) AS 'Project End Date'
+	 , '$'+CONVERT(NVARCHAR(50), CONVERT(money,O.EstimatedRevenue),1)  AS 'Estimated Revenue'
+	 , O.SalespersonLastName + ',' + O.SalespersonFirstName AS 'Salesperson'
+	 , p.LastName + ',' + p.FirstName AS 'Owner'
+	 , O.PracticeName AS 'PracticeArea'	
+	 , CASE WHEN proj.ProjectId IS NULL 
+	        THEN
+			   'NO'
+		    ELSE
+			   'YES'
+	   END AS 'Linked to Project?'
+	 , proj.ProjectNumber AS 'Project Number'
+FROM
+	dbo.v_Opportunity O
+	INNER JOIN dbo.Person p
+		ON o.OwnerId = p.PersonId
+	LEFT JOIN dbo.Project proj
+		ON proj.ProjectId = o.ProjectId
+WHERE
+	O.OpportunityStatusId = 1
+END
 
-        SELECT	ROW_NUMBER() OVER (ORDER BY opt.OpportunityId) AS 'Review Order', opt.OpportunityId,
-				optstat.[Name] AS 'Status', opt.Priority, opt.ProjectedStartDate AS 'Projected Start Date',
-				client.[Name] AS 'Account name', prgroup.[Name] AS 'Business Unit', opt.[Name] AS 'Opportunity name', 
-				pers.FirstName + ' ' + pers.LastName AS 'Salesperson', opt.BuyerName AS 'Buyer Name',
-				pract.[Name] AS 'Practice', opt.OpportunityNumber AS 'Opportunity Number', rev.[Name] AS 'Revenue type',
-				proj.[ProjectNumber] as 'Attached Project',  dbo.GetOpportunityHistory(opt.OpportunityId) AS 'History'
-        FROM dbo.Opportunity AS opt 
-			INNER JOIN dbo.RevenueType AS rev ON rev.RevenueTypeId = opt.RevenueType
-			INNER JOIN dbo.OpportunityStatus AS optstat ON opt.OpportunityStatusId = optstat.OpportunityStatusId
-			INNER JOIN dbo.Client AS client ON opt.ClientId = client.ClientId
-			LEFT OUTER JOIN dbo.Person AS pers ON opt.SalespersonId = pers.PersonId
-			INNER JOIN dbo.Practice AS pract ON opt.PracticeId = pract.PracticeId
-			LEFT OUTER JOIN dbo.Project AS proj ON opt.ProjectId = proj.ProjectId		
-			LEFT OUTER JOIN dbo.ProjectGroup AS prgroup ON opt.ClientId = prgroup.ClientId AND proj.GroupId = prgroup.GroupId
-
-
-
-    END
 
