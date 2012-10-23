f8c27e1b4432f133b487acd2955628f6c55d1227
SainathC <SainathC>
2012-10-23 02:30:00 -0500

    Comments - Defect Number: 3070; Defect RecordID: 3066; Server Address: localhost; Server Port: 99; Database Name: Practice Management; Defect Summary: Skills: New enhancements to the Skills Functionality; Defect Description: Please see the attachment for the desired Skills enhancements. In section 1.4 of the document there are new skills to be added to the db. These can be added independently from the functional enhancements described in the rest of the document.
    As before, contact Mike Paulsmeyer (cc: me) for specific functional questions as he created the specification. (MikeP@logic2020.com)
    
    - Matt

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/SkillsEntry.aspx.designer.cs
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/SkillsEntry.aspx.designer.cs
@@ -13,6 +13,15 @@ namespace PraticeManagement {
     public partial class SkillsEntry {
         
         /// <summary>
+        /// lblUserName control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblUserName;
+        
+        /// <summary>
         /// loadingProgress control.
         /// </summary>
         /// <remarks>
@@ -29,15 +38,6 @@ namespace PraticeManagement {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.UpdatePanel upSkills;
-        
-        /// <summary>
-        /// lblUserName control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Label lblUserName;
         
         /// <summary>
         /// tcSkillsEntry control.
