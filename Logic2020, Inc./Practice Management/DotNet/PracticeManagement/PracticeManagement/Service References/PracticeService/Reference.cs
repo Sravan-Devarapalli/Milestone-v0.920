b1ba4023d52fe6b8c02759bbdb49ff3d03fd56c3
SainathC <SainathC>
2012-10-25 09:38:00 -0500

    Comments - Defect Number: 3096; Defect RecordID: 3092; Server Address: localhost; Server Port: 99; Database Name: Practice Management; Defect Summary: Project Details: Need a Capabilities field to track type of services performed by Project; Defect Description: We would like to track the capabilities we are executing on a project as part of the information we capture on the Project Details page.
    The user should be able to select multiple capabilities for a single project.
    
    See the attached file for a depiction of the Practice Area to Capability mapping. Each Capability is a child of its Practice Area parent.
    
    I envision leveraging a multi-select dropdown similar to the "Project Managers" field. The content of the multi-select would then be a concatenation of the Practice Area name and the Capability Name sorted alphabetically (see the attachment for examples). I'm open to alternative user experiences, however, as I can't speak to how effective my recommendation is from a usability perspective and welcome your feedback.
    
    As a second phase to this requirement, I also need a Configuration page for these capabilities that will enable an Administrator to add new capabilities or edit existing capabilities including name changes and associating each capability to one and only one parent Practice Area. Only Active Practice areas should be open for associating new capabilities.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/Service References/PracticeService/Reference.cs
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/Service References/PracticeService/Reference.cs
@@ -28,25 +28,25 @@ namespace PraticeManagement.PracticeService {
         DataTransferObjects.Practice[] PracticeGetById(System.Nullable<int> id);
         
         [System.ServiceModel.OperationContractAttribute(Action="http://tempuri.org/IPracticeService/UpdatePractice", ReplyAction="http://tempuri.org/IPracticeService/UpdatePracticeResponse")]
-        void UpdatePractice(DataTransferObjects.Practice practice);
+        void UpdatePractice(DataTransferObjects.Practice practice, string userLogin);
         
         [System.ServiceModel.OperationContractAttribute(Action="http://tempuri.org/IPracticeService/InsertPractice", ReplyAction="http://tempuri.org/IPracticeService/InsertPracticeResponse")]
-        int InsertPractice(DataTransferObjects.Practice practice);
+        int InsertPractice(DataTransferObjects.Practice practice, string userLogin);
         
         [System.ServiceModel.OperationContractAttribute(Action="http://tempuri.org/IPracticeService/RemovePractice", ReplyAction="http://tempuri.org/IPracticeService/RemovePracticeResponse")]
-        void RemovePractice(DataTransferObjects.Practice practice);
+        void RemovePractice(DataTransferObjects.Practice practice, string userLogin);
         
         [System.ServiceModel.OperationContractAttribute(Action="http://tempuri.org/IPracticeService/GetPracticeCapabilities", ReplyAction="http://tempuri.org/IPracticeService/GetPracticeCapabilitiesResponse")]
         DataTransferObjects.PracticeCapability[] GetPracticeCapabilities(System.Nullable<int> practiceId, System.Nullable<int> capabilityId);
         
         [System.ServiceModel.OperationContractAttribute(Action="http://tempuri.org/IPracticeService/CapabilityDelete", ReplyAction="http://tempuri.org/IPracticeService/CapabilityDeleteResponse")]
-        void CapabilityDelete(int capabilityId);
+        void CapabilityDelete(int capabilityId, string userLogin);
         
         [System.ServiceModel.OperationContractAttribute(Action="http://tempuri.org/IPracticeService/CapabilityUpdate", ReplyAction="http://tempuri.org/IPracticeService/CapabilityUpdateResponse")]
-        void CapabilityUpdate(DataTransferObjects.PracticeCapability capability);
+        void CapabilityUpdate(DataTransferObjects.PracticeCapability capability, string userLogin);
         
         [System.ServiceModel.OperationContractAttribute(Action="http://tempuri.org/IPracticeService/CapabilityInsert", ReplyAction="http://tempuri.org/IPracticeService/CapabilityInsertResponse")]
-        void CapabilityInsert(DataTransferObjects.PracticeCapability capability);
+        void CapabilityInsert(DataTransferObjects.PracticeCapability capability, string userLogin);
     }
     
     [System.CodeDom.Compiler.GeneratedCodeAttribute("System.ServiceModel", "4.0.0.0")]
@@ -57,7 +57,7 @@ namespace PraticeManagement.PracticeService {
     [System.CodeDom.Compiler.GeneratedCodeAttribute("System.ServiceModel", "4.0.0.0")]
     public partial class PracticeServiceClient : System.ServiceModel.ClientBase<PraticeManagement.PracticeService.IPracticeService>, PraticeManagement.PracticeService.IPracticeService {
         
-      
+       
         
         public PracticeServiceClient(string endpointConfigurationName) : 
                 base(endpointConfigurationName) {
@@ -91,32 +91,32 @@ namespace PraticeManagement.PracticeService {
             return base.Channel.PracticeGetById(id);
         }
         
-        public void UpdatePractice(DataTransferObjects.Practice practice) {
-            base.Channel.UpdatePractice(practice);
+        public void UpdatePractice(DataTransferObjects.Practice practice, string userLogin) {
+            base.Channel.UpdatePractice(practice, userLogin);
         }
         
-        public int InsertPractice(DataTransferObjects.Practice practice) {
-            return base.Channel.InsertPractice(practice);
+        public int InsertPractice(DataTransferObjects.Practice practice, string userLogin) {
+            return base.Channel.InsertPractice(practice, userLogin);
         }
         
-        public void RemovePractice(DataTransferObjects.Practice practice) {
-            base.Channel.RemovePractice(practice);
+        public void RemovePractice(DataTransferObjects.Practice practice, string userLogin) {
+            base.Channel.RemovePractice(practice, userLogin);
         }
         
         public DataTransferObjects.PracticeCapability[] GetPracticeCapabilities(System.Nullable<int> practiceId, System.Nullable<int> capabilityId) {
             return base.Channel.GetPracticeCapabilities(practiceId, capabilityId);
         }
         
-        public void CapabilityDelete(int capabilityId) {
-            base.Channel.CapabilityDelete(capabilityId);
+        public void CapabilityDelete(int capabilityId, string userLogin) {
+            base.Channel.CapabilityDelete(capabilityId, userLogin);
         }
         
-        public void CapabilityUpdate(DataTransferObjects.PracticeCapability capability) {
-            base.Channel.CapabilityUpdate(capability);
+        public void CapabilityUpdate(DataTransferObjects.PracticeCapability capability, string userLogin) {
+            base.Channel.CapabilityUpdate(capability, userLogin);
         }
         
-        public void CapabilityInsert(DataTransferObjects.PracticeCapability capability) {
-            base.Channel.CapabilityInsert(capability);
+        public void CapabilityInsert(DataTransferObjects.PracticeCapability capability, string userLogin) {
+            base.Channel.CapabilityInsert(capability, userLogin);
         }
     }
 }
