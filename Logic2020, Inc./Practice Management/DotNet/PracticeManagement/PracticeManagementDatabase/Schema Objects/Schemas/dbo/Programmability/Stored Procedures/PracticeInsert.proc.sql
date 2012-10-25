6e1b4034d060ef00dd38e7bd3d5bdb165cb5c8a6
SainathC <SainathC>
2012-10-25 08:00:00 -0500

    Comments - Defect Number: 3096; Defect RecordID: 3092; Server Address: localhost; Server Port: 99; Database Name: Practice Management; Defect Summary: Project Details: Need a Capabilities field to track type of services performed by Project; Defect Description: We would like to track the capabilities we are executing on a project as part of the information we capture on the Project Details page.
    The user should be able to select multiple capabilities for a single project.
    
    See the attached file for a depiction of the Practice Area to Capability mapping. Each Capability is a child of its Practice Area parent.
    
    I envision leveraging a multi-select dropdown similar to the "Project Managers" field. The content of the multi-select would then be a concatenation of the Practice Area name and the Capability Name sorted alphabetically (see the attachment for examples). I'm open to alternative user experiences, however, as I can't speak to how effective my recommendation is from a usability perspective and welcome your feedback.
    
    As a second phase to this requirement, I also need a Configuration page for these capabilities that will enable an Administrator to add new capabilities or edit existing capabilities including name changes and associating each capability to one and only one parent Practice Area. Only Active Practice areas should be open for associating new capabilities.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/PracticeInsert.proc.sql
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/PracticeInsert.proc.sql
@@ -5,61 +5,85 @@
 -- =============================================
 CREATE PROCEDURE dbo.PracticeInsert 
 	@Name VARCHAR(100),
+	@Abbreviation NVARCHAR(100) = NULL,
 	@PracticeManagerId INT,
 	@IsActive BIT = 1,
 	@IsCompanyInternal BIT = 0 	
 AS
 BEGIN
 	SET NOCOUNT ON;
+	BEGIN TRY
+		BEGIN TRAN PracticeInsert_Tran;
 
-	DECLARE @IsNotesRequired BIT = 1
+		DECLARE @IsNotesRequired BIT = 1
 
-	SELECT @IsNotesRequired =(SELECT  s.Value 
-							FROM dbo.Settings AS s
-							WHERE SettingsKey='NotesRequiredForTimeEntry' AND TypeId=4)
+		SELECT @IsNotesRequired =(SELECT  s.Value 
+								FROM dbo.Settings AS s
+								WHERE SettingsKey='NotesRequiredForTimeEntry' AND TypeId=4)
 
-	IF(@IsActive = 0)
-	BEGIN
-	SET @IsNotesRequired = 1
-	END
-	
-	IF EXISTS(SELECT 1 FROM dbo.Practice WHERE [Name] = @Name)
-	BEGIN
+		IF(@IsActive = 0)
+		BEGIN
+		SET @IsNotesRequired = 1
+		END
 		DECLARE @Error NVARCHAR(200)
-		SET @Error = 'This Practice already exists. Please add a different Practice.'
-		RAISERROR(@Error,16,1)
-		
-	END
-	DECLARE @PracticeId INT,
-			@Today DATETIME
-	 SET @Today  = CONVERT(DATETIME,CONVERT(DATE,GETDATE()))
+		IF EXISTS(SELECT 1 FROM dbo.Practice WHERE [Name] = @Name)
+		BEGIN
+			SET @Error = 'This Practice already exists. Please add a different Practice.'
+			RAISERROR(@Error,16,1)
+		END
+		IF EXISTS(SELECT 1 FROM dbo.Practice WHERE ISNULL(Abbreviation,0) = @Abbreviation)
+		BEGIN
+			SET @Error = 'This practice area abbreviation already exists. Please add a different practice area abbreviation.'
+			RAISERROR(@Error,16,1)
+		END
 
-	INSERT INTO dbo.Practice (
-		[Name],
-		PracticeManagerId,
-		IsActive,
-		IsCompanyInternal,
-		IsNotesRequired
-	) VALUES ( 
-		@Name,
-		@PracticeManagerId,
-		@IsActive,
-		@IsCompanyInternal,
-		@IsNotesRequired)
+		DECLARE @PracticeId INT
+
+		INSERT INTO dbo.Practice (
+			[Name],
+			[Abbreviation],
+			PracticeManagerId,
+			IsActive,
+			IsCompanyInternal,
+			IsNotesRequired
+		) VALUES ( 
+			@Name,
+			@Abbreviation,
+			@PracticeManagerId,
+			@IsActive,
+			@IsCompanyInternal,
+			@IsNotesRequired)
+
 		SELECT @PracticeId = SCOPE_IDENTITY()
-	INSERT INTO dbo.PracticeManagerHistory
-					(PracticeId,		
-					PracticeManagerId,	
-					StartDate,			
-					EndDate)
-			VALUES
-				(
-				@PracticeId,
-				@PracticeManagerId,
-				GETDATE(),
-				NULL)
-	EXEC dbo.PracticeStatusHistoryUpdate
-		@PracticeId = @PracticeId,
-		@IsActive = @IsActive
+
+		INSERT INTO dbo.PracticeManagerHistory
+						(PracticeId,		
+						PracticeManagerId,	
+						StartDate,			
+						EndDate)
+				VALUES
+					(
+					@PracticeId,
+					@PracticeManagerId,
+					dbo.InsertingTime(),
+					NULL)
+		EXEC dbo.PracticeStatusHistoryUpdate
+			@PracticeId = @PracticeId,
+			@IsActive = @IsActive
+
+	COMMIT TRAN PracticeInsert_Tran;
+	END TRY
+	BEGIN CATCH
+	ROLLBACK TRAN PracticeInsert_Tran;
+		DECLARE	 @ERROR_STATE	tinyint
+				,@ERROR_SEVERITY		tinyint
+				,@ERROR_MESSAGE		    nvarchar(2000)
+				,@InitialTranCount		tinyint
+
+		SET	 @ERROR_MESSAGE		= ERROR_MESSAGE()
+		SET  @ERROR_SEVERITY	= ERROR_SEVERITY()
+		SET  @ERROR_STATE		= ERROR_STATE()
+		RAISERROR ('%s', @ERROR_SEVERITY, @ERROR_STATE, @ERROR_MESSAGE)
+	END CATCH
 END
 
