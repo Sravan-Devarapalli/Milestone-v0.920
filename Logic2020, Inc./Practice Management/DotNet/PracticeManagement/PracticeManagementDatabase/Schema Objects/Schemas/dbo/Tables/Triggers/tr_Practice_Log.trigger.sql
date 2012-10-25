b06b2f992f94cdb7eedac8fbb6323734ce7e9455
SainathC <SainathC>
2012-10-25 09:38:00 -0500

    Comments - Defect Number: 3096; Defect RecordID: 3092; Server Address: localhost; Server Port: 99; Database Name: Practice Management; Defect Summary: Project Details: Need a Capabilities field to track type of services performed by Project; Defect Description: We would like to track the capabilities we are executing on a project as part of the information we capture on the Project Details page.
    The user should be able to select multiple capabilities for a single project.
    
    See the attached file for a depiction of the Practice Area to Capability mapping. Each Capability is a child of its Practice Area parent.
    
    I envision leveraging a multi-select dropdown similar to the "Project Managers" field. The content of the multi-select would then be a concatenation of the Practice Area name and the Capability Name sorted alphabetically (see the attachment for examples). I'm open to alternative user experiences, however, as I can't speak to how effective my recommendation is from a usability perspective and welcome your feedback.
    
    As a second phase to this requirement, I also need a Configuration page for these capabilities that will enable an Administrator to add new capabilities or edit existing capabilities including name changes and associating each capability to one and only one parent Practice Area. Only Active Practice areas should be open for associating new capabilities.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/Triggers/tr_Practice_Log.trigger.sql
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/Triggers/tr_Practice_Log.trigger.sql
@@ -5,9 +5,95 @@ AS
 BEGIN
 	-- Ensure the temporary table exists
 	EXEC SessionLogPrepare @UserLogin = NULL
+	DECLARE @CurrentPMTime DATETIME 
+	SET @CurrentPMTime = dbo.InsertingTime()
 	
-	
+	;WITH NEW_VALUES AS
+	(
+		SELECT i.PracticeId,
+				i.Name AS [Practice],
+				i.Abbreviation,
+				CASE WHEN i.IsActive = 1 THEN 'YES' ELSE 'NO' END [IsActive],
+				CASE WHEN i.IsCompanyInternal = 1 THEN 'YES' ELSE 'NO' END [IsCompanyInternal],
+				CASE WHEN i.IsNotesRequired = 1 THEN 'YES' ELSE 'NO' END [IsNotesRequired],
+				p.PersonId as [PracticeManagerId],
+				p.LastName + ', ' + p.FirstName as [PracticeManager]
+		  FROM inserted AS i
+		  INNER JOIN dbo.Person P ON P.PersonId = i.PracticeManagerId
+	),
 
+	OLD_VALUES AS
+	(
+			SELECT d.PracticeId,
+				d.Name AS [Practice],
+				d.Abbreviation,
+				CASE WHEN d.IsActive = 1 THEN 'YES' ELSE 'NO' END [IsActive],
+				CASE WHEN d.IsCompanyInternal = 1 THEN 'YES' ELSE 'NO' END [IsCompanyInternal],
+				CASE WHEN d.IsNotesRequired = 1 THEN 'YES' ELSE 'NO' END [IsNotesRequired],
+				p.PersonId as [PracticeManagerId],
+				p.LastName + ', ' + p.FirstName as [PracticeManager]
+		  FROM deleted AS d
+		  INNER JOIN dbo.Person P ON P.PersonId = d.PracticeManagerId
+	)
+
+	-- Log an activity
+	INSERT INTO dbo.UserActivityLog
+	            (ActivityTypeID,
+	             SessionID,
+	             SystemUser,
+	             Workstation,
+	             ApplicationName,
+	             UserLogin,
+	             PersonID,
+	             LastName,
+	             FirstName,
+				 Data,
+	             LogData,
+	             LogDate)
+	SELECT  CASE
+	           WHEN d.PracticeId IS NULL THEN 3
+	           WHEN i.PracticeId IS NULL THEN 5
+	           ELSE 4
+	       END as ActivityTypeID,
+	       l.SessionID,
+	       l.SystemUser,
+	       l.Workstation,
+	       l.ApplicationName,
+	       l.UserLogin,
+	       l.PersonID,
+	       l.LastName,
+	       l.FirstName,
+	      Data = CONVERT(NVARCHAR(MAX),(SELECT *
+					    FROM NEW_VALUES
+					         FULL JOIN OLD_VALUES ON NEW_VALUES.PracticeId = OLD_VALUES.PracticeId
+			           WHERE NEW_VALUES.PracticeId = ISNULL(i.PracticeId, d.PracticeId) OR OLD_VALUES.PracticeId = ISNULL(i.PracticeId, d.PracticeId)
+					  FOR XML AUTO, ROOT('Practice'))),
+		LogData = (SELECT 
+						NEW_VALUES.PracticeId 
+						,NEW_VALUES.Practice
+						,NEW_VALUES.Abbreviation
+						,NEW_VALUES.IsActive
+						,NEW_VALUES.IsCompanyInternal
+						,NEW_VALUES.IsNotesRequired
+						,NEW_VALUES.PracticeManagerId
+						,NEW_VALUES.[PracticeManager]
+						,OLD_VALUES.PracticeId 
+						,OLD_VALUES.Practice
+						,OLD_VALUES.Abbreviation
+						,OLD_VALUES.IsActive
+						,OLD_VALUES.IsCompanyInternal
+						,OLD_VALUES.IsNotesRequired
+						,OLD_VALUES.PracticeManagerId
+						,OLD_VALUES.[PracticeManager]
+					 FROM NEW_VALUES
+					         FULL JOIN OLD_VALUES ON NEW_VALUES.PracticeId = OLD_VALUES.PracticeId
+			           WHERE NEW_VALUES.PracticeId = ISNULL(i.PracticeId, d.PracticeId) OR OLD_VALUES.PracticeId = ISNULL(i.PracticeId, d.PracticeId)
+					FOR XML AUTO, ROOT('Practice'), TYPE),
+			@CurrentPMTime
+	  FROM inserted AS i
+	       FULL JOIN deleted AS d ON i.PracticeId = d.PracticeId
+	       INNER JOIN dbo.SessionLogData AS l ON l.SessionID = @@SPID
+	
 	 -- End logging session
 	EXEC dbo.SessionLogUnprepare
 END
