6f10691d078b593df1f76b3c77efc939a745fe5b
SrinivasM <SrinivasM>
2012-11-06 09:18:00 -0500

    Comments - Defect Number: 3103; Defect RecordID: 3099; Server Address: localhost; Server Port: 99; Database Name: Practice Management; Defect Summary: Skills: Enhancement for adding/updating consultant profile picture.; Defect Description: With reference to Defect #3070.
    Include picture on the skills profile page
    
    a. At the bottom of the skills entry screen, add a button labeled Add/Update Consultant Picture, so that user can upload/update his/her picture to his profile by clicking on that button.
    b. Add consultants photo on the skills profile.
    
    The parameters for uploading users profile picture should be as mentioned below.
    
    1. Dimensions: Recommended to upload picture of 120 x 100 dimensions for the better view of the picture in Skills Profile page, but can upload picture of any dimensions.
    2. File size: No restrictions, user can upload picture of any size.
    3. File Type: User is restricted to upload file types of only JPG, JPEG, Png, Bmp and Gif.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/Skills/Programmability/Stored Procedures/GetPersonWithHasPictureField.proc.sql
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/Skills/Programmability/Stored Procedures/GetPersonWithHasPictureField.proc.sql
@@ -8,8 +8,8 @@ BEGIN
 	SELECT p.PersonId,
 	       p.LastName,
 		   p.FirstName,
-		   CASE WHEN p.PersonPicture IS NULL THEN 0
-		        ELSE 1 END AS 'HasPicture'		    
+		   CASE WHEN p.PictureData IS NULL THEN 0
+		        ELSE 1 END AS 'HasPicture'
 	FROM dbo.[Person] p
 	WHERE p.PersonId = @PersonId
 END
