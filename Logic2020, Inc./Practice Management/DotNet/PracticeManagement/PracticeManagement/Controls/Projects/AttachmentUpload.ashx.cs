dee3c5d9f017b685ab252dd4e2cd5470fd004bfe
thulasiramp <thulasiramp>
2012-11-08 03:05:00 -0500

    Comments - Defect Number: 3098; Defect RecordID: 3094; Server Address: localhost; Server Port: 99; Database Name: Practice Management; Defect Summary: Project Details - expand Attachment category choices and supported file types and enable multiple files to be added at one tim@; Defect Description: On the Project Details page, we'd like to expand the list of Attachment Categories to include the following additional options: Proposal Project Estimate
    Currently the Attachment categories are SOW, MSA, and Change Request.
    
    Additionally, it appears that the system restricts the Attachment file type to either PDF or Word doc. If this is true, we'd like to expand the list of supported file types to include Excel (.xls, .xlw, .xlsx etc.), PowerPoint (.ppt, .pptx), MS Project (.mpp), Visio (.vsd), Exchange (.msg), OneNote , ZIP and/or RAR files.
    
    I'm not certain of all the applicable extensions.
    
    Finally, I'd like to have the option to add multiple files of the same type (SOW, MSA, Change Request, Proposal, Project Estimate) at one time versus one at a time.

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/Controls/Projects/AttachmentUpload.ashx.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Web;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PraticeManagement.Controls.Projects
+{
+    /// <summary>
+    /// Summary description for AttachmentUpload
+    /// </summary>
+    public class AttachmentUpload : IHttpHandler
+    {
+
+        public void ProcessRequest(HttpContext context)
+        {
+            try
+            {
+                HttpPostedFile postedFile = context.Request.Files["Filedata"];
+                int projectId = Convert.ToInt32(context.Request.QueryString["ProjectId"]);
+                int category = Convert.ToInt32(context.Request.QueryString["categoryId"]);
+
+                byte[] fileData = new byte[postedFile.InputStream.Length];
+                postedFile.InputStream.Read(fileData, 0, fileData.Length);
+
+                PraticeManagement.AttachmentService.ProjectAttachment attachment = new PraticeManagement.AttachmentService.ProjectAttachment();
+                attachment.AttachmentFileName = postedFile.FileName;
+                attachment.AttachmentData = fileData;
+                attachment.AttachmentSize = fileData.Length;                
+
+                attachment.Category = (PraticeManagement.AttachmentService.ProjectAttachmentCategory)category;
+                if (projectId != 0)
+                {
+                    PraticeManagement.AttachmentService.AttachmentService svc = PraticeManagement.Utils.WCFClientUtility.GetAttachmentService();
+                    svc.SaveProjectAttachment(attachment, projectId, context.User.Identity.Name);
+                    context.Response.Write("Uploaded");
+                }
+                else
+                {
+                    attachment.UploadedDate = DateTime.Now;
+                    string file = AttachmentUpload.ToJSON(attachment);
+
+                    context.Response.Write(file);
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        private static string ToJSON(Object obj)
+        {
+            System.Web.Script.Serialization.JavaScriptSerializer serializer = new System.Web.Script.Serialization.JavaScriptSerializer();
+            serializer.MaxJsonLength = int.MaxValue;
+            return serializer.Serialize(obj);
+
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
