daae6a26c38a45571cba279426d1b574717fbb32
SainathC <SainathC>
2012-11-15 07:46:00 -0500

    Comments - Defect Number: 3104; Defect RecordID: 3100; Server Address: localhost; Server Port: 99; Database Name: Practice Management; Defect Summary: Reports / Resources / Consulting Utilization: Utilization is calculated incorrectly before the persons re-hired dates.; Defect Description: In consulting utilization report person is considered as terminated before the re-hire dates and the utilization is calculated only from the persons re-hired date. Similarly in Consulting Capacity report person is considered as terminated before the re-hire dates.
    Steps to reproduce:
    
    1. Add a person Person A on 10/01/2012 with W2-Salary pay type.
    2. Add Person A in a project say Testing Project from 10/01/2012 to 11/30/2012 for 8 hrs/day.
    3. Run Consulting Utilization report between 10/01/2012 and 10/31/2012.
    Result: The utilization of Person A between 10/01/2012 and 10/31/2012 would be displayed as 100%.
    
    
    4. Terminate Person A on 11/01/2012.
    5. Re-hire the person on 11/04/2012 with W2-Salary pay type.
    6. Add Person A in a project Testing Project from 11/04/2012 to 11/30/2012 for 8 hrs/day.
    7. Run Consulting Utilization report between 10/01/2012 and 11/30/2012.
    Result: The utilization for Person A is considered only from 11/04/2012 and the person is considered as terminated from 10/01/2012 and 11/04/2012.
    
    
    Actual Result: Person is considered as terminated before his/her re-hire dates and the utilization is considered only from persons re-hire date.
    
    Expected Result: Utilization for a person should also be calculated before persons rehired date if applicable.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/GetTimeOffSeriesPeriod.proc.sql
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/GetTimeOffSeriesPeriod.proc.sql
@@ -5,58 +5,19 @@
 )
 AS
 BEGIN
-	
-	IF 0 = (SELECT PC.IsSeries
-		FROM dbo.PersonCalendar PC
-			WHERE PC.PersonId = @PersonId AND PC.Date = @Date)
-	BEGIN
-		SELECT @Date 'StartDate', @Date 'EndDate', PC.ApprovedBy 'ApprovedBy', p.FirstName + ', ' + p.LastName 'ApprovedByName'
-		FROM PersonCalendar PC
-		LEFT JOIN Person P ON P.PersonId = PC.ApprovedBy
-		WHERE PC.PersonId = @PersonId AND PC.Date = @Date
-	END
-	ELSE
-	BEGIN
-		
-		;WITH AfterConsecutiveDates AS
-		(
-			SELECT PCC.*
-			FROM PersonCalendar PCC
-			WHERE PCC.Date = @Date AND PCC.PersonId = @PersonId AND PCC.IsSeries = 1
-			UNION ALL
-			SELECT PC.*
-			FROM AfterConsecutiveDates CD
-			JOIN PersonCalendar PC ON ((DATEPART(DW, CD.date) = 6 AND PC.date = DATEADD(DD,3, CD.date) )
-											OR  PC.date = DATEADD(DD,1, CD.date)
-										)
-									AND PC.PersonId = CD.PersonId AND PC.IsSeries = 1 AND PC.TimeTypeId = CD.TimeTypeId
-									AND PC.ActualHours = CD.ActualHours AND ISNULL(PC.ApprovedBy, 0) = ISNULL(CD.ApprovedBy, 0)
-		),
-		BeforeConsecutiveDates AS
-		(
-			SELECT PCC.*
-			FROM PersonCalendar PCC
-			WHERE PCC.Date = @Date AND PCC.PersonId = @PersonId AND PCC.IsSeries = 1
-			UNION ALL
-			SELECT PC.*
-			FROM BeforeConsecutiveDates CD
-			JOIN PersonCalendar PC ON ((DATEPART(DW, CD.date) = 2 AND PC.date = DATEADD(DD, -3, CD.date))
-											OR  PC.date = DATEADD(DD, -1, CD.date)
-										)
-									AND PC.PersonId = CD.PersonId AND PC.IsSeries = 1 AND PC.TimeTypeId = CD.TimeTypeId
-									AND PC.ActualHours = CD.ActualHours AND ISNULL(PC.ApprovedBy, 0) = ISNULL(CD.ApprovedBy, 0)
-		)
 
-		SELECT MIN(CD.Date) 'StartDate', MAX(CD.Date) 'EndDate', CD.ApprovedBy 'ApprovedBy', p.FirstName + ', ' + p.LastName 'ApprovedByName'
-		FROM 
-		(
-			SELECT * FROM BeforeConsecutiveDates BD
-			UNION
-			SELECT * FROM AfterConsecutiveDates AD
-		) CD
-		LEFT JOIN dbo.Person P ON P.PersonId = CD.ApprovedBy
-		GROUP BY CD.PersonId, CD.ApprovedBy, P.FirstName, P.LastName
+DECLARE @SeriesID BIGINT 
 
-	END
+SELECT @SeriesID = PC.SeriesId 
+FROM  dbo.PersonCalendar AS PC
+WHERE PC.Date = @Date AND PC.PersonId = @PersonId
+
+        SELECT MIN(PC.Date) 'StartDate',  MAX(PC.Date) 'EndDate', 
+		PC.ApprovedBy 'ApprovedBy', p.FirstName + ', ' + p.LastName 'ApprovedByName'
+		FROM dbo.PersonCalendar AS PC
+		INNER JOIN dbo.Person AS P ON P.PersonId = Pc.PersonId
+		WHERE PC.SeriesId = @SeriesID
+		GROUP BY Pc.SeriesId, p.FirstName , p.LastName ,PC.ApprovedBy
 
 END
+
