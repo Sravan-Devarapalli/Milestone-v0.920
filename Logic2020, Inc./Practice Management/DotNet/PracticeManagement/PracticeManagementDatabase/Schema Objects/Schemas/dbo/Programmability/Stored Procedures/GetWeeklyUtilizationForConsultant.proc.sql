03e1571b221c3bf1496adcc9017bd6a44e502bdd
SainathC <SainathC>
2012-11-15 09:35:00 -0500

    Comments - Defect Number: 3104; Defect RecordID: 3100; Server Address: localhost; Server Port: 99; Database Name: Practice Management; Defect Summary: Reports / Resources / Consulting Utilization: Utilization is calculated incorrectly before the persons re-hired dates.; Defect Description: In consulting utilization report person is considered as terminated before the re-hire dates and the utilization is calculated only from the persons re-hired date. Similarly in Consulting Capacity report person is considered as terminated before the re-hire dates.
    Steps to reproduce:
    
    1. Add a person Person A on 10/01/2012 with W2-Salary pay type.
    2. Add Person A in a project say Testing Project from 10/01/2012 to 11/30/2012 for 8 hrs/day.
    3. Run Consulting Utilization report between 10/01/2012 and 10/31/2012.
    Result: The utilization of Person A between 10/01/2012 and 10/31/2012 would be displayed as 100%.
    
    
    4. Terminate Person A on 11/01/2012.
    5. Re-hire the person on 11/04/2012 with W2-Salary pay type.
    6. Add Person A in a project Testing Project from 11/04/2012 to 11/30/2012 for 8 hrs/day.
    7. Run Consulting Utilization report between 10/01/2012 and 11/30/2012.
    Result: The utilization for Person A is considered only from 11/04/2012 and the person is considered as terminated from 10/01/2012 and 11/04/2012.
    
    
    Actual Result: Person is considered as terminated before his/her re-hire dates and the utilization is considered only from persons re-hire date.
    
    Expected Result: Utilization for a person should also be calculated before persons rehired date if applicable.

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/GetWeeklyUtilizationForConsultant.proc.sql
@@ -0,0 +1,68 @@
+﻿CREATE PROCEDURE [dbo].[GetWeeklyUtilizationForConsultant]
+    @StartDate DATETIME,
+    @Step INT = 7,
+    @DaysForward INT = 184,
+    @ActivePersons BIT = 1,
+    @ActiveProjects BIT = 1,
+    @ProjectedPersons BIT = 1,
+    @ProjectedProjects BIT = 1,
+    @ExperimentalProjects BIT = 1,
+	@InternalProjects	BIT = 1,
+	@TimescaleIds NVARCHAR(4000) = NULL,
+	@PracticeIds NVARCHAR(4000) = NULL,
+	@ExcludeInternalPractices BIT = 0,
+	@IsSampleReport BIT = 0
+	
+AS 
+   BEGIN
+   /*
+   1.If @IsSampleReport is 1 THEN Populate all practices in @PracticeIds.
+   2.If step is 7
+	 a.If STARTDATE is SUNDAY then set STARTDATE to MONDAY of the STARTDATE week.
+	 b.If STARTDATE is not sat then set ENDDATE to sat of the ENDDATE week.
+
+   3.If step is 30
+	 a.If STARTDATE is SUNDAY then set STARTDATE to MONDAY of the STARTDATE week.
+	 b.If ENDDATE is not sat then set ENDDATE to sat of the ENDDATE week.
+   4.Get the WeeklyUtlization of the person for the given filters.
+   */
+        SET NOCOUNT ON ;
+        IF (@IsSampleReport = 1)
+        BEGIN
+			SELECT @PracticeIds = COALESCE(@PracticeIds+',' ,'') + Convert(VARCHAR,PracticeId)
+			FROM Practice
+			ORDER BY Name
+			SET @PracticeIds = ','+@PracticeIds+','
+        END
+
+		DECLARE @EndRange  DATETIME
+		SET @EndRange = DATEADD(dd , @DaysForward, @StartDate) - 1
+		IF(@Step = 7)
+		BEGIN
+			IF(DATEPART(DW,@StartDate)>2)
+			BEGIN
+				SELECT @StartDate = @StartDate - DATEPART(DW,@StartDate)+2
+			END
+			IF(DATEPART(DW,@StartDate)<7)
+			BEGIN
+				SELECT @EndRange = DATEADD(dd , 7-DATEPART(DW,@EndRange), @EndRange)
+			END
+		END
+		ELSE IF (@Step = 30)
+		BEGIN
+                
+			IF(DATEPART(DW,@StartDate)>2)
+			BEGIN
+				SELECT @StartDate = @StartDate - DATEPART(DW,@StartDate)+2
+			END
+			IF(DATEPART(DW,@EndRange)<7)
+			BEGIN
+				SELECT @EndRange = DATEADD(dd , 7-DATEPART(DW,@EndRange), @EndRange)
+			END
+		END
+
+		SELECT WUT.PersonId,WUT.WeeklyUtlization
+		FROM dbo.GetWeeklyUtilizationTable(@StartDate,@EndRange, @Step, @ActivePersons, @ActiveProjects, @ProjectedPersons, @ProjectedProjects,@ExperimentalProjects,@InternalProjects,@TimescaleIds,@PracticeIds,@ExcludeInternalPractices) AS WUT 
+		ORDER BY WUT.PersonId,WUT.StartDate
+				 
+    END
