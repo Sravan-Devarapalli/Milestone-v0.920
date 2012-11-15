4c8c2480d419147dfcaca212a99e466baf5f2f4f
SainathC <SainathC>
2012-11-15 07:46:00 -0500

    Comments - Defect Number: 3104; Defect RecordID: 3100; Server Address: localhost; Server Port: 99; Database Name: Practice Management; Defect Summary: Reports / Resources / Consulting Utilization: Utilization is calculated incorrectly before the persons re-hired dates.; Defect Description: In consulting utilization report person is considered as terminated before the re-hire dates and the utilization is calculated only from the persons re-hired date. Similarly in Consulting Capacity report person is considered as terminated before the re-hire dates.
    Steps to reproduce:
    
    1. Add a person Person A on 10/01/2012 with W2-Salary pay type.
    2. Add Person A in a project say Testing Project from 10/01/2012 to 11/30/2012 for 8 hrs/day.
    3. Run Consulting Utilization report between 10/01/2012 and 10/31/2012.
    Result: The utilization of Person A between 10/01/2012 and 10/31/2012 would be displayed as 100%.
    
    
    4. Terminate Person A on 11/01/2012.
    5. Re-hire the person on 11/04/2012 with W2-Salary pay type.
    6. Add Person A in a project Testing Project from 11/04/2012 to 11/30/2012 for 8 hrs/day.
    7. Run Consulting Utilization report between 10/01/2012 and 11/30/2012.
    Result: The utilization for Person A is considered only from 11/04/2012 and the person is considered as terminated from 10/01/2012 and 11/04/2012.
    
    
    Actual Result: Person is considered as terminated before his/her re-hire dates and the utilization is considered only from persons re-hire date.
    
    Expected Result: Utilization for a person should also be calculated before persons rehired date if applicable.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/DeleteTimeEntry.proc.sql
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/DeleteTimeEntry.proc.sql
@@ -36,29 +36,11 @@ BEGIN
 		FROM dbo.PersonCalendar PC
 		WHERE PC.PersonId = @personId AND PC.Date BETWEEN @StartDate AND @EndDate AND PC.TimeTypeId = @TimeTypeId
 
-		SET @StartDate = @StartDate - 2
-		SET @EndDate = @EndDate + 2
 
-		--Update the series.
-		;WITH NeedToModifyDates AS
-		(
-			SELECT  PC.PersonId, C.Date, CONVERT(BIT, 0) 'IsSeries'
-			FROM Calendar C
-			JOIN PersonCalendar PC ON C.Date IN (@StartDate, @EndDate ) AND C.Date = PC.Date AND PC.PersonId = @personId AND PC.DayOff = 1 AND PC.IsSeries = 1
-			LEFT JOIN PersonCalendar APC ON PC.PersonId = APC.PersonId AND APC.DayOff = 1 AND APC.IsSeries = 1 AND APC.ActualHours = PC.ActualHours AND APC.TimeTypeId = PC.TimeTypeId AND ISNULL(APC.ApprovedBy, 0) = ISNULL(PC.ApprovedBy, 0)--APC:- AffectedPersonCalendar
-						AND (APC.date = DATEADD(DD,1, C.date)
-								OR  APC.date = DATEADD(DD, -1, C.date)
-							)
-			GROUP BY PC.PersonId, C.date
-			Having COUNT(APC.date) < 1
-		)
-
-		UPDATE PC
-			SET IsSeries = NTMF.IsSeries
-		FROM PersonCalendar PC
-		JOIN NeedToModifyDates NTMF ON NTMF.PersonId = PC.PersonId AND NTMF.Date = PC.Date
+		
 	
 	EXEC dbo.SessionLogUnprepare
 
  COMMIT TRAN TimeEntryDelete
 END
+
