dad5b8a75b65a44fc932db0eae35076e8d989c09
SainathC <SainathC>
2012-11-15 09:35:00 -0500

    Comments - Defect Number: 3104; Defect RecordID: 3100; Server Address: localhost; Server Port: 99; Database Name: Practice Management; Defect Summary: Reports / Resources / Consulting Utilization: Utilization is calculated incorrectly before the persons re-hired dates.; Defect Description: In consulting utilization report person is considered as terminated before the re-hire dates and the utilization is calculated only from the persons re-hired date. Similarly in Consulting Capacity report person is considered as terminated before the re-hire dates.
    Steps to reproduce:
    
    1. Add a person Person A on 10/01/2012 with W2-Salary pay type.
    2. Add Person A in a project say Testing Project from 10/01/2012 to 11/30/2012 for 8 hrs/day.
    3. Run Consulting Utilization report between 10/01/2012 and 10/31/2012.
    Result: The utilization of Person A between 10/01/2012 and 10/31/2012 would be displayed as 100%.
    
    
    4. Terminate Person A on 11/01/2012.
    5. Re-hire the person on 11/04/2012 with W2-Salary pay type.
    6. Add Person A in a project Testing Project from 11/04/2012 to 11/30/2012 for 8 hrs/day.
    7. Run Consulting Utilization report between 10/01/2012 and 11/30/2012.
    Result: The utilization for Person A is considered only from 11/04/2012 and the person is considered as terminated from 10/01/2012 and 11/04/2012.
    
    
    Actual Result: Person is considered as terminated before his/her re-hire dates and the utilization is considered only from persons re-hire date.
    
    Expected Result: Utilization for a person should also be calculated before persons rehired date if applicable.

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Functions/GetWeeklyUtilizationTable.function.sql
@@ -0,0 +1,148 @@
+﻿CREATE FUNCTION [dbo].[GetWeeklyUtilizationTable]
+(
+	@StartDate DATETIME,
+    @EndDate DATETIME,
+	@Step INT = 7,
+    @ActivePersons BIT = 1,
+    @ActiveProjects BIT = 1,
+    @ProjectedPersons BIT = 1,
+    @ProjectedProjects BIT = 1,
+    @ExperimentalProjects BIT = 1,
+	@InternalProjects	BIT = 1,
+	@TimescaleIds NVARCHAR(4000) = NULL,
+	@PracticeIds NVARCHAR(4000) = NULL,
+	@ExcludeInternalPractices BIT = 0
+)
+RETURNS TABLE 
+AS
+RETURN
+
+/*
+	1.Ranges:Gets the Ranges for the given step For the given dates( i.e. STARTDATE and ENDDATE).
+	2.CurrentConsultantsWithRanges : Gets the consultants for the given filters ( i.e. @TimescaleIds,@PracticeIds,@ActivePersons,@ProjectedPersons,@ExcludeInternalPractices) mapped with Ranges.
+	3.CurrentConsultantsWithVacationDays : Populate  VacationDays of the Consultants for the CurrentConsultantsWithRanges records.
+	4.CurrentConsultantsWithProjectedHours : Populate  ProjectedHours of the Consultants for the CurrentConsultantsWithVacationDays records.
+	5.CurrentConsultantsWithAvaliableHours : Populate  AvaliableHours of the Consultants for the CurrentConsultantsWithVacationDays records.
+	6.CurrentConsultantsWithAllHours : Populate  all hours of the Consultants for the CurrentConsultantsWithVacationDays records.
+	7.Final result : calculate the WeeklyUtlization with the obtained hours.
+
+	Calculation of VacationDays: 
+
+	Calculation of ProjectedHours:
+
+	Calculation of AvaliableHours:
+
+
+*/
+
+WIth Ranges
+AS
+(
+	SELECT  c.MonthStartDate as StartDate,c.MonthEndDate  AS EndDate
+	FROM dbo.Calendar c
+	WHERE c.date BETWEEN @StartDate and @EndDate
+	AND @Step = 30
+	GROUP BY c.MonthStartDate,c.MonthEndDate  
+	UNION ALL
+	SELECT  c.date,c.date + 6
+	FROM dbo.Calendar c
+	WHERE c.date BETWEEN @StartDate and @EndDate
+	AND DATEDIFF(day,@StartDate,c.date) % 7 = 0
+	AND @Step = 7
+	UNION ALL
+	SELECT  c.date,c.date
+	FROM dbo.Calendar c
+	WHERE c.date BETWEEN @StartDate and @EndDate
+	AND @Step = 1
+),
+CurrentConsultantsWithRanges
+AS
+(
+	SELECT  P.PersonId,
+			r.StartDate,
+			CASE WHEN r.EndDate > @EndDate THEN @EndDate ELSE r.EndDate END EndDate,
+			PCPT.Timescale
+    FROM     Ranges r
+	CROSS JOIN dbo.Person AS p
+	INNER JOIN dbo.Timescale T ON T.TimescaleId IN ( SELECT ResultId FROM [dbo].[ConvertStringListIntoTable](@TimescaleIds))
+	INNER JOIN dbo.GetCurrentPayTypeTable() AS PCPT ON PCPT.PersonId = p.PersonId AND T.TimescaleId = PCPT.Timescale
+    LEFT JOIN dbo.Practice AS pr ON p.DefaultPractice = pr.PracticeId
+    WHERE   (p.IsStrawman = 0)
+            AND ( (@ActivePersons = 1 AND p.PersonStatusId IN (1,5)) -- active and termination pending statues
+					OR
+                    (@ProjectedPersons = 1 AND p.PersonStatusId = 3) -- projected status
+				)
+			AND (					
+					p.DefaultPractice IN (SELECT ResultId FROM [dbo].[ConvertStringListIntoTable](@PracticeIds))
+					AND (pr.IsCompanyInternal = 0 AND @ExcludeInternalPractices  = 1 OR @ExcludeInternalPractices = 0)
+				)
+),
+CurrentConsultantsWithVacationDays
+AS
+(
+	SELECT	CCWR.PersonId,
+			CCWR.StartDate,
+			CCWR.EndDate,
+			CCWR.Timescale, 
+			ISNULL(COUNT(PC.Date),0) AS VacationDays
+	FROM CurrentConsultantsWithRanges CCWR
+	LEFT JOIN dbo.v_PersonCalendar PC ON PC.PersonId = CCWR.PersonId 
+										AND	PC.Date BETWEEN CCWR.StartDate AND CCWR.EndDate 
+										AND PC.DayOff = 1 
+	GROUP BY CCWR.PersonId,CCWR.StartDate,CCWR.EndDate,CCWR.Timescale
+),
+CurrentConsultantsWithProjectedHours
+AS
+(
+ SELECT  S.PersonId,CONVERT(INT,SUM(S.HoursPerDay)) ProjectedHours,CCWV.StartDate ,CCWV.Timescale
+    FROM CurrentConsultantsWithVacationDays CCWV
+	INNER JOIN dbo.v_MilestonePersonSchedule AS S ON CCWV.PersonId = S.PersonId
+    WHERE  s.MilestoneId <> (SELECT MilestoneId FROM dbo.DefaultMilestoneSetting)
+			AND s.IsDefaultMileStone = 0
+			AND s.Date BETWEEN CCWV.startDate AND CCWV.endDate AND 
+			(@ActiveProjects = 1 AND s.ProjectStatusId = 3 OR		--  3 - Active
+			 @ProjectedProjects = 1 AND s.ProjectStatusId = 2 OR	--  2 - Projected
+			 @ExperimentalProjects = 1 AND s.ProjectStatusId = 5 OR	--  5 - Experimental
+			 @InternalProjects = 1 AND s.ProjectStatusId = 6) --6 - Internal
+	GROUP BY s.PersonId ,CCWV.StartDate ,CCWV.Timescale
+),
+CurrentConsultantsWithAvaliableHours
+AS
+(
+--Salary Persons AvaliableHours
+	SELECT PC.PersonId ,(COUNT(*) * 8) AS AvaliableHours ,CCWV.StartDate 
+	FROM CurrentConsultantsWithVacationDays CCWV
+	INNER JOIN dbo.v_PersonCalendar PC ON CCWV.PersonId = PC.PersonId 
+										AND CCWV.Timescale = 2 
+										AND PC.Date BETWEEN CCWV.StartDate AND CCWV.EndDate
+										AND PC.DayOff = 0
+	GROUP BY PC.PersonId,CCWV.StartDate 
+	UNION 
+	SELECT CCWP.PersonId,CCWP.ProjectedHours AS AvaliableHours ,CCWP.StartDate FROM  CurrentConsultantsWithProjectedHours CCWP WHERE CCWP.Timescale != 2 -- Non SalaryPerson AvaliableHours
+),
+CurrentConsultantsWithAllHours
+AS
+(
+	SELECT CCWV.PersonId,CCWV.StartDate,CCWV.EndDate,CCWV.VacationDays,ISNULL(CCWA.AvaliableHours,0) AS AvaliableHours,ISNULL(CCWP.ProjectedHours,0) AS ProjectedHours
+	FROM CurrentConsultantsWithVacationDays CCWV
+	LEFT JOIN CurrentConsultantsWithAvaliableHours CCWA ON CCWV.PersonId = CCWA.PersonId AND CCWV.StartDate = CCWA.StartDate
+	LEFT JOIN CurrentConsultantsWithProjectedHours CCWP ON CCWV.PersonId = CCWP.PersonId AND CCWV.StartDate = CCWP.StartDate
+)
+SELECT	CC.PersonId,
+		CC.StartDate,
+		CC.EndDate,
+		CC.AvaliableHours,
+		CC.ProjectedHours,
+		CASE	WHEN CC.VacationDays >= @Step
+				THEN -1
+				WHEN (
+					(@Step = 1 AND DATEPART(dw, CC.StartDate)IN(7,1)) 
+						OR
+					(CC.AvaliableHours = 0)
+					)
+				THEN 0
+				ELSE CEILING(100*CC.ProjectedHours/CC.AvaliableHours) 
+				END AS WeeklyUtlization
+FROM CurrentConsultantsWithAllHours CC
+
+
