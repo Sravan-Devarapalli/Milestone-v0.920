6db490a1f6c0fc539dd315c69955070a7bcf2fd9
SainathC <SainathC>
2012-11-15 07:46:00 -0500

    Comments - Defect Number: 3104; Defect RecordID: 3100; Server Address: localhost; Server Port: 99; Database Name: Practice Management; Defect Summary: Reports / Resources / Consulting Utilization: Utilization is calculated incorrectly before the persons re-hired dates.; Defect Description: In consulting utilization report person is considered as terminated before the re-hire dates and the utilization is calculated only from the persons re-hired date. Similarly in Consulting Capacity report person is considered as terminated before the re-hire dates.
    Steps to reproduce:
    
    1. Add a person Person A on 10/01/2012 with W2-Salary pay type.
    2. Add Person A in a project say Testing Project from 10/01/2012 to 11/30/2012 for 8 hrs/day.
    3. Run Consulting Utilization report between 10/01/2012 and 10/31/2012.
    Result: The utilization of Person A between 10/01/2012 and 10/31/2012 would be displayed as 100%.
    
    
    4. Terminate Person A on 11/01/2012.
    5. Re-hire the person on 11/04/2012 with W2-Salary pay type.
    6. Add Person A in a project Testing Project from 11/04/2012 to 11/30/2012 for 8 hrs/day.
    7. Run Consulting Utilization report between 10/01/2012 and 11/30/2012.
    Result: The utilization for Person A is considered only from 11/04/2012 and the person is considered as terminated from 10/01/2012 and 11/04/2012.
    
    
    Actual Result: Person is considered as terminated before his/her re-hire dates and the utilization is considered only from persons re-hire date.
    
    Expected Result: Utilization for a person should also be calculated before persons rehired date if applicable.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/CalendarUpdate.proc.sql
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/CalendarUpdate.proc.sql
@@ -123,40 +123,7 @@ AS
 	ELSE 
 	BEGIN 
 
-		/*
-			If we keep holiday then we need to exclude the timeOff days from the series.
-		*/
-		;WITH NeedToModifyDate AS
-		(
-			--APC:- AffectedPersonCalendar
-			--AFAPC:-  AffectedPersonCalendarForAffectedPersonCalendar
-			SELECT  PC.PersonId, APC.Date 'Date', CONVERT(BIT, 0) 'IsSeries'
-			FROM @Dates C
-			JOIN PersonCalendar PC ON C.Date = PC.Date AND PC.DayOff = 1 AND PC.IsSeries = 1
-			LEFT JOIN PersonCalendar APC ON PC.PersonId = APC.PersonId AND APC.DayOff = 1 AND APC.IsSeries = 1 AND APC.TimeTypeId = PC.TimeTypeId AND APC.ActualHours = PC.ActualHours AND ISNULL(APC.ApprovedBy, 0) = ISNULL(PC.ApprovedBy, 0)
-						AND ((DATEPART(DW, C.date) = 6 AND APC.date = DATEADD(DD,3, C.date) ) 
-								OR (DATEPART(DW, C.date) = 2 AND APC.date = DATEADD(DD, -3, C.date))
-								OR  APC.date = DATEADD(DD,1, C.date)
-								OR  APC.date = DATEADD(DD, -1, C.date)
-							)
-			LEFT JOIN PersonCalendar AFAPC ON APC.PersonId = AFAPC.PersonId AND AFAPC.DayOff = 1 AND AFAPC.IsSeries = 1 AND AFAPC.TimeTypeId = APC.TimeTypeId AND AFAPC.ActualHours = APC.ActualHours AND ISNULL(AFAPC.ApprovedBy, 0) = ISNULL(APC.ApprovedBy, 0)
-						AND ((DATEPART(DW, APC.date) = 6 AND AFAPC.date = DATEADD(DD,3, APC.date) )
-								OR (DATEPART(DW, APC.date) = 2 AND AFAPC.date = DATEADD(DD, -3, APC.date))
-								OR AFAPC.date = DATEADD(DD,1, APC.date)
-								OR AFAPC.date = DATEADD(DD, -1, APC.date)
-							)
-			GROUP BY PC.PersonId, C.date, APC.Date
-			Having COUNT(AFAPC.date) < 2
-			UNION
-			SELECT PC.PersonId, C.date 'Date', CONVERT(BIT, 0) 'IsSeries'
-			FROM @Dates C
-			JOIN PersonCalendar PC ON C.Date = PC.Date AND PC.DayOff = 1 AND PC.IsSeries = 1
-		)
-
-		UPDATE PC
-		SET IsSeries = NTMD.IsSeries
-		FROM PersonCalendar PC
-		JOIN NeedToModifyDate NTMD ON NTMD.PersonId = PC.PersonId AND NTMD.Date = PC.Date
+		
 		
 		/* Insert  Company holiday date as Any person substitute day date */
 		DECLARE @SubDatesForPersons TABLE ([SubstituteDate] DATETIME,PersonId INT,[HolidayDate] DATETIME,IsW2Salaried BIT)
