9d736d640cf76866460f01fac41be335b4b0929c
SainathC <SainathC>
2012-11-20 01:07:00 -0500

    Comments - Defect Number: 3105; Defect RecordID: 3101; Server Address: localhost; Server Port: 99; Database Name: Practice Management; Defect Summary: Person Detail > Add Person: Workflow Enhancements; Defect Description: We wish to make enhancements to the "Add Person" page and its associated tabs in order to streamline the addition of new employee records into Practice Management. We wish to create a "wizard"-type experience for users entering new person details into PM, ensuring that all required fields are filled out in totality and streamlining the data entry experience for users. One current challenge is a lack of accuracy in entering information, and many of the proposed enhancements are for the purpose of ensuring consistent and accurate entry of data. Another purpose for the desired changes is to automate some of the communications that must occur when a new person is added to the system and/or when their status changes.
    Please see the attachment for the desired enhancements and new requirements. There are also several questions to GGK embedded in the document asking about the purpose of certain fields and any impact if they are removed.

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementService/TitleService.svc.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using DataTransferObjects;
+using DataAccess;
+using System.ServiceModel.Activation;
+
+namespace PracticeManagementService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "TitleService" in code, svc and config file together.
+    [AspNetCompatibilityRequirements(RequirementsMode = AspNetCompatibilityRequirementsMode.Allowed)]
+    public class TitleService : ITitleService
+    {
+
+        public List<Title> GetAllTitles()
+        {
+            try
+            {
+                return TitleDal.GetAllTitles();
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+
+        }
+
+
+        public Title GetTitleById(int titleId)
+        {
+            try
+            {
+                return TitleDal.GetTitleById(titleId);
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+
+        }
+
+
+        public void TitleInset(string title, int titleTypeId, int sortOrder, int pTOAccural, int? minimumSalary, int? maximumSalary, string userLogin)
+        {
+            try
+            {
+                TitleDal.TitleInset(title, titleTypeId, sortOrder, pTOAccural, minimumSalary, maximumSalary, userLogin);
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+
+        }
+
+
+        public void TitleUpdate(int titleId, string title, int titleTypeId, int sortOrder, int pTOAccural, int? minimumSalary, int? maximumSalary, string userLogin)
+        {
+            try
+            {
+                TitleDal.TitleUpdate(titleId, title, titleTypeId, sortOrder, pTOAccural, minimumSalary, maximumSalary, userLogin);
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+
+        }
+
+
+        public void TitleDelete(int titleId, string userLogin)
+        {
+            try
+            {
+                TitleDal.TitleDelete(titleId, userLogin);
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+
+        }
+    }
+}
+
