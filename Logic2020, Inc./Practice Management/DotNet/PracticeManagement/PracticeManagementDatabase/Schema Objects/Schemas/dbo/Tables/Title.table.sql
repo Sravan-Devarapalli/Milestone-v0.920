8fb8ea5cc1a045ce4b5aa68fd0a7fbbdf9d7a160
SainathC <SainathC>
2012-11-20 01:12:00 -0500

    Comments - Defect Number: 3105; Defect RecordID: 3101; Server Address: localhost; Server Port: 99; Database Name: Practice Management; Defect Summary: Person Detail > Add Person: Workflow Enhancements; Defect Description: We wish to make enhancements to the "Add Person" page and its associated tabs in order to streamline the addition of new employee records into Practice Management. We wish to create a "wizard"-type experience for users entering new person details into PM, ensuring that all required fields are filled out in totality and streamlining the data entry experience for users. One current challenge is a lack of accuracy in entering information, and many of the proposed enhancements are for the purpose of ensuring consistent and accurate entry of data. Another purpose for the desired changes is to automate some of the communications that must occur when a new person is added to the system and/or when their status changes.
    Please see the attachment for the desired enhancements and new requirements. There are also several questions to GGK embedded in the document asking about the purpose of certain fields and any impact if they are removed.

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/Title.table.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[Title]
+(
+	TitleId			INT	IDENTITY (1, 1) NOT NULL,
+	Title			NVARCHAR(100) NOT NULL,
+	TitleTypeId		INT NOT NULL,
+	SortOrder		INT NOT NULL CONSTRAINT DF_Title_SortOrder DEFAULT(0),
+	PTOAccrual		INT NOT NULL CONSTRAINT DF_Title_PTOAccrual DEFAULT(0),
+	MinimumSalary	INT NULL,
+	MaximumSalary	INT NULL,
+	CONSTRAINT PK_Title_TitleId     PRIMARY KEY CLUSTERED(TitleId),
+	CONSTRAINT [UQ_Title_Title] UNIQUE NONCLUSTERED(Title) ON [PRIMARY]
+)
+
