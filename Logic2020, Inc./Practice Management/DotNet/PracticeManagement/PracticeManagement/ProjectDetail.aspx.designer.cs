13314dd0666bdaeda6730473bcb5f4fc6117d2cd
thulasiramp <thulasiramp>
2012-11-20 05:24:00 -0500

    Comments - Defect Number: 3098; Defect RecordID: 3094; Server Address: localhost; Server Port: 99; Database Name: Practice Management; Defect Summary: Project Details - expand Attachment category choices and supported file types and enable multiple files to be added at one tim@; Defect Description: On the Project Details page, we'd like to expand the list of Attachment Categories to include the following additional options: Proposal Project Estimate
    Currently the Attachment categories are SOW, MSA, and Change Request.
    
    Additionally, it appears that the system restricts the Attachment file type to either PDF or Word doc. If this is true, we'd like to expand the list of supported file types to include Excel (.xls, .xlw, .xlsx etc.), PowerPoint (.ppt, .pptx), MS Project (.mpp), Visio (.vsd), Exchange (.msg), OneNote , ZIP and/or RAR files.
    
    I'm not certain of all the applicable extensions.
    
    Finally, I'd like to have the option to add multiple files of the same type (SOW, MSA, Change Request, Proposal, Project Estimate) at one time versus one at a time.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/ProjectDetail.aspx.designer.cs
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/ProjectDetail.aspx.designer.cs
@@ -1273,15 +1273,6 @@ namespace PraticeManagement {
         protected global::System.Web.UI.WebControls.Button btnUpload;
         
         /// <summary>
-        /// hdnAttachment control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.HiddenField hdnAttachment;
-        
-        /// <summary>
         /// btnCancel control.
         /// </summary>
         /// <remarks>
