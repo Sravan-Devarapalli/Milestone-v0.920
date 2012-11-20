983de1ec6be318ebe2d0de47e3f5546db2ce438b
SainathC <SainathC>
2012-11-20 01:07:00 -0500

    Comments - Defect Number: 3105; Defect RecordID: 3101; Server Address: localhost; Server Port: 99; Database Name: Practice Management; Defect Summary: Person Detail > Add Person: Workflow Enhancements; Defect Description: We wish to make enhancements to the "Add Person" page and its associated tabs in order to streamline the addition of new employee records into Practice Management. We wish to create a "wizard"-type experience for users entering new person details into PM, ensuring that all required fields are filled out in totality and streamlining the data entry experience for users. One current challenge is a lack of accuracy in entering information, and many of the proposed enhancements are for the purpose of ensuring consistent and accurate entry of data. Another purpose for the desired changes is to automate some of the communications that must occur when a new person is added to the system and/or when their status changes.
    Please see the attachment for the desired enhancements and new requirements. There are also several questions to GGK embedded in the document asking about the purpose of certain fields and any impact if they are removed.

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/DataTransferObjects/Title.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+
+namespace DataTransferObjects
+{
+    [Serializable]
+    [DataContract]
+    public class Title
+    {
+        [DataMember]
+        public int TitleId
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public string TitleName
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public TitleType TitleType
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public int SortOrder
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public int PTOAccrual
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public int? MinimumSalary
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public int? MaximumSalary
+        {
+            get;
+            set;
+        }
+
+    }
+}
+
