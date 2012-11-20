617e2df0509e843b55aac684879bf16fc564b09e
SainathC <SainathC>
2012-11-20 01:12:00 -0500

    Comments - Defect Number: 3105; Defect RecordID: 3101; Server Address: localhost; Server Port: 99; Database Name: Practice Management; Defect Summary: Person Detail > Add Person: Workflow Enhancements; Defect Description: We wish to make enhancements to the "Add Person" page and its associated tabs in order to streamline the addition of new employee records into Practice Management. We wish to create a "wizard"-type experience for users entering new person details into PM, ensuring that all required fields are filled out in totality and streamlining the data entry experience for users. One current challenge is a lack of accuracy in entering information, and many of the proposed enhancements are for the purpose of ensuring consistent and accurate entry of data. Another purpose for the desired changes is to automate some of the communications that must occur when a new person is added to the system and/or when their status changes.
    Please see the attachment for the desired enhancements and new requirements. There are also several questions to GGK embedded in the document asking about the purpose of certain fields and any impact if they are removed.

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/TitleUpdate.proc.sql
@@ -0,0 +1,27 @@
+﻿CREATE PROCEDURE [dbo].[TitleUpdate]
+(
+	@TitleId		INT,
+	@Title			NVARCHAR(100),
+	@TitleTypeId	INT,
+	@SortOrder		INT,
+	@PTOAccrual		INT,
+	@MinimumSalary	INT,
+	@MaximumSalary	INT,
+	@UserLogin  NVARCHAR(255) 
+)
+AS
+BEGIN
+	EXEC SessionLogPrepare @UserLogin = @UserLogin
+
+	UPDATE dbo.Title
+	SET Title = @Title,
+	TitleTypeId = @TitleTypeId,
+	SortOrder = @SortOrder,
+	PTOAccrual = @PTOAccrual,
+	MinimumSalary = @MinimumSalary,
+	MaximumSalary = @MaximumSalary
+	WHERE TitleId = @TitleId
+
+	EXEC SessionLogUnprepare 
+END
+
