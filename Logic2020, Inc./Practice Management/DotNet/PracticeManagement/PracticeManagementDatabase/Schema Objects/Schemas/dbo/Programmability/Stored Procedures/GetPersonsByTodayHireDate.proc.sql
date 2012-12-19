258263e2be76ad416b0bbc5056b45c25c2479580
SainathC <SainathC>
2012-12-19 08:15:00 -0500

    Comments - Defect Number: 3105; Defect RecordID: 3101; Server Address: localhost; Server Port: 99; Database Name: Practice Management; Defect Summary: Person Detail > Add Person: Workflow Enhancements; Defect Description: We wish to make enhancements to the "Add Person" page and its associated tabs in order to streamline the addition of new employee records into Practice Management. We wish to create a "wizard"-type experience for users entering new person details into PM, ensuring that all required fields are filled out in totality and streamlining the data entry experience for users. One current challenge is a lack of accuracy in entering information, and many of the proposed enhancements are for the purpose of ensuring consistent and accurate entry of data. Another purpose for the desired changes is to automate some of the communications that must occur when a new person is added to the system and/or when their status changes.
    Please see the attachment for the desired enhancements and new requirements. There are also several questions to GGK embedded in the document asking about the purpose of certain fields and any impact if they are removed.

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/GetPersonsByTodayHireDate.proc.sql
@@ -0,0 +1,24 @@
+﻿CREATE PROCEDURE [dbo].[GetPersonsByTodayHireDate]	
+AS
+BEGIN
+	DECLARE @Today DATETIME
+	SELECT @Today = CONVERT(DATETIME,CONVERT(DATE,[dbo].[GettingPMTime](GETUTCDATE())))
+
+	--unlock out
+	UPDATE m
+	SET m.IsLockedOut = 0,
+	    m.Password = ''
+	FROM   dbo.aspnet_Users AS u
+	INNER JOIN dbo.aspnet_Applications AS a ON u.ApplicationId = a.ApplicationId
+	INNER JOIN  dbo.aspnet_Membership AS m ON u.UserId = m.UserId
+	INNER JOIN Person as P ON u.LoweredUserName = LOWER(P.Alias) 
+	WHERE P.PersonStatusId IN (1,5) AND HireDate = (@Today - 1) AND P.IsWelcomeEmailSent = 0 AND a.LoweredApplicationName =LOWER('PracticeManagement')
+
+	SELECT P.PersonId,
+	       p.FirstName,
+		   p.Alias
+	FROM  Person as P 
+	WHERE P.PersonStatusId IN (1,5) AND HireDate = (@Today - 1) AND P.IsWelcomeEmailSent = 0
+
+END
+
