d48ffbc212bf7ef1a46e1d39850ea875ec695966
SainathC <SainathC>
2012-12-19 08:17:00 -0500

    Comments - Defect Number: 3105; Defect RecordID: 3101; Server Address: localhost; Server Port: 99; Database Name: Practice Management; Defect Summary: Person Detail > Add Person: Workflow Enhancements; Defect Description: We wish to make enhancements to the "Add Person" page and its associated tabs in order to streamline the addition of new employee records into Practice Management. We wish to create a "wizard"-type experience for users entering new person details into PM, ensuring that all required fields are filled out in totality and streamlining the data entry experience for users. One current challenge is a lack of accuracy in entering information, and many of the proposed enhancements are for the purpose of ensuring consistent and accurate entry of data. Another purpose for the desired changes is to automate some of the communications that must occur when a new person is added to the system and/or when their status changes.
    Please see the attachment for the desired enhancements and new requirements. There are also several questions to GGK embedded in the document asking about the purpose of certain fields and any impact if they are removed.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementService/MailUtil.cs
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementService/MailUtil.cs
@@ -1,26 +1,45 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Net;
 using System.Net.Mail;
-using System.Configuration;
+using DataAccess;
 using DataTransferObjects;
 using Microsoft.WindowsAzure.ServiceRuntime;
-using System.Net;
-using DataAccess;
+using System.Web;
+using System.Configuration;
 
 namespace PracticeManagementService
 {
     internal static class MailUtil
     {
-        #region Constants
-
-        internal const string NewUserEmailTemplateKey = "NewUserEmailTemplate";
-        internal const string ModifiedUserEmailTemplateKey = "ModifiedUserEmailTemplate";
-        internal const string ResetPasswordEmailTemplateKey = "ResetPasswordEmailTemplate";
-        private const string TestSettingsEmailTemplateKey = "TestSettingsEmailTemplate";
-        private const string WelcomeEmailTemplateKey = "WelcomeEmailTemplate";
-
-        #endregion
-
         #region Methods
+
+        private static bool IsAzureWebRole()
+        {
+            try
+            {
+                return RoleEnvironment.IsAvailable;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        private static bool IsUAT
+        {
+            get
+            {
+                try
+                {
+                    return ConfigurationManager.AppSettings["Environment"] == "UAT";
+                }
+                catch
+                {
+                    return false;
+                }
+            }
+        }
 
         /// <summary>
         /// Sends a notification to user with his/her credentials.
@@ -28,76 +47,73 @@ namespace PracticeManagementService
         /// <param name="person"></param>
         /// <param name="password"></param>
         /// <param name="template"></param>
-        internal static void SendNotification(Person person, string password, string template)
+        internal static void SendResetPasswordNotification(Person person, string password)
         {
-            MailMessage message = new MailMessage();
+            var emailTemplate = EmailTemplateDAL.EmailTemplateGetByName(Resources.Messages.ResetPasswordEmailTemplateName);
             DateTime now = DateTime.Now;
-            message.To.Add(new MailAddress(person.Alias, string.Format("{0} {0}", person.FirstName, person.LastName)));
-            message.Subject = "Practice Management - User Account";
-            message.Body =
-                string.Format(template, person.FirstName, person.LastName, person.Alias, password, now.ToLongDateString(), now.ToShortTimeString());
-            message.IsBodyHtml = true;
+            string loginUrl = "http://practice.logic2020.com/";
+            if (IsUAT)
+            {
+                loginUrl = "http://65.52.17.100/Login.aspx";
+            }
 
-            SmtpClient client = MailUtil.GetSmtpClient();
-            var smtpSettings = SettingsHelper.GetSMTPSettings();
-            message.From = new MailAddress(smtpSettings.PMSupportEmail);
-            client.Send(message);
+            var body = string.Format(emailTemplate.Body, person.FirstName, person.LastName, person.Alias, password, now.ToLongDateString(), now.ToShortTimeString(), loginUrl);
+            Email(emailTemplate.Subject, body, true, person.Alias, string.Empty, null, false, string.Format("{0} {0}", person.FirstName, person.LastName));
         }
 
-        internal static void SendForgotPasswordNotification(string username, string password, EmailTemplate template,
-                                                            string PMLoginPageUrl, string PMChangePasswordPageUrl)
+        internal static void SendForgotPasswordNotification(string username, string password, EmailTemplate emailTemplate, string PMLoginPageUrl, string PMChangePasswordPageUrl)
         {
-            PMChangePasswordPageUrl = string.Format(PMChangePasswordPageUrl, username,
-                System.Web.HttpUtility.UrlEncode(password));
-            MailMessage message = new MailMessage();
-            DateTime now = DateTime.Now;
-            message.To.Add(new MailAddress(username));
-            message.Subject = template.Subject;
-            message.Body =
-                string.Format(template.Body, PMLoginPageUrl, PMChangePasswordPageUrl, password);
-            message.IsBodyHtml = true;
-
-            SmtpClient client = MailUtil.GetSmtpClient();
-
-            var smtpSettings = SettingsHelper.GetSMTPSettings();
-            message.From = new MailAddress(smtpSettings.PMSupportEmail);
-
-            client.Send(message);
+            PMChangePasswordPageUrl = string.Format(PMChangePasswordPageUrl, username, System.Web.HttpUtility.UrlEncode(password));
+            var body = string.Format(emailTemplate.Body, PMLoginPageUrl, PMChangePasswordPageUrl, password);
+            Email(emailTemplate.Subject, body, true, username, string.Empty, null);
         }
 
         internal static void SendWelcomeEmail(string firstName, string username, string password, string companyName, string loginPageUrl)
         {
-
             var emailTemplate = EmailTemplateDAL.EmailTemplateGetByName(Resources.Messages.WelcomeEmailTemplateName);
             var smtpSettings = SettingsHelper.GetSMTPSettings();
+            var body = string.Format(emailTemplate.Body, firstName, companyName, username, password, loginPageUrl, smtpSettings.PMSupportEmail);
+            Email(emailTemplate.Subject, body, true, username, string.Empty, null);
+        }
 
-            MailMessage message = new MailMessage();
-            DateTime now = DateTime.Now;
-            message.To.Add(new MailAddress(username));
-            message.Subject = emailTemplate.Subject;
-            message.Body = string.Format(emailTemplate.Body, firstName, companyName, username, password, loginPageUrl, smtpSettings.PMSupportEmail);
+        internal static void SendAdministratorAddedEmail(string firstName, string lastName)
+        {
+            var emailTemplate = EmailTemplateDAL.EmailTemplateGetByName(Resources.Messages.AdministratorAddedTemplateName);
+            Email(emailTemplate.Subject, string.Format(emailTemplate.Body, firstName, lastName), true, emailTemplate.EmailTemplateTo, string.Empty, null, true);
+        }
 
-            message.IsBodyHtml = true;
+        internal static void SendActivateAccountEmail(string firstName, string lastName, string startDate, string emailAddress, string title, string payType, string telephoneNumber)
+        {
+            var emailTemplate = EmailTemplateDAL.EmailTemplateGetByName(Resources.Messages.ActivateAccountTemplateName);
+            var body = string.Format(emailTemplate.Body, firstName, lastName, startDate, emailAddress, title, payType, telephoneNumber);
+            Email(emailTemplate.Subject, body, true, emailTemplate.EmailTemplateTo, string.Empty, null);
+        }
 
-            SmtpClient client = MailUtil.GetSmtpClient();
+        internal static void SendHireDateChangedEmail(string firstName, string lastName, string oldHireDate, string newHireDate, string emailAddress, string title, string payType, string telephoneNumber)
+        {
+            var emailTemplate = EmailTemplateDAL.EmailTemplateGetByName(Resources.Messages.HireDateChangedTemplateName);
+            var body = string.Format(emailTemplate.Body, firstName, lastName, oldHireDate, newHireDate, emailAddress, title, payType, telephoneNumber);
+            Email(emailTemplate.Subject, body, true, emailTemplate.EmailTemplateTo, string.Empty, null);
+        }
 
-            message.From = new MailAddress(smtpSettings.PMSupportEmail);
-            client.Send(message);
+        internal static void SendDeactivateAccountEmail(string firstName, string lastName, string date)
+        {
+            var emailTemplate = EmailTemplateDAL.EmailTemplateGetByName(Resources.Messages.DeActivateAccountTemplateName);
+            var body = string.Format(emailTemplate.Body, firstName, lastName, date);
+            Email(emailTemplate.Subject, body, true, emailTemplate.EmailTemplateTo, string.Empty, null);
         }
 
         internal static void SendLockedOutNotificationEmail(string userName, string loginPageUrl)
         {
             var emailTemplate = EmailTemplateDAL.EmailTemplateGetByName(Resources.Messages.LockedOutEmailTemplateName);
-            var smtpSettings = SettingsHelper.GetSMTPSettings();
             var companyName = ConfigurationDAL.GetCompanyName();
-
             var lockedOutMinitues = SettingsHelper.GetResourceValueByTypeAndKey(SettingsType.Application, Constants.ResourceKeys.UnlockUserMinituesKey);
             string output = string.Empty;
             int mins = 30;
             try
             {
                 mins = Convert.ToInt32(lockedOutMinitues);
-                output = lockedOutMinitues + " minitue(s)";
+                output = lockedOutMinitues + " minute(s)";
                 if (mins > 59)
                 {
                     output = (mins / 60).ToString() + " hour(s)";
@@ -109,91 +125,25 @@ namespace PracticeManagementService
             }
             catch
             {
-                output = "30 minitue(s)";
+                output = "30 minute(s)";
             }
-
-            MailMessage message = new MailMessage();
-            DateTime now = DateTime.Now;
-            message.To.Add(new MailAddress(userName));
-            if (!string.IsNullOrEmpty(emailTemplate.EmailTemplateCc))
-            {
-                message.CC.Add(emailTemplate.EmailTemplateCc);
-            }
-
-            message.Subject = string.Format(emailTemplate.Subject, companyName);
-            message.Body = string.Format(emailTemplate.Body, output);
-
-            message.IsBodyHtml = true;
-
-            SmtpClient client = MailUtil.GetSmtpClient();
-
-            message.From = new MailAddress(smtpSettings.PMSupportEmail);
-            client.Send(message);
-        }
-
-        private static bool IsSSLEnabled()
-        {
-            bool enableSSL = false;
-
-            try
-            {
-                if (IsAzureWebRole())
-                {
-                    if (Boolean.TryParse(RoleEnvironment.GetConfigurationSettingValue("EnableSSL"), out enableSSL))
-                    {
-                        return enableSSL;
-                    }
-                    return enableSSL;
-                }
-                else
-                {
-                    return Convert.ToBoolean(ConfigurationManager.AppSettings.Get("EnableSSL"));
-                }
-            }
-            catch
-            {
-                return enableSSL;
-            }
-        }
-
-        private static Boolean IsAzureWebRole()
-        {
-            try
-            {
-                if (RoleEnvironment.IsAvailable)
-                {
-                    return true;
-                }
-                return false;
-            }
-            catch
-            {
-                return false;
-            }
+            var body = string.Format(emailTemplate.Body, output);
+            Email(string.Format(emailTemplate.Subject, companyName), body, true, userName, string.Empty, null, false, null, emailTemplate.EmailTemplateCc);
         }
 
         public static bool VerifySMTPSettings(string mailServer, int portNumber, bool sSLEnabled, string userName, string password, string pMSupportEmail)
         {
-            var emailTemplate = EmailTemplateDAL.EmailTemplateGetByName(TestSettingsEmailTemplateKey);
-
-            MailMessage message = new MailMessage();
-            message.To.Add(new MailAddress(pMSupportEmail));
-            message.Subject = emailTemplate.Subject;
-            message.Body = emailTemplate.Body;
-            message.IsBodyHtml = true;
-
-            SmtpClient client = new SmtpClient(mailServer, portNumber);
-            client.EnableSsl = sSLEnabled;
-            client.Credentials = new NetworkCredential(userName, password);
-            message.From = new MailAddress(userName);
-            client.Send(message);
-
+            var emailTemplate = EmailTemplateDAL.EmailTemplateGetByName(Resources.Messages.TestSettingsEmailTemplateName);
+            Email(emailTemplate.Subject, emailTemplate.Body, true, pMSupportEmail, string.Empty, null);
             return true;
         }
 
-        public static SmtpClient GetSmtpClient()
+        public static SmtpClient GetSmtpClient(SMTPSettings smtpSettings = null)
         {
-            var smtpSettings = SettingsHelper.GetSMTPSettings();
+            if (smtpSettings == null)
+            {
+                smtpSettings = SettingsHelper.GetSMTPSettings();
+            }
 
             SmtpClient client = new SmtpClient(smtpSettings.MailServer, smtpSettings.PortNumber);
             client.EnableSsl = smtpSettings.SSLEnabled;
@@ -202,9 +152,85 @@ namespace PracticeManagementService
             return client;
         }
 
+        /// <summary>
+        /// This will send an email from PMSupportEmail address with the given subject, body, isbodyHtml, comma seperated To addresses, Comma seperated Bcc Addresses and Any attachments.
+        /// </summary>
+        /// <param name="subject"></param>
+        /// <param name="body"></param>
+        /// <param name="isBodyHtml"></param>
+        /// <param name="commaSeperatedToAddresses"></param>
+        /// <param name="commaSeperatedBccAddresses"></param>
+        /// <param name="attachments"></param>
+        public static void Email(string subject, string body, bool isBodyHtml, string commaSeperatedToAddresses, string commaSeperatedBccAddresses, List<Attachment> attachments, bool isHighPriority = false, string commaSeperatedToAddressesDisplayNames = "", string commaSeperatedCCAddresses = "")
+        {
+            var smtpSettings = SettingsHelper.GetSMTPSettings();
+
+            MailMessage message = new MailMessage();
+            message.Priority = isHighPriority ? MailPriority.High : MailPriority.Normal;
+            var addresses = commaSeperatedToAddresses.Split(',');
+            string[] addressesDisplayName = null;
+            addressesDisplayName = !string.IsNullOrEmpty(commaSeperatedToAddressesDisplayNames) ? commaSeperatedToAddressesDisplayNames.Split(',') : addresses;
+            addressesDisplayName = !string.IsNullOrEmpty(commaSeperatedToAddressesDisplayNames) && addressesDisplayName.Length == addresses.Length ? addressesDisplayName : addresses;
+
+            for (int i = 0; i < addresses.Length; i++)
+            {
+                message.To.Add(new MailAddress(addresses[i], addressesDisplayName[i]));
+            }
+
+            if (!string.IsNullOrEmpty(commaSeperatedBccAddresses))
+            {
+                var bccAddresses = commaSeperatedBccAddresses.Split(',');
+                foreach (var address in bccAddresses)
+                {
+                    message.Bcc.Add(new MailAddress(address));
+                }
+            }
+
+            if (!string.IsNullOrEmpty(commaSeperatedCCAddresses))
+            {
+                var ccAddresses = commaSeperatedCCAddresses.Split(',');
+                foreach (var address in ccAddresses)
+                {
+                    message.CC.Add(new MailAddress(address));
+                }
+            }
+            try
+            {
+                if (!IsUAT)
+                {
+                    message.Subject = subject;
+                }
+                else
+                {
+                    message.Subject = "(UAT) " + subject;
+                }
+            }
+            catch (Exception e)
+            {
+                string logData = string.Format(Constants.Formatting.ErrorLogMessage, message.Subject, "MailUtils", string.Empty,
+                   HttpUtility.HtmlEncode(e.Message), e.Source, e.InnerException == null ? string.Empty : HttpUtility.HtmlEncode(e.InnerException.Message), e.InnerException == null ? string.Empty : e.InnerException.Source);
+                ActivityLogDAL.ActivityLogInsert(20, logData);
+            }
+
+            message.Body = body;
+
+            message.IsBodyHtml = isBodyHtml;
+
+            if (attachments != null && attachments.Count > 0)
+            {
+                foreach (var item in attachments)
+                {
+                    message.Attachments.Add(item);
+                }
+            }
+
+            SmtpClient client = MailUtil.GetSmtpClient(smtpSettings);
+
+            message.From = new MailAddress(smtpSettings.PMSupportEmail);
+            client.Send(message);
+        }
+
         #endregion
-
-
     }
 }
 
