f2a719f750a4fbca9c3f3d19af7a2de0878a7393
SainathC <SainathC>
2012-12-19 08:17:00 -0500

    Comments - Defect Number: 3105; Defect RecordID: 3101; Server Address: localhost; Server Port: 99; Database Name: Practice Management; Defect Summary: Person Detail > Add Person: Workflow Enhancements; Defect Description: We wish to make enhancements to the "Add Person" page and its associated tabs in order to streamline the addition of new employee records into Practice Management. We wish to create a "wizard"-type experience for users entering new person details into PM, ensuring that all required fields are filled out in totality and streamlining the data entry experience for users. One current challenge is a lack of accuracy in entering information, and many of the proposed enhancements are for the purpose of ensuring consistent and accurate entry of data. Another purpose for the desired changes is to automate some of the communications that must occur when a new person is added to the system and/or when their status changes.
    Please see the attachment for the desired enhancements and new requirements. There are also several questions to GGK embedded in the document asking about the purpose of certain fields and any impact if they are removed.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/PayGetHistoryByPerson.proc.sql
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/PayGetHistoryByPerson.proc.sql
@@ -19,21 +19,19 @@ AS
 	       p.Amount,
 	       p.Timescale,
 	       p.AmountHourly,
-	       p.TimesPaidPerMonth,
-	       p.Terms,
 	       p.VacationDays,
 	       p.BonusAmount,
 	       p.BonusHoursToCollect,
 	       p.IsYearBonus,
-	       p.DefaultHoursPerDay,
 	       p.TimescaleName,
-		   p.SeniorityId,
-		   s.Name SeniorityName,
 		   p.PracticeId,
 		   pr.Name PracticeName,
-		   p.SalesCommissionFractionOfMargin
+		   Tt.TitleId,
+		   Tt.Title,
+		   p.SLTApproval,
+		   p.SLTPTOApproval
 	  FROM dbo.v_Pay AS p
-	  LEFT JOIN dbo.Seniority s ON s.SeniorityId = p.SeniorityId
+	  INNER JOIN dbo.Title Tt ON Tt.TitleId = P.TitleId
 	  LEFT JOIN dbo.Practice pr on pr.PracticeId = p.PracticeId
 	 WHERE p.PersonId = @PersonId
 	ORDER BY p.StartDate
