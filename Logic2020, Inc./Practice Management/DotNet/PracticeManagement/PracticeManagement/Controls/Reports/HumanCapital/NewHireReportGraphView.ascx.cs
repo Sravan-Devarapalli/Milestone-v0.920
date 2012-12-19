86782cf1798ab4f34283cdd4f61e776616c16121
SainathC <SainathC>
2012-12-19 08:17:00 -0500

    Comments - Defect Number: 3105; Defect RecordID: 3101; Server Address: localhost; Server Port: 99; Database Name: Practice Management; Defect Summary: Person Detail > Add Person: Workflow Enhancements; Defect Description: We wish to make enhancements to the "Add Person" page and its associated tabs in order to streamline the addition of new employee records into Practice Management. We wish to create a "wizard"-type experience for users entering new person details into PM, ensuring that all required fields are filled out in totality and streamlining the data entry experience for users. One current challenge is a lack of accuracy in entering information, and many of the proposed enhancements are for the purpose of ensuring consistent and accurate entry of data. Another purpose for the desired changes is to automate some of the communications that must occur when a new person is added to the system and/or when their status changes.
    Please see the attachment for the desired enhancements and new requirements. There are also several questions to GGK embedded in the document asking about the purpose of certain fields and any impact if they are removed.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/Controls/Reports/HumanCapital/NewHireReportGraphView.ascx.cs
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/Controls/Reports/HumanCapital/NewHireReportGraphView.ascx.cs
@@ -1,13 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
-using System.Web;
-using System.Web.UI;
+using System.Web.UI.DataVisualization.Charting;
 using System.Web.UI.WebControls;
 using DataTransferObjects;
-using System.Web.UI.DataVisualization.Charting;
-using PraticeManagement.Utils;
-using System.Drawing;
+using System.Web.UI;
 
 namespace PraticeManagement.Controls.Reports.HumanCapital
 {
@@ -16,9 +14,11 @@ namespace PraticeManagement.Controls.Reports.HumanCapital
         #region constant
 
         private const string MAIN_CHART_AREA_NAME = "MainArea";
-        public const string SeeNewHiresbySeniority = "See New Hires by Seniority";
+        public const string SeeNewHiresbyTitle = "See New Hires by Title";
         public const string SeeNewHiresbyRecruiter = "See New Hires by Recruiter";
-
+        private const int TitleLabelRow = 0;
+        private const int TitleTypeLabelRow = 1;
+        private const int TitleLabelLength = 20;
         #endregion
 
         #region properties
@@ -28,21 +28,21 @@ namespace PraticeManagement.Controls.Reports.HumanCapital
             get { return ((PraticeManagement.Reporting.NewHireReport)Page); }
         }
 
-        private Dictionary<string, int> Seniorities { get; set; }
+        private Dictionary<string, int> Titles { get; set; }
 
         private Dictionary<string, int> Recruiters { get; set; }
 
-        private List<SeniorityCategory> SeniorityCategoryList { get; set; }
+        private List<TitleType> TitleTypeList { get; set; }
 
-        private List<Seniority> SeniorityList { get; set; }
+        private List<DataTransferObjects.Title> TitleList { get; set; }
 
         private List<Person> RecuriterList { get; set; }
 
-        private bool IsSeniorityGraph
+        private bool IsTitleGraph
         {
             get
             {
-                if (hlnkGraph.Text == SeeNewHiresbySeniority)
+                if (hlnkGraph.Text == SeeNewHiresbyTitle)
                 {
                     return false;
                 }
@@ -63,13 +63,13 @@ namespace PraticeManagement.Controls.Reports.HumanCapital
 
         protected void hlnkGraph_Click(object sender, EventArgs e)
         {
-            if (hlnkGraph.Text == SeeNewHiresbySeniority)
+            if (hlnkGraph.Text == SeeNewHiresbyTitle)
             {
                 hlnkGraph.Text = SeeNewHiresbyRecruiter;
             }
             else
             {
-                hlnkGraph.Text = SeeNewHiresbySeniority;
+                hlnkGraph.Text = SeeNewHiresbyTitle;
             }
             PopulateGraph();
         }
@@ -90,20 +90,20 @@ namespace PraticeManagement.Controls.Reports.HumanCapital
             string selectedValue = postBackDetails[0].Trim();
             lbTotalHires.Text = postBackDetails[1];
             var data = ServiceCallers.Custom.Report(r => r.NewHireReport(HostingPage.StartDate.Value, HostingPage.EndDate.Value, HostingPage.PersonStatus, HostingPage.PayTypes, HostingPage.Practices, HostingPage.ExcludeInternalProjects, null, null, null, null)).ToList();
-            bool isSeniority;
-            Boolean.TryParse(postBackDetails[2], out isSeniority);
-            if (isSeniority)
+            bool isTitle;
+            Boolean.TryParse(postBackDetails[2], out isTitle);
+            if (isTitle)
             {
-                lbName.Text = "Seniority: " + selectedValue;
-                data = data.Where(p => p.Seniority != null ? p.Seniority.Name == selectedValue : selectedValue == Constants.FilterKeys.Unassigned).ToList();
+                lbName.Text = "Title: " + selectedValue;
+                data = data.Where(p => p.Title != null ? p.Title.TitleName == selectedValue : selectedValue == Constants.FilterKeys.Unassigned).ToList();
             }
             else
             {
                 lbName.Text = "Recruiter: " + selectedValue;
                 bool isUnassigned = selectedValue.Equals(Constants.FilterKeys.Unassigned);
-                data = data.Where(p => p.RecruiterCommission.Any() ? p.RecruiterCommission.First().Recruiter.PersonLastFirstName == selectedValue : isUnassigned).ToList();
+                data = data.Where(p => p.RecruiterId.HasValue ? p.RecruiterLastFirstName == selectedValue : isUnassigned).ToList();
             }
-            tpSummary.BtnExportToExcelButton.Attributes["IsSeniority"] = isSeniority.ToString();
+            tpSummary.BtnExportToExcelButton.Attributes["IsTitle"] = isTitle.ToString();
             tpSummary.BtnExportToExcelButton.Attributes["FilterValue"] = selectedValue;
             tpSummary.PopUpFilteredPerson = data;
             tpSummary.PopulateData(true);
@@ -116,6 +116,7 @@ namespace PraticeManagement.Controls.Reports.HumanCapital
 
         public void PopulateGraph()
         {
+
             List<Person> data = ServiceCallers.Custom.Report(r => r.NewHireReport(HostingPage.StartDate.Value, HostingPage.EndDate.Value, HostingPage.PersonStatus, HostingPage.PayTypes, HostingPage.Practices, HostingPage.ExcludeInternalProjects, null, null, null, null)).ToList();
             HostingPage.PopulateHeaderSection(data);
             if (data.Count > 0)
@@ -134,37 +135,27 @@ namespace PraticeManagement.Controls.Reports.HumanCapital
 
         private void PopulateGraphAxisData(List<Person> data)
         {
-            if (IsSeniorityGraph)
+            if (IsTitleGraph)
             {
-
-                SeniorityCategoryList = ServiceCallers.Custom.Person(p => p.ListAllSeniorityCategories()).OrderBy(s => s.Id).ToList();
-                SeniorityList = ServiceCallers.Custom.Person(p => p.ListSeniorities()).OrderBy(s => s.SeniorityCategory.Id).ThenByDescending(s=>s.SeniorityValue).ToList();
-                if (data.Any(p => p.Seniority == null))
+                TitleTypeList = ServiceCallers.Custom.Title(p => p.GetTitleTypes()).OrderBy(s => s.TitleTypeId).ToList();
+                TitleList = ServiceCallers.Custom.Title(p => p.GetAllTitles()).OrderBy(s => s.TitleType.TitleTypeId).ThenBy(s => s.SortOrder).ToList();
+                if (data.Any(p => p.Title == null))
                 {
-                    SeniorityCategoryList.Add(new SeniorityCategory() { Id = 0, Name = Constants.FilterKeys.Unassigned });
-                    SeniorityList.Add(new Seniority { Id = 0, Name = Constants.FilterKeys.Unassigned, SeniorityCategory = new SeniorityCategory() { Id = 0, Name = Constants.FilterKeys.Unassigned } });
+                    TitleTypeList.Add(new TitleType() { TitleTypeId = 0, TitleTypeName = Constants.FilterKeys.Unassigned });
+                    TitleList.Add(new DataTransferObjects.Title { TitleId = 0, TitleName = Constants.FilterKeys.Unassigned, TitleType = new TitleType() { TitleTypeId = 0, TitleTypeName = Constants.FilterKeys.Unassigned } });
                 }
-                if (Seniorities == null)
-                    Seniorities = new Dictionary<string, int>();
-                foreach (var S in SeniorityList)
+                if (Titles == null)
+                    Titles = new Dictionary<string, int>();
+                foreach (var S in TitleList)
                 {
-                    int count = data.Count(p => p.Seniority != null ? p.Seniority.Id == S.Id : S.Id == 0);
-                    Seniorities.Add(S.Name, count);
+                    int count = data.Count(p => p.Title != null ? p.Title.TitleId == S.TitleId : S.TitleId == 0);
+                    Titles.Add(S.TitleName, count);
                 }
-
-                int startPos = 1;
-                foreach (var sc in SeniorityCategoryList)
-                {
-                    sc.StartPosition = startPos;
-                    sc.EndPosition = startPos + SeniorityList.Count(p => p.SeniorityCategory.Id == sc.Id) - 1;
-                    startPos = sc.EndPosition + 1;
-                }
-
             }
             else
             {
                 RecuriterList = ServiceCallers.Custom.Person(p => p.GetPersonListWithRole("Recruiter")).ToList();
-                List<Person> _recurterList = data.Select(p => p.RecruiterCommission.Any() ? p.RecruiterCommission.First().Recruiter : new Person { LastName = Constants.FilterKeys.Unassigned, Id = 0 }).Distinct().ToList();
+                List<Person> _recurterList = data.Select(p => p.RecruiterId.HasValue ? new Person { LastName = p.RecruiterLastName, FirstName = p.RecruiterFirstName, Id = p.RecruiterId.Value } : new Person { LastName = Constants.FilterKeys.Unassigned, Id = 0 }).Distinct().ToList();
                 if (_recurterList.Count > 0)
                 {
                     RecuriterList = RecuriterList.Concat(_recurterList).Distinct().ToList();
@@ -174,7 +165,7 @@ namespace PraticeManagement.Controls.Reports.HumanCapital
                     Recruiters = new Dictionary<string, int>();
                 foreach (var R in RecuriterList)
                 {
-                    int count = data.Count(p => p.RecruiterCommission.Any() ? p.RecruiterCommission.First().Recruiter.Id.Value == R.Id.Value : R.Id.Value == 0);
+                    int count = data.Count(p => p.RecruiterId.HasValue ? p.RecruiterId.Value == R.Id.Value : R.Id.Value == 0);
                     Recruiters.Add(R.Id == 0 ? R.LastName : R.PersonLastFirstName, count);
                 }
 
@@ -183,50 +174,90 @@ namespace PraticeManagement.Controls.Reports.HumanCapital
 
         private void LoadChartData(List<Person> data)
         {
-            if (IsSeniorityGraph)
+            if (IsTitleGraph)
             {
-                var seniorityList = Seniorities.Select(p => new { name = p.Key, count = p.Value }).ToList();
-                chrtNewHireReportBySeniority.Visible = true;
+                var titleList = Titles.Select(p => new { name = p.Key, count = p.Value }).ToList();
+                chrtNewHireReportByTitle.Visible = true;
                 chrtNewHireReportByRecruiter.Visible = false;
-                foreach (var sc in SeniorityCategoryList)
+                chrtNewHireReportByTitle.ChartAreas[MAIN_CHART_AREA_NAME].AxisX.CustomLabels.Clear();
+
+                int startPosTitleType = 1;
+                foreach (var sc in TitleTypeList)
                 {
-                    CustomLabel sCLabel = new CustomLabel(sc.StartPosition, sc.EndPosition, sc.Name, 1, LabelMarkStyle.LineSideMark, GridTickTypes.Gridline);
-                    sCLabel.ToolTip = sc.Name;
-                    sCLabel.MarkColor = Color.Black;
-                    sCLabel.ForeColor = Color.Black; 
-                    chrtNewHireReportBySeniority.ChartAreas[MAIN_CHART_AREA_NAME].AxisX.CustomLabels.Add(sCLabel);
+                    sc.StartPosition = startPosTitleType;
+                    sc.EndPosition = startPosTitleType + TitleList.Count(p => p.TitleType.TitleTypeId == sc.TitleTypeId) - 1;
+
+                    AddCustomLabelToAxis(sc.StartPosition, sc.EndPosition, sc.TitleTypeName, sc.TitleTypeName, TitleTypeLabelRow, LabelMarkStyle.LineSideMark, GridTickTypes.Gridline,
+                        chrtNewHireReportByTitle.ChartAreas[MAIN_CHART_AREA_NAME].AxisX);
+                    startPosTitleType = sc.EndPosition + 1;
+
                 }
-                chrtNewHireReportBySeniority.DataSource = seniorityList;
-                chrtNewHireReportBySeniority.DataBind();
+
+                double startPosTitle = 0.5;
+                foreach (var t in TitleList)
+                {
+                    AddCustomLabelToAxis(startPosTitle, startPosTitle + 1, GetSubString(t.TitleName), t.TitleName, TitleLabelRow, LabelMarkStyle.None, GridTickTypes.None,
+                        chrtNewHireReportByTitle.ChartAreas[MAIN_CHART_AREA_NAME].AxisX);
+                    startPosTitle++;
+
+                }
+                chrtNewHireReportByTitle.DataSource = titleList;
+                chrtNewHireReportByTitle.DataBind();
             }
             else
             {
                 var recruiterList = Recruiters.Select(p => new { name = p.Key, count = p.Value }).ToList();
                 recruiterList = recruiterList.OrderBy(p => p.name).ToList();
-                chrtNewHireReportBySeniority.Visible = false;
+                chrtNewHireReportByTitle.Visible = false;
                 chrtNewHireReportByRecruiter.Visible = true;
+                chrtNewHireReportByRecruiter.ChartAreas[MAIN_CHART_AREA_NAME].AxisX.CustomLabels.Clear();
+
+                double startPosRecruiter = 0.5;
+                foreach (var r in recruiterList)
+                {
+                    var name = r.name;
+                    AddCustomLabelToAxis(startPosRecruiter, startPosRecruiter + 1, GetSubString(name), name, TitleLabelRow, LabelMarkStyle.None, GridTickTypes.None,
+                        chrtNewHireReportByRecruiter.ChartAreas[MAIN_CHART_AREA_NAME].AxisX);
+                    startPosRecruiter++;
+
+                }
                 chrtNewHireReportByRecruiter.DataSource = recruiterList;
                 chrtNewHireReportByRecruiter.DataBind();
             }
             InitChart();
         }
 
-        private string GetSeniorityCategory(int i)
+        private void AddCustomLabelToAxis(double startPosition, double endPosition, string text, string toolTip, int labelRow, LabelMarkStyle labelMarkStyle, GridTickTypes gridTickType, Axis axis)
         {
-            string seniorityCategory = string.Empty;
-            foreach (var sc in SeniorityCategoryList)
+            CustomLabel customLabel = new CustomLabel(startPosition, endPosition, text, labelRow, labelMarkStyle, gridTickType);
+            customLabel.ToolTip = toolTip;
+            customLabel.MarkColor = Color.Black;
+            customLabel.ForeColor = Color.Black;
+            axis.CustomLabels.Add(customLabel);
+            axis.LabelStyle.Font = new Font("Arial", 10, FontStyle.Regular);
+        }
+
+        private string GetSubString(string text)
+        {
+            return (text.Length > TitleLabelLength) ? text.Substring(0, TitleLabelLength) + "..." : text;
+        }
+
+        private string GetTitleType(int i)
+        {
+            string titleType = string.Empty;
+            foreach (var sc in TitleTypeList)
             {
                 if (i >= sc.StartPosition && i <= sc.EndPosition)
                 {
-                    return sc.Name;
+                    return sc.TitleTypeName;
                 }
             }
-            return seniorityCategory;
+            return titleType;
         }
 
-        private Color GetSeniorityCategoryColor(string sc)
+        private Color GetTitleTypeColor(string sc)
         {
-            if (sc == "Business Track")
+            if (sc == "Business")
             {
                 return Color.FromArgb(59, 100, 150);
             }
@@ -234,7 +265,7 @@ namespace PraticeManagement.Controls.Reports.HumanCapital
             {
                 return Color.MediumTurquoise;
             }
-            else if (sc == "Technical Track")
+            else if (sc == "Technology")
             {
                 return Color.FromArgb(59, 148, 237);
             }
@@ -246,24 +277,31 @@ namespace PraticeManagement.Controls.Reports.HumanCapital
 
         private void InitChart()
         {
-            if (IsSeniorityGraph)
+            if (IsTitleGraph)
             {
-                for (int i = 0; i < chrtNewHireReportBySeniority.Series[0].Points.Count; i++)
+                for (int i = 0; i < chrtNewHireReportByTitle.Series[0].Points.Count; i++)
                 {
-                    var point = chrtNewHireReportBySeniority.Series[0].Points[i];
-                    string sc = GetSeniorityCategory(i + 1);
-                    point.Color = GetSeniorityCategoryColor(sc);
+                    var point = chrtNewHireReportByTitle.Series[0].Points[i];
+                    string sc = GetTitleType(i + 1);
+                    point.Color = GetTitleTypeColor(sc);
                 }
 
-                chrtNewHireReportBySeniority.Width = Seniorities.Count * 70 < 400 ? 400 : Seniorities.Count * 70;
-                chrtNewHireReportBySeniority.Height = 600;
-                InitAxis(chrtNewHireReportBySeniority.ChartAreas[MAIN_CHART_AREA_NAME].AxisX, "Seniority", false);
-                InitAxis(chrtNewHireReportBySeniority.ChartAreas[MAIN_CHART_AREA_NAME].AxisY, "Number of New Hires", true);
+                var pointWidth = (Titles.Count > 25) ? 70 - (Titles.Count - 25) * 5 : 70;
+                pointWidth = pointWidth < 20 ? 20 : pointWidth;
+                int width = Titles.Count * pointWidth < 400 ? 400 : Titles.Count * pointWidth;
+                chrtNewHireReportByTitle.Width = width;// > HostingPage.TotalPageWidth - 150 ? HostingPage.TotalPageWidth - 150 : width;
+                chrtNewHireReportByTitle.Height = (Titles.Max(c => c.Value) * 9 < 800) ? 800 : Titles.Max(c => c.Value) * 9;
+                InitAxis(chrtNewHireReportByTitle.ChartAreas[MAIN_CHART_AREA_NAME].AxisX, "Title", false);
+                InitAxis(chrtNewHireReportByTitle.ChartAreas[MAIN_CHART_AREA_NAME].AxisY, "Number of New Hires", true);
+
             }
             else
             {
-                chrtNewHireReportByRecruiter.Width = Recruiters.Count * 70;
-                chrtNewHireReportByRecruiter.Height = 600;
+                var pointWidth = (Recruiters.Count > 25) ? 70 - (Recruiters.Count - 25) * 5 : 70;
+                pointWidth = pointWidth < 20 ? 20 : pointWidth;
+                int width = Recruiters.Count * pointWidth < 400 ? 400 : Recruiters.Count * pointWidth;
+                chrtNewHireReportByRecruiter.Width = width ;//> HostingPage.TotalPageWidth - 150 ? HostingPage.TotalPageWidth - 150 : width;
+                chrtNewHireReportByRecruiter.Height = (Recruiters.Max(c => c.Value) * 9 < 800) ? 800 : Recruiters.Max(c => c.Value) * 9;
                 InitAxis(chrtNewHireReportByRecruiter.ChartAreas[MAIN_CHART_AREA_NAME].AxisX, "Recruiter", false);
                 InitAxis(chrtNewHireReportByRecruiter.ChartAreas[MAIN_CHART_AREA_NAME].AxisY, "Number of New Hires", true);
             }
@@ -272,13 +310,13 @@ namespace PraticeManagement.Controls.Reports.HumanCapital
 
         private void UpdateChartTitle()
         {
-            if (IsSeniorityGraph)
+            if (IsTitleGraph)
             {
-                chrtNewHireReportBySeniority.Titles.Clear();
-                chrtNewHireReportBySeniority.Titles.Add("New Hires By Seniority");
-                chrtNewHireReportBySeniority.Titles.Add(HostingPage.GraphRange);
-                chrtNewHireReportBySeniority.Titles[0].Font =
-                chrtNewHireReportBySeniority.Titles[1].Font = new Font("Arial", 16, FontStyle.Bold);
+                chrtNewHireReportByTitle.Titles.Clear();
+                chrtNewHireReportByTitle.Titles.Add("New Hires By Title");
+                chrtNewHireReportByTitle.Titles.Add(HostingPage.GraphRange);
+                chrtNewHireReportByTitle.Titles[0].Font =
+                chrtNewHireReportByTitle.Titles[1].Font = new Font("Arial", 16, FontStyle.Bold);
             }
             else
             {
