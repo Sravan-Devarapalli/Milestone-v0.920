df8546aff31b13c93e0b980853f3bb16e2a23fd2
SainathC <SainathC>
2012-12-19 08:17:00 -0500

    Comments - Defect Number: 3105; Defect RecordID: 3101; Server Address: localhost; Server Port: 99; Database Name: Practice Management; Defect Summary: Person Detail > Add Person: Workflow Enhancements; Defect Description: We wish to make enhancements to the "Add Person" page and its associated tabs in order to streamline the addition of new employee records into Practice Management. We wish to create a "wizard"-type experience for users entering new person details into PM, ensuring that all required fields are filled out in totality and streamlining the data entry experience for users. One current challenge is a lack of accuracy in entering information, and many of the proposed enhancements are for the purpose of ensuring consistent and accurate entry of data. Another purpose for the desired changes is to automate some of the communications that must occur when a new person is added to the system and/or when their status changes.
    Please see the attachment for the desired enhancements and new requirements. There are also several questions to GGK embedded in the document asking about the purpose of certain fields and any impact if they are removed.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/GetAllTitles.proc.sql
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/GetAllTitles.proc.sql
@@ -2,9 +2,22 @@
 AS
 BEGIN
 
-	SELECT T.TitleId,T.Title,TT.TitleTypeId,TT.TitleType,T.SortOrder,T.PTOAccrual,T.MinimumSalary,T.MaximumSalary
+	SELECT T.TitleId
+	 , T.Title
+	 , TT.TitleTypeId
+	 , TT.TitleType
+	 , TT.SortOrder AS TitleTypeSortOrder
+	 , T.SortOrder
+	 , T.PTOAccrual
+	 , T.MinimumSalary
+	 , T.MaximumSalary
+	 , CASE WHEN EXISTS (SELECT 1 FROM Person AS p WHERE p.TitleId = T.TitleId ) OR
+				 EXISTS(SELECT 1 FROM Pay AS pa	WHERE pa.TitleId = T.TitleId) 
+			THEN CAST(1 AS BIT)				   	 
+			ELSE CAST(0 AS BIT)
+	   END AS [TitleInUse]
 	FROM dbo.Title T
-	INNER JOIN dbo.TitleType TT ON T.TitleTypeId = TT.TitleTypeId
-	ORDER BY T.SortOrder,T.Title
-
+		INNER JOIN dbo.TitleType TT ON T.TitleTypeId = TT.TitleTypeId
+	ORDER BY TT.SortOrder,T.SortOrder
 END
+
