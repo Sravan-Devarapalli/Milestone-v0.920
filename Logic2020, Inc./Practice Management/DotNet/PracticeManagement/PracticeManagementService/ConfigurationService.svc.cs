0697005f82ee8913920123d31b024b333bd1dd40
SainathC <SainathC>
2012-12-19 08:17:00 -0500

    Comments - Defect Number: 3105; Defect RecordID: 3101; Server Address: localhost; Server Port: 99; Database Name: Practice Management; Defect Summary: Person Detail > Add Person: Workflow Enhancements; Defect Description: We wish to make enhancements to the "Add Person" page and its associated tabs in order to streamline the addition of new employee records into Practice Management. We wish to create a "wizard"-type experience for users entering new person details into PM, ensuring that all required fields are filled out in totality and streamlining the data entry experience for users. One current challenge is a lack of accuracy in entering information, and many of the proposed enhancements are for the purpose of ensuring consistent and accurate entry of data. Another purpose for the desired changes is to automate some of the communications that must occur when a new person is added to the system and/or when their status changes.
    Please see the attachment for the desired enhancements and new requirements. There are also several questions to GGK embedded in the document asking about the purpose of certain fields and any impact if they are removed.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementService/ConfigurationService.svc.cs
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementService/ConfigurationService.svc.cs
@@ -1,21 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
-using DataTransferObjects;
-using DataAccess;
-using DataTransferObjects.ContextObjects;
 using System.Net.Mail;
+using DataAccess;
+using DataTransferObjects;
+using DataTransferObjects.ContextObjects;
 
 namespace PracticeManagementService
 {
     [System.ServiceModel.Activation.AspNetCompatibilityRequirements(RequirementsMode = System.ServiceModel.Activation.AspNetCompatibilityRequirementsMode.Allowed)]
     public class ConfigurationService : IConfigurationService
     {
-        #region Constants
-
-        const string ErrorLogMessage = @"<Error><NEW_VALUES	Login = ""{0}"" SourcePage = ""{1}"" SourceQuery = ""{2}""  ExcMsg=""{3}"" ExcSrc=""{4}"" InnerExcMsg=""{5}"" InnerExcSrc=""{6}""><OLD_VALUES /></NEW_VALUES></Error>";
-
-        #endregion  Constants
-
         #region "IConfigurationService Members"
 
         public List<EmailTemplate> GetAllEmailTemplates()
@@ -43,44 +37,9 @@ namespace PracticeManagementService
             return EmailTemplateDAL.DeleteEmailTemplate(templateId);
         }
 
-        public void CheckProjectedProjects(int templateId, string companyName)
-        {
-            try
-            {
-                var projects = new Dictionary<int, EmailRecepients>();
-                string body, subject = string.Empty;
-                EmailTemplateDAL.CheckProjectedProjects(templateId, projects, out body, out subject);
-                SendEmails(projects, body, subject, companyName);
-            }
-            catch (Exception e)
-            {
-                string logData = string.Format(ErrorLogMessage, "CheckProjectedProjects", "ConfigurationService.svc", string.Empty,
-                    e.Message, e.Source, e.InnerException == null ? string.Empty : e.InnerException.Message, e.InnerException == null ? string.Empty : e.InnerException.Source);
-                ActivityLogDAL.ActivityLogInsert(20, logData);
-            }
-
-        }
-
         public EmailData GetEmailData(EmailContext emailContext)
         {
             return EmailTemplateDAL.GetEmailData(emailContext);
-        }
-
-        public void CheckProjectedProjectsByHireDate(int templateId, string companyName)
-        {
-            try
-            {
-                var projects = new Dictionary<int, EmailRecepients>();
-                string body, subject = string.Empty;
-                EmailTemplateDAL.CheckProjectedProjectsByHireDate(templateId, projects, out body, out subject);
-                SendEmails(projects, body, subject, companyName);
-            }
-            catch (Exception e)
-            {
-                string logData = string.Format(ErrorLogMessage, "CheckProjectedProjectsByHireDate", "ConfigurationService.svc", string.Empty,
-                    e.Message, e.Source, e.InnerException == null ? string.Empty : e.InnerException.Message, e.InnerException == null ? string.Empty : e.InnerException.Source);
-                ActivityLogDAL.ActivityLogInsert(20, logData);
-            }
         }
 
         public CompanyLogo GetCompanyLogoData()
@@ -93,35 +52,12 @@ namespace PracticeManagementService
             ConfigurationDAL.SaveCompanyLogoData(title, imagename, imagePath, data);
         }
 
-        #endregion "IConfigurationService Members"
-
-        #region "Email related functions"
-
-        private void SendEmails(Dictionary<int, EmailRecepients> projects, string body, string subject, string companyName)
+        public List<string> GetAllDomains()
         {
-            if (!string.IsNullOrEmpty(body) && !string.IsNullOrEmpty(subject))
-            {
-                var provider = (PracticeManagement.IEMailDeliveryProvider)Activator.CreateInstance(Type.GetType(System.Configuration.ConfigurationManager.AppSettings["EmailDeliveryProviderType"]));
-                foreach (KeyValuePair<int, EmailRecepients> project in projects)
-                {
-                    string resSubject = subject.Replace("<CompanyName>", companyName);
-                    string resBody = body;
-                    foreach (KeyValuePair<string, string> param in project.Value.Parameters)
-                    {
-                        resSubject = resSubject.Replace(string.Format("<{0}>", param.Key), param.Value);
-                        resBody = resBody.Replace(string.Format("<{0}>", param.Key), param.Value);
-                    }
-
-                    provider.SendMail(project.Value.ToPersonAddresses,
-                                        project.Value.CcPersonAddresses,
-                                        System.Configuration.ConfigurationManager.AppSettings["EmailAddressFrom"],
-                                        resSubject,
-                                        resBody);
-                }
-            }
+            return ConfigurationDAL.GetAllDomains();
         }
 
-        #endregion "Email related functions"
+        #endregion "IConfigurationService Members"
 
         public void SaveResourceKeyValuePairs(SettingsType settingType, Dictionary<string, string> dictionary)
         {
