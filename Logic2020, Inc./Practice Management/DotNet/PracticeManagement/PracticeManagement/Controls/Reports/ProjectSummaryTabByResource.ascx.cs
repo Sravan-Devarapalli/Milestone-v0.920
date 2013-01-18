746043e1c791d6eead1d9287d816fbfaa3a5adbe
thulasiramp <thulasiramp>
2013-01-18 04:15:00 -0500

    Comments - Defect Number: 3111; Defect RecordID: 3107; Server Address: localhost; Server Port: 99; Database Name: Practice Management; Defect Summary: Reports > Add "PersonID" column to all Excel exports where an Employee Name is listed; Defect Description: In order to help us perform analysis across various systems, we are using the "PersonID" assigned by Practice Management as the standard identifying ID for every employee (e.g., C123456). We are requesting that in all Excel export reports created by the system where the Employee Name is listed, a new column called "Employee ID" is listed that contains the individual's "PersonID". In all instances, the column should be the first column in the spreadsheet.
    We have identified the following reports that need this column added. Could you please confirm whether there are any other reports we missed that we need the new column added?
    Configuration > Persons > Export
    Reports > Time > By Time Period > By Resource
    Reports > Time > By Project > By Resource
    Reports > Time > By Person
    Reports > Human Capital > New Hire
    Reports > Human Capital > New Hire Graphs > By Seniority Detail
    Reports > Human Capital > New Hire Graphs > By Recruiter Detail
    Reports > Human Capital > Termination
    Reports > Human Capital > Termination Graphs > YTD Detail
    Reports > Human Capital > Termination Graphs > Last 12 Months Detail

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/Controls/Reports/ProjectSummaryTabByResource.ascx.cs
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/Controls/Reports/ProjectSummaryTabByResource.ascx.cs
@@ -19,6 +19,12 @@ namespace PraticeManagement.Controls.Reports
     public partial class ProjectSummaryTabByResource : System.Web.UI.UserControl
     {
         private string ProjectSummaryByResourceExport = "Project Summary Report By Resource";
+        private string ByPersonByResourceUrl = "PersonDetailTimeReport.aspx?StartDate={0}&EndDate={1}&PeriodSelected={2}&PersonId={3}";
+        private string ShowPanel = "ShowPanel('{0}', '{1}','{2}');";
+        private string HidePanel = "HidePanel('{0}');";
+        private string OnMouseOver = "onmouseover";
+        private string OnMouseOut = "onmouseout";
+
         #region Variables
         private HtmlImage ImgProjectRoleFilter { get; set; }
 
@@ -40,6 +46,16 @@ namespace PraticeManagement.Controls.Reports
             get { return (PraticeManagement.Controls.Reports.ProjectSummaryByResource)HostingPage.ByResourceControl; }
         }
 
+        private Label LblProjectedHours { get; set; }
+
+        private Label LblBillable { get; set; }
+
+        private Label LblNonBillable { get; set; }
+
+        private Label LblActualHours { get; set; }
+
+        private Label LblBillableHoursVariance { get; set; }
+
         #endregion
 
         #region Methods
@@ -59,14 +75,46 @@ namespace PraticeManagement.Controls.Reports
                 cblProjectRoles.SaveSelectedIndexesInViewState();
                 ImgProjectRoleFilter.Attributes["onclick"] = string.Format("Filter_Click(\'{0}\',\'{1}\',\'{2}\',\'{3}\');", cblProjectRoles.FilterPopupClientID,
                   cblProjectRoles.SelectedIndexes, cblProjectRoles.CheckBoxListObject.ClientID, cblProjectRoles.WaterMarkTextBoxBehaviorID);
+
+                //Populate header hover               
+                PopulateHeaderHoverLabels(reportData.ToList());
             }
             else
             {
                 divEmptyMessage.Style["display"] = "";
                 repResource.Visible = false;
             }
-            btnExportToPDF.Enabled = 
+            btnExportToPDF.Enabled =
             btnExportToExcel.Enabled = reportData.Count() > 0;
+        }
+
+        private void PopulateHeaderHoverLabels(List<PersonLevelGroupedHours> reportData)
+        {
+            LblProjectedHours.Attributes[OnMouseOver] = string.Format(ShowPanel, LblProjectedHours.ClientID, pnlTotalProjectedHours.ClientID, 0);
+            LblProjectedHours.Attributes[OnMouseOut] = string.Format(HidePanel, pnlTotalProjectedHours.ClientID);
+
+            LblBillable.Attributes[OnMouseOver] = string.Format(ShowPanel, LblBillable.ClientID, pnlTotalBillableHours.ClientID, 0);
+            LblBillable.Attributes[OnMouseOut] = string.Format(HidePanel, pnlTotalBillableHours.ClientID);
+
+            LblNonBillable.Attributes[OnMouseOver] = string.Format(ShowPanel, LblNonBillable.ClientID, pnlTotalNonBillableHours.ClientID, 0);
+            LblNonBillable.Attributes[OnMouseOut] = string.Format(HidePanel, pnlTotalNonBillableHours.ClientID);
+
+            LblActualHours.Attributes[OnMouseOver] = string.Format(ShowPanel, LblActualHours.ClientID, pnlTotalActualHours.ClientID, 0);
+            LblActualHours.Attributes[OnMouseOut] = string.Format(HidePanel, pnlTotalActualHours.ClientID);
+
+            LblBillableHoursVariance.Attributes[OnMouseOver] = string.Format(ShowPanel, LblBillableHoursVariance.ClientID, pnlBillableHoursVariance.ClientID, 0);
+            LblBillableHoursVariance.Attributes[OnMouseOut] = string.Format(HidePanel, pnlBillableHoursVariance.ClientID);
+
+            double totalBillableHoursVariance = reportData.Sum(p => p.BillableHoursVariance);
+            lblTotalProjectedHours.Text = reportData.Sum(p => p.ForecastedHours).ToString(Constants.Formatting.DoubleValue);
+            lblTotalBillableHours.Text = lblTotalBillablePanlActual.Text = reportData.Sum(p => p.BillableHours).ToString(Constants.Formatting.DoubleValue);
+            lblTotalNonBillableHours.Text = lblTotalNonBillablePanlActual.Text = reportData.Sum(p => p.NonBillableHours).ToString(Constants.Formatting.DoubleValue);
+            lblTotalActualHours.Text = reportData.Sum(p => p.TotalHours).ToString(Constants.Formatting.DoubleValue);
+            lblTotalBillableHoursVariance.Text = totalBillableHoursVariance.ToString(Constants.Formatting.DoubleValue);
+            if (totalBillableHoursVariance < 0)
+            {
+                lblExclamationMarkPanl.Visible = true;
+            }
         }
 
         private void PopulateProjectRoleFilter(List<PersonLevelGroupedHours> reportData)
@@ -86,10 +134,19 @@ namespace PraticeManagement.Controls.Reports
             if (e.Item.ItemType == ListItemType.Header)
             {
                 ImgProjectRoleFilter = e.Item.FindControl("imgProjectRoleFilter") as HtmlImage;
+
+                LblProjectedHours = e.Item.FindControl("lblProjectedHours") as Label;
+                LblBillable = e.Item.FindControl("lblBillable") as Label;
+                LblNonBillable = e.Item.FindControl("lblNonBillable") as Label;
+                LblActualHours = e.Item.FindControl("lblActualHours") as Label;
+                LblBillableHoursVariance = e.Item.FindControl("lblBillableHoursVariance") as Label;
             }
             else if (e.Item.ItemType == ListItemType.Item || e.Item.ItemType == ListItemType.AlternatingItem)
             {
-
+                var dataItem = (PersonLevelGroupedHours)e.Item.DataItem;
+                var LnkActualHours = e.Item.FindControl("lnkActualHours") as LinkButton;
+                LnkActualHours.Attributes["NavigationUrl"] = string.Format(ByPersonByResourceUrl, (HostingPage.StartDate.HasValue) ? HostingPage.StartDate.Value.ToString(Constants.Formatting.EntryDateFormat) : null,
+                    (HostingPage.EndDate.HasValue) ? HostingPage.EndDate.Value.Date.ToString(Constants.Formatting.EntryDateFormat) : null, (HostingPage.PeriodSelected != "*") ? HostingPage.PeriodSelected : "0", dataItem.Person.Id);
             }
         }
 
@@ -141,17 +198,21 @@ namespace PraticeManagement.Controls.Reports
             if (data.Count > 0)
             {
                 //Header
+                sb.Append("Employee Id");
+                sb.Append("\t");
                 sb.Append("Resource");
                 sb.Append("\t");
                 sb.Append("Project Role");
+                sb.Append("\t");
+                sb.Append("Projected Hours");
                 sb.Append("\t");
                 sb.Append("Billable");
                 sb.Append("\t");
                 sb.Append("Non-Billable");
                 sb.Append("\t");
-                sb.Append("Total");
+                sb.Append("Actual Hours");
                 sb.Append("\t");
-                sb.Append("Person Variance (in Hours)");
+                sb.Append("Billable Hours Variance");
                 sb.Append("\t");
                 sb.AppendLine();
 
@@ -160,9 +221,13 @@ namespace PraticeManagement.Controls.Reports
                 //Data
                 foreach (var byPerson in list)
                 {
+                    sb.Append(byPerson.Person.EmployeeNumber);
+                    sb.Append("\t");
                     sb.Append(byPerson.Person.HtmlEncodedName);
                     sb.Append("\t");
                     sb.Append(byPerson.Person.ProjectRoleName);
+                    sb.Append("\t");
+                    sb.Append(GetDoubleFormat(byPerson.ForecastedHours));
                     sb.Append("\t");
                     sb.Append(GetDoubleFormat(byPerson.BillableHours));
                     sb.Append("\t");
@@ -170,7 +235,7 @@ namespace PraticeManagement.Controls.Reports
                     sb.Append("\t");
                     sb.Append(GetDoubleFormat(byPerson.TotalHours));
                     sb.Append("\t");
-                    sb.Append(byPerson.Variance);
+                    sb.Append(GetDoubleFormat(byPerson.BillableHoursVariance));
                     sb.Append("\t");
                     sb.AppendLine();
                 }
