7cc5a18b114cafb536a56e6e84d79407319113eb
SainathC <SainathC>
2013-03-11 08:35:00 -0500

    Comments - Defect Number: 3110; Defect RecordID: 3106; Server Address: localhost; Server Port: 99; Database Name: Practice Management; Defect Summary: Projects - Summary: Need to use Actual Revenue and Actual Margin when calculating values for prior months; Defect Description: In the Project Summary page, currently the Revenue and Margin calculations use values from the Project Details page to forecast Revenue and Margin values. This works fine if a user is looking at the current month and future months to see Project Summary financial forecasts. However, with our robust Time Entry we ought to be leveraging the actual time entry data to drive our Revenue and Margin calculations for activities in the past. Therefore, I am requesting an enhancement to the Project Summary page and calculations to incorporate our Time Entry data to calculate Actual Revenue and Actual Margin any time we view the Project Summary results for past months.
    To increase flexibility, I'd like to add a checkbox under the "Show Projects for ..." entry called, "Use Actual Time Entry data for Previous Month calculations" that will enable a user to view results using only Projected Values (i.e. to produce the report as it runs today) if turned off and will use Time Entry data for previous month calculations if turned on. The default value of this checkbox will be "On".
    
    When the report runs using the Time Entry data to calculate Revenue and Margin, a previous month is defined as any month preceding the current month (i.e. the current month will always use the Projected Values regardless of whether the checkbox is on or off and regardless of whether Time Entry data exists for the Current Month). This will allow a settling period for the current month's time entry.
    
    For Projects with Fixed Fee pricing, the fixed fee Revenue would be applied to a month's revenue calculation based on the percent of completion of the Milestone. (E.g. Milestone Start Date = 10/1/2012 and End Date is 11/15/2012 and is a fixed price milestone with a payment = $12,000. If a user runs the Project Summary in December 2012, November's Revenue will include $4,000 in fixed fee revenue while October's Revenue reflects $8,000 in Revenue. NOTE the above is simplified for explanatory purposes only, assuming an equal distribution of hours worked over the six week project.) I believe this is how PM functions now for calculating Revenue of Fixed Fee milestones on the Project Summary report. Please correct me if I am wrong.
    
    The Export of Project Summary should also be adjusted. See the attached file as an example new file. The Export will be the same file regardless of whether the "Use Actual Time Entry data for Previous Month calculations" checkbox is On/Off as the Export will include both Projected Values for Revenue and Cont. Margin and Actual Values. The Export will contain two worksheets - one for Summary - Projected and one for Summary - Actuals.
    
    The Project Summary page's results should be pre-cached each night to maximize performance when viewed each day. The caching should pre-build the report views for data within the Current Fiscal Year and the Previous Fiscal Year. This should expedite performance of the displayed data on the screen and limit errors when running the report across large time periods.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/FinancialsListByProjectPeriod.proc.sql
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/FinancialsListByProjectPeriod.proc.sql
@@ -1,26 +1,44 @@
 ﻿-- =============================================
 -- Author:		Anatoliy Lokshin
 -- Create date: 9-11-2008
--- Updated by:	Anatoliy Lokshin
--- Update date:	9-18-2008
+-- Updated by:	Sainath
+-- Update date:	
 -- Description:	Selects financils for the specified project and period grouped by months
 -- =============================================
-CREATE PROCEDURE [dbo].[FinancialsListByProjectPeriod]
+CREATE PROCEDURE [dbo].[FinancialsListByProjectPeriod] 
 (
 	@ProjectId   VARCHAR(2500),
 	@StartDate   DATETIME,
-	@EndDate     DATETIME
-)
+	@EndDate     DATETIME,
+	@UseActuals	 BIT = 0
+) WITH RECOMPILE
 AS
+BEGIN
 	SET NOCOUNT ON
-	
+	SET ANSI_WARNINGS OFF
+
+	DECLARE @ProjectIdLocal   VARCHAR(2500),
+			@StartDateLocal   DATETIME,
+			@EndDateLocal     DATETIME
+			
+
+	SELECT @ProjectIdLocal =@ProjectId ,
+		   @StartDateLocal=@StartDate ,
+		   @EndDateLocal=@EndDate
+
 	DECLARE @ProjectIDs TABLE
 	(
 		ResultId INT
 	)
-	
+	DECLARE @Today DATETIME, @CurrentMonthStartDate DATETIME,@InsertingTime DATETIME
+
+	SELECT @Today = CONVERT(DATE, dbo.GettingPMTime(GETUTCDATE())),@InsertingTime = dbo.InsertingTime()
+	SELECT @CurrentMonthStartDate = C.MonthStartDate
+	FROM dbo.Calendar C
+	WHERE C.Date = @Today
+
 	INSERT INTO @ProjectIDs
-	SELECT * FROM dbo.ConvertStringListIntoTable(@ProjectId)
+	SELECT * FROM dbo.ConvertStringListIntoTable(@ProjectIdLocal)
 
 	;WITH FinancialsRetro AS 
 	(
@@ -39,46 +57,57 @@ AS
 		   f.PersonHoursPerDay,
 		   f.PracticeManagementCommissionSub,
 		   f.PracticeManagementCommissionOwn ,
-		   f.PracticeManagerId,
 		   f.PersonId,
-		   f.Discount
-	FROM v_FinancialsRetrospective f
+		   f.Discount,
+		   f.IsHourlyAmount,
+		   f.BillableHOursPerDay,
+  		   f.NonBillableHoursPerDay,
+		   f.ActualHoursPerDay,
+		   f.BillRate,
+		   f.OverheadRate,
+		   f.BonusRate,
+		   f.VacationRate
+	FROM [v_FinancialsRetrospectiveActualHours] f
 	WHERE f.ProjectId  IN (SELECT * FROM @ProjectIDs) 
+		AND f.Date BETWEEN @StartDateLocal AND @EndDateLocal
 	),
-	ProjectFinancials
+	ActualAndProjectedValuesDaily
 	AS
 	(
-	SELECT f.ProjectId,
-		   C.MonthStartDate AS FinancialDate,
-		   C.MonthEndDate	AS MonthEnd,
-	       SUM(f.PersonMilestoneDailyAmount) AS Revenue,
-
-	       SUM(f.PersonMilestoneDailyAmount - f.PersonDiscountDailyAmount) AS RevenueNet,
-	       
-		   SUM(f.PersonMilestoneDailyAmount - f.PersonDiscountDailyAmount -
-						(CASE WHEN f.SLHR >= f.PayRate + f.MLFOverheadRate 
-							  THEN f.SLHR ELSE f.PayRate + f.MLFOverheadRate END) 
-					    *ISNULL(f.PersonHoursPerDay, 0)) GrossMargin,
-		   
-		   ISNULL(SUM((CASE WHEN f.SLHR >=  f.PayRate + f.MLFOverheadRate THEN f.SLHR ELSE  f.PayRate + f.MLFOverheadRate END)*ISNULL(f.PersonHoursPerDay, 0)),0) Cogs,
-	       ISNULL(SUM(f.PersonHoursPerDay), 0) AS Hours,
-	       
-	       (SUM((f.PersonMilestoneDailyAmount - f.PersonDiscountDailyAmount -
+	SELECT	f.ProjectId,
+			f.PersonId,
+			f.Date,
+			SUM(f.PersonMilestoneDailyAmount) AS ProjectedRevenueperDay,
+			SUM(f.PersonMilestoneDailyAmount - f.PersonDiscountDailyAmount) AS ProjectedRevenueNet,
+			SUM(f.PersonMilestoneDailyAmount - f.PersonDiscountDailyAmount - (
+																				CASE WHEN f.SLHR >= f.PayRate + f.MLFOverheadRate THEN f.SLHR 
+																				ELSE f.PayRate + f.MLFOverheadRate END
+																			) * ISNULL(f.PersonHoursPerDay, 0)) AS  ProjectedGrossMargin,
+			ISNULL(SUM((CASE WHEN f.SLHR >=  f.PayRate + f.MLFOverheadRate THEN f.SLHR ELSE  f.PayRate + f.MLFOverheadRate END)*ISNULL(f.PersonHoursPerDay, 0)),0) ProjectedCogsperDay,
+			SUM(CASE WHEN f.IsHourlyAmount = 0 THEN f.PersonMilestoneDailyAmount ELSE 0 END ) AS FixedActualRevenuePerDay,
+			(ISNULL(SUM(f.BillRate* f.ActualHoursPerDay),0) / ISNULL(NULLIF(SUM(f.ActualHoursPerDay),0),1)) * MAX(CASE WHEN f.IsHourlyAmount = 1 THEN  f.BillableHOursPerDay ELSE 0 END) HourlyActualRevenuePerDay,
+			SUM(CASE WHEN f.IsHourlyAmount = 0 
+					THEN 	f.PersonMilestoneDailyAmount - f.PersonDiscountDailyAmount - (CASE WHEN f.SLHR >= f.PayRate + f.MLFOverheadRate  THEN f.SLHR ELSE f.PayRate + f.MLFOverheadRate END) * ISNULL(f.PersonHoursPerDay, 0)
+					ELSE 0
+				END) AS FixedActualMarginPerDay,
+				((ISNULL(SUM(f.BillRate* f.ActualHoursPerDay),0) / ISNULL(NULLIF(SUM(f.ActualHoursPerDay),0),1)) * MAX(CASE WHEN f.IsHourlyAmount = 1 THEN  f.BillableHOursPerDay ELSE 0 END))
+				 -  (
+						MAX( CASE WHEN f.IsHourlyAmount = 1 THEN  f.BillableHOursPerDay + f.NonBillableHoursPerDay ELSE 0 END ) * 
+						ISNULL( CASE WHEN ISNULL(SUM(f.ActualHoursPerDay),0) > 0 
+									THEN SUM((CASE WHEN f.SLHR >=  f.PayRate + f.MLFOverheadRate THEN f.SLHR ELSE  f.PayRate + f.MLFOverheadRate END)* f.ActualHoursPerDay) / SUM(f.ActualHoursPerDay)
+									ELSE SUM((CASE WHEN f.SLHR >=  f.PayRate + f.MLFOverheadRate THEN f.SLHR ELSE  f.PayRate + f.MLFOverheadRate END)) / ISNULL(COUNT(f.PayRate),1)  
+								END ,0)
+					) AS HourlyActualMarginPerDay,
+			ISNULL(SUM(f.PersonHoursPerDay), 0) AS ProjectedHoursPerDay,
+			(SUM((f.PersonMilestoneDailyAmount - f.PersonDiscountDailyAmount -
 						(CASE WHEN f.SLHR >=  f.PayRate +f.MLFOverheadRate 
 							  THEN f.SLHR ELSE f.PayRate +f.MLFOverheadRate END) 
 					    *ISNULL(f.PersonHoursPerDay, 0))* (f.ProjectSalesCommisionFraction/100))) SalesCommission,
-
-	       SUM((f.PersonMilestoneDailyAmount - f.PersonDiscountDailyAmount -
-	            (CASE WHEN f.SLHR >=  f.PayRate +f.MLFOverheadRate 
-							  THEN f.SLHR ELSE f.PayRate +f.MLFOverheadRate END) * ISNULL(f.PersonHoursPerDay, 0)) *
-	           (f.PracticeManagementCommissionSub + CASE f.PracticeManagerId WHEN f.PersonId THEN f.PracticeManagementCommissionOwn ELSE 0 END)) / 100 AS PracticeManagementCommission,
-		  		   min(f.Discount) as Discount
-	  FROM FinancialsRetro AS f
-	  INNER JOIN Calendar C ON C.Date = f.Date
-	  WHERE  
-		f.ProjectId IN (SELECT * FROM @ProjectIDs) 
-		AND f.Date BETWEEN @StartDate AND @EndDate
-	GROUP BY f.ProjectId, C.MonthStartDate, C.MonthEndDate
+			0 AS PracticeManagementCommission,
+		  	min(f.Discount) as Discount,
+			MIN(CONVERT(INT, f.IsHourlyAmount)) as IsHourlyAmount
+	FROM FinancialsRetro AS f
+	GROUP BY f.ProjectId, f.PersonId, f.Date
 	),
 	ProjectExpensesMonthly
 	AS
@@ -90,25 +119,54 @@ AS
 			C.MonthEndDate AS MonthEnd
 		FROM dbo.ProjectExpense as pexp
 		JOIN dbo.Calendar c ON c.Date BETWEEN pexp.StartDate AND pexp.EndDate
-		WHERE ProjectId IN (SELECT * FROM @ProjectIDs) AND c.Date BETWEEN @StartDate	AND @EndDate
+		WHERE ProjectId IN (SELECT * FROM @ProjectIDs) AND c.Date BETWEEN @StartDateLocal AND @EndDateLocal
 		GROUP BY pexp.ProjectId, C.MonthStartDate, C.MonthEndDate
+	), 
+	ActualAndProjectedValuesMonthly  AS
+	(SELECT CT.ProjectId, 
+			C.MonthStartDate AS FinancialDate, 
+			C.MonthEndDate AS MonthEnd,
+			SUM(ISNULL(CT.ProjectedRevenueperDay, 0)) AS ProjectedRevenue,
+			SUM(ISNULL(CT.ProjectedRevenueNet, 0)) as ProjectedRevenueNet,
+			SUM(ISNULL(CT.ProjectedGrossMargin, 0)) as ProjectedGrossMargin,
+			SUM(ISNULL(CT.HourlyActualRevenuePerDay, 0) + ISNULL(CT.FixedActualRevenuePerDay, 0)) AS ActualRevenue,
+			SUM(ISNULL(CT.HourlyActualMarginPerDay, 0) + ISNULL(CT.FixedActualMarginPerDay, 0)) as ActualMargin,
+			SUM(ISNULL(CT.ProjectedCogsperDay, 0)) as ProjectedCogs,
+			MAX(ISNULL(CT.Discount, 0)) Discount,
+			SUM(ISNULL(CT.ProjectedHoursPerDay, 0)) as ProjectedHoursPerMonth,
+			SUM(ISNULL(CT.SalesCommission, 0)) as SalesCommission,
+			SUM(ISNULL(CT.PracticeManagementCommission, 0)) as PracticeManagementCommission
+	FROM ActualAndProjectedValuesDaily CT
+	INNER JOIN dbo.Calendar C ON C.Date = CT.Date 
+	GROUP BY CT.ProjectId, C.MonthStartDate, C.MonthEndDate
 	)
-	
 	SELECT
-		ISNULL(pf.ProjectId,PEM.ProjectId) ProjectId,
-		ISNULL(pf.FinancialDate,PEM.FinancialDate) FinancialDate,
-		ISNULL(pf.MonthEnd,PEM.MonthEnd) MonthEnd,
-		ISNULL(pf.Revenue,0)+ISNULL(PEM.Reimbursement,0)-ISNULL(PEM.Expense,0) AS 'Revenue',
-		ISNULL(pf.RevenueNet,0)+ISNULL(PEM.Reimbursement,0)-ISNULL(PEM.Expense,0)   as 'RevenueNet',
-		ISNULL(pf.Cogs,0) Cogs ,
-		ISNULL(pf.GrossMargin,0)+(ISNULL(PEM.Reimbursement,0)-ISNULL(PEM.Expense,0))* (1 - ISNULL(pf.Discount,0)/100)  as 'GrossMargin',
-		ISNULL(pf.Hours,0) Hours,
-		ISNULL(pf.SalesCommission,0) SalesCommission,
-		ISNULL(pf.PracticeManagementCommission,0) PracticeManagementCommission,
-		ISNULL(PEM.Expense,0) Expense,
-		ISNULL(PEM.Reimbursement,0) ReimbursedExpense
-	FROM ProjectFinancials pf
+		ISNULL(APV.ProjectId,PEM.ProjectId) ProjectId,
+		ISNULL(APV.FinancialDate,PEM.FinancialDate) FinancialDate,
+		ISNULL(APV.MonthEnd,PEM.MonthEnd) MonthEnd,
+		CONVERT(DECIMAL(18,2),ISNULL(APV.ProjectedRevenue,0)) AS 'Revenue',
+		CONVERT(DECIMAL(18,2),ISNULL(APV.ProjectedRevenueNet,0))   as 'RevenueNet',
+		CONVERT(DECIMAL(18,2),ISNULL(APV.ProjectedCogs,0)) Cogs ,
+		CONVERT(DECIMAL(18,2),ISNULL(APV.ProjectedGrossMargin,0)+(ISNULL(PEM.Reimbursement,0)-ISNULL(PEM.Expense,0)) * (1 - ISNULL(APV.Discount,0)/100))  as 'GrossMargin',
+		CONVERT(DECIMAL(18,2),ISNULL(APV.ProjectedHoursPerMonth,0)) Hours,
+		CONVERT(DECIMAL(18,2),ISNULL(APV.SalesCommission,0)) SalesCommission,
+		CONVERT(DECIMAL(18,2),ISNULL(APV.PracticeManagementCommission,0)) PracticeManagementCommission,
+		CONVERT(DECIMAL(18,2),ISNULL(PEM.Expense,0)) Expense,
+		CONVERT(DECIMAL(18,2),ISNULL(PEM.Reimbursement,0)) ReimbursedExpense,
+		CASE WHEN ISNULL(APV.FinancialDate,PEM.FinancialDate) < @CurrentMonthStartDate 
+			 THEN CONVERT(DECIMAL(18,6), ISNULL(APV.ActualRevenue,0))
+			 ELSE CONVERT(DECIMAL(18,6), ISNULL(APV.ProjectedRevenue,0))
+			 END ActualRevenue,
+		CASE WHEN ISNULL(APV.FinancialDate,PEM.FinancialDate) < @CurrentMonthStartDate 
+			 THEN CONVERT(DECIMAL(18,6), ISNULL(APV.ActualMargin,0) - (ISNULL(APV.ActualRevenue,0) * ISNULL(APV.Discount,0)/100) + ((ISNULL(PEM.Reimbursement,0)-ISNULL(PEM.Expense,0)) * (1 - ISNULL(APV.Discount,0)/100)))
+			 ELSE CONVERT(DECIMAL(18,6), ISNULL(APV.ProjectedGrossMargin,0) + (ISNULL(PEM.Reimbursement,0)-ISNULL(PEM.Expense,0)) * (1 - ISNULL(APV.Discount,0)/100))
+			 END ActualGrossMargin,
+		CONVERT(BIT,1) AS IsMonthlyRecord,
+		@InsertingTime AS  CreatedDate,	
+		CONVERT(DATE,@InsertingTime)  AS CacheDate
+	FROM ActualAndProjectedValuesMonthly APV
 	FULL JOIN ProjectExpensesMonthly PEM 
-	ON PEM.ProjectId = pf.ProjectId AND pf.FinancialDate = PEM.FinancialDate  AND Pf.MonthEnd = PEM.MonthEnd
-	
+	ON PEM.ProjectId = APV.ProjectId AND APV.FinancialDate = PEM.FinancialDate  AND APV.MonthEnd = PEM.MonthEnd
+
+END
 
