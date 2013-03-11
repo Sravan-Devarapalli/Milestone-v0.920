6a4c8c9319fcca2ad98d74ac2ba36a692fd80312
SainathC <SainathC>
2013-03-11 07:48:00 -0500

    Comments - Defect Number: 3110; Defect RecordID: 3106; Server Address: localhost; Server Port: 99; Database Name: Practice Management; Defect Summary: Projects - Summary: Need to use Actual Revenue and Actual Margin when calculating values for prior months; Defect Description: In the Project Summary page, currently the Revenue and Margin calculations use values from the Project Details page to forecast Revenue and Margin values. This works fine if a user is looking at the current month and future months to see Project Summary financial forecasts. However, with our robust Time Entry we ought to be leveraging the actual time entry data to drive our Revenue and Margin calculations for activities in the past. Therefore, I am requesting an enhancement to the Project Summary page and calculations to incorporate our Time Entry data to calculate Actual Revenue and Actual Margin any time we view the Project Summary results for past months.
    To increase flexibility, I'd like to add a checkbox under the "Show Projects for ..." entry called, "Use Actual Time Entry data for Previous Month calculations" that will enable a user to view results using only Projected Values (i.e. to produce the report as it runs today) if turned off and will use Time Entry data for previous month calculations if turned on. The default value of this checkbox will be "On".
    
    When the report runs using the Time Entry data to calculate Revenue and Margin, a previous month is defined as any month preceding the current month (i.e. the current month will always use the Projected Values regardless of whether the checkbox is on or off and regardless of whether Time Entry data exists for the Current Month). This will allow a settling period for the current month's time entry.
    
    For Projects with Fixed Fee pricing, the fixed fee Revenue would be applied to a month's revenue calculation based on the percent of completion of the Milestone. (E.g. Milestone Start Date = 10/1/2012 and End Date is 11/15/2012 and is a fixed price milestone with a payment = $12,000. If a user runs the Project Summary in December 2012, November's Revenue will include $4,000 in fixed fee revenue while October's Revenue reflects $8,000 in Revenue. NOTE the above is simplified for explanatory purposes only, assuming an equal distribution of hours worked over the six week project.) I believe this is how PM functions now for calculating Revenue of Fixed Fee milestones on the Project Summary report. Please correct me if I am wrong.
    
    The Export of Project Summary should also be adjusted. See the attached file as an example new file. The Export will be the same file regardless of whether the "Use Actual Time Entry data for Previous Month calculations" checkbox is On/Off as the Export will include both Projected Values for Revenue and Cont. Margin and Actual Values. The Export will contain two worksheets - one for Summary - Projected and one for Summary - Actuals.
    
    The Project Summary page's results should be pre-cached each night to maximize performance when viewed each day. The caching should pre-build the report views for data within the Current Fiscal Year and the Previous Fiscal Year. This should expedite performance of the displayed data on the screen and limit errors when running the report across large time periods.

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Views/v_FinancialsRetrospectiveActualHours.view.sql
@@ -0,0 +1,136 @@
+﻿CREATE VIEW [dbo].[v_FinancialsRetrospectiveActualHours]
+AS
+WITH ActualTimeEntries
+AS 
+(
+	SELECT CC.ProjectId,
+			TE.PersonId,
+			TE.ChargeCodeDate,
+			SUM(CASE WHEN TEH.IsChargeable = 1 THEN TEH.ActualHours ELSE 0 END) BillableHOursPerDay,
+			SUM(CASE WHEN TEH.IsChargeable = 0 THEN TEH.ActualHours ELSE 0 END) NonBillableHoursPerDay,
+			CAST(CASE WHEN SUM(CAST(m.IsHourlyAmount as INT)) > 0 THEN 1 ELSE 0 END AS BIT) AS IsHourlyAmount
+	FROM TimeEntry TE
+	JOIN TimeEntryHours TEH ON TEH.TimeEntryId = TE.TimeEntryId
+	JOIN ChargeCode CC on CC.Id = TE.ChargeCodeId
+	JOIN Project P ON p.ProjectId = CC.ProjectId AND P.IsAllowedToShow = 1
+	LEFT JOIN Milestone m ON m.ProjectId = p.ProjectId 
+	GROUP BY CC.ProjectId, TE.PersonId, TE.ChargeCodeDate
+),
+MileStoneEntries
+AS
+(
+	SELECT  m.ProjectId,
+			m.[MilestoneId],
+			mp.PersonId,
+			cal.Date,
+			MPE.Id,
+			MPE.Amount,
+			m.IsHourlyAmount,	
+			m.IsDefault,
+			SUM(mpe.HoursPerDay) AS ActualHoursPerDay,
+			-- dbo.PersonProjectedHoursPerDay(cal.DayOff,cal.companydayoff,cal.TimeoffHours,mpe.HoursPerDay) AS HoursPerDay,
+			--Removed Inline Function for the sake of performance. When ever PersonProjectedHoursPerDay function is updated need to update below case when also.
+			SUM(CONVERT(DECIMAL(4,2),CASE WHEN cal.DayOff = 0  THEN mpe.HoursPerDay -- No Time-off and no company holiday
+				WHEN (cal.DayOff = 1 and cal.companydayoff = 1) OR (cal.DayOff = 1 AND cal.companydayoff = 0 AND ISNULL(cal.TimeoffHours,8) = 8) THEN 0 -- only company holiday OR person complete dayoff
+				ELSE mpe.HoursPerDay * (1-(cal.TimeoffHours/8)) --person partial day off
+			END)) AS HoursPerDay,
+			SUM(CONVERT(DECIMAL(4,2),CASE WHEN cal.DayOff = 0  THEN mpe.HoursPerDay -- No Time-off and no company holiday
+				WHEN (cal.DayOff = 1 and cal.companydayoff = 1) OR (cal.DayOff = 1 AND cal.companydayoff = 0 AND ISNULL(cal.TimeoffHours,8) = 8) THEN 0 -- only company holiday OR person complete dayoff
+				ELSE mpe.HoursPerDay * (1-(cal.TimeoffHours/8)) --person partial day off
+			END) * mpe.Amount) AS PersonMilestoneDailyAmount--PersonLevel
+			FROM dbo.Project P 
+			INNER JOIN dbo.[Milestone] AS m ON P.ProjectId=m.ProjectId AND p.IsAllowedToShow = 1
+			INNER JOIN dbo.MilestonePerson AS mp ON m.[MilestoneId] = mp.[MilestoneId]
+			INNER JOIN dbo.MilestonePersonEntry AS mpe ON mp.MilestonePersonId = mpe.MilestonePersonId
+			INNER JOIN dbo.PersonCalendarAuto AS cal ON cal.Date BETWEEN mpe.Startdate AND mpe.EndDate AND cal.PersonId = mp.PersonId 
+		    GROUP BY  m.ProjectId,m.[MilestoneId],mp.PersonId,cal.Date,m.IsHourlyAmount ,m.IsDefault,MPE.Id,MPE.Amount
+)
+SELECT	pro.ProjectId,
+		Per.PersonId,
+		c.Date,
+		AE.BillableHOursPerDay,
+		AE.NonBillableHoursPerDay,
+		ISNULL(ME.IsHourlyAmount,AE.IsHourlyAmount) AS IsHourlyAmount,
+		ME.HoursPerDay AS PersonHoursPerDay,
+		ME.ActualHoursPerDay,
+		r.Discount,
+		r.HoursPerDay,
+		0 AS PracticeManagementCommissionOwn,
+		0 AS PracticeManagementCommissionSub,
+		CASE
+	           WHEN ME.IsHourlyAmount = 1 OR r.HoursPerDay = 0
+	           THEN ME.PersonMilestoneDailyAmount
+	           ELSE ISNULL(r.MilestoneDailyAmount * ME.HoursPerDay / r.HoursPerDay, r.MilestoneDailyAmount)
+	       END AS PersonMilestoneDailyAmount,--Person Level Daily Amount
+		   CASE
+	           WHEN ME.IsHourlyAmount = 1 OR r.HoursPerDay = 0
+	           THEN ISNULL(ME.PersonMilestoneDailyAmount * r.Discount / 100, 0)
+	           ELSE ISNULL(r.MilestoneDailyAmount * ME.HoursPerDay * r.Discount / (r.HoursPerDay * 100), r.MilestoneDailyAmount * r.Discount / 100)
+	       END AS PersonDiscountDailyAmount, --Entry Level Daily Discount Amount
+		 CASE
+	           WHEN ME.IsHourlyAmount = 1
+	           THEN ME.Amount
+	           WHEN ME.IsHourlyAmount = 0 AND r.HoursPerDay = 0
+	           THEN 0
+	           ELSE r.MilestoneDailyAmount / r.HoursPerDay
+		   END AS BillRate,
+		     CASE 
+			WHEN p.Timescale = 4
+			THEN p.HourlyRate * 0.01 * ME.Amount
+			ELSE p.HourlyRate
+		   END AS PayRate, 	-- new payrate that takes into account that % unit is used in the Amount instead of $ unit
+	       CASE p.BonusHoursToCollect
+	           WHEN 0 THEN 0
+	           ELSE p.BonusAmount / (CASE WHEN p.IsYearBonus = 1 THEN HY.HoursInYear ELSE p.BonusHoursToCollect END)
+	       END AS BonusRate,
+	        SUM(CASE o.OverheadRateTypeId
+	                       -- Multipliers
+	                       WHEN 2 THEN
+	                           (CASE
+	                                 WHEN r.IsHourlyAmount = 1
+	                                 THEN ME.Amount
+	                                 WHEN r.IsHourlyAmount = 0 OR r.HoursPerDay = 0
+	                                 THEN 0
+	                                 ELSE r.MilestoneDailyAmount / r.HoursPerDay
+	                             END) * o.Rate / 100 
+	                       WHEN 4 THEN p.HourlyRate * o.Rate / 100
+	                       -- Fixed
+	                       WHEN 3 THEN (o.Rate * 12 / HY.HoursInYear) 
+	                       ELSE o.Rate
+	                   END) AS OverheadRate,
+
+	           	ISNULL((CASE MLFO.OverheadRateTypeId
+	                       -- Multipliers
+	                       WHEN 2 THEN
+	                           (CASE
+	                                 WHEN ME.IsHourlyAmount = 1
+	                                 THEN ME.Amount
+	                                 WHEN ME.IsHourlyAmount = 0 OR r.HoursPerDay = 0
+	                                 THEN 0
+	                                 ELSE r.MilestoneDailyAmount / r.HoursPerDay
+	                             END) * MLFO.Rate / 100
+	                       WHEN 4 THEN p.HourlyRate * MLFO.Rate / 100
+	                       -- Fixed
+	                       WHEN 3 THEN (MLFO.Rate * 12 / HY.HoursInYear)
+	                       ELSE MLFO.Rate 
+						   END)
+	                   ,0) MLFOverheadRate,
+		      (CASE 
+			WHEN p.Timescale = 4
+			THEN p.HourlyRate * 0.01 * ME.Amount
+			ELSE p.HourlyRate END) * ISNULL(p.VacationDays,0)* ME.HoursPerDay/HY.HoursInYear VacationRate
+FROM ActualTimeEntries AS AE --ActualEntriesByPerson
+		FULL JOIN MileStoneEntries AS ME ON ME.ProjectId = AE.ProjectId AND AE.PersonId = ME.PersonId AND ME.Date = AE.ChargeCodeDate 
+		INNER JOIN dbo.Person Per ON per.PersonId = ISNULL(ME.PersonId,AE.PersonId)
+		INNER JOIN dbo.Project Pro ON Pro.ProjectId = ISNULL(ME.ProjectId,AE.ProjectId) 
+		INNER JOIN dbo.Calendar C ON c.Date = ISNULL(ME.Date,AE.ChargeCodeDate)
+		INNER JOIN dbo.GetFutureDateTable() FD ON 1=1 --For improving query performance we are using table valued function instead of scalar function.
+		LEFT JOIN dbo.[v_PersonPayRetrospective] AS p ON p.PersonId = per.PersonId AND p.Date = c.Date
+		LEFT JOIN v_MLFOverheadFixedRateTimescale MLFO ON MLFO.TimescaleId = p.Timescale AND c.Date BETWEEN MLFO.StartDate AND ISNULL(MLFO.EndDate,FD.FutureDate)
+		LEFT JOIN dbo.v_OverheadFixedRateTimescale AS o ON p.Date BETWEEN o.StartDate AND ISNULL(o.EndDate, FD.FutureDate) AND o.Inactive = 0 AND o.TimescaleId = p.Timescale
+		LEFT JOIN V_WorkinHoursByYear HY ON HY.[Year] = YEAR(c.date)
+		LEFT JOIN dbo.v_MilestoneRevenueRetrospective AS r ON ME.MilestoneId = r.MilestoneId AND c.Date = r.Date
+	--	where pro.projectid = 666 AND per.PersonId = 3963 AND c.Date = '2013-02-04'
+	GROUP BY pro.ProjectId,Per.PersonId,c.Date,AE.BillableHOursPerDay,AE.NonBillableHoursPerDay,ME.IsHourlyAmount,ME.HoursPerDay,ME.PersonMilestoneDailyAmount,
+	p.Timescale,p.HourlyRate,p.BonusHoursToCollect,p.BonusAmount,p.IsYearBonus,HY.HoursInYear,p.VacationDays,
+	MLFO.OverheadRateTypeId,r.HoursPerDay,r.MilestoneDailyAmount,r.Discount,MLFO.Rate,ME.Amount,ME.Id,ME.ActualHoursPerDay,AE.IsHourlyAmount
