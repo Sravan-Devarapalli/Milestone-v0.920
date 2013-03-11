ef46561b7a278810676a2202a3adcd7e3ed1b394
SainathC <SainathC>
2013-03-11 06:59:00 -0500

    Comments - Defect Number: 3120; Defect RecordID: 3116; Server Address: localhost; Server Port: 99; Database Name: Practice Management; Defect Summary: Milestone Details: Enhancement to look to the PTO hours value to determine how much time should be included in (or exclude fro@; Defect Description: Mail thread..
    From: Matt Reilly <MattR@logic2020.com>
    Date: January 22, 2013, 3:29:25 AM GMT+05:30
    To: "MadhanA (madhana@ggktech.com)" <madhana@ggktech.com>
    Subject: FW: PM Bug?
    Madhan,
    
    On the Project Details  Milestone page, how does PM calculate the Projected Hours value  does it look at the PTO hours a person logs to decrease the hours? Im guessing it does based on the example sited below. Although the e-mail below is referring to the new Time Report enhancement, the Time Report draws its Projected Hours directly from the calculation performed for the Project Details  Milestone which did not change with this enhancement. My supposition is that when calculating Projected Hours, PM looks to see if the resource has PTO on their calendar and if Yes it does not consider that day a Working Day which will decrease the Projected Hours listed for that resource by a full 8 hours. If the check is that simple, I believe PM needs to be enhanced as the actual hours of PTO is not always 8 hours. Therefore, PM should look to the PTO hours value to determine how much time to include in (or exclude from) the Projected Hours calculation.
    
    In the example below, I would have expected Josh Browns Projected Hours to be 175 hours instead of 168 hours since he had only 1 hour of PTO during this period.
    
    Please let me know what you find.
    Thanks,
    Matt
    
    Matthew Reilly, Director of Consulting// Logic20/20 // MattR@logic2020.com
    t.206-576-0491//c.425-503-5825// www.logic2020.com
    
    From: Eric Wan
    Sent: Friday, January 18, 2013 2:09 PM
    To: Matt Reilly
    Subject: PM Bug?
    
    Hi Matt,
    
    After checking GFS Apptio Project P061101 I noticed that Josh is displaying only 168 projected hours for this month, instead of 176:
    
    As far as I can tell his hours have been set up correctly on the Project Resources tab.
    
    I suspect that because Josh entered one hour of PTO on the 15th, PM treated the entire day as a non-billable day. Or is there another reason I didnt check?
    
    Thanks,
    Eric Wan// Logic20/20 // ericw@logic2020.com
    t.425-722-8368//c.206-349-1236// www.logic2020.com

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/DataTransferObjects/DatePoint.cs
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/DataTransferObjects/DatePoint.cs
@@ -6,7 +6,7 @@ using DataTransferObjects.TimeEntry;
 namespace DataTransferObjects
 {
     [Serializable]
-    [DebuggerDisplay("DatePoint: Date = {Date}, Value = {Value}, DayOff = {DayOff}")]
+    [DebuggerDisplay("DatePoint: Date = {Date}, Value = {Value}, DayOff = {DayOff}, CompanyDayOff = {CompanyDayOff} , TimeOffHours = {TimeOffHours}")]
     public class DatePoint : IEquatable<DatePoint>, IComparable<DatePoint>
     {
         #region Properties
@@ -21,13 +21,25 @@ namespace DataTransferObjects
         /// Is this date a daff off for that person
         /// </summary>
         [DataMember]
-        public bool DayOff{ get; set; }
+        public bool DayOff { get; set; }
 
         /// <summary>
         /// Value, can be null
         /// </summary>
         [DataMember]
         public double? Value { get; set; }
+
+        /// <summary>
+        /// Is this date a company daff off for that person
+        /// </summary>
+        [DataMember]
+        public bool CompanyDayOff { get; set; }
+
+        /// <summary>
+        /// time off hours for person
+        /// </summary>
+        [DataMember]
+        public double? TimeOffHours { get; set; }
 
         #endregion
 
@@ -36,11 +48,13 @@ namespace DataTransferObjects
         public static DatePoint Create(CalendarItem cItem, double? val)
         {
             return new DatePoint
-                       {
-                           Date = cItem.Date,
-                           Value = val,
-                           DayOff = cItem.DayOff
-                       };
+            {
+                Date = cItem.Date,
+                Value = val,
+                DayOff = cItem.DayOff,
+                CompanyDayOff = cItem.CompanyDayOff,
+                TimeOffHours = cItem.ActualHours
+            };
         }
 
         public static DatePoint Create(CalendarItem cItem)
@@ -51,23 +65,25 @@ namespace DataTransferObjects
         public DatePoint Clone()
         {
             return new DatePoint
-                       {
-                           Date = Date,
-                           Value = Value,
-                           DayOff = DayOff
-                       };
+            {
+                Date = Date,
+                Value = Value,
+                DayOff = DayOff,
+                CompanyDayOff = CompanyDayOff,
+                TimeOffHours = TimeOffHours
+            };
         }
 
         public static DatePoint FromTimeEntry(TimeEntryRecord te)
         {
             return new DatePoint
-                       {
-                           Date = te.ChargeCodeDate,
-                           Value = te.ActualHours
-                       };
+            {
+                Date = te.ChargeCodeDate,
+                Value = te.ActualHours
+            };
         }
 
-        #endregion    
+        #endregion
 
         #region IEquatable<DatePoint> Members
 
