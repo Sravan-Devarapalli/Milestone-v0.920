ac7a7bce3f4b948b1de17bc005bba8bc334b37f4
SainathC <SainathC>
2013-03-16 03:24:00 -0500

    Comments - Defect Number: 3124; Defect RecordID: 3120; Server Address: localhost; Server Port: 99; Database Name: Practice Management; Defect Summary: Update the margin test functionality to count all the working days in a year, excluding Saturdays and Sundays.; Defect Description: [Madhan.A] As per the existing ongoing functionality in Margin Test the total hours in a year is considered as 2080hrs irrespective of whether the considered year is leap year or not. But total hours ARE considered as 2088hrs in leap year and as 2080hrs for other years in project details calculations. So as to maintain a consistency in the project do you want us to make changes in Margin test page such that it will consider the total hours in leap year as 2088hrs and as 2080hrs for other years? But we think it would be better and accurate if we consider the total hours in a year by explicitly counting all the working days in a year and excluding Saturdays and Sundays instead of taking up fixed hours for a year. Please let us know your suggestion on this.
    [LN] We agree with your recommendation. Please update the margin test functionality to count all the working days in a year, excluding Saturdays and Sundays.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Views/V_WorkinHoursByYear.view.sql
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Views/V_WorkinHoursByYear.view.sql
@@ -1,9 +1,4 @@
 ﻿CREATE VIEW [dbo].[V_WorkinHoursByYear]
 AS 
-	SELECT	cal.[Year],
-			CONVERT(INT,cal.[DaysInYear] * Convert(DECIMAL(4,2),s.Value)) AS HoursInYear
-	FROM dbo.Calendar cal
-	INNER JOIN  Settings s ON s.SettingsKey='DefaultHoursPerDay'
-	GROUP BY cal.[Year],cal.[DaysInYear],s.Value
-	
 
+SELECT	* FROM dbo.WorkinHoursByYear cal
