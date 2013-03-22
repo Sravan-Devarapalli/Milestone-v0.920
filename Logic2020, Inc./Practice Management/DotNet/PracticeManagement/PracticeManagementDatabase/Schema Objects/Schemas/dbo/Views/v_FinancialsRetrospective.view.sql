9d847b3ba167253062bb1aa15b3db3ed811cd3c0
SainathC <SainathC>
2013-03-22 01:41:00 -0500

    Comments - Defect Number: 3120; Defect RecordID: 3116; Server Address: localhost; Server Port: 99; Database Name: Practice Management; Defect Summary: Milestone Details: Enhancement to look to the PTO hours value to determine how much time should be included in (or exclude fro@; Defect Description: Mail thread..
    From: Matt Reilly <MattR@logic2020.com>
    Date: January 22, 2013, 3:29:25 AM GMT+05:30
    To: "MadhanA (madhana@ggktech.com)" <madhana@ggktech.com>
    Subject: FW: PM Bug?
    Madhan,
    
    On the Project Details  Milestone page, how does PM calculate the Projected Hours value  does it look at the PTO hours a person logs to decrease the hours? Im guessing it does based on the example sited below. Although the e-mail below is referring to the new Time Report enhancement, the Time Report draws its Projected Hours directly from the calculation performed for the Project Details  Milestone which did not change with this enhancement. My supposition is that when calculating Projected Hours, PM looks to see if the resource has PTO on their calendar and if Yes it does not consider that day a Working Day which will decrease the Projected Hours listed for that resource by a full 8 hours. If the check is that simple, I believe PM needs to be enhanced as the actual hours of PTO is not always 8 hours. Therefore, PM should look to the PTO hours value to determine how much time to include in (or exclude from) the Projected Hours calculation.
    
    In the example below, I would have expected Josh Browns Projected Hours to be 175 hours instead of 168 hours since he had only 1 hour of PTO during this period.
    
    Please let me know what you find.
    Thanks,
    Matt
    
    Matthew Reilly, Director of Consulting// Logic20/20 // MattR@logic2020.com
    t.206-576-0491//c.425-503-5825// www.logic2020.com
    
    From: Eric Wan
    Sent: Friday, January 18, 2013 2:09 PM
    To: Matt Reilly
    Subject: PM Bug?
    
    Hi Matt,
    
    After checking GFS Apptio Project P061101 I noticed that Josh is displaying only 168 projected hours for this month, instead of 176:
    
    As far as I can tell his hours have been set up correctly on the Project Resources tab.
    
    I suspect that because Josh entered one hour of PTO on the 15th, PM treated the entire day as a non-billable day. Or is there another reason I didnt check?
    
    Thanks,
    Eric Wan// Logic20/20 // ericw@logic2020.com
    t.425-722-8368//c.206-349-1236// www.logic2020.com

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Views/v_FinancialsRetrospective.view.sql
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Views/v_FinancialsRetrospective.view.sql
@@ -2,24 +2,13 @@
 AS
 	SELECT r.ProjectId,
 		   r.MilestoneId,
-	       r.PracticeManagerId,
 		   r.Date,
 		   r.IsHourlyAmount,
-	       CASE
-	           WHEN r.IsHourlyAmount = 1 OR r.HoursPerDay = 0
-	           THEN r.MilestoneDailyAmount
-	           ELSE ISNULL(r.MilestoneDailyAmount * m.HoursPerDay / r.HoursPerDay, r.MilestoneDailyAmount)
-	       END AS MilestoneDailyAmount,
 	       CASE
 	           WHEN r.IsHourlyAmount = 1 OR r.HoursPerDay = 0
 	           THEN ISNULL(m.Amount*m.HoursPerDay, 0)
 	           ELSE ISNULL(r.MilestoneDailyAmount * m.HoursPerDay / r.HoursPerDay, r.MilestoneDailyAmount)
 	       END AS PersonMilestoneDailyAmount,--Entry Level Daily Amount
-	       CASE
-	           WHEN r.IsHourlyAmount = 1 OR r.HoursPerDay = 0
-	           THEN r.MilestoneDailyAmount * r.Discount / 100
-	           ELSE ISNULL(r.MilestoneDailyAmount * m.HoursPerDay * r.Discount / (r.HoursPerDay * 100), r.MilestoneDailyAmount * r.Discount / 100)
-	       END AS DiscountDailyAmount,
 		   CASE
 	           WHEN r.IsHourlyAmount = 1 OR r.HoursPerDay = 0
 	           THEN ISNULL(m.Amount * m.HoursPerDay * r.Discount / 100, 0)
@@ -31,31 +20,13 @@ AS
 		   m.EntryId,
 	       m.EntryStartDate,
 	       m.HoursPerDay AS PersonHoursPerDay,--Entry level Hours Per Day
-		   CASE
-	           WHEN r.IsHourlyAmount = 1
-	           THEN m.Amount
-	           WHEN r.IsHourlyAmount = 0 AND r.HoursPerDay = 0
-	           THEN 0
-	           ELSE r.MilestoneDailyAmount / r.HoursPerDay
-		   END AS BillRate,
-		   CASE
-	           WHEN r.IsHourlyAmount = 1
-	           THEN m.Amount * r.Discount / 100
-	           WHEN r.IsHourlyAmount = 0 AND r.HoursPerDay = 0
-	           THEN 0
-	           ELSE r.MilestoneDailyAmount * r.Discount / (r.HoursPerDay * 100)
-		   END AS DiscountRate,
 		   p.Timescale,
-		   --p.HourlyRate AS PayRate, -- this it how it was before Timescale (4, Hourly/POR)
+		   p.BonusRate,
 		   CASE 
 			WHEN p.Timescale = 4
 			THEN p.HourlyRate * 0.01 * m.Amount
 			ELSE p.HourlyRate
-		   END AS PayRate, 	-- new payrate that takes into account that % unit is used in the Amount instead of $ unit
-	       CASE p.BonusHoursToCollect
-	           WHEN 0 THEN 0
-	           ELSE p.BonusAmount / (CASE WHEN p.IsYearBonus = 1 THEN HY.HoursInYear ELSE p.BonusHoursToCollect END)
-	       END AS BonusRate,
+		   END AS PayRate,
 	       SUM(CASE o.OverheadRateTypeId
 	                       -- Multipliers
 	                       WHEN 2 THEN
@@ -68,46 +39,24 @@ AS
 	                             END) * o.Rate / 100 
 	                       WHEN 4 THEN p.HourlyRate * o.Rate / 100
 	                       -- Fixed
-	                       WHEN 3 THEN o.Rate * 12 / HY.HoursInYear 
+	                       WHEN 3 THEN o.Rate * 12 / r.HoursInYear 
 	                       ELSE o.Rate
 	                   END) AS OverheadRate,
-	                   
-			ISNULL((CASE MLFO.OverheadRateTypeId
-	                       -- Multipliers
-	                       WHEN 2 THEN
-	                           (CASE
-	                                 WHEN r.IsHourlyAmount = 1
-	                                 THEN m.Amount
-	                                 WHEN r.IsHourlyAmount = 0 OR r.HoursPerDay = 0
-	                                 THEN 0
-	                                 ELSE r.MilestoneDailyAmount / r.HoursPerDay
-	                             END) * MLFO.Rate / 100
-	                       WHEN 4 THEN p.HourlyRate * MLFO.Rate / 100
-	                       -- Fixed
-	                       WHEN 3 THEN MLFO.Rate * 12 / HY.HoursInYear
-	                       ELSE MLFO.Rate
-						   END)
-	                   ,0) MLFOverheadRate,
-		   (CASE 
-			WHEN p.Timescale = 4
-			THEN p.HourlyRate * 0.01 * m.Amount
-			ELSE p.HourlyRate END) * ISNULL(p.VacationDays,0) * m.HoursPerDay/HY.HoursInYear VacationRate,
-		   0 AS PracticeManagementCommissionOwn,
-	       0 AS PracticeManagementCommissionSub
+			ISNULL(p.HourlyRate * MLFO.Rate / 100,0) MLFOverheadRate,
+			--Vacation days are allowed only for w2 salary person
+		   (CASE WHEN p.Timescale = 2
+				 THEN ISNULL(p.HourlyRate * p.VacationDays * m.HoursPerDay,0)/r.HoursInYear
+			ELSE 0 END)  VacationRate
 	  FROM dbo.v_MilestoneRevenueRetrospective AS r
 		   -- Linking to persons
 	       INNER JOIN dbo.v_MilestonePersonSchedule m ON m.MilestoneId = r.MilestoneId AND m.Date = r.Date
 	       INNER JOIN dbo.GetFutureDateTable() FD ON 1=1 --For improving query performance we are using table valued function instead of scalar function.
 	       -- Salary
 		   LEFT JOIN dbo.v_PersonPayRetrospective AS p ON p.PersonId = m.PersonId AND p.Date = r.Date
-	       LEFT JOIN dbo.v_OverheadFixedRateTimescale AS o ON p.Date BETWEEN o.StartDate AND ISNULL(o.EndDate, FD.FutureDate)
-															  AND o.Inactive = 0
-															  AND o.TimescaleId = p.Timescale
-		  INNER JOIN V_WorkinHoursByYear HY ON r.date BETWEEN HY.[YearStartDate] AND HY.[YearEndDate]
-		  LEFT JOIN v_MLFOverheadFixedRateTimescale MLFO ON MLFO.TimescaleId = p.Timescale
-								AND r.Date >= MLFO.StartDate 
-								AND (r.Date <= MLFO.EndDate OR MLFO.EndDate IS NULL)
-	GROUP BY r.ProjectId, r.MilestoneId, r.Date, r.MilestoneDailyAmount, r.Discount, p.HourlyRate,p.VacationDays,HY.HoursInYear,
-	         m.Amount, p.BonusAmount,p.IsYearBonus, p.BonusHoursToCollect, p.Timescale, r.HoursPerDay,
-	         r.IsHourlyAmount, m.HoursPerDay, m.PersonId,m.EntryId, m.EntryStartDate, r.PracticeManagerId,MLFO.OverheadRateTypeId,MLFO.Rate
-
+		   LEFT JOIN v_MLFOverheadFixedRateTimescale MLFO ON MLFO.TimescaleId = p.Timescale
+															AND p.Date BETWEEN MLFO.StartDate AND ISNULL(MLFO.EndDate, FD.FutureDate)
+	       LEFT JOIN dbo.v_OverheadFixedRateTimescale AS o ON o.TimescaleId = p.Timescale 
+															AND p.Date BETWEEN o.StartDate AND ISNULL(o.EndDate, FD.FutureDate)
+	GROUP BY r.ProjectId, r.Discount, r.MilestoneId,r.IsHourlyAmount,r.Date,r.MilestoneDailyAmount,r.HoursPerDay,r.HoursInYear,
+			 m.PersonId,m.EntryId, m.EntryStartDate,m.Amount,m.HoursPerDay,
+			 p.Timescale,p.HourlyRate,p.VacationDays,p.BonusRate,MLFO.Rate
