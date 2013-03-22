5d027168f4be08fe5a179812c79763821b2041c3
SainathC <SainathC>
2013-03-22 01:41:00 -0500

    Comments - Defect Number: 3120; Defect RecordID: 3116; Server Address: localhost; Server Port: 99; Database Name: Practice Management; Defect Summary: Milestone Details: Enhancement to look to the PTO hours value to determine how much time should be included in (or exclude fro@; Defect Description: Mail thread..
    From: Matt Reilly <MattR@logic2020.com>
    Date: January 22, 2013, 3:29:25 AM GMT+05:30
    To: "MadhanA (madhana@ggktech.com)" <madhana@ggktech.com>
    Subject: FW: PM Bug?
    Madhan,
    
    On the Project Details  Milestone page, how does PM calculate the Projected Hours value  does it look at the PTO hours a person logs to decrease the hours? Im guessing it does based on the example sited below. Although the e-mail below is referring to the new Time Report enhancement, the Time Report draws its Projected Hours directly from the calculation performed for the Project Details  Milestone which did not change with this enhancement. My supposition is that when calculating Projected Hours, PM looks to see if the resource has PTO on their calendar and if Yes it does not consider that day a Working Day which will decrease the Projected Hours listed for that resource by a full 8 hours. If the check is that simple, I believe PM needs to be enhanced as the actual hours of PTO is not always 8 hours. Therefore, PM should look to the PTO hours value to determine how much time to include in (or exclude from) the Projected Hours calculation.
    
    In the example below, I would have expected Josh Browns Projected Hours to be 175 hours instead of 168 hours since he had only 1 hour of PTO during this period.
    
    Please let me know what you find.
    Thanks,
    Matt
    
    Matthew Reilly, Director of Consulting// Logic20/20 // MattR@logic2020.com
    t.206-576-0491//c.425-503-5825// www.logic2020.com
    
    From: Eric Wan
    Sent: Friday, January 18, 2013 2:09 PM
    To: Matt Reilly
    Subject: PM Bug?
    
    Hi Matt,
    
    After checking GFS Apptio Project P061101 I noticed that Josh is displaying only 168 projected hours for this month, instead of 176:
    
    As far as I can tell his hours have been set up correctly on the Project Resources tab.
    
    I suspect that because Josh entered one hour of PTO on the 15th, PM treated the entire day as a non-billable day. Or is there another reason I didnt check?
    
    Thanks,
    Eric Wan// Logic20/20 // ericw@logic2020.com
    t.425-722-8368//c.206-349-1236// www.logic2020.com

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/FinancialsGetByMilestonePersonsMonthly.proc.sql
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/FinancialsGetByMilestonePersonsMonthly.proc.sql
@@ -23,9 +23,6 @@ AS
 		   ISNULL(f.PayRate,0) PayRate,
 		   f.MLFOverheadRate,
 		   f.PersonHoursPerDay,
-		   f.PracticeManagementCommissionSub,
-		   f.PracticeManagementCommissionOwn ,
-		   f.PracticeManagerId,
 		   f.PersonId,
 		   f.Discount,
 		   f.EntryId
@@ -39,19 +36,13 @@ AS
 		   MPE.EndDate,
 	       C.MonthStartDate AS FinancialDate,
 	       C.MonthEndDate AS MonthEnd,
-
 	       ISNULL(SUM(f.PersonMilestoneDailyAmount), 0) AS Revenue,
-
-	      ISNULL(SUM(f.PersonMilestoneDailyAmount - f.PersonDiscountDailyAmount), 0) AS RevenueNet,
-	       
-
+		   ISNULL(SUM(f.PersonMilestoneDailyAmount - f.PersonDiscountDailyAmount), 0) AS RevenueNet,
 	       ISNULL(SUM((CASE WHEN f.SLHR >= f.PayRate + f.MLFOverheadRate THEN f.SLHR ELSE  f.PayRate + f.MLFOverheadRate END)*ISNULL(f.PersonHoursPerDay, 0)),0) Cogs,
-	       
 	       ISNULL(SUM(f.PersonMilestoneDailyAmount - f.PersonDiscountDailyAmount -
 						(CASE WHEN f.SLHR >= f.PayRate+f.MLFOverheadRate 
 							  THEN f.SLHR ELSE  f.PayRate +f.MLFOverheadRate END) 
 					    *ISNULL(f.PersonHoursPerDay, 0)), 0) GrossMargin,
-
 
 	       ISNULL(SUM(f.PersonHoursPerDay), 0) AS Hours,
 
@@ -59,14 +50,10 @@ AS
 						(CASE WHEN f.SLHR >=  f.PayRate +f.MLFOverheadRate 
 							  THEN f.SLHR ELSE f.PayRate +f.MLFOverheadRate END) 
 					    *ISNULL(f.PersonHoursPerDay, 0))* (f.ProjectSalesCommisionFraction/100))) SalesCommission,
-
-	       SUM((f.PersonMilestoneDailyAmount - f.PersonDiscountDailyAmount -
-	            (CASE WHEN f.SLHR >=  f.PayRate +f.MLFOverheadRate 
-							  THEN f.SLHR ELSE f.PayRate +f.MLFOverheadRate END)  * ISNULL(f.PersonHoursPerDay, 0)) *
-	           (f.PracticeManagementCommissionSub + CASE f.PracticeManagerId WHEN f.PersonId THEN f.PracticeManagementCommissionOwn ELSE 0 END)) / 100 AS PracticeManagementCommission
+			0.0 AS PracticeManagementCommission
 	  FROM FinancialsRetro AS f
 	  INNER JOIN dbo.Calendar C ON C.Date = f.Date
 	  JOIN MilestonePersonEntry MPE ON f.EntryId = MPE.Id AND f.Date BETWEEN MPE.StartDate AND MPE.EndDate
-	 WHERE f.MilestoneId = @MilestoneIdLocal AND f.PersonId IS NOT NULL--AND f.PersonId = @PersonId AND f.EntryStartDate = @EntryStartDate
+	 WHERE f.MilestoneId = @MilestoneIdLocal AND f.PersonId IS NOT NULL
 	GROUP BY f.ProjectId,f.EntryId, f.PersonId, MPE.StartDate, MPE.EndDate, C.MonthStartDate, C.MonthEndDate
 
