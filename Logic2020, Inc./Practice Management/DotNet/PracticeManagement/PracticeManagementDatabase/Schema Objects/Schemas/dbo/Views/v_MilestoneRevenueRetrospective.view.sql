d0380a7f9031446ea08ac3e1928986c99baba934
SainathC <SainathC>
2013-03-22 01:41:00 -0500

    Comments - Defect Number: 3120; Defect RecordID: 3116; Server Address: localhost; Server Port: 99; Database Name: Practice Management; Defect Summary: Milestone Details: Enhancement to look to the PTO hours value to determine how much time should be included in (or exclude fro@; Defect Description: Mail thread..
    From: Matt Reilly <MattR@logic2020.com>
    Date: January 22, 2013, 3:29:25 AM GMT+05:30
    To: "MadhanA (madhana@ggktech.com)" <madhana@ggktech.com>
    Subject: FW: PM Bug?
    Madhan,
    
    On the Project Details  Milestone page, how does PM calculate the Projected Hours value  does it look at the PTO hours a person logs to decrease the hours? Im guessing it does based on the example sited below. Although the e-mail below is referring to the new Time Report enhancement, the Time Report draws its Projected Hours directly from the calculation performed for the Project Details  Milestone which did not change with this enhancement. My supposition is that when calculating Projected Hours, PM looks to see if the resource has PTO on their calendar and if Yes it does not consider that day a Working Day which will decrease the Projected Hours listed for that resource by a full 8 hours. If the check is that simple, I believe PM needs to be enhanced as the actual hours of PTO is not always 8 hours. Therefore, PM should look to the PTO hours value to determine how much time to include in (or exclude from) the Projected Hours calculation.
    
    In the example below, I would have expected Josh Browns Projected Hours to be 175 hours instead of 168 hours since he had only 1 hour of PTO during this period.
    
    Please let me know what you find.
    Thanks,
    Matt
    
    Matthew Reilly, Director of Consulting// Logic20/20 // MattR@logic2020.com
    t.206-576-0491//c.425-503-5825// www.logic2020.com
    
    From: Eric Wan
    Sent: Friday, January 18, 2013 2:09 PM
    To: Matt Reilly
    Subject: PM Bug?
    
    Hi Matt,
    
    After checking GFS Apptio Project P061101 I noticed that Josh is displaying only 168 projected hours for this month, instead of 176:
    
    As far as I can tell his hours have been set up correctly on the Project Resources tab.
    
    I suspect that because Josh entered one hour of PTO on the 15th, PM treated the entire day as a non-billable day. Or is there another reason I didnt check?
    
    Thanks,
    Eric Wan// Logic20/20 // ericw@logic2020.com
    t.425-722-8368//c.206-349-1236// www.logic2020.com

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Views/v_MilestoneRevenueRetrospective.view.sql
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Views/v_MilestoneRevenueRetrospective.view.sql
@@ -1,17 +1,11 @@
-﻿-- =============================================
--- Author:		Anatoliy Lokshin
--- Create date: 9-10-2008
--- Updated by:	Anatoliy Lokshin
--- Update date:	10-13-2008
--- Description:	Selects a revenue day-by-day 
--- =============================================
-CREATE VIEW dbo.v_MilestoneRevenueRetrospective--Day level Milestone Amount & hours
+﻿CREATE VIEW dbo.v_MilestoneRevenueRetrospective--Day level Milestone Amount & hours
 AS
 	WITH CTE 
 	AS 
 	(
 		SELECT s.Date, s.MilestoneId, SUM(HoursPerDay) AS HoursPerDay
 		FROM dbo.v_MilestonePersonSchedule AS s
+		WHERE s.IsHourlyAmount = 0
 		GROUP BY s.Date, s.MilestoneId
 	)
 	SELECT -- Milestones with a fixed amount
@@ -19,32 +13,22 @@ AS
 		m.ProjectId,
 		cal.Date,
 		m.IsHourlyAmount,
-		ISNULL((m.Amount/* Milestone fixed amount */ 
-						/ 
-						NULLIF(MTHours.TotalHours,0)
-							/* Milestone Total  Hours */
-							
-						)* ISNULL(d.HoursPerDay, 0)/* Milestone Total  Hours per day*/,
-				(CASE (DATEDIFF(dd, m.StartDate, m.ProjectedDeliveryDate) + 1)
-					WHEN 0 THEN 0
-					ELSE m.Amount / (DATEDIFF(dd, m.StartDate, m.ProjectedDeliveryDate) + 1)
-				END)) AS MilestoneDailyAmount /* ((Milestone fixed amount/Milestone Total  Hours)* Milestone Total  Hours per day)  */,
+		ISNULL((m.Amount/ NULLIF(MTHours.TotalHours,0))* d.HoursPerDay,0) AS MilestoneDailyAmount /* ((Milestone fixed amount/Milestone Total  Hours)* Milestone Total  Hours per day)  */,
 		m.StartDate AS MilestoneStartDate,
 		m.ProjectedDeliveryDate,
 		p.Discount,
-		prac.PracticeManagerId,
+		HY.HoursInYear,
 		d.HoursPerDay/* Milestone Total  Hours per day*/
-	FROM dbo.Milestone AS m
+	FROM dbo.Project AS p 
+		INNER JOIN dbo.Milestone AS m ON m.ProjectId = p.ProjectId AND P.IsAdministrative = 0 AND P.ProjectId != 174 AND  m.IsHourlyAmount = 0
 		INNER JOIN dbo.Calendar AS cal ON cal.Date BETWEEN m.StartDate AND m.ProjectedDeliveryDate
-		INNER JOIN dbo.Project AS p ON m.ProjectId = p.ProjectId
-		INNER JOIN dbo.Practice AS prac ON p.PracticeId = prac.PracticeId
 		INNER JOIN (
 						SELECT s.MilestoneId, SUM(s.HoursPerDay) AS TotalHours
 						FROM CTE AS s 
 						GROUP BY s.MilestoneId
 					) AS MTHours  ON MTHours.MilestoneId  = m.MilestoneId
-		LEFT JOIN CTE AS d ON d.date = cal.Date and m.MilestoneId = d.MileStoneId
-	WHERE m.IsHourlyAmount = 0
+		INNER JOIN CTE AS d ON d.date = cal.Date and m.MilestoneId = d.MileStoneId
+		INNER JOIN V_WorkinHoursByYear HY ON cal.date BETWEEN HY.[YearStartDate] AND HY.[YearEndDate]
 	UNION ALL
 	SELECT -- Milestones with a hourly amount
 		   mp.MilestoneId,
@@ -55,11 +39,9 @@ AS
 		   MAX(mp.StartDate) AS MilestoneStartDate,
 		   MAX(mp.ProjectedDeliveryDate) AS ProjectedDeliveryDate,
            MAX(p.Discount) AS Discount,
-	       prac.PracticeManagerId,
+		   MAX(HY.HoursInYear) AS HoursInYear,
 	       SUM(mp.HoursPerDay) AS HoursPerDay/* Milestone Total  Hours per day*/
 	  FROM dbo.v_MilestonePersonSchedule mp
-	       INNER JOIN dbo.Project AS p ON mp.ProjectId = p.ProjectId
-	       INNER JOIN dbo.Practice AS prac ON p.PracticeId = prac.PracticeId
-	 WHERE mp.IsHourlyAmount = 1
-	GROUP BY mp.MilestoneId, mp.ProjectId, mp.Date, mp.IsHourlyAmount, prac.PracticeManagerId
-
+	       INNER JOIN dbo.Project AS p ON mp.ProjectId = p.ProjectId AND mp.IsHourlyAmount = 1
+		   INNER JOIN V_WorkinHoursByYear HY ON mp.date BETWEEN HY.[YearStartDate] AND HY.[YearEndDate]
+	GROUP BY mp.MilestoneId, mp.ProjectId, mp.Date, mp.IsHourlyAmount
