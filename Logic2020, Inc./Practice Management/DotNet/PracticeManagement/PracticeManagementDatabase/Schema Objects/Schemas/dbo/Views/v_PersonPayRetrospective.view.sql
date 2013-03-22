c36f98fd46b1cd11c479d25c1caeb8038f0e8415
SainathC <SainathC>
2013-03-22 01:41:00 -0500

    Comments - Defect Number: 3120; Defect RecordID: 3116; Server Address: localhost; Server Port: 99; Database Name: Practice Management; Defect Summary: Milestone Details: Enhancement to look to the PTO hours value to determine how much time should be included in (or exclude fro@; Defect Description: Mail thread..
    From: Matt Reilly <MattR@logic2020.com>
    Date: January 22, 2013, 3:29:25 AM GMT+05:30
    To: "MadhanA (madhana@ggktech.com)" <madhana@ggktech.com>
    Subject: FW: PM Bug?
    Madhan,
    
    On the Project Details  Milestone page, how does PM calculate the Projected Hours value  does it look at the PTO hours a person logs to decrease the hours? Im guessing it does based on the example sited below. Although the e-mail below is referring to the new Time Report enhancement, the Time Report draws its Projected Hours directly from the calculation performed for the Project Details  Milestone which did not change with this enhancement. My supposition is that when calculating Projected Hours, PM looks to see if the resource has PTO on their calendar and if Yes it does not consider that day a Working Day which will decrease the Projected Hours listed for that resource by a full 8 hours. If the check is that simple, I believe PM needs to be enhanced as the actual hours of PTO is not always 8 hours. Therefore, PM should look to the PTO hours value to determine how much time to include in (or exclude from) the Projected Hours calculation.
    
    In the example below, I would have expected Josh Browns Projected Hours to be 175 hours instead of 168 hours since he had only 1 hour of PTO during this period.
    
    Please let me know what you find.
    Thanks,
    Matt
    
    Matthew Reilly, Director of Consulting// Logic20/20 // MattR@logic2020.com
    t.206-576-0491//c.425-503-5825// www.logic2020.com
    
    From: Eric Wan
    Sent: Friday, January 18, 2013 2:09 PM
    To: Matt Reilly
    Subject: PM Bug?
    
    Hi Matt,
    
    After checking GFS Apptio Project P061101 I noticed that Josh is displaying only 168 projected hours for this month, instead of 176:
    
    As far as I can tell his hours have been set up correctly on the Project Resources tab.
    
    I suspect that because Josh entered one hour of PTO on the 15th, PM treated the entire day as a non-billable day. Or is there another reason I didnt check?
    
    Thanks,
    Eric Wan// Logic20/20 // ericw@logic2020.com
    t.425-722-8368//c.206-349-1236// www.logic2020.com

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Views/v_PersonPayRetrospective.view.sql
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Views/v_PersonPayRetrospective.view.sql
@@ -4,21 +4,15 @@ AS
 	       cal.Date,
 	       p.Amount AS Rate,
 	       p.Timescale,
-	       t.Name AS TimescaleName,
 	       CASE
 	           WHEN p.Timescale IN (1, 3, 4)
 	           THEN p.Amount
 	           ELSE p.Amount / HY.HoursInYear
 	       END AS HourlyRate,
-	       p.BonusAmount,
-           p.BonusHoursToCollect,
-           p.PracticeId,
-		   p.VacationDays,
-		   CAST(CASE p.BonusHoursToCollect WHEN GHY.HoursPerYear THEN 1 ELSE 0 END AS BIT) AS IsYearBonus
+  		   p.BonusAmount / (CASE WHEN p.BonusHoursToCollect = GHY.HoursPerYear THEN HY.HoursInYear ELSE NULLIF(p.BonusHoursToCollect,0) END) AS BonusRate,
+		   p.VacationDays
 	  FROM dbo.PersonCalendarAuto AS cal
-	       INNER JOIN dbo.Pay AS p 
-	           ON cal.PersonId = p.Person AND p.StartDate <= cal.Date AND p.EndDate > cal.date  
-	       INNER JOIN dbo.Timescale AS t ON p.Timescale = t.TimescaleId
+	       INNER JOIN dbo.Pay AS p ON cal.PersonId = p.Person AND p.StartDate <= cal.Date AND p.EndDate > cal.date  
 		   INNER JOIN dbo.[BonusHoursPerYearTable]() GHY ON 1=1--For improving query performance we are using table valued function instead of scalar function.
 	       INNER JOIN V_WorkinHoursByYear HY ON cal.Date BETWEEN HY.[YearStartDate] AND HY.[YearEndDate]
 
