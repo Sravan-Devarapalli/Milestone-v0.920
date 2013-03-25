602454152ad58071f56c3956b84dd8526f76a020
SainathC <SainathC>
2013-03-25 01:52:00 -0500

    Comments - Defect Number: 3129; Defect RecordID: 3125; Server Address: localhost; Server Port: 99; Database Name: Practice Management; Defect Summary: Reports > Resources > Consulting Demand Report Enhancements; Defect Description: The attached mock-ups describe new enhancements we are requesting to be made to the current Consulting Demand Report. These enhancements will replace the current report, and the current report can be deprecated from the system. While the fundamental data pieces and calculations behind the report are not changing, the enhancements center around restructuring how the data is presented, including various slices both in tabular and graphical formats. From a overal formatting perspective, the new Consulting Demand Report should be structured very similarly to the current Human Capital and Time Reports. The same types of functionality, such as the utilization of hover, pop-ups, and sortable columns will be used.
    There will be one nomenclature change. The current value of "Role" as it applies to Strawmen contained Practice Management should be changed to "Title". As a result, when adding or editing a Strawman, the input field title of "Role" should be changed to "Title."
    
    Please review the attached requirements and provide a high-level estimate of when the enhancements could be delivered to UAT.

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/GetConsultantDemandForPeriod.proc.sql
@@ -0,0 +1,110 @@
+﻿/* Views in Consultant Demand Pages:
+1.Summary View- @StartDate,@EndDate,@IsSummary
+2.Details View -@StartDate,@EndDate,@IsDetail
+	a.Group by Month-@GroupByMonth,@ViewByTitleSkill
+	b.Group by Title/Skill-@GroupByTitleSkill,@ViewByTitleSkill
+	c.Group by Title :@GroupByTitle,@ViewBySkill
+	d.Group by Skill :@GroupBySkill,@ViewByTitle
+3.Graphs View  @StartDate,@EndDate,@IsGraph,
+	a.Group By Month: @GroupByMonth
+		(i).View By Title  : @ViewByTitle
+		(ii).View By Skill : @ViewBySkill
+	b.Group By Title :@GroupByTitle,@ViewByTitle
+	c.Group By Skill :@GroupBySkill,@ViewBySkill
+*/
+CREATE PROCEDURE dbo.GetConsultantDemandForPeriod
+ (
+	@StartDate DATETIME,
+	@EndDate DATETIME,
+	@Titles VARCHAR(MAX) = NULL,
+	@Skills VARCHAR(MAX) = NULL,
+	--Report Type
+	@IsSummary BIT=0,
+	@IsDetail BIT=0,
+	@IsGraph BIT=0,
+	--Group By
+	@GroupByMonth BIT=0,
+	@GroupByTitle BIT=0,
+	@GroupBySkill BIT=0,
+	@GroupByTitleSkill BIT=0,
+	--View By
+	@ViewByTitle BIT = 0,
+	@ViewBySkill BIT = 0,
+	@ViewByTitleSkill BIT = 0
+)
+AS
+BEGIN
+
+	DECLARE @Query NVARCHAR(4000) = ' FROM [dbo].[v_ConsultingDemand] CD ',
+			@GroupBy NVARCHAR(500) = ' GROUP BY ',
+			@OrderBy NVARCHAR(500) = ' ORDER BY ',
+			@Select NVARCHAR(500) = 'SELECT ',
+			@Where  NVARCHAR(500) = ' WHERE CD.MonthStartDate BETWEEN DATEADD(D,-DAY(@StartDate)+1,@StartDate) AND @EndDate'+
+									CASE WHEN @Titles IS NOT NULL THEN ' AND CD.Title IN (''' + REPLACE(@Titles,',',''',''') + ''')'  ELSE '' END +
+									CASE WHEN @Skills IS NOT NULL THEN ' AND CD.Skill IN (''' + REPLACE(@Skills,',',''',''') + ''')' ELSE '' END ,
+			@Columns NVARCHAR(500) = ''
+
+	--1.Summary View
+	IF @IsSummary=1
+	BEGIN
+
+		SELECT @GroupBy = @GroupBy + 'CD.MonthStartDate,CD.Title,CD.Skill',
+				@OrderBy = @OrderBy + 'CD.MonthStartDate,CD.Title,CD.Skill',
+				@Select = @Select+ 'CD.MonthStartDate,CD.Title,CD.Skill,SUM(COUNT) AS [COUNT]'
+
+	END
+	--2.Details View 
+	ELSE IF @IsDetail=1 
+	BEGIN
+		
+		SELECT @Columns = 'CD.ClientId,CD.AccountName,CD.ProjectId,CD.ProjectNumber,CD.ProjectName,CD.OpportunityId,CD.OpportunityNumber,CD.ResourceStartDate' + 
+			CASE 
+				WHEN @GroupByMonth = 1 THEN ',CD.MonthStartDate'
+				WHEN @GroupByTitleSkill = 1 THEN ',CD.Title,CD.Skill'
+				WHEN @GroupByTitle = 1 THEN ',CD.Title'
+				WHEN @GroupBySkill = 1 THEN ',CD.Skill'
+				ELSE ''
+			END + 
+			CASE 
+				WHEN @GroupByTitleSkill = 0 AND @ViewByTitleSkill = 1 THEN ',CD.Title,CD.Skill'
+				WHEN @GroupByTitleSkill = 0 AND @GroupByTitle = 0 AND @ViewByTitle = 1 THEN ',CD.Title'
+				WHEN @GroupByTitleSkill = 0 AND @GroupBySkill = 0 AND @ViewBySkill = 1 THEN ',CD.Skill'
+				ELSE ''
+			END,
+			@OrderBy = @OrderBy + ' 1 '
+			SELECT	@Select = @Select + @Columns + ',SUM(COUNT) AS [COUNT]',
+					@GroupBy = @GroupBy + @Columns
+
+	END
+	--2.Graph View 
+	ELSE IF @IsGraph=1 
+	BEGIN
+
+	SELECT @Columns =
+			CASE 
+				WHEN @GroupByMonth = 1 THEN 'CD.MonthStartDate'
+				WHEN @GroupByTitleSkill = 1 THEN 'CD.Title,CD.Skill'
+				WHEN @GroupByTitle = 1 THEN 'CD.Title'
+				WHEN @GroupBySkill = 1 THEN 'CD.Skill'
+				ELSE ''
+			END + 
+			CASE 
+				WHEN @GroupByTitleSkill = 0 AND @ViewByTitleSkill = 1 THEN ',CD.Title,CD.Skill'
+				WHEN @GroupByTitleSkill = 0 AND @GroupByTitle = 0 AND @ViewByTitle = 1 THEN ',CD.Title'
+				WHEN @GroupByTitleSkill = 0 AND @GroupBySkill = 0 AND @ViewBySkill = 1 THEN ',CD.Skill'
+				ELSE ''
+			END,
+			@OrderBy = @OrderBy + ' 1 '
+
+			SELECT	@Select = @Select + @Columns + ',SUM(COUNT) AS [COUNT]',
+					@GroupBy = @GroupBy + @Columns
+
+	END
+	
+	SELECT @Query = @Select + @Query + @Where + @GroupBy + @OrderBy
+
+	--Print @Query
+	EXEC sp_executeSql  @Query,N'@StartDate DATETIME,@EndDate DATETIME',@StartDate=@StartDate,@EndDate=@EndDate
+
+END
+
