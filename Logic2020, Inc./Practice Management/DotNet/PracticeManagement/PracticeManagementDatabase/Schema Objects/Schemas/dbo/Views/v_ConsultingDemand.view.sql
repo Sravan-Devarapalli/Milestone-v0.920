ed6fed795ddd9114852578dc9ffb9009b8d5f0de
SainathC <SainathC>
2013-03-25 01:52:00 -0500

    Comments - Defect Number: 3129; Defect RecordID: 3125; Server Address: localhost; Server Port: 99; Database Name: Practice Management; Defect Summary: Reports > Resources > Consulting Demand Report Enhancements; Defect Description: The attached mock-ups describe new enhancements we are requesting to be made to the current Consulting Demand Report. These enhancements will replace the current report, and the current report can be deprecated from the system. While the fundamental data pieces and calculations behind the report are not changing, the enhancements center around restructuring how the data is presented, including various slices both in tabular and graphical formats. From a overal formatting perspective, the new Consulting Demand Report should be structured very similarly to the current Human Capital and Time Reports. The same types of functionality, such as the utilization of hover, pop-ups, and sortable columns will be used.
    There will be one nomenclature change. The current value of "Role" as it applies to Strawmen contained Practice Management should be changed to "Title". As a result, when adding or editing a Strawman, the input field title of "Role" should be changed to "Title."
    
    Please review the attached requirements and provide a high-level estimate of when the enhancements could be delivered to UAT.

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Views/v_ConsultingDemand.view.sql
@@ -0,0 +1,69 @@
+﻿CREATE VIEW [dbo].[v_ConsultingDemand]
+	AS 
+	SELECT	CAL.MonthStartDate,
+			C.ClientId,
+			C.Name AS AccountName,
+			P.ProjectId,
+			P.ProjectNumber,
+			P.Name AS ProjectName,
+			O.OpportunityId,
+			O.OpportunityNumber,
+			Per.Personid,
+			Per.FirstName AS Skill,
+			Per.LastName AS Title,
+			CASE WHEN MPE.StartDate < CAL.MonthStartDate THEN CAL.MonthStartDate ELSE MPE.StartDate END ResourceStartDate,
+			COUNT(DISTINCT MPE.Id) as [Count]
+	FROM dbo.Person Per
+	INNER JOIN dbo.MilestonePerson MP ON MP.PersonId =Per.PersonId 
+									AND Per.IsStrawman=1
+	INNER JOIN dbo.Milestone M ON MP.MilestoneId=M.MilestoneId
+	INNER JOIN Project P ON M.ProjectId = P.ProjectId 
+	INNER JOIN Client C ON P.ClientId=C.ClientId
+	INNER JOIN MilestonePersonEntry MPE ON MPE.MilestonePersonId=MP.MilestonePersonId 
+	INNER JOIN dbo.Calendar CAL ON MPE.StartDate<=cal.MonthEndDate 
+								AND cal.MonthStartDate <= MPE.EndDate
+	LEFT JOIN Opportunity O ON O.ProjectId=P.ProjectId
+	GROUP BY CAL.MonthStartDate,
+			 C.ClientId,
+			 C.Name,
+			 P.ProjectId,
+			 P.Name,
+			 P.ProjectNumber,
+			 O.OpportunityId,
+			 O.OpportunityNumber,
+			 Per.PersonId,
+			 Per.FirstName,
+			 Per.LastName,
+			 MPE.StartDate
+	UNION 
+	SELECT CAL.MonthStartDate,
+		   C.ClientId,
+		   C.Name AS AccountName,
+		   -1 AS ProjectId,
+		   '' AS ProjectNumber,
+		   O.Name AS ProjectName,
+		   O.OpportunityId,
+		   O.OpportunityNumber,
+		   Per.PersonId,
+		   Per.FirstName,
+		   Per.LastName,
+		   CASE WHEN OP.NeedBy<cal.MonthStartDate THEN cal.MonthStartDate ELSE OP.NeedBy END ResourceStartDate,
+		   SUM(OP.Quantity)/MAX(CAl.DaysInMonth) as [Count]
+	FROM dbo.Opportunity O 
+	INNER JOIN Client C ON O.ClientId=C.ClientId
+							AND O.ProjectId IS NULL 
+	INNER JOIN dbo.OpportunityPersons OP ON OP.RelationTypeId = 2 
+										AND OP.OpportunityId=O.OpportunityId 
+	INNER JOIN dbo.Person Per ON OP.PersonId=Per.PersonId
+	INNER JOIN dbo.Calendar CAL ON OP.NeedBy between CAL.MonthStartDate AND CAL.MonthEndDate
+	GROUP BY CAL.MonthStartDate,
+			 C.ClientId,
+			 C.Name,
+			 O.OpportunityId,
+			 O.OpportunityNumber,
+			 O.Name,
+			 Per.PersonId,
+			 Per.FirstName,
+			 Per.LastName,
+			 OP.NeedBy
+
