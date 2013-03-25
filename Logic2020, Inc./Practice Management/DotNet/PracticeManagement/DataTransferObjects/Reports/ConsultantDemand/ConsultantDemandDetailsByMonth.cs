7ea17edba7e11749d5b4b4de2fe678293e01ef07
SainathC <SainathC>
2013-03-25 01:52:00 -0500

    Comments - Defect Number: 3129; Defect RecordID: 3125; Server Address: localhost; Server Port: 99; Database Name: Practice Management; Defect Summary: Reports > Resources > Consulting Demand Report Enhancements; Defect Description: The attached mock-ups describe new enhancements we are requesting to be made to the current Consulting Demand Report. These enhancements will replace the current report, and the current report can be deprecated from the system. While the fundamental data pieces and calculations behind the report are not changing, the enhancements center around restructuring how the data is presented, including various slices both in tabular and graphical formats. From a overal formatting perspective, the new Consulting Demand Report should be structured very similarly to the current Human Capital and Time Reports. The same types of functionality, such as the utilization of hover, pop-ups, and sortable columns will be used.
    There will be one nomenclature change. The current value of "Role" as it applies to Strawmen contained Practice Management should be changed to "Title". As a result, when adding or editing a Strawman, the input field title of "Role" should be changed to "Title."
    
    Please review the attached requirements and provide a high-level estimate of when the enhancements could be delivered to UAT.

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/DataTransferObjects/Reports/ConsultantDemand/ConsultantDemandDetailsByMonth.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+
+namespace DataTransferObjects.Reports.ConsultingDemand
+{
+
+    [DataContract]
+    [Serializable]
+    public class ConsultantDemandDetailsByMonth
+    {
+        [DataMember]
+        public string Title { get; set; }
+       [DataMember]
+        public string Skill { get; set; }
+        [DataMember]
+        public string OpportunityNumber { get; set; }
+        [DataMember]
+        public string ProjectNumber { get; set; }
+        [DataMember]
+        public int? OpportunityId { get; set; }
+        [DataMember]
+        public int? ProjectId { get; set; }
+        [DataMember]
+        public string ProjectName { get; set; }
+        [DataMember]
+        public int AccountId { get; set; }
+        [DataMember]
+        public string AccountName { get; set; }
+        [DataMember]
+        public DateTime ResourceStartDate { get; set; }
+        [DataMember]
+        public int Count { get; set; }
+    }
+}
+
