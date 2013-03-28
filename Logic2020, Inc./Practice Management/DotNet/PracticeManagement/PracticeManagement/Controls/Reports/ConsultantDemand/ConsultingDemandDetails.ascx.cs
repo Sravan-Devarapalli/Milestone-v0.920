71ac0eab39399d5a8d6e64228364d8d211bf1df5
SainathC <SainathC>
2013-03-28 05:05:00 -0500

    Comments - Defect Number: 3129; Defect RecordID: 3125; Server Address: localhost; Server Port: 99; Database Name: Practice Management; Defect Summary: Reports > Resources > Consulting Demand Report Enhancements; Defect Description: The attached mock-ups describe new enhancements we are requesting to be made to the current Consulting Demand Report. These enhancements will replace the current report, and the current report can be deprecated from the system. While the fundamental data pieces and calculations behind the report are not changing, the enhancements center around restructuring how the data is presented, including various slices both in tabular and graphical formats. From a overal formatting perspective, the new Consulting Demand Report should be structured very similarly to the current Human Capital and Time Reports. The same types of functionality, such as the utilization of hover, pop-ups, and sortable columns will be used.
    There will be one nomenclature change. The current value of "Role" as it applies to Strawmen contained Practice Management should be changed to "Title". As a result, when adding or editing a Strawman, the input field title of "Role" should be changed to "Title."
    
    Please review the attached requirements and provide a high-level estimate of when the enhancements could be delivered to UAT.

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/Controls/Reports/ConsultantDemand/ConsultingDemandDetails.ascx.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Script.Serialization;
+using System.Web.UI.WebControls;
+using AjaxControlToolkit;
+using DataTransferObjects.Reports.ConsultingDemand;
+
+namespace PraticeManagement.Controls.Reports.ConsultantDemand
+{
+    public partial class ConsultingDemandDetails : System.Web.UI.UserControl
+    {
+        private PraticeManagement.Reports.ConsultingDemand_New HostingPage
+        {
+            get { return ((PraticeManagement.Reports.ConsultingDemand_New)Page); }
+        }
+
+        public int GrandTotal = 0;
+
+        public HiddenField _hdTitle
+        {
+            get
+            {
+                return hdTitle;
+            }
+        }
+
+        public HiddenField _hdSkill
+        {
+            get
+            {
+                return hdSkill;
+            }
+        }
+
+        public HiddenField _hdIsSummaryPage
+        {
+            get
+            {
+                return hdIsSummaryPage;
+            }
+        }
+
+        public string groupBy
+        {
+            get
+            {
+                return hdnGroupBy.Value;
+            }
+
+            set
+            {
+                hdnGroupBy.Value = value;
+                if (value == "month")
+                {
+                    btnGroupBy.ToolTip = btnGroupBy.Text = "Group By Title";
+                }
+                else
+                {
+                    btnGroupBy.ToolTip = btnGroupBy.Text = "Group By Month";
+                }
+                
+            }
+        }
+
+        private List<KeyValuePair<string, string>> CollapsiblePanelExtenderClientIds
+        {
+            get;
+            set;
+        }
+
+        private List<string> CollapsiblePanelDateExtenderClientIds
+        {
+            get;
+            set;
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            btnExpandOrCollapseAll.Attributes["onclick"] = "return CollapseOrExpandAll(" + btnExpandOrCollapseAll.ClientID +
+                                                          ", " + hdnCollapsed.ClientID +
+                                                          ", " + hdncpeExtendersIds.ClientID +
+                                                          ");";
+
+            btnExpandOrCollapseAll.Text = btnExpandOrCollapseAll.ToolTip = (hdnCollapsed.Value.ToLower() == "true") ? "Expand All" : "Collapse All";
+            if (!IsPostBack)
+            {
+                hdnGroupBy.Value = "title";
+            }
+        }
+
+        protected void btnGroupBy_OnClick(object sender, EventArgs e)
+        {
+            if (groupBy == "month")
+            {
+                hdnGroupBy.Value = "title";
+            }
+            else if (groupBy == "title")
+            {
+                hdnGroupBy.Value = "month";
+            }
+            PopulateData();
+
+            if (!string.IsNullOrEmpty(_hdIsSummaryPage.Value) && _hdIsSummaryPage.Value == true.ToString())
+            {
+                var hostingPage = (PraticeManagement.Reports.ConsultingDemand_New)Page;
+                hostingPage.SummaryControl.ConsultantDetailPopup.Show();
+            }
+
+        }
+
+        protected void repByTitleSkill_ItemDataBound(object sender, RepeaterItemEventArgs e)
+        {
+
+            if (e.Item.ItemType == ListItemType.Header)
+            {
+                CollapsiblePanelDateExtenderClientIds = new List<string>();
+            }
+
+            if (e.Item.ItemType == ListItemType.Item || e.Item.ItemType == ListItemType.AlternatingItem)
+            {
+                Repeater repDetails = (Repeater)e.Item.FindControl("repDetails");
+                ConsultantGroupbyTitleSkill dataitem = (ConsultantGroupbyTitleSkill)e.Item.DataItem;
+                var result = dataitem.ConsultantDetails;
+                repDetails.DataSource = result;
+                var cpeDetails = e.Item.FindControl("cpeDetails") as CollapsiblePanelExtender;
+                cpeDetails.BehaviorID = Guid.NewGuid().ToString();
+                CollapsiblePanelDateExtenderClientIds.Add(cpeDetails.BehaviorID);
+                repDetails.DataBind();
+            }
+            else if (e.Item.ItemType == ListItemType.Footer)
+            {
+                JavaScriptSerializer jss = new JavaScriptSerializer();
+                var output = jss.Serialize(CollapsiblePanelDateExtenderClientIds);
+                hdncpeExtendersIds.Value = output;
+                btnExpandOrCollapseAll.Text = btnExpandOrCollapseAll.ToolTip = "Expand All";
+                hdnCollapsed.Value = "true";
+            }
+        }
+
+        protected void repByMonth_ItemDataBound(object sender, RepeaterItemEventArgs e)
+        {
+            if (e.Item.ItemType == ListItemType.Header)
+            {
+                CollapsiblePanelDateExtenderClientIds = new List<string>();
+            }
+
+            if (e.Item.ItemType == ListItemType.Item || e.Item.ItemType == ListItemType.AlternatingItem)
+            {
+                Repeater repDetails = (Repeater)e.Item.FindControl("repDetails");
+                ConsultantGroupByMonth dataitem = (ConsultantGroupByMonth)e.Item.DataItem;
+                var result = dataitem.ConsultantDetailsByMonth;
+                repDetails.DataSource = result;
+                var cpeDetails = e.Item.FindControl("cpeDetails") as CollapsiblePanelExtender;
+                cpeDetails.BehaviorID = Guid.NewGuid().ToString();
+                CollapsiblePanelDateExtenderClientIds.Add(cpeDetails.BehaviorID);
+                repDetails.DataBind();
+            }
+            else if (e.Item.ItemType == ListItemType.Footer)
+            {
+                JavaScriptSerializer jss = new JavaScriptSerializer();
+                var output = jss.Serialize(CollapsiblePanelDateExtenderClientIds);
+                hdncpeExtendersIds.Value = output;
+                btnExpandOrCollapseAll.Text = btnExpandOrCollapseAll.ToolTip = "Expand All";
+                hdnCollapsed.Value = "true";
+            }
+        }
+
+        public void PopulateData()
+        {
+            string title = hdTitle.Value;
+            string skill = hdSkill.Value;
+            string groupby = hdnGroupBy.Value;
+            if (groupby == "title")
+            {
+                repByMonth.Visible = false;
+                repByTitleSkill.Visible = true;
+                List<ConsultantGroupbyTitleSkill> data = ServiceCallers.Custom.Report(r => r.ConsultingDemandDetailsByTitleSkill(HostingPage.StartDate.Value, HostingPage.EndDate.Value, title, skill)).ToList();
+                GrandTotal = data.Sum(p => p.TotalCount);
+                repByTitleSkill.DataSource = data;
+                repByTitleSkill.DataBind();
+            }
+            else if (groupby == "month")
+            {
+                repByTitleSkill.Visible = false;
+                repByMonth.Visible = true;
+                List<ConsultantGroupByMonth> data = ServiceCallers.Custom.Report(r => r.ConsultingDemandDetailsByMonth(HostingPage.StartDate.Value, HostingPage.EndDate.Value, title, skill)).ToList();
+                GrandTotal = data.Sum(p => p.TotalCount);
+                repByMonth.DataSource = data;
+                repByMonth.DataBind();
+            }
+        }
+    }
+}
