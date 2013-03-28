f1d01c19d73a9515dd721e51458585efe56c405b
SainathC <SainathC>
2013-03-28 05:06:00 -0500

    Comments - Defect Number: 3129; Defect RecordID: 3125; Server Address: localhost; Server Port: 99; Database Name: Practice Management; Defect Summary: Reports > Resources > Consulting Demand Report Enhancements; Defect Description: The attached mock-ups describe new enhancements we are requesting to be made to the current Consulting Demand Report. These enhancements will replace the current report, and the current report can be deprecated from the system. While the fundamental data pieces and calculations behind the report are not changing, the enhancements center around restructuring how the data is presented, including various slices both in tabular and graphical formats. From a overal formatting perspective, the new Consulting Demand Report should be structured very similarly to the current Human Capital and Time Reports. The same types of functionality, such as the utilization of hover, pop-ups, and sortable columns will be used.
    There will be one nomenclature change. The current value of "Role" as it applies to Strawmen contained Practice Management should be changed to "Title". As a result, when adding or editing a Strawman, the input field title of "Role" should be changed to "Title."
    
    Please review the attached requirements and provide a high-level estimate of when the enhancements could be delivered to UAT.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/DataTransferObjects/Reports/ConsultantDemand/ConsultantGroupbyTitleSkill.cs
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/DataTransferObjects/Reports/ConsultantDemand/ConsultantGroupbyTitleSkill.cs
@@ -12,13 +12,13 @@ namespace DataTransferObjects.Reports.ConsultingDemand
     {
         [DataMember]
         public string Title { get; set; }
-        
+
         [DataMember]
         public string Skill { get; set; }
-        
+
         [DataMember]
-        public Dictionary<string, int> Count { get; set; }
-        
+        public Dictionary<string, int> MonthCount { get; set; }
+
         [DataMember]
         public List<ConsultantDemandDetails> ConsultantDetails { get; set; }
 
@@ -30,11 +30,19 @@ namespace DataTransferObjects.Reports.ConsultingDemand
                 {
                     return ConsultantDetails.Sum(p => p.Count);
                 }
-                else if(Count != null)
+                else if (MonthCount != null)
                 {
-                    return Count.Values.Sum();
+                    return MonthCount.Values.Sum();
                 }
                 return 0;
+            }
+        }
+
+        public string TitleSkill
+        {
+            get
+            {
+                return Title + "," + Skill;
             }
         }
 
