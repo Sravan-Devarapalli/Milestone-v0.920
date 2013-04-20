708f422d5112fef96dd1bdea71fbb4dfd2fe000f
SainathC <SainathC>
2013-04-20 04:53:00 -0500

    none

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Functions/IsProjectGroupInUse.function.sql
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Functions/IsProjectGroupInUse.function.sql
@@ -1,12 +1,16 @@
 ﻿CREATE FUNCTION [dbo].[IsProjectGroupInUse]
 (
-	@ProjectGroupId int
+	@ProjectGroupId INT,
+	@Code VARCHAR(6)
 )
 RETURNS INT
 AS
 BEGIN
-
-	IF EXISTS(SELECT TOP 1 1 FROM dbo.Project WHERE GroupId = @ProjectGroupId)
+	IF(@Code = 'B0001')
+	BEGIN
+		RETURN 1
+	END
+	ELSE IF EXISTS(SELECT TOP 1 1 FROM dbo.Project WHERE GroupId = @ProjectGroupId)
 	BEGIN
 		RETURN 1
 	END
