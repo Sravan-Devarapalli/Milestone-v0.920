82d9e26e5f9afcdf5d2d01fe4ba7e9f1d5059cec
SainathC <SainathC>
2013-05-01 05:05:00 -0500

    Comments - Defect Number: 3136; Defect RecordID: 3132; Server Address: localhost; Server Port: 99; Database Name: Practice Management; Defect Summary: Project Details > New "CSAT" tab; Defect Description: In order to capture and track customer satisfaction ("CSAT") on our project engagements, we are requesting that a new tab be created on the Project Details page. The tab, using the similar functionality already existing in Practice Management, consists of a table where summary information is listed. Click "Add CSAT" opens a screen to enter required data and free text fields.
    Business requirements and a mock-up of the new tab are attached. In addition, we are also working on requirements for a reporting component for CSAT, and will provide the specs as soon as we have them drafted.
    
    Please review the mock-up attached and provide us a level of effort estimate to complete this first phase of enhancements.

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/GetMilestoneAndCSATCountsByProject.proc.sql
@@ -0,0 +1,11 @@
+﻿CREATE PROCEDURE [dbo].[GetMilestoneAndCSATCountsByProject]
+(
+	@ProjectId   INT
+)
+AS
+BEGIN
+
+	SELECT	(SELECT COUNT(*) FROM dbo.Milestone M WHERE ProjectId= @ProjectId) AS MilestoneCount,
+			(SELECT COUNT(*) FROM dbo.ProjectCSAT PC WHERE ProjectId= @ProjectId) AS CSATCount
+
+END
