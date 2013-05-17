d7e48042de4a13da70c284ed3d4b16b08e394551
SainathC <SainathC>
2013-05-17 08:50:00 -0500

    Comments - Defect Number: 3136; Defect RecordID: 3132; Server Address: localhost; Server Port: 99; Database Name: Practice Management; Defect Summary: Project Details > New "CSAT" tab; Defect Description: In order to capture and track customer satisfaction ("CSAT") on our project engagements, we are requesting that a new tab be created on the Project Details page. The tab, using the similar functionality already existing in Practice Management, consists of a table where summary information is listed. Click "Add CSAT" opens a screen to enter required data and free text fields.
    Business requirements and a mock-up of the new tab are attached. In addition, we are also working on requirements for a reporting component for CSAT, and will provide the specs as soon as we have them drafted.
    
    Please review the mock-up attached and provide us a level of effort estimate to complete this first phase of enhancements.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/CSATSummaryReport.proc.sql
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/CSATSummaryReport.proc.sql
@@ -24,8 +24,7 @@ BEGIN
 		  SELECT PC.ProjectId, MAX(ModifiedDate) AS ModifiedDate,COUNT(*) AS NumberOfCSATs
 		  FROM dbo.ProjectCSAT PC
 		  INNER JOIN dbo.Project P ON pc.ProjectId=p.ProjectId AND P.ProjectStatusId IN (3,4)
-		  WHERE PC.ReviewEndDate BETWEEN @StartDate AND @EndDate
-		        AND PC.CompletionDate BETWEEN @StartDate AND @EndDate
+		  WHERE PC.CompletionDate BETWEEN @StartDate AND @EndDate
 		        AND	(
 						@PracticeIds IS NULL
 						OR P.PracticeId IN ( SELECT Ids FROM  @PracticeIdsTable)
@@ -72,20 +71,21 @@ BEGIN
 			    CSATReviewer.LastName+', '+CSATReviewer.FirstName AS CSATReviewer,
 				PCSAT.Comments
 		FROM Project P
-		INNER JOIN ProjectsRecentlyUpdatedCSATS PRC ON P.ProjectId = PRC.ProjectId 
-		INNER JOIN dbo.Client C ON C.ClientId = P.ClientId
+		INNER JOIN dbo.Client C ON C.ClientId = P.ClientId AND P.ProjectStatusId IN (3,4) AND p.IsAllowedToShow =1
 		INNER JOIN dbo.ProjectGroup PG ON PG.GroupId = P.GroupId 
 		INNER JOIN dbo.BusinessGroup BG ON BG.BusinessGroupId = PG.BusinessGroupId
 		INNER JOIN dbo.ProjectStatus PS ON PS.ProjectStatusId = P.ProjectStatusId
 		INNER JOIN dbo.Practice PR ON PR.PracticeId = P.PracticeId
-		INNER JOIN dbo.ProjectCSAT PCSAT ON PCSAT.ProjectId = P.ProjectId  AND ( @IsExport = 1 OR PCSAT.ModifiedDate = PRC.ModifiedDate) 
-		INNER JOIN dbo.Person Per1 ON Per1.PersonId = P.ProjectOwnerId
-		LEFT JOIN RecentProjectCompletedStatus RPCS ON RPCS.ProjectId = P.ProjectId 
 		INNER JOIN dbo.Commission Comm ON Comm.ProjectId = P.ProjectId AND Comm.CommissionType = 1 
 		INNER JOIN dbo.Person Per3 ON Per3.PersonId = Comm.PersonId
-		INNER JOIN dbo.Person CSATReviewer ON CSATReviewer.PersonId = PCSAT.ReviewerId
+		INNER JOIN dbo.Person Per1 ON Per1.PersonId = P.ProjectOwnerId
 		LEFT JOIN dbo.Person Per2 ON Per2.PersonId = P.DirectorId
 		LEFT JOIN dbo.Person CSATOwner ON CSATOwner.PersonId = P.ReviewerId
-		
+		LEFT JOIN RecentProjectCompletedStatus RPCS ON RPCS.ProjectId = P.ProjectId 
+		LEFT JOIN ProjectsRecentlyUpdatedCSATS PRC ON P.ProjectId = PRC.ProjectId 
+		LEFT JOIN dbo.ProjectCSAT PCSAT ON PCSAT.ProjectId = P.ProjectId  AND ( @IsExport = 1 OR PCSAT.ModifiedDate = PRC.ModifiedDate) 
+		LEFT JOIN dbo.Person CSATReviewer ON CSATReviewer.PersonId = PCSAT.ReviewerId
+		WHERE (P.ProjectId = PRC.ProjectId OR @IsExport = 1)
+		ORDER BY PCSAT.CompletionDate DESC,p.ProjectNumber ASC
 END
 
