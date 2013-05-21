393d49f6115774e74ee189de5d152097f579b9a5
SainathC <SainathC>
2013-05-21 05:58:00 -0500

    Comments - Defect Number: 3129; Defect RecordID: 3125; Server Address: localhost; Server Port: 99; Database Name: Practice Management; Defect Summary: Reports > Resources > Consulting Demand Report Enhancements; Defect Description: The attached mock-ups describe new enhancements we are requesting to be made to the current Consulting Demand Report. These enhancements will replace the current report, and the current report can be deprecated from the system. While the fundamental data pieces and calculations behind the report are not changing, the enhancements center around restructuring how the data is presented, including various slices both in tabular and graphical formats. From a overal formatting perspective, the new Consulting Demand Report should be structured very similarly to the current Human Capital and Time Reports. The same types of functionality, such as the utilization of hover, pop-ups, and sortable columns will be used.
    There will be one nomenclature change. The current value of "Role" as it applies to Strawmen contained Practice Management should be changed to "Title". As a result, when adding or editing a Strawman, the input field title of "Role" should be changed to "Title."
    
    Please review the attached requirements and provide a high-level estimate of when the enhancements could be delivered to UAT.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/DataTransferObjects/Reports/ConsultantDemand/ConsultantDemandDetailsByMonthByTitle.cs
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/DataTransferObjects/Reports/ConsultantDemand/ConsultantDemandDetailsByMonthByTitle.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Runtime.Serialization;
+using System.Web;
 
 namespace DataTransferObjects.Reports.ConsultingDemand
 {
@@ -12,22 +13,52 @@ namespace DataTransferObjects.Reports.ConsultingDemand
     {
         [DataMember]
         public string Skill { get; set; }
+        public string HtmlEncodedSkill
+        {
+            get
+            {
+                return HttpUtility.HtmlEncode(Skill);
+            }
+        }
         [DataMember]
         public string OpportunityNumber { get; set; }
         [DataMember]
         public string ProjectNumber { get; set; }
         [DataMember]
         public string ProjectDescription { get; set; }
+        public string HtmlEncodedProjectDescription
+        {
+            get
+            {
+                return HttpUtility.HtmlEncode(ProjectDescription);
+            }
+        }
         [DataMember]
         public int? OpportunityId { get; set; }
         [DataMember]
         public int? ProjectId { get; set; }
         [DataMember]
         public string ProjectName { get; set; }
+        public string HtmlEncodedProjectName
+        {
+            get
+            {
+                return HttpUtility.HtmlEncode(ProjectName);
+            }
+        }
+        [DataMember]
+        public string SalesStage { get; set; }
         [DataMember]
         public int AccountId { get; set; }
         [DataMember]
         public string AccountName { get; set; }
+        public string HtmlEncodedAccountName
+        {
+            get
+            {
+                return HttpUtility.HtmlEncode(AccountName);
+            }
+        }
         [DataMember]
         public DateTime ResourceStartDate { get; set; }
         [DataMember]
