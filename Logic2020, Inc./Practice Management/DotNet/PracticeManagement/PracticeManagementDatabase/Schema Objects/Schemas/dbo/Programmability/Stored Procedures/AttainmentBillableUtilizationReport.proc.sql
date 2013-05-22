dc787922d62246360a41e12a004e9e43e0437445
SainathC <SainathC>
2013-05-22 10:30:00 -0500

    Comments - Defect Number: 3148; Defect RecordID: 3144; Server Address: localhost; Server Port: 99; Database Name: Practice Management; Defect Summary: Reports > Attainment Report Extract > Add new tab for Billable Utilization; Defect Description: We need to add a new third tab to the Attainment report entitled "Billable Utilization". It will become a new source sheet for the Attainment report that is being developed. The extract should display the billable utilization by month, by quarter, and YTD for every Active member of the Logic20/20 consulting organization (excluding internal resources and offshore resources).
    We need the following columns in the report. The Month, Quarters, and YTD columns report billable utilization for the individual for that time period in the current year.
    
    Resource Name (Last Name, First Name)
    Title
    Pay Type
    January
    February
    March
    April
    May
    June
    July
    August
    September
    October
    November
    December
    Q1
    Q2
    Q3
    Q4
    YTD
    
    Please prioritze building this new extract over all current activities as we would like to be able to use it, if at all possible, for Friday's financial reporting to leadership.

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/AttainmentBillableUtilizationReport.proc.sql
@@ -0,0 +1,99 @@
+﻿CREATE PROCEDURE [dbo].[AttainmentBillableUtilizationReport]
+(
+	@StartDate DATETIME ,
+	@EndDate DATETIME
+)
+AS
+BEGIN
+		DECLARE @StartDateLocal DATETIME ,
+				@EndDateLocal DATETIME ,
+				@HolidayTimeType INT ,
+				@FutureDate DATETIME,
+				@CurrtenDate DATETIME 
+
+			
+		SELECT @StartDateLocal = CONVERT(DATE, @StartDate), 
+				@EndDateLocal = CONVERT(DATE, @EndDate),
+				@CurrtenDate = dbo.GettingPMTime(GETUTCDATE()),
+				@FutureDate = dbo.GetFutureDate()
+			 
+		;WITH Ranges
+		AS
+		(
+			SELECT MonthStartDate AS StartDate,MonthEndDate AS EndDate,DATENAME(MM,MonthStartDate) AS RangeType,DATEPART(MM,MonthStartDate) AS ColOrder FROM dbo.Calendar C
+			WHERE C.DATE between @StartDateLocal and @EndDateLocal
+			GROUP BY C.MonthStartDate,C.MonthEndDate 
+			UNION ALL
+			SELECT QuarterStartDate,QuarterEndDate,'Q'+CONVERT(NVARCHAR,DATEPART(Q,QuarterStartDate)),DATEPART(Q,QuarterStartDate)+12 FROM dbo.Calendar C
+			WHERE C.DATE between @StartDateLocal and @EndDateLocal
+			GROUP BY C.QuarterStartDate,C.QuarterEndDate
+			UNION ALL
+			SELECT DATEADD(d,-DATEPART(dy,@CurrtenDate)+1,@CurrtenDate),DATEADD(d,-1,CONVERT(DATE,@CurrtenDate)),'YTD',17
+		),
+		PersonsList
+		AS 
+		(
+			SELECT P.PersonId,pay.Timescale
+			FROM  dbo.Person P
+			INNER JOIN dbo.Pay pay ON P.PersonId = pay.Person
+			WHERE @CurrtenDate BETWEEN pay.StartDate AND ISNULL(pay.EndDate,@FutureDate) AND pay.Timescale IN (1,2)
+					AND P.PersonStatusId IN (1,5) AND P.DivisionId = 2 AND P.IsOffShore = 0 
+		) ,
+		PersonWithRanges
+		AS
+		(
+		SELECT  pl.PersonId,R.StartDate,R.EndDate,pl.Timescale,R.RangeType,R.ColOrder
+		FROM PersonsList pl
+		CROSS JOIN Ranges  R
+		),
+		PersonRangesWithDefaultHours
+		AS
+		(
+			SELECT  PR.Personid,
+					PR.StartDate,
+					PR.EndDate,
+					PR.Timescale,
+				    (COUNT(CAL.Date) * 8) AS DefaultHours --Estimated working hours per day is 8.
+			FROM  PersonWithRanges PR
+			INNER JOIN dbo.v_PersonHistory AS P ON PR.PersonId = P.PersonId
+			INNER JOIN dbo.Calendar AS CAL ON CAL.Date BETWEEN PR.StartDate AND PR.EndDate
+												AND CAL.Date BETWEEN P.HireDate AND ISNULL(P.TerminationDate,@FutureDate)
+			WHERE DATENAME(weekday,CAL.Date) != 'Saturday' AND DATENAME(weekday,CAL.Date) != 'Sunday'
+			GROUP BY PR.Personid,PR.StartDate,PR.EndDate,PR.Timescale
+		),
+		PersonListWithBillingHours
+		AS
+		(
+			SELECT  PR.PersonId ,
+					PR.StartDate,
+					PR.EndDate,
+					PR.DefaultHours,
+					PR.Timescale,
+					ROUND(SUM(CASE WHEN Pro.ProjectNumber != 'P031000'
+								THEN TEH.ActualHours
+								ELSE 0
+								END), 2) AS BillableHours 
+			FROM  dbo.TimeEntry TE
+			INNER JOIN dbo.TimeEntryHours TEH ON TEH.TimeEntryId = te.TimeEntryId
+			INNER JOIN PersonRangesWithDefaultHours PR ON  TE.PersonId = Pr.PersonId 
+												AND TE.ChargeCodeDate BETWEEN PR.StartDate AND PR.EndDate
+			INNER JOIN dbo.ChargeCode CC ON CC.Id = TE.ChargeCodeId
+			INNER JOIN dbo.Project PRO ON PRO.ProjectId = CC.ProjectId
+			WHERE     TEH.IsChargeable = 1 
+			GROUP BY  PR.PersonId ,
+						PR.StartDate,
+						PR.EndDate,
+						PR.DefaultHours,PR.Timescale
+		)
+		SELECT PR.PersonId,P.FirstName,P.LastName,T.Title,TS.Name AS TimeScaleName,pr.StartDate,PR.EndDate,PR.RangeType,
+				CONVERT(DECIMAL(10,4),ROUND(
+											(ISNULL(PLb.BillableHours,0)/ISNULL(NULLIF(Plb.DefaultHours,0),1)) 
+											,1)
+											) AS BillableUtilizationPercent 
+		FROM PersonWithRanges PR
+		LEFT JOIN PersonListWithBillingHours PLB ON PR.PersonId = PLB.PersonId AND PR.StartDate = PLB.StartDate AND PR.EndDate = PLB.EndDate
+		INNER JOIN dbo.Person P ON P.PersonId = PR.PersonId
+		INNER JOIN dbo.Title T ON T.TitleId = P.TitleId 
+		INNER JOIN dbo.Timescale TS ON TS.TimescaleId = PR.Timescale 
+		ORDER BY PR.PersonId,PR.ColOrder
+END
