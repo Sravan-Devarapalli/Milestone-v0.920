1986b03e09e94b2c47e7e1dc5e11c1530baed0a4
SainathC <SainathC>
2013-05-27 01:30:00 -0500

    Comments - Defect Number: 3150; Defect RecordID: 3146; Server Address: localhost; Server Port: 99; Database Name: Practice Management; Defect Summary: SQL query needed - pull all W2 Salaried employees and the projects they worked on from 1/1/2013 through 6/1/2013; Defect Description: I am almost positive we've requested this in the past but I cannot locate an e-mail or previous TTP ticket on this request:
    To support our Mid-Year Performance Review process I am looking to capture the W2-Salaried employees in the Consulting Division and what project(s) they worked on between the dates Jan. 1, 2013 and June 1, 2013. I'd like the data in an Excel export with the following details:
    
    Pay Type (W2 Salary only)
    Division (Consulting only)
    Hire Date
    Person ID
    First Name
    Last Name
    Account
    Business Group
    Business Unit
    Project # (e.g. P012345)
    Project Owner
    Start Date (based on the milestone when did they start on the project)
    End Date (based on the milestone when did they end on the project)
    
    Many people will have multiple rows each as they will have worked on a variety of projects and potentially accounts during the 1/1/2013 to 6/1/2013 period.
    
    For the Start Date and End Date values, some resources may have strange schedules. Therefore, it would be sufficient to simply put the earliest date they were assigned to the project and the latest date. For example, if someone was assigned to Project X as of 1/5/2013 through 1/21/2013 and then again from 2/25/2013 to 2/28/2013. I would be okay with seeing 1/5/2013 in the Start Date column and 2/28/2013 in the End Date column.
    
    This would probably make sense to setup as a Stored Procedure as we will need this again next year if a procedure does not already exist.
    
    I hope this is clear but if not do not hesitate to ask questions. I'd love to have this early in the week next week (May 28th) if at all possible.
    
    Thanks,
    Matt

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/GetSalariedConsultantsAssignedDaysByProjects.proc.sql
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/GetSalariedConsultantsAssignedDaysByProjects.proc.sql
@@ -7,45 +7,75 @@
 -- =============================================
 
 CREATE PROCEDURE [dbo].[GetSalariedConsultantsAssignedDaysByProjects]
+(
+	@StartDate DATETIME  = '20130101',
+	@Enddate DATETIME    = '20130601'
+)
 AS
 BEGIN
-	DECLARE @FutureDate DATETIME ,@Date20120101 DATETIME
-	SELECT @FutureDate = dbo.GetFutureDate(), @Date20120101 = CAST('20120101' AS DATETIME)
+	DECLARE @FutureDate DATETIME 
+	SELECT @FutureDate = dbo.GetFutureDate()
 
 	;WITH CTE AS 
 	(
-		SELECT	P.ProjectNumber,
-				P.Name , 
-				Pers.LastName,
+		SELECT	T.Name AS PayType,
+				PD.DivisionName AS Division,
+				Pers.HireDate,
+				Pers.EmployeeNumber,
 				Pers.FirstName ,
+				Pers.LastName,
+				Cli.Name As Account,
+				BG.Name AS BusinessGroup,
+				PG.Name AS BusinessUnit,
+				P.ProjectNumber,
 				P.ProjectId,
 				C.Date,
-				Pers.PersonId
-		FROM Project P
-		JOIN Milestone M ON M.ProjectId = P.ProjectId
-		JOIN MilestonePerson MP ON MP.MilestoneId = M.MilestoneId
-		JOIN Person Pers ON Pers.PersonId = MP.PersonId AND Pers.IsStrawman = 0
-		JOIN MilestonePersonEntry MPE ON MPE.MilestonePersonId = MP.MilestonePersonId
-		JOIN Calendar C  ON C.Date Between MPE.StartDate AND MPE.EndDate
-		JOIN Pay pay ON pay.Person = MP.PersonId AND pay.Timescale = 2 AND C.Date Between pay.StartDate AND ISNULL(pay.EndDate-1,@FutureDate)
-
-		WHERE P.ProjectStatusId = 3 AND PerS.PersonStatusId IN(1,5)  AND P.ProjectId !=174 AND P.EndDate >= @Date20120101
-		GROUP BY P.ProjectId,P.ProjectNumber,Pers.LastName ,Pers.FirstName ,C.Date,P.Name,Pers.PersonId 
-
-
+				Pers.PersonId,
+				Po.LastName + ', '+ Po.FirstName AS ProjectOwner
+		FROM dbo.Project P
+		INNER JOIN dbo.Milestone M ON M.ProjectId = P.ProjectId
+		INNER JOIN dbo.MilestonePerson MP ON MP.MilestoneId = M.MilestoneId
+		INNER JOIN dbo.Person Pers ON Pers.PersonId = MP.PersonId AND Pers.IsStrawman = 0 AND Pers.DivisionId =  2-- Consulting
+		INNER JOIN dbo.MilestonePersonEntry MPE ON MPE.MilestonePersonId = MP.MilestonePersonId
+		INNER JOIN dbo.Calendar C  ON C.Date Between MPE.StartDate AND MPE.EndDate AND C.Date Between @StartDate AND @Enddate
+		INNER JOIN dbo.Pay pay ON pay.Person = MP.PersonId AND pay.Timescale = 2 AND C.Date Between pay.StartDate AND ISNULL(pay.EndDate-1,@FutureDate)
+		INNER JOIN dbo.Timescale T ON pay.Timescale = T.TimescaleId
+		INNER JOIN dbo.PersonDivision pd ON pd.DivisionId = Pers.DivisionId
+		INNER JOIN dbo.Client Cli ON Cli.ClientId = P.ClientId
+		INNER JOIN dbo.ProjectGroup PG ON PG.GroupId = P.GroupId
+		INNER JOIN dbo.BusinessGroup BG ON BG.BusinessGroupId = PG.BusinessGroupId
+		Left JOIN dbo.Person PO ON PO.PersonId = P.ProjectOwnerId
+		WHERE P.ProjectStatusId = 3 AND PerS.PersonStatusId IN(1,5)  AND P.ProjectId !=174 AND MPE.StartDate <= @Enddate AND @StartDate <= MPE.EndDate
+		GROUP BY P.ProjectId,P.ProjectNumber,Pers.LastName ,Pers.FirstName ,C.Date,P.Name,Pers.PersonId,T.Name,PD.DivisionName,Pers.HireDate,Pers.EmployeeNumber,Cli.Name,BG.Name,PG.Name,Po.LastName,Po.FirstName
 	) 
 
-	SELECT	P.LastName + ', '+ P.FirstName AS [Consultant Name],
-			P.ProjectNumber,
-			P.Name AS [Project Name],
+	SELECT	P.PayType AS [Pay Type],
+			P.Division AS [Division],
+			P.HireDate AS [Hire Date],
+			P.EmployeeNumber AS [Person ID],
+			P.FirstName AS [First Name] ,
+			P.LastName AS [Last Name],
+			P.Account,
+			P.BusinessGroup AS [Business Group],
+			P.BusinessUnit AS [Business Unit],
+			P.ProjectNumber AS Project,
+			P.ProjectOwner AS [Project Owner],
 			MIN(P.Date)[Roll on Date],
-			MAX(P.Date)[Roll off Date],			
-			dbo.GetProjectManagerNames(P.ProjectId) AS ProjectManagers,
+			MAX(P.Date)[Roll off Date],
+			--dbo.GetProjectManagerNames(P.ProjectId) AS ProjectManagers,
 			COUNT(*) AS [Days]
-			
 	FROM CTE AS P
-
-	GROUP BY P.ProjectId,P.ProjectNumber,P.LastName, P.FirstName,P.Name,P.PersonId
+	GROUP BY P.PayType,
+			P.Division,
+			P.HireDate,
+			P.EmployeeNumber,
+			P.FirstName ,
+			P.LastName,
+			P.Account,
+			P.BusinessGroup,
+			P.BusinessUnit,
+			P.ProjectNumber,
+			P.ProjectOwner
 	ORDER BY P.LastName, P.FirstName,[Days]
 END
 
