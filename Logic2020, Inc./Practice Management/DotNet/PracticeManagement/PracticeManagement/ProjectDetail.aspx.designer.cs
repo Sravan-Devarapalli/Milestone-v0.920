6df1dd9790ca6d06371b0868682bee439db9b947
SainathC <SainathC>
2013-05-27 23:14:00 -0500

    Comments - Defect Number: 3142; Defect RecordID: 3138; Server Address: localhost; Server Port: 99; Database Name: Practice Management; Defect Summary: Project Details > New "PO Number" Field; Defect Description: Please add a new text field under Pricing List entitled PO Number. The field is not required to save Project Details. Please add a new PO Number column at the end of current Project Summary Export (both Projected and Actuals tabs).

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/ProjectDetail.aspx.designer.cs
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/ProjectDetail.aspx.designer.cs
@@ -553,15 +553,6 @@ namespace PraticeManagement {
         protected global::System.Web.UI.WebControls.DropDownList ddlCSATOwner;
         
         /// <summary>
-        /// custCSATOwner control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.CustomValidator custCSATOwner;
-        
-        /// <summary>
         /// imgMailToCSATOwner control.
         /// </summary>
         /// <remarks>
