28906d73399c308f6dea2c60cd48e23a4f175b5d
SainathC <SainathC>
2013-06-04 06:14:00 -0500

    Comments - Defect Number: 3153; Defect RecordID: 3149; Server Address: localhost; Server Port: 99; Database Name: Practice Management; Defect Summary: "By Time Period Report > "Paychex" export not including terminated employees correctly; Defect Description: We may have encountered an issue where Terminated employees are only included in the "Paychex" report export in the "By Time Period" report if the pay type of "1099 Hourly" is included in the filter.
    Example: employees Brennan Fee, Bobby Shashidar, and David Graves terminated during the pay period of 5/16-5/31. All employees have Pay Types of "W2-Salary".
    
    Reproduction Steps:
    Run the By Time Period report for the range of "Payroll - Previous" and view "By Resource". Select the checkbox "Include persons with no time entered in report output"
    Under "Pay Type," select ONLY "W2-Salary" and "W2-Hourly"
    Click the "Paychex" export button and open the report.
    Terminated employees Brennan Fee, Bobby Shashidar, and David Graves do not show in the report.
    However, if the report is run again, and no filters are applied under "Pay Type" (i.e., "All Pay Types" is selected), then the three employees show correctly in the Paychex export.
    
    Could you please investigate the cause of this issue and push the fix directly to Production? For clarity, this "Paychex" export should list all employees with a pay types of W2-Salary and W2-Hourly who entered time into PM for the time period selected. Selection of filters on the report view in PM should not affect the results of this Paychex report export.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementService/IReportService.cs
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementService/IReportService.cs
@@ -50,7 +50,7 @@ namespace PracticeManagementService
         List<Milestone> GetMilestonesForProject(string projectNumber);
 
         [OperationContract]
-        List<PersonLevelPayCheck> TimePeriodSummaryByResourcePayCheck(DateTime startDate, DateTime endDate, bool includePersonsWithNoTimeEntries, string personIds, string seniorityIds, string timescaleNames, string personStatusIds, string personDivisionIds);
+        List<PersonLevelPayCheck> TimePeriodSummaryByResourcePayCheck(DateTime startDate, DateTime endDate, bool includePersonsWithNoTimeEntries, string personIds, string titleIds, string timescaleNames, string personStatusIds, string personDivisionIds);
 
         [OperationContract]
         List<PersonLevelTimeEntriesHistory> TimeEntryAuditReportByPerson(DateTime startDate, DateTime endDate);
