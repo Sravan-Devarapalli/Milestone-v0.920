9be02b05095efaf486babee9ee5c88e4a65246d2
SainathC <SainathC>
2013-06-12 11:19:00 -0500

    Comments - Defect Number: 3159; Defect RecordID: 3155; Server Address: localhost; Server Port: 99; Database Name: Practice Management; Defect Summary: Attainment Report Extract > New Column Fields; Defect Description: Our Attainment reporting continues to evolve, and I am now building reports based on quarterly data. Instead of manually calculating revenue and contribution margin by Quarter and YTD, I would like to add these fields to the "Summary-Projected" and "Summary-Actuals" tabs. Could you please add five new columns (columns AE thru AI) to both the "Summary - Projected" and "Summary-Actuals" tabs entitled the following: "Q1 Total" "Q2 Total" "Q3 Total" "Q4 Total" "YTD"
    Please also make the default selection in the drop-down for the report time range "Current FY".

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/AttainmentFinancialListByProject.proc.sql
@@ -0,0 +1,184 @@
+﻿CREATE PROCEDURE [dbo].[AttainmentFinancialListByProject]
+(
+	@ProjectId   VARCHAR(2500),
+	@StartDate   DATETIME,
+	@EndDate     DATETIME,
+	@CalculateQuarterValues BIT = 0,
+	@CalculateYearToDateValues BIT = 0
+) WITH RECOMPILE
+AS
+BEGIN
+	SET NOCOUNT ON
+	SET ANSI_WARNINGS OFF
+
+	DECLARE @ProjectIdLocal   VARCHAR(2500),
+			@StartDateLocal   DATETIME,
+			@EndDateLocal     DATETIME
+			
+
+	SELECT @ProjectIdLocal =@ProjectId ,
+		   @StartDateLocal=@StartDate ,
+		   @EndDateLocal=@EndDate
+
+	DECLARE @ProjectIDs TABLE
+	(
+		ResultId INT
+	)
+	DECLARE @Today DATETIME, @CurrentMonthStartDate DATETIME,@InsertingTime DATETIME
+
+	SELECT @Today = CONVERT(DATE, dbo.GettingPMTime(GETUTCDATE())),@InsertingTime = dbo.InsertingTime()
+	SELECT @CurrentMonthStartDate = C.MonthStartDate
+	FROM dbo.Calendar C
+	WHERE C.Date = @Today
+
+	INSERT INTO @ProjectIDs
+	SELECT * FROM dbo.ConvertStringListIntoTable(@ProjectIdLocal)
+	
+	DECLARE @Ranges TABLE (StartDate DATETIME,EndDate DATETIME,RangeType NVARCHAR(11),ColOrder INT)
+	
+	INSERT INTO @Ranges 
+	SELECT	MonthStartDate AS StartDate,
+			MonthEndDate AS EndDate,
+			CONVERT(NVARCHAR(11),CONVERT(DATE,MonthStartDate)) AS RangeType,
+			DATEPART(MM,MonthStartDate) AS ColOrder
+	FROM dbo.Calendar C
+	WHERE C.DATE between @StartDateLocal and @EndDateLocal
+	GROUP BY C.MonthStartDate,C.MonthEndDate 
+	
+	IF (@CalculateQuarterValues = 1)
+	BEGIN
+		INSERT INTO @Ranges 
+		SELECT QuarterStartDate,QuarterEndDate,'Q'+CONVERT(NVARCHAR,DATEPART(Q,QuarterStartDate)),DATEPART(Q,QuarterStartDate)+12 FROM dbo.Calendar C
+		WHERE C.DATE between @StartDateLocal and @EndDateLocal 
+		GROUP BY C.QuarterStartDate,C.QuarterEndDate
+	END
+
+	IF (@CalculateYearToDateValues = 1)
+	BEGIN
+		INSERT INTO @Ranges 
+		SELECT DATEADD(d,-DATEPART(dy,@Today)+1,@Today) ,DATEADD(d,-1,CONVERT(DATE,@Today)),'YTD',17
+	END
+
+	;WITH FinancialsRetro AS 
+	(
+	SELECT f.ProjectId,
+		   f.Date, 
+		   f.PersonMilestoneDailyAmount,
+		   f.PersonDiscountDailyAmount,
+		   ISNULL((SELECT SUM(c.FractionOfMargin) 
+							  FROM dbo.Commission AS  c 
+							  WHERE c.ProjectId = f.ProjectId 
+									AND c.CommissionType = 1
+								),0) ProjectSalesCommisionFraction,
+		   (ISNULL(f.PayRate, 0) + ISNULL(f.OverheadRate, 0)+ISNULL(f.BonusRate,0)+ISNULL(f.VacationRate,0)) SLHR,
+		   ISNULL(f.PayRate,0) PayRate,
+		   f.MLFOverheadRate,
+		   f.PersonHoursPerDay,
+		   f.PersonId,
+		   f.Discount,
+		   f.IsHourlyAmount,
+		   f.BillableHOursPerDay,
+  		   f.NonBillableHoursPerDay,
+		   f.ActualHoursPerDay,
+		   f.BillRate,
+		   f.OverheadRate,
+		   f.BonusRate,
+		   f.VacationRate
+	FROM [v_FinancialsRetrospectiveActualHours] f
+	WHERE f.ProjectId  IN (SELECT * FROM @ProjectIDs) 
+		AND f.Date BETWEEN @StartDateLocal AND @EndDateLocal
+	),
+	ActualAndProjectedValuesDaily
+	AS
+	(
+	SELECT	f.ProjectId,
+			f.PersonId,
+			f.Date,
+			SUM(f.PersonMilestoneDailyAmount) AS ProjectedRevenueperDay,
+			SUM(f.PersonMilestoneDailyAmount - f.PersonDiscountDailyAmount) AS ProjectedRevenueNet,
+			SUM(f.PersonMilestoneDailyAmount - f.PersonDiscountDailyAmount - (
+																				CASE WHEN f.SLHR >= f.PayRate + f.MLFOverheadRate THEN f.SLHR 
+																				ELSE f.PayRate + f.MLFOverheadRate END
+																			) * ISNULL(f.PersonHoursPerDay, 0)) AS  ProjectedGrossMargin,
+			ISNULL(SUM((CASE WHEN f.SLHR >=  f.PayRate + f.MLFOverheadRate THEN f.SLHR ELSE  f.PayRate + f.MLFOverheadRate END)*ISNULL(f.PersonHoursPerDay, 0)),0) ProjectedCogsperDay,
+			SUM(CASE WHEN f.IsHourlyAmount = 0 THEN f.PersonMilestoneDailyAmount ELSE 0 END ) AS FixedActualRevenuePerDay,
+			(ISNULL(SUM(CASE WHEN f.IsHourlyAmount = 1 THEN f.BillRate* f.ActualHoursPerDay ELSE 0 END),0) / ISNULL(NULLIF(SUM(CASE WHEN f.IsHourlyAmount = 1 THEN f.ActualHoursPerDay ELSE 0 END),0),1)) * MAX(CASE WHEN f.IsHourlyAmount = 1 THEN  f.BillableHOursPerDay ELSE 0 END) HourlyActualRevenuePerDay,
+			SUM(CASE WHEN f.IsHourlyAmount = 0 
+					THEN 	f.PersonMilestoneDailyAmount - f.PersonDiscountDailyAmount - (CASE WHEN f.SLHR >= f.PayRate + f.MLFOverheadRate  THEN f.SLHR ELSE f.PayRate + f.MLFOverheadRate END) * ISNULL(f.PersonHoursPerDay, 0)
+					ELSE 0
+				END) AS FixedActualMarginPerDay,
+				((ISNULL(SUM(CASE WHEN f.IsHourlyAmount = 1 THEN f.BillRate* f.ActualHoursPerDay ELSE 0 END),0) / ISNULL(NULLIF(SUM(CASE WHEN f.IsHourlyAmount = 1 THEN f.ActualHoursPerDay ELSE 0 END),0),1)) * MAX(CASE WHEN f.IsHourlyAmount = 1 THEN  f.BillableHOursPerDay ELSE 0 END))
+				 -  (
+						MAX( CASE WHEN f.IsHourlyAmount = 1 THEN  f.BillableHOursPerDay + f.NonBillableHoursPerDay ELSE 0 END ) * 
+						ISNULL( CASE WHEN ISNULL(SUM(CASE WHEN f.IsHourlyAmount = 1 THEN f.ActualHoursPerDay ELSE 0 END),0) > 0 
+									THEN SUM((CASE WHEN f.SLHR >=  f.PayRate + f.MLFOverheadRate THEN f.SLHR ELSE  f.PayRate + f.MLFOverheadRate END)* CASE WHEN f.IsHourlyAmount = 1 THEN f.ActualHoursPerDay ELSE 0 END) / SUM(CASE WHEN f.IsHourlyAmount = 1 THEN f.ActualHoursPerDay ELSE 0 END)
+									ELSE SUM((CASE WHEN f.SLHR >=  f.PayRate + f.MLFOverheadRate THEN f.SLHR ELSE  f.PayRate + f.MLFOverheadRate END)) / ISNULL(COUNT(f.PayRate),1)  
+								END ,0)
+					) AS HourlyActualMarginPerDay,
+			ISNULL(SUM(f.PersonHoursPerDay), 0) AS ProjectedHoursPerDay,
+			(SUM((f.PersonMilestoneDailyAmount - f.PersonDiscountDailyAmount -
+						(CASE WHEN f.SLHR >=  f.PayRate +f.MLFOverheadRate 
+							  THEN f.SLHR ELSE f.PayRate +f.MLFOverheadRate END) 
+					    *ISNULL(f.PersonHoursPerDay, 0))* (f.ProjectSalesCommisionFraction/100))) SalesCommission,
+			0 AS PracticeManagementCommission,
+		  	min(f.Discount) as Discount,
+			MIN(CONVERT(INT, f.IsHourlyAmount)) as IsHourlyAmount
+	FROM FinancialsRetro AS f
+	GROUP BY f.ProjectId, f.PersonId, f.Date
+	),
+	ProjectExpensesMonthly
+	AS
+	(
+		SELECT pexp.ProjectId,
+			CONVERT(DECIMAL(18,2),SUM(pexp.Amount/((DATEDIFF(dd,pexp.StartDate,pexp.EndDate)+1)))) Expense,
+			CONVERT(DECIMAL(18,2),SUM(pexp.Reimbursement*0.01*pexp.Amount /((DATEDIFF(dd,pexp.StartDate,pexp.EndDate)+1)))) Reimbursement,
+			C.StartDate AS FinancialDate,
+			C.EndDate AS MonthEnd,
+			C.RangeType
+		FROM dbo.ProjectExpense as pexp
+		JOIN @Ranges c ON c.StartDate <= pexp.EndDate AND pexp.StartDate <= C.EndDate
+		WHERE ProjectId IN (SELECT * FROM @ProjectIDs)
+		GROUP BY pexp.ProjectId, C.StartDate, C.EndDate,C.RangeType
+	), 
+	ActualAndProjectedValuesMonthly  AS
+	(SELECT CT.ProjectId, 
+			C.StartDate AS FinancialDate, 
+			C.EndDate AS MonthEnd,
+			C.RangeType,
+			SUM(ISNULL(CT.ProjectedRevenueperDay, 0)) AS ProjectedRevenue,
+			SUM(ISNULL(CT.ProjectedRevenueNet, 0)) as ProjectedRevenueNet,
+			SUM(ISNULL(CT.ProjectedGrossMargin, 0)) as ProjectedGrossMargin,
+			SUM(ISNULL(CT.HourlyActualRevenuePerDay, 0) + ISNULL(CT.FixedActualRevenuePerDay, 0)) AS ActualRevenue,
+			SUM(ISNULL(CT.HourlyActualMarginPerDay, 0) + ISNULL(CT.FixedActualMarginPerDay, 0)) as ActualMargin,
+			SUM(ISNULL(CT.ProjectedCogsperDay, 0)) as ProjectedCogs,
+			MAX(ISNULL(CT.Discount, 0)) Discount,
+			SUM(ISNULL(CT.ProjectedHoursPerDay, 0)) as ProjectedHoursPerMonth,
+			SUM(ISNULL(CT.SalesCommission, 0)) as SalesCommission,
+			SUM(ISNULL(CT.PracticeManagementCommission, 0)) as PracticeManagementCommission
+	FROM ActualAndProjectedValuesDaily CT
+	INNER JOIN @Ranges C ON CT.Date BETWEEN C.StartDate AND C.EndDate
+	GROUP BY CT.ProjectId, C.StartDate, C.EndDate,C.RangeType
+	)
+	SELECT
+		ISNULL(APV.ProjectId,PEM.ProjectId) ProjectId,
+		ISNULL(APV.FinancialDate,PEM.FinancialDate) AS FinancialDate,
+		ISNULL(APV.MonthEnd,PEM.MonthEnd) MonthEnd,
+		ISNULL(APV.RangeType,PEM.RangeType) RangeType,
+		CONVERT(DECIMAL(18,2),ISNULL(APV.ProjectedRevenue,0)) AS 'Revenue',
+		CONVERT(DECIMAL(18,2),ISNULL(APV.ProjectedGrossMargin,0)+(ISNULL(PEM.Reimbursement,0)-ISNULL(PEM.Expense,0)) * (1 - ISNULL(APV.Discount,0)/100))  as 'GrossMargin',
+		CASE WHEN ISNULL(APV.FinancialDate,PEM.FinancialDate) < @CurrentMonthStartDate 
+			 THEN CONVERT(DECIMAL(18,6), ISNULL(APV.ActualRevenue,0))
+			 ELSE CONVERT(DECIMAL(18,6), ISNULL(APV.ProjectedRevenue,0))
+			 END ActualRevenue,
+		CASE WHEN ISNULL(APV.FinancialDate,PEM.FinancialDate) < @CurrentMonthStartDate 
+			 THEN CONVERT(DECIMAL(18,6), ISNULL(APV.ActualMargin,0) - (ISNULL(APV.ActualRevenue,0) * ISNULL(APV.Discount,0)/100) + ((ISNULL(PEM.Reimbursement,0)-ISNULL(PEM.Expense,0)) * (1 - ISNULL(APV.Discount,0)/100)))
+			 ELSE CONVERT(DECIMAL(18,6), ISNULL(APV.ProjectedGrossMargin,0) + (ISNULL(PEM.Reimbursement,0)-ISNULL(PEM.Expense,0)) * (1 - ISNULL(APV.Discount,0)/100))
+			 END ActualGrossMargin,
+		CONVERT(BIT,1) AS IsMonthlyRecord,
+		@InsertingTime AS  CreatedDate,	
+		CONVERT(DATE,@InsertingTime)  AS CacheDate
+	FROM ActualAndProjectedValuesMonthly APV
+	FULL JOIN ProjectExpensesMonthly PEM 
+	ON PEM.ProjectId = APV.ProjectId AND APV.FinancialDate = PEM.FinancialDate  AND APV.MonthEnd = PEM.MonthEnd
+	ORDER BY ProjectId
+END
