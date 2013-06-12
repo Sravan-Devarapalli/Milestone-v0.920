1aa01b9b37df8c65572809ac6aa236baa25ddcdc
SainathC <SainathC>
2013-06-12 11:19:00 -0500

    Comments - Defect Number: 3159; Defect RecordID: 3155; Server Address: localhost; Server Port: 99; Database Name: Practice Management; Defect Summary: Attainment Report Extract > New Column Fields; Defect Description: Our Attainment reporting continues to evolve, and I am now building reports based on quarterly data. Instead of manually calculating revenue and contribution margin by Quarter and YTD, I would like to add these fields to the "Summary-Projected" and "Summary-Actuals" tabs. Could you please add five new columns (columns AE thru AI) to both the "Summary - Projected" and "Summary-Actuals" tabs entitled the following: "Q1 Total" "Q2 Total" "Q3 Total" "Q4 Total" "YTD"
    Please also make the default selection in the drop-down for the report time range "Current FY".

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/DataTransferObjects/Financials/RangeType.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+
+namespace DataTransferObjects.Financials
+{
+
+    [DataContract]
+    [Serializable]
+    public class RangeType
+    {
+        [DataMember]
+        public DateTime StartDate { get; set; }
+
+
+        [DataMember]
+        public DateTime EndDate { get; set; }
+
+        [DataMember]
+        public string Range
+        {
+            get;
+            set;
+        }
+    }
+}
+
