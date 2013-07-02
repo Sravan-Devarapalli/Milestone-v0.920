9f54e1876fa33e158f4562eb00cc5017d0773454
SainathC <SainathC>
2013-07-02 10:03:00 -0500

    Comments - Made code clean up with the help of Resharper plugin.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/ClientDAL/ProjectStatusDAL.cs
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/ClientDAL/ProjectStatusDAL.cs
@@ -7,65 +7,63 @@ using DataTransferObjects;
 
 namespace DataAccess
 {
-	/// <summary>
-	/// Provides an access to the ProjectStatus table.
-	/// </summary>
-	public static class ProjectStatusDAL
-	{
-		#region Constants
+    /// <summary>
+    /// Provides an access to the ProjectStatus table.
+    /// </summary>
+    public static class ProjectStatusDAL
+    {
+        #region Constants
 
-		private const string ProjectStatusListAllProcedure = "dbo.ProjectStatusListAll";
+        private const string ProjectStatusListAllProcedure = "dbo.ProjectStatusListAll";
 
-		private const string ProjectStatusIdColumn = "ProjectStatusId";
-		private const string NameColumn = "Name";
+        private const string ProjectStatusIdColumn = "ProjectStatusId";
+        private const string NameColumn = "Name";
 
-		#endregion
+        #endregion Constants
 
-		#region Methods
+        #region Methods
 
-		/// <summary>
-		/// Retrives the list of the project statuses.
-		/// </summary>
-		/// <returns>The list of the <see cref="ProjectStatus"/> objects.</returns>
-		public static List<ProjectStatus> ProjectStatusListAll()
-		{
-			using (SqlConnection connection = new SqlConnection(DataSourceHelper.DataConnection))
-			using (SqlCommand command = new SqlCommand(ProjectStatusListAllProcedure, connection))
-			{
-				command.CommandType = CommandType.StoredProcedure;
+        /// <summary>
+        /// Retrives the list of the project statuses.
+        /// </summary>
+        /// <returns>The list of the <see cref="ProjectStatus"/> objects.</returns>
+        public static List<ProjectStatus> ProjectStatusListAll()
+        {
+            using (SqlConnection connection = new SqlConnection(DataSourceHelper.DataConnection))
+            using (SqlCommand command = new SqlCommand(ProjectStatusListAllProcedure, connection))
+            {
+                command.CommandType = CommandType.StoredProcedure;
 
-				connection.Open();
-				using (SqlDataReader reader = command.ExecuteReader())
-				{
-					List<ProjectStatus> result = new List<ProjectStatus>();
+                connection.Open();
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    List<ProjectStatus> result = new List<ProjectStatus>();
 
-					ReadProjectStatuses(reader, result);
+                    ReadProjectStatuses(reader, result);
 
-					return result;
-				}
-			}
-		}
+                    return result;
+                }
+            }
+        }
 
-		private static void ReadProjectStatuses(DbDataReader reader, List<ProjectStatus> result)
-		{
-			if (reader.HasRows)
-			{
-				int projectStatusIdIndex = reader.GetOrdinal(ProjectStatusIdColumn);
-				int nameIndex = reader.GetOrdinal(NameColumn);
+        private static void ReadProjectStatuses(DbDataReader reader, List<ProjectStatus> result)
+        {
+            if (!reader.HasRows) return;
+            int projectStatusIdIndex = reader.GetOrdinal(ProjectStatusIdColumn);
+            int nameIndex = reader.GetOrdinal(NameColumn);
 
-				while (reader.Read())
-				{
-					ProjectStatus status = new ProjectStatus();
+            while (reader.Read())
+            {
+                ProjectStatus status = new ProjectStatus
+                    {
+                        Id = reader.GetInt32(projectStatusIdIndex),
+                        Name = reader.GetString(nameIndex)
+                    };
 
-					status.Id = reader.GetInt32(projectStatusIdIndex);
-					status.Name = reader.GetString(nameIndex);
+                result.Add(status);
+            }
+        }
 
-					result.Add(status);
-				}
-			}
-		}
-
-		#endregion
-	}
+        #endregion Methods
+    }
 }
-
