6331b2d1ca3569126e01d7c20724178f26dae651
SainathC <SainathC>
2013-07-02 11:00:00 -0500

    Comments - Made code clean up with the help of Resharper plugin.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/DataTransferObjects/Pay.cs
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/DataTransferObjects/Pay.cs
@@ -5,27 +5,26 @@ using System.Web;
 namespace DataTransferObjects
 {
     [DataContract]
-	[Serializable]
-	public class Pay : ICloneable
-	{
-		
-		#region Properties
+    [Serializable]
+    public class Pay : ICloneable
+    {
+        #region Properties
 
-		/// <summary>
-		/// Gets or sets an ID of the person the pay intended for.
-		/// </summary>
-		[DataMember]
-		public int PersonId
-		{
-			get;
-			set;
-		}
+        /// <summary>
+        /// Gets or sets an ID of the person the pay intended for.
+        /// </summary>
+        [DataMember]
+        public int PersonId
+        {
+            get;
+            set;
+        }
 
         /// <summary>
         /// When pay is effective
         /// </summary>
         [DataMember]
-		public DateTime StartDate
+        public DateTime StartDate
         {
             get;
             set;
@@ -34,34 +33,35 @@ namespace DataTransferObjects
         /// <summary>
         /// When pay is no longer effective
         /// </summary>
-		[DataMember]
-		public DateTime? EndDate
+        [DataMember]
+        public DateTime? EndDate
         {
             get;
             set;
         }
 
-		/// <summary>
-		/// Gets or sets an old StartDate value to be used during update
-		/// </summary>
-		[DataMember]
-		public DateTime? OldStartDate
-		{
-			get;
-			set;
-		}
+        /// <summary>
+        /// Gets or sets an old StartDate value to be used during update
+        /// </summary>
+        [DataMember]
+        public DateTime? OldStartDate
+        {
+            get;
+            set;
+        }
 
-		/// <summary>
-		/// Gets or sets an old EndDate value to be used during update
-		/// </summary>
-		[DataMember]
-		public DateTime? OldEndDate
-		{
-			get;
-			set;
-		}
+        /// <summary>
+        /// Gets or sets an old EndDate value to be used during update
+        /// </summary>
+        [DataMember]
+        public DateTime? OldEndDate
+        {
+            get;
+            set;
+        }
 
         private PracticeManagementCurrency _amount;
+
         /// <summary>
         /// Rate of pay
         /// </summary>
@@ -79,6 +79,7 @@ namespace DataTransferObjects
         }
 
         private TimescaleType _timescale;
+
         /// <summary>
         /// per annum, hourly, ...
         /// </summary>
@@ -93,25 +94,26 @@ namespace DataTransferObjects
             {
                 _timescale = value;
 
-                _amount.FormatStyle = 
-                _amountHourly.FormatStyle = 
-                    _timescale == TimescaleType.PercRevenue 
-                    ? 
+                _amount.FormatStyle =
+                _amountHourly.FormatStyle =
+                    _timescale == TimescaleType.PercRevenue
+                    ?
                     NumberFormatStyle.Percent : NumberFormatStyle.General;
             }
         }
 
-		/// <summary>
-		/// The name for the <see cref="Timescale"/>
-		/// </summary>
-		[DataMember]
-		public string TimescaleName
-		{
-			get;
-			set;
-		}
+        /// <summary>
+        /// The name for the <see cref="Timescale"/>
+        /// </summary>
+        [DataMember]
+        public string TimescaleName
+        {
+            get;
+            set;
+        }
 
         private PracticeManagementCurrency _amountHourly;
+
         /// <summary>
         /// Gets or sets an amount per hour.
         /// </summary>
@@ -128,45 +130,45 @@ namespace DataTransferObjects
             }
         }
 
-		/// <summary>
-		/// Gets or sets a number of the vacation days per year.
-		/// </summary>
-		[DataMember]
-		public int? VacationDays
-		{
-			get;
-			set;
-		}
+        /// <summary>
+        /// Gets or sets a number of the vacation days per year.
+        /// </summary>
+        [DataMember]
+        public int? VacationDays
+        {
+            get;
+            set;
+        }
 
-		/// <summary>
-		/// Gets or sets a bonus amount.
-		/// </summary>
-		[DataMember]
-		public PracticeManagementCurrency BonusAmount
-		{
-			get;
-			set;
-		}
+        /// <summary>
+        /// Gets or sets a bonus amount.
+        /// </summary>
+        [DataMember]
+        public PracticeManagementCurrency BonusAmount
+        {
+            get;
+            set;
+        }
 
-		/// <summary>
-		/// Gets or sets a periodicity of the bonus payments.
-		/// </summary>
-		[DataMember]
-		public int? BonusHoursToCollect
-		{
-			get;
-			set;
-		}
+        /// <summary>
+        /// Gets or sets a periodicity of the bonus payments.
+        /// </summary>
+        [DataMember]
+        public int? BonusHoursToCollect
+        {
+            get;
+            set;
+        }
 
-		/// <summary>
-		/// Gets or sets if the bonus is year one.
-		/// </summary>
-		[DataMember]
-		public bool IsYearBonus
-		{
-			get;
-			set;
-		}
+        /// <summary>
+        /// Gets or sets if the bonus is year one.
+        /// </summary>
+        [DataMember]
+        public bool IsYearBonus
+        {
+            get;
+            set;
+        }
 
         /// <summary>
         /// TitleId of the person which will be active from the start date.
@@ -209,8 +211,8 @@ namespace DataTransferObjects
         }
 
         public string HtmlEncodedPracticeName
-        {   
-             get
+        {
+            get
             {
                 return HttpUtility.HtmlEncode(PracticeName);
             }
@@ -240,31 +242,30 @@ namespace DataTransferObjects
 
         public object Clone()
         {
-            return (object) new Pay()
-            {
-                PersonId = this.PersonId,
-                StartDate = this.StartDate,
-                EndDate = this.EndDate,
-                OldStartDate = this.OldStartDate,
-                OldEndDate = this.OldEndDate,
-                Amount = this.Amount,
-                Timescale = this.Timescale,
-                TimescaleName = this.TimescaleName,
-                AmountHourly = this.AmountHourly,
-                VacationDays = this.VacationDays,
-                BonusAmount = this.BonusAmount,
-                BonusHoursToCollect = this.BonusHoursToCollect,
-                IsYearBonus = this.IsYearBonus,
-                TitleId = this.TitleId,
-                TitleName = this.TitleName,
-                PracticeId = this.PracticeId,
-                PracticeName = this.PracticeName,
-                SLTApproval = this.SLTApproval,
-                SLTPTOApproval = this.SLTPTOApproval
-            };
+            return new Pay
+                {
+                    PersonId = PersonId,
+                    StartDate = StartDate,
+                    EndDate = EndDate,
+                    OldStartDate = OldStartDate,
+                    OldEndDate = OldEndDate,
+                    Amount = Amount,
+                    Timescale = Timescale,
+                    TimescaleName = TimescaleName,
+                    AmountHourly = AmountHourly,
+                    VacationDays = VacationDays,
+                    BonusAmount = BonusAmount,
+                    BonusHoursToCollect = BonusHoursToCollect,
+                    IsYearBonus = IsYearBonus,
+                    TitleId = TitleId,
+                    TitleName = TitleName,
+                    PracticeId = PracticeId,
+                    PracticeName = PracticeName,
+                    SLTApproval = SLTApproval,
+                    SLTPTOApproval = SLTPTOApproval
+                };
         }
 
-		#endregion
+        #endregion Properties
     }
 }
-
