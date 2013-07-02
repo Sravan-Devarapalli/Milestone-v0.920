e8ab41e867bba32fca2363737ec9e5038a2028bc
SainathC <SainathC>
2013-07-02 10:03:00 -0500

    Comments - Made code clean up with the help of Resharper plugin.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/ClientDAL/OverHeadHistoryDAL.cs
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/ClientDAL/OverHeadHistoryDAL.cs
@@ -1,44 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using DataTransferObjects;
-using System.Data.SqlClient;
 using System.Data;
-using DataAccess.Other;
 using System.Data.Common;
+using System.Data.SqlClient;
+using DataAccess.Other;
+using DataTransferObjects;
 
 namespace DataAccess
 {
     public static class OverHeadHistoryDAL
     {
-
         private static void ReadOverheadHistory(DbDataReader reader, List<OverHeadHistory> result)
         {
-            if (reader.HasRows)
+            if (!reader.HasRows) return;
+            int startDateIndex = reader.GetOrdinal(Constants.ColumnNames.StartDateColumn);
+            int endDateIndex = reader.GetOrdinal(Constants.ColumnNames.EndDateColumn);
+            int w2Salary_RateIndex = reader.GetOrdinal(Constants.ColumnNames.W2Salary_RateColumn);
+            int w2Hourly_RateIndex = reader.GetOrdinal(Constants.ColumnNames.W2Hourly_RateColumn);
+            int _1099_Hourly_RateIndex = reader.GetOrdinal(Constants.ColumnNames._1099_Hourly_RateColumn);
+
+            while (reader.Read())
             {
-                int startDateIndex = reader.GetOrdinal(Constants.ColumnNames.StartDateColumn);
-                int endDateIndex = reader.GetOrdinal(Constants.ColumnNames.EndDateColumn);
-                int w2Salary_RateIndex = reader.GetOrdinal(Constants.ColumnNames.W2Salary_RateColumn);
-                int w2Hourly_RateIndex = reader.GetOrdinal(Constants.ColumnNames.W2Hourly_RateColumn);
-                int _1099_Hourly_RateIndex = reader.GetOrdinal(Constants.ColumnNames._1099_Hourly_RateColumn);
+                OverHeadHistory mlfHistory = new OverHeadHistory
+                    {
+                        StartDate = reader.GetDateTime(startDateIndex),
+                        EndDate = !reader.IsDBNull(endDateIndex) ? (DateTime?)reader.GetDateTime(endDateIndex) : null,
+                        W2Salary_Rate = reader.GetDecimal(w2Salary_RateIndex),
+                        W2Hourly_Rate = reader.GetDecimal(w2Hourly_RateIndex),
+                        _1099_Hourly_Rate = reader.GetDecimal(_1099_Hourly_RateIndex)
+                    };
 
-                while (reader.Read())
-                {
-                    OverHeadHistory mlfHistory = new OverHeadHistory();
-
-                    mlfHistory.StartDate =
-                            reader.GetDateTime(startDateIndex);
-                    mlfHistory.EndDate =
-                            !reader.IsDBNull(endDateIndex) ? (DateTime?)reader.GetDateTime(endDateIndex) : null;
-
-                    mlfHistory.W2Salary_Rate = reader.GetDecimal(w2Salary_RateIndex);
-                    mlfHistory.W2Hourly_Rate = reader.GetDecimal(w2Hourly_RateIndex);
-                    mlfHistory._1099_Hourly_Rate = reader.GetDecimal(_1099_Hourly_RateIndex);
-                    result.Add(mlfHistory);
-                }
+                result.Add(mlfHistory);
             }
         }
+
         public static List<OverHeadHistory> GetOverheadHistory()
         {
             using (SqlConnection connection = new SqlConnection(DataSourceHelper.DataConnection))
@@ -58,4 +53,3 @@ namespace DataAccess
         }
     }
 }
-
