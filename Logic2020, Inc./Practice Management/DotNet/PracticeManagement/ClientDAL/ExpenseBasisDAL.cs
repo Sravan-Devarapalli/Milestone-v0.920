f354f743924123c372ec785de8800817fcf2f19c
SainathC <SainathC>
2013-07-02 10:03:00 -0500

    Comments - Made code clean up with the help of Resharper plugin.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/ClientDAL/ExpenseBasisDAL.cs
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/ClientDAL/ExpenseBasisDAL.cs
@@ -7,54 +7,51 @@ using DataTransferObjects;
 
 namespace DataAccess
 {
-	/// <summary>
-	/// Provides an access to the ExpenseBasis database table.
-	/// </summary>
-	public static class ExpenseBasisDAL
-	{
-		private const string ExpenseBasisListAllProcedure = "dbo.ExpenseBasisListAll";
+    /// <summary>
+    /// Provides an access to the ExpenseBasis database table.
+    /// </summary>
+    public static class ExpenseBasisDAL
+    {
+        private const string ExpenseBasisListAllProcedure = "dbo.ExpenseBasisListAll";
 
-		private const string ExpenseBasisIdColumn = "ExpenseBasisId";
-		private const string NameColumn = "Name";
+        private const string ExpenseBasisIdColumn = "ExpenseBasisId";
+        private const string NameColumn = "Name";
 
-		/// <summary>
-		/// Retrieves the list of the Expense Bases.
-		/// </summary>
-		/// <returns>The list of the <see cref="ExpenseBasis"/> objects.</returns>
-		public static List<ExpenseBasis> ExpenseBasisListAll()
-		{
-			using (SqlConnection connection = new SqlConnection(DataSourceHelper.DataConnection))
-			using (SqlCommand command = new SqlCommand(ExpenseBasisListAllProcedure, connection))
-			{
-				command.CommandType = CommandType.StoredProcedure;
+        /// <summary>
+        /// Retrieves the list of the Expense Bases.
+        /// </summary>
+        /// <returns>The list of the <see cref="ExpenseBasis"/> objects.</returns>
+        public static List<ExpenseBasis> ExpenseBasisListAll()
+        {
+            using (SqlConnection connection = new SqlConnection(DataSourceHelper.DataConnection))
+            using (SqlCommand command = new SqlCommand(ExpenseBasisListAllProcedure, connection))
+            {
+                command.CommandType = CommandType.StoredProcedure;
 
-				connection.Open();
+                connection.Open();
 
-				using (SqlDataReader reader = command.ExecuteReader())
-				{
-					List<ExpenseBasis> result = new List<ExpenseBasis>();
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    List<ExpenseBasis> result = new List<ExpenseBasis>();
 
-					ReadExpenseBases(reader, result);
+                    ReadExpenseBases(reader, result);
 
-					return result;
-				}
-			}
-		}
+                    return result;
+                }
+            }
+        }
 
-		private static void ReadExpenseBases(DbDataReader reader, List<ExpenseBasis> result)
-		{
-			if (reader.HasRows)
-			{
-				int expenseBasisIdIndex = reader.GetOrdinal(ExpenseBasisIdColumn);
-				int nameIndex = reader.GetOrdinal(NameColumn);
+        private static void ReadExpenseBases(DbDataReader reader, List<ExpenseBasis> result)
+        {
+            if (!reader.HasRows) return;
+            int expenseBasisIdIndex = reader.GetOrdinal(ExpenseBasisIdColumn);
+            int nameIndex = reader.GetOrdinal(NameColumn);
 
-				while (reader.Read())
-				{
-					result.Add(
-						new ExpenseBasis() { Id = reader.GetInt32(expenseBasisIdIndex), Name = reader.GetString(nameIndex) });
-				}
-			}
-		}
-	}
+            while (reader.Read())
+            {
+                result.Add(
+                    new ExpenseBasis { Id = reader.GetInt32(expenseBasisIdIndex), Name = reader.GetString(nameIndex) });
+            }
+        }
+    }
 }
-
