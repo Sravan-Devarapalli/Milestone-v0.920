481be95b9b55a7a00df708594baa9078c61aa3ab
SainathC <SainathC>
2013-07-02 10:03:00 -0500

    Comments - Made code clean up with the help of Resharper plugin.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/ClientDAL/WeekPaidOptionDAL.cs
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/ClientDAL/WeekPaidOptionDAL.cs
@@ -6,54 +6,51 @@ using DataTransferObjects;
 
 namespace DataAccess
 {
-	/// <summary>
-	/// Provides an access to the WeekPaidOption database table
-	/// </summary>
-	public static class WeekPaidOptionDAL
-	{
-		private const string WeekPaidOptionListAllProcedure = "dbo.WeekPaidOptionListAll";
+    /// <summary>
+    /// Provides an access to the WeekPaidOption database table
+    /// </summary>
+    public static class WeekPaidOptionDAL
+    {
+        private const string WeekPaidOptionListAllProcedure = "dbo.WeekPaidOptionListAll";
 
-		private const string WeekPaidOptionIdColumn = "WeekPaidOptionId";
-		private const string NameColumn = "Name";
+        private const string WeekPaidOptionIdColumn = "WeekPaidOptionId";
+        private const string NameColumn = "Name";
 
-		/// <summary>
-		/// Retrieves the list of the Week Paid Options
-		/// </summary>
-		/// <returns>The list of the <see cref="WeekPaidOption"/> objects.</returns>
-		public static List<WeekPaidOption> WeekPaidOptionListAll()
-		{
-			using (SqlConnection connection = new SqlConnection(DataSourceHelper.DataConnection))
-			using (SqlCommand command = new SqlCommand(WeekPaidOptionListAllProcedure, connection))
-			{
-				command.CommandType = CommandType.StoredProcedure;
+        /// <summary>
+        /// Retrieves the list of the Week Paid Options
+        /// </summary>
+        /// <returns>The list of the <see cref="WeekPaidOption"/> objects.</returns>
+        public static List<WeekPaidOption> WeekPaidOptionListAll()
+        {
+            using (SqlConnection connection = new SqlConnection(DataSourceHelper.DataConnection))
+            using (SqlCommand command = new SqlCommand(WeekPaidOptionListAllProcedure, connection))
+            {
+                command.CommandType = CommandType.StoredProcedure;
 
-				connection.Open();
+                connection.Open();
 
-				using (SqlDataReader reader = command.ExecuteReader())
-				{
-					List<WeekPaidOption> result = new List<WeekPaidOption>();
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    List<WeekPaidOption> result = new List<WeekPaidOption>();
 
-					ReadWeekPaidOptions(reader, result);
+                    ReadWeekPaidOptions(reader, result);
 
-					return result;
-				}
-			}
-		}
+                    return result;
+                }
+            }
+        }
 
-		private static void ReadWeekPaidOptions(SqlDataReader reader, List<WeekPaidOption> result)
-		{
-			if (reader.HasRows)
-			{
-				int weekPaidOptionIdIndex = reader.GetOrdinal(WeekPaidOptionIdColumn);
-				int nameIndex = reader.GetOrdinal(NameColumn);
+        private static void ReadWeekPaidOptions(SqlDataReader reader, List<WeekPaidOption> result)
+        {
+            if (!reader.HasRows) return;
+            int weekPaidOptionIdIndex = reader.GetOrdinal(WeekPaidOptionIdColumn);
+            int nameIndex = reader.GetOrdinal(NameColumn);
 
-				while (reader.Read())
-				{
-					result.Add(
-						new WeekPaidOption() { Id = reader.GetInt32(weekPaidOptionIdIndex), Name = reader.GetString(nameIndex) });
-				}
-			}
-		}
-	}
+            while (reader.Read())
+            {
+                result.Add(
+                    new WeekPaidOption { Id = reader.GetInt32(weekPaidOptionIdIndex), Name = reader.GetString(nameIndex) });
+            }
+        }
+    }
 }
-
