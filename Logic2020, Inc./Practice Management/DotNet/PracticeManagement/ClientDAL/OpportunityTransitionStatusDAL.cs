887ec2a99a942c52c01a5768120e4f31cbf92656
SainathC <SainathC>
2013-07-02 10:03:00 -0500

    Comments - Made code clean up with the help of Resharper plugin.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/ClientDAL/OpportunityTransitionStatusDAL.cs
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/ClientDAL/OpportunityTransitionStatusDAL.cs
@@ -7,65 +7,62 @@ using DataTransferObjects;
 
 namespace DataAccess
 {
-	/// <summary>
-	/// Provides an access to the OpportunityTransitionStatus database table.
-	/// </summary>
-	public static class OpportunityTransitionStatusDAL
-	{
-		#region Constants
+    /// <summary>
+    /// Provides an access to the OpportunityTransitionStatus database table.
+    /// </summary>
+    public static class OpportunityTransitionStatusDAL
+    {
+        #region Constants
 
-		private const string OpportunityTransitionStatusListAllProcedure = "dbo.OpportunityTransitionStatusListAll";
+        private const string OpportunityTransitionStatusListAllProcedure = "dbo.OpportunityTransitionStatusListAll";
 
-		private const string OpportunityTransitionStatusIdColumn = "OpportunityTransitionStatusId";
-		private const string NameColumn = "Name";
+        private const string OpportunityTransitionStatusIdColumn = "OpportunityTransitionStatusId";
+        private const string NameColumn = "Name";
 
-		#endregion
+        #endregion Constants
 
-		#region Constants
+        #region Constants
 
-		/// <summary>
-		/// Retrieves a list of the Opportunity Transition Statuses objects.
-		/// </summary>
-		/// <returns>A list of the <see cref="OpportunityTransitionStatus"/> objects.</returns>
-		public static List<OpportunityTransitionStatus> OpportunityTransitionStatusListAll()
-		{
-			using (SqlConnection connection = new SqlConnection(DataSourceHelper.DataConnection))
-			using (SqlCommand command =
-				new SqlCommand(OpportunityTransitionStatusListAllProcedure, connection))
-			{
-				command.CommandType = CommandType.StoredProcedure;
+        /// <summary>
+        /// Retrieves a list of the Opportunity Transition Statuses objects.
+        /// </summary>
+        /// <returns>A list of the <see cref="OpportunityTransitionStatus"/> objects.</returns>
+        public static List<OpportunityTransitionStatus> OpportunityTransitionStatusListAll()
+        {
+            using (SqlConnection connection = new SqlConnection(DataSourceHelper.DataConnection))
+            using (SqlCommand command =
+                new SqlCommand(OpportunityTransitionStatusListAllProcedure, connection))
+            {
+                command.CommandType = CommandType.StoredProcedure;
 
-				connection.Open();
-				using (SqlDataReader reader = command.ExecuteReader())
-				{
-					List<OpportunityTransitionStatus> result = new List<OpportunityTransitionStatus>();
-					ReadOpportunityTransitionStatuses(reader, result);
-					return result;
-				}
-			}
-		}
+                connection.Open();
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    List<OpportunityTransitionStatus> result = new List<OpportunityTransitionStatus>();
+                    ReadOpportunityTransitionStatuses(reader, result);
+                    return result;
+                }
+            }
+        }
 
-		private static void ReadOpportunityTransitionStatuses(DbDataReader reader, List<OpportunityTransitionStatus> result)
-		{
-			if (reader.HasRows)
-			{
-				int opportunityTransitionStatusIdIndex =
-					reader.GetOrdinal(OpportunityTransitionStatusIdColumn);
-				int nameIndex = reader.GetOrdinal(NameColumn);
+        private static void ReadOpportunityTransitionStatuses(DbDataReader reader, List<OpportunityTransitionStatus> result)
+        {
+            if (!reader.HasRows) return;
+            int opportunityTransitionStatusIdIndex =
+                reader.GetOrdinal(OpportunityTransitionStatusIdColumn);
+            int nameIndex = reader.GetOrdinal(NameColumn);
 
-				while (reader.Read())
-				{
-					result.Add(
-						new OpportunityTransitionStatus()
-						{
-							Id = reader.GetInt32(opportunityTransitionStatusIdIndex),
-							Name = reader.GetString(nameIndex)
-						});
-				}
-			}
-		}
+            while (reader.Read())
+            {
+                result.Add(
+                    new OpportunityTransitionStatus()
+                        {
+                            Id = reader.GetInt32(opportunityTransitionStatusIdIndex),
+                            Name = reader.GetString(nameIndex)
+                        });
+            }
+        }
 
-		#endregion
-	}
+        #endregion Constants
+    }
 }
-
