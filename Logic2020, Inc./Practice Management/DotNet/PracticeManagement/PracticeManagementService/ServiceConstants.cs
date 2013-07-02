a7299a8bb14ac1191517af4c5e0d9e77ef9bb79f
SainathC <SainathC>
2013-07-02 10:44:00 -0500

    Comments - Made code clean up with the help of Resharper plugin.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementService/ServiceConstants.cs
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementService/ServiceConstants.cs
@@ -3,20 +3,19 @@ using System.Transactions;
 
 namespace PracticeManagementService
 {
-	/// <summary>
-	/// Contains common constants.
-	/// </summary>
-	public static class ServiceConstants
-	{
-		/// <summary>
-		/// Default options for a distributed transaction.
-		/// </summary>
-		public static readonly TransactionOptions DistributedTransactionOptions =
-			new TransactionOptions()
-			{
-				IsolationLevel = IsolationLevel.ReadCommitted,
-				Timeout = TimeSpan.FromSeconds(600)
-			};
-	}
+    /// <summary>
+    /// Contains common constants.
+    /// </summary>
+    public static class ServiceConstants
+    {
+        /// <summary>
+        /// Default options for a distributed transaction.
+        /// </summary>
+        public static readonly TransactionOptions DistributedTransactionOptions =
+            new TransactionOptions
+                {
+                    IsolationLevel = IsolationLevel.ReadCommitted,
+                    Timeout = TimeSpan.FromSeconds(600)
+                };
+    }
 }
-
