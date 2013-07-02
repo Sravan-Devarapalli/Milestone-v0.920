af484db6680c6629da845e2b6dd91ca49d7ef4ff
SainathC <SainathC>
2013-07-02 10:44:00 -0500

    Comments - Made code clean up with the help of Resharper plugin.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementService/TimescaleService.svc.cs
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementService/TimescaleService.svc.cs
@@ -1,25 +1,25 @@
-﻿using System.ServiceModel.Activation;
+﻿using System.Collections.Generic;
+using System.ServiceModel.Activation;
 using DataAccess;
 using DataTransferObjects;
-using System.Collections.Generic;
 
 namespace PracticeManagementService
 {
-	// NOTE: If you change the class name "TimescaleService" here, you must also update the reference to "TimescaleService" in Web.config.
-	[AspNetCompatibilityRequirements(RequirementsMode = AspNetCompatibilityRequirementsMode.Allowed)]
-	public class TimescaleService : ITimescaleService
-	{
-		#region ITimescaleService Members
+    // NOTE: If you change the class name "TimescaleService" here, you must also update the reference to "TimescaleService" in Web.config.
+    [AspNetCompatibilityRequirements(RequirementsMode = AspNetCompatibilityRequirementsMode.Allowed)]
+    public class TimescaleService : ITimescaleService
+    {
+        #region ITimescaleService Members
 
-		/// <summary>
-		/// Retrives a <see cref="Timescale"/> by its ID.
-		/// </summary>
-		/// <param name="timescaleId">An ID of the <see cref="Timescale"/> to be retrieved.</param>
-		/// <returns>A <see cref="Timescale"/> object if found and null otherwise.</returns>
-		public Timescale GetById(DataTransferObjects.TimescaleType timescaleId)
-		{
-			return TimescaleDAL.GetById(timescaleId);
-		}
+        /// <summary>
+        /// Retrives a <see cref="Timescale"/> by its ID.
+        /// </summary>
+        /// <param name="timescaleId">An ID of the <see cref="Timescale"/> to be retrieved.</param>
+        /// <returns>A <see cref="Timescale"/> object if found and null otherwise.</returns>
+        public Timescale GetById(TimescaleType timescaleId)
+        {
+            return TimescaleDAL.GetById(timescaleId);
+        }
 
         /// <summary>
         /// Retrives all the timescale types.
@@ -29,7 +29,7 @@ namespace PracticeManagementService
         {
             return TimescaleDAL.GetAll();
         }
-		#endregion
-	}
-}
 
+        #endregion ITimescaleService Members
+    }
+}
