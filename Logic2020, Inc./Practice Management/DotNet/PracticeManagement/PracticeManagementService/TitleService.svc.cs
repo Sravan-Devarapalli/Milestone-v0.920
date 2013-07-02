05532d58ec8dbf73cc21792adc36ef4162d58d41
SainathC <SainathC>
2013-07-02 10:44:00 -0500

    Comments - Made code clean up with the help of Resharper plugin.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementService/TitleService.svc.cs
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementService/TitleService.svc.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.ServiceModel.Activation;
 using DataAccess;
 using DataTransferObjects;
@@ -80,4 +79,3 @@ namespace PracticeManagementService
         }
     }
 }
-
