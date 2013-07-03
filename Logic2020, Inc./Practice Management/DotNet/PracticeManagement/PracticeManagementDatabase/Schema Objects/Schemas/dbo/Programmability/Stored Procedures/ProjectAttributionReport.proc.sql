e60065449d432a018b1216fac07224cb7105c07f
SainathC <SainathC>
2013-07-03 01:37:00 -0500

    Comments - Defect Number: 3160; Defect RecordID: 3156; Server Address: localhost; Server Port: 99; Database Name: Practice Management; Defect Summary: Project Details > Commission Tab New Functionality; Defect Description: A recent change for some Logic20/20 employees (Client Directors and Senior Managers) has been the implementation of targets for sales and delivery of client projects. Bonuses are now based on the amount they contribute to the sales and delivery of our projects. The Finance team has been tracking this manually right now, but we recognize that adding some of this tracking functionality to PM would make help make the process more efficient.
    Attached is a mock-up of the enhancements I am requesting to the current "Commissions" tab in Project Details. All current functionality on that tab should be deprecated. Access to the tab should be limited to individuals with the title of Senior Manager and the Senior Leadership and Admin roles.
    
    The UI on the Project Details will feed an Excel-based extract (see mock-up also attached) that should be added as a new tab entitled "Sales Delivery Attributions" to the Attainment report. Please also create a stored procedure so that this report could be run separately if a person wished. Please set up the stored procedure the same as the others that you've done for us recently, allowing us to specify the time range.
    
    Unrelated to the Commission tab, but related to general clean up and organization of the information on the Project Tab, please move the "Project Time Types" tab and related functionality, and move it up into the main row of tabs. It currently sits as an "orphan" at the bottom of the Project Details page, and I've received user feedback that it's placement on the page can cause confusion as to its purpose.
    
    Could you please review the attached and let me know of any questions? What will be the approximate level of effort for this request?

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/ProjectAttributionReport.proc.sql
@@ -0,0 +1,35 @@
+﻿CREATE PROCEDURE [dbo].[ProjectAttributionReport]
+	(
+	@StartDate	DATETIME,
+	@EndDate	DATETIME
+	)
+AS
+BEGIN
+	SELECT	ART.Name AS RecordType,
+			AT.Name AS AttributionType,
+			PS.Name AS ProjectStatus,
+			P.ProjectNumber,
+			C.Name AS Account,
+			BG.Name AS BusinessGroup,
+			PG.Name AS BusinessUnit,
+			P.Name AS ProjectName,
+			BT.Name AS NewOrExtension,
+		   CASE WHEN ART.IsRangeType = 1 THEN person.LastName+', '+person.FirstName ELSE practice.Name END AS Name,
+		   CASE WHEN ART.IsRangeType = 1 THEN title.Title ELSE '' END AS Title,
+		   ISNULL(A.StartDate,P.StartDate) AS StartDate,
+		   ISNULL(A.EndDate,P.EndDate) AS EndDate,
+		   A.Percentage AS CommissionPercentage
+	FROM dbo.Attribution AS A
+	INNER JOIN dbo.Project AS P ON P.ProjectId = A.ProjectId 
+	INNER JOIN dbo.AttributionTypes AS AT ON AT.AttributionTypeId = A.AttributionTypeId
+	INNER JOIN dbo.AttributionRecordTypes AS ART ON ART.AttributionRecordId = A.AttributionRecordTypeId
+	INNER JOIN dbo.ProjectStatus AS PS ON PS.ProjectStatusId = P.ProjectStatusId
+	INNER JOIN dbo.Client AS C ON C.ClientId = P.ClientId
+	INNER JOIN dbo.ProjectGroup AS PG ON PG.GroupId = P.GroupId
+	INNER JOIN dbo.BusinessGroup AS BG ON BG.BusinessGroupId = PG.BusinessGroupId
+	LEFT JOIN dbo.BusinessType AS BT ON BT.BusinessTypeId = P.BusinessTypeId 
+	LEFT JOIN dbo.Person AS person ON person.PersonId = A.TargetId
+	LEFT JOIN dbo.Title AS title ON title.TitleId = person.TitleId
+	LEFT JOIN dbo.Practice AS practice ON practice.PracticeId = A.TargetId
+	WHERE (@StartDate <= A.EndDate AND A.StartDate <= @EndDate) OR (ART.IsRangeType = 0 AND @StartDate<=P.EndDate AND P.StartDate <= @EndDate) AND P.ProjectStatusId IN (2,3,4)
+END
