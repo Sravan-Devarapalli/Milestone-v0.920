930611bcf572b0f59f09692298392c9e8b270ec2
SainathC <SainathC>
2013-07-03 03:35:00 -0500

    Comments - Defect Number: 3160; Defect RecordID: 3156; Server Address: localhost; Server Port: 99; Database Name: Practice Management; Defect Summary: Project Details > Commission Tab New Functionality; Defect Description: A recent change for some Logic20/20 employees (Client Directors and Senior Managers) has been the implementation of targets for sales and delivery of client projects. Bonuses are now based on the amount they contribute to the sales and delivery of our projects. The Finance team has been tracking this manually right now, but we recognize that adding some of this tracking functionality to PM would make help make the process more efficient.
    Attached is a mock-up of the enhancements I am requesting to the current "Commissions" tab in Project Details. All current functionality on that tab should be deprecated. Access to the tab should be limited to individuals with the title of Senior Manager and the Senior Leadership and Admin roles.
    
    The UI on the Project Details will feed an Excel-based extract (see mock-up also attached) that should be added as a new tab entitled "Sales Delivery Attributions" to the Attainment report. Please also create a stored procedure so that this report could be run separately if a person wished. Please set up the stored procedure the same as the others that you've done for us recently, allowing us to specify the time range.
    
    Unrelated to the Commission tab, but related to general clean up and organization of the information on the Project Tab, please move the "Project Time Types" tab and related functionality, and move it up into the main row of tabs. It currently sits as an "orphan" at the bottom of the Project Details page, and I've received user feedback that it's placement on the page can cause confusion as to its purpose.
    
    Could you please review the attached and let me know of any questions? What will be the approximate level of effort for this request?

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Views/v_Milestone.view.sql
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Views/v_Milestone.view.sql
@@ -1,11 +1,4 @@
-﻿-- =============================================
--- Author:		Anatoliy Lokshin
--- Create date: 5-28-2008
--- Updated by:	Sainath C
--- Update date:	06-05-2012
--- Description:	List milestones with their details
--- =============================================
-CREATE VIEW dbo.v_Milestone
+﻿CREATE VIEW dbo.v_Milestone
 AS
 	SELECT c.ClientId,
 		   c.IsMarginColorInfoEnabled,
@@ -29,9 +22,6 @@ AS
 	       m.IsChargeable AS 'MilestoneIsChargeable',
 	       m.ConsultantsCanAdjust,
 	       CAST(ISNULL(h.MilestoneHours, 0) AS DECIMAL(18,2)) ExpectedHours,
-	       ISNULL((SELECT SUM(cm.FractionOfMargin)
-	                 FROM dbo.Commission AS cm
-	                WHERE cm.ProjectId = m.ProjectId AND cm.CommissionType = 1), 0) AS SalesCommission,
 	       ISNULL(h.PersonCount, 0) AS PersonCount,
 	       (SELECT COUNT(*)
 	          FROM dbo.Calendar AS cal
@@ -39,10 +29,10 @@ AS
 	       p.BuyerName,
            p.GroupId,
 		   p.IsAllowedToShow,
-		   p.ProjectOwnerId
+		   p.ProjectOwnerId,
+		   p.SalesPersonId
 	  FROM dbo.Milestone AS m
 	       INNER JOIN dbo.Project AS p ON m.ProjectId = p.ProjectId
 	       INNER JOIN dbo.Client AS c ON p.ClientId = c.ClientId
 		   INNER JOIN dbo.ProjectStatus AS ps on p.ProjectStatusId=ps.ProjectStatusId
 	       LEFT JOIN dbo.v_MilestoneHours AS h ON m.MilestoneId = h.MilestoneId
-
