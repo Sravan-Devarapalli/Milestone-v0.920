dce75d065ff50da98489c39f91e0f32555f31e4c
SainathC <SainathC>
2013-07-03 01:57:00 -0500

    Comments - Defect Number: 3160; Defect RecordID: 3156; Server Address: localhost; Server Port: 99; Database Name: Practice Management; Defect Summary: Project Details > Commission Tab New Functionality; Defect Description: A recent change for some Logic20/20 employees (Client Directors and Senior Managers) has been the implementation of targets for sales and delivery of client projects. Bonuses are now based on the amount they contribute to the sales and delivery of our projects. The Finance team has been tracking this manually right now, but we recognize that adding some of this tracking functionality to PM would make help make the process more efficient.
    Attached is a mock-up of the enhancements I am requesting to the current "Commissions" tab in Project Details. All current functionality on that tab should be deprecated. Access to the tab should be limited to individuals with the title of Senior Manager and the Senior Leadership and Admin roles.
    
    The UI on the Project Details will feed an Excel-based extract (see mock-up also attached) that should be added as a new tab entitled "Sales Delivery Attributions" to the Attainment report. Please also create a stored procedure so that this report could be run separately if a person wished. Please set up the stored procedure the same as the others that you've done for us recently, allowing us to specify the time range.
    
    Unrelated to the Commission tab, but related to general clean up and organization of the information on the Project Tab, please move the "Project Time Types" tab and related functionality, and move it up into the main row of tabs. It currently sits as an "orphan" at the bottom of the Project Details page, and I've received user feedback that it's placement on the page can cause confusion as to its purpose.
    
    Could you please review the attached and let me know of any questions? What will be the approximate level of effort for this request?

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/Controls/Projects/ProjectFinancials.ascx.cs
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/Controls/Projects/ProjectFinancials.ascx.cs
@@ -1,11 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Globalization;
 using System.Web.UI;
+using System.Web.UI.MobileControls;
 using DataTransferObjects;
 using PraticeManagement.ProjectService;
 using PraticeManagement.Security;
-using System.Web.UI.MobileControls;
-using System.Collections.Generic;
 using PraticeManagement.Utils;
 
 namespace PraticeManagement.Controls.Projects
@@ -17,23 +17,21 @@ namespace PraticeManagement.Controls.Projects
         private const string BenchCssClass = "Bench";
         private const string MarginCssClass = "Margin";
 
-        #endregion
+        #endregion Constants
 
         #region Fields
 
         private SeniorityAnalyzer _milestonesSeniorityAnalyzer;
         private ComputedFinancials _financials;
 
-        #endregion
+        #endregion Fields
 
         public Project Project { get; set; }
 
         protected void Page_PreRender(object sender, EventArgs e)
         {
-
             InitSeniorityAnalyzers();
             DisplayInterestValues(Project);
-
         }
 
         private void InitSeniorityAnalyzers()
@@ -55,10 +53,10 @@ namespace PraticeManagement.Controls.Projects
 
         private void DisplayInterestValues(Project project)
         {
-            // Interest values            
+            // Interest values
             if (Financials != null)
             {
-                var hide =_milestonesSeniorityAnalyzer.GreaterSeniorityExists;
+                var hide = _milestonesSeniorityAnalyzer.GreaterSeniorityExists;
                 var ht = Resources.Controls.HiddenCellText;
 
                 var nfi = new NumberFormatInfo { CurrencyDecimalDigits = 0, CurrencySymbol = "$" };
@@ -72,14 +70,14 @@ namespace PraticeManagement.Controls.Projects
                 }
                 lblDiscount.Text = String.Format(nfi, "{0}", project.Discount);
                 var discountAmt = (Financials.Revenue.Value - Financials.RevenueNet.Value);
-                lblDiscountAmount.Text = discountAmt>=0?
+                lblDiscountAmount.Text = discountAmt >= 0 ?
                     String.Format(nfi, "{0:c}", discountAmt) : ((PracticeManagementCurrency)discountAmt).ToString();
-                lblRevenueNet.Text = Financials.RevenueNet.Value>=0?
-                    (String.Format(nfi, "{0:c}", Financials.RevenueNet.Value)):
+                lblRevenueNet.Text = Financials.RevenueNet.Value >= 0 ?
+                    (String.Format(nfi, "{0:c}", Financials.RevenueNet.Value)) :
                     Financials.RevenueNet.ToString();
                 lblEstimatedCogs.Text = hide ? ht : String.Format(nfi, "{0:c}", Financials.Cogs.Value);
                 lblExpenses.Text = hide ? ht : String.Format(nfi, "{0:c}", Financials.Expenses * ((100 - project.Discount) / 100));
-                lblReimbursedExpenses.Text = hide ? ht : String.Format(nfi, "{0:c}", Financials.ReimbursedExpenses * ((100- project.Discount)/100) );
+                lblReimbursedExpenses.Text = hide ? ht : String.Format(nfi, "{0:c}", Financials.ReimbursedExpenses * ((100 - project.Discount) / 100));
                 lblGrossMargin.Text = hide ? ht : String.Format(nfi, "{0:c}", Financials.GrossMargin.Value);
                 lblGrossMargin.CssClass =
                     Financials.GrossMargin.Value < 0 ? BenchCssClass : MarginCssClass;
@@ -93,24 +91,15 @@ namespace PraticeManagement.Controls.Projects
                     SetBackgroundColorForMargin(project.Client.Id.Value, project.Client.IsMarginColorInfoEnabled);
                 }
 
-                lblSalesCommission.Text =
-                    hide ? ht : 
-                    (Financials.SalesCommission.Value>=0?
-                    String.Format(nfi, "{0:c}", Financials.SalesCommission.Value) : Financials.SalesCommission.ToString());
                 lblEstimatedMargin.Text =
                     hide ? ht : String.Format(nfi, "{0:c}", Financials.MarginNet.Value);
                 lblEstimatedMargin.CssClass =
                     Financials.MarginNet.Value < 0 ? BenchCssClass : MarginCssClass;
-                lblPracticeManagerCommission.Text = hide
-                                                        ? ht
-                                                        : String.Format(nfi, "{0:c}",
-                                                                        Financials.PracticeManagementCommission.Value);
             }
         }
 
         private void SetBackgroundColorForMargin(int clientId, bool? individualClientMarginColorInfoEnabled)
         {
-
             int margin = (int)Financials.TargetMargin;
             List<ClientMarginColorInfo> cmciList = new List<ClientMarginColorInfo>();
 
@@ -150,4 +139,3 @@ namespace PraticeManagement.Controls.Projects
         }
     }
 }
-
