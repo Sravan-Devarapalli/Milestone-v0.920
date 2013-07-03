9ebe23d12c27ad978c29b5c3fc2d2fe9e48d74b5
SainathC <SainathC>
2013-07-03 03:35:00 -0500

    Comments - Defect Number: 3160; Defect RecordID: 3156; Server Address: localhost; Server Port: 99; Database Name: Practice Management; Defect Summary: Project Details > Commission Tab New Functionality; Defect Description: A recent change for some Logic20/20 employees (Client Directors and Senior Managers) has been the implementation of targets for sales and delivery of client projects. Bonuses are now based on the amount they contribute to the sales and delivery of our projects. The Finance team has been tracking this manually right now, but we recognize that adding some of this tracking functionality to PM would make help make the process more efficient.
    Attached is a mock-up of the enhancements I am requesting to the current "Commissions" tab in Project Details. All current functionality on that tab should be deprecated. Access to the tab should be limited to individuals with the title of Senior Manager and the Senior Leadership and Admin roles.
    
    The UI on the Project Details will feed an Excel-based extract (see mock-up also attached) that should be added as a new tab entitled "Sales Delivery Attributions" to the Attainment report. Please also create a stored procedure so that this report could be run separately if a person wished. Please set up the stored procedure the same as the others that you've done for us recently, allowing us to specify the time range.
    
    Unrelated to the Commission tab, but related to general clean up and organization of the information on the Project Tab, please move the "Project Time Types" tab and related functionality, and move it up into the main row of tabs. It currently sits as an "orphan" at the bottom of the Project Details page, and I've received user feedback that it's placement on the page can cause confusion as to its purpose.
    
    Could you please review the attached and let me know of any questions? What will be the approximate level of effort for this request?

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/ProjectListAll.proc.sql
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/ProjectListAll.proc.sql
@@ -1,17 +1,12 @@
-﻿
-CREATE PROCEDURE [dbo].[ProjectListAll]
+﻿CREATE PROCEDURE  [dbo].[ProjectListAll]
 (
-	@ClientId              INT = NULL, 
 	@ShowProjected         BIT = 0,
 	@ShowCompleted         BIT = 0,
     @ShowActive            BIT = 0,
-	@ShowExperimental      BIT = 0,
-	@SalespersonId         INT = NULL,
-	@PracticeManagerId     INT = NULL,
-	@PracticeId            INT = NULL,
-	@ProjectGroupId        INT = NULL
+	@ShowExperimental      BIT = 0
 )
 AS
+BEGIN
 	SET NOCOUNT ON;
 
 	SELECT p.ClientId,
@@ -42,23 +37,13 @@ AS
 	  FROM dbo.v_Project AS p
 	  INNER JOIN dbo.ProjectGroup AS pg 
 	  ON p.GroupId = pg.GroupId
-	 WHERE (p.ClientId = @ClientId OR @ClientId IS NULL)
-	   AND (p.GroupId = @ProjectGroupId OR @ProjectGroupId IS NULL)
-	   AND (p.PracticeId = @PracticeId OR @PracticeId IS NULL)
-	   AND (   (@ShowProjected = 1 AND p.ProjectStatusId = 2)
-            OR (@ShowActive = 1 AND p.ProjectStatusId = 3)
-            OR (@ShowCompleted = 1 AND p.ProjectStatusId = 4)
-	        OR (@ShowExperimental = 1 AND (p.ProjectStatusId = 5 OR p.ProjectStatusId = 1)))
-       AND (   (@SalespersonId IS NULL AND @PracticeManagerId IS NULL)
-	        OR EXISTS (SELECT 1
-	                      FROM dbo.v_PersonProjectCommission AS c
-	                     WHERE c.ProjectId = p.ProjectId AND c.PersonId = @SalespersonId AND c.CommissionType = 1
-	                   UNION ALL
-	                   SELECT 1
-	                     FROM dbo.v_PersonProjectCommission AS c
-	                    WHERE c.ProjectId = p.ProjectId AND c.PersonId = @PracticeManagerId AND c.CommissionType = 2 
-	                   )
-	       )
+	 WHERE  (
+				(@ShowProjected = 1 AND p.ProjectStatusId = 2)
+            OR	(@ShowActive = 1 AND p.ProjectStatusId = 3)
+            OR	(@ShowCompleted = 1 AND p.ProjectStatusId = 4)
+	        OR	(@ShowExperimental = 1 AND (p.ProjectStatusId = 5 OR p.ProjectStatusId = 1))
+			)
+    
 	ORDER BY p.Name
-
+END
 
