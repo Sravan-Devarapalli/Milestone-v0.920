2600bc07c72dd10aca32a3d116e47b572c91f3e1
SainathC <SainathC>
2013-07-05 04:01:00 -0500

    Comments - Made code clean up with the help of CodeMaid plugin.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/DataTransferObjects/CompositeObjects/TimeEntriesGroupedByPersonProject.cs
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/DataTransferObjects/CompositeObjects/TimeEntriesGroupedByPersonProject.cs
@@ -1,7 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Runtime.Serialization;
 using DataTransferObjects.TimeEntry;
 
@@ -22,4 +20,3 @@ namespace DataTransferObjects.CompositeObjects
         { get; set; }
     }
 }
-
