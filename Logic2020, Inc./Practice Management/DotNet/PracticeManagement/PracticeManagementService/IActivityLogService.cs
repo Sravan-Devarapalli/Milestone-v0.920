c4dc180022d40e76110aa21891bee3e82e38762c
SainathC <SainathC>
2013-07-05 04:01:00 -0500

    Comments - Made code clean up with the help of CodeMaid plugin.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementService/IActivityLogService.cs
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementService/IActivityLogService.cs
@@ -6,32 +6,32 @@ using DataTransferObjects.ContextObjects;
 
 namespace PracticeManagementService
 {
-	[ServiceContract]
-	public interface IActivityLogService
-	{
-	    /// <summary>
-	    /// Retrives a list of activity log items for the specified period.
-	    /// </summary>
-	    /// <param name="context"></param>
-	    /// <param name="pageSize">A page size.</param>
-	    /// <param name="pageNo">A number of the page to be retrived.</param>
-	    /// <returns>The list of the <see cref="ActivityLogItem"/></returns>
-	    [OperationContract]
-		List<ActivityLogItem> ActivityLogList(ActivityLogSelectContext context, int pageSize, int pageNo);
+    [ServiceContract]
+    public interface IActivityLogService
+    {
+        /// <summary>
+        /// Retrives a list of activity log items for the specified period.
+        /// </summary>
+        /// <param name="context"></param>
+        /// <param name="pageSize">A page size.</param>
+        /// <param name="pageNo">A number of the page to be retrived.</param>
+        /// <returns>The list of the <see cref="ActivityLogItem"/></returns>
+        [OperationContract]
+        List<ActivityLogItem> ActivityLogList(ActivityLogSelectContext context, int pageSize, int pageNo);
 
-	    /// <summary>
-	    /// Retrives a number of the activity log items for the specified period.
-	    /// </summary>
-	    /// <param name="context"></param>
-	    /// <returns>The number of user's activities.</returns>
-	    [OperationContract]
+        /// <summary>
+        /// Retrives a number of the activity log items for the specified period.
+        /// </summary>
+        /// <param name="context"></param>
+        /// <returns>The number of user's activities.</returns>
+        [OperationContract]
         int ActivityLogGetCount(ActivityLogSelectContext context);
 
-	    /// <summary>
-	    /// Add item to log
-	    /// </summary>
-	    /// <param name="activityTypeId">Id of log type</param>
-	    /// <param name="logData">Xml as string of log</param>
+        /// <summary>
+        /// Add item to log
+        /// </summary>
+        /// <param name="activityTypeId">Id of log type</param>
+        /// <param name="logData">Xml as string of log</param>
         [OperationContract]
         void ActivityLogInsert(int activityTypeId, string logData);
 
@@ -43,4 +43,3 @@ namespace PracticeManagementService
         string GetDatabaseVersion();
     }
 }
-
