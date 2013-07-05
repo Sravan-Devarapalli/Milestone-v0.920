09ae71e5154012ad79b83e987e32b752dc44c7ec
SainathC <SainathC>
2013-07-05 04:01:00 -0500

    Comments - Made code clean up with the help of CodeMaid plugin.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/DataTransferObjects/ProjectStatus.cs
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/DataTransferObjects/ProjectStatus.cs
@@ -3,36 +3,36 @@ using System.Runtime.Serialization;
 
 namespace DataTransferObjects
 {
-	/// <summary>
-	/// Represents the stutus of the <see cref="Project"/>.
-	/// </summary>
-	[DataContract]
-	[Serializable]
-	public class ProjectStatus
-	{
-		#region Properties
+    /// <summary>
+    /// Represents the stutus of the <see cref="Project"/>.
+    /// </summary>
+    [DataContract]
+    [Serializable]
+    public class ProjectStatus
+    {
+        #region Properties
 
-		/// <summary>
-		/// Gets or sets an ID of the status.
-		/// </summary>
-		[DataMember]
-		public int Id
-		{
-			get;
-			set;
-		}
+        /// <summary>
+        /// Gets or sets an ID of the status.
+        /// </summary>
+        [DataMember]
+        public int Id
+        {
+            get;
+            set;
+        }
 
-		/// <summary>
-		/// Sets or sets a name of the status.
-		/// </summary>
-		[DataMember]
-		public string Name
-		{
-			get;
-			set;
-		}
+        /// <summary>
+        /// Sets or sets a name of the status.
+        /// </summary>
+        [DataMember]
+        public string Name
+        {
+            get;
+            set;
+        }
 
-		#endregion
+        #endregion Properties
 
         #region Computed values
 
@@ -44,29 +44,29 @@ namespace DataTransferObjects
             }
         }
 
-        #endregion
+        #endregion Computed values
 
-		#region Methods
+        #region Methods
 
-		/// <summary>
-		/// Returns the hash code for the instance.
-		/// </summary>
-		/// <returns>The hash code.</returns>
-		public override int GetHashCode()
-		{
-			return Id.GetHashCode();
-		}
+        /// <summary>
+        /// Returns the hash code for the instance.
+        /// </summary>
+        /// <returns>The hash code.</returns>
+        public override int GetHashCode()
+        {
+            return Id.GetHashCode();
+        }
 
-		/// <summary>
-		/// Determines whether the instance is equals to the another one.
-		/// </summary>
-		/// <param name="obj">The object to compare with.</param>
-		/// <returns>true if the objects are equal and false otherwise.</returns>
-		public override bool Equals(object obj)
-		{
-			return (obj is ProjectStatus) && ((ProjectStatus)obj).Id == Id;
-		}
+        /// <summary>
+        /// Determines whether the instance is equals to the another one.
+        /// </summary>
+        /// <param name="obj">The object to compare with.</param>
+        /// <returns>true if the objects are equal and false otherwise.</returns>
+        public override bool Equals(object obj)
+        {
+            return (obj is ProjectStatus) && ((ProjectStatus)obj).Id == Id;
+        }
 
-		#endregion
-	}
+        #endregion Methods
+    }
 }
