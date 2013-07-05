067c0d3468ff48ce6bc5b29b1e02d6f9de0f007b
SainathC <SainathC>
2013-07-05 04:01:00 -0500

    Comments - Made code clean up with the help of CodeMaid plugin.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/DataTransferObjects/ContextObjects/ConsultantMilestonesContext.cs
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/DataTransferObjects/ContextObjects/ConsultantMilestonesContext.cs
@@ -5,15 +5,23 @@ namespace DataTransferObjects.ContextObjects
     public class ConsultantMilestonesContext
     {
         public int PersonId { get; set; }
+
         public DateTime StartDate { get; set; }
+
         public DateTime EndDate { get; set; }
+
         public bool IncludeActiveProjects { get; set; }
+
         public bool IncludeProjectedProjects { get; set; }
+
         public bool IncludeInactiveProjects { get; set; }
+
         public bool IncludeCompletedProjects { get; set; }
+
         public bool IncludeInternalProjects { get; set; }
+
         public bool IncludeExperimentalProjects { get; set; }
+
         public bool IncludeDefaultMileStone { get; set; }
     }
 }
-
