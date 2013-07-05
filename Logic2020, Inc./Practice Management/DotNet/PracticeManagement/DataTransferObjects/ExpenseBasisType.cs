718c1c67345d8c395ad2ff923df22990db6694db
SainathC <SainathC>
2013-07-05 04:01:00 -0500

    Comments - Made code clean up with the help of CodeMaid plugin.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/DataTransferObjects/ExpenseBasisType.cs
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/DataTransferObjects/ExpenseBasisType.cs
@@ -1,12 +1,10 @@
-﻿
-namespace DataTransferObjects
+﻿namespace DataTransferObjects
 {
-	public enum ExpenseBasisType
-	{
-		AbsolutAmount = 1,
-		RevenuePercentage = 2,
-		CogsPercentage = 3,
-		GrossMarginPercentage = 4
-	}
+    public enum ExpenseBasisType
+    {
+        AbsolutAmount = 1,
+        RevenuePercentage = 2,
+        CogsPercentage = 3,
+        GrossMarginPercentage = 4
+    }
 }
-
