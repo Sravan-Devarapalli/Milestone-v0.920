70ee49f89e3a1ab98c3aa71b935359b6ccc77eec
SainathC <SainathC>
2013-07-05 04:01:00 -0500

    Comments - Made code clean up with the help of CodeMaid plugin.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/DataTransferObjects/NumberFormatStyle.cs
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/DataTransferObjects/NumberFormatStyle.cs
@@ -2,47 +2,48 @@
 
 namespace DataTransferObjects
 {
-	/// <summary>
-	/// Determines the styles to display the numbers
-	/// </summary>
-	[DataContract]
-	public enum NumberFormatStyle
-	{
-		/// <summary>
-		/// General formatting without coloring
-		/// </summary>
-		[EnumMember]
-		General = 0,
+    /// <summary>
+    /// Determines the styles to display the numbers
+    /// </summary>
+    [DataContract]
+    public enum NumberFormatStyle
+    {
+        /// <summary>
+        /// General formatting without coloring
+        /// </summary>
+        [EnumMember]
+        General = 0,
 
-		/// <summary>
-		/// Explicitly specify to use formattong as a negative number
-		/// </summary>
-		[EnumMember]
-		Negative = 1,
+        /// <summary>
+        /// Explicitly specify to use formattong as a negative number
+        /// </summary>
+        [EnumMember]
+        Negative = 1,
 
-		/// <summary>
-		/// Specify formatting as a revenue value
-		/// </summary>
-		[EnumMember]
-		Revenue = 2,
+        /// <summary>
+        /// Specify formatting as a revenue value
+        /// </summary>
+        [EnumMember]
+        Revenue = 2,
 
-		/// <summary>
-		/// Specifies formatting as a COGS value
-		/// </summary>
-		[EnumMember]
-		Cogs = 4,
+        /// <summary>
+        /// Specifies formatting as a COGS value
+        /// </summary>
+        [EnumMember]
+        Cogs = 4,
 
-		/// <summary>
-		/// Specify formatting as a margin value
-		/// </summary>
-		[EnumMember]
-		Margin = 8,
+        /// <summary>
+        /// Specify formatting as a margin value
+        /// </summary>
+        [EnumMember]
+        Margin = 8,
 
-		/// <summary>
-		/// Specifies a value is a total
-		/// </summary>
-		[EnumMember]
-		Total = 16,
+        /// <summary>
+        /// Specifies a value is a total
+        /// </summary>
+        [EnumMember]
+        Total = 16,
+
         /*
 		/// <summary>
 		/// Specifies that the value must be formatted as a decimal
@@ -50,6 +51,7 @@ namespace DataTransferObjects
 		[EnumMember]
 		Decimal = 32
          */
+
         /// <summary>
         /// Specifies that the value must be formatted as percentage
         /// </summary>
@@ -57,4 +59,3 @@ namespace DataTransferObjects
         Percent = 32
     }
 }
-
