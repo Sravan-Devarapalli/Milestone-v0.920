364c2b475e1817d9a347dff3db04eb912c2f5f2c
SainathC <SainathC>
2013-07-05 04:01:00 -0500

    Comments - Made code clean up with the help of CodeMaid plugin.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/DataTransferObjects/Skills/PersonContactInfo.cs
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/DataTransferObjects/Skills/PersonContactInfo.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Runtime.Serialization;
 
 namespace DataTransferObjects.Skills
@@ -44,7 +41,5 @@ namespace DataTransferObjects.Skills
             set;
             get;
         }
-        
     }
 }
-
