46630dc155fb9e36f1e1607fe43876ab73c96307
SainathC <SainathC>
2013-07-05 04:01:00 -0500

    Comments - Made code clean up with the help of CodeMaid plugin.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/DataTransferObjects/MonthlyExpense.cs
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/DataTransferObjects/MonthlyExpense.cs
@@ -4,109 +4,108 @@ using System.Runtime.Serialization;
 
 namespace DataTransferObjects
 {
-	/// <summary>
-	/// Represents a montly expenses list for the item
-	/// </summary>
-	[DataContract]
-	[Serializable]
-	public class MonthlyExpense
-	{
-		#region Properties
+    /// <summary>
+    /// Represents a montly expenses list for the item
+    /// </summary>
+    [DataContract]
+    [Serializable]
+    public class MonthlyExpense
+    {
+        #region Properties
 
-		/// <summary>
-		/// Gets or sets a name of the Expense Item.
-		/// </summary>
-		[DataMember]
-		public string Name
-		{
-			get;
-			set;
-		}
+        /// <summary>
+        /// Gets or sets a name of the Expense Item.
+        /// </summary>
+        [DataMember]
+        public string Name
+        {
+            get;
+            set;
+        }
 
-		/// <summary>
-		/// Gets or sets a Category of the Monthly Expense Item.
-		/// </summary>
-		[DataMember]
-		public ExpenseCategory ExpenseCategory
-		{
-			get;
-			set;
-		}
+        /// <summary>
+        /// Gets or sets a Category of the Monthly Expense Item.
+        /// </summary>
+        [DataMember]
+        public ExpenseCategory ExpenseCategory
+        {
+            get;
+            set;
+        }
 
-		/// <summary>
-		/// Gets or sets a Basis of the Monthly Expense Item.
-		/// </summary>
-		[DataMember]
-		public ExpenseBasis ExpenseBasis
-		{
-			get;
-			set;
-		}
+        /// <summary>
+        /// Gets or sets a Basis of the Monthly Expense Item.
+        /// </summary>
+        [DataMember]
+        public ExpenseBasis ExpenseBasis
+        {
+            get;
+            set;
+        }
 
-		/// <summary>
-		/// Gets or sets a Week Paid Option of the Monthly Expense Item.
-		/// </summary>
-		[DataMember]
-		public WeekPaidOption WeekPaidOption
-		{
-			get;
-			set;
-		}
+        /// <summary>
+        /// Gets or sets a Week Paid Option of the Monthly Expense Item.
+        /// </summary>
+        [DataMember]
+        public WeekPaidOption WeekPaidOption
+        {
+            get;
+            set;
+        }
 
-		/// <summary>
-		/// Gets or sets the amount by months for the Monthly Expense Item.
-		/// </summary>
-		[DataMember]
-		public Dictionary<DateTime, decimal> MonthlyAmount
-		{
-			get;
-			set;
-		}
+        /// <summary>
+        /// Gets or sets the amount by months for the Monthly Expense Item.
+        /// </summary>
+        [DataMember]
+        public Dictionary<DateTime, decimal> MonthlyAmount
+        {
+            get;
+            set;
+        }
 
-		/// <summary>
-		/// Gets or sets a min month the expense was selected.
-		/// </summary>
-		[DataMember]
-		public DateTime MinMonth
-		{
-			get;
-			set;
-		}
+        /// <summary>
+        /// Gets or sets a min month the expense was selected.
+        /// </summary>
+        [DataMember]
+        public DateTime MinMonth
+        {
+            get;
+            set;
+        }
 
-		/// <summary>
-		/// Gets or sets a max month the expense was selected.
-		/// </summary>
-		[DataMember]
-		public DateTime MaxMonth
-		{
-			get;
-			set;
-		}
+        /// <summary>
+        /// Gets or sets a max month the expense was selected.
+        /// </summary>
+        [DataMember]
+        public DateTime MaxMonth
+        {
+            get;
+            set;
+        }
 
-		/// <summary>
-		/// Gets or sets an old name of the item.
-		/// </summary>
-		[DataMember]
-		public string OldName
-		{
-			get;
-			set;
-		}
+        /// <summary>
+        /// Gets or sets an old name of the item.
+        /// </summary>
+        [DataMember]
+        public string OldName
+        {
+            get;
+            set;
+        }
 
-		#endregion
+        #endregion Properties
 
-		#region Construction
+        #region Construction
 
-		/// <summary>
-		/// Creates a new empty object of the <see cref="MonthlyExpense"/> class.
-		/// </summary>
-		public MonthlyExpense()
-		{
-			MinMonth = DateTime.MaxValue;
-			MaxMonth = DateTime.MinValue;
-		}
+        /// <summary>
+        /// Creates a new empty object of the <see cref="MonthlyExpense"/> class.
+        /// </summary>
+        public MonthlyExpense()
+        {
+            MinMonth = DateTime.MaxValue;
+            MaxMonth = DateTime.MinValue;
+        }
 
-		#endregion
-	}
+        #endregion Construction
+    }
 }
-
