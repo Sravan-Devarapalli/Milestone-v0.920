567d587d334c8501748615aa20d451091fbab014
SainathC <SainathC>
2013-07-05 04:01:00 -0500

    Comments - Made code clean up with the help of CodeMaid plugin.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/DataTransferObjects/Timescale.cs
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/DataTransferObjects/Timescale.cs
@@ -3,32 +3,31 @@ using System.Runtime.Serialization;
 
 namespace DataTransferObjects
 {
-	/// <summary>
-	/// Represents a timescale entity
-	/// </summary>
-	[DataContract]
-	[Serializable]
-	public class Timescale
-	{
-		/// <summary>
-		/// Gets or sets a <see cref="Timescale"/> Id value.
-		/// </summary>
-		[DataMember]
-		public int Id
-		{
-			get;
-			set;
-		}
+    /// <summary>
+    /// Represents a timescale entity
+    /// </summary>
+    [DataContract]
+    [Serializable]
+    public class Timescale
+    {
+        /// <summary>
+        /// Gets or sets a <see cref="Timescale"/> Id value.
+        /// </summary>
+        [DataMember]
+        public int Id
+        {
+            get;
+            set;
+        }
 
-		/// <summary>
-		/// Gets or sets a name of the <see cref="Timescale"/>.
-		/// </summary>
-		[DataMember]
-		public string Name
-		{
-			get;
-			set;
-		}
-	}
+        /// <summary>
+        /// Gets or sets a name of the <see cref="Timescale"/>.
+        /// </summary>
+        [DataMember]
+        public string Name
+        {
+            get;
+            set;
+        }
+    }
 }
-
