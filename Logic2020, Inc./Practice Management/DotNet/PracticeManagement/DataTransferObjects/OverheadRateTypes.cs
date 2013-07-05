efa6a357d5b92636af48f5d907087bba681174ca
SainathC <SainathC>
2013-07-05 04:01:00 -0500

    Comments - Made code clean up with the help of CodeMaid plugin.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/DataTransferObjects/OverheadRateTypes.cs
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/DataTransferObjects/OverheadRateTypes.cs
@@ -1,15 +1,13 @@
-﻿
-namespace DataTransferObjects
+﻿namespace DataTransferObjects
 {
-	/// <summary>
-	/// Enlists the possible types of the person overhead
-	/// </summary>
-	public enum OverheadRateTypes
-	{
-		HourlyAddOn = 1,
-		BillRateMultiplier = 2,
-		MonthlyCost = 3,
-		PayRateMultiplier = 4
-	}
+    /// <summary>
+    /// Enlists the possible types of the person overhead
+    /// </summary>
+    public enum OverheadRateTypes
+    {
+        HourlyAddOn = 1,
+        BillRateMultiplier = 2,
+        MonthlyCost = 3,
+        PayRateMultiplier = 4
+    }
 }
-
