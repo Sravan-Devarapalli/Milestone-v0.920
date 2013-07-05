5c079d64a1875c0373d52c9e7d4cb9d5389df580
SainathC <SainathC>
2013-07-05 04:01:00 -0500

    Comments - Made code clean up with the help of CodeMaid plugin.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementService/PersonStatusService.svc.cs
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementService/PersonStatusService.svc.cs
@@ -5,21 +5,20 @@ using DataTransferObjects;
 
 namespace PracticeManagementService
 {
-	[AspNetCompatibilityRequirements(RequirementsMode = AspNetCompatibilityRequirementsMode.Allowed)]
-	public class PersonStatusService : IPersonStatusService
-	{
-		#region IPersonStatusService Members
+    [AspNetCompatibilityRequirements(RequirementsMode = AspNetCompatibilityRequirementsMode.Allowed)]
+    public class PersonStatusService : IPersonStatusService
+    {
+        #region IPersonStatusService Members
 
-		/// <summary>
-		/// Retrives the list of the person statuses.
-		/// </summary>
-		/// <returns>The list of the <see cref="PersonStatus"/> objects.</returns>
-		public List<PersonStatus> GetPersonStatuses()
-		{
-			return PersonStatusDAL.PersonStatusListAll();
-		}
+        /// <summary>
+        /// Retrives the list of the person statuses.
+        /// </summary>
+        /// <returns>The list of the <see cref="PersonStatus"/> objects.</returns>
+        public List<PersonStatus> GetPersonStatuses()
+        {
+            return PersonStatusDAL.PersonStatusListAll();
+        }
 
-		#endregion
-	}
+        #endregion IPersonStatusService Members
+    }
 }
-
