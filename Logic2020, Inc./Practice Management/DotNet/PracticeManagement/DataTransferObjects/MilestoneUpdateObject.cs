012c2fb09810e6643afee9302cec393bc0e0e361
SainathC <SainathC>
2013-07-05 04:01:00 -0500

    Comments - Made code clean up with the help of CodeMaid plugin.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/DataTransferObjects/MilestoneUpdateObject.cs
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/DataTransferObjects/MilestoneUpdateObject.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Runtime.Serialization;
 
 namespace DataTransferObjects
@@ -17,4 +14,3 @@ namespace DataTransferObjects
         public bool? IsEndDateChangeReflectedForMilestoneAndPersons { get; set; }
     }
 }
-
