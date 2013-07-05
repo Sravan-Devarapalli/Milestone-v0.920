5af44c9c59972b861c69da54557bfda4c9ae86ca
SainathC <SainathC>
2013-07-05 04:01:00 -0500

    Comments - Made code clean up with the help of CodeMaid plugin.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/DataTransferObjects/OpportunityTransitionStatusType.cs
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/DataTransferObjects/OpportunityTransitionStatusType.cs
@@ -1,30 +1,35 @@
-﻿
-using System.Runtime.Serialization;
+﻿using System.Runtime.Serialization;
 
 namespace DataTransferObjects
 {
-	/// <summary>
-	/// Determines the status for the <see cref="OpportunityTransition"/>.
-	/// </summary>
+    /// <summary>
+    /// Determines the status for the <see cref="OpportunityTransition"/>.
+    /// </summary>
     [DataContract]
     public enum OpportunityTransitionStatusType
-	{
+    {
         [EnumMember]
         All = 0,
+
         [EnumMember]
         Created = 1,
+
         [EnumMember]
         Notes = 2,
+
         [EnumMember]
         Proposed = 3,
+
         [EnumMember]
         Presented = 4,
+
         [EnumMember]
         SendOut = 5,
+
         [EnumMember]
         Pipeline = 6,
+
         [EnumMember]
         Lost = 7
-	}
+    }
 }
-
