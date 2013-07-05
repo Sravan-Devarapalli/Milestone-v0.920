d15ef77139d56bf1685427a53088f625a559020f
SainathC <SainathC>
2013-07-05 04:01:00 -0500

    Comments - Made code clean up with the help of CodeMaid plugin.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/DataTransferObjects/OpportunityStatus.cs
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/DataTransferObjects/OpportunityStatus.cs
@@ -3,32 +3,31 @@ using System.Runtime.Serialization;
 
 namespace DataTransferObjects
 {
-	/// <summary>
-	/// Reprecentes an opportunity status.
-	/// </summary>
-	[Serializable]
-	[DataContract]
-	public class OpportunityStatus
-	{
-		/// <summary>
-		/// Gets or sets an ID of the opportunity status.
-		/// </summary>
-		[DataMember]
-		public int Id
-		{
-			get;
-			set;
-		}
+    /// <summary>
+    /// Reprecentes an opportunity status.
+    /// </summary>
+    [Serializable]
+    [DataContract]
+    public class OpportunityStatus
+    {
+        /// <summary>
+        /// Gets or sets an ID of the opportunity status.
+        /// </summary>
+        [DataMember]
+        public int Id
+        {
+            get;
+            set;
+        }
 
-		/// <summary>
-		/// Gets or sets a name of the opportunity status.
-		/// </summary>
-		[DataMember]
-		public string Name
-		{
-			get;
-			set;
-		}
-	}
+        /// <summary>
+        /// Gets or sets a name of the opportunity status.
+        /// </summary>
+        [DataMember]
+        public string Name
+        {
+            get;
+            set;
+        }
+    }
 }
-
