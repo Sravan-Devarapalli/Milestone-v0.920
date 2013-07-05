0b20eb63d1e2f384525dee4a4b729d97a283e9ac
SainathC <SainathC>
2013-07-05 04:01:00 -0500

    Comments - Made code clean up with the help of CodeMaid plugin.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/DataTransferObjects/ExpenseCategory.cs
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/DataTransferObjects/ExpenseCategory.cs
@@ -3,32 +3,31 @@ using System.Runtime.Serialization;
 
 namespace DataTransferObjects
 {
-	/// <summary>
-	/// Represents an Expense Category entity
-	/// </summary>
-	[DataContract]
-	[Serializable]
-	public class ExpenseCategory
-	{
-		/// <summary>
-		/// Gets or sets an ID of the Expense Category
-		/// </summary>
-		[DataMember]
-		public int Id
-		{
-			get;
-			set;
-		}
+    /// <summary>
+    /// Represents an Expense Category entity
+    /// </summary>
+    [DataContract]
+    [Serializable]
+    public class ExpenseCategory
+    {
+        /// <summary>
+        /// Gets or sets an ID of the Expense Category
+        /// </summary>
+        [DataMember]
+        public int Id
+        {
+            get;
+            set;
+        }
 
-		/// <summary>
-		/// Gets or sets a name of the Expense Category
-		/// </summary>
-		[DataMember]
-		public string Name
-		{
-			get;
-			set;
-		}
-	}
+        /// <summary>
+        /// Gets or sets a name of the Expense Category
+        /// </summary>
+        [DataMember]
+        public string Name
+        {
+            get;
+            set;
+        }
+    }
 }
-
