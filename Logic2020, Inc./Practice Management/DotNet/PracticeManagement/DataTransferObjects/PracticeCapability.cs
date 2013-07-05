bff58e56a98f08cac2b58355a8fcb45b5ac5aa7a
SainathC <SainathC>
2013-07-05 04:01:00 -0500

    Comments - Made code clean up with the help of CodeMaid plugin.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/DataTransferObjects/PracticeCapability.cs
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/DataTransferObjects/PracticeCapability.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Runtime.Serialization;
 using System.Web;
 
@@ -44,7 +41,5 @@ namespace DataTransferObjects
                 return PracticeAbbreviation + " - " + Name;
             }
         }
-
     }
 }
-
