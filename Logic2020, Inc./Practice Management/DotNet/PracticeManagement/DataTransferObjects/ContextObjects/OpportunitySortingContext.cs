db0982c3163846b84019af8d60afb47443963ada
SainathC <SainathC>
2013-07-05 04:01:00 -0500

    Comments - Made code clean up with the help of CodeMaid plugin.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/DataTransferObjects/ContextObjects/OpportunitySortingContext.cs
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/DataTransferObjects/ContextObjects/OpportunitySortingContext.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Runtime.Serialization;
 
 namespace DataTransferObjects.ContextObjects
@@ -17,4 +14,3 @@ namespace DataTransferObjects.ContextObjects
         public string SortDirection { get; set; }
     }
 }
-
