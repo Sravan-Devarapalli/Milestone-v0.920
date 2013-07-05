1be0884d8a905d9d8fc45edd6bcbc734217336db
SainathC <SainathC>
2013-07-05 04:01:00 -0500

    Comments - Made code clean up with the help of CodeMaid plugin.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementService/PersonRoleService.svc.cs
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementService/PersonRoleService.svc.cs
@@ -5,18 +5,17 @@ using DataTransferObjects;
 
 namespace PracticeManagementService
 {
-	// NOTE: If you change the class name "PersonRoleService" here, you must also update the reference to "PersonRoleService" in Web.config.
-	[AspNetCompatibilityRequirements(RequirementsMode = AspNetCompatibilityRequirementsMode.Allowed)]
-	public class PersonRoleService : IPersonRoleService
-	{
-		/// <summary>
-		/// Retrives a list of the avalable role for the person.
-		/// </summary>
-		/// <returns>The list of <see cref="PersonRole"/> objects.</returns>
-		public List<PersonRole> GetPersonRoles()
-		{
-			return PersonRoleDAL.PersonRoleListAll();
-		}
-	}
+    // NOTE: If you change the class name "PersonRoleService" here, you must also update the reference to "PersonRoleService" in Web.config.
+    [AspNetCompatibilityRequirements(RequirementsMode = AspNetCompatibilityRequirementsMode.Allowed)]
+    public class PersonRoleService : IPersonRoleService
+    {
+        /// <summary>
+        /// Retrives a list of the avalable role for the person.
+        /// </summary>
+        /// <returns>The list of <see cref="PersonRole"/> objects.</returns>
+        public List<PersonRole> GetPersonRoles()
+        {
+            return PersonRoleDAL.PersonRoleListAll();
+        }
+    }
 }
-
