585d300241c0049520fe77a40e6c1c01ecc0f7ad
SainathC <SainathC>
2013-07-05 04:01:00 -0500

    Comments - Made code clean up with the help of CodeMaid plugin.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/DataTransferObjects/PersonStatusType.cs
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/DataTransferObjects/PersonStatusType.cs
@@ -1,27 +1,29 @@
-using System.Runtime.Serialization;
 using System.ComponentModel;
-
+using System.Runtime.Serialization;
 
 namespace DataTransferObjects
 {
-	/// <summary>
-	/// Determines the status of project
-	/// </summary>
-	[DataContract]
-	public enum PersonStatusType
-	{
-		[EnumMember]
-		Active = 1,
-		[EnumMember]
-		Terminated = 2,
-		[EnumMember]
+    /// <summary>
+    /// Determines the status of project
+    /// </summary>
+    [DataContract]
+    public enum PersonStatusType
+    {
+        [EnumMember]
+        Active = 1,
+
+        [EnumMember]
+        Terminated = 2,
+
+        [EnumMember]
         //Projected = Contingent
         Contingent = 3,
-		[EnumMember]
-		Inactive = 4,
+
+        [EnumMember]
+        Inactive = 4,
+
         [EnumMember]
         [Description("Termination Pending")]
         TerminationPending = 5
-	}
+    }
 }
-
