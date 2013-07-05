5c796a285f8ce6b8479ce40f9c02424cbf052cb3
SainathC <SainathC>
2013-07-05 04:01:00 -0500

    Comments - Made code clean up with the help of CodeMaid plugin.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/DataTransferObjects/Skills/PersonTraining.cs
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/DataTransferObjects/Skills/PersonTraining.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Runtime.Serialization;
 
 namespace DataTransferObjects.Skills
@@ -10,7 +7,6 @@ namespace DataTransferObjects.Skills
     [Serializable]
     public class PersonTraining
     {
-       
         public Person Person
         {
             set;
@@ -64,8 +60,5 @@ namespace DataTransferObjects.Skills
             set;
             get;
         }
-
-
     }
 }
-
