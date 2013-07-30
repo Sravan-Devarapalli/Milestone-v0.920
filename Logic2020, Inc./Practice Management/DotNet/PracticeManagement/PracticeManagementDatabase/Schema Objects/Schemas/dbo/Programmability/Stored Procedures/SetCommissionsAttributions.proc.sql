572db9303f05521c9cb1c0d5e2530ec2191bd389
SainathC <SainathC>
2013-07-30 09:34:00 -0500

    Comments - Defect Number: 3160; Defect RecordID: 3156; Server Address: localhost; Server Port: 99; Database Name: Practice Management; Defect Summary: Project Details > Commission Tab New Functionality; Defect Description: A recent change for some Logic20/20 employees (Client Directors and Senior Managers) has been the implementation of targets for sales and delivery of client projects. Bonuses are now based on the amount they contribute to the sales and delivery of our projects. The Finance team has been tracking this manually right now, but we recognize that adding some of this tracking functionality to PM would make help make the process more efficient.
    Attached is a mock-up of the enhancements I am requesting to the current "Commissions" tab in Project Details. All current functionality on that tab should be deprecated. Access to the tab should be limited to individuals with the title of Senior Manager and the Senior Leadership and Admin roles.
    
    The UI on the Project Details will feed an Excel-based extract (see mock-up also attached) that should be added as a new tab entitled "Sales Delivery Attributions" to the Attainment report. Please also create a stored procedure so that this report could be run separately if a person wished. Please set up the stored procedure the same as the others that you've done for us recently, allowing us to specify the time range.
    
    Unrelated to the Commission tab, but related to general clean up and organization of the information on the Project Tab, please move the "Project Time Types" tab and related functionality, and move it up into the main row of tabs. It currently sits as an "orphan" at the bottom of the Project Details page, and I've received user feedback that it's placement on the page can cause confusion as to its purpose.
    
    Could you please review the attached and let me know of any questions? What will be the approximate level of effort for this request?

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/SetCommissionsAttributions.proc.sql
@@ -0,0 +1,73 @@
+﻿--Need to call this sproc whenever person hire date,termination date,division changed.
+CREATE PROCEDURE [dbo].[SetCommissionsAttributions]
+(
+	@PersonId INT
+)
+AS
+BEGIN
+	DECLARE @PersonStatusId		INT,
+			@ConsultingDivId	INT,
+			@BusinessDevelopmentDivId INT,
+	  		@W2SalaryId			INT,
+			@W2HourlyId			INT
+			
+	  SELECT	@W2SalaryId = TimescaleId FROM dbo.Timescale WHERE Name = 'W2-Salary'
+	  SELECT	@W2HourlyId = TimescaleId FROM dbo.Timescale WHERE Name = 'W2-Hourly'
+	  SELECT    @ConsultingDivId = DivisionId FROM dbo.PersonDivision WHERE DivisionName = 'Consulting'
+	  SELECT    @BusinessDevelopmentDivId = DivisionId FROM dbo.PersonDivision WHERE DivisionName = 'Business Development'
+
+	  --Deleting Attribution Records based on Hire date and Termination date.		
+		DELETE A 
+		FROM	dbo.Attribution A
+		LEFT JOIN v_PersonHistory PH ON PH.PersonId = A.TargetId AND (PH.TerminationDate IS NULL OR A.StartDate <= PH.TerminationDate) AND (PH.HireDate <= A.EndDate)
+		WHERE A.AttributionRecordTypeId = 1 AND PH.PersonId IS NULL AND A.TargetId = @PersonId
+		
+		--Deleting Attribution Records based on Division History.
+		DELETE A 
+		FROM	dbo.Attribution A
+		LEFT JOIN v_DivisionHistory DH ON DH.PersonId = A.TargetId AND (DH.EndDate IS NULL OR A.StartDate < DH.EndDate) AND (DH.StartDate <= A.EndDate) AND DH.DivisionId IN (@ConsultingDivId,@BusinessDevelopmentDivId)
+		WHERE A.AttributionRecordTypeId = 1 AND DH.PersonId IS NULL AND A.TargetId = @PersonId
+		
+		--Updating Attribution Records based on Hire date and Termination date. 
+		;WITH UpdatableAttributions
+		AS
+		(
+		SELECT *,RANK() over (PARTITION	by A.AttributionId order by PH.HireDate) as Rank
+		FROM	dbo.Attribution A
+		INNER JOIN v_PersonHistory PH ON PH.PersonId = A.TargetId AND (PH.TerminationDate IS NULL OR A.StartDate <= PH.TerminationDate) AND (PH.HireDate <= A.EndDate)
+		WHERE A.AttributionRecordTypeId = 1 AND A.TargetId = @PersonId
+		)
+		UPDATE A
+		SET A.StartDate = CASE WHEN A.StartDate > UA.HireDate THEN A.StartDate ELSE UA.HireDate END,
+			A.EndDate = CASE WHEN UA.TerminationDate IS NULL OR A.EndDate < UA.TerminationDate  THEN A.EndDate ELSE UA.TerminationDate END
+	 	FROM UpdatableAttributions UA
+		INNER JOIN dbo.Attribution A ON A.AttributionId = UA.AttributionId 
+		WHERE UA.Rank = 1 AND 
+							( 
+								A.StartDate <> CASE WHEN A.StartDate > UA.HireDate THEN A.StartDate ELSE UA.HireDate END 
+								OR 
+								 A.EndDate <> CASE WHEN UA.TerminationDate IS NULL OR A.EndDate < UA.TerminationDate  THEN A.EndDate ELSE UA.TerminationDate END
+							 )
+
+		--Updating Attribution Records based on Division History.
+		;WITH UpdatableDivisionAttributions
+		AS
+		(
+		SELECT A.AttributionId,A.StartDate AStartDate,A.EndDate AEndDate,DH.StartDate DStartDate,DH.EndDate DEndDate,RANK() over (PARTITION	by A.AttributionId order by DH.StartDate) as Rank
+		FROM	dbo.Attribution A
+		INNER JOIN v_DivisionHistory DH ON DH.PersonId = A.TargetId AND (DH.EndDate IS NULL OR A.StartDate < DH.EndDate) AND (DH.StartDate <= A.EndDate) AND DH.DivisionId IN (@ConsultingDivId,@BusinessDevelopmentDivId)
+		WHERE A.AttributionRecordTypeId = 1 AND A.TargetId = @PersonId
+		)
+		UPDATE A
+		SET A.StartDate = CASE WHEN A.StartDate > UA.DStartDate THEN A.StartDate ELSE UA.DStartDate END,
+			A.EndDate = CASE WHEN UA.DEndDate IS NULL OR A.EndDate < DATEADD(dd,-1,UA.DEndDate)  THEN A.EndDate ELSE DATEADD(dd,-1,UA.DEndDate) END
+	 	FROM UpdatableDivisionAttributions UA
+		INNER JOIN dbo.Attribution A ON A.AttributionId = UA.AttributionId 
+		WHERE UA.Rank = 1 
+		AND 
+			( 
+				A.StartDate <> CASE WHEN A.StartDate > UA.DStartDate THEN A.StartDate ELSE UA.DStartDate END
+				OR 
+				A.EndDate <> CASE WHEN UA.DEndDate IS NULL OR A.EndDate < DATEADD(dd,-1,UA.DEndDate)  THEN A.EndDate ELSE DATEADD(dd,-1,UA.DEndDate) END
+			)
+END
