7e807f6aefa6d92c688851392c9fef3187bff706
SainathC <SainathC>
2013-07-30 09:35:00 -0500

    Comments - Defect Number: 3160; Defect RecordID: 3156; Server Address: localhost; Server Port: 99; Database Name: Practice Management; Defect Summary: Project Details > Commission Tab New Functionality; Defect Description: A recent change for some Logic20/20 employees (Client Directors and Senior Managers) has been the implementation of targets for sales and delivery of client projects. Bonuses are now based on the amount they contribute to the sales and delivery of our projects. The Finance team has been tracking this manually right now, but we recognize that adding some of this tracking functionality to PM would make help make the process more efficient.
    Attached is a mock-up of the enhancements I am requesting to the current "Commissions" tab in Project Details. All current functionality on that tab should be deprecated. Access to the tab should be limited to individuals with the title of Senior Manager and the Senior Leadership and Admin roles.
    
    The UI on the Project Details will feed an Excel-based extract (see mock-up also attached) that should be added as a new tab entitled "Sales Delivery Attributions" to the Attainment report. Please also create a stored procedure so that this report could be run separately if a person wished. Please set up the stored procedure the same as the others that you've done for us recently, allowing us to specify the time range.
    
    Unrelated to the Commission tab, but related to general clean up and organization of the information on the Project Tab, please move the "Project Time Types" tab and related functionality, and move it up into the main row of tabs. It currently sits as an "orphan" at the bottom of the Project Details page, and I've received user feedback that it's placement on the page can cause confusion as to its purpose.
    
    Could you please review the attached and let me know of any questions? What will be the approximate level of effort for this request?

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/MilestoneInsert.proc.sql
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/MilestoneInsert.proc.sql
@@ -22,6 +22,11 @@ BEGIN
 	DECLARE @MilestoneCount INT
 	SELECT @MilestoneCount = COUNT(*) FROM dbo.Milestone WHERE ProjectId = @ProjectId
 
+	DECLARE	@W2SalaryTimescaleId INT,
+			@W2HourlyTimescaleId INT 
+	SELECT	@W2SalaryTimescaleId = TimescaleId FROM dbo.Timescale WHERE Name = 'W2-Salary'
+	SELECT  @W2HourlyTimescaleId = TimescaleId FROM dbo.Timescale WHERE Name = 'W2-Hourly'
+
 	INSERT INTO dbo.Milestone
 	            (ProjectId, Description, Amount, StartDate,
 	             ProjectedDeliveryDate, IsHourlyAmount, ConsultantsCanAdjust, IsChargeable, IsDefault)
@@ -34,21 +39,30 @@ BEGIN
 	BEGIN
 	
 		INSERT INTO dbo.Attribution(ProjectId,AttributionRecordTypeId,AttributionTypeId,TargetId,StartDate,EndDate,Percentage)
-		SELECT P.ProjectId,AR.AttributionRecordId,2 AS AttributionRecordTypeId,P.DirectorId,P.StartDate,P.EndDate,100 AS Percentage
+		SELECT P.ProjectId,AR.AttributionRecordId,2 AS AttributionRecordTypeId,P.DirectorId,
+					 CASE WHEN P.StartDate > pay.StartDate THEN P.StartDate ELSE pay.StartDate END,
+					 CASE WHEN P.EndDate < pay.EndDateOrig  THEN P.EndDate ELSE pay.EndDateOrig-1 END,100 AS Percentage
 		FROM dbo.Project P 
 		INNER JOIN dbo.AttributionRecordTypes AR ON AR.IsRangeType = 1 AND P.ProjectId = @ProjectId
-		WHERE P.DirectorId IS NOT NULL 
+		INNER JOIN v_Pay pay ON pay.PersonId = P.DirectorId 
+		WHERE P.DirectorId IS NOT NULL AND  (P.StartDate < pay.EndDateOrig) AND (pay.StartDate <= P.EndDate) AND pay.Timescale IN (@W2SalaryTimescaleId,@W2HourlyTimescaleId)
 		UNION 
-		SELECT P.ProjectId,AR.AttributionRecordId,2,P.SeniorManagerId,P.StartDate,P.EndDate,100
+		SELECT  P.ProjectId,AR.AttributionRecordId,2,P.SeniorManagerId,
+					 CASE WHEN P.StartDate > pay.StartDate THEN P.StartDate ELSE pay.StartDate END,
+					 CASE WHEN P.EndDate < pay.EndDateOrig  THEN P.EndDate ELSE pay.EndDateOrig-1 END,100 AS Percentage
 		FROM dbo.Project P 
 		INNER JOIN dbo.AttributionRecordTypes AR ON AR.IsRangeType = 1 AND P.ProjectId = @ProjectId
-		WHERE P.SeniorManagerId IS NOT NULL
-		UNION 
-		SELECT P.ProjectId,AR.AttributionRecordId,1,P.SalesPersonId,P.StartDate,P.EndDate,100
+		INNER JOIN v_Pay pay ON pay.PersonId =  P.SeniorManagerId AND  (P.StartDate <= pay.EndDateOrig) AND (pay.StartDate <= P.EndDate)
+		WHERE P.SeniorManagerId IS NOT NULL AND  (P.StartDate < pay.EndDateOrig) AND (pay.StartDate <= P.EndDate) AND pay.Timescale IN (@W2SalaryTimescaleId,@W2HourlyTimescaleId)
+		UNION
+		SELECT  P.ProjectId,AR.AttributionRecordId,1,P.SalesPersonId,
+					 CASE WHEN P.StartDate > pay.StartDate THEN P.StartDate ELSE pay.StartDate END,
+					 CASE WHEN P.EndDate < pay.EndDateOrig  THEN P.EndDate ELSE pay.EndDateOrig-1 END,100 AS Percentage
 		FROM dbo.Project P
 		INNER JOIN dbo.AttributionRecordTypes AR ON AR.IsRangeType = 1 AND P.ProjectId = @ProjectId
-		WHERE P.SalesPersonId IS NOT NULL
-		UNION 
+		INNER JOIN v_Pay pay ON pay.PersonId = P.SalesPersonId AND  (P.StartDate <= pay.EndDateOrig) AND (pay.StartDate <= P.EndDate)
+		WHERE P.SalesPersonId IS NOT NULL AND  (P.StartDate < pay.EndDateOrig) AND (pay.StartDate <= P.EndDate) AND pay.Timescale IN (@W2SalaryTimescaleId,@W2HourlyTimescaleId)
+		UNION
 		SELECT P.ProjectId,AR.AttributionRecordId,AT.AttributionTypeId,P.PracticeId,NULL,NULL,100
 		FROM dbo.Project P
 		INNER JOIN dbo.AttributionRecordTypes AR ON AR.IsPercentageType = 1 AND P.ProjectId = @ProjectId
