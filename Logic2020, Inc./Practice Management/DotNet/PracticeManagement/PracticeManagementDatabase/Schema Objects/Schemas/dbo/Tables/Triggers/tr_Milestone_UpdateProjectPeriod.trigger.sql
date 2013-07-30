5452daa1a43db8e3286a1a2deb77c07d12394f82
SainathC <SainathC>
2013-07-30 09:35:00 -0500

    Comments - Defect Number: 3160; Defect RecordID: 3156; Server Address: localhost; Server Port: 99; Database Name: Practice Management; Defect Summary: Project Details > Commission Tab New Functionality; Defect Description: A recent change for some Logic20/20 employees (Client Directors and Senior Managers) has been the implementation of targets for sales and delivery of client projects. Bonuses are now based on the amount they contribute to the sales and delivery of our projects. The Finance team has been tracking this manually right now, but we recognize that adding some of this tracking functionality to PM would make help make the process more efficient.
    Attached is a mock-up of the enhancements I am requesting to the current "Commissions" tab in Project Details. All current functionality on that tab should be deprecated. Access to the tab should be limited to individuals with the title of Senior Manager and the Senior Leadership and Admin roles.
    
    The UI on the Project Details will feed an Excel-based extract (see mock-up also attached) that should be added as a new tab entitled "Sales Delivery Attributions" to the Attainment report. Please also create a stored procedure so that this report could be run separately if a person wished. Please set up the stored procedure the same as the others that you've done for us recently, allowing us to specify the time range.
    
    Unrelated to the Commission tab, but related to general clean up and organization of the information on the Project Tab, please move the "Project Time Types" tab and related functionality, and move it up into the main row of tabs. It currently sits as an "orphan" at the bottom of the Project Details page, and I've received user feedback that it's placement on the page can cause confusion as to its purpose.
    
    Could you please review the attached and let me know of any questions? What will be the approximate level of effort for this request?

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/Triggers/tr_Milestone_UpdateProjectPeriod.trigger.sql
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/Triggers/tr_Milestone_UpdateProjectPeriod.trigger.sql
@@ -1,5 +1,4 @@
-﻿
--- =============================================
+﻿-- =============================================
 -- Author:		Anatoliy Lokshin
 -- Create date: 8-12-2008
 -- Updated by:	Srinivas.M
@@ -18,20 +17,33 @@ AS
 	FROM SessionLogData
 	WHERE SessionID = @@SPID
 	
-	DECLARE @StartDate DATETIME,
-			@EndDate	DATETIME,
-			@ProjectId	INT
+	DECLARE @NewProjectStartDate DATETIME,
+			@NewProjectEndDate	DATETIME,
+			@ProjectId	INT,
+			@OldProjectStartDate	DATETIME,
+			@OldProjectEndDate		DATETIME,
+			@W2SalaryTimescaleId INT,
+			@W2HourlyTimescaleId INT,
+			@ConsultingDivId	INT,
+			@BusinessDevelopmentDivId INT
+
+	SELECT	@W2SalaryTimescaleId = TimescaleId FROM dbo.Timescale WHERE Name = 'W2-Salary'
+	SELECT  @W2HourlyTimescaleId = TimescaleId FROM dbo.Timescale WHERE Name = 'W2-Hourly'
+	SELECT  @ConsultingDivId = DivisionId FROM dbo.PersonDivision WHERE DivisionName = 'Consulting'
+	SELECT  @BusinessDevelopmentDivId = DivisionId FROM dbo.PersonDivision WHERE DivisionName = 'Business Development'
 
 	SELECT @ProjectId = P.ProjectId,
-			@StartDate = (SELECT MIN(StartDate) FROM dbo.Milestone AS m WHERE m.ProjectId = P.ProjectId),
-			@EndDate = (SELECT MAX(ProjectedDeliveryDate) FROM dbo.Milestone AS m WHERE m.ProjectId = P.ProjectId)
+			@NewProjectStartDate = (SELECT MIN(StartDate) FROM dbo.Milestone AS m WHERE m.ProjectId = P.ProjectId),
+			@NewProjectEndDate = (SELECT MAX(ProjectedDeliveryDate) FROM dbo.Milestone AS m WHERE m.ProjectId = P.ProjectId),
+			@OldProjectStartDate = P.StartDate,
+			@OldProjectEndDate = P.EndDate
 	FROM dbo.Project P
 	 WHERE EXISTS (SELECT 1 FROM inserted AS i WHERE i.ProjectId = P.ProjectId)
 	    OR EXISTS (SELECT 1 FROM deleted AS i WHERE i.ProjectId = P.ProjectId)
 
 	UPDATE dbo.Project
-	   SET StartDate = @StartDate,
-	       EndDate = @EndDate
+	   SET StartDate = @NewProjectStartDate,
+	       EndDate = @NewProjectEndDate
 	FROM Project P
 	WHERE P.ProjectId = @ProjectId
 	
@@ -41,26 +53,107 @@ AS
 	END
 
 	UPDATE PTRS
-		SET EndDate = @EndDate + (7 - DATEPART(dw,@EndDate))
+		SET EndDate = @NewProjectEndDate + (7 - DATEPART(dw,@NewProjectEndDate))
 	FROM [dbo].[PersonTimeEntryRecursiveSelection] PTRS
 	WHERE PTRS.ProjectId = @ProjectId AND PTRS.IsRecursive = 1
 
-	UPDATE A
-		SET A.StartDate = CASE WHEN A.StartDate < @StartDate THEN @StartDate ELSE A.StartDate END,--max startdate
-			A.EndDate = CASE WHEN A.EndDate > @EndDate THEN @EndDate ELSE A.EndDate END--min enddate
+	 --Updates commission attribution records as per the new project range for the overlapping records.
+	UPDATE A																				
+		SET A.StartDate = CASE WHEN A.StartDate < @NewProjectStartDate THEN @NewProjectStartDate ELSE A.StartDate END,--max startdate
+			A.EndDate = CASE WHEN A.EndDate > @NewProjectEndDate THEN @NewProjectEndDate ELSE A.EndDate END--min enddate
 	FROM [dbo].Attribution A
 	INNER JOIN AttributionRecordTypes ART ON A.AttributionRecordTypeId = ART.AttributionRecordId AND ART.IsRangeType = 1
-	WHERE A.ProjectId = @ProjectId AND A.StartDate <= @EndDate AND @StartDate <= A.EndDate AND 
+	WHERE A.ProjectId = @ProjectId AND A.StartDate <= @NewProjectEndDate AND @NewProjectStartDate <= A.EndDate AND 
 			(
-			 A.StartDate <> CASE WHEN A.StartDate < @StartDate THEN @StartDate ELSE A.StartDate END
-				OR A.EndDate <> CASE WHEN A.EndDate > @EndDate THEN @EndDate ELSE A.EndDate END
+			 A.StartDate <> CASE WHEN A.StartDate < @NewProjectStartDate THEN @NewProjectStartDate ELSE A.StartDate END
+				OR A.EndDate <> CASE WHEN A.EndDate > @NewProjectEndDate THEN @NewProjectEndDate ELSE A.EndDate END
 			)
+ 
+	DECLARE @Attributions TABLE(Id INT,AType INT)
+	--Extending dates If there is any overlapping of new and old project dates.
+	IF @NewProjectStartDate <= @OldProjectEndDate AND @OldProjectStartDate <= @NewProjectEndDate    
+	BEGIN
+			--Extend the attribution Start date if the project start date is extended as per the person pay history
+			UPDATE A 
+			SET A.StartDate = CASE WHEN @NewProjectStartDate > pay.StartDate THEN @NewProjectStartDate ELSE pay.StartDate END
+			FROM dbo.Attribution A
+			INNER JOIN dbo.[v_PayTimescaleHistory] pay ON pay.PersonId = A.TargetId 
+			WHERE A.ProjectId = @ProjectId 
+				AND A.AttributionRecordTypeId = 1 
+				AND pay.Timescale IN (@W2SalaryTimescaleId,@W2HourlyTimescaleId) 
+				AND A.StartDate < pay.EndDate 
+				AND pay.StartDate <= A.EndDate
+				AND @NewProjectStartDate < @OldProjectStartDate 
+				AND A.StartDate = @OldProjectStartDate
 
-	DELETE A
-	FROM [dbo].Attribution A
-	INNER JOIN AttributionRecordTypes ART ON A.AttributionRecordTypeId = ART.AttributionRecordId AND ART.IsRangeType = 1
-	WHERE A.ProjectId = @ProjectId AND (A.StartDate > @EndDate OR @StartDate > A.EndDate)
+			--Extend the attribution end date if the project end date is extended as per the person pay history
+			UPDATE A
+			SET A.EndDate = CASE WHEN @NewProjectEndDate < pay.EndDate  THEN @NewProjectEndDate ELSE DATEADD(dd,-1,pay.EndDate) END
+			FROM dbo.Attribution A
+			INNER JOIN dbo.[v_PayTimescaleHistory] pay ON pay.PersonId = A.TargetId 
+			WHERE A.ProjectId = @ProjectId 
+				AND A.AttributionRecordTypeId = 1 
+				AND pay.Timescale IN (@W2SalaryTimescaleId,@W2HourlyTimescaleId) 
+				AND (A.StartDate < pay.EndDate) 
+				AND (pay.StartDate <= A.EndDate) 
+				AND @NewProjectEndDate > @OldProjectEndDate 
+				AND A.EndDate = @OldProjectEndDate  
 
+			----Extend the attribution Start date if the project start date is extended as per the division history
+			--UPDATE A 
+			--SET A.StartDate = CASE WHEN @NewProjectStartDate > dh.StartDate THEN @NewProjectStartDate ELSE dh.StartDate END
+			--FROM dbo.Attribution A
+			--INNER JOIN dbo.v_DivisionHistory dh ON dh.PersonId = A.TargetId 
+			--WHERE A.ProjectId = @ProjectId 
+			--	AND A.AttributionRecordTypeId = 1 
+			--	AND dh.DivisionId IN (@ConsultingDivId,@BusinessDevelopmentDivId) 
+			--	AND A.StartDate < dh.EndDate 
+			--	AND dh.StartDate <= A.EndDate
+			--	AND @NewProjectStartDate < @OldProjectStartDate 
+			--	AND A.StartDate = @OldProjectStartDate
+
+			----Extend the attribution end date if the project end date is extended as per the division history
+			--UPDATE A
+			--SET A.EndDate = CASE WHEN @NewProjectEndDate < dh.EndDate  THEN @NewProjectEndDate ELSE DATEADD(dd,-1,dh.EndDate) END
+			--FROM dbo.Attribution A
+			--INNER JOIN dbo.v_DivisionHistory dh ON dh.PersonId = A.TargetId 
+			--WHERE A.ProjectId = @ProjectId 
+			--	AND A.AttributionRecordTypeId = 1 
+			--	AND dh.DivisionId IN (@ConsultingDivId,@BusinessDevelopmentDivId) 
+			--	AND (A.StartDate < dh.EndDate) 
+			--	AND (dh.StartDate <= A.EndDate) 
+			--	AND @NewProjectEndDate > @OldProjectEndDate 
+			--	AND A.EndDate = @OldProjectEndDate  
+
+			DELETE A
+			FROM [dbo].Attribution A
+			INNER JOIN AttributionRecordTypes ART ON A.AttributionRecordTypeId = ART.AttributionRecordId AND ART.IsRangeType = 1
+			WHERE A.ProjectId = @ProjectId AND (A.StartDate > @NewProjectEndDate OR @NewProjectStartDate > A.EndDate)
+
+	END
+	ELSE 
+	--If new project range is completly out of old project range. 
+	BEGIN   
+		  	INSERT INTO @Attributions
+			SELECT A.TargetId,A.AttributionTypeId
+			FROM dbo.Attribution A
+			WHERE A.ProjectId = @ProjectId AND A.AttributionRecordTypeId = 1
+			GROUP BY A.TargetId,A.AttributionTypeId
+
+			DELETE A
+			FROM dbo.Attribution A
+			WHERE A.ProjectId = @ProjectId AND A.AttributionRecordTypeId = 1
+
+			INSERT INTO Attribution(ProjectId,AttributionRecordTypeId,AttributionTypeId,TargetId,StartDate,EndDate,Percentage)
+			SELECT @ProjectId,AR.AttributionRecordId,A.AType,A.Id,
+					 CASE WHEN @NewProjectStartDate > pay.StartDate THEN @NewProjectStartDate ELSE pay.StartDate END,
+					 CASE WHEN @NewProjectEndDate < pay.EndDate  THEN @NewProjectEndDate ELSE DATEADD(dd,-1,pay.EndDate) END,100 AS Percentage
+			FROM @Attributions A
+			INNER JOIN dbo.AttributionRecordTypes AR ON AR.IsRangeType = 1
+			INNER JOIN v_Pay pay ON pay.PersonId = A.Id  
+			WHERE (@NewProjectStartDate < pay.EndDate) AND (pay.StartDate <= @NewProjectEndDate) AND pay.Timescale IN (@W2SalaryTimescaleId,@W2HourlyTimescaleId)
+
+	END
 	
 	IF  ( SELECT UserLogin FROM SessionLogData WHERE SessionID = @@SPID) IS NULL
 	BEGIN
