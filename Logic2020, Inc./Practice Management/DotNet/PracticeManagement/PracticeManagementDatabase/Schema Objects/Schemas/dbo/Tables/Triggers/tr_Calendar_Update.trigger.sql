bbd90f9418875c2adb175f5293ece5283b7a7164
SainathC <SainathC>
2013-08-07 07:04:00 -0500

    Comments - Defect Number: 3168; Defect RecordID: 3164; Server Address: localhost; Server Port: 99; Database Name: Practice Management; Defect Summary: Time Off Categories > Adding record of the user who entered the time off in PTO.; Defect Description: We have discovered that the team needs better visibility into which users are adding PTO to individual calendars. To provide that visibility, I'm requesting two enhancements:
    Like with Time-Off designations "Unpaid" and "Other Reportable Time", please add to the hover description on the person's calendar as well in the notes on Time Entry the language "Approved By <First Name> <Last Name>." The name displayed should be of the person who entered the time off on the person's calendar. If the user entered time off into his/her own timesheet via Time Entry, then the comment should change to read "Entered by <First Name> <Last Name>." Please make these updates for Time Off types "PTO," "Jury Duty," and "Bereavement."
    In the Audit Log (Configuration > Troubleshooting > Audit Log), please add a new category in the Pick List entitled "ALL Calendar Events". Selecting this item in the drop down will return record of all changes made to the user's calendar (i.e., who entered time off and when, who edited time and when, etc.). This should be a record of the actual user who made the change (i.e., if an Admin made a change, the system should track the actual user, and not use "Pracadmin").

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/Triggers/tr_Calendar_Update.trigger.sql
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/Triggers/tr_Calendar_Update.trigger.sql
@@ -12,6 +12,11 @@ AS
 BEGIN
 SET NOCOUNT ON;
 
+-- Ensure the temporary table exists
+	EXEC SessionLogPrepare @UserLogin = NULL
+	DECLARE @CurrentPMTime DATETIME 
+	SET @CurrentPMTime = dbo.InsertingTime()
+	
 	UPDATE PCA
 	SET PCA.DayOff = ISNULL(pcal.DayOff, i.DayOff),
 		PCA.CompanyDayOff = i.DayOff,
@@ -22,5 +27,96 @@ SET NOCOUNT ON;
 	LEFT JOIN dbo.PersonCalendar AS pcal ON pcal.Date = i.Date AND pcal.PersonId = p.PersonId AND PCA.PersonId = pcal.PersonId
 	WHERE PCA.DayOff <> ISNULL(pcal.DayOff, i.DayOff) OR PCA.CompanyDayOff <> i.DayOff
 
+	;WITH NEW_VALUES AS
+	(
+	SELECT  Date,
+			DayOff,
+			IsRecurring,
+			HolidayDescription
+	FROM 
+	(
+	SELECT	ROW_NUMBER() OVER(ORDER BY i.DATE) AS RowNumber,
+			CONVERT(NVARCHAR(10), i.Date, 101) AS Date,
+			CASE WHEN i.DayOff = 1 THEN 'YES' ELSE 'NO' END AS DayOff,
+			CASE WHEN i.IsRecurring = 1 THEN 'YES' ELSE 'NO' END AS IsRecurring,
+			i.HolidayDescription
+	FROM inserted AS i
+	INNER JOIN deleted  AS d on d.Date = i.Date AND i.DayOff <> d.DayOff AND i.DayOff = 1
+	) as A
+	where a.RowNumber = 1
+	),
+
+	OLD_VALUES AS
+	(
+	SELECT  Date,
+			DayOff,
+			IsRecurring,
+			HolidayDescription
+	FROM 
+	(
+	SELECT	ROW_NUMBER() OVER(ORDER BY D.DATE) AS RowNumber,
+			CONVERT(NVARCHAR(10), d.Date, 101) AS Date,
+			CASE WHEN d.DayOff = 1 THEN 'YES' ELSE 'NO' END AS DayOff,
+			CASE WHEN d.IsRecurring = 1 THEN 'YES' ELSE 'NO' END AS IsRecurring,
+			d.HolidayDescription
+	FROM deleted AS d
+	INNER JOIN inserted AS i on d.Date = i.Date AND i.DayOff <> d.DayOff AND d.DayOff = 1
+	) as A
+	where a.RowNumber = 1
+	)
+
+	-- Log an activity
+	INSERT INTO dbo.UserActivityLog
+	            (ActivityTypeID,
+	             SessionID,
+	             SystemUser,
+	             Workstation,
+	             ApplicationName,
+	             UserLogin,
+	             PersonID,
+	             LastName,
+	             FirstName,
+				 Data,
+	             LogData,
+	             LogDate)
+	SELECT  CASE
+	           WHEN d.Date IS NULL THEN 3
+			   WHEN i.Date IS NULL THEN 5
+	           ELSE 4
+	       END as ActivityTypeID,
+	       l.SessionID,
+	       l.SystemUser,
+	       l.Workstation,
+	       l.ApplicationName,
+	       l.UserLogin,
+	       l.PersonID,
+	       l.LastName,
+	       l.FirstName,
+		   Data = CONVERT(NVARCHAR(MAX),(SELECT *
+					    FROM NEW_VALUES
+					         FULL JOIN OLD_VALUES ON NEW_VALUES.Date = OLD_VALUES.Date 
+			           WHERE NEW_VALUES.Date = ISNULL(i.Date, d.Date) OR OLD_VALUES.Date = ISNULL(i.Date, d.Date)
+					  FOR XML AUTO, ROOT('CompanyHoliday'))),
+		   LogData = (SELECT 
+						 NEW_VALUES.Date 
+						,NEW_VALUES.DayOff
+						,NEW_VALUES.IsRecurring
+						,NEW_VALUES.HolidayDescription
+
+						,OLD_VALUES.Date 
+						,OLD_VALUES.DayOff
+						,OLD_VALUES.IsRecurring
+						,OLD_VALUES.HolidayDescription
+
+						FROM NEW_VALUES
+					         FULL JOIN OLD_VALUES ON NEW_VALUES.Date = OLD_VALUES.Date
+			            WHERE NEW_VALUES.Date = ISNULL(i.Date , d.Date ) OR OLD_VALUES.Date = ISNULL(i.Date , d.Date)
+					FOR XML AUTO, ROOT('CompanyHoliday'), TYPE),
+					@CurrentPMTime
+	  FROM NEW_VALUES AS i
+	       FULL JOIN OLD_VALUES AS d ON i.Date = d.Date
+	       INNER JOIN dbo.SessionLogData AS l ON l.SessionID = @@SPID
+	-- End logging session
+	 EXEC dbo.SessionLogUnprepare
 END
 
