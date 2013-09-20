3abd034ea107096923de4c93331cf900daf6888e
SainathC <SainathC>
2013-09-20 07:22:00 -0500

    Comments - Defect Number: 3179; Defect RecordID: 3175; Server Address: localhost; Server Port: 99; Database Name: Practice Management; Defect Summary: Project Summary Report > Projects not appearing in view ; Defect Description: Employee Patrick O'Briant reports an issue in viewing projects in the Projects > Summary view. He reports that when he runs Project Summary (using filters of "Microsoft" account and "All" for the rest of the filters) projects that have Stephanie Stocker as a salesperson do not appear for him. His hypothesis is that because Stephanie is a terminated employee, Practice Management is filtering out projects assigned to her as a sales person. When I run the report, I can see all projects, including ones where Stephanie Stocker is a salesperson, as I expect, but when I become Patrick as a user, I was able to confirm his issue.
    Could you please investigate any potential cause of why Patrick, based on his title/roles/seniority allocated to him, may be seeing a different view than me in regard to projects assigned to Stephanie Stocker?
    
    
    [Harshitha]
    In the project summary page, if an Administrator user selects 'All' in sales persons list then we show the projects of terminated sales persons also, But if the users with role other than administrator selects All in sales persons list then we show the projects of only active sales persons.
    
    This is the reason why Lisa is able to see the projects of Stephanie Stocker and Patrick O'Briant is not able to.
    
    This is behaving in the same way from the start.
    
    We have an option to resolve this:
    1. Showing terminated sales persons in the drop down of project summary page such that the logged in user selects the sales person whose projects he wish to view.
    
    LN: Thank you very much for the analysis. Please proceed with your recommendation.
    
    ---
    
    A completely unrelated request: when I submit a new ticket, TTP automatically populates the assignment to Madhan. Could you please instruct me how to change it to your name?
    
    [Harshitha]
    We have updated the configuration of Test track such that whenever a ticket is created then it will be assigned to Harshitha Tenneti instead of Madhan.
    Attached a document of steps for this task.
    
    LN: Thank you very much!

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/PersonListSalesperson.proc.sql
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/PersonListSalesperson.proc.sql
@@ -1,7 +1,24 @@
-﻿CREATE PROCEDURE dbo.PersonListSalesperson
+﻿/*
+We are using the sproc in the following pages
+Persondetail - params(includeterminated=1)  - returns all 1.persons with role 'salesperson'  2. persons who are assigned as salespersons. 
+-----
+Groupby     -  params(personid,includeterminated=1) personid is null if logged in admin otherwise loggedin person id - returns all  1.persons with role 'salesperson'  2. persons who are assigned as salespersons.  if personid is null otherwise it gets the persons on those he has permissions or the 
+				salespersons of the projects where the personid is assigned as project owner or project manager or salesperson 
+-----
+Projects - same as above
+-----
+Clientdetails - params(includeterminated=0) - returns all active salespersons 
+----- 
+Opportunitydetail - same as above
+----
+Projectdetail  - params(personid,includeterminated=0) personid is null if logged in "admin" or "Client Director" otherwise loggedin person id - returns all active salespersons if personid is null otherwise it gets the persons on those 
+he has permissions or the salespersons of the projects where the personid is assigned as project owner or project manager or salesperson 
+*/
+CREATE PROCEDURE dbo.PersonListSalesperson
     (
-      @IncludeInactive BIT ,
-      @PersonId INT = NULL
+      @IncludeTerminated BIT ,
+      @PersonId INT = NULL ,
+	  @ShowAssignedSalesPersons BIT = 0
     )
 AS 
 BEGIN
@@ -9,6 +26,14 @@ BEGIN
 	/*
 		Showing Sales Person of the Projects, Where the Logged in User is Project Manager OR Sales Person of 
 		the Project(applicable only for Project Lead role Person) as part of #2941.
+
+	If @PersonId != null	
+	    1.Checks whether person have 'only' project lead role.
+			If Yes:Lists the active salespersons of the projects where the @PersonId is assigned as ProjectOwner or ProjectManager or ProjectSalesPerson(It includes terminated persons if @IncludeTerminated = 1)
+			Else : Lists the active persons who have role as "Salesperson"  (It includes terminated persons if @IncludeTerminated = 1) on whom(SalesPerson) he has permissions(which appears in Permissions tab of Persondetail page.) or
+			      salespersons of the projects where the @PersonId is assigned as ProjectOwner or ProjectManager or ProjectSalesPerson and the persons who are assigned as salespersons to a project if @ShowAssignedSalesPersons = 1
+	Otherwise
+		1.Lists all the active persons who have role as "Salesperson"  (It includes terminated persons if @IncludeTerminated = 1) and the persons who are assigned as salespersons to a project if @ShowAssignedSalesPersons = 1 
 	*/
 
 	DECLARE @UserHasHighRoleThanProjectLead INT = NULL
@@ -59,7 +84,7 @@ BEGIN
 		JOIN v_Person SP ON SP.PersonId = P.SalesPersonId
 		WHERE (PM.ProjectManagerId = @PersonId OR P.SalesPersonId = @PersonId OR P.projectOwnerId = @PersonId) --Logged in User should be Project Manager or Sales Person of the Project.
 			AND (SP.PersonStatusId IN (1,5) /* Active person only */
-					OR @IncludeInactive = 1)
+					OR @IncludeTerminated = 1)
 			AND sp.IsStrawman = 0
 
 	END
@@ -133,8 +158,12 @@ BEGIN
 									INNER JOIN dbo.aspnet_Users AS u ON p.Alias = u.UserName
 									INNER JOIN dbo.aspnet_UsersInRoles AS ur ON u.UserId = ur.UserId
 									INNER JOIN dbo.aspnet_Roles AS r ON ur.RoleId = r.RoleId
-						   WHERE    r.RoleName = 'Salesperson' AND 
-									(p.PersonStatusId IN (1,3,5))
+									LEFT JOIN dbo.Project AS pro ON pro.SalesPersonId = p.PersonId 
+						   WHERE    (	r.RoleName = 'Salesperson'
+										OR
+										(@ShowAssignedSalesPersons = 1 AND pro.SalesPersonId IS NOT NULL)
+									) 
+									AND (@IncludeTerminated = 1 OR p.PersonStatusId IN (1,3,5))
 									AND ( @PersonId IS NULL
 										  OR p.PersonId IN (
 										  SELECT sp.PersonId
