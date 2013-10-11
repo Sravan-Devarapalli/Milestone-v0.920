aba3dbd7e95225f82f703c97962ad31d0ee39bed
SainathC <SainathC>
2013-10-11 05:18:00 -0500

    Comments - Defect Number: 3135; Defect RecordID: 3131; Server Address: localhost; Server Port: 99; Database Name: Practice Management; Defect Summary: Consulting Utilization Report > Graphically represent vacation of less than five days in duration.; Defect Description: Employee Nadia Golyaeva has PTO scheduled and entered on the calendar for August 27 - September 3. I expected to see on the Consulting Utilization report the week of August 25th to be colored as "gray" to reflect the time off. Employees David Sisson, Chad Beggs, and Anders Westby are a few examples of vacation time that are appearing correctly on the Utilization Report.
    Could you please investigate and confirm that vacation time is being reflected correctly in the Utilization report?

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Functions/GetWeeklyUtilizationTable.function.sql
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Functions/GetWeeklyUtilizationTable.function.sql
@@ -22,7 +22,7 @@ RETURN
 	2.CurrentConsultantsWithRanges : Gets the consultants for the given filters ( i.e. @TimescaleIds,@PracticeIds,@ActivePersons,@ProjectedPersons,@ExcludeInternalPractices) mapped with Ranges.
 	3.CurrentConsultantsWithVacationDays : Populate  VacationDays of the Consultants for the CurrentConsultantsWithRanges records.
 	4.CurrentConsultantsWithProjectedHours : Populate  ProjectedHours of the Consultants for the CurrentConsultantsWithVacationDays records.
-	5.CurrentConsultantsWithAvaliableHours : Populate  AvaliableHours of the Consultants for the CurrentConsultantsWithVacationDays records.
+	5.CurrentConsultantsWithAvailableHours : Populate  AvailableHours of the Consultants for the CurrentConsultantsWithVacationDays records.
 	6.CurrentConsultantsWithAllHours : Populate  all hours of the Consultants for the CurrentConsultantsWithVacationDays records.
 	7.Final result : calculate the WeeklyUtlization with the obtained hours.
 
@@ -30,7 +30,7 @@ RETURN
 
 	Calculation of ProjectedHours:
 
-	Calculation of AvaliableHours:
+	Calculation of AvailableHours:
 
 
 */
@@ -61,11 +61,13 @@ AS
 	SELECT  P.PersonId,
 			r.StartDate,
 			CASE WHEN r.EndDate > @EndDate THEN @EndDate ELSE r.EndDate END EndDate,
-			PCPT.Timescale
+			pay.Timescale,
+			RANK() OVER (PARTITION BY P.PersonId,r.StartDate,r.EndDate  ORDER BY pay.startdate DESC) LatestPayRank
     FROM     Ranges r
 	CROSS JOIN dbo.Person AS p
 	INNER JOIN dbo.Timescale T ON T.TimescaleId IN ( SELECT ResultId FROM [dbo].[ConvertStringListIntoTable](@TimescaleIds))
-	INNER JOIN dbo.GetCurrentPayTypeTable() AS PCPT ON PCPT.PersonId = p.PersonId AND T.TimescaleId = PCPT.Timescale
+	INNER JOIN dbo.[GetLatestPayWithInTheGivenRange](@StartDate,@EndDate) AS PCPT ON PCPT.PersonId = p.PersonId AND T.TimescaleId = PCPT.Timescale  
+	LEFT JOIN dbo.Pay AS pay ON pay.Person = p.PersonId AND r.EndDate >= pay.StartDate AND r.StartDate <= (pay.EndDate-1)
     LEFT JOIN dbo.Practice AS pr ON p.DefaultPractice = pr.PracticeId
     WHERE   (p.IsStrawman = 0)
             AND ( (@ActivePersons = 1 AND p.PersonStatusId IN (1,5)) -- active and termination pending statues
@@ -84,17 +86,21 @@ AS
 			CCWR.StartDate,
 			CCWR.EndDate,
 			CCWR.Timescale, 
-			ISNULL(COUNT(PC.Date),0) AS VacationDays
+			ISNULL(COUNT(PC.Date),0) AS VacationDays,
+			ISNULL(SUM(CASE WHEN PC.CompanyDayOff = 0 THEN 1 ELSE 0 END),0) AS VacationDaysExcludingCompanyDayOff
 	FROM CurrentConsultantsWithRanges CCWR
-	LEFT JOIN dbo.v_PersonCalendar PC ON PC.PersonId = CCWR.PersonId 
+	LEFT JOIN dbo.PersonCalendarAuto PC ON PC.PersonId = CCWR.PersonId 
 										AND	PC.Date BETWEEN CCWR.StartDate AND CCWR.EndDate 
 										AND (
-												PC.CompanyDayOff = 1
+												(
+													PC.CompanyDayOff = 1 AND PC.DayOff = 1
+												)
 												OR
 												(
-													PC.DayOff = 1 AND PC.ActualHours = 8--if a person has added Timeoff  for complete 8 hr then the day is treated as vacation day.
+													PC.DayOff = 1 AND PC.TimeOffHours = 8--if a person has added Timeoff  for complete 8 hr then the day is treated as vacation day.
 												) 
 											)
+	WHERE CCWR.LatestPayRank = 1
 	GROUP BY CCWR.PersonId,CCWR.StartDate,CCWR.EndDate,CCWR.Timescale
 ),
 CurrentConsultantsWithProjectedHours
@@ -111,42 +117,44 @@ AS
 			 @InternalProjects = 1 AND s.ProjectStatusId = 6) --6 - Internal
 	GROUP BY s.PersonId ,CCWV.StartDate ,CCWV.Timescale
 ),
-CurrentConsultantsWithAvaliableHours
+CurrentConsultantsWithAvailableHours
 AS
 (
---Salary Persons AvaliableHours
-	SELECT PC.PersonId ,CAST(SUM(8 - ISNULL(PC.ActualHours,0)) AS DECIMAL(10,2)) AS AvaliableHours ,CCWV.StartDate 
+--Salary Persons AvailableHours
+	SELECT PC.PersonId ,CAST(SUM(8 - ISNULL(PC.TimeOffHours,0)) AS DECIMAL(10,2)) AS AvailableHours ,CCWV.StartDate 
 	FROM CurrentConsultantsWithVacationDays CCWV
-	INNER JOIN dbo.v_PersonCalendar PC ON CCWV.PersonId = PC.PersonId 
+	INNER JOIN dbo.PersonCalendarAuto PC ON CCWV.PersonId = PC.PersonId 
 										AND CCWV.Timescale = 2 
 										AND PC.Date BETWEEN CCWV.StartDate AND CCWV.EndDate
 										AND (PC.DayOff = 0 OR (PC.DayOff = 1 AND PC.CompanyDayOff = 0))
 	GROUP BY PC.PersonId,CCWV.StartDate 
 	UNION 
-	SELECT CCWP.PersonId,CCWP.ProjectedHours AS AvaliableHours ,CCWP.StartDate FROM  CurrentConsultantsWithProjectedHours CCWP WHERE CCWP.Timescale != 2 -- Non SalaryPerson AvaliableHours
+	SELECT CCWP.PersonId,CCWP.ProjectedHours AS AvailableHours ,CCWP.StartDate FROM  CurrentConsultantsWithProjectedHours CCWP WHERE CCWP.Timescale != 2 -- Non SalaryPerson AvailableHours
 ),
 CurrentConsultantsWithAllHours
 AS
 (
-	SELECT CCWV.PersonId,CCWV.StartDate,CCWV.EndDate,CCWV.VacationDays,ISNULL(CCWA.AvaliableHours,0) AS AvaliableHours,ISNULL(CCWP.ProjectedHours,0) AS ProjectedHours
+	SELECT CCWV.PersonId,CCWV.StartDate,CCWV.EndDate,CCWV.VacationDays,CCWV.VacationDaysExcludingCompanyDayOff,ISNULL(CCWA.AvailableHours,0) AS AvailableHours,ISNULL(CCWP.ProjectedHours,0) AS ProjectedHours,CCWV.Timescale
 	FROM CurrentConsultantsWithVacationDays CCWV
-	LEFT JOIN CurrentConsultantsWithAvaliableHours CCWA ON CCWV.PersonId = CCWA.PersonId AND CCWV.StartDate = CCWA.StartDate
+	LEFT JOIN CurrentConsultantsWithAvailableHours CCWA ON CCWV.PersonId = CCWA.PersonId AND CCWV.StartDate = CCWA.StartDate
 	LEFT JOIN CurrentConsultantsWithProjectedHours CCWP ON CCWV.PersonId = CCWP.PersonId AND CCWV.StartDate = CCWP.StartDate
 )
 SELECT	CC.PersonId,
 		CC.StartDate,
 		CC.EndDate,
-		CC.AvaliableHours,
+		CC.AvailableHours,
 		CC.ProjectedHours,
+		CC.Timescale,
+		CC.VacationDaysExcludingCompanyDayOff AS VacationDays,
 		CONVERT(INT,CASE	WHEN CC.VacationDays >= @Step
 				THEN -1
 				WHEN (
 					(@Step = 1 AND DATEPART(dw, CC.StartDate)IN(7,1)) 
 						OR
-					(CC.AvaliableHours = 0)
+					(CC.AvailableHours = 0)
 					)
 				THEN 0
-				ELSE CEILING(100*CC.ProjectedHours/CC.AvaliableHours) 
+				ELSE CEILING(100*CC.ProjectedHours/CC.AvailableHours) 
 				END) AS WeeklyUtlization
 FROM CurrentConsultantsWithAllHours CC
 
