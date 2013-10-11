b46c0a912ceec4b2d6c02fa3e8bdf8a516b35db3
SainathC <SainathC>
2013-10-11 05:18:00 -0500

    Comments - Defect Number: 3135; Defect RecordID: 3131; Server Address: localhost; Server Port: 99; Database Name: Practice Management; Defect Summary: Consulting Utilization Report > Graphically represent vacation of less than five days in duration.; Defect Description: Employee Nadia Golyaeva has PTO scheduled and entered on the calendar for August 27 - September 3. I expected to see on the Consulting Utilization report the week of August 25th to be colored as "gray" to reflect the time off. Employees David Sisson, Chad Beggs, and Anders Westby are a few examples of vacation time that are appearing correctly on the Utilization Report.
    Could you please investigate and confirm that vacation time is being reflected correctly in the Utilization report?

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/TerminationReasons.table.sql
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/TerminationReasons.table.sql
@@ -6,6 +6,8 @@
 	IsW2HourlyRule		BIT NOT NULL CONSTRAINT DF_TerminationReasons_IsW2HourlyRule DEFAULT(0),
 	Is1099Rule			BIT NOT NULL CONSTRAINT DF_TerminationReasons_Is1099Rule DEFAULT(0),
 	IsContingentRule	BIT NOT NULL CONSTRAINT DF_TerminationReasons_IsContingentRule DEFAULT(0),
+	IsPersonWorkedRule	BIT NOT NULL CONSTRAINT DF_TerminationReasons_IsPersonWorkedRule DEFAULT(1),
 	IsVisible			BIT NOT NULL CONSTRAINT DF_TerminationReasons_IsVisible DEFAULT(1),
 	CONSTRAINT PK_TerminationReasons_TerminationReasonId PRIMARY KEY (TerminationReasonId)
 )
+
