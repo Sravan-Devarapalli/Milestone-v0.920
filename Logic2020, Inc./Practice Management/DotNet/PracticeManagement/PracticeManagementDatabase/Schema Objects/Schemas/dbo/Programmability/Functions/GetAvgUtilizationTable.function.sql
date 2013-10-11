6c388a725ea0fc0d7def244b0508ebf04345ffbb
SainathC <SainathC>
2013-10-11 05:18:00 -0500

    Comments - Defect Number: 3135; Defect RecordID: 3131; Server Address: localhost; Server Port: 99; Database Name: Practice Management; Defect Summary: Consulting Utilization Report > Graphically represent vacation of less than five days in duration.; Defect Description: Employee Nadia Golyaeva has PTO scheduled and entered on the calendar for August 27 - September 3. I expected to see on the Consulting Utilization report the week of August 25th to be colored as "gray" to reflect the time off. Employees David Sisson, Chad Beggs, and Anders Westby are a few examples of vacation time that are appearing correctly on the Utilization Report.
    Could you please investigate and confirm that vacation time is being reflected correctly in the Utilization report?

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Functions/GetAvgUtilizationTable.function.sql
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Functions/GetAvgUtilizationTable.function.sql
@@ -22,9 +22,9 @@ RETURN
 	WITH SalaryPersonsAvaliableHours
 	AS
 	(
-		SELECT PC.PersonId ,CAST(SUM(8 - ISNULL(PC.ActualHours,0)) AS DECIMAL(10,2)) AS AvaliableHours 
-		FROM dbo.v_PersonCalendar PC
-		INNER JOIN dbo.GetCurrentPayTypeTable() CPT ON CPT.PersonId = PC.PersonId AND CPT.Timescale = 2
+		SELECT PC.PersonId ,CAST(SUM(8 - ISNULL(PC.TimeOffHours,0)) AS DECIMAL(10,2)) AS AvaliableHours 
+		FROM dbo.PersonCalendarAuto PC
+		INNER JOIN dbo.[GetLatestPayWithInTheGivenRange](@StartDate,@EndDate) CPT ON CPT.PersonId = PC.PersonId AND CPT.Timescale = 2
 		WHERE PC.Date BETWEEN @StartDate AND @EndDate 
 				AND (PC.DayOff = 0 OR (PC.DayOff = 1 AND PC.CompanyDayOff = 0))
 		GROUP BY PC.PersonId
@@ -34,7 +34,7 @@ RETURN
 	(
 	SELECT  p.PersonId,CAST (ISNULL(SUM(MPS.HoursPerDay),0) AS DECIMAL(10,2)) AS ProjectedHours 
     FROM    dbo.Person P 
-    INNER JOIN dbo.GetCurrentPayTypeTable() CPT ON CPT.PersonId = P.PersonId AND CPT.Timescale = 2
+    INNER JOIN dbo.[GetLatestPayWithInTheGivenRange](@StartDate,@EndDate) CPT ON CPT.PersonId = P.PersonId AND CPT.Timescale = 2
 	LEFT JOIN dbo.v_MilestonePersonSchedule AS MPS ON P.PersonId = MPS.PersonId 
 												AND MPS.MilestoneId <> (SELECT MilestoneId FROM dbo.DefaultMilestoneSetting)
 												AND MPS.Date BETWEEN @StartDate AND @EndDate
