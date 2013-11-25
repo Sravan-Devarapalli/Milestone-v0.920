7cbe046c23abe2dd589f6234ff974f73a79bc049
SainathC <SainathC>
2013-11-25 04:42:00 -0500

    Comments - Defect Number: 3201; Defect RecordID: 3197; Server Address: localhost; Server Port: 99; Database Name: Practice Management; Defect Summary: Reports > Account: Enhancements to Functionality; addition of Projected Hours, Billable Hours Variance, and Estimated Billings; Defect Description: Recent changes made to the Time > By Project and By Person reports to add Billable Hours Variance and Estimated Billings has been received very favorably by the organization. They have requested that we replicate some of the changes made on the Time Reports to the Account report (accessed by Reports > Account).
    On the Business Unit" Tab:
     Add an Actual Hours column: calculated the same as on the Time > By Project Report, although the Actual Hour number will be sum of Actual Hours for each of the projects for that time period.
    [Harshitha Tenneti]:
    1.We are assuming that the actual hours column to be next to Non-billable column.
    
    LN: Yes, to the right ofthe "Non-Billable" column, as in the By Project Time Report.
    
    2. In Business unit tab, we have "Total hours" column which is a summation of Billable,Non-Billable and BD hours.
    Do you want us to rename this column as "Actual hours" or add another column with summation of Billable and Non-billable hours(As in Time>By project report)?
    
    LN: Excellent point. Let's add a new column to the left of the "Total" column called "Project Hours" and sum only Billable and Non-Billable (i.e., do not sum BD hours). The current "Total" Hours rename to "Total BU Hours"
    
     Projected Hours: total projected hours for all resources on all of the projects for that business unit
    [Harshitha Tenneti]: We are assuming that the Projected hours column will be prior to Billable column.
    
    LN: Yes, please. Thank you for clarifying.
    
     Remove the bar graphic for Percent of Total Hours and display only the percentage number
     The calculation for Total remains the same
    Summary Area above the table: Remove Total Project Hours from the summary area above the table, but add summation of Total Projected Hours and Total Actual Hours next to BD Hours
       Total Projected Hours = the sum of all of the Projected Hours for that account based on the report parameters selected. It also updates as filters are changed.
       Total Actual Hours = the sum of all of the Actual Hours for that account based on the report parameters selected. It also updates as filters are changed.
       Total BD Hours: Calculation remains the same; no change to functionality
    Summary Area above the table: Under Account Name, change "Persons" to "People" and change "BUs" to "Business Unit(s)" (i.e., Microsoft: 10BUs, 32 Projects, 64 Persons: Change "Persons" to "People" and "BUs" to "Business Units")
    
    On the Project Tab:
     Each project name should be hyperlinked  the hyperlink takes the user back to the projects Project Details page. The Project Detail page opens in a new tab.
     Rename Billing Column to Billing Type
     Add an Actual Hours column: calculated the same as on the Time > By Project Report, although the Actual Hour number will be sum of Actual Hours for the project. The number should be hyperlinked (opens in a new tab) to take the user to the Time > By Project Report for that project for the specified time range.
     Total Estimated Billings: As on the Time > By Project Report, this number will reflect the total Estimated Billings if a Time and Materials project, or denotes FF if a fixed fee engagement.
     Add a Billable Hours Variance column, calculated in the same method as the Billable Hours Variance column on the Time Report, although like the other columns being added here, it should be sum of the Billable Hours variance for all resources. It should have the same ! functionality that communicates project overrun/underrun.
     Remove the current Project Variance (in Hours) column, and supporting current graphic
     The order of the columns, from left to right,

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/AccountSummaryByBusinessUnit.proc.sql
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/AccountSummaryByBusinessUnit.proc.sql
@@ -23,85 +23,128 @@ BEGIN
 	FROM dbo.ConvertStringListIntoTable(@BusinessUnitIds) BU
 	
 	
-		SELECT    PG.GroupId, 
-					PG.Name AS GroupName, 
-					PG.Active,
-					PG.Code AS GroupCode,
-					ROUND(SUM(CASE WHEN TEH.IsChargeable = 1
-										AND Pro.ProjectNumber != 'P031000'
-									THEN TEH.ActualHours
-									ELSE 0
-								END), 2) AS BillableHours ,
-					ROUND(SUM(CASE WHEN TEH.IsChargeable = 0
-										AND CC.TimeEntrySectionId <> 2
-										AND Pro.ProjectNumber != 'P031000'
-									THEN TEH.ActualHours
-									ELSE 0
-								END), 2) AS NonBillableHours ,
-					ROUND(SUM(CASE WHEN CC.TimeEntrySectionId = 2
-									THEN TEH.ActualHours
-									ELSE 0
-								END), 2) AS BusinessDevelopmentHours,
-					COUNT(DISTINCT CC.ProjectId) AS ProjectsCount
-		FROM      dbo.TimeEntry TE
-				INNER JOIN dbo.TimeEntryHours TEH ON TEH.TimeEntryId = te.TimeEntryId
-												AND TE.ChargeCodeDate BETWEEN @StartDateLocal
-												AND
-												@EndDateLocal
-				INNER JOIN dbo.ChargeCode CC ON CC.Id = TE.ChargeCodeId
-				INNER JOIN dbo.Person P ON P.PersonId = TE.PersonId
-				INNER JOIN dbo.PersonStatusHistory PTSH ON PTSH.PersonId = P.PersonId
-												AND TE.ChargeCodeDate BETWEEN PTSH.StartDate
-																			AND ISNULL(PTSH.EndDate,@FutureDate)
-				INNER JOIN dbo.ProjectGroup PG ON PG.GroupId = CC.ProjectGroupId
-				LEFT JOIN dbo.Project Pro ON Pro.ProjectId = CC.ProjectId
-		WHERE CC.ClientId = @AccountId
-				AND	TE.ChargeCodeDate <= ISNULL(P.TerminationDate,
-											@FutureDate)
-				AND ( CC.timeTypeId != @HolidayTimeType
-						OR ( CC.timeTypeId = @HolidayTimeType
-							AND PTSH.PersonStatusId IN (1,5) -- ACTIVE And Terminated Pending
-							)
-					)
-				AND (@BusinessUnitIds IS NULL
-					OR CC.ProjectGroupId IN (SELECT *
-											FROM @BusinessUnitIdsTable
-											)
-					)
-		GROUP BY  PG.GroupId, PG.Name, PG.Active, PG.Code
-
-		
+	     ;WITH ProjectedHours
+		AS
+		(
+			SELECT Pro.ProjectId,Pro.GroupId,
+				  ROUND(SUM(CASE WHEN P.IsStrawman = 0 THEN dbo.PersonProjectedHoursPerDay(PC.DayOff,PC.CompanyDayOff,PC.TimeOffHours,MPE.HoursPerDay) ELSE 0 END),2)  AS ForecastedHours
+		    FROM  dbo.Project Pro
+			LEFT JOIN dbo.Milestone AS M ON M.ProjectId = Pro.ProjectId
+			LEFT JOIN dbo.MilestonePerson AS MP ON MP.MilestoneId = M.MilestoneId
+			LEFT JOIN dbo.MilestonePersonEntry AS MPE ON MP.MilestonePersonId = MPE.MilestonePersonId
+			LEFT JOIN dbo.person AS P ON P.PersonId = MP.PersonId 
+			LEFT JOIN dbo.PersonCalendarAuto PC ON PC.PersonId = MP.PersonId
+			WHERE (@BusinessUnitIds IS NULL OR (Pro.GroupId IN (SELECT Id FROM @BusinessUnitIdsTable))) AND Pro.ClientId = @AccountId
+				   AND Pro.ProjectStatusId IN (3,4) --Active and Completed status as per #3201 
+				   AND Pro.StartDate IS NOT NULL AND Pro.EndDate IS NOT NULL
+				   AND PC.Date BETWEEN @StartDateLocal AND @EndDateLocal
+				   AND PC.Date BETWEEN MPE.StartDate AND MPE.EndDate
+			GROUP BY  Pro.ProjectId,Pro.GroupId
+		),
+		 ProjectsDoNotHaveMilestonePersonEntries
+		AS
+		(
+			SELECT DISTINCT p.ProjectId,p.GroupId
+			FROM dbo.Project P
+			INNER JOIN dbo.Milestone M ON M.ProjectId = P.ProjectId  
+			LEFT JOIN dbo.MilestonePerson MP ON MP.MilestoneId = M.MilestoneId
+			LEFT JOIN dbo.MilestonePersonEntry MPE ON MPE.MilestonePersonId = MP.MilestonePersonId
+											AND MPE.StartDate <= @EndDateLocal AND @StartDateLocal <= MPE.EndDate
+			WHERE 
+				(MPE.Id IS NULL OR MP.MilestoneId IS NULL)
+				AND P.ProjectStatusId IN (3,4) 
+				AND (@BusinessUnitIds IS NULL OR (P.GroupId IN (SELECT Id FROM @BusinessUnitIdsTable)))
+				AND P.ClientId = @AccountId
+				AND M.StartDate <= @EndDateLocal AND @StartDateLocal <= M.ProjectedDeliveryDate
+		)
+		 ,TimeEntryHours
+		 AS
+		 (
+			SELECT    Pro.ProjectId, CC.ProjectGroupId,
+						ROUND(SUM(CASE WHEN TEH.IsChargeable = 1
+											AND Pro.ProjectNumber != 'P031000'
+										THEN TEH.ActualHours
+										ELSE 0
+									END), 2) AS BillableHours ,
+						ROUND(SUM(CASE WHEN TEH.IsChargeable = 0
+											AND CC.TimeEntrySectionId <> 2
+											AND Pro.ProjectNumber != 'P031000'
+										THEN TEH.ActualHours
+										ELSE 0
+									END), 2) AS NonBillableHours ,
+						ROUND(SUM(CASE WHEN CC.TimeEntrySectionId = 2
+										THEN TEH.ActualHours
+										ELSE 0
+									END), 2) AS BusinessDevelopmentHours
+									
+			FROM      dbo.TimeEntry TE
+					INNER JOIN dbo.TimeEntryHours TEH ON TEH.TimeEntryId = te.TimeEntryId
+													AND TE.ChargeCodeDate BETWEEN @StartDateLocal
+													AND
+													@EndDateLocal
+					INNER JOIN dbo.ChargeCode CC ON CC.Id = TE.ChargeCodeId
+					INNER JOIN dbo.Person P ON P.PersonId = TE.PersonId
+					INNER JOIN dbo.PersonStatusHistory PTSH ON PTSH.PersonId = P.PersonId
+													AND TE.ChargeCodeDate BETWEEN PTSH.StartDate
+																				AND ISNULL(PTSH.EndDate,@FutureDate)
+					INNER JOIN dbo.ProjectGroup PG ON PG.GroupId = CC.ProjectGroupId
+					INNER JOIN dbo.Project Pro ON Pro.ProjectId = CC.ProjectId
+			WHERE CC.ClientId = @AccountId
+					AND	TE.ChargeCodeDate <= ISNULL(P.TerminationDate,
+												@FutureDate)
+					AND ( CC.timeTypeId != @HolidayTimeType
+							OR ( CC.timeTypeId = @HolidayTimeType
+								AND PTSH.PersonStatusId IN (1,5) -- ACTIVE And Terminated Pending
+								)
+						)
+					AND (@BusinessUnitIds IS NULL
+						OR CC.ProjectGroupId IN (SELECT *
+												FROM @BusinessUnitIdsTable
+												)
+						)
+					AND	Pro.ProjectStatusId IN (3,4) -- Active and Completed status as per #3201 
+			GROUP BY  Pro.ProjectId,CC.ProjectGroupId
+		)
+	
+			SELECT	ISNULL(ISNULL(PH.GroupId, TH.ProjectGroupId),PNP.GroupId) AS GroupId,
+				PG.Name AS GroupName, 
+				PG.Active, 
+				PG.Code AS GroupCode,
+				SUM(CASE WHEN P.ProjectStatusId = 4 THEN 1 ELSE 0 END) AS CompletedProjectsCount,
+				SUM(CASE WHEN P.ProjectStatusId = 3 THEN 1 ELSE 0 END) AS ActiveProjectsCount,
+			    CAST((ROUND(ISNULL(SUM(PH.ForecastedHours),0), 2)) AS float) as ProjectedHours,
+				ROUND(ISNULL(SUM(TH.BillableHours),0), 2) as BillableHours,
+				ROUND(ISNULL(SUM(TH.NonBillableHours),0), 2) as NonBillableHours,
+				ROUND(ISNULL(SUM(TH.BusinessDevelopmentHours),0), 2) as BusinessDevelopmentHours
+		FROM TimeEntryHours TH  
+		FULL JOIN ProjectedHours PH ON PH.ProjectId = TH.ProjectId
+		FULL JOIN ProjectsDoNotHaveMilestonePersonEntries PNP ON PNP.ProjectId = PH.ProjectId OR PNP.ProjectId = TH.ProjectId 
+		LEFT JOIN ProjectGroup PG ON PG.GroupId = ISNULL(ISNULL(PH.GroupId, TH.ProjectGroupId),PNP.GroupId)
+		LEFT JOIN dbo.Project P ON P.ProjectId =  ISNULL(ISNULL(PH.ProjectId, TH.ProjectId),PNP.ProjectId)
+		WHERE P.ProjectNumber != 'P031000'	
+		GROUP BY ISNULL(ISNULL(PH.GroupId, TH.ProjectGroupId),PNP.GroupId),PG.Name,PG.Active,PG.Code
 	
 
-		SELECT COUNT(DISTINCT TE.PersonId) AS PersonsCount,
-			C.Name AS ClientName,
-			C.Code AS ClientCode,
-			C.ClientId AS ClientId
-		FROM    dbo.TimeEntry TE
-				INNER JOIN dbo.ChargeCode CC ON CC.Id = TE.ChargeCodeId
-												AND TE.ChargeCodeDate BETWEEN @StartDateLocal
-												AND
-												@EndDateLocal
-				INNER JOIN dbo.Person P ON P.PersonId = TE.PersonId
-				INNER JOIN dbo.PersonStatusHistory PTSH ON PTSH.PersonId = P.PersonId
-												AND TE.ChargeCodeDate BETWEEN PTSH.StartDate
-																			AND ISNULL(PTSH.EndDate,@FutureDate)
-				INNER JOIN dbo.ProjectGroup PG ON PG.GroupId = CC.ProjectGroupId
-				LEFT JOIN dbo.Project Pro ON Pro.ProjectId = CC.ProjectId
-				INNER JOIN dbo.Client C ON C.ClientId = CC.ClientId
-		WHERE CC.ClientId = @AccountId
-				AND	TE.ChargeCodeDate <= ISNULL(P.TerminationDate,
-											@FutureDate)
-				AND ( CC.timeTypeId != @HolidayTimeType
-						OR ( CC.timeTypeId = @HolidayTimeType
-							AND PTSH.PersonStatusId IN (1,5) -- ACTIVE And Terminated Pending
-							)
-					)
+		SELECT COUNT(DISTINCT p.PersonId) as PersonsCount,
+				c.Name AS ClientName,
+				c.Code AS ClientCode,
+				c.ClientId AS ClientId
+		FROM dbo.Project Pro
+				INNER JOIN dbo.Milestone AS M ON M.ProjectId = Pro.ProjectId
+				INNER JOIN dbo.MilestonePerson AS MP ON MP.MilestoneId = M.MilestoneId
+				INNER JOIN dbo.MilestonePersonEntry AS MPE ON MP.MilestonePersonId = MPE.MilestonePersonId
+				INNER JOIN dbo.person AS P ON P.PersonId = MP.PersonId
+				INNER JOIN dbo.Client C ON C.ClientId = Pro.ClientId
+		WHERE C.ClientId = @AccountId
+				AND MPE.StartDate <= @EndDateLocal AND @StartDateLocal <= MPE.EndDate
+				AND Pro.ProjectStatusId IN (3,4) --Active AND Completed Status
 				AND (@BusinessUnitIds IS NULL
-					OR CC.ProjectGroupId IN (SELECT *
+					OR Pro.GroupId IN (SELECT *
 											FROM @BusinessUnitIdsTable
 											)
 					)
 		GROUP BY C.ClientId, C.Name, C.Code
+		
 
 END
+
