aa467d0c14de5a8019e920ed179368839877da23
SainathC <SainathC>
2013-11-25 04:42:00 -0500

    Comments - Defect Number: 3201; Defect RecordID: 3197; Server Address: localhost; Server Port: 99; Database Name: Practice Management; Defect Summary: Reports > Account: Enhancements to Functionality; addition of Projected Hours, Billable Hours Variance, and Estimated Billings; Defect Description: Recent changes made to the Time > By Project and By Person reports to add Billable Hours Variance and Estimated Billings has been received very favorably by the organization. They have requested that we replicate some of the changes made on the Time Reports to the Account report (accessed by Reports > Account).
    On the Business Unit" Tab:
     Add an Actual Hours column: calculated the same as on the Time > By Project Report, although the Actual Hour number will be sum of Actual Hours for each of the projects for that time period.
    [Harshitha Tenneti]:
    1.We are assuming that the actual hours column to be next to Non-billable column.
    
    LN: Yes, to the right ofthe "Non-Billable" column, as in the By Project Time Report.
    
    2. In Business unit tab, we have "Total hours" column which is a summation of Billable,Non-Billable and BD hours.
    Do you want us to rename this column as "Actual hours" or add another column with summation of Billable and Non-billable hours(As in Time>By project report)?
    
    LN: Excellent point. Let's add a new column to the left of the "Total" column called "Project Hours" and sum only Billable and Non-Billable (i.e., do not sum BD hours). The current "Total" Hours rename to "Total BU Hours"
    
     Projected Hours: total projected hours for all resources on all of the projects for that business unit
    [Harshitha Tenneti]: We are assuming that the Projected hours column will be prior to Billable column.
    
    LN: Yes, please. Thank you for clarifying.
    
     Remove the bar graphic for Percent of Total Hours and display only the percentage number
     The calculation for Total remains the same
    Summary Area above the table: Remove Total Project Hours from the summary area above the table, but add summation of Total Projected Hours and Total Actual Hours next to BD Hours
       Total Projected Hours = the sum of all of the Projected Hours for that account based on the report parameters selected. It also updates as filters are changed.
       Total Actual Hours = the sum of all of the Actual Hours for that account based on the report parameters selected. It also updates as filters are changed.
       Total BD Hours: Calculation remains the same; no change to functionality
    Summary Area above the table: Under Account Name, change "Persons" to "People" and change "BUs" to "Business Unit(s)" (i.e., Microsoft: 10BUs, 32 Projects, 64 Persons: Change "Persons" to "People" and "BUs" to "Business Units")
    
    On the Project Tab:
     Each project name should be hyperlinked  the hyperlink takes the user back to the projects Project Details page. The Project Detail page opens in a new tab.
     Rename Billing Column to Billing Type
     Add an Actual Hours column: calculated the same as on the Time > By Project Report, although the Actual Hour number will be sum of Actual Hours for the project. The number should be hyperlinked (opens in a new tab) to take the user to the Time > By Project Report for that project for the specified time range.
     Total Estimated Billings: As on the Time > By Project Report, this number will reflect the total Estimated Billings if a Time and Materials project, or denotes FF if a fixed fee engagement.
     Add a Billable Hours Variance column, calculated in the same method as the Billable Hours Variance column on the Time Report, although like the other columns being added here, it should be sum of the Billable Hours variance for all resources. It should have the same ! functionality that communicates project overrun/underrun.
     Remove the current Project Variance (in Hours) column, and supporting current graphic
     The order of the columns, from left to right,

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/AccountSummaryByProject.proc.sql
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/AccountSummaryByProject.proc.sql
@@ -11,7 +11,7 @@ AS
 BEGIN
 	
 
-	DECLARE @StartDateLocal DATETIME ,
+		DECLARE @StartDateLocal DATETIME ,
 		@EndDateLocal DATETIME ,
 		@Today DATE ,
 		@HolidayTimeType INT,
@@ -40,30 +40,147 @@ BEGIN
 		 , @HolidayTimeType = dbo.GetHolidayTimeTypeId()
 		 , @FutureDate = dbo.GetFutureDate()
 
-	;WITH ProjectForeCastedHoursUntilToday
+	;WITH PersonForeCastedHoursForBillRate
+		AS
+		(	
+			SELECT   MP.PersonId ,
+					 Pro.ProjectId,
+					AVG(MPE.Amount) AS BillRate,
+					PC.Date,
+					SUM(dbo.PersonProjectedHoursPerDay(PC.DayOff,PC.CompanyDayOff,PC.TimeOffHours,MPE.HoursPerDay)) AS ForecastedHours
+			FROM     dbo.MilestonePersonEntry AS MPE
+					INNER JOIN dbo.MilestonePerson AS MP ON MP.MilestonePersonId = MPE.MilestonePersonId
+					INNER JOIN dbo.Milestone AS M ON M.MilestoneId = MP.MilestoneId
+					INNER JOIN dbo.Project Pro ON Pro.ProjectId = M.ProjectId
+					INNER JOIN dbo.person AS P ON P.PersonId = MP.PersonId AND P.IsStrawman = 0
+					INNER JOIN dbo.PersonCalendarAuto PC ON PC.PersonId = MP.PersonId
+											AND PC.Date BETWEEN MPE.StartDate AND MPE.EndDate	
+					LEFT  JOIN dbo.PersonRole AS PR ON PR.PersonRoleId = MPE.PersonRoleId
+			WHERE    Pro.ClientId = @AccountId
+					 AND (@BusinessUnitIds IS NULL OR (Pro.GroupId IN (SELECT Ids FROM @BusinessUnitIdsTable)))
+					 AND PC.Date BETWEEN MPE.StartDate AND MPE.EndDate
+					 AND PC.Date BETWEEN @StartDateLocal AND @EndDateLocal 
+					
+			GROUP BY MP.PersonId,Pro.ProjectId,PC.Date 
+		),
+		TimeEntryPersonsForBillRate 
+		AS
+		(
+			SELECT TE.PersonId,PRO.ProjectId,TE.ChargeCodeDate,
+			SUM(CASE WHEN ( TEH.IsChargeable = 1 AND PRO.ProjectNumber != 'P031000'
+								AND TE.ChargeCodeDate < @Today
+										) THEN TEH.ActualHours
+									ELSE 0
+								END) BillableHoursUntilToday,
+			SUM(CASE WHEN TEH.IsChargeable = 1  AND PRO.ProjectNumber != 'P031000'
+									THEN TEH.ActualHours
+									ELSE 0
+								END) AS BillableHours ,
+			SUM(CASE WHEN TEH.IsChargeable = 0 OR PRO.ProjectNumber != 'P031000'
+									THEN TEH.ActualHours
+									ELSE 0
+								END) AS NonBillableHours
+			--TE.*, TEH.IsChargeable, TEH.ActualHours, CC.*, PTSH.PersonStatusId
+			FROM TimeEntry TE
+			INNER JOIN dbo.ChargeCode AS CC ON CC.Id = TE.ChargeCodeId
+			INNER JOIN dbo.TimeEntryHours AS TEH ON TEH.TimeEntryId = TE.TimeEntryId
+														AND TE.ChargeCodeDate BETWEEN @StartDateLocal AND @EndDateLocal
+			INNER JOIN dbo.PersonStatusHistory PTSH ON PTSH.PersonId = TE.PersonId
+													AND TE.ChargeCodeDate BETWEEN PTSH.StartDate
+													AND ISNULL(PTSH.EndDate,@FutureDate)
+			INNER JOIN dbo.Person P ON P.PersonId = TE.PersonId 
+										AND TE.ChargeCodeDate <= ISNULL(P.TerminationDate,@FutureDate)
+										AND ( 
+												CC.timeTypeId != @HolidayTimeType
+												OR ( 
+														CC.timeTypeId = @HolidayTimeType
+														AND PTSH.PersonStatusId IN (1,5)
+													)
+											)
+			INNER JOIN dbo.Project PRO ON PRO.ProjectId = CC.ProjectId
+			WHERE  CC.ClientId = @AccountId
+					AND (@BusinessUnitIds IS NULL
+							OR CC.ProjectGroupId IN (SELECT Ids
+													FROM @BusinessUnitIdsTable )
+						)
+					 AND (@ProjectStatusIds IS NULL
+							 OR PRO.ProjectStatusId IN (SELECT Ids
+														FROM @ProjectStatusIdsTable )
+						)
+			GROUP BY TE.PersonId,PRO.ProjectId,TE.ChargeCodeDate
+		),
+	EstimatedBillingsByProject
+		AS 
+		(
+		  SELECT Proj.ProjectId,
+		         ROUND(SUM(Proj.BillRate*Proj.BillableHours),2) AS EstBillings
+		  From
+			(SELECT	PFR.ProjectId,
+					PFR.BillRate,
+					SUM(PFR.ForecastedHours) AS ForecastedHours,
+					ROUND(ISNULL(SUM(TEP.BillableHours),0), 2) AS BillableHours ,
+					ROUND(ISNULL(SUM(TEP.NonBillableHours),0), 2) AS NonBillableHours
+			FROM PersonForeCastedHoursForBillRate	PFR
+			INNER JOIN TimeEntryPersonsForBillRate TEP ON TEP.PersonId = PFR.PersonId AND TEP.ChargeCodeDate = PFR.Date AND PFR.ProjectId = TEP.ProjectId
+			WHERE PFR.BillRate IS NOT NULL
+			GROUP BY PFR.ProjectId,PFR.BillRate)Proj
+			GROUP BY Proj.ProjectId
+		),
+	 ProjectsDoNotHaveMilestonePersonEntries
+	 AS
+	 (
+	 SELECT  p.ProjectId,p.GroupId,p.ClientId,P.ProjectStatusId,P.StartDate,P.EndDate
+			, MIN(CAST(M.IsHourlyAmount AS INT)) MinimumValue
+			, MAX(CAST(M.IsHourlyAmount AS INT)) MaximumValue
+	 FROM dbo.Project P
+	 INNER JOIN dbo.Milestone M ON M.ProjectId = P.ProjectId  
+	 LEFT JOIN dbo.MilestonePerson MP ON MP.MilestoneId = M.MilestoneId
+	 LEFT JOIN dbo.MilestonePersonEntry MPE ON MPE.MilestonePersonId = MP.MilestonePersonId
+	                                   AND MPE.StartDate <= @EndDateLocal AND @StartDateLocal <= MPE.EndDate
+	 WHERE 
+		   (MPE.Id IS NULL OR MP.MilestoneId IS NULL)
+		   AND 
+		   P.ProjectStatusId IN (3,4) 
+		   AND (@BusinessUnitIds IS NULL
+					OR P.GroupId IN (SELECT Ids
+											FROM @BusinessUnitIdsTable )
+				)
+		   AND P.ClientId = @AccountId
+	       AND M.StartDate <= @EndDateLocal AND @StartDateLocal <= M.ProjectedDeliveryDate
+	 GROUP BY p.ProjectId,p.GroupId,p.ClientId,P.ProjectStatusId,P.StartDate,P.EndDate
+	 )
+	 ,
+	 ProjectForeCastedHoursUntilToday
 	AS (
-		SELECT M.ProjectId
-				, SUM(dbo.PersonProjectedHoursPerDay(PC.DayOff,PC.CompanyDayOff,PC.TimeOffHours,MPE.HoursPerDay)) AS ForecastedHoursUntilToday
+		SELECT	Pro.ProjectId
+				,Pro.ClientId
+				, SUM(CASE WHEN PC.Date < @Today AND P.IsStrawman = 0 THEN (dbo.PersonProjectedHoursPerDay(PC.DayOff,PC.CompanyDayOff,PC.TimeOffHours,MPE.HoursPerDay))
+									ELSE 0
+								END) AS ForecastedHoursUntilToday
+				, AVG(ISNULL(MPE.Amount,0)) as BillRate
+				, ROUND(SUM(CASE WHEN P.IsStrawman = 0 THEN dbo.PersonProjectedHoursPerDay(PC.DayOff,PC.CompanyDayOff,PC.TimeOffHours,MPE.HoursPerDay) ELSE 0 END),2)  AS ForecastedHours
 				, MIN(CAST(M.IsHourlyAmount AS INT)) MinimumValue
 				, MAX(CAST(M.IsHourlyAmount AS INT)) MaximumValue
-		FROM
-			dbo.MilestonePersonEntry AS MPE
-			INNER JOIN dbo.MilestonePerson AS MP
-				ON MP.MilestonePersonId = MPE.MilestonePersonId
-			INNER JOIN dbo.Milestone AS M
-				ON M.MilestoneId = MP.MilestoneId
-			INNER JOIN dbo.person AS P
-				ON P.PersonId = MP.PersonId AND P.IsStrawman = 0
-			INNER JOIN dbo.PersonCalendarAuto PC
-				ON PC.PersonId = MP.PersonId AND PC.Date BETWEEN MPE.StartDate AND MPE.EndDate AND PC.Date BETWEEN @StartDateLocal AND CASE
-					WHEN @EndDateLocal > DATEADD(DAY, -1, @Today) THEN
-						DATEADD(DAY, -1, @Today)
-					ELSE
-						@EndDateLocal
-				END
+		FROM	dbo.Project Pro
+				LEFT JOIN dbo.Milestone AS M ON M.ProjectId = Pro.ProjectId
+				LEFT JOIN dbo.MilestonePerson AS MP ON MP.MilestoneId = M.MilestoneId
+				LEFT JOIN dbo.MilestonePersonEntry AS MPE ON MP.MilestonePersonId = MPE.MilestonePersonId
+				LEFT JOIN dbo.person AS P ON P.PersonId = MP.PersonId 
+				LEFT JOIN dbo.PersonCalendarAuto PC ON PC.PersonId = MP.PersonId
+						  
+		WHERE Pro.ProjectStatusId IN (3,4) --Active and Completed status as per #3201 
+			  AND Pro.ClientId = @AccountId
+			   AND (@BusinessUnitIds IS NULL
+					OR Pro.GroupId IN (SELECT Ids
+											FROM @BusinessUnitIdsTable )
+				)
+			  AND Pro.StartDate IS NOT NULL AND Pro.EndDate IS NOT NULL
+		 AND PC.Date BETWEEN MPE.StartDate AND MPE.EndDate
+						   AND PC.Date BETWEEN @StartDateLocal AND @EndDateLocal   
 		GROUP BY
-			M.ProjectId
-	),
+			Pro.ProjectId,Pro.ClientId
+	)
+	,
 	HoursData
 	AS ( SELECT PRO.ProjectId
 			  , CC.ClientId
@@ -117,6 +234,7 @@ BEGIN
 					 OR PRO.ProjectStatusId IN (SELECT Ids
 												FROM @ProjectStatusIdsTable )
 				)
+			 AND PRO.ProjectStatusId IN (3,4) --Active and Completed status as per #3201 
 		 GROUP BY
 			 PRO.ProjectId
 		   , CC.ClientId
@@ -127,59 +245,94 @@ BEGIN
 		   , CC.ProjectGroupId
 
 	)
-	SELECT C.ClientId
+
+		SELECT C.ClientId
 		 , C.Name AS ClientName
 		 , C.Code AS ClientCode
 		 , PG.GroupId AS GroupId
 		 , PG.Name AS GroupName
 		 , PG.Code AS GroupCode
-		 , HD.ProjectId
-		 , HD.ProjectName
-		 , HD.ProjectNumber
+		 , P.ProjectId
+		 , P.Name AS ProjectName
+		 , P.ProjectNumber
 		 , PS.ProjectStatusId
 		 , PS.Name AS ProjectStatusName
-		 , HD.BillableHours
-		 , HD.NonBillableHours
-		 , ISNULL(pfh.ForecastedHoursUntilToday, 0) AS ForecastedHoursUntilToday
-		 , BillableHoursUntilToday
-		 , HD.TimeEntrySectionId
+		 , ISNULL(HD.BillableHours,0) AS BillableHours 
+		 , ISNULL(HD.NonBillableHours,0) AS NonBillableHours
+		 , ISNULL(pfh.ForecastedHours, 0) AS ForecastedHoursUntilToday
+		 , ISNULL(pfh.ForecastedHours, 0) AS ForecastedHours
+		 , ISNULL(HD.BillableHours,0) AS BillableHoursUntilToday
+		 , ISNULL(HD.TimeEntrySectionId,-1) AS TimeEntrySectionId
 		 , (CASE
-			   WHEN (pfh.MinimumValue IS NULL) THEN
+			   WHEN (ISNULL(pfh.MinimumValue,PNP.MinimumValue) IS NULL) THEN
 				   ''
-			   WHEN (pfh.MinimumValue = pfh.MaximumValue AND pfh.MinimumValue = 0) THEN
+			   WHEN  pfh.MinimumValue IS NOT NULL AND PNP.MinimumValue IS NOT NULL AND pfh.MinimumValue = PFH.MaximumValue AND PNP.MinimumValue = PNP.MaximumValue AND pfh.MinimumValue = 0 AND PNP.MinimumValue = 0 THEN
+			       'Fixed'
+			   WHEN  pfh.MinimumValue IS NOT NULL AND PNP.MinimumValue IS NOT NULL AND pfh.MinimumValue = PFH.MaximumValue AND PNP.MinimumValue = PNP.MaximumValue AND pfh.MinimumValue = 1 AND PNP.MinimumValue = 1 THEN
+			       'Hourly'
+			   WHEN  pfh.MinimumValue IS NOT NULL AND PNP.MinimumValue IS NOT NULL AND (pfh.MinimumValue <> PFH.MaximumValue OR PNP.MinimumValue <> PNP.MaximumValue OR pfh.MinimumValue <> PNP.MaximumValue OR PNP.MinimumValue <> pfh.MaximumValue) THEN
+			       'Both'
+			   WHEN (ISNULL(pfh.MinimumValue,PNP.MinimumValue) = ISNULL(pfh.MaximumValue,PNP.MaximumValue) AND ISNULL(pfh.MinimumValue,PNP.MinimumValue) = 0) THEN
 				   'Fixed'
-			   WHEN (pfh.MinimumValue = pfh.MaximumValue AND pfh.MinimumValue = 1) THEN
+			   WHEN (ISNULL(pfh.MinimumValue,PNP.MinimumValue) = ISNULL(pfh.MaximumValue,PNP.MaximumValue) AND ISNULL(pfh.MinimumValue,PNP.MinimumValue) = 1) THEN
 				   'Hourly'
 			   ELSE
 				   'Both'
 		   END) AS BillingType
+		 , (CASE
+			  WHEN  pfh.MinimumValue IS NOT NULL AND PNP.MinimumValue IS NOT NULL AND pfh.MinimumValue = PFH.MaximumValue AND PNP.MinimumValue = PNP.MaximumValue AND pfh.MinimumValue = 0 AND PNP.MinimumValue = 0 THEN
+			       -1
+			   WHEN  pfh.MinimumValue IS NOT NULL AND PNP.MinimumValue IS NOT NULL AND pfh.MinimumValue = PFH.MaximumValue AND PNP.MinimumValue = PNP.MaximumValue AND pfh.MinimumValue = 1 AND PNP.MinimumValue = 1 THEN
+			       ISNULL(EBP.EstBillings,0)
+			   WHEN  pfh.MinimumValue IS NOT NULL AND PNP.MinimumValue IS NOT NULL AND (pfh.MinimumValue <> PFH.MaximumValue OR PNP.MinimumValue <> PNP.MaximumValue OR pfh.MinimumValue <> PNP.MaximumValue OR PNP.MinimumValue <> pfh.MaximumValue) THEN
+			       ISNULL(EBP.EstBillings,0)
+			   WHEN (ISNULL(pfh.MinimumValue,PNP.MinimumValue) = ISNULL(pfh.MaximumValue,PNP.MaximumValue) AND ISNULL(pfh.MinimumValue,PNP.MinimumValue) = 0) THEN
+				   -1
+			   ELSE
+				   ISNULL(EBP.EstBillings,0)
+		   END) AS EstimatedBillings
 	FROM
 		HoursData HD
-		INNER JOIN Client C
-			ON C.ClientId = HD.ClientId
-		INNER JOIN ProjectStatus PS
-			ON PS.ProjectStatusId = HD.ProjectStatusId
-		INNER JOIN ProjectGroup PG
-			ON PG.GroupId = HD.GroupId
-		LEFT JOIN ProjectForeCastedHoursUntilToday pfh
-			ON pfh.ProjectId = HD.ProjectId
+		FULL JOIN ProjectForeCastedHoursUntilToday pfh
+			ON pfh.ProjectId = HD.ProjectId AND pfh.ClientId = HD.ClientId
+		FULL JOIN ProjectsDoNotHaveMilestonePersonEntries PNP ON PNP.ProjectId = pfh.ProjectId OR PNP.ProjectId = HD.ProjectId
+		LEFT JOIN dbo.Project P 
+			ON P.ProjectId = isnull(ISNULL(pfh.ProjectId,HD.ProjectId),pnp.ProjectId)
+		LEFT JOIN EstimatedBillingsByProject EBP 
+			ON EBP.ProjectId = P.ProjectId
+		LEFT JOIN Client C
+			ON C.ClientId = ISNULL(ISNULL(pfh.ClientId,HD.ClientId),pnp.ClientId)
+		LEFT JOIN ProjectStatus PS
+			ON PS.ProjectStatusId = P.ProjectStatusId
+		LEFT JOIN ProjectGroup PG
+			ON PG.GroupId = ISNULL(ISNULL(P.GroupId,HD.GroupId),pnp.GroupId)
+		
 	WHERE
 		((@ProjectBillingTypes IS NULL)
 		OR ((CASE
-			WHEN (pfh.MinimumValue IS NULL) THEN
-				''
-			WHEN (pfh.MinimumValue = pfh.MaximumValue
-				AND pfh.MinimumValue = 0) THEN
-				'Fixed'
-			WHEN (pfh.MinimumValue = pfh.MaximumValue
-				AND pfh.MinimumValue = 1) THEN
-				'Hourly'
-			ELSE
-				'Both'
-			END) IN (SELECT PBT.BillingType
+			   WHEN (ISNULL(pfh.MinimumValue,PNP.MinimumValue) IS NULL) THEN
+				   ''
+			   WHEN  pfh.MinimumValue IS NOT NULL AND PNP.MinimumValue IS NOT NULL AND pfh.MinimumValue = PFH.MaximumValue AND PNP.MinimumValue = PNP.MaximumValue AND pfh.MinimumValue = 0 AND PNP.MinimumValue = 0 THEN
+			       'Fixed'
+			   WHEN  pfh.MinimumValue IS NOT NULL AND PNP.MinimumValue IS NOT NULL AND pfh.MinimumValue = PFH.MaximumValue AND PNP.MinimumValue = PNP.MaximumValue AND pfh.MinimumValue = 1 AND PNP.MinimumValue = 1 THEN
+			       'Hourly'
+			   WHEN  pfh.MinimumValue IS NOT NULL AND PNP.MinimumValue IS NOT NULL AND (pfh.MinimumValue <> PFH.MaximumValue OR PNP.MinimumValue <> PNP.MaximumValue OR pfh.MinimumValue <> PNP.MaximumValue OR PNP.MinimumValue <> pfh.MaximumValue) THEN
+			       'Both'
+			   WHEN (ISNULL(pfh.MinimumValue,PNP.MinimumValue) = ISNULL(pfh.MaximumValue,PNP.MaximumValue) AND ISNULL(pfh.MinimumValue,PNP.MinimumValue) = 0) THEN
+				   'Fixed'
+			   WHEN (ISNULL(pfh.MinimumValue,PNP.MinimumValue) = ISNULL(pfh.MaximumValue,PNP.MaximumValue) AND ISNULL(pfh.MinimumValue,PNP.MinimumValue) = 1) THEN
+				   'Hourly'
+			   ELSE
+				   'Both'
+		   END) IN (SELECT PBT.BillingType
 					 FROM @ProjectBillingTypesTable PBT )
 			)
 		)
+		AND (@ProjectStatusIds IS NULL
+					 OR P.ProjectStatusId IN (SELECT Ids
+												FROM @ProjectStatusIdsTable )
+				)
+		AND P.ProjectNumber != 'P031000'	
 	ORDER BY
 		HD.TimeEntrySectionId
 	  , HD.ProjectNumber
@@ -195,49 +348,43 @@ BEGIN
 					M.ProjectId
 	),
 	PersonsCountCTE
-	AS ( SELECT COUNT(DISTINCT TE.PersonId) AS PersonsCount
-		 FROM
-			 dbo.TimeEntry TE
-			 INNER JOIN dbo.ChargeCode CC
-				 ON CC.Id = TE.ChargeCodeId
-			 INNER JOIN dbo.Person P
-				 ON P.PersonId = TE.PersonId
-			 INNER JOIN dbo.PersonStatusHistory PTSH
-				 ON PTSH.PersonId = P.PersonId AND TE.ChargeCodeDate BETWEEN PTSH.StartDate AND ISNULL(PTSH.EndDate,@FutureDate)
-			 INNER JOIN dbo.Project PRO
-				 ON PRO.ProjectId = CC.ProjectId
-			 LEFT JOIN ProjectForeCastedHoursUntilToday pfh
-				 ON pfh.ProjectId = PRO.ProjectId
-		 WHERE  TE.ChargeCodeDate BETWEEN @StartDateLocal AND @EndDateLocal AND
-			 CC.ClientId = @AccountId
-			 AND TE.ChargeCodeDate <= ISNULL(P.TerminationDate, @FutureDate)
-			 AND (CC.timeTypeId != @HolidayTimeType
-			 OR (CC.timeTypeId = @HolidayTimeType
-			 AND PTSH.PersonStatusId IN (1,5) -- ACTIVE And Terminated Pending
-			 ))
-			 AND (@BusinessUnitIds IS NULL
-					 OR CC.ProjectGroupId IN (SELECT Ids
-											  FROM @BusinessUnitIdsTable )
-				)
-			 AND (@ProjectStatusIds IS NULL
-					 OR PRO.ProjectStatusId IN (SELECT Ids
-												FROM @ProjectStatusIdsTable )
-				)
-			 AND ((@ProjectBillingTypes IS NULL)
-			 OR ((CASE
-				 WHEN (pfh.MinimumValue IS NULL) THEN
-					 ''
-				 WHEN (pfh.MinimumValue = pfh.MaximumValue
-					 AND pfh.MinimumValue = 0) THEN
-					 'Fixed'
-				 WHEN (pfh.MinimumValue = pfh.MaximumValue
-					 AND pfh.MinimumValue = 1) THEN
-					 'Hourly'
-				 ELSE
-					 'Both'
-				 END) IN (SELECT PBT.BillingType
-						  FROM @ProjectBillingTypesTable PBT )
-				)
+	AS ( SELECT 
+	            COUNT(DISTINCT P.PersonId) AS PersonsCount
+		 FROM dbo.Project Pro
+			  INNER JOIN dbo.Milestone AS M ON M.ProjectId = Pro.ProjectId
+			  INNER JOIN dbo.MilestonePerson AS MP ON MP.MilestoneId = M.MilestoneId
+			  INNER JOIN dbo.MilestonePersonEntry AS MPE ON MP.MilestonePersonId = MPE.MilestonePersonId
+			  INNER JOIN dbo.person AS P ON P.PersonId = MP.PersonId
+			  INNER JOIN dbo.Client C ON C.ClientId = Pro.ClientId
+			  LEFT JOIN ProjectForeCastedHoursUntilToday pfh
+				 ON pfh.ProjectId = PRO.ProjectId 
+		 WHERE  MPE.StartDate <= @EndDateLocal AND @StartDateLocal <= MPE.EndDate
+			     AND Pro.ProjectStatusId IN (3,4) --Active AND Completed Status
+				 AND Pro.ClientId = @AccountId
+				 AND (
+						@BusinessUnitIds IS NULL
+						 OR Pro.GroupId IN (SELECT Ids
+												  FROM @BusinessUnitIdsTable )
+					)
+				 AND (@ProjectStatusIds IS NULL
+						 OR PRO.ProjectStatusId IN (SELECT Ids
+													FROM @ProjectStatusIdsTable )
+					)
+				 AND ((@ProjectBillingTypes IS NULL)
+				 OR ((CASE
+					 WHEN (pfh.MinimumValue IS NULL) THEN
+						 ''
+					 WHEN (pfh.MinimumValue = pfh.MaximumValue
+						 AND pfh.MinimumValue = 0) THEN
+						 'Fixed'
+					 WHEN (pfh.MinimumValue = pfh.MaximumValue
+						 AND pfh.MinimumValue = 1) THEN
+						 'Hourly'
+					 ELSE
+						 'Both'
+					 END) IN (SELECT PBT.BillingType
+							  FROM @ProjectBillingTypesTable PBT )
+					)
 			)
 	)
 
