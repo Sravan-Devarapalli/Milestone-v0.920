7259401bcb607ebd937832d6369aadc48a81a73d
SainathC <SainathC>
2013-12-12 07:29:00 -0500

    Comments - Defect Number: 3209; Defect RecordID: 3205; Server Address: localhost; Server Port: 99; Database Name: Practice Management; Defect Summary: Project /opportunity permissions tab in Add person page.; Defect Description: The entries made to Project /opportunity permissions tab in Add person page are not being saved. The entries made are being saved if they are entered from person detailed page (Already added user).
    Steps to reproduce:
    1. Log on to Practice management.
    2. Go to Add person page.
    3. Add details for adding a person.
    4. Add permissions for the person on given accounts, business units, sales persons, project managers and Practice areas.
    5. Add compensation for the person and save him.
    6. Now open his person detail page and verify if the entries added in Project /opportunity permissions tab are saved.
    
    Could you please let us your response on this?

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/PersonDetail.aspx.designer.cs
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/PersonDetail.aspx.designer.cs
@@ -103,15 +103,6 @@ namespace PraticeManagement {
         protected global::System.Web.UI.WebControls.RequiredFieldValidator reqPersonStatus;
         
         /// <summary>
-        /// custPersonStatus control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.CustomValidator custPersonStatus;
-        
-        /// <summary>
         /// txtLastName control.
         /// </summary>
         /// <remarks>
