a46777f4bc40f7d2fae9016ce8666f5881eedbbc
SainathC <SainathC>
2014-02-14 07:57:00 -0500

    Comments - Defect Number: 3233; Defect RecordID: 3229; Server Address: localhost; Server Port: 99; Database Name: Practice Management; Defect Summary: "Recruiting Metrics": New Persons Detail tab, Configuration page, and export; Defect Description: The Recruiting team is currently struggling with fulfilling the reporting requirements that the Senior Leadership Team has requested. The Recruiting Teams Applicant Tracking System is not customable, making it difficult to track the metrics that they wish to see. To that end, they have requested an enhancement to Practice Management to capture a few new data fields critical to their reporting that is manually-driven right now given the inefficiencies of the other system.
    Given the team's urgent need for reporting, I'd like to make this the high priority task for the GGK team right now.
    
    
    Create a new tab on the Person Details page entitled Recruiting Metrics tab (a hand-written mock-up is attached):
    1. The location of the tab should be between Compensation and Employment History.
    2. In the Add New Employee workflow (that begins when a user clicks the Add Person button the Persons page), the Recruiting Metrics tab should be added to that workflow as a required tab to complete before the user can complete the workflow.
    3. The fields on the new Recruiting Metrics tabs will be:
    Recruiter: Required drop-down field. Move the current Recruiter field to this new tab.
    Job Seeker Status: Not a required field. Radio button allows one of two choices: Active Candidate or Passive Candidate
    Source: Not a required field. The drop-down list is generated from the list customizable on the new Recruiting Metrics page under Configuration > Company Resources > Recruiting Metrics
    Targeted: Not a required field. The drop-down list is generated from the list customizable on the new Recruiting Metrics page under Configuration > Company Resources > Recruiting Metrics
    Employee Referral: Not a required field. Radio button allows one of two choices: No and Yes. If user selects Yes, then a drop down field next to the Yes radio button becomes selectable. The drop-down list displays a list of all users with a status of Active that have a Pay Type of W2-Salary or W2-Hourly.
    
    Create a new Configuration page under Company Resources entitled Recruiting Metrics:
    1. There will be two tabs on the Recruiting Metrics configuration page entitled Candidate Source Field Customization and Targeted Company Field Customization
    2. Use the Configuration > Financials > Salary Bands and PTO Accrual as a guide for the format of each table. The tables should have the same adding/editing/deleting functionality as that table as well.
    3. The Candidate Source Field Customization tab contains one table with two columns: Source Name and Sort Order. The fields to input into the table are:
    
    Source Name  Sort Order
    Agency  5
    Bellevue College  10
    Career Site  15
    Client Referral  20
    College Job Posting  25
    Database Resume Search  30
    DICE- Resume Search  35
    DICE.com  40
    Email  45
    Employee Referral  50
    Employee Referral- Emailed to recruiting  55
    Facebook  60
    Glassdoor.com  65
    Google+  70
    Google+ Candidate Search  75
    Hiring Manager  80
    Import  85
    Indeed.com  90
    Internal Employee  95
    Internal Transfer  100
    Job Board- Other  105
    Job Notification  110
    Jobvite Facebook App  115
    LinkedIn  120
    LinkedIn- Resume/Candidate Search  125
    LinkedIn User Group  130
    Monster.com  135
    Monster.com- Resume Search  140
    Networking Event  145
    Recruiter  150
    SimplyHired.com  155
    Twitter  160
    UW Foster Event  165
    Web Search  170
    
    4. The Targeted Company Field Customization tab contains one table with two columns: Source Name and Sort Order. The fields to input into the table are:
    Targeted Company  Sort Order
    A.T. Kearney  5
    Accenture  10
    Avanade  15
    Bain & Company  20
    BearingPoint  25
    Booz & Company  30
    Boston Consulting  35
    Capgemini  40
    CSC  45
    Deloitte  50
    E&Y  55
    Hitachi  60
    IBM  65
    KPMG  70
    McKinsey  75
    Navigant Consulting

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/Controls/Reports/AuditByProject.ascx.cs
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/Controls/Reports/AuditByProject.ascx.cs
@@ -8,12 +8,95 @@ using DataTransferObjects.Reports;
 using System.Text;
 using DataTransferObjects.TimeEntry;
 using DataTransferObjects;
+using PraticeManagement.Utils.Excel;
+using PraticeManagement.Utils;
+using System.Data;
 
 namespace PraticeManagement.Controls.Reports
 {
     public partial class AuditByProject : System.Web.UI.UserControl
     {
         private string AuditReportExport = "Audit Report By Project";
+        private int coloumnsCount = 1;
+        private int headerRowsCount = 1;
+
+        private SheetStyles HeaderSheetStyle
+        {
+            get
+            {
+                CellStyles cellStyle = new CellStyles();
+                cellStyle.IsBold = true;
+                cellStyle.BorderStyle = NPOI.SS.UserModel.BorderStyle.NONE;
+                cellStyle.FontHeight = 350;
+                CellStyles[] cellStylearray = { cellStyle };
+                RowStyles headerrowStyle = new RowStyles(cellStylearray);
+                headerrowStyle.Height = 500;
+
+                CellStyles dataCellStyle = new CellStyles();
+                dataCellStyle.IsBold = true;
+                dataCellStyle.BorderStyle = NPOI.SS.UserModel.BorderStyle.NONE;
+                dataCellStyle.FontHeight = 200;
+                CellStyles[] dataCellStylearray = { dataCellStyle };
+                RowStyles datarowStyle = new RowStyles(dataCellStylearray);
+                datarowStyle.Height = 350;
+
+                RowStyles[] rowStylearray = { headerrowStyle, datarowStyle};
+
+                SheetStyles sheetStyle = new SheetStyles(rowStylearray);
+                sheetStyle.MergeRegion.Add(new int[] { 0, 0, 0, coloumnsCount - 1 });
+                sheetStyle.IsAutoResize = false;
+
+                return sheetStyle;
+            }
+        }
+
+        private SheetStyles DataSheetStyle
+        {
+            get
+            {
+                CellStyles headerCellStyle = new CellStyles();
+                headerCellStyle.IsBold = true;
+                headerCellStyle.HorizontalAlignment = NPOI.SS.UserModel.HorizontalAlignment.CENTER;
+                List<CellStyles> headerCellStyleList = new List<CellStyles>();
+                headerCellStyleList.Add(headerCellStyle);
+                RowStyles headerrowStyle = new RowStyles(headerCellStyleList.ToArray());
+
+                CellStyles dataCellStyle = new CellStyles();
+
+                CellStyles dataDateCellStyle = new CellStyles();
+                dataDateCellStyle.DataFormat = "mm/dd/yy;@";
+
+                CellStyles[] dataCellStylearray = { dataCellStyle,
+                                                    dataCellStyle, 
+                                                    dataCellStyle,
+                                                    dataCellStyle,
+                                                  dataDateCellStyle,
+                                                  dataDateCellStyle,
+                                                  dataCellStyle,
+                                                    dataCellStyle, 
+                                                    dataCellStyle,
+                                                    dataCellStyle,
+                                                    dataCellStyle,
+                                                    dataCellStyle, 
+                                                    dataCellStyle,
+                                                    dataCellStyle,
+                                                    dataCellStyle,
+                                                    dataCellStyle, 
+                                                    dataCellStyle
+                                                  };
+
+                RowStyles datarowStyle = new RowStyles(dataCellStylearray);
+
+                RowStyles[] rowStylearray = { headerrowStyle, datarowStyle };
+                SheetStyles sheetStyle = new SheetStyles(rowStylearray);
+                sheetStyle.TopRowNo = headerRowsCount;
+                sheetStyle.IsFreezePane = true;
+                sheetStyle.FreezePanColSplit = 0;
+                sheetStyle.FreezePanRowSplit = headerRowsCount;
+
+                return sheetStyle;
+            }
+        }
 
         private PraticeManagement.Reporting.Audit HostingPage
         {
@@ -61,114 +144,101 @@ namespace PraticeManagement.Controls.Reports
         protected void btnExportToExcel_OnClick(object sender, EventArgs e)
         {
             DataHelper.InsertExportActivityLogMessage(AuditReportExport);
+            List<SheetStyles> sheetStylesList = new List<SheetStyles>();
+            var dataSetList = new List<DataSet>();
             if (HostingPage.StartDate.HasValue && HostingPage.EndDate.HasValue)
             {
-                List<ProjectLevelTimeEntriesHistory> data = ServiceCallers.Custom.Report(r => r.TimeEntryAuditReportByProject(HostingPage.StartDate.Value, HostingPage.EndDate.Value)).ToList();
-                data = data.OrderBy(p => p.Project.ProjectNumber).ToList();
-                StringBuilder sb = new StringBuilder();
-                sb.Append("Time Entry Audit");
-                sb.Append("\t");
-                sb.AppendLine();
-                sb.Append(data.Count + " Person(s) Affected");
-                sb.Append("\t");
-                sb.AppendLine();
-                sb.Append(HostingPage.Range);
-                sb.Append("\t");
-                sb.AppendLine();
-                sb.AppendLine();
-                if (data.Count > 0)
-                {
-                    //Header
-                    sb.Append("Employee Id");
-                    sb.Append("\t");
-                    sb.Append("Person Name");
-                    sb.Append("\t");
-                    sb.Append("Status");
-                    sb.Append("\t");
-                    sb.Append("Pay Types");
-                    sb.Append("\t");
-                    sb.Append("Affected Date");
-                    sb.Append("\t");
-                    sb.Append("Modified Date");
-                    sb.Append("\t");
-                    sb.Append("Account Name");
-                    sb.Append("\t");
-                    sb.Append("Business Unit");
-                    sb.Append("\t");
-                    sb.Append("Project");
-                    sb.Append("\t");
-                    sb.Append("Project Name");
-                    sb.Append("\t");
-                    sb.Append("Phase");
-                    sb.Append("\t");
-                    sb.Append("Work Type");
-                    sb.Append("\t");
-                    sb.Append("B/NB");
-                    sb.Append("\t");
-                    sb.Append("Original");
-                    sb.Append("\t");
-                    sb.Append("New");
-                    sb.Append("\t");
-                    sb.Append("Net Change");
-                    sb.Append("\t");
-                    sb.Append("Note");
-                    sb.Append("\t");
-                    sb.AppendLine();
+                List<ProjectLevelTimeEntriesHistory> report = ServiceCallers.Custom.Report(r => r.TimeEntryAuditReportByProject(HostingPage.StartDate.Value, HostingPage.EndDate.Value)).ToList();
+                report = report.OrderBy(p => p.Project.ProjectNumber).ToList();
 
-                    //Data
-                    foreach (ProjectLevelTimeEntriesHistory projectLevelTimeEntriesHistory in data)
-                    {
-                        foreach (PersonLevelTimeEntriesHistory personLevelTimeEntrie in projectLevelTimeEntriesHistory.PersonLevelTimeEntries)
-                        {
-                            foreach (TimeEntryRecord timeEntryRecord in personLevelTimeEntrie.TimeEntryRecords)
-                            {
-                                sb.Append(personLevelTimeEntrie.Person.EmployeeNumber);
-                                sb.Append("\t");
-                                sb.Append(personLevelTimeEntrie.Person.HtmlEncodedName);
-                                sb.Append("\t");
-                                sb.Append(personLevelTimeEntrie.Person.Status.Name);
-                                sb.Append("\t");
-                                sb.Append(personLevelTimeEntrie.Person.CurrentPay != null ? personLevelTimeEntrie.Person.CurrentPay.TimescaleName : string.Empty);
-                                sb.Append("\t");
-                                sb.Append(timeEntryRecord.ChargeCodeDate.ToString("MM/dd/yyyy"));
-                                sb.Append("\t");
-                                sb.Append(timeEntryRecord.ModifiedDate.ToString("MM/dd/yyyy"));
-                                sb.Append("\t");
-                                sb.Append(timeEntryRecord.ChargeCode.Client.HtmlEncodedName);
-                                sb.Append("\t");
-                                sb.Append(timeEntryRecord.ChargeCode.ProjectGroup.HtmlEncodedName);
-                                sb.Append("\t");
-                                sb.Append(timeEntryRecord.ChargeCode.Project.ProjectNumber);
-                                sb.Append("\t");
-                                sb.Append(timeEntryRecord.ChargeCode.Project.HtmlEncodedName);
-                                sb.Append("\t");
-                                sb.Append(timeEntryRecord.ChargeCode.Phase);
-                                sb.Append("\t");
-                                sb.Append(timeEntryRecord.ChargeCode.TimeType.Name);
-                                sb.Append("\t");
-                                sb.Append(timeEntryRecord.IsChargeable ? "B" : "NB");
-                                sb.Append("\t");
-                                sb.Append(timeEntryRecord.OldHours);
-                                sb.Append("\t");
-                                sb.Append(timeEntryRecord.ActualHours);
-                                sb.Append("\t");
-                                sb.Append(timeEntryRecord.NetChange);
-                                sb.Append("\t");
-                                sb.Append(timeEntryRecord.HtmlEncodedNoteForExport);
-                                sb.Append("\t");
-                                sb.AppendLine();
-                            }
-                        }
-                    }
+                if (report.Count > 0)
+                {
+                    DataTable header1 = new DataTable();
+                    header1.Columns.Add("Time Entry Audit");
+
+                    header1.Rows.Add(report.Count + " Person(s) Affected");
+                    header1.Rows.Add(HostingPage.RangeForExcel);
+
+                    headerRowsCount = header1.Rows.Count + 3;
+                    var data = PrepareDataTable(report);
+                    coloumnsCount = data.Columns.Count;
+                    sheetStylesList.Add(HeaderSheetStyle);
+                    sheetStylesList.Add(DataSheetStyle);
+                    var dataset = new DataSet();
+                    dataset.DataSetName = "Audit_ByProject";
+                    dataset.Tables.Add(header1);
+                    dataset.Tables.Add(data);
+                    dataSetList.Add(dataset);
                 }
                 else
                 {
-                    sb.Append("There are no Time Entries that were changed afterwards by any Employee for the selected range.");
+                    string dateRangeTitle = "There are no Time Entries that were changed afterwards by any Employee for the selected range.";
+                    DataTable header = new DataTable();
+                    header.Columns.Add(dateRangeTitle);
+                    sheetStylesList.Add(HeaderSheetStyle);
+                    var dataset = new DataSet();
+                    dataset.DataSetName = "Audit_ByProject";
+                    dataset.Tables.Add(header);
+                    dataSetList.Add(dataset);
                 }
                 //“Time_Entry_Audit_[StartOfRange]_[EndOfRange].xls”.  
                 var filename = string.Format("{0}_{1}-{2}.xls", "Time_Entry_Audit", HostingPage.StartDate.Value.ToString("MM.dd.yyyy"), HostingPage.EndDate.Value.ToString("MM.dd.yyyy"));
-                GridViewExportUtil.Export(filename, sb);
+                NPOIExcel.Export(filename, dataSetList, sheetStylesList);
             }
+        }
+
+        public DataTable PrepareDataTable(List<ProjectLevelTimeEntriesHistory> report)
+        {
+            DataTable data = new DataTable();
+            List<object> rownew;
+            List<object> row;
+
+            data.Columns.Add("Employee Id");
+            data.Columns.Add("Person Name");
+            data.Columns.Add("Status");
+            data.Columns.Add("Pay Types");
+            data.Columns.Add("Affected Date");
+            data.Columns.Add("Modified Date");
+            data.Columns.Add("Account Name");
+            data.Columns.Add("Business Unit");
+            data.Columns.Add("Project");
+            data.Columns.Add("Project Name");
+            data.Columns.Add("Phase");
+            data.Columns.Add("Work Type");
+            data.Columns.Add("B/NB");
+            data.Columns.Add("Original");
+            data.Columns.Add("New");
+            data.Columns.Add("New Change");
+            data.Columns.Add("Note");
+            foreach (ProjectLevelTimeEntriesHistory projectLevelTimeEntriesHistory in report)
+            {
+                foreach (PersonLevelTimeEntriesHistory personLevelTimeEntrie in projectLevelTimeEntriesHistory.PersonLevelTimeEntries)
+                {
+                    foreach (TimeEntryRecord timeEntryRecord in personLevelTimeEntrie.TimeEntryRecords)
+                    {
+                        row = new List<object>();
+                        row.Add(personLevelTimeEntrie.Person.EmployeeNumber);
+                        row.Add(personLevelTimeEntrie.Person.HtmlEncodedName);
+                        row.Add(personLevelTimeEntrie.Person.Status.Name);
+                        row.Add(personLevelTimeEntrie.Person.CurrentPay != null ? personLevelTimeEntrie.Person.CurrentPay.TimescaleName : string.Empty);
+                        row.Add(timeEntryRecord.ChargeCodeDate);
+                        row.Add(timeEntryRecord.ModifiedDate);
+                        row.Add(timeEntryRecord.ChargeCode.Client.HtmlEncodedName);
+                        row.Add(timeEntryRecord.ChargeCode.ProjectGroup.HtmlEncodedName);
+                        row.Add(timeEntryRecord.ChargeCode.Project.ProjectNumber);
+                        row.Add(timeEntryRecord.ChargeCode.Project.HtmlEncodedName);
+                        row.Add(timeEntryRecord.ChargeCode.Phase);
+                        row.Add(timeEntryRecord.ChargeCode.TimeType.Name);
+                        row.Add(timeEntryRecord.IsChargeable ? "B" : "NB");
+                        row.Add(timeEntryRecord.OldHours);
+                        row.Add(timeEntryRecord.ActualHours);
+                        row.Add(timeEntryRecord.NetChange);
+                        row.Add(timeEntryRecord.HtmlEncodedNoteForExport);
+                        data.Rows.Add(row.ToArray());
+                    }
+                }
+            }
+            return data;
         }
 
         protected void btnExportToPDF_OnClick(object sender, EventArgs e)
