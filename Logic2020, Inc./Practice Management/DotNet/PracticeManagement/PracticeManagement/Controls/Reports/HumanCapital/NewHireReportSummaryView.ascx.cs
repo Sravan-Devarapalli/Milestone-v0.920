3f9cebd7e67aea81fc41d0c0bbcd63c9ffc08bb5
SainathC <SainathC>
2014-02-18 04:45:00 -0500

    Comments - Defect Number: 3229; Defect RecordID: 3225; Server Address: localhost; Server Port: 99; Database Name: Practice Management; Defect Summary: Person Excel Export > Change file type from a Web Page format to XLS; Defect Description: The export on the Persons Page (Configuration > Persons > Export) downloads in a Web Page (HTML) format rather than Excel. The user is notified of this anytime they wish to save.
    Could you please change the export format to XLS?
    
    Thanks!

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/Controls/Reports/HumanCapital/NewHireReportSummaryView.ascx.cs
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/Controls/Reports/HumanCapital/NewHireReportSummaryView.ascx.cs
@@ -144,6 +144,7 @@ namespace PraticeManagement.Controls.Reports.HumanCapital
         
         protected void btnExportToExcel_OnClick(object sender, EventArgs e)
         {
+            var filename = string.Format("{0}_{1}-{2}.xls", "NewHireReport", HostingPage.StartDate.Value.ToString("MM.dd.yyyy"), HostingPage.EndDate.Value.ToString("MM.dd.yyyy"));
             var btn = sender as Button;
             bool isTitle;
             Boolean.TryParse(btn.Attributes["IsTitle"], out isTitle);
@@ -181,7 +182,7 @@ namespace PraticeManagement.Controls.Reports.HumanCapital
                     sheetStylesList.Add(HeaderSheetStyle);
                     sheetStylesList.Add(DataSheetStyle);
                     var dataset = new DataSet();
-                    dataset.DataSetName = "NewHireReport";
+                    dataset.DataSetName = filename;
                     dataset.Tables.Add(header1);
                     dataset.Tables.Add(data);
                     dataSetList.Add(dataset);
@@ -193,12 +194,11 @@ namespace PraticeManagement.Controls.Reports.HumanCapital
                     header.Columns.Add(dateRangeTitle);
                     sheetStylesList.Add(HeaderSheetStyle);
                     var dataset = new DataSet();
-                    dataset.DataSetName = "NewHireReport";
+                    dataset.DataSetName = filename;
                     dataset.Tables.Add(header);
                     dataSetList.Add(dataset);
                 }
 
-                var filename = string.Format("{0}_{1}-{2}.xls", "NewHireReport", HostingPage.StartDate.Value.ToString("MM.dd.yyyy"), HostingPage.EndDate.Value.ToString("MM.dd.yyyy"));
                 NPOIExcel.Export(filename, dataSetList, sheetStylesList);
                 
             }
