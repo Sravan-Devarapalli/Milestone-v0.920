9669007a59a57254dcb035f5269317cfa66c8c61
SainathC <SainathC>
2014-02-18 04:45:00 -0500

    Comments - Defect Number: 3229; Defect RecordID: 3225; Server Address: localhost; Server Port: 99; Database Name: Practice Management; Defect Summary: Person Excel Export > Change file type from a Web Page format to XLS; Defect Description: The export on the Persons Page (Configuration > Persons > Export) downloads in a Web Page (HTML) format rather than Excel. The user is notified of this anytime they wish to save.
    Could you please change the export format to XLS?
    
    Thanks!

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/Controls/Reports/AuditByProject.ascx.cs
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/Controls/Reports/AuditByProject.ascx.cs
@@ -143,6 +143,8 @@ namespace PraticeManagement.Controls.Reports
 
         protected void btnExportToExcel_OnClick(object sender, EventArgs e)
         {
+            //“Time_Entry_Audit_[StartOfRange]_[EndOfRange].xls”.  
+            var filename = string.Format("{0}_{1}-{2}.xls", "Time_Entry_Audit", HostingPage.StartDate.Value.ToString("MM.dd.yyyy"), HostingPage.EndDate.Value.ToString("MM.dd.yyyy"));
             DataHelper.InsertExportActivityLogMessage(AuditReportExport);
             List<SheetStyles> sheetStylesList = new List<SheetStyles>();
             var dataSetList = new List<DataSet>();
@@ -165,7 +167,7 @@ namespace PraticeManagement.Controls.Reports
                     sheetStylesList.Add(HeaderSheetStyle);
                     sheetStylesList.Add(DataSheetStyle);
                     var dataset = new DataSet();
-                    dataset.DataSetName = "Audit_ByProject";
+                    dataset.DataSetName = filename;
                     dataset.Tables.Add(header1);
                     dataset.Tables.Add(data);
                     dataSetList.Add(dataset);
@@ -177,12 +179,11 @@ namespace PraticeManagement.Controls.Reports
                     header.Columns.Add(dateRangeTitle);
                     sheetStylesList.Add(HeaderSheetStyle);
                     var dataset = new DataSet();
-                    dataset.DataSetName = "Audit_ByProject";
+                    dataset.DataSetName = filename;
                     dataset.Tables.Add(header);
                     dataSetList.Add(dataset);
                 }
-                //“Time_Entry_Audit_[StartOfRange]_[EndOfRange].xls”.  
-                var filename = string.Format("{0}_{1}-{2}.xls", "Time_Entry_Audit", HostingPage.StartDate.Value.ToString("MM.dd.yyyy"), HostingPage.EndDate.Value.ToString("MM.dd.yyyy"));
+                
                 NPOIExcel.Export(filename, dataSetList, sheetStylesList);
             }
         }
