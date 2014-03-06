656682e69ab696957e03eb51218f3d983884b674
SainathC <SainathC>
2014-03-06 02:46:00 -0500

    Comments - Defect Number: 3241; Defect RecordID: 3237; Server Address: localhost; Server Port: 99; Database Name: Practice Management; Defect Summary: New Field on Persons Details: "Cohort Assignment"; Defect Description: We have developed a new career counseling model at Logic20/20, where all consultants are assigned to a group that meets on a regular basis to discuss career development. We call these groups "Cohorts". I would like to add a field in Practice Management to help us track and manage Cohort assignments.
    New Field Creation: "Cohort Assignment" new field on Person Details page:
    Please place the new "Cohort Assignment" field directly to the right of the current "Career Counselor" field
    The current "Set to Practice Area Owner" link should moved to the right of the new "Cohort Assignment" field and renamed "Set Career Counselor to Practice Area Owner"
    The "Cohort Assignment" field is a drop-down menu with six items. At this time, I'm not looking to develop a table to that would edit these fields - any edits would need to be processed via a ticket to GGK. The six items in the drop-down are:
    1. Unassigned (Default value)
    2. Cohort 1
    3. Cohort 2
    4. Cohort 3
    5. Cohort 4
    6. Cohort 5
    Please include changes to the Cohort in the Activity Log under the "Changed Persons" category.
    
    To help us manage the assignment and management of Cohorts, I have a few changes to the automated emails:
    For the current "ACTION REQUIRED: Begin on-boarding process for new hire" email sent to IT, please add HR@logic2020.com as a recipient to the email in addition to the current IT@logic2020.com and update the last paragraph of the current email to the following:
    
    CURRENT VERBIAGE: "Please create an Office 365 account and set up the individuals laptop. The email account must be created by 11:59 pm the night before the individuals start date in order for Practice Management to successfully send the individuals log-in credentials."
    NEW VERBIAGE:
    "Actions Required:
    HR - Please update the individual's Career Counselor and Cohort Assignment fields as appropriate as soon as possbile.
    IT - Please create an Office 365 account and set up the individuals laptop. The email account must be created by 11:59 pm the night before the individuals start date in order for Practice Management to successfully send the individuals log-in credentials."
    
    Upon the "Cohort Assignment" field in Practice Management changing (example - from default "Unassigned" to "Cohort 1," or from "Cohort 1" to "Cohort 4", the system sends an automatic notification to IT@logic2020.com with the following verbiage:
    SUBJECT: ACTION REQUIRED: Change in Cohort Assignment
    BODY TEXT: "This is a message to alert you that <PERSON NAME>'s Cohort Assignment has been changed from <OLD COHORT ASSIGNMENT> to <NEW COHORT ASSIGNMENT>. Please update the appropriate Cohort email distribution lists as soon as possible. Please reach out to HR@logic2020.com with any questions.
    
    Please also create a new stored procedure to help us track Career Counselor and Cohort Assignments. The stored procedure should have the same variable date range as other stored procedures. Included in the report should be only individuals with a Pay Type of W2-Salary. The fields should include:
    Employee ID
    Person Name (Last Name, First Name)
    Status
    Title
    Hire Date
    Termination Date
    Career Counselor
    Cohort Assignment

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/PersonDetail.aspx.designer.cs
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/PersonDetail.aspx.designer.cs
@@ -166,6 +166,15 @@ namespace PraticeManagement {
         protected global::PraticeManagement.Controls.Configuration.DefaultUser defaultManager;
         
         /// <summary>
+        /// ddlCohortAssignment control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList ddlCohortAssignment;
+        
+        /// <summary>
         /// lbSetPracticeOwner control.
         /// </summary>
         /// <remarks>
