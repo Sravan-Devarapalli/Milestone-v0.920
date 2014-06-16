e44c0e196d928a0b3d42dc6d4b97a65300a8ba6c
SainathC <SainathC>
2014-06-16 04:20:00 -0500

    Comments - Defect Number: 3266; Defect RecordID: 3262; Server Address: localhost; Server Port: 99; Database Name: Practice Management; Defect Summary: Add icon to delete Milestone within project; Defect Description: Users currently have the ability to delete a milestone by clicking within a Milestone into Milestone Details and clicking the "Delete Milestone" button. Can we please add a new "X" icon next to Edit ("Pencil" icon) on the Project Details page on the Milestone tab to make the deletion process a bit easier for users. The "X" icon would have the same functionality as the current "Delete Milestone" button. Mock-up is below.
    Please let me know what questions you have!

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/Controls/Projects/ProjectMilestonesFinancials.ascx.cs
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/Controls/Projects/ProjectMilestonesFinancials.ascx.cs
@@ -5,6 +5,11 @@ using System.Web.UI.WebControls;
 using DataTransferObjects;
 using PraticeManagement.Security;
 using PraticeManagement.Utils;
+using PraticeManagement.MilestoneService;
+using System.ServiceModel;
+using System.Collections.Generic;
+using PraticeManagement.ProjectService;
+using System.Linq;
 
 namespace PraticeManagement.Controls.Projects
 {
@@ -39,9 +44,45 @@ namespace PraticeManagement.Controls.Projects
 
         #region Properties
 
+        public bool IsAttributionPanelDisplayed
+        {
+            get
+            {
+                if (ViewState["IsAttributionPanelDisplayed_Key"] == null)
+                {
+                    ViewState["IsAttributionPanelDisplayed_Key"] = false;
+                }
+
+                return (bool)ViewState["IsAttributionPanelDisplayed_Key"];
+            }
+            set
+            {
+                ViewState["IsAttributionPanelDisplayed_Key"] = value;
+            }
+        }
+
         private PraticeManagement.ProjectDetail HostingPage
         {
             get { return ((PraticeManagement.ProjectDetail)Page); }
+        }
+
+        public List<int> MilestoneCSATAttributionCount
+        {
+            get
+            {
+                using (var serviceClient = new MilestoneServiceClient())
+                {
+                    try
+                    {
+                        return serviceClient.GetMilestoneAndCSATCountsByProject(HostingPage.ProjectId.Value).ToList();
+                    }
+                    catch (FaultException<ExceptionDetail>)
+                    {
+                        serviceClient.Abort();
+                        throw;
+                    }
+                }
+            }
         }
 
         protected int? ProjectId
@@ -84,6 +125,22 @@ namespace PraticeManagement.Controls.Projects
             }
         }
 
+        public int MilestoneId
+        {
+            get
+            {
+                if(ViewState["MilestoneId_Key"] == null)
+                {
+                    ViewState["MilestoneId_Key"] = -1;
+                }
+                return (int)ViewState["MilestoneId_Key"];
+            }
+            set
+            {
+                ViewState["MilestoneId_Key"] = value;
+            }
+        }
+
         public string ValidationGroup { get; set; }
 
         #endregion Properties
@@ -121,6 +178,238 @@ namespace PraticeManagement.Controls.Projects
             }
         }
 
+        protected void cvAttributionPopup_ServerValidate(object sender, ServerValidateEventArgs e)
+        {
+            e.IsValid = true;
+            if (IsAttributionPanelDisplayed)
+                return;
+            var milestone = GetMilestoneById(MilestoneId);
+            List<Attribution> attributionList = new List<Attribution>();
+            List<bool> extendAttributionDates = new List<bool>();
+            using (var service = new MilestoneServiceClient())
+            {
+                if (milestone != null)
+                {
+                    attributionList =
+                        service.IsProjectAttributionConflictsWithMilestoneChanges(MilestoneId,
+                                                                                  milestone.StartDate,
+                                                                                  milestone.EndDate,
+                                                                                  hdnIsUpdate.Value == true.ToString()).ToList();
+
+                }
+            }
+            if (attributionList.Any())
+            {
+                trAttributionRecord.Visible = true;
+                IsAttributionPanelDisplayed = true;
+                mpeAttribution.Show();
+                HostingPage.IsOtherPanelDisplay = true;
+                if (attributionList.Any(x => x.AttributionType == AttributionTypes.Delivery))
+                {
+                    repDeliveryPersons.DataSource =
+                        attributionList.Where(x => x.AttributionType == AttributionTypes.Delivery)
+                                       .OrderBy(x => x.TargetName)
+                                       .ThenBy(x => x.StartDate);
+                    repDeliveryPersons.DataBind();
+                }
+                if (attributionList.Any(x => x.AttributionType == AttributionTypes.Sales))
+                {
+                    repSalesPersons.DataSource =
+                        attributionList.Where(x => x.AttributionType == AttributionTypes.Sales)
+                                       .OrderBy(x => x.TargetName)
+                                       .ThenBy(x => x.StartDate);
+                    repSalesPersons.DataBind();
+                }
+                e.IsValid = false;
+            }
+            else
+                trAttributionRecord.Visible = false;
+
+            if (hdnIsUpdate.Value != true.ToString())
+            {
+                trCommissionsStartDateExtend.Visible = false;
+                trCommissionsEndDateExtend.Visible = false;
+            }
+        }
+
+        public void MilestoneValidate(GridViewRow row)
+        {
+            CustomValidator custExpenseValidate = row.FindControl("custExpenseValidate") as CustomValidator;
+            CustomValidator custProjectStatus = row.FindControl("custProjectStatus") as CustomValidator;
+            CustomValidator custCSATValidate = row.FindControl("custCSATValidate") as CustomValidator;
+            CustomValidator custAttribution = row.FindControl("custAttribution") as CustomValidator;
+            CustomValidator custFeedback = row.FindControl("custFeedback") as CustomValidator;
+            custExpenseValidate.Validate();
+            custProjectStatus.Validate();
+            custCSATValidate.Validate();
+            custAttribution.Validate();
+            custFeedback.Validate();
+        }
+
+        protected void btnOkAttribution_Click(object sender, EventArgs e)
+        {
+            if (hdnIsUpdate.Value == false.ToString())
+                CallAppropriateDelete();
+        }
+
+        public void CallAppropriateDelete()
+        {
+            foreach(GridViewRow row in gvRevenueMilestones.Rows)
+            {
+                var imgDeleteBtn = row.FindControl("imgMilestoneDelete") as ImageButton;
+                int milestoneId;
+                int.TryParse(imgDeleteBtn.Attributes["MilestoneId"], out milestoneId);
+                if(milestoneId == MilestoneId)
+                {
+                    imgMilestoneDelete_Click(imgDeleteBtn,new EventArgs());
+                    break;
+                }
+            }
+        }
+
+        protected void btnCancelAttribution_Click(object sender, EventArgs e)
+        {
+            IsAttributionPanelDisplayed = false;
+            mpeAttribution.Hide();
+        }
+
+        protected void imgMilestoneDelete_Click(object sender, EventArgs e)
+        {
+            ImageButton imgDeletebtn = sender as ImageButton;
+            GridViewRow row = imgDeletebtn.NamingContainer as GridViewRow;
+            ImageButton btnDelete = row.FindControl("imgMilestoneDelete") as ImageButton;
+            int milestoneId;
+            int.TryParse(btnDelete.Attributes["MilestoneId"], out milestoneId);
+            MilestoneId = milestoneId;
+            MilestoneValidate(row);
+            if (Page.IsValid)
+            {
+                hdnIsUpdate.Value = false.ToString();
+                Page.Validate("AttributionPopup");
+            }
+            else
+            {
+                mpeErrorPanel.Show();
+                HostingPage.IsOtherPanelDisplay = true;
+            }
+            if (Page.IsValid)
+            {
+                try
+                {
+                    DeleteRecord(milestoneId);
+                    IsAttributionPanelDisplayed = false;
+                    Response.Redirect(HostingPage.Page.Request.RawUrl);
+                }
+                catch (Exception exception)
+                {
+                    lblError.ShowErrorMessage("{0}", exception.Message);
+                }
+            }
+            else
+            {
+                HostingPage.IsOtherPanelDisplay = true;
+            }
+        }
+
+        private void DeleteRecord(int milestoneId)
+        {
+            var milestone = new Milestone { Id = (int?)milestoneId };
+
+            using (var serviceClient = new MilestoneServiceClient())
+            {
+                try
+                {
+                    serviceClient.DeleteMilestone(milestone, HostingPage.User.Identity.Name);
+                }
+                catch (FaultException<ExceptionDetail>)
+                {
+                    serviceClient.Abort();
+                    throw;
+                }
+            }
+        }
+
+        public Milestone GetMilestoneById(int id)
+        {
+            using (var serviceClient = new MilestoneServiceClient())
+            {
+                try
+                {
+                    return serviceClient.GetMilestoneDetail(id);
+                }
+                catch (FaultException<ExceptionDetail>)
+                {
+                    serviceClient.Abort();
+                    throw;
+                }
+            }
+        }
+
+        protected void custExpenseValidate_OnServerValidate(object sender, ServerValidateEventArgs e)
+        {
+            CustomValidator custExpense = sender as CustomValidator;
+            GridViewRow row = custExpense.NamingContainer as GridViewRow;
+            ImageButton btnDelete = row.FindControl("imgMilestoneDelete") as ImageButton;
+            int milestoneId;
+            int.TryParse(btnDelete.Attributes["MilestoneId"], out milestoneId);
+            var milestone = GetMilestoneById(milestoneId);
+            if (milestone.Project.StartDate.Value == milestone.StartDate ||
+                milestone.Project.EndDate.Value == milestone.EndDate)
+            {
+                using (var service = new MilestoneServiceClient())
+                {
+                    if (service.CheckIfExpensesExistsForMilestonePeriod(milestoneId, null, null))
+                    {
+                        e.IsValid = false;
+                    }
+                    else
+                    {
+                        e.IsValid = true;
+                    }
+                }
+            }
+            else
+            {
+                e.IsValid = true;
+            }
+        }
+
+        protected void custProjectStatus_OnServerValidate(object sender, ServerValidateEventArgs e)
+        {
+            e.IsValid = (HostingPage.Project.Status.StatusType == ProjectStatusType.Active && MilestoneCSATAttributionCount[0] == 1) ? false : true;
+        }
+
+        protected void custCSATValidate_OnServerValidate(object sender, ServerValidateEventArgs e)
+        {
+            e.IsValid = (MilestoneCSATAttributionCount[0] != 1 || MilestoneCSATAttributionCount[1] <= 0);
+        }
+
+        protected void custAttribution_OnServerValidate(object sender, ServerValidateEventArgs e)
+        {
+            e.IsValid = (MilestoneCSATAttributionCount[0] != 1 || MilestoneCSATAttributionCount[2] <= 0);
+        }
+
+        protected void custFeedback_OnServerValidate(object sender, ServerValidateEventArgs e)
+        {
+            e.IsValid = true;
+            CustomValidator custFeedback = sender as CustomValidator;
+            GridViewRow row = custFeedback.NamingContainer as GridViewRow;
+            ImageButton btnDelete = row.FindControl("imgMilestoneDelete") as ImageButton;
+            int milestoneId;
+            int.TryParse(btnDelete.Attributes["MilestoneId"], out milestoneId);
+            using (var serviceClient = new ProjectServiceClient())
+            {
+                try
+                {
+                    e.IsValid = !serviceClient.CheckIfFeedbackExists(null, milestoneId, null);
+                }
+                catch (CommunicationException)
+                {
+                    serviceClient.Abort();
+                    throw;
+                }
+            }
+        }
         protected void imgbtnCancel_OnClick(object sender, EventArgs e)
         {
             gvRevenueMilestones.EditIndex = -1;
