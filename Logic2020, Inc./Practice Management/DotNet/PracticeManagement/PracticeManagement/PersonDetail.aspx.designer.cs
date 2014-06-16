b1ab4103216ad5c087e159dd3d3bc5beba97e9a3
SainathC <SainathC>
2014-06-16 04:13:00 -0500

    Comments - Defect Number: 3272; Defect RecordID: 3268; Server Address: localhost; Server Port: 99; Database Name: Practice Management; Defect Summary: Person Details > "ADP ID" field: Add ability to include letters; Defect Description: I have a couple small requests related to ticket #3264 that we just closed: We just learned that the field needs to allow the capture of letters as well as numbers. Could you please remove the current field limitation that limits users to the input of numbers only?
    Would you also delete all previous data entered in that field for all Active employees?
    
    Feel free to push to Production at your convenience.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/PersonDetail.aspx.designer.cs
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/PersonDetail.aspx.designer.cs
@@ -472,15 +472,6 @@ namespace PraticeManagement {
         protected global::System.Web.UI.WebControls.TextBox txtPayCheckId;
         
         /// <summary>
-        /// ftePayCheckId control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::AjaxControlToolkit.FilteredTextBoxExtender ftePayCheckId;
-        
-        /// <summary>
         /// ddlPersonType control.
         /// </summary>
         /// <remarks>
