a6987f96fe2fd42ba66a342d1b9a6b452b7cdde0
SainathC <SainathC>
2014-06-30 02:05:00 -0500

    Comments - Defect Number: 3257; Defect RecordID: 3253; Server Address: localhost; Server Port: 99; Database Name: Practice Management; Defect Summary: Analysis and Recommendations Project - Page Load Optimization; Defect Description: One of the key pieces of feedback I hear from Practice Management time and again is how slow the system is to take load pages. The concerns are across the entire system, but the specific pain point for the Consulting organization is the length of time it takes to tab between tabs in Project Details and the length of time it takes to load and add resource details (i.e., adding a new resource to Milestone Details, changing start and end date, etc.)
    I understand that we are limited by the system's inherent architecture in many ways, but I would like to get your help in analyzing how we could improve page load and field load times and make the system faster overall. I'm hoping that there are some options available to us to alleviate some of the frustration while not having to get to the point of a complete rearchitecture of the system!
    
    My high-level thinking would be that your analysis would be delivered in three parts:
    Analysis of the areas of the system that are slowest to respond and the primary reason for that slowness (I can provide more anecdotal evidence if you'd like!)
    Recommended short-, medium-, and long-term fixes, and
    A level of difficulty and level of effort it would take to complete each recommendation.
    
    From this analysis, I'll review with leadership and prioritize from there.
    
    Do let me know your thoughts if this would be a valuable exploration!

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Functions/GetWeeklyUtilizationTable.function.sql
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Functions/GetWeeklyUtilizationTable.function.sql
@@ -54,7 +54,8 @@ AS
 	FROM dbo.Calendar c
 	WHERE c.date BETWEEN @StartDate and @EndDate
 	AND @Step = 1
-),
+)
+,
 CurrentConsultantsWithRanges
 AS
 (
@@ -65,19 +66,39 @@ AS
 			RANK() OVER (PARTITION BY P.PersonId,r.StartDate,r.EndDate  ORDER BY pay.startdate DESC) LatestPayRank
     FROM     Ranges r
 	CROSS JOIN dbo.Person AS p
-	INNER JOIN dbo.Timescale T ON T.TimescaleId IN ( SELECT ResultId FROM [dbo].[ConvertStringListIntoTable](@TimescaleIds))
-	INNER JOIN dbo.[GetLatestPayWithInTheGivenRange](@StartDate,@EndDate) AS PCPT ON PCPT.PersonId = p.PersonId AND T.TimescaleId = PCPT.Timescale  
+	INNER JOIN dbo.[GetLatestPayWithInTheGivenRange](@StartDate,@EndDate) AS PCPT ON PCPT.PersonId = p.PersonId AND EXISTS (SELECT ResultId FROM [dbo].[ConvertStringListIntoTable](@TimescaleIds) WHERE ResultId = PCPT.Timescale)
 	LEFT JOIN dbo.Pay AS pay ON pay.Person = p.PersonId AND r.EndDate >= pay.StartDate AND r.StartDate <= (pay.EndDate-1)
     LEFT JOIN dbo.Practice AS pr ON p.DefaultPractice = pr.PracticeId
-    WHERE   (p.IsStrawman = 0)
+    WHERE   (					
+					EXISTS (SELECT ResultId FROM [dbo].[ConvertStringListIntoTable](@PracticeIds) WHERE p.DefaultPractice = ResultId)
+					AND (pr.IsCompanyInternal = 0 AND @ExcludeInternalPractices  = 1 OR @ExcludeInternalPractices = 0)
+				)
             AND ( (@ActivePersons = 1 AND p.PersonStatusId IN (1,5)) -- active and termination pending statues
 					OR
                     (@ProjectedPersons = 1 AND p.PersonStatusId = 3) -- projected status
 				)
-			AND (					
-					p.DefaultPractice IN (SELECT ResultId FROM [dbo].[ConvertStringListIntoTable](@PracticeIds))
-					AND (pr.IsCompanyInternal = 0 AND @ExcludeInternalPractices  = 1 OR @ExcludeInternalPractices = 0)
-				)
+			AND (p.IsStrawman = 0)
+),
+CurrentConsultantsWithRanges2
+AS
+(
+SELECT PersonId,StartDate,EndDate,Timescale
+FROM CurrentConsultantsWithRanges
+WHERE LatestPayRank = 1
+),
+CurrentConsultantsWithProjectedHours
+AS
+(
+ SELECT  S.PersonId,CONVERT(DECIMAL(10,2),SUM(S.HoursPerDay)) ProjectedHours,CCWV.StartDate ,CCWV.Timescale
+    FROM CurrentConsultantsWithRanges2 CCWV
+	INNER JOIN dbo.v_MilestonePersonSchedule AS S ON CCWV.PersonId = S.PersonId
+    WHERE  s.MilestoneId <> (SELECT MilestoneId FROM dbo.DefaultMilestoneSetting)
+			AND s.Date BETWEEN CCWV.startDate AND CCWV.endDate AND 
+			(@ActiveProjects = 1 AND s.ProjectStatusId = 3 OR		--  3 - Active
+			 @ProjectedProjects = 1 AND s.ProjectStatusId = 2 OR	--  2 - Projected
+			 @ExperimentalProjects = 1 AND s.ProjectStatusId = 5 OR	--  5 - Experimental
+			 @InternalProjects = 1 AND s.ProjectStatusId = 6) --6 - Internal
+	GROUP BY s.PersonId ,CCWV.StartDate ,CCWV.Timescale
 ),
 CurrentConsultantsWithVacationDays
 AS
@@ -86,12 +107,10 @@ AS
 			CCWR.StartDate,
 			CCWR.EndDate,
 			CCWR.Timescale, 
-			ISNULL(COUNT(PC.Date),0) AS VacationDays,
-			CASE WHEN CCWR.Timescale = 2 THEN ISNULL(COUNT(PC.Date),0) ELSE ISNULL(SUM(CASE WHEN PC.CompanyDayOff = 1 AND pc.DayOff = 1 AND (DATEPART(DW,PC.Date) <> 1 AND DATEPART(DW,PC.Date) <> 7) THEN 0 ELSE (case when PC.Date is not null then 1 else 0 end) END),0) END AS RedefinedVacationDays,
-			ISNULL(SUM(CASE WHEN PC.CompanyDayOff = 0 THEN 1 ELSE 0 END),0) AS VacationDaysExcludingCompanyDayOff,
+				CASE WHEN CCWR.Timescale = 2 THEN ISNULL(COUNT(PC.Date),0) ELSE ISNULL(SUM(CASE WHEN PC.CompanyDayOff = 1 AND pc.DayOff = 1 AND (DATEPART(DW,PC.Date) <> 1 AND DATEPART(DW,PC.Date) <> 7) THEN 0 ELSE (case when PC.Date is not null then 1 else 0 end) END),0) END AS RedefinedVacationDays,
 			ISNULL(SUM(CASE WHEN DATEPART(DW,PC.Date) <> 1 AND DATEPART(DW,PC.Date) <> 7 THEN 1 ELSE 0 END),0) AS VacationDaysIncludingCompanyDayOffsNotWeekends
-	FROM CurrentConsultantsWithRanges CCWR
-	LEFT JOIN dbo.PersonCalendarAuto PC ON PC.PersonId = CCWR.PersonId 
+	FROM CurrentConsultantsWithRanges2 CCWR
+	INNER JOIN dbo.PersonCalendarAuto PC ON PC.PersonId = CCWR.PersonId 
 										AND	PC.Date BETWEEN CCWR.StartDate AND CCWR.EndDate 
 										AND (
 												(
@@ -102,29 +121,15 @@ AS
 													PC.DayOff = 1 AND PC.TimeOffHours = 8--if a person has added Timeoff  for complete 8 hr then the day is treated as vacation day.
 												) 
 											)
-	WHERE CCWR.LatestPayRank = 1
 	GROUP BY CCWR.PersonId,CCWR.StartDate,CCWR.EndDate,CCWR.Timescale
-),
-CurrentConsultantsWithProjectedHours
-AS
-(
- SELECT  S.PersonId,CONVERT(DECIMAL(10,2),SUM(S.HoursPerDay)) ProjectedHours,CCWV.StartDate ,CCWV.Timescale
-    FROM CurrentConsultantsWithVacationDays CCWV
-	INNER JOIN dbo.v_MilestonePersonSchedule AS S ON CCWV.PersonId = S.PersonId
-    WHERE  s.MilestoneId <> (SELECT MilestoneId FROM dbo.DefaultMilestoneSetting)
-			AND s.Date BETWEEN CCWV.startDate AND CCWV.endDate AND 
-			(@ActiveProjects = 1 AND s.ProjectStatusId = 3 OR		--  3 - Active
-			 @ProjectedProjects = 1 AND s.ProjectStatusId = 2 OR	--  2 - Projected
-			 @ExperimentalProjects = 1 AND s.ProjectStatusId = 5 OR	--  5 - Experimental
-			 @InternalProjects = 1 AND s.ProjectStatusId = 6) --6 - Internal
-	GROUP BY s.PersonId ,CCWV.StartDate ,CCWV.Timescale
-),
+)
+,
 CurrentConsultantsWithAvailableHours
 AS
 (
 --Salary Persons AvailableHours
 	SELECT PC.PersonId ,CAST(SUM(8 - ISNULL(PC.TimeOffHours,0)) AS DECIMAL(10,2)) AS AvailableHours ,CCWV.StartDate 
-	FROM CurrentConsultantsWithVacationDays CCWV
+	FROM CurrentConsultantsWithRanges2 CCWV
 	INNER JOIN dbo.PersonCalendarAuto PC ON CCWV.PersonId = PC.PersonId 
 										AND CCWV.Timescale = 2 
 										AND PC.Date BETWEEN CCWV.StartDate AND CCWV.EndDate
@@ -136,8 +141,9 @@ AS
 CurrentConsultantsWithAllHours
 AS
 (
-	SELECT CCWV.PersonId,CCWV.StartDate,CCWV.EndDate,CCWV.RedefinedVacationDays,CCWV.VacationDaysIncludingCompanyDayOffsNotWeekends,ISNULL(CCWA.AvailableHours,0) AS AvailableHours,ISNULL(CCWP.ProjectedHours,0) AS ProjectedHours,CCWV.Timescale
-	FROM CurrentConsultantsWithVacationDays CCWV
+	SELECT CR.PersonId,CR.StartDate,CR.EndDate,CCWV.RedefinedVacationDays,CCWV.VacationDaysIncludingCompanyDayOffsNotWeekends,ISNULL(CCWA.AvailableHours,0) AS AvailableHours,ISNULL(CCWP.ProjectedHours,0) AS ProjectedHours,CR.Timescale
+	FROM CurrentConsultantsWithRanges2 CR
+	LEFT JOIN CurrentConsultantsWithVacationDays CCWV ON CCWV.PersonId = CR.PersonId AND CCWV.StartDate = CR.StartDate
 	LEFT JOIN CurrentConsultantsWithAvailableHours CCWA ON CCWV.PersonId = CCWA.PersonId AND CCWV.StartDate = CCWA.StartDate
 	LEFT JOIN CurrentConsultantsWithProjectedHours CCWP ON CCWV.PersonId = CCWP.PersonId AND CCWV.StartDate = CCWP.StartDate
 )
@@ -147,7 +153,7 @@ SELECT	CC.PersonId,
 		CC.AvailableHours,
 		CC.ProjectedHours,
 		CC.Timescale,
-		CC.VacationDaysIncludingCompanyDayOffsNotWeekends AS VacationDays,
+		ISNULL(CC.VacationDaysIncludingCompanyDayOffsNotWeekends,0) AS VacationDays,
 		CONVERT(INT,CASE	WHEN CC.RedefinedVacationDays >= @Step
 				THEN -1
 				WHEN CC.AvailableHours = 0
@@ -155,5 +161,4 @@ SELECT	CC.PersonId,
 				ELSE CEILING(100*CC.ProjectedHours/CC.AvailableHours) 
 				END) AS WeeklyUtlization
 FROM CurrentConsultantsWithAllHours CC
-
 
