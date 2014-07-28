82ce7278514fb39112f639a1aeb56b9fb76903bb
SainathC <SainathC>
2014-07-28 04:28:00 -0500

    Comments - Defect Number: 3277; Defect RecordID: 3273; Server Address: localhost; Server Port: 99; Database Name: Practice Management; Defect Summary: Add a new project status type called 'Proposed'; Defect Description: Currently, there are four project types Active, Projected, Internal, Experimental.
    We would like a fifth project type added called Proposed".
    
    Please investigate on dependencies for this and inform Ellen on them.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/Controls/Reports/UTilTimelineFilter.ascx.designer.cs
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/Controls/Reports/UTilTimelineFilter.ascx.designer.cs
@@ -346,6 +346,15 @@ namespace PraticeManagement.Controls.Reports {
         protected global::System.Web.UI.WebControls.CheckBox chbInternalProjects;
         
         /// <summary>
+        /// chbProposedProjects control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.CheckBox chbProposedProjects;
+        
+        /// <summary>
         /// cblPractices control.
         /// </summary>
         /// <remarks>
