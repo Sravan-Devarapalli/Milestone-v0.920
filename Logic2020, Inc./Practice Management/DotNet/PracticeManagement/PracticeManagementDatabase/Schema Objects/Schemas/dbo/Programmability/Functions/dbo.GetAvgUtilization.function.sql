b6b958aaa56539a04c998798768672ba5fee5198
SainathC <SainathC>
2014-07-28 04:28:00 -0500

    Comments - Defect Number: 3277; Defect RecordID: 3273; Server Address: localhost; Server Port: 99; Database Name: Practice Management; Defect Summary: Add a new project status type called 'Proposed'; Defect Description: Currently, there are four project types Active, Projected, Internal, Experimental.
    We would like a fifth project type added called Proposed".
    
    Please investigate on dependencies for this and inform Ellen on them.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Functions/dbo.GetAvgUtilization.function.sql
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Functions/dbo.GetAvgUtilization.function.sql
@@ -6,6 +6,7 @@
 	@ActiveProjects BIT = 1,
 	@ProjectedProjects BIT = 1,
 	@ExperimentalProjects BIT = 1,
+	@ProposedProjects BIT=1,
 	@InternalProjects	BIT = 1  
 )
 RETURNS INT
@@ -22,13 +23,13 @@ BEGIN
     -- Iterate through all days
 
 		BEGIN
-			SET @av = dbo.GetNumberAvaliableHours(@PersonId, @StartDate, @EndRange, @ActiveProjects, @ProjectedProjects, @ExperimentalProjects ,@InternalProjects)
+			SET @av = dbo.GetNumberAvaliableHours(@PersonId, @StartDate, @EndRange, @ActiveProjects, @ProjectedProjects, @ExperimentalProjects ,@ProposedProjects,@InternalProjects)
 			
 			IF (@av = 0 OR @av IS NULL)
 				SET @wUtil = 0
 			ELSE 		
 				SET @wUtil = CEILING(
-					100*ISNULL(dbo.GetNumberProjectedHours(@PersonId, @StartDate, @EndRange, @ActiveProjects, @ProjectedProjects, @ExperimentalProjects, @InternalProjects), 0) / 
+					100*ISNULL(dbo.GetNumberProjectedHours(@PersonId, @StartDate, @EndRange, @ActiveProjects, @ProjectedProjects, @ExperimentalProjects,@ProposedProjects, @InternalProjects), 0) / 
 						@av)
 		END 
  
