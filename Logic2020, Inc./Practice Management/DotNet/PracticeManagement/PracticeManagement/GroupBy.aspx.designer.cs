bf61c3a6075dcf078e9b8cde8c67d7f616908ed9
SainathC <SainathC>
2014-07-28 04:28:00 -0500

    Comments - Defect Number: 3277; Defect RecordID: 3273; Server Address: localhost; Server Port: 99; Database Name: Practice Management; Defect Summary: Add a new project status type called 'Proposed'; Defect Description: Currently, there are four project types Active, Projected, Internal, Experimental.
    We would like a fifth project type added called Proposed".
    
    Please investigate on dependencies for this and inform Ellen on them.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/GroupBy.aspx.designer.cs
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/GroupBy.aspx.designer.cs
@@ -193,6 +193,15 @@ namespace PraticeManagement {
         protected global::System.Web.UI.WebControls.CheckBox chbInactive;
         
         /// <summary>
+        /// chbProposed control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.CheckBox chbProposed;
+        
+        /// <summary>
         /// chbProjected control.
         /// </summary>
         /// <remarks>
