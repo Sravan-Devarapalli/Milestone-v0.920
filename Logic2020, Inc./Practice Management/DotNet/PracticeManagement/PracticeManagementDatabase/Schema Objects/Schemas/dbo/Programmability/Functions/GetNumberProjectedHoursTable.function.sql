f64dc7d4954f861668fff2de9caa7e3d534b4e70
SainathC <SainathC>
2014-07-28 04:28:00 -0500

    Comments - Defect Number: 3277; Defect RecordID: 3273; Server Address: localhost; Server Port: 99; Database Name: Practice Management; Defect Summary: Add a new project status type called 'Proposed'; Defect Description: Currently, there are four project types Active, Projected, Internal, Experimental.
    We would like a fifth project type added called Proposed".
    
    Please investigate on dependencies for this and inform Ellen on them.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Functions/GetNumberProjectedHoursTable.function.sql
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Functions/GetNumberProjectedHoursTable.function.sql
@@ -5,6 +5,7 @@
    	  @ActiveProjects BIT = 1,
 	  @ProjectedProjects BIT = 1,
 	  @ExperimentalProjects BIT = 1,
+	  @ProposedProjects BIT =1,
 	  @InternalProjects	BIT = 1
 )
 RETURNS TABLE 
@@ -19,6 +20,7 @@ RETURN
 			(@ActiveProjects = 1 AND pr.ProjectStatusId = 3 OR		--  3 - Active
 			 @ProjectedProjects = 1 AND pr.ProjectStatusId = 2 OR	--  2 - Projected
 			 @ExperimentalProjects = 1 AND pr.ProjectStatusId = 5 OR	--  5 - Experimental
+			 @ProposedProjects =1 AND pr.ProjectStatusId = 7 OR --7-proposed
 			 @InternalProjects = 1 AND pr.ProjectStatusId = 6) --6 - Internal
 	GROUP BY s.PersonId 
 
