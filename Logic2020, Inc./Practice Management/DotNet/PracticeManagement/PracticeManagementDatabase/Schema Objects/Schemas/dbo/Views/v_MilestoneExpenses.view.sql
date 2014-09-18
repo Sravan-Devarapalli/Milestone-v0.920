289e0d479772d0a36062d1157c20697c73516059
SainathC <SainathC>
2014-09-18 01:44:00 -0500

    Comments - Defect Number: 3281; Defect RecordID: 3277; Server Address: localhost; Server Port: 99; Database Name: Practice Management; Defect Summary: Add Expenses functionality in milestone detail page.; Defect Description: It appears that you can only add an expense at the Project level. The Add Expense button is not available when youre in a specific milestone. We would like to have that functionality at all levels of the project.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Views/v_MilestoneExpenses.view.sql
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Views/v_MilestoneExpenses.view.sql
@@ -1,23 +1,8 @@
-﻿CREATE view dbo.v_MilestoneExpenses
-as
- With CTEMileStonesPerDay
+﻿CREATE VIEW dbo.v_MilestoneExpenses
 AS
-	(SELECT  
-		pexp.ProjectId,
-	    c.Date,
-	    COUNT(DISTINCT MilestoneId) MileStonesPerDay 
-	from dbo.ProjectExpense as pexp
-	JOIN dbo.Calendar c ON c.Date BETWEEN pexp.StartDate AND pexp.EndDate
-	JOIN dbo.Milestone M ON M.ProjectId = pexp.ProjectId AND c.Date BETWEEN M.StartDate AND M.ProjectedDeliveryDate
-	GROUP BY c.Date,pexp.ProjectId
-	)
-SELECT M.MilestoneId,
-	  M.ProjectId,
-	  CONVERT(DECIMAL(18,2),SUM(pexp.Amount/ ((DATEDIFF(dd,pexp.StartDate,pexp.EndDate)+1)*MileStonesPerDay))) Expense,
-	  CONVERT(DECIMAL(18,2),SUM(pexp.Reimbursement*0.01*pexp.Amount / ((DATEDIFF(dd,pexp.StartDate,pexp.EndDate)+1)*MileStonesPerDay))) ReimbursedExpense 
-FROM CTEMileStonesPerDay CTE
-JOIN dbo.ProjectExpense as pexp 
-ON CTE.ProjectId = pexp.ProjectId AND CTE.Date BETWEEN pexp.StartDate AND pexp.EndDate
-JOIN dbo.Milestone M ON M.ProjectId = pexp.ProjectId AND CTE.Date BETWEEN M.StartDate AND M.ProjectedDeliveryDate
-GROUP BY M.MilestoneId,M.ProjectId
- 
+ 	SELECT MilestoneId
+		  ,ProjectId
+		  ,Amount as Expense
+		  ,[Reimbursement] * 0.01 * [Amount] as ReimbursedExpense
+	FROM  dbo.ProjectExpense
+
