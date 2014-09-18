3c198c65689f9f4c7a6b71cb3a4c9968422d92ba
SainathC <SainathC>
2014-09-18 01:44:00 -0500

    Comments - Defect Number: 3281; Defect RecordID: 3277; Server Address: localhost; Server Port: 99; Database Name: Practice Management; Defect Summary: Add Expenses functionality in milestone detail page.; Defect Description: It appears that you can only add an expense at the Project level. The Add Expense button is not available when youre in a specific milestone. We would like to have that functionality at all levels of the project.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/ProjectExpenseGetAllForProject.proc.sql
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/ProjectExpenseGetAllForProject.proc.sql
@@ -1,13 +1,18 @@
 ﻿CREATE PROCEDURE [dbo].[ProjectExpenseGetAllForProject]
 	@ProjectId INT
 AS
-	SELECT Id ExpenseId,
-			Name ExpenseName,
-			ProjectId,
-			StartDate,
-			EndDate,
-			[Amount] ExpenseAmount,
-			[Reimbursement] ExpenseReimbursement
-	FROM dbo.ProjectExpense
-	WHERE ProjectId = @ProjectId
-	ORDER BY StartDate
+BEGIN
+	SELECT PE.Id ExpenseId,
+			PE.Name ExpenseName,
+			PE.ProjectId,
+			PE.StartDate,
+			PE.EndDate,
+			PE.Amount ExpenseAmount,
+			PE.Reimbursement ExpenseReimbursement,
+			M.Description AS MilestoneName
+	FROM dbo.ProjectExpense PE
+	LEFT JOIN dbo.Milestone M ON M.MilestoneId = PE.MilestoneId
+	WHERE PE.ProjectId = @ProjectId
+	ORDER BY PE.StartDate
+END
+
