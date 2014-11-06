d6dbfba6219d4d13b1343df2a4a0c179c3b898d3
SainathC <SainathC>
2014-11-06 04:42:00 -0500

    Comments - Defect Number: 3292; Defect RecordID: 3288; Server Address: localhost; Server Port: 99; Database Name: Practice Management; Defect Summary: Consulting utilization report: Part Time PTO does not show up in Utilization Report coloring; Defect Description: I entered in PTO time for 7.5 hours for both a 1 day occurrence and in a separate series of a few days. I later looked up the utilization report. The PTO entries, which were approved, did not show up in the default view. Upon clicking into my specific time, the PTO time would be visible.
    expected:
    I would have expected that the utilization bars would show partial PTO in the high level view as well as the detailed individual view

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/ConsultantUtilizationDailyByPerson.proc.sql
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/ConsultantUtilizationDailyByPerson.proc.sql
@@ -40,6 +40,17 @@ AS
                 INNER JOIN dbo.PersonStatus AS st ON p.PersonStatusId = st.PersonStatusId
                 INNER JOIN dbo.Timescale AS paytp ON paytp.TimescaleId = dbo.GetCurrentPayType(@PersonId)
                 LEFT JOIN dbo.Practice AS pr ON p.DefaultPractice = pr.PracticeId
-        WHERE p.PersonId  = @PersonId        
+        WHERE p.PersonId  = @PersonId     
+		
+		SELECT	PC.PersonId,PC.Date,
+				CASE WHEN PC.DayOff=1 AND PC.CompanyDayOff=0 THEN 1
+				ELSE 0 END AS IsTimeOff,Cal.HolidayDescription,
+				ROUND(ISNULL(PC.TimeOffHours,0),2) TimeOffHours
+		FROM dbo.PersonCalendarAuto PC 
+		LEFT JOIN dbo.Calendar AS Cal ON Cal.Date=PC.Date
+		WHERE PC.PersonId = @PersonId AND
+			  PC.[Date] BETWEEN @StartDate AND @EndDate AND
+			  PC.DayOff=1 AND (PC.TimeOffHours>0 OR PC.CompanyDayOff=1) AND DATEPART(DW,PC.Date) NOT IN (1,7)
+		ORDER BY PC.PersonId,PC.Date   
     END
 
