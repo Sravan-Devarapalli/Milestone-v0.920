77445ac1367beb98c5d5d72ef18dbdaeb6f76df2
SainathC <SainathC>
2014-11-13 02:22:00 -0500

    Comments - Defect Number: 3297; Defect RecordID: 3293; Server Address: localhost; Server Port: 99; Database Name: Practice Management; Defect Summary: Include projects with 'Completed ' status iin consulting utilization report; Defect Description: That makes sense. But, there isnt a completed option to click on the Utilization report, so if we are trying to look at this historically, we cant get an accurate picture. Please open a ticket to add the completed button.
    From: Harshitha Tenneti [mailto:harshithat@ggktech.com]
    Sent: Wednesday, November 5, 2014 10:03 PM
    To: Ellen Boyer
    Cc: Nick Callahan
    Subject: RE: Potential bug in PM
    
    Hi Ellen,
    
    In consulting utilization report we show only the projects with Active, Projected, Internal, Experimental and Proposed statuses.
    
    
    The project to which Derek child is assigned is with Completed status. So, we will not show his utilization for that project in the report.
    
    
    --
    Thank you,
    Harshitha Tenneti | @GGKTech| Office: +91-40-67670404| www.ggktech.com | Delivering Commitments.@
    Tower 2.1, Level 13, Waverock building, APIIC IT/ITES SEZ, Gachibowli(Nanakramguda), Hyderabad  500008
    GGKTechnologiesrespects your privacy. This email is intended only for the use of the party to which it is addressed and may contain information that is privileged, confidential, or protected by law. If you have received this message in error, or do not want to receive any further emails from us, please notify us immediately by replying to the message and deleting it from your computer.
    
    From: Ellen Boyer [mailto:EllenB@logic2020.com]
    Sent: Thursday, November 06, 2014 5:15 AM
    To: Harshitha Tenneti
    Cc: Nick Callahan
    Subject: Potential bug in PM
    
    When we run the Utilization report, there are no billable hours for Derek Child. Yet, in the time entry system, he has chargeable hours (and also in the Attainment report that we run, it shows his chargeable hours). (Screen shots are attached)
    
    Please research quickly and let us know if you can fix this

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Functions/GetNumberProjectedHours.function.sql
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Functions/GetNumberProjectedHours.function.sql
@@ -12,7 +12,8 @@ CREATE FUNCTION dbo.GetNumberProjectedHours
 	  @ProjectedProjects BIT = 1,
 	  @ExperimentalProjects BIT = 1,
 	  @ProposedProjects BIT =1,
-	  @InternalProjects	BIT = 1
+	  @InternalProjects	BIT = 1,
+	  @CompletedProjects BIT = 1
     )
 RETURNS INT
 AS BEGIN
@@ -42,8 +43,9 @@ DECLARE @DefaultMilestoneId INT
 			 @ProjectedProjects = 1 AND pr.ProjectStatusId = 2 OR	--  2 - Projected
 			 @ExperimentalProjects = 1 AND pr.ProjectStatusId = 5 OR	--  5 - Experimental
 			 @ProposedProjects = 1 AND pr.ProjectStatusId = 7 OR --7-Proposed
-			 @InternalProjects = 1 AND pr.ProjectStatusId = 6) --6 - Internal
-
+			 @InternalProjects = 1 AND pr.ProjectStatusId = 6 OR--6 - Internal
+			 @CompletedProjects = 1 AND pr.ProjectStatusId = 4 -- 4 - Completed
+			 )
     RETURN @res
    END
 
