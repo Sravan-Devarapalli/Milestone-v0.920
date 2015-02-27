4c20d64aca10f5228d016f7fb0b90c5d0d3af8b4
SainathC <SainathC>
2015-02-27 07:34:00 -0500

    Comments - Defect Number: 3301; Defect RecordID: 3297; Server Address: localhost; Server Port: 99; Database Name: Practice Management; Defect Summary: New Microsoft 18 months requirement; Defect Description: Harshithat  As we have discussed previously, Microsoft has implemented a new policy that states that after working (being badged) for 18 months, contractors must not work for Microsoft for a 6 month period. After that 6 month period, they can come back and work for another 18 months. We need a way to track how long each of our consultants have been badged at Microsoft as well as track their 6 mo. absence from working at Microsoft. It is important to note that the dates that are being tracked are whether or not the consultant has a Microsoft badge (which allows access to the systems and facilities). A consultant could work on a Microsoft project without a badge (remotely and not needing to use information in their systems) and this time would not be counted against the new policy.
    I have attached a DRAFT of some requirements for being able to track this. Id like to set up a separate call (does next Monday at 8am our time work?) to discuss adding this tracking functionality in PM.

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/Reports/Badge/BadgeRequestNotApprovedReport.aspx.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using PraticeManagement.Utils.Excel;
+using System.Data;
+using PraticeManagement.Utils;
+using DataTransferObjects;
+
+namespace PraticeManagement.Reports.Badge
+{
+    public partial class BadgeRequestNotApprovedReport : System.Web.UI.Page
+    {
+        private int coloumnsCount = 1;
+        private int headerRowsCount = 1;
+
+        private SheetStyles HeaderSheetStyle
+        {
+            get
+            {
+                CellStyles cellStyle = new CellStyles();
+                cellStyle.IsBold = true;
+                cellStyle.BorderStyle = NPOI.SS.UserModel.BorderStyle.NONE;
+                cellStyle.FontHeight = 350;
+                CellStyles[] cellStylearray = { cellStyle };
+                RowStyles headerrowStyle = new RowStyles(cellStylearray);
+                headerrowStyle.Height = 500;
+
+                CellStyles dataCellStyle = new CellStyles();
+                CellStyles[] dataCellStylearray = { dataCellStyle };
+                RowStyles datarowStyle = new RowStyles(dataCellStylearray);
+
+                RowStyles[] rowStylearray = { headerrowStyle, datarowStyle };
+
+                SheetStyles sheetStyle = new SheetStyles(rowStylearray);
+                sheetStyle.MergeRegion.Add(new int[] { 0, 0, 0, coloumnsCount > 12 ? coloumnsCount : 13 - 1 });
+                sheetStyle.IsAutoResize = false;
+
+                return sheetStyle;
+            }
+        }
+
+        private SheetStyles DataSheetStyle
+        {
+            get
+            {
+                CellStyles headerCellStyle = new CellStyles();
+                headerCellStyle.IsBold = true;
+                headerCellStyle.HorizontalAlignment = NPOI.SS.UserModel.HorizontalAlignment.CENTER;
+
+                CellStyles dataDateCellStyle = new CellStyles();
+                dataDateCellStyle.DataFormat = "mm/dd/yy;@";
+                dataDateCellStyle.HorizontalAlignment = NPOI.SS.UserModel.HorizontalAlignment.CENTER;
+
+                List<CellStyles> headerCellStyleList = new List<CellStyles>() { headerCellStyle };
+
+                RowStyles headerrowStyle = new RowStyles(headerCellStyleList.ToArray());
+
+                CellStyles dataCellStyle = new CellStyles();
+
+                var dataCellStylearray = new List<CellStyles>() { dataCellStyle, dataCellStyle, dataDateCellStyle, dataDateCellStyle, dataDateCellStyle };
+
+                RowStyles datarowStyle = new RowStyles(dataCellStylearray.ToArray());
+                RowStyles[] rowStylearray = { headerrowStyle, datarowStyle };
+                SheetStyles sheetStyle = new SheetStyles(rowStylearray);
+                sheetStyle.TopRowNo = headerRowsCount;
+                sheetStyle.IsFreezePane = true;
+                sheetStyle.FreezePanColSplit = 0;
+                sheetStyle.FreezePanRowSplit = headerRowsCount;
+                return sheetStyle;
+            }
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+                PopulateData();
+        }
+
+        public void PopulateData()
+        {
+            var badgeList = ServiceCallers.Custom.Report(r=>r.GetBadgeRequestNotApprovedList());
+            if (badgeList.Length > 0)
+            {
+                divEmptyMessage.Style.Add("display", "none");
+                repbadgeNotApproved.DataSource = badgeList;
+                repbadgeNotApproved.DataBind();
+                repbadgeNotApproved.Visible = true;
+            }
+            else
+            {
+                repbadgeNotApproved.Visible = false;
+                divEmptyMessage.Style.Remove("display");
+            }
+        }
+
+        protected string GetDateFormat(DateTime? date)
+        {
+            return date.HasValue ? date.Value.ToString(Constants.Formatting.EntryDateFormat) : string.Empty;
+        }
+
+        protected string GetProjectDetailsLink(int? projectId)
+        {
+            return Utils.Generic.GetTargetUrlWithReturn(String.Format(Constants.ApplicationPages.DetailRedirectFormat, Constants.ApplicationPages.ProjectDetail, projectId.Value),
+                                                        Constants.ApplicationPages.PersonsPage);
+        }
+
+        protected void btnExportToExcel_OnClick(object sender, EventArgs e)
+        {
+            var filename = "BadgeRequestNotApprovedReport.xls";
+            var sheetStylesList = new List<SheetStyles>();
+            var dataSetList = new List<DataSet>();
+            var report = ServiceCallers.Custom.Report(r => r.GetBadgeRequestNotApprovedList()).ToList();
+            if (report.Count > 0)
+            {
+                var dateRangeTitle = "Badge Request Not Approved Report";
+                var header = new DataTable();
+                header.Columns.Add(dateRangeTitle);
+                headerRowsCount = header.Rows.Count + 3;
+                var data = PrepareDataTable(report);
+                coloumnsCount = data.Columns.Count;
+                sheetStylesList.Add(HeaderSheetStyle);
+                sheetStylesList.Add(DataSheetStyle);
+                var dataset = new DataSet();
+                dataset.DataSetName = "BadgeNotApproved";
+                dataset.Tables.Add(header);
+                dataset.Tables.Add(data);
+                dataSetList.Add(dataset);
+            }
+            else
+            {
+                string dateRangeTitle = "There are no resources whose badge requestes are not approved.";
+                DataTable header = new DataTable();
+                header.Columns.Add(dateRangeTitle);
+                sheetStylesList.Add(HeaderSheetStyle);
+                var dataset = new DataSet();
+                dataset.DataSetName = "BadgeNotApproved";
+                dataset.Tables.Add(header);
+                dataSetList.Add(dataset);
+            }
+            NPOIExcel.Export(filename, dataSetList, sheetStylesList);
+        }
+
+        public DataTable PrepareDataTable(List<MSBadge> report)
+        {
+            DataTable data = new DataTable();
+            List<object> row;
+
+            data.Columns.Add("Current Badge Requests Not Yet Approved");
+            data.Columns.Add("Project #");
+            data.Columns.Add("Request Date");
+            data.Columns.Add("Badge Start");
+            data.Columns.Add("Badge End");
+            foreach (var reportItem in report)
+            {
+                row = new List<object>();
+                row.Add(reportItem.Person.Name);
+                row.Add(reportItem.Project.ProjectNumber);
+                row.Add(reportItem.PlannedEndDate.HasValue ? reportItem.PlannedEndDate.Value.ToShortDateString() : string.Empty);
+                row.Add(reportItem.BadgeStartDate.HasValue ? reportItem.BadgeStartDate.Value.ToShortDateString() : string.Empty);
+                row.Add(reportItem.BadgeEndDate.HasValue ? reportItem.BadgeEndDate.Value.ToShortDateString() : string.Empty);
+                data.Rows.Add(row.ToArray());
+            }
+            return data;
+        }
+    }
+}
