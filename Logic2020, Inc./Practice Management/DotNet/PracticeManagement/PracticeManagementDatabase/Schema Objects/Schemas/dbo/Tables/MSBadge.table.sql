3519477eb7182eca4aa1a7288d0a8f7c28a17a99
SainathC <SainathC>
2015-02-27 07:34:00 -0500

    Comments - Defect Number: 3301; Defect RecordID: 3297; Server Address: localhost; Server Port: 99; Database Name: Practice Management; Defect Summary: New Microsoft 18 months requirement; Defect Description: Harshithat  As we have discussed previously, Microsoft has implemented a new policy that states that after working (being badged) for 18 months, contractors must not work for Microsoft for a 6 month period. After that 6 month period, they can come back and work for another 18 months. We need a way to track how long each of our consultants have been badged at Microsoft as well as track their 6 mo. absence from working at Microsoft. It is important to note that the dates that are being tracked are whether or not the consultant has a Microsoft badge (which allows access to the systems and facilities). A consultant could work on a Microsoft project without a badge (remotely and not needing to use information in their systems) and this time would not be counted against the new policy.
    I have attached a DRAFT of some requirements for being able to track this. Id like to set up a separate call (does next Monday at 8am our time work?) to discuss adding this tracking functionality in PM.

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/MSBadge.table.sql
@@ -0,0 +1,23 @@
+﻿CREATE TABLE [dbo].[MSBadge]
+(
+	PersonId			INT			NOT NULL,
+	BadgeStartDate		DATETIME	NULL,
+	BadgeEndDate		DATETIME	NULL,
+	PlannedEndDate		DATETIME	NULL,
+	BreakStartDate		DATETIME	NULL,
+	BreakEndDate		DATETIME	NULL,
+	IsBlocked			BIT			NOT NULL,
+	BlockStartDate		DATETIME	NULL,
+	BlockEndDate		DATETIME	NULL,
+	IsPreviousBadge		BIT			NOT NULL,
+	PreviousBadgeAlias	NVARCHAR(20) NULL,
+	LastBadgeStartDate	DATETIME	NULL,
+	LastBadgeEndDate	DATETIME	NULL,
+	IsException			BIT			NOT NULL,
+	ExceptionStartDate	DATETIME	NULL,
+	ExceptionEndDate	DATETIME	NULL,
+	BadgeStartDateSource NVARCHAR(30) NULL,
+	PlannedEndDateSource NVARCHAR(30) NULL,
+	BadgeEndDateSource NVARCHAR(30) NULL
+)
+
