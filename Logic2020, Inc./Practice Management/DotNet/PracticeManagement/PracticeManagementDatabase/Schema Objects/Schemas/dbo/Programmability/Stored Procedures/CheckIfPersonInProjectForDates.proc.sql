a8ac54a0808e9b8aa6f518c2694664887ee73aa4
SainathC <SainathC>
2015-02-27 07:34:00 -0500

    Comments - Defect Number: 3301; Defect RecordID: 3297; Server Address: localhost; Server Port: 99; Database Name: Practice Management; Defect Summary: New Microsoft 18 months requirement; Defect Description: Harshithat  As we have discussed previously, Microsoft has implemented a new policy that states that after working (being badged) for 18 months, contractors must not work for Microsoft for a 6 month period. After that 6 month period, they can come back and work for another 18 months. We need a way to track how long each of our consultants have been badged at Microsoft as well as track their 6 mo. absence from working at Microsoft. It is important to note that the dates that are being tracked are whether or not the consultant has a Microsoft badge (which allows access to the systems and facilities). A consultant could work on a Microsoft project without a badge (remotely and not needing to use information in their systems) and this time would not be counted against the new policy.
    I have attached a DRAFT of some requirements for being able to track this. Id like to set up a separate call (does next Monday at 8am our time work?) to discuss adding this tracking functionality in PM.

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/CheckIfPersonInProjectForDates.proc.sql
@@ -0,0 +1,23 @@
+﻿CREATE PROCEDURE [dbo].[CheckIfPersonInProjectForDates]
+(
+	@PersonId	INT,
+	@StartDate	DATETIME,
+	@EndDate	DATETIME
+)
+AS
+BEGIN
+
+	IF EXISTS(SELECT 1 FROM dbo.MilestonePersonEntry MPE
+					   JOIN dbo.MilestonePerson MP ON MP.MilestonePersonId = MPE.MilestonePersonId
+					   WHERE MPE.IsBadgeRequired = 1 AND 
+							(MPE.StartDate <= @EndDate AND @StartDate <= MPE.EndDate)
+							AND MP.PersonId = @PersonId)
+	BEGIN
+			SELECT CONVERT(BIT,1) Result 
+	END
+	ELSE
+	BEGIN
+			SELECT CONVERT(BIT,0) Result
+	END
+
+END
