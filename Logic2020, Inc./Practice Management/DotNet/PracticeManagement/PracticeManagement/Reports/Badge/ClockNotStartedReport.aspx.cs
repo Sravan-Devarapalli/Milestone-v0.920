235f0e39777a560e1e964385fbb7ba5e1124b712
SainathC <SainathC>
2015-02-27 07:33:00 -0500

    Comments - Defect Number: 3301; Defect RecordID: 3297; Server Address: localhost; Server Port: 99; Database Name: Practice Management; Defect Summary: New Microsoft 18 months requirement; Defect Description: Harshithat  As we have discussed previously, Microsoft has implemented a new policy that states that after working (being badged) for 18 months, contractors must not work for Microsoft for a 6 month period. After that 6 month period, they can come back and work for another 18 months. We need a way to track how long each of our consultants have been badged at Microsoft as well as track their 6 mo. absence from working at Microsoft. It is important to note that the dates that are being tracked are whether or not the consultant has a Microsoft badge (which allows access to the systems and facilities). A consultant could work on a Microsoft project without a badge (remotely and not needing to use information in their systems) and this time would not be counted against the new policy.
    I have attached a DRAFT of some requirements for being able to track this. Id like to set up a separate call (does next Monday at 8am our time work?) to discuss adding this tracking functionality in PM.

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/Reports/Badge/ClockNotStartedReport.aspx.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using DataTransferObjects;
+using PraticeManagement.Utils.Excel;
+using System.Data;
+using PraticeManagement.Utils;
+
+namespace PraticeManagement.Reports.Badge
+{
+    public partial class ClockNotStartedReport : System.Web.UI.Page
+    {
+        public const string StartDateKey = "StartDate";
+        public const string EndDateKey = "EndDate";
+        private int coloumnsCount = 1;
+        private int headerRowsCount = 1;
+
+        private SheetStyles HeaderSheetStyle
+        {
+            get
+            {
+                CellStyles cellStyle = new CellStyles();
+                cellStyle.IsBold = true;
+                cellStyle.BorderStyle = NPOI.SS.UserModel.BorderStyle.NONE;
+                cellStyle.FontHeight = 350;
+                CellStyles[] cellStylearray = { cellStyle };
+                RowStyles headerrowStyle = new RowStyles(cellStylearray);
+                headerrowStyle.Height = 500;
+
+                CellStyles dataCellStyle = new CellStyles();
+                CellStyles[] dataCellStylearray = { dataCellStyle };
+                RowStyles datarowStyle = new RowStyles(dataCellStylearray);
+
+                RowStyles[] rowStylearray = { headerrowStyle, datarowStyle };
+
+                SheetStyles sheetStyle = new SheetStyles(rowStylearray);
+                sheetStyle.MergeRegion.Add(new int[] { 0, 0, 0, coloumnsCount > 12 ? coloumnsCount : 13 - 1 });
+                sheetStyle.IsAutoResize = false;
+
+                return sheetStyle;
+            }
+        }
+
+        private SheetStyles DataSheetStyle
+        {
+            get
+            {
+                CellStyles headerCellStyle = new CellStyles();
+                headerCellStyle.IsBold = true;
+                headerCellStyle.HorizontalAlignment = NPOI.SS.UserModel.HorizontalAlignment.CENTER;
+
+                CellStyles dataDateCellStyle = new CellStyles();
+                dataDateCellStyle.DataFormat = "mm/dd/yy;@";
+                dataDateCellStyle.HorizontalAlignment = NPOI.SS.UserModel.HorizontalAlignment.CENTER;
+
+                List<CellStyles> headerCellStyleList = new List<CellStyles>() { headerCellStyle };
+
+                RowStyles headerrowStyle = new RowStyles(headerCellStyleList.ToArray());
+
+                CellStyles dataCellStyle = new CellStyles();
+
+                var dataCellStylearray = new List<CellStyles>() { dataCellStyle, dataDateCellStyle, dataDateCellStyle };
+
+                RowStyles datarowStyle = new RowStyles(dataCellStylearray.ToArray());
+                RowStyles[] rowStylearray = { headerrowStyle, datarowStyle };
+                SheetStyles sheetStyle = new SheetStyles(rowStylearray);
+                sheetStyle.TopRowNo = headerRowsCount;
+                sheetStyle.IsFreezePane = true;
+                sheetStyle.FreezePanColSplit = 0;
+                sheetStyle.FreezePanRowSplit = headerRowsCount;
+                return sheetStyle;
+            }
+        }
+
+        public string StartDateFromQueryString
+        {
+            get
+            {
+                return Request.QueryString[StartDateKey];
+            }
+        }
+
+        public string EndDateFromQueryString
+        {
+            get
+            {
+                return Request.QueryString[EndDateKey];
+            }
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                dtpEnd.DateValue = DataTransferObjects.Utils.Generic.MonthEndDate(DateTime.Now);
+                dtpStart.DateValue = DataTransferObjects.Utils.Generic.MonthStartDate(DateTime.Now);
+                if (!String.IsNullOrEmpty(StartDateFromQueryString))
+                {
+                    dtpStart.DateValue = Convert.ToDateTime(StartDateFromQueryString);
+                    dtpEnd.DateValue = Convert.ToDateTime(EndDateFromQueryString);
+                    btnUpdateView_Click(btnUpdateView, new EventArgs());
+                }
+            }
+        }
+
+        protected void custNotMorethan2Years_ServerValidate(object source, ServerValidateEventArgs args)
+        {
+            if (!reqBadgeStart.IsValid || !cvLastBadgeStart.IsValid || !reqbadgeEnd.IsValid || !cvbadgeEnd.IsValid)
+                return;
+            var totalMonths = (((dtpEnd.DateValue.Year - dtpStart.DateValue.Year) * 12) + dtpEnd.DateValue.Month - dtpStart.DateValue.Month);
+            args.IsValid = totalMonths < 24;
+        }
+
+        protected void custNotBeforeJuly_ServerValidate(object source, ServerValidateEventArgs args)
+        {
+            if (!reqBadgeStart.IsValid || !cvLastBadgeStart.IsValid || !reqbadgeEnd.IsValid || !cvbadgeEnd.IsValid)
+                return;
+            args.IsValid = dtpStart.DateValue >= new DateTime(2014, 7, 1);
+        }
+
+        protected void btnUpdateView_Click(object sender, EventArgs e)
+        {
+            Page.Validate("BadgeReport");
+            if (!Page.IsValid)
+            {
+                divWholePage.Style.Add("display", "none");
+                return;
+            }
+            divWholePage.Style.Remove("display");
+            PopulateData();
+        }
+
+        public void PopulateData()
+        {
+            lblRange.Text = dtpStart.DateValue.ToString(Constants.Formatting.EntryDateFormat) + " - " + dtpEnd.DateValue.ToString(Constants.Formatting.EntryDateFormat);
+            var resources = ServiceCallers.Custom.Report(r => r.ListBadgeResourcesByType(dtpStart.DateValue, dtpEnd.DateValue, false, true, false, false,false).ToList());
+            repclocknotStarted.DataSource = resources;
+            repclocknotStarted.DataBind();
+            if (resources.Count > 0)
+            {
+                divEmptyMessage.Style.Add("display", "none");
+                repclocknotStarted.Visible = tblRange.Visible = true;
+            }
+            else
+            {
+                divEmptyMessage.Style.Remove("display");
+                repclocknotStarted.Visible = tblRange.Visible = false;
+            }
+        }
+
+        protected void repclocknotStarted_ItemDataBound(object sender, RepeaterItemEventArgs e)
+        {
+            if (e.Item.ItemType == ListItemType.Item || e.Item.ItemType == ListItemType.AlternatingItem)
+            {
+                var dataItem = (MSBadge)e.Item.DataItem;
+                var lblBadgeStart = e.Item.FindControl("lblBadgeStart") as Label;
+                var lblBadgeEnd = e.Item.FindControl("lblBadgeEnd") as Label;
+                lblBadgeEnd.Text = dataItem.BadgeEndDate.HasValue ? dataItem.BadgeEndDate.Value.ToShortDateString() : string.Empty;
+                lblBadgeStart.Text = dataItem.BadgeStartDate.HasValue ? dataItem.BadgeStartDate.Value.ToShortDateString() : string.Empty;
+            }
+        }
+
+        protected void btnExportToExcel_OnClick(object sender, EventArgs e)
+        {
+            var filename = string.Format("ClockNotStartedReport_{0}-{1}.xls", dtpStart.DateValue.ToString("MM_dd_yyyy"), dtpEnd.DateValue.ToString("MM_dd_yyyy"));
+            var sheetStylesList = new List<SheetStyles>();
+            var dataSetList = new List<DataSet>();
+            var report = ServiceCallers.Custom.Report(r => r.ListBadgeResourcesByType(dtpStart.DateValue, dtpEnd.DateValue, false, true, false, false,false).ToList());
+            if (report.Count > 0)
+            {
+                string dateRangeTitle = string.Format("18 Month clock not started report for the period: {0} to {1}", dtpStart.DateValue.ToString(Constants.Formatting.EntryDateFormat), dtpEnd.DateValue.ToString(Constants.Formatting.EntryDateFormat));
+                DataTable header = new DataTable();
+                header.Columns.Add(dateRangeTitle);
+                headerRowsCount = header.Rows.Count + 3;
+                var data = PrepareDataTable(report);
+                coloumnsCount = data.Columns.Count;
+                sheetStylesList.Add(HeaderSheetStyle);
+                sheetStylesList.Add(DataSheetStyle);
+                var dataset = new DataSet();
+                dataset.DataSetName = "ClockNotStarted";
+                dataset.Tables.Add(header);
+                dataset.Tables.Add(data);
+                dataSetList.Add(dataset);
+            }
+            else
+            {
+                string dateRangeTitle = "There are no resources whose clock not started for the selected dates.";
+                DataTable header = new DataTable();
+                header.Columns.Add(dateRangeTitle);
+                sheetStylesList.Add(HeaderSheetStyle);
+                var dataset = new DataSet();
+                dataset.DataSetName = "ClockNotStarted";
+                dataset.Tables.Add(header);
+                dataSetList.Add(dataset);
+            }
+            NPOIExcel.Export(filename, dataSetList, sheetStylesList);
+        }
+
+        public DataTable PrepareDataTable(List<MSBadge> report)
+        {
+            DataTable data = new DataTable();
+            List<object> row;
+
+            data.Columns.Add("List of Resources with 18-Month Clock Not Started");
+            data.Columns.Add("18 Month Start");
+            data.Columns.Add("18 Month End");
+            foreach (var reportItem in report)
+            {
+                row = new List<object>();
+                row.Add(reportItem.Person.Name);
+                row.Add(reportItem.BadgeStartDate.HasValue ? reportItem.BadgeStartDate.Value.ToShortDateString() : string.Empty);
+                row.Add(reportItem.BadgeEndDate.HasValue ? reportItem.BadgeEndDate.Value.ToShortDateString() : string.Empty);
+                data.Rows.Add(row.ToArray());
+            }
+            return data;
+        }
+    }
+}
