b0ba4bc2930c597547eb0672703324816982f6b7
SainathC <SainathC>
2015-02-27 07:34:00 -0500

    Comments - Defect Number: 3301; Defect RecordID: 3297; Server Address: localhost; Server Port: 99; Database Name: Practice Management; Defect Summary: New Microsoft 18 months requirement; Defect Description: Harshithat  As we have discussed previously, Microsoft has implemented a new policy that states that after working (being badged) for 18 months, contractors must not work for Microsoft for a 6 month period. After that 6 month period, they can come back and work for another 18 months. We need a way to track how long each of our consultants have been badged at Microsoft as well as track their 6 mo. absence from working at Microsoft. It is important to note that the dates that are being tracked are whether or not the consultant has a Microsoft badge (which allows access to the systems and facilities). A consultant could work on a Microsoft project without a badge (remotely and not needing to use information in their systems) and this time would not be counted against the new policy.
    I have attached a DRAFT of some requirements for being able to track this. Id like to set up a separate call (does next Monday at 8am our time work?) to discuss adding this tracking functionality in PM.

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/GetBadgeRequestNotApprovedList.proc.sql
@@ -0,0 +1,21 @@
+﻿CREATE PROCEDURE [dbo].[GetBadgeRequestNotApprovedList]
+AS
+BEGIN
+
+	SELECT	Per.LastName,
+			Per.FirstName,
+			P.ProjectId,
+			P.Name AS ProjectName,
+			P.ProjectNumber,
+			MPE.BadgeStartDate,
+			MPE.BadgeEndDate,
+			MPE.BadgeRequestDate
+	FROM dbo.MilestonePersonEntry MPE 
+	INNER JOIN dbo.MilestonePerson MP ON MP.MilestonePersonId = MPE.MilestonePersonId
+	INNER JOIN dbo.Milestone M ON M.MilestoneId = MP.MilestoneId
+	INNER JOIN dbo.Project P ON P.ProjectId = M.ProjectId
+	INNER JOIN dbo.Person Per ON Per.PersonId = MP.PersonId
+	WHERE MPE.IsBadgeRequired = 1 AND MPE.IsApproved = 0
+	ORDER BY Per.LastName,Per.FirstName
+
+END
