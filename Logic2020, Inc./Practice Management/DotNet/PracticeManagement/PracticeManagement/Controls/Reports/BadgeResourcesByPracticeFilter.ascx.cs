24763f2d59022492a860c2d4404642b793410ce8
SainathC <SainathC>
2015-02-27 07:33:00 -0500

    Comments - Defect Number: 3301; Defect RecordID: 3297; Server Address: localhost; Server Port: 99; Database Name: Practice Management; Defect Summary: New Microsoft 18 months requirement; Defect Description: Harshithat  As we have discussed previously, Microsoft has implemented a new policy that states that after working (being badged) for 18 months, contractors must not work for Microsoft for a 6 month period. After that 6 month period, they can come back and work for another 18 months. We need a way to track how long each of our consultants have been badged at Microsoft as well as track their 6 mo. absence from working at Microsoft. It is important to note that the dates that are being tracked are whether or not the consultant has a Microsoft badge (which allows access to the systems and facilities). A consultant could work on a Microsoft project without a badge (remotely and not needing to use information in their systems) and this time would not be counted against the new policy.
    I have attached a DRAFT of some requirements for being able to track this. Id like to set up a separate call (does next Monday at 8am our time work?) to discuss adding this tracking functionality in PM.

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/Controls/Reports/BadgeResourcesByPracticeFilter.ascx.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Text;
+
+namespace PraticeManagement.Controls.Reports
+{
+    public partial class BadgeResourcesByPracticeFilter : System.Web.UI.UserControl
+    {
+        public string Practices
+        {
+            get
+            {
+                var clientList = new StringBuilder();
+                foreach (ListItem item in cblPractices.Items)
+                    if (item.Selected)
+                        clientList.Append(item.Value).Append(',');
+                return clientList.ToString();
+            }
+        }
+
+        public string TitleIds
+        {
+            get
+            {
+                var clientList = new StringBuilder();
+                foreach (ListItem item in cblTitles.Items)
+                    if (item.Selected)
+                        clientList.Append(item.Value).Append(',');
+                return clientList.ToString();
+            }
+        }
+
+        public int ddlType //1-practice area, 2-business title, 3-technical title
+        {
+            get;
+            set;
+        }
+
+        public bool IsBadgedNotOnProject
+        {
+            get
+            {
+                return chbBadgedNotOnProject.Checked;
+            }
+        }
+
+        public bool IsBadgedOnProject
+        {
+            get
+            {
+                return chbBadgedOnProject.Checked;
+            }
+        }
+
+        public bool IsClockNotStarted
+        {
+            get
+            {
+                return chbClockNotStarted.Checked;
+            }
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                if (ddlType == 1)
+                {
+                    DataHelper.FillPracticeListOnlyActive(cblPractices, Resources.Controls.AllPracticesText);
+                    cblPractices.SelectAll();
+                    tdPractices.Visible = true;
+                    tdTitles.Visible = false;
+                    lblCategory.Text = "Practice Area";
+                }
+                else
+                {
+                    var titles = ServiceCallers.Custom.Title(t => t.GetAllTitles());
+                    titles = titles.Where(t => t.TitleType.TitleTypeId == ddlType - 1).ToArray();
+                    DataHelper.FillTitleList(cblTitles, "All Titles",true,ddlType-1);
+                    cblTitles.SelectAll();
+                    tdTitles.Visible = true;
+                    tdPractices.Visible = false;
+                    lblCategory.Text = "Title";
+                }
+            }
+        }
+    }
+}
