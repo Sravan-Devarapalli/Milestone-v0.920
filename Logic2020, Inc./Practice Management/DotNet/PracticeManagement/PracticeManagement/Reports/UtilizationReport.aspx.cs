a5fb6b4c40de1b2fe7f8b4c2f526650cebfaa21a
SainathC <SainathC>
2015-03-23 02:33:00 -0500

    Comments - Defect Number: 3301; Defect RecordID: 3297; Server Address: localhost; Server Port: 99; Database Name: PM1; Defect Summary: New Microsoft 18 months requirement; Defect Description: Harshithat  As we have discussed previously, Microsoft has implemented a new policy that states that after working (being badged) for 18 months, contractors must not work for Microsoft for a 6 month period. After that 6 month period, they can come back and work for another 18 months. We need a way to track how long each of our consultants have been badged at Microsoft as well as track their 6 mo. absence from working at Microsoft. It is important to note that the dates that are being tracked are whether or not the consultant has a Microsoft badge (which allows access to the systems and facilities). A consultant could work on a Microsoft project without a badge (remotely and not needing to use information in their systems) and this time would not be counted against the new policy.
    I have attached a DRAFT of some requirements for being able to track this. Id like to set up a separate call (does next Monday at 8am our time work?) to discuss adding this tracking functionality in PM.

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/Reports/UtilizationReport.aspx.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.Security;
+using PraticeManagement.Controls;
+using DataTransferObjects.Reports;
+
+namespace PraticeManagement.Reports
+{
+    public partial class UtilizationReport : System.Web.UI.Page
+    {
+        public DateTime WeekStartDate
+        { get; set; }
+
+        public DateTime WeekEndDate
+        { get; set; }
+
+        public DateTime YearStartDate
+        {
+            get
+            {
+                var now = Utils.Generic.GetNowWithTimeZone();
+                return Utils.Calendar.YearStartDate(now);
+            }
+        }
+
+        public DateTime YTDEndDate
+        {
+            get
+            {
+                return Utils.Generic.GetNowWithTimeZone();
+            }
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                var now = Utils.Generic.GetNowWithTimeZone();
+                WeekStartDate = Utils.Calendar.WeekStartDate(now);
+                WeekEndDate = Utils.Calendar.WeekStartDate(now);
+                PopulateDropdownValues();
+                PopulateUtilizationValues();
+            }
+        }
+
+        protected void ddlPerson_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            PopulateUtilizationValues();
+        }
+
+        public void PopulateDropdownValues()
+        {
+            var currentPerson = DataHelper.CurrentPerson;
+            var userIsAdministrator = Roles.IsUserInRole(DataTransferObjects.Constants.RoleNames.AdministratorRoleName);
+            if (userIsAdministrator)
+            {
+                var persons = ServiceCallers.Custom.Person(p => p.PersonsListHavingActiveStatusDuringThisPeriod(WeekStartDate, WeekEndDate));
+                DataHelper.FillPersonList(ddlPerson, null, persons, null);
+                ListItem selectedPerson = ddlPerson.Items.FindByValue(currentPerson.Id.Value.ToString()); //ddlPerson.SelectedValue = currentPerson.Id.Value.ToString();
+                if (selectedPerson != null)
+                {
+                    ddlPerson.SelectedValue = currentPerson.Id.Value.ToString();
+                }
+            }
+            else
+            {
+                lblPerson.Visible = true;
+                ddlPerson.Visible = false;
+                lblPerson.Text = currentPerson.HtmlEncodedName;
+            }
+        }
+
+        public void PopulateUtilizationValues()
+        {
+            var currentPerson = DataHelper.CurrentPerson;
+            var userIsAdministrator = Roles.IsUserInRole(DataTransferObjects.Constants.RoleNames.AdministratorRoleName);
+            var totalHours = new PersonTimeEntriesTotals();
+            var personId = userIsAdministrator ? Convert.ToInt32(ddlPerson.SelectedValue) : currentPerson.Id.Value;
+            totalHours = ServiceCallers.Custom.Report(r => r.UtilizationReport(personId, YearStartDate, YTDEndDate));
+            lblTargetHours.Text = totalHours.AvailableHours.ToString();
+            lblBillableTime.Text = lblBillableTime2.Text = totalHours.BillableHoursUntilToday.ToString();
+            lblAllocatedBillable.Text = totalHours.ProjectedHours.ToString();
+            lblUtilization.Text = totalHours.BillableUtilizationPercentage;
+            lblAllocatedVsTarget.Text = totalHours.BillableAllocatedVsTarget;
+            lblAllocatedVsTarget.Style["color"] = totalHours.BillableAllocatedVsTargetValue >= 0 ? "Black" : "#F00";
+        }
+    }
+}
