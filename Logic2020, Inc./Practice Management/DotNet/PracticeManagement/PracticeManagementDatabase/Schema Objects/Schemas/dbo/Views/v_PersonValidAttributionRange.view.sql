ad1fc802abb2e7a7fd9f1b436001aba6a7b1c5ef
SainathC <SainathC>
2015-03-23 02:34:00 -0500

    Comments - Defect Number: 3301; Defect RecordID: 3297; Server Address: localhost; Server Port: 99; Database Name: PM1; Defect Summary: New Microsoft 18 months requirement; Defect Description: Harshithat  As we have discussed previously, Microsoft has implemented a new policy that states that after working (being badged) for 18 months, contractors must not work for Microsoft for a 6 month period. After that 6 month period, they can come back and work for another 18 months. We need a way to track how long each of our consultants have been badged at Microsoft as well as track their 6 mo. absence from working at Microsoft. It is important to note that the dates that are being tracked are whether or not the consultant has a Microsoft badge (which allows access to the systems and facilities). A consultant could work on a Microsoft project without a badge (remotely and not needing to use information in their systems) and this time would not be counted against the new policy.
    I have attached a DRAFT of some requirements for being able to track this. Id like to set up a separate call (does next Monday at 8am our time work?) to discuss adding this tracking functionality in PM.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Views/v_PersonValidAttributionRange.view.sql
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Views/v_PersonValidAttributionRange.view.sql
@@ -15,5 +15,5 @@
 	  CASE WHEN div.EndDate IS NOT NULL AND ph.EndDate > DATEADD(DD,-1,div.EndDate) THEN DATEADD(DD,-1,div.EndDate) ELSE ph.EndDate END AS Enddate
 	FROM PersonPayWithHistory ph
 	INNER JOIN v_DivisionHistory div ON ph.PersonId = div.personid AND (div.EndDate IS NULL OR ph.StartDate < div.EndDate) AND div.StartDate <= ph.EndDate
-	WHERE ISNULL(div.DivisionId,0) IN (1,2) --Consulting,BusinessDevelopment
+	WHERE ISNULL(div.DivisionId,0) IN (1,2,5,6,7) --Consulting,BusinessDevelopment
 
