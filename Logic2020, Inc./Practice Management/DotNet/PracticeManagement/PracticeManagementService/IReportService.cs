36b7b9b20f4d08f071eb9040f91d330284452004
SainathC <SainathC>
2015-04-14 07:08:00 -0500

    Comments - Defect Number: 3301; Defect RecordID: 3297; Server Address: localhost; Server Port: 99; Database Name: PM1; Defect Summary: New Microsoft 18 months requirement; Defect Description: Harshithat  As we have discussed previously, Microsoft has implemented a new policy that states that after working (being badged) for 18 months, contractors must not work for Microsoft for a 6 month period. After that 6 month period, they can come back and work for another 18 months. We need a way to track how long each of our consultants have been badged at Microsoft as well as track their 6 mo. absence from working at Microsoft. It is important to note that the dates that are being tracked are whether or not the consultant has a Microsoft badge (which allows access to the systems and facilities). A consultant could work on a Microsoft project without a badge (remotely and not needing to use information in their systems) and this time would not be counted against the new policy.
    I have attached a DRAFT of some requirements for being able to track this. Id like to set up a separate call (does next Monday at 8am our time work?) to discuss adding this tracking functionality in PM.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementService/IReportService.cs
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementService/IReportService.cs
@@ -161,16 +161,16 @@ namespace PracticeManagementService
         List<ProjectLevelGroupedHours> NonBillableReport(DateTime startDate, DateTime endDate, string projectNumber, string directorIds, string businessUnitIds, string practiceIds);
 
         [OperationContract]
-        List<BadgedResourcesByTime> BadgedResourcesByTimeReport(DateTime startDate, DateTime endDate, int step);
+        List<BadgedResourcesByTime> BadgedResourcesByTimeReport(string payTypes, DateTime startDate, DateTime endDate, int step);
 
         [OperationContract]
-        List<MSBadge> ListBadgeResourcesByType(DateTime startDate, DateTime endDate, bool isNotBadged, bool isClockNotStart, bool isBlocked, bool isBreak, bool badgedOnProject);
+        List<MSBadge> ListBadgeResourcesByType(string paytypes,DateTime startDate, DateTime endDate, bool isNotBadged, bool isClockNotStart, bool isBlocked, bool isBreak, bool badgedOnProject);
 
         [OperationContract]
-        List<GroupByPractice> ResourcesByPracticeReport(string practices, DateTime startDate, DateTime endDate, int step);
+        List<GroupByPractice> ResourcesByPracticeReport(string paytypes, string practices, DateTime startDate, DateTime endDate, int step);
 
         [OperationContract]
-        List<GroupbyTitle> ResourcesByTitleReport(string titles, DateTime startDate, DateTime endDate, int step);
+        List<GroupbyTitle> ResourcesByTitleReport(string paytypes,string titles, DateTime startDate, DateTime endDate, int step);
 
         [OperationContract]
         List<MSBadge> GetBadgeRequestNotApprovedList();
