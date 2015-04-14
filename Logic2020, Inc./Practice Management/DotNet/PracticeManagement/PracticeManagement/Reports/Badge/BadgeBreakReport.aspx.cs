2126f3713fe97649237986f05c4897c3c01da59e
SainathC <SainathC>
2015-04-14 07:08:00 -0500

    Comments - Defect Number: 3301; Defect RecordID: 3297; Server Address: localhost; Server Port: 99; Database Name: PM1; Defect Summary: New Microsoft 18 months requirement; Defect Description: Harshithat  As we have discussed previously, Microsoft has implemented a new policy that states that after working (being badged) for 18 months, contractors must not work for Microsoft for a 6 month period. After that 6 month period, they can come back and work for another 18 months. We need a way to track how long each of our consultants have been badged at Microsoft as well as track their 6 mo. absence from working at Microsoft. It is important to note that the dates that are being tracked are whether or not the consultant has a Microsoft badge (which allows access to the systems and facilities). A consultant could work on a Microsoft project without a badge (remotely and not needing to use information in their systems) and this time would not be counted against the new policy.
    I have attached a DRAFT of some requirements for being able to track this. Id like to set up a separate call (does next Monday at 8am our time work?) to discuss adding this tracking functionality in PM.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/Reports/Badge/BadgeBreakReport.aspx.cs
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/Reports/Badge/BadgeBreakReport.aspx.cs
@@ -8,6 +8,7 @@ using DataTransferObjects;
 using PraticeManagement.Utils.Excel;
 using System.Data;
 using PraticeManagement.Utils;
+using PraticeManagement.Controls;
 
 namespace PraticeManagement.Reports.Badge
 {
@@ -15,6 +16,7 @@ namespace PraticeManagement.Reports.Badge
     {
         public const string StartDateKey = "StartDate";
         public const string EndDateKey = "EndDate";
+        public const string PayTypesKey = "PayTypes";
         private int coloumnsCount = 1;
         private int headerRowsCount = 1;
 
@@ -91,16 +93,27 @@ namespace PraticeManagement.Reports.Badge
             }
         }
 
+        public string PayTypesFromQueryString
+        {
+            get
+            {
+                return Request.QueryString[PayTypesKey];
+            }
+        }
+
         protected void Page_Load(object sender, EventArgs e)
         {
             if (!IsPostBack)
             {
                 dtpEnd.DateValue = DataTransferObjects.Utils.Generic.MonthEndDate(DateTime.Now);
                 dtpStart.DateValue = DataTransferObjects.Utils.Generic.MonthStartDate(DateTime.Now);
+                DataHelper.FillTimescaleList(this.cblPayTypes, Resources.Controls.AllTypes);
+                cblPayTypes.SelectItems(new List<int>() { 1, 2 });
                 if (!String.IsNullOrEmpty(StartDateFromQueryString))
                 {
                     dtpStart.DateValue = Convert.ToDateTime(StartDateFromQueryString);
                     dtpEnd.DateValue = Convert.ToDateTime(EndDateFromQueryString);
+                    cblPayTypes.SelectedItems = PayTypesFromQueryString == "null" ? null : PayTypesFromQueryString;
                     btnUpdateView_Click(btnUpdateView, new EventArgs());
                 }
             }
@@ -136,7 +149,8 @@ namespace PraticeManagement.Reports.Badge
         public void PopulateData()
         {
             lblRange.Text = dtpStart.DateValue.ToString(Constants.Formatting.EntryDateFormat) + " - " + dtpEnd.DateValue.ToString(Constants.Formatting.EntryDateFormat);
-            var resources = ServiceCallers.Custom.Report(r => r.ListBadgeResourcesByType(dtpStart.DateValue, dtpEnd.DateValue, false, false, false, true,false).ToList());
+            var paytypes = cblPayTypes.areAllSelected ? null : cblPayTypes.SelectedItems;
+            var resources = ServiceCallers.Custom.Report(r => r.ListBadgeResourcesByType(paytypes,dtpStart.DateValue, dtpEnd.DateValue, false, false, false, true, false).ToList());
             repblocked.DataSource = resources;
             repblocked.DataBind();
             if (resources.Count > 0)
@@ -156,7 +170,8 @@ namespace PraticeManagement.Reports.Badge
             var filename = string.Format("6MosBreakReport_{0}-{1}.xls", dtpStart.DateValue.ToString("MM_dd_yyyy"), dtpEnd.DateValue.ToString("MM_dd_yyyy"));
             var sheetStylesList = new List<SheetStyles>();
             var dataSetList = new List<DataSet>();
-            var report = ServiceCallers.Custom.Report(r => r.ListBadgeResourcesByType(dtpStart.DateValue, dtpEnd.DateValue, false, false, false, true,false).ToList());
+            var paytypes = cblPayTypes.areAllSelected ? null : cblPayTypes.SelectedItems;
+            var report = ServiceCallers.Custom.Report(r => r.ListBadgeResourcesByType(paytypes,dtpStart.DateValue, dtpEnd.DateValue, false, false, false, true, false).ToList());
             if (report.Count > 0)
             {
                 string dateRangeTitle = string.Format("6 mos break report for the period: {0} to {1}", dtpStart.DateValue.ToString(Constants.Formatting.EntryDateFormat), dtpEnd.DateValue.ToString(Constants.Formatting.EntryDateFormat));
@@ -221,3 +236,4 @@ namespace PraticeManagement.Reports.Badge
         }
     }
 }
+
