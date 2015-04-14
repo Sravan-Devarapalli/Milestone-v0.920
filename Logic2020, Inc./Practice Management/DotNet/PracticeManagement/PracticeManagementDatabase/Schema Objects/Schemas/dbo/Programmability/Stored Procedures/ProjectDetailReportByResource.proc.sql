021afb381ee525a7b7222da15f39c228f1908233
SainathC <SainathC>
2015-04-14 07:39:00 -0500

    Comments - Defect Number: 3308; Defect RecordID: 3304; Server Address: localhost; Server Port: 99; Database Name: PM1; Defect Summary: Additions to Burn Report Export; Defect Description: From: Nick Callahan [mailto:nickc@logic2020.com] Sent: Thursday, April 02, 2015 5:31 PM To: Harshitha Tenneti Subject: RE: FW: FYI | Status on Additions to Burn Report Export
    I made a mistake.we need these in the By Resource view L
    
    From: Harshitha Tenneti [mailto:harshithat@ggktech.com]
    Sent: Thursday, April 2, 2015 3:59 AM
    To: Nick Callahan
    Cc: Tammie Knight
    Subject: RE: FW: FYI | Status on Additions to Burn Report Export
    
    Hi Nick,
    
    We have updated the changes to UAT. Please review and let us know in case of any changes.
    
    Note:
    1. The projected hours columns that was supposed to be added with this change is already present in the excel export.
    2. The Bill rate column will be the sum of bill rates of all the employees in the milestone for the current week.
    
    Please let us know when can we deploy this to Production.
    
    --
    Thank you,
    Harshitha Tenneti | @GGKTech| Office: +91-40-67670404| www.ggktech.com | Delivering Commitments.@
    Tower 2.1, Level 13, Waverock building, APIIC IT/ITES SEZ, Gachibowli(Nanakramguda), Hyderabad  500008
    GGKTechnologiesrespects your privacy. This email is intended only for the use of the party to which it is addressed and may contain information that is privileged, confidential, or protected by law. If you have received this message in error, or do not want to receive any further emails from us, please notify us immediately by replying to the message and deleting it from your computer.
    
    From: Nick Callahan [mailto:nickc@logic2020.com]
    Sent: Wednesday, March 25, 2015 10:12 PM
    To: Harshitha Tenneti
    Cc: Tammie Knight
    Subject: RE: FW: FYI | Status on Additions to Burn Report Export
    
    Answers inline below.
    
    From: Harshitha Tenneti [mailto:harshithat@ggktech.com]
    Sent: Wednesday, March 25, 2015 2:00 AM
    To: Nick Callahan
    Subject: Re: FW: FYI | Status on Additions to Burn Report Export
    
    Hi Nick,
    
    Please answer us for the following questions:
    
    1. What is 'week summary' that is mentioned in requirement? Per our discussion this morning: Reports  Time  Reports  By Time  Range: This Week, View: By Project
    2. Do we need to make changes in detail tab of "reports > time reports >By Project" screen? No
    3. Do we also need to show the mentioned columns in the UI of the detail report? No. But the fields DO need to be added to the Export: Excel contents.
    4. Regarding "On any day that you run the report, for 'projected' hours ensure that they equal 'actual' hours as the time has already occurred.", the projected hours will not equal actual hours in the following scenarios: - ignore this requirement. I spoke to Josh and it is not required.
    a. When the report is run for future days
    b. Report is run for past days, but the person did not enter time entries for the days
    c. Report is run for past days, but the person did not enter time entries equal to his projected time, for example: persons projected time =8 hrs., he entered time for 7 hrs.
    --
    Thank you,
    Harshitha Tenneti |GGKTech|Direct: +91-40-67670404|www.ggktech.com|Delivering Commitments
    Tower 2.1, Level 13, Waverock building, APIIC IT/ITES SEZ, Gachibowli(Nanakramguda), Hyderabad  500008
    GGKTechnologiesrespects your privacy. This email is intended only for the use of the party to which it is addressed and may contain information that is privileged, confidential, or protected by law. If you have received this message in error, or do not want to receive any further emails from us, please notify us immediately by replying to the message and deleting it from your computer.
    
    On Wed, Mar 25, 2015 at 5:19 AM, Nick Callahan <nickc@logic2020.com> wrote:
    
    Harshithat,
    
    Can I please get an estimate on the effort to execute the following:
    
    >> Add 3 fields to
    @@
    reports > time reports > project summary detail export: week summary, bill rate, and pro

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/ProjectDetailReportByResource.proc.sql
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/ProjectDetailReportByResource.proc.sql
@@ -10,7 +10,8 @@ CREATE PROCEDURE [dbo].[ProjectDetailReportByResource]
 	  @MilestoneId INT = NULL ,
 	  @StartDate DATETIME = NULL ,
 	  @EndDate DATETIME = NULL ,
-	  @PersonRoleNames NVARCHAR(MAX) = NULL
+	  @PersonRoleNames NVARCHAR(MAX) = NULL,
+	  @IsExport BIT = 0 
 	)
 AS 
 	BEGIN
@@ -35,9 +36,14 @@ AS
 		FROM    dbo.Project AS P
 		WHERE   P.ProjectNumber = @ProjectNumberLocal
 				AND @ProjectNumberLocal != 'P999918' --Business Development Project 
-
-		IF ( @ProjectId IS NOT NULL ) 
-			BEGIN
+		DECLARE @PersonRoleNamesTable TABLE
+			(
+				RoleName NVARCHAR(1024)
+			)
+		IF(@IsExport = 0)
+		BEGIN
+			IF ( @ProjectId IS NOT NULL ) 
+																																																																																																																																																												BEGIN
 
 				SET @Today = dbo.GettingPMTime(GETUTCDATE())
 				SET @MilestoneIdLocal = @MilestoneId
@@ -61,10 +67,7 @@ AS
 						WHERE   M.MilestoneId = @MilestoneIdLocal 
 					END
 
-				DECLARE @PersonRoleNamesTable TABLE
-					(
-					  RoleName NVARCHAR(1024)
-					)
+				
 				INSERT  INTO @PersonRoleNamesTable
 						SELECT  ResultString
 						FROM    [dbo].[ConvertXmlStringInToStringTable](@PersonRoleNames);
@@ -197,9 +200,181 @@ AS
 								  TEP.TimeTypeName
 
 			END
+			ELSE 
+				BEGIN
+					RAISERROR('There is no Project with this Project Number.', 16, 1)
+				END
+		END
+		ELSE
+		BEGIN
+		    IF ( @ProjectId IS NOT NULL ) 
+			BEGIN
+
+				SET @Today = dbo.GettingPMTime(GETUTCDATE())
+				SET @MilestoneIdLocal = @MilestoneId
+				SET @HolidayTimeType = dbo.GetHolidayTimeTypeId()
+				SET @ORTTimeTypeId = dbo.GetORTTimeTypeId()
+				SET @UnpaidTimeTypeId = dbo.GetUnpaidTimeTypeId()
+
+				IF ( @StartDate IS NOT NULL
+					 AND @EndDate IS NOT NULL
+				   ) 
+					BEGIN
+						SET @StartDateLocal = CONVERT(DATE, @StartDate)
+						SET @EndDateLocal = CONVERT(DATE, @EndDate)
+					END
+	
+				IF ( @MilestoneIdLocal IS NOT NULL ) 
+					BEGIN
+						SELECT  @MilestoneStartDate = M.StartDate ,
+								@MilestoneEndDate = M.ProjectedDeliveryDate
+						FROM    dbo.Milestone AS M
+						WHERE   M.MilestoneId = @MilestoneIdLocal 
+					END
+
+				INSERT  INTO @PersonRoleNamesTable
+						SELECT  ResultString
+						FROM    [dbo].[ConvertXmlStringInToStringTable](@PersonRoleNames);
+
+						WITH PersonForeCastedHoursRoleValues
+					AS
+					(	
+						SELECT   MP.PersonId ,
+								AVG(MPE.Amount) AS BillRate,
+								PC.Date,								
+								SUM(dbo.PersonProjectedHoursPerDay(PC.DayOff,PC.CompanyDayOff,PC.TimeOffHours,MPE.HoursPerDay)) AS ForecastedHours,
+								MAX(ISNULL(PR.RoleValue, 0)) AS MaxRoleValue ,
+								MIN(CAST(M.IsHourlyAmount AS INT)) MinimumValue ,
+								MAX(CAST(M.IsHourlyAmount AS INT)) MaximumValue 
+						FROM     dbo.MilestonePersonEntry AS MPE
+								INNER JOIN dbo.MilestonePerson AS MP ON MP.MilestonePersonId = MPE.MilestonePersonId
+								INNER JOIN dbo.Milestone AS M ON M.MilestoneId = MP.MilestoneId
+								INNER JOIN dbo.person AS P ON P.PersonId = MP.PersonId AND P.IsStrawman = 0
+								INNER JOIN dbo.PersonCalendarAuto PC ON PC.PersonId = MP.PersonId
+														AND PC.Date BETWEEN MPE.StartDate AND MPE.EndDate	
+								LEFT  JOIN dbo.PersonRole AS PR ON PR.PersonRoleId = MPE.PersonRoleId
+						WHERE    M.ProjectId = @ProjectId
+								AND ( @MilestoneIdLocal IS NULL
+										OR M.MilestoneId = @MilestoneIdLocal
+									)
+								AND ( ( @StartDateLocal IS NULL
+										AND @EndDateLocal IS NULL
+										)
+										OR ( PC.Date BETWEEN @StartDateLocal AND @EndDateLocal )
+									)
+								AND PC.DayOff = 0
+								AND M.ProjectId <> 174
+						GROUP BY MP.PersonId,PC.Date 
+					)
+					,PersonForeCastedHours
+					AS
+					(
+					SELECT p.personid,SUM(ForecastedHours) AS ForecastedHours
+					FROM PersonForeCastedHoursRoleValues p
+					GROUP BY p.personid
+					)
+					,TimeEntryPersons AS
+					(
+						SELECT TE.*, TEH.IsChargeable, TEH.ActualHours, CC.ProjectId, CC.TimeEntrySectionId, PTSH.PersonStatusId, TT.TimeTypeId, TT.Name AS 'TimeTypeName', TT.Code AS 'TimeTypeCode'
+						FROM TimeEntry TE
+						INNER JOIN dbo.ChargeCode AS CC ON CC.Id = TE.ChargeCodeId AND CC.ProjectId = @ProjectId
+						INNER JOIN dbo.TimeEntryHours AS TEH ON TEH.TimeEntryId = TE.TimeEntryId
+																  AND ( ( @MilestoneIdLocal IS NULL )
+																	OR ( TE.ChargeCodeDate BETWEEN @MilestoneStartDate AND @MilestoneEndDate )
+																  )
+																  AND ( ( @StartDateLocal IS NULL AND @EndDateLocal IS NULL )
+																	OR ( TE.ChargeCodeDate BETWEEN @StartDateLocal AND @EndDateLocal )
+																  )
+						INNER JOIN dbo.PersonStatusHistory PTSH ON PTSH.PersonId = TE.PersonId
+															  AND TE.ChargeCodeDate BETWEEN PTSH.StartDate
+															  AND ISNULL(PTSH.EndDate,@FutureDate)
+						INNER JOIN dbo.TimeType TT ON TT.TimeTypeId = CC.TimeTypeId
+					)
+
+						SELECT	P.PersonId ,
+								P.LastName ,
+								P.FirstName ,
+								P.IsOffshore ,
+								TEP.TimeTypeName AS TimeTypeName ,
+								TEP.TimeTypeCode AS TimeTypeCode ,
+								isnull(TEP.ChargeCodeDate,PFR.Date) as ChargeCodeDate,
+								( CASE WHEN  TEP.TimeEntrySectionId = 4
+									   THEN TEP.Note
+											+ dbo.GetApprovedByName(isnull(TEP.ChargeCodeDate,PFR.Date),
+																  TEP.TimeTypeId,
+																  P.PersonId)
+									   ELSE TEP.Note
+								  END ) AS Note,
+								(
+								  CASE 
+										WHEN ( (PFR.MinimumValue = PFR.MaximumValue AND PFR.MinimumValue = 1) 
+												OR PFR.MinimumValue <> PFR.MaximumValue) 
+										THEN ISNULL(PFR.BillRate,0) 
+										ELSE 0 
+								END)  BillRate,
+								TEP.TimeEntrySectionId ,
+								P.EmployeeNumber,
+
+								ROUND(SUM(CASE WHEN TEP.IsChargeable = 1  AND @ProjectNumberLocal != 'P031000'
+										   THEN TEP.ActualHours
+										   ELSE 0
+									  END), 2) AS BillableHours ,
+							ROUND(SUM(CASE WHEN TEP.IsChargeable = 0  OR @ProjectNumberLocal = 'P031000'
+										   THEN TEP.ActualHours
+										   ELSE 0
+									  END), 2) AS NonBillableHours ,
+							ISNULL(PR.Name, '') AS ProjectRoleName ,
+							ROUND(MAX(ISNULL(PFH.ForecastedHours, 0)), 2) AS ForecastedHours,
+							ROUND(MAX(ISNULL(PFR.ForecastedHours, 0)), 2) AS ForecastedHoursDaily,
+							CASE	WHEN ( PFR.MinimumValue IS NULL ) THEN ''
+										WHEN ( PFR.MinimumValue = PFR.MaximumValue AND PFR.MinimumValue = 0 ) THEN 'Fixed'
+	 									WHEN ( PFR.MinimumValue = PFR.MaximumValue AND PFR.MinimumValue = 1) THEN 'Hourly'
+								ELSE 'Both'
+								END  AS BillingType
+						FROM PersonForeCastedHoursRoleValues	PFR
+						FULL JOIN TimeEntryPersons TEP ON TEP.PersonId = PFR.PersonId AND TEP.ChargeCodeDate = PFR.Date
+						INNER JOIN dbo.person P ON P.personid = ISNULL(PFR.PersonId,TEP.PersonId)
+						LEFT JOIN PersonForeCastedHours PFH ON p.personid = PFH.personid
+						LEFT  JOIN dbo.PersonRole AS PR ON PR.RoleValue = PFR.MaxRoleValue
+						WHERE ( (TEP.TimeEntryId IS NOT NULL 
+									AND TEP.ChargeCodeDate <= ISNULL(P.TerminationDate,@FutureDate)
+								    AND ( TEP.timeTypeId != @HolidayTimeType
+										 OR ( TEP.timeTypeId = @HolidayTimeType
+											  AND TEP.PersonStatusId IN (1,5)
+											)
+									   )
+								 )
+							 OR PFR.PersonId IS NOT NULL
+							)
+							AND ( @PersonRoleNames IS NULL
+								  OR ISNULL(PR.Name, '') IN (
+								  SELECT    RoleName
+								  FROM      @PersonRoleNamesTable )
+								)
+						GROUP BY	P.personid,
+									P.LastName ,
+									P.FirstName ,
+									P.IsOffshore ,
+									P.EmployeeNumber,
+									TEP.TimeTypeName,
+									TEP.TimeTypeCode ,
+									isnull(TEP.ChargeCodeDate,PFR.Date) ,
+									TEP.TimeTypeId ,
+									TEP.TimeEntrySectionId,
+									TEP.Note,
+									PFR.MinimumValue,
+									PFR.MaximumValue,
+									PFR.BillRate,
+									PR.Name
+						ORDER BY  P.LastName ,
+								  P.FirstName ,
+								  isnull(TEP.ChargeCodeDate,PFR.Date) ,
+								  TEP.TimeTypeName
+			END
 		ELSE 
 			BEGIN
 				RAISERROR('There is no Project with this Project Number.', 16, 1)
 			END
+		END
 	END
 
