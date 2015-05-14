06b499be95a673b1f480c0ef005217901d02fb4c
SainathC <SainathC>
2015-05-14 09:29:00 -0500

    Comments - Defect Number: 3301; Defect RecordID: 3297; Server Address: localhost; Server Port: 99; Database Name: PM1; Defect Summary: New Microsoft 18 months requirement; Defect Description: Harshithat  As we have discussed previously, Microsoft has implemented a new policy that states that after working (being badged) for 18 months, contractors must not work for Microsoft for a 6 month period. After that 6 month period, they can come back and work for another 18 months. We need a way to track how long each of our consultants have been badged at Microsoft as well as track their 6 mo. absence from working at Microsoft. It is important to note that the dates that are being tracked are whether or not the consultant has a Microsoft badge (which allows access to the systems and facilities). A consultant could work on a Microsoft project without a badge (remotely and not needing to use information in their systems) and this time would not be counted against the new policy.
    I have attached a DRAFT of some requirements for being able to track this. Id like to set up a separate call (does next Monday at 8am our time work?) to discuss adding this tracking functionality in PM.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Views/v_CurrentMSBadge.view.sql
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Views/v_CurrentMSBadge.view.sql
@@ -1,19 +1,39 @@
 ﻿CREATE VIEW [dbo].[v_CurrentMSBadge]
 	AS 
+	--With HistoryCTE
+	--AS
+	--(
+	--	SELECT *,DATEADD(mm,6,ProjectPlannedEndDate) as ReportRunMax,ROW_NUMBER() OVER(PARTITION BY PersonId ORDER BY badgestartdate) RNum
+	--	FROM dbo.BadgeHistoryForReports
+	--),
+	--HistoryWithRunDates
+	--AS
+	--(
+	--	SELECT c1.PersonId,c1.BadgeStartDate,c1.BadgeEndDate,c1.ProjectPlannedEndDate,c1.BreakStartDate,c1.BreakEndDate,CASE WHEN c1.rnum = 1 THEN NULL ELSE (SELECT ReportRunMax+1 FROM HistoryCTE c2 WHERE c2.PersonId = c1.PersonId and c2.RNum = c1.RNum-1) END AS ReportRunMin,
+	--	       CASE WHEN c1.rnum =(SELECT MAX(C3.RNUM) FROM HistoryCTE c3 WHERE c3.PersonId = c1.PersonId GROUP BY c3.PersonId) THEN NULL ELSE ReportRunMax END AS ReportRunMax 
+	--	FROM HistoryCTE c1
+	--)
+	--SELECT HR.*,M.BlockStartDate,M.BlockEndDate,M.DeactivatedDate,M.ExcludeInReports
+	--FROM HistoryWithRunDates HR 
+	--INNER JOIN dbo.MSBadge M ON M.PersonId = HR.PersonId
+	--WHERE (HR.ReportRunMin is null or dbo.GettingPMTime(GETUTCDATE()) >= HR.ReportRunMin) and (HR.ReportRunMax is null or dbo.GettingPMTime(GETUTCDATE()) <= HR.ReportRunMax)
 	With HistoryCTE
 	AS
 	(
-		SELECT *,DATEADD(mm,6,ProjectPlannedEndDate) as ReportRunMax,ROW_NUMBER() OVER(PARTITION BY PersonId ORDER BY badgestartdate) RNum
-		FROM dbo.BadgeHistoryForReports
+		SELECT Id,PersonId,BadgeStartDate,ROW_NUMBER() OVER (PARTITION BY PersonId ORDER BY BadgeStartDate) RNum
+		FROM dbo.BadgeHistoryForReports 
 	),
-	HistoryWithRunDates
+	H1
 	AS
 	(
-		SELECT c1.PersonId,c1.BadgeStartDate,c1.BadgeEndDate,c1.ProjectPlannedEndDate,c1.BreakStartDate,c1.BreakEndDate,CASE WHEN c1.rnum = 1 THEN NULL ELSE (SELECT ReportRunMax+1 FROM HistoryCTE c2 WHERE c2.PersonId = c1.PersonId and c2.RNum = c1.RNum-1) END AS ReportRunMin,
-		       CASE WHEN c1.rnum =(SELECT MAX(C3.RNUM) FROM HistoryCTE c3 WHERE c3.PersonId = c1.PersonId GROUP BY c3.PersonId) THEN NULL ELSE ReportRunMax END AS ReportRunMax 
-		FROM HistoryCTE c1
+		SELECT C1.Id, C1.PersonId, CASE WHEN C1.RNum = 1 THEN NULL ELSE C1.BadgeStartDate END AS StartDate,C2.BadgeStartDate-1 AS EndDate,C1.RNum
+		FROM HistoryCTE C1
+		LEFT JOIN HistoryCTE C2 ON C2.PersonId = C1.PersonId AND C2.RNum = C1.RNum + 1
 	)
-	SELECT HR.*,M.BlockStartDate,M.BlockEndDate 
-	FROM HistoryWithRunDates HR 
-	INNER JOIN dbo.MSBadge M ON M.PersonId = HR.PersonId
-	WHERE (HR.ReportRunMin is null or dbo.GettingPMTime(GETUTCDATE()) >= HR.ReportRunMin) and (HR.ReportRunMax is null or dbo.GettingPMTime(GETUTCDATE()) <= HR.ReportRunMax)
+	SELECT B.*,M.IsBlocked,M.BlockStartDate,M.BlockEndDate,M.DeactivatedDate,M.ExcludeInReports
+	FROM H1
+	INNER JOIN BadgeHistoryForReports B ON B.PersonId = H1.PersonId AND B.Id = H1.Id
+	INNER JOIN dbo.MSBadge M ON M.PersonId = H1.PersonId
+	WHERE (H1.StartDate IS NULL OR H1.StartDate <= dbo.GettingPMTime(GETUTCDATE())) AND (H1.EndDate IS NULL OR dbo.GettingPMTime(GETUTCDATE()) <= H1.EndDate)
+
+
