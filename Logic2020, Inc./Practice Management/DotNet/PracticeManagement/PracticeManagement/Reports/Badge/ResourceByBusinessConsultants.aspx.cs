89908da09c5814f82843b614ca5951435d954282
SainathC <SainathC>
2015-05-14 09:29:00 -0500

    Comments - Defect Number: 3301; Defect RecordID: 3297; Server Address: localhost; Server Port: 99; Database Name: PM1; Defect Summary: New Microsoft 18 months requirement; Defect Description: Harshithat  As we have discussed previously, Microsoft has implemented a new policy that states that after working (being badged) for 18 months, contractors must not work for Microsoft for a 6 month period. After that 6 month period, they can come back and work for another 18 months. We need a way to track how long each of our consultants have been badged at Microsoft as well as track their 6 mo. absence from working at Microsoft. It is important to note that the dates that are being tracked are whether or not the consultant has a Microsoft badge (which allows access to the systems and facilities). A consultant could work on a Microsoft project without a badge (remotely and not needing to use information in their systems) and this time would not be counted against the new policy.
    I have attached a DRAFT of some requirements for being able to track this. Id like to set up a separate call (does next Monday at 8am our time work?) to discuss adding this tracking functionality in PM.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/Reports/Badge/ResourceByBusinessConsultants.aspx.cs
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/Reports/Badge/ResourceByBusinessConsultants.aspx.cs
@@ -99,6 +99,14 @@ namespace PraticeManagement.Reports.Badge
             }
         }
 
+        public string SelectedPersonStatuses
+        {
+            get
+            {
+                return filter.PersonStatus;
+            }
+        }
+
         public int SelectedView
         {
             get
@@ -192,7 +200,7 @@ namespace PraticeManagement.Reports.Badge
                 return;
             }
             divWholePage.Style.Remove("display");
-            var data = ServiceCallers.Custom.Report(r => r.ResourcesByTitleReport(SelectedPayTypes,SelectedTitles, dtpStart.DateValue, dtpEnd.DateValue, SelectedView)).ToList();
+            var data = ServiceCallers.Custom.Report(r => r.ResourcesByTitleReport(SelectedPayTypes, SelectedPersonStatuses,SelectedTitles, dtpStart.DateValue, dtpEnd.DateValue, SelectedView)).ToList();
             TitleList = data;
             if (TitleList.Count > 0)
             {
@@ -458,7 +466,7 @@ namespace PraticeManagement.Reports.Badge
             var filename = string.Format("AvailableResourceByBusinessConsultants_{0}-{1}.xls", dtpStart.DateValue.ToString("MM_dd_yyyy"), dtpEnd.DateValue.ToString("MM_dd_yyyy"));
             var sheetStylesList = new List<SheetStyles>();
             var dataSetList = new List<DataSet>();
-            var report = ServiceCallers.Custom.Report(r => r.ResourcesByTitleReport(SelectedPayTypes,SelectedTitles, dtpStart.DateValue, dtpEnd.DateValue, SelectedView)).ToList();
+            var report = ServiceCallers.Custom.Report(r => r.ResourcesByTitleReport(SelectedPayTypes, SelectedPersonStatuses, SelectedTitles, dtpStart.DateValue, dtpEnd.DateValue, SelectedView)).ToList();
             TitleList = report;
             if (TitleList.Count > 0)
             {
