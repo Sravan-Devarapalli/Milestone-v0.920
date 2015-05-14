5664fa244f46afa73fc5351975fe0b0863352fbb
SainathC <SainathC>
2015-05-14 09:29:00 -0500

    Comments - Defect Number: 3301; Defect RecordID: 3297; Server Address: localhost; Server Port: 99; Database Name: PM1; Defect Summary: New Microsoft 18 months requirement; Defect Description: Harshithat  As we have discussed previously, Microsoft has implemented a new policy that states that after working (being badged) for 18 months, contractors must not work for Microsoft for a 6 month period. After that 6 month period, they can come back and work for another 18 months. We need a way to track how long each of our consultants have been badged at Microsoft as well as track their 6 mo. absence from working at Microsoft. It is important to note that the dates that are being tracked are whether or not the consultant has a Microsoft badge (which allows access to the systems and facilities). A consultant could work on a Microsoft project without a badge (remotely and not needing to use information in their systems) and this time would not be counted against the new policy.
    I have attached a DRAFT of some requirements for being able to track this. Id like to set up a separate call (does next Monday at 8am our time work?) to discuss adding this tracking functionality in PM.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/BadgeHistoryForReports.table.sql
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/BadgeHistoryForReports.table.sql
@@ -7,6 +7,9 @@
 	ProjectPlannedEndDate		DATETIME	NULL,
 	BreakStartDate		DATETIME	NULL,
 	BreakEndDate		DATETIME	NULL,
+	BadgeStartDateSource NVARCHAR(30) NULL,
+	PlannedEndDateSource NVARCHAR(30) NULL,
+	BadgeEndDateSource NVARCHAR(30) NULL,
 	CONSTRAINT PK_BadgeHistoryForReports PRIMARY KEY (Id)
 )
 
