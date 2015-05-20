bb65d8dc7429176501148fcba33926dae9d8fc84
SainathC <SainathC>
2015-05-20 02:31:00 -0500

    Comments - Defect Number: 3285; Defect RecordID: 3281; Server Address: localhost; Server Port: 99; Database Name: PM1; Defect Summary: Restructuring of the reports in PM; Defect Description: Following is a proposed restructuring of the reports available in PM based on recently conversations about exported reports not consistent with the on-screen reports. Currently Practice Manager provides reports from several different menu options, and provides an Export button. The spreadsheets exported in several cases include more fields than were displayed in the on-screen report. We would like to change that in the following way (some of these items are just informational and dont require action): 1. Consulting Demand Graphs Tab Resource View Consulting Demand Summary Tab a. Export for Summary should provide only what is shown on screen. b. Export for Detail should provide what is currently being exported for Summary plus Sales Stage column. 2. CSAT a. Since this report is run from the Reports menu, it is okay that it contains additional columns, assuming security roles limit who has access to the report. No action necessary. 3. Project Feedback a. Since this report is run from the Reports menu, it is okay that it contains additional columns, assuming security roles limit who has access to the report. b. However, the page view includes Resource Title where the Excel export does not. Export should include Resource Title. 4. New Hire Report a. Since this report is run from the Reports menu, no action necessary. 5. Termination Report a. Since this report is run from the Reports menu, no action necessary. 6. Opportunity List a. In Page View, Sales Team should be renamed to Sales Person (as is named in Excel export) b. Export button needs to be moved to top of form, not at bottom. c. It does not look like Excel export contains more than 1 or 2 additional fields than page view, and those fields are not critical. d. Recommend leaving this report where it is. 7. Persons List a. Move Export button to top of form. b. If page view shows only Active employees, Excel export should as well. Filter by Active employees. c. The export for this report should include only the fields in the page view. d. Additionally, this report's expanded view should be available under the Reports menu by the System Admin role. Columns shown should be all of the columns that are currently in the exported report (Termination Date, pay rates, etc.) 8. Project Summary a. Export button needs to be at top of form, not bottom. b. Excel export should export only what is provided in page view. c. Excel export should not have 1 row for Revenue and another nearly identical row for Contribution Margin. There should only be 1 row and Contribution Margin should be another column. d. The detail that is currently provided in the export of this report should be available under the Report menu item. It should show on-screen in the page view all of the columns that in the current Excel export. 9. Time by Person a. Extra columns are okay to keep in the Excel export - Account, Account Name, Business Unit, Business Unit Name, Project #. No further action needed. 10. Time by Project a. Extra columns are okay to keep in the Excel export. No further action needed. 11. Time by Time Period a. Extra columns are okay to keep in the Excel export. No further action needed. 12. Time-Audit a. Extra columns are okay to keep in the Excel export. No further action needed. 13. Time, Calendar a. The Export button on this form should export only Paid Time Off for the individual selected, not the whole company. b. Add a report under Reports/Time menu titled Time Off. It should show on the screen all of the columns that are currently being exported from the Time Calendar  Export button, and an Export button provided on this screen. This report should be available to all Roles except Consultant.
    Is it poss

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/Controls/Reports/ConsultantDemand/ConsultingDemandDetails.ascx.designer.cs
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/Controls/Reports/ConsultantDemand/ConsultingDemandDetails.ascx.designer.cs
@@ -94,24 +94,6 @@ namespace PraticeManagement.Controls.Reports.ConsultantDemand {
         protected global::System.Web.UI.WebControls.HiddenField hdIsSummaryPage;
         
         /// <summary>
-        /// lblExport control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Label lblExport;
-        
-        /// <summary>
-        /// btnExport control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Button btnExport;
-        
-        /// <summary>
         /// repByTitleSkill control.
         /// </summary>
         /// <remarks>
