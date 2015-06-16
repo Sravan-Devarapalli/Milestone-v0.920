2bbb6ff119ea626914394618b1e21f58f5d2ecbd
SainathC <SainathC>
2015-06-16 02:28:00 -0500

    Comments - Defect Number: 3301; Defect RecordID: 3297; Server Address: localhost; Server Port: 99; Database Name: PM1; Defect Summary: New Microsoft 18 months requirement; Defect Description: Harshithat  As we have discussed previously, Microsoft has implemented a new policy that states that after working (being badged) for 18 months, contractors must not work for Microsoft for a 6 month period. After that 6 month period, they can come back and work for another 18 months. We need a way to track how long each of our consultants have been badged at Microsoft as well as track their 6 mo. absence from working at Microsoft. It is important to note that the dates that are being tracked are whether or not the consultant has a Microsoft badge (which allows access to the systems and facilities). A consultant could work on a Microsoft project without a badge (remotely and not needing to use information in their systems) and this time would not be counted against the new policy.
    I have attached a DRAFT of some requirements for being able to track this. Id like to set up a separate call (does next Monday at 8am our time work?) to discuss adding this tracking functionality in PM.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/GetAllBadgeDetails.proc.sql
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/GetAllBadgeDetails.proc.sql
@@ -35,10 +35,10 @@ BEGIN
 			P.TitleId,
 			P.Title
 	FROM dbo.v_Person P
-	LEFT JOIN dbo.MSBadge M ON M.PersonId = P.PersonId
+	LEFT JOIN v_CurrentMSBadge M ON M.PersonId = P.PersonId
 	INNER JOIN dbo.GetCurrentPayTypeTable() CP ON CP.PersonId = P.PersonId
 	LEFT JOIN dbo.Timescale T ON T.TimescaleId = CP.Timescale
-	WHERE M.ExcludeInReports = 0 AND P.PersonStatusId IN (1,5) -- Active and Termination Pending
+	WHERE ISNULL(M.ExcludeInReports,0) = 0  AND P.PersonStatusId IN (1,5) -- Active and Termination Pending
 	AND P.IsStrawman = 0
 	AND (@PayTypeIds IS NULL OR CP.Timescale IN (SELECT Ids FROM @PayTypeIdsTable))
 	AND P.PersonStatusId IN (SELECT Ids FROM @PersonStatusIdsTable)
