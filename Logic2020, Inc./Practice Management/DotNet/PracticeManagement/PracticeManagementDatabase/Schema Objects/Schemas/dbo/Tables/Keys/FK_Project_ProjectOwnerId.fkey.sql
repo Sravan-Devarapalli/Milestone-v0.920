d9504b590144f19ff8f39f3258fa4dc8cbe5dc57
SainathC <SainathC>
2015-06-23 11:09:00 -0500

    Comments - Defect Number: 3312; Defect RecordID: 3308; Server Address: localhost; Server Port: 99; Database Name: PM1; Defect Summary: E00046 - ANALYSIS | PROJECT DETAILS - CHANGING FIELDS, UPDATING REPORT ; Defect Description: ANALYSIS | PROJECT DETAILS - CHANGING FIELDS, UPDATING REPORT (see picture for details) *Please send info on how these fields are currently being used before any changes are in effect (which reports do they feed into, how security works for each field) *Need to update fields on Project Details page (see picture) *Need Project Summary report to include updated fields (see picture)

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/Keys/FK_Project_ProjectOwnerId.fkey.sql
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/Keys/FK_Project_ProjectOwnerId.fkey.sql
@@ -3,5 +3,5 @@
 -- Create Date: 2012-05-21
 ---------------------------
 ALTER TABLE [dbo].[Project]
-    ADD CONSTRAINT [FK_Project_ProjectOwnerId] FOREIGN KEY ([ProjectOwnerId]) 
+    ADD CONSTRAINT [FK_Project_ProjectOwnerId] FOREIGN KEY ([ProjectManagerId]) 
 	REFERENCES [dbo].[Person] ([PersonId]) ON DELETE NO ACTION ON UPDATE NO ACTION;
