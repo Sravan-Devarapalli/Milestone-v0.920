b0220666ee2bfaa0f9abd439d7a03fec707cd9e4
SainathC <SainathC>
2015-06-23 11:09:00 -0500

    Comments - Defect Number: 3312; Defect RecordID: 3308; Server Address: localhost; Server Port: 99; Database Name: PM1; Defect Summary: E00046 - ANALYSIS | PROJECT DETAILS - CHANGING FIELDS, UPDATING REPORT ; Defect Description: ANALYSIS | PROJECT DETAILS - CHANGING FIELDS, UPDATING REPORT (see picture for details) *Please send info on how these fields are currently being used before any changes are in effect (which reports do they feed into, how security works for each field) *Need to update fields on Project Details page (see picture) *Need Project Summary report to include updated fields (see picture)

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/PersonDetail.aspx.designer.cs
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/PersonDetail.aspx.designer.cs
@@ -157,6 +157,15 @@ namespace PraticeManagement {
         protected global::PraticeManagement.Controls.Configuration.DefaultUser defaultManager;
         
         /// <summary>
+        /// lblCohort control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblCohort;
+        
+        /// <summary>
         /// ddlCohortAssignment control.
         /// </summary>
         /// <remarks>
