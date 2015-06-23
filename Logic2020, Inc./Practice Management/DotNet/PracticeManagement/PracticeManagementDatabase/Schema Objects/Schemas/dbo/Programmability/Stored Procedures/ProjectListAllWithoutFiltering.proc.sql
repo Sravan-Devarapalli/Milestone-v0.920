2189bf2f577abd3d37fc6e2ed496dccb27d3fc7f
SainathC <SainathC>
2015-06-23 11:09:00 -0500

    Comments - Defect Number: 3312; Defect RecordID: 3308; Server Address: localhost; Server Port: 99; Database Name: PM1; Defect Summary: E00046 - ANALYSIS | PROJECT DETAILS - CHANGING FIELDS, UPDATING REPORT ; Defect Description: ANALYSIS | PROJECT DETAILS - CHANGING FIELDS, UPDATING REPORT (see picture for details) *Please send info on how these fields are currently being used before any changes are in effect (which reports do they feed into, how security works for each field) *Need to update fields on Project Details page (see picture) *Need Project Summary report to include updated fields (see picture)

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/ProjectListAllWithoutFiltering.proc.sql
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/ProjectListAllWithoutFiltering.proc.sql
@@ -26,7 +26,7 @@ AS
 	       p.ClientIsChargeable,
 	       p.ProjectIsChargeable,
 		   p.ProjectManagersIdFirstNameLastName,
-		   p.DirectorId,
+		   p.ExecutiveInChargeId AS DirectorId,
 		   p.DirectorLastName,
 		   p.DirectorFirstName,
 		   pg.Name AS GroupName,
@@ -42,14 +42,18 @@ AS
 			CASE WHEN p.IsSeniorManagerUnassigned = 1 THEN 'Unassigned' ELSE  sm.LastName+', ' +sm.FirstName END AS 'SeniorManagerName',
 		   re.PersonId AS 'ReviewerId',
 		   re.LastName+', ' +re.FirstName AS 'ReviewerName',
-		   p.PONumber
+		   p.PONumber,
+		   P.ProjectManagerId AS ProjectOwnerId,
+		   Powner.LastName AS [ProjectOwnerLastName],
+		   Powner.FirstName AS [ProjectOwnerFirstName]
 	FROM v_Project p
 	LEFT JOIN dbo.ProjectGroup AS pg ON p.GroupId = pg.GroupId
 	LEFT JOIN dbo.Person cp ON cp.PersonId = p.SalesPersonId
-    LEFT JOIN Person as sm on sm.PersonId = p.SeniorManagerId
+    LEFT JOIN Person as sm on sm.PersonId = p.EngagementManagerId
 	LEFT JOIN Person as re on re.PersonId = p.ReviewerId
 	LEFT JOIN dbo.BusinessGroup AS BG ON PG.BusinessGroupId=BG.BusinessGroupId
 	LEFT JOIN dbo.PricingList AS PL ON P.PricingListId=PL.PricingListId 
+	LEFT JOIN dbo.Person AS Powner ON Powner.PersonId = P.ProjectManagerId
 	WHERE P.ProjectId <> @DefaultProjectId
 	AND P.IsAllowedToShow = 1
 
