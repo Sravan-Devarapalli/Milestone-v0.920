695648bac49088e1b8d9498cecb4d1ca05d2727f
SainathC <SainathC>
2015-07-01 01:15:00 -0500

    Comments - Defect Number: 3314; Defect RecordID: 3310; Server Address: localhost; Server Port: 99; Database Name: PM1; Defect Summary: E00002 - Fix margin calculations; Defect Description: Margins *Fix margin calculations ---- not considering expenses/revenues for expenses *Change Margin so that it matches Financial Statement margin *Evaluate Margin Test calculation, including change management and impact of what people can see

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/FinancialsGetByProject.proc.sql
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/FinancialsGetByProject.proc.sql
@@ -53,7 +53,7 @@ AS
 		P.StartDate FinancialDate,
 		p.EndDate MonthEnd,
 		ISNULL(pf.Revenue,0)  as 'Revenue',
-		ISNULL(pf.RevenueNet,0) as 'RevenueNet',
+		ISNULL(pf.RevenueNet,0)+ISNULL(PE.ReimbursedExpenseSum,0) as 'RevenueNet',
 		CASE WHEN (pr.IsCompanyInternal = 1) THEN 0
 		ELSE ISNULL(pf.Cogs,0) END AS 'Cogs',
 		ISNULL(pf.GrossMargin,0)+((ISNULL(PE.ReimbursedExpenseSum,0)-ISNULL(PE.ExpenseSum,0)))  as 'GrossMargin',
