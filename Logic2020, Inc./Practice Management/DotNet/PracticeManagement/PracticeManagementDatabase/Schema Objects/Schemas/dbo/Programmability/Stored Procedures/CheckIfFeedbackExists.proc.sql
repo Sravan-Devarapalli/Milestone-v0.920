7375c22bf9d9e7221815033ddf7994b495ff7d41
SainathC <SainathC>
2015-07-14 08:51:00 -0500

    Comments - Defect Number: 3293; Defect RecordID: 3289; Server Address: localhost; Server Port: 99; Database Name: PM1; Defect Summary: Changes in Feedback functionality; Defect Description: Attached are some scenarios in connection with Project review reporting. We can discuss on the call today.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/CheckIfFeedbackExists.proc.sql
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/CheckIfFeedbackExists.proc.sql
@@ -1,17 +1,23 @@
 ﻿CREATE PROCEDURE [dbo].[CheckIfFeedbackExists]
 (
 	@MilestonePersonId INT=NULL,
-	@MilestoneId INT=NULL,
-	@ProjectId	INT=NULL
+	@MilestoneId	   INT=NULL,
+	@StartDate		   DATETIME=NULL,
+	@EndDate	       DATETIME=NULL
 )
 AS
 BEGIN
-    DECLARE @ProjectIdLocal INT=NULL
-	SELECT @ProjectIdLocal= ProjectId FROM dbo.Milestone WHERE @MilestoneId IS NOT NULL AND MilestoneId = @MilestoneId
+    DECLARE @ProjectIdLocal			INT=NULL,
+			@StartDateLocal			DATETIME,
+			@EndDateLocal			DATETIME,
+			@PersonStartDateLocal	DATETIME,
+			@PersonEndDateLocal		DATETIME
+	
+	SELECT @ProjectIdLocal= ProjectId, @StartDateLocal= StartDate, @EndDateLocal= ProjectedDeliveryDate FROM dbo.Milestone WHERE @MilestoneId IS NOT NULL AND MilestoneId = @MilestoneId
 	
 	IF @MilestonePersonId IS NOT NULL
 	BEGIN
-		IF EXISTS(SELECT 1 FROM dbo.ProjectFeedback WHERE MilestonePersonId = @MilestonePersonId AND FeedbackStatusId = 1)
+		IF EXISTS(SELECT 1 FROM dbo.ProjectFeedback WHERE MilestonePersonId = @MilestonePersonId AND FeedbackStatusId = 1 AND ReviewPeriodStartDate <= @EndDate AND @StartDate <= ReviewPeriodEndDate)
 		BEGIN
 			SELECT CONVERT(BIT,1) Result 
 		END
@@ -23,19 +29,7 @@ BEGIN
    
     IF @MilestoneId IS NOT NULL
 	BEGIN
-		IF EXISTS(SELECT 1 FROM dbo.ProjectFeedback WHERE ProjectId = @ProjectIdLocal AND FeedbackStatusId = 1)
-		BEGIN
-			SELECT CONVERT(BIT,1) Result 
-		END
-		ELSE
-		BEGIN
-			SELECT CONVERT(BIT,0) Result
-		END
-	END
-
-	IF @ProjectId IS NOT NULL
-	BEGIN
-		IF EXISTS(SELECT 1 FROM dbo.ProjectFeedback WHERE ProjectId = @ProjectId AND FeedbackStatusId = 1)
+		IF EXISTS(SELECT 1 FROM dbo.ProjectFeedback WHERE ProjectId = @ProjectIdLocal AND FeedbackStatusId = 1 AND ReviewPeriodStartDate <= @EndDateLocal AND @StartDateLocal <= ReviewPeriodEndDate)
 		BEGIN
 			SELECT CONVERT(BIT,1) Result 
 		END
