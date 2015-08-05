5ac358b8f60ce6486a5e494880b6b7bc8db69e23
SainathC <SainathC>
2015-08-05 03:24:00 -0500

    Comments - Defect Number: 3293; Defect RecordID: 3289; Server Address: localhost; Server Port: 99; Database Name: PM1; Defect Summary: Changes in Feedback functionality; Defect Description: Attached are some scenarios in connection with Project review reporting. We can discuss on the call today.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/MilestoneResourceUpdate.proc.sql
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/MilestoneResourceUpdate.proc.sql
@@ -46,11 +46,6 @@ BEGIN
 		 
 		DELETE MPE FROM dbo.MilestonePersonEntry MPE
 		JOIN @DeleteMileStonePersonEntriesTable Temp ON Temp.MilestonePersonId = MPE.MilestonePersonId
-		 
-		DELETE PF
-		FROM dbo.ProjectFeedback PF
-		INNER JOIN dbo.MilestonePerson MP ON MP.MilestonePersonId = PF.MilestonePersonId
-		INNER JOIN @DeleteMileStonePersonEntriesTable Temp ON Temp.MilestonePersonId = MP.MilestonePersonId
 
 		DELETE MP FROM dbo.MilestonePerson MP
 		JOIN @DeleteMileStonePersonEntriesTable Temp ON Temp.MilestonePersonId = MP.MilestonePersonId
@@ -84,12 +79,6 @@ BEGIN
 		WHERE NOT EXISTS (SELECT 1 FROM dbo.MilestonePersonEntry MPE
 						 WHERE  TEMPmpe.MilestonePersonId = MPE.MilestonePersonId 
 								AND TEMPmpe.PersonRoleId = MPE.PersonRoleId)
-		DELETE PF
-		FROM dbo.ProjectFeedback PF
-		INNER JOIN dbo.MilestonePerson MP ON MP.MilestonePersonId = PF.MilestonePersonId
-		LEFT JOIN dbo.MilestonePersonEntry MPE ON MPE.MilestonePersonId = MP.MilestonePersonId
-		WHERE MPE.MilestonePersonId IS NULL AND MP.MilestoneId = @MilestoneId
-		
 		
 		DELETE MP 
 		FROM dbo.MilestonePerson MP
