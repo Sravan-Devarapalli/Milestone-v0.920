cff4b7f8052909e8c22b3051e47e371ab9e71ff9
SainathC <SainathC>
2015-08-05 03:24:00 -0500

    Comments - Defect Number: 3293; Defect RecordID: 3289; Server Address: localhost; Server Port: 99; Database Name: PM1; Defect Summary: Changes in Feedback functionality; Defect Description: Attached are some scenarios in connection with Project review reporting. We can discuss on the call today.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/InsertProjectFeedbackByMilestonePersonId.proc.sql
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/InsertProjectFeedbackByMilestonePersonId.proc.sql
@@ -1,98 +1,199 @@
 ﻿CREATE PROCEDURE [dbo].[InsertProjectFeedbackByMilestonePersonId]
 (
 	@MilestonePersonId   INT=NULL,
-	@MilestoneId	INT=NULL
+	@MilestoneId	INT=NULL,
+	@ProjectId		INT=NULL
 )
 AS
 BEGIN
 
+	DECLARE @ProjectIdLocal INT,
+	@PersonId INT
 
-DECLARE @TestTable TABLE(MilestonePersonId int,Date Datetime, RowNumber int,primary key(RowNumber, MilestonePersonId))
-
-DELETE PF
-FROM dbo.ProjectFeedback PF
-INNER JOIN dbo.MilestonePerson MP ON MP.MilestonePersonId = PF.MilestonePersonId
-INNER JOIN dbo.Milestone M ON M.MilestoneId = MP.MilestoneId
-WHERE (@MilestonePersonId IS NULL OR MP.MilestonePersonId = @MilestonePersonId)
-	  AND (@MilestoneId IS NULL OR M.MilestoneId = @MilestoneId)
-	  AND PF.FeedbackStatusId <> 1
-
-;WITH ReArrangedMilestoneRecords 
-	AS
-	(
-		SELECT ROW_NUMBER() OVER(ORDER BY MilestonePersonId) RNo,MilestonePersonId,StartDate,EndDate
-		FROM MilestonePersonEntry
-		UNION ALL
-		SELECT A.RNo,A.MilestonePersonId,B.StartDate,B.EndDate
-		FROM ReArrangedMilestoneRecords  A
-		JOIN MilestonePersonEntry B
-		ON A.MilestonePersonId = B.MilestonePersonId
-		AND A.EndDate +1 = B.StartDate
-	),
-	MilestoneRecordsContinuity
-	as
-	(
-	SELECT P.MilestonePersonId, P.StartDate,P.EndDate
-	FROM
-	(
-		SELECT A.RNo,A.MilestonePersonId,MIN(A.StartDate) StartDate,MAX(A.EndDate) EndDate
-		FROM ReArrangedMilestoneRecords A
-		GROUP BY A.RNo,A.MilestonePersonId
-	) P
-	LEFT JOIN ReArrangedMilestoneRecords B ON P.MilestonePersonId = B.MilestonePersonId AND P.StartDate BETWEEN B.StartDate AND B.EndDate AND P.RNo <> B.RNo
-	INNER JOIN dbo.MilestonePerson MP ON MP.MilestonePersonId = P.MilestonePersonId
-	INNER JOIN dbo.Milestone M ON M.MilestoneId = MP.MilestoneId
-	INNER JOIN dbo.Person Per ON Per.PersonId = MP.PersonId
-	INNER JOIN dbo.Title T ON T.TitleId = Per.TitleId
-	INNER JOIN dbo.GetCurrentPayTypeTable() GC ON GC.PersonId = Per.PersonId 
-	WHERE B.MilestonePersonId IS NULL 
-	AND DATEDIFF(WEEK,P.StartDate,P.EndDate) >= 6
-	AND (@MilestonePersonId IS NULL OR P.MilestonePersonId = @MilestonePersonId)
-	AND (@MilestoneId IS NULL OR M.MilestoneId = @MilestoneId)
-	AND Per.PersonStatusId IN (1,5) AND Per.IsStrawman = 0
-	AND T.Title NOT IN ('Senior Director','Client Director','Practice Director')
-	AND GC.Timescale = 2
-	AND M.ProjectId <> 174
-	)
-	,
-	AfterRemovingFeedbackRecords
-	as
-	(
-	SELECT  m.MilestonePersonId,c.Date
-	FROM MilestoneRecordsContinuity m
-	JOIN Calendar c on c.Date BETWEEN m.StartDate AND m.EndDate
-	LEFT JOIN ProjectFeedback pf on pf.MilestonePersonId = m.MilestonePersonId AND c.Date BETWEEN pf.ReviewPeriodStartDate AND pf.ReviewPeriodEndDate AND pf.FeedbackStatusId = 1
-	WHERE pf.ReviewPeriodStartDate IS NULL
-	)
+	SELECT @ProjectIdLocal = M.ProjectId 
+	FROM Milestone M
+	JOIN MilestonePerson MP ON MP.MilestoneId = M.MilestoneId
+	WHERE (@MilestonePersonId IS NULL OR MP.MilestonePersonId = @MilestonePersonId)
+		  AND (@MilestoneId IS NULL OR M.MilestoneId = @MilestoneId)
 	
-	INSERT INTO @TestTable
-	SELECT MilestonePersonId,ar.date,ROW_NUMBER() OVER(partition by AR.MilestonePersonId ORDER BY ar.date)
-	FROM AfterRemovingFeedbackRecords AR
-	
-	;WITH FeedbackFreeReArrangedMilestoneRecords
-	AS
-	(
-				SELECT  pc.*,DATEADD([day],
-					(-1 * 
-					( DENSE_RANK() OVER(PARTITION BY PC.MilestonePersonId ORDER BY pc.date) /* Dense Rank */ )) ,pc.date) as GroupedDate
-			FROM  @TestTable AS pc
-	),
-	FinalRanges
-	AS
-	(
-		SELECT MilestonePersonId,MIN(Date) StartDate,MAX(Date) EndDate
-		FROM FeedbackFreeReArrangedMilestoneRecords
-		GROUP BY MilestonePersonId,GroupedDate
-	)
-	INSERT INTO dbo.ProjectFeedback(ProjectId,PersonId,ReviewPeriodStartDate,ReviewPeriodEndDate,DueDate,FeedbackStatusId,IsCanceled,CompletionCertificateBy,CompletionCertificateDate,CancelationReason,MilestonePersonId,NextIntialMailSendDate)
-	SELECT M.ProjectId,MP.PersonId,C.[Date],CASE WHEN DATEADD(MONTH,3,C.Date) > P.EndDate THEN P.EndDate ELSE DATEADD(MONTH,3,C.Date)-1 END,CASE WHEN DATEADD(MONTH,3,C.Date) > P.EndDate THEN DATEADD(WEEK,2,P.EndDate) ELSE DATEADD(WEEK,2,DATEADD(MONTH,3,C.Date)-1) END,
-	       2,0,NULL,NULL,NULL,P.MilestonePersonId,  CASE WHEN DATEADD(MONTH,3,C.Date) > P.EndDate THEN P.EndDate ELSE DATEADD(MONTH,3,C.Date)-1 END
-	FROM FinalRanges p
-	JOIN dbo.MilestonePerson MP ON MP.MilestonePersonId = p.MilestonePersonId
-	JOIN dbo.Milestone M ON M.MilestoneId = MP.MilestoneId
-	JOIN Calendar C ON (DATEDIFF(MONTH, P.StartDate, C.[Date]) % 3 = 0 ) AND DATEPART(DD,P.StartDate) = DATEPART(DD,C.[Date]) and C.[Date] BETWEEN P.StartDate AND P.EndDate
-	ORDER BY MilestonePersonId
-    OPTION (MAXRECURSION 2500);
+	SELECT @PersonId = PersonId FROM MilestonePerson WHERE MilestonePersonId = @MilestonePersonId
 
+	DECLARE @Temp TABLE(PersonId INT,StartDate DATETIME,EndDate DATETIME,Hours REAL,HoursInRange REAL,GroupBy INT,IsGap BIT)
+
+	DELETE PF
+	FROM dbo.ProjectFeedback PF
+	INNER JOIN dbo.MilestonePerson MP ON MP.PersonId = PF.PersonId
+	INNER JOIN dbo.Milestone M ON M.MilestoneId = MP.MilestoneId 
+	WHERE (@MilestonePersonId IS NULL OR MP.MilestonePersonId = @MilestonePersonId)
+		  AND (@MilestoneId IS NULL OR PF.ProjectId = @ProjectIdLocal)
+		  AND (@ProjectId  IS NULL OR PF.ProjectId = @ProjectId)
+		  AND (PF.FeedbackStatusId <> 1 AND PF.IsCanceled = 0)
+
+		Declare @PersonIdC INT
+		Declare @NextPersonId INT
+		Declare @startDate DATETIME
+		Declare @TempStartDate DATETIME
+		Declare @beforestopDate DATETIME
+		Declare @stopDate DATETIME
+		Declare @SummedHours	real
+		Declare @Hours	real
+		DECLARE @ChangeStartDateFlag BIT=0
+		DECLARE @HoursInRange REAL
+		DECLARE @IsGap	BIT=0
+		DECLARE @Groupby INT=0
+		DECLARE cursor_temp CURSOR -- Declare and initialise cursor
+
+		LOCAL SCROLL STATIC
+		FOR
+		 WITH MilestoneRecordsContinuity
+			AS
+			(   
+				SELECT distinct Per.PersonId,m.ProjectId,dbo.GreaterDateBetweenTwo(mpe.StartDate,pay.StartDate) AS StartDate,dbo.SmallerDateBetweenTwo(mpe.EndDate,pay.EndDateOrig-1) AS EndDate--,ROW_NUMBER() over(partition by Per.PersonId,m.ProjectId order by mpe.StartDate) as RNo
+				FROM dbo.Person Per
+				JOIN MilestonePerson mp on mp.Personid = Per.PersonId
+				JOIN Milestone m on m.MilestoneId = mp.MilestoneId
+				JOIN MilestonePersonEntry mpe ON mpe.MilestonePersonId = mp.MilestonePersonId
+				JOIN dbo.Project P ON P.ProjectId = M.ProjectId
+				INNER JOIN dbo.Title T ON T.TitleId = Per.TitleId
+				INNER JOIN v_Pay pay ON pay.PersonId = Per.PersonId AND MPE.StartDate <= pay.EndDateOrig-1 AND pay.StartDate <= MPE.EndDate
+				AND (@MilestonePersonId IS NULL OR (mp.personid = @PersonId and m.ProjectId = @ProjectIdLocal))
+				AND (@MilestoneId IS NULL OR M.ProjectId = @ProjectIdLocal)
+				AND (@ProjectId  IS NULL OR M.ProjectId = @ProjectId)
+				AND Per.IsStrawman = 0
+				AND T.Title NOT IN ('Senior Director','Client Director','Practice Director')
+				AND pay.Timescale IN (1,2,3,4) -- 
+				AND M.ProjectId <> 174
+				AND P.IsAllowedToShow = 1
+			)
+			,
+			PersonHoursByDay
+			AS
+			(
+				select MRC.PersonId,MRC.ProjectId,MPS.Date,SUM(MPS.HoursPerDay) AS Hours from MilestoneRecordsContinuity as mrc
+				join v_MilestonePersonSchedule as mps on mps.ProjectId = mrc.ProjectId and mps.PersonId = mrc.PersonId
+				where mps.Date BETWEEN mrc.StartDate AND mrc.EndDate
+				GROUP BY MRC.PersonId,MRC.ProjectId,MPS.Date
+			)
+			SELECT PHD.PersonId,PHD.Date,PHD.Hours
+			FROM PersonHoursByDay PHD
+			LEFT JOIN dbo.ProjectFeedback PF ON PF.PersonId = PHD.PersonId AND PF.ProjectId = PHD.ProjectId AND PHD.Date BETWEEN PF.ReviewPeriodStartDate AND PF.ReviewPeriodEndDate AND (PF.FeedbackStatusId = 1 OR PF.IsCanceled = 1)
+			WHERE PF.FeedbackId IS NULL
+			ORDER BY PHD.PersonId,PHD.Date
+
+		OPEN cursor_temp
+
+		FETCH NEXT FROM cursor_temp INTO
+		@PersonIdC, @startDate,@SummedHours
+
+		SET @beforestopDate = @startDate
+		SET @TempStartDate = @startDate
+		SET @HoursInRange = @SummedHours
+
+		 FETCH NEXT FROM cursor_temp INTO
+		@NextPersonId,@stopDate,@Hours
+
+		 WHILE @@Fetch_status = 0 --SUCCESS
+		 BEGIN
+
+		 IF @PersonIdC = @NextPersonId
+		 BEGIN
+     
+			 IF DATEDIFF(DAY,@beforestopDate,@stopDate) >= 42 
+			 BEGIN
+
+				INSERT INTO @Temp
+				SELECT @PersonIdC,@startDate,@beforestopDate,@SummedHours,@HoursInRange,@Groupby,@IsGap
+				SET @PersonIdC = @NextPersonId
+				SET @startDate = @stopDate
+				SET @beforestopDate = @stopDate
+				SET @SummedHours = @Hours
+				SET @HoursInRange = @Hours
+				SET @IsGap = 0
+				SET @Groupby = @Groupby+1
+				FETCH NEXT FROM cursor_temp INTO
+				 @NextPersonId,@stopDate,@Hours
+			 END
+			 ELSE
+			 BEGIN
+
+				 IF(@IsGap = 0) AND DATEDIFF(DAY,@beforestopDate,@stopDate) > 1
+				 BEGIN
+					SET @IsGap = 1
+				 END
+
+				IF(FLOOR(@SummedHours/520) <> FLOOR((@SummedHours+@Hours)/520))
+				BEGIN
+					SET @ChangeStartDateFlag= 1
+					IF @SummedHours+@Hours >= 520 AND @HoursInRange+@Hours >= 240
+					BEGIN
+						SET @Groupby = @Groupby+1
+					END
+					INSERT INTO @Temp
+					SELECT @PersonIdC,@TempStartDate,@stopDate,@SummedHours+@Hours,@HoursInRange+@Hours,@Groupby,@IsGap
+					SET @HoursInRange = 0
+					SET @IsGap = 0
+				END
+				SET @beforestopDate = @stopDate
+				SET @SummedHours = @SummedHours+@Hours
+				SET @HoursInRange = @HoursInRange+@Hours
+				FETCH NEXT FROM cursor_temp INTO
+				 @NextPersonId,@stopDate,@Hours
+				IF(@ChangeStartDateFlag = 1)
+				BEGIN
+					SET @TempStartDate = @stopDate
+					SET @startDate = @stopDate
+					SET @ChangeStartDateFlag = 0
+				END
+			 END
+
+		 END
+		 ELSE
+		 BEGIN
+			IF @SummedHours >= 520 AND @HoursInRange >= 240
+			BEGIN
+				 SET @Groupby = @Groupby+1
+			END
+			INSERT INTO @Temp
+			SELECT @PersonIdC,@TempStartDate,@beforestopDate,@SummedHours,@HoursInRange,@Groupby,@IsGap
+
+			SET @Groupby = @Groupby+1 
+			SET @PersonIdC = @NextPersonId
+			SET @startDate = @stopDate
+			SET @TempStartDate = @stopDate
+			SET @beforestopDate = @stopDate
+			SET @SummedHours = @Hours
+			SET @HoursInRange = @Hours
+			SET @IsGap = 0
+				FETCH NEXT FROM cursor_temp INTO
+					@NextPersonId,@stopDate,@Hours
+
+		 END
+		 End
+		IF @SummedHours >= 520 AND @HoursInRange >= 240
+		BEGIN
+			SET @Groupby = @Groupby+1
+		END 
+		INSERT INTO @Temp
+		SELECT @PersonIdC,@startDate,@beforestopDate,@SummedHours,@HoursInRange,@Groupby,@IsGap
+
+		CLOSE cursor_temp
+		DEALLOCATE cursor_temp
+
+		INSERT INTO dbo.ProjectFeedback(ProjectId,PersonId,ReviewPeriodStartDate,ReviewPeriodEndDate,DueDate,FeedbackStatusId,IsCanceled,CompletionCertificateBy,CompletionCertificateDate,CancelationReason,NextIntialMailSendDate,IsGap)
+		SELECT CASE WHEN @ProjectId IS NULL THEN @ProjectIdLocal ELSE @ProjectId END,
+			   Max(PersonId),
+			   MIN(StartDate),
+			   MAX(EndDate), 
+			   DATEADD(WEEK,2,MAX(EndDate)),
+			   2,
+			   0,
+			   NULL,
+			   NULL,
+			   NULL,
+			   MAX(EndDate),
+			   MAX(CAST(IsGap  AS INT)) 
+		FROM @Temp 
+		WHERE Hours >= 240
+		GROUP BY GroupBy
 END
 
