97799e64e119f59dd7e6b81e79cc0ba5f73ca6d7
SainathC <SainathC>
2015-08-27 05:20:00 -0500

    Comments - NPOI version update changes.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/Utils/RecruitingMetricsReportExcelUtil.cs
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/Utils/RecruitingMetricsReportExcelUtil.cs
@@ -37,7 +37,7 @@ namespace PraticeManagement.Utils
             {
                 CellStyles cellStyle = new CellStyles();
                 cellStyle.IsBold = true;
-                cellStyle.BorderStyle = NPOI.SS.UserModel.BorderStyle.NONE;
+                cellStyle.BorderStyle = NPOI.SS.UserModel.BorderStyle.None;
                 cellStyle.FontHeight = 350;
                 CellStyles[] cellStylearray = { cellStyle };
                 RowStyles headerrowStyle = new RowStyles(cellStylearray);
@@ -63,7 +63,7 @@ namespace PraticeManagement.Utils
             {
                 CellStyles headerCellStyle = new CellStyles();
                 headerCellStyle.IsBold = true;
-                headerCellStyle.HorizontalAlignment = NPOI.SS.UserModel.HorizontalAlignment.CENTER;
+                headerCellStyle.HorizontalAlignment = NPOI.SS.UserModel.HorizontalAlignment.Center;
                 List<CellStyles> headerCellStyleList = new List<CellStyles>();
                 headerCellStyleList.Add(headerCellStyle);
                 RowStyles headerrowStyle = new RowStyles(headerCellStyleList.ToArray());
