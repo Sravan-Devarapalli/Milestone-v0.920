91f1418afcf520ce3ca1996503ce592373d6d9ef
SainathC <SainathC>
2015-09-10 07:01:00 -0500

    Comments - Optimized the sproc to reduce the load time for time entry page.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/PersonsListHavingActiveStatusDuringThisPeriod.proc.sql
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/PersonsListHavingActiveStatusDuringThisPeriod.proc.sql
@@ -4,21 +4,25 @@
       @EndDate DATETIME 
     )
 AS 
-    BEGIN
-		DECLARE @FutureDate DATETIME
+BEGIN
+		DECLARE @FutureDate DATETIME,
+			    @StartDateLocal	DATETIME,
+				@EndDateLocal	DATETIME
 		SELECT @FutureDate = dbo.GetFutureDate()
+		SET	   @StartDateLocal = @StartDate
+		SET	   @EndDateLocal = @EndDate
 
-        SELECT DISTINCT p.PersonId ,
-                per.FirstName ,
-                per.LastName 
-        FROM    dbo.v_PersonHistory AS p
-                INNER JOIN dbo.PersonStatusHistory PSH ON PSH.PersonId = p.PersonId
+		SELECT DISTINCT p.PersonId ,
+				per.FirstName ,
+				per.LastName 
+		FROM    dbo.v_PersonHistory AS p
+				INNER JOIN dbo.PersonStatusHistory PSH ON PSH.PersonId = p.PersonId
 				INNER JOIN dbo.Person per ON per.PersonId = P.PersonId
-        WHERE   PSH.PersonStatusId IN (1,5) -- ACTIVE Status
-                AND P.HireDate  <= @EndDate AND ISNULL(P.TerminationDate,@FutureDate)  >= @StartDate 
+		WHERE   PSH.PersonStatusId IN (1,5) -- ACTIVE Status
+				AND P.HireDate  <= @EndDate AND ISNULL(P.TerminationDate,@FutureDate)  >= @StartDate 
 				AND PSH.StartDate <= @EndDate AND ISNULL(PSH.EndDate,@FutureDate)  >= @StartDate
-        ORDER BY per.LastName ,
-                 per.FirstName
-
-    END
+		ORDER BY per.LastName ,
+					per.FirstName
+		OPTION (RECOMPILE);
+END
 
