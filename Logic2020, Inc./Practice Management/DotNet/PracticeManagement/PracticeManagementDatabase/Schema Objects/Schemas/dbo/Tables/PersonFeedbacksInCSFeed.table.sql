3de90b4f61b24472fc6bcee608c2624e16a342da
SainathC <SainathC>
2015-09-18 02:22:00 -0500

    Comments - Defect Number: 3316; Defect RecordID: 3312; Server Address: localhost; Server Port: 99; Database Name: PM1; Defect Summary: E00053 - Project Review Feedback Triggering to CM; Defect Description: Attached the estimates document and requirements.

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/PersonFeedbacksInCSFeed.table.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[PersonFeedbacksInCSFeed]
+(
+	Id			INT		IDENTITY(1,1) NOT NULL,
+	PersonId	INT		NOT NULL,
+	ReviewStartDate	DATETIME NOT NULL,
+	ReviewEndDate	DATETIME NOT NULL,
+	ProjectId	INT		NOT NULL,
+	Count		INT		NOT NULL
+	CONSTRAINT PK_PersonFeedbacksInCSFeed PRIMARY KEY (Id)
+)
+
