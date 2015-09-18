9623233ada4e6132b3454a748cccd2daafa96623
SainathC <SainathC>
2015-09-18 02:22:00 -0500

    Comments - Defect Number: 3316; Defect RecordID: 3312; Server Address: localhost; Server Port: 99; Database Name: PM1; Defect Summary: E00053 - Project Review Feedback Triggering to CM; Defect Description: Attached the estimates document and requirements.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/PersonDivision.table.sql
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/PersonDivision.table.sql
@@ -2,5 +2,7 @@
 (
 	[DivisionId]	INT IDENTITY(1,1) NOT NULL,
 	[DivisionName]	NVARCHAR(100) NOT NULL,
+	[PracticeDirectorId]	INT	NULL
 	CONSTRAINT [PK_PersonDivision_DivisionId]	PRIMARY KEY ([DivisionId])
 )
+
