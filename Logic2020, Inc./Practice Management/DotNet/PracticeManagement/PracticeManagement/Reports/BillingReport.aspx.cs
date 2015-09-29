d391bd7196807c6f9b3727cc02400a5a6f7f40f3
SainathC <SainathC>
2015-09-29 02:20:00 -0500

    Comments - Defect Number: 3318; Defect RecordID: 3314; Server Address: localhost; Server Port: 99; Database Name: PM1; Defect Summary: PM-New change requests; Defect Description: On Sat, Sep 19, 2015 at 4:46 AM, Kris Rushing Raines <KrisR@logic2020.com> wrote: Hi Harshitha,
    We have a number of what we think should be small/simple changes. Please see below.
    
    1. Utilization Report  mock ups in the attachment
    a. E00006
     i. On the Utilization reports: add a check box to report filter titled Exclude Investment Resources
     ii. In person detail: add a check box titled  Investment Resource
     iii. On the Utilization reports: if check box is checked, do not include investment resources on the report
    @@
    b. E00010
     i. Add project number and name in the color bars.
    c. E00030
     i. Add Division as a filter option in the report parameters
    
    2. All reports
    a. First names = Preferred First Name
     i. If the Preferred First Name field is empty use the Legal First Name field
    
    3. New Hire Email
    a. Add the following fields:
     i. Offshore status  Y/N
     ii. Career Manager
     iii. Division
    
    Please let me know if you have any questions.
    
    Regards,
    Kris Rushing Raines, Consulting Operations // Logic20/20 //krisr@logic2020.com
    t:206.576.0423// www.logic2020.com/

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/Reports/BillingReport.aspx.cs
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/Reports/BillingReport.aspx.cs
@@ -214,7 +214,7 @@ namespace PraticeManagement.Reports
 
         private void FillInitDirectorsList()
         {
-            DataHelper.FillDirectorsList(cblDirector, "All Executives in Charge", null);
+            DataHelper.FillDirectorsList(cblDirector, "All Executives in Charge", null,true);
             cblDirector.SelectAll();
         }
 
