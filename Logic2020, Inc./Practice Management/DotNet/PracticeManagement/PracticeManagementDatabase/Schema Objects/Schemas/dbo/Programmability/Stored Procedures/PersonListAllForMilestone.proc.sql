70d7d9fbf252c2afc12abb3883cf33d3929ff37b
SainathC <SainathC>
2015-09-30 09:04:00 -0500

    Comments - Included people with the combination of Division = Operations and Practice Area = Information Technology in milestone>resources drop down as per Nick statement on 09/29/2015.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/PersonListAllForMilestone.proc.sql
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/PersonListAllForMilestone.proc.sql
@@ -65,5 +65,22 @@ AS
 	LEFT JOIN dbo.aspnet_Roles ur
 	ON ur.RoleId = uir.RoleId
 	WHERE p.PersonStatusId IN (1,5) AND ur.RoleName IN ('Client Director','Practice Area Manager')
+
+	UNION
+	
+	SELECT	P.PersonId,
+			P.FirstName,
+			P.LastName,
+			P.IsDefaultManager,
+			P.HireDate,
+			P.IsStrawman AS IsStrawman
+	FROM dbo.Person P
+	INNER JOIN v_PersonHistoryAndStrawman AS PH ON PH.PersonId = p.PersonId
+	LEFT JOIN dbo.Practice AS Pra ON Pra.PracticeId = p.DefaultPractice
+	LEFT JOIN dbo.PersonDivision PD ON PD.DivisionId = P.DivisionId
+	WHERE	PH.HireDate <= @EndDate AND (PH.TerminationDate IS NULL OR @StartDate <= PH.TerminationDate) AND
+			P.IsStrawman = 0 AND
+			Pra.Name = 'Information Technology' AND 
+			PD.DivisionName = 'Operations'
 	ORDER BY 3,2
 
