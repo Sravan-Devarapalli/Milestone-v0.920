35c7498b112d5714e39689249b4261f7952e5e87
SainathC <SainathC>
2015-10-07 01:42:00 -0500

    none

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Views/v_CurrentMSBadge.view.sql
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Views/v_CurrentMSBadge.view.sql
@@ -1,22 +1,5 @@
 ﻿CREATE VIEW [dbo].[v_CurrentMSBadge]
 	AS 
-	--With HistoryCTE
-	--AS
-	--(
-	--	SELECT *,DATEADD(mm,6,ProjectPlannedEndDate) as ReportRunMax,ROW_NUMBER() OVER(PARTITION BY PersonId ORDER BY badgestartdate) RNum
-	--	FROM dbo.BadgeHistoryForReports
-	--),
-	--HistoryWithRunDates
-	--AS
-	--(
-	--	SELECT c1.PersonId,c1.BadgeStartDate,c1.BadgeEndDate,c1.ProjectPlannedEndDate,c1.BreakStartDate,c1.BreakEndDate,CASE WHEN c1.rnum = 1 THEN NULL ELSE (SELECT ReportRunMax+1 FROM HistoryCTE c2 WHERE c2.PersonId = c1.PersonId and c2.RNum = c1.RNum-1) END AS ReportRunMin,
-	--	       CASE WHEN c1.rnum =(SELECT MAX(C3.RNUM) FROM HistoryCTE c3 WHERE c3.PersonId = c1.PersonId GROUP BY c3.PersonId) THEN NULL ELSE ReportRunMax END AS ReportRunMax 
-	--	FROM HistoryCTE c1
-	--)
-	--SELECT HR.*,M.BlockStartDate,M.BlockEndDate,M.DeactivatedDate,M.ExcludeInReports
-	--FROM HistoryWithRunDates HR 
-	--INNER JOIN dbo.MSBadge M ON M.PersonId = HR.PersonId
-	--WHERE (HR.ReportRunMin is null or dbo.GettingPMTime(GETUTCDATE()) >= HR.ReportRunMin) and (HR.ReportRunMax is null or dbo.GettingPMTime(GETUTCDATE()) <= HR.ReportRunMax)
 	With HistoryCTE
 	AS
 	(
