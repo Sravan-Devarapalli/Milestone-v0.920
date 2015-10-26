36d4111465a290f20c61d234870049125ea536df
SainathC <SainathC>
2015-10-26 02:13:00 -0500

    Comments - No Margin Issue Changes.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/MilestonePersonEntriesWithFinancialsByMilestoneId.proc.sql
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/MilestonePersonEntriesWithFinancialsByMilestoneId.proc.sql
@@ -77,6 +77,7 @@ BEGIN
 	SELECT  f.EntryId,
 			f.PersonId,
 			ISNULL(SUM(f.PersonMilestoneDailyAmount), 0) AS Revenue,
+			ISNULL(SUM(f.PersonMilestoneDailyAmount - f.PersonDiscountDailyAmount),0) AS RevenueNet,
 			ISNULL(SUM(f.PersonMilestoneDailyAmount - f.PersonDiscountDailyAmount -
 						(CASE WHEN f.SLHR >= f.PayRate + f.MLFOverheadRate 
 							  THEN f.SLHR 
