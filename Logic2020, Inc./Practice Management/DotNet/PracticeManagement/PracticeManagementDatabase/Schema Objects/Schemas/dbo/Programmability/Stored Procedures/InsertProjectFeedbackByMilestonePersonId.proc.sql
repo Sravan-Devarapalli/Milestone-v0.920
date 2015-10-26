12168a554a3414b254b1d9eda717a048ab69b599
SainathC <SainathC>
2015-10-26 02:28:00 -0500

    Comments - Defect Number: 3293; Defect RecordID: 3289; Server Address: localhost; Server Port: 99; Database Name: PM1; Defect Summary: Changes in Feedback functionality; Defect Description: Attached are some scenarios in connection with Project review reporting. We can discuss on the call today.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/InsertProjectFeedbackByMilestonePersonId.proc.sql
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/InsertProjectFeedbackByMilestonePersonId.proc.sql
@@ -2,13 +2,14 @@
 (
 	@MilestonePersonId   INT=NULL,
 	@MilestoneId	INT=NULL,
-	@ProjectId		INT=NULL
+	@ProjectId		INT=NULL,
+	@PersonId		INT=NULL
 )
 AS
 BEGIN
 
 	DECLARE @ProjectIdLocal INT,
-	@PersonId INT
+	@PersonIdLocal INT
 
 	SELECT @ProjectIdLocal = M.ProjectId 
 	FROM Milestone M
@@ -16,23 +17,26 @@ BEGIN
 	WHERE (@MilestonePersonId IS NULL OR MP.MilestonePersonId = @MilestonePersonId)
 		  AND (@MilestoneId IS NULL OR M.MilestoneId = @MilestoneId)
 	
-	SELECT @PersonId = PersonId FROM MilestonePerson WHERE MilestonePersonId = @MilestonePersonId
+	SELECT @PersonIdLocal = PersonId FROM MilestonePerson WHERE MilestonePersonId = @MilestonePersonId
 
-	DECLARE @Temp TABLE(PersonId INT,StartDate DATETIME,EndDate DATETIME,Hours REAL,HoursInRange REAL,GroupBy INT,IsGap BIT)
+	DECLARE @Temp TABLE(PersonId INT,ProjectId INT,StartDate DATETIME,EndDate DATETIME,Hours REAL,HoursInRange REAL,GroupBy INT,IsGap BIT)
 
 	DELETE PF
 	FROM dbo.ProjectFeedback PF
-	INNER JOIN dbo.MilestonePerson MP ON MP.PersonId = PF.PersonId
-	INNER JOIN dbo.Milestone M ON M.MilestoneId = MP.MilestoneId 
-	WHERE (@MilestonePersonId IS NULL OR (PF.personid = @PersonId and PF.ProjectId = @ProjectIdLocal))
+	LEFT JOIN dbo.MilestonePerson MP ON MP.PersonId = PF.PersonId
+	LEFT JOIN dbo.Milestone M ON M.MilestoneId = MP.MilestoneId 
+	WHERE (@MilestonePersonId IS NULL OR (PF.personid = @PersonIdLocal and PF.ProjectId = @ProjectIdLocal))
 		  AND (@MilestoneId IS NULL OR PF.ProjectId = @ProjectIdLocal)
 		  AND (@ProjectId  IS NULL OR PF.ProjectId = @ProjectId)
+		  AND (@PersonId IS NULL OR PF.PersonId = @PersonId)
 		  AND (PF.FeedbackStatusId <> 1 AND PF.IsCanceled = 0)
 
 	EXEC [dbo].[InsertFeedbackByWeekWise] @MilestonePersonId = @MilestonePersonId, @MilestoneId =@MilestoneId, @ProjectId = @ProjectId
 
 		Declare @PersonIdC INT
+		Declare @ProjectIdC INT
 		Declare @NextPersonId INT
+		Declare @NextProjectId INT
 		Declare @startDate DATETIME
 		Declare @TempStartDate DATETIME
 		Declare @beforestopDate DATETIME
@@ -58,9 +62,10 @@ BEGIN
 				JOIN dbo.Project P ON P.ProjectId = M.ProjectId
 				INNER JOIN dbo.Title T ON T.TitleId = Per.TitleId
 				INNER JOIN v_Pay pay ON pay.PersonId = Per.PersonId AND MPE.StartDate <= pay.EndDateOrig-1 AND pay.StartDate <= MPE.EndDate
-				AND (@MilestonePersonId IS NULL OR (mp.personid = @PersonId and m.ProjectId = @ProjectIdLocal))
+				AND (@MilestonePersonId IS NULL OR (mp.personid = @PersonIdLocal and m.ProjectId = @ProjectIdLocal))
 				AND (@MilestoneId IS NULL OR M.ProjectId = @ProjectIdLocal)
 				AND (@ProjectId  IS NULL OR M.ProjectId = @ProjectId)
+				AND (@PersonId IS NULL OR MP.PersonId = @PersonId)
 				AND Per.IsStrawman = 0
 				AND T.Title NOT IN ('Senior Director','Client Director','Practice Director')
 				AND pay.Timescale IN (1,2,3,4) -- AS PER E00053
@@ -76,114 +81,196 @@ BEGIN
 				where mps.Date BETWEEN mrc.StartDate AND mrc.EndDate
 				GROUP BY MRC.PersonId,MRC.ProjectId,MPS.Date
 			)
-			SELECT PHD.PersonId,PHD.Date,PHD.Hours
+			SELECT PHD.PersonId,PHD.ProjectId,PHD.Date,PHD.Hours
 			FROM PersonHoursByDay PHD
 			LEFT JOIN dbo.ProjectFeedback PF ON PF.PersonId = PHD.PersonId AND PF.ProjectId = PHD.ProjectId AND PHD.Date BETWEEN PF.ReviewPeriodStartDate AND PF.ReviewPeriodEndDate AND (PF.FeedbackStatusId = 1 OR PF.IsCanceled = 1)
 			WHERE PF.FeedbackId IS NULL
 			AND PHD.Date >= '20150101'
-			ORDER BY PHD.PersonId,PHD.Date
+			ORDER BY PHD.PersonId,PHD.ProjectId,PHD.Date
 
 		OPEN cursor_temp
 
 		FETCH NEXT FROM cursor_temp INTO
-		@PersonIdC, @startDate,@SummedHours
+		@PersonIdC, @ProjectIdC, @startDate,@SummedHours
 
 		SET @beforestopDate = @startDate
 		SET @TempStartDate = @startDate
 		SET @HoursInRange = @SummedHours
 
 		 FETCH NEXT FROM cursor_temp INTO
-		@NextPersonId,@stopDate,@Hours
+		@NextPersonId,@NextProjectId,@stopDate,@Hours
 
-		 WHILE @@Fetch_status = 0 --SUCCESS
-		 BEGIN
-
-		 IF @PersonIdC = @NextPersonId
-		 BEGIN
-     
-			 IF DATEDIFF(DAY,@beforestopDate,@stopDate) >= 42 
-			 BEGIN
-
-				INSERT INTO @Temp
-				SELECT @PersonIdC,@startDate,@beforestopDate,@SummedHours,@HoursInRange,@Groupby,@IsGap
-				SET @PersonIdC = @NextPersonId
-				SET @startDate = @stopDate
-				SET @beforestopDate = @stopDate
-				SET @SummedHours = @Hours
-				SET @HoursInRange = @Hours
-				SET @IsGap = 0
-				SET @Groupby = @Groupby+1
-				FETCH NEXT FROM cursor_temp INTO
-				 @NextPersonId,@stopDate,@Hours
-			 END
-			 ELSE
-			 BEGIN
-
-				 IF(@IsGap = 0) AND DATEDIFF(DAY,@beforestopDate,@stopDate) > 1
-				 BEGIN
-					SET @IsGap = 1
-				 END
-
-				IF(FLOOR(@SummedHours/520) <> FLOOR((@SummedHours+@Hours)/520))
-				BEGIN
-					SET @ChangeStartDateFlag= 1
-					IF @SummedHours+@Hours >= 520 AND @HoursInRange+@Hours >= 240
-					BEGIN
-						SET @Groupby = @Groupby+1
-					END
-					INSERT INTO @Temp
-					SELECT @PersonIdC,@TempStartDate,@stopDate,@SummedHours+@Hours,@HoursInRange+@Hours,@Groupby,@IsGap
-					SET @HoursInRange = 0
-					SET @IsGap = 0
-				END
-				SET @beforestopDate = @stopDate
-				SET @SummedHours = @SummedHours+@Hours
-				SET @HoursInRange = @HoursInRange+@Hours
-				FETCH NEXT FROM cursor_temp INTO
-				 @NextPersonId,@stopDate,@Hours
-				IF(@ChangeStartDateFlag = 1)
-				BEGIN
-					SET @TempStartDate = @stopDate
-					SET @startDate = @stopDate
-					SET @ChangeStartDateFlag = 0
-				END
-			 END
-
-		 END
-		 ELSE
-		 BEGIN
-			IF @SummedHours >= 520 AND @HoursInRange >= 240
-			BEGIN
-				 SET @Groupby = @Groupby+1
-			END
+WHILE @@Fetch_status = 0 --SUCCESS
+BEGIN
+    IF @ProjectIdC = @NextProjectId
+	BEGIN
+		IF @PersonIdC = @NextPersonId
+		BEGIN
+		IF DATEDIFF(DAY, @beforestopDate, @stopDate) >= 42
+		BEGIN
 			INSERT INTO @Temp
-			SELECT @PersonIdC,@TempStartDate,@beforestopDate,@SummedHours,@HoursInRange,@Groupby,@IsGap
+			SELECT @PersonIdC
+				,@ProjectIdC
+				,@startDate
+				,@beforestopDate
+				,@SummedHours
+				,@HoursInRange
+				,@Groupby
+				,@IsGap
 
-			SET @Groupby = @Groupby+1 
 			SET @PersonIdC = @NextPersonId
+			SET @ProjectIdC = @NextProjectId
 			SET @startDate = @stopDate
-			SET @TempStartDate = @stopDate
 			SET @beforestopDate = @stopDate
 			SET @SummedHours = @Hours
 			SET @HoursInRange = @Hours
 			SET @IsGap = 0
-				FETCH NEXT FROM cursor_temp INTO
-					@NextPersonId,@stopDate,@Hours
+			SET @Groupby = @Groupby + 1
 
-		 END
-		 End
-		IF @SummedHours >= 520 AND @HoursInRange >= 240
+			FETCH NEXT
+			FROM cursor_temp
+			INTO @NextPersonId
+				,@NextProjectId
+				,@stopDate
+				,@Hours
+		END
+		ELSE
 		BEGIN
-			SET @Groupby = @Groupby+1
-		END 
+			IF (@IsGap = 0)
+				AND DATEDIFF(DAY, @beforestopDate, @stopDate) > 1
+			BEGIN
+				SET @IsGap = 1
+			END
+
+			IF (FLOOR(@SummedHours / 520) <> FLOOR((@SummedHours + @Hours) / 520))
+			BEGIN
+				SET @ChangeStartDateFlag = 1
+
+				IF @SummedHours + @Hours >= 520
+					AND @HoursInRange + @Hours >= 240
+				BEGIN
+					SET @Groupby = @Groupby + 1
+				END
+
+				INSERT INTO @Temp
+				SELECT @PersonIdC
+				    ,@ProjectIdC
+					,@TempStartDate
+					,@stopDate
+					,@SummedHours + @Hours
+					,@HoursInRange + @Hours
+					,@Groupby
+					,@IsGap
+
+				SET @HoursInRange = 0
+				SET @IsGap = 0
+			END
+
+			SET @beforestopDate = @stopDate
+			SET @SummedHours = @SummedHours + @Hours
+			SET @HoursInRange = @HoursInRange + @Hours
+
+			FETCH NEXT
+			FROM cursor_temp
+			INTO @NextPersonId
+				,@NextProjectId
+				,@stopDate
+				,@Hours
+
+			IF (@ChangeStartDateFlag = 1)
+			BEGIN
+				SET @TempStartDate = @stopDate
+				SET @startDate = @stopDate
+				SET @ChangeStartDateFlag = 0
+			END
+		END
+	END
+		ELSE
+		BEGIN
+		IF @SummedHours >= 520
+			AND @HoursInRange >= 240
+		BEGIN
+			SET @Groupby = @Groupby + 1
+		END
+
 		INSERT INTO @Temp
-		SELECT @PersonIdC,@startDate,@beforestopDate,@SummedHours,@HoursInRange,@Groupby,@IsGap
+		SELECT @PersonIdC
+		    ,@ProjectIdC
+			,@TempStartDate
+			,@beforestopDate
+			,@SummedHours
+			,@HoursInRange
+			,@Groupby
+			,@IsGap
+
+		SET @Groupby = @Groupby + 1
+		SET @PersonIdC = @NextPersonId
+		SET @ProjectIdC = @NextProjectId
+		SET @startDate = @stopDate
+		SET @TempStartDate = @stopDate
+		SET @beforestopDate = @stopDate
+		SET @SummedHours = @Hours
+		SET @HoursInRange = @Hours
+		SET @IsGap = 0
+
+		FETCH NEXT
+		FROM cursor_temp
+		INTO @NextPersonId
+			,@NextProjectId
+			,@stopDate
+			,@Hours
+	END
+	END
+	ELSE
+	BEGIN
+		IF @SummedHours >= 520
+			AND @HoursInRange >= 240
+		BEGIN
+			SET @Groupby = @Groupby + 1
+		END
+
+		INSERT INTO @Temp
+		SELECT @PersonIdC
+		    ,@ProjectIdC
+			,@TempStartDate
+			,@beforestopDate
+			,@SummedHours
+			,@HoursInRange
+			,@Groupby
+			,@IsGap
+
+		SET @Groupby = @Groupby + 1
+		SET @PersonIdC = @NextPersonId
+		SET @ProjectIdC = @NextProjectId
+		SET @startDate = @stopDate
+		SET @TempStartDate = @stopDate
+		SET @beforestopDate = @stopDate
+		SET @SummedHours = @Hours
+		SET @HoursInRange = @Hours
+		SET @IsGap = 0
+
+		FETCH NEXT
+		FROM cursor_temp
+		INTO @NextPersonId
+			,@NextProjectId
+			,@stopDate
+			,@Hours
+	END
+END
+
+IF @SummedHours >= 520
+	AND @HoursInRange >= 240
+BEGIN
+	SET @Groupby = @Groupby + 1
+END
+		INSERT INTO @Temp
+		SELECT @PersonIdC,@ProjectIdC,@startDate,@beforestopDate,@SummedHours,@HoursInRange,@Groupby,@IsGap
 
 		CLOSE cursor_temp
 		DEALLOCATE cursor_temp
 
 		INSERT INTO dbo.ProjectFeedback(ProjectId,PersonId,ReviewPeriodStartDate,ReviewPeriodEndDate,DueDate,FeedbackStatusId,IsCanceled,CompletionCertificateBy,CompletionCertificateDate,CancelationReason,NextIntialMailSendDate,IsGap)
-		SELECT CASE WHEN @ProjectId IS NULL THEN @ProjectIdLocal ELSE @ProjectId END,
+		SELECT MAX(ProjectId),
 			   Max(PersonId),
 			   MIN(StartDate),
 			   MAX(EndDate), 
