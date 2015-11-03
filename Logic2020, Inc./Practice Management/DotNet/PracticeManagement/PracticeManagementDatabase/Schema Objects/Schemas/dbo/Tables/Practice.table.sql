69437d0f9ba1bf48068f5b8412d6c856b0bc8d37
SainathC <SainathC>
2015-11-03 08:45:00 -0500

    Comments - Made offshore practice area and offshore resources to be not available in Utilization report.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/Practice.table.sql
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/Practice.table.sql
@@ -5,7 +5,8 @@
     [IsActive]          BIT           NOT NULL,
     [IsCompanyInternal] BIT           NOT NULL,
 	[Abbreviation]      NVARCHAR (100)  NULL,
-	IsNotesRequired     BIT   CONSTRAINT Df_Practice_IsNotesRequired DEFAULT (1) NOT NULL
+	IsNotesRequired     BIT   CONSTRAINT Df_Practice_IsNotesRequired DEFAULT (1) NOT NULL,
+	[ShowInUtilizationReport]	BIT NOT NULL CONSTRAINT DF_Practice_ShowInUtilizationReport DEFAULT(1)
 );
 
 
