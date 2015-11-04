33258baebc23b46a2d9a8b6b8c4bb5f3a5c6a271
SainathC <SainathC>
2015-11-04 05:04:00 -0500

    Comments - Remove 'Consulting' division from divisions dropdown in person detail page.

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/DataTransferObjects/PersonDivision.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+
+namespace DataTransferObjects
+{
+    [DataContract]
+    [Serializable]
+    public class PersonDivision
+    {
+        [DataMember]
+        public int DivisionId { get; set; }
+
+        [DataMember]
+        public string DivisionName { get; set; }
+
+        [DataMember]
+        public bool Inactive { get; set; }
+    }
+}
+
