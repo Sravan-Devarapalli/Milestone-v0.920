c8c00aaa17fd85d40538f22e935c958c2888b9b2
SainathC <SainathC>
2015-11-04 05:05:00 -0500

    Comments - Remove 'Consulting' division from divisions dropdown in person detail page.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/PersonDivision.table.sql
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Tables/PersonDivision.table.sql
@@ -2,7 +2,8 @@
 (
 	[DivisionId]	INT IDENTITY(1,1) NOT NULL,
 	[DivisionName]	NVARCHAR(100) NOT NULL,
-	[PracticeDirectorId]	INT	NULL
+	[PracticeDirectorId]	INT	NULL,
+	[Inactive] BIT NOT NULL CONSTRAINT DF_PersonDivision_Inactive DEFAULT(0)
 	CONSTRAINT [PK_PersonDivision_DivisionId]	PRIMARY KEY ([DivisionId])
 )
 
