fe033ac237b6bbe6901af2dee046023ba20be16c
SainathC <SainathC>
2015-11-18 04:31:00 -0500

    Comments - Defect Number: 3320; Defect RecordID: 3316; Server Address: localhost; Server Port: 99; Database Name: PM1; Defect Summary: E00060-18 mos management meeting report; Defect Description:

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/Controls/Persons/MSBadge.ascx.designer.cs
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/Controls/Persons/MSBadge.ascx.designer.cs
@@ -454,6 +454,15 @@ namespace PraticeManagement.Controls.Persons {
         protected global::System.Web.UI.WebControls.CustomValidator cvExceptionStartAfterJuly;
         
         /// <summary>
+        /// chbManageServiceContract control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.CheckBox chbManageServiceContract;
+        
+        /// <summary>
         /// dtpBlockEnd control.
         /// </summary>
         /// <remarks>
