3198f08d2627d25116a012dbddf927a0403a86c8
SainathC <SainathC>
2015-12-10 01:31:00 -0500

    Comments - Defect Number: 3332; Defect RecordID: 3328; Server Address: localhost; Server Port: 99; Database Name: PM1; Defect Summary: 'The date range description is not being shown on the top left of the Project feedback report' task. ; Defect Description: These can move to prodon Thursday(tomorrow). Apologies for the delayed response.
    -Nick
    
    Nick Callahan, IT Manager //Logic20/20//nickc@logic2020.com
    t.206-576-0409//www.logic2020.com
    
    From:Harshitha Tenneti [mailto:harshithat@ggktech.com]
    Sent:Monday, December 7, 2015 3:58 AM
    To:Nick Callahan <nickc@logic2020.com>; Kris Rushing Raines <KrisR@logic2020.com>
    Cc:'Logic 20/20 Core Team' <logic2020@ggktech.com>
    Subject:Tasks for prod deployment-12/9/2015
    
    Hi Nick/Kris,
    
    We have following tasks in UAT now. Please let us know if we can deploy them to Production in our next release.
    'Showing&amp;insteadof &hovertextofdropdownvaluesinProjectdetailpage'issue.
    Replacing Paychex ID with ADP ID in person history.
    'The date range description is not being shown on the top left of theProjectfeedback report'task.
    --
    Thank you,
    Harshitha Tenneti|@GGKTech| Office: +91-40-67670404|www.ggktech.com|Delivering Commitments.@
    Tower 2.1, Level 13, Waverock building, APIIC IT/ITES SEZ, Gachibowli(Nanakramguda), Hyderabad  500008
    GGKTechnologiesrespects your privacy. This email is intended only for the use of the party to which it is addressed and may contain information that is privileged, confidential, or protected by law. If you have received this message in error, or do not want to receive any further emails from us, please notify us immediately by replying to the message and deleting it from your computer.
    
    In Project Feedback Report date range description is not available on top of the report.

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/Reports/ProjectFeedbackReport.aspx.designer.cs
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/Reports/ProjectFeedbackReport.aspx.designer.cs
@@ -238,6 +238,15 @@ namespace PraticeManagement.Reports {
         protected global::System.Web.UI.HtmlControls.HtmlGenericControl divWholePage;
         
         /// <summary>
+        /// lbRange control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Literal lbRange;
+        
+        /// <summary>
         /// ltrlCompleted control.
         /// </summary>
         /// <remarks>
