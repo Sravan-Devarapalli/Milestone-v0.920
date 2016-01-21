453e85e4d5a2c7b0e2413943f9afd2acb3b8454d
SainathC <SainathC>
2016-01-21 04:43:00 -0500

    Comments - Defect Number: 3335; Defect RecordID: 3331; Server Address: localhost; Server Port: 99; Database Name: PM1; Defect Summary: Do not [reset] each time one of the parameters is selected; Defect Description: When parameters available to select data for reports, do not clear selection after each time (i.e. leave atpriorselection). Additionally, do not [reset] each time one of the parameters is selected

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/SaveReportFilterValues.proc.sql
@@ -0,0 +1,18 @@
+﻿CREATE PROCEDURE [dbo].[SaveReportFilterValues]
+	@CurrentUserId INT,
+	@ReportId	   INT,
+	@ReportFilters NVARCHAR (MAX),
+	@PreviousUserId INT
+AS
+BEGIN
+	IF(NOT EXISTS(SELECT * FROM ReportFilterValues WHERE CurrentUserId=@CurrentUserId AND ReportId=@ReportId AND PreviousUserId=@PreviousUserId))
+		BEGIN 
+			INSERT INTO ReportFilterValues (Id,CurrentUserId,ReportId,ReportFilters,PreviousUserId) 
+			VALUES (NEWID(),@CurrentUserId,@ReportId,@ReportFilters,@PreviousUserId)
+		END
+	ELSE
+		BEGIN
+			UPDATE ReportFilterValues SET ReportFilters=@ReportFilters WHERE CurrentUserId=@CurrentUserId AND ReportId=@ReportId AND PreviousUserId=@PreviousUserId
+		END
+END
+
