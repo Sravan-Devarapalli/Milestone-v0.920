5bc4a4f259a1e912e44be1b1c77acea7fb00f2c0
SainathC <SainathC>
2016-01-21 04:43:00 -0500

    Comments - Defect Number: 3335; Defect RecordID: 3331; Server Address: localhost; Server Port: 99; Database Name: PM1; Defect Summary: Do not [reset] each time one of the parameters is selected; Defect Description: When parameters available to select data for reports, do not clear selection after each time (i.e. leave atpriorselection). Additionally, do not [reset] each time one of the parameters is selected

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagement/Utils/ReportsFilterHelper.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using DataTransferObjects;
+using PraticeManagement.Controls;
+using System.Web.UI;
+
+namespace PraticeManagement.Utils
+{
+    public class ReportsFilterHelper
+    {
+       
+        public static void SaveFilterValues(ReportName report, object filter)
+        {
+           
+            string filterData = SerializationHelper.SerializeBase64(filter);
+            ServiceCallers.Custom.Person(p => p.SaveReportFilterValues(PracticeManagementMain.CurrentUserID, (int)report, filterData, PracticeManagementMain.PreviousUserId));
+        }
+
+        public static object GetFilterValues(ReportName report)
+        {
+            string filterData = ServiceCallers.Custom.Person(p => p.GetReportFilterValues(PracticeManagementMain.CurrentUserID, (int)report, PracticeManagementMain.PreviousUserId));
+            if (!string.IsNullOrEmpty(filterData))
+            {
+                return SerializationHelper.DeserializeBase64(filterData);
+            }
+            return null;
+        }
+    }
+}
+
