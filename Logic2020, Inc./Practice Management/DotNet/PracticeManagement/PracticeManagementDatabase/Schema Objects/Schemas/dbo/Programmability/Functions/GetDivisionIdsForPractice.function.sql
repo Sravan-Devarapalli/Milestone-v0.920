0f6c929a8b93dcf72ff443df8b1cedd10290b0c4
SainathC <SainathC>
2016-02-18 02:54:00 -0500

    Comments - Defect Number: 3337; Defect RecordID: 3333; Server Address: localhost; Server Port: 99; Database Name: PM1; Defect Summary: Remove Unassigned from Career Manager drop down; Defect Description:

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Functions/GetDivisionIdsForPractice.function.sql
@@ -0,0 +1,17 @@
+﻿CREATE FUNCTION [dbo].[GetDivisionIdsForPractice]
+(
+	@PracticeId INT
+)
+RETURNS NVARCHAR(MAX)
+AS
+BEGIN
+  
+	DECLARE @Temp NVARCHAR(MAX) = ''
+  
+	SELECT @Temp = @Temp + CAST(DPA.DivisionId AS NVARCHAR(5))+','
+	FROM DivisionPracticeArea DPA
+	WHERE DPA.PracticeId=@PracticeId
+
+	RETURN @Temp
+END
+GO
