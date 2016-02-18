8e01e183d79d1acb472a9c5d67d13c7ba5cc3915
SainathC <SainathC>
2016-02-18 02:55:00 -0500

    Comments - Defect Number: 3337; Defect RecordID: 3333; Server Address: localhost; Server Port: 99; Database Name: PM1; Defect Summary: Remove Unassigned from Career Manager drop down; Defect Description:

--- Logic2020, Inc./Practice Management/DotNet/PracticeManagement/DataTransferObjects/PersonDivision.cs
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/DataTransferObjects/PersonDivision.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Runtime.Serialization;
+using System.Web;
 
 namespace DataTransferObjects
 {
@@ -11,13 +12,53 @@ namespace DataTransferObjects
     public class PersonDivision
     {
         [DataMember]
-        public int DivisionId { get; set; }
+        public int DivisionId
+        {
+            get;
+            set;
+        }
 
         [DataMember]
-        public string DivisionName { get; set; }
+        public string DivisionName
+        {
+            get;
+            set;
+        }
 
         [DataMember]
-        public bool Inactive { get; set; }
+        public bool Inactive
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public bool ShowCareerManagerLink
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public Person DivisionOwner
+        {
+            get;
+            set;
+        }
+
+        public int DivisionOwnerId
+        {
+            get { return DivisionOwner != null && DivisionOwner.Id.HasValue ? DivisionOwner.Id.Value : -1; }
+        
+        }
+
+        public string HtmlEncodedName
+        {
+            get
+            {
+                return HttpUtility.HtmlEncode(DivisionName);
+            }
+        }
     }
 }
 
