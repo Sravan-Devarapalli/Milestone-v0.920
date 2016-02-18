83a28dfa0d731529212cfcfb48daaccbc83679ad
SainathC <SainathC>
2016-02-18 02:54:00 -0500

    Comments - Defect Number: 3337; Defect RecordID: 3333; Server Address: localhost; Server Port: 99; Database Name: PM1; Defect Summary: Remove Unassigned from Career Manager drop down; Defect Description:

---
+++ Logic2020, Inc./Practice Management/DotNet/PracticeManagement/PracticeManagementDatabase/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/GetPersonDivisions.proc.sql
@@ -0,0 +1,13 @@
+﻿CREATE PROCEDURE [dbo].[GetPersonDivisions]
+AS
+BEGIN
+	SELECT pd.DivisionId,
+		   pd.DivisionName,
+		   pd.Inactive,
+		   pers.FirstName,
+		   pers.LastName,
+		   pers.PersonId
+		   FROM dbo.persondivision pd
+		   LEFT JOIN dbo.Person AS pers ON pd.DivisionOwnerId = pers.PersonId
+		   WHERE Inactive=0
+END 
